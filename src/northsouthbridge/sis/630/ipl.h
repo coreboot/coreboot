40e83c3b5e18194f54304db1273e28f1b60b0180
Ronald G. Minnich <rminnich@gmail.com>
2000-10-17 13:37:01 +0000

    reorg

---
+++ src/northsouthbridge/sis/630/ipl.h
@@ -0,0 +1,37 @@
+#define PCI_COMMAND_PORT	0xcf8
+#define PCI_DATA_PORT		0xcfc
+
+#define NORTH_BRIDGE_BASE_ADDR	0x80000000
+#define LPC_BRIDGE_BASE_ADDR	0x80000800
+
+#define SMB_BASE_ADDR		0x5080
+
+#define DOC_WIN_SEG		0xfe00
+#define DOC_STACK_SEG		0x0400
+#define SPL_RAM_SEG		0x8000
+
+#define DOC_SPL_START_BLK	2	/* 0,1 for IPL */
+#define DOC_SPL_SIZE_IN_PAGE	126     /* 1 page = 512 bytes, 256k */
+
+#define RET_LABEL(label)	\
+	jmp label##_done
+
+#define CALL_LABEL(label)	\
+	jmp label		;\
+label##_done:
+
+#define CALL_SP(func) \
+	lea	0f, %sp	; \
+	jmp func	; \
+0:
+
+#define RET_SP \
+	jmp *%sp
+
+#define CALL_BP(func) \
+	lea	0f, %bp	; \
+	jmp func	; \
+0:
+
+#define RET_BP \
+	jmp *%bp

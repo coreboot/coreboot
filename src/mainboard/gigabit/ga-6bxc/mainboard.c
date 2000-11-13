d496bf5e53a8b6c4920ff2348b346ba690aeacf4
Ronald G. Minnich <rminnich@gmail.com>
2000-11-13 18:15:00 +0000

    New files; these do result in a buildable l440gx+ linuxbios

---
+++ src/mainboard/gigabit/ga-6bxc/mainboard.c
@@ -0,0 +1,8 @@
+#include <printk.h>
+#include <pci.h>
+
+#include <cpu/p5/io.h>
+
+void mainboard_fixup()
+{
+}
---
+++ src/northbridge/intel/440gx/northbridge.c
@@ -0,0 +1,38 @@
+#include <pci.h>
+
+
+unsigned long sizeram()
+{
+	/*
+	 * This is written for BX but should work also for GX.
+	 */
+	unsigned long totalmem;
+	unsigned char banks;
+
+        struct pci_dev *pcidev;
+
+	/* pci_find_device is way overkill for the host bridge! 
+	 * Plus the BX & GX have different device numbers so it
+	 * prevents code sharing.
+	 */
+	pcidev = pci_find_slot(0, PCI_DEVFN(0,0));
+        pci_read_config_byte(pcidev, 0x67, &banks);
+
+	totalmem = (unsigned long) banks *8 * 1024;
+
+	if (banks == 0) {
+		totalmem = 0x80000000UL;
+	}
+
+	return totalmem;
+}
+
+
+#ifdef HAVE_FRAMEBUFFER
+
+void intel_framebuffer_on()
+{
+	outl(0x8000a004, 0xcf8);
+	outb(0x03, 0xcfc);
+}
+#endif

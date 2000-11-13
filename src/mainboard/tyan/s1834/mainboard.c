e8b24d3cdea6258668c0c82c6c4dd56c08594246
Ronald G. Minnich <rminnich@gmail.com>
2000-11-13 22:56:05 +0000

    For tyan s1834 and winbond superio ...

---
+++ src/mainboard/tyan/s1834/mainboard.c
@@ -0,0 +1,6 @@
+void
+mainboard_fixup()
+{
+  void southbridge_fixup(void);
+  southbridge_fixup();
+}
--- src/southbridge/via/vt82c686/southbridge.c
+++ src/southbridge/via/vt82c686/southbridge.c
@@ -50,6 +50,7 @@ void southbridge_fixup()
 	enables |= 0x80;
 	pcibios_write_config_byte(0, devfn, 0x81, enables);
 
+#ifndef DISABLE_SOUTHBRIDGE_COM_PORTS
 	// enable com1 and com2. 
 	enables = pcibios_read_config_byte(0, devfn, 0x83, &enables);
 	// 0x80 is enable com port b, 0x1 is to make it com2, 0x8 is enable com port a as com1
@@ -59,6 +60,7 @@ void southbridge_fixup()
 	// set com1 to 115 kbaud
 	// not clear how to do this yet. 
 	// forget it; done in assembly. 
+#endif
 	// enable IDE, since Linux won't do it.
 	// First do some more things to devfn (7,0)
 	// note: this should already be cleared, according to the book. 

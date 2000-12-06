ba4403a177435f5bd9f897d2154ae2456714c15a
Li-Ta Lo <ollie@lanl.gov>
2000-12-06 03:58:57 +0000

    finally, a fullproof implementation for MTRR setting

--- src/cpu/p6/mtrr.c
+++ src/cpu/p6/mtrr.c
@@ -156,15 +156,10 @@ void intel_set_var_mtrr(unsigned int reg, unsigned long base, unsigned long size
 
 }
 
-
-/*
- * fms: find most sigificant bit set.
- *
- * stolen from Linux Kernel Source
- */
+/* fms: find most sigificant bit set, stolen from Linux Kernel Source. */
 static __inline__ int fms(int x)
 {
- 	int r;
+	int r;
 
 	__asm__("bsrl %1,%0\n\t"
 	        "jnz 1f\n\t"
@@ -174,37 +169,64 @@ static __inline__ int fms(int x)
 }
 
 /* setting up variable and fixed mtrr
-   ToDo: 1. still need to find out how to set size and alignment correctly
-         2. should we invalid cache by INVLD or WBINVD ?? */
+ *
+ * From Intel Vol. III Section 9.12.4, the Range Size and Base Alignment has some kind of requirement:
+ *	1. The range size must be 2^N byte for N >= 12 (i.e 4KB minimum).
+ *	2. The base address must be 2^N aligned, where the N here is equal to the N in previous
+ *	   requirement. So a 8K range must be 8K aligned not 4K aligned.
+ *
+ * These requirement is meet by "decompositing" the ramsize into Sum(Cn * 2^n, n == [0..N], Cn = [0, 1]).
+ * For Cm = 1, there is a WB range of 2^m size at base address Sum(Cm * 2^m, m == [N..n]).
+ * A 124MB (128MB - 4MB SMA) example:
+ * 	ramsize = 124MB == 64MB (at 0MB) + 32MB (at 64MB) + 16MB (at 96MB ) + 8MB (at 112MB) + 4MB (120MB).
+ * But this wastes a lot of MTRR registers so we use another more "aggresive" way with Uncacheable Regions.
+ *
+ * In the Uncacheable Resion scheme, we try to cover the whole ramsize by one WB region as possible,
+ * If (an only if) this can not be done we will try to decomposite the ramesize, the mathematical formula
+ * whould be ramsize = Sum(Cn * 2^n, n == [0..N], Cn = [-1, 0, 1]). For Cn = -1, a Uncachable Region is used.
+ * The same 124MB example:
+ * 	ramsize = 124MB == 128MB WB (at 0MB) + 4MB UC (at 124MB)
+ * or a 156MB (128MB + 32MB - 4MB SMA) example:
+ *	ramsize = 156MB == 128MB WB (at 0MB) + 32MB WB (at 128MB) + 4MB UC (at 156MB)
+ */
 #ifdef INTEL_PPRO_MTRR
 #ifdef ENABLE_FIXED_AND_VARIABLE_MTRRS
 void intel_set_mtrr(unsigned long rambase, unsigned long ramsizeK)
 {
 	unsigned int reg = 0;
-	unsigned long range;
+	unsigned long range_wb, range_uc;
 
 	DBG("\n");
-	while (ramsizeK != 0 && reg <= 3) {
+
+	while (ramsizeK != 0 && reg <= 6) {
 		intel_post(0x60 + reg);
 
-		range = 1 << fms(ramsizeK);
-		DBG("Setting variable MTRR %d, base: %dMB, range: %dMB, type: WB\n",
-		    reg, rambase >> 10, range >> 10);
+		range_wb = 1 << (fms(ramsizeK) + 1);
+		range_uc = range_wb - ramsizeK;
 
-		intel_set_var_mtrr(reg, rambase * 1024, range * 1024, MTRR_TYPE_WRBACK);
+		if ((ramsizeK % range_uc) == 0) {
+			DBG("Setting variable MTRR %d, base: %4dMB, range: %4dMB, type: WB\n",
+			    reg, rambase >> 10, range_wb >> 10);
+			intel_set_var_mtrr(reg++, rambase * 1024, range_wb * 1024,
+					   MTRR_TYPE_WRBACK);
+			rambase += ramsizeK;
 
-		rambase += range;
-		ramsizeK -= range;
-		reg++;
-	}
+			DBG("Setting variable MTRR %d, base: %4dMB, range: %4dMB, type: UC\n",
+			    reg, rambase >> 10, range_uc >> 10);
+			intel_set_var_mtrr(reg++, rambase * 1024, range_uc * 1024,
+					   MTRR_TYPE_UNCACHABLE);
+			ramsizeK = 0; /* effectivly a break */
+		} else {
+			range_wb >>= 1;
 
-	/* The "remainder" of the memory ranges. FixMe: should we secrify this range or set it
-	   as WB and trust Linux Kernel (or Intel CPU ?? ) */
-	if (ramsizeK != 0) {
-		range = 1 << (fms(ramsizeK) + 1);
-		DBG("Setting variable MTRR %d, base: %dMB, range: %dMB, type: UC\n",
-		    reg, rambase >> 10, range >> 10);
-		intel_set_var_mtrr(reg, rambase * 1024, range * 1024, MTRR_TYPE_UNCACHABLE);
+			DBG("Setting variable MTRR %d, base: %4dMB, range: %4dMB, type: WB\n",
+			    reg, rambase >> 10, range_wb >> 10);
+			intel_set_var_mtrr(reg++, rambase * 1024, range_wb * 1024,
+					   MTRR_TYPE_WRBACK);
+
+			rambase += range_wb;
+			ramsizeK -= range_wb;
+		}
 	}
 
 	intel_set_fixed_mtrr();
@@ -218,7 +240,6 @@ void intel_set_mtrr(unsigned long rambase, unsigned long ramsizeK)
 {
 	intel_set_var_mtrr(0, 0, ramsizeK * 1024, MTRR_TYPE_WRBACK);
 	intel_enable_var_mtrr();
-	//intel_set_fixed_mtrr();
 }
 #endif /* ENABLE_FIXED_AND_VARIABLE_MTRRS */
 #endif /* INTEL_PPRO_MTRR */

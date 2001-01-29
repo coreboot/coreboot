ce8453cbde191bac651868160438e1392050967d
Li-Ta Lo <ollie@lanl.gov>
2001-01-29 08:01:49 +0000

    minor changes to to reflect 730

--- src/mainboard/pcchips/m810lmr/mainboard.c
+++ src/mainboard/pcchips/m810lmr/mainboard.c
@@ -8,12 +8,11 @@ mainboard_fixup(void)
 void
 final_mainboard_fixup(void)
 {
-    void final_southbridge_fixup(void);
-    void final_superio_fixup(void);
+	void final_southbridge_fixup(void);
+	void final_superio_fixup(void);
 
-    printk(KERN_INFO 
-	   "Winfast 6300 (and similar)...");
+	printk(KERN_INFO "PCCHIPS M810LMR (and similar)...");
 
-    final_southbridge_fixup();
-    final_superio_fixup();
+	final_southbridge_fixup();
+	final_superio_fixup();
 }

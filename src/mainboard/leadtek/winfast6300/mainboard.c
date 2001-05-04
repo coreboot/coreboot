ba26f0bd1ad8b21b26370400516d2db60d33c437
Li-Ta Lo <ollie@lanl.gov>
2001-05-04 05:36:57 +0000

    add nsuperio support

--- src/mainboard/leadtek/winfast6300/mainboard.c
+++ src/mainboard/leadtek/winfast6300/mainboard.c
@@ -8,14 +8,14 @@ mainboard_fixup(void)
 void
 final_mainboard_fixup(void)
 {
-    void final_southbridge_fixup(void);
-    void final_superio_fixup(void);
+	void final_southbridge_fixup(void);
+	void final_superio_fixup(void);
 
-    printk(KERN_INFO 
-	   "Winfast 6300 (and similar)...");
+	printk(KERN_INFO 
+	       "Winfast 6300 (and similar)...");
 
-    final_southbridge_fixup();
+	final_southbridge_fixup();
 #ifndef USE_NEW_SUPERIO_INTERFACE
-    final_superio_fixup();
+	final_superio_fixup();
 #endif
 }
--- src/mainboard/matsonic/ms7308e/mainboard.c
+++ src/mainboard/matsonic/ms7308e/mainboard.c
@@ -8,12 +8,14 @@ mainboard_fixup(void)
 void
 final_mainboard_fixup(void)
 {
-    void final_southbridge_fixup(void);
-    void final_superio_fixup(void);
+	void final_southbridge_fixup(void);
+	void final_superio_fixup(void);
 
-    printk(KERN_INFO 
-	   "Matsonic MS7308E (and similar)");
+	printk(KERN_INFO 
+	       "Matsonic MS7308E (and similar)");
 
-    final_southbridge_fixup();
-    final_superio_fixup();
+	final_southbridge_fixup();
+#ifndef USE_NEW_SUPERIO_INTERFACE
+	final_superio_fixup();
+#endif
 }

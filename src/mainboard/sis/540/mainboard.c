42e38b5a3e10487797bb1291198e93f2000a5c78
Li-Ta Lo <ollie@lanl.gov>
2001-05-23 02:48:26 +0000

    add sis 540 support

---
+++ src/mainboard/sis/540/mainboard.c
@@ -0,0 +1,21 @@
+#include <printk.h>
+
+void
+mainboard_fixup(void)
+{
+}
+
+void
+final_mainboard_fixup(void)
+{
+    void final_southbridge_fixup(void);
+    void final_superio_fixup(void);
+
+    printk(KERN_INFO 
+	   "SiS 540 (and similar)...");
+
+    final_southbridge_fixup();
+#ifndef USE_NEW_SUPERIO_INTERFACE
+    final_superio_fixup();
+#endif
+}

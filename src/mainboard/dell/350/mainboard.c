bd557f33aa3dd100e8ce33c61f862483621fde1c
Ronald G. Minnich <rminnich@gmail.com>
2001-07-06 23:13:48 +0000

    New files for dlel 350

---
+++ src/mainboard/dell/350/mainboard.c
@@ -0,0 +1,20 @@
+#include <printk.h>
+
+void
+mainboard_fixup(void)
+{
+}
+
+void
+final_mainboard_fixup(void)
+{
+	void final_southbridge_fixup(void);
+	void final_superio_fixup(void);
+
+	printk(KERN_INFO "DELL 350\n");
+
+	final_southbridge_fixup();
+#ifndef USE_NEW_SUPERIO_INTERFACE
+	final_superio_fixup();
+#endif
+}

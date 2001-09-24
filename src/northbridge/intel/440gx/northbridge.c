2dfc41206b97c6ef4366ef4b1a45cd41ae5ac92a
Ronald G. Minnich <rminnich@gmail.com>
2001-09-24 03:48:19 +0000

    Typo in the HOWTO for SiS 630 Quick patch for fix in the raminit code for 440gx == we need to fix this monday.

--- src/northbridge/intel/440gx/northbridge.c
+++ src/northbridge/intel/440gx/northbridge.c
@@ -1,6 +1,34 @@
 #include <pci.h>
 #include <cpu/p5/io.h>
+#include <printk.h>
 
+void
+dumpramregs(struct pci_dev *pcidev)
+{
+	int i, j;
+
+	for(i = 0x50; i < 0x90; i += 16) {
+		printk_err("%02x: ", i);
+		for(j = 0; j < 16; j++) {
+			unsigned char val;
+			pci_read_config_byte(pcidev, i+j, &val);
+			printk_err("%02x ", val);
+		}
+		printk_err("\n");
+	}
+
+	for(i = 0xc0; i < 0xe0; i += 16) {
+		printk_err("%02x: ", i);
+		for(j = 0; j < 16; j++) {
+			unsigned char val;
+			pci_read_config_byte(pcidev, i+j, &val);
+			printk_err("%02x ", val);
+		}
+		printk_err("\n");
+	}
+
+
+}
 unsigned long sizeram()
 {
 	/*
@@ -23,6 +51,7 @@ unsigned long sizeram()
 	if (banks == 0) {
 		totalmem = 0x80000000UL;
 	}
+	dumpramregs(pcidev);
 
 	return totalmem;
 }

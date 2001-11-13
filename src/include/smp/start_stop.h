ff2025e3d17a22d6d735d9c8c0d52c8243095561
Li-Ta Lo <ollie@lanl.gov>
2001-11-13 06:31:17 +0000

    support non-smp and no serial console cases

--- src/include/smp/start_stop.h
+++ src/include/smp/start_stop.h
@@ -11,6 +11,7 @@ void startup_other_cpus(unsigned long *processor_map);
 #else
 #define this_processors_id()	0
 #define startup_other_cpus(p)	do {} while(0)
+#define processor_index(p) 0
 #endif
 
 #endif /* SMP_START_STOP_H */

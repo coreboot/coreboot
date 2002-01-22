f85eb0d48edf922182e0cd73006e710f7f6a9cd9
Ronald G. Minnich <rminnich@gmail.com>
2002-01-22 02:26:47 +0000

    k7sem support

--- src/mainboard/elitegroup/k7sem/mainboard.c
+++ src/mainboard/elitegroup/k7sem/mainboard.c
@@ -13,7 +13,20 @@ final_mainboard_fixup(void)
 	void final_southbridge_fixup(void);
 	void final_superio_fixup(void);
 	extern unsigned long slotsizeM[];
+
 	printk_info("elitegroup k7sem (and similar)...");
+
+#if 0
+	// THIS WORKS in the sense of turning it off. 
+	// but it happens too late in the game.
+	printk_info("disabling sis 900 ethernet, old val 0x%x\n", 
+		    pcibios_read_config_byte(0, 0x8, 0x7c));
+
+        pcibios_write_config_byte(0, 0x8, 0x7c, 0x0f);
+
+	printk_info("disabling sis 900 ethernet, new val 0x%x\n", 
+		    pcibios_read_config_byte(0, 0x8, 0x7c));
+#endif
 
 	// we need to fix up the K7 MSRs. 
 	// to do this, we get the DIMM sizes in slot1 and 2, and 

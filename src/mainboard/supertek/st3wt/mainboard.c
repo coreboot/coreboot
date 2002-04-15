588760c029f0a9cd4975873614380b66184efa10
Ronald G. Minnich <rminnich@gmail.com>
2002-04-15 14:02:19 +0000

    commit patches from stefan.

--- src/mainboard/supertek/st3wt/mainboard.c
+++ src/mainboard/supertek/st3wt/mainboard.c
@@ -1,4 +1,5 @@
 /* 
+    Supertek/st32t, derived from:
     freebios/src/mainboard/cocom/voyager2/mainboard.c
 
     Copyright (c) 2002 Christer Weinigel <wingel@hack.org>
@@ -22,10 +23,8 @@ mainboard_fixup()
 void
 final_mainboard_fixup()
 {
-#if 0
 	struct pci_dev *dev;
 	int i;
-#endif
 
 	void final_southbridge_fixup(void);
 	final_southbridge_fixup();
@@ -34,7 +33,7 @@ final_mainboard_fixup()
 
         dev = pci_find_slot(0, PCI_DEVFN(0x0f, 0));
         if (dev) {
-                printk_debug("nano: Setting eth0 IRQ to %d (INTB)\n",
+                printk_debug("st-3wt: Setting eth0 IRQ to %d (INTB)\n",
 					CS5530_INTB);
                 pci_write_config_byte(dev, PCI_INTERRUPT_LINE, CS5530_INTB);
         }
@@ -42,7 +41,6 @@ final_mainboard_fixup()
 
 /*
     Local variables:
-        compile-command: "make -C /export/bios/voyager2"
         c-basic-offset: 8
     End:
 */

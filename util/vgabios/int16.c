3fcf5b30708bcd8c1fb30fb976530a8b7cda38c5
Ronald G. Minnich <rminnich@gmail.com>
2002-04-23 20:47:04 +0000

    new files for ints

---
+++ util/vgabios/int16.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int int16_handler(void)
+{
+	printf("\nint16: keyboard not supported right now.\n");
+	return 1;
+}
---
+++ util/vgabios/pci.h
@@ -0,0 +1,4 @@
+void x_outb(u16 port, u8 val);
+#define outb x_outb
+
+

f91e1b83fcc9e20f4c91e5a1d92086b838fa6f6f
Li-Ta Lo <ollie@lanl.gov>
2002-05-17 08:15:47 +0000

    K7 Specific MSR is not classified now.

--- src/include/cpu/k7/mtrr.h
+++ src/include/cpu/k7/mtrr.h
@@ -3,7 +3,10 @@
 
 #include <cpu/p6/mtrr.h>
 
-# if USE_AMD_NDA_CODE
-# endif /* USE_AMD_NDA_CODE */
+#define TOP_MEM    0xc001001A
+#define TOP_MEM2   0xc001001D
+#define IORR_FIRST 0xC0010016
+#define IORR_LAST  0xC0010019
+#define SYSCFG     0xC0010010
 
 #endif /* CPU_K7_MTRR_H */
--- src/mainboard/elitegroup/k7sem/mainboard.c
+++ src/mainboard/elitegroup/k7sem/mainboard.c
@@ -8,35 +8,18 @@ mainboard_fixup(void)
 void
 final_mainboard_fixup(void)
 {
-	void fixk7msr_2dimms(unsigned long dimm1sizeK, 
-		unsigned long dimm2sizeK);
 	void final_southbridge_fixup(void);
 	void final_superio_fixup(void);
+	void k7_cpufixup(unsigned long ramsizeK);
+
 	extern unsigned long slotsizeM[];
 
 	printk_info("elitegroup k7sem (and similar)...");
 
-#if 0
-	// THIS WORKS in the sense of turning it off. 
-	// but it happens too late in the game.
-	printk_info("disabling sis 900 ethernet, old val 0x%x\n", 
-		    pcibios_read_config_byte(0, 0x8, 0x7c));
+	//k7_cpufixup(slotsizeM[0] + slotsizeM[1]);
 
-        pcibios_write_config_byte(0, 0x8, 0x7c, 0x0f);
-
-	printk_info("disabling sis 900 ethernet, new val 0x%x\n", 
-		    pcibios_read_config_byte(0, 0x8, 0x7c));
-#endif
-
-	// we need to fix up the K7 MSRs. 
-	// to do this, we get the DIMM sizes in slot1 and 2, and 
-	// call fixk7msr_2dimms
-	// this is all very mainboard-specific ...
-
-	printk_err("Fixing mainboard for dimms %dK + %dK\n", 
-		slotsizeM[0], slotsizeM[1]);
-	fixk7msr_2dimms(slotsizeM[0], slotsizeM[1]);
 	final_southbridge_fixup();
+
 #ifndef USE_NEW_SUPERIO_INTERFACE
 	final_superio_fixup();
 #endif

5bcc3e0242b6aa07f771a69cf5b70c200fc0e257
Steven James <pyro@linuxlabs.com>
2003-04-10 20:51:31 +0000

    Support for udelay in baremetal

--- util/baremetal/include/string.h
+++ util/baremetal/include/string.h
@@ -6,7 +6,7 @@
 // yes, linux has fancy ones. We don't care. This stuff gets used 
 // hardly at all. And the pain of including those files is just too high.
 
-//extern inline void strcpy(char *dst, char *src) {while (*src) *dst++ = *src++;}
+extern inline void strcpy(char *dst, char *src) {while (*src) *dst++ = *src++;}
 
 //extern inline int strlen(char *src) { int i = 0; while (*src++) i++; return i;}
 

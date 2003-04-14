bf2d57b99a5aecc7d5ff50bf63704b38ed06a884
Ronald G. Minnich <rminnich@gmail.com>
2003-04-14 22:56:30 +0000

    new mobo

---
+++ src/mainboard/elitegroup/p6stp-fl/mainboard.c
@@ -0,0 +1,20 @@
+#include <printk.h>
+
+void
+mainboard_fixup(void)
+{
+}
+
+void
+final_mainboard_fixup(void)
+{
+	void final_southbridge_fixup(void);
+	void final_superio_fixup(void);
+
+	printk_info("Elitegroup P6STP-FL\n");
+
+	final_southbridge_fixup();
+#ifndef USE_NEW_SUPERIO_INTERFACE
+	//final_superio_fixup();
+#endif
+}

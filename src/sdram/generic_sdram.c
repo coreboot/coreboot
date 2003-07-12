548593ad666065a5f63216aebb0176b82e82fda6
Eric Biederman <ebiederm@xmission.com>
2003-07-12 01:46:53 +0000

    - generalize generic_sdram.c
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@954 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/sdram/generic_sdram.c
+++ src/sdram/generic_sdram.c
@@ -7,29 +7,22 @@ void sdram_no_memory(void)
 }
 
 /* Setup SDRAM */
-void sdram_initialize(void)
+void sdram_initialize(const struct mem_controller *ctrl)
 {
 	print_debug("Ram1\r\n");
 	/* Set the registers we can set once to reasonable values */
-	sdram_set_registers();
+	sdram_set_registers(ctrl);
 
 	print_debug("Ram2\r\n");
 	/* Now setup those things we can auto detect */
-	sdram_set_spd_registers();
+	sdram_set_spd_registers(ctrl);
 
 	print_debug("Ram3\r\n");
 	/* Now that everything is setup enable the SDRAM.
 	 * Some chipsets do the work for use while on others 
 	 * we need to it by hand.
 	 */
-	sdram_enable();
+	sdram_enable(ctrl);
 
 	print_debug("Ram4\r\n");
-	sdram_first_normal_reference();
-
-	print_debug("Ram5\r\n");
-	sdram_enable_refresh();
-	sdram_special_finishup();
-
-	print_debug("Ram6\r\n");
 }

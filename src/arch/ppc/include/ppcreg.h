04091a67e574d79f0f8d5276e09f2e15217b20e7
Greg Watson <jarrah@users.sourceforge.net>
2003-07-28 21:18:26 +0000

    PVR support
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1054 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/arch/ppc/include/ppcreg.h
+++ src/arch/ppc/include/ppcreg.h
@@ -116,6 +116,71 @@
 #define BAT_READ_ONLY	0x00000001
 #define BAT_READ_WRITE	0x00000002
 
+/* Processor Version Register */
+
+/* Processor Version Register (PVR) field extraction */
+
+#define	PVR_VER(pvr)  (((pvr) >>  16) & 0xFFFF)	/* Version field */
+#define	PVR_REV(pvr)  (((pvr) >>   0) & 0xFFFF)	/* Revison field */
+
+/*
+ * IBM has further subdivided the standard PowerPC 16-bit version and
+ * revision subfields of the PVR for the PowerPC 403s into the following:
+ */
+
+#define	PVR_FAM(pvr)	(((pvr) >> 20) & 0xFFF)	/* Family field */
+#define	PVR_MEM(pvr)	(((pvr) >> 16) & 0xF)	/* Member field */
+#define	PVR_CORE(pvr)	(((pvr) >> 12) & 0xF)	/* Core field */
+#define	PVR_CFG(pvr)	(((pvr) >>  8) & 0xF)	/* Configuration field */
+#define	PVR_MAJ(pvr)	(((pvr) >>  4) & 0xF)	/* Major revision field */
+#define	PVR_MIN(pvr)	(((pvr) >>  0) & 0xF)	/* Minor revision field */
+
+/* Processor Version Numbers */
+
+#define	PVR_403GA	0x00200000
+#define	PVR_403GB	0x00200100
+#define	PVR_403GC	0x00200200
+#define	PVR_403GCX	0x00201400
+#define	PVR_405GP	0x40110000
+#define	PVR_405GP_RB	0x40110040
+#define	PVR_405GP_RC	0x40110082
+#define	PVR_405GP_RD	0x401100C4
+#define	PVR_405GP_RE	0x40110145  /* same as pc405cr rev c */
+#define	PVR_405CR_RA	0x40110041
+#define	PVR_405CR_RB	0x401100C5
+#define	PVR_405CR_RC	0x40110145  /* same as pc405gp rev e */
+#define	PVR_405GPR_RB	0x50910951
+#define	PVR_440GP_RB	0x40120440
+#define	PVR_440GP_RC	0x40120481
+#define	PVR_405EP_RB	0x51210950
+#define	PVR_601		0x00010000
+#define	PVR_602		0x00050000
+#define	PVR_603		0x00030000
+#define	PVR_603e	0x00060000
+#define	PVR_603ev	0x00070000
+#define	PVR_603r	0x00071000
+#define	PVR_604		0x00040000
+#define	PVR_604e	0x00090000
+#define	PVR_604r	0x000A0000
+#define	PVR_620		0x00140000
+#define	PVR_740		0x00080000
+#define	PVR_750		PVR_740
+#define	PVR_740P	0x10080000
+#define	PVR_750P	PVR_740P
+/*
+ * For the 8xx processors, all of them report the same PVR family for
+ * the PowerPC core. The various versions of these processors must be
+ * differentiated by the version number in the Communication Processor
+ * Module (CPM).
+ */
+#define	PVR_821		0x00500000
+#define	PVR_823		PVR_821
+#define	PVR_850		PVR_821
+#define	PVR_860		PVR_821
+#define	PVR_7400       	0x000C0000
+#define	PVR_8240	0x00810100
+#define	PVR_8260	PVR_8240
+
 #ifndef ASM
 unsigned __getmsr(void);
 void __setmsr(unsigned value);

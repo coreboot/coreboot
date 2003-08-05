95bbf58b8c1620a6a6171453a76b3b0ab5de68e4
Ronald G. Minnich <rminnich@gmail.com>
2003-08-05 13:43:18 +0000

    missing file chip.h
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1071 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/cpu/k8/chip.h
@@ -0,0 +1,9 @@
+extern struct chip_control cpu_k8_control;
+
+struct ht_link {
+		struct chip *chip;
+		unsigned int ht_width, ht_speed;
+};
+struct cpu_k8_config {
+	struct ht_link up, down, across;
+};

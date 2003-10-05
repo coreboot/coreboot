e046fbbb2cae62934ba4aae85d56456e7d7b3ff4
Greg Watson <jarrah@users.sourceforge.net>
2003-10-05 05:14:42 +0000

    new cpu types
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1193 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/arch/ppc/lib/cpuid.c
+++ src/arch/ppc/lib/cpuid.c
@@ -11,39 +11,48 @@ void display_cpuid(void)
     unsigned version = ppc_getpvr() & 0xffff;
     const char *cpu_string = 0;
     switch(type) {
-        case 1:
+        case 0x0001:
             cpu_string = "601";
             break;
-        case 3:
+        case 0x0003:
             cpu_string = "603";
             break;
-        case 4:
+        case 0x0004:
             cpu_string = "604";
             break;
-        case 6:
+        case 0x0006:
             cpu_string = "603e";
             break;
-        case 7:
+        case 0x0007:
             cpu_string = "603ev";
             break;
-        case 8:
+        case 0x0008:
             cpu_string = "750";
             break;
-        case 9:
+        case 0x0009:
             cpu_string = "604e";
             break;
-        case 10:
+        case 0x000a:
             cpu_string = "604ev5 (MachV)";
             break;
-        case 12:
+        case 0x000c:
             cpu_string = "7400";
             break;
-        case 50:
+        case 0x0032:
             cpu_string = "821";
             break;
-        case 80:
+        case 0x0050:
             cpu_string = "860";
             break;
+	case 0x4011:
+	    cpu_string = "405GP";
+	    break;
+	case 0x5091:
+	    cpu_string = "405GPr";
+	    break;
+	case 0x5121:
+	    cpu_string = "405EP";
+	    break;
 	case 0x800c:
 	    cpu_string = "7410";
 	    break;

14a90f1aafa4ba151c9be224044749bda6ed520e
Greg Watson <jarrah@users.sourceforge.net>
2003-11-02 17:05:20 +0000

    *** empty log message ***
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1245 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/arch/ppc/init/ppc_main.c
+++ src/arch/ppc/init/ppc_main.c
@@ -3,6 +3,8 @@
  * gwatson@lanl.gov
  */
 
+#include <sdram.h>
+
 extern unsigned _iseg[];
 extern unsigned _liseg[];
 extern unsigned _eliseg[];
@@ -15,7 +17,7 @@ void (*hardwaremain)(int) = _iseg;
  *
  * - turn on real memory
  * - relocate our payload into real memory
- * - start executing payload
+ * - start hardwaremain() which does remainder of setup
  */
 
 void ppc_main(void)

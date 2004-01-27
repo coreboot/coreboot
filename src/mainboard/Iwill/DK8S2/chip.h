22489894e189616bb5694cfed8bd951951e68fae
Ronald G. Minnich <rminnich@gmail.com>
2004-01-27 17:08:03 +0000

    will mainboards
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1357 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/mainboard/Iwill/DK8S2/chip.h
@@ -0,0 +1,5 @@
+struct chip_control mainboard_arima_hdama_control;
+
+struct mainboard_arima_hdama_config {
+	int nothing;
+};
---
+++ src/mainboard/Iwill/DK8S2/mainboard.c
@@ -0,0 +1,41 @@
+#include <console/console.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <device/pci_ids.h>
+#include <device/pci_ops.h>
+
+#include <arch/io.h>
+#include <device/chip.h>
+#include "../../../northbridge/amd/amdk8/northbridge.h"
+#include "chip.h"
+
+
+unsigned long initial_apicid[CONFIG_MAX_CPUS] =
+{
+	0, 1,
+};
+
+static struct device_operations mainboard_operations = {
+	.read_resources   = root_dev_read_resources,
+	.set_resources    = root_dev_set_resources,
+	.enable_resources = enable_childrens_resources,
+	.init             = 0,
+	.scan_bus         = amdk8_scan_root_bus,
+	.enable           = 0,
+};
+
+static void enumerate(struct chip *chip)
+{
+	struct chip *child;
+	dev_root.ops = &mainboard_operations;
+	chip->dev = &dev_root;
+	chip->bus = 0;
+	for(child = chip->children; child; child = child->next) {
+		child->bus = &dev_root.link[0];
+	}
+}
+struct chip_control mainboard_arima_hdama_control = {
+	.enumerate = enumerate, 
+	.name      = "Arima HDAMA mainboard ",
+};
+

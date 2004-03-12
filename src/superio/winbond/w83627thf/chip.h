fd714cf120bdea2faf638efeaf4617c80522ce16
Ronald G. Minnich <rminnich@gmail.com>
2004-03-12 23:02:05 +0000

    new support
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1395 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/superio/winbond/w83627thf/chip.h
+++ src/superio/winbond/w83627thf/chip.h
@@ -1,9 +1,3 @@
-#ifndef PNP_INDEX_REG
-#define PNP_INDEX_REG   0x15C
-#endif
-#ifndef PNP_DATA_REG
-#define PNP_DATA_REG    0x15D
-#endif
 #ifndef SIO_COM1
 #define SIO_COM1_BASE   0x3F8
 #endif
@@ -13,9 +7,10 @@
 
 extern struct chip_control superio_winbond_w83627thf_control;
 
+#include <pc80/keyboard.h>
+#include <uart8250.h>
+
 struct superio_winbond_w83627thf_config {
-  struct com_ports com1, com2;
-  
-	struct lpt_ports lpt;
-	int port;
+	struct uart8250 com1, com2;
+	struct pc_keyboard keyboard;
 };

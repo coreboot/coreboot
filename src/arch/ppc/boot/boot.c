96ce0b4bd147af313baf05612e80b959b61290f0
Greg Watson <jarrah@users.sourceforge.net>
2004-03-13 03:46:41 +0000

    removed unused code
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1412 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/arch/ppc/boot/boot.c
+++ src/arch/ppc/boot/boot.c
@@ -4,58 +4,7 @@
 #include <string.h>
 #include <console/console.h>
 
-
-#ifndef CMD_LINE
-#define CMD_LINE ""
-#endif
-
-
-
-#define UPSZ(X) ((sizeof(X) + 3) &~3)
-
-static struct {
-	Elf_Bhdr hdr;
-	Elf_Nhdr ft_hdr;
-	unsigned char ft_desc[UPSZ(FIRMWARE_TYPE)];
-	Elf_Nhdr bl_hdr;
-	unsigned char bl_desc[UPSZ(BOOTLOADER)];
-	Elf_Nhdr blv_hdr;
-	unsigned char blv_desc[UPSZ(BOOTLOADER_VERSION)];
-	Elf_Nhdr cmd_hdr;
-	unsigned char cmd_desc[UPSZ(CMD_LINE)];
-} elf_boot_notes = {
-	.hdr = {
-		.b_signature = 0x0E1FB007,
-		.b_size = sizeof(elf_boot_notes),
-		.b_checksum = 0,
-		.b_records = 4,
-	},
-	.ft_hdr = {
-		.n_namesz = 0,
-		.n_descsz = sizeof(FIRMWARE_TYPE),
-		.n_type = EBN_FIRMWARE_TYPE,
-	},
-	.ft_desc = FIRMWARE_TYPE,
-	.bl_hdr = {
-		.n_namesz = 0,
-		.n_descsz = sizeof(BOOTLOADER),
-		.n_type = EBN_BOOTLOADER_NAME,
-	},
-	.bl_desc = BOOTLOADER,
-	.blv_hdr = {
-		.n_namesz = 0,
-		.n_descsz = sizeof(BOOTLOADER_VERSION),
-		.n_type = EBN_BOOTLOADER_VERSION,
-	},
-	.blv_desc = BOOTLOADER_VERSION,
-	.cmd_hdr = {
-		.n_namesz = 0,
-		.n_descsz = sizeof(CMD_LINE),
-		.n_type = EBN_COMMAND_LINE,
-	},
-	.cmd_desc = CMD_LINE,
-};
-
+extern void flush_dcache(void);
 
 int elf_check_arch(Elf_ehdr *ehdr)
 {
@@ -70,6 +19,7 @@ int elf_check_arch(Elf_ehdr *ehdr)
 void jmp_to_elf_entry(void *entry, unsigned long buffer)
 {
 	void (*kernel_entry)(void);       
+
 	kernel_entry = entry;
 
 	/*

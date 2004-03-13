0f62047061dcbfebef2471792a0dac5b13a78579
Greg Watson <jarrah@users.sourceforge.net>
2004-03-13 03:43:29 +0000

    byteorder routines
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1411 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/arch/i386/include/arch/byteorder.h
@@ -0,0 +1,17 @@
+#ifndef _BYTEORDER_H
+#define _BYTEORDER_H
+
+#define __LITTLE_ENDIAN 1234
+
+#include <swab.h>
+
+#define cpu_to_le32(x) ((unsigned int)(x))
+#define le32_to_cpu(x) ((unsigned int)(x))
+#define cpu_to_le16(x) ((unsigned short)(x))
+#define le16_to_cpu(x) ((unsigned short)(x))
+#define cpu_to_be32(x) swab32((x))
+#define be32_to_cpu(x) swab32((x))
+#define cpu_to_be16(x) swab16((x))
+#define be16_to_cpu(x) swab16((x))
+
+#endif /* _BYTEORDER_H */

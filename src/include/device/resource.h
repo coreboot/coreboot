e3c79ed89beddde0655775da6d5947a56b86f3cf
Li-Ta Lo <ollie@lanl.gov>
2004-03-19 00:08:48 +0000

    number of bits should be called log 2 rather than base 2
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1442 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/include/device/resource.h
+++ src/include/device/resource.h
@@ -66,8 +66,8 @@ struct resource {
 	unsigned long limit;	/* Largest valid value base + size -1 */
 	unsigned long flags;	/* Descriptions of the kind of resource */
 	unsigned long index;	/* Bus specific per device resource id */
-	unsigned char align;	/* Required alignment (base 2) of the resource */
-	unsigned char gran;	/* Granularity (base 2) of the resource */
+	unsigned char align;	/* Required alignment (log 2) of the resource */
+	unsigned char gran;	/* Granularity (log 2) of the resource */
 	/* Alignment must be >= the granularity of the resource */
 };
 

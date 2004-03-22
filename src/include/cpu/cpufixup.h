2619721692f979656ec26d9a24dd777c875a0e53
Ronald G. Minnich <rminnich@gmail.com>
2004-03-22 21:44:51 +0000

    fixed to follow linuxbios usage.
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1463 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/include/cpu/cpufixup.h
+++ src/include/cpu/cpufixup.h
@@ -8,16 +8,21 @@ struct mem_range;
 #include <cpu/p6/cpufixup.h>
 
 #if CPU_FIXUP == 1 
-#  if defined(k8)
+#  if (k8==1)
+#warning "====>Temporary notice that we are using k8 cpufixup"
 #    define cpufixup(mem) k8_cpufixup(mem)
-#  elif defined(k7)
+#  elif (k7==1)
+#warning "====>Temporary notice that we are using k7 cpufixup"
 #    define cpufixup(mem) k7_cpufixup(mem)
-#  elif defined(i786)
+#  elif (i786==1)
+#warning "====>Temporary notice that we are using i786 cpufixup"
 #    define cpufixup(mem) i786_cpufixup(mem)
-#  elif defined(i686)
+#  elif (i686==1)
+#warning "====>Temporary notice that we are using i686 cpufixup"
 #    define cpufixup(mem) p6_cpufixup(mem)
 #  endif
 #else
+#warning "====>YOU DID NOT DEFINE ONE OF: k8, k7, i786, i686"
 #  define cpufixup(mem) do {} while(0)
 #endif
 

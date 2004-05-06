3bb314725aee90ae9436ce3b1d7e9e29f8e2d473
Li-Ta Lo <ollie@lanl.gov>
2004-05-06 21:39:39 +0000

    replace up,across,down with ltd0,ldt1, ldt2 Although it is not used currently, misuse of terminolog is still a misuse.
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1548 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/cpu/k8/chip.h
+++ src/cpu/k8/chip.h
@@ -1,9 +1,10 @@
 extern struct chip_control cpu_k8_control;
 
 struct ht_link {
-		struct chip *chip;
-		unsigned int ht_width, ht_speed;
+	struct chip *chip;
+	unsigned int ht_width, ht_speed;
 };
+
 struct cpu_k8_config {
-	struct ht_link up, down, across;
+	struct ht_link ldt0, ldt1, ldt2;
 };

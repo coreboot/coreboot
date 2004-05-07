89666e4893bf51eab4f20137f2f65a385d37ec09
Li-Ta Lo <ollie@lanl.gov>
2004-05-07 21:54:23 +0000

    change walk_static_devices() to scan_static_bus()
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1550 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/include/device/device.h
+++ src/include/device/device.h
@@ -101,7 +101,7 @@ device_t dev_find_slot (unsigned int bus, unsigned int devfn);
 struct device_operations default_dev_ops_root;
 extern void root_dev_read_resources(device_t dev);
 extern void root_dev_set_resources(device_t dev);
-extern unsigned int walk_static_devices(device_t bus, unsigned int max);
+extern unsigned int scan_static_bus(device_t bus, unsigned int max);
 extern void enable_childrens_resources(device_t dev);
 extern unsigned int root_dev_scan_pci_bus(device_t root, unsigned int max);
 

4a3bb76aa85ba720b607152d853bd7e1964a9f6c
Stefan Reinauer <stepan@openbios.org>
2004-06-28 11:57:31 +0000

    commit initial qemu support (see http://fabrice.bellard.free.fr/qemu/)
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1610 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/mainboard/emulation/qemu-i386/auto.c
@@ -0,0 +1,38 @@
+#define ASSEMBLY 1
+
+#include <stdint.h>
+#include <device/pci_def.h>
+#include <device/pci_ids.h>
+#include <cpu/p6/apic.h>
+#include <arch/io.h>
+#include <device/pnp_def.h>
+#include <arch/romcc_io.h>
+#include <arch/hlt.h>
+#include "pc80/serial.c"
+#include "arch/i386/lib/console.c"
+#include "cpu/p6/earlymtrr.c"
+
+/*
+ */
+void udelay(int usecs) 
+{
+	int i;
+	for(i = 0; i < usecs; i++)
+		outb(i&0xff, 0x80);
+}
+
+#include "lib/delay.c"
+#include "cpu/p6/boot_cpu.c"
+#include "debug.c"
+
+static void main(void)
+{
+	/*	init_timer();*/
+	outb(5, 0x80);
+	
+	uart_init();
+	console_init();
+	
+	//print_pci_devices();
+	//dump_pci_devices();
+}
---
+++ src/mainboard/emulation/qemu-i386/chip.h
@@ -0,0 +1,5 @@
+extern struct chip_control mainboard_emulation_qemu_i386_control;
+
+struct mainboard_emulation_qemu_i386_config {
+	int nothing;
+};

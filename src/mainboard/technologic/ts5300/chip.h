198409afe08df8285f80327307670e9d859c9e2b
Stefan Reinauer <stepan@openbios.org>
2004-08-07 19:58:46 +0000

    create some technologic systems ts530 infrastructure
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1628 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/mainboard/technologic/ts5300/chip.h
@@ -0,0 +1,5 @@
+extern struct chip_control mainboard_technologic_ts5300_control;
+
+struct mainboard_technologic_ts5300_config {
+	int nothing;
+};
---
+++ src/mainboard/technologic/ts5300/failover.c
@@ -0,0 +1,29 @@
+#define ASSEMBLY 1
+#include <stdint.h>
+#include <device/pci_def.h>
+#include <device/pci_ids.h>
+#include <arch/io.h>
+#include "arch/romcc_io.h"
+#include "pc80/mc146818rtc_early.c"
+#include "cpu/p6/boot_cpu.c"
+
+static void main(void)
+{
+  /* for now, just always assume failure */
+
+#if 0
+	/* Is this a cpu reset? */
+	if (cpu_init_detected()) {
+		if (last_boot_normal()) {
+			asm("jmp __normal_image");
+		} else {
+			asm("jmp __cpu_reset");
+		}
+	}
+
+	/* This is the primary cpu how should I boot? */
+	else if (do_normal_boot()) {
+		asm("jmp __normal_image");
+	}
+#endif
+}

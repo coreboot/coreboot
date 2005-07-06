c8c720a80138af945f8f5b7fa9d0c6400f23fc9a
arch import user (historical) <svn@openbios.org>
2005-07-06 16:59:18 +0000

    Revision: linuxbios@linuxbios.org--devel/freebios--devel--2.0--patch-16 Creator:  Ronald G. Minnich <rminnich@lanl.gov>
    add cpu directory and files for sc520
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1934 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/cpu/amd/sc520/chip.h
@@ -0,0 +1,4 @@
+extern struct chip_operations cpu_amd_sc520_ops;
+
+struct cpu_amd_sc520_config {
+};
---
+++ src/cpu/amd/sc520/sc520.c
@@ -0,0 +1,7 @@
+#include <device/device.h>
+#include "chip.h"
+
+
+struct chip_operations cpu_amd_sc520_ops = {
+	CHIP_NAME("AMD SC520")
+};

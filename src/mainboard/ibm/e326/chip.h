14cde9e96a777f9d75016a13b23fab0480515f58
Li-Ta Lo <ollie@lanl.gov>
2005-07-07 21:31:50 +0000

    add ibm e326 support, basically it is the same as e325
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@1981 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/mainboard/ibm/e326/chip.h
@@ -0,0 +1,5 @@
+extern struct chip_operations mainboard_ibm_e326_ops;
+
+struct mainboard_ibm_e326_config {
+	int nothing;
+};
---
+++ src/mainboard/ibm/e326/mainboard.c
@@ -0,0 +1,10 @@
+#include <console/console.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <device/pci_ids.h>
+#include <device/pci_ops.h>
+#include "chip.h"
+
+struct chip_operations mainboard_ibm_e326_ops = {
+	CHIP_NAME("IBM E326 mainboard ")
+};

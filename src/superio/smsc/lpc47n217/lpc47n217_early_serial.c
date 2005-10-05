6ab43fcc48992d46096842b17a00dd51dd835c69
Stefan Reinauer <stepan@openbios.org>
2005-10-05 18:17:45 +0000

    Updating FSF address in the code.
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2051 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/superio/smsc/lpc47n217/lpc47n217_early_serial.c
+++ src/superio/smsc/lpc47n217/lpc47n217_early_serial.c
@@ -17,7 +17,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * $Log: lpc47n217_early_serial.c,v $
  * Revision 1.1.1.1  2005/07/11 15:28:51  smagnani

abaf71a2d5a62d0de88c2f69b038f8646835ffe3
Stefan Reinauer <stepan@openbios.org>
2006-08-29 00:45:42 +0000

    it8661f support from Uwe Hermann
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2393 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/superio/ite/it8661f/it8661f.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2006 Uwe Hermann <uwe@hermann-uwe.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+/* Datasheet: http://www.ite.com.tw/product_info/PC/Brief-IT8661_2.asp */
+/* Status: untested on real hardware, but it compiles. */
+
+/* This chip doesn't seem to have keyboard and mouse support. */
+
+#define IT8661F_FDC  0x00 /* Floppy */
+#define IT8661F_SP1  0x01 /* Com1 */
+#define IT8661F_SP2  0x02 /* Com2 */
+#define IT8661F_PP   0x03 /* Parallel port */
+#define IT8661F_IR   0x04 /* IR */
+#define IT8661F_GPIO 0x05 /* GPIO & Alternate Function Configuration */
+
---
+++ src/superio/ite/it8661f/superio.c
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2006 Uwe Hermann <uwe@hermann-uwe.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+/* This chip doesn't seem to have keyboard and mouse support. */
+
+#include <uart8250.h>
+/* #include <pc80/keyboard.h> */
+#include "chip.h"
+#include "it8661f.h"
+
+static void init(device_t dev)
+{
+	struct superio_ITE_it8661f_config *conf;
+	struct resource *res0, *res1;
+
+	if (!dev->enabled) {
+		return;
+	}
+
+	conf = dev->chip_info;
+
+	switch (dev->path.u.pnp.device) {
+	case IT8661F_FDC: /* TODO. */
+		break;
+	case IT8661F_SP1:
+		res0 = find_resource(dev, PNP_IDX_IO0);
+		init_uart8250(res0->base, &conf->com1);
+		break;
+	case IT8661F_SP2:
+		res0 = find_resource(dev, PNP_IDX_IO0);
+		init_uart8250(res0->base, &conf->com2);
+		break;
+	case IT8661F_PP: /* TODO. */
+		break;
+	case IT8661F_IR: /* TODO. */
+		break;
+	}
+}
+
+static struct device_operations ops = {
+	.read_resources   = pnp_read_resources,
+	.set_resources    = pnp_set_resources,
+	.enable_resources = pnp_enable_resources,
+	.enable           = pnp_enable,
+	.init             = init,
+};
+
+/* TODO: FDC, PP, IR, GPIO. */
+static struct pnp_info pnp_dev_info[] = {
+ { &ops, IT8661F_SP1,  PNP_IO0 | PNP_IRQ0, { 0x7f8, 0 }, },
+ { &ops, IT8661F_SP2,  PNP_IO0 | PNP_IRQ0 | PNP_DRQ0 | PNP_DRQ1, { 0x7f8, 0 }, },
+};
+
+static void enable_dev(struct device *dev)
+{
+	pnp_enable_devices(dev, &pnp_ops,
+		sizeof(pnp_dev_info)/sizeof(pnp_dev_info[0]), pnp_dev_info);
+}
+
+struct chip_operations superio_ITE_it8661f_ops = {
+	CHIP_NAME("ITE it8661f")
+	.enable_dev = enable_dev,
+};
+

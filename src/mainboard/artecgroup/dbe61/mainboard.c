efba85f00e16c541eb8205b8dbb5611dc18b9544
Ronald G. Minnich <rminnich@gmail.com>
2006-09-19 19:30:11 +0000

    commit moire changes
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2428 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/artecgroup/dbe61/mainboard.c
+++ src/mainboard/artecgroup/dbe61/mainboard.c
@@ -4,35 +4,31 @@
 #include <device/pci_ids.h>
 #include <device/pci_ops.h>
 #include <arch/io.h>
+#include <cpu/x86/msr.h>
+#include <cpu/amd/lxdef.h>
 #include "chip.h"
 
+#define DIVIL_LBAR_GPIO		0x5140000c
+
+static void init_gpio()
+{
+	msr_t msr;
+	printk_debug("Initializing GPIO module...\n");
+
+	// initialize the GPIO LBAR
+	msr.lo = GPIO_BASE;
+	msr.hi = 0x0000f001;
+	wrmsr(DIVIL_LBAR_GPIO, msr);
+	msr = rdmsr(DIVIL_LBAR_GPIO);
+	printk_debug("DIVIL_LBAR_GPIO set to 0x%08x 0x%08x\n", msr.hi, msr.lo);
+}
 
 static void init(struct device *dev)
 {
-	unsigned bus = 0;
-	unsigned devNic = PCI_DEVFN(0xd, 0);	
-	unsigned devUsb = PCI_DEVFN(0xf, 4);
-	device_t usb = NULL, nic = NULL;
-	unsigned char irqUsb = 0xa, irqNic = 0xb;
-
+	// BOARD-SPECIFIC INIT
 	printk_debug("ARTECGROUP DBE61 ENTER %s\n", __FUNCTION__);
 
-	// FIXME: do we need to initialize USB OHCI this way?
-	printk_debug("%s (%x,%x) set USB PCI interrupt line to %d\n", 
-		__FUNCTION__, bus, devUsb, irqUsb);
-
-	// initialize the USB controller
-	usb = dev_find_slot(bus, devUsb);
-	if (!usb) printk_err("Could not find USB\n");
-	else pci_write_config8(usb, PCI_INTERRUPT_LINE, irqUsb);
-
-	printk_debug("%s (%x,%x) set NIC PCI interrupt line to %d\n", 
-		__FUNCTION__, bus, devNic, irqNic);
-
-	// initialize the Realtek NIC
-	nic = dev_find_slot(bus, devNic);
-	if (!nic) printk_err("Could not find USB\n");
-	else pci_write_config8(nic, PCI_INTERRUPT_LINE, irqNic);
+	init_gpio();
 
 	printk_debug("ARTECGROUP DBE61 EXIT %s\n", __FUNCTION__);
 }

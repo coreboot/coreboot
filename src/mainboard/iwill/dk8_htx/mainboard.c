814b458b633dcf478e9e375d0a3b741b3a153c0f
Stefan Reinauer <stepan@openbios.org>
2006-10-24 12:46:55 +0000

    fix leftover typo
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2470 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/iwill/dk8_htx/mainboard.c
+++ src/mainboard/iwill/dk8_htx/mainboard.c
@@ -6,7 +6,7 @@
 #include "chip.h"
 
 #if CONFIG_CHIP_NAME == 1
-struct chip_operations mainboard_Iwill_dk8_htx_ops = {
+struct chip_operations mainboard_iwill_dk8_htx_ops = {
 	CHIP_NAME("Iwill DK8-HTX mainboard")
 };
 #endif

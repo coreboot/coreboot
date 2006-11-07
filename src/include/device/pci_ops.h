da145f2f9e011ef117de6ac0663297353970f897
Segher Boessenkool <segher@kernel.crashing.org>
2006-11-07 02:50:13 +0000

    kbuild: Fix Makefile syntax error
    Whitespace problems _can_ be harmful.  Oh how we all love "make".
    
    Signed-off-by: Segher Boessenkool <segher@kernel.crashing.org>
    
    
    git-svn-id: svn://coreboot.org/repository/LinuxBIOSv3@33 f3766cd6-281f-0410-b1cd-43a5c92072e9

--- src/include/device/pci_ops.h
+++ src/include/device/pci_ops.h
@@ -17,14 +17,14 @@
 #ifndef PCI_OPS_H
 #define PCI_OPS_H
 
-#include <stdint.h>
+#include <arch/types.h>
 #include <device/device.h>
 
-uint8_t  pci_read_config8(device_t dev, unsigned where);
-uint16_t pci_read_config16(device_t dev, unsigned where);
-uint32_t pci_read_config32(device_t dev, unsigned where);
-void pci_write_config8(device_t dev, unsigned where, uint8_t val);
-void pci_write_config16(device_t dev, unsigned where, uint16_t val);
-void pci_write_config32(device_t dev, unsigned where, uint32_t val);
+u8 pci_read_config8(device_t dev, unsigned where);
+u16 pci_read_config16(device_t dev, unsigned where);
+u32 pci_read_config32(device_t dev, unsigned where);
+void pci_write_config8(device_t dev, unsigned where, u8 val);
+void pci_write_config16(device_t dev, unsigned where, u16 val);
+void pci_write_config32(device_t dev, unsigned where, u32 val);
 
 #endif /* PCI_OPS_H */

2a44fbf2a014f48aceb3e601a391c7377d346caf
Uwe Hermann <uwe@hermann-uwe.de>
2006-12-01 15:25:21 +0000

    Status update (trivial).
    See http://www.linuxbios.org/pipermail/linuxbios/2006-November/017195.html
    
    Signed-off-by: Uwe Hermann <uwe@hermann-uwe.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2514 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/superio/ite/it8712f/it8712f.h
+++ src/superio/ite/it8712f/it8712f.h
@@ -19,7 +19,7 @@
  */
 
 /* Datasheet: http://www.ite.com.tw/product_info/PC/Brief-IT8712_2.asp */
-/* Status: Untested on real hardware, but it compiles. */
+/* Status: Com1 is tested and works. */
 
 #define IT8712F_FDC  0x00 /* Floppy */
 #define IT8712F_SP1  0x01 /* Com1 */

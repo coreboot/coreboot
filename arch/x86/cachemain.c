fb1793faca89f390a73c9ce9a2c0644d6227321d
Uwe Hermann <uwe@hermann-uwe.de>
2007-01-30 11:10:39 +0000

    Add a license header where it was missing. Use standard LinuxBIOS license header (trivial, no semantic changes).
    Signed-off-by: Uwe Hermann <uwe@hermann-uwe.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://coreboot.org/repository/LinuxBIOSv3@63 f3766cd6-281f-0410-b1cd-43a5c92072e9

--- arch/x86/cachemain.c
+++ arch/x86/cachemain.c
@@ -1,6 +1,20 @@
-/* GPLv2.
+/*
+ * This file is part of the LinuxBIOS project.
  *
- * Copyright (C) 2007 Stefan Reinauer <stepan@coresystems.de>, coresystems GmbH
+ * Copyright (C) 2007 Stefan Reinauer <stepan@coresystems.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
 #include <arch/types.h>

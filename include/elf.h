2393fcedfa1b05af37372221e941ce16da6cae39
Ronald G. Minnich <rminnich@gmail.com>
2007-02-25 21:11:30 +0000

    Put back FSF headers to ensure the provenance of this file is understood. This file has been tested and works with FILO 0.5.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>
    Acked-by: Ronald G. Minnich <rminnich@gmail.com>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    
    git-svn-id: svn://coreboot.org/repository/LinuxBIOSv3@123 f3766cd6-281f-0410-b1cd-43a5c92072e9

--- include/elf.h
+++ include/elf.h
@@ -1,29 +1,35 @@
-/*
- * Copright (C) 2001 Eric Biederman
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA, 02110-1301 USA
- *
+/* This file defines standard ELF types, structures, and macros.
+   Copyright (C) 1995-2003, 2004, 2005 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA. 
+ * 
+ * This file includes modifications made in  2001 by Eric Biederman for LinuxBIOS
  */
 
-#ifndef ELF_H
-#define	ELF_H
+#ifndef _ELF_H
+#define	_ELF_H 1
 
-/* Standard ELF types.  */
-
-#include <stdint.h>
 #include <stddef.h>
 #include <arch/elf.h>
+#include <stdint.h>
+
+__BEGIN_DECLS
+
+/* Standard ELF types.  */
 
 /* Type for a 16-bit quantity.  */
 typedef uint16_t Elf32_Half;

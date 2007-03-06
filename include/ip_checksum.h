3acfef5b84758ba89a7065efd70b75fc36e5e6b2
Uwe Hermann <uwe@hermann-uwe.de>
2007-03-06 14:46:57 +0000

    Update compute_ip_checksum.c and ip_checksum.h to the versions from kexec-tools 1.101 (the current version).
    Document the origin of the files in HACKING and in the files themselves.
    
    Signed-off-by: Uwe Hermann <uwe@hermann-uwe.de>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    
    git-svn-id: svn://coreboot.org/repository/LinuxBIOSv3@203 f3766cd6-281f-0410-b1cd-43a5c92072e9

--- include/ip_checksum.h
+++ include/ip_checksum.h
@@ -1,8 +1,29 @@
+/*
+ * This file is part of the LinuxBIOS project.
+ *
+ * It was taken from kexec-tools 1.101.
+ *
+ * Copyright (C) 2003,2004 Eric Biederman <ebiederm@xmission.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
 #ifndef IP_CHECKSUM_H
 #define IP_CHECKSUM_H
 
 unsigned long compute_ip_checksum(void *addr, unsigned long length);
 unsigned long add_ip_checksums(unsigned long offset, unsigned long sum, unsigned long new);
+unsigned long negate_ip_checksum(unsigned long sum);
 
 #endif /* IP_CHECKSUM_H */
-
--- lib/compute_ip_checksum.c
+++ lib/compute_ip_checksum.c
@@ -1,36 +1,78 @@
+/*
+ * This file is part of the LinuxBIOS project.
+ *
+ * It was taken from kexec-tools 1.101.
+ *
+ * Copyright (C) 2003,2004 Eric Biederman <ebiederm@xmission.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
 #include <stdint.h>
 #include <ip_checksum.h>
+#include <string.h>
 
 unsigned long compute_ip_checksum(void *addr, unsigned long length)
 {
-	uint8_t *ptr;
-	volatile union {
-		uint8_t  byte[2];
-		uint16_t word;
-	} value;
+	uint16_t *ptr;
 	unsigned long sum;
-	unsigned long i;
-	/* In the most straight forward way possible,
-	 * compute an ip style checksum.
-	 */
+	unsigned long len;
+	unsigned long laddr;
+	/* compute an ip style checksum */
+	laddr = (unsigned long )addr;
 	sum = 0;
-	ptr = addr;
-	for(i = 0; i < length; i++) {
-		unsigned long value;
-		value = ptr[i];
-		if (i & 1) {
-			value <<= 8;
-		}
-		/* Add the new value */
-		sum += value;
-		/* Wrap around the carry */
-		if (sum > 0xFFFF) {
-			sum = (sum + (sum >> 16)) & 0xFFFF;
-		}
+	if (laddr & 1) {
+		uint16_t buffer;
+		unsigned char *ptr;
+		/* copy the first byte into a 2 byte buffer.
+		 * This way automatically handles the endian question
+		 * of which byte (low or high) the last byte goes in.
+		 */
+		buffer = 0;
+		ptr = addr;
+		memcpy(&buffer, ptr, 1);
+		sum += buffer;
+		if (sum > 0xFFFF)
+			sum -= 0xFFFF;
+		length -= 1;
+		addr = ptr +1;
+		
 	}
-	value.byte[0] = sum & 0xff;
-	value.byte[1] = (sum >> 8) & 0xff;
-	return (~value.word) & 0xFFFF;
+	len = length >> 1;
+	ptr = addr;
+	while (len--) {
+		sum += *(ptr++);
+		if (sum > 0xFFFF)
+			sum -= 0xFFFF;
+	}
+	addr = ptr;
+	if (length & 1) {
+		uint16_t buffer;
+		unsigned char *ptr;
+		/* copy the last byte into a 2 byte buffer.
+		 * This way automatically handles the endian question
+		 * of which byte (low or high) the last byte goes in.
+		 */
+		buffer = 0;
+		ptr = addr;
+		memcpy(&buffer, ptr, 1);
+		sum += buffer;
+		if (sum > 0xFFFF)
+			sum -= 0xFFFF;
+	}
+	return (~sum) & 0xFFFF;
+	
 }
 
 unsigned long add_ip_checksums(unsigned long offset, unsigned long sum, unsigned long new)
@@ -50,4 +92,13 @@ unsigned long add_ip_checksums(unsigned long offset, unsigned long sum, unsigned
 		checksum -= 0xFFFF;
 	}
 	return (~checksum) & 0xFFFF;
+}
+
+unsigned long negate_ip_checksum(unsigned long sum)
+{
+	sum = ~sum & 0xFFFF;
+
+	sum = 0xFFFF - sum;
+
+	return ~sum & 0xFFFF;
 }

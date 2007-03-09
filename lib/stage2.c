4448a95a55883ee7b0eeb5a359406c05a7d71ff5
Uwe Hermann <uwe@hermann-uwe.de>
2007-03-09 02:13:49 +0000

    The file lib/stage2.c is a complete rewrite from scratch and doesn't really share any code with LinuxBIOSv2. Thus:
     * Update the copyright year for the code by Ron Minnich to 2007.
    
     * Add missing copyright line for the changes made by Stefan Reinauer.
    
     * Drop LANL text which is a left-over from V2 times and doesn't apply
       anymore here, as this is a complete rewrite.
    
    Needs an ack by both, Ron and Stefan.
    
    Signed-off-by: Uwe Hermann <uwe@hermann-uwe.de>
    Acked-by: Ronald G. Minnich <rminnich@gmail.com>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    
    git-svn-id: svn://coreboot.org/repository/LinuxBIOSv3@225 f3766cd6-281f-0410-b1cd-43a5c92072e9

--- lib/stage2.c
+++ lib/stage2.c
@@ -1,7 +1,10 @@
 /*
  * This file is part of the LinuxBIOS project.
  *
- * Copyright (C) 2000 Ron Minnich <rminnich@lanl.gov>
+ * Copyright (C) 2007 Ron Minnich <rminnich@lanl.gov>
+ *
+ * Copyright (C) 2007 coresystems GmbH
+ * Written by Stefan Reinauer <stepan@coresystems.de> for coresystems GmbH.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,25 +21,6 @@
  */
 
 /* stage2 - LinuxBIOS ram based setup */
-
-/*
-This software and ancillary information (herein called SOFTWARE)
-called LinuxBIOS is made available under the terms described
-here.  The SOFTWARE has been approved for release with associated
-LA-CC Number 00-34.  Unless otherwise indicated, this SOFTWARE has
-been authored by an employee or employees of the University of
-California, operator of the Los Alamos National Laboratory under
-Contract No. W-7405-ENG-36 with the U.S. Department of Energy.  The
-U.S. Government has rights to use, reproduce, and distribute this
-SOFTWARE.  The public may copy, distribute, prepare derivative works
-and publicly display this SOFTWARE without charge, provided that this
-Notice and any statement of authorship are reproduced on all copies.
-Neither the Government nor the University makes any warranty, express
-or implied, or assumes any liability or responsibility for the use of
-this SOFTWARE.  If SOFTWARE is modified to produce derivative works,
-such modified SOFTWARE should be clearly marked, so as not to confuse
-it with the version available from LANL.
- */
 
 /*
  * C Bootstrap code for the LinuxBIOS

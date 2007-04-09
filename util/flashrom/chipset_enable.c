4e5cc9dcd03ef8705e5db462229f24be0e3dce9a
Randall Philipson <rtphilipson@cox.net>
2007-04-09 22:27:45 +0000

    add support for CX700 builtin southbridge Signed-off-by: Randall Philipson <rtphilipson@cox.net> Acked-by: Ronald G. Minnich <rminnich@gmail.com> Acked-by: Stefan Reinauer <stepan@coresystems.de>
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2599 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- util/flashrom/chipset_enable.c
+++ util/flashrom/chipset_enable.c
@@ -410,6 +410,7 @@ static FLASH_ENABLE enables[] = {
 	{0x1106, 0x8231, "VT8231", enable_flash_vt823x},
 	{0x1106, 0x3177, "VT8235", enable_flash_vt823x},
 	{0x1106, 0x3227, "VT8237", enable_flash_vt823x},
+	{0x1106, 0x8324, "CX700" , enable_flash_vt823x},
 	{0x1106, 0x0686, "VT82C686", enable_flash_amd8111},
 	{0x1078, 0x0100, "CS5530", enable_flash_cs5530},
 	{0x100b, 0x0510, "SC1100", enable_flash_sc1100},

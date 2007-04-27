989de36703684e3686de730b1a56e41eeca5d5d5
Ward Vandewege <ward@gnu.org>
2007-04-27 21:24:53 +0000

    Add fan control support to ITE IT8716F.
    Signed-off-by: Ward Vandewege <ward@gnu.org>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2620 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/superio/ite/it8716f/superio.c
+++ src/superio/ite/it8716f/superio.c
@@ -23,6 +23,7 @@
 
 #include <device/device.h>
 #include <device/pnp.h>
+#include <console/console.h>
 #include <uart8250.h>
 #include <pc80/keyboard.h>
 #include "chip.h"
@@ -49,6 +50,31 @@ static void pnp_exit_ext_func_mode(device_t dev)
 	pnp_write_config(dev, 0x02, 0x02);
 }
 
+static void pnp_write_index(unsigned long port_base, uint8_t reg, uint8_t value)
+{
+	outb(reg, port_base);
+	outb(value, port_base + 1);
+}
+
+static uint8_t pnp_read_index(unsigned long port_base, uint8_t reg)
+{
+	outb(reg, port_base);
+	return inb(port_base + 1);
+}
+
+static void init_ec(unsigned long base)
+{
+	uint8_t value;
+
+	// Read out current value of FAN_CTL Control register (0x14)
+	value = pnp_read_index(base, 0x14);
+	printk_debug("FAN_CTL: reg = 0x%04x, read value = 0x%02x\r\n",base + 0x14, value);
+	
+	// Set FAN_CTL Control register (0x14) polarity to High, and activate fans 1, 2 and 3
+	pnp_write_index(base, 0x14, value | 0x87);
+	printk_debug("FAN_CTL: reg = 0x%04x, writing value = 0x%02x\r\n",base + 0x14, value | 0x87);
+}
+
 static void it8716f_init(device_t dev)
 {
 	struct superio_ite_it8716f_config *conf;
@@ -60,7 +86,7 @@ static void it8716f_init(device_t dev)
 
 	conf = dev->chip_info;
 
-	/* TODO: FDC, PP, EC, KBCM, MIDI, GAME, IR. */
+	/* TODO: FDC, PP, KBCM, MIDI, GAME, IR. */
 	switch (dev->path.u.pnp.device) {
 	case IT8716F_SP1:
 		res0 = find_resource(dev, PNP_IDX_IO0);
@@ -69,6 +95,11 @@ static void it8716f_init(device_t dev)
 	case IT8716F_SP2:
 		res0 = find_resource(dev, PNP_IDX_IO0);
 		init_uart8250(res0->base, &conf->com2);
+		break;
+	case IT8716F_EC:
+		res0 = find_resource(dev, PNP_IDX_IO0);
+#define EC_INDEX_PORT 5
+		init_ec(res0->base + EC_INDEX_PORT);
 		break;
 	case IT8716F_KBCK:
 		res0 = find_resource(dev, PNP_IDX_IO0);
@@ -113,7 +144,7 @@ static struct pnp_info pnp_dev_info[] = {
 	{ &ops, IT8716F_SP1, PNP_IO0 | PNP_IRQ0, { 0x7f8, 0 }, },
 	{ &ops, IT8716F_SP2, PNP_IO0 | PNP_IRQ0, { 0x7f8, 0 }, },
 	{ &ops, IT8716F_PP, PNP_IO0 | PNP_IRQ0 | PNP_DRQ0, { 0x07f8, 0}, },
-	{ &ops, IT8716F_EC, },
+	{ &ops, IT8716F_EC, PNP_IO0 | PNP_IO1 | PNP_IRQ0, { 0x7f8, 0 }, { 0x7f8, 0x4}, },
 	{ &ops, IT8716F_KBCK, PNP_IO0 | PNP_IO1 | PNP_IRQ0, { 0x7ff, 0 }, { 0x7ff, 0x4}, },
 	{ &ops, IT8716F_KBCM, PNP_IRQ0, },
 	// No 7 { 0,},

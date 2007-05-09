a846a5effcf46afac9f65df49ce5350b36dc1096
Roger Zauner <roger@eskimo.com>
2007-05-09 17:54:25 +0000

    The superio needs 0x87 sent twice to 0x3f0 to enable extended function (power-on strapping). Although this is already done in superio.c, it's not being done when w83977tf_early_serial.c is executed. As such, no console_init() without it.
    Signed-off-by: Roger Zauner <roger@eskimo.com>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2645 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/superio/winbond/w83977tf/w83977tf_early_serial.c
+++ src/superio/winbond/w83977tf/w83977tf_early_serial.c
@@ -1,11 +1,25 @@
 #include <arch/romcc_io.h>
 #include "w83977tf.h"
 
-
-static void w83977tf_enable_serial(device_t dev, unsigned iobase)
+static inline void pnp_enter_ext_func_mode(device_t dev)
 {
+	unsigned int port = dev >> 8;
+	outb(0x87, port);
+	outb(0x87, port);
+}
+
+static void pnp_exit_ext_func_mode(device_t dev)
+{
+	unsigned int port = dev >> 8;
+	outb(0xaa, port);
+}
+
+static void w83977tf_enable_serial(device_t dev, unsigned int iobase)
+{
+	pnp_enter_ext_func_mode(dev);
 	pnp_set_logical_device(dev);
 	pnp_set_enable(dev, 0);
 	pnp_set_iobase(dev, PNP_IDX_IO0, iobase);
 	pnp_set_enable(dev, 1);
+	pnp_exit_ext_func_mode(dev);
 }

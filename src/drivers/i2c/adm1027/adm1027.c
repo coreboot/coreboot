4aae668fed2dc32b86009b283d880c68d56d1369
Ward Vandewege <ward@gnu.org>
2007-05-11 19:23:57 +0000

    Clean up whitespace in preparation for another patch to fix fan control.
    This is nothing more than the result of running
    
      indent -npro -kr -i8 -ts8 -sob -l80 -ss -ncs adm1027.c
    
    Signed-off-by: Ward Vandewege <ward@gnu.org>
    Signed-off-by: Uwe Hermann <uwe@hermann-uwe.de>
    Acked-by: Ronald G. Minnich <rminnich@gmail.com>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2663 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/drivers/i2c/adm1027/adm1027.c
+++ src/drivers/i2c/adm1027/adm1027.c
@@ -8,58 +8,59 @@
 #include <cpu/x86/msr.h>
 #include "chip.h"
 
-#define ADM1027_REG_CONFIG1 0x40
-#define CFG1_STRT     	0x01
-#define CFG1_LOCK  	0x02
-#define CFG1_RDY	0x04
-#define CFG1_FSPD       0x08
-#define CFG1_VXI	0x10
-#define CFT1_FSPDIS     0x20
-#define CFG1_TODIS     	0x40
-#define CFG1_VCC	0x80
-#define ADM1027_REG_CONFIG2 0x73
-#define ADM1027_REG_CONFIG3 0x78
+#define ADM1027_REG_CONFIG1	0x40
+#define CFG1_STRT		0x01
+#define CFG1_LOCK		0x02
+#define CFG1_RDY		0x04
+#define CFG1_FSPD		0x08
+#define CFG1_VXI		0x10
+#define CFT1_FSPDIS		0x20
+#define CFG1_TODIS		0x40
+#define CFG1_VCC		0x80
+#define ADM1027_REG_CONFIG2	0x73
+#define ADM1027_REG_CONFIG3	0x78
 
 static void adm1027_enable_monitoring(device_t dev)
-{        
-        int result;
-        result = smbus_read_byte(dev, ADM1027_REG_CONFIG1);
-       
-	if(!(result & CFG1_RDY) ) { 
+{
+	int result;
+
+	result = smbus_read_byte(dev, ADM1027_REG_CONFIG1);
+
+	if (!(result & CFG1_RDY)) {
 		printk_debug("ADM1027: monitoring not ready\r\n");
 		return;
 	}
-        result = (result | CFG1_STRT);
-        result = smbus_write_byte(dev, ADM1027_REG_CONFIG1, result);
-        
-        result = smbus_read_byte(dev, ADM1027_REG_CONFIG1);
-        if (!(result & CFG1_STRT)) {
-                printk_debug("ADM1027: monitoring would not enable\r\n");
-        }
+	result = (result | CFG1_STRT);
+	result = smbus_write_byte(dev, ADM1027_REG_CONFIG1, result);
+
+	result = smbus_read_byte(dev, ADM1027_REG_CONFIG1);
+	if (!(result & CFG1_STRT)) {
+		printk_debug("ADM1027: monitoring would not enable\r\n");
+	}
 	printk_debug("ADM1027: monitoring enabled\r\n");
 }
 
 static void adm1027_init(device_t dev)
 {
-	if (dev->enabled && dev->path.type == DEVICE_PATH_I2C)
-	{
-		if(ops_smbus_bus(get_pbus_smbus(dev))) {
-			if( dev->bus->dev->path.type == DEVICE_PATH_I2C) smbus_set_link(dev); // it is under mux 
+	if (dev->enabled && dev->path.type == DEVICE_PATH_I2C) {
+		if (ops_smbus_bus(get_pbus_smbus(dev))) {
+			if (dev->bus->dev->path.type == DEVICE_PATH_I2C)
+				smbus_set_link(dev);	// it is under mux 
 			adm1027_enable_monitoring(dev);
 		}
-		
-	}
 
+	}
 }
+
 static void adm1027_noop(device_t dummy)
 {
 }
 
 static struct device_operations adm1027_operations = {
-        .read_resources   = adm1027_noop,
-        .set_resources    = adm1027_noop,
-        .enable_resources = adm1027_noop,
-        .init             = adm1027_init,
+	.read_resources		= adm1027_noop,
+	.set_resources		= adm1027_noop,
+	.enable_resources	= adm1027_noop,
+	.init			= adm1027_init,
 };
 
 static void enable_dev(struct device *dev)
@@ -69,5 +70,5 @@ static void enable_dev(struct device *dev)
 
 struct chip_operations drivers_i2c_adm1027_ops = {
 	CHIP_NAME("adm1027")
-	.enable_dev = enable_dev, 
+	.enable_dev = enable_dev,
 };

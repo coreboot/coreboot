84448165f96b10ddd63a8b111d3eff442f5a55d6
Ronald G. Minnich <rminnich@gmail.com>
2007-06-27 20:09:47 +0000

    Superio commits. Signed-off-by: Ronald G. Minnich <rminnich@gmail.com> Acked-by: Stefan Reinauer <stepan@coresystems.de>
    git-svn-id: svn://coreboot.org/repository/LinuxBIOSv3@380 f3766cd6-281f-0410-b1cd-43a5c92072e9

--- superio/winbond/w83627hf/superio.c
+++ superio/winbond/w83627hf/superio.c
@@ -1,9 +1,24 @@
-/* Copyright 2000  AG Electronics Ltd. 
+/*
+ * This file is part of the LinuxBIOS project.
+ *
+ * Copyright 2000  AG Electronics Ltd. 
  * Copyright 2003-2004 Linux Networx 
  * Copyright 2004 Tyan 
  * By LYH change from PC87360 
  * Copyright 2007 coresystems GmbH 
- * This code is distributed without warranty under the GPL v2 (see COPYING) 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
 #include <io.h>
@@ -15,7 +30,7 @@
 #include <uart8250.h>
 #include <keyboard.h>
 // #include <pc80/mc146818rtc.h>
-#include "chip.h"
+#include <statictree.h>
 #include "w83627hf.h"
 
 
@@ -101,6 +116,7 @@ static void w83627hf_init(struct device * dev)
 {
 	struct superio_winbond_w83627hf_config *conf;
 	struct resource *res0, *res1;
+	struct pc_keyboard keyboard;
 
 #if 1
 	printk(BIOS_ERR, "dummy init XXXX\n");
@@ -125,7 +141,7 @@ static void w83627hf_init(struct device * dev)
 	case W83627HF_KBC:
 		res0 = find_resource(dev, PNP_IDX_IO0);
 		res1 = find_resource(dev, PNP_IDX_IO1);
-		init_pc_keyboard(res0->base, res1->base, &conf->keyboard);
+		init_pc_keyboard(res0->base, res1->base, &keyboard);
 		break;
         case W83627HF_HWM:
                 res0 = find_resource(dev, PNP_IDX_IO0);
--- superio/winbond/w83627hf/w83627hf_early_serial.c
+++ superio/winbond/w83627hf/w83627hf_early_serial.c
@@ -1,22 +1,36 @@
+/*
+ * This file is part of the LinuxBIOS project.
+ *
+ * Copyright 2000  AG Electronics Ltd. 
+ * Copyright 2003-2004 Linux Networx 
+ * Copyright 2004 Tyan 
+ * By LYH change from PC87360 
+ * Copyright 2007 coresystems GmbH 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+#include <io.h>
+#include <device/pnp.h>
 #include "w83627hf.h"
 
-static inline void pnp_enter_ext_func_mode(struct device *dev) 
+void w83627hf_enable_serial(u8 dev, u8 serial, u16 iobase)
 {
-	unsigned port = dev>>8;
-        outb(0x87, port);
-        outb(0x87, port);
-}
-static void pnp_exit_ext_func_mode(struct device *dev) 
-{
-	unsigned port = dev>>8;
-        outb(0xaa, port);
-}
-static void w83627hf_enable_serial(struct device *dev, unsigned iobase)
-{
-	pnp_enter_ext_func_mode(dev);
-	pnp_set_logical_device(dev);
-	pnp_set_enable(dev, 0);
-	pnp_set_iobase(dev, PNP_IDX_IO0, iobase);
-	pnp_set_enable(dev, 1);
-	pnp_exit_ext_func_mode(dev);
+	rawpnp_enter_ext_func_mode(dev);
+	rawpnp_set_logical_device(dev,serial);
+	rawpnp_set_enable(dev, 0);
+	rawpnp_set_iobase(dev, PNP_IDX_IO0, iobase);
+	rawpnp_set_enable(dev, 1);
+	rawpnp_exit_ext_func_mode(dev);
 }

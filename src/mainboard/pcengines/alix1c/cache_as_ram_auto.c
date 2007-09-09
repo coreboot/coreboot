cbc95f32fb1f3208dbf7cbe08ed1f19f96c28fe6
Ronald G. Minnich <rminnich@gmail.com>
2007-09-09 19:43:31 +0000

    Partial changes and fixup. Removed reset.c and added copyright headers. Remove debug.c. It is not used and should not be here.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2766 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/pcengines/alix1c/cache_as_ram_auto.c
+++ src/mainboard/pcengines/alix1c/cache_as_ram_auto.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the LinuxBIOS project.
+ *
+ * Copyright (C) 2007 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
 #define ASSEMBLY 1
 
 #include <stdint.h>
@@ -92,8 +111,8 @@ void cache_as_ram_main(void)
 
 	sdram_initialize(1, memctrl);
 
-	/* Check all of memory */
-	ram_check(0x00000000, 640*1024);
+	/* Check memory */
+	ram_check(0x00000000, 640 * 1024);
 
 	/* Switch from Cache as RAM to real RAM */
 	/* There are two ways we could think about this.
--- src/mainboard/pcengines/alix1c/mainboard.c
+++ src/mainboard/pcengines/alix1c/mainboard.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the LinuxBIOS project.
+ *
+ * Copyright (C) 2007 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
 #include <console/console.h>
 #include <device/device.h>
 #include <device/pci.h>
@@ -135,20 +154,19 @@ void print_conf(void) {
 #endif //DEFAULT_CONSOLE_LOGLEVEL >= BIOS_ERR
 }
 
-static void init(struct device *dev) {
-
+static void init(struct device *dev)
+{
 	printk_debug("ALIX1C ENTER %s\n", __FUNCTION__);
-
 	printk_debug("ALIX1C EXIT %s\n", __FUNCTION__);
 }
 
 static void enable_dev(struct device *dev)
 {
-        dev->ops->init = init;
+	dev->ops->init = init;
 }
 
 struct chip_operations mainboard_pcengines_alix1c_ops = {
-	CHIP_NAME("PC ENGINES  ALIX1C Mainboard")
-        .enable_dev = enable_dev,
+	CHIP_NAME("PC Engines ALIX1.C Mainboard")
+	.enable_dev = enable_dev,
 };
 

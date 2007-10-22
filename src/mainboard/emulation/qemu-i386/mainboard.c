d56981f778a0f2de5d31af658c6589672126a32b
Stefan Reinauer <stepan@coresystems.de>
2007-10-22 10:07:46 +0000

    This is a hack for easier testing of GRUB2 in LinuxBIOSv2 since it is still our most wide-spread codebase.
    The patch is pretty trivial, and nobody except Torsten even looked at
    it in a week, so....
    
    Signed-off-by: Stefan Reinauer <stepan@coresystems.de>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2880 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/emulation/qemu-i386/mainboard.c
+++ src/mainboard/emulation/qemu-i386/mainboard.c
@@ -1,5 +1,37 @@
+#include <console/console.h>
 #include <device/device.h>
+#include <device/pci.h>
+#include <device/pci_ids.h>
+#include <device/pci_ops.h>
+#include <arch/io.h>
 #include "chip.h"
+
+void vga_enable_console();
+
+static void vga_init(device_t dev)
+{
+	/* code to make vga init run in real mode - does work but against the current Linuxbios philosophy */
+	printk_debug("INSTALL REAL-MODE IDT\n");
+        setup_realmode_idt();
+        printk_debug("DO THE VGA BIOS\n");
+        do_vgabios();
+
+	vga_enable_console();
+}
+
+static struct device_operations vga_operations = {
+	.read_resources   = pci_dev_read_resources,
+	.set_resources    = pci_dev_set_resources,
+	.enable_resources = pci_dev_enable_resources,
+	.init             = vga_init,
+	.ops_pci          = 0,
+};
+
+static struct pci_driver vga_driver __pci_driver = {
+	.ops = &vga_operations,
+	.vendor = 0x1013,
+	.device = 0x00b8,
+};
 
 struct chip_operations mainboard_emulation_qemu_i386_ops = {
 	CHIP_NAME("QEMU Mainboard")

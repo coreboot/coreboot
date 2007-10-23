8b8383611541978804bc7660503a091bef358d5a
Stefan Reinauer <stepan@coresystems.de>
2007-10-23 18:59:21 +0000

    Add support for the Intel mFCPGA 478 socket. (trivial)
    Signed-off-by: Stefan Reinauer <stepan@coresystems.de>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@2888 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/cpu/intel/socket_mFCPGA478/chip.h
@@ -0,0 +1,4 @@
+extern struct chip_operations cpu_intel_socket_mFCPGA478_ops;
+
+struct cpu_intel_socket_mFCPGA478_config {
+};
---
+++ src/cpu/intel/socket_mFCPGA478/socket_mFCPGA478.c
@@ -0,0 +1,7 @@
+#include <device/device.h>
+#include "chip.h"
+
+
+struct chip_operations cpu_intel_socket_mFCPGA478_ops = {
+	CHIP_NAME("Socket mFCPGA478 CPU")
+};

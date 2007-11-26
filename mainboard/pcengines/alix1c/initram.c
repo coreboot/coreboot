9d16f2066e0859766d5473942d7ca0f5a11fa14c
Uwe Hermann <uwe@hermann-uwe.de>
2007-11-26 22:12:12 +0000

    Initial support for the PC Engines ALIX1.C board.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://coreboot.org/repository/LinuxBIOSv3@521 f3766cd6-281f-0410-b1cd-43a5c92072e9

---
+++ mainboard/pcengines/alix1c/initram.c
@@ -0,0 +1,76 @@
+/*
+ * This file is part of the LinuxBIOS project.
+ *
+ * Copyright (C) 2007 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#define _MAINOBJECT
+
+#include <types.h>
+#include <lib.h>
+#include <console.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <string.h>
+#include <msr.h>
+#include <io.h>
+#include <amd_geodelx.h>
+#include <northbridge/amd/geodelx/raminit.h>
+
+#define MANUALCONF 0		/* Do automatic strapped PLL config. */
+
+#define PLLMSRHI 0x00001490	/* Manual settings for the PLL */
+#define PLLMSRLO 0x02000030
+
+#define DIMM0 ((u8) 0xA0)
+#define DIMM1 ((u8) 0xA2)
+
+/**
+ * Placeholder in case we ever need it. Since this file is a template for
+ * other boards, we want this here and we want the call in the right place.
+ */
+static void mb_gpio_init(void)
+{
+	/* Early mainboard specific GPIO setup */
+}
+
+/**
+ * Main for initram for the PC Engines ALIX1.C. It might seem that you could
+ * somehow do these functions in, e.g., the CPU code, but the order of
+ * operations and what those operations are is VERY strongly mainboard
+ * dependent. It's best to leave it in the mainboard code.
+ */
+int main(void)
+{
+	u8 smb_devices[] = { DIMM0, DIMM1 };
+
+	post_code(POST_START_OF_MAIN);
+
+	system_preinit();
+	mb_gpio_init();
+	pll_reset(MANUALCONF, PLLMSRHI, PLLMSRLO);
+	cpu_reg_init(0, DIMM0, DIMM1);
+
+	sdram_set_registers();
+	sdram_set_spd_registers(DIMM0, DIMM1);
+	sdram_enable(DIMM0, DIMM1);
+
+	/* Check memory. */
+	/* ram_check(0, 640 * 1024); */
+
+	return 0;
+}
---
+++ mainboard/pcengines/alix1c/stage1.c
@@ -0,0 +1,47 @@
+/*
+ * This file is part of the LinuxBIOS project.
+ *
+ * Copyright (C) 2007 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include <types.h>
+#include <lib.h>
+#include <console.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <string.h>
+#include <msr.h>
+#include <io.h>
+#include <amd_geodelx.h>
+#include <southbridge/amd/cs5536/cs5536.h>
+
+void hardware_stage1(void)
+{
+	post_code(POST_START_OF_MAIN);
+
+	geodelx_msr_init();
+
+	cs5536_stage1();
+
+	/* NOTE: Must do this AFTER the early_setup! It is counting on some
+	 * early MSR setup for the CS5536. We do this early for debug.
+	 * Real setup should be done in chipset init via Config.lb.
+	 *
+	 * TODO: Drop Config.lb reference, update comment.
+	 */
+	cs5536_setup_onchipuart();
+}

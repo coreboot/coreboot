8d2ed3010bb601ecd01a727203e3c1ee0f23c8d3
Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
2007-12-03 20:41:02 +0000

    Ron mentioned he had strange hangs in pll_reset on various targets. This may be due to miscompilation of XIP objects which do not have _MAINBOBJECT defined. This issue was impossible to see on qemu because no such object existed. Introduce initram_printktest.c in the Qemu target, which will test for miscompilation and crash with a descriptive error message.
    This has been build tested and runtime tested on Qemu, and with my
    compiler/linker combination it indeed crashes.
    gcc (GCC) 4.2.1 (SUSE Linux)
    GNU ld (GNU Binutils) 2.17.50.20070726-14 (SUSE Linux)
    
    Trying with gcc-4.1 (GCC) 4.1.3 20070724 (prerelease) (SUSE Linux) and
    the linker above had exactly the same results.
    
    Unless we manage to fix the bug uncovered by this patch, leaving the
    Qemu target in crashing state is the best thing we can do because this
    behaviour mirrors the state of all other targets.
    
    Ron says: I am comfortable with this. If hardware is broken, qemu should
    be broken. I avidly wait the fix :-)
    
    Signed-off-by: Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
    Acked-by: Ronald G. Minnich <rminnich@gmail.com>
    
    
    git-svn-id: svn://coreboot.org/repository/LinuxBIOSv3@535 f3766cd6-281f-0410-b1cd-43a5c92072e9

--- mainboard/emulation/qemu-x86/initram_printktest.c
+++ mainboard/emulation/qemu-x86/initram_printktest.c
@@ -1,7 +1,6 @@
 /*
  * This file is part of the LinuxBIOS project.
  *
- * Copyright (C) 2007 Stefan Reinauer <stepan@coresystems.de>
  * Copyright (C) 2007 Carl-Daniel Hailfinger
  *
  * This program is free software; you can redistribute it and/or modify
@@ -18,33 +17,15 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
-#define _MAINOBJECT
-
 #include <console.h>
 
-int printktest(void);
-
-/* printktest1() is here to increase the likelihood of main() not ending up at
- * the beginning of the file, so we can check whether the entry point at main()
- * was honored.
- */
-int printktest1(void)
+int printktest(void)
 {
-	printk(BIOS_INFO, "printktest1: If the immediately preceding line does"
-		" not say \"Nothing to do.\", then execution did not start at"
-		" main()\n");
-
-	return 0;
-}
-
-int main(void)
-{
-	printk(BIOS_INFO, "RAM init code started.\n");
-	printk(BIOS_INFO, "Nothing to do.\n");
-	printktest1();
-	printk(BIOS_INFO, "Trying absolute call from non-_MAINOBJECT XIP code.\n");
-	printktest();
-	printk(BIOS_INFO, "Done.\n");
+	/* If printk succeeds, it will print the message below. This is not a
+	 * success message after a test, but a success message used as test.
+	 * In case of compiler/linker bugs the printk call is likely to crash.
+	 */
+	printk(BIOS_INFO, "Absolute call successful.\n");
 
 	return 0;
 }

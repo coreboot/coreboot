7980349f4f89db004fac5131166f2ba5fa0048fa
Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
2008-02-15 23:58:09 +0000

    Remove dead code protected by #if 0 since it appeared.
    Signed-off-by: Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>
    Acked-by: Marc Jones <marc.jones@amd.com> 
    
    
    git-svn-id: svn://coreboot.org/repository/coreboot-v3@602 f3766cd6-281f-0410-b1cd-43a5c92072e9

--- include/arch/x86/pciconf.h
+++ include/arch/x86/pciconf.h
@@ -8,15 +8,6 @@
 #define	PCI_CONF_REG_INDEX	0xcf8
 #define	PCI_CONF_REG_DATA	0xcfc
 
-/* WTF for now */
-#if 0
-#if PCI_IO_CFG_EXT == 0
-#define CONFIG_ADDR(bus,devfn,where) (((bus) << 16) | ((devfn) << 8) | (where))
-#else
-#define CONFIG_ADDR(bus,devfn,where) (((bus) << 16) | ((devfn) << 8) | (where & 0xff) | ((where & 0xf00)<<16) )
-#endif
-#endif
-#define CONFIG_ADDR(bus,devfn,where) (((bus) << 16) | ((devfn) << 8) | (where))
 #endif
 
 #endif /* ARCH_X86_PCICONF_H */

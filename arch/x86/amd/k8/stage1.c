coreboot/coreboot

commit a492ff7cc976efd825d4ab354823ecaa901e2f71

Kill off stage1_mtrr.c completely, and bring in mtrr.c for stage2 from v2. Signed-off-by: Corey Osgood <corey.osgood@gmail.com>
It could use some cleanup, but looks good.

Acked-by: Peter Stuge <peter@stuge.se>

With some cleanup.

Acked-by: Ronald G. Minnich <rminnich@gmail.com>




git-svn-id: svn://coreboot.org/repository/coreboot-v3@1085 f3766cd6-281f-0410-b1cd-43a5c92072e9

include/arch/x86/mtrr.h
@@ -35,7 +35,6 @@
 void x86_setup_var_mtrrs(unsigned address_bits);
 void x86_setup_mtrrs(unsigned address_bits);
 int x86_mtrr_check(void);
-void early_mtrr_init(void);
 
 #endif
 

mainboard/amd/dbm690t/Makefile
@@ -22,7 +22,6 @@
 			
 STAGE0_MAINBOARD_SRC := $(src)/lib/clog2.c \
 			$(src)/mainboard/$(MAINBOARDDIR)/stage1.c \
-			$(src)/arch/x86/stage1_mtrr.c \
 			$(src)/arch/x86/amd/model_fxx/dualcore_id.c \
 			$(src)/arch/x86/amd/model_fxx/stage1.c \
 			$(src)/northbridge/amd/k8/get_nodes.c \
@@ -36,7 +35,6 @@ INITRAM_SRC= $(src)/mainboard/$(MAINBOARDDIR)/initram.c \
 			$(src)/northbridge/amd/k8/incoherent_ht.c \
 			$(src)/northbridge/amd/k8/coherent_ht.c \
 			$(src)/arch/x86/pci_ops_conf1.c \
-			$(src)/arch/x86/stage1_mtrr.c \
 			$(src)/arch/x86/amd/model_fxx/dualcore.c \
 			$(src)/arch/x86/amd/model_fxx/fidvid.c \
 			$(src)/arch/x86/amd/model_fxx/init_cpus.c \

mainboard/amd/serengeti/Makefile
@@ -22,7 +22,6 @@
 			
 STAGE0_MAINBOARD_SRC := $(src)/lib/clog2.c \
 			$(src)/mainboard/$(MAINBOARDDIR)/stage1.c \
-			$(src)/arch/x86/stage1_mtrr.c \
 			$(src)/arch/x86/amd/model_fxx/dualcore_id.c \
 			$(src)/arch/x86/amd/model_fxx/stage1.c \
 			$(src)/arch/x86/resourcemap.c \
@@ -42,7 +41,6 @@ INITRAM_SRC= $(src)/mainboard/$(MAINBOARDDIR)/initram.c \
 			$(src)/northbridge/amd/k8/incoherent_ht.c \
 			$(src)/northbridge/amd/k8/util.c \
 			$(src)/arch/x86/pci_ops_conf1.c \
-			$(src)/arch/x86/stage1_mtrr.c \
 			$(src)/southbridge/amd/amd8111/stage1_smbus.c \
 			$(src)/arch/x86/amd/model_fxx/dualcore.c \
 			$(src)/arch/x86/amd/model_fxx/fidvid.c \

mainboard/gigabyte/m57sli/Makefile
@@ -22,7 +22,6 @@
 STAGE0_MAINBOARD_SRC := $(src)/lib/clog2.c \
 			$(src)/mainboard/$(MAINBOARDDIR)/stage1.c \
 			$(src)/arch/x86/resourcemap.c \
-			$(src)/arch/x86/stage1_mtrr.c \
 			$(src)/arch/x86/amd/model_fxx/dualcore_id.c \
 			$(src)/arch/x86/amd/model_fxx/stage1.c \
 			$(src)/northbridge/amd/k8/get_nodes.c \

arch/x86/Makefile
@@ -205,7 +205,11 @@ STAGE2_LIB_SRC       = stage2.c clog2.c mem.c tables.c delay.c \
 
 STAGE2_ARCH_X86_SRC  = archtables.c coreboot_table.c multiboot.c udelay_io.c
 STAGE2_ARCH_X86_SRC += pci_ops_auto.c pci_ops_conf1.c 
-STAGE2_ARCH_X86_SRC += keyboard.c i8259.c isa-dma.c
+STAGE2_ARCH_X86_SRC += keyboard.c i8259.c isa-dma.c 
+
+ifeq ($(CONFIG_MTRR),y)
+STAGE2_ARCH_X86_SRC += mtrr.c
+endif
 
 ifeq ($(CONFIG_PIRQ_TABLE),y)
 STAGE2_ARCH_X86_SRC += pirq_routing.c

mainboard/jetway/j7f2/Makefile
@@ -23,7 +23,6 @@
 STAGE0_MAINBOARD_SRC := $(src)/mainboard/$(MAINBOARDDIR)/stage1.c \
 			$(src)/northbridge/via/cn700/stage1.c \
 			$(src)/southbridge/via/vt8237/stage1.c \
-			$(src)/arch/x86/stage1_mtrr.c
 
 INITRAM_SRC =   	$(src)/mainboard/$(MAINBOARDDIR)/initram.c \
 			$(src)/northbridge/via/cn700/initram.c \

mainboard/via/epia-cn/Makefile
@@ -19,7 +19,6 @@
 ##
 
 STAGE0_MAINBOARD_SRC :=  $(src)/lib/clog2.c \
-			$(src)/arch/x86/stage1_mtrr.c \
 			$(src)/mainboard/$(MAINBOARDDIR)/stage1.c \
 			$(src)/southbridge/via/vt8237/stage1.c \
 			$(src)/northbridge/via/cn700/stage1.c \

arch/x86/Kconfig
@@ -191,6 +191,15 @@ config CARSIZE
 	help
 	  This option sets the size of the area used for CAR.
 
+config MTRR
+	bool
+	default n if CPU_AMD_GEODELX
+	default y if CPU_AMD_K8
+	default y if CPU_VIA_C7
+	default y if CPU_INTEL_CORE2
+	help
+	  This option indicates if a cpu has MTRR support.
+
 config CBMEMK
 	hex
 	default 0x1000 if CPU_I586

commit 034ea337970ac05c34c706c3efc30416f0f50559

Fix breakage of k8 targets caused by r1085. Thanks to Myles Watson for tracking down the offending commit.
Signed-off-by: Corey Osgood <corey.osgood@gmail.com>
Acked-by: Peter Stuge <peter@stuge.se>



git-svn-id: svn://coreboot.org/repository/coreboot-v3@1088 f3766cd6-281f-0410-b1cd-43a5c92072e9

arch/x86/amd/k8/stage1.c
@@ -35,7 +35,7 @@
  */
 void set_init_ram_access(void)
 {
-	set_var_mtrr(0, 0x00000000, CONFIG_CBMEMK << 10, MTRR_TYPE_WRBACK);
+	stage1_set_var_mtrr(0, 0x00000000, CONFIG_CBMEMK << 10, MTRR_TYPE_WRBACK);
 }
 
 /**

arch/x86/stage1_mtrr.c
@@ -25,7 +25,7 @@ void disable_var_mtrr(unsigned int reg)
 	wrmsr(MTRRphysMask_MSR(reg), zero);
 }
 
-void set_var_mtrr(
+void stage1_set_var_mtrr(
 	unsigned long reg, unsigned long base, unsigned long size, unsigned long type)
 
 {
@@ -63,7 +63,7 @@ void cache_cbmem(int type)
 {
 	/* Enable caching for 0 - 1MB using variable mtrr */
 	disable_cache();
-	set_var_mtrr(0, 0x00000000, CONFIG_CBMEMK << 10, type);
+	stage1_set_var_mtrr(0, 0x00000000, CONFIG_CBMEMK << 10, type);
 	enable_cache();
 }
 
@@ -98,7 +98,7 @@ void do_early_mtrr_init(const unsigned long *mtrr_msrs)
 	/* enable write through caching so we can do execute in place
 	 * on the flash rom.
 	 */
-	set_var_mtrr(1, XIP_ROM_BASE, XIP_ROM_SIZE, MTRR_TYPE_WRBACK);
+	stage1_set_var_mtrr(1, XIP_ROM_BASE, XIP_ROM_SIZE, MTRR_TYPE_WRBACK);
 #endif
 #endif
 

include/arch/x86/mtrr.h
@@ -35,6 +35,8 @@
 void x86_setup_var_mtrrs(unsigned address_bits);
 void x86_setup_mtrrs(unsigned address_bits);
 int x86_mtrr_check(void);
+void stage1_set_var_mtrr(unsigned long reg, unsigned long base,
+				unsigned long size, unsigned long type);
 
 #endif
 

mainboard/amd/dbm690t/Makefile
@@ -26,6 +26,7 @@ STAGE0_MAINBOARD_SRC := $(src)/lib/clog2.c \
 			$(src)/arch/x86/amd/model_fxx/stage1.c \
 			$(src)/northbridge/amd/k8/get_nodes.c \
 			$(src)/northbridge/amd/k8/libstage1.c \
+			$(src)/arch/x86/stage1_mtrr.c
 
 INITRAM_SRC= $(src)/mainboard/$(MAINBOARDDIR)/initram.c \
 			$(src)/northbridge/amd/k8/raminit.c \

mainboard/amd/serengeti/Makefile
@@ -30,6 +30,7 @@ STAGE0_MAINBOARD_SRC := $(src)/lib/clog2.c \
 			$(src)/southbridge/amd/amd8111/stage1_smbus.c \
 			$(src)/southbridge/amd/amd8111/stage1_ctrl.c \
 			$(src)/southbridge/amd/amd8111/stage1_enable_rom.c \
+			$(src)/arch/x86/stage1_mtrr.c
 
 STAGE0_DYNAMIC_SRC :=	$(obj)/mainboard/$(MAINBOARDDIR)/option_table.c
 

mainboard/gigabyte/m57sli/Makefile
@@ -29,6 +29,7 @@ STAGE0_MAINBOARD_SRC := $(src)/lib/clog2.c \
 			$(src)/northbridge/amd/k8/libstage1.c \
 			$(src)/southbridge/nvidia/mcp55/stage1_smbus.c \
 			$(src)/southbridge/nvidia/mcp55/stage1_enable_rom.c \
+			$(src)/arch/x86/stage1_mtrr.c
 
 STAGE0_DYNAMIC_SRC :=	$(obj)/mainboard/$(MAINBOARDDIR)/option_table.c
 
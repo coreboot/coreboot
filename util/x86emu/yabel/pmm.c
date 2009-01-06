9fce7f464ad5e4375a523566db8617ca3c5dc031
Uwe Hermann <uwe@hermann-uwe.de>
2009-01-06 00:05:45 +0000

    Add missing BSD license header to the PMM files.
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://coreboot.org/repository/coreboot-v3@1101 f3766cd6-281f-0410-b1cd-43a5c92072e9

--- util/x86emu/yabel/pmm.c
+++ util/x86emu/yabel/pmm.c
@@ -1,6 +1,11 @@
 /****************************************************************************
  * YABEL BIOS Emulator
  *
+ * This program and the accompanying materials
+ * are made available under the terms of the BSD License
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
  * Copyright 2008 Pattrick Hueper <phueper@hueper.net>
  ****************************************************************************/
 

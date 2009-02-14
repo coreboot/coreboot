a175533dc34cd8b2c98b47124cae2bbd7fcb10d4
Rudolf Marek <r.marek@assembler.cz>
2009-02-14 16:23:16 +0000

    Change Log: Bellongs to r3947
    Following patch adds dynamically generated P-States infrastructure as well as
    M2V-MX SE as example how to do that. It is based on AMD code and mine code for
    ACPI generation.
    
    Signed-off-by: Rudolf Marek <r.marek@assembler.cz>
    Acked-by: Peter Stuge <peter@stuge.se>
    
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@3948 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/include/cpu/amd/model_fxx_powernow.h
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2009 Rudolf Marek <r.marek@assembler.cz>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+#ifndef MODEL_FXX_POWERNOW_H
+#define MODEL_FXX_POWERNOW_H
+
+int amd_model_fxx_generate_powernow(u32 pcontrol_blk, u8 plen, u8 onlyBSP);
+
+#endif

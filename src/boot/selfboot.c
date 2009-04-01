c7798895d30fb3f2f915865dd5755e6df922bc2d
ebiederm <ebiederm@xmission.com (Eric W. Biederman)>
2009-04-01 11:03:32 +0000

    Add copyright notices to two files, src/boot/elfboot.c and its derivative src/boot/selfboot.c.
    The mail in which Eric asserts authorship on elfboot.c is
    quoted below, selfboot.c was substantially edited by Ron.
    With that information in mind the change is trivial.
    
    Signed-off-by: Patrick Georgi <patrick.georgi@coresystems.de>
    Acked-by: Patrick Georgi <patrick.georgi@coresystems.de>
    
    
    
    From: ebiederm@xmission.com (Eric W. Biederman)
    Date: Wed, 01 Apr 2009 03:31:15 -0700
    To: Patrick Georgi <patrick@georgi-clan.de>
    
    Patrick Georgi <patrick@georgi-clan.de> writes:
    
    > Hi,
    >
    > We found some file in the coreboot tree that we suspect is yours.
    > Unfortunately,
    > both copyright notice and license are missing.
    > Could you please take a look at it, and state whether it's yours,
    > and if so,
    > what license is to be attached?
    
    Yes. GPLv2
    
    > The file in question is
    > http://tracker.coreboot.org/trac/coreboot/browser/trunk/coreboot-v2/src/boot/elfboot.c
    > and its history goes back to
    > http://tracker.coreboot.org/trac/coreboot/log/trunk/LinuxBIOSv2/src/boot/elfboot.c?rev=2890
    
    
    Eric
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@4040 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/boot/selfboot.c
+++ src/boot/selfboot.c
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2003 Eric W. Biederman <ebiederm@xmission.com>
+ * Copyright (C) 2009 Ron Minnich <rminnich@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA, 02110-1301 USA
+ */
+
 #include <console/console.h>
 #include <part/fallback_boot.h>
 #include <boot/elf.h>

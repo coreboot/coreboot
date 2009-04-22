b5ab323a4036d86654c562b957d12f4dd4789bde
Stefan Reinauer <stepan@coresystems.de>
2009-04-22 07:23:00 +0000

    A small utility to dump the RAM of a laptop's Embedded/Environmental Controller. Nothing fancy, does not know any laptops, EC types, or what the values mean. It just dumps them. For the dump method, have a look at the ACPI 3.0b spec.
    Signed-off-by: Stefan Reinauer <stepan@coresystems.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@4163 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ util/ectool/ec.c
@@ -0,0 +1,119 @@
+/*
+ * This file is part of the ectool project.
+ *
+ * Copyright (C) 2008-2009 coresystems GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/io.h>
+#include "ec.h"
+
+extern int verbose;
+
+#define debug(x...) if (verbose) printf(x)
+
+int send_ec_command(uint8_t command)
+{
+	int timeout;
+
+	timeout = 0x7ff;
+	while ((inb(EC_SC) & EC_IBF) && --timeout) {
+		usleep(10);
+		if ((timeout & 0xff) == 0)
+			debug(".");
+	}
+	if (!timeout) {
+		printf("Timeout while sending command 0x%02x to EC!\n", 
+				command);
+		// return -1;
+	}
+
+	outb(command, EC_SC);
+	return 0;
+}
+
+int send_ec_data(uint8_t data)
+{
+	int timeout;
+
+	timeout = 0x7ff;
+	while ((inb(EC_SC) & EC_IBF) && --timeout) { // wait for IBF = 0
+		usleep(10);
+		if ((timeout & 0xff) == 0)
+			debug(".");
+	}
+	if (!timeout) {
+		printf("Timeout while sending data 0x%02x to EC!\n",
+				data);
+		// return -1;
+	}
+
+	outb(data, EC_DATA);
+
+	return 0;
+}
+
+int send_ec_data_nowait(uint8_t data)
+{
+	outb(data, EC_DATA);
+
+	return 0;
+}
+
+uint8_t recv_ec_data(void)
+{
+	int timeout;
+	uint8_t data;
+
+	timeout = 0x7fff;
+	while (--timeout) { // Wait for OBF = 1
+		if (inb(EC_SC) & EC_OBF) {
+			break;
+		}
+		usleep(10);
+		if ((timeout & 0xff) == 0)
+			debug(".");
+	}
+	if (!timeout) {
+		printf("\nTimeout while receiving data from EC!\n");
+		// return -1;
+	}
+
+	data = inb(EC_DATA);
+	debug("recv_ec_data: 0x%02x\n", data);
+
+	return data;
+}
+
+uint8_t ec_read(uint8_t addr)
+{
+	send_ec_command(0x80);
+	send_ec_data(addr);
+
+	return recv_ec_data();
+}
+
+int ec_write(uint8_t addr, uint8_t data)
+{
+	send_ec_command(0x81);
+	send_ec_data(addr);
+	return send_ec_data(data);
+}
+
---
+++ util/ectool/ectool.c
@@ -0,0 +1,104 @@
+/*
+ * This file is part of the ectool project.
+ *
+ * Copyright (C) 2008-2009 coresystems GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <sys/io.h>
+#include <ec.h>
+
+#define ECTOOL_VERSION "0.1"
+
+void print_version(void)
+{
+	printf("ectool v%s -- ", ECTOOL_VERSION);
+	printf("Copyright (C) 2008-2009 coresystems GmbH\n\n");
+	printf(
+    "This program is free software: you can redistribute it and/or modify\n"
+    "it under the terms of the GNU General Public License as published by\n"
+    "the Free Software Foundation, version 2 of the License.\n\n"
+    "This program is distributed in the hope that it will be useful,\n"
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+    "GNU General Public License for more details.\n\n"
+    "You should have received a copy of the GNU General Public License\n"
+    "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\n");
+}
+
+void print_usage(const char *name)
+{
+	printf("usage: %s [-vh?V]\n", name);
+	printf("\n"
+	     "   -v | --version:                   print the version\n"
+	     "   -h | --help:                      print this help\n\n"
+	     "   -V | --verbose:                   print debug information\n"
+	     "\n");
+	exit(1);
+}
+
+int verbose = 0;
+
+int main(int argc, char *argv[])
+{
+	int i, opt, option_index = 0;
+
+	static struct option long_options[] = {
+		{"version", 0, 0, 'v'},
+		{"help", 0, 0, 'h'},
+		{"verbose", 0, 0, 'V'},
+		{0, 0, 0, 0}
+	};
+
+	while ((opt = getopt_long(argc, argv, "vh?V",
+                                  long_options, &option_index)) != EOF) {
+		switch (opt) {
+		case 'v':
+			print_version();
+			exit(0);
+			break;
+		case 'V':
+			verbose = 1;
+			break;
+		case 'h':
+		case '?':
+		default:
+			print_usage(argv[0]);
+			exit(0);
+			break;
+		}
+	}
+
+	if (iopl(3)) {
+		printf("You need to be root.\n");
+		exit(1);
+	}
+
+	printf("EC RAM:\n");
+	for (i=0; i < 0x100; i++) {
+		if ((i % 0x10) == 0) 
+			printf ("\n%02x: ", i);
+		printf("%02x ", ec_read(i));
+	}
+	printf("\n\n");
+	return 0;
+}

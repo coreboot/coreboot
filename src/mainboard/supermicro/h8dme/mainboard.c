55faef348a8dd20ac777f75fe00e8772a7655d7a
Ward Vandewege <ward@gnu.org>
2009-04-27 20:19:06 +0000

    Add high tables support for Supermicro H8DME.
    Signed-off-by: Ward Vandewege <ward@gnu.org>
    Acked-by: Myles Watson <mylesgw@gmail.com>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@4221 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/supermicro/h8dme/mainboard.c
+++ src/mainboard/supermicro/h8dme/mainboard.c
@@ -16,12 +16,22 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
-#include <console/console.h>
 #include <device/device.h>
-#include <device/pci.h>
-#include <device/pci_ids.h>
-#include <device/pci_ops.h>
+#include <console/console.h>
+#include <boot/tables.h>
 #include "chip.h"
+
+/* in arch/i386/boot/tables.c */
+extern uint64_t high_tables_base, high_tables_size;
+
+int add_mainboard_resources(struct lb_memory *mem)
+{
+#if HAVE_HIGH_TABLES == 1
+  lb_add_memory_range(mem, LB_MEM_TABLE,
+    high_tables_base, high_tables_size);
+#endif
+  return 0;
+}
 
 struct chip_operations mainboard_ops = {
 	CHIP_NAME("Supermicro H8DME Mainboard")

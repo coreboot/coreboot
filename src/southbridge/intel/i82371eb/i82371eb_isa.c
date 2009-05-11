0520d55f5b20ed0b5f192a695aff033320875233
Myles Watson <mylesgw@gmail.com>
2009-05-11 22:44:14 +0000

    This patch adds high table support to qemu.  It was already added to src/northbridge/intel/i440bx/ but not to src/cpu/emulation/qemu-x86/northbridge.c
    It also adds a driver for the ISA device that is found when using
    0.9.1  If you look in a log without this patch you won't find the RTC
    init lines.
    
    Signed-off-by: Myles Watson <mylesgw@gmail.com>
    Acked-by: Ronald G. Minnich <rminnich@gmail.com>
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@4269 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/southbridge/intel/i82371eb/i82371eb_isa.c
+++ src/southbridge/intel/i82371eb/i82371eb_isa.c
@@ -70,3 +70,9 @@ static const struct pci_driver isa_driver __pci_driver = {
 	.vendor	= PCI_VENDOR_ID_INTEL,
 	.device	= PCI_DEVICE_ID_INTEL_82371AB_ISA,
 };
+
+static const struct pci_driver isa_SB_driver __pci_driver = {
+	.ops	= &isa_ops,
+	.vendor	= PCI_VENDOR_ID_INTEL,
+	.device	= PCI_DEVICE_ID_INTEL_82371SB_ISA,
+};

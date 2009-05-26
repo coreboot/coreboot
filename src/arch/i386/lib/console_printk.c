21dbe8ad3cdd02e6665ff201e91695bba405d7a6
Patrick Georgi <patrick.georgi@coresystems.de>
2009-05-26 14:49:59 +0000

    Make printk_* behaviour more consistent. Without it, side effects in the arguments (eg. a pci config read, or variable increment) "vanish" with the message, and the behaviour changes.
    Some of these effects might be unwanted, but at least they are consistent now.
    To reduce the memory footprint slightly, the formatted strings are discarded. 
    
    Signed-off-by: Patrick Georgi <patrick.georgi@coresystems.de>
    Acked-by: Myles Watson <mylesgw@gmail.com>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@4309 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/arch/i386/lib/console_printk.c
+++ src/arch/i386/lib/console_printk.c
@@ -13,39 +13,39 @@ extern int do_printk(int msg_level, const char *fmt, ...);
 
 #if MAXIMUM_CONSOLE_LOGLEVEL <= BIOS_EMERG
 #undef  printk_emerg
-#define printk_emerg(fmt, arg...)   do {} while(0)
+#define printk_emerg(fmt, arg...)   do_printk(BIOS_EMERG   , "", ##arg)
 #endif
 #if MAXIMUM_CONSOLE_LOGLEVEL <= BIOS_ALERT
 #undef  printk_alert
-#define printk_alert(fmt, arg...)   do {} while(0)
+#define printk_alert(fmt, arg...)   do_printk(BIOS_EMERG   , "", ##arg)
 #endif
 #if MAXIMUM_CONSOLE_LOGLEVEL <= BIOS_CRIT
 #undef  printk_crit
-#define printk_crit(fmt, arg...)    do {} while(0)
+#define printk_crit(fmt, arg...)    do_printk(BIOS_EMERG   , "", ##arg)
 #endif
 #if MAXIMUM_CONSOLE_LOGLEVEL <= BIOS_ERR
 #undef  printk_err
-#define printk_err(fmt, arg...)     do {} while(0)
+#define printk_err(fmt, arg...)     do_printk(BIOS_EMERG   , "", ##arg)
 #endif
 #if MAXIMUM_CONSOLE_LOGLEVEL <= BIOS_WARNING
 #undef  printk_warning
-#define printk_warning(fmt, arg...) do {} while(0)
+#define printk_warning(fmt, arg...) do_printk(BIOS_EMERG   , "", ##arg)
 #endif
 #if MAXIMUM_CONSOLE_LOGLEVEL <= BIOS_NOTICE
 #undef  printk_notice
-#define printk_notice(fmt, arg...)  do {} while(0)
+#define printk_notice(fmt, arg...)  do_printk(BIOS_EMERG   , "", ##arg)
 #endif
 #if MAXIMUM_CONSOLE_LOGLEVEL <= BIOS_INFO
 #undef  printk_info
-#define printk_info(fmt, arg...)    do {} while(0)
+#define printk_info(fmt, arg...)    do_printk(BIOS_EMERG   , "", ##arg)
 #endif
 #if MAXIMUM_CONSOLE_LOGLEVEL <= BIOS_DEBUG
 #undef  printk_debug
-#define printk_debug(fmt, arg...)   do {} while(0)
+#define printk_debug(fmt, arg...)   do_printk(BIOS_EMERG   , "", ##arg)
 #endif
 #if MAXIMUM_CONSOLE_LOGLEVEL <= BIOS_SPEW
 #undef  printk_spew
-#define printk_spew(fmt, arg...)    do {} while(0)
+#define printk_spew(fmt, arg...)    do_printk(BIOS_EMERG   , "", ##arg)
 #endif
 
 #define print_emerg(STR)   printk_emerg  ("%s", (STR))

a519fe77b60123f1fba46f1f93580d5628a88555
Rudolf Marek <r.marek@assembler.cz>
2009-05-31 17:00:25 +0000

    Following patch moves all vt8237 fadt.c from mainboard/* file to chipset directory just with one common file.
    Changes to FADT: move to rev4, fix the generic register descriptors, detect additional VT8237S features.
    Change the compiler to CORE , its revision to 42. 
    
    Signed-off-by: Rudolf Marek <r.marek@assembler.cz>
    Acked-by: Peter Stuge <peter@stuge.se>
    
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@4327 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/southbridge/via/vt8237r/vt8237_fadt.c
+++ src/southbridge/via/vt8237r/vt8237_fadt.c
@@ -2,7 +2,7 @@
  * This file is part of the coreboot project.
  *
  * Copyright (C) 2004 Nick Barker <nick.barker9@btinternet.com>
- * Copyright (C) 2007 Rudolf Marek <r.marek@assembler.cz>
+ * Copyright (C) 2007, 2009 Rudolf Marek <r.marek@assembler.cz>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -21,23 +21,35 @@
 
 #include <string.h>
 #include <arch/acpi.h>
-#include <../../../southbridge/via/vt8237r/vt8237r.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <device/pci_ids.h>
+#include "vt8237r.h"
 
 /**
- * Create the Fixed ACPI Description Tables (FADT) for this board.
+ * Create the Fixed ACPI Description Tables (FADT) for any board with this SB.
  */
 void acpi_create_fadt(acpi_fadt_t *fadt, acpi_facs_t *facs, void *dsdt)
 {
 	acpi_header_t *header = &(fadt->header);
+	device_t dev;
+	int is_vt8237s = 0;
+
+	/* Power management controller */
+	dev = dev_find_device(PCI_VENDOR_ID_VIA,
+				PCI_DEVICE_ID_VIA_VT8237S_LPC, 0);
+
+	if (dev)
+		is_vt8237s = 1;
 
 	memset((void *) fadt, 0, sizeof(acpi_fadt_t));
 	memcpy(header->signature, "FACP", 4);
 	header->length = 244;
-	header->revision = 1;
+	header->revision = 4;
 	memcpy(header->oem_id, "COREBO", 6);
 	memcpy(header->oem_table_id, "COREBOOT", 8);
-	memcpy(header->asl_compiler_id, "IASL", 4);
-	header->asl_compiler_revision = 0;
+	memcpy(header->asl_compiler_id, "CORE", 4);
+	header->asl_compiler_revision = 42;
 
 	fadt->firmware_ctrl = facs;
 	fadt->dsdt = dsdt;
@@ -53,18 +65,26 @@ void acpi_create_fadt(acpi_fadt_t *fadt, acpi_facs_t *facs, void *dsdt)
 	fadt->pm1b_evt_blk = 0x0;
 	fadt->pm1a_cnt_blk = VT8237R_ACPI_IO_BASE + 0x4;
 	fadt->pm1b_cnt_blk = 0x0;
+	/* once we support C2/C3 this could be set to 0x22 and chipset needs to be adjusted too */
 	fadt->pm2_cnt_blk = 0x0;
 	fadt->pm_tmr_blk = VT8237R_ACPI_IO_BASE + 0x8;
 	fadt->gpe0_blk = VT8237R_ACPI_IO_BASE + 0x20;
-	fadt->gpe1_blk = 0x0;
+	if (is_vt8237s) {
+		fadt->gpe1_blk = VT8237R_ACPI_IO_BASE + 0x60;
+		fadt->gpe1_base = 0x10;
+		fadt->gpe1_blk_len = 4;
+	} else {
+		fadt->gpe1_blk = 0x0;
+		fadt->gpe1_base = 0;
+		fadt->gpe1_blk_len = 0;
+	}
 
 	fadt->pm1_evt_len = 4;
 	fadt->pm1_cnt_len = 2;
 	fadt->pm2_cnt_len = 0;
 	fadt->pm_tmr_len = 4;
 	fadt->gpe0_blk_len = 4;
-	fadt->gpe1_blk_len = 0;
-	fadt->gpe1_base = 0;
+
 	fadt->cst_cnt = 0;
 	fadt->p_lvl2_lat = 90;
 	fadt->p_lvl3_lat = 900;
@@ -77,7 +97,7 @@ void acpi_create_fadt(acpi_fadt_t *fadt, acpi_facs_t *facs, void *dsdt)
 	fadt->century = 0x32;
 	/* We have legacy devices, 8042, VGA is ok to probe, MSI are not supported */
 	fadt->iapc_boot_arch = 0xb;
-	/* fixme */
+	/* check me */
 	fadt->flags = 0xa5;
 
 	fadt->reset_reg.space_id = 0;
@@ -94,59 +114,59 @@ void acpi_create_fadt(acpi_fadt_t *fadt, acpi_facs_t *facs, void *dsdt)
 	fadt->x_dsdt_h = 0;
 
 	fadt->x_pm1a_evt_blk.space_id = 1;
-	fadt->x_pm1a_evt_blk.bit_width = 4;
+	fadt->x_pm1a_evt_blk.bit_width = fadt->pm1_evt_len * 8;
 	fadt->x_pm1a_evt_blk.bit_offset = 0;
 	fadt->x_pm1a_evt_blk.resv = 0;
-	fadt->x_pm1a_evt_blk.addrl = VT8237R_ACPI_IO_BASE;
+	fadt->x_pm1a_evt_blk.addrl = fadt->pm1a_evt_blk;
 	fadt->x_pm1a_evt_blk.addrh = 0x0;
 
 	fadt->x_pm1b_evt_blk.space_id = 1;
-	fadt->x_pm1b_evt_blk.bit_width = 4;
+	fadt->x_pm1b_evt_blk.bit_width = fadt->pm1_evt_len * 8;
 	fadt->x_pm1b_evt_blk.bit_offset = 0;
 	fadt->x_pm1b_evt_blk.resv = 0;
-	fadt->x_pm1b_evt_blk.addrl = 0x0;
+	fadt->x_pm1b_evt_blk.addrl = fadt->pm1b_evt_blk;
 	fadt->x_pm1b_evt_blk.addrh = 0x0;
 
 	fadt->x_pm1a_cnt_blk.space_id = 1;
-	fadt->x_pm1a_cnt_blk.bit_width = 2;
+	fadt->x_pm1a_cnt_blk.bit_width = fadt->pm1_cnt_len * 8;
 	fadt->x_pm1a_cnt_blk.bit_offset = 0;
 	fadt->x_pm1a_cnt_blk.resv = 0;
-	fadt->x_pm1a_cnt_blk.addrl = VT8237R_ACPI_IO_BASE + 0x4;
+	fadt->x_pm1a_cnt_blk.addrl = fadt->pm1a_cnt_blk;
 	fadt->x_pm1a_cnt_blk.addrh = 0x0;
 
 	fadt->x_pm1b_cnt_blk.space_id = 1;
-	fadt->x_pm1b_cnt_blk.bit_width = 2;
+	fadt->x_pm1b_cnt_blk.bit_width = fadt->pm1_cnt_len * 8;
 	fadt->x_pm1b_cnt_blk.bit_offset = 0;
 	fadt->x_pm1b_cnt_blk.resv = 0;
-	fadt->x_pm1b_cnt_blk.addrl = 0x0;
+	fadt->x_pm1b_cnt_blk.addrl = fadt->pm1b_cnt_blk;
 	fadt->x_pm1b_cnt_blk.addrh = 0x0;
 
 	fadt->x_pm2_cnt_blk.space_id = 1;
-	fadt->x_pm2_cnt_blk.bit_width = 0;
+	fadt->x_pm2_cnt_blk.bit_width = fadt->pm2_cnt_len * 8;
 	fadt->x_pm2_cnt_blk.bit_offset = 0;
 	fadt->x_pm2_cnt_blk.resv = 0;
-	fadt->x_pm2_cnt_blk.addrl = 0x0;
+	fadt->x_pm2_cnt_blk.addrl = fadt->pm2_cnt_blk;
 	fadt->x_pm2_cnt_blk.addrh = 0x0;
 
 	fadt->x_pm_tmr_blk.space_id = 1;
-	fadt->x_pm_tmr_blk.bit_width = 4;
+	fadt->x_pm_tmr_blk.bit_width = fadt->pm_tmr_len * 8;
 	fadt->x_pm_tmr_blk.bit_offset = 0;
 	fadt->x_pm_tmr_blk.resv = 0;
-	fadt->x_pm_tmr_blk.addrl = VT8237R_ACPI_IO_BASE + 0x8;
+	fadt->x_pm_tmr_blk.addrl = fadt->pm_tmr_blk;
 	fadt->x_pm_tmr_blk.addrh = 0x0;
 
 	fadt->x_gpe0_blk.space_id = 1;
-	fadt->x_gpe0_blk.bit_width = 0;
+	fadt->x_gpe0_blk.bit_width = fadt->gpe0_blk_len * 8;
 	fadt->x_gpe0_blk.bit_offset = 0;
 	fadt->x_gpe0_blk.resv = 0;
-	fadt->x_gpe0_blk.addrl = VT8237R_ACPI_IO_BASE + 0x20;
+	fadt->x_gpe0_blk.addrl = fadt->gpe0_blk;
 	fadt->x_gpe0_blk.addrh = 0x0;
 
 	fadt->x_gpe1_blk.space_id = 1;
-	fadt->x_gpe1_blk.bit_width = 0;
+	fadt->x_gpe1_blk.bit_width = fadt->gpe1_blk_len * 8;;
 	fadt->x_gpe1_blk.bit_offset = 0;
 	fadt->x_gpe1_blk.resv = 0;
-	fadt->x_gpe1_blk.addrl = 0x0;
+	fadt->x_gpe1_blk.addrl = fadt->gpe1_blk;
 	fadt->x_gpe1_blk.addrh = 0x0;
 
 	header->checksum = acpi_checksum((void *) fadt, sizeof(acpi_fadt_t));

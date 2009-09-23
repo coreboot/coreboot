6c641ee035fbef06ca0cbb42c921fb7028cf5216
Stefan Reinauer <stepan@coresystems.de>
2009-09-23 21:52:45 +0000

    fix some wrong versions of the FSF's address (trivial)
    Signed-off-by: Stefan Reinauer <stepan@coresystems.de>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@4664 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/cpu/amd/model_gx2/vsmsetup.c
+++ src/cpu/amd/model_gx2/vsmsetup.c
@@ -19,7 +19,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * LA-CC is the Los Alamos Control and Compliance Number, see also:
  * http://supply.lanl.gov/property/customs/eximguide/default.shtml

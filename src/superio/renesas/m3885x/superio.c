f00052afbfffe87a3f0e07bbb5b8453f48ad19f5
Stefan Reinauer <stepan@coresystems.de>
2010-01-17 13:51:48 +0000

    Add support for Renesas M3885x Embedded Controller
    Signed-off-by: Stefan Reinauer <stepan@coresystems.de>
    Acked-by: Ronald G. Minnich <rminnich@gmail.com>
    Acked-by: Peter Stuge <peter@stuge.se>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@5028 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

---
+++ src/superio/renesas/m3885x/superio.c
@@ -0,0 +1,79 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2009 coresystems GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include <arch/io.h>
+#include <device/device.h>
+#include <device/pnp.h>
+#include <console/console.h>
+#include <device/smbus.h>
+#include <string.h>
+#include <bitops.h>
+#include <uart8250.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "chip.h"
+
+void set_kbc_ps2_mode(void);
+void m3885_configure_multikey(void);
+
+static void m3885x_init(device_t dev)
+{
+	struct superio_renesas_m3885x_config *conf = dev->chip_info;
+
+	if (!dev->enabled)
+		return;
+
+	printk_debug("Renesas M3885x: Initializing keyboard.\n");
+	set_kbc_ps2_mode();
+	init_pc_keyboard(0x60, 0x64, &conf->keyboard);
+	m3885_configure_multikey();
+}
+
+
+static void m3885x_read_resources(device_t dev)
+{
+	// nothing, but this function avoids an error on serial console.
+}
+
+static void m3885x_enable_resources(device_t dev)
+{
+	// nothing, but this function avoids an error on serial console.
+}
+
+static struct device_operations ops = {
+	.init             = m3885x_init,
+	.read_resources   = m3885x_read_resources,
+	.enable_resources = m3885x_enable_resources
+};
+
+static struct pnp_info pnp_dev_info[] = {
+        { &ops, 0, 0, { 0, 0 }, }
+};
+
+static void enable_dev(device_t dev)
+{
+	pnp_enable_devices(dev, &pnp_ops, ARRAY_SIZE(pnp_dev_info), pnp_dev_info);
+}
+
+struct chip_operations superio_renesas_m3885x_ops = {
+	CHIP_NAME("Renesas M3885x Super I/O")
+	.enable_dev = enable_dev,
+};
+
+

6f0074e6b63b056a40589f23f8347e4a3bf6e88b
Joseph Smith <joe@settoplinux.org>
2010-01-30 14:56:15 +0000

    Trivial fixup on IP1000 and RM4100 copyright entries. Signed-off-by: Joseph Smith <joe@settoplinux.org> Acked-by: Joseph Smith <joe@settoplinux.org>
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@5069 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/thomson/ip1000/auto.c
+++ src/mainboard/thomson/ip1000/auto.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the coreboot project.
  *
- * Copyright (C) 2010 Joseph Smith <joe@settoplinux.org>
+ * Copyright (C) 2008-2010 Joseph Smith <joe@settoplinux.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

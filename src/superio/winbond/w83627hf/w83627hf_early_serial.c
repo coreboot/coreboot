d37d2221843004c56c116dfbea4a47a955f76a85
Uwe Hermann <uwe@hermann-uwe.de>
2010-02-14 16:17:55 +0000

    Add missing Copyright line, sorry.
    Signed-off-by: Uwe Hermann <uwe@hermann-uwe.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@5126 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/superio/winbond/w83627hf/w83627hf_early_serial.c
+++ src/superio/winbond/w83627hf/w83627hf_early_serial.c
@@ -4,6 +4,7 @@
  * Copyright (C) 2000 AG Electronics Ltd.
  * Copyright (C) 2003-2004 Linux Networx
  * Copyright (C) 2004 Tyan By LYH change from PC87360
+ * Copyright (C) 2010 Win Enterprises (anishp@win-ent.com)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

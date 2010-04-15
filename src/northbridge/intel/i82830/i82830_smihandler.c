c30a6e859e20dbadbad006f2f93068e7f9c36043
Stefan Reinauer <stepan@coresystems.de>
2010-04-15 08:26:30 +0000

    the dump function assumed that the mbi data comes right after the header. Which is not (always) the case.
    Signed-off-by: Stefan Reinauer <stepan@coresystems.de>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@5441 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/northbridge/intel/i82830/i82830_smihandler.c
+++ src/northbridge/intel/i82830/i82830_smihandler.c
@@ -262,7 +262,8 @@ static void mbi_call(u8 subf, banner_id_t *banner_id)
 
 				getobj->banner.retsts = MSH_OK;
 #ifdef DEBUG_SMI_I82830
-				dump(banner_id, sizeof(getobj) + len);
+				dump(banner_id, sizeof(getobj));
+				dump(getobj->buffer + OBJ_OFFSET, len);
 #endif
 				break;
 			}

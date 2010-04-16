83cce3e8def1a87bff94ba1ab3f94a2eaf85af6a
Myles Watson <mylesgw@gmail.com>
2010-04-16 13:43:49 +0000

    Fix a typo to remove a few more warnings.
    Signed-off-by: Myles Watson <mylesgw@gmail.com>
    Acked-by: Myles Watson <mylesgw@gmail.com>
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@5452 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/include/cpu/amd/microcode.h
+++ src/include/cpu/amd/microcode.h
@@ -1,5 +1,5 @@
 #ifndef CPU_AMD_MICROCODE_H
-#define CPU_AMD_MICORCODE_H
+#define CPU_AMD_MICROCODE_H
 
 void amd_update_microcode(void *microcode_updates, unsigned processor_rev_id);
 void update_microcode(u32 processor_rev_id);

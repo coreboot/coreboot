95bf86be342ea43a99fc7430480d527484d938f9
Stefan Reinauer <stepan@coresystems.de>
2010-04-30 20:30:47 +0000

    Remove some warnings. For code that is called from the mainboard romstage.c files using prototypes is the way to go I think. It would make our life a lot easier should we ever decide to move (some mainboards) over to not #include all those .c files in romstage.c anymore.
    Signed-off-by: Stefan Reinauer <stepan@coresystems.de>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@5518 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/superio/winbond/w83627hf/w83627hf.h
+++ src/superio/winbond/w83627hf/w83627hf.h
@@ -109,3 +109,9 @@
 #define W83627HF_HAP1		0x14
 #define W83627HF_XSCNF		0x15
 #define W83627HF_XWBCNF		0x16
+
+#if defined(__PRE_RAM__) && !defined(__ROMCC__)
+void w83627hf_disable_dev(device_t dev);
+void w83627hf_enable_dev(device_t dev, unsigned iobase);
+#endif
+
--- src/superio/winbond/w83627hf/w83627hf_early_init.c
+++ src/superio/winbond/w83627hf/w83627hf_early_init.c
@@ -23,13 +23,13 @@
 #include <arch/romcc_io.h>
 #include "w83627hf.h"
 
-static void w83627hf_disable_dev(device_t dev)
+void w83627hf_disable_dev(device_t dev)
 {
 	pnp_set_logical_device(dev);
 	pnp_set_enable(dev, 0);
 }
 
-static void w83627hf_enable_dev(device_t dev, unsigned iobase)
+void w83627hf_enable_dev(device_t dev, unsigned iobase)
 {
 	pnp_set_logical_device(dev);
 	pnp_set_enable(dev, 0);

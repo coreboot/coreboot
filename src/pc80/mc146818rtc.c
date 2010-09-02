e448eba75942df3eefbd877b37b63f82c4faf440
Myles Watson <mylesgw@gmail.com>
2010-09-02 18:29:31 +0000

    Revert 5762.  It silently broke a lot of boards because abuild was broken.
    Signed-off-by: Myles Watson <mylesgw@gmail.com>
    Acked-by: Myles Watson <mylesgw@gmail.com>
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@5764 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/pc80/mc146818rtc.c
+++ src/pc80/mc146818rtc.c
@@ -2,9 +2,6 @@
 #include <pc80/mc146818rtc.h>
 #include <boot/coreboot_tables.h>
 #include <string.h>
-#if CONFIG_USE_OPTION_TABLE
-#include <option_table.h>
-#endif
 
 /* control registers - Moto names
  */

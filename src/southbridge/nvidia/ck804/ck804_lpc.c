8af6d5574ba6d9ea847b27ae582396240b3e8d41
Uwe Hermann <uwe@hermann-uwe.de>
2010-10-17 19:13:18 +0000

    Use common GPL-header format in CK804 files, add missing ones (trivial).
    Signed-off-by: Uwe Hermann <uwe@hermann-uwe.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@5956 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/southbridge/nvidia/ck804/ck804_lpc.c
+++ src/southbridge/nvidia/ck804/ck804_lpc.c
@@ -1,8 +1,23 @@
 /*
- * (C) 2003 Linux Networx, SuSE Linux AG
- * Copyright 2004 Tyan Computer
- *  by yhlu@tyan.com
- *  2006.1 yhlu add dest apicid for IRQ0
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2003 Linux Networx
+ * Copyright (C) 2003 SuSE Linux AG
+ * Copyright (C) 2004 Tyan Computer
+ * Written by Yinghai Lu <yhlu@tyan.com> for Tyan Computer.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
 #include <console/console.h>

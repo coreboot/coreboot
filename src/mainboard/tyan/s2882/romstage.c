9fb2e3ab762c5428d9bee0a20eb0a4dc413be62a
Patrick Georgi <patrick@georgi-clan.de>
2010-11-07 17:49:05 +0000

    FIRST_CPU, SECOND_CPU, TOTAL_CPUS are only used in one other place, and that defines these symbols itself (and identical, too). Drop them.
    Signed-off-by: Patrick Georgi <patrick@georgi-clan.de>
    Acked-by: Patrick Georgi <patrick@georgi-clan.de>
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@6034 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/tyan/s2882/romstage.c
+++ src/mainboard/tyan/s2882/romstage.c
@@ -73,12 +73,6 @@ static inline int spd_read_byte(unsigned device, unsigned address)
 #endif
 #include "cpu/amd/dualcore/dualcore.c"
 
-#define FIRST_CPU  1
-#define SECOND_CPU 1
-#define TOTAL_CPUS (FIRST_CPU + SECOND_CPU)
-
-
-
 #include "cpu/amd/car/post_cache_as_ram.c"
 
 #include "cpu/amd/model_fxx/init_cpus.c"

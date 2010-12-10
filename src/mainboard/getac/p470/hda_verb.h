198cb96387c457affa01696405ffaa4531e8e361
Uwe Hermann <uwe@hermann-uwe.de>
2010-12-10 12:52:50 +0000

    Two hda_verb.h files: Add more comments.
    Signed-off-by: Uwe Hermann <uwe@hermann-uwe.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@6160 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/getac/p470/hda_verb.h
+++ src/mainboard/getac/p470/hda_verb.h
@@ -5,8 +5,7 @@
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; version 2 of
- * the License.
+ * published by the Free Software Foundation; version 2 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,17 +14,16 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
- * MA 02110-1301 USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 static const u32 mainboard_cim_verb_data[] = {
 	/* coreboot specific header */
-	0x10ec0262,	// Codec Vendor ID / Device ID
+	0x10ec0262,	// Codec Vendor / Device ID: Realtek ALC262
 	0x10714700,	// Subsystem ID
 	0x0000000d,	// Number of jacks
 
-	/* HDA Codec Subsystem ID Verb Table: 0x10ec0000 */
+	/* NID 0x01, HDA Codec Subsystem ID Verb Table: 0x10ec0000 */
 	0x00172000,
 	0x00172100,
 	0x001722EC,
@@ -33,68 +31,79 @@ static const u32 mainboard_cim_verb_data[] = {
 
 	/* Pin Widget Verb Table */
 
-	/* Pin Complex (NID 0x12) */
+	/* Pin Complex (NID 0x12), DMIC */
 	0x01271CF0,
 	0x01271D11,
 	0x01271E11,
 	0x01271F41,
-	/* Pin Complex (NID 0x14) */
+
+	/* Pin Complex (NID 0x14), LINE_OUT (port D) */
 	0x01471C10,
 	0x01471D01,
 	0x01471E13,
 	0x01471F99,
-	/* Pin Complex (NID 0x15) */
+
+	/* Pin Complex (NID 0x15), HP_OUT (port A) */
 	0x01571C20,
 	0x01571D40,
 	0x01571E21,
 	0x01571F01,
-	/* Pin Complex (NID 0x16) */
+
+	/* Pin Complex (NID 0x16), MONO-OUT */
 	0x01671CF0,
 	0x01671D11,
 	0x01671E11,
 	0x01671F41,
-	/* Pin Complex (NID 0x18) */
+
+	/* Pin Complex (NID 0x18), MIC1 (port B) */
 	0x01871C30,
 	0x01871D98,
 	0x01871EA1,
 	0x01871F01,
-	/* Pin Complex (NID 0x19) */
+
+	/* Pin Complex (NID 0x19), MIC2 (port F) */
 	0x01971C31,
 	0x01971D09,
 	0x01971EA3,
 	0x01971F99,
-	/* Pin Complex (NID 0x1A) */
+
+	/* Pin Complex (NID 0x1A), LINE1 (port C) */
 	0x01A71C3F,
 	0x01A71D98,
 	0x01A71EA1,
 	0x01A71F02,
-	/* Pin Complex (NID 0x1B) */
+
+	/* Pin Complex (NID 0x1B), LINE2 (port E) */
 	0x01B71C1F,
 	0x01B71D40,
 	0x01B71E21,
 	0x01B71F02,
-	/* Pin Complex (NID 0x1C) */
+
+	/* Pin Complex (NID 0x1C), CD_IN */
 	0x01C71CF0,
 	0x01C71D11,
 	0x01C71E11,
 	0x01C71F41,
-	/* Pin Complex (NID 0x1D) */
+
+	/* Pin Complex (NID 0x1D), PCBEEP */
 	0x01D71CF0,
 	0x01D71D11,
 	0x01D71E11,
 	0x01D71F41,
-	/* Pin Complex (NID 0x1E) */
+
+	/* Pin Complex (NID 0x1E), S/PDIF-OUT */
 	0x01E71CF0,
 	0x01E71D11,
 	0x01E71E11,
 	0x01E71F41,
-	/* Pin Complex (NID 0x1F) */
+
+	/* Pin Complex (NID 0x1F), S/PDIF-IN */
 	0x01F71CF0,
 	0x01F71D11,
 	0x01F71E11,
 	0x01F71F41,
 };
 
-extern const u32 * cim_verb_data;
+extern const u32 *cim_verb_data;
 extern u32 cim_verb_data_size;
 
--- src/mainboard/nvidia/l1_2pvv/hda_verb.h
+++ src/mainboard/nvidia/l1_2pvv/hda_verb.h
@@ -7,8 +7,7 @@
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; version 2 of
- * the License.
+ * published by the Free Software Foundation; version 2 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,83 +16,94 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
- * MA 02110-1301 USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 static u32 mainboard_cim_verb_data[] = {
 	/* coreboot specific header */
-	0x10ec0880,	// Codec Vendor ID / Device ID
+	0x10ec0880,	// Codec Vendor / Device ID: Realtek ALC880
 	0x00000000,	// Subsystem ID
 	0x0000000d,	// Number of jacks
 
-	/* HDA Codec Subsystem ID Verb Table: 0x0000e601 */
+	/* NID 0x01, HDA Codec Subsystem ID Verb Table: 0x0000e601 */
 	0x00172001,
 	0x001721e6,
 	0x00172200,
 	0x00172300,
 
+	/* NID 0x14, FRONT-OUT-L/R */
 	0x01471c10,
 	0x01471d44,
 	0x01471e01,
 	0x01471f01,
-	// 1
+
+	/* NID 0x15, SURR-OUT-L/R */
 	0x01571c12,
 	0x01571d14,
 	0x01571e01,
 	0x01571f01,
-	// 2
+
+	/* NID 0x16, CEN/LFE-OUT */
 	0x01671c11,
 	0x01671d60,
 	0x01671e01,
 	0x01671f01,
-	// 3
+
+	/* NID 0x17, SIDE-SURR-L/R */
 	0x01771c14,
 	0x01771d20,
 	0x01771e01,
 	0x01771f01,
-	// 4
+
+	/* NID 0x18, MIC1-L/R, VREFO */
 	0x01871c30,
 	0x01871d9c,
 	0x01871ea1,
 	0x01871f01,
-	// 5
+
+	/* NID 0x19, MIC2-L/R, VREFO */
 	0x01971c40,
 	0x01971d9c,
 	0x01971ea1,
 	0x01971f02,
-	// 6
+
+	/* NID 0x1a, LINE1-L/R, VREFO */
 	0x01a71c31,
 	0x01a71d34,
 	0x01a71e81,
 	0x01a71f01,
-	// 7
+
+	/* NID 0x1b, LINE2-L/R, VREFO */
 	0x01b71c1f,
 	0x01b71d44,
 	0x01b71e21,
 	0x01b71f02,
-	// 8
+
+	/* NID 0x1c, CD-L/R / GND */
 	0x01c71cf0,
 	0x01c71d11,
 	0x01c71e11,
 	0x01c71f41,
-	// 9
+
+	/* NID 0x1d, PCBEEP */
 	0x01d71c3e,
 	0x01d71d01,
 	0x01d71e83,
 	0x01d71f99,
-	// 10
+
+	/* NID 0x1e, S/PDIF-OUT */
 	0x01e71c20,
 	0x01e71d41,
 	0x01e71e45,
 	0x01e71f01,
-	// 11
+
+	/* NID 0x1f, S/PDIF-IN */
 	0x01f71c50,
 	0x01f71d91,
 	0x01f71ec5,
 	0x01f71f01,
 };
 
-extern u32 * cim_verb_data;
+extern u32 *cim_verb_data;
 extern u32 cim_verb_data_size;
 

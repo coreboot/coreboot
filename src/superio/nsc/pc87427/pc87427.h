6559f43dd96845c7fde9720a6c1b720aa1cb9361
Stefan Reinauer <stepan@coreboot.org>
2010-12-17 00:13:54 +0000

    add license headers to some trivial files and pc87427.h Mostly done according to initial file creator.
    Signed-off-by: Stefan Reinauer <stepan@coreboot.org>
    Acked-by: Stefan Reinauer <stepan@coreboot.org>
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@6192 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/superio/nsc/pc87427/pc87427.h
+++ src/superio/nsc/pc87427/pc87427.h
@@ -1,3 +1,24 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2000 AG Electronics Ltd.
+ * Copyright (C) 2003-2004 Linux Networx
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
 #ifndef SUPERIO_NSC_PC87427_PC87427_H
 #define SUPERIO_NSC_PC87427_PC87427_H
 

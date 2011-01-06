965c43b4db8df752a2db609fef01c7235291c144
Marc Bertens <mbertens@xs4all.nl>
2011-01-06 23:03:46 +0000

    Various Nokia IP530 fixes.
     - Correct default ROM image size for this board (512KB is correct).
    
     - devicetree.cb: Add AUX I/O config (mainly GPIO settings).
       This allows you to control the LEDs in the front panel and JP900/JP901
       can be read.
    
     - irq_tables.c: Rework PIRQ table to make more onboard devices work.
       Also, avoid IRQ9.
    
     - mainboard.c: Drop unneeded functions, everything is done in devicetree.cb.
    
    Signed-off-by: Marc Bertens <mbertens@xs4all.nl>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@6247 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/nokia/ip530/mainboard.c
+++ src/mainboard/nokia/ip530/mainboard.c
@@ -20,44 +20,7 @@
 
 #include <device/device.h>
 #include "chip.h"
-#include <device/pci_def.h>
-#include <device/pci.h>
-#include <arch/io.h>
-#include <device/pnp_def.h>
-#include <console/console.h>
-#define OUTB	outb
-#define INB		inb
-
-/*
-*	Taken from flashrom project
-*	Generic Super I/O helper functions
-*/
-static uint8_t sio_read(uint16_t port, uint8_t reg)
-{
-	OUTB( reg, port );
-	return ( INB( port + 1 ) );
-}
-
-static void sio_write(uint16_t port, uint8_t reg, uint8_t data)
-{
-	OUTB( reg, port );
-	OUTB( data, port + 1 );
-	return;
-}
-
-static void nokia_ip530_board_enable( device_t dev )
-{
-	print_debug( "Setting up IP530-Super I/O devices\n");
-	sio_write( 0x20, 0x03, 0x80 );
-	printk( BIOS_DEBUG, "--Register 0x03 = %X := 0x80\n", sio_read( 0x20, 0x03 ) );
-	sio_write( 0x20, 0x22, 0x30 );
-	printk( BIOS_DEBUG, "--Register 0x22 = %X := 0x30\n", sio_read( 0x20, 0x22 ) );
-	sio_write( 0x20, 0x24, 0x84 );
-	printk( BIOS_DEBUG, "--Register 0x24 = %X := 0x84\n", sio_read( 0x20, 0x24 ) );
-	return;
-}
 
 struct chip_operations mainboard_ops = {
 	CHIP_NAME("Nokia IP530 Mainboard")
-	.enable_dev = nokia_ip530_board_enable,
 };

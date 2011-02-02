539500ee3373b91985aec9749ae35fb5a94e1c51
Uwe Hermann <uwe@hermann-uwe.de>
2011-02-02 23:56:15 +0000

    pmh7.[ch]: Add missing license headers.
    Signed-off-by: Uwe Hermann <uwe@hermann-uwe.de>
    Acked-by: Uwe Hermann <uwe@hermann-uwe.de>
    
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@6328 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/ec/lenovo/pmh7/pmh7.c
+++ src/ec/lenovo/pmh7/pmh7.c
@@ -1,9 +1,27 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 Sven Schnelle <svens@stackframe.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
 #include <arch/io.h>
 #include <console/console.h>
 #include <device/device.h>
 #include <device/pnp.h>
 #include <stdlib.h>
-
 #include "pmh7.h"
 
 void pmh7_register_set_bit(int reg, int bit)
@@ -41,13 +59,13 @@ void pmh7_register_write(int reg, int val)
 static void enable_dev(device_t dev)
 {
 	struct resource *resource;
+
 	resource = new_resource(dev, EC_LENOVO_PMH7_INDEX);
 	resource->flags = IORESOURCE_IO | IORESOURCE_FIXED;
 	resource->base = EC_LENOVO_PMH7_BASE;
 	resource->size = 16;
 	resource->align = 5;
 	resource->gran = 5;
-
 }
 
 struct chip_operations ec_lenovo_pmh7_ops = {

ceccd8dd676deca966a0b47377a3eb7d485d9f12
Patrick Georgi <patrick.georgi@secunet.com>
2011-05-12 06:53:52 +0000

    Remove uart_init() in Siemens sitemp-g1p1
    uart_init() was moved to common code in r6531, but I
    missed that when integrating the new mainboard code.
    
    Signed-off-by: Patrick Georgi <patrick.georgi@secunet.com>
    Acked-by: Patrick Georgi <patrick.georgi@secunet.com>
    
    
    git-svn-id: svn://svn.coreboot.org/coreboot/trunk@6568 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

--- src/mainboard/siemens/sitemp_g1p1/romstage.c
+++ src/mainboard/siemens/sitemp_g1p1/romstage.c
@@ -244,7 +244,6 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	it8712f_enable_serial(0, CONFIG_TTYS0_BASE);
 	it8712f_kill_watchdog(); 
 
-	uart_init();
 	console_init();
 #if defined(DUMP_CMOS_RAM) && (DUMP_CMOS_RAM == 1)
 	check_cmos();  // rebooting in case of corrupted cmos !!!!!		

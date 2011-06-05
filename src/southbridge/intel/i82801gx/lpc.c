f641e32ce44f9621c98075d4a327aebfdec85e1f
Sven Schnelle <svens@stackframe.org>
2011-06-05 11:39:12 +0200

    i82801gx: enable ACPI during S3 resume
    disabling ACPI during S3 wakeup breaks ACPI wakeup, as the
    Host OS is assuming that ACPI is enabled.
    
    Change-Id: I8ced72c4b553d41a57f26d64998118e8a77621f8
    Signed-off-by: Sven Schnelle <svens@stackframe.org>

--- src/southbridge/intel/i82801gx/lpc.c
+++ src/southbridge/intel/i82801gx/lpc.c
@@ -340,6 +340,7 @@ static void i82801gx_lock_smm(struct device *dev)
 	u8 reg8;
 #endif
 
+	if (acpi_slp_type != 3) {
 #if ENABLE_ACPI_MODE_IN_COREBOOT
 	printk(BIOS_DEBUG, "Enabling ACPI via APMC:\n");
 	outb(0xe1, 0xb2); // Enable ACPI mode
@@ -349,6 +350,10 @@ static void i82801gx_lock_smm(struct device *dev)
 	outb(0x1e, 0xb2); // Disable ACPI mode
 	printk(BIOS_DEBUG, "done.\n");
 #endif
+	} else {
+		printk(BIOS_DEBUG, "S3 wakeup, enabling ACPI via APMC\n");
+		outb(APM_CNT_ACPI_ENABLE, APM_CNT);
+	}
 	/* Don't allow evil boot loaders, kernels, or
 	 * userspace applications to deceive us:
 	 */

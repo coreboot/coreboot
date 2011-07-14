deaac0427cf5023064fb2975d1efcb1e934b1208
efdesign98 <efdesign98@gmail.com>
2011-07-13 17:16:13 -0700

    Add AMD Family 10 support to cpu folder
    This change adds the AMD Family 10 cpu support to the
    cpu folder.  It also updates the makefiles of the Families
    12 and 14 to take advantage of a pair of shared files that
    are moved to the cpu/agesa folder.
    
    Change-Id: Ibd3a50ea7a3028bd6a2d2583f021506b73e2fce2
    Signed-off-by: Frank Vibrans <frank.vibrans@amd.com>
    Signed-off-by: efdesign98 <efdesign98@gmail.com>

--- src/cpu/amd/agesa/apic_timer.c
+++ src/cpu/amd/agesa/apic_timer.c
@@ -34,25 +34,26 @@
 
 void init_timer(void)
 {
-  /* Set the apic timer to no interrupts and periodic mode */
-  lapic_write(LAPIC_LVTT, (1 << 17)|(1<< 16)|(0 << 12)|(0 << 0));
+	/* Set the apic timer to no interrupts and periodic mode */
+	lapic_write(LAPIC_LVTT, (1 << 17)|(1<< 16)|(0 << 12)|(0 << 0));
 
-  /* Set the divider to 1, no divider */
-  lapic_write(LAPIC_TDCR, LAPIC_TDR_DIV_1);
+	/* Set the divider to 1, no divider */
+	lapic_write(LAPIC_TDCR, LAPIC_TDR_DIV_1);
 
-  /* Set the initial counter to 0xffffffff */
-  lapic_write(LAPIC_TMICT, 0xffffffff);
+	/* Set the initial counter to 0xffffffff */
+	lapic_write(LAPIC_TMICT, 0xffffffff);
+
 }
 
 
 void udelay(u32 usecs)
 {
-  u32 start, value, ticks;
-  /* Calculate the number of ticks to run, our FSB runs a 200Mhz */
-  ticks = usecs * 200;
-  start = lapic_read(LAPIC_TMCCT);
-  do {
-    value = lapic_read(LAPIC_TMCCT);
-  } while((start - value) < ticks);
+	u32 start, value, ticks;
+	/* Calculate the number of ticks to run, our FSB runs a 200Mhz */
+	ticks = usecs * 200;
+	start = lapic_read(LAPIC_TMCCT);
+	do {
+		value = lapic_read(LAPIC_TMCCT);
+	} while((start - value) < ticks);
 
 }

d8862ac4e3d74587d140e3a52d93567c7b2cc75a
efdesign98 <efdesign98@gmail.com>
2011-07-13 17:49:25 -0700

    Add AMD Family 10 cpu support to northbridge folder
    This change adds the AMD Family 10 cpu support to the northbridge
    folder.  The northbridge/amd/agesa Kconfig and Makefile.inc are
    changed as well.
    
    Change-Id: Id76e9fa388c79ac469a673aaedaa4f1bfd7619d9
    Signed-off-by: Frank Vibrans <frank.vibrans@amd.com>
    Signed-off-by: efdesign98 <efdesign98@gmail.com>

---
+++ src/northbridge/amd/agesa/family10/reset_test.h
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * copy from src/northbridge/amd/amdfam10/reset_test.c
+ */
+
+#ifndef _RESET_TEST_H_
+#define _RESET_TEST_H_
+
+#include "amdfam10.h"	/* NODE_PCI */
+
+#define NODE_ID			0x60
+#define HT_INIT_CONTROL		0x6c
+#define HTIC_ColdR_Detect	(1<<4)
+#define HTIC_BIOSR_Detect	(1<<5)
+#define HTIC_INIT_Detect	(1<<6)
+
+static inline u32 warm_reset_detect(u8 nodeid)
+{
+	u32 htic;
+	device_t device;
+	device = NODE_PCI(nodeid, 0);
+	htic = pci_io_read_config32(device, HT_INIT_CONTROL);
+	return (htic & HTIC_ColdR_Detect) && !(htic & HTIC_BIOSR_Detect);
+}
+
+static inline void distinguish_cpu_resets(u8 nodeid)
+{
+	u32 htic;
+	device_t device;
+	device = NODE_PCI(nodeid, 0);
+	htic = pci_io_read_config32(device, HT_INIT_CONTROL);
+	htic |= HTIC_ColdR_Detect | HTIC_BIOSR_Detect | HTIC_INIT_Detect;
+	pci_io_write_config32(device, HT_INIT_CONTROL, htic);
+}
+
+#endif

6a9fd9d6bb89ecdf3b49fb057182200a5cd9ee0f
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2011-08-01 05:16:20 -0500

    NOTFORMERGE: VX900 early init, Nano CPU init, and EPIA M850 board
    Early initialization for VIA VX900:
    
    SMBUS is functional.
    Raminit works with 1 rank, but untested with several ranks.
    DRAM calibration when more than one DIMM is present is not fully
    implemented. Correct MRS command mapping not implemented for DIMMS
    which swap address pins.
    
    The delay calibration strategy is to put some predefined delays for
    the MD input delays, and run hardware calibration on the other
    three delays (DQS input, DQ/DQS output).
    For the future, we could change this strategy to one that uses the
    hardware calibration of MD input. Unfortunately, according to my
    testing, the MD input delay calibration does not work reliably.
    
    VIA nano CPU init:
    
    Microcode updating works. Attempting to change the voltage or
    frequency of the CPU without applying the microcode update will
    hang the CPU, so we only do transitions if we can verify the
    microcode has been updated.
    The microcode is updated directly from CBFS. A few residual pieces of
    code remain from the time microcode was loaded from an array.
    
    VX900 + VX800 related changes:
    
    Added LPC driver struct in VX800 code. The LPCs seem to be similar
    enough that the VX800 LPC code works on the VX900
    
    VIA EPIA M850 board:
    Minimum infrastructure to compile a coreboot image for this board is
    added; most PCI devices are switched off in devicetree.cb
    
    ========== Other Changes: ==========
    
    UPDATE_CPU_MICROCODE:
    
    Moved UPDATE_CPU_MICROCODE to src/cpu/Kconfig to make it a more
    "global" option, and made it depend on
    RESPECT_UPDATE_CPU_MICROCODE_OPTION
    I will submit this as a separate patch once I figure out how to only
    push this part of my branch
    
    Include microcode in CBFS:
    
    Added option to the build system to include a microcode file in CBFS.
    The option will only be visible if the CPU's Kconfig selects
    CPU_MICROCODE_FROM_CBFS
    This option tells the build system that the CPU code is capable of
    finding and applying microcode updates from CBFS.
    I can turn this into a separate patch if there is enough interest
    
    You might also have noticed a commented-out option in Kconfig:
    CPU_MICROCODE_CBFS_GENERATE
    The idea behind this is to take a C array, compile it, and generate a
    microcode file automatically. This means the user won't have to fiddle
    with microcode files, which might be next to impossible to obtain.
    Since this option requires more intricate modifications to the build
    system, I have put it off for now.
    
    Change-Id: I7624944dbc05fbf3019897a116954d71dfda0031
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

---
+++ src/northbridge/via/vx900/northbridge.c
@@ -0,0 +1,91 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012  Alexandru Gagniuc <mr.nuke.me@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "vx900.h"
+#include "chip.h"
+
+#include <console/console.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <device/pci_ids.h>
+#include <cpu/cpu.h>
+#include <cbmem.h>
+
+static void vx900_read_resources(device_t dev)
+{
+	print_debug("========= VX900 memory sizing & Co. ==========\n");
+	int idx = 0;
+	/* FIXME: just a quick memory sizing. fix me later */
+	unsigned long tolm = (1<<29);
+	ram_resource(dev, idx++, 0, 640);
+	/* Leave a hole for vga, 0xa0000 - 0xc0000 */
+	ram_resource(dev, idx++, 768, ((tolm >> 10) - 768));
+	#if CONFIG_WRITE_HIGH_TABLES
+	/* Leave some space for ACPI, PIRQ and MP tables */
+	high_tables_base = tolm - HIGH_MEMORY_SIZE;
+	high_tables_size = HIGH_MEMORY_SIZE;
+	printk(BIOS_DEBUG, "high_tables_base: %08llx, size %lld\n",
+	       high_tables_base, high_tables_size);
+	#endif
+}
+
+static void pci_domain_set_resources(device_t dev)
+{
+	assign_resources(dev->link_list);
+}
+
+static void cpu_bus_init(device_t dev)
+{
+	initialize_cpus(dev->link_list);
+}
+
+static void cpu_bus_noop(device_t dev)
+{
+}
+static struct device_operations cpu_bus_ops = {
+	.read_resources   = cpu_bus_noop,
+	.set_resources    = cpu_bus_noop,
+	.enable_resources = cpu_bus_noop,
+	.init             = cpu_bus_init,
+	.scan_bus         = 0,
+};
+
+static struct device_operations pci_domain_ops = {
+	.read_resources   = vx900_read_resources,
+	.set_resources    = pci_domain_set_resources,
+	.enable_resources = NULL,
+	.init             = NULL,
+	.scan_bus         = pci_domain_scan_bus,
+};
+
+static void enable_dev(device_t dev)
+{
+	/* Set the operations if it is a special bus type */
+	if (dev->path.type == DEVICE_PATH_PCI_DOMAIN) {
+		dev->ops = &pci_domain_ops;
+	} else if (dev->path.type == DEVICE_PATH_APIC_CLUSTER) {
+		dev->ops = &cpu_bus_ops;
+	}
+}
+
+struct chip_operations northbridge_via_vx900_ops = {
+	CHIP_NAME("VIA VX900 Chipset")
+	.enable_dev = enable_dev,
+};
+

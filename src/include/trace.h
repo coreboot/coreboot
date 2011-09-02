7f0e93060e720149bb59023d608a67cfc21542b1
Rudolf Marek <r.marek@assembler.cz>
2011-09-02 23:23:41 +0200

    Add support for the tracing infastructure in coreboot.
    The compiler is forced to emmit special functions on every
    entry/exit of the function. Add a compile time option
    to support it. Function entries will be printed in
    the console. The CONFIG_TRACE has more documentation.
    
    Patch for userspace tools will follow.
    
    Change-Id: I2cbeb3f104892b034c8756f86ed05bf71187c3f3
    Signed-off-by: Rudolf Marek <r.marek@assembler.cz>
    Reviewed-on: http://review.coreboot.org/178
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

---
+++ src/include/trace.h
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 Rudolf Marek <r.marek@assembler.cz>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef __TRACE_H
+#define __TRACE_H
+
+
+#ifdef __PRE_RAM__
+
+#define DISABLE_TRACE
+#define ENABLE_TRACE
+#define DISABLE_TRACE_ON_FUNCTION
+
+#else /* !__PRE_RAM__ */
+
+#if CONFIG_TRACE
+
+void __cyg_profile_func_enter( void *, void * )
+				 __attribute__ ((no_instrument_function));
+
+void __cyg_profile_func_exit( void *, void * )
+				__attribute__ ((no_instrument_function));
+
+extern volatile int trace_dis;
+
+#define DISABLE_TRACE  do { trace_dis = 1; } while (0);
+#define ENABLE_TRACE    do { trace_dis = 0; } while (0);
+#define DISABLE_TRACE_ON_FUNCTION  __attribute__ ((no_instrument_function));
+
+#else /* !CONFIG_TRACE */
+
+#define DISABLE_TRACE
+#define ENABLE_TRACE
+#define DISABLE_TRACE_ON_FUNCTION
+
+#endif
+#endif
+#endif

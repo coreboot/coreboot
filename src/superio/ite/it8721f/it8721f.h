e169f82edf661ee922215ffe7fc6f413c7fa9181
QingPei Wang <wangqingpei@gmail.com>
2011-09-13 13:35:43 +0800

    Add IT8721F support
    only the serial port is tested, keyboard/mouse are gonna
    to be tested later, it may also need some more patches
    to make it work completely.
    
    Change-Id: Ie9464d01c5d5760ebc800b3cd15a4ab2bad2e09f
    Signed-off-by: QingPei Wang <wangqingpei@gmail.com>
    Reviewed-on: http://review.coreboot.org/204
    Tested-by: build bot (Jenkins)
    Reviewed-by: Marc Jones <marcj303@gmail.com>

---
+++ src/superio/ite/it8721f/it8721f.h
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2006 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 QingPei Wang <wangqingpei@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef SUPERIO_ITE_IT8721F_IT8721F_H
+#define SUPERIO_ITE_IT8721F_IT8721F_H
+
+#define IT8721F_FDC  0x00 /* Floppy */
+#define IT8721F_SP1  0x01 /* Com1 */
+#define IT8721F_SP2  0x02 /* Com2 */
+#define IT8721F_PP   0x03 /* Parallel port */
+#define IT8721F_EC   0x04 /* Environment controller */
+#define IT8721F_KBCK 0x05 /* PS/2 keyboard */
+#define IT8721F_KBCM 0x06 /* PS/2 mouse */
+#define IT8721F_GPIO 0x07 /* GPIO */
+#define IT8721F_IR   0x0a /* Consumer IR */
+
+#if defined(__PRE_RAM__) && !defined(__ROMCC__)
+void it8721f_24mhz_clkin(void);
+void it8721f_disable_reboot(void);
+void it8721f_enable_serial(device_t dev, u16 iobase);
+#endif
+
+#endif

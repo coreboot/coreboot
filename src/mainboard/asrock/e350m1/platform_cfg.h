abade616d0d2b24571588cec8766d450278a2fe8
efdesign98 <efdesign98@gmail.com>
2011-09-14 12:04:16 -0600

    Update to Asrock E350m1 for AMD F14 C0
    This updates the E350m1 Agesa wrapper code to fix an
    issue with AmdLateRunApTask.  It now passes the function
    parameter through to the Agesa routine.  There is also
    a change to the platform_cfg.h file that makes the
    definition of BIOS_SIZE dependent on whether or not
    it was defined earlier.
    
    Change-Id: I19942c7d3ecd229a13ef0a69fa7e5b1ea0b909bf
    Signed-off-by: Frank Vibrans <frank.vibrans@amd.com>
    Signed-off-by: efdesign98 <efdesign98@gmail.com>

--- src/mainboard/asrock/e350m1/platform_cfg.h
+++ src/mainboard/asrock/e350m1/platform_cfg.h
@@ -36,14 +36,16 @@
  * bigger than 1M you have to set the ROM size outside CIMx module and
  * before AGESA module get call.
  */
-#if CONFIG_COREBOOT_ROMSIZE_KB_1024 == 1
-  #define BIOS_SIZE BIOS_SIZE_1M
-#elif CONFIG_COREBOOT_ROMSIZE_KB_2048 == 1
-  #define BIOS_SIZE BIOS_SIZE_2M
-#elif CONFIG_COREBOOT_ROMSIZE_KB_4096 == 1
-  #define BIOS_SIZE BIOS_SIZE_4M
-#elif CONFIG_COREBOOT_ROMSIZE_KB_8192 == 1
-  #define BIOS_SIZE BIOS_SIZE_8M
+#ifndef BIOS_SIZE
+  #if CONFIG_COREBOOT_ROMSIZE_KB_1024 == 1
+	#define BIOS_SIZE BIOS_SIZE_1M
+  #elif CONFIG_COREBOOT_ROMSIZE_KB_2048 == 1
+	#define BIOS_SIZE BIOS_SIZE_2M
+  #elif CONFIG_COREBOOT_ROMSIZE_KB_4096 == 1
+	#define BIOS_SIZE BIOS_SIZE_4M
+  #elif CONFIG_COREBOOT_ROMSIZE_KB_8192 == 1
+	#define BIOS_SIZE BIOS_SIZE_8M
+  #endif
 #endif
 
 /**
@@ -56,7 +58,7 @@
 
 /**
  * @def SB_HPET_TIMER
- * @breif
+ * @brief
  *  0 - Disable hpet
  *  1 - Enable  hpet
  */
@@ -79,7 +81,7 @@
 
 /**
  * @def PCI_CLOCK_CTRL
- * @breif bit[0-4] used for PCI Slots Clock Control,
+ * @brief bit[0-4] used for PCI Slots Clock Control,
  *   0 - disable
  *   1 - enable
  *  PCI SLOT 0 define at BIT0
@@ -92,26 +94,26 @@
 
 /**
  * @def SATA_CONTROLLER
- * @breif INCHIP Sata Controller
+ * @brief INCHIP Sata Controller
  */
 #define SATA_CONTROLLER		CIMX_OPTION_ENABLED
 
 /**
  * @def SATA_MODE
- * @breif INCHIP Sata Controller Mode
+ * @brief INCHIP Sata Controller Mode
  *   NOTE: DO NOT ALLOW SATA & IDE use same mode
  */
 #define SATA_MODE			NATIVE_IDE_MODE
 
 /**
- * @breif INCHIP Sata IDE Controller Mode
+ * @brief INCHIP Sata IDE Controller Mode
  */
 #define IDE_LEGACY_MODE			0
 #define IDE_NATIVE_MODE			1
 
 /**
  * @def SATA_IDE_MODE
- * @breif INCHIP Sata IDE Controller Mode
+ * @brief INCHIP Sata IDE Controller Mode
  *   NOTE: DO NOT ALLOW SATA & IDE use same mode
  */
 #define SATA_IDE_MODE			IDE_LEGACY_MODE
@@ -155,7 +157,7 @@
 #define AZALIA_ENABLE			2
 
 /**
- * @breif INCHIP HDA controller
+ * @brief INCHIP HDA controller
  */
 #define AZALIA_CONTROLLER		AZALIA_AUTO
 

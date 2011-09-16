1025f3afc85be633451c4312ab26d179d47132e5
Gabe Black <gabeblack@google.com>
2011-09-16 02:18:56 -0700

    Add an implementation for the memchr library function
    Change-Id: Icded479d246f7cce8a3d2154c69f75178fa513e1
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: http://review.coreboot.org/708
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>
    Tested-by: build bot (Jenkins)
    Reviewed-by: Mathias Krause <minipli@googlemail.com>

--- src/include/string.h
+++ src/include/string.h
@@ -8,6 +8,7 @@ void *memcpy(void *dest, const void *src, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
+void *memchr(const void *s, int c, size_t n);
 #if !defined(__PRE_RAM__)
 int sprintf(char * buf, const char *fmt, ...);
 #endif

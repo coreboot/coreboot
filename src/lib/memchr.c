daf3d89987751a461bc8d1db86e13644acc46047
Gabe Black <gabeblack@google.com>
2011-09-16 02:18:56 -0700

    Add an implementation for the memchr library function
    Change-Id: Icded479d246f7cce8a3d2154c69f75178fa513e1
    Signed-off-by: Gabe Black <gabeblack@google.com>

---
+++ src/lib/memchr.c
@@ -0,0 +1,11 @@
+#include <string.h>
+void *memchr(const void *s, int c, size_t n)
+{
+	const unsigned char *sc = s;
+	while (n--) {
+		if (*sc == (unsigned char)c)
+			return (void *)sc;
+		sc++;
+	}
+	return NULL;
+}

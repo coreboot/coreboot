f24a9b904926af4039bc2530918d77a886f029b7
Vadim Bendebury <vbendeb@chromium.org>
2011-09-29 17:27:15 -0700

    CBMEM CONSOLE: Add CBMEM console driver implementation.
    The CBMEM console driver saves console output in a CBMEM area, which
    then is made available to Linux applications for perusing.
    
    There are some system limitations which need to be worked around
    to achieve this goal:
    
    - some console traffic is generated before DRAM is initialized,
      leave alone CBMEM initialized.
    
    - after the RAM based stage starts, a lot of traffic is generated
      before CBMEM is initialized.
    
    As a result, the console log lives in three different places -
    the bottom of the cache as RAM space, the CBMEM buffer (where it
    is expected to be) and a static buffer used early in the RAM
    stage.
    
    When execution starts (in the cache as RAM mode), the console
    buffer is allocated at the bottom of the cache as RAM memory
    address range. Once DRAM is initialized, the CBMEM structure is
    initialized, and then the console buffer contents are copied from
    the bottom of the cache as RAM space into the CBMEM area right
    before the cache as RAM mode is disabled. The
    src/lib/cbmem_console.c:cbmemc_reinit() takes care of the
    copying.
    
    At this point the cache as RAM memory is about to be disabled,
    but the ROM stage is still going generating console output. To
    make sure this output is not lost, cbmemc_reinit() saves the new
    buffer address at a fixed location (0x600 was chosen for this),
    and the actual "printing" function checks to see if the RAM is
    already initialized (the stack is in RAM), and if so, gets the
    console buffer pointer from this location instead of using the
    cache as RAM address.
    
    When the RAM stage starts, a static buffer is used to store the
    console output, as the CBMEM buffer location is not known. Then,
    when CBMEM is reinitialized, cbmemc_reinit() again takes care of
    the copying.
    
    In case the allocated buffers are not large enough, the excessive
    data is dropped, and the copying routine adds some text to the
    output buffer to indicate that there has been data lost and how
    many characters were dropped.
    
    Change-Id: I8c126e31db6cb2141f7f4f97c5047f39a8db44fc
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- src/include/console/console.h
+++ src/include/console/console.h
@@ -33,6 +33,9 @@
 #if CONFIG_CONSOLE_NE2K
 #include <console/ne2k.h>
 #endif
+#if CONFIG_CONSOLE_CBMEM
+#include <console/cbmem_console.h>
+#endif
 
 #ifndef __PRE_RAM__
 void console_tx_byte(unsigned char byte);

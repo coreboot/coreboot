20a75ca9c0a326ab4113311f6a56dd668be4e283
Stefan Reinauer <reinauer@chromium.org>
2011-10-14 15:11:16 -0700

    Add macros for 64bit byte order swapping
    Change-Id: Ic31ccd41ba3e0af7046eafc29221810d4cd196c8
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

--- src/arch/x86/include/arch/byteorder.h
+++ src/arch/x86/include/arch/byteorder.h
@@ -5,10 +5,14 @@
 
 #include <swab.h>
 
+#define cpu_to_le64(x) ((unsigned long)(x))
+#define le64_to_cpu(x) ((unsigned long)(x))
 #define cpu_to_le32(x) ((unsigned int)(x))
 #define le32_to_cpu(x) ((unsigned int)(x))
 #define cpu_to_le16(x) ((unsigned short)(x))
 #define le16_to_cpu(x) ((unsigned short)(x))
+#define cpu_to_be64(x) swab64(x)
+#define be64_to_cpu(x) swab64(x)
 #define cpu_to_be32(x) swab32((x))
 #define be32_to_cpu(x) swab32((x))
 #define cpu_to_be16(x) swab16((x))

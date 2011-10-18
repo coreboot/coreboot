ba9a6694ba1c06c4e441dda5a9ef9c125208ac00
Kyösti Mälkki <kyosti.malkki@gmail.com>
2011-10-18 17:03:12 +0300

    Add support for E7505 northbridge.
    Adapted from northbridge/intel/e7501 with only minor changes.
    This commit provides minimal patch from e7501 and I prefer any
    cosmetic clean-up to be done after initial merge.
    
    Due the incomplete register specifications, it is safer to have
    e7505 as a separate directory in case I improve it to support
    wider range of memory configurations. I have no e7501 to test with.
    
    Change-Id: Iba3bf9d69ff5e9d9ef3a6ebf8259f048c55d637d
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

---
+++ src/northbridge/intel/e7505/reset_test.c
@@ -0,0 +1,18 @@
+/* Convert to C by yhlu */
+#define MCH_DRC 0x7c
+#define DRC_DONE (1 << 29)
+	/* If I have already booted once skip a bunch of initialization */
+	/* To see if I have already booted I check to see if memory
+	 * has been enabled.
+	 */
+static int bios_reset_detected(void) {
+	uint32_t dword;
+
+	dword = pci_read_config32(PCI_DEV(0, 0, 0), MCH_DRC);
+
+	if( (dword & DRC_DONE) != 0 ) {
+		return 1;
+	}
+
+	return 0;
+}

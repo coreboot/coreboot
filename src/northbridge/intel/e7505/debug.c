b15975bf5a4a1b9ed4d83e2c8caf622d71a7e4d5
Stefan Reinauer <reinauer@chromium.org>
2011-10-21 12:57:59 -0700

    copy e7501 component to e7505
    Change-Id: Ie69a6b6a040a8b0e7693083b3a2d13c327a165b3
    Signed-off-by: Stefan Reinauer <reinauer@google.com>
    Reviewed-on: http://review.coreboot.org/310
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>

---
+++ src/northbridge/intel/e7505/debug.c
@@ -0,0 +1,275 @@
+/*
+ * generic debug code, used by mainboard specific romstage.c
+ *
+ */
+#if 1
+static void print_debug_pci_dev(unsigned dev)
+{
+	print_debug("PCI: ");
+	print_debug_hex8((dev >> 16) & 0xff);
+	print_debug_char(':');
+	print_debug_hex8((dev >> 11) & 0x1f);
+	print_debug_char('.');
+	print_debug_hex8((dev >> 8) & 7);
+}
+
+static inline void print_pci_devices(void)
+{
+	device_t dev;
+	for(dev = PCI_DEV(0, 0, 0);
+		dev <= PCI_DEV(0xff, 0x1f, 0x7);
+		dev += PCI_DEV(0,0,1)) {
+		uint32_t id;
+		id = pci_read_config32(dev, PCI_VENDOR_ID);
+		if (((id & 0xffff) == 0x0000) || ((id & 0xffff) == 0xffff) ||
+			(((id >> 16) & 0xffff) == 0xffff) ||
+			(((id >> 16) & 0xffff) == 0x0000)) {
+			continue;
+		}
+		print_debug_pci_dev(dev);
+		print_debug("\n");
+	}
+}
+
+static void dump_pci_device(unsigned dev)
+{
+	int i;
+	print_debug_pci_dev(dev);
+
+	for(i = 0; i < 256; i++) {
+		unsigned char val;
+		if ((i & 0x0f) == 0) {
+#if CONFIG_CACHE_AS_RAM
+                        printk(BIOS_DEBUG, "\n%02x:",i);
+#else
+			print_debug("\n");
+			print_debug_hex8(i);
+			print_debug_char(':');
+#endif
+		}
+		val = pci_read_config8(dev, i);
+#if CONFIG_CACHE_AS_RAM
+		printk(BIOS_DEBUG, " %02x", val);
+#else
+		print_debug_char(' ');
+		print_debug_hex8(val);
+#endif
+	}
+	print_debug("\n");
+}
+
+static inline void dump_pci_devices(void)
+{
+	device_t dev;
+	for(dev = PCI_DEV(0, 0, 0);
+		dev <= PCI_DEV(0xff, 0x1f, 0x7);
+		dev += PCI_DEV(0,0,1)) {
+		uint32_t id;
+		id = pci_read_config32(dev, PCI_VENDOR_ID);
+		if (((id & 0xffff) == 0x0000) || ((id & 0xffff) == 0xffff) ||
+			(((id >> 16) & 0xffff) == 0xffff) ||
+			(((id >> 16) & 0xffff) == 0x0000)) {
+			continue;
+		}
+		dump_pci_device(dev);
+	}
+}
+
+static inline void dump_pci_devices_on_bus(unsigned busn)
+{
+        device_t dev;
+        for(dev = PCI_DEV(busn, 0, 0);
+                dev <= PCI_DEV(busn, 0x1f, 0x7);
+                dev += PCI_DEV(0,0,1)) {
+                uint32_t id;
+                id = pci_read_config32(dev, PCI_VENDOR_ID);
+                if (((id & 0xffff) == 0x0000) || ((id & 0xffff) == 0xffff) ||
+                        (((id >> 16) & 0xffff) == 0xffff) ||
+                        (((id >> 16) & 0xffff) == 0x0000)) {
+                        continue;
+                }
+                dump_pci_device(dev);
+        }
+}
+
+static inline void dump_spd_registers(const struct mem_controller *ctrl)
+{
+	int i;
+	print_debug("\n");
+	for(i = 0; i < 4; i++) {
+		unsigned device;
+		device = ctrl->channel0[i];
+		if (device) {
+			int j;
+#if CONFIG_CACHE_AS_RAM
+			printk(BIOS_DEBUG, "dimm: %02x.0: %02x", i, device);
+#else
+			print_debug("dimm: ");
+			print_debug_hex8(i);
+			print_debug(".0: ");
+			print_debug_hex8(device);
+#endif
+			for(j = 0; j < 128; j++) {
+				int status;
+				unsigned char byte;
+				if ((j & 0xf) == 0) {
+#if CONFIG_CACHE_AS_RAM
+					printk(BIOS_DEBUG, "\n%02x: ", j);
+#else
+					print_debug("\n");
+					print_debug_hex8(j);
+					print_debug(": ");
+#endif
+				}
+				status = smbus_read_byte(device, j);
+				if (status < 0) {
+					break;
+				}
+				byte = status & 0xff;
+#if CONFIG_CACHE_AS_RAM
+				printk(BIOS_DEBUG, "%02x ", byte);
+#else
+				print_debug_hex8(byte);
+				print_debug_char(' ');
+#endif
+			}
+			print_debug("\n");
+		}
+		device = ctrl->channel1[i];
+		if (device) {
+			int j;
+#if CONFIG_CACHE_AS_RAM
+                        printk(BIOS_DEBUG, "dimm: %02x.1: %02x", i, device);
+#else
+			print_debug("dimm: ");
+			print_debug_hex8(i);
+			print_debug(".1: ");
+			print_debug_hex8(device);
+#endif
+			for(j = 0; j < 128; j++) {
+				int status;
+				unsigned char byte;
+				if ((j & 0xf) == 0) {
+#if CONFIG_CACHE_AS_RAM
+                                        printk(BIOS_DEBUG, "\n%02x: ", j);
+#else
+					print_debug("\n");
+					print_debug_hex8(j);
+					print_debug(": ");
+#endif
+				}
+				status = smbus_read_byte(device, j);
+				if (status < 0) {
+					break;
+				}
+				byte = status & 0xff;
+#if CONFIG_CACHE_AS_RAM
+                                printk(BIOS_DEBUG, "%02x ", byte);
+#else
+				print_debug_hex8(byte);
+				print_debug_char(' ');
+#endif
+			}
+			print_debug("\n");
+		}
+	}
+}
+static inline void dump_smbus_registers(void)
+{
+	unsigned device;
+        print_debug("\n");
+        for(device = 1; device < 0x80; device++) {
+                int j;
+		if( smbus_read_byte(device, 0) < 0 ) continue;
+#if CONFIG_CACHE_AS_RAM
+		printk(BIOS_DEBUG, "smbus: %02x", device);
+#else
+                print_debug("smbus: ");
+                print_debug_hex8(device);
+#endif
+                for(j = 0; j < 256; j++) {
+                	int status;
+                        unsigned char byte;
+                        status = smbus_read_byte(device, j);
+                        if (status < 0) {
+				break;
+                        }
+                        if ((j & 0xf) == 0) {
+#if CONFIG_CACHE_AS_RAM
+				printk(BIOS_DEBUG, "\n%02x: ",j);
+#else
+                	        print_debug("\n");
+                                print_debug_hex8(j);
+                                print_debug(": ");
+#endif
+                        }
+                        byte = status & 0xff;
+#if CONFIG_CACHE_AS_RAM
+                        printk(BIOS_DEBUG, "%02x ", byte);
+#else
+                        print_debug_hex8(byte);
+                        print_debug_char(' ');
+#endif
+                }
+                print_debug("\n");
+	}
+}
+
+static inline void dump_io_resources(unsigned port)
+{
+
+	int i;
+#if CONFIG_CACHE_AS_RAM
+	printk(BIOS_DEBUG, "%04x:\n", port);
+#else
+        print_debug_hex16(port);
+        print_debug(":\n");
+#endif
+        for(i=0;i<256;i++) {
+                uint8_t val;
+                if ((i & 0x0f) == 0) {
+#if CONFIG_CACHE_AS_RAM
+			printk(BIOS_DEBUG, "%02x:", i);
+#else
+                        print_debug_hex8(i);
+                        print_debug_char(':');
+#endif
+                }
+                val = inb(port);
+#if CONFIG_CACHE_AS_RAM
+		printk(BIOS_DEBUG, " %02x",val);
+#else
+                print_debug_char(' ');
+                print_debug_hex8(val);
+#endif
+                if ((i & 0x0f) == 0x0f) {
+                        print_debug("\n");
+                }
+		port++;
+        }
+}
+
+static inline void dump_mem(unsigned start, unsigned end)
+{
+        unsigned i;
+	print_debug("dump_mem:");
+        for(i=start;i<end;i++) {
+		if((i & 0xf)==0) {
+#if CONFIG_CACHE_AS_RAM
+			printk(BIOS_DEBUG, "\n%08x:", i);
+#else
+			print_debug("\n");
+			print_debug_hex32(i);
+			print_debug(":");
+#endif
+		}
+#if CONFIG_CACHE_AS_RAM
+		printk(BIOS_DEBUG, " %02x", (unsigned char)*((unsigned char *)i));
+#else
+		print_debug(" ");
+             	print_debug_hex8((unsigned char)*((unsigned char *)i));
+#endif
+        }
+        print_debug("\n");
+ }
+#endif
---
+++ src/northbridge/intel/e7505/northbridge.c
@@ -0,0 +1,141 @@
+#include <console/console.h>
+#include <arch/io.h>
+#include <stdint.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <cpu/cpu.h>
+#include <stdlib.h>
+#include <string.h>
+#include <bitops.h>
+#include "chip.h"
+
+#if CONFIG_WRITE_HIGH_TABLES==1
+#include <cbmem.h>
+#endif
+
+static void pci_domain_set_resources(device_t dev)
+{
+	device_t mc_dev;
+        uint32_t pci_tolm;
+
+        pci_tolm = find_pci_tolm(dev->link_list);
+	mc_dev = dev->link_list->children;
+	if (mc_dev) {
+		/* Figure out which areas are/should be occupied by RAM.
+		 * This is all computed in kilobytes and converted to/from
+		 * the memory controller right at the edges.
+		 * Having different variables in different units is
+		 * too confusing to get right.  Kilobytes are good up to
+		 * 4 Terabytes of RAM...
+		 */
+		uint16_t tolm_r, remapbase_r, remaplimit_r;
+		unsigned long tomk, tolmk;
+		unsigned long remapbasek, remaplimitk;
+		int idx;
+
+		/* Get the value of the highest DRB. This tells the end of
+		 * the physical memory.  The units are ticks of 64MB
+		 * i.e. 1 means 64MB.
+		 */
+		tomk = ((unsigned long)pci_read_config8(mc_dev, 0x67)) << 16;
+		/* Compute the top of Low memory */
+		tolmk = pci_tolm >> 10;
+		if (tolmk >= tomk) {
+			/* The PCI hole does not overlap memory
+			 * we won't use the remap window.
+			 */
+			tolmk = tomk;
+			remapbasek   = 0x3ff << 16;
+			remaplimitk  = 0 << 16;
+		}
+		else {
+			/* The PCI memory hole overlaps memory
+			 * setup the remap window.
+			 */
+			/* Find the bottom of the remap window
+			 * is it above 4G?
+			 */
+			remapbasek = 4*1024*1024;
+			if (tomk > remapbasek) {
+				remapbasek = tomk;
+			}
+			/* Find the limit of the remap window */
+			remaplimitk = (remapbasek + (4*1024*1024 - tolmk) - (1 << 16));
+		}
+		/* Write the ram configuration registers,
+		 * preserving the reserved bits.
+		 */
+		tolm_r = pci_read_config16(mc_dev, 0xc4);
+		tolm_r = ((tolmk >> 17) << 11) | (tolm_r & 0x7ff);
+		pci_write_config16(mc_dev, 0xc4, tolm_r);
+
+		remapbase_r = pci_read_config16(mc_dev, 0xc6);
+		remapbase_r = (remapbasek >> 16) | (remapbase_r & 0xfc00);
+		pci_write_config16(mc_dev, 0xc6, remapbase_r);
+
+		remaplimit_r = pci_read_config16(mc_dev, 0xc8);
+		remaplimit_r = (remaplimitk >> 16) | (remaplimit_r & 0xfc00);
+		pci_write_config16(mc_dev, 0xc8, remaplimit_r);
+
+		/* Report the memory regions */
+		idx = 10;
+		ram_resource(dev, idx++, 0, 640);
+		ram_resource(dev, idx++, 768, tolmk - 768);
+		if (tomk > 4*1024*1024) {
+			ram_resource(dev, idx++, 4096*1024, tomk - 4*1024*1024);
+		}
+		if (remaplimitk >= remapbasek) {
+			ram_resource(dev, idx++, remapbasek,
+				(remaplimitk + 64*1024) - remapbasek);
+		}
+
+#if CONFIG_WRITE_HIGH_TABLES==1
+		/* Leave some space for ACPI, PIRQ and MP tables */
+		high_tables_base = (tolmk * 1024) - HIGH_MEMORY_SIZE;
+		high_tables_size = HIGH_MEMORY_SIZE;
+#endif
+	}
+	assign_resources(dev->link_list);
+}
+
+static struct device_operations pci_domain_ops = {
+        .read_resources   = pci_domain_read_resources,
+        .set_resources    = pci_domain_set_resources,
+        .enable_resources = NULL,
+        .init             = NULL,
+        .scan_bus         = pci_domain_scan_bus,
+	.ops_pci_bus      = &pci_cf8_conf1,
+};
+
+static void cpu_bus_init(device_t dev)
+{
+        initialize_cpus(dev->link_list);
+}
+
+static void cpu_bus_noop(device_t dev)
+{
+}
+
+static struct device_operations cpu_bus_ops = {
+        .read_resources   = cpu_bus_noop,
+        .set_resources    = cpu_bus_noop,
+        .enable_resources = cpu_bus_noop,
+        .init             = cpu_bus_init,
+        .scan_bus         = 0,
+};
+
+static void enable_dev(struct device *dev)
+{
+        /* Set the operations if it is a special bus type */
+        if (dev->path.type == DEVICE_PATH_PCI_DOMAIN) {
+                dev->ops = &pci_domain_ops;
+        }
+        else if (dev->path.type == DEVICE_PATH_APIC_CLUSTER) {
+                dev->ops = &cpu_bus_ops;
+        }
+}
+
+struct chip_operations northbridge_intel_e7501_ops = {
+	CHIP_NAME("Intel E7501 Northbridge")
+	.enable_dev = enable_dev,
+};

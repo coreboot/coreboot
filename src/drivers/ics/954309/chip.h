b4631f9483fc7ddcad53ed66901fc970a6b45f2d
Sven Schnelle <svens@stackframe.org>
2011-10-23 15:53:47 +0200

    Add driver for ICS954309 clock generator
    Change-Id: Iac7e91cdd995dad1954eaa2d4dd52bffa293fc95
    Signed-off-by: Sven Schnelle <svens@stackframe.org>

---
+++ src/drivers/ics/954309/chip.h
@@ -0,0 +1,16 @@
+extern struct chip_operations drivers_ics_954309_ops;
+
+struct drivers_ics_954309_config {
+	u8 reg0;
+	u8 reg1;
+	u8 reg2;
+	u8 reg3;
+	u8 reg4;
+	u8 reg5;
+	u8 reg6;
+	u8 reg7;
+	u8 reg8;
+	u8 reg9;
+	u8 reg10;
+	u8 reg11;
+};

6e237f330edcfee3ec6ca0202e919ab9e2441966
Duncan Laurie <dlaurie@chromium.org>
2011-10-25 14:15:11 -0700

    Add support for enabling PCIe Common Clock and ASPM
    These are guarded by individual Kconfig entries. The deprecated
    CONFIG_PCIE_TUNING defines have been removed in favor of using specific
    config options.
    
    This is the generic half, there is board-specific pieces
    still to come that tune before and after ASPM is enabled.
    
    Change-Id: I3fe46282eada67629e9eeeed07e487dff54f2729
    Signed-off-by: Duncan Laurie <dlaurie@google.com>

--- src/include/device/pciexp.h
+++ src/include/device/pciexp.h
@@ -2,6 +2,13 @@
 #define DEVICE_PCIEXP_H
 /* (c) 2005 Linux Networx GPL see COPYING for details */
 
+enum aspm_type {
+	PCIE_ASPM_NONE = 0,
+	PCIE_ASPM_L0S  = 1,
+	PCIE_ASPM_L1   = 2,
+	PCIE_ASPM_BOTH = 3,
+};
+
 unsigned int pciexp_scan_bus(struct bus *bus, unsigned int min_devfn,
 			     unsigned int max_devfn, unsigned int max);
 unsigned int pciexp_scan_bridge(device_t dev, unsigned int max);

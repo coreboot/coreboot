24e70cf2a2594bda06b4e6de943a75a27d0256f8
Florian Zumbiehl <florz@florz.de>
2011-11-01 20:18:30 +0100

    factor out common config for k8x8xx's dram_enable() and vt8237r_cfg()
    Instead of writing to config registers in k8x8xx's dram_enable()
    and reading those back in vt8237r_cfg(), factor out generation of
    the values and reuse that in both places.
    
    Change-Id: I87a37398efe84b33e6678df74cd40b5abfe4f879
    Signed-off-by: Florian Zumbiehl <florz@florz.de>

--- src/southbridge/via/k8t890/k8x8xx.h
+++ src/southbridge/via/k8t890/k8x8xx.h
@@ -21,6 +21,31 @@
 #ifndef SOUTHBRIDGE_VIA_K8T890_K8X8XX_H
 #define SOUTHBRIDGE_VIA_K8T890_K8X8XX_H
 
+#include <cpu/x86/msr.h>
+#include <cpu/amd/mtrr.h>
 #include "k8t890.h"
+
+struct k8x8xx_vt8237_mirrored_regs {
+	u16 low_top_address;
+	u8 rom_shadow_ctrl_pg_c,
+		rom_shadow_ctrl_pg_d,
+		rom_shadow_ctrl_pg_e_memhole_smi_decoding,
+		rom_shadow_ctrl_pg_f_memhole,
+		smm_apic_decoding,
+		shadow_mem_ctrl;
+};
+
+static inline void k8x8xx_vt8237_mirrored_regs_fill(struct k8x8xx_vt8237_mirrored_regs *regs){
+	msr_t msr;
+
+	regs->rom_shadow_ctrl_pg_c = 0xff;
+	regs->rom_shadow_ctrl_pg_d = 0xff;
+	regs->rom_shadow_ctrl_pg_e_memhole_smi_decoding = 0xff;
+	regs->rom_shadow_ctrl_pg_f_memhole = 0x30;
+	regs->smm_apic_decoding = 0x19;
+	msr = rdmsr(TOP_MEM);
+	regs->shadow_mem_ctrl = msr.lo >> 24;
+	regs->low_top_address = msr.lo >> 16;
+}
 
 #endif /* SOUTHBRIDGE_VIA_K8T890_K8X8XX_H */

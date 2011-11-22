3344880a57c8e1083f5580d6990885f3b976ae6c
Rudolf Marek <r.marek@assembler.cz>
2011-11-23 00:23:43 +0100

    Fix the DIMM slot mapping
    Fix the DIMM mappings, channel 0 is "B" on board,
    and secondary channel is on 0x51,0x53
    
    Change-Id: I8c49c4efb90a4297aaea0be2159435dadab9ac0a
    Signed-off-by: Rudolf Marek <r.marek@assembler.cz>

--- src/mainboard/asus/a8v-e_se/romstage.c
+++ src/mainboard/asus/a8v-e_se/romstage.c
@@ -147,10 +147,10 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	static const uint16_t spd_addr[] = {
 		// Node 0
 		DIMM0, DIMM2, 0, 0,
-		DIMM1, DIMM3, 0, 0,
+		0, 0, 0, 0,
 		// Node 1
-		DIMM4, DIMM6, 0, 0,
-		DIMM5, DIMM7, 0, 0,
+		DIMM1, DIMM3, 0, 0,
+		0, 0, 0, 0,
 	};
 	unsigned bsp_apicid = 0;
 	int needs_reset = 0;

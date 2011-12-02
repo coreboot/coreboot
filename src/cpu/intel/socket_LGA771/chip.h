3220062a8c6312889c98688c6cee0ced13e31dbf
Sven Schnelle <svens@stackframe.org>
2011-12-02 16:21:35 +0100

    Add Intel Socket LGA771
    Change-Id: Iee7d3ff2884d8c43ff1af498160589e551bc9cc8
    Signed-off-by: Sven Schnelle <svens@stackframe.org>

---
+++ src/cpu/intel/socket_LGA771/chip.h
@@ -0,0 +1,4 @@
+extern struct chip_operations cpu_intel_socket_LGA771_ops;
+
+struct cpu_intel_socket_LGA771_config {
+};
---
+++ src/cpu/intel/socket_LGA771/socket_LGA771.c
@@ -0,0 +1,7 @@
+#include <device/device.h>
+#include "chip.h"
+
+
+struct chip_operations cpu_intel_socket_LGA771_ops = {
+	CHIP_NAME("Socket LGA771 CPU")
+};

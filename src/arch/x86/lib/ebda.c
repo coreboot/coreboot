a3b2c440baca27f9275b2e4bb7cb591c565fc08d
Duncan Laurie <dlaurie@chromium.org>
2012-01-17 09:03:11 -0800

    Prepare the BIOS data areas before device init.
    Since we do not run option roms in normal mode nothing was
    initializing the BDA/EBDA and yet Linux depends very much
    on it having sane values here.  For the most part the kernel
    tries to work around this not being initialized, but every
    once in awhile (1/300 boots or so) it would end up reading
    something that looked sane from BDA but was not and then
    it would panic.
    
    In this change the EBDA is unconditionally setup before devices
    are initialized.  I'm not set on the location in dev_initialize()
    but there does not seem to be another place to hook it in so
    that it runs just once for ALL platforms regardless of whether
    they use option roms or not. (possibly hardwaremain?)
    
    The EBDA setup code has been moved into its own location in
    arch/x86/lib/ebda.c so it can be compiled in even if the option
    rom code is not.
    
    The low memory size is still set to 1MB which is enough to make
    linux happy without having to hook into each mainboard to get a
    more appropriate value.  The setup_ebda() function takes inputs
    so it could be changed for a mainboard if needed.
    
    OLD/BROKEN would read garbage.  Examples from different boots:
    ebda_addr=0x75e80 lowmem=0x1553400
    ebda_addr=0x5e080 lowmem=0x3e51400
    ebda_addr=0x7aa80 lowmem=0x2f8a800
    
    NEW/FIXED now reads consistent values:
    ebda_addr=0xf6000 lowmem=0x100000
    
    Change-Id: I6cb79f0e3e43cc65f7e5fe98b6cad1a557ccd949
    Signed-off-by: Duncan Laurie <dlaurie@google.com>

---
+++ src/arch/x86/lib/ebda.c
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 The Chromium OS Authors. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include <arch/io.h>
+#include <arch/ebda.h>
+
+void setup_ebda(u32 low_memory_size, u16 ebda_segment, u16 ebda_size)
+{
+	if (!low_memory_size || !ebda_segment || !ebda_size)
+		return;
+
+	/* clear BIOS DATA AREA */
+	memset((void *)X86_BDA_BASE, 0, X86_BDA_SIZE);
+
+	write16(X86_EBDA_LOWMEM, (low_memory_size >> 10));
+	write16(X86_EBDA_SEGMENT, ebda_segment);
+
+	/* Set up EBDA */
+	memset((void *)(ebda_segment << 4), 0, ebda_size);
+	write16((ebda_segment << 4), (ebda_size >> 10));
+}
+
+void setup_default_ebda(void)
+{
+	setup_ebda(DEFAULT_EBDA_LOWMEM,
+		   DEFAULT_EBDA_SEGMENT,
+		   DEFAULT_EBDA_SIZE);
+}

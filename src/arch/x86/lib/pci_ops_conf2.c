07a809248b817de1f1476cf84d4b89f8e98e43eb
Vikram Narayanan <vikram186@gmail.com>
2012-01-24 19:17:47 +0530

    pci_ops_conf: Indentation fixes
    Indentation fixes in src/arch/x86/lib/pci_ops_conf{1,2}.c
    
    Change-Id: I56e8ff6d2ee3a0b871b40577e10c99dea4b3b1bd
    Signed-off-by: Vikram Narayanan <vikram186@gmail.com>

--- src/arch/x86/lib/pci_ops_conf2.c
+++ src/arch/x86/lib/pci_ops_conf2.c
@@ -12,7 +12,8 @@
 #define FUNC(devfn)		(((devfn & 7) << 1) | 0xf0)
 #define SET(bus,devfn)		outb(FUNC(devfn), 0xCF8); outb(bus, 0xCFA);
 
-static uint8_t pci_conf2_read_config8(struct bus *pbus, int bus, int devfn, int where)
+static uint8_t pci_conf2_read_config8(struct bus *pbus, int bus, int devfn,
+				      int where)
 {
 	uint8_t value;
 	SET(bus, devfn);
@@ -21,7 +22,8 @@ static uint8_t pci_conf2_read_config8(struct bus *pbus, int bus, int devfn, int 
 	return value;
 }
 
-static uint16_t pci_conf2_read_config16(struct bus *pbus, int bus, int devfn, int where)
+static uint16_t pci_conf2_read_config16(struct bus *pbus, int bus, int devfn,
+					int where)
 {
 	uint16_t value;
 	SET(bus, devfn);
@@ -30,7 +32,8 @@ static uint16_t pci_conf2_read_config16(struct bus *pbus, int bus, int devfn, in
 	return value;
 }
 
-static uint32_t pci_conf2_read_config32(struct bus *pbus, int bus, int devfn, int where)
+static uint32_t pci_conf2_read_config32(struct bus *pbus, int bus, int devfn,
+					int where)
 {
 	uint32_t value;
 	SET(bus, devfn);
@@ -39,21 +42,24 @@ static uint32_t pci_conf2_read_config32(struct bus *pbus, int bus, int devfn, in
 	return value;
 }
 
-static void pci_conf2_write_config8(struct bus *pbus, int bus, int devfn, int where, uint8_t value)
+static void pci_conf2_write_config8(struct bus *pbus, int bus, int devfn,
+				    int where, uint8_t value)
 {
 	SET(bus, devfn);
 	outb(value, IOADDR(devfn, where));
 	outb(0, 0xCF8);
 }
 
-static void pci_conf2_write_config16(struct bus *pbus, int bus, int devfn, int where, uint16_t value)
+static void pci_conf2_write_config16(struct bus *pbus, int bus, int devfn,
+				     int where, uint16_t value)
 {
 	SET(bus, devfn);
 	outw(value, IOADDR(devfn, where));
 	outb(0, 0xCF8);
 }
 
-static void pci_conf2_write_config32(struct bus *pbus, int bus, int devfn, int where, uint32_t value)
+static void pci_conf2_write_config32(struct bus *pbus, int bus, int devfn,
+				     int where, uint32_t value)
 {
 	SET(bus, devfn);
 	outl(value, IOADDR(devfn, where));
@@ -64,13 +70,11 @@ static void pci_conf2_write_config32(struct bus *pbus, int bus, int devfn, int w
 #undef IOADDR
 #undef FUNC
 
-const struct pci_bus_operations pci_cf8_conf2 =
-{
-	.read8  = pci_conf2_read_config8,
+const struct pci_bus_operations pci_cf8_conf2 = {
+	.read8 = pci_conf2_read_config8,
 	.read16 = pci_conf2_read_config16,
 	.read32 = pci_conf2_read_config32,
-	.write8  = pci_conf2_write_config8,
+	.write8 = pci_conf2_write_config8,
 	.write16 = pci_conf2_write_config16,
 	.write32 = pci_conf2_write_config32,
 };
-

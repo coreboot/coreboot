76c976b87d92902e39be9b2d5d60c13b1a81dd75
Vikram Narayanan <vikram186@gmail.com>
2012-01-25 20:40:40 +0530

    vga: removed inclusion of .c files
    Add local vga.h for prototypes.
    
    Change-Id: I5ff627c6420d4b7fd1bc9a537f406ef6d9597522
    Signed-off-by: Vikram Narayanan <vikram186@gmail.com>
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

---
+++ src/pc80/vga/vga.h
@@ -0,0 +1,17 @@
+#ifndef VGA_H
+#define VGA_H
+
+/*
+ * Basic palette.
+ */
+struct palette {
+        unsigned char red;
+        unsigned char green;
+        unsigned char blue;
+};
+
+extern const struct palette default_vga_palette[0x100];
+
+extern const unsigned char vga_font_8x16[256][16];
+
+#endif /* VGA_H */

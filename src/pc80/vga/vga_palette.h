28d92e158bd1b0647d9fdd18b3e7fc7b054de165
Vikram Narayanan <vikram186@gmail.com>
2012-01-25 20:40:40 +0530

    vga: removed inclusion of .c file
    Renamed vga_palette.c -> vga_palette.h
    Moved structure initialization to vga.c file.
    Made appropriate changes in vga.c
    
    Change-Id: If3e998e609e0a2fcbfad3df79fbbc755acd996c1
    Signed-off-by: Vikram Narayanan <vikram186@gmail.com>

---
+++ src/pc80/vga/vga_palette.h
@@ -0,0 +1,13 @@
+#ifndef VGA_PALETTE_H
+#define VGA_PALETTE_H
+
+/*
+ * Basic palette.
+ */
+struct palette {
+        unsigned char red;
+        unsigned char green;
+        unsigned char blue;
+};
+
+#endif /* VGA_PALETTE_H */

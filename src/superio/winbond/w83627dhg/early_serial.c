b6bebf4968019fd38a7086cbf350a9c26b4451a3
Kerry Sheh <shekairui@gmail.com>
2012-02-07 20:32:37 +0800

    SIO: Winbond w83627dhg update
    1. Stop include c file.
    2. W83627dhg Pin 89, Pin 90 are multi function pins,
       add support to select them to I2C function.
    
    Change-Id: I42eaaf7d70aa48d7edf2710349b51e401526c1a6
    Signed-off-by: Kerry Sheh <kerry.she@amd.com>
    Signed-off-by: Kerry Sheh <shekairui@gmail.com>

--- src/superio/winbond/w83627dhg/early_serial.c
+++ src/superio/winbond/w83627dhg/early_serial.c
@@ -2,6 +2,7 @@
  * This file is part of the coreboot project.
  *
  * Copyright (C) 2008 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2012 Advanced Micro Devices, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,24 +19,26 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
+#include <arch/io.h>
 #include <arch/romcc_io.h>
+#include <device/pnp_def.h>
 #include <stdint.h>
 #include "w83627dhg.h"
 
-static void pnp_enter_ext_func_mode(device_t dev)
+void pnp_enter_ext_func_mode(device_t dev)
 {
 	u16 port = dev >> 8;
 	outb(0x87, port);
 	outb(0x87, port);
 }
 
-static void pnp_exit_ext_func_mode(device_t dev)
+void pnp_exit_ext_func_mode(device_t dev)
 {
 	u16 port = dev >> 8;
 	outb(0xaa, port);
 }
 
-static void w83627dhg_enable_serial(device_t dev, u16 iobase)
+void w83627dhg_enable_serial(device_t dev, u16 iobase)
 {
 	pnp_enter_ext_func_mode(dev);
 	pnp_set_logical_device(dev);
@@ -44,3 +47,23 @@ static void w83627dhg_enable_serial(device_t dev, u16 iobase)
 	pnp_set_enable(dev, 1);
 	pnp_exit_ext_func_mode(dev);
 }
+
+/**
+ * Select Pin 89, Pin 90 function as I2C interface SDA, SCL.
+ *  {Pin 89, Pin 90} function can be selected as {GP33, GP32}, or
+ *  {RSTOUT3#, RSTOUT2#} or {SDA, SCL}
+ */
+void w83627dhg_enable_i2c(device_t dev)
+{
+	u8 val;
+
+	pnp_enter_ext_func_mode(dev);
+	pnp_set_logical_device(dev);
+
+	val = pnp_read_config(dev, 0x2A);
+	val |= 1 << 1;
+	pnp_write_config(dev, 0x2A, val);
+
+	pnp_exit_ext_func_mode(dev);
+}
+

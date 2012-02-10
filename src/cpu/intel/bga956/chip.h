b8948dffe9dc658a915399f80a0e06d78bf983f4
Kyösti Mälkki <kyosti.malkki@gmail.com>
2012-02-10 12:42:49 +0200

    Intel cpus: apply un-written naming rules
    Kconfig directives to select chip drivers for compile literally
    match the chip directory names capitalized and underscored.
    
    Note: CPU_INTEL_CORE2 was used on both model_6fx and model_1067x.
    
    Change-Id: I8fa5ba71b14dcce79ab2a2c1c69b3bc36edbdea0
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/cpu/intel/bga956/chip.h
+++
@@ -1,4 +0,0 @@
-extern struct chip_operations cpu_intel_bga956_ops;
-
-struct cpu_intel_bga956_config {
-};
---
+++ src/cpu/intel/socket_BGA956/socket_BGA956.c
@@ -0,0 +1,7 @@
+#include <device/device.h>
+#include "chip.h"
+
+
+struct chip_operations cpu_intel_socket_bga956_ops = {
+	CHIP_NAME("Socket BGA956 CPU")
+};

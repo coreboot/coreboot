ea119ccdfd79f8373abf1b0b2717718e5d97d05d
Stefan Reinauer <reinauer@chromium.org>
2012-03-30 13:52:58 -0700

    Add a helper function to determine the number of enabled CPUs
    Change-Id: Ia72926002571e0f250849fa5db048bd8b2e92400
    Signed-off-by: Duncan Laurie <dlaurie@google.com>

--- src/include/device/device.h
+++ src/include/device/device.h
@@ -133,6 +133,7 @@ device_t dev_find_class (unsigned int class, device_t from);
 device_t dev_find_slot (unsigned int bus, unsigned int devfn);
 device_t dev_find_slot_on_smbus (unsigned int bus, unsigned int addr);
 device_t dev_find_lapic(unsigned apic_id);
+int dev_count_cpu(void);
 
 /* Debug functions */
 void print_resource_tree(struct device * root, int debug_level,

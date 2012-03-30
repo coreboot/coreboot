f62e2d8ad35b6f7e1b6c56c04361fece45f9650b
Stefan Reinauer <reinauer@chromium.org>
2012-03-30 15:04:07 -0700

    Add support for ITE IT8772F SuperI/O chip
    Change-Id: I8e80c22eb0f3cb68f2457be6b2e7894df60ed632
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

---
+++ src/superio/ite/it8772f/it8772f.h
@@ -0,0 +1,120 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 The ChromiumOS Authors.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef SUPERIO_ITE_IT8772F_IT8772F_H
+#define SUPERIO_ITE_IT8772F_IT8772F_H
+
+#define IT8772F_BASE 0x2e
+
+#define IT8772F_FDC  0x00 /* Floppy disk controller */
+#define IT8772F_SP1  0x01 /* Com1 */
+#define IT8772F_EC   0x04 /* Environment controller */
+#define IT8772F_KBCK 0x05 /* Keyboard */
+#define IT8772F_KBCM 0x06 /* Mouse */
+#define IT8772F_GPIO 0x07 /* GPIO */
+#define IT8772F_IR   0x0a /* Consumer IR */
+
+/* Environmental Controller interface */
+#define IT8772F_INTERFACE_SELECT		0x0a
+#define  IT8772F_INTERFACE_PSEUDO_EOC		(1 << 7)
+#define  IT8772F_INTERFACE_SMB_ENABLE		(1 << 6)
+#define  IT8772F_INTERFACE_SEL_DISABLE		(0 << 4)
+#define  IT8772F_INTERFACE_SEL_SST_SLAVE	(1 << 4)
+#define  IT8772F_INTERFACE_SEL_PECI		(2 << 4)
+#define  IT8772F_INTERFACE_SEL_SST_HOST		(3 << 4)
+#define  IT8772F_INTERFACE_CLOCK_32MHZ		(0 << 3)
+#define  IT8772F_INTERFACE_CLOCK_24MHZ		(1 << 3)
+#define  IT8772F_INTERFACE_SPEED_TOLERANCE	(1 << 2)
+#define  IT8772F_INTERFACE_PECI_AWFCS		(1 << 0)
+#define IT8772F_FAN_TAC2_READ			0x0e
+#define IT8772F_FAN_TAC3_READ			0x0f
+#define IT8772F_FAN_MAIN_CTL			0x13
+#define  IT8772F_FAN_MAIN_CTL_TAC_SMART(x)	(1 << ((x)-1))
+#define  IT8772F_FAN_MAIN_CTL_TAC_EN(x)		(1 << ((x)+3))
+#define IT8772F_FAN_CTL_MODE			0x14
+#define  IT8772F_FAN_CTL_ON(x)			(1 << ((x)-1))
+#define  IT8772F_FAN_PWM_MIN_DUTY		(1 << 3)
+#define  IT8772F_FAN_PWM_CLOCK_48MHZ		(0 << 4)
+#define  IT8772F_FAN_PWM_CLOCK_24MHZ		(1 << 4)
+#define  IT8772F_FAN_PWM_CLOCK_8MHZ		(3 << 4)
+#define  IT8772F_FAN_PWM_CLOCK_6MHZ		(4 << 4)
+#define  IT8772F_FAN_PWM_CLOCK_3MHZ		(5 << 4)
+#define  IT8772F_FAN_PWM_CLOCK_1_5MHZ		(6 << 4)
+#define  IT8772F_FAN_PWM_CLOCK_51KHZ		(7 << 4)
+#define  IT8772F_FAN_CTL_POLARITY_LOW		(0 << 7)
+#define  IT8772F_FAN_CTL_POLARITY_HIGH		(1 << 7)
+#define IT8772F_FAN_CTL2_PWM_MODE		0x16
+#define IT8772F_FAN_CTL3_PWM_MODE		0x17
+#define  IT8772F_FAN_CTL_PWM_MODE_SOFTWARE	(0 << 7)
+#define  IT8772F_FAN_CTL_PWM_MODE_AUTOMATIC	(1 << 7)
+#define IT8772F_ADC_TEMP_CHANNEL_ENABLE		0x51
+#define IT8772F_FAN_CTL2_PWM_START		0x6b
+#define IT8772F_FAN_CTL2_AUTO_MODE		0x6c
+#define IT8772F_FAN_CTL3_PWM_START		0x73
+#define IT8772F_FAN_CTL3_AUTO_MODE		0x74
+#define  IT8772F_FAN_CTL_AUTO_SMOOTHING_DIS	(0 << 7)
+#define  IT8772F_FAN_CTL_AUTO_SMOOTHING_EN	(1 << 7)
+#define IT8772F_EXTEMP_STATUS			0x88
+#define IT8772F_EXTEMP_ADDRESS			0x89
+#define IT8772F_EXTEMP_WRITE_LENGTH		0x8a
+#define IT8772F_EXTEMP_READ_LENGTH		0x8b
+#define IT8772F_EXTEMP_COMMAND			0x8c
+#define IT8772F_EXTEMP_WRITE_DATA_2		0x8d
+#define IT8772F_EXTEMP_CONTROL			0x8e
+#define  IT8772F_EXTEMP_CONTROL_AUTO_32HZ	(0 << 6)
+#define  IT8772F_EXTEMP_CONTROL_AUTO_16HZ	(1 << 6)
+#define  IT8772F_EXTEMP_CONTROL_AUTO_8HZ	(2 << 6)
+#define  IT8772F_EXTEMP_CONTROL_AUTO_4HZ	(3 << 6)
+#define  IT8772F_EXTEMP_CONTROL_AUTO_START	(1 << 5)
+#define  IT8772F_EXTEMP_CONTROL_AUTO_ABORT	(1 << 4)
+#define  IT8772F_EXTEMP_CONTROL_AUTO_TWO_DOMAIN	(1 << 3)
+#define  IT8772F_EXTEMP_CONTROL_CONTENTION	(1 << 2)
+#define  IT8772F_EXTEMP_CONTROL_SST_IDLE_HIGH	(1 << 1)
+#define  IT8772F_EXTEMP_CONTROL_START		(1 << 0)
+
+/* Standard PECI GetTemp */
+#define PECI_CLIENT_ADDRESS			0x30
+#define PECI_GETTEMP_COMMAND			0x01
+#define PECI_GETTEMP_WRITE_LENGTH		0x01
+#define PECI_GETTEMP_READ_LENGTH		0x02
+
+/* GPIO interface */
+#define IT8772F_GPIO_LED_BLINK1_PINMAP		0xf8
+#define IT8772F_GPIO_LED_BLINK1_CONTROL		0xf9
+
+#define GPIO_REG_SELECT(x)   (0x25 + (x))
+#define GPIO_REG_POLARITY(x) (0xb0 + (x))
+#define GPIO_REG_PULLUP(x)   (0xb8 + (x))
+#define GPIO_REG_ENABLE(x)   (0xc0 + (x))
+#define GPIO_REG_OUTPUT(x)   (0xc8 + (x))
+
+#ifndef __ROMCC__
+u8 it8772f_sio_read(u8 index);
+void it8772f_sio_write(u8 index, u8 value);
+void it8772f_enable_serial(device_t dev, u16 iobase);
+void it8772f_kill_watchdog(void);
+void it8772f_24mhz_clkin(void);
+void it8772f_enable_3vsbsw(void);
+void it8772f_ac_resume_southbridge(void);
+void it8772f_gpio_setup(int set, u8 func_select, u8 polarity, u8 pullup,
+			u8 output, u8 enable);
+#endif
+
+#endif
---
+++ src/superio/ite/it8772f/superio.c
@@ -0,0 +1,250 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 The ChromiumOS Authors.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include <device/device.h>
+#include <device/pnp.h>
+#include <uart8250.h>
+#include <pc80/keyboard.h>
+#include <arch/io.h>
+#include <stdlib.h>
+#include "chip.h"
+#include "it8772f.h"
+
+static void pnp_enter_ext_func_mode(device_t dev)
+{
+	u16 port = dev->path.pnp.port;
+
+	outb(0x87, port);
+	outb(0x01, port);
+	outb(0x55, port);
+	outb((port == 0x4e) ? 0xaa : 0x55, port);
+}
+
+static void pnp_exit_ext_func_mode(device_t dev)
+{
+	pnp_write_config(dev, 0x02, 0x02);
+}
+
+static inline u8 it8772f_envc_read(struct resource *res, u8 addr)
+{
+	outb(addr, res->base + 5);
+	return inb(res->base + 6);
+}
+
+static inline void it8772f_envc_write(struct resource *res, u8 addr, u8 value)
+{
+	outb(addr, res->base + 5);
+	outb(value, res->base + 6);
+}
+
+/*
+ * Setup External Temperature to read via PECI into TMPINx register
+ */
+static void it8772f_enable_peci(struct resource *res, int tmpin)
+{
+	if (tmpin < 1 || tmpin > 3)
+		return;
+
+	/* Enable PECI interface */
+	it8772f_envc_write(res, IT8772F_INTERFACE_SELECT,
+			   IT8772F_INTERFACE_SEL_PECI |
+			   IT8772F_INTERFACE_SPEED_TOLERANCE);
+
+	/* Setup External Temperature using PECI GetTemp */
+	it8772f_envc_write(res, IT8772F_EXTEMP_ADDRESS,
+			   PECI_CLIENT_ADDRESS);
+	it8772f_envc_write(res, IT8772F_EXTEMP_COMMAND,
+			   PECI_GETTEMP_COMMAND);
+	it8772f_envc_write(res, IT8772F_EXTEMP_WRITE_LENGTH,
+			   PECI_GETTEMP_WRITE_LENGTH);
+	it8772f_envc_write(res, IT8772F_EXTEMP_READ_LENGTH,
+			   PECI_GETTEMP_READ_LENGTH);
+	it8772f_envc_write(res, IT8772F_EXTEMP_CONTROL,
+			   IT8772F_EXTEMP_CONTROL_AUTO_4HZ |
+			   IT8772F_EXTEMP_CONTROL_AUTO_START);
+
+	/* External Temperature reported in TMPINx register */
+	it8772f_envc_write(res, IT8772F_ADC_TEMP_CHANNEL_ENABLE,
+			   (tmpin & 3) << 6);
+}
+
+/*
+ * Setup a FAN PWM interface for software control
+ */
+static void it8772f_enable_fan(struct resource *res, int fan)
+{
+	u8 reg;
+
+	if (fan < 1 || fan > 3)
+		return;
+
+	/* Enable 6MHz (23.43kHz PWM) active high output */
+	reg = it8772f_envc_read(res, IT8772F_FAN_CTL_MODE);
+	reg |= IT8772F_FAN_CTL_ON(fan) |
+		IT8772F_FAN_PWM_CLOCK_6MHZ |
+		IT8772F_FAN_CTL_POLARITY_HIGH;
+	it8772f_envc_write(res, IT8772F_FAN_CTL_MODE, reg);
+
+	/* Enable output in smart mode */
+	reg = it8772f_envc_read(res, IT8772F_FAN_MAIN_CTL);
+	reg |= IT8772F_FAN_MAIN_CTL_TAC_SMART(fan);
+	reg |= IT8772F_FAN_MAIN_CTL_TAC_EN(fan);
+	it8772f_envc_write(res, IT8772F_FAN_MAIN_CTL, reg);
+
+	switch (fan) {
+	case 2:
+		/* Enable software operation */
+		it8772f_envc_write(res, IT8772F_FAN_CTL2_PWM_MODE,
+				   IT8772F_FAN_CTL_PWM_MODE_SOFTWARE);
+		/* Disable Smoothing */
+		it8772f_envc_write(res, IT8772F_FAN_CTL2_AUTO_MODE,
+				   IT8772F_FAN_CTL_AUTO_SMOOTHING_DIS);
+		/* Set a default medium fan speed */
+		it8772f_envc_write(res, IT8772F_FAN_CTL2_PWM_START, 0x80);
+		break;
+	case 3:
+		/* Enable software operation */
+		it8772f_envc_write(res, IT8772F_FAN_CTL3_PWM_MODE,
+				   IT8772F_FAN_CTL_PWM_MODE_SOFTWARE);
+		/* Disable Smoothing */
+		it8772f_envc_write(res, IT8772F_FAN_CTL3_AUTO_MODE,
+				   IT8772F_FAN_CTL_AUTO_SMOOTHING_DIS);
+		/* Set a default medium fan speed */
+		it8772f_envc_write(res, IT8772F_FAN_CTL3_PWM_START, 0x80);
+		break;
+	}
+}
+
+static void it8772f_init(device_t dev)
+{
+	struct superio_ite_it8772f_config *conf = dev->chip_info;
+	struct resource *res;
+
+	if (!dev->enabled)
+		return;
+
+	switch (dev->path.pnp.device) {
+	case IT8772F_EC:
+		res = find_resource(dev, PNP_IDX_IO0);
+		if (!res)
+			break;
+
+		/* Enable PECI if configured */
+		it8772f_enable_peci(res, conf->peci_tmpin);
+
+		/* Enable FANx if configured */
+		if (conf->fan1_enable)
+			it8772f_enable_fan(res, 1);
+		if (conf->fan2_enable)
+			it8772f_enable_fan(res, 2);
+		if (conf->fan3_enable)
+			it8772f_enable_fan(res, 3);
+		break;
+	case IT8772F_GPIO:
+		/* Set GPIO output levels */
+		res = find_resource(dev, PNP_IDX_IO1);
+		if (res) {
+			if (conf->gpio_set1)
+				outb(conf->gpio_set1, res->base + 0);
+			if (conf->gpio_set2)
+				outb(conf->gpio_set2, res->base + 1);
+			if (conf->gpio_set3)
+				outb(conf->gpio_set3, res->base + 2);
+			if (conf->gpio_set4)
+				outb(conf->gpio_set4, res->base + 3);
+			if (conf->gpio_set5)
+				outb(conf->gpio_set5, res->base + 4);
+			if (conf->gpio_set6)
+				outb(conf->gpio_set6, res->base + 5);
+		}
+		break;
+	case IT8772F_KBCK:
+		if (!conf->skip_keyboard) {
+			set_kbc_ps2_mode();
+			pc_keyboard_init(&conf->keyboard);
+		}
+		break;
+	case IT8772F_KBCM:
+		break;
+	case IT8772F_IR:
+		break;
+	}
+}
+
+static void it8772f_pnp_set_resources(device_t dev)
+{
+	pnp_enter_ext_func_mode(dev);
+	pnp_set_resources(dev);
+	pnp_exit_ext_func_mode(dev);
+}
+
+static void it8772f_pnp_enable_resources(device_t dev)
+{
+	pnp_enter_ext_func_mode(dev);
+	pnp_enable_resources(dev);
+	pnp_exit_ext_func_mode(dev);
+}
+
+static void it8772f_pnp_enable(device_t dev)
+{
+	pnp_enter_ext_func_mode(dev);
+	pnp_set_logical_device(dev);
+	pnp_set_enable(dev, !!dev->enabled);
+	pnp_exit_ext_func_mode(dev);
+}
+
+static struct device_operations ops = {
+	.read_resources		= pnp_read_resources,
+	.set_resources		= it8772f_pnp_set_resources,
+	.enable_resources	= it8772f_pnp_enable_resources,
+	.enable			= it8772f_pnp_enable,
+	.init			= it8772f_init,
+};
+
+static struct pnp_info pnp_dev_info[] = {
+	/* Floppy Disk Controller */
+	{ &ops, IT8772F_FDC, PNP_IO0 | PNP_IRQ0, {0x0ff8, 0}, },
+	/* Serial Port 1 */
+	{ &ops, IT8772F_SP1, PNP_IO0 | PNP_IRQ0, {0x0ff8, 0}, },
+	/* Environmental Controller */
+	{ &ops, IT8772F_EC, PNP_IO0 | PNP_IO1 | PNP_IRQ0,
+	  {0x0ff8, 0}, {0x0ffc, 4}, },
+	/* KBC Keyboard */
+	{ &ops, IT8772F_KBCK, PNP_IO0 | PNP_IO1 | PNP_IRQ0,
+	  {0x0fff, 0}, {0x0fff, 4}, },
+	/* KBC Mouse */
+	{ &ops, IT8772F_KBCM, PNP_IRQ0, },
+	/* 27 GPIOs */
+	{ &ops, IT8772F_GPIO, PNP_IO0 | PNP_IO1,
+	  {0x0fff, 0}, {0x0ff8, 0}, },
+	/* Infrared */
+	{ &ops, IT8772F_IR, PNP_IO0 | PNP_IRQ0, {0x0ff8, 0}, },
+};
+
+static void enable_dev(struct device *dev)
+{
+	pnp_enable_devices(dev, &pnp_ops,
+		ARRAY_SIZE(pnp_dev_info), pnp_dev_info);
+}
+
+struct chip_operations superio_ite_it8772f_ops = {
+	CHIP_NAME("ITE IT8772F Super I/O")
+	.enable_dev = enable_dev,
+};

39764b7b5f5f997928ffca01fd89469d95016f4e
Ron Minnich <rminnich@gmail.com>
2012-04-05 23:51:18 -0700

    Add multicore support to coreboot
    This set of changes supports the ability for multicore support in coreboot.
    To give people a chance to examine the structure, and to make bisecting
    for bugs easier, we are implementing the patch in 3 stages. This first stage
    introduces the basic mechanism but does not make any visible change in
    behavior. The APs, instead of being immediately spun down, are asked to
    run one debug print function and then spun down.
    
    The means by which APs are tasked was implemented in the NIX operating
    system in 2011, see: http://code.google.com/p/nix-os/
    The APs come alive and spin on a memory location, contained
    in a per-AP structure. Tasking is accomplished
    by setting parameters into an argument array and then writing a function
    pointer into the memory location. The AP indicates completion by writing
    zero to the memory location. The BSP can wait for the AP to finish
    (synchronous) or going off to do other work (asynchronous).
    
    This way of tasking APs is incredibly cheap and fast: in the minimal
    case, one memory write suffices to launch an AP into doing work.
    
    This code has been tested on a sandybridge system (chromebook)
    and on another 4-core sandybridge system.
    
    Change-Id: I19f8587562fd499e98457aa9f11b52400c105697
    Signed-off-by: Ron Minnich <rminnich@gmail.com>

--- src/include/cpu/cpu.h
+++ src/include/cpu/cpu.h
@@ -5,9 +5,15 @@ struct device;
 struct bus;
 #include <arch/cpu.h>
 
-void cpu_initialize(void);
+void cpu_initialize(struct cpu_info *info);
 void initialize_cpus(struct bus *cpu_bus);
-void secondary_cpu_init(void);
+void secondary_cpu_init(u32 unused);
+int start_work(unsigned int core, workfunc f, u32 a, u32 b, u32 c);
+int wait_work(unsigned int core, u32 *retval, unsigned int maxwait);
+int run_work(unsigned int core, workfunc f, u32 a, u32 b, u32 c, u32 *retval,
+	unsigned int timeout);
+void cpu_work(struct cpu_info *info);
+
 
 #if !CONFIG_WAIT_BEFORE_CPUS_INIT
 	#define cpus_ready_for_init() do {} while(0)

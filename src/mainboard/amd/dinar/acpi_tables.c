a9a03db0d3018a9f5b79fb942dccd29438eaf7f4
zbao <fishbaozi@gmail.com>
2012-04-11 15:41:17 +0800

    Fix messy code in ALIB
    The address return from PICK_ALIB is not protected by Coreboot. We
    need to use the "current", which is the correct way to allocate the
    ACPI table.
    
    Change-Id: I250066eb5f755275f75c37789ce8760de35b046b
    Signed-off-by: Zheng Bao <zheng.bao@amd.com>
    Signed-off-by: zbao <fishbaozi@gmail.com>

--- src/mainboard/amd/dinar/acpi_tables.c
+++ src/mainboard/amd/dinar/acpi_tables.c
@@ -264,9 +264,8 @@ unsigned long write_acpi_tables(unsigned long start)
 	alib = (acpi_header_t *)agesawrapper_getlateinitptr (PICK_ALIB);
 	if (alib != NULL) {
 		memcpy((void *)current, alib, alib->length);
-		ssdt = (acpi_header_t *) current;
+		acpi_add_table(rsdp, (void *)current);
 		current += alib->length;
-		acpi_add_table(rsdp,alib);
 	} else {
 		printk(BIOS_DEBUG, "	AGESA ALIB SSDT table NULL. Skipping.\n");
 	}

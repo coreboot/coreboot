38096d75f68d9d8e8d59b5247bc7a877be2c3534
Stefan Reinauer <stefan.reinauer@coreboot.org>
2012-04-27 21:34:16 +0200

    Rework ACPI CST table generation
    ... in order to unify the Sandybridge and Lenovo implementations
    currently used in the tree.
    
    - use acpi_addr_t in acpigen_write_register()
    - use acpi_cstate_t for cstate tables (and fix up
      the x60 and t60)
    - drop cst_entry from acpigen.h
    
    Change-Id: Icb87418d44d355f607c4a67300107b40f40b3b3f
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

--- src/arch/x86/include/arch/acpigen.h
+++ src/arch/x86/include/arch/acpigen.h
@@ -23,17 +23,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
-
-struct cst_entry {
-	int type;
-	int width;
-	int offset;
-	int addrsize;
-	u64 address;
-	int ctype;
-	int latency;
-	int power;
-};
+#include <arch/acpi.h>
 
 void acpigen_patch_len(int len);
 void acpigen_set_current(char *curr);
@@ -56,16 +46,16 @@ int acpigen_write_PSS_package(u32 coreFreq, u32 power, u32 transLat, u32 busmLat
 			u32 control, u32 status);
 typedef enum { SW_ALL=0xfc, SW_ANY=0xfd, HW_ALL=0xfe } PSD_coord;
 int acpigen_write_PSD_package(u32 domain, u32 numprocs, PSD_coord coordtype);
-int acpigen_write_CST_package(struct cst_entry *entry, int nentries);
+int acpigen_write_CST_package(acpi_cstate_t *entry, int nentries);
 int acpigen_write_processor(u8 cpuindex, u32 pblock_addr, u8 pblock_len);
 int acpigen_write_mem32fixed(int readwrite, u32 base, u32 size);
 int acpigen_write_io16(u16 min, u16 max, u8 align, u8 len, u8 decode16);
-int acpigen_write_register(int type, int width, int offset, int addrsize, u64 address);
+int acpigen_write_register(acpi_addr_t *addr);
 int acpigen_write_resourcetemplate_header(void);
 int acpigen_write_resourcetemplate_footer(int len);
 int acpigen_write_mainboard_resource_template(void);
 int acpigen_write_mainboard_resources(const char *scope, const char *name);
 
-int get_cst_entries(struct cst_entry **);
+int get_cst_entries(acpi_cstate_t **);
 
 #endif

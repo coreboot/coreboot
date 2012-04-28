f7738ffdbf221cb4ae80ce3172e8dfa3a54e043d
Martin Roth <martin@se-eng.com>
2012-04-28 10:25:20 -0600

    Add simple PMIO & PMIO2 read/write routines to CIMX wrapper
    These are the PMIO & PMIO2 read & write routines from
    src/southbridge/amd/sb800/sb800.c & sb800.h for use in the cimx
    tree.  Currently most platforms using CIMX are calling WritePMIO()
    directly from the src/vendorcode/amd/cimx/sbX00 directories
    instead of using a wrapper function.
    These functions only do byte reads & writes.
    
    Change-Id: I881a6e2d4ddbba3dbdf4dd33e06313fe88b3682a
    Signed-off-by: Martin L Roth <martin@se-eng.com>

---
+++ src/southbridge/amd/cimx/cimx_util.h
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2010 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef CIMX_UTIL_H
+#define CIMX_UTIL_H
+
+#include <stdint.h>
+
+/* Power management index/data registers */
+#define PM_INDEX	0xcd6
+#define PM_DATA		0xcd7
+#define PM2_INDEX	0xcd0
+#define PM2_DATA	0xcd1
+
+void pm_iowrite(u8 reg, u8 value);
+u8 pm_ioread(u8 reg);
+void pm2_iowrite(u8 reg, u8 value);
+u8 pm2_ioread(u8 reg);
+
+#endif /* CIMX_UTIL_H */

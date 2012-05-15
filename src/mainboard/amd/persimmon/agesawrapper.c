a5a543259f455c6d99cf1a8e69cd12cb4bafee20
Marc Jones <marc.jones@se-eng.com>
2012-05-15 16:08:24 -0600

    Fix Persimmon build without S3.
    In the heap function, only check for S3 check when it is built in
    with CONFIG_HAVE_ACPI_RESUME.
    
    Change-Id: I439275a4e1b7b446b499bcf90c925785a14b980d
    Signed-off-by: Marc Jones <marc.jones@se-eng.com>

--- src/mainboard/amd/persimmon/agesawrapper.c
+++ src/mainboard/amd/persimmon/agesawrapper.c
@@ -253,10 +253,12 @@ UINT32 GetHeapBase(
 {
 	UINT32 heap;
 
+#if CONFIG_HAVE_APCI_RESUME
 	/* Both romstage and ramstage has this S3 detect. */
 	if (acpi_get_sleep_type() == 3)
 		heap = (UINT32)cbmem_find(CBMEM_ID_RESUME_SCRATCH) + (CONFIG_HIGH_SCRATCH_MEMORY_SIZE - BIOS_HEAP_SIZE); /* himem_heap_base + high_stack_size */
 	else
+#endif
 		heap = BIOS_HEAP_START_ADDRESS; /* low mem */
 
 	return heap;

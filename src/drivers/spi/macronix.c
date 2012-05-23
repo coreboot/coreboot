b3d2676231863076a3a3965142aeda130ea3f423
Stefan Reinauer <reinauer@chromium.org>
2012-05-23 11:03:29 -0700

    Reduce default verbosity of SPI flash drivers
    Only print PP: lines if CONFIG_DEBUG_SPI_FLASH is enabled.
    
    Change-Id: If25e916ecb585f37c90d42980e933a6cd1a3d956
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

--- src/drivers/spi/macronix.c
+++ src/drivers/spi/macronix.c
@@ -141,9 +141,11 @@ static int macronix_write(struct spi_flash *flash,
 		cmd[2] = (offset >> 8) & 0xff;
 		cmd[3] = offset & 0xff;
 
+#if CONFIG_DEBUG_SPI_FLASH
 		printk(BIOS_SPEW, "PP: 0x%p => cmd = { 0x%02x 0x%02x%02x%02x }"
 		     " chunk_len = %zu\n",
 		     buf + actual, cmd[0], cmd[1], cmd[2], cmd[3], chunk_len);
+#endif
 
 		ret = spi_flash_cmd(flash->spi, CMD_MX25XX_WREN, NULL, 0);
 		if (ret < 0) {

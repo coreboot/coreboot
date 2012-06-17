042c1461fb777e583e5de48edf9326e47ee5595f
Sven Schnelle <svens@stackframe.org>
2012-06-17 10:32:55 +0200

    Use broadcast SIPI to startup siblings
    The current code for initializing AP cpus has several shortcomings:
    
    - it assumes APIC IDs are sequential
    - it uses only the BSP for determining the AP count, which is bad if
      there's more than one physical CPU, and CPUs are of different type
    
    Note that the new code call cpu->ops->init() in parallel, and therefore
    some CPU code needs to be changed to address that. One example are old
    Intel HT enabled CPUs which can't do microcode update in parallel.
    
    Change-Id: Ic48a1ebab6a7c52aa76765f497268af09fa38c25
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    Reviewed-on: http://review.coreboot.org/1139
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/include/cpu/cpu.h
+++ src/include/cpu/cpu.h
@@ -4,10 +4,12 @@
 #include <arch/cpu.h>
 
 #if !defined(__ROMCC__)
-void cpu_initialize(void);
+void cpu_initialize(struct bus *cpu_bus, int index);
 struct bus;
 void initialize_cpus(struct bus *cpu_bus);
-void secondary_cpu_init(void);
+void secondary_cpu_init(int index);
+
+extern unsigned int cpucount;
 
 #if !CONFIG_WAIT_BEFORE_CPUS_INIT
 	#define cpus_ready_for_init() do {} while(0)

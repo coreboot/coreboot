1bd276a9a866c54c1d9ec4dbb00d14be2ef01a49
Vadim Bendebury <vbendeb@chromium.org>
2012-06-19 04:48:28 +0000

    Add standard header to prevent multiple inclusion
    This include file needs to be prevented from being included multiple
    times.
    
    Change-Id: I42e0cbe38d332b919f22e331eaf7a0251929e1dc
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- src/arch/x86/include/arch/cbfs.h
+++ src/arch/x86/include/arch/cbfs.h
@@ -1,3 +1,25 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 The ChromiumOS Authors.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef __INCLUDE_ARCH_CBFS__
+#define __INCLUDE_ARCH_CBFS__
+
 static void *walkcbfs(char *target)
 {
 	void *entry;
@@ -23,4 +45,4 @@ static inline void call(unsigned long addr, unsigned long bist)
 {
 	asm volatile ("jmp *%0\n\t" : : "r" (addr), "a" (bist));
 }
-
+#endif

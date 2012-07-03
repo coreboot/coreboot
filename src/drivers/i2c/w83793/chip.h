ca68297cda83030f417175ef3f256babe9ccf398
Sven Schnelle <svens@stackframe.org>
2012-07-03 21:33:47 +0200

    Add Nuvoton W83793 hardware monitor driver
    Change-Id: I3ecb5c8666eea247bf4c31aaf9426bd9ef66bf68
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    Reviewed-on: http://review.coreboot.org/1166
    Tested-by: build bot (Jenkins)

---
+++ src/drivers/i2c/w83793/chip.h
@@ -0,0 +1,21 @@
+extern struct chip_operations drivers_i2c_w83793_ops;
+
+struct drivers_i2c_w83793_config {
+	u8 mfc;
+	u8 fanin;
+	u8 peci_agent_conf;
+	u8 tcase0;
+	u8 tcase1;
+	u8 tcase2;
+	u8 tcase3;
+	u8 tr_enable;
+	u8 critical_temperature;
+
+	u8 td1_fan_select;
+	u8 td2_fan_select;
+	u8 td3_fan_select;
+	u8 td4_fan_select;
+
+	u8 tr1_fan_select;
+	u8 tr2_fan_select;
+};

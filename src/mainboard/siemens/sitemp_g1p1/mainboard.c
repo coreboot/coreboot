27584986fb9c8be478d635fb0c456c293ceac62b
Kyösti Mälkki <kyosti.malkki@gmail.com>
2012-07-10 14:26:43 +0300

    AMD and GFXUMA: drop redundant use of lb_add_memory_range()
    Use of uma_resource() in AMD northbridge code created a memory
    resource marked as reserved. Such resources are removed
    from system memory in write_coreboot_table().
    
    Change-Id: Ib5e49e851d6622d8ece9d6d612e245b3962b9167
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/mainboard/siemens/sitemp_g1p1/mainboard.c
+++ src/mainboard/siemens/sitemp_g1p1/mainboard.c
@@ -873,14 +873,6 @@ int add_mainboard_resources(struct lb_memory *mem)
 		printk(BIOS_INFO, "mmconf: base=%0llx size=%0llx\n", res->base, res->size);
 		lb_add_memory_range(mem, LB_MEM_RESERVED, res->base, res->size);
 	}
-	/* UMA is removed from system memory in the northbridge code, but
-	 * in some circumstances we want the memory mentioned as reserved.
- 	 */
-#if CONFIG_GFXUMA
-	printk(BIOS_INFO, "uma_memory_base=0x%llx, uma_memory_size=0x%llx \n",
-	uma_memory_base, uma_memory_size);
-	lb_add_memory_range(mem, LB_MEM_RESERVED, uma_memory_base, uma_memory_size);
-#endif
 	return 0;
 }
 

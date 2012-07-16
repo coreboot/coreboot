fa4e269220a4c7d1ec5b03da23420bb7fc744646
Duncan Laurie <dlaurie@chromium.org>
2012-07-16 12:19:00 -0700

    CPU: Add option to set TCC activation offset
    The default TCC activation offset is 0, which means TCC
    activation starts at Tj_max.  For devices with limited
    cooling ability it may be desired to lower TCC activation.
    
    This adds an option that can be declared in the devicetree
    to set the TCC activation to a non-zero value.
    
    Enable tcc_offset=15 in devicetree.cb and build/boot
    the BIOS and check that the value is set in the MSR:
    
    > and $(shr $(rdmsr 0 0x1a2) 24) 0xf
    0xf
    
    Change-Id: I88f6857b40fd354f70fa9d5d9c1d8ceaea6dfcd1
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>

--- src/cpu/intel/model_206ax/chip.h
+++ src/cpu/intel/model_206ax/chip.h
@@ -34,4 +34,6 @@ struct cpu_intel_model_206ax_config {
 	int c1_acpower;		/* ACPI C1 on AC Power */
 	int c2_acpower;		/* ACPI C2 on AC Power */
 	int c3_acpower;		/* ACPI C3 on AC Power */
+
+	int tcc_offset;		/* TCC Activation Offset */
 };

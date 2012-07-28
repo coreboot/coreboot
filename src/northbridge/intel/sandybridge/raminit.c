29a889d3d4ae4b5e2f1b839e889411f3e5a1cb36
Sven Schnelle <svens@stackframe.org>
2012-07-28 09:28:56 +0200

    sandybridge: reinitialize usbdebug after MRC
    MRC messes with USB devices, so we have to reinitialize
    USB debug after MRC has finished.
    
    Change-Id: I45c0a687cebd69d0a31235bb870f8c455f42d4f2
    Signed-off-by: Sven Schnelle <svens@stackframe.org>

--- src/northbridge/intel/sandybridge/raminit.c
+++ src/northbridge/intel/sandybridge/raminit.c
@@ -265,6 +265,11 @@ void sdram_initialize(struct pei_data *pei_data)
 		die("UEFI PEI System Agent not found.\n");
 	}
 
+#if CONFIG_USBDEBUG
+	/* mrc.bin reconfigures USB, so reinit it to have debug */
+	early_usbdebug_init();
+#endif
+
 	/* For reference print the System Agent version
 	 * after executing the UEFI PEI stage.
 	 */

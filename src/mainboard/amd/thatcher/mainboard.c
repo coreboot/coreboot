df0989c565bb8824ede572965b3bcdcd84d9d367
zbao <fishbaozi@gmail.com>
2012-07-31 20:02:41 +0800

    AMD Thatcher Board based on trinity
    Thatcher features: Family 15 trinity FP2. Hudson.
    close to Parmer.
    This board and parmer both need to revert the change
    http://review.coreboot.org/#/c/1359/,
    otherwise the mainboard_enable can not be called.
    
    Change-Id: I54e1cfca845fbcea1d3aad5eff08d760d0d215c9
    Signed-off-by: Zheng Bao <zheng.bao@amd.com>
    Signed-off-by: zbao <fishbaozi@gmail.com>

---
+++ src/mainboard/amd/thatcher/mainboard.c
@@ -0,0 +1,76 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include <console/console.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <arch/io.h>
+#include <boot/tables.h>
+#include <cpu/x86/msr.h>
+#include <cpu/amd/mtrr.h>
+#include <device/pci_def.h>
+#include <arch/acpi.h>
+#include "BiosCallOuts.h"
+#include <cpu/amd/agesa/s3_resume.h>
+#include "agesawrapper.h"
+
+/*************************************************
+ * enable the dedicated function in thatcher board.
+ *************************************************/
+static void thatcher_enable(device_t dev)
+{
+	msr_t msr;
+	printk(BIOS_INFO, "Mainboard " CONFIG_MAINBOARD_PART_NUMBER " Enable.\n");
+	/*
+	 * The mainboard is the first place that we get control in ramstage. Check
+	 * for S3 resume and call the approriate AGESA/CIMx resume functions.
+	 */
+	msr = rdmsr(0xC0011020);
+	msr.lo &= ~(1 << 28);
+	wrmsr(0xC0011020, msr);
+
+	msr = rdmsr(0xC0011022);
+	msr.lo &= ~(1 << 4);
+	msr.lo &= ~(1 << 13);
+	wrmsr(0xC0011022, msr);
+
+	msr = rdmsr(0xC0011023);
+	msr.lo &= ~(1 << 23);
+	wrmsr(0xC0011023, msr);
+
+#if CONFIG_HAVE_ACPI_RESUME == 1
+	acpi_slp_type = acpi_get_sleep_type();
+	if (acpi_slp_type == 3)
+		agesawrapper_fchs3earlyrestore();
+
+#endif
+	setup_uma_memory();
+}
+
+int add_mainboard_resources(struct lb_memory *mem)
+{
+	/* UMA is removed from system memory in the northbridge code, but
+	 * in some circumstances we want the memory mentioned as reserved.
+	 */
+	return 0;
+}
+struct chip_operations mainboard_ops = {
+	CHIP_NAME(CONFIG_MAINBOARD_VENDOR " " CONFIG_MAINBOARD_PART_NUMBER " Mainboard")
+	.enable_dev = thatcher_enable,
+};

760338b6a17e992c8cdf918c6a1dbdd322bee3f0
Stefan Reinauer <stefan.reinauer@coreboot.org>
2012-08-07 22:11:42 +0200

    Revert "Fix mainboard level enable_dev()"
    This reverts commit 15cf0adc3edaf184d98a3b3c228e0858ff7b24d3
    
    There is more stuff broken. Hence backing out the complete chip.h removal.

--- src/mainboard/emulation/qemu-x86/chip.h
+++ src/mainboard/emulation/qemu-x86/chip.h
@@ -1,3 +1,7 @@
+extern struct chip_operations mainboard_ops;
+
+struct mainboard_config {};
+
 struct mainboard_emulation_qemu_x86_config {};
 
 extern struct chip_operations mainboard_emulation_qemu_x86_ops;

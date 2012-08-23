afa829d18c8bcbd69e0c360bd1b076669297d403
Stefan Reinauer <reinauer@google.com>
2012-08-22 17:01:08 -0700

    x86 memcpy: Copy 4 bytes at once
    This is a slight improvement over the rep movsb loop
    
    Change-Id: Id71d9bfe5330b154a5c62fac85ce3955ae89b057
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

--- src/arch/x86/lib/memcpy.c
+++ src/arch/x86/lib/memcpy.c
@@ -5,11 +5,13 @@ void *memcpy(void *dest, const void *src, size_t n)
 	unsigned long d0, d1, d2;
 
 	asm volatile(
-		"rep movsb"
-		: "=S"(d0), "=D"(d1), "=c"(d2)
-		: "0"(src), "1"(dest), "2"(n)
+		"rep ; movsl\n\t"
+		"movl %4,%%ecx\n\t"
+		"rep ; movsb\n\t"
+		: "=&c" (d0), "=&D" (d1), "=&S" (d2)
+		: "0" (n >> 2), "g" (n & 3), "1" (dest), "2" (src)
 		: "memory"
-		);
+	);
 
 	return dest;
 }

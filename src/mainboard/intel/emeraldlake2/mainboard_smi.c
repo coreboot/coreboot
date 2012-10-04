89871848fdb567fdc3046494c2ce3dc5e7e3cb44
Duncan Laurie <dlaurie@chromium.org>
2012-10-03 19:01:57 -0700

    SMM: Avoid use of global variables in SMI handler
    Using global variables with the TSEG is a bad idea because
    they are not relocated properly right now.  Instead make
    the variables static and add accessor functions for the
    rest of SMM to use.
    
    At the same time drop the tcg/smi1 pointers as they are
    not setup or ever used.  (the debug output is added back
    in a subsequent commit)
    
    Change-Id: If0b2d47df4e482ead71bf713c1ef748da840073b
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>

--- src/mainboard/intel/emeraldlake2/mainboard_smi.c
+++ src/mainboard/intel/emeraldlake2/mainboard_smi.c
@@ -27,17 +27,12 @@
 #include <northbridge/intel/sandybridge/sandybridge.h>
 #include <cpu/intel/model_206ax/model_206ax.h>
 
-/* The southbridge SMI handler checks whether gnvs has a
- * valid pointer before calling the trap handler
- */
-extern global_nvs_t *gnvs;
-
 int mainboard_io_trap_handler(int smif)
 {
 	switch (smif) {
 	case 0x99:
 		printk(BIOS_DEBUG, "Sample\n");
-		gnvs->smif = 0;
+		smm_get_gnvs()->smif = 0;
 		break;
 	default:
 		return 0;

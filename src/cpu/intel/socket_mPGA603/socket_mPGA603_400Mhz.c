f3eb6a951e47d12511cb50fb33a69b6d63bc96b7
Kyösti Mälkki <kyosti.malkki@gmail.com>
2012-10-07 14:56:22 +0200

    Fix typo in mPGA603 socket
    Change-Id: I7a49d5fc13fb605a47c3c1662758ebd5935e7780
    Signed-off-by: Patrick Georgi <patrick@georgi-clan.de>

--- src/cpu/intel/socket_mPGA603/socket_mPGA603_400Mhz.c
+++ src/cpu/intel/socket_mPGA603/socket_mPGA603_400Mhz.c
@@ -1,6 +1,6 @@
 #include <device/device.h>
 
 
-struct chip_opertations cpu_intel_socket_mPGA603_ops = {
+struct chip_operations cpu_intel_socket_mPGA603_ops = {
 	CHIP_NAME("Socket mPGA603 400Mhz CPU")
 };

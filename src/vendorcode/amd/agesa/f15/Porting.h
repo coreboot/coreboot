8bd5bc79149bbe98d2bf8052b5f01b16ccae0e64
Siyuan Wang <wangsiyuanbuaa@gmail.com>
2012-10-19 21:02:39 +0800

    change conflicted typedef in src/vendorcode/amd/agesa/f15/Porting.h
    src/vendorcode/amd/agesa/f15/Porting.h has some conflicted typedef with
    src/include/cpu/amd/common/cbtypes.h. These conflicted defines can lead to errors.
    
    Change-Id: Idad0794018bf0bd0e4e52a5aa062a12766d56c8e
    Signed-off-by: Siyuan Wang <SiYuan.Wang@amd.com>
    Signed-off-by: Siyuan Wang <wangsiyuanbuaa@gmail.com>

--- src/vendorcode/amd/agesa/f15/Porting.h
+++ src/vendorcode/amd/agesa/f15/Porting.h
@@ -232,12 +232,12 @@
   typedef unsigned char  BOOLEAN;
   typedef   signed char  INT8;
   typedef   signed short INT16;
-  typedef   signed long  INT32;
-  typedef unsigned char  CHAR8;
+  typedef   signed int  INT32;
+  typedef   signed char  CHAR8;
   typedef unsigned char  UINT8;
   typedef unsigned short UINT16;
-  typedef unsigned long  UINT32;
-  typedef unsigned long  UINTN;
+  typedef unsigned int  UINT32;
+  typedef unsigned int  UINTN;
   typedef unsigned long  long UINT64;
   typedef void VOID;
   //typedef unsigned long  size_t;

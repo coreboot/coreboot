f6d3e8dd2874965f55bcbafdd5132c830fd62f30
David Hendricks <dhendrix@chromium.org>
2012-10-30 20:47:51 -0700

    WIP: initial Snow board (ARM/Exynos 5250) support
    This pulls in lots of sources from U-Boot for the Snow board, which is
    used in Google's Chromebook based on the Exynos 5250 SoC from Samsung.
    This patch will likely be broken down significantly, I'm just
    uploading it as a draft for convenience. It does not compile and is
    nowhere near complete.
    
    When compiling in the ChromeOS SDK chroot environment, use
    "CROSS_COMPILE=armv7a-cros-linux-gnueabi-". For now this patch also
    includes a config.snow file which sets the Kconfig options for Exynos
    (cp config.snow .config && make oldconfig && make).
    
    Overview of the code structure:
    - src/arch/armv7 - Common ARMv7 architecture files. This includes
      common headers and libraries.
    
    - src/cpu/samsung - Samsung SoC files, including Exynos5 (a Cortex-A15
      implementation). Since this is an SoC we'll forego the x86-style
      {north,south}bridge and cpu distinction. We may try to split some
      stuff out before the final version if prudent.
    
    - src/mainboard/google/snow - Mainboard directory (public name is
      "snow"). There are references to the Samsung SMDK5250 which is
      the development kit. Maybe we can add that as a separate board
      along with the Samsung Arndale (www.arndaleboard.org).
    
    TODO items:
    - The difference between romstage and ramstage is confusing (in
      context of this code). board_init_f() seems like it should be
      done in ramstage, since caches should be set up when start.S calls
      it.
    - Remove Linux-isms (superfluous headers).
    - Incorporate dianders' DRAM fix from u-boot
    - Make sure CBFS works...
    - Do something about device tree / ACPI
    - (Re)move config.snow
    - Remove unneeded S5P stuff
    
    Change-Id: Ic3ed72c88948af0ecb37f5ea9f2056451738146a
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

---
+++ src/arch/armv7/lib/eabi_compat.c
@@ -0,0 +1,29 @@
+/*
+ * Utility functions needed for (some) EABI conformant tool chains.
+ *
+ * (C) Copyright 2009 Wolfgang Denk <wd@denx.de>
+ *
+ * This program is Free Software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ */
+
+#include <common.h>
+
+int raise (int signum)
+{
+#if !defined(CONFIG_SPL_BUILD) || defined(CONFIG_SPL_LIBCOMMON_SUPPORT)
+	printf("raise: Signal # %d caught\n", signum);
+#endif
+	return 0;
+}
+
+/* Dummy function to avoid linker complaints */
+void __aeabi_unwind_cpp_pr0(void)
+{
+};
+
+void __aeabi_unwind_cpp_pr1(void)
+{
+};
---
+++ src/include/device/power/max77686.h
@@ -0,0 +1,134 @@
+/*
+ *  Copyright (C) 2012 Samsung Electronics
+ *  Alim Akhtar <alim.akhtar@samsung.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __MAX77686_H_
+#define __MAX77686_H_
+
+enum max77686_regnum {
+	PMIC_BUCK1 = 0,
+	PMIC_BUCK2,
+	PMIC_BUCK3,
+	PMIC_BUCK4,
+	PMIC_BUCK5,
+	PMIC_BUCK6,
+	PMIC_BUCK7,
+	PMIC_BUCK8,
+	PMIC_BUCK9,
+	PMIC_LDO1,
+	PMIC_LDO2,
+	PMIC_LDO3,
+	PMIC_LDO4,
+	PMIC_LDO5,
+	PMIC_LDO6,
+	PMIC_LDO7,
+	PMIC_LDO8,
+	PMIC_LDO9,
+	PMIC_LDO10,
+	PMIC_LDO11,
+	PMIC_LDO12,
+	PMIC_LDO13,
+	PMIC_LDO14,
+	PMIC_LDO15,
+	PMIC_LDO16,
+	PMIC_LDO17,
+	PMIC_LDO18,
+	PMIC_LDO19,
+	PMIC_LDO20,
+	PMIC_LDO21,
+	PMIC_LDO22,
+	PMIC_LDO23,
+	PMIC_LDO24,
+	PMIC_LDO25,
+	PMIC_LDO26,
+	PMIC_EN32KHZ_CP,
+};
+
+/**
+ * struct max77686_para - max77686 register parameters
+ * @param vol_addr	i2c address of the given buck/ldo register
+ * @param vol_bitpos	bit position to be set or clear within register
+ * @param vol_bitmask	bit mask value
+ * @param reg_enaddr	control register address, which enable the given
+ *			given buck/ldo.
+ * @param reg_enbitpos	bit position to be enabled
+ * @param reg_enbiton	value to be written to buck/ldo to make it ON
+ * @param reg_enbitoff	value to be written to buck/ldo to make it OFF
+ * @param vol_min	minimum voltage level supported by given buck/ldo
+ * @param vol_div	voltage division value of given buck/ldo
+ */
+struct max77686_para {
+	enum max77686_regnum regnum;
+	u8	vol_addr;
+	u8	vol_bitpos;
+	u8	vol_bitmask;
+	u8	reg_enaddr;
+	u8	reg_enbitpos;
+	u8	reg_enbitmask;
+	u8	reg_enbiton;
+	u8	reg_enbitoff;
+	u32	vol_min;
+	u32	vol_div;
+};
+
+/* I2C device address for pmic max77686 */
+#define MAX77686_I2C_ADDR (0x12 >> 1)
+
+enum {
+	REG_DISABLE = 0,
+	REG_ENABLE
+};
+
+enum {
+	MAX77686_MV = 0,	/* mili volt */
+	MAX77686_UV		/* micro volt */
+};
+
+/**
+ * This function enables the 32KHz coprocessor clock.
+ *
+ * Return 0 if ok, else -1
+ */
+int max77686_enable_32khz_cp(void);
+
+/**
+ * Set the required voltage level of pmic
+ *
+ * @param reg		register number of buck/ldo to be set
+ * @param volt		voltage level to be set
+ * @param enable	enable or disable bit
+ * @param volt_units	MAX77686_MV or MAX77686_UV, unit of the
+ *			voltage parameters
+ *
+ * @return		Return 0 if ok, else -1
+ */
+int max77686_volsetting(enum max77686_regnum reg, unsigned int volt,
+			int enable, int volt_units);
+
+/**
+ * Disable charging of the RTC backup battery
+ *
+ * @return		Return 0 if ok, else -1
+ */
+int max77686_disable_backup_batt(void);
+
+#endif /* __MAX77686_PMIC_H_ */

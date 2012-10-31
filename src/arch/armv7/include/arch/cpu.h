bcf55e951fbed7fa4067e44fbd338029dc84a82f
David Hendricks <dhendrix@chromium.org>
2012-10-30 20:47:51 -0700

    WIP: initial Snow board (ARM/Exynos 5250) support
    This pulls in lots of sources from U-Boot for the Snow board, which is
    used in Google's Chromebook based on the Exynos 5250 SoC from Samsung.
    This patch will likely be broken down significantly, I'm just
    uploading it as a draft for convenience.
    
    When compiling in the ChromeOS SDK chroot environment, use
    "CROSS_COMPILE=armv7a-cros-linux-gnueabi-". You may also use crossgcc
    with "-p armv7a-eabi".
    
    For now this patch also includes a config.snow file which sets the
    Kconfig options for Exynos5250 (cp config.snow .config &&
    make oldconfig && make).
    
    Overview of the code structure:
    - src/arch/armv7 - Common ARMv7 architecture files. This includes
      common headers and libraries.
    
    - src/cpu/samsung - Samsung SoC files, including Exynos5 (a Cortex-A15
      implementation). Since this is an SoC we'll forego the x86-style
      {north,south}bridge and cpu distinction. We may try to split some
      stuff out before the final version if prudent.
    
    - src/mainboard/google/snow - Mainboard directory (public name is
      "snow"). There are references to the Samsung SMDK5250 which is
      the development kit. Maybe we can add that as a separate board
      along with the Samsung Arndale (www.arndaleboard.org).
    
    TODO items:
    - The difference between romstage and ramstage is confusing (in
      context of this code). board_init_f() seems like it should be
      done in ramstage, since caches should be set up when start.S calls
      it.
    - Remove superfluous Linux code
    - Split out changes to console (e.g. src/console/Kconfig)
    - Incorporate dianders' DRAM fix from u-boot
    - Make sure CBFS works...
    - Do something about device tree / ACPI
    - (Re)move config.snow
    - Remove unneeded S5P stuff
    
    Get rid of linux include files. Merge asm/system.h into system.h,
    put in conditional __ASSEMBlY__ code.
    
    get rid of asm/. We're not linux and much of this junk is not needed.
    It builds without the asm/ directory.
    
    Change-Id: Ic3ed72c88948af0ecb37f5ea9f2056451738146a
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

--- src/arch/armv7/include/arch/cpu.h
+++ src/arch/armv7/include/arch/cpu.h
@@ -22,4 +22,25 @@
 
 #define asmlinkage
 
+#if !defined(__PRE_RAM__)
+#include <device/device.h>
+
+struct cpu_driver {
+	struct device_operations *ops;
+	struct cpu_device_id *id_table;
+};
+
+struct cpu_info {
+	device_t cpu;
+	unsigned long index;
+};
+
+struct cpuinfo_arm {
+        uint8_t    arm;            /* CPU family */
+        uint8_t    arm_vendor;     /* CPU vendor */
+        uint8_t    arm_model;
+};
+
 #endif
+
+#endif /* __ARCH_CPU_H__ */

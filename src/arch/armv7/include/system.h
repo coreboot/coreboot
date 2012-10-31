77cba62069cefa507ba4b6a4d4f85ec48d7d9b15
David Hendricks <dhendrix@chromium.org>
2012-10-30 20:47:51 -0700

    WIP: initial Snow board (ARM/Exynos 5250) support
    This pulls in lots of sources from U-Boot for the Snow board, which is
    used in Google's Chromebook based on the Exynos 5250 SoC from Samsung.
    This patch will likely be broken down significantly, I'm just
    uploading it as a draft for convenience.
    
    When compiling in the ChromeOS SDK chroot environment, use
    "CROSS_COMPILE=armv7a-cros-linux-gnueabi-". You may also use crossgcc
    with "-p armv7a-eabi".
    
    For now this patch also includes a config.snow file which sets the
    Kconfig options for Exynos5250 (cp config.snow .config &&
    make oldconfig && make).
    
    Overview of the code structure:
    - src/arch/armv7 - Common ARMv7 architecture files. This includes
      common headers and libraries.
    
    - src/cpu/samsung - Samsung SoC files, including Exynos5 (a Cortex-A15
      implementation). Since this is an SoC we'll forego the x86-style
      {north,south}bridge and cpu distinction. We may try to split some
      stuff out before the final version if prudent.
    
    - src/mainboard/google/snow - Mainboard directory (public name is
      "snow"). There are references to the Samsung SMDK5250 which is
      the development kit. Maybe we can add that as a separate board
      along with the Samsung Arndale (www.arndaleboard.org).
    
    TODO items:
    - The difference between romstage and ramstage is confusing (in
      context of this code). board_init_f() seems like it should be
      done in ramstage, since caches should be set up when start.S calls
      it.
    - Remove superfluous Linux code
    - Split out changes to console (e.g. src/console/Kconfig)
    - Incorporate dianders' DRAM fix from u-boot
    - Make sure CBFS works...
    - Do something about device tree / ACPI
    - (Re)move config.snow
    - Remove unneeded S5P stuff
    
    Get rid of linux include files. Merge asm/system.h into system.h,
    put in conditional __ASSEMBlY__ code.
    
    get rid of asm/. We're not linux and much of this junk is not needed.
    It builds without the asm/ directory.
    
    Change-Id: Ic3ed72c88948af0ecb37f5ea9f2056451738146a
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

---
+++ src/arch/armv7/include/system.h
@@ -0,0 +1,123 @@
+/* FIXME(dhendrix): This is split out from asm/system.h. */
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+#define CPU_ARCH_UNKNOWN	0
+#define CPU_ARCH_ARMv3		1
+#define CPU_ARCH_ARMv4		2
+#define CPU_ARCH_ARMv4T		3
+#define CPU_ARCH_ARMv5		4
+#define CPU_ARCH_ARMv5T		5
+#define CPU_ARCH_ARMv5TE	6
+#define CPU_ARCH_ARMv5TEJ	7
+#define CPU_ARCH_ARMv6		8
+#define CPU_ARCH_ARMv7		9
+
+/*
+ * CR1 bits (CP#15 CR1)
+ */
+#define CR_M	(1 << 0)	/* MMU enable				*/
+#define CR_A	(1 << 1)	/* Alignment abort enable		*/
+#define CR_C	(1 << 2)	/* Dcache enable			*/
+#define CR_W	(1 << 3)	/* Write buffer enable			*/
+#define CR_P	(1 << 4)	/* 32-bit exception handler		*/
+#define CR_D	(1 << 5)	/* 32-bit data address range		*/
+#define CR_L	(1 << 6)	/* Implementation defined		*/
+#define CR_B	(1 << 7)	/* Big endian				*/
+#define CR_S	(1 << 8)	/* System MMU protection		*/
+#define CR_R	(1 << 9)	/* ROM MMU protection			*/
+#define CR_F	(1 << 10)	/* Implementation defined		*/
+#define CR_Z	(1 << 11)	/* Implementation defined		*/
+#define CR_I	(1 << 12)	/* Icache enable			*/
+#define CR_V	(1 << 13)	/* Vectors relocated to 0xffff0000	*/
+#define CR_RR	(1 << 14)	/* Round Robin cache replacement	*/
+#define CR_L4	(1 << 15)	/* LDR pc can set T bit			*/
+#define CR_DT	(1 << 16)
+#define CR_IT	(1 << 18)
+#define CR_ST	(1 << 19)
+#define CR_FI	(1 << 21)	/* Fast interrupt (lower latency mode)	*/
+#define CR_U	(1 << 22)	/* Unaligned access operation		*/
+#define CR_XP	(1 << 23)	/* Extended page tables			*/
+#define CR_VE	(1 << 24)	/* Vectored interrupts			*/
+#define CR_EE	(1 << 25)	/* Exception (Big) Endian		*/
+#define CR_TRE	(1 << 28)	/* TEX remap enable			*/
+#define CR_AFE	(1 << 29)	/* Access flag enable			*/
+#define CR_TE	(1 << 30)	/* Thumb exception enable		*/
+
+/*
+ * This is used to ensure the compiler did actually allocate the register we
+ * asked it for some inline assembly sequences.  Apparently we can't trust
+ * the compiler from one version to another so a bit of paranoia won't hurt.
+ * This string is meant to be concatenated with the inline asm string and
+ * will cause compilation to stop on mismatch.
+ * (for details, see gcc PR 15089)
+ */
+#define __asmeq(x, y)  ".ifnc " x "," y " ; .err ; .endif\n\t"
+
+#define isb() __asm__ __volatile__ ("" : : : "memory")
+
+#define nop() __asm__ __volatile__("mov\tr0,r0\t@ nop\n\t");
+
+#define arch_align_stack(x) (x)
+
+#ifndef __ASSEMBLY__
+static inline unsigned int get_cr(void)
+{
+	unsigned int val;
+	asm("mrc p15, 0, %0, c1, c0, 0	@ get CR" : "=r" (val) : : "cc");
+	return val;
+}
+
+static inline void set_cr(unsigned int val)
+{
+	asm volatile("mcr p15, 0, %0, c1, c0, 0	@ set CR"
+	  : : "r" (val) : "cc");
+	isb();
+}
+
+/* options available for data cache on each page */
+enum dcache_option {
+	DCACHE_OFF,
+	DCACHE_WRITETHROUGH,
+	DCACHE_WRITEBACK,
+};
+
+/* Size of an MMU section */
+enum {
+	MMU_SECTION_SHIFT	= 20,
+	MMU_SECTION_SIZE	= 1 << MMU_SECTION_SHIFT,
+};
+
+/**
+ * Change the cache settings for a region.
+ *
+ * \param start		start address of memory region to change
+ * \param size		size of memory region to change
+ * \param option	dcache option to select
+ */
+void mmu_set_region_dcache(unsigned long start, int size,
+			enum dcache_option option);
+
+/**
+ * Register an update to the page tables, and flush the TLB
+ *
+ * \param start		start address of update in page table
+ * \param stop		stop address of update in page table
+ */
+void mmu_page_table_flush(unsigned long start, unsigned long stop);
+
+void dram_bank_mmu_setup(unsigned long start, unsigned long size);
+
+void arm_init_before_mmu(void);
+
+ /*
+  * FIXME: sdelay, sr32, and wait_on_value originally came from
+  * arch/arm/cpu/armv7/exynos5/setup.h in u-boot but do not seem
+  * specific to exynos5...
+  */
+void sdelay(unsigned long loops);
+void sr32(void *addr, u32 start_bit, u32 num_bits, u32 value);
+u32 wait_on_value(u32 read_bit_mask, u32 match_value, void *read_addr,
+		  u32 bound);
+#endif // __ASSEMBLY__
+#endif	/* SYSTEM_H_ */

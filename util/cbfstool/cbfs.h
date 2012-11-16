17b6fd778024d205a0d4a9323bccd24653ea3200
David Hendricks <dhendrix@chromium.org>
2012-11-16 14:48:22 -0800

    CBFS re-factoring for multiple architectures
    This is an initial re-factoring of CBFS code to enable multiple
    architectures.
    
    Change-Id: Ia05f5d1cc0c2851df7780a54796b414d72eb9304
    Signed-off-by: Stefan Reinauer <reinauer@google.com>
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- util/cbfstool/cbfs.h
+++ util/cbfstool/cbfs.h
@@ -21,6 +21,12 @@
 
 #include <stdint.h>
 
+#define CBFS_HEADER_MAGIC  0x4F524243
+#define CBFS_HEADPTR_ADDR_X86 0xFFFFFFFC
+#define VERSION1 0x31313131
+#define VERSION2 0x31313132
+#define VERSION  VERSION2
+
 struct cbfs_header {
 	uint32_t magic;
 	uint32_t version;
@@ -28,8 +34,15 @@ struct cbfs_header {
 	uint32_t bootblocksize;
 	uint32_t align;
 	uint32_t offset;
-	uint32_t pad[2];
+	uint32_t architecture;
+	uint32_t pad[1];
 } __attribute__ ((packed));
+
+// "Legacy" refers to cbfs headers before architecture is defined (i.e., X86).
+#define CBFS_ARCHITECTURE_LEGACY   0xFFFFFFFF
+#define CBFS_ARCHITECTURE_X86      0x10
+#define CBFS_ARCHITECTURE_ARMEL    0x20		/* ARM endian-little */
+#define CBFS_ARCHITECTURE_ARMEB    0x30		/* ARM endian-big */
 
 struct cbfs_file {
 	uint8_t magic[8];
@@ -94,7 +107,7 @@ struct cbfs_payload {
  */
 #define CBFS_COMPONENT_NULL 0xFFFFFFFF
 
-int cbfs_file_header(uint32_t physaddr);
+int cbfs_file_header(unsigned long physaddr);
 struct cbfs_file *cbfs_create_empty_file(uint32_t physaddr, uint32_t size);
 
 #endif

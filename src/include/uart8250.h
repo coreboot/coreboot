fcbbff1e28a4b978339bdf352e9d83364eb04f36
Stefan Reinauer <reinauer@chromium.org>
2012-12-11 12:49:32 -0800

    Fix UART8250 console prototypes
    and disable IO mapped UARTs on ARMV7 per default
    
    Change-Id: I712c4677cbc8519323970556718f9bb6327d83c8
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

--- src/include/uart8250.h
+++ src/include/uart8250.h
@@ -115,6 +115,7 @@
 #define UART_LCS	CONFIG_TTYS0_LCS
 
 #ifndef __ROMCC__
+#if CONFIG_CONSOLE_SERIAL8250
 unsigned char uart8250_rx_byte(unsigned base_port);
 int uart8250_can_rx_byte(unsigned base_port);
 void uart8250_tx_byte(unsigned base_port, unsigned char data);
@@ -125,6 +126,8 @@ void uart8250_tx_flush(unsigned base_port);
  */
 void uart8250_init(unsigned base_port, unsigned divisor);
 void uart_init(void);
+#endif
+#if CONFIG_CONSOLE_SERIAL8250MEM
 void uartmem_init(void);
 
 /* and the same for memory mapped uarts */
@@ -136,12 +139,12 @@ void uart8250_mem_init(unsigned base_port, unsigned divisor);
 u32 uart_mem_init(void);
 u32 uartmem_getbaseaddr(void);
 
-#if defined(__PRE_RAM__) && CONFIG_DRIVERS_OXFORD_OXPCIE && \
-	CONFIG_CONSOLE_SERIAL8250MEM
+#if defined(__PRE_RAM__) && CONFIG_DRIVERS_OXFORD_OXPCIE
 /* and special init for OXPCIe based cards */
 extern int oxford_oxpcie_present;
 
 void oxford_init(void);
+#endif
 #endif
 
 #endif /* __ROMCC__ */

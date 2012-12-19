fac2629e57267b8d6b9c193df3afc2e5304547cb
Aladyshev Konstantin <aladyshev@nicevt.ru>
2012-12-19 09:31:01 +0400

    AGESA: Use `Flag=AGESA_SUCCESS` instead of `TRUE` in DMI related functions
    Success return value in DMI functions GetDmiInfoMain(..) and GetType4Type7Info(...) of AGESA vendorcode is "Flag = TRUE".
    
    This results in a failure of init late function:
    
        "agesawrapper_amdinitlate failed: 1"
    
    It happens because TRUE = 1 = AGESA_UNSUPPORTED.
    
    Replacing TRUE with AGESA_SUCCESS (= 0) fixes this problem.
    
    Only family f15tn does not have such bug.
    
    This patch just replaces TRUE with AGESA_SUCCESS, but maybe all DMI functions should be copied from Trinity family?
    
    Tested on Supermicro H8QGI board with 4 AMD Opteron 6234 processors (f15).
    
    Change-Id: I51bf91333c088a825b92d4a44d1ebe4380c8026c
    Signed-off-by: Aladyshev Konstantin <aladyshev@nicevt.ru>
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- src/vendorcode/amd/agesa/f14/Proc/CPU/Feature/cpuDmi.c
+++ src/vendorcode/amd/agesa/f14/Proc/CPU/Feature/cpuDmi.c
@@ -220,7 +220,7 @@ GetDmiInfoMain (
   CPU_GET_MEM_INFO CpuGetMemInfo;
 
   MsrData = 0;
-  Flag = TRUE;
+  Flag = AGESA_SUCCESS;
   ProcData = NULL;
   MemInfo = NULL;
   DmiBufferPtr = *DmiTable;
@@ -429,7 +429,7 @@ GetType4Type7Info (
   PROC_FAMILY_TABLE   *ProcData;
   CPU_LOGICAL_ID LogicalID;
 
-  Flag = TRUE;
+  Flag = AGESA_SUCCESS;
   DmiBufferPtr = (DMI_INFO *) ApExeParams->RelatedDataBlock;
   GetLogicalIdOfCurrentCore (&LogicalID, &ApExeParams->StdHeader);
 

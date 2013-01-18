2bc0f387a8a3871351aa6e5974fff13ff26d9973
Gabe Black <gabeblack@chromium.com>
2013-01-17 22:26:36 -0800

    armv7: add a wrapper for romstage's main() for ARM ISA
    This adds a wrapper around main() in romstage which is compiled using
    -marm. This assumes that the bootblock branches to romstage in ARM
    mode.
    
    The long-term idea is to enforce ABI compatibility when handing off to
    the next stage by using shims which are which are compiled in a pre-
    determiend manner and leave the main portions of each stage up to
    whatever the compiler wants. So it will eventually look like this:
    1. bootblock_main (ARM/Thumb)
    2. bootblock_exit (ARM)
    3. romstage_entry (ARM)
    4. romstage_main (ARM/Thumb)
    
    (credit to Gabe Black for writing the patch, I'm just uploading it)
    
    Change-Id: I4fdb8d2c6c2c0a7178bcb9154c378ddce0567309
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    Signed-off-by: Gabe Black <gabeblack@chromium.org>

---
+++ src/arch/armv7/romstage_main.c
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 The ChromiumOS Authors.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+void main(void);
+
+/* romstage_main is simply a stub to invoke main(), to handle ARM mode switching
+ * (ARM/Thumb) properly. */
+
+void romstage_main(void) __attribute__((section(".text.entry.armv7")));
+void romstage_main(void)
+{
+	main();
+}

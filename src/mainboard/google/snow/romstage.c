00dcf5a703e522b3b69485d5cc4af481c3be5fb0
David Hendricks <dhendrix@chromium.org>
2013-01-17 20:52:21 -0800

    WIP: armv7/snow patch dump
    ** do not submit **
    
    This is just a patch dump so others can more easily get up to speed.
    Magic offsets still need to be fixed, but at least we're finding our
    ROM stage location and jumping to it. Unfortunately we never actually
    seem to make it into romstage...
    
    The user should see a bunch of prints to the UART, and PSHOLD will
    be held high so the machine should not turn itself off.
    
    Depends-On: 2173
    Change-Id: Iae4d2f9e7f429cb1df15d49daf9a08b88d75d79d
    Signed-off-by: David Hendricks <dhendrix@chromium.org>

--- src/mainboard/google/snow/romstage.c
+++ src/mainboard/google/snow/romstage.c
@@ -21,12 +21,86 @@
 #include <system.h>
 #include <cache.h>
 
+#include <arch/io.h>
+
+/* FIXME: make i2c.h use standard types */
+#define uchar unsigned char
+#define uint  unsigned int
+#include <device/i2c.h>
+
+#include <cpu/samsung/s5p-common/s3c24x0_i2c.h>
+#include "cpu/samsung/exynos5250/dmc.h"
+#include <cpu/samsung/exynos5250/power.h>
+#include <cpu/samsung/exynos5250/clock_init.h>
+#include <cpu/samsung/exynos5-common/uart.h>
+#include <console/console.h>
+
 static void mmu_setup(void)
 {
 	dram_bank_mmu_setup(CONFIG_SYS_SDRAM_BASE, CONFIG_DRAM_SIZE_MB * 1024);
 }
 
+#define RX_FIFO_COUNT_MASK	0xff
+#define RX_FIFO_FULL_MASK	(1 << 8)
+#define TX_FIFO_FULL_MASK	(1 << 24)
+static uint32_t uart3_base = CONFIG_CONSOLE_SERIAL_UART_ADDRESS;
+
+static int exynos5_uart_err_check(int op)
+{
+	//struct s5p_uart *const uart = s5p_get_base_uart(dev_index);
+	struct s5p_uart *uart = (struct s5p_uart *)uart3_base;
+	unsigned int mask;
+
+	/*
+	 * UERSTAT
+	 * Break Detect	[3]
+	 * Frame Err	[2] : receive operation
+	 * Parity Err	[1] : receive operation
+	 * Overrun Err	[0] : receive operation
+	 */
+	if (op)
+		mask = 0x8;
+	else
+		mask = 0xf;
+
+	return readl(&uart->uerstat) & mask;
+}
+
+/*
+ * Output a single byte to the serial port.
+ */
+static void exynos5_uart_tx_byte(unsigned char data)
+{
+//	struct s5p_uart *const uart = s5p_get_base_uart(dev_index);
+	struct s5p_uart *uart = (struct s5p_uart *)uart3_base;
+
+	if (data == '\n')
+		exynos5_uart_tx_byte('\r');
+
+	/* wait for room in the tx FIFO */
+	while ((readl(uart->ufstat) & TX_FIFO_FULL_MASK)) {
+		if (exynos5_uart_err_check(1))
+			return;
+	}
+
+	writeb(data, &uart->utxh);
+}
+
 void main(void)
 {
+	int i;
+	char hello[] = "hello from romstage";
+//	volatile unsigned long *pshold = (unsigned long *)0x1004330c;
+//	i2c_init(CONFIG_SYS_I2C_SPEED, CONFIG_SYS_I2C_SLAVE);
+//	power_init();
+//	clock_init();
+//	exynos_pinmux_config(PERIPH_ID_UART3, PINMUX_FLAG_NONE);
+//	console_init();
+	printk(BIOS_INFO, "hello from romstage\n");
+	for (i = 0; i < 19; i++)
+		exynos5_uart_tx_byte(hello[i]);
+	exynos5_uart_tx_byte('\r');
+
+//	*pshold &= ~0x100;	/* shut down */
 	mmu_setup();
 }

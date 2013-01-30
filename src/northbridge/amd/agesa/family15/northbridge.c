371631534a8e8100611962c69390657ced404f10
Mike Loptien <mike.loptien@se-eng.com>
2013-01-30 14:12:01 -0700

    Family 15: Update for string portability
    Update function messages to be more portable by using
    the __func__ compiler command instead of hard coded
    function names.
    
    Change-Id: Ie71fec39df5e7703d35d6505dc7d5b55179e2c7e
    Signed-off-by: Mike Loptien <mike.loptien@se-eng.com>

--- src/northbridge/amd/agesa/family15/northbridge.c
+++ src/northbridge/amd/agesa/family15/northbridge.c
@@ -570,12 +570,12 @@ static void domain_enable_resources(device_t dev)
 {
 	u32 val;
 	/* Must be called after PCI enumeration and resource allocation */
-	printk(BIOS_DEBUG, "\nFam15 - domain_enable_resources: AmdInitMid.\n");
+	printk(BIOS_DEBUG, "\nFam15 - %s: AmdInitMid.\n", __func__);
 	val = agesawrapper_amdinitmid();
 	if (val) {
 		printk(BIOS_DEBUG, "agesawrapper_amdinitmid failed: %x \n", val);
 	}
-	printk(BIOS_DEBUG, "  ader - leaving domain_enable_resources.\n");
+	printk(BIOS_DEBUG, "  ader - leaving %s.\n", __func__);
 }
 
 

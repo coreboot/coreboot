543a6824580331373a2d77694c23905dc7ca48a7
Stefan Reinauer <reinauer@chromium.org>
2013-02-04 15:39:13 -0800

    cbfstool: support parsing UEFI firmware volumes
    This removes the hack implemented in http://review.coreboot.org/#/c/2280
    (and should make using 64bit Tiano easier, but that's not yet supported)
    
    Change-Id: Ie30129c4102dfbd41584177f39057b31f5a937fd
    Signed-off-by: Stefan Reinauer <reinauer@google.com>
    Reviewed-on: http://review.coreboot.org/2281
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

---
+++ util/cbfstool/fv.h
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 Google, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#define FV_SIGNATURE 0x4856465f
+typedef struct {
+	uint8_t  padding[16];
+	uint8_t  guid[16];
+	uint64_t fv_length;
+	uint32_t signature;
+	uint32_t attributes;
+	uint16_t header_length;
+	uint16_t checksum;
+	uint16_t ext_header_offs;
+	uint8_t  reserved;
+	uint8_t  revision;
+	/* not used here: block map entries */
+} firmware_volume_header_t;
+
+#define FILETYPE_SEC 0x03
+typedef struct {
+	uint8_t  name[16];
+	uint16_t integrity;
+	uint8_t  file_type;
+	uint8_t  attributes;
+	uint8_t  size[3];
+	uint8_t  state;
+} ffs_file_header_t;
+
+#define SECTION_PE32 0x10
+typedef struct {
+	uint8_t size[3];
+	uint8_t section_type;
+} common_section_header_t;

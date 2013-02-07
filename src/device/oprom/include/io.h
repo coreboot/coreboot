632d6fe3fc8b71e00438bddf972ff2936447a03c
Patrick Georgi <patrick.georgi@secunet.com>
2013-02-07 15:53:54 +0100

    YABEL: use system {in,out}[bwl] on x86
    The prototypes that were recently defined only work for the
    internal implementations.
    
    Change-Id: Ib34bb75a0b882533da550b9cd17cd777c2463e02
    Signed-off-by: Patrick Georgi <patrick.georgi@secunet.com>
    Reviewed-on: http://review.coreboot.org/2318
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/device/oprom/include/io.h
+++ src/device/oprom/include/io.h
@@ -1,3 +1,6 @@
+#if CONFIG_ARCH_X86
+#include <arch/io.h>
+#else
 void outb(u8 val, u16 port);
 void outw(u16 val, u16 port);
 void outl(u32 val, u16 port);
@@ -5,5 +8,4 @@ void outl(u32 val, u16 port);
 u8 inb(u16 port);
 u16 inw(u16 port);
 u32 inl(u16 port);
-
-
+#endif

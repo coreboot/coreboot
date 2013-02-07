87d6550c1f2a902592be6604921a7f557c264168
Hung-Te Lin <hungte@chromium.org>
2013-02-07 21:38:41 +0800

    armv7/snow: Move clock initialization from bootblock to romstage.
    Exynos system clock can be initialized before RAM init, not necessary to be in
    the very beginning (boot block). This helps reducing bootblock dependency.
    
    Verified to boot on armv7/snow.
    
    Note: this patch was originally introduced in 2308, but there were
    some ordering issues so it was reverted.
    
    Change-Id: Ibc91c0e26ea8881751fc088754f5c6161d011b68
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>
    Signed-off-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-on: http://review.coreboot.org/2320
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/mainboard/google/snow/bootblock.c
+++ src/mainboard/google/snow/bootblock.c
@@ -19,28 +19,20 @@
 
 #include <types.h>
 #include <arch/io.h>
-#include <device/i2c.h>
-#include <cpu/samsung/exynos5250/clk.h>
-#include <cpu/samsung/exynos5250/dmc.h>
-#include <cpu/samsung/exynos5250/periph.h>
-#include <cpu/samsung/exynos5250/clock_init.h>
-#include <src/cpu/samsung/exynos5250/power.h>
-#include <drivers/maxim/max77686/max77686.h>
+#include <cbfs.h>
+#include <uart.h>
 #include <console/console.h>
+#include <cpu/samsung/exynos5250/periph.h>
+#include <cpu/samsung/exynos5250/pinmux.h>
 
 #define I2C0_BASE	0x12c60000
 
 void bootblock_mainboard_init(void);
 void bootblock_mainboard_init(void)
 {
-	struct mem_timings *mem;
-	struct arm_clk_ratios *arm_ratios;
-
-	mem = get_mem_timings();
-	arm_ratios = get_arm_clk_ratios();
-	system_clock_init(mem, arm_ratios);
+#if CONFIG_EARLY_CONSOLE
 	exynos_pinmux_config(PERIPH_ID_UART3, PINMUX_FLAG_NONE);
-
 	console_init();
 	printk(BIOS_INFO, "\n\n\n%s: UART initialized\n", __func__);
+#endif
 }

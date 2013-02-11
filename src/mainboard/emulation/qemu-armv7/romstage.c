90f4c5b663330a7e24b7f26a6e39e3b8a43bc521
Hung-Te Lin <hungte@chromium.org>
2013-02-12 00:07:38 +0800

    armv7: Add emulation/qemu-armv7 board.
    To simplify testing ARM implementation, we need a QEMU configuration for
    ARM. The qemu-armv7 provides serial output, CBFS simulation, and full
    boot path (bootblock, romstage, ramstage) to verify the boot loader
    functionality.
    
    To run with QEMU:
     export QEMU_AUDIO_DRV=none
     qemu-system-arm -M vexpress-a9 -m 1024M -nographic -kernel build/coreboot.rom
    
    Verified to boot until ramstage loaded successfully by QEMU v1.0.50.
    
    Change-Id: I1f23ffaf408199811a0756236821c7e0f2a85004
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

---
+++ src/mainboard/emulation/qemu-armv7/romstage.c
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <cbfs.h>
+#include <common.h>
+#include <console/console.h>
+#include <arch/stages.h>
+
+void main(void)
+{
+        void *entry;
+	console_init();
+	printk(BIOS_INFO, "hello from romstage\n");
+
+	entry = cbfs_load_stage(CBFS_DEFAULT_MEDIA, "fallback/coreboot_ram");
+	printk(BIOS_INFO, "entry is 0x%p, leaving romstage.\n", entry);
+
+	stage_exit(entry);
+}
---
+++ src/mainboard/emulation/qemu-armv7/timer.c
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+void udelay(unsigned int n);
+void udelay(unsigned int n) {
+	/* TODO provide delay here. */
+}
+
+int init_timer(void);
+int init_timer(void) {
+	return 0;
+}

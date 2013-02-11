2476d18a6e61287b7d3b3584b4815fba3cba98fa
Hung-Te Lin <hungte@chromium.org>
2013-02-12 00:07:38 +0800

    armv7: Add emulation/qemu-armv7 board.
    To simplify testing ARM implementation, we need a QEMU configuration for
    ARM. The qemu-armv7 provides serial output, CBFS simulation, and full
    boot path (bootblock, romstage, ramstage) to verify the boot loader
    functionality.
    
    To run with QEMU:
     export QEMU_AUDIO_DRV=none
     qemu-system-arm -M vexpress-a9 -m 1024M -nographic -kernel build/coreboot.rom
    
    Verified to boot until ramstage loaded successfully by QEMU v1.0.50.
    
    Change-Id: I1f23ffaf408199811a0756236821c7e0f2a85004
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

---
+++ src/mainboard/emulation/qemu-armv7/uart.c
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <console/console.h>
+#include <uart.h>
+
+#define VEXPRESS_UART0_IO_ADDRESS	(0x10009000)
+
+static void pl011_init_dev(void) {
+}
+
+static void pl011_uart_tx_byte(unsigned char data) {
+	static volatile unsigned int *uart0_address =
+			(unsigned int *)VEXPRESS_UART0_IO_ADDRESS;
+
+	*uart0_address = (unsigned int)data;
+}
+
+static void pl011_uart_tx_flush(void) {
+}
+
+#if !defined(__PRE_RAM__)
+
+static const struct console_driver pl011_uart_console __console = {
+	.init     = pl011_init_dev,
+	.tx_byte  = pl011_uart_tx_byte,
+	.tx_flush = pl011_uart_tx_flush,
+};
+
+#else
+void uart_init(void)
+{
+	pl011_init_dev();
+}
+
+void uart_tx_byte(unsigned char data)
+{
+	pl011_uart_tx_byte(data);
+}
+
+void uart_tx_flush(void) {
+	pl011_uart_tx_flush();
+}
+#endif

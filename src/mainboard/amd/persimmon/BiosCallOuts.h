fa8702cf2a30f32a2d9918548276e1b7b6ec0d2a
Jens Rottmann <JRottmann@LiPPERTembedded.de>
2013-02-18 19:40:33 +0100

    Persimmon: adapt PCIe reset code copied from Inagua to actually match Persimmon
    Comparing Persimmon and Inagua schematics and Coreboot code show the PCIe reset
    code has been blindly copied even though it doesn't suit the Persimmon at all.
    
    The Inagua can employ GPIOs 21, 25, 02 to manually reset devices on APU PCIe
    lanes 0/1, 2, 3 respectively. (Appearently the motivation for this is to revive
    buggy PCIe gen1 devices which got confused by PCIe gen2 signal training.)
    
    However the Persimmon not only doesn't support this, it even needs these 3 pins
    for the PCI interface! Instead it uses GPIO50 to reset devices on lanes 0-2 all
    at once. Lane 3 is unconnected anyway.
    
    This patch adapts the Persimmon mainboard code according to the DB-FT1 rev. D
    schematics.
    
    Change-Id: I05a657d9bf8cc59acc4f5174eb20375165c860c7
    Signed-off-by: Jens Rottmann <JRottmann@LiPPERTembedded.de>
    Reviewed-on: http://review.coreboot.org/2446
    Tested-by: build bot (Jenkins)
    Reviewed-by: Peter Stuge <peter@stuge.se>

--- src/mainboard/amd/persimmon/BiosCallOuts.h
+++ src/mainboard/amd/persimmon/BiosCallOuts.h
@@ -69,12 +69,7 @@ AGESA_STATUS BiosHookBeforeDramInitRecovery (UINT32 Func, UINT32 Data, VOID *Con
 AGESA_STATUS BiosHookBeforeExitSelfRefresh (UINT32 Func, UINT32 Data, VOID *ConfigPtr);
 /* PCIE slot reset control */
 AGESA_STATUS BiosGnbPcieSlotReset (UINT32 Func, UINT32 Data, VOID *ConfigPtr);
-#define SB_GPIO_REG02	2
-#define SB_GPIO_REG09	9
-#define SB_GPIO_REG10	10
-#define SB_GPIO_REG15	15
-#define SB_GPIO_REG17	17
-#define SB_GPIO_REG21	21
-#define SB_GPIO_REG25	25
-#define SB_GPIO_REG28	28
+
+#define SB_GPIO_REG50	50
+
 #endif //_BIOS_CALLOUT_H_

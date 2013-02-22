50f313c8b2cae372d3d3868940c445aeb221ec1e
Patrick Georgi <patrick@georgi-clan.de>
2013-02-22 20:19:20 +0100

    */acpi_tables.c: Use ALIGN macro
    At the request of Paul Menzel, I reran an
    old classic of a coccinelle script:
      @@
      expression E;
      @@
      -(E + 7) & -8
      +ALIGN(E, 8)
    
      @@
      expression E;
      @@
      -(E + 15) & -16
      +ALIGN(E, 16)
    
    Change-Id: I01da31b241585e361380f75aacf3deddb13d11c3
    Signed-off-by: Patrick Georgi <patrick@georgi-clan.de>
    Reviewed-on: http://review.coreboot.org/2487
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martin.roth@se-eng.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- src/mainboard/amd/thatcher/acpi_tables.c
+++ src/mainboard/amd/thatcher/acpi_tables.c
@@ -155,7 +155,7 @@ unsigned long write_acpi_tables(unsigned long start)
 	get_bus_conf(); /* it will get sblk, pci1234, hcdn, and sbdn */
 
 	/* Align ACPI tables to 16 bytes */
-	start = (start + 0x0f) & -0x10;
+	start = ALIGN(start, 16);
 	current = start;
 
 	printk(BIOS_INFO, "ACPI: Writing ACPI tables at %lx...\n", start);
@@ -173,7 +173,7 @@ unsigned long write_acpi_tables(unsigned long start)
 	acpi_write_rsdt(rsdt);
 
 	/* DSDT */
-	current = (current + 0x07) & -0x08;
+	current = ALIGN(current, 8);
 	printk(BIOS_DEBUG, "ACPI:    * DSDT at %lx\n", current);
 	dsdt = (acpi_header_t *)current; /* it will used by fadt */
 	memcpy(dsdt, &AmlCode, sizeof(acpi_header_t));
@@ -182,14 +182,14 @@ unsigned long write_acpi_tables(unsigned long start)
 	printk(BIOS_DEBUG, "ACPI:    * DSDT @ %p Length %x\n",dsdt,dsdt->length);
 
 	/* FACS */ /* it needs 64 bit alignment */
-	current = (current + 0x07) & -0x08;
+	current = ALIGN(current, 8);
 	printk(BIOS_DEBUG, "ACPI: * FACS at %lx\n", current);
 	facs = (acpi_facs_t *) current; /* it will be used by fadt */
 	current += sizeof(acpi_facs_t);
 	acpi_create_facs(facs);
 
 	/* FADT */
-	current = (current + 0x07) & -0x08;
+	current = ALIGN(current, 8);
 	printk(BIOS_DEBUG, "ACPI:  * FADT at %lx\n", current);
 	fadt = (acpi_fadt_t *) current;
 	current += sizeof(acpi_fadt_t);
@@ -200,7 +200,7 @@ unsigned long write_acpi_tables(unsigned long start)
 	/*
 	 * We explicitly add these tables later on:
 	 */
-	current = (current + 0x07) & -0x08;
+	current = ALIGN(current, 8);
 	printk(BIOS_DEBUG, "ACPI:  * HPET at %lx\n", current);
 	hpet = (acpi_hpet_t *) current;
 	current += sizeof(acpi_hpet_t);
@@ -208,7 +208,7 @@ unsigned long write_acpi_tables(unsigned long start)
 	acpi_add_table(rsdp, hpet);
 
 	/* If we want to use HPET Timers Linux wants an MADT */
-	current = (current + 0x07) & -0x08;
+	current = ALIGN(current, 8);
 	printk(BIOS_DEBUG, "ACPI:  * MADT at %lx\n",current);
 	madt = (acpi_madt_t *) current;
 	acpi_create_madt(madt);
@@ -216,13 +216,13 @@ unsigned long write_acpi_tables(unsigned long start)
 	acpi_add_table(rsdp, madt);
 
 	/* HEST */
-	current = (current + 0x07) & -0x08;
+	current = ALIGN(current, 8);
 	hest = (acpi_hest_t *)current;
 	acpi_write_hest((void *)current);
 	acpi_add_table(rsdp, (void *)current);
 	current += ((acpi_header_t *)current)->length;
 
-	current   = (current + 0x07) & -0x08;
+	current   = ALIGN(current, 8);
 	printk(BIOS_DEBUG, "ACPI:    * IVRS at %lx\n", current);
 	ivrs = agesawrapper_getlateinitptr(PICK_IVRS);
 	if (ivrs != NULL) {
@@ -235,7 +235,7 @@ unsigned long write_acpi_tables(unsigned long start)
 	}
 
 	/* SRAT */
-	current = (current + 0x07) & -0x08;
+	current = ALIGN(current, 8);
 	printk(BIOS_DEBUG, "ACPI:    * SRAT at %lx\n", current);
 	srat = (acpi_srat_t *) agesawrapper_getlateinitptr (PICK_SRAT);
 	if (srat != NULL) {
@@ -248,7 +248,7 @@ unsigned long write_acpi_tables(unsigned long start)
 	}
 
 	/* SLIT */
-	current = (current + 0x07) & -0x08;
+	current = ALIGN(current, 8);
 	printk(BIOS_DEBUG, "ACPI:   * SLIT at %lx\n", current);
 	slit = (acpi_slit_t *) agesawrapper_getlateinitptr (PICK_SLIT);
 	if (slit != NULL) {
@@ -261,7 +261,7 @@ unsigned long write_acpi_tables(unsigned long start)
 	}
 
 	/* ALIB */
-	current = (current + 0x0f) & -0x10;
+	current = ALIGN(current, 16);
 	printk(BIOS_DEBUG, "ACPI:  * AGESA ALIB SSDT at %lx\n", current);
 	alib = (acpi_header_t *)agesawrapper_getlateinitptr (PICK_ALIB);
 	if (alib != NULL) {
@@ -276,7 +276,7 @@ unsigned long write_acpi_tables(unsigned long start)
 
 	/* this pstate ssdt may cause Blue Screen: Fixed: Keep this comment for a while. */
 	/* SSDT */
-	current   = ( current + 0x0f) & -0x10;
+	current   = ALIGN(current, 16);
 	printk(BIOS_DEBUG, "ACPI:    * SSDT at %lx\n", current);
 	ssdt = (acpi_header_t *)agesawrapper_getlateinitptr (PICK_PSTATE);
 	if (ssdt != NULL) {

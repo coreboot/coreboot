c40a1a5ec1ad2925f38d7f1dcc037e33ae4cb1e3
Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>
2013-02-24 12:01:44 +0100

    Lenovo ThinkPad X60: Add Native VGA init.
    The code has been taken from the google link mainboard
      and modified to fit the ThinkPad X60.
    
    Change-Id: Ie16e45163acdc651ea46699ecc33055bfd34099c
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>

--- src/mainboard/lenovo/x60/mainboard.c
+++ src/mainboard/lenovo/x60/mainboard.c
@@ -36,6 +36,9 @@
 #include "dock.h"
 #include <arch/x86/include/arch/acpigen.h>
 
+int i915lightup(unsigned int physbase, unsigned int iobase, unsigned int mmio,
+        unsigned int gfx);
+
 static acpi_cstate_t cst_entries[] = {
 	{ 1,  1, 1000, { 0x7f, 1, 2, { 0 }, 1, 0 } },
 	{ 2,  1,  500, { 0x01, 8, 0, { 0 }, DEFAULT_PMBASE + LV2, 0 } },

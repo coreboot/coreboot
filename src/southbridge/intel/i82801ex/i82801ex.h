1cca340942957009ad74e24ef04bdd5eb44aabaf
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-02-26 19:21:39 +0200

    Use defines for some i82801ex/gx registers
    Change-Id: I0069ec26278b82d61ce5bcfb94d77647dfd3254b
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: http://review.coreboot.org/2530
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/southbridge/intel/i82801ex/i82801ex.h
+++ src/southbridge/intel/i82801ex/i82801ex.h
@@ -12,4 +12,11 @@ extern void i82801ex_enable(device_t dev);
 #define RTC_CONF        0xd8
 #define GEN_PMCON_3     0xa4
 
+#define PMBASE          0x40
+#define ACPI_CNTL       0x44
+#define   ACPI_EN	(1 << 4)
+#define GPIO_BASE       0x58
+#define GPIO_CNTL       0x5C
+#define   GPIO_EN	(1 << 4)
+
 #endif /* I82801EX_H */

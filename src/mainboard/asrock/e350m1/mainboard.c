dddb77790818a66bd761e56ec6329587cd1756c8
Jens Rottmann <JRottmann@LiPPERTembedded.de>
2013-02-26 15:56:11 +0100

    AMD Fam14 boards: reduce unnecessary differences, 2nd attempt
    This patch reduces unnecessary differences between AMD Inagua, Persimmon,
    Union Station, South Station and Asrock E350M1. It's only cosmetical, but
    makes them a little bit easier to compare.
    
    This is the remainder of the original http://review.coreboot.org/2464,
    parts of which somehow got lost in a flurry of refactoring and splitting
    patches.
    
    Change-Id: I034228be9edaaa4122506763d7bb4158f8e0ec53
    Signed-off-by: Jens Rottmann <JRottmann@LiPPERTembedded.de>

--- src/mainboard/asrock/e350m1/mainboard.c
+++ src/mainboard/asrock/e350m1/mainboard.c
@@ -45,9 +45,9 @@ void set_pcie_dereset(void)
 }
 
 
-/*************************************************
-* enable the dedicated function in e350m1 board.
-*************************************************/
+/**********************************************
+ * Enable the dedicated functions of the board.
+ **********************************************/
 static void mainboard_enable(device_t dev)
 {
 	printk(BIOS_INFO, "Mainboard " CONFIG_MAINBOARD_PART_NUMBER " Enable.\n");

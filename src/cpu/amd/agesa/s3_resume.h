coreboot/coreboot

commit 22ec9f9a72955d2364061db72520b85602077414

AMD S3: Introduce Kconfig variable 'S3_DATA_SIZE'
Currently the size of the volatile storage for S3 reserved in the
image is hardcoded to 32768 bytes. Make that configurable by
introducing the Kconfig 'S3_DATA_SIZE'.

As the storage space is needed for storing non-volatile, volatile and
MTRR data, add a check if the size is big enough.

Change-Id: I9152797cf0045c8da48109a9d760e417717686db
Signed-off-by: Zheng Bao <zheng.bao@amd.com>
Signed-off-by: Zheng Bao <fishbaozi@gmail.com>
Reviewed-on: http://review.coreboot.org/2383
Tested-by: build bot (Jenkins)
Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

src/cpu/amd/agesa/s3_resume.h
@@ -28,6 +28,10 @@
 #define S3_DATA_MTRR_POS	(CONFIG_S3_DATA_POS + S3_DATA_VOLATILE_SIZE)
 #define S3_DATA_NONVOLATILE_POS	(CONFIG_S3_DATA_POS + S3_DATA_VOLATILE_SIZE + S3_DATA_MTRR_SIZE)
 
+#if (S3_DATA_VOLATILE_SIZE + S3_DATA_MTRR_SIZE + S3_DATA_NONVOLATILE_SIZE) > CONFIG_S3_DATA_SIZE
+#error "Please increase the value of S3_DATA_SIZE"
+#endif
+
 typedef enum {
 	S3DataTypeNonVolatile=0,            ///< NonVolatile Data Type
 	S3DataTypeVolatile                  ///< Volatile Data Type

src/southbridge/amd/Makefile.inc
@@ -22,7 +22,7 @@ ifeq ($(CONFIG_CPU_AMD_AGESA), y)
 $(obj)/coreboot_s3nv.rom: $(obj)/config.h
 	echo "    S3 NVRAM   $(CONFIG_S3_DATA_POS) (S3 storage area)"
 	# force C locale, so cygwin awk doesn't try to interpret the 0xff below as UTF-8 (or worse)
-	LC_ALL=C awk 'BEGIN {for (i=0; i<32768; i++) {printf "%c", 255}}' > $@.tmp
+	printf %d $(CONFIG_S3_DATA_SIZE) | LC_ALL=C awk '{for (i=0; i<$$1; i++) {printf "%c", 255}}' > $@.tmp
 	mv $@.tmp $@
 
 cbfs-files-y += s3nv

src/southbridge/amd/agesa/hudson/Kconfig
@@ -209,6 +209,14 @@ config S3_DATA_POS
 	  For a system with S3 feature, the BIOS needs to save some data to
 	  non-volatile storage at cold boot stage.
 
+config S3_DATA_SIZE
+	int "S3 volatile storage size"
+	default 32768
+	depends on HAVE_ACPI_RESUME
+	help
+	  For a system with S3 feature, the BIOS needs to save some data to
+	  non-volatile storage at cold boot stage.
+
 config HUDSON_LEGACY_FREE
 	bool "System is legacy free"
 	help

src/southbridge/amd/cimx/sb700/Kconfig
@@ -68,5 +68,13 @@ config S3_DATA_POS
 	  For a system with S3 feature, the BIOS needs to save some data to
 	  non-volatile storage at cold boot stage.
 
+config S3_DATA_SIZE
+	int "S3 volatile storage size"
+	default 32768
+	depends on HAVE_ACPI_RESUME
+	help
+	  For a system with S3 feature, the BIOS needs to save some data to
+	  non-volatile storage at cold boot stage.
+
 endif #SOUTHBRIDGE_AMD_CIMX_SB700
 

src/southbridge/amd/cimx/sb800/Kconfig
@@ -130,6 +130,14 @@ config S3_DATA_POS
 	  For a system with S3 feature, the BIOS needs to save some data to
 	  non-volatile storage at cold boot stage.
 
+config S3_DATA_SIZE
+	int "S3 volatile storage size"
+	default 32768
+	depends on HAVE_ACPI_RESUME
+	help
+	  For a system with S3 feature, the BIOS needs to save some data to
+	  non-volatile storage at cold boot stage.
+
 config SB800_IMC_FWM
 	bool "Add IMC firmware"
 	default n

src/southbridge/amd/cimx/sb900/Kconfig
@@ -61,5 +61,13 @@ config S3_DATA_POS
 	  For a system with S3 feature, the BIOS needs to save some data to
 	  non-volatile storage at cold boot stage.
 
+config S3_DATA_SIZE
+	int "S3 volatile storage size"
+	default 32768
+	depends on HAVE_ACPI_RESUME
+	help
+	  For a system with S3 feature, the BIOS needs to save some data to
+	  non-volatile storage at cold boot stage.
+
 endif #SOUTHBRIDGE_AMD_CIMX_SB900
 

commit 12242d0cd697e6b9c04ca505fd5ed71f9ff03206

AGESA: skip s3_resume.h if CONFIG_HAVE_ACPI_RESUME is disabled
Commit 22ec9f9a introduced a check throwing an error if S3_DATA_SIZE isn't
big enough.  However without CONFIG_HAVE_ACPI_RESUME S3_DATA_SIZE isn't
defined at all and compilation will fail if s3_resume.h is included.

This patch makes it again possible turn off HAVE_ACPI_RESUME relatively
easily in Parmer/Thatcher/Persimmon's Kconfig if you don't care about S3
and don't want flash writes on every boot.

Change-Id: I999e4b7634bf172d8380fd14cba6f7f03468fee3
Signed-off-by: Jens Rottmann <JRottmann@LiPPERTembedded.de>

src/cpu/amd/agesa/s3_resume.h
@@ -20,6 +20,8 @@
 #ifndef S3_RESUME_H
 #define S3_RESUME_H
 
+#if CONFIG_HAVE_ACPI_RESUME
+
 /* The size needs to be 4k aligned, which is the sector size of most flashes. */
 #define S3_DATA_VOLATILE_SIZE	0x6000
 #define S3_DATA_MTRR_SIZE	0x1000
@@ -46,5 +48,7 @@ void move_stack_high_mem(void);
 u32 OemAgesaSaveS3Info (S3_DATA_TYPE S3DataType, u32 DataSize, void *Data);
 void OemAgesaGetS3Info (S3_DATA_TYPE S3DataType, u32 *DataSize, void **Data);
 void OemAgesaSaveMtrr (void);
+
+#endif
 
 #endif
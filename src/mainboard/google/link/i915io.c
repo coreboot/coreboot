665e3d23f0ca5bfac0d1ad9b7a8cde383ec56289
Ronald G. Minnich <rminnich@google.com>
2013-02-27 09:54:47 -0800

    link/graphics: add functions to support aux channel communications
    For full integration of FUI into coreboot, we need aux channel
    communcations.  The intel_dp.c is a file taken from Linux and is
    used for aux channel comms.  This file has been cut down to work
    with coreboot.  For now it is associated with the link mainboard
    until we get a better handle on how this all fits together.  This
    code is almost certainly usable on other platforms in the long term.
    But one step at a time.
    
    Change-Id: I7be4c56e0a7903f3901ac86e12b28f3bdc0f7947
    Signed-off-by: Ronald G. Minnich <rminnich@google.com>
    Reviewed-on: http://review.coreboot.org/2834
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/mainboard/google/link/i915io.c
+++ src/mainboard/google/link/i915io.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
+#include <stdint.h>
 #include "i915io.h"
 
 struct iodef iodefs[] = {

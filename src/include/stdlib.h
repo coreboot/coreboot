2d73802270fe32bd5c769c0d93401d67392dd4bf
Aaron Durbin <adurbin@chromium.org>
2013-03-28 16:17:11 -0500

    stdlib: add ALIGN_UP and ALIGN_DOWN macros
    There wasn't an equivalent to align down so add ALIGN_DOWN.
    For symmetry provide an ALIGN_UP macro as well.
    
    Change-Id: I7033109311eeb15c8c69c649878785378790feb9
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>

--- src/include/stdlib.h
+++ src/include/stdlib.h
@@ -7,6 +7,8 @@
 
 #define ALIGN(x,a)              __ALIGN_MASK(x,(typeof(x))(a)-1UL)
 #define __ALIGN_MASK(x,mask)    (((x)+(mask))&~(mask))
+#define ALIGN_UP(x,a)           ALIGN((x),(a))
+#define ALIGN_DOWN(x,a)         ((x) & ~((typeof(x))(a)-1UL))
 
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 #define MAX(a,b) ((a) > (b) ? (a) : (b))

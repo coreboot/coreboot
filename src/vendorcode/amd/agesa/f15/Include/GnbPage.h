07e0f1bf1a2781102e6f6a242a7a97944b0f3ba0
Paul Menzel <paulepanter@users.sourceforge.net>
2013-04-13 15:58:03 +0200

    AMD AGESA: Fix argument list for `PCIE_DDI_DATA_INITIALIZER` in comments
    When looking into possible reasons for a proposed revert [1], I noticed
    that the comments use four arguments for `PCIE_DDI_DATA_INITIALIZER`,
    but the actual definition only uses three.
    
        $ git grep -A1 PCIE_DDI_DATA_INITIALIZER # manually squeeze whitespace in output
        […]
        --
        src/vendorcode/amd/agesa/f10/AGESA.h:#define  PCIE_DDI_DATA_INITIALIZER(mConnectorType, mAuxIndex, mHpdIndex ) \
        src/vendorcode/amd/agesa/f10/AGESA.h-{mConnectorType, mAuxIndex, mHpdIndex}
        --
        src/vendorcode/amd/agesa/f10/AGESA.h:   *      PCIE_DDI_DATA_INITIALIZER (ConnectorType
        src/vendorcode/amd/agesa/f10/AGESA.h-   *    },
        --
        src/vendorcode/amd/agesa/f10/AGESA.h:   *      PCIE_DDI_DATA_INITIALIZER (ConnectorType
        src/vendorcode/amd/agesa/f10/AGESA.h-   *    }
        --
        […]
    
    So remove the fourth argument in the comments. Luckily the compiler,
    at least gcc, warns about a wrong number of arguments, and therefore
    no incorrect code resulted from the wrong documentation.
    
    [1] http://review.coreboot.org/#/c/3077/
    
    Change-Id: I3e5a02c66a23af1eb2d86be8dbc7aaa3e5cea05e
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-on: http://review.coreboot.org/3080
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/vendorcode/amd/agesa/f15/Include/GnbPage.h
+++ src/vendorcode/amd/agesa/f15/Include/GnbPage.h
@@ -139,7 +139,7 @@
  *    {
  *      DESCRIPTOR_TERMINATE_LIST,   //Descriptor flags
  *      PCIE_ENGINE_DATA_INITIALIZER (PcieDdiEngine, 24, 32),
- *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1, 0)
+ *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1)
  *    }
  *  }
  * // Dual Link DVI on dedicated display lanes. DP0_TXP/N[0]..DP0_TXP/N[3] - master, DP1_TXP/N[0]..DP1_TXP/N[3] - slave.
@@ -147,7 +147,7 @@
  *    {
  *      DESCRIPTOR_TERMINATE_LIST,   //Descriptor flags
  *      PCIE_ENGINE_DATA_INITIALIZER (PcieDdiEngine, 32, 24),
- *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1, 0)
+ *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1)
  *    }
  *  }
  * // Dual Link DVI on PCIe lanes. P_GFX_TXP/N[0]..P_GFX_TXP/N[3] - master, P_GFX_TXP/N[4]..P_GFX_TXP/N[7] - slave.
@@ -155,7 +155,7 @@
  *    {
  *      DESCRIPTOR_TERMINATE_LIST,   //Descriptor flags
  *      PCIE_ENGINE_DATA_INITIALIZER (PcieDdiEngine, 8, 15),
- *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1, 0)
+ *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1)
  *    }
  *  }
  * // Dual Link DVI on PCIe lanes. P_GFX_TXP/N[7]..P_GFX_TXP/N[4] - master, P_GFX_TXP/N[0]..P_GFX_TXP/N[3] - slave.
@@ -163,7 +163,7 @@
  *    {
  *      DESCRIPTOR_TERMINATE_LIST,   //Descriptor flags
  *      PCIE_ENGINE_DATA_INITIALIZER (PcieDdiEngine, 15, 8),
- *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1, 0)
+ *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1)
  *    }
  *  }
  * // Dual Link DVI on PCIe lanes. P_GFX_TXP/N[8]..P_GFX_TXP/N[11] - master, P_GFX_TXP/N[12]..P_GFX_TXP/N[15] - slave.
@@ -171,7 +171,7 @@
  *    {
  *      DESCRIPTOR_TERMINATE_LIST,   //Descriptor flags
  *      PCIE_ENGINE_DATA_INITIALIZER (PcieDdiEngine, 16, 23),
- *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1, 0)
+ *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1)
  *    }
  *  }
  * // Dual Link DVI on PCIe lanes. P_GFX_TXP/N[12]..P_GFX_TXP/N[15] - master, P_GFX_TXP/N[8]..P_GFX_TXP/N[11] - slave.
@@ -179,7 +179,7 @@
  *    {
  *      DESCRIPTOR_TERMINATE_LIST,   //Descriptor flags
  *      PCIE_ENGINE_DATA_INITIALIZER (PcieDdiEngine, 23, 16),
- *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1, 0)
+ *      PCIE_DDI_DATA_INITIALIZER (ConnectorTypeDualLinkDvi, Aux1, Hdp1)
  *    }
  *  }
  * @endcode

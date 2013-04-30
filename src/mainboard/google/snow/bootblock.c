032dd14514e72777475c3e39395486627846b7c9
Hung-Te Lin <hungte@chromium.org>
2013-04-30 15:31:48 +0800

    Google/Snow: Remove unnecessary serial console init code.
    The "console_init" does initialize UART driver (which will setup peripheral and
    pinmux) and print starting message. Duplicated initialization can be removed.
    
    Also, console_init (from console.c) is always linked to bootblock (and will do
    nothing if CONFIG_EARLY_CONSOLE is not defined) so it's safe to remove #ifdef.
    
    Verified by building and booting on Google/Snow, with and without
    CONFIG_EARLY_CONSOLE.
    
    Change-Id: I0c6b4d4eb1a4e81af0f65bcb032978dfb945c63d
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-on: http://review.coreboot.org/3150
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/mainboard/google/snow/bootblock.c
+++ src/mainboard/google/snow/bootblock.c
@@ -43,9 +43,5 @@ void bootblock_mainboard_init(void)
 			exynos_pinmux_config(PERIPH_ID_SPI1, PINMUX_FLAG_NONE);
 			break;
 	}
-#if CONFIG_EARLY_CONSOLE
-	exynos_pinmux_config(PERIPH_ID_UART3, PINMUX_FLAG_NONE);
 	console_init();
-	printk(BIOS_INFO, "\n\n\n%s: UART initialized\n", __func__);
-#endif
 }

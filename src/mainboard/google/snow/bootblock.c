e7d72473ba665d0639ad8964b10ea007af0fc79e
Hung-Te Lin <hungte@chromium.org>
2013-04-30 16:11:32 +0800

    Google/Snow: Remove duplicated SPI1 initialization in bootblock.
    The firmware media source (SPI1) is already initialized by Exynos iROM.
    There is no need to do it again.
    
    Verified by building and booting Google/Snow successfully.
    
    Change-Id: I89390506aa825397c0d7e52ad7503f1cb808f7db
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>

--- src/mainboard/google/snow/bootblock.c
+++ src/mainboard/google/snow/bootblock.c
@@ -40,9 +40,14 @@ void bootblock_mainboard_init(void)
 			 * We want to do this as early as we can.
 			 */
 			timer_start();
-			exynos_pinmux_config(PERIPH_ID_SPI1, PINMUX_FLAG_NONE);
 			break;
 	}
+
+	/* For most ARM systems, we have to initialize firmware media source
+	 * (ex, SPI, SD/MMC, or eMMC) now; but for Exynos platform, that is
+	 * already handled by iROM so there's no need to setup again.
+	 */
+
 #if CONFIG_EARLY_CONSOLE
 	exynos_pinmux_config(PERIPH_ID_UART3, PINMUX_FLAG_NONE);
 	console_init();

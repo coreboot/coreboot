f7bc16e31aaba6a187fa453a501968ec13526163
Stefan Reinauer <reinauer@chromium.org>
2013-05-06 16:49:56 -0700

    hardwaremain: drop boot_complete parameter
    it has been unused since 9 years or so, hence drop it.
    
    Change-Id: I0706feb7b3f2ada8ecb92176a94f6a8df53eaaa1
    Signed-off-by: Stefan Reinauer <reinauer@google.com>

--- src/mainboard/google/snow/ramstage.c
+++ src/mainboard/google/snow/ramstage.c
@@ -77,12 +77,10 @@ void fill_lb_framebuffer(struct lb_framebuffer *framebuffer)
 }
 
 
-void hardwaremain(int boot_complete);
+void hardwaremain(void);
 void main(void)
 {
-	console_init();
-	printk(BIOS_INFO,
-	       "hello from ramstage; now with deluxe exception handling.\n");
+	/* FIXME this should be moved elsewhere. We don't want ramstage.c */
 
 	/* set up coreboot tables */
 	high_tables_size = CONFIG_COREBOOT_TABLES_SIZE;
@@ -114,7 +112,7 @@ void main(void)
 
 	power_enable_xclkout();
 
-	hardwaremain(0);
+	hardwaremain();
 }
 
 /* TODO: transplanted DP stuff, clean up once we have something that works */

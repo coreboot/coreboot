33cde9a0ba62e8888e614a84f98860fe3a9ce82a
Martin Roth <martin.roth@se-eng.com>
2013-05-10 13:40:29 -0600

    Make early x86 POST codes written to IO port optional
    This continues the work done in patch 6b908d08ab
    http://review.coreboot.org/#/c/1685/
    and makes the early x86 post codes follow the same options.
    
    Change-Id: Idf0c17b27b3516e79a9a53048bc203245f7c18ff
    Signed-off-by: Martin Roth <martin.roth@se-eng.com>
    Reviewed-on: http://review.coreboot.org/3237
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/include/cpu/x86/post_code.h
+++ src/include/cpu/x86/post_code.h
@@ -1,7 +1,12 @@
 
 #include <console/post_codes.h>
 
+
+#if CONFIG_IO_POST
 #define post_code(value)        \
 	movb    $value, %al;    \
-	outb    %al, $0x80
+	outb    %al, $CONFIG_IO_POST_PORT
 
+#else
+#define post_code(value)
+#endif

907cbdb748f7ade1e6ed4947c6b2561624ca739c
Gabe Black <gabeblack@google.com>
2013-05-16 05:53:40 -0700

    pit: Add a "pit" mainboard which is mostly a copy of "snow".
    This change adds a pit mainboard which is mostly a copy of snow, except that
    mentions of the 5250 were replaced with the 5420, and mentions of snow were
    replaced with pit.
    
    Change-Id: I8eb0ce379eb2fa353bb88d5656a0c5e2290afbf0
    Signed-off-by: Gabe Black <gabeblack@chromium.org>

---
+++ src/mainboard/google/pit/wakeup.c
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2013 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <cpu/samsung/exynos5420/gpio.h>
+#include <cpu/samsung/exynos5420/wakeup.h>
+
+int wakeup_need_reset(void)
+{
+	/* The "wake up" event is not reliable (known as "bad wakeup") and needs
+	 * reset if GPIO value is high. */
+	return gpio_get_value(GPIO_Y10);
+}
+

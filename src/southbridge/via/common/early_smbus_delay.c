c6c5e9947b0d9dfd9f00986150d02628634b234e
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2013-05-21 12:35:08 -0500

    early_smbus: Add early SMBus implementation for VIA chipsets
    Add a common implementation of SMBus functionality for early chipsets. Note
    however, that existing via chipsets are not ported to this code. Porting
    will require hardware testing to make sure everything is fine.
    
    This code is used in the VIA VX900 branch.
    
    Change-Id: If5ad8cd0942ac02d358a0139967e7d85d395660f
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

---
+++ src/southbridge/via/common/early_smbus_delay.c
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011-2013  Alexandru Gagniuc <mr.nuke.me@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "via_early_smbus.h"
+
+/**
+ * \brief Brief delay for SMBus transactions
+ */
+void smbus_delay(void)
+{
+	inb(0x80);
+}

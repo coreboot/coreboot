3c4ed89da39b2d80d4a50a088915e6858eabccf0
Duncan Laurie <dlaurie@chromium.org>
2013-05-22 16:13:49 -0700

    slippy: Clean up for easier porting
    Minor tweaks to variable names in the slippy mainboard
    that make it easier to base a new board from without
    as much renaming.
    
    Also properly set up the thermal variables for the
    thermal zone that is defined in ACPI instead of using
    the generic setup from WTM2.
    
    Change-Id: I752c1a50bfdc06b6ddad95bd1331c6870b9f9df2
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/56328

--- src/mainboard/google/slippy/thermal.h
+++ src/mainboard/google/slippy/thermal.h
@@ -20,38 +20,24 @@
 #ifndef THERMAL_H
 #define THERMAL_H
 
-/* Fan is OFF */
-#define FAN4_THRESHOLD_OFF	0
-#define FAN4_THRESHOLD_ON	0
-#define FAN4_PWM		0x00
+/* Config TDP Sensor ID */
+#define CTDP_SENSOR_ID			1 /* PECI */
 
-/* Fan is at LOW speed */
-#define FAN3_THRESHOLD_OFF	48
-#define FAN3_THRESHOLD_ON	55
-#define FAN3_PWM		0x40
+/* Config TDP Nominal */
+#define CTDP_NOMINAL_THRESHOLD_OFF	0
+#define CTDP_NOMINAL_THRESHOLD_ON	0
 
-/* Fan is at MEDIUM speed */
-#define FAN2_THRESHOLD_OFF	52
-#define FAN2_THRESHOLD_ON	64
-#define FAN2_PWM		0x80
-
-/* Fan is at HIGH speed */
-#define FAN1_THRESHOLD_OFF	60
-#define FAN1_THRESHOLD_ON	68
-#define FAN1_PWM		0xb0
-
-/* Fan is at FULL speed */
-#define FAN0_THRESHOLD_OFF	66
-#define FAN0_THRESHOLD_ON	78
-#define FAN0_PWM		0xff
+/* Config TDP Down */
+#define CTDP_DOWN_THRESHOLD_OFF		80
+#define CTDP_DOWN_THRESHOLD_ON		90
 
 /* Temperature which OS will shutdown at */
-#define CRITICAL_TEMPERATURE	100
+#define CRITICAL_TEMPERATURE		104
 
 /* Temperature which OS will throttle CPU */
-#define PASSIVE_TEMPERATURE	90
+#define PASSIVE_TEMPERATURE		100
 
 /* Tj_max value for calculating PECI CPU temperature */
-#define MAX_TEMPERATURE		105
+#define MAX_TEMPERATURE			105
 
 #endif

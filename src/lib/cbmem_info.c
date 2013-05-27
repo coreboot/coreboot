940b86ecd3534db3b4a07e61a9bec5a526b5b5ae
Rudolf Marek <r.marek@assembler.cz>
2013-05-27 16:09:44 +0200

    AMD Fam15tn: Add support for AGESA runtime allocation in CBMEM
    The IOMMU AGESA needs a reserved scratch space and it wants
    to allocate the stuff for runtime. So provide a simple
    allocator for 4 KB CBMEM page.
    
    Change-Id: I53bdfcd2cd69f84fbfbc6edea53a051f516c05cc
    Signed-off-by: Rudolf Marek <r.marek@assembler.cz>

--- src/lib/cbmem_info.c
+++ src/lib/cbmem_info.c
@@ -47,6 +47,7 @@ static struct cbmem_id_to_name {
 	{ CBMEM_ID_ROOT,		"CBMEM ROOT " },
 	{ CBMEM_ID_VBOOT_HANDOFF,	"VBOOT      " },
 	{ CBMEM_ID_CAR_GLOBALS,		"CAR GLOBALS" },
+	{ CBMEM_ID_AGESA_RUNTIME,	"AGESA RSVD " },
 };
 
 void cbmem_print_entry(int n, u32 id, u64 base, u64 size)

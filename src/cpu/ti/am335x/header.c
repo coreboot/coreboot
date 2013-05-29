7d3faf97f80957a59cd6b2abb890fd8acea38c6a
Gabe Black <gabeblack@chromium.org>
2013-05-29 16:42:35 +0200

    am335x: Build an omap style header and an image with it at the front.
    Loading on an OMAP SOC requires that the first sector of the image have a
    configuration header, and, when not an execute in place image, an additional
    header which describes how big the image is and where it should be loaded.
    This change adds some infrastructure to statically build that header using C
    code, and to paste the header onto the front of coreboot.rom in a new top
    level target file called MLO.
    
    The configuration header we're using is as inert as possible, in line with
    what U-Boot is doing. I think it could be used to give additional
    configuration parameters to the built-in ROM on the SOC, but we don't need to
    do that, and there didn't seem to be any actual documentation how to do that.
    Because the header is built from C and is defined per CPU, it would be
    possible to include extra settings in other CPUs if desired.
    
    Adding a new top level build target is a bit disruptive, but should be
    contained to the am335x directory and not interfere with other mainboards.
    
    Change-Id: I06d346a4050c20963b3c7c6e8a152070bf2d145a
    Signed-off-by: Gabe Black <gabeblack@chromium.org>

---
+++ src/cpu/ti/am335x/header.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2013 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <config.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "header.h"
+
+struct config_headers {
+	// The table of contents.
+	struct configuration_header_toc_item toc_chsettings;
+	struct configuration_header_toc_item toc_end;
+
+	// An inert instance of chsettings.
+	struct configuration_header_settings chsettings;
+} __attribute__((packed));
+
+struct omap_image_headers {
+	union {
+		struct config_headers config_headers;
+		uint8_t bytes[512];
+	};
+	struct gp_device_header image_header;
+};
+
+struct omap_image_headers headers __attribute__((section(".header"))) = {
+	.config_headers = {
+		.toc_chsettings = {
+			.start = offsetof(struct omap_image_headers,
+					  config_headers.chsettings),
+			.size = sizeof(struct configuration_header_settings),
+			.reserved = { 0, 0, 0 },
+			.filename = "CHSETTINGS\0"
+		},
+		.toc_end = {
+			.start = 0xffffffff,
+			.size = 0xffffffff,
+			.reserved = { 0xffffffff, 0xffffffff, 0xffffffff },
+			.filename = { 0xff, 0xff, 0xff, 0xff,
+				      0xff, 0xff, 0xff, 0xff,
+				      0xff, 0xff, 0xff, 0xff }
+		},
+		.chsettings = {
+			.key = 0xc0c0c0c1,
+			.valid = 0,
+			.version = 1,
+			.reserved = 0,
+			.flags = 0
+		}
+	},
+	.image_header = {
+		.size = CONFIG_ROM_SIZE,
+		.destination = CONFIG_BOOTBLOCK_BASE
+	}
+};

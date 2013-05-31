bcef8afec0e531ef2144c54bc1be39985ac26dd3
Gerd Hoffmann <kraxel@redhat.com>
2013-05-31 09:23:26 +0200

    qemu: add x86 cpu
    This patch adds a qemu x86 cpu chip.  It has no initialization function
    as this isn't needed on virtual hardware.  A virtual machine can have
    pretty much any CPU: qemu emulates a wide range of x86 CPUs (try 'qemu
    -cpu ? for a list), also with 'qemu -cpu host' the guest will see a cpu
    which is (almost) identical to the one on the host machine.  So I've
    added X86_VENDOR_ANY as wildcard match for the cpu_table.
    
    Change-Id: Ib01210694b09702e41ed806f31d0033e840a863f
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

---
+++ src/cpu/qemu-x86/qemu.c
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <cpu/cpu.h>
+#include <device/device.h>
+
+static struct device_operations cpu_dev_ops = {
+};
+
+static struct cpu_device_id cpu_table[] = {
+	{ X86_VENDOR_ANY, 0 },
+	{ 0, 0 },
+};
+
+static const struct cpu_driver driver __cpu_driver = {
+	.ops      = &cpu_dev_ops,
+	.id_table = cpu_table,
+};
+
+struct chip_operations cpu_x86_qemu_ops = {
+	CHIP_NAME("QEMU x86 CPU")
+};

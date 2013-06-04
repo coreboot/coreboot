7cced0d20e44c1bbcd0b5d4aced53c682fcd23fc
Duncan Laurie <dlaurie@chromium.org>
2013-06-04 10:03:34 -0700

    ec: Add romstage function for checking and rebooting EC
    Now that we are executing VbInit() in coreboot we can end up
    in a situation where the recovery reason is consumed during
    VbInit (end of romstage) and then the EC is rebooted to RO
    during ramstage EC init, thereby losing the recovery reason.
    
    Two possiblities are to remove the EC check+reboot from ramstage
    and let it happen in depthcharge.  This however means that the
    system has to boot all the way into depthcharge and then reboot
    the EC and the system again.
    
    Instead if we do a check in romstage before VbInit() is called
    then we can reboot the EC into RO early and avoid booting all
    the way to depthcharge first.
    
    This change adds a ramstage version the EC init function and
    calls it from the shared romstage code immediately after the
    PCH decode windows are setup.
    
    Change-Id: I30d2a0c7131b8e4ec30c63eea36944ec111a8fba
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: http://review.coreboot.org/3744
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/ec/google/chromeec/ec.h
+++ src/ec/google/chromeec/ec.h
@@ -33,6 +33,7 @@ u16 google_chromeec_get_board_version(void);
 void google_chromeec_init(void);
 #endif
 
+void google_chromeec_early_init(void);
 uint8_t google_chromeec_calc_checksum(const uint8_t *data, int size);
 u32 google_chromeec_get_events_b(void);
 int google_chromeec_kbbacklight(int percent);

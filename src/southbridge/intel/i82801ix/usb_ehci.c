c0cd550cdf70deea9bcdf28e04cfaee3dfd3e758
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-06-07 22:16:52 +0300

    usbdebug: Drop duplicates of EHCI BAR relocation code
    When USBDEBUG is not selected, resource allocator assigns EHCI
    BAR with normal pci_dev_set_resources() call.
    
    With USBDEBUG selected, all the additional work that needs to be
    done is independent of the hardware platform and was actually
    functionally identical in all the copies removed.
    
    Change-Id: I8e136a5da4efedf60b6dd7068c0488153efaaf8e
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/southbridge/intel/i82801ix/usb_ehci.c
+++ src/southbridge/intel/i82801ix/usb_ehci.c
@@ -58,29 +58,6 @@ static void usb_ehci_set_subsystem(device_t dev, unsigned vendor, unsigned devic
 	pci_write_config8(dev, 0x80, access_cntl);
 }
 
-static void usb_ehci_set_resources(struct device *dev)
-{
-#if CONFIG_USBDEBUG
-	struct resource *res;
-	u32 base;
-	u32 usb_debug;
-
-	usb_debug = get_ehci_debug();
-	set_ehci_debug(0);
-#endif
-	pci_dev_set_resources(dev);
-
-#if CONFIG_USBDEBUG
-	res = find_resource(dev, 0x10);
-	set_ehci_debug(usb_debug);
-	if (!res) return;
-	base = res->base;
-	set_ehci_base(base);
-	report_resource_stored(dev, res, "");
-#endif
-}
-
-
 static const unsigned short pci_device_ids[] = {
 	0x293a,
 	0x293c,
@@ -92,8 +69,8 @@ static struct pci_operations lops_pci = {
 };
 
 static struct device_operations usb_ehci_ops = {
-	.read_resources		= pci_dev_read_resources,
-	.set_resources		= usb_ehci_set_resources,
+	.read_resources		= pci_ehci_read_resources,
+	.set_resources		= pci_dev_set_resources,
 	.enable_resources	= pci_dev_enable_resources,
 	.init			= usb_ehci_init,
 	.scan_bus		= 0,

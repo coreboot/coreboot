f6f5cb07704c924a00eb31f332f05ee27a952e46
Vladimir Serbinenko <phcoder@gmail.com>
2013-06-07 02:11:41 +0200

    Make version look like something thinkpad_acpi would accept
    thinkpad_acpi checks that BIOS version matches some pattern.
    Report version in this form.
    
    Not cleaned up as the idea of this patch seems to be met with resistance.
    Can make it Thinkpad-specific if the idea is accepted.
    
    Change-Id: I15e33e87e7a7f42d6a06f12fb39b5172153af8a1
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

--- src/mainboard/lenovo/x230/mainboard.c
+++ src/mainboard/lenovo/x230/mainboard.c
@@ -38,6 +38,7 @@
 #include <device/pci.h>
 #include <cbfs.h>
 #include <pc80/keyboard.h>
+#include <build.h>
 
 void mainboard_suspend_resume(void)
 {
@@ -129,6 +130,15 @@ static int int15_handler(void)
 	return res;
 }
 #endif
+
+const char *smbios_mainboard_bios_version(void)
+{
+	/* Satisfy thinkpad_acpi.  */
+	if (strlen(CONFIG_LOCALVERSION))
+		return "CBET4000 " CONFIG_LOCALVERSION;
+	else
+		return "CBET4000 " COREBOOT_VERSION;
+}
 
 const char *smbios_mainboard_version(void)
 {

b2f574e4dc0e538346127e6e93641f9c796b11e2
Peter Stuge <peter@stuge.se>
2013-06-08 01:25:43 +0200

    northbridge: i945: Native VGA init: use UMA address
    The patch was made by Peter Stuge, I just splitted it
      and added a commit message.
    
    Change-Id: I4e88c26b70ea8cb249d7613c749b3edc5e3b5e7f
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>

--- src/northbridge/intel/i945/gma.c
+++ src/northbridge/intel/i945/gma.c
@@ -52,14 +52,13 @@ static void gma_func0_init(struct device *dev)
 #if CONFIG_MAINBOARD_DO_NATIVE_VGA_INIT
 	/* This should probably run before post VBIOS init. */
 	printk(BIOS_SPEW, "Initializing VGA without OPROM.\n");
-	u32 iobase, mmiobase, physbase, graphics_base;
+	u32 iobase, mmiobase, graphics_base;
 	iobase = dev->resource_list[1].base;
 	mmiobase = dev->resource_list[0].base;
-	physbase = pci_read_config32(dev, 0x5c) & ~0xf;
 	graphics_base = dev->resource_list[2].base + 0x20000;
 
 	int i915lightup(u32 physbase, u32 iobase, u32 mmiobase, u32 gfx);
-	i915lightup(physbase, iobase, mmiobase, graphics_base);
+	i915lightup(uma_memory_base, iobase, mmiobase, graphics_base);
 #endif
 
 }

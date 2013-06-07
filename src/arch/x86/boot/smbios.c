6e032fc83e1b4379ce930d87440f1715f8476212
Vladimir Serbinenko <phcoder@gmail.com>
2013-06-07 02:11:41 +0200

    Make version look like something thinkpad_acpi would accept
    thinkpad_acpi checks that BIOS version matches some pattern.
    Report version in this form.
    
    Not cleaned up as the idea of this patch seems to be met with resistance.
    Can make it Thinkpad-specific if the idea is accepted.
    
    Change-Id: I15e33e87e7a7f42d6a06f12fb39b5172153af8a1
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

--- src/arch/x86/boot/smbios.c
+++ src/arch/x86/boot/smbios.c
@@ -130,12 +130,15 @@ static int smbios_write_type0(unsigned long *current, int handle)
 
 	t->vendor = smbios_add_string(t->eos, "coreboot");
 #if !CONFIG_CHROMEOS
+#ifndef CONFIG_VENDOR_VERSION
+#define CONFIG_VENDOR_VERSION ""
+#endif
 	t->bios_release_date = smbios_add_string(t->eos, COREBOOT_DMI_DATE);
 
 	if (strlen(CONFIG_LOCALVERSION))
-		t->bios_version = smbios_add_string(t->eos, CONFIG_LOCALVERSION);
+		t->bios_version = smbios_add_string(t->eos, CONFIG_VENDOR_VERSION CONFIG_LOCALVERSION);
 	else
-		t->bios_version = smbios_add_string(t->eos, COREBOOT_VERSION);
+		t->bios_version = smbios_add_string(t->eos, CONFIG_VENDOR_VERSION COREBOOT_VERSION);
 #else
 #define SPACES \
 	"                                                                  "

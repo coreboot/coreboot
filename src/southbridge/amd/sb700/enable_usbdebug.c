737bbec6e706c8ff59a91420e77e09f4cc2ced00
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-06-11 16:32:01 +0300

    usbdebug: Fix uses without EARLY_CONSOLE
    With introduction of EARLY_CONSOLE option, enable_usbdebug()
    may be used in either romstage or ramstage. The choice of
    using simple device model was introduced in intel/i82801gx.
    
    Change-Id: I2de8d9c77bb458ba797c3aac9e2cd0d653e06684
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/southbridge/amd/sb700/enable_usbdebug.c
+++ src/southbridge/amd/sb700/enable_usbdebug.c
@@ -18,6 +18,10 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#ifndef __PRE_RAM__
+#define __PRE_RAM__
+#endif
+
 #include <stdint.h>
 #include <arch/io.h>
 #include <usbdebug.h>

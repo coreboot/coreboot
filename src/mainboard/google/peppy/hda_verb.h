b1211359fa3f308b9e67ec629054e9318e7ddc17
Martin Roth <martin.roth@se-eng.com>
2013-06-13 14:24:35 +0800

    Peppy: Update Realtek ALC283 verb table
    Update peppy's verb tables for the Realtek ALC283 Audio Codec.
    
    ALC283 Configuration:
    Digital Mic - NID 12h: Disabled
    Speakers    - NID 14h: Enabled
    Mono out    - NID 17h: Disabled
    Mic 1       - NID 18h: Disabled
    Mic 2       - NID 19h: Headphone Jack
    Line1       - NID 1Ah: Internal Mic
    Line2       - NID 1Bh: Disabled
    PCBEEP      - NID 1Dh: Enabled
    SPDIF       - NID 1Eh: Disabled
    HP-OUT      - NID 21h: Headphone Jack
    
    Mic 1 doesn't seem to really be available, but the documentation
    refers to NID 18h as MIC1, so it's being disabled as it's not
    being used.  The onboard microphone has been moved to line 1.
    
    I had my peppy modified to attach the mic to line1 and mic1 now
    works with this patch.  Mic2 looks harder to rework, so I think
    that will have to wait for the DVT boards.
    
    Change-Id: I7d6ce6b428806b6aed1d36e7e25302fa5ae14b21
    Signed-off-by: Martin Roth <martin.roth@se-eng.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/58880
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- src/mainboard/google/peppy/hda_verb.h
+++ src/mainboard/google/peppy/hda_verb.h
@@ -23,6 +23,11 @@ static const u32 mainboard_cim_verb_data[] = {
 	0x10ec0283,	// Subsystem ID
 	0x0000000b,	// Number of jacks (NID entries)
 
+	/* Bits 31:28 - Codec Address */
+	/* Bits 27:20 - NID */
+	/* Bits 19:8 - Verb ID */
+	/* Bits 7:0  - Payload */
+
 	/* NID 0x01, HDA Codec Subsystem ID Verb Table: 0x10ec0283 */
 	0x00172083,
 	0x00172102,
@@ -31,64 +36,64 @@ static const u32 mainboard_cim_verb_data[] = {
 
 	/* Pin Widget Verb Table */
 
-	/* Pin Complex (NID 0x12) DMIC - interior mobile lid */
-	0x00571c10,
-	0x00571d10,
-	0x00571ea6,
-	0x00571fb7,
+	/* Pin Complex (NID 0x12) DMIC - Disabled */
+	0x01271cf0, //
+	0x01271d11, //
+	0x01271e11, //
+	0x01271f41, //
 
-	/* Pin Complex (NID 0x14) SPKR-OUT PORTD */
-	0x01471c10, // group 1, front left/right
+	/* Pin Complex (NID 0x14) SPKR-OUT - Internal Speakers */
+	0x01471c10, // group 1, cap 0
 	0x01471d01, // no connector, no jack detect
 	0x01471e17, // speaker out, analog
-	0x01471f90, // fixed function, internal
+	0x01471f90, // fixed function, internal, Location N/A
 
-	/* Pin Complex (NID 0x17)  */
-	0x01771cf0,
-	0x01771d11,
-	0x01771e11,
-	0x01771f41,
+	/* Pin Complex (NID 0x17) MONO Out - Disabled */
+	0x01771cf0, //
+	0x01771d11, //
+	0x01771e11, //
+	0x01771f41, //
 
-	/* Pin Complex (NID 0x18)  MIC1 PORTB */
-	0x01971c11, // group 1, cap 1
+	/* Pin Complex (NID 0x18) Disabled */
+	0x01871cf0, //
+	0x01871d11, //
+	0x01871e11, //
+	0x01871f41, //
+
+	/* Pin Complex (NID 0x19)  MIC2 - 3.5mm Jack */
+	0x01971c20, // group2, cap 0
 	0x01971d10, // black, jack detect
-	0x01971ea7, // mic in, analog
-	0x01971f03, // connector, left panel
+	0x01971ea1, // Mic in, 3.5mm Jack
+	0x01971f03, // connector, External left panel
 
-	/* Pin Complex (NID 0x19)  MIC2 PORTF */
-	0x01871cf0,
-	0x01871d11,
-	0x01871e11,
-	0x01871f41,
+	/* Pin Complex (NID 0x1A)  LINE1 - Internal Mic */
+	0x01a71c11, // group 1, cap 1
+	0x01a71d01, // no connector, no jack detect
+	0x01a71ea7, // mic in, analog connection
+	0x01a71f90, // Fixed function, internal, Location N/A
 
-	/* Pin Complex (NID 0x1A)  LINE1 PORTC */
-	0x01a71cf0,
-	0x01a71d11,
-	0x01a71e11,
-	0x01a71f41,
+	/* Pin Complex (NID 0x1B)  LINE2 - Disabled */
+	0x01b71cf0, //
+	0x01b71d11, //
+	0x01b71e11, //
+	0x01b71f41, //
 
-	/* Pin Complex (NID 0x1B)  LINE2 PORTE */
-	0x01a71cf0,
-	0x01a71d11,
-	0x01a71e11,
-	0x01a71f41,
-
-	/* Pin Complex (NID 0x1d)  PCBeep */
+	/* Pin Complex (NID 0x1D)  PCBeep */
 	0x01d71c2d, // eapd low on ex-amp, laptop, custom enable
 	0x01d71d81, // mute spkr on hpout
 	0x01d71e15, // pcbeep en able, checksum
-	0x01d71f40, // no physical, internal
+	0x01d71f40, // no physical, Internal, Location N/A
 
-	/* Pin Complex (NID 0x1E)  SPDIF-OUT */
-	0x01e71cf0,
-	0x01e71d11,
-	0x01e71e11,
-	0x01e71f41,
+	/* Pin Complex (NID 0x1E)  SPDIF-OUT - Disabled*/
+	0x01e71cf0, //
+	0x01e71d11, //
+	0x01e71e11, //
+	0x01e71f41, //
 
-	/* Pin Complex (NID 0x21) HPOUT PORT-I */
-	0x02171c1f, // group1,
+	/* Pin Complex (NID 0x21) HP-OUT - 3.5mm Jack*/
+	0x02171c21, // group2, cap 1
 	0x02171d10, // black, jack detect
-	0x02171e21, // HPOut, 1/8 stereo
+	0x02171e21, // HPOut, 3.5mm Jack
 	0x02171f03, // connector, left panel
 
 };

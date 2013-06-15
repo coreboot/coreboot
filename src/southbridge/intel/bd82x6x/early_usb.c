8eec290f7beb6b0150855e8316ab5cd856ba2d4b
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-06-15 12:29:23 +0300

    bd82x6x: Fix early EHCI BAR programming
    Change EHCI #2 to different BAR from EHCI #1.
    
    Even if the ECHI controllers are not to be addressed, it is bad idea
    to set two different devices to claim the same PCI memory cycles.
    
    Change-Id: Ib6f7cfac5acf3f8170508547d1584af90273e8c1
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/southbridge/intel/bd82x6x/early_usb.c
+++ src/southbridge/intel/bd82x6x/early_usb.c
@@ -49,9 +49,9 @@ void enable_usb_bar(void)
 	cmd |= PCI_COMMAND_MASTER | PCI_COMMAND_MEMORY;
 	pci_write_config32(usb0, PCI_COMMAND, cmd);
 
-	/* USB Controller 1 */
+	/* USB Controller 2 */
 	pci_write_config32(usb1, PCI_BASE_ADDRESS_0,
-			   PCH_EHCI1_TEMP_BAR0);
+			   PCH_EHCI2_TEMP_BAR0);
 	cmd = pci_read_config32(usb1, PCI_COMMAND);
 	cmd |= PCI_COMMAND_MASTER | PCI_COMMAND_MEMORY;
 	pci_write_config32(usb1, PCI_COMMAND, cmd);

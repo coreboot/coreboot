7325290197c8a484b28ec832675c92a924719bf3
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-06-15 12:30:13 +0300

    lynxpoint: Fix early EHCI BAR programming
    LynxPoint LP has only EHCI controller #1.
    Change EHCI #2 to different BAR from EHCI #1.
    
    Change-Id: I95c59fb9d5f09afd152872e9bc0418dc67e4aeb2
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/southbridge/intel/lynxpoint/early_usb.c
+++ src/southbridge/intel/lynxpoint/early_usb.c
@@ -37,7 +37,9 @@
 void enable_usb_bar(void)
 {
 	device_t usb0 = PCH_EHCI1_DEV;
+#if !CONFIG_INTEL_LYNXPOINT_LP
 	device_t usb1 = PCH_EHCI2_DEV;
+#endif
 	u32 cmd;
 
 	/* USB Controller 1 */
@@ -47,10 +49,12 @@ void enable_usb_bar(void)
 	cmd |= PCI_COMMAND_MASTER | PCI_COMMAND_MEMORY;
 	pci_write_config32(usb0, PCI_COMMAND, cmd);
 
-	/* USB Controller 1 */
+	/* USB Controller 2 */
+#if !CONFIG_INTEL_LYNXPOINT_LP
 	pci_write_config32(usb1, PCI_BASE_ADDRESS_0,
-			   PCH_EHCI1_TEMP_BAR0);
+			   PCH_EHCI2_TEMP_BAR0);
 	cmd = pci_read_config32(usb1, PCI_COMMAND);
 	cmd |= PCI_COMMAND_MASTER | PCI_COMMAND_MEMORY;
 	pci_write_config32(usb1, PCI_COMMAND, cmd);
+#endif
 }

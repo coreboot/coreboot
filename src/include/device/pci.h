961cdaa6c24e94638e6da4074440422bb3432fda
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-06-15 17:16:56 +0300

    Add test to match struct device with pci_devfn_t
    Add a function to test if pci_devfn_t matches with a device
    instance of struct device, by comparing bus:dev.fn.
    
    Change-Id: Ic6c3148ac62c7183246d83302ee504b17064c794
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/include/device/pci.h
+++ src/include/device/pci.h
@@ -79,6 +79,7 @@ unsigned pci_find_capability(device_t dev, unsigned cap);
 struct resource *pci_get_resource(struct device *dev, unsigned long index);
 void pci_dev_set_subsystem(device_t dev, unsigned vendor, unsigned device);
 void pci_dev_init(struct device *dev);
+unsigned int pci_match_simple_dev(device_t dev, pci_devfn_t sdev);
 
 void pci_assign_irqs(unsigned bus, unsigned slot,
 		     const unsigned char pIntAtoD[4]);

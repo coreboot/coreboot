725dd55ad4207b228aea24779cc1ae69fe44baf5
Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>
2013-06-23 19:39:03 +0200

    Lenovo T60: Add CMOS defaults.
    After removing power and the CMOS Battery, putting it back
      and booting coreboot we have:
        # ./nvramtool -a
        boot_option = Fallback
        last_boot = Fallback
        baud_rate = 115200
        debug_level = Spew
        hyper_threading = Enable
        nmi = Enable
        boot_devices = ''
        boot_default = 0x41
        cmos_defaults_loaded = Yes
        lpt = Enable
        touchpad = Disable
        bluetooth = Enable
        volume = 0x93
        first_battery = Primary
    
    The code for handling the invalid CMOS space in mainboard.c
      is now useless and so it was removed.
    
    Change-Id: I86ec6a7f73e32948adff9087d4af5372a49a46a5
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>

--- src/mainboard/lenovo/t60/mainboard.c
+++ src/mainboard/lenovo/t60/mainboard.c
@@ -50,7 +50,6 @@ static void mainboard_enable(device_t dev)
 {
 	struct southbridge_intel_i82801gx_config *config;
 	device_t dev0, idedev;
-	u8 defaults_loaded = 0;
 
 	/* If we're resuming from suspend, blink suspend LED */
 	dev0 = dev_find_slot(0, PCI_DEVFN(0,0));
@@ -77,22 +76,6 @@ static void mainboard_enable(device_t dev)
 	/* set dock status led */
 	ec_write(0x0c, 0x08);
 	ec_write(0x0c, inb(0x164c) & 8 ? 0x89 : 0x09);
-
-	if (get_option(&defaults_loaded, "cmos_defaults_loaded") < 0) {
-		printk(BIOS_INFO, "failed to get cmos_defaults_loaded");
-		defaults_loaded = 0;
-	}
-
-	if (!defaults_loaded) {
-		printk(BIOS_INFO, "Restoring CMOS defaults\n");
-		set_option("tft_brightness", &(u8[]){ 0xff });
-		set_option("volume", &(u8[]){ 0x03 });
-		/* set baudrate to 115200 baud */
-		set_option("baud_rate", &(u8[]){ 0x00 });
-		/* set default debug_level (DEFAULT_CONSOLE_LOGLEVEL starts at 1) */
-		set_option("debug_level", &(u8[]) { CONFIG_DEFAULT_CONSOLE_LOGLEVEL+1 });
-		set_option("cmos_defaults_loaded", &(u8[]){ 0x01 });
-	}
 }
 
 struct chip_operations mainboard_ops = {

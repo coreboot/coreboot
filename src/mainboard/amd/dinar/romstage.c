61eac1d21a061778c5963d14bdef7ad4b15c8220
Bruce Griffith <Bruce.Griffith@se-eng.com>
2013-06-25 14:02:41 -0600

    Dinar: Eliminate compiler warnings from build
    Fix a bunch of compiler-generated warning messages.  These fixes are
    mainly copied into AMD Family 15 server boards from similar code
    in AMD Family 15tn (Trinity) boards.
    
    Change-Id: I93cef8899170b5575e7fb7c55181b381a7bcd9d8
    Signed-off-by: Bruce Griffith <Bruce.Griffith@se-eng.com>

--- src/mainboard/amd/dinar/romstage.c
+++ src/mainboard/amd/dinar/romstage.c
@@ -41,6 +41,8 @@
 #define SERIAL_DEV PNP_DEV(CONFIG_SIO_PORT, SMSCSUPERIO_SP1)
 
 void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx);
+void disable_cache_as_ram(void);
+
 u32 agesawrapper_amdinitmmio (void);
 u32 agesawrapper_amdinitreset (void);
 u32 agesawrapper_amdinitearly (void);

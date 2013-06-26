b25a9da6e7692036fd34ee9327bc082e10c29ae0
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-06-26 08:19:14 +0300

    Unify PCI configuration cycles
    Split PCI IO configuration and MMIO configuration cycles to separate
    files. Modern hardware does not use IO cycles for PCI configuration
    after initial setup in bootblock.
    
    Note that the pci_mmio_ and pcie_ functions were different in masking
    the alignment for register address. PCI standard requires that 16-bit
    and 32-bit configuration register writes do not cross boundaries.
    
    Change-Id: Ie6441283e1a033b4b395e972c18c31277f973897
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: http://review.coreboot.org/3554
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@google.com>

--- src/arch/x86/include/arch/pci_mmio_cfg.h
+++ src/arch/x86/include/arch/pci_mmio_cfg.h
@@ -37,7 +37,7 @@ static inline __attribute__ ((always_inline))
 u16 pcie_read_config16(device_t dev, unsigned int where)
 {
 	unsigned long addr;
-	addr = DEFAULT_PCIEXBAR | dev | where;
+	addr = DEFAULT_PCIEXBAR | dev | (where & ~1);
 	return read16(addr);
 }
 
@@ -45,7 +45,7 @@ static inline __attribute__ ((always_inline))
 u32 pcie_read_config32(device_t dev, unsigned int where)
 {
 	unsigned long addr;
-	addr = DEFAULT_PCIEXBAR | dev | where;
+	addr = DEFAULT_PCIEXBAR | dev | (where & ~3);
 	return read32(addr);
 }
 
@@ -61,7 +61,7 @@ static inline __attribute__ ((always_inline))
 void pcie_write_config16(device_t dev, unsigned int where, u16 value)
 {
 	unsigned long addr;
-	addr = DEFAULT_PCIEXBAR | dev | where;
+	addr = DEFAULT_PCIEXBAR | dev | (where & ~1);
 	write16(addr, value);
 }
 
@@ -69,7 +69,7 @@ static inline __attribute__ ((always_inline))
 void pcie_write_config32(device_t dev, unsigned int where, u32 value)
 {
 	unsigned long addr;
-	addr = DEFAULT_PCIEXBAR | dev | where;
+	addr = DEFAULT_PCIEXBAR | dev | (where & ~3);
 	write32(addr, value);
 }
 
@@ -93,6 +93,24 @@ void pcie_or_config32(device_t dev, unsigned int where, u32 ormask)
 	u32 value = pcie_read_config32(dev, where);
 	pcie_write_config32(dev, where, value | ormask);
 }
+
+#define pci_mmio_read_config8 	pcie_read_config8
+#define pci_mmio_read_config16	pcie_read_config16
+#define pci_mmio_read_config32	pcie_read_config32
+
+#define pci_mmio_write_config8 	pcie_write_config8
+#define pci_mmio_write_config16	pcie_write_config16
+#define pci_mmio_write_config32	pcie_write_config32
+
+#if CONFIG_MMCONF_SUPPORT_DEFAULT
+#define pci_read_config8 	pcie_read_config8
+#define pci_read_config16	pcie_read_config16
+#define pci_read_config32	pcie_read_config32
+
+#define pci_write_config8 	pcie_write_config8
+#define pci_write_config16	pcie_write_config16
+#define pci_write_config32	pcie_write_config32
+#endif
 
 #endif /* CONFIG_MMCONF_SUPPORT */
 #endif /* _PCI_MMIO_CFG_H */

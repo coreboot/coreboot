d6ff9e7deb0680b7d54958377834bc3c7606ba0c
Shawn Nematbakhsh <shawnn@chromium.org>
2013-06-26 18:11:23 -0700

    peppy: RAM_ID + storage changes for next build.
    - Update RAM_ID table.
    - Add DEVSLP0 signal to NGFF SATA port.
    
    Note: After this change, old Micron 2GB boards will no longer boot.
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    
    Change-Id: Id68a1d6ace2702cca9c37305726cd55a0bde5005
    Reviewed-on: https://gerrit.chromium.org/gerrit/60167
    Tested-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-by: Dave Parker <dparker@chromium.org>
    Commit-Queue: Dave Parker <dparker@chromium.org>
    Reviewed-on: http://review.coreboot.org/4340
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/mainboard/google/peppy/gpio.h
+++ src/mainboard/google/peppy/gpio.h
@@ -56,7 +56,7 @@ const struct pch_lp_gpio_map mainboard_gpio_map[] = {
 	LP_GPIO_NATIVE,        /* 30: NATIVE: PCH_SUSWARN_L */
 	LP_GPIO_NATIVE,        /* 31: NATIVE: ACPRESENT */
 	LP_GPIO_NATIVE,        /* 32: NATIVE: LPC_CLKRUN_L */
-	LP_GPIO_UNUSED,        /* 33: UNUSED */
+	LP_GPIO_NATIVE,        /* 33: NATIVE: DEVSLP0 */
 	LP_GPIO_ACPI_SMI,      /* 34: EC_SMI_L */
 	LP_GPIO_ACPI_SMI,      /* 35: PCH_NMI_DBG_L (route in NMI_EN) */
 	LP_GPIO_ACPI_SCI,      /* 36: EC_SCI_L */

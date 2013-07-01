8b635bb43568c0e9171bbdfb4e3a9e704759d041
Gabe Black <gabeblack@chromium.org>
2013-07-01 04:28:23 -0700

    CBFS: Use memmove instead of memcpy when loading a file from CBFS.
    It might be the case that a file is being loaded from a portion of CBFS which
    has already been loaded into a limitted bit of memory somewhere, and we want
    to load that file in place, effectively, so that its original location in
    CBFS overlaps with its new location. That's only guaranteed to work if you use
    memmove instead of memcpy.
    
    In the case that the copy is 'sane', i.e. dest <= src, we can just call
    memcpy, which gets us the processor-optimized code. We've got no statistics
    on how common the overlapping case is.
    
    Change-Id: Id550138c875907749fff05f330fcd2fb5f9ed924
    Signed-off-by: Gabe Black <gabeblack@chromium.org>

--- src/lib/memmove.c
+++ src/lib/memmove.c
@@ -1,13 +1,38 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include <string.h>
+
 void *memmove(void *vdest, const void *vsrc, size_t count)
 {
 	const char *src = vsrc;
 	char *dest = vdest;
 
+	/* N.B. You may be tempted to not copy in the case
+	 * that dest == src. Please don't make that mistake.
+	 */
 	if (dest <= src) {
-		while (count--) {
-			*dest++ = *src++;
-		}
+		/* easy way to get the processor-optimized copy */
+		memcpy(vdest, vsrc, count);
 	} else {
 		src  += count - 1;
 		dest += count - 1;

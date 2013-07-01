248979a18abed70a1a82d802ffc4ba595b474ad5
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-07-01 11:21:53 +0300

    intel/i945: Use MMCONF_SUPPORT_DEFAULT
    Change all PCI configuration accesses to MMIO on all boards
    with i945 chipset. To enable MMIO style access, add explicit
    PCI IO config write in the bootblock.
    
    Change-Id: Ia1ab73f1a2dcda87db4eb9b2ffddc6f7b4382b01
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

---
+++ src/northbridge/intel/i945/bootblock.c
@@ -0,0 +1,24 @@
+#include <arch/io.h>
+
+/* Just re-define this instead of including i945.h. It blows up romcc. */
+#define PCIEXBAR	0x48
+
+static void bootblock_northbridge_init(void)
+{
+	uint32_t reg;
+
+	/*
+	 * The "io" variant of the config access is explicitly used to
+	 * setup the PCIEXBAR because CONFIG_MMCONF_SUPPORT_DEFAULT is set to
+	 * to true. That way all subsequent non-explicit config accesses use
+	 * MCFG. This code also assumes that bootblock_northbridge_init() is
+	 * the first thing called in the non-asm boot block code. The final
+	 * assumption is that no assembly code is using the
+	 * CONFIG_MMCONF_SUPPORT_DEFAULT option to do PCI config acceses.
+	 *
+	 * The PCIEXBAR is assumed to live in the memory mapped IO space under
+	 * 4GiB.
+	 */
+	reg = CONFIG_MMCONF_BASE_ADDRESS | 4 | 1; /* 64MiB - 0-63 buses. */
+	pci_io_write_config32(PCI_DEV(0,0,0), PCIEXBAR, reg);
+}

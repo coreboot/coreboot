9d7db422b238c6eb5e643f6bf950374fe5df0bb8
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-07-06 11:41:21 +0300

    usbdebug: Use CAR migration
    If we already initialized EHCI controller and USB device in romstage,
    locate active configuration from salvaged CAR_GLOBAL and avoid doing
    the hardware initialisation again.
    
    Change-Id: I7cb3a359488b25abc9de49c96c0197f6563a4a2c
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/lib/cbmem_info.c
+++ src/lib/cbmem_info.c
@@ -48,6 +48,7 @@ static struct cbmem_id_to_name {
 	{ CBMEM_ID_VBOOT_HANDOFF,	"VBOOT      " },
 	{ CBMEM_ID_CAR_GLOBALS,		"CAR GLOBALS" },
 	{ CBMEM_ID_AGESA_RUNTIME,	"AGESA RSVD " },
+	{ CBMEM_ID_EHCI_DEBUG,	"USBDEBUG   " },
 };
 
 void cbmem_print_entry(int n, u32 id, u64 base, u64 size)

c8fd7db869f1ab6a2628d686962ed59f9a076f57
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-07-07 11:18:23 +0300

    arch/armv7: Drop PCI config operations
    Leave mostly empty stub file to not break includes.
    
    Change-Id: I1c2b47dfccc612e3d342cc3c3083630154869ec2
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/arch/armv7/include/arch/pci_ops.h
+++ src/arch/armv7/include/arch/pci_ops.h
@@ -1,19 +1,9 @@
-#ifndef ARCH_I386_PCI_OPS_H
-#define ARCH_I386_PCI_OPS_H
-
-extern const struct pci_bus_operations pci_cf8_conf1;
-
-#if CONFIG_MMCONF_SUPPORT
-extern const struct pci_bus_operations pci_ops_mmconf;
-#endif
+#ifndef ARCH_ARMV7_PCI_OPS_H
+#define ARCH_ARMV7_PCI_OPS_H
 
 static inline const struct pci_bus_operations *pci_config_default(void)
 {
-	return &pci_cf8_conf1;
+	return NULL;
 }
 
-static inline void pci_set_method(device_t dev)
-{
-	dev->ops->ops_pci_bus = pci_config_default();
-}
-#endif /* ARCH_I386_PCI_OPS_H */
+#endif /* ARCH_ARMV7_PCI_OPS_H */

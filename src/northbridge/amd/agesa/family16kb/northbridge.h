c4d16df7cb791dded954d254193884ea7173c39b
Siyuan Wang <wangsiyuanbuaa@gmail.com>
2013-07-09 17:16:20 +0800

    AGESA F16kb northbridge wrapper for Kabini
    src/arch/x86/boot/tables.c and src/include/device/pci_ids.h are also
    changed because these two files are depend on F16kb northbridge macros
    
    Change-Id: Iedc842f0b230826675703fc78ed8001a978319c5
    Signed-off-by: Siyuan Wang <SiYuan.Wang@amd.com>
    Signed-off-by: Siyuan Wang <wangsiyuanbuaa@gmail.com>
    Signed-off-by: Bruce Griffith <Bruce.Griffith@se-eng.com>

---
+++ src/northbridge/amd/agesa/family16kb/northbridge.h
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef NORTHBRIDGE_AMD_AGESA_FAM16H_H
+#define NORTHBRIDGE_AMD_AGESA_FAM16H_H
+
+static struct device_operations pci_domain_ops;
+static struct device_operations cpu_bus_ops;
+
+#endif /* NORTHBRIDGE_AMD_AGESA_FAM16H_H */

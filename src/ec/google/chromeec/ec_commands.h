2b7927e57e588b0f26b662802729c4b928addf99
Martin Roth <martin.roth@se-eng.com>
2013-07-09 21:52:41 -0600

    ec: Fix spelling
    Change-Id: I5e4d35572c43f07bec5ec0bcd75c717723228e2f
    Signed-off-by: Martin Roth <martin.roth@se-eng.com>

--- src/ec/google/chromeec/ec_commands.h
+++ src/ec/google/chromeec/ec_commands.h
@@ -255,7 +255,7 @@ struct ec_lpc_host_args {
  * If EC gets a command and this flag is not set, this is an old-style command.
  * Command version is 0 and params from host are at EC_LPC_ADDR_OLD_PARAM with
  * unknown length.  EC must respond with an old-style response (that is,
- * withouth setting EC_HOST_ARGS_FLAG_TO_HOST).
+ * without setting EC_HOST_ARGS_FLAG_TO_HOST).
  */
 #define EC_HOST_ARGS_FLAG_FROM_HOST 0x01
 /*
@@ -748,7 +748,7 @@ struct ec_response_port80_last_boot {
 /*****************************************************************************/
 /* Thermal engine commands */
 
-/* Set thershold value */
+/* Set threshold value */
 #define EC_CMD_THERMAL_SET_THRESHOLD 0x50
 
 struct ec_params_thermal_set_threshold {

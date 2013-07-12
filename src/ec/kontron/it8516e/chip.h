9ce71b3cca0b59fdc262d89dafc8f75efbc167a6
Nico Huber <nico.huber@secunet.com>
2013-07-12 14:43:11 +0200

    ec/kontron/it8516e: Add PWM limits option
    Add an option to set minimal and maximal PWM percentages when the fan is
    in temperature controlled mode. Also fix a non-ascii flaw.
    
    Change-Id: I85ae244bee2145bf17d6c29e93dd4871540985c8
    Signed-off-by: Nico Huber <nico.huber@secunet.com>
    Reviewed-on: http://review.coreboot.org/3774
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/ec/kontron/it8516e/chip.h
+++ src/ec/kontron/it8516e/chip.h
@@ -30,12 +30,15 @@ struct ec_kontron_it8516e_config {
 	 * Can be overwritten by
 	 *   systemp_type,
 	 *   fan1_mode, fan2_mode,
-	 *   fan1_target, and fan2_target
+	 *   fan1_target, fan2_target,
+	 *   fan1_min, fan2_min, fan1_max, and fan2_max
 	 * nvram options.
 	 */
 	enum it8516e_systemp_types default_systemp;
 	enum it8516e_fan_modes default_fan_mode[2];
-	u16 default_fan_target[2]; /* PWM: % / Speed: RPM / Thermal: °C */
+	u16 default_fan_target[2]; /* PWM: % / Speed: RPM / Thermal: degree C */
+	u8 default_fan_min[2]; /* min PWM in % */
+	u8 default_fan_max[2]; /* max PWM in % */
 };
 
 #endif /* EC_KONTRON_IT8516E_CHIP_H */

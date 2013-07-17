b58f3245cd31c78f72d162fdb4b48610819ea225
Damien Zammit <damien@zamaudio.com>
2013-07-17 23:59:40 +1000

    inteltool/inteltool.c: Add CPUID raw model and remove colon
    The output now looks like
    
    	$ sudo ./inteltool
    	CPU: ID 0x1067a, Processor Type 0, Family 6, Model 17, Stepping a
    	[…]
    
    and is useful for finding out the Intel processor under `src/cpu/intel`.
    
    While at it, remove the colon after »Processor Type« too.
    
    Change-Id: I1b939ab9466f053c0b86683df145c7d2c8baadb4
    Signed-off-by: Damien Zammit <damien@zamaudio.com>
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- util/inteltool/inteltool.c
+++ util/inteltool/inteltool.c
@@ -374,8 +374,8 @@ int main(int argc, char *argv[])
 	 * left-shifted "Extended Model" fields.
 	 * http://download.intel.com/design/processor/applnots/24161832.pdf
 	 */
-	printf("CPU: Processor Type: %x, Family %x, Model %x, Stepping %x\n",
-			(id >> 12) & 0x3, ((id >> 8) & 0xf) + ((id >> 20) & 0xff),
+	printf("CPU: ID 0x%x, Processor Type %x, Family %x, Model %x, Stepping %x\n",
+			id, (id >> 12) & 0x3, ((id >> 8) & 0xf) + ((id >> 20) & 0xff),
 			((id >> 12) & 0xf0) + ((id >> 4) & 0xf), (id & 0xf));
 
 	/* Determine names */

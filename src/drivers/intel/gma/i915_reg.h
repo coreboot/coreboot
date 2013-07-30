e3c61f83cbee0a9297c9c62e54bfcfe855823b23
Furquan Shaikh <furquan@google.com>
2013-07-30 12:41:08 -0700

    Calculate transcoder flags based on pipe config
    Works fine with all three panels with the change of 6 bits per color.
    
    Change-Id: Ia47d152e62d1879150d8cf9a6657b62007ef5c0e
    Reviewed-on: https://gerrit.chromium.org/gerrit/63762
    Reviewed-by: Ronald G. Minnich <rminnich@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>

--- src/drivers/intel/gma/i915_reg.h
+++ src/drivers/intel/gma/i915_reg.h
@@ -4554,6 +4554,7 @@
 #define  DDI_BUF_EMP_MASK			(0xf<<24)
 #define  DDI_BUF_IS_IDLE			(1<<7)
 #define  DDI_A_4_LANES				(1<<4)
+#define  DDI_PORT_WIDTH(width)                  (((width) - 1) << 1)
 #define  DDI_PORT_WIDTH_X1			(0<<1)
 #define  DDI_PORT_WIDTH_X2			(1<<1)
 #define  DDI_PORT_WIDTH_X4			(3<<1)

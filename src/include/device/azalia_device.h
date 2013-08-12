502f230c36f33e4e736e49b4ac14cc76caf1d467
Andrew Wu <arw@dmp.com.tw>
2013-08-12 20:07:47 +0800

    Add a generic Intel HD audio (Azalia) module azalia_device.c
    This module uses cim_verb_data to detect and initialize HD audio
    codecs.
    The module source code is based on southbridge/intel/sch/audio.c and
    southbridge/nvidia/mcp55/azalia.c.
    
    Change-Id: I810fef6fdcf55d66f62da58c3d7d99f006559d6e
    Signed-off-by: Andrew Wu <arw@dmp.com.tw>

---
+++ src/include/device/azalia_device.h
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 DMP Electronics Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef DEVICE_AZALIA_H
+#define DEVICE_AZALIA_H
+
+#include <device/device.h>
+
+void azalia_audio_init(struct device *dev);
+extern struct device_operations default_azalia_audio_ops;
+
+#endif /* DEVICE_AZALIA_H */

4eb3ddf2b20d1472bea54903a2403aba05907aaa
Aaron Durbin <adurbin@chromium.org>
2013-09-07 00:41:48 -0500

    baytrail: add initial support
    The initial Bay Trail code is intended to support
    the mobile and desktop version of Bay Trail. This support
    can train memory and execute through ramstage. However,
    the resource allocation is not curently handled correctly.
    The MRC cache parameters are successfully saved and reused
    after the initial cold boot.
    
    BUG=chrome-os-partner:22292
    BRANCH=None
    TEST=Built and booted on a reference board through ramstage.
    
    Change-Id: I238ede326802aad272c6cca39d7ad4f161d813f5
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/168387
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

---
+++ src/soc/intel/baytrail/baytrail/lpc.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef _BAYTRAIL_LPC_H_
+#define _BAYTRAIL_LPC_H_
+
+/* PCI config registers in LPC bridge. */
+#define ABASE		0x40
+#define PBASE		0x44
+#define GBASE		0x48
+#define IOBASE		0x4c
+#define IBASE		0x50
+#define SBASE		0x54
+#define MPBASE		0x58
+#define UART_CONT	0x80
+#define RCBA		0xf0
+
+#endif /* _BAYTRAIL_LPC_H_ */
---
+++ src/soc/intel/baytrail/memmap.c
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 Google, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied wacbmem_entryanty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include <arch/io.h>
+#include <cbmem.h>
+#include <baytrail/iosf.h>
+
+unsigned long get_top_of_ram(void)
+{
+	return (unsigned long)(iosf_bunit_read(BUNIT_SMRRL) << 20);
+}

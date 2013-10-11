80d99aef700d334f2b59f1f786c1910628ded86d
Aaron Durbin <adurbin@chromium.org>
2013-10-10 20:54:57 -0500

    baytrail: add reset support
    Bay Trail has the following types of resets it supports:
     - Soft reset (INIT# to cpu) - write 0x1 to I/O 0x92
     - Soft reset (INIT# to cpu)- write 0x4 to I/0 0xcf9
     - Cold reset (S0->S5->S0) - write 0xe to I/0 0xcf9
     - Warm reset (PMC_PLTRST# assertion) - write 0x6 to I/O 0xcf9
     - Global reset (S0->S5->S0 with TXE reset) - write 0x6 or 0xe to
       0xcf9 but with ETR[20] set.
    
    While these are documented this support currently provides support
    for 2nd soft reset as well as cold and warm reset.
    
    BUG=chrome-os-partner:23249
    BRANCH=None
    TEST=Built and booted.
    
    Change-Id: I9746e7c8aed0ffc29e7afa137798e38c5da9c888
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/172710
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>

---
+++ src/soc/intel/baytrail/reset.c
@@ -0,0 +1,47 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <arch/io.h>
+#include <baytrail/pmc.h>
+#include <baytrail/reset.h>
+
+void cold_reset(void)
+{
+	/* S0->S5->S0 trip. */
+	outb(RST_CPU | SYS_RST | FULL_RST, RST_CNT);
+}
+
+void warm_reset(void)
+{
+	/* PMC_PLTRST# asserted. */
+	outb(RST_CPU | SYS_RST, RST_CNT);
+}
+
+void soft_reset(void)
+{
+	/* Sends INIT# to CPU */
+	outb(RST_CPU, RST_CNT);
+}
+
+void hard_reset(void)
+{
+	/* Don't power cycle on hard_reset(). It's not really clear what the
+	 * semantics should be for the meaning of hard_reset(). */
+	warm_reset();
+}

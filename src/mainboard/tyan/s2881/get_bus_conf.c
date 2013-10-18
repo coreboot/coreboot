16447c8da5637f7165c1600e5cc780914e7f96d7
Paul Menzel <paulepanter@users.sourceforge.net>
2013-10-18 09:42:55 +0200

    get_bus_conf.c: Run files with spaces instead of tabulators through `indent -linux`
    For the board Win Enterprises MB6047 board, the board Tyan S2891
    (Thunder K8SRE) is taken as template [1]. During review it turned out,
    `get_bus_conf.c` has spaces instead of tabs in some lines. To not
    spread these coding style errors further, correct all `get_bus_conf.c`
    with spaces at the beginning of at least one line. `indent` from Debian
    package indent 2.2.11-4 is used for that with the following command.
    
    	$ git grep -l '       ' src/mainboard/*/*/get_bus_conf.c | xargs indent -linux
    
    Please note that more errors than just the spaces at the beginning of
    lines are fixed by that.
    
    [1] http://review.coreboot.org/3974
    
    Change-Id: Ia0c37339aa69b92a1b518fa5e49adc4a7628ae5d
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- src/mainboard/tyan/s2881/get_bus_conf.c
+++ src/mainboard/tyan/s2881/get_bus_conf.c
@@ -10,7 +10,6 @@
 #include <cpu/amd/amdk8_sysconf.h>
 #include <stdlib.h>
 
-
 // Global variables for MB layouts and these will be shared by irqtable mptable and acpi_tables
 //busnum is default
 unsigned char bus_8131_0 = 1;
@@ -18,14 +17,13 @@ unsigned char bus_8131_1 = 2;
 unsigned char bus_8131_2 = 3;
 unsigned char bus_8111_0 = 1;
 unsigned char bus_8111_1 = 4;
-unsigned apicid_8111 ;
+unsigned apicid_8111;
 unsigned apicid_8131_1;
 unsigned apicid_8131_2;
 
-unsigned pci1234x[] =
-{        //Here you only need to set value in pci1234 for HT-IO that could be installed or not
-	 //You may need to preset pci1234 for HTIO board, please refer to src/northbridge/amd/amdk8/get_sblk_pci1234.c for detail
-        0x0000ff0,
+unsigned pci1234x[] = {		//Here you only need to set value in pci1234 for HT-IO that could be installed or not
+	//You may need to preset pci1234 for HTIO board, please refer to src/northbridge/amd/amdk8/get_sblk_pci1234.c for detail
+	0x0000ff0,
 //        0x0000ff0,
 //        0x0000ff0,
 //        0x0000ff0,
@@ -34,10 +32,10 @@ unsigned pci1234x[] =
 //        0x0000ff0,
 //        0x0000ff0
 };
-unsigned hcdnx[] =
-{ //HT Chain device num, actually it is unit id base of every ht device in chain, assume every chain only have 4 ht device at most
+
+unsigned hcdnx[] = {		//HT Chain device num, actually it is unit id base of every ht device in chain, assume every chain only have 4 ht device at most
 	0x20202020,
-//	0x20202020,
+//      0x20202020,
 //        0x20202020,
 //        0x20202020,
 //        0x20202020,
@@ -45,9 +43,8 @@ unsigned hcdnx[] =
 //        0x20202020,
 //        0x20202020,
 };
+
 unsigned sbdn3;
-
-
 
 static unsigned get_bus_conf_done = 0;
 
@@ -56,20 +53,21 @@ void get_bus_conf(void)
 
 	unsigned apicid_base;
 
-        device_t dev;
-        int i;
+	device_t dev;
+	int i;
 
-        if(get_bus_conf_done==1) return; //do it only once
+	if (get_bus_conf_done == 1)
+		return;		//do it only once
 
-        get_bus_conf_done = 1;
+	get_bus_conf_done = 1;
 
-        sysconf.hc_possible_num = ARRAY_SIZE(pci1234x);
-        for(i=0;i<sysconf.hc_possible_num; i++) {
-                sysconf.pci1234[i] = pci1234x[i];
-                sysconf.hcdn[i] = hcdnx[i];
-        }
+	sysconf.hc_possible_num = ARRAY_SIZE(pci1234x);
+	for (i = 0; i < sysconf.hc_possible_num; i++) {
+		sysconf.pci1234[i] = pci1234x[i];
+		sysconf.hcdn[i] = hcdnx[i];
+	}
 
-        get_sblk_pci1234();
+	get_sblk_pci1234();
 
 	sysconf.sbdn = (sysconf.hcdn[0] >> 8) & 0xff;
 	sbdn3 = sysconf.hcdn[0] & 0xff;
@@ -77,33 +75,35 @@ void get_bus_conf(void)
 	bus_8131_0 = (sysconf.pci1234[0] >> 16) & 0xff;
 	bus_8111_0 = bus_8131_0;
 
-                /* 8111 */
-        dev = dev_find_slot(bus_8111_0, PCI_DEVFN(sysconf.sbdn,0));
-        if (dev) {
-                bus_8111_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
-        }
-	else {
-                printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:03.0, using defaults\n", bus_8111_0);
-        }
+	/* 8111 */
+	dev = dev_find_slot(bus_8111_0, PCI_DEVFN(sysconf.sbdn, 0));
+	if (dev) {
+		bus_8111_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
+	} else {
+		printk(BIOS_DEBUG,
+		       "ERROR - could not find PCI %02x:03.0, using defaults\n",
+		       bus_8111_0);
+	}
 
-        /* 8131-1 */
-        dev = dev_find_slot(bus_8131_0, PCI_DEVFN(sbdn3,0));
-        if (dev) {
-                bus_8131_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
-        }
-        else {
-                printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:01.0, using defaults\n", bus_8131_0);
-        }
+	/* 8131-1 */
+	dev = dev_find_slot(bus_8131_0, PCI_DEVFN(sbdn3, 0));
+	if (dev) {
+		bus_8131_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
+	} else {
+		printk(BIOS_DEBUG,
+		       "ERROR - could not find PCI %02x:01.0, using defaults\n",
+		       bus_8131_0);
+	}
 
-        /* 8132-2 */
-        dev = dev_find_slot(bus_8131_0, PCI_DEVFN(sbdn3+1,0));
-        if (dev) {
-                bus_8131_2 = pci_read_config8(dev, PCI_SECONDARY_BUS);
-        }
-        else {
-                printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:02.0, using defaults\n", bus_8131_0);
-        }
-
+	/* 8132-2 */
+	dev = dev_find_slot(bus_8131_0, PCI_DEVFN(sbdn3 + 1, 0));
+	if (dev) {
+		bus_8131_2 = pci_read_config8(dev, PCI_SECONDARY_BUS);
+	} else {
+		printk(BIOS_DEBUG,
+		       "ERROR - could not find PCI %02x:02.0, using defaults\n",
+		       bus_8131_0);
+	}
 
 /*I/O APICs:	APIC ID	Version	State		Address*/
 #if CONFIG_LOGICAL_CPUS
@@ -111,7 +111,7 @@ void get_bus_conf(void)
 #else
 	apicid_base = CONFIG_MAX_PHYSICAL_CPUS;
 #endif
-	apicid_8111 = apicid_base+0;
-	apicid_8131_1 = apicid_base+1;
-	apicid_8131_2 = apicid_base+2;
+	apicid_8111 = apicid_base + 0;
+	apicid_8131_1 = apicid_base + 1;
+	apicid_8131_2 = apicid_base + 2;
 }

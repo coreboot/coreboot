c077a9fd04cefaaefbab5866a4c0c37ff2d3f407
Paul Menzel <paulepanter@users.sourceforge.net>
2013-10-18 09:42:55 +0200

    get_bus_conf.c: reindent with indent
    Change-Id: Ia0c37339aa69b92a1b518fa5e49adc4a7628ae5d
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- src/mainboard/amd/serengeti_cheetah/get_bus_conf.c
+++ src/mainboard/amd/serengeti_cheetah/get_bus_conf.c
@@ -15,11 +15,10 @@
 // Global variables for MB layouts and these will be shared by irqtable mptable and acpi_tables
 struct mb_sysconf_t mb_sysconf;
 
-static unsigned pci1234x[] =
-{        //Here you only need to set value in pci1234 for HT-IO that could be installed or not
-	 //You may need to preset pci1234 for HTIO board, please refer to src/northbridge/amd/amdk8/get_sblk_pci1234.c for detail
-        0x0000ff0,
-        0x0000ff0,
+static unsigned pci1234x[] = {	//Here you only need to set value in pci1234 for HT-IO that could be installed or not
+	//You may need to preset pci1234 for HTIO board, please refer to src/northbridge/amd/amdk8/get_sblk_pci1234.c for detail
+	0x0000ff0,
+	0x0000ff0,
 //        0x0000ff0,
 //        0x0000ff0,
 //        0x0000ff0,
@@ -27,8 +26,8 @@ static unsigned pci1234x[] =
 //        0x0000ff0,
 //        0x0000ff0
 };
-static unsigned hcdnx[] =
-{ //HT Chain device num, actually it is unit id base of every ht device in chain, assume every chain only have 4 ht device at most
+
+static unsigned hcdnx[] = {	//HT Chain device num, actually it is unit id base of every ht device in chain, assume every chain only have 4 ht device at most
 	0x20202020,
 	0x20202020,
 //        0x20202020,
@@ -39,39 +38,37 @@ static unsigned hcdnx[] =
 //        0x20202020,
 };
 
-
-
 static unsigned get_bus_conf_done = 0;
 
 static unsigned get_hcid(unsigned i)
 {
-        unsigned id = 0;
+	unsigned id = 0;
 
-        unsigned busn = (sysconf.pci1234[i] >> 16) & 0xff;
+	unsigned busn = (sysconf.pci1234[i] >> 16) & 0xff;
 
-        unsigned devn = sysconf.hcdn[i] & 0xff;
+	unsigned devn = sysconf.hcdn[i] & 0xff;
 
-        device_t dev;
+	device_t dev;
 
-        dev = dev_find_slot(busn, PCI_DEVFN(devn,0));
+	dev = dev_find_slot(busn, PCI_DEVFN(devn, 0));
 
-        switch (dev->device) {
-        case 0x7458: //8132
-                id = 1;
-                break;
-        case 0x7454: //8151
-                id = 2;
+	switch (dev->device) {
+	case 0x7458:		//8132
+		id = 1;
 		break;
-        case 0x7450: //8131
-                id = 3;
-                break;
-        }
+	case 0x7454:		//8151
+		id = 2;
+		break;
+	case 0x7450:		//8131
+		id = 3;
+		break;
+	}
 
-        // we may need more way to find out hcid: subsystem id? GPIO read ?
+	// we may need more way to find out hcid: subsystem id? GPIO read ?
 
-        // we need use id for 1. bus num, 2. mptable, 3. acpi table
+	// we need use id for 1. bus num, 2. mptable, 3. acpi table
 
-        return id;
+	return id;
 }
 
 void get_bus_conf(void)
@@ -79,11 +76,12 @@ void get_bus_conf(void)
 
 	unsigned apicid_base;
 
-        device_t dev;
+	device_t dev;
 	int i, j;
 	struct mb_sysconf_t *m;
 
-	if(get_bus_conf_done == 1) return; //do it only once
+	if (get_bus_conf_done == 1)
+		return;		//do it only once
 
 	get_bus_conf_done = 1;
 
@@ -92,7 +90,7 @@ void get_bus_conf(void)
 	m = sysconf.mb;
 
 	sysconf.hc_possible_num = ARRAY_SIZE(pci1234x);
-	for(i=0;i<sysconf.hc_possible_num; i++) {
+	for (i = 0; i < sysconf.hc_possible_num; i++) {
 		sysconf.pci1234[i] = pci1234x[i];
 		sysconf.hcdn[i] = hcdnx[i];
 	}
@@ -105,91 +103,106 @@ void get_bus_conf(void)
 	m->bus_8132_0 = (sysconf.pci1234[0] >> 16) & 0xff;
 	m->bus_8111_0 = m->bus_8132_0;
 
-                /* 8111 */
-        dev = dev_find_slot(m->bus_8111_0, PCI_DEVFN(sysconf.sbdn,0));
-        if (dev) {
-                m->bus_8111_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
-        }
-	else {
-                printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:%02x.0, using defaults\n", m->bus_8111_0, sysconf.sbdn);
-        }
+	/* 8111 */
+	dev = dev_find_slot(m->bus_8111_0, PCI_DEVFN(sysconf.sbdn, 0));
+	if (dev) {
+		m->bus_8111_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
+	} else {
+		printk(BIOS_DEBUG,
+		       "ERROR - could not find PCI %02x:%02x.0, using defaults\n",
+		       m->bus_8111_0, sysconf.sbdn);
+	}
 
-        /* 8132-1 */
-        dev = dev_find_slot(m->bus_8132_0, PCI_DEVFN(m->sbdn3,0));
-        if (dev) {
-                m->bus_8132_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
-        }
-        else {
-                printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:%02x.0, using defaults\n", m->bus_8132_0, m->sbdn3);
-        }
+	/* 8132-1 */
+	dev = dev_find_slot(m->bus_8132_0, PCI_DEVFN(m->sbdn3, 0));
+	if (dev) {
+		m->bus_8132_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
+	} else {
+		printk(BIOS_DEBUG,
+		       "ERROR - could not find PCI %02x:%02x.0, using defaults\n",
+		       m->bus_8132_0, m->sbdn3);
+	}
 
-        /* 8132-2 */
-        dev = dev_find_slot(m->bus_8132_0, PCI_DEVFN(m->sbdn3+1,0));
-        if (dev) {
-                m->bus_8132_2 = pci_read_config8(dev, PCI_SECONDARY_BUS);
-        }
-        else {
-                printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:%02x.0, using defaults\n", m->bus_8132_0, m->sbdn3+1);
-        }
+	/* 8132-2 */
+	dev = dev_find_slot(m->bus_8132_0, PCI_DEVFN(m->sbdn3 + 1, 0));
+	if (dev) {
+		m->bus_8132_2 = pci_read_config8(dev, PCI_SECONDARY_BUS);
+	} else {
+		printk(BIOS_DEBUG,
+		       "ERROR - could not find PCI %02x:%02x.0, using defaults\n",
+		       m->bus_8132_0, m->sbdn3 + 1);
+	}
 
-        /* HT chain 1 */
-        j=0;
-        for(i=1; i< sysconf.hc_possible_num; i++) {
-                if(!(sysconf.pci1234[i] & 0x1) ) continue;
+	/* HT chain 1 */
+	j = 0;
+	for (i = 1; i < sysconf.hc_possible_num; i++) {
+		if (!(sysconf.pci1234[i] & 0x1))
+			continue;
 
-                // check hcid type here
-                sysconf.hcid[i] = get_hcid(i);
+		// check hcid type here
+		sysconf.hcid[i] = get_hcid(i);
 
-                switch(sysconf.hcid[i]) {
+		switch (sysconf.hcid[i]) {
 
-                case 1: //8132
-		case 3: //8131
+		case 1:	//8132
+		case 3:	//8131
 
-                        m->bus_8132a[j][0] = (sysconf.pci1234[i] >> 16) & 0xff;
+			m->bus_8132a[j][0] = (sysconf.pci1234[i] >> 16) & 0xff;
 
-                        m->sbdn3a[j] = sysconf.hcdn[i] & 0xff;
+			m->sbdn3a[j] = sysconf.hcdn[i] & 0xff;
 
-                        /* 8132-1 */
-                        dev = dev_find_slot(m->bus_8132a[j][0], PCI_DEVFN(m->sbdn3a[j],0));
-                        if (dev) {
-                                m->bus_8132a[j][1] = pci_read_config8(dev, PCI_SECONDARY_BUS);
-                        }
-                        else {
-                        printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:%02x.0, using defaults\n", m->bus_8132a[j][0], m->sbdn3a[j]);
-                        }
+			/* 8132-1 */
+			dev =
+			    dev_find_slot(m->bus_8132a[j][0],
+					  PCI_DEVFN(m->sbdn3a[j], 0));
+			if (dev) {
+				m->bus_8132a[j][1] =
+				    pci_read_config8(dev, PCI_SECONDARY_BUS);
+			} else {
+				printk(BIOS_DEBUG,
+				       "ERROR - could not find PCI %02x:%02x.0, using defaults\n",
+				       m->bus_8132a[j][0], m->sbdn3a[j]);
+			}
 
-                        /* 8132-2 */
-                        dev = dev_find_slot(m->bus_8132a[j][0], PCI_DEVFN(m->sbdn3a[j]+1,0));
-                        if (dev) {
-                                m->bus_8132a[j][2] = pci_read_config8(dev, PCI_SECONDARY_BUS);
-                                }
-                        else {
-                                printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:%02x.0, using defaults\n", m->bus_8132a[j][0], m->sbdn3a[j]+1);
-                        }
+			/* 8132-2 */
+			dev =
+			    dev_find_slot(m->bus_8132a[j][0],
+					  PCI_DEVFN(m->sbdn3a[j] + 1, 0));
+			if (dev) {
+				m->bus_8132a[j][2] =
+				    pci_read_config8(dev, PCI_SECONDARY_BUS);
+			} else {
+				printk(BIOS_DEBUG,
+				       "ERROR - could not find PCI %02x:%02x.0, using defaults\n",
+				       m->bus_8132a[j][0], m->sbdn3a[j] + 1);
+			}
 
-                        break;
+			break;
 
-                case 2: //8151
+		case 2:	//8151
 
-                        m->bus_8151[j][0] = (sysconf.pci1234[i] >> 16) & 0xff;
-                        m->sbdn5[j] = sysconf.hcdn[i] & 0xff;
-                        /* 8151 */
-                        dev = dev_find_slot(m->bus_8151[j][0], PCI_DEVFN(m->sbdn5[j]+1, 0));
+			m->bus_8151[j][0] = (sysconf.pci1234[i] >> 16) & 0xff;
+			m->sbdn5[j] = sysconf.hcdn[i] & 0xff;
+			/* 8151 */
+			dev =
+			    dev_find_slot(m->bus_8151[j][0],
+					  PCI_DEVFN(m->sbdn5[j] + 1, 0));
 
-                        if (dev) {
-                                m->bus_8151[j][1] = pci_read_config8(dev, PCI_SECONDARY_BUS);
-        //                        printk(BIOS_DEBUG, "bus_8151_1=%d\n",bus_8151[j][1]);
-                        }
-                        else {
-                                printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:%02x.0, using defaults\n", m->bus_8151[j][0], m->sbdn5[j]+1);
-                        }
+			if (dev) {
+				m->bus_8151[j][1] =
+				    pci_read_config8(dev, PCI_SECONDARY_BUS);
+				//                        printk(BIOS_DEBUG, "bus_8151_1=%d\n",bus_8151[j][1]);
+			} else {
+				printk(BIOS_DEBUG,
+				       "ERROR - could not find PCI %02x:%02x.0, using defaults\n",
+				       m->bus_8151[j][0], m->sbdn5[j] + 1);
+			}
 
-                        break;
-                }
+			break;
+		}
 
-                j++;
-        }
-
+		j++;
+	}
 
 /*I/O APICs:	APIC ID	Version	State		Address*/
 #if CONFIG_LOGICAL_CPUS
@@ -197,12 +210,12 @@ void get_bus_conf(void)
 #else
 	apicid_base = CONFIG_MAX_PHYSICAL_CPUS;
 #endif
-	m->apicid_8111 = apicid_base+0;
-	m->apicid_8132_1 = apicid_base+1;
-	m->apicid_8132_2 = apicid_base+2;
-        for(i=0;i<j;i++) {
-                m->apicid_8132a[i][0] = apicid_base + 3 + i*2;
-                m->apicid_8132a[i][1] = apicid_base + 3 + i*2 + 1;
-        }
+	m->apicid_8111 = apicid_base + 0;
+	m->apicid_8132_1 = apicid_base + 1;
+	m->apicid_8132_2 = apicid_base + 2;
+	for (i = 0; i < j; i++) {
+		m->apicid_8132a[i][0] = apicid_base + 3 + i * 2;
+		m->apicid_8132a[i][1] = apicid_base + 3 + i * 2 + 1;
+	}
 
 }
--- src/mainboard/msi/ms9185/get_bus_conf.c
+++ src/mainboard/msi/ms9185/get_bus_conf.c
@@ -38,11 +38,10 @@
 // Global variables for MB layouts and these will be shared by irqtable mptable and acpi_tables
 struct mb_sysconf_t mb_sysconf;
 
-static unsigned pci1234x[] =
-{        //Here you only need to set value in pci1234 for HT-IO that could be installed or not
-         //You may need to preset pci1234 for HTIO board, please refer to src/northbridge/amd/amdk8/get_sblk_pci1234.c for detail
-        0x0000ff0,
-        0x0000ff0,
+static unsigned pci1234x[] = {	//Here you only need to set value in pci1234 for HT-IO that could be installed or not
+	//You may need to preset pci1234 for HTIO board, please refer to src/northbridge/amd/amdk8/get_sblk_pci1234.c for detail
+	0x0000ff0,
+	0x0000ff0,
 //        0x0000ff0,
 //        0x0000ff0,
 //        0x0000ff0,
@@ -50,10 +49,10 @@ static unsigned pci1234x[] =
 //        0x0000ff0,
 //        0x0000ff0
 };
-static unsigned hcdnx[] =
-{ //HT Chain device num, actually it is unit id base of every ht device in chain, assume every chain only have 4 ht device at most
-        0x20202020,
-        0x20202020,
+
+static unsigned hcdnx[] = {	//HT Chain device num, actually it is unit id base of every ht device in chain, assume every chain only have 4 ht device at most
+	0x20202020,
+	0x20202020,
 //        0x20202020,
 //        0x20202020,
 //        0x20202020,
@@ -62,73 +61,77 @@ static unsigned hcdnx[] =
 //        0x20202020,
 };
 
-
-
 static unsigned get_bus_conf_done = 0;
 
 void get_bus_conf(void)
 {
 
-       unsigned apicid_base;
+	unsigned apicid_base;
 
-        device_t dev;
-        int i;
-       struct mb_sysconf_t *m;
+	device_t dev;
+	int i;
+	struct mb_sysconf_t *m;
 
-        if(get_bus_conf_done==1) return; //do it only once
+	if (get_bus_conf_done == 1)
+		return;		//do it only once
 
-        get_bus_conf_done = 1;
+	get_bus_conf_done = 1;
 
-       sysconf.mb = &mb_sysconf;
+	sysconf.mb = &mb_sysconf;
 
-       m = sysconf.mb;
+	m = sysconf.mb;
 
-        sysconf.hc_possible_num = ARRAY_SIZE(pci1234x);
+	sysconf.hc_possible_num = ARRAY_SIZE(pci1234x);
 
-        for(i=0;i<sysconf.hc_possible_num; i++) {
-                sysconf.pci1234[i] = pci1234x[i];
-                sysconf.hcdn[i] = hcdnx[i];
-        }
+	for (i = 0; i < sysconf.hc_possible_num; i++) {
+		sysconf.pci1234[i] = pci1234x[i];
+		sysconf.hcdn[i] = hcdnx[i];
+	}
 
-        get_sblk_pci1234();
+	get_sblk_pci1234();
 
-        sysconf.sbdn = (sysconf.hcdn[0] >> 8) & 0xff;
-        m->sbdn2 = sysconf.hcdn[0] & 0xff; // bcm5780
+	sysconf.sbdn = (sysconf.hcdn[0] >> 8) & 0xff;
+	m->sbdn2 = sysconf.hcdn[0] & 0xff;	// bcm5780
 
-       m->bus_bcm5785_0 = (sysconf.pci1234[0] >> 16) & 0xff;
-       m->bus_bcm5780[0] = m->bus_bcm5785_0;
+	m->bus_bcm5785_0 = (sysconf.pci1234[0] >> 16) & 0xff;
+	m->bus_bcm5780[0] = m->bus_bcm5785_0;
 
-                /* bcm5785 */
-        dev = dev_find_slot(m->bus_bcm5785_0, PCI_DEVFN(sysconf.sbdn,0));
-        if (dev) {
-                m->bus_bcm5785_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
-               dev = dev_find_slot(m->bus_bcm5785_1, PCI_DEVFN(0xd,0));
-               if(dev) {
-                       m->bus_bcm5785_1_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
-               }
-        }
-       else {
-                printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:%02x.0, using defaults\n", m->bus_bcm5785_0, sysconf.sbdn);
-        }
+	/* bcm5785 */
+	dev = dev_find_slot(m->bus_bcm5785_0, PCI_DEVFN(sysconf.sbdn, 0));
+	if (dev) {
+		m->bus_bcm5785_1 = pci_read_config8(dev, PCI_SECONDARY_BUS);
+		dev = dev_find_slot(m->bus_bcm5785_1, PCI_DEVFN(0xd, 0));
+		if (dev) {
+			m->bus_bcm5785_1_1 =
+			    pci_read_config8(dev, PCI_SECONDARY_BUS);
+		}
+	} else {
+		printk(BIOS_DEBUG,
+		       "ERROR - could not find PCI %02x:%02x.0, using defaults\n",
+		       m->bus_bcm5785_0, sysconf.sbdn);
+	}
 
-               /* bcm5780 */
-       for(i = 1; i < 7; i++) {
-               dev = dev_find_slot(m->bus_bcm5780[0], PCI_DEVFN(m->sbdn2 + i - 1,0));
-               if(dev) {
-                       m->bus_bcm5780[i] = pci_read_config8(dev, PCI_SECONDARY_BUS);
-               }
-               else {
-                       printk(BIOS_DEBUG, "ERROR - could not find PCI %02x:%02x.0, using defaults\n", m->bus_bcm5780[0], m->sbdn2+i-1);
-               }
-       }
-
+	/* bcm5780 */
+	for (i = 1; i < 7; i++) {
+		dev =
+		    dev_find_slot(m->bus_bcm5780[0],
+				  PCI_DEVFN(m->sbdn2 + i - 1, 0));
+		if (dev) {
+			m->bus_bcm5780[i] =
+			    pci_read_config8(dev, PCI_SECONDARY_BUS);
+		} else {
+			printk(BIOS_DEBUG,
+			       "ERROR - could not find PCI %02x:%02x.0, using defaults\n",
+			       m->bus_bcm5780[0], m->sbdn2 + i - 1);
+		}
+	}
 
 /*I/O APICs:   APIC ID Version State           Address*/
 #if CONFIG_LOGICAL_CPUS
-       apicid_base = get_apicid_base(3);
+	apicid_base = get_apicid_base(3);
 #else
-       apicid_base = CONFIG_MAX_PHYSICAL_CPUS;
+	apicid_base = CONFIG_MAX_PHYSICAL_CPUS;
 #endif
-       for(i=0;i<3;i++)
-               m->apicid_bcm5785[i] = apicid_base+i;
+	for (i = 0; i < 3; i++)
+		m->apicid_bcm5785[i] = apicid_base + i;
 }

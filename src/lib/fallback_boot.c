a258325ee3703fd17749fab9e8237b313c79ac10
Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>
2013-10-21 01:56:47 +0200

    Add a KEEP_BOOT_COUNT Kconfig option.
    The use case of that option is to inform coreboot (trough the nvram) at the
      next boot, that the computer could not fully boot to boot to an usable state.
      In that case, the boot count is incremented by one.
    
    Previously there was no way to tell coreboot that the computer really booted
      successfully, because it was assumed that if set_boot_successful was called
      in ramstage, then the computer would have booted successfully.
    
    However many things can go wrong after that point, for instance the payload
      could fail to boot, or the operating system's kernel could fail to boot too,
      due to the wrong configurations passed to it by coreboot and the payload.
    
    Change-Id: I01af053455eb6bd2f7a4f9d37e8c234ba8d55250
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>

--- src/lib/fallback_boot.c
+++ src/lib/fallback_boot.c
@@ -1,6 +1,8 @@
 #include <console/console.h>
 #include <fallback.h>
+#include <string.h>
 #include <watchdog.h>
+#include <arch/acpi.h>
 #include <arch/io.h>
 
 void boot_successful(void)
@@ -10,8 +12,16 @@ void boot_successful(void)
 
 	vbe_textmode_console();
 #endif
+
+#if CONFIG_KEEP_BOOT_COUNT
+	/* we don't want to resume with the wrong prefix next time */
+	if (acpi_slp_type == 3 &&
+	    strncmp(CONFIG_CBFS_PREFIX, "fallback", sizeof("fallback")))
+		set_boot_successful();
+#else
 	/* Remember this was a successful boot */
 	set_boot_successful();
+#endif
 
 	/* turn off the boot watchdog */
 	watchdog_off();

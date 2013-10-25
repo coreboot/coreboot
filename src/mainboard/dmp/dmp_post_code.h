8e400bfadde430449048a67b1a2e54cbb37ba461
Andrew Wu <arw@dmp.com.tw>
2013-10-25 16:22:57 +0800

    dmp/vortex86ex: Move DMP specific POST code defines into one file
    Move into src/mainboard/dmp/dmp_post_code.h
    
    Change-Id: If9f4d842f352eb41618e71f49a226d3cc4ad0b46
    Signed-off-by: Andrew Wu <arw@dmp.com.tw>

---
+++ src/mainboard/dmp/dmp_post_code.h
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 DMP Electronics Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef DMP_POST_CODE_H
+#define DMP_POST_CODE_H
+
+/* DMP Vortex86EX specific POST codes */
+
+#define POST_DMP_KBD_FW_UPLOAD			0x06
+#define POST_DMP_KBD_CHK_READY			0x07
+#define POST_DMP_KBD_IS_READY			0x08
+#define POST_DMP_KBD_FW_VERIFY_ERR		0x82
+#define POST_DMP_ID_ERR				0x85
+#define POST_DMP_DRAM_TEST_ERR			0x86
+#define POST_DMP_DRAM_SIZING_ERR		0x77
+
+#endif /* DMP_POST_CODE_H*/

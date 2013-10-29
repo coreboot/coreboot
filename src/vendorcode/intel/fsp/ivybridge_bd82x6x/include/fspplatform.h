98528fac6b7c3d9cd0f01e321d0f50f46aa73d42
Marc Jones <marc.jones@se-eng.com>
2013-10-29 17:57:30 -0600

    Add Intel FSP northbridge support Sandybridge and Ivybridge
    Add support for Sandybridge and Ivybridge using the Intel FSP.
    The FSP is different enough to warrant its own source files.
    This source handle the majority of FSP interaction.
    
    "Intel® Firmware Support Package (Intel® FSP) provides key
    programming information for initializing Intel® silicon and can be
    easily integrated into a boot loader of the developer’s choice.
    It is easy to adopt, scalable to design, reduces time-to-market, and
    is economical to build."
    http://www.intel.com/content/www/us/en/intelligent-systems/intel-firmware-support-package/intel-fsp-overview.html
    
    Change-Id: Ib879c6b0fbf2eb1cbf929a87f592df29ac48bcc5
    Signed-off-by: Marc Jones <marc.jones@se-eng.com>

---
+++ src/vendorcode/intel/fsp/ivybridge_bd82x6x/include/fspplatform.h
@@ -0,0 +1,64 @@
+/** @file
+
+Copyright (C) 2013, Intel Corporation
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+* Neither the name of Intel Corporation nor the names of its contributors may
+  be used to endorse or promote products derived from this software without
+  specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+  THE POSSIBILITY OF SUCH DAMAGE.
+
+**/
+
+#ifndef _FSP_PLATFORM_H_
+#define _FSP_PLATFORM_H_
+
+#include "fsptypes.h"
+#include "fspapi.h"
+#include "mem_config.h"
+
+#pragma pack(1)
+
+typedef struct {
+    MEM_CONFIG  *MemoryConfig;
+} FSP_INIT_RT_PLATFORM_BUFFER;
+
+typedef struct {
+    uint8_t  HTEnable;
+    uint8_t  TurboEnable;
+    uint8_t  MemoryDownEnable;
+    uint8_t  FastBootEnable;
+} PLATFORM_CONFIG;
+
+typedef struct {
+    const PLATFORM_CONFIG  *PlatformConfig;
+} FSP_INIT_RT_CONFIG_BUFFER;
+
+typedef struct {
+    FSP_INIT_RT_COMMON_BUFFER  Common;
+    FSP_INIT_RT_CONFIG_BUFFER  PlatformConfiguration;
+    FSP_INIT_RT_PLATFORM_BUFFER  Platform;
+} FSP_INIT_RT_BUFFER;
+
+#pragma pack()
+
+#endif
+

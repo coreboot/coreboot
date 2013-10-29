eb023718481e76bb34f08a74a476f0a61a6c9120
Marc Jones <marc.jones@se-eng.com>
2013-10-29 17:46:54 -0600

    Add FSP version on the Intel 206ax CPU core
    Add support for 206ax using the Intel FSP.
    The FSP is different enough to warrant its own source files
    for now. It has different CAR code, micorcode, and FSP inclusion.
    I may be possible to combine at some point with the mrc based
    solution used but the chromebooks.
    
    Change-Id: I5105631af34e9c3a804ace908c4205f073abb9b4
    Signed-off-by: Marc Jones <marc.jones@se-eng.com>

---
+++ src/cpu/intel/fsp_model_206ax/microcode_blob.h
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 Google Inc.
+ * Copyright (C) 2013 Sage Electronic Engineering, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#if IS_ENABLED(CONFIG_CPU_INTEL_FSP_MODEL_206AX)
+	/* Size is 0x2800 - Update in Kconfigs when any included file changes*/
+	#include <microcode-m12206a7_00000029.h>
+#endif
+
+#if IS_ENABLED(CONFIG_CPU_INTEL_FSP_MODEL_306AX)
+	/* Size is 0xC000 - Update in Kconfigs when any included file changes*/
+	#include <microcode-m12306a2_00000008.h>
+	#include <microcode-m12306a4_00000007.h>
+	#include <microcode-m12306a5_00000007.h>
+	#include <microcode-m12306a8_00000010.h>
+	#include <microcode-m12306a9_00000019.h>
+#endif

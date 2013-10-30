3ee7141374b44c1340c58d7d9efff8692a874aea
Marc Jones <marc.jones@se-eng.com>
2013-10-30 16:18:07 -0600

    sio1007: Properly build '.c' files
    Properly build the super i/o .c files.  This prevents including
    the .c file directly in romstage, which is generally bad practice.
    Adding a Makefile and a .h file to include.
    
    Change-Id: I0be66e94d3062a2c4a445cee2f12ec249598dc8b
    Signed-off-by: Marc Jones <marc.jones@se-eng.com>

---
+++ src/superio/smsc/sio1007/chip.h
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 Sage Electronic Engineering LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef SUPERIO_SMSC_1007_CHIP_H
+#define SUPERIO_SMSC_1007_CHIP_H
+
+void sio1007_setreg(u16 lpc_port, u8 reg, u8 value, u8 mask);
+int sio1007_enable_uart_at(u16 port);
+
+#endif

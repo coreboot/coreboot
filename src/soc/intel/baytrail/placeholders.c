59ecde27ef89302d1eb4858419b93713156d99c9
Aaron Durbin <adurbin@chromium.org>
2013-10-30 14:36:11 -0500

    baytrail: fix uninitialized acpi structures
    The callers of the following functions assume the storage
    area provided by the pointers is initialized. That's not the
    case as these were just place holders.
    - void acpi_create_intel_hpet(acpi_hpet_t * hpet);
    - void acpi_create_serialio_ssdt(acpi_header_t *ssdt);
    
    To fix this properly initialize the hpet entry, and just remove
    the serialio_ssdt function entirely.
    
    BUG=chrome-os-partner:23505
    BRANCH=None
    TEST=Built and booted through depthcharge on rambi. Noted no more
         ACPI errors relating to invalid length.
    
    Change-Id: If56ab033562ef2d755e9c9de42f507c95d291aba
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/174716
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/soc/intel/baytrail/placeholders.c
+++ src/soc/intel/baytrail/placeholders.c
@@ -7,10 +7,6 @@
 
 void generate_cpu_entries(void) {}
 
-void acpi_create_intel_hpet(acpi_hpet_t * hpet) {}
-
-void acpi_create_serialio_ssdt(acpi_header_t *ssdt) {}
-
 unsigned long acpi_fill_mcfg(unsigned long current) { return current; }
 
 void smm_init(void) {}

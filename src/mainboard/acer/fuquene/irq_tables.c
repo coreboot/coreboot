947f04d8fa297e28499d1a874620970a73eaa818
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2013-11-02 04:58:30 +1100

    Fix breakage from Acer Aspire initial support by stubbing out missing components (WIP).
    Change-Id: I431d901019b877b8a36e0ee6c0fc280c57235e8a
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

---
+++ src/mainboard/acer/fuquene/irq_tables.c
@@ -0,0 +1,1 @@
+void main(unsigned long bist) {}
---
+++ src/mainboard/acer/fuquene/romstage.c
@@ -0,0 +1,6 @@
+// FIXME
+
+void main(unsigned long bist) {}
+
+void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx);
+void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx) {}

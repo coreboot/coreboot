6aa9f1f0eb97e315ab4db8e6da1d13db7ee7858f
Duncan Laurie <dlaurie@chromium.org>
2013-11-07 12:47:35 -0800

    baytrail: Add BCLK and IACORE to pattrs
    The bus clock speed is needed when building ACPI P-state tables
    so extract that function and have the value be saved in pattrs.
    
    The various IACORE values are also needed, but rather than have
    the ACPI code to the bit manipulation have the pattrs store an
    array of the possible values for it to use directly.
    
    BUG=chrome-os-partner:23505
    BRANCH=none
    TEST=build and boot on rambi
    
    Change-Id: I5ac06ccf66e9109186dd01342dbb6ccdd334ca69
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/176140
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Aaron Durbin <adurbin@chromium.org>
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: http://review.coreboot.org/4953
    Tested-by: build bot (Jenkins)
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/soc/intel/baytrail/baytrail/pattrs.h
+++ src/soc/intel/baytrail/baytrail/pattrs.h
@@ -23,6 +23,14 @@
 #include <stdint.h>
 #include <cpu/x86/msr.h>
 
+enum {
+	IACORE_MIN,
+	IACORE_LFM,
+	IACORE_MAX,
+	IACORE_TURBO,
+	IACORE_END
+};
+
 /* The pattrs structure is a common place to stash pertinent information
  * about the processor or platform. Instead of going to the source (msrs, cpuid)
  * every time an attribute is needed use the pattrs structure.
@@ -30,14 +38,15 @@
 struct pattrs {
 	msr_t platform_id;
 	msr_t platform_info;
-	msr_t iacore_ratios;
-	msr_t iacore_vids;
+	int iacore_ratios[IACORE_END];
+	int iacore_vids[IACORE_END];
 	uint32_t cpuid;
 	int revid;
 	int stepping;
 	const void *microcode_patch;
 	int address_bits;
 	int num_cpus;
+	unsigned bclk_khz;
 };
 
 /* This is just to hide the abstraction w/o relying on how the underlying

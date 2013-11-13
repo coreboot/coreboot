afaaa3a618ea677f20c9db75d09e311db0197906
Aaron Durbin <adurbin@chromium.org>
2013-11-13 14:31:16 -0600

    rambi: fixup settings so trackpad can be found in kernel
    The kernel chromeos_laptop driver nomenclature expects the
    board name to not be in all caps. Fix this as well as the i2c
    address for the trackpad.
    
    BUG=chrome-os-partner:24307
    BRANCH=None
    TEST=Built and booted. trackpad device is found. IRQs still not
         working yet.
    
    Change-Id: Id6be8ee4bce2835e303ea4fe63944be80d2d7ec2
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/176680
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-on: http://review.coreboot.org/4970
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>

--- src/mainboard/google/rambi/onboard.h
+++ src/mainboard/google/rambi/onboard.h
@@ -25,13 +25,13 @@
 #define BOARD_TRACKPAD_NAME             "trackpad"
 #define BOARD_TRACKPAD_IRQ              GPIO_S0_DED_IRQ(TPAD_IRQ_OFFSET)
 #define BOARD_TRACKPAD_WAKE_GPIO        1       /* GPSSUS1 */
-#define BOARD_TRACKPAD_I2C_BUS          1       /* TODO(shawnn): Check this */
-#define BOARD_TRACKPAD_I2C_ADDR         0x67    /* TODO(shawnn): Check this */
+#define BOARD_TRACKPAD_I2C_BUS          0
+#define BOARD_TRACKPAD_I2C_ADDR         0x4b
 
 #define BOARD_TOUCHSCREEN_NAME          "touchscreen"
 #define BOARD_TOUCHSCREEN_IRQ           GPIO_S0_DED_IRQ(TOUCH_IRQ_OFFSET)
 #define BOARD_TOUCHSCREEN_WAKE_GPIO     2       /* GPSSUS2 */
-#define BOARD_TOUCHSCREEN_I2C_BUS       2       /* TODO(shawnn): Check this */
+#define BOARD_TOUCHSCREEN_I2C_BUS       5
 #define BOARD_TOUCHSCREEN_I2C_ADDR      0x4a    /* TODO(shawnn): Check this */
 
 #endif

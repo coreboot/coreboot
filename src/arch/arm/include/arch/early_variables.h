d84afd3e61af870e01b5be66530ea5df4469b0c8
Julius Werner <jwerner@chromium.org>
2013-11-13 16:06:34 -0800

    arm: Remove CAR_MIGRATE Kconfig and associated cruft
    This is essentially a revert of commit 10bd772d. The CAR_MIGRATE
    mechanism is only useful to migrate variables from a special region
    (e.g. cache as RAM) into DRAM-backed CBMEM between different parts of
    the romstage (it does not persist into ramstage). Since ARM devices use
    SRAM for which there is no reason to become inaccessible in later parts
    of the romstage, this mechanism isn't useful for them. Removing it makes
    the romstage.ld script much simpler, which has the nice side-effect of
    putting the BSS at the end of the memory image (so that cbfstool can
    actually figure out that it doesn't need to be part of the ROM image).
    
    Old-Change-Id: I50e91d8bd51b5deb19446d9da48699edecbef6ea
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/176761
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    (cherry picked from commit ebfd698e57c902e2f39a0cfc1bc2b02665e47ec6)
    
    console: Make cbmem depend on x86.
    
    The cbmem implementation isn't supported on anything other than x86 right now
    and actually causes memory corruption on ARM machines. Until that's fixed, this
    will prevent people from turning it on and causing hard to track down errors.
    
    Old-Change-Id: I00e8aacf008acfe2f76d4eab82570f7c1cc89cab
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/191107
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Gabe Black <gabeblack@chromium.org>
    Tested-by: Gabe Black <gabeblack@chromium.org>
    (cherry picked from commit e54f16e346a7f2c66d802fb78a6b24e53b732b83)
    
    Squashed two related commits for cbmem support on arm.
    
    Change-Id: I2be48cea348ee5dc8ca3632d743500aa111bab08
    Signed-off-by: Isaac Christensen <isaac.christensen@se-eng.com>
    Reviewed-on: http://review.coreboot.org/6888
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>
    Tested-by: build bot (Jenkins)

--- src/arch/arm/include/arch/early_variables.h
+++ src/arch/arm/include/arch/early_variables.h
@@ -20,40 +20,13 @@
 #ifndef ARCH_EARLY_VARIABLES_H
 #define ARCH_EARLY_VARIABLES_H
 
-#ifdef __PRE_RAM__
-#define CAR_GLOBAL __attribute__((section(".car.global_data")))
-#define CAR_CBMEM __attribute__((section(".car.cbmem_console")))
-#else
 #define CAR_GLOBAL
 #define CAR_CBMEM
-#endif
 
-#if defined(__PRE_RAM__)
-#define CAR_MIGRATE_ATTR __attribute__ ((used,section (".car.migrate")))
-
-/* Call migrate_fn_() when CAR globals are migrated. */
-#define CAR_MIGRATE(migrate_fn_) \
-	static void (* const migrate_fn_ ## _ptr)(void) CAR_MIGRATE_ATTR = \
-	migrate_fn_;
-
-/* Get the correct pointer for the CAR global variable. */
-void *car_get_var_ptr(void *var);
-
-/* Get and set a primitive type global variable. */
-#define car_get_var(var) \
-	*(typeof(var) *)car_get_var_ptr(&(var))
-#define car_set_var(var, val) \
-	do { car_get_var(var) = (val); } while(0)
-
-/* Migrate the CAR variables to memory. */
-void car_migrate_variables(void);
-
-#else
 #define CAR_MIGRATE(migrate_fn_)
 static inline void *car_get_var_ptr(void *var) { return var; }
 #define car_get_var(var) (var)
 #define car_set_var(var, val) do { (var) = (val); } while (0)
 static inline void car_migrate_variables(void) { }
-#endif
 
 #endif

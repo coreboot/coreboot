bb38cebbd460d88ffafea31eb54f3d68d5f659e7
Vladimir Serbinenko <phcoder@gmail.com>
2013-11-14 19:11:19 +0100

    Lenovo X201 support (mobo part)
    Lenovo X201 is a recent laptop by lenovo based on nehalem chipset and
    representing good value for both geeks and less-geeks alike. This commit
    allows to run it without factory BIOS.
    
    Change-Id: I4f82e7c33b899a165693d6fec4eeb932b2c627bd
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

---
+++ src/mainboard/lenovo/x201/dock.c
@@ -0,0 +1,62 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 Sven Schnelle <svens@stackframe.org>
+ * Copyright (C) 2013 Vladimir Serbinenko <phcoder@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#include <console/console.h>
+#include <arch/io.h>
+#include <device/device.h>
+#include <device/pci.h>
+#if !defined (__PRE_RAM__) && !defined (__SMM__)
+#define PCI_DEV(bus, dev, fn) dev_find_slot (bus, PCI_DEVFN (dev, fn))
+#endif
+#include <delay.h>
+#include "dock.h"
+#include "southbridge/intel/bd82x6x/pch.h"
+#include <ec/acpi/ec.h>
+
+int dock_connect(void)
+{
+	ec_set_bit(0x02, 0);
+	ec_set_bit(0x1a, 0);
+	ec_set_bit(0xfe, 4);
+	return 0;
+}
+
+void dock_disconnect(void)
+{
+	ec_clr_bit(0x02, 0);
+	ec_clr_bit(0x1a, 0);
+	ec_clr_bit(0xfe, 4);
+}
+
+int dock_present(void)
+{
+	u16 gpiobase = pci_read_config16(PCH_LPC_DEV, GPIO_BASE) & 0xfffc;
+	u8 st = inb(gpiobase + 0x0c);
+	printk(BIOS_DEBUG, "GPIO status is 0x%x\n", st);
+
+	return !((st >> 3) & 1);
+}
+
+int dock_ultrabay_device_present(void)
+{
+	return 0;
+}

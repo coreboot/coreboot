98a25d19ebe5f80a255b77f3bb57cc137e9382cc
Ronald G. Minnich <rminnich@google.com>
2013-11-25 10:02:51 -0800

    armv8: add support for armv8 cpu
    This is an emulation cpu.
    
    Lots of stuff missing, clearly, but I want to get the basic outline right.
    
    Change-Id: I31b912dc7d2f40f43490cf5c1ea836fcdfd48f2c
    Signed-off-by: Ronald G. Minnich <rminnich@google.com>

---
+++ src/cpu/armltd/armv8/cache.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2013 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <aarch64.h>
+
+/*
+ * Sets L2 cache related parameters before enabling data cache
+ */
+void v8_outer_cache_enable(void)
+{
+}
+
+/* stubs so we don't need weak symbols in cache_v8.c */
+void v8_outer_cache_disable(void)
+{
+}
+
+void v8_outer_cache_flush_all(void)
+{
+}
+
+void v8_outer_cache_inval_all(void)
+{
+}
+
+void v8_outer_cache_flush_range(u32 start, u32 end)
+{
+}
+
+void v8_outer_cache_inval_range(u32 start, u32 end)
+{
+}

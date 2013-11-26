a37383db8081b66eafc3860ede23edbfd39ac8ad
Vladimir Serbinenko <phcoder@gmail.com>
2013-11-26 02:41:26 +0100

    Replace all occurences of sprintf with snprintf
    THis reduces risks of bufer overflows.
    
    Change-Id: I77f80e76efec16ac0a0af83d76430a8126a7602d
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>
    Reviewed-on: http://review.coreboot.org/4279
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/mainboard/google/stout/i915.c
+++ src/mainboard/google/stout/i915.c
@@ -88,7 +88,7 @@ setgtt(int start, int end, unsigned long base, int inc)
 static char *regname(unsigned long addr)
 {
 	static char name[16];
-	sprintf(name, "0x%lx", addr);
+	snprintf(name, sizeof (name), "0x%lx", addr);
 	return name;
 }
 

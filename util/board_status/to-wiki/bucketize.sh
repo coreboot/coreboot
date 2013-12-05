50bc0a6baa180a82759cabf99b1d27313486d27f
Patrick Georgi <patrick@georgi-clan.de>
2013-12-05 18:11:33 +0100

    Add scripts to export board status data to wiki
    It's a start...
    
    Change-Id: Ibdb0b64ab0349df58bcad5ce553bf0dbec636925
    Signed-off-by: Patrick Georgi <patrick@georgi-clan.de>

---
+++ util/board_status/to-wiki/bucketize.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# usage: $0 [weekly|monthly|quarterly] < filenames
+#   sorts files of the form VENDOR/BOARD/COMMIT/DATE/revision.txt
+#   into buckets of the given granularity
+
+weekly() {
+	date --date="$1" +%YW%V
+}
+
+monthly() {
+	date --date="$1" +%Y-%m
+}
+
+quarterly() {
+	date --date="$1" "+%Y %m"  | awk '{ q=int(($2-1)/3+1); print $1 "Q" q}'
+}
+
+# TODO: restrict $1 to allowed values
+
+curr=""
+sort -r -k4 -t/ | while read file; do
+	timestamp=`printf $file | cut -d/ -f4`
+	new=`$1 $timestamp`
+	if [ "$new" != "$curr" ]; then
+		if [ "$curr" != "" ]; then
+			printf "\n"
+		fi
+		printf "$new:"
+		curr=$new
+	fi
+	printf "$file "
+done
+printf "\n"
+

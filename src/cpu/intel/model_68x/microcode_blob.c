0eea8e98b031e76f97408c1630f778520a963694
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2013-12-06 23:14:54 -0600

    cpu/intel: Make all Intel CPUs load microcode from CBFS
    The sequence to inject microcode updates is virtually the same for all
    Intel CPUs. The same function is used to inject the update in both CBFS
    and hardcoded cases, and in both of these cases, the microcode resides in
    the ROM. This should be a safe change across the board.
    
    The function which loaded compiled-in microcode is also removed here in
    order to prevent it from being used in the future.
    
    Change-Id: I2cc8220cc4cd4a87aa7fc750e6c60ccdfa9986e9
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

---
+++ src/cpu/intel/model_68x/microcode_blob.c
@@ -0,0 +1,25 @@
+unsigned microcode_updates[] = {
+	#include "microcode-534-MU16810d.h"
+	#include "microcode-535-MU16810e.h"
+	#include "microcode-536-MU16810f.h"
+	#include "microcode-537-MU268110.h"
+	#include "microcode-538-MU168111.h"
+	#include "microcode-550-MU168307.h"
+	#include "microcode-551-MU168308.h"
+	#include "microcode-727-MU168313.h"
+	#include "microcode-728-MU168314.h"
+	#include "microcode-729-MU268310.h"
+	#include "microcode-611-MU168607.h"
+	#include "microcode-612-MU168608.h"
+	#include "microcode-615-MU16860a.h"
+	#include "microcode-617-MU16860c.h"
+	#include "microcode-618-MU268602.h"
+	#include "microcode-662-MU168a01.h"
+	#include "microcode-691-MU168a04.h"
+	#include "microcode-692-MU168a05.h"
+	/*  Dummy terminator  */
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0,
+};
---
+++ src/cpu/intel/model_f3x/microcode_blob.c
@@ -0,0 +1,16 @@
+unsigned microcode_updates[] = {
+	/* WARNING - Intel has a new data structure that has variable length
+	 * microcode update lengths.  They are encoded in int 8 and 9.  A
+	 * dummy header of nulls must terminate the list.
+	 */
+
+#include "microcode-1290-m0df320a.h"
+#include "microcode-1467-m0df330c.h"
+#include "microcode-1468-m1df3417.h"
+
+	/*  Dummy terminator  */
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0,
+};

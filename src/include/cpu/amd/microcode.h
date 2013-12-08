f0a13ceb639f7a7d5a6e84a2c89f3deab0de757a
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-12-08 07:20:48 +0200

    AMD boards: Fix includes for microcode updates
    No ROMCC involved, no need to include .c files in romstage.c.
    
    Change-Id: I8a2aaf84276f2931d0a0557ba29e359fa06e2fba
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: http://review.coreboot.org/4501
    Tested-by: build bot (Jenkins)
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- src/include/cpu/amd/microcode.h
+++ src/include/cpu/amd/microcode.h
@@ -2,7 +2,12 @@
 #define CPU_AMD_MICROCODE_H
 
 void amd_update_microcode(void *microcode_updates, unsigned processor_rev_id);
-void update_microcode(u32 processor_rev_id);
 void model_fxx_update_microcode(unsigned cpu_deviceid);
+
+#if CONFIG_UPDATE_CPU_MICROCODE
+void update_microcode(u32 processor_rev_id);
+#else
+#define update_microcode(x)
+#endif
 #endif /* CPU_AMD_MICROCODE_H */
 

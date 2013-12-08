fda358486a11ed1ae1140dcc17c56eeba3a77e64
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-12-08 07:56:34 +0200

    cpu/amd (non-AGESA): Replace UPDATE_CPU_MICROCODE with SUPPORT_UCODE_IN_CBFS
    Change-Id: I71e5b19bf451cef857cad2e2bb4bd8cc19d0ddd0
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/include/cpu/amd/microcode.h
+++ src/include/cpu/amd/microcode.h
@@ -1,7 +1,7 @@
 #ifndef CPU_AMD_MICROCODE_H
 #define CPU_AMD_MICROCODE_H
 
-#if CONFIG_UPDATE_CPU_MICROCODE
+#if CONFIG_SUPPORT_CPU_UCODE_IN_CBFS
 void amd_update_microcode_from_cbfs(unsigned processor_rev_id);
 void update_microcode(u32 processor_rev_id);
 void model_fxx_update_microcode(unsigned cpu_deviceid);

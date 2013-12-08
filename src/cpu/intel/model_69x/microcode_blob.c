1ebd959647a835e5afc5dfc549f48caf4c619819
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2013-12-08 16:41:18 -0600

    cpu/intel: Remove dummy terminators from microcode blobs
    Now that CBFS microcode no longer requires a NULL termination, remove the
    dummy terminators from all microcode blobs. This also enables microcode
    blobs from different CPU models to be linked in the same
    cpu_microcode_blob.bin without the terminators getting in the way.
    
    Change-Id: I25a6454780fd5d56ae7660b0733ac4f8c4d90096
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- src/cpu/intel/model_69x/microcode_blob.c
+++ src/cpu/intel/model_69x/microcode_blob.c
@@ -4,10 +4,4 @@ unsigned microcode_updates_69x[] = {
 	#include "microcode-1376-m8069547.h"
 	#include "microcode-1373-m1069507.h"
 	#include "microcode-1374-m2069507.h"
-
-	/*  Dummy terminator  */
-        0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0,
 };
--- src/cpu/intel/model_f3x/microcode_blob.c
+++ src/cpu/intel/model_f3x/microcode_blob.c
@@ -1,16 +1,5 @@
 unsigned microcode_updates_f3x[] = {
-	/* WARNING - Intel has a new data structure that has variable length
-	 * microcode update lengths.  They are encoded in int 8 and 9.  A
-	 * dummy header of nulls must terminate the list.
-	 */
-
 #include "microcode-1290-m0df320a.h"
 #include "microcode-1467-m0df330c.h"
 #include "microcode-1468-m1df3417.h"
-
-	/*  Dummy terminator  */
-        0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0,
-        0x0, 0x0, 0x0, 0x0,
 };

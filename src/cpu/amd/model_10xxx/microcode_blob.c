d60911dbe03d1dfe6bc615ef9dfba9cca91288f5
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-12-11 13:03:04 +0200

    amd/model_10xxx: Drop AMD_UCODE_PATCH_FILE selection
    Include microcode updates in CBFS for every CPU revision the platform
    can support, as changing to different CPU revision should not require
    a coreboot rebuild.
    
    This increases CBFS usage from 2 kB to 14 kB.
    
    Change-Id: I6bf90221a688f1a54e49641ce3ba378c5bf659f9
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/cpu/amd/model_10xxx/microcode_blob.c
+++ src/cpu/amd/model_10xxx/microcode_blob.c
@@ -18,7 +18,13 @@
  */
 
 unsigned char microcode[] __attribute__ ((aligned(16))) = {
-#include CONFIG_AMD_UCODE_PATCH_FILE
+#include "mc_patch_01000086.h"
+#include "mc_patch_01000095.h"
+#include "mc_patch_01000096.h"
+#include "mc_patch_0100009f.h"
+#include "mc_patch_010000b6.h"
+#include "mc_patch_010000bf.h"
+#include "mc_patch_010000c4.h"
 
 	/*  Dummy terminator  */
         0x0, 0x0, 0x0, 0x0,

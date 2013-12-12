d0aa09a112190fe02505030c8583750e8f43ea26
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-12-12 06:50:21 +0200

    cpu/amd (non-AGESA): Fix CPU id_mapping_table
    In case a matching item is not in the array, the invalid use of
    sizeof will cause access beyond the end of array.
    
    Change-Id: I2be6d8f046d06702be21ed0155529c1bf41ede26
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/cpu/amd/model_10xxx/update_microcode.c
+++ src/cpu/amd/model_10xxx/update_microcode.c
@@ -41,38 +41,36 @@
  * 00100FA0h (PH-E0)     10A0h                  010000bfh
  */
 
-static u32 get_equivalent_processor_rev_id(u32 orig_id) {
-	static unsigned id_mapping_table[] = {
-		0x100f00, 0x1000,
-		0x100f01, 0x1000,
-		0x100f02, 0x1000,
-		0x100f20, 0x1020,
-		0x100f21, 0x1020,
-		0x100f2A, 0x1020,
-		0x100f22, 0x1022,
-		0x100f23, 0x1022,
-		0x100f42, 0x1041,
-		0x100f43, 0x1043,
-		0x100f62, 0x1062,
-		0x100f63, 0x1043,
-		0x100f81, 0x1081,
-		0x100fa0, 0x10A0,
-	};
+static const u32 id_mapping_table[] = {
+	0x100f00, 0x1000,
+	0x100f01, 0x1000,
+	0x100f02, 0x1000,
+	0x100f20, 0x1020,
+	0x100f21, 0x1020,
+	0x100f2A, 0x1020,
+	0x100f22, 0x1022,
+	0x100f23, 0x1022,
+	0x100f42, 0x1041,
+	0x100f43, 0x1043,
+	0x100f62, 0x1062,
+	0x100f63, 0x1043,
+	0x100f81, 0x1081,
+	0x100fa0, 0x10A0,
+};
+#define ID_MAPPING_TABLE_LEN (sizeof(id_mapping_table) / sizeof(id_mapping_table[0]))
 
-	u32 new_id;
+static unsigned get_equivalent_processor_rev_id(unsigned orig_id)
+{
+	unsigned new_id = 0;
 	int i;
 
-	new_id = 0;
-
-	for (i = 0; i < sizeof(id_mapping_table); i += 2 ) {
-		if(id_mapping_table[i]==orig_id) {
+	for (i = 0; i < ID_MAPPING_TABLE_LEN; i += 2) {
+		if (id_mapping_table[i]==orig_id) {
 			new_id = id_mapping_table[i + 1];
 			break;
 		}
 	}
-
 	return new_id;
-
 }
 
 void update_microcode(u32 cpu_deviceid)

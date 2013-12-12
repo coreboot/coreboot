3d78b2c8e557304f13d6e9e91d97fe626632c4a9
Kyösti Mälkki <kyosti.malkki@gmail.com>
2013-12-12 06:50:21 +0200

    cpu/amd (non-AGESA): Fix CPU id_mapping_table
    In case a matching item is not in the array, the invalid use of
    sizeof will causeaccess beyond the end of array.
    
    Change-Id: I2be6d8f046d06702be21ed0155529c1bf41ede26
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/cpu/amd/model_fxx/model_fxx_update_microcode.c
+++ src/cpu/amd/model_fxx/model_fxx_update_microcode.c
@@ -22,49 +22,43 @@
 #include <stdint.h>
 #include <cpu/amd/microcode.h>
 
-static unsigned get_equivalent_processor_rev_id(unsigned orig_id) {
-	static unsigned id_mapping_table[] = {
-	#if !CONFIG_K8_REV_F_SUPPORT
-	        0x0f48, 0x0048,
-	        0x0f58, 0x0048,
+static const unsigned id_mapping_table[] = {
+#if !CONFIG_K8_REV_F_SUPPORT
+        0x0f48, 0x0048,
+        0x0f58, 0x0048,
 
-	        0x0f4a, 0x004a,
-	        0x0f5a, 0x004a,
-	        0x0f7a, 0x004a,
-	        0x0f82, 0x004a,
-	        0x0fc0, 0x004a,
-	        0x0ff0, 0x004a,
+        0x0f4a, 0x004a,
+        0x0f5a, 0x004a,
+        0x0f7a, 0x004a,
+        0x0f82, 0x004a,
+        0x0fc0, 0x004a,
+        0x0ff0, 0x004a,
 
-	        0x10f50, 0x0150,
-	        0x10f70, 0x0150,
-	        0x10f80, 0x0150,
-	        0x10fc0, 0x0150,
-	        0x10ff0, 0x0150,
+        0x10f50, 0x0150,
+        0x10f70, 0x0150,
+        0x10f80, 0x0150,
+        0x10fc0, 0x0150,
+        0x10ff0, 0x0150,
 
-	        0x20f10, 0x0210,
-	        0x20f12, 0x0210,
-	        0x20f32, 0x0210,
-	        0x20fb1, 0x0210,
-	#endif
+        0x20f10, 0x0210,
+        0x20f12, 0x0210,
+        0x20f32, 0x0210,
+        0x20fb1, 0x0210,
+#endif
+};
+#define ID_MAPPING_TABLE_LEN (sizeof(id_mapping_table) / sizeof(id_mapping_table[0]))
 
-	#if CONFIG_K8_REV_F_SUPPORT
-
-	#endif
-
-	};
-
-	unsigned new_id;
+static unsigned get_equivalent_processor_rev_id(unsigned orig_id)
+{
+	unsigned new_id = 0;
 	int i;
 
-	new_id = 0;
-
-	for(i=0; i<sizeof(id_mapping_table); i+=2 ) {
-		if(id_mapping_table[i]==orig_id) {
-			new_id = id_mapping_table[i+1];
+	for (i = 0; i < ID_MAPPING_TABLE_LEN; i += 2) {
+		if (id_mapping_table[i]==orig_id) {
+			new_id = id_mapping_table[i + 1];
 			break;
 		}
 	}
-
 	return new_id;
 }
 

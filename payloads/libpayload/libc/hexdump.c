86545f7978eac21d925f1b0ce874ca03fbd24c5c
Ronald G. Minnich <rminnich@gmail.com>
2013-12-16 06:46:00 +0100

    libpayload: Add simple hexdump function
     - prints hex and ascii
     - detects duplicate all zero lines
    
    Change-Id: I084b3072bc05725b23c5c3ca0dbf1533f164a08c
    Signed-off-by: Stefan Reinauer <reinauer@google.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/63660
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Commit-Queue: Stefan Reinauer <reinauer@chromium.org>
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>
    Author:  Ronald G. Minnich <rminnich@gmail.com>
    Reviewed-on: http://review.coreboot.org/4393
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Tested-by: build bot (Jenkins)

---
+++ payloads/libpayload/libc/hexdump.c
@@ -0,0 +1,64 @@
+/*
+ * This file is part of the libpayload project.
+ *
+ * Copyright 2013 Google Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <libpayload.h>
+
+void hexdump(void *memory, int length)
+{
+	int i;
+	uint8_t *m;
+	int all_zero = 0;
+
+	m = (uint8_t *) memory;
+
+	for (i = 0; i < length; i += 16) {
+		int j;
+
+		all_zero++;
+		for (j = 0; j < 16; j++) {
+			if (m[i + j] != 0) {
+				all_zero = 0;
+				break;
+			}
+		}
+
+		if (all_zero < 2) {
+			printf("%08lx:", memory + i);
+			for (j = 0; j < 16; j++)
+				printf(" %02x", m[i + j]);
+			printf("  ");
+			for (j = 0; j < 16; j++)
+				printf("%c",
+				       isprint(m[i + j]) ? m[i + j] : '.');
+			printf("\n");
+		} else if (all_zero == 2) {
+			printf("...\n");
+		}
+	}
+}

de8c53a3317f114e3252d9aa4574a89204ea43eb
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2013-12-24 16:48:03 -0500

    cpu/allwinner/a10: Refactor API for gating clocks to peripherals
    Rather than having to track which bit in which register should be
    cleared or set to gate or ungate the clock to a certain peripheral,
    provide a simplified enum which encodes the register and bit. This
    change comes with a function which decodes the enum and gates/ungates
    the clock.
    
    This also removes the register-dependent bitmasks for APB0 and APB1
    gating registers.
    
    Change-Id: Ib3ca16e54eb37eadc3ceb88f4ccc497829ac34bc
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

---
+++ src/cpu/allwinner/a10/clock.c
@@ -0,0 +1,42 @@
+/*
+ * Helpers for clock control and gating on Allwinner CPUs
+ *
+ * Copyright (C) 2013  Alexandru Gagniuc <mr.nuke.me@gmail.com>
+ * Subject to the GNU GPL v2, or (at your option) any later version.
+ */
+
+#include "clock.h"
+
+#include <arch/io.h>
+
+/**
+ * \brief Enable the clock source for the peripheral
+ *
+ * @param[in] periph peripheral and clock type to enable @see a1x_clken
+ */
+void a1x_periph_clock_enable(enum a1x_clken periph)
+{
+	void *addr;
+	u32 reg32;
+
+	addr = (void *)A1X_CCM_BASE + (periph >> 5);
+	reg32 = read32(addr);
+	reg32 |= 1 << (periph & 0x1f);
+	write32(reg32, addr);
+}
+
+/**
+ * \brief Disable the clock source for the peripheral
+ *
+ * @param[in] periph peripheral and clock type to disable @see a1x_clken
+ */
+void a1x_periph_clock_disable(enum a1x_clken periph)
+{
+	void *addr;
+	u32 reg32;
+
+	addr = (void *)A1X_CCM_BASE + (periph >> 5);
+	reg32 = read32(addr);
+	reg32 &= ~(1 << (periph & 0x1f));
+	write32(reg32, addr);
+}

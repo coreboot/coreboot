6d51f5dfe91139928572a2e18722a049b5543b38
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-01-03 04:24:35 -0500

    cpu/allwinner/a10: Add minimal ramstage driver
    Change-Id: I857755976b17b0e492c086162f395a77933eeed8
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Reviewed-on: http://review.coreboot.org/4698
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ src/cpu/allwinner/a10/cbmem.c
@@ -0,0 +1,18 @@
+/*
+ * Provides cbmem utilities for romstage and ramstage
+ *
+ * Copyright (C) 2013 Alexandru Gagniuc <mr.nuke.me@gmail.com>
+ * Subject to the GNU GPL v2, or (at your option) any later version.
+ */
+
+#include "ram_segs.h"
+#include <cbmem.h>
+
+#if IS_ENABLED(CONFIG_DYNAMIC_CBMEM)
+
+void *cbmem_top(void)
+{
+	return a1x_get_cbmem_top();
+}
+
+#endif

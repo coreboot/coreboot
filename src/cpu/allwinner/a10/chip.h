1ff338c2816a21ff60e43c60526c222982937fe2
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-01-03 04:24:35 -0500

    cpu/allwinner/a10: Add minimal ramstage driver
    Change-Id: I857755976b17b0e492c086162f395a77933eeed8
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

---
+++ src/cpu/allwinner/a10/chip.h
@@ -0,0 +1,11 @@
+/*
+ * Allwinnwer A10 devicetree config struct
+ *
+ * Copyright (C) 2013  Alexandru Gagniuc <mr.nuke.me@gmail.com>
+ * Subject to the GNU GPL v2, or (at your option) any later version.
+ */
+
+#include <types.h>
+
+struct cpu_allwinner_a10_config {
+};

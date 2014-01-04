25aec295958292ceca73fbce446ccc05bc6f3c39
Vladimir Serbinenko <phcoder@gmail.com>
2014-01-05 00:51:12 +0100

    X230: current tree as-is.
    Known issues:
    - No keyboard backlight (thinklight works)
    - No S3
    - EHCI output sometimes get "stuck" until key is pressed after sysagent execution.
    - no S3
    - MRC fails if MRC cache is supplied. For now MRC cache is disabled.
    - MRC sometimes hangs with message:
    System Agent: Initializing PCH
    {433e0f9f-05ae-410a-a0c3-bf298ecb2
    
    So a watchdog is reenabled to reboot if this happens
    
    Not tested:
    - ultranav
    - fingerprint
    - dock
    
    Change-Id: Ida1374b8c7f99da7d2415b33c33ccc2c270a2282
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

---
+++ src/mainboard/lenovo/x230/ec.h
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 The Chromium OS Authors. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef BUTTERFLY_EC_H
+#define BUTTERFLY_EC_H
+
+#define EC_SCI_GPI  13   /* GPIO13 is EC_SCI# */
+
+/* EC SMI sources TODO: MLR- make defines */
+
+#ifndef __ACPI__
+extern void butterfly_ec_init(void);
+#endif
+
+#endif // BUTTERFLY_EC_H

ad66b3643b1b32717805067bc43ca74fbb1e8892
Vladimir Serbinenko <phcoder@gmail.com>
2014-01-05 06:40:36 +0100

    ROMSIZE: Add option for 12M chips.
    On X230 2 real chips (8 + 4) are merged into one virtual 12M chip.
    
    Change-Id: I04e676e4a0a5b074d8a45dbfa8a595629fd1b66d
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

--- src/mainboard/supermicro/h8scm/agesawrapper.c
+++ src/mainboard/supermicro/h8scm/agesawrapper.c
@@ -192,9 +192,9 @@ UINT32 agesawrapper_amdinitmmio(VOID)
 	LibAmdMsrWrite(0xC001001F, &MsrReg, &StdHeader);
 
 	/* Set ROM cache onto WP to decrease post time */
-	MsrReg = (0x0100000000 - CONFIG_ROM_SIZE) | 5;
+	MsrReg = (0x0100000000 - CONFIG_ALIGNED_ROM_SIZE) | 5;
 	LibAmdMsrWrite (0x20C, &MsrReg, &StdHeader);
-	MsrReg = ((1ULL << CONFIG_CPU_ADDR_BITS) - CONFIG_ROM_SIZE) | 0x800ull;
+	MsrReg = ((1ULL << CONFIG_CPU_ADDR_BITS) - CONFIG_ALIGNED_ROM_SIZE) | 0x800ull;
 	LibAmdMsrWrite(0x20D, &MsrReg, &StdHeader);
 
 	Status = AGESA_SUCCESS;

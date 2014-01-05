4eb07e70399097d0e795cbea49fde55bc8a35a22
Vladimir Serbinenko <phcoder@gmail.com>
2014-01-05 06:50:10 +0100

    acpi/ec: Add missing delays
    Without these delays on fast systems like X230 the port is read before it's
    updated.
    
    Change-Id: I3e01fc348cc5170cec108a05095ba301055ed6b0
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

--- src/ec/acpi/ec.c
+++ src/ec/acpi/ec.c
@@ -53,6 +53,8 @@ int send_ec_command(u8 command)
 		// return -1;
 	}
 
+	udelay(10);
+
 	outb(command, ec_cmd_reg);
 	return 0;
 }
@@ -72,6 +74,8 @@ int send_ec_data(u8 data)
 				data);
 		// return -1;
 	}
+
+	udelay(10);
 
 	outb(data, ec_data_reg);
 
@@ -103,6 +107,8 @@ u8 recv_ec_data(void)
 		printk(BIOS_DEBUG, "\nTimeout while receiving data from EC!\n");
 		// return -1;
 	}
+
+	udelay(10);
 
 	data = inb(ec_data_reg);
 	printk(BIOS_SPEW, "recv_ec_data: 0x%02x\n", data);

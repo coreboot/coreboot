5ae0c8228faab5d2294b97e34f99be2a4d381126
Shawn Nematbakhsh <shawnn@chromium.org>
2014-01-07 12:37:59 -0800

    rambi: Move KBD_IRQ pin for Rambi 2.0 board
    KBD_IRQ# is moved to GPIO SC101, with SC50 going back to its original
    SERIRQ function.
    
    Note that this change breaks Rambi 1.5 keyboard functionality.
    
    BUG=chrome-os-partner:24424
    TEST=Manual on Rambi 2.0. Verify KB functions in OS with SC50 / SERIRQ KB
    interrupt toggling removed from EC code.
    BRANCH=Rambi, Glimmer, Clapper
    
    Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Change-Id: I3fa40441741ea9d52a6e2ff15925570510b5b82b
    Reviewed-on: https://chromium-review.googlesource.com/181757
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/google/rambi/gpio.c
+++ src/mainboard/google/rambi/gpio.c
@@ -106,7 +106,7 @@ static const struct soc_gpio_map gpscore_gpio_map[] = {
 	GPIO_FUNC1,	/* S0-SC047 - PCLK_TPM */
 	GPIO_FUNC1,	/* S0-SC048 - CLK_PCI_EC */
 	GPIO_FUNC1,	/* S0-SC049 - LPC_CLKRUN_L */
-	GPIO_DIRQ,	/* S0-SC050 - IRQ_SERIRQ -- using for keyboard irq */
+	GPIO_FUNC(1, PULL_UP, 10K),	/* S0-SC050 - IRQ_SERIRQ */
 	GPIO_NC,	/* S0-SC051 - SMB_SOC_DATA (XDP) */
 	GPIO_NC,	/* S0-SC052 - SMB_SOC_CLK (XDP) */
 	GPIO_NC,	/* S0-SC053 - SMB_SOC_ALERTB (NC) */
@@ -157,7 +157,7 @@ static const struct soc_gpio_map gpscore_gpio_map[] = {
 	GPIO_NC,	/* S0-SC098 - NC */
 	GPIO_NC,	/* S0-SC099 - NC */
 	GPIO_NC,	/* S0-SC100 - NC */
-	GPIO_NC,	/* S0-SC101 - NC*/
+	GPIO_DIRQ,	/* S0-SC101 - KBD_IRQ# */
 	GPIO_END
 };
 

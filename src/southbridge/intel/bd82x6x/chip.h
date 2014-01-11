ca30b3e9e11b28ced7987e94e1f784ba5f85b366
Vladimir Serbinenko <phcoder@gmail.com>
2014-01-11 07:46:50 +0100

    ibexpeak / bd82x6x: Make SATA mode user-visible option.
    Ability to choose compatibility mode is interesting for testing payloads and
    OS for compatibility with older systems.
    
    As per comments
    "ide_legacy_combined # TODO: Does nothing since
    		      generations, remove from sb code?"
    The "combined" mode was removed. It wasn't used by any mobo and the code for
    it is almost identical to IDE one other than few bits relating to interrupt
    handling and ISA mode.
    
    Change-Id: I407a8fac753b513812a86bef5abcf39c6d81472e
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

--- src/southbridge/intel/bd82x6x/chip.h
+++ src/southbridge/intel/bd82x6x/chip.h
@@ -64,8 +64,6 @@ struct southbridge_intel_bd82x6x_config {
 	uint16_t alt_gp_smi_en;
 
 	/* IDE configuration */
-	uint32_t ide_legacy_combined;
-	uint32_t sata_ahci;
 	uint8_t sata_port_map;
 	uint32_t sata_port0_gen3_tx;
 	uint32_t sata_port1_gen3_tx;

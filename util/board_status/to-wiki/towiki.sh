a16649247e56af5ee4203235c2e5ad434a5ca25c
Vladimir Serbinenko <phcoder@gmail.com>
2014-01-18 13:11:23 +0100

    boardstatus: Take vendor name from src/mainboard/Kconfig.
    This way we get right capitalization and abbreviation expansion.
    
    Change-Id: I033a1da3529ef9d232eb48704ab5a7cf5c6536fe
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

--- util/board_status/to-wiki/towiki.sh
+++ util/board_status/to-wiki/towiki.sh
@@ -178,7 +178,11 @@ EOF
 	    fi
 	fi
 
-	vendor_nice="$(echo "$vendor" |sed -e "s/\(.\)/\u\1/g")";
+	vendor_nice="$(grep -A1 -i "config VENDOR_$vendor" "$COREBOOT_DIR"/src/mainboard/Kconfig|tail -n1|sed -n 's,^[[:space:]]*bool[[:space:]]*"\(.*\)"[[:space:]]*$,\1,p')"
+
+	if [ -z "$vendor_nice" ]; then
+	    vendor_nice="$(echo "$vendor" |sed -e "s/\(.\)/\u\1/g")";
+	fi
 	if [ -z "$board_nice" ]; then
 	    board_nice="$(echo "$board" |sed -e "s,_, ,g;s/\(.\)/\u\1/g")";
 	fi

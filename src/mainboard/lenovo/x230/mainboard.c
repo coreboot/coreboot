b2939f7486b80a3f7fc5fc7fd5723dd58984d31d
Vladimir Serbinenko <phcoder@gmail.com>
2014-01-22 17:12:35 +0100

    lenovo/x230: Add missing copyright line.
    Change-Id: I5ecd25e23cebf83d4ae9300307aaac527e05c377
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>
    Reviewed-on: http://review.coreboot.org/4778
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>

--- src/mainboard/lenovo/x230/mainboard.c
+++ src/mainboard/lenovo/x230/mainboard.c
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2007-2009 coresystems GmbH
  * Copyright (C) 2011-2012 Google Inc.
+ * Copyright (C) 2014 Vladimir Serbinenko
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

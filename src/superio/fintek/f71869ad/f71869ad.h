7976113521a1c4f5a603222a38ba0e96a0a47469
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2014-01-23 22:12:25 +1100

    Add initial support for Fintek F71869AD Super I/O.
    Data-sheet can be found here:
    http://www.fintek.com.tw/files/productfiles/F71869_V1.1.pdf
    
    Change-Id: I41f1ee20517dd179a4dee914ab7f6332739e326e
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

---
+++ src/superio/fintek/f71869ad/f71869ad.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef SUPERIO_FINTEK_F71869AD_F71869AD_H
+#define SUPERIO_FINTEK_F71869AD_F71869AD_H
+
+/* Logical Device Numbers (LDN). */
+#define F71869AD_FDC    0x00	/* Floppy */
+#define F71869AD_SP1    0x01	/* UART1 */
+#define F71869AD_SP2    0x02	/* UART2 */
+#define F71869AD_PP     0x03	/* Parallel port */
+#define F71869AD_HWM    0x04	/* Hardware monitor */
+#define F71869AD_KBC    0x05	/* PS/2 keyboard and mouse */
+#define F71869AD_GPIO   0x06	/* General Purpose I/O (GPIO) */
+#define F71869AD_BSEL   0x07	/* BSEL */
+#define F71869AD_PME    0x0a	/* Power Management Events (PME) and ACPI */
+
+#endif /* SUPERIO_FINTEK_F71869AD_F71869AD_H */
---
+++ src/superio/fintek/f71869ad/superio.c
@@ -0,0 +1,96 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <arch/io.h>
+#include <device/device.h>
+#include <device/pnp.h>
+#include <superio/conf_mode.h>
+#include <console/console.h>
+#include <stdlib.h>
+#include <uart8250.h>
+
+#include "chip.h"
+#include "f71869ad.h"
+
+static void f71869ad_init(device_t dev)
+{
+	struct superio_fintek_f71869ad_config *conf = dev->chip_info;
+	struct resource *res0;
+
+	if (!dev->enabled)
+		return;
+
+	switch(dev->path.pnp.device) {
+	/* TODO: Might potentially need code for HWM or FDC etc. */
+	case F71869AD_KBC:
+		res0 = find_resource(dev, PNP_IDX_IO0);
+		pc_keyboard_init(&conf->keyboard);
+		break;
+	}
+}
+
+static struct device_operations ops = {
+	.read_resources   = pnp_read_resources,
+	.set_resources    = pnp_set_resources,
+	.enable_resources = pnp_enable_resources,
+	.enable           = pnp_alt_enable,
+	.init             = f71869ad_init,
+	.ops_pnp_mode     = &pnp_conf_mode_8787_aa,
+};
+
+/*
+ * io_info contains the mask 0x07f8. Given 16 register, each 8 bits wide of a
+ * logical we device we need a mask of the following form:
+ *
+ *  MSB                 LSB
+ *    v                 v
+ * 0x[15..11][10..3][2..0]
+ *    ------  ^^^^^  ^^^^
+ *    null      |      |
+ *              |      +------ Register index
+ *              |
+ *              +------------- Comparses agients base address and
+ *                             asserts a chip_select on match.
+ *
+ *  i.e., 0x07F8 = [00000][11111111][000]
+ *
+ * XXX: verify flags and masks are correct.
+ */
+static struct pnp_info pnp_dev_info[] = {
+	{ &ops, F71869AD_FDC,  PNP_IO0 | PNP_IRQ0 | PNP_DRQ0, {0x07f8, 0}, },
+	{ &ops, F71869AD_SP1,  PNP_IO0 | PNP_IRQ0, {0x07f8, 0}, },
+	{ &ops, F71869AD_SP2,  PNP_IO0 | PNP_IRQ0, {0x07f8, 0}, },
+	{ &ops, F71869AD_PP,   PNP_IO0 | PNP_IRQ0 | PNP_DRQ0, {0x07f8, 0}, },
+	{ &ops, F71869AD_HWM,  PNP_IO0 | PNP_IRQ0, {0x0ff8, 0}, },
+	{ &ops, F71869AD_KBC,  PNP_IO0 | PNP_IRQ0 | PNP_IRQ1, {0x07ff, 0}, },
+	{ &ops, F71869AD_GPIO, },
+	{ &ops, F71869AD_BSEL,  PNP_IO0, {0x07f8, 0}, },
+	{ &ops, F71869AD_PME, },
+};
+
+static void enable_dev(device_t dev)
+{
+	pnp_enable_devices(dev, &ops, ARRAY_SIZE(pnp_dev_info), pnp_dev_info);
+}
+
+struct chip_operations superio_fintek_f71869ad_ops = {
+	CHIP_NAME("Fintek F71869AD Super I/O")
+	.enable_dev = enable_dev
+};

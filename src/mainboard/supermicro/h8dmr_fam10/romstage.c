4549e5a6650b4d4634a46285796e63e31c99f9c8
Paul Menzel <paulepanter@users.sourceforge.net>
2014-02-02 22:05:48 +0100

    AMD K8 boards’ `romstage.c`: Spell sync*hr*onize correctly
    Change-Id: I92e6e7f1292f66642aa0336064a4eccba104dd08
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-on: http://review.coreboot.org/5101
    Reviewed-by: Rudolf Marek <r.marek@assembler.cz>
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Tested-by: build bot (Jenkins)

--- src/mainboard/supermicro/h8dmr_fam10/romstage.c
+++ src/mainboard/supermicro/h8dmr_fam10/romstage.c
@@ -199,7 +199,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	       msr.hi, msr.lo);
 #endif
 
-	init_timer(); // Need to use TMICT to synconize FID/VID
+	init_timer(); // Need to use TMICT to synchronize FID/VID
 
 	wants_reset = mcp55_early_setup_x();
 

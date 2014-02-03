c79dfdb9910ba368e3473f758c84b0f24064833f
Kyösti Mälkki <kyosti.malkki@gmail.com>
2014-02-03 19:43:08 +0200

    console: Drop IO and Oxford (PCI) UARTs on armv7
    Change-Id: Ia410b61c4babdfa3c984539527a9739462d3ad80
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: http://review.coreboot.org/5141
    Tested-by: build bot (Jenkins)
    Reviewed-by: David Hendricks <dhendrix@chromium.org>

--- src/arch/armv7/early_console.c
+++ src/arch/armv7/early_console.c
@@ -26,20 +26,8 @@ void console_tx_byte(unsigned char byte)
 	if (byte == '\n')
 		console_tx_byte('\r');
 
-#if CONFIG_CONSOLE_SERIAL8250MEM
-	if (oxford_oxpcie_present) {
-		uart8250_mem_tx_byte(
-			CONFIG_OXFORD_OXPCIE_BASE_ADDRESS + 0x1000, byte);
-	}
-#endif
-#if CONFIG_CONSOLE_SERIAL8250
-	uart8250_tx_byte(CONFIG_TTYS0_BASE, byte);
-#endif
 #if CONFIG_CONSOLE_SERIAL_UART
 	uart_tx_byte(byte);
-#endif
-#if CONFIG_USBDEBUG
-	usbdebug_tx_byte(0, byte);
 #endif
 #if CONFIG_CONSOLE_CBMEM && !defined(__BOOT_BLOCK__)
 	cbmemc_tx_byte(byte);
@@ -48,17 +36,8 @@ void console_tx_byte(unsigned char byte)
 
 static void _console_tx_flush(void)
 {
-#if CONFIG_CONSOLE_SERIAL8250MEM
-	uart8250_mem_tx_flush(CONFIG_OXFORD_OXPCIE_BASE_ADDRESS + 0x1000);
-#endif
-#if CONFIG_CONSOLE_SERIAL8250
-	uart8250_tx_flush(CONFIG_TTYS0_BASE);
-#endif
 #if CONFIG_CONSOLE_SERIAL_UART
 	uart_tx_flush();
-#endif
-#if CONFIG_USBDEBUG
-	usbdebug_tx_flush(0);
 #endif
 }
 

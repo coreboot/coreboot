6935d3aad1075397248477a6c16ed0a2d7d80947
Kyösti Mälkki <kyosti.malkki@gmail.com>
2014-02-03 17:04:22 +0200

    console: Drop driver list in ramstage
    This framework was only available in ramstage. So we had to define
    console output functions separately for bootblock, romstage and SMM.
    Follow-up patches will re-enable all the consoles removed here,
    in a more flexible fashion, and with less lines-of-code and copy-paste.
    
    Also the driver list is not in a well-defined order and some of the
    loops could exit without visiting all drivers.
    
    NOTE: This build has no console in ramstage.
    
    Change-Id: Iaddc495aaca37e2a6c2c3f802a0dba27bf227a3e
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/include/console/uart.h
+++ src/include/console/uart.h
@@ -43,7 +43,6 @@ void uart_init(void);
 void uart_tx_byte(unsigned char data);
 void uart_tx_flush(void);
 unsigned char uart_rx_byte(void);
-int uart_can_rx_byte(void);
 
 unsigned int uart_platform_base(int idx);
 

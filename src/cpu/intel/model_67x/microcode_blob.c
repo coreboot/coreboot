27717d787d6431957efb6407687fd7dc1d6f9be8
Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>
2014-02-03 11:27:41 +0100

    Remove non-free intel microcode
    TODO: probably too much stuff was removed, fix that.
    
    Change-Id: I905c2a897c38e3aed3c2cfbd923ac34b8ee2daee
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>

--- src/cpu/intel/model_67x/microcode_blob.c
+++
@@ -1,8 +0,0 @@
-unsigned microcode_updates_67x[] = {
-	/* Include microcode updates here. */
-	#include "microcode-293-MU267114.h"
-	#include "microcode-530-MU16730e.h"
-	#include "microcode-531-MU26732e.h"
-	#include "microcode-539-MU167210.h"
-	#include "microcode-540-MU267238.h"
-};
--- src/cpu/intel/model_6ex/microcode_blob.c
+++
@@ -1,5 +0,0 @@
-unsigned microcode_updates_6ex[] = {
-	#include "microcode-1624-m206e839.h"
-	#include "microcode-1729-m206ec54.h"
-	#include "microcode-1869-m806ec59.h"
-};

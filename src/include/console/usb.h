a0a9e1998f39b0e155d70a87b0a70c34c26c6e67
Kyösti Mälkki <kyosti.malkki@gmail.com>
2014-02-10 00:00:44 +0200

    usbdebug: Unify console API
    Struct dbgp_pipe would not be suitable for use with xHCI.
    Just use an index, it is easy to setup in Kconfig if our
    future debug setup has separate pipes for console
    output and debugging/traceings.
    
    Change-Id: Icbbd28f03113b208016f80217ab801d598d443a8
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/include/console/usb.h
+++ src/include/console/usb.h
@@ -21,17 +21,11 @@
 #ifndef _CONSOLE_USB_H_
 #define _CONSOLE_USB_H_
 
-struct dbgp_pipe;
-
 int usbdebug_init(void);
 
-struct dbgp_pipe *dbgp_console_output(void);
-struct dbgp_pipe *dbgp_console_input(void);
-int dbgp_ep_is_active(struct dbgp_pipe *pipe);
-int dbgp_bulk_write_x(struct dbgp_pipe *pipe, const char *bytes, int size);
-int dbgp_bulk_read_x(struct dbgp_pipe *pipe, void *data, int size);
-void usbdebug_tx_byte(struct dbgp_pipe *pipe, unsigned char data);
-void usbdebug_tx_flush(struct dbgp_pipe *pipe);
-unsigned char usbdebug_rx_byte(struct dbgp_pipe *pipe);
+void usb_tx_byte(int idx, unsigned char data);
+void usb_tx_flush(int idx);
+unsigned char usb_rx_byte(int idx);
+int usb_can_rx_byte(int idx);
 
 #endif /* _CONSOLE_USB_H_ */

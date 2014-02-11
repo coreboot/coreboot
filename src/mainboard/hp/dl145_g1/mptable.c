8a41a2e28ff0f94fdc0fd20d3f1a483b78bb9138
Oskar Enoksson <enok@lysator.liu.se>
2014-02-11 22:19:35 +0100

    hp/dl145_g1: Add missing copyright notes
    Missing copyright notes added.
    
    Change-Id: I55b320a169b1125017c63b7a2384078465e7ce6e
    Signed-off-by: Oskar Enoksson <enok@lysator.liu.se>

--- src/mainboard/hp/dl145_g1/mptable.c
+++ src/mainboard/hp/dl145_g1/mptable.c
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2011,2014 Oskar Enoksson <enok@lysator.liu.se>
+ * Subject to the GNU GPL v2, or (at your option) any later version.
+ */
 #include <console/console.h>
 #include <arch/smp/mpspec.h>
 #include <arch/ioapic.h>

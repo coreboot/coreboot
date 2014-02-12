3d24edab2c2b5228277a96b8763d061343e3da00
Mohammed Habibulla <moch@chromium.org>
2014-02-12 10:07:34 -0800

    google/panther: Add new thermal values
    Based on latest thermal report
    
    BUG=chrome-os-partner:24532
    TEST=boot tested on panther
    BRANCH=panther
    
    Change-Id: I4b8639f926fc3cf57eb5329818b9b912bfbe222d
    Signed-off-by: Mohammed Habibulla <moch@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/186113
    Reviewed-by: Stefan Reinauer <reinauer@chromium.org>
    Reviewed-on: http://review.coreboot.org/6004
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>

--- src/mainboard/google/panther/thermal.h
+++ src/mainboard/google/panther/thermal.h
@@ -28,21 +28,21 @@
 /* Fan is at LOW speed */
 #define FAN3_THRESHOLD_OFF	40
 #define FAN3_THRESHOLD_ON	50
-#define FAN3_PWM		0x6b
+#define FAN3_PWM		0x5a
 
 /* Fan is at MEDIUM speed */
-#define FAN2_THRESHOLD_OFF	75
-#define FAN2_THRESHOLD_ON	83
-#define FAN2_PWM		0xcc
+#define FAN2_THRESHOLD_OFF	58
+#define FAN2_THRESHOLD_ON	67
+#define FAN2_PWM		0xa6
 
 /* Fan is at HIGH speed */
-#define FAN1_THRESHOLD_OFF	86
-#define FAN1_THRESHOLD_ON	90
-#define FAN1_PWM		0xe5
+#define FAN1_THRESHOLD_OFF	67
+#define FAN1_THRESHOLD_ON	75
+#define FAN1_PWM		0xc0
 
 /* Fan is at FULL speed */
-#define FAN0_THRESHOLD_OFF	93
-#define FAN0_THRESHOLD_ON	96
+#define FAN0_THRESHOLD_OFF	85
+#define FAN0_THRESHOLD_ON	90
 #define FAN0_PWM		0xff
 
 /* Temperature which OS will shutdown at */

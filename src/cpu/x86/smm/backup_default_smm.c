dad6d794d27bbecf4e46499a38543a481050d2d1
Aaron Durbin <adurbin@chromium.org>
2014-02-13 10:26:18 -0600

    x86: provide infrastructure to backup default SMM region
    Certain CPUs require the default SMM region to be backed up
    on resume after a suspend. The reason is that in order to
    relocate the SMM region the default SMM region has to be used.
    As coreboot is unaware of how that memory is used it needs to
    be backed up. Therefore provide a common method for doing this.
    
    Change-Id: I65fe1317dc0b2203cb29118564fdba995770ffea
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/cpu/x86/smm/backup_default_smm.c
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2014 Google Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <string.h>
+#include <arch/acpi.h>
+#include <console/console.h>
+#include <cbmem.h>
+#include <cpu/x86/smm.h>
+
+void *backup_default_smm_area(void)
+{
+	void *save_area;
+	const void *default_smm = (void *)SMM_DEFAULT_BASE;
+
+	if (!IS_ENABLED(CONFIG_HAVE_ACPI_RESUME))
+		return NULL;
+
+	/*
+	 * The buffer needs to be preallocated regardless. In the non-resume
+	 * path it will be allocated for handling resume. Note that cbmem_add()
+	 * does a find before the addition.
+	 */
+	save_area = cbmem_add(CBMEM_ID_SMM_SAVE_SPACE, SMM_DEFAULT_SIZE);
+
+	if (save_area == NULL) {
+		printk(BIOS_DEBUG, "SMM save area not added.\n");
+		return NULL;
+	}
+
+	/* Only back up the area on S3 resume. */
+	if (acpi_slp_type == 3) {
+		memcpy(save_area, default_smm, SMM_DEFAULT_SIZE);
+		return save_area;
+	}
+
+	/*
+	 * Not the S3 resume path. No need to restore memory contents after
+	 * SMM relocation.
+	 */
+	return NULL;
+}
+
+void restore_default_smm_area(void *smm_save_area)
+{
+	void *default_smm = (void *)SMM_DEFAULT_BASE;
+
+	if (smm_save_area == NULL)
+		return;
+
+	memcpy(default_smm, smm_save_area, SMM_DEFAULT_SIZE);
+}

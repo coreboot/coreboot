9c538a7fba5bc5f2b13169c99465bd6ac77d7c01
Chris Douglass <cdouglass.orion@gmail.com>
2014-02-14 13:51:26 -0500

    add support for adesto SPI flash parts
    This patch adds support for the following Adesto Technologies
    SPI Flash parts.
    
    AT25DF081
    AT25DF321
    AT25DF641
    
    It has been tested on an Orion VPX7654 board populated
    with an AT25DF321A part. The "08" and "64" densities have not
    been tested.
    
    These parts are the successors of the Ateml AT26DF line that
    was spun out or purchased by Adesto.
    
    In this patch, adesto.c is identical to winbond.c with part
    entries for the Adesto parts. The datasheet for the AT25DF parts
    includes a "100MHz" programming command in addition to the "85MHz"
    command that is currently used but this patch does not add support
    for that enhanced programming mode.
    
    Change-Id: If82d075fd9000030480c412c645dcae2c8bb7439
    Signed-off-by: Christopher Douglass <cdouglass.orion@gmail.com>

---
+++ src/drivers/spi/adesto.c
@@ -0,0 +1,198 @@
+/*
+ * adesto.c
+ * Driver for Adesto Technologies SPI flash
+ * Copyright 2014 Orion Technologies, LLC
+ * Author: Chris Douglass <cdouglass <at> oriontechnologies.com>
+ *
+ * based on adesto.c
+ *
+ * Copyright 2008, Network Appliance Inc.
+ * Author: Jason McMullan <mcmullan <at> netapp.com>
+ * Licensed under the GPL-2 or later.
+ */
+
+#include <stdlib.h>
+#include <spi_flash.h>
+#include "spi_flash_internal.h"
+
+/* at25dfxx-specific commands */
+#define CMD_AT25DF_WREN		0x06	/* Write Enable */
+#define CMD_AT25DF_WRDI		0x04	/* Write Disable */
+#define CMD_AT25DF_RDSR		0x05	/* Read Status Register */
+#define CMD_AT25DF_WRSR		0x01	/* Write Status Register */
+#define CMD_AT25DF_READ		0x03	/* Read Data Bytes */
+#define CMD_AT25DF_FAST_READ	0x0b	/* Read Data Bytes at Higher Speed */
+#define CMD_AT25DF_PP		0x02	/* Page Program */
+#define CMD_AT25DF_SE		0x20	/* Sector (4K) Erase */
+#define CMD_AT25DF_BE		0xd8	/* Block (64K) Erase */
+#define CMD_AT25DF_CE		0xc7	/* Chip Erase */
+#define CMD_AT25DF_DP		0xb9	/* Deep Power-down */
+#define CMD_AT25DF_RES		0xab	/* Release from DP, and Read Signature */
+
+struct adesto_spi_flash_params {
+	uint16_t	id;
+	/* Log2 of page size in power-of-two mode */
+	uint8_t		l2_page_size;
+	uint16_t	pages_per_sector;
+	uint16_t	sectors_per_block;
+	uint16_t	nr_blocks;
+	const char	*name;
+};
+
+/* spi_flash needs to be first so upper layers can free() it */
+struct adesto_spi_flash {
+	struct spi_flash flash;
+	const struct adesto_spi_flash_params *params;
+};
+
+static inline struct adesto_spi_flash *
+to_adesto_spi_flash(struct spi_flash *flash)
+{
+	return container_of(flash, struct adesto_spi_flash, flash);
+}
+
+static const struct adesto_spi_flash_params adesto_spi_flash_table[] = {
+	{
+		.id			= 0x4501,
+		.l2_page_size		= 8,
+		.pages_per_sector	= 16,
+		.sectors_per_block	= 16,
+		.nr_blocks		= 16,
+		.name			= "AT25DF081",
+	},
+	{
+		.id			= 0x4701,
+		.l2_page_size		= 8,
+		.pages_per_sector	= 16,
+		.sectors_per_block	= 16,
+		.nr_blocks		= 64,
+		.name			= "AT25DF321",
+	},
+	{
+		.id			= 0x4800,
+		.l2_page_size		= 8,
+		.pages_per_sector	= 16,
+		.sectors_per_block	= 16,
+		.nr_blocks		= 128,
+		.name			= "AT25DF641",
+	},
+};
+
+static int adesto_write(struct spi_flash *flash,
+		u32 offset, size_t len, const void *buf)
+{
+	struct adesto_spi_flash *stm = to_adesto_spi_flash(flash);
+	unsigned long byte_addr;
+	unsigned long page_size;
+	size_t chunk_len;
+	size_t actual;
+	int ret;
+	u8 cmd[4];
+
+	page_size = min(1 << stm->params->l2_page_size, CONTROLLER_PAGE_LIMIT);
+	byte_addr = offset % page_size;
+
+	flash->spi->rw = SPI_WRITE_FLAG;
+	ret = spi_claim_bus(flash->spi);
+	if (ret) {
+		printk(BIOS_WARNING, "SF: Unable to claim SPI bus\n");
+		return ret;
+	}
+
+	for (actual = 0; actual < len; actual += chunk_len) {
+		chunk_len = min(len - actual, page_size - byte_addr);
+
+		cmd[0] = CMD_AT25DF_PP;
+		cmd[1] = (offset >> 16) & 0xff;
+		cmd[2] = (offset >> 8) & 0xff;
+		cmd[3] = offset & 0xff;
+#if CONFIG_DEBUG_SPI_FLASH
+		printk(BIOS_SPEW, "PP: 0x%p => cmd = { 0x%02x 0x%02x%02x%02x }"
+		        " chunk_len = %zu\n", buf + actual,
+			cmd[0], cmd[1], cmd[2], cmd[3], chunk_len);
+#endif
+
+		ret = spi_flash_cmd(flash->spi, CMD_AT25DF_WREN, NULL, 0);
+		if (ret < 0) {
+			printk(BIOS_WARNING, "SF: Enabling Write failed\n");
+			goto out;
+		}
+
+		ret = spi_flash_cmd_write(flash->spi, cmd, 4,
+				buf + actual, chunk_len);
+		if (ret < 0) {
+			printk(BIOS_WARNING, "SF: adesto Page Program failed\n");
+			goto out;
+		}
+
+		ret = spi_flash_cmd_wait_ready(flash, SPI_FLASH_PROG_TIMEOUT);
+		if (ret)
+			goto out;
+
+		offset += chunk_len;
+		byte_addr = 0;
+	}
+
+#if CONFIG_DEBUG_SPI_FLASH
+	printk(BIOS_SPEW, "SF: adesto: Successfully programmed %zu bytes @"
+			" 0x%lx\n", len, (unsigned long)(offset - len));
+#endif
+	ret = 0;
+
+out:
+	spi_release_bus(flash->spi);
+	return ret;
+}
+
+static int adesto_erase(struct spi_flash *flash, u32 offset, size_t len)
+{
+	return spi_flash_cmd_erase(flash, CMD_AT25DF_SE, offset, len);
+}
+
+struct spi_flash *spi_flash_probe_adesto(struct spi_slave *spi, u8 *idcode)
+{
+	const struct adesto_spi_flash_params *params;
+	unsigned page_size;
+	struct adesto_spi_flash *stm;
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(adesto_spi_flash_table); i++) {
+		params = &adesto_spi_flash_table[i];
+		if (params->id == ((idcode[1] << 8) | idcode[2]))
+			break;
+	}
+
+	if (i == ARRAY_SIZE(adesto_spi_flash_table)) {
+		printk(BIOS_WARNING, "SF: Unsupported adesto ID %02x%02x\n",
+				idcode[1], idcode[2]);
+		return NULL;
+	}
+
+	stm = malloc(sizeof(struct adesto_spi_flash));
+	if (!stm) {
+		printk(BIOS_WARNING, "SF: Failed to allocate memory\n");
+		return NULL;
+	}
+
+	stm->params = params;
+	stm->flash.spi = spi;
+	stm->flash.name = params->name;
+
+	/* Assuming power-of-two page size initially. */
+	page_size = 1 << params->l2_page_size;
+
+	stm->flash.write = adesto_write;
+	stm->flash.erase = adesto_erase;
+#if CONFIG_SPI_FLASH_NO_FAST_READ
+	stm->flash.read = spi_flash_cmd_read_slow;
+#else
+	stm->flash.read = spi_flash_cmd_read_fast;
+#endif
+	stm->flash.sector_size = (1 << stm->params->l2_page_size) *
+		stm->params->pages_per_sector;
+	stm->flash.size = page_size * params->pages_per_sector
+				* params->sectors_per_block
+				* params->nr_blocks;
+
+	return &stm->flash;
+}

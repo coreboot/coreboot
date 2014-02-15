8e28db92229bb4ef3bba2a6681f3893f9e6d7532
Vladimir Serbinenko <phcoder@gmail.com>
2014-02-15 17:00:46 +0100

    lenovo/x60: Support digitizer on X60t and X201t.
    Change-Id: I5b0399a8edca3b73aa7d515d2c446c31b3239fa5
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

--- src/odm_oem/lenovo/lenovo.h
+++ src/odm_oem/lenovo/lenovo.h
@@ -1,1 +1,4 @@
 const char *lenovo_mainboard_partnumber(void);
+int drivers_lenovo_is_wacom_present(void);
+void drivers_lenovo_serial_ports_ssdt_generate(const char *scope,
+					       int have_dock_serial);

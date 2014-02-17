6dd34b07b51ceea1036a5a29a2d7cf10222fc035
Kyösti Mälkki <kyosti.malkki@gmail.com>
2014-02-17 19:37:52 +0200

    uart8250: Fix and unify baudrate divisor calculation
    Divisor is a function of requested baudrate, platform-specific
    reference clock and amount of oversampling done on the UART reference.
    Calculate this parameter with divisor rounded to nearest integer.
    
    When building without option_table or when there is no entry for
    baud_rate, CONFIG_TTYS0_BAUD is used for default baudrate.
    
    For OxPCIe use of 4 MHz for reference was arbitrary giving correct
    divisor for 115200 but somewhat inaccurate for lower baudrates.
    Actual hardware is 62500000 with 16 times oversampling.
    
    FIXME: Field for baudrate in lb_tables is still incorrect.
    
    Change-Id: I68539738469af780fadd3392263dd9b3d5964d2d
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/drivers/uart/util.c
+++ src/drivers/uart/util.c
@@ -1,8 +1,6 @@
 /*
  * This file is part of the coreboot project.
  *
- * Copyright (C) 2012 The ChromiumOS Authors.  All rights reserved.
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 2 of the License.
@@ -17,31 +15,34 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#ifndef UART_H
-#define UART_H
+#include <console/console.h>
+#include <uart.h>
+#if CONFIG_USE_OPTION_TABLE
+#include <pc80/mc146818rtc.h>
+#include "option_table.h"
+#endif
 
-/* Return the clock frequency UART uses as reference clock for
- * baudrate generator. */
-unsigned int uart_platform_refclk(void);
+unsigned int default_baudrate(void)
+{
+#if !defined(__SMM__) && CONFIG_USE_OPTION_TABLE
+	static const unsigned baud[8] =
+		{ 115200, 57600, 38400, 19200, 9600, 4800, 2400, 1200 };
+	unsigned b_index = 0;
+#if defined(__PRE_RAM__)
+	b_index = read_option(baud_rate, 0xff);
+#else
+	if (get_option(&b_index, "baud_rate") != CB_SUCCESS)
+		b_index = 0xff;
+#endif
+	if (b_index < 8)
+		return baud[b_index];
+#endif
+	return CONFIG_TTYS0_BAUD;
+}
 
-/* Return the baudrate determined from option_table, or when that is
- * not used, CONFIG_TTYS0_BAUD.
- */
-unsigned int default_baudrate(void);
-
-/* Returns the divisor value for a given baudrate.
- * The formula to satisfy is:
- *    refclk / divisor = baudrate * oversample
- */
+/* Calculate divisor. Do not floor but round to nearest integer. */
 unsigned int uart_baudrate_divisor(unsigned int baudrate,
-	unsigned int refclk, unsigned int oversample);
-
-
-unsigned char uart_rx_byte(void);
-void uart_tx_byte(unsigned char data);
-void uart_tx_flush(void);
-void uart_init(void);
-
-uint32_t uartmem_getbaseaddr(void);
-
-#endif /* UART_H */
+	unsigned int refclk, unsigned int oversample)
+{
+	return (1 + (2 * refclk) / (baudrate * oversample)) / 2;
+}

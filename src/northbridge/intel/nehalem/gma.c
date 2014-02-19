688514a1ad3358a73673d2ce8dc753c3e3f7cae7
Vladimir Serbinenko <phcoder@gmail.com>
2014-02-19 22:18:08 +0100

    nehalem: Replace video init.
    Old video init just replayed the sequence.
    This one actually computes the values.
    
    Change-Id: Ic1fe7a2e90dc2cc36ac0d8bcea5cfabc583f09a3
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>

--- src/northbridge/intel/nehalem/gma.c
+++ src/northbridge/intel/nehalem/gma.c
@@ -28,6 +28,11 @@
 #include <device/pci_ops.h>
 #include <cpu/x86/msr.h>
 #include <cpu/x86/mtrr.h>
+#include <drivers/intel/gma/edid.h>
+#include <drivers/intel/gma/i915.h>
+#include <pc80/vga.h>
+#include <pc80/vga_io.h>
+
 
 #include "chip.h"
 #include "nehalem.h"
@@ -545,16 +550,6 @@ static void gma_pm_init_pre_vbios(struct device *dev)
 	gtt_write(0x6c024, reg32);
 }
 
-#include <pc80/vga.h>
-#include <pc80/vga_io.h>
-
-#if CONFIG_MAINBOARD_DO_NATIVE_VGA_INIT
-static void fake_vbios(void)
-{
-#include "fake_vbios.c"
-}
-#endif
-
 static void gma_pm_init_post_vbios(struct device *dev)
 {
 	struct northbridge_intel_nehalem_config *conf = dev->chip_info;
@@ -620,6 +615,388 @@ static void gma_pm_init_post_vbios(struct device *dev)
 	}
 }
 
+#if IS_ENABLED(CONFIG_MAINBOARD_DO_NATIVE_VGA_INIT)
+
+static void train_link(u32 mmio)
+{
+	write32(mmio + 0x000f000c, 0x00002040);
+	mdelay(1);
+	write32(mmio + 0x000f000c, 0x00002050);
+	write32(mmio + 0x00060100, 0x00044000);
+	mdelay(1);
+	write32(mmio + 0x00070008, 0x00000040);
+	write32(mmio + 0x000f0008, 0x00000040);
+	write32(mmio + 0x000f000c, 0x00022050);
+	write32(mmio + 0x00070008, 0x00000050);
+	write32(mmio + 0x00070008, 0x80000050);
+	write32(mmio + 0x00041000, 0x0020298e);
+
+	/* Clear interrupts. */
+	write32(mmio + DEIIR, 0xffffffff);
+
+	write32(mmio + 0x000f0018, 0x000000ff);
+	write32(mmio + 0x000f1018, 0x000000ff);
+	write32(mmio + 0x000f000c, 0x001a2050);
+	write32(mmio + 0x00060100, 0x001c4000);
+	write32(mmio + 0x00060100, 0x801c4000);
+	write32(mmio + 0x000f000c, 0x801a2050);
+	write32(mmio + 0x00060100, 0x801c4000);
+	write32(mmio + 0x000f000c, 0x801a2050);
+	mdelay(1);
+
+	read32(mmio + 0x000f0014); // = 0x00000100
+	write32(mmio + 0x000f0014, 0x00000100);
+	write32(mmio + 0x00060100, 0x901c4000);
+	write32(mmio + 0x000f000c, 0x901a2050);
+	mdelay(1);
+	read32(mmio + 0x000f0014); // = 0x00000600
+}
+
+static void power_port(u32 mmio)
+{
+	read32(mmio + 0x000e1100); // = 0x00000000
+	write32(mmio + 0x000e1100, 0x00000000);
+	write32(mmio + 0x000e1100, 0x00010000);
+	read32(mmio + 0x000e1100); // = 0x00010000
+	read32(mmio + 0x000e1100); // = 0x00010000
+	read32(mmio + 0x000e1100); // = 0x00000000
+	write32(mmio + 0x000e1100, 0x00000000);
+	read32(mmio + 0x000e1100); // = 0x00000000
+	read32(mmio + 0x000e4200); // = 0x0000001c
+	write32(mmio + 0x000e4210, 0x8004003e);
+	write32(mmio + 0x000e4214, 0x80060002);
+	write32(mmio + 0x000e4218, 0x01000000);
+	read32(mmio + 0x000e4210); // = 0x5144003e
+	write32(mmio + 0x000e4210, 0x5344003e);
+	read32(mmio + 0x000e4210); // = 0x0144003e
+	write32(mmio + 0x000e4210, 0x8074003e);
+	read32(mmio + 0x000e4210); // = 0x5144003e
+	read32(mmio + 0x000e4210); // = 0x5144003e
+	write32(mmio + 0x000e4210, 0x5344003e);
+	read32(mmio + 0x000e4210); // = 0x0144003e
+	write32(mmio + 0x000e4210, 0x8074003e);
+	read32(mmio + 0x000e4210); // = 0x5144003e
+	read32(mmio + 0x000e4210); // = 0x5144003e
+	write32(mmio + 0x000e4210, 0x5344003e);
+	read32(mmio + 0x000e4210); // = 0x0144003e
+	write32(mmio + 0x000e4210, 0x8074003e);
+	read32(mmio + 0x000e4210); // = 0x5144003e
+	read32(mmio + 0x000e4210); // = 0x5144003e
+	write32(mmio + 0x000e4210, 0x5344003e);
+	write32(mmio + 0x000e4f00, 0x0100030c);
+	write32(mmio + 0x000e4f04, 0x00b8230c);
+	write32(mmio + 0x000e4f08, 0x06f8930c);
+	write32(mmio + 0x000e4f0c, 0x09f8e38e);
+	write32(mmio + 0x000e4f10, 0x00b8030c);
+	write32(mmio + 0x000e4f14, 0x0b78830c);
+	write32(mmio + 0x000e4f18, 0x0ff8d3cf);
+	write32(mmio + 0x000e4f1c, 0x01e8030c);
+	write32(mmio + 0x000e4f20, 0x0ff863cf);
+	write32(mmio + 0x000e4f24, 0x0ff803cf);
+	write32(mmio + 0x000c4030, 0x00001000);
+	read32(mmio + 0x000c4000); // = 0x00000000
+	write32(mmio + 0x000c4030, 0x00001000);
+	read32(mmio + 0x000e1150); // = 0x0000001c
+	write32(mmio + 0x000e1150, 0x0000089c);
+	write32(mmio + 0x000fcc00, 0x01986f00);
+	write32(mmio + 0x000fcc0c, 0x01986f00);
+	write32(mmio + 0x000fcc18, 0x01986f00);
+	write32(mmio + 0x000fcc24, 0x01986f00);
+	read32(mmio + 0x000c4000); // = 0x00000000
+	read32(mmio + 0x000e1180); // = 0x40000002
+}
+
+static void intel_gma_init(const struct northbridge_intel_nehalem_config *info,
+			   u32 mmio, u32 physbase, u32 gttbase)
+{
+	int i;
+	u8 edid_data[128];
+	struct edid edid;
+	u32 hactive, vactive, right_border, bottom_border;
+	int hpolarity, vpolarity;
+	u32 vsync, hsync, vblank, hblank, hfront_porch, vfront_porch;
+	u32 candp1, candn;
+	u32 best_delta = 0xffffffff;
+	u32 target_frequency;
+	u32 pixel_p1 = 1;
+	u32 pixel_n = 1;
+	u32 pixel_m1 = 1;
+	u32 pixel_m2 = 1;
+	u32 link_frequency = info->gpu_link_frequency_270_mhz ? 270000 : 162000;
+	u32 data_m1;
+	u32 data_n1 = 0x00800000;
+	u32 link_m1;
+	u32 link_n1 = 0x00080000;
+
+	write32(mmio + 0x00070080, 0x00000000);
+	write32(mmio + 0x00070180, 0x00000000);
+	write32(mmio + 0x00071180, 0x00000000);
+	write32(mmio + 0x00041000, 0x8000298e);
+	write32(mmio + 0x0007019c, 0x00000000);
+	write32(mmio + 0x0007119c, 0x00000000);
+	write32(mmio + 0x000fc008, 0x2c010000);
+	write32(mmio + 0x000fc020, 0x2c010000);
+	write32(mmio + 0x000fc038, 0x2c010000);
+	write32(mmio + 0x000fc050, 0x2c010000);
+	write32(mmio + 0x000fc408, 0x2c010000);
+	write32(mmio + 0x000fc420, 0x2c010000);
+	write32(mmio + 0x000fc438, 0x2c010000);
+	write32(mmio + 0x000fc450, 0x2c010000);
+	vga_gr_write(0x18, 0);
+	write32(mmio + 0x00042004, 0x02000000);
+	write32(mmio + 0x000fd034, 0x8421ffe0);
+
+	/* Setup GTT.  */
+	for (i = 0; i < 0x1fff; i++)
+		write32(gttbase + (i << 2), physbase | (i << 12) | 1);
+
+	vga_misc_write(0x67);
+
+	const u8 cr[] = { 0x5f, 0x4f, 0x50, 0x82, 0x55, 0x81, 0xbf, 0x1f,
+		    0x00, 0x4f, 0x0d, 0x0e, 0x00, 0x00, 0x00, 0x00,
+		    0x9c, 0x8e, 0x8f, 0x28, 0x1f, 0x96, 0xb9, 0xa3,
+		    0xff
+	};
+	vga_cr_write(0x11, 0);
+
+	for (i = 0; i <= 0x18; i++)
+		vga_cr_write(i, cr[i]);
+
+	power_port(mmio);
+
+	intel_gmbus_read_edid(mmio, 3, 0x50, edid_data);
+	decode_edid(edid_data,
+		    sizeof(edid_data), &edid);
+
+	/* Disable screen memory to prevent garbage from appearing.  */
+	vga_sr_write(1, vga_sr_read(1) | 0x20);
+
+	vga_textmode_init();
+
+	hactive = edid.x_resolution;
+	vactive = edid.y_resolution;
+	right_border = edid.hborder;
+	bottom_border = edid.vborder;
+	hpolarity = (edid.phsync == '-');
+	vpolarity = (edid.pvsync == '-');
+	vsync = edid.vspw;
+	hsync = edid.hspw;
+	vblank = edid.vbl;
+	hblank = edid.hbl;
+	hfront_porch = edid.hso;
+	vfront_porch = edid.vso;
+
+	target_frequency = info->gpu_lvds_dual_channel ? edid.pixel_clock
+		: (2 * edid.pixel_clock);
+
+	/* Find suitable divisors.  */
+	for (candp1 = 1; candp1 <= 8; candp1++)
+		for (candn = 5; candn <= 10; candn++) {
+			unsigned cur_frequency;
+			unsigned m; /* 77 - 131.  */
+			unsigned denom; /* 35 - 560.  */
+			unsigned current_delta;
+
+			denom = candn * candp1 * 7;
+			/* Doesnt overflow for up to
+			   5000000 kHz = 5 GHz.  */
+			m = (target_frequency * denom + 60000) / 120000;
+
+			if (m < 77 || m > 131)
+				continue;
+
+			cur_frequency = (120000 * m) / denom;
+			if (target_frequency > cur_frequency)
+				current_delta = target_frequency - cur_frequency;
+			else
+				current_delta = cur_frequency - target_frequency;
+
+
+			if (best_delta > current_delta) {
+				best_delta = current_delta;
+				pixel_n = candn;
+				pixel_p1 = candp1;
+				pixel_m2 = ((m + 3) % 5) + 7;
+				pixel_m1 = (m - pixel_m2) / 5;
+			}
+		}
+
+	if (best_delta == 0xffffffff) {
+		printk (BIOS_ERR, "Couldn't find GFX clock divisors\n");
+		return;
+	}
+
+	link_m1 = ((uint64_t)link_n1 * edid.pixel_clock) / link_frequency;
+	data_m1 = ((uint64_t)data_n1 * 18 * edid.pixel_clock)
+		/ (link_frequency * 8 * info->gpu_lvds_n_lanes);
+
+	printk(BIOS_INFO, "bringing up panel at resolution %d x %d\n",
+	       hactive, vactive);
+	printk(BIOS_DEBUG, "Borders %d x %d\n",
+	       right_border, bottom_border);
+	printk(BIOS_DEBUG, "Blank %d x %d\n",
+	       hblank, vblank);
+	printk(BIOS_DEBUG, "Sync %d x %d\n",
+	       hsync, vsync);
+	printk(BIOS_DEBUG, "Front porch %d x %d\n",
+	       hfront_porch, vfront_porch);
+	printk(BIOS_DEBUG, (info->gpu_use_spread_spectrum_clock
+			    ? "Spread spectrum clock\n" : "DREF clock\n"));
+	printk(BIOS_DEBUG,
+	       info->gpu_lvds_dual_channel ? "Dual channel\n" : "Single channel\n");
+	printk(BIOS_DEBUG, "Polarities %d, %d\n",
+	       hpolarity, vpolarity);
+	printk(BIOS_DEBUG, "Data M1=%d, N1=%d\n",
+	       data_m1, data_n1);
+	printk(BIOS_DEBUG, "Link frequency %d kHz\n",
+	       link_frequency);
+	printk(BIOS_DEBUG, "Link M1=%d, N1=%d\n",
+	       link_m1, link_n1);
+	printk(BIOS_DEBUG, "Pixel N=%d, M1=%d, M2=%d, P1=%d\n",
+	       pixel_n, pixel_m1, pixel_m2, pixel_p1);
+	printk(BIOS_DEBUG, "Pixel clock %d kHz\n",
+	       120000 * (5 * pixel_m1 + pixel_m2) / pixel_n
+	       / (pixel_p1 * 7));
+
+	write32(mmio + PCH_LVDS,
+		(hpolarity << 20) | (vpolarity << 21)
+		| (info->gpu_lvds_dual_channel ? LVDS_CLOCK_B_POWERUP_ALL
+		   | LVDS_CLOCK_BOTH_POWERUP_ALL : 0)
+		| LVDS_BORDER_ENABLE | LVDS_CLOCK_A_POWERUP_ALL
+		| LVDS_DETECTED);
+	write32(mmio + BLC_PWM_CPU_CTL2, (1 << 31));
+	write32(mmio + PCH_DREF_CONTROL, (info->gpu_use_spread_spectrum_clock
+					  ? 0x1002 : 0x400));
+	mdelay(1);
+	write32(mmio + PCH_PP_CONTROL, PANEL_UNLOCK_REGS
+		| (read32(mmio + PCH_PP_CONTROL) & ~PANEL_UNLOCK_MASK));
+	write32(mmio + _PCH_FP0(0),
+		((pixel_n - 2) << 16)
+		| ((pixel_m1 - 2) << 8) | pixel_m2);
+	write32(mmio + _PCH_DPLL(0),
+		DPLL_VCO_ENABLE | DPLLB_MODE_LVDS
+		| (info->gpu_lvds_dual_channel ? DPLLB_LVDS_P2_CLOCK_DIV_7
+		   : DPLLB_LVDS_P2_CLOCK_DIV_14)
+		| (0x10000 << (pixel_p1 - 1))
+		| ((info->gpu_use_spread_spectrum_clock ? 3 : 0) << 13)
+		| (0x1 << (pixel_p1 - 1)));
+	mdelay(1);
+	write32(mmio + _PCH_DPLL(0),
+		DPLL_VCO_ENABLE | DPLLB_MODE_LVDS
+		| (info->gpu_lvds_dual_channel ? DPLLB_LVDS_P2_CLOCK_DIV_7
+		   : DPLLB_LVDS_P2_CLOCK_DIV_14)
+		| (0x10000 << (pixel_p1 - 1))
+		| ((info->gpu_use_spread_spectrum_clock ? 3 : 0) << 13)
+		| (0x1 << (pixel_p1 - 1)));
+	/* Re-lock the registers.  */
+	write32(mmio + PCH_PP_CONTROL,
+		(read32(mmio + PCH_PP_CONTROL) & ~PANEL_UNLOCK_MASK));
+
+	write32(mmio + PCH_LVDS,
+		(hpolarity << 20) | (vpolarity << 21)
+		| (info->gpu_lvds_dual_channel ? LVDS_CLOCK_B_POWERUP_ALL
+		   | LVDS_CLOCK_BOTH_POWERUP_ALL : 0)
+		| LVDS_BORDER_ENABLE | LVDS_CLOCK_A_POWERUP_ALL
+		| LVDS_DETECTED);
+
+	write32(mmio + HTOTAL(0),
+		((hactive + right_border + hblank - 1) << 16)
+		| (hactive - 1));
+	write32(mmio + HBLANK(0),
+		((hactive + right_border + hblank - 1) << 16)
+		| (hactive + right_border - 1));
+	write32(mmio + HSYNC(0),
+		((hactive + right_border + hfront_porch + hsync - 1) << 16)
+		| (hactive + right_border + hfront_porch - 1));
+
+	write32(mmio + VTOTAL(0), ((vactive + bottom_border + vblank - 1) << 16)
+		| (vactive - 1));
+	write32(mmio + VBLANK(0), ((vactive + bottom_border + vblank - 1) << 16)
+		| (vactive + bottom_border - 1));
+	write32(mmio + VSYNC(0),
+		(vactive + bottom_border + vfront_porch + vsync - 1)
+		| (vactive + bottom_border + vfront_porch - 1));
+
+	write32(mmio + PIPESRC(0), 0x02cf018f);
+	write32(mmio + PIPECONF(0), PIPECONF_DISABLE);
+
+	write32(mmio + 0x6001c,0x27f018f); // 02cf018f
+	write32(mmio + 0x68080,0x80800000); // 00000000
+
+	write32(mmio + PF_WIN_SZ(0), vactive | (hactive << 16));
+	read32(mmio + 0x00070008);
+	read32(mmio + 0x00070008);
+
+	mdelay(1);
+
+	write32(mmio + PIPE_DATA_M1(0), 0x7e000000 | data_m1);
+	write32(mmio + PIPE_DATA_N1(0), data_n1);
+	write32(mmio + PIPE_LINK_M1(0), link_m1);
+	write32(mmio + PIPE_LINK_N1(0), link_n1);
+
+	train_link(mmio);
+
+	write32(mmio + TRANS_HTOTAL(0),
+		((hactive + right_border + hblank - 1) << 16)
+		| (hactive - 1));
+	write32(mmio + TRANS_HBLANK(0),
+		((hactive + right_border + hblank - 1) << 16)
+		| (hactive + right_border - 1));
+	write32(mmio + TRANS_HSYNC(0),
+		((hactive + right_border + hfront_porch + hsync - 1) << 16)
+		| (hactive + right_border + hfront_porch - 1));
+
+	write32(mmio + TRANS_VTOTAL(0),
+		((vactive + bottom_border + vblank - 1) << 16)
+		| (vactive - 1));
+	write32(mmio + TRANS_VBLANK(0),
+		((vactive + bottom_border + vblank - 1) << 16)
+		| (vactive + bottom_border - 1));
+	write32(mmio + TRANS_VSYNC(0),
+		(vactive + bottom_border + vfront_porch + vsync - 1)
+		| (vactive + bottom_border + vfront_porch - 1));
+
+	write32(mmio + 0x00060100, 0xb01c4000);
+	write32(mmio + 0x000f000c, 0xb01a2050);
+	mdelay(1);
+	write32(mmio + 0x000f0008, 0x80000040);
+	write32(mmio + PCH_LVDS,
+		LVDS_PORT_ENABLE
+		| (hpolarity << 20) | (vpolarity << 21)
+		| (info->gpu_lvds_dual_channel ? LVDS_CLOCK_B_POWERUP_ALL
+		   | LVDS_CLOCK_BOTH_POWERUP_ALL : 0)
+		| LVDS_BORDER_ENABLE | LVDS_CLOCK_A_POWERUP_ALL
+		| LVDS_DETECTED);
+
+	write32(mmio + PCH_PP_CONTROL, PANEL_UNLOCK_REGS | PANEL_POWER_OFF);
+	write32(mmio + PCH_PP_CONTROL, PANEL_UNLOCK_REGS | PANEL_POWER_RESET);
+	mdelay(1);
+	write32(mmio + PCH_PP_CONTROL, PANEL_UNLOCK_REGS
+		| PANEL_POWER_ON | PANEL_POWER_RESET);
+
+	printk (BIOS_DEBUG, "waiting for panel powerup\n");
+	while (1)
+	{
+		u32 reg32;
+		reg32 = read32(mmio + PCH_PP_STATUS);
+		if (((reg32 >> 28) & 3) == 0)
+			break;
+	}
+	printk (BIOS_DEBUG, "panel powered up\n");
+
+	write32(mmio + PCH_PP_CONTROL, PANEL_POWER_ON | PANEL_POWER_RESET);
+
+	/* Enable screen memory.  */
+	vga_sr_write(1, vga_sr_read(1) & ~0x20);
+
+	/* Clear interrupts. */
+	write32(mmio + DEIIR, 0xffffffff);
+	write32(mmio + SDEIIR, 0xffffffff);
+}
+
+#endif
+
+
 static void gma_func0_init(struct device *dev)
 {
 	u32 reg32;
@@ -636,8 +1013,18 @@ static void gma_func0_init(struct device *dev)
 	/* PCI Init, will run VBIOS */
 	pci_dev_init(dev);
 #else
-	printk(BIOS_SPEW, "Initializing VGA without OPROM.\n");
-	fake_vbios();
+	u32 physbase, gttbase;
+	struct northbridge_intel_nehalem_config *conf = dev->chip_info;
+
+	physbase = pci_read_config32(dev, 0x5c) & ~0xf;
+	gttbase = pci_read_config32(dev_find_slot(0, PCI_DEVFN(0, 0)),
+				    D0F0_GTT_BASE);
+
+	if (gtt_res && gtt_res->base && physbase && gttbase) {
+		printk(BIOS_SPEW, "Initializing VGA without OPROM. MMIO 0x%llx\n",
+		       gtt_res->base);
+		intel_gma_init(conf, gtt_res->base, physbase, gttbase);
+	}
 #endif
 
 	/* Linux relies on VBT for panel info.  */

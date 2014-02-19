0537e7e5a778f75dc626af60eb2d81940b4afde5
Aaron Durbin <adurbin@chromium.org>
2014-02-18 19:36:05 -0600

    coreboot: don't return struct lb_memory * from write_tables()
    No one is interrogating the write_tables() return value. Therefore,
    drop it.
    
    Change-Id: I97e707f071942239c9a0fa0914af3679ee7a9c3c
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>

--- src/arch/armv7/tables.c
+++ src/arch/armv7/tables.c
@@ -46,7 +46,7 @@ void cbmem_fail_resume(void)
 {
 }
 
-struct lb_memory *write_tables(void)
+void write_tables(void)
 {
 	unsigned long table_pointer, new_table_pointer;
 
@@ -56,7 +56,7 @@ struct lb_memory *write_tables(void)
 						MAX_COREBOOT_TABLE_SIZE);
 	if (!table_pointer) {
 		printk(BIOS_ERR, "Could not add CBMEM for coreboot table.\n");
-		return NULL;
+		return;
 	}
 
 	new_table_pointer = write_coreboot_table(0UL, 0UL,
@@ -74,6 +74,4 @@ struct lb_memory *write_tables(void)
 
 	/* Print CBMEM sections */
 	cbmem_list();
-
-	return get_lb_mem();
 }

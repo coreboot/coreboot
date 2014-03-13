9df2762597d6762093b5d8570d3de4df5d43ea0c
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2014-03-14 01:06:22 +1100

    drivers/hwm: Add a generic hwm driver.
    Make generic, testing with Fintek Super I/O....
    
    Change-Id: Ic7bdea55907e379aad9e74b87b514e8038ef9fd0
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

---
+++ src/include/hwm/superio_hwm.h
@@ -0,0 +1,10 @@
+#ifndef SUPERIO_HWM_H
+#define SUPERIO_HWM_H
+
+#include <device/device.h>
+
+/* Initialization parameters struct?? */
+
+void hwm_init(device_t dev, u16 iobase);
+
+#endif /* SUPERIO_HWM_H */

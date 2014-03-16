1e8df4775969691437ccd46ecaa0ab30c2937f3f
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2014-03-16 17:24:18 +1100

    Lenovo T530 [2/2]: actually implement mainboard support.
    Step 2: change the Lenovo X230 code to adapt it to the new board's
    hardware with the great guidance from Vladimir (phcoder) to find the
    correct GPIO's.
    
    The machine has:
     - Chipset: Intel QM77
     - GPU's: Intel Integrated HD Graphics
            : Discrete NVIDIA NVS 5400M (1 GB VRAM) with Optimus Technology
    and pci configuration of:
     -[0000:00]-+-00.0  8086:0154
                +-02.0  8086:0166
                +-16.0  8086:1e3a
                +-19.0  8086:1502
                +-1a.0  8086:1e2d
                +-1b.0  8086:1e20
                +-1c.0-[02]----00.0  1180:e823
                +-1c.1-[03]----00.0  8086:4238
                +-1c.2-[04-0b]--
                +-1d.0  8086:1e26
                +-1f.0  8086:1e55
                +-1f.2  8086:1e03
                \-1f.3  8086:1e22
    
    Change-Id: Iee12c3edc22df4a7935b7fb7ff4a320c21c4239b
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

--- src/mainboard/lenovo/t530/thermal.h
+++ src/mainboard/lenovo/t530/thermal.h
@@ -18,8 +18,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#ifndef X230_THERMAL_H
-#define X230_THERMAL_H
+#ifndef T530_THERMAL_H
+#define T530_THERMAL_H
 
 	/* Temperature which OS will shutdown at */
 	#define CRITICAL_TEMPERATURE	100
@@ -27,4 +27,4 @@
 	/* Temperature which OS will throttle CPU */
 	#define PASSIVE_TEMPERATURE	90
 
-#endif
+#endif /* T530_THERMAL_H */

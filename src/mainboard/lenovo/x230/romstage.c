8089f1780635daae24ded0ac7fb9dc2d2f2a01bb
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2014-03-16 20:42:40 +1100

    mainboard/lenovo/x230 Fix usage of GNU field designator extension
    In C99 we defined a syntax for this. GCC's old syntax was deprecated.
    
    Change-Id: If8c53b5370be9101b9e5f2dfa88a6229f500a0f6
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>
    Reviewed-on: http://review.coreboot.org/5392
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Tested-by: build bot (Jenkins)

--- src/mainboard/lenovo/x230/romstage.c
+++ src/mainboard/lenovo/x230/romstage.c
@@ -121,34 +121,34 @@ void main(unsigned long bist)
 	}
 
 	struct pei_data pei_data = {
-		pei_version: PEI_VERSION,
-		mchbar: DEFAULT_MCHBAR,
-		dmibar: DEFAULT_DMIBAR,
-		epbar: DEFAULT_EPBAR,
-		pciexbar: CONFIG_MMCONF_BASE_ADDRESS,
-		smbusbar: SMBUS_IO_BASE,
-		wdbbar: 0x4000000,
-		wdbsize: 0x1000,
-		hpet_address: CONFIG_HPET_ADDRESS,
-		rcba: DEFAULT_RCBABASE,
-		pmbase: DEFAULT_PMBASE,
-		gpiobase: DEFAULT_GPIOBASE,
-		thermalbase: 0xfed08000,
-		system_type: 0, // 0 Mobile, 1 Desktop/Server
-		tseg_size: CONFIG_SMM_TSEG_SIZE,
-		spd_addresses: { 0xA0, 0x00,0xA2,0x00 },
-		ts_addresses: { 0x00, 0x00, 0x00, 0x00 },
-		ec_present: 1,
-		gbe_enable: 1,
-		ddr3lv_support: 0,
+		.pei_version = PEI_VERSION,
+		.mchbar = DEFAULT_MCHBAR,
+		.dmibar = DEFAULT_DMIBAR,
+		.epbar = DEFAULT_EPBAR,
+		.pciexbar = CONFIG_MMCONF_BASE_ADDRESS,
+		.smbusbar = SMBUS_IO_BASE,
+		.wdbbar = 0x4000000,
+		.wdbsize = 0x1000,
+		.hpet_address = CONFIG_HPET_ADDRESS,
+		.rcba = DEFAULT_RCBABASE,
+		.pmbase = DEFAULT_PMBASE,
+		.gpiobase = DEFAULT_GPIOBASE,
+		.thermalbase = 0xfed08000,
+		.system_type = 0, // 0 Mobile, 1 Desktop/Server
+		.tseg_size = CONFIG_SMM_TSEG_SIZE,
+		.spd_addresses = { 0xA0, 0x00,0xA2,0x00 },
+		.ts_addresses = { 0x00, 0x00, 0x00, 0x00 },
+		.ec_present = 1,
+		.gbe_enable = 1,
+		.ddr3lv_support = 0,
 		// 0 = leave channel enabled
 		// 1 = disable dimm 0 on channel
 		// 2 = disable dimm 1 on channel
 		// 3 = disable dimm 0+1 on channel
-		dimm_channel0_disabled: 2,
-		dimm_channel1_disabled: 2,
-		max_ddr3_freq: 1600,
-		usb_port_config: {
+		.dimm_channel0_disabled = 2,
+		.dimm_channel1_disabled = 2,
+		.max_ddr3_freq = 1600,
+		.usb_port_config = {
 			 /* enabled   usb oc pin    length */
 			{ 1, 0, 0x0080 }, /* P0 (left, fan side), OC 0 */
 			{ 1, 1, 0x0080 }, /* P1 (left touchpad side), OC 1 */
@@ -165,7 +165,7 @@ void main(unsigned long bist)
 			{ 1, 0, 0x0040 }, /* P12: wlan, no OC */
 			{ 1, 0, 0x0080 }, /* P13: webcam, no OC */
 		},
-		ddr_refresh_rate_config: 2, /* Force double refresh rate */
+		.ddr_refresh_rate_config = 2, /* Force double refresh rate */
 	};
 
 	timestamp_init(get_initial_timestamp());

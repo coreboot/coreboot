da01d9435163a1799fdbf8c60c988bb2f8566f29
Furquan Shaikh <furquan@google.com>
2014-03-19 14:31:23 -0700

    storm: Add generic support skeleton for storm
    Skeleton for storm mainboard
    
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/190724
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    (cherry picked from commit ba371d410768fae169da929a23c40139d26a55d3)
    
    Removed 'select ARCH_ARM' and added 'select BOARD_ROMSIZE_KB_1024' to
    the Kconfig.
    
    Change-Id: I55c0ad6a47515ba4124b99a69d5776db2365f06e
    Signed-off-by: Isaac Christensen <isaac.christensen@se-eng.com>
    Reviewed-on: http://review.coreboot.org/6975
    Tested-by: build bot (Jenkins)
    Reviewed-by: Furquan Shaikh <furquan@google.com>

---
+++ src/mainboard/google/storm/romstage.c
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2014 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <arch/stages.h>
+#include <cbfs.h>
+#include <console/console.h>
+
+void main(void)
+{
+	void *entry;
+
+	entry = cbfs_load_stage(CBFS_DEFAULT_MEDIA, "fallback/coreboot_ram");
+	stage_exit(entry);
+}

6dbc680a90353a5ac5553ca5686af6220f61191e
Gabe Black <gabeblack@google.com>
2014-03-27 20:50:25 -0700

    spi: Remove unused constants from spi-generic.h.
    These constants aren't used anywhere.
    
    Change-Id: Ifdad9b088a281909892edb34dcb58419e0e123ba
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/192047
    Reviewed-by: Gabe Black <gabeblack@chromium.org>
    Tested-by: Gabe Black <gabeblack@chromium.org>
    Commit-Queue: Gabe Black <gabeblack@chromium.org>
    (cherry picked from commit 3cdaf9dd7de6ae71efc9e74335d876ec55f9ca0a)
    Signed-off-by: Isaac Christensen <isaac.christensen@se-eng.com>
    Reviewed-on: http://review.coreboot.org/7008
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Marc Jones <marc.jones@se-eng.com>

--- src/include/spi-generic.h
+++ src/include/spi-generic.h
@@ -28,22 +28,6 @@
 
 /* Controller-specific definitions: */
 
-/* SPI mode flags */
-#define	SPI_CPHA	0x01			/* clock phase */
-#define	SPI_CPOL	0x02			/* clock polarity */
-#define	SPI_MODE_0	(0|0)			/* (original MicroWire) */
-#define	SPI_MODE_1	(0|SPI_CPHA)
-#define	SPI_MODE_2	(SPI_CPOL|0)
-#define	SPI_MODE_3	(SPI_CPOL|SPI_CPHA)
-#define	SPI_CS_HIGH	0x04			/* CS active high */
-#define	SPI_LSB_FIRST	0x08			/* per-word bits-on-wire */
-#define	SPI_3WIRE	0x10			/* SI/SO signals shared */
-#define	SPI_LOOP	0x20			/* loopback mode */
-
-/* SPI transfer flags */
-#define SPI_XFER_BEGIN	0x01			/* Assert CS before transfer */
-#define SPI_XFER_END	0x02			/* Deassert CS after transfer */
-
 /* SPI opcodes */
 #define SPI_OPCODE_WREN 0x06
 #define SPI_OPCODE_FAST_READ 0x0b

374251d9019ad0d99b8ea309e0e3eb2b55251a4c
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-03-29 18:06:57 -0500

    cpu/amd/agesa/s3_resume.c: Specify include paths from AGESA_ROOT
    Following the same reasoning as in commit
    * 1d87dac hp/pavilion_m6_1035dx: Sanitize #includes
    include AGESA files with a path relative to AGESA_ROOT. We cannot
    with more than one generation of AGESA, hence the path being relative
    to AGESA_ROOT.
    
    Change-Id: If15c4cbfd42e0264264fdb3e8c426a47609ad41f
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Reviewed-on: http://review.coreboot.org/5426
    Tested-by: build bot (Jenkins)
    Reviewed-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>

--- src/cpu/amd/agesa/s3_resume.c
+++ src/cpu/amd/agesa/s3_resume.c
@@ -17,8 +17,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,  MA 02110-1301 USA
  */
 
-#include "AGESA.h"
-#include "amdlib.h"
+#include <AGESA.h>
+#include <Lib/amdlib.h>
 #include <console/console.h>
 #include <cpu/x86/msr.h>
 #include <cpu/x86/mtrr.h>

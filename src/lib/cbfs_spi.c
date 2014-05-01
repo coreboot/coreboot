adcb095e9e1e535b9331648dab0d628afd1336ca
Vadim Bendebury <vbendeb@chromium.org>
2014-05-01 12:23:09 -0700

    Provide a common CBFS wrapper for SPI storage
    Coreboot has all necessary infrastructure to use the proper SPI flash
    interface in bootblock for CBFS. This patch creates a common CBFS
    wrapper which can be enabled on different platforms as required.
    
    COMMON_CBFS_SPI_WRAPPER, a new configuration option, enables the
    common CBFS interface and prevents default inclusion of all SPI chip
    drivers, only explicitly configured ones will be included when the new
    feature is enabled. Since the wrapper uses the same driver at all
    stages, enabling the new feature will also make it necessary to
    include the SPI chip drivers in bootblock and romstage images.
    
    init_default_cbfs_media() can now be common for different platforms,
    and as such is defined in the library.
    
    BUG=none
    TEST=manual
       . with this change and the rest of the patches coreboot on AP148
         comes up all the way to attempting to boot the payload (reading
         earlier stages from the SPI flash along the way).
    
    Original-Change-Id: Ia887bb7f386a0e23a110e38001d86f9d43fadf2c
    Original-Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Original-Reviewed-on: https://chromium-review.googlesource.com/197800
    Original-Tested-by: Vadim Bendebury <vbendeb@google.com>
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Original-Reviewed-by: David Hendricks <dhendrix@chromium.org>
    (cherry picked from commit 60eb16ebe624f9420c6191afa6ba239b8e83a6e6)
    Signed-off-by: Marc Jones <marc.jones@se-eng.com>
    
    Change-Id: I7b0bf3dda915c227659ab62743e405312dedaf41
    Reviewed-on: http://review.coreboot.org/7932
    Tested-by: build bot (Jenkins)
    Reviewed-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

---
+++ src/lib/cbfs_spi.c
@@ -0,0 +1,95 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2014 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/*
+ * This file provides a common CBFS wrapper for SPI storage. SPI driver
+ * context is expanded with the buffer descriptor used to store data read from
+ * SPI.
+ */
+
+#include <cbfs.h>
+#include <spi_flash.h>
+
+/* SPI flash as CBFS media. */
+struct cbfs_spi_context {
+	struct spi_flash *spi_flash_info;
+	struct cbfs_simple_buffer buffer;
+};
+
+static struct cbfs_spi_context spi_context;
+
+static int cbfs_media_open(struct cbfs_media *media)
+{
+	return 0;
+}
+
+static int cbfs_media_close(struct cbfs_media *media)
+{
+	return 0;
+}
+
+static size_t cbfs_media_read(struct cbfs_media *media,
+			      void *dest, size_t offset,
+			      size_t count)
+{
+	struct cbfs_spi_context *context = media->context;
+
+	return context->spi_flash_info->read
+		(context->spi_flash_info, offset, count, dest) ? 0 : count;
+}
+
+static void *cbfs_media_map(struct cbfs_media *media,
+			    size_t offset, size_t count)
+{
+	struct cbfs_spi_context *context = media->context;
+
+	return cbfs_simple_buffer_map(&context->buffer, media, offset, count);
+}
+
+static void *cbfs_media_unmap(struct cbfs_media *media,
+			       const void *address)
+{
+	struct cbfs_spi_context *context = media->context;
+
+	return cbfs_simple_buffer_unmap(&context->buffer, address);
+}
+
+int init_default_cbfs_media(struct cbfs_media *media)
+{
+	if (spi_context.buffer.buffer)
+		return 0; /* It has been already initialized. */
+
+	spi_context.spi_flash_info = spi_flash_probe
+		(CONFIG_BOOT_MEDIA_SPI_BUS, 0);
+	if (!spi_context.spi_flash_info)
+		return -1;
+
+	spi_context.buffer.buffer = (void *)CONFIG_CBFS_CACHE_ADDRESS;
+	spi_context.buffer.size = CONFIG_CBFS_CACHE_SIZE;
+
+	media->context = &spi_context;
+
+	media->open = cbfs_media_open;
+	media->close = cbfs_media_close;
+	media->read = cbfs_media_read;
+	media->map = cbfs_media_map;
+	media->unmap = cbfs_media_unmap;
+
+	return 0;
+}

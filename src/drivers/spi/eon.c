79f60a5c9d24667928ca11c44eb8c1fccf4a8183
Marc Jones <marc.jones@se-eng.com>
2014-05-01 14:53:24 -0600

    SPI: Fix Eon support
    The Eon SPI25 code had a number of issues:
     - fix page write calculation
     - fix erase segment
     - fix id check
     - fix sector size
     - make commands EN25 generic
    This makes the code similar to other SPI25 devices used in coreboot.
    
    BUG=chrome-os-partner:25907
    BRANCH=baytrail(rambi)
    TEST=Read and write MRC and ELOG on Glimmer with Eon device.
    
    Original-Change-Id: I7667eab28b850790d92a591c869788d51c26a56c
    Original-Signed-off-by: Marc Jones <marc.jones@se-eng.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/198323
    Original-Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Original-Commit-Queue: Marc Jones <marc.jones@se-eng.com>
    Original-Tested-by: Marc Jones <marc.jones@se-eng.com>
    (cherry picked from commit 2ee0da695bf6a6c6aedc0dd2b3a3b7c9c3165bca)
    Signed-off-by: Marc Jones <marc.jones@se-eng.com>
    
    Change-Id: I8917e778cd62f3745189336d23c0c6118887d893
    Reviewed-on: http://review.coreboot.org/7934
    Tested-by: build bot (Jenkins)
    Reviewed-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

--- src/drivers/spi/eon.c
+++ src/drivers/spi/eon.c
@@ -1,5 +1,7 @@
 /*
  * (C) Copyright 2010, ucRobotics Inc.
+ * Copyright (c) 2014, Sage Electronic Engineering, LLC
+ * .
  * Author: Chong Huang <chuang@ucrobotics.com>
  * Licensed under the GPL-2 or later.
  */
@@ -9,24 +11,24 @@
 
 #include "spi_flash_internal.h"
 
-/* EN25Q128-specific commands */
-#define CMD_EN25Q128_WREN	0x06    /* Write Enable */
-#define CMD_EN25Q128_WRDI	0x04    /* Write Disable */
-#define CMD_EN25Q128_RDSR	0x05    /* Read Status Register */
-#define CMD_EN25Q128_WRSR	0x01    /* Write Status Register */
-#define CMD_EN25Q128_READ	0x03    /* Read Data Bytes */
-#define CMD_EN25Q128_FAST_READ	0x0b    /* Read Data Bytes at Higher Speed */
-#define CMD_EN25Q128_PP		0x02    /* Page Program */
-#define CMD_EN25Q128_SE		0x20    /* Sector Erase */
-#define CMD_EN25Q128_BE		0xd8    /* Block Erase */
-#define CMD_EN25Q128_DP		0xb9    /* Deep Power-down */
-#define CMD_EN25Q128_RES	0xab    /* Release from DP, and Read Signature */
+/* EN25*-specific commands */
+#define CMD_EN25_WREN		0x06	/* Write Enable */
+#define CMD_EN25_WRDI		0x04	/* Write Disable */
+#define CMD_EN25_RDSR		0x05	/* Read Status Register */
+#define CMD_EN25_WRSR		0x01	/* Write Status Register */
+#define CMD_EN25_READ		0x03	/* Read Data Bytes */
+#define CMD_EN25_FAST_READ	0x0b	/* Read Data Bytes at Higher Speed */
+#define CMD_EN25_PP		0x02	/* Page Program */
+#define CMD_EN25_SE		0x20	/* Sector Erase */
+#define CMD_EN25_BE		0xd8	/* Block Erase */
+#define CMD_EN25_DP		0xb9	/* Deep Power-down */
+#define CMD_EN25_RES		0xab	/* Release from DP, and Read Signature */
 
-#define EON_ID_EN25Q64		0x17
-#define EON_ID_EN25Q128		0x18
+#define EON_ID_EN25Q128		0x3018
+#define EON_ID_EN25Q64		0x3017
 
 struct eon_spi_flash_params {
-	u8 idcode1;
+	u16 id;
 	u16 page_size;
 	u16 pages_per_sector;
 	u16 sectors_per_block;
@@ -47,7 +49,7 @@ static inline struct eon_spi_flash *to_eon_spi_flash(struct spi_flash *flash)
 
 static const struct eon_spi_flash_params eon_spi_flash_table[] = {
 	{
-		.idcode1 = EON_ID_EN25Q128,
+		.id = EON_ID_EN25Q128,
 		.page_size = 256,
 		.pages_per_sector = 16,
 		.sectors_per_block = 16,
@@ -55,7 +57,7 @@ static const struct eon_spi_flash_params eon_spi_flash_table[] = {
 		.name = "EN25Q128",
 	},
 	{
-		.idcode1 = EON_ID_EN25Q64,
+		.id = EON_ID_EN25Q64,
 		.page_size = 256,
 		.pages_per_sector = 16,
 		.sectors_per_block = 16,
@@ -68,7 +70,6 @@ static int eon_write(struct spi_flash *flash,
 		     u32 offset, size_t len, const void *buf)
 {
 	struct eon_spi_flash *eon = to_eon_spi_flash(flash);
-	unsigned long page_addr;
 	unsigned long byte_addr;
 	unsigned long page_size;
 	size_t chunk_len;
@@ -76,19 +77,25 @@ static int eon_write(struct spi_flash *flash,
 	int ret = 0;
 	u8 cmd[4];
 
-	page_size = eon->params->page_size;
-	page_addr = offset / page_size;
+	page_size = 1 << eon->params->page_size;
 	byte_addr = offset % page_size;
 
 	flash->spi->rw = SPI_WRITE_FLAG;
 
 	for (actual = 0; actual < len; actual += chunk_len) {
 		chunk_len = min(len - actual, page_size - byte_addr);
+		chunk_len = spi_crop_chunk(sizeof(cmd), chunk_len);
 
-		cmd[0] = CMD_EN25Q128_PP;
-		cmd[1] = page_addr >> 8;
-		cmd[2] = page_addr;
-		cmd[3] = byte_addr;
+		ret = spi_flash_cmd(flash->spi, CMD_EN25_WREN, NULL, 0);
+		if (ret < 0) {
+			printk(BIOS_WARNING, "SF: Enabling Write failed\n");
+			goto out;
+		}
+
+		cmd[0] = CMD_EN25_PP;
+		cmd[1] = (offset >> 16) & 0xff;
+		cmd[2] = (offset >> 8) & 0xff;
+		cmd[3] = offset & 0xff;
 
 #if CONFIG_DEBUG_SPI_FLASH
 		printk(BIOS_SPEW,
@@ -96,38 +103,35 @@ static int eon_write(struct spi_flash *flash,
 		     buf + actual, cmd[0], cmd[1], cmd[2], cmd[3], chunk_len);
 #endif
 
-		ret = spi_flash_cmd(flash->spi, CMD_EN25Q128_WREN, NULL, 0);
-		if (ret < 0) {
-			printk(BIOS_WARNING, "SF: Enabling Write failed\n");
-			break;
-		}
-
-		ret = spi_flash_cmd_write(flash->spi, cmd, 4,
+		ret = spi_flash_cmd_write(flash->spi, cmd, sizeof(cmd),
 					  buf + actual, chunk_len);
 		if (ret < 0) {
 			printk(BIOS_WARNING, "SF: EON Page Program failed\n");
-			break;
+			goto out;
 		}
 
 		ret = spi_flash_cmd_wait_ready(flash, SPI_FLASH_PROG_TIMEOUT);
-		if (ret)
-			break;
+		if (ret) {
+			printk(BIOS_WARNING, "SF: EON Page Program timeout\n");
+			goto out;
+		}
 
-		page_addr++;
+		offset += chunk_len;
 		byte_addr = 0;
 	}
 
 #if CONFIG_DEBUG_SPI_FLASH
-	printk(BIOS_SPEW, "SF: EON: Successfully programmed %zu bytes @ 0x%x\n",
-	      len, offset);
+	printk(BIOS_SPEW, "SF: EON: Successfully programmed %zu bytes @ %#x\n",
+	       len, (unsigned int)(offset - len));
 #endif
 
+out:
 	return ret;
 }
 
 static int eon_erase(struct spi_flash *flash, u32 offset, size_t len)
 {
-	return spi_flash_cmd_erase(flash, CMD_EN25Q128_BE, offset, len);
+	return spi_flash_cmd_erase(flash, CMD_EN25_SE, offset, len);
 }
 
 struct spi_flash *spi_flash_probe_eon(struct spi_slave *spi, u8 *idcode)
@@ -138,12 +142,13 @@ struct spi_flash *spi_flash_probe_eon(struct spi_slave *spi, u8 *idcode)
 
 	for (i = 0; i < ARRAY_SIZE(eon_spi_flash_table); ++i) {
 		params = &eon_spi_flash_table[i];
-		if (params->idcode1 == idcode[2])
+		if (params->id == ((idcode[1] << 8) | idcode[2]))
 			break;
 	}
 
 	if (i == ARRAY_SIZE(eon_spi_flash_table)) {
-		printk(BIOS_WARNING, "SF: Unsupported EON ID %02x\n", idcode[1]);
+		printk(BIOS_WARNING, "SF: Unsupported EON ID %#02x%02x\n",
+		       idcode[1], idcode[2]);
 		return NULL;
 	}
 
@@ -160,8 +165,7 @@ struct spi_flash *spi_flash_probe_eon(struct spi_slave *spi, u8 *idcode)
 	eon->flash.write = eon_write;
 	eon->flash.erase = eon_erase;
 	eon->flash.read = spi_flash_cmd_read_fast;
-	eon->flash.sector_size = params->page_size * params->pages_per_sector
-	    * params->sectors_per_block;
+	eon->flash.sector_size = params->page_size * params->pages_per_sector;
 	eon->flash.size = params->page_size * params->pages_per_sector
 	    * params->nr_sectors;
 

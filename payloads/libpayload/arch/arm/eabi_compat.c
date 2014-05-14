d6b001e36d2371345ca1b9de99981d0389807868
Hung-Te Lin <hungte@chromium.org>
2014-05-14 12:43:57 +0800

    libpayload: arm: Add EABI compatible utility functions.
    Some EABI conformant toolchains like GCC need additional functions like raise.
    To prevent payloads adding arch-specific implementations everywhere, we should
    provide the default version in libpayload.
    
    BUG=none
    TEST=emerge-nyan libpayload # pass
    BRANCH=none
    
    Original-Change-Id: Id1e3c29590aa5881aefd944a7551949ce9a47b8f
    Original-Signed-off-by: Hung-Te Lin <hungte@chromium.org>
    Original-Reviewed-on: https://chromium-review.googlesource.com/199686
    (cherry picked from commit 395810c4b744dbb720050f79a2c1a30e81464554)
    Signed-off-by: Marc Jones <marc.jones@se-eng.com>
    
    Change-Id: I2e1d8c8cb519f8e788c22d081132d23b49b8f822
    Reviewed-on: http://review.coreboot.org/7906
    Reviewed-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

---
+++ payloads/libpayload/arch/arm/eabi_compat.c
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the libpayload project.
+ *
+ * Copyright (C) 2014 Google, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Function(s) needed for some EABI conformant tool chains (ex GCC).
+ */
+
+#include <die.h>
+
+int raise(int signum) __attribute__((weak));
+int raise(int signum)
+{
+	die("signal %d raised.\n", signum);
+	return -1;
+}
+
+/* ARM-defined Personality Routines, currently 3 in ARM EHABI. */
+
+void __aeabi_unwind_cpp_pr0(void) __attribute__((weak));
+void __aeabi_unwind_cpp_pr0(void)
+{
+}
+
+void __aeabi_unwind_cpp_pr1(void) __attribute__((weak));
+void __aeabi_unwind_cpp_pr1(void)
+{
+}
+
+void __aeabi_unwind_cpp_pr2(void) __attribute__((weak));
+void __aeabi_unwind_cpp_pr2(void)
+{
+}

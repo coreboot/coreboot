87eacacb35ee45128a6aac61fb657e6f2e374f09
Paul Menzel <paulepanter@users.sourceforge.net>
2014-06-05 07:52:49 +0200

    superio/nuvoton: Use official spelling of Nuvoton in `CHIP_NAME`
    The official spelling of Nuvoton is not all uppercase. Only the first
    letter is uppercase. See the footer of the Nuvoton Web site.
    
    Change-Id: I6ccd4194d7be0c89f8b332fcca5feb2420a4de1e
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-on: https://review.coreboot.org/5928
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/superio/nuvoton/wpcm450/superio.c
+++ src/superio/nuvoton/wpcm450/superio.c
@@ -56,6 +56,6 @@ static void enable_dev(struct device *dev)
 }
 
 struct chip_operations superio_nuvoton_wpcm450_ops = {
-	CHIP_NAME("NUVOTON WPCM450 Super I/O")
+	CHIP_NAME("Nuvoton WPCM450 Super I/O")
 	.enable_dev = enable_dev,
 };

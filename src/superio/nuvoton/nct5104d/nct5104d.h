0240f9492b79509c5b679190853405dc218244f0
Dave Frodin <dave.frodin@se-eng.com>
2014-06-05 12:01:36 -0600

    nuvoton/nct5104d: Update the #defined LDNs
    Change-Id: I4e4bc09a8f8fabe68519a29dc421af82c76c9873
    Signed-off-by: Dave Frodin <dave.frodin@se-eng.com>
    Reviewed-on: http://review.coreboot.org/5944
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/superio/nuvoton/nct5104d/nct5104d.h
+++ src/superio/nuvoton/nct5104d/nct5104d.h
@@ -2,6 +2,7 @@
  * This file is part of the coreboot project.
  *
  * Copyright (C) 2011 Advanced Micro Devices, Inc.
+ * Copyright (C) 2014 Sage Electronic Engineering, LLC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,10 +23,14 @@
 #define SUPERIO_NUVOTON_NCT5104D_NCT5104D_H
 
 /* Logical Device Numbers (LDN). */
-#define NCT5104D_SP1  0x02 /* Com1 */
-#define NCT5104D_SP2  0x03 /* Com2 */
-#define NCT5104D_GPIO_WDT 0x08 /* GPIO WDT Interface */
-#define NCT5104D_GPIO_PP_OD 0xF /* GPIO Push-Pull / Open drain select  */
+#define NCT5104D_FDC        0x00 /* FDC - not pinned out */
+#define NCT5104D_SP1        0x02 /* UARTA */
+#define NCT5104D_SP2        0x03 /* UARTB */
+#define NCT5104D_GPIO_WDT   0x08 /* GPIO WDT Interface */
+#define NCT5104D_GPIO_PP_OD 0x0F /* GPIO Push-Pull / Open drain select  */
+#define NCT5104D_SP3        0x10 /* UARTC */
+#define NCT5104D_SP4        0x11 /* UARTD */
+#define NCT5104D_PORT80     0x14 /* PORT 80 */
 
 /* Virtual Logical Device Numbers (LDN) */
 #define NCT5104D_GPIO_V 0x07 /* GPIO - 0,1,6 Interface */

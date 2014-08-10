1a59039c24cfe5c74a805064d3a360709ad16526
Bruce Griffith <Bruce.Griffith@se-eng.com>
2014-08-10 17:09:15 -0600

    AMD Steppe Eagle: New integrated southbridge (Avalon)
    00730F01 contains the Avalon southbridge and a Platform Security
    Processor (PSP). Supporting the PSP requires specific binaries to
    be included in the ROM.  The fletcher utility is used to sign PSP
    binaries.
    
    The IMC access routines are not accessible for newer AMD parts that
    use pre-compiled AGESA.  Change the Hudson code such that the IMC
    code is not compiled if IMC is not selected in Kconfig.
    
    Disable compilation of resume.c if HAVE_ACPI_RESUME is disabled.
    The newer AMD mainboards will initially be released without ACPI
    resume support (S3) due to the use of AGESA internals in the
    existing Hudson routines.  The Makefile change allows newer
    mainboards to avoid the API issues.
    
    Change Kconfig such that the FWM flag is always set for PSP-enabled
    parts.  This has the side effect of forcing the generation of the
    FWM directory in the absence of GEC, IMC, and xHCI.
    
    Change-Id: I6d056f54b60a64300841599490b9fafd561c4a7d
    Signed-off-by: Bruce Griffith <Bruce.Griffith@se-eng.com>
    Reviewed-on: http://review.coreboot.org/6677
    Tested-by: build bot (Jenkins)
    Reviewed-by: WANG Siyuan <wangsiyuanbuaa@gmail.com>
    Reviewed-by: Zheng Bao <zheng.bao@amd.com>

--- src/southbridge/amd/agesa/hudson/amd_pci_int_types.h
+++ src/southbridge/amd/agesa/hudson/amd_pci_int_types.h
@@ -39,6 +39,16 @@ const char * intr_types[] = {
 	[0x30] = "Dev18.0 INTA", "Dev18.2 INTB", "Dev19.0 INTA", "Dev19.2 INTB", "Dev22.0 INTA", "Dev22.2 INTB",
 	[0x40] = "RSVD\t", "SATA\t",
 };
+#elif IS_ENABLED(CONFIG_SOUTHBRIDGE_AMD_AGESA_AVALON)
+const char * intr_types[] = {
+	[0x00] = "INTA#\t", "INTB#\t", "INTC#\t", "INTD#\t", "INTE#\t", "INTF#\t", "INTG#\t", "INTH#\t",
+	[0x08] = "Misc\t", "Misc0\t", "Misc1\t", "Misc2\t", "Ser IRQ INTA", "Ser IRQ INTB", "Ser IRQ INTC", "Ser IRQ INTD",
+	[0x10] = "SCI\t", "SMBUS0\t", "ASF\t", "HDA\t", "FC\t\t", "GEC\t", "PerMon\t", "SD\t",
+	[0x20] = "IMC INT0\t", "IMC INT1\t", "IMC INT2\t", "IMC INT3\t", "IMC INT4\t", "IMC INT5\t",
+	[0x30] = "Dev18.0 INTA", "Dev18.2 INTB", "Dev19.0 INTA", "Dev19.2 INTB", "Dev22.0 INTA", "Dev22.2 INTB",
+	[0x40] = "RSVD\t", "SATA\t",
+	[0x60] = "RSVD\t", "RSVD\t", "GPIO\t",
+};
 #endif
 
 #endif /* AMD_PCI_INT_TYPES_H */

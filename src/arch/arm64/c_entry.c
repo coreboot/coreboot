30f08ff094a6d36ade1cdeb53ce6062852bed910
Aaron Durbin <adurbin@chromium.org>
2014-08-27 15:30:56 -0500

    arm64: move seeding stack to C
    Move the stack seeding out of assembly and into C so the
    code in stage_entry.S can more easily be used. The seeding
    of the stack doesn't touch at least 256 bytes to account
    for current usage at time fo the call.
    
    BUG=chrome-os-partner:31545
    BRANCH=None
    TEST=Built and booted into kernel on ryu.
    
    Change-Id: Ib9659ec4265652461bde746140567f21533cc265
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: f478cfe175aa674cdfdbbd890663eeaad9d82b1f
    Original-Change-Id: I44004220a02b1ff06d27a0555eb4e96d9e213544
    Original-Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Original-Reviewed-on: https://chromium-review.googlesource.com/214770
    Original-Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: http://review.coreboot.org/9014
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/arch/arm64/c_entry.c
+++ src/arch/arm64/c_entry.c
@@ -18,14 +18,34 @@
  */
 
 #include <arch/stages.h>
+#include <arch/cpu.h>
 
 void __attribute__((weak)) arm64_soc_init(void)
 {
 	/* Default weak implementation does nothing. */
 }
 
+static void seed_stack(void)
+{
+	char *stack_begin;
+	uint64_t *slot;
+	int i;
+	int size;
+
+	stack_begin = cpu_get_stack(smp_processor_id());
+	stack_begin -= CONFIG_STACK_SIZE;
+	slot = (void *)stack_begin;
+
+	/* Pad out 256 bytes for current usage. */
+	size = CONFIG_STACK_SIZE - 256;
+	size /= sizeof(*slot);
+	for (i = 0; i < size; i++)
+		*slot++ = 0xdeadbeefdeadbeefULL;
+}
+
 void arm64_init(void)
 {
+	seed_stack();
 	arm64_soc_init();
 	main();
 }

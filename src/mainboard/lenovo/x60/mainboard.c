23f04e47b81b382dda6348acab562ed6e7f45cca
Vladimir Serbinenko <phcoder@gmail.com>
2014-08-31 21:58:13 +0200

    lenovo/x60: Remove leftover declaration
    Change-Id: I1ab2118a3127dfacef6a389abd59050493e640fb
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>
    Reviewed-on: http://review.coreboot.org/6817
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>
    Tested-by: build bot (Jenkins)

--- src/mainboard/lenovo/x60/mainboard.c
+++ src/mainboard/lenovo/x60/mainboard.c
@@ -42,9 +42,6 @@
 #include <x86emu/x86emu.h>
 #define PANEL INT15_5F35_CL_DISPLAY_DEFAULT
 
-int i915lightup(unsigned int physbase, unsigned int iobase, unsigned int mmio,
-	unsigned int gfx);
-
 static acpi_cstate_t cst_entries[] = {
 	{ 1,  1, 1000, { 0x7f, 1, 2, { 0 }, 1, 0 } },
 	{ 2,  1,  500, { 0x01, 8, 0, { 0 }, DEFAULT_PMBASE + LV2, 0 } },

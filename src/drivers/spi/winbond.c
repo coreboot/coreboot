bdae9bedcdf5650abee089564c47ecbf2ba70f79
Mohan D'Costa <mohan@ndr.co.jp>
2014-09-25 14:40:44 +0900

    spi: Add support for Winbond W25Q128FW
    The W25Q128FW spi part is programatically equivalent
    to the other W25Q128 parts except it operates at 1.8V.
    Just add a new entry with the appropriate ID.
    
    Tested on a modified MinnowMax Board.
    
    Change-Id: Id6a426418a7f785a9d959b02a9e3d2ffc421804f
    Signed-off-by: Mohan D'Costa <mohan@ndr.co.jp>
    Reviewed-on: http://review.coreboot.org/6971
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Martin Roth <gaumless@gmail.com>
    Tested-by: build bot (Jenkins)

--- src/drivers/spi/winbond.c
+++ src/drivers/spi/winbond.c
@@ -110,6 +110,14 @@ static const struct winbond_spi_flash_params winbond_spi_flash_table[] = {
 		.nr_blocks		= 256,
 		.name			= "W25Q128",
 	},
+	{
+		.id			= 0x6018,
+		.l2_page_size		= 8,
+		.pages_per_sector	= 16,
+		.sectors_per_block	= 16,
+		.nr_blocks		= 256,
+		.name			= "W25Q128FW",
+	},
 };
 
 static int winbond_write(struct spi_flash *flash,

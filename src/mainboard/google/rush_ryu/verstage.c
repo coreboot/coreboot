b4006438855ec8d5e1c0e5012d7b0b85b5416dbb
Furquan Shaikh <furquan@google.com>
2014-10-04 17:01:48 -0700

    ryu: Add vboot2 support
    CQ-DEPEND=CL:221598, CL:*178568
    BUG=chrome-os-partner:32684
    BRANCH=None
    TEST=Compiles and boots to kernel prompt
    
    Original-Change-Id: If7c725333b45a92f951ab674c3e4bd6a51c180c2
    Original-Signed-off-by: Furquan Shaikh <furquan@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/221577
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Original-Tested-by: Furquan Shaikh <furquan@chromium.org>
    Original-Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    
    (cherry picked from commit 9f5a6ae8cb6e7136ab0f0158a864dfc8ccf5c24f)
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    
    Change-Id: If83dece2b4f2aa7d1457c723131efaa9b1169009
    Reviewed-on: http://review.coreboot.org/9431
    Tested-by: build bot (Jenkins)
    Reviewed-by: Furquan Shaikh <furquan@google.com>

---
+++ src/mainboard/google/rush_ryu/verstage.c
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2014 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <soc/addressmap.h>
+#include <soc/funitcfg.h>
+#include <soc/padconfig.h>
+#include <soc/verstage.h>
+#include <soc/nvidia/tegra/i2c.h>
+#include "gpio.h"
+#include "pmic.h"
+
+static const struct pad_config tpm_pads[] = {
+	PAD_CFG_SFIO(CAM_I2C_SCL, PINMUX_INPUT_ENABLE, I2C3),
+	PAD_CFG_SFIO(CAM_I2C_SDA, PINMUX_INPUT_ENABLE, I2C3),
+};
+
+static const struct pad_config ec_i2c_pads[] = {
+	PAD_CFG_SFIO(GEN2_I2C_SCL, PINMUX_OPEN_DRAIN|PINMUX_INPUT_ENABLE, I2C2),
+	PAD_CFG_SFIO(GEN2_I2C_SDA, PINMUX_OPEN_DRAIN|PINMUX_INPUT_ENABLE, I2C2),
+};
+
+static const struct funit_cfg funits[] = {
+	/* TPM on I2C3  @ 400kHz */
+	FUNIT_CFG(I2C3, PLLP, 400, tpm_pads, ARRAY_SIZE(tpm_pads)),
+	/* EC on I2C2 - pulled to 3.3V @ 100kHz */
+	FUNIT_CFG(I2C2, PLLP, 100, ec_i2c_pads, ARRAY_SIZE(ec_i2c_pads)),
+};
+
+void verstage_mainboard_init(void)
+{
+	soc_configure_funits(funits, ARRAY_SIZE(funits));
+
+	/* TPM */
+	i2c_init(I2C3_BUS);
+	/* EC */
+	i2c_init(I2C2_BUS);
+}

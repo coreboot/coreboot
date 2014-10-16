32005aa9587db57e2ddaf5dfcbe6280f6c876580
Vadim Bendebury <vbendeb@chromium.org>
2014-10-16 11:07:41 -0700

    bg4cd: add SPI controller driver skeleton
    This file provides the SOC specific SPI driver API, it needs to be
    filled up with code. Function descriptions can be found in
    src/include/spi-generic.h.
    
    BRANCH=none
    BUG=chrome-os-partner:32631
    TEST=compiles with the upcoming patches applied.
    
    Change-Id: I3546d5f9fb2971f4ccb7a57ce8164fd77686af72
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 0583f17fe3f6a258321765b91eae608e33577afe
    Original-Change-Id: I0ee04ca17874a13403007bba80d5e8a7708bc625
    Original-Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Original-Reviewed-on: https://chromium-review.googlesource.com/223719
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: http://review.coreboot.org/9355
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

---
+++ src/soc/marvell/bg4cd/spi.c
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the coreboot project.
+ * Copyright 2014 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <stddef.h>
+#include <spi-generic.h>
+
+struct spi_slave *spi_setup_slave(unsigned int bus, unsigned int cs)
+{
+	return NULL;
+}
+
+int spi_claim_bus(struct spi_slave *slave)
+{
+	return 0;
+}
+
+void spi_release_bus(struct spi_slave *slave)
+{
+}
+
+int spi_xfer(struct spi_slave *slave, const void *dout,
+	     unsigned out_bytes, void *din, unsigned in_bytes)
+{
+	return 0;
+}

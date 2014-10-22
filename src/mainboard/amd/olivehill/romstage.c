coreboot/coreboot

commit 7b23ae0e8938eb71453cbc28c2cc74c14a4039ae

AGESA: Trace execution with AGESAWRAPPER()
Implement logging just once to have uniform output.

Change-Id: I8db694a3bf6b1af459bdf98f7acb99edf4dd07f7
Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
Reviewed-on: http://review.coreboot.org/6180
Tested-by: build bot (Jenkins)
Reviewed-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

src/mainboard/amd/olivehill/romstage.c
@@ -31,6 +31,7 @@
 #include <console/loglevel.h>
 #include "cpu/amd/car.h"
 #include "agesawrapper.h"
+#include <northbridge/amd/agesa/agesawrapper_call.h>
 #include "cpu/x86/bist.h"
 #include "cpu/x86/lapic.h"
 #include "southbridge/amd/agesa/hudson/hudson.h"
@@ -50,7 +51,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	outb(0xD2, 0xcd6);
 	outb(0x00, 0xcd7);
 
-	val = agesawrapper_amdinitmmio();
+	AGESAWRAPPER(amdinitmmio);
 
 	hudson_lpc_port80();
 
@@ -76,56 +77,28 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 		val = inb(0xcd6);
 
 	post_code(0x37);
-	val = agesawrapper_amdinitreset();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitreset failed: %x \n", val);
-	}
-
+	AGESAWRAPPER(amdinitreset);
 	post_code(0x38);
 	printk(BIOS_DEBUG, "Got past yangtze_early_setup\n");
 
 	post_code(0x39);
 
-	val = agesawrapper_amdinitearly ();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitearly failed: %x \n", val);
-	}
-	printk(BIOS_DEBUG, "Got past agesawrapper_amdinitearly\n");
-
+	AGESAWRAPPER(amdinitearly);
 	int s3resume = acpi_is_wakeup_early() && acpi_s3_resume_allowed();
 	if (!s3resume) {
 		post_code(0x40);
-		val = agesawrapper_amdinitpost ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitpost failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitpost\n");
-
+		AGESAWRAPPER(amdinitpost);
 		post_code(0x41);
-		val = agesawrapper_amdinitenv ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitenv failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitenv\n");
+		AGESAWRAPPER(amdinitenv);
 		/* TODO: Disable cache is not ok. */
 		disable_cache_as_ram();
 	} else { /* S3 detect */
 		printk(BIOS_INFO, "S3 detected\n");
 
 		post_code(0x60);
-		printk(BIOS_DEBUG, "agesawrapper_amdinitresume ");
-		val = agesawrapper_amdinitresume();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amdinitresume);
 
-		printk(BIOS_DEBUG, "agesawrapper_amds3laterestore ");
-		val = agesawrapper_amds3laterestore ();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amds3laterestore);
 
 		post_code(0x61);
 		prepare_for_resume();

src/mainboard/amd/parmer/romstage.c
@@ -31,6 +31,7 @@
 #include <console/loglevel.h>
 #include "cpu/amd/car.h"
 #include "agesawrapper.h"
+#include <northbridge/amd/agesa/agesawrapper_call.h>
 #include "cpu/x86/bist.h"
 #include "cpu/x86/lapic.h"
 #include "southbridge/amd/agesa/hudson/hudson.h"
@@ -41,7 +42,7 @@
 void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 {
 	u32 val;
-	val = agesawrapper_amdinitmmio();
+	AGESAWRAPPER(amdinitmmio);
 
 	hudson_lpc_port80();
 
@@ -62,53 +63,24 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	printk(BIOS_DEBUG, "cpu_init_detectedx = %08lx \n", cpu_init_detectedx);
 
 	post_code(0x37);
-	printk(BIOS_DEBUG, "agesawrapper_amdinitreset ");
-	val = agesawrapper_amdinitreset();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitreset failed: %x \n", val);
-	}
-
+	AGESAWRAPPER(amdinitreset);
 	post_code(0x39);
 
-	val = agesawrapper_amdinitearly ();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitearly failed: %x \n", val);
-	}
-	printk(BIOS_DEBUG, "Got past agesawrapper_amdinitearly\n");
-
+	AGESAWRAPPER(amdinitearly);
 	int s3resume = acpi_is_wakeup_early() && acpi_s3_resume_allowed();
 	if (!s3resume) {
 		post_code(0x40);
-		val = agesawrapper_amdinitpost ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitpost failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitpost\n");
-
+		AGESAWRAPPER(amdinitpost);
 		post_code(0x41);
-		val = agesawrapper_amdinitenv ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitenv failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitenv\n");
+		AGESAWRAPPER(amdinitenv);
 		disable_cache_as_ram();
 	} else {		/* S3 detect */
 		printk(BIOS_INFO, "S3 detected\n");
 
 		post_code(0x60);
-		printk(BIOS_DEBUG, "agesawrapper_amdinitresume ");
-		val = agesawrapper_amdinitresume();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amdinitresume);
 
-		printk(BIOS_DEBUG, "agesawrapper_amds3laterestore ");
-		val = agesawrapper_amds3laterestore ();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amds3laterestore);
 
 		post_code(0x61);
 		prepare_for_resume();

src/mainboard/amd/thatcher/romstage.c
@@ -31,6 +31,7 @@
 #include <console/loglevel.h>
 #include "cpu/amd/car.h"
 #include "agesawrapper.h"
+#include <northbridge/amd/agesa/agesawrapper_call.h>
 #include "cpu/x86/bist.h"
 #include "cpu/x86/lapic.h"
 #include "southbridge/amd/agesa/hudson/hudson.h"
@@ -46,7 +47,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	u32 val;
 	u8 byte;
 	device_t dev;
-	val = agesawrapper_amdinitmmio();
+	AGESAWRAPPER(amdinitmmio);
 
 	hudson_lpc_port80();
 	//__asm__ volatile ("1: jmp 1b");
@@ -79,53 +80,24 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	printk(BIOS_DEBUG, "cpu_init_detectedx = %08lx \n", cpu_init_detectedx);
 
 	post_code(0x37);
-	printk(BIOS_DEBUG, "agesawrapper_amdinitreset ");
-	val = agesawrapper_amdinitreset();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitreset failed: %x \n", val);
-	}
-
+	AGESAWRAPPER(amdinitreset);
 	post_code(0x39);
 
-	val = agesawrapper_amdinitearly ();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitearly failed: %x \n", val);
-	}
-	printk(BIOS_DEBUG, "Got past agesawrapper_amdinitearly\n");
-
+	AGESAWRAPPER(amdinitearly);
 	int s3resume = acpi_is_wakeup_early() && acpi_s3_resume_allowed();
 	if (!s3resume) {
 		post_code(0x40);
-		val = agesawrapper_amdinitpost ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitpost failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitpost\n");
-
+		AGESAWRAPPER(amdinitpost);
 		post_code(0x41);
-		val = agesawrapper_amdinitenv ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitenv failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitenv\n");
+		AGESAWRAPPER(amdinitenv);
 		disable_cache_as_ram();
 	} else {		/* S3 detect */
 		printk(BIOS_INFO, "S3 detected\n");
 
 		post_code(0x60);
-		printk(BIOS_DEBUG, "agesawrapper_amdinitresume ");
-		val = agesawrapper_amdinitresume();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amdinitresume);
 
-		printk(BIOS_DEBUG, "agesawrapper_amds3laterestore ");
-		val = agesawrapper_amds3laterestore ();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amds3laterestore);
 
 		post_code(0x61);
 		prepare_for_resume();

src/mainboard/amd/torpedo/romstage.c
@@ -29,6 +29,7 @@
 #include <console/loglevel.h>
 #include "cpu/amd/car.h"
 #include "agesawrapper.h"
+#include <northbridge/amd/agesa/agesawrapper_call.h>
 #include "cpu/x86/bist.h"
 #include <superio/smsc/kbc1100/kbc1100.h>
 #include "cpu/x86/lapic.h"
@@ -44,12 +45,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	u32 val;
 
 	post_code(0x35);
-	printk(BIOS_DEBUG, "agesawrapper_amdinitmmio ");
-	val = agesawrapper_amdinitmmio();
-	if (val)
-		printk(BIOS_DEBUG, "error level: %x \n", val);
-	else
-		printk(BIOS_DEBUG, "passed.\n");
+	AGESAWRAPPER(amdinitmmio);
 
 	if (!cpu_init_detectedx && boot_cpu()) {
 		post_code(0x30);
@@ -76,28 +72,13 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	printk(BIOS_DEBUG, "cpu_init_detectedx = %08lx \n", cpu_init_detectedx);
 
 	post_code(0x36);
-	printk(BIOS_DEBUG, "agesawrapper_amdinitreset ");
-	val = agesawrapper_amdinitreset();
-	if (val)
-		printk(BIOS_DEBUG, "error level: %x \n", val);
-	else
-		printk(BIOS_DEBUG, "passed.\n");
+	AGESAWRAPPER(amdinitreset);
 
 	post_code(0x37);
-	printk(BIOS_DEBUG, "agesawrapper_amdinitearly ");
-	val = agesawrapper_amdinitearly();
-	if (val)
-		printk(BIOS_DEBUG, "error level: %x \n", val);
-	else
-		printk(BIOS_DEBUG, "passed.\n");
+	AGESAWRAPPER(amdinitearly);
 
 	post_code(0x38);
-	printk(BIOS_DEBUG, "agesawrapper_amdinitpost ");
-	val = agesawrapper_amdinitpost();
-	if (val)
-		printk(BIOS_DEBUG, "error level: %x \n", val);
-	else
-		printk(BIOS_DEBUG, "passed.\n");
+	AGESAWRAPPER(amdinitpost);
 
 	post_code(0x39);
 	printk(BIOS_DEBUG, "sb_before_pci_init ");
@@ -105,12 +86,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	printk(BIOS_DEBUG, "passed.\n");
 
 	post_code(0x40);
-	printk(BIOS_DEBUG, "agesawrapper_amdinitenv ");
-	val = agesawrapper_amdinitenv();
-	if (val)
-		printk(BIOS_DEBUG, "error level: %x \n", val);
-	else
-		printk(BIOS_DEBUG, "passed.\n");
+	AGESAWRAPPER(amdinitenv);
 
 	post_code(0x43);
 	copy_and_run();

src/mainboard/asrock/imb-a180/romstage.c
@@ -31,6 +31,7 @@
 #include <console/loglevel.h>
 #include "cpu/amd/car.h"
 #include "agesawrapper.h"
+#include <northbridge/amd/agesa/agesawrapper_call.h>
 #include "cpu/x86/bist.h"
 #include "cpu/x86/lapic.h"
 #include "southbridge/amd/agesa/hudson/hudson.h"
@@ -55,7 +56,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	//outb(0xD2, 0xcd6);
 	//outb(0x00, 0xcd7);
 
-	val = agesawrapper_amdinitmmio();
+	AGESAWRAPPER(amdinitmmio);
 
 	hudson_lpc_port80();
 
@@ -100,56 +101,28 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 		val = inb(0xcd6);
 
 	post_code(0x37);
-	val = agesawrapper_amdinitreset();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitreset failed: %x \n", val);
-	}
-
+	AGESAWRAPPER(amdinitreset);
 	post_code(0x38);
 	printk(BIOS_DEBUG, "Got past yangtze_early_setup\n");
 
 	post_code(0x39);
 
-	val = agesawrapper_amdinitearly ();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitearly failed: %x \n", val);
-	}
-	printk(BIOS_DEBUG, "Got past agesawrapper_amdinitearly\n");
-
+	AGESAWRAPPER(amdinitearly);
 	int s3resume = acpi_is_wakeup_early() && acpi_s3_resume_allowed();
 	if (!s3resume) {
 		post_code(0x40);
-		val = agesawrapper_amdinitpost ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitpost failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitpost\n");
-
+		AGESAWRAPPER(amdinitpost);
 		post_code(0x41);
-		val = agesawrapper_amdinitenv ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitenv failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitenv\n");
+		AGESAWRAPPER(amdinitenv);
 		/* TODO: Disable cache is not ok. */
 		disable_cache_as_ram();
 	} else { /* S3 detect */
 		printk(BIOS_INFO, "S3 detected\n");
 
 		post_code(0x60);
-		printk(BIOS_DEBUG, "agesawrapper_amdinitresume ");
-		val = agesawrapper_amdinitresume();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amdinitresume);
 
-		printk(BIOS_DEBUG, "agesawrapper_amds3laterestore ");
-		val = agesawrapper_amds3laterestore ();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amds3laterestore);
 
 		post_code(0x61);
 		prepare_for_resume();

src/mainboard/asus/f2a85-m/romstage.c
@@ -19,6 +19,7 @@
  */
 
 #include "agesawrapper.h"
+#include <northbridge/amd/agesa/agesawrapper_call.h>
 
 #include <arch/acpi.h>
 #include <arch/cpu.h>
@@ -75,7 +76,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	hudson_lpc_port80();
 #endif
 
-	val = agesawrapper_amdinitmmio();
+	AGESAWRAPPER(amdinitmmio);
 
 	if (!cpu_init_detectedx && boot_cpu()) {
 
@@ -127,53 +128,23 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	printk(BIOS_DEBUG, "cpu_init_detectedx = %08lx \n", cpu_init_detectedx);
 
 	post_code(0x37);
-	printk(BIOS_DEBUG, "agesawrapper_amdinitreset ");
-	val = agesawrapper_amdinitreset();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitreset failed: %x \n", val);
-	}
-
+	AGESAWRAPPER(amdinitreset);
 	post_code(0x39);
 
-	val = agesawrapper_amdinitearly ();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitearly failed: %x \n", val);
-	}
-	printk(BIOS_DEBUG, "Got past agesawrapper_amdinitearly\n");
-
+	AGESAWRAPPER(amdinitearly);
 	int s3resume = acpi_is_wakeup_early() && acpi_s3_resume_allowed();
 	if (!s3resume) {
 		post_code(0x40);
-		val = agesawrapper_amdinitpost ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitpost failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitpost\n");
-
+		AGESAWRAPPER(amdinitpost);
 		post_code(0x41);
-		val = agesawrapper_amdinitenv ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitenv failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitenv\n");
+		AGESAWRAPPER(amdinitenv);
 		disable_cache_as_ram();
 	} else {		/* S3 detect */
 		printk(BIOS_INFO, "S3 detected\n");
 
 		post_code(0x60);
-		printk(BIOS_DEBUG, "agesawrapper_amdinitresume ");
-		val = agesawrapper_amdinitresume();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
-
-		printk(BIOS_DEBUG, "agesawrapper_amds3laterestore ");
-		val = agesawrapper_amds3laterestore ();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amdinitresume);
+		AGESAWRAPPER(amds3laterestore);
 
 		post_code(0x61);
 		prepare_for_resume();

src/mainboard/hp/pavilion_m6_1035dx/romstage.c
@@ -18,6 +18,7 @@
  */
 
 #include "agesawrapper.h"
+#include <northbridge/amd/agesa/agesawrapper_call.h>
 
 #include <arch/acpi.h>
 #include <arch/cpu.h>
@@ -38,7 +39,7 @@
 void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 {
 	u32 val;
-	val = agesawrapper_amdinitmmio();
+	AGESAWRAPPER(amdinitmmio);
 
 	hudson_lpc_port80();
 
@@ -59,53 +60,24 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	printk(BIOS_DEBUG, "cpu_init_detectedx = %08lx \n", cpu_init_detectedx);
 
 	post_code(0x37);
-	printk(BIOS_DEBUG, "agesawrapper_amdinitreset ");
-	val = agesawrapper_amdinitreset();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitreset failed: %x \n", val);
-	}
-
+	AGESAWRAPPER(amdinitreset);
 	post_code(0x39);
 
-	val = agesawrapper_amdinitearly ();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitearly failed: %x \n", val);
-	}
-	printk(BIOS_DEBUG, "Got past agesawrapper_amdinitearly\n");
-
+	AGESAWRAPPER(amdinitearly);
 	int s3resume = acpi_is_wakeup_early() && acpi_s3_resume_allowed();
 	if (!s3resume) {
 		post_code(0x40);
-		val = agesawrapper_amdinitpost ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitpost failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitpost\n");
-
+		AGESAWRAPPER(amdinitpost);
 		post_code(0x41);
-		val = agesawrapper_amdinitenv ();
-		if(val) {
-			printk(BIOS_DEBUG, "agesawrapper_amdinitenv failed: %x \n", val);
-		}
-		printk(BIOS_DEBUG, "Got past agesawrapper_amdinitenv\n");
+		AGESAWRAPPER(amdinitenv);
 		disable_cache_as_ram();
 	} else {		/* S3 detect */
 		printk(BIOS_INFO, "S3 detected\n");
 
 		post_code(0x60);
-		printk(BIOS_DEBUG, "agesawrapper_amdinitresume ");
-		val = agesawrapper_amdinitresume();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amdinitresume);
 
-		printk(BIOS_DEBUG, "agesawrapper_amds3laterestore ");
-		val = agesawrapper_amds3laterestore ();
-		if (val)
-			printk(BIOS_DEBUG, "error level: %x \n", val);
-		else
-			printk(BIOS_DEBUG, "passed.\n");
+		AGESAWRAPPER(amds3laterestore);
 
 		post_code(0x61);
 		prepare_for_resume();

src/mainboard/supermicro/h8qgi/romstage.c
@@ -28,6 +28,7 @@
 #include "cpu/x86/lapic.h"
 #include "cpu/amd/car.h"
 #include "agesawrapper.h"
+#include <northbridge/amd/agesa/agesawrapper_call.h>
 #include "northbridge/amd/agesa/family10/reset_test.h"
 #include <nb_cimx.h>
 #include <sb_cimx.h>
@@ -65,12 +66,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	printk(BIOS_DEBUG, "cpu_init_detectedx = %08lx \n", cpu_init_detectedx);
 
 	post_code(0x37);
-	val = agesawrapper_amdinitreset();
-	if (val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitreset failed: %x \n", val);
-	} else {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitreset passed\n");
-	}
+	AGESAWRAPPER(amdinitreset);
 
 	if (!cpu_init_detectedx && boot_cpu()) {
 		post_code(0x38);
@@ -85,12 +81,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 		sb_Poweron_Init();
 	}
 	post_code(0x3B);
-	val = agesawrapper_amdinitearly();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitearly failed: %x \n", val);
-	} else {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitearly passed\n");
-	}
+	AGESAWRAPPER(amdinitearly);
 
 	post_code(0x3C);
 	/* W83627DHG pin89,90 function select is RSTOUT3#, RSTOUT2# by default.
@@ -110,20 +101,10 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	}
 
 	post_code(0x40);
-	val = agesawrapper_amdinitpost();
-	if (val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitpost failed: %x \n", val);
-	} else {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitpost passed\n");
-	}
+	AGESAWRAPPER(amdinitpost);
 
 	post_code(0x41);
-	val = agesawrapper_amdinitenv();
-	if(val) {
-		printk(BIOS_DEBUG, "agesawrapper_amdinitenv failed: %x \n", val);
-	}
-	printk(BIOS_DEBUG, "agesawrapper_amdinitenv passed\n");
-
+	AGESAWRAPPER(amdinitenv);
 	post_code(0x42);
 
 	post_code(0x50);

commit 30fbb4c23e7b867c707d61520c05cc4c6c17e174

AGESA boards: Fix early agesawrapper_amdinitmmio()
Regression introduced with commit

   7b23ae0 AGESA: Trace execution with AGESAWRAPPER()

As the call is made before console_init() is called it must
not call any printk(). Debugging Olivehill and Parmer platforms
using a custom FPGA (as these boards have no Super-IO UART) have
been observed to halt and/or delay at early boot.

Change-Id: I3ab4e5378db44aece9046c8636cde1053ce5390d
Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
Reviewed-on: http://review.coreboot.org/7059
Reviewed-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>
Tested-by: build bot (Jenkins)
Reviewed-by: WANG Siyuan <wangsiyuanbuaa@gmail.com>

src/mainboard/amd/olivehill/romstage.c
@@ -51,7 +51,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	outb(0xD2, 0xcd6);
 	outb(0x00, 0xcd7);
 
-	AGESAWRAPPER(amdinitmmio);
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 
 	hudson_lpc_port80();
 

src/mainboard/amd/olivehillplus/romstage.c
@@ -41,7 +41,6 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 {
 	u32 val;
 	volatile int halt = 0;
-	AGESA_STATUS status = AGESA_UNSUPPORTED;
 
 	/*
 	 *  In Hudson RRG, PMIOxD2[5:4] is "Drive strength control for
@@ -54,14 +53,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	outb(0xD2, 0xcd6);
 	outb(0x00, 0xcd7);
 
-	/*
-	 * The following should be a call to AGESAWRAPPER() macro, but
-	 * that would use console output before it is initialized.
-	 */
-	status = agesawrapper_amdinitmmio();
-	if (AGESA_SUCCESS != status) {
-		printk(BIOS_WARNING, "AmdInitMmio reported %s\n", decodeAGESA_STATUS(status));
-	}
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 
 	hudson_lpc_port80();
 

src/mainboard/amd/parmer/romstage.c
@@ -42,7 +42,8 @@
 void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 {
 	u32 val;
-	AGESAWRAPPER(amdinitmmio);
+
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 
 	hudson_lpc_port80();
 

src/mainboard/amd/thatcher/romstage.c
@@ -47,7 +47,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	u32 val;
 	u8 byte;
 	device_t dev;
-	AGESAWRAPPER(amdinitmmio);
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 
 	hudson_lpc_port80();
 	//__asm__ volatile ("1: jmp 1b");

src/mainboard/amd/torpedo/romstage.c
@@ -45,7 +45,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	u32 val;
 
 	post_code(0x35);
-	AGESAWRAPPER(amdinitmmio);
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 
 	if (!cpu_init_detectedx && boot_cpu()) {
 		post_code(0x30);

src/mainboard/asrock/imb-a180/romstage.c
@@ -56,7 +56,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	//outb(0xD2, 0xcd6);
 	//outb(0x00, 0xcd7);
 
-	AGESAWRAPPER(amdinitmmio);
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 
 	hudson_lpc_port80();
 

src/mainboard/asus/f2a85-m/romstage.c
@@ -76,7 +76,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	hudson_lpc_port80();
 #endif
 
-	AGESAWRAPPER(amdinitmmio);
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 
 	if (!cpu_init_detectedx && boot_cpu()) {
 

src/mainboard/hp/pavilion_m6_1035dx/romstage.c
@@ -39,7 +39,7 @@
 void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 {
 	u32 val;
-	AGESAWRAPPER(amdinitmmio);
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 
 	hudson_lpc_port80();
 

src/mainboard/supermicro/h8qgi/romstage.c
@@ -46,7 +46,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	u32 val;
 
 	post_code(0x30);
-	agesawrapper_amdinitmmio();
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 	post_code(0x31);
 
 	/* Halt if there was a built in self test failure */

src/mainboard/supermicro/h8scm/romstage.c
@@ -45,7 +45,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	u32 val;
 
 	post_code(0x30);
-	agesawrapper_amdinitmmio();
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 	post_code(0x31);
 
 	/* Halt if there was a built in self test failure */

src/mainboard/tyan/s8226/romstage.c
@@ -44,7 +44,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	u32 val;
 
 	post_code(0x30);
-	agesawrapper_amdinitmmio();
+	AGESAWRAPPER_PRE_CONSOLE(amdinitmmio);
 	post_code(0x31);
 
 	/* Halt if there was a built in self test failure */

src/northbridge/amd/agesa/agesawrapper_call.h
@@ -57,4 +57,6 @@ static inline u32 do_agesawrapper(AGESA_STATUS (*func)(void), const char *name)
 
 #define AGESAWRAPPER(func) do_agesawrapper(agesawrapper_ ## func, #func)
 
+#define AGESAWRAPPER_PRE_CONSOLE(func) agesawrapper_ ## func()
+
 #endif
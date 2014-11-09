bf8722aac82d926380f84530ec92d10291693260
Vladimir Serbinenko <phcoder@gmail.com>
2014-11-09 13:17:39 +0100

    Make set_bios_reset into normal rather than weak function
    Change-Id: I2efa254537f83fe689fd07fe6ec80f0446ad5a9d
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>
    Reviewed-on: http://review.coreboot.org/7370
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Tested-by: build bot (Jenkins)
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

--- src/northbridge/amd/amdfam10/reset_test.c
+++ src/northbridge/amd/amdfam10/reset_test.c
@@ -83,8 +83,8 @@ static u32 warm_reset_detect(u8 nodeid)
 	return (htic & HTIC_ColdR_Detect) && !(htic & HTIC_BIOSR_Detect);
 }
 
-void __attribute__ ((weak)) set_bios_reset(void);
-void __attribute__ ((weak)) set_bios_reset(void)
+void set_bios_reset(void);
+void set_bios_reset(void)
 {
 
 	u32 nodes;
--- src/northbridge/amd/amdk8/reset_test.c
+++ src/northbridge/amd/amdk8/reset_test.c
@@ -44,8 +44,8 @@ static inline void distinguish_cpu_resets(unsigned nodeid)
 	pci_write_config32(device, HT_INIT_CONTROL, htic);
 }
 
-void __attribute__ ((weak)) set_bios_reset(void);
-void __attribute__ ((weak)) set_bios_reset(void)
+void set_bios_reset(void);
+void set_bios_reset(void)
 {
 	u32 htic;
 	htic = pci_read_config32(PCI_DEV(0, 0x18, 0), HT_INIT_CONTROL);

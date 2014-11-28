1b2f2a071488bd15ce80194e85d318cd44659e54
Patrick Georgi <pgeorgi@google.com>
2014-11-28 22:00:22 +0100

    Introduce halt()
    It's a portable and generic way to halt the system.
    Useful when waiting for the platform to reset.
    
    Change-Id: Ie07f3333d294a4d3e982cbc2ab9014c94b39fce0
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: http://review.coreboot.org/7605
    Tested-by: build bot (Jenkins)
    Reviewed-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

---
+++ src/include/halt.h
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2014 The ChromiumOS Authors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#ifndef __HALT_H__
+#define __HALT_H__
+
+#ifdef __ROMCC__
+#include <lib/halt.c>
+#else
+/**
+ * halt the system reliably
+ */
+void __attribute__((noreturn)) halt(void);
+#endif /* __ROMCC__ */
+#endif /* __HALT_H__ */
---
+++ src/lib/halt.c
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2014 The ChromiumOS Authors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#include <arch/hlt.h>
+#include <halt.h>
+
+void halt(void) {
+	while (1) {
+		hlt();
+	}
+}

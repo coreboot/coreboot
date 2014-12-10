9d0ae597d7978f4d1dff201da7e48749e8f9af57
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-10 16:44:44 -0600

    lib/edid.c: Use 'hh' instead of 'h' length modifier in printk
    The length modifiers for unsigned char were accidentally changed from
    'hh' to 'h' with commit:
    
    * 1c8ee21 edid: Change static variables to auto variables.
    
    Change-Id: I4b3e63cbcde7635b842894f776373f7946bd0df8
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Reviewed-on: http://review.coreboot.org/4477
    Tested-by: build bot (Jenkins)
    Reviewed-by: Marc Jones <marc.jones@se-eng.com>

--- src/lib/edid.c
+++ src/lib/edid.c
@@ -525,14 +525,14 @@ static int
 do_checksum(unsigned char *x)
 {
 	int valid = 0;
-	printk(BIOS_SPEW, "Checksum: 0x%hx", x[0x7f]);
+	printk(BIOS_SPEW, "Checksum: 0x%hhx", x[0x7f]);
 	{
 		unsigned char sum = 0;
 		int i;
 		for (i = 0; i < 128; i++)
 			sum += x[i];
 		if (sum) {
-			printk(BIOS_SPEW, " (should be 0x%hx)", (unsigned char)(x[0x7f] - sum));
+			printk(BIOS_SPEW, " (should be 0x%hhx)", (unsigned char)(x[0x7f] - sum));
 		} else {
 			valid = 1;
 			printk(BIOS_SPEW, " (valid)");
@@ -1003,7 +1003,7 @@ int decode_edid(unsigned char *edid, int size, struct edid *out)
 		if (edid[0x11] > 0x0f) {
 			if (edid[0x10] == 0xff) {
 				c.has_valid_year = 1;
-				printk(BIOS_SPEW, "Made week %hd of model year %hd\n", edid[0x10],
+				printk(BIOS_SPEW, "Made week %hhd of model year %hhd\n", edid[0x10],
 				       edid[0x11]);
 				out->week = edid[0x10];
 				out->year = edid[0x11];
@@ -1011,7 +1011,7 @@ int decode_edid(unsigned char *edid, int size, struct edid *out)
 				/* we know it's at least 2013, when this code was written */
 				if (edid[0x11] + 90 <= 2013) {
 					c.has_valid_year = 1;
-					printk(BIOS_SPEW, "Made week %hd of %hd\n",
+					printk(BIOS_SPEW, "Made week %hhd of %hhd\n",
 					       edid[0x10], edid[0x11] + 1990);
 					out->week = edid[0x10];
 					out->year = edid[0x11] + 1990;
@@ -1020,7 +1020,7 @@ int decode_edid(unsigned char *edid, int size, struct edid *out)
 		}
 	}
 
-	printk(BIOS_SPEW, "EDID version: %hd.%hd\n", edid[0x12], edid[0x13]);
+	printk(BIOS_SPEW, "EDID version: %hhd.%hhd\n", edid[0x12], edid[0x13]);
 	out->version[0] = edid[0x12];
 	out->version[1] = edid[0x13];
 

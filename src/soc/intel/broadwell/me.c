8cc5dc1dd2d7373ebbbb5eccafda144a8d59fdfe
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2015-01-07 15:50:43 +1100

    soc/intel/broadwell/me.c: Prevent unused function warning
    Put function under same guard as its call site so that the
    compiler does not emit a warn about unused functions upon
    a false branch of the guard.
    
    Change-Id: I899d539ec5fbb87e7469415cc8d15837ba8e63f3
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>
    Reviewed-on: http://review.coreboot.org/8156
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Marc Jones <marc.jones@se-eng.com>
    Tested-by: build bot (Jenkins)

--- src/soc/intel/broadwell/me.c
+++ src/soc/intel/broadwell/me.c
@@ -477,12 +477,6 @@ void intel_me_mbp_clear(device_t dev)
 }
 
 #if (CONFIG_DEFAULT_CONSOLE_LOGLEVEL >= BIOS_DEBUG)
-static inline void print_cap(const char *name, int state)
-{
-	printk(BIOS_DEBUG, "ME Capability: %-41s : %sabled\n",
-	       name, state ? " en" : "dis");
-}
-
 static void me_print_fw_version(mbp_fw_version_name *vers_name)
 {
 	if (!vers_name) {
@@ -496,6 +490,12 @@ static void me_print_fw_version(mbp_fw_version_name *vers_name)
 }
 
 #if CONFIG_DEBUG_INTEL_ME
+static inline void print_cap(const char *name, int state)
+{
+	printk(BIOS_DEBUG, "ME Capability: %-41s : %sabled\n",
+	       name, state ? " en" : "dis");
+}
+
 /* Get ME Firmware Capabilities */
 static int mkhi_get_fwcaps(mbp_mefwcaps *cap)
 {
@@ -534,7 +534,7 @@ static void me_print_fwcaps(mbp_mefwcaps *cap)
 	print_cap("IntelR Capability Licensing Service (CLS)", cap->intel_cls);
 	print_cap("IntelR Power Sharing Technology (MPC)", cap->intel_mpc);
 	print_cap("ICC Over Clocking", cap->icc_over_clocking);
-        print_cap("Protected Audio Video Path (PAVP)", cap->pavp);
+	print_cap("Protected Audio Video Path (PAVP)", cap->pavp);
 	print_cap("IPV6", cap->ipv6);
 	print_cap("KVM Remote Control (KVM)", cap->kvm);
 	print_cap("Outbreak Containment Heuristic (OCH)", cap->och);

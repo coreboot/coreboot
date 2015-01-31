d161aba09b6512beea0f647f9ecd8dc10fd15808
Jonathan A. Kollasch <jakllsch@kollasch.net>
2015-01-31 14:23:36 -0600

    winent/mb6047: Remove redundant P-State generation
    Recently ck804/lpc.c started generating pstates for us.
    
    Change-Id: Ie47fff0516e0e838fdcd5084074ce2cabfe7e290
    Signed-off-by: Jonathan A. Kollasch <jakllsch@kollasch.net>
    Reviewed-on: http://review.coreboot.org/8318
    Tested-by: build bot (Jenkins)
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/mainboard/winent/mb6047/mainboard.c
+++ src/mainboard/winent/mb6047/mainboard.c
@@ -1,18 +1,7 @@
-#include <console/console.h>
 #include <device/device.h>
-#include <arch/acpi.h>
-#include <cpu/amd/powernow.h>
-#include <arch/acpi.h>
-#include <arch/acpigen.h>
-#include <cpu/amd/amdk8_sysconf.h>
-
-static void mainboard_acpi_fill_ssdt_generator(device_t device) {
-	amd_generate_powernow(0, 0, 0);
-}
 
 static void mainboard_enable(device_t dev)
 {
-	dev->ops->acpi_fill_ssdt_generator = mainboard_acpi_fill_ssdt_generator;
 }
 
 struct chip_operations mainboard_ops = {

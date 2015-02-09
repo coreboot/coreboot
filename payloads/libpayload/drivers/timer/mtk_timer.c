7267ccbae861d6acf7edfa256b758e2836e21945
Yidi Lin <yidi.lin@mediatek.com>
2015-02-09 16:34:44 +0800

    libpayload: add mediatek timer driver
    Change-Id: Ifb19cf97d4db6c7394521e549968a0cfb6ed1c75
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 0137652ca07e290bb3cb1cc82a00b44ac7bcc7bf
    Original-Change-Id: Ica649927d3533c847b24e520e8fe73d75fb9e786
    Original-Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/257375
    Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: http://review.coreboot.org/10170
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

---
+++ payloads/libpayload/drivers/timer/mtk_timer.c
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2014 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+#include <arch/io.h>
+#include <libpayload.h>
+#include <libpayload-config.h>
+#include <stdint.h>
+
+static uint32_t *const mtk_tmrus = (void*)CONFIG_LP_TIMER_MTK_ADDRESS;
+
+uint64_t timer_hz(void)
+{
+	return CONFIG_LP_TIMER_MTK_HZ;
+}
+
+uint64_t timer_raw_value(void)
+{
+	return (uint64_t)readl(mtk_tmrus);
+}

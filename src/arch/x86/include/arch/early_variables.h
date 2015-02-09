coreboot/coreboot

commit 27cf24727c26874cbe61734d01074f6da077d1b9

car globals: add "used" attribute
Otherwise clang feels free to optimize away that variable
(somewhat) and revive it in a different form inside .bss.

They probably have the language lawyery excuse for why
that's perfectly legal, so let's play it safe.
(relevant URL, sorry ron: http://llvm.org/bugs/show_bug.cgi?id=9520)

Change-Id: I603312ceea7207088dd29453cc8fb8f48c31af21
Signed-off-by: Patrick Georgi <patrick@georgi-clan.de>
Reviewed-on: http://review.coreboot.org/7357
Reviewed-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>
Tested-by: build bot (Jenkins)

src/arch/x86/include/arch/early_variables.h
@@ -23,7 +23,7 @@
 #ifdef __PRE_RAM__
 asm(".section .car.global_data,\"w\",@nobits");
 asm(".previous");
-#define CAR_GLOBAL __attribute__((section(".car.global_data")))
+#define CAR_GLOBAL __attribute__((used,section(".car.global_data")))
 #else
 #define CAR_GLOBAL
 #endif

commit ad4fa21705e66a0f350597da043c845aa79bce99

Get rid of .car.global_data warnings for GCC build
The "used" attribute was added in commit 27cf2472 which caused these
warnings to start appearing when using the standard coreboot GCC
toolchain:

{standard input}: Assembler messages:
{standard input}:96: Warning: ignoring changed section type for .car.global_data
{standard input}:96: Warning: ignoring changed section attributes for
.car.global_data

The # at the end of the section name causes the assembler to
ignore everything following the name. I verified that the resulting
binaries are the same with and without the #.		

Change-Id: Iaac8042533842ed887f33895f083b613a18f496a
Signed-off-by: Martin Roth <gaumless@gmail.com>
Reviewed-on: http://review.coreboot.org/8301
Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
Tested-by: build bot (Jenkins)
Reviewed-by: Aaron Durbin <adurbin@google.com>

src/arch/x86/include/arch/early_variables.h
@@ -23,7 +23,11 @@
 #ifdef __PRE_RAM__
 asm(".section .car.global_data,\"w\",@nobits");
 asm(".previous");
+#ifdef __clang__
 #define CAR_GLOBAL __attribute__((used,section(".car.global_data")))
+#else
+#define CAR_GLOBAL __attribute__((used,section(".car.global_data#")))
+#endif /* __clang__ */
 #else
 #define CAR_GLOBAL
 #endif
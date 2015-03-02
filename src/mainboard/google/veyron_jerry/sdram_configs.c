5792e3b71f205446155bb40bea9c52dfe1969d8b
jinkun.hong <jinkun.hong@rock-chips.com>
2015-03-02 14:10:50 +0800

    veyron_jerry: support K4B8G1646Q-4GB and H5TC8G63XXX-4GB ddr3
    add the K4B8G1646Q-4GB and H5TC8G63XXX-4GB ddr3 inf file,
    and use ram_id 1110 correspond to K4B8G1646Q-4GB ddr3
    use ram_id 1111 correspond to H5TC8G63XXX-4GB ddr3
    
    BUG=None
    TEST=Boot veyron_jerry normal
    BRANCH=None
    
    Change-Id: I3398516a9f2c2e44c9f5d08d0a3ab6e76b5c6f5f
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: b8dfc455bb93c2daf567e3b6e39c0a715e44311c
    Original-Change-Id: I90250cb84eb140f93c4fc655fb3b90584dd515c0
    Original-Signed-off-by: jinkun.hong <jinkun.hong@rock-chips.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/255010
    Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: http://review.coreboot.org/9826
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/mainboard/google/veyron_jerry/sdram_configs.c
+++ src/mainboard/google/veyron_jerry/sdram_configs.c
@@ -39,8 +39,8 @@ static struct rk3288_sdram_params sdram_configs[] = {
 #include "sdram_inf/sdram-unused.inc"			/* ram_code = 1011 */
 #include "sdram_inf/sdram-unused.inc"			/* ram_code = 1100 */
 #include "sdram_inf/sdram-unused.inc"			/* ram_code = 1101 */
-#include "sdram_inf/sdram-unused.inc"			/* ram_code = 1110 */
-#include "sdram_inf/sdram-unused.inc"			/* ram_code = 1111 */
+#include "sdram_inf/sdram-ddr3-K4B8G1646Q-4GB.inc"	/* ram_code = 1110 */
+#include "sdram_inf/sdram-ddr3-H5TC8G63XXX-4GB.inc"	/* ram_code = 1111 */
 };
 
 const struct rk3288_sdram_params *get_sdram_config()

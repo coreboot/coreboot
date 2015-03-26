5538e50bc46a1402f7c3b955ca5c74adc1d91cc9
Patrick Georgi <pgeorgi@chromium.org>
2015-03-26 14:09:07 +0100

    armv4: Build in SMP settings with non-SMP armv4 CPUs
    There are some ARMv8/ARMv4 SoC where the ARMv8 part needs to be
    SMP aware but the ARMv4 part does not.
    Until we need real SMP on ARMv4, work around that situation
    with stub defines.
    
    Change-Id: Iec5b4302b19c17fe2b3f677b84a8edf4b4902946
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-on: http://review.coreboot.org/9046
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

---
+++ src/arch/arm/include/armv4/arch/smp/spinlock.h
@@ -0,0 +1,15 @@
+#ifndef _ARCH_SMP_SPINLOCK_H
+#define _ARCH_SMP_SPINLOCK_H
+
+#define DECLARE_SPIN_LOCK(x)
+#define barrier()		do {} while(0)
+#define spin_is_locked(lock)	0
+#define spin_unlock_wait(lock)	do {} while(0)
+#define spin_lock(lock)		do {} while(0)
+#define spin_unlock(lock)	do {} while(0)
+#define cpu_relax()		do {} while(0)
+
+#include <smp/node.h>
+#define boot_cpu() 1
+
+#endif

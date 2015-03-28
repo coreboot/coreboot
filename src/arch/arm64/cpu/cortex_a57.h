36d3586e787addff57bb835c3aa1664b6c8ece94
Furquan Shaikh <furquan@google.com>
2015-03-27 22:55:59 -0700

    arm64: Allow cpu specific early setup
    Call arm64_cpu_early_setup to allow cpu-specific initialization to be
    performed. Also, add support for setting SMPEN bit for cortex a57
    within arm64_cpu_early_setup.
    
    BUG=chrome-os-partner:38222
    BRANCH=None
    TEST=Compiles successfully and SMP works for foster
    
    Change-Id: Ifa4e6134dbce3ad63046b3dd9b947c3d9134d5e7
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: fe0eac5f416efcf9f7b05388a17444205a8352c0
    Original-Change-Id: I28a05a20e6adf084cd0bf94bdd0c3b492632107c
    Original-Signed-off-by: Furquan Shaikh <furquan@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/262993
    Original-Trybot-Ready: Furquan Shaikh <furquan@chromium.org>
    Original-Tested-by: Furquan Shaikh <furquan@chromium.org>
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Original-Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: http://review.coreboot.org/9977
    Tested-by: build bot (Jenkins)
    Reviewed-by: Marc Jones <marc.jones@se-eng.com>

---
+++ src/arch/arm64/cpu/cortex_a57.h
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2015 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA, 02110-1301 USA
+ */
+
+#ifndef __ARCH_ARM64_CORTEX_A57_H__
+#define __ARCH_ARM64_CORTEX_A57_H__
+
+#define CPUECTLR_EL1	S3_1_c15_c2_1
+#define SMPEN_SHIFT	6
+
+#endif /* __ARCH_ARM64_CORTEX_A57_H__ */

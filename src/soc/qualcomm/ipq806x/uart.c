mtk09422/chromiumos-third_party-coreboot

ipq806x: Change all SoC headers to <soc/headername.h> system
This patch aligns ipq806x to the new SoC header include scheme.
Also alphabetized headers in affected files since we touch them anyway.

BUG=None
TEST=Tested with whole series. Compiled Storm.

Change-Id: I283cc7e6094be977d67ed4146f376cebcea6774a
Signed-off-by: Julius Werner <jwerner@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/224502
Reviewed-by: Aaron Durbin <adurbin@chromium.org>

src/soc/qualcomm/ipq806x/uart.c
@@ -34,12 +34,12 @@
 #include <arch/io.h>
 #include <console/console.h>
 #include <delay.h>
-#include <clock.h>
-#include <gpio.h>
-#include <gsbi.h>
-#include <uart.h>
-#include <ipq_uart.h>
+#include <soc/clock.h>
+#include <soc/gpio.h>
+#include <soc/gsbi.h>
+#include <soc/ipq_uart.h>
 #include <stdint.h>
+#include <uart.h>
 
 #define FIFO_DATA_SIZE	4
 

coreboot/coreboot

Change-Id: Icb81a77e6f458625f5379a980e8760388dd3a1f9
Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
Original-Commit-Id: 1bf23774c9ffa5d08c211f3658d39adcfa47b339
Original-Change-Id: I283cc7e6094be977d67ed4146f376cebcea6774a
Original-Signed-off-by: Julius Werner <jwerner@chromium.org>
Original-Reviewed-on: https://chromium-review.googlesource.com/224502
Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
Reviewed-on: http://review.coreboot.org/9368
Reviewed-by: Patrick Georgi <pgeorgi@google.com>
Tested-by: Patrick Georgi <pgeorgi@google.com>

src/soc/qualcomm/ipq806x/uart.c
@@ -34,12 +34,12 @@
 #include <arch/io.h>
 #include <boot/coreboot_tables.h>
 #include <console/console.h>
-#include <delay.h>
-#include <clock.h>
-#include <gpio.h>
-#include <gsbi.h>
 #include <console/uart.h>
-#include <ipq_uart.h>
+#include <delay.h>
+#include <soc/clock.h>
+#include <soc/gpio.h>
+#include <soc/gsbi.h>
+#include <soc/ipq_uart.h>
 #include <stdint.h>
 #include <stdlib.h>
 
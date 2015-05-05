d6fac38d771119399118516f8066c1564ea7fcec
Stefan Reinauer <stefan.reinauer@coreboot.org>
2015-05-05 13:35:18 -0700

    vgabios: fix compilation
    This utility links in coreboot code, and has been broken for a long
    time. These changes get it to compile again.
    
    Change-Id: I69445a8b3cbfc9a2b560c68b8de2e080837ec502
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Reviewed-on: http://review.coreboot.org/10112
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

---
+++ util/vgabios/include/stdtypes.h
@@ -0,0 +1,18 @@
+#ifndef _STDTYPES_H
+#define _STDTYPES_H
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+
+typedef uint8_t bool;
+#define true	1
+#define false	0
+
+#endif /* _STDTYPES_H */

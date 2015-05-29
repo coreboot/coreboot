641529b0678d7399c9072f23891151ecfb13ae27
Wenkai Du <wenkai.du@intel.com>
2015-05-29 10:54:27 -0700

    TPM: Add Infineon SLB9670 SPI TPM support
    This patch provides support for TPM Infineon SLB9670 by adding its
    device ID to the list.
    
    BRANCH=None
    BUG=chrome-os-partner:40640
    TEST=Built and test SLB9670 on SKL U Reference board Fab 2
    
    Change-Id: I2d26fc6c7d074881f2e6189e1325808544b7d26d
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 3c92884be75b631c302801e162292c245ed7bf5d
    Original-Change-Id: I4607fc96f70175b2461b40ba61e7a821e187de40
    Original-Signed-off-by: Wenkai Du <wenkai.du@intel.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/274053
    Original-Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: http://review.coreboot.org/10387
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/drivers/pc80/tpm/tpm.c
+++ src/drivers/pc80/tpm/tpm.c
@@ -128,6 +128,7 @@ static const struct device_name atmel_devices[] = {
 static const struct device_name infineon_devices[] = {
 	{0x000b, "SLB9635 TT 1.2"},
 	{0x001a, "SLB9660 TT 1.2"},
+	{0x001b, "SLB9670 TT 1.2"},
 	{0xffff}
 };
 

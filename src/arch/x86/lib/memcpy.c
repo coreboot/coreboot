7c35af2bc3152cfc678561465f60884d56380632
Stefan Reinauer <stefan.reinauer@coreboot.org>
2015-06-17 16:09:10 -0700

    x86: make memcpy 64bit safe
    This does not optimize memcpy for 64bit, it merely makes it compile.
    
    Change-Id: I69ad6bd0c3d5f617d9222643abf7a2ba7c2a0359
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Signed-off-by: Scott Duplichan <scott@notabs.org>
    Reviewed-on: http://review.coreboot.org/10575
    Tested-by: build bot (Jenkins)
    Reviewed-by: Marc Jones <marc.jones@se-eng.com>

--- src/arch/x86/lib/memcpy.c
+++ src/arch/x86/lib/memcpy.c
@@ -5,8 +5,13 @@ void *memcpy(void *dest, const void *src, size_t n)
 	unsigned long d0, d1, d2;
 
 	asm volatile(
+#ifdef __x86_64__
+		"rep ; movsd\n\t"
+		"mov %4,%%rcx\n\t"
+#else
 		"rep ; movsl\n\t"
 		"movl %4,%%ecx\n\t"
+#endif
 		"rep ; movsb\n\t"
 		: "=&c" (d0), "=&D" (d1), "=&S" (d2)
 		: "0" (n >> 2), "g" (n & 3), "1" (dest), "2" (src)

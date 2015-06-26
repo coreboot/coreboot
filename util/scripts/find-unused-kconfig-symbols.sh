327036998649c62e11b2fbb5779582c3554203e1
Patrick Georgi <pgeorgi@chromium.org>
2015-06-26 23:23:21 +0200

    util/scripts: add some support scripts
    These scripts were bit-rotting on my box and may be useful for somebody else.
    
    no-fsf-addresses.sh removes various FSF addresses from license headers
    
    find-unused-kconfig-symbols.sh points out Kconfig variables that may be
    unused. There are some false positives, but it serves as a starting point.
    
    Change-Id: I8ddb5bea5fe87d39eed5f39f32077944b37d0665
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-on: http://review.coreboot.org/10675
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

---
+++ util/scripts/find-unused-kconfig-symbols.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Copyright (c) 2015 Google Inc.
+#
+# Permission to use, copy, modify, and/or distribute this software for
+# any purpose with or without fee is hereby granted, provided that the
+# above copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+# FIXME/NOTE: some false positives in dmp/vortex86ex
+KCONFIG_FILE_LIST=$(git ls-files |grep "/Kconfig$")
+for i in $(git grep -h "^config " $KCONFIG_FILE_LIST |cut -d' ' -f2 |sort -u); do
+	git grep -q "CONFIG_$i" || \
+	git grep -Eq "(depends on |select |default ).*$i" $KCONFIG_FILE_LIST || \
+	echo $i not found
+done | cat
---
+++ util/scripts/no-fsf-addresses.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Copyright (c) 2015 Google Inc.
+#
+# Permission to use, copy, modify, and/or distribute this software for
+# any purpose with or without fee is hereby granted, provided that the
+# above copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+sed -i \
+	-e "s:Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, *MA[, ]*02110-1301[, ]*USA:Foundation, Inc.:" \
+	-e "s:Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA:Foundation, Inc.:" \
+	-e "s:Foundation, Inc., 59 Temple Place[-, ]*Suite 330, Boston, MA *02111-1307[, ]*USA:Foundation, Inc.:" \
+	-e "s:Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.:Foundation, Inc.:" \
+	-e "/Foundation, Inc./ N;s:Foundation, Inc.* USA\.* *:Foundation, Inc. :;s:Foundation, Inc. $:Foundation, Inc.:" \
+	$(git ls-files $* | \
+		grep -Ev "^($(git submodule status |cut -d' ' -f3 |tr '\012' '|' | sed "s,|$,,"))$" | \
+		grep -Ev "\.patch$|_shipped$|\<LICENSE_GPL$|\<LGPL.txt$|\<COPYING$|\<DISCLAIMER$|^util/kconfig/|\<no-fsf-addresses.sh$")

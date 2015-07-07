mtk09422/chromiumos-third_party-coreboot

rk3288: Use timestamp region for pre-cbmem timestamps
BUG=None
BRANCH=None
TEST=Compiles successfully for veyron_pinky

Change-Id: I3862e9bf2c32085c921adae4c1dcdf88ff0f3ff3
Signed-off-by: Furquan Shaikh <furquan@google.com>
Reviewed-on: https://chromium-review.googlesource.com/227243
Reviewed-by: Aaron Durbin <adurbin@chromium.org>
Commit-Queue: Furquan Shaikh <furquan@chromium.org>
Tested-by: Furquan Shaikh <furquan@chromium.org>

src/soc/rockchip/rk3288/bootblock.c
@@ -26,10 +26,13 @@
 #include <soc/grf.h>
 #include <soc/timer.h>
 #include <symbols.h>
+#include <timestamp.h>
 
 void bootblock_cpu_init(void)
 {
 	rk3288_init_timer();
+
+	timestamp_early_init(timestamp_get());
 
 	if (IS_ENABLED(CONFIG_CONSOLE_SERIAL_UART)) {
 		switch (CONFIG_CONSOLE_SERIAL_UART_ADDRESS) {

coreboot/coreboot

Original-Change-Id: I3862e9bf2c32085c921adae4c1dcdf88ff0f3ff3
Original-Signed-off-by: Furquan Shaikh <furquan@google.com>
Original-Reviewed-on: https://chromium-review.googlesource.com/227243
Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
Original-Commit-Queue: Furquan Shaikh <furquan@chromium.org>
Original-Tested-by: Furquan Shaikh <furquan@chromium.org>
(cherry picked from commit 0fabdbb05826160beb8ee8f89339b18a49e87ab8)
Signed-off-by: Marc Jones <marc.jones@se-eng.com>
Change-Id: I4504d29a43084d4bd406626899b25903200fa6d7
Reviewed-on: http://review.coreboot.org/10740
Tested-by: build bot (Jenkins)

src/soc/rockchip/rk3288/bootblock.c
@@ -26,9 +26,12 @@
 #include <soc/grf.h>
 #include <soc/timer.h>
 #include <symbols.h>
+#include <timestamp.h>
 
 void bootblock_soc_init(void)
 {
+	timestamp_init(timestamp_get());
+
 	rkclk_init();
 
 	mmu_init();
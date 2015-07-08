808f784db5f22784619f306f76a9da94706142cc
Stefan Reinauer <stefan.reinauer@coreboot.org>
2015-07-08 23:18:11 +0200

    arm: enhance eabi compat functions
    This fixes issues with our clang reference toolchain on ARM.
    
    Change-Id: Ib754941059285f15332bc694814aff6285969545
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Reviewed-on: http://review.coreboot.org/10857
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>
    Tested-by: build bot (Jenkins)

--- src/arch/arm/eabi_compat.c
+++ src/arch/arm/eabi_compat.c
@@ -9,6 +9,9 @@
  * License, or (at your option) any later version.
  */
 
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
 #include <console/console.h>
 
 /* FIXME(dhendrix): prototypes added for assembler */
@@ -23,9 +26,21 @@ int raise (int signum)
 void __aeabi_unwind_cpp_pr0(void) __attribute__((used));
 void __aeabi_unwind_cpp_pr0(void)
 {
-};
+}
 
 void __aeabi_unwind_cpp_pr1(void) __attribute__((used));
 void __aeabi_unwind_cpp_pr1(void)
 {
-};
+}
+
+void __aeabi_memcpy(void *dest, const void *src, size_t n);
+void __aeabi_memcpy(void *dest, const void *src, size_t n)
+{
+	(void) memcpy(dest, src, n);
+}
+
+void __aeabi_memset(void *dest, size_t n, int c);
+void __aeabi_memset(void *dest, size_t n, int c)
+{
+	(void) memset(dest, c, n);
+}

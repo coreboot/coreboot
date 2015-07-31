7605a5ac575d80ef68f06f7a7f59522c865a4b36
Patrick Georgi <pgeorgi@google.com>
2015-07-31 17:18:16 +0200

    google/stout: Fix ELOG related ifdefs
    The used functions require the ELOG_GSMI feature, not just ELOG.
    
    Change-Id: If38cf0b710d9236012bfb1f0b119c10f9e533a25
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: http://review.coreboot.org/11098
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/mainboard/google/stout/ec.c
+++ src/mainboard/google/stout/ec.c
@@ -81,7 +81,7 @@ void stout_ec_finalize_smm(void)
 	if (ec_reg & 0x8) {
 		printk(BIOS_ERR, "  EC Fan Error\n");
 		critical_shutdown = 1;
-#if CONFIG_ELOG
+#if CONFIG_ELOG_GSMI
 		elog_add_event_word(EC_EVENT_BATTERY_CRITICAL, EC_EVENT_FAN_ERROR);
 #endif
 	}
@@ -91,7 +91,7 @@ void stout_ec_finalize_smm(void)
 	if (ec_reg & 0x80) {
 		printk(BIOS_ERR, "  EC Thermal Device Error\n");
 		critical_shutdown = 1;
-#if CONFIG_ELOG
+#if CONFIG_ELOG_GSMI
 		elog_add_event_word(EC_EVENT_BATTERY_CRITICAL, EC_EVENT_THERMAL);
 #endif
 	}
@@ -103,14 +103,14 @@ void stout_ec_finalize_smm(void)
 	if ((ec_reg & 0xCF) == 0xC0) {
 		printk(BIOS_ERR, "  EC Critical Battery Error\n");
 		critical_shutdown = 1;
-#if CONFIG_ELOG
+#if CONFIG_ELOG_GSMI
 		elog_add_event_word(ELOG_TYPE_EC_EVENT, EC_EVENT_BATTERY_CRITICAL);
 #endif
 	}
 
 	if ((ec_reg & 0x8F) == 0x8F) {
 		printk(BIOS_ERR, "  EC Read Battery Error\n");
-#if CONFIG_ELOG
+#if CONFIG_ELOG_GSMI
 		elog_add_event_word(ELOG_TYPE_EC_EVENT, EC_EVENT_BATTERY);
 #endif
 	}

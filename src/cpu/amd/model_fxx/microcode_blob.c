9b9400dc90906fddadc0303994422ec011a8f6e7
Stefan Reinauer <reinauer@chromium.org>
2015-08-06 15:56:40 -0700

    amd/model_fxx: Move CPU microcode to 3rdparty/blobs
    Change-Id: I1a772be9d72aa6d6552f5ba21c20b28e400677e9
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Reviewed-on: http://review.coreboot.org/11131
    Tested-by: build bot (Jenkins)
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- src/cpu/amd/model_fxx/microcode_blob.c
+++ src/cpu/amd/model_fxx/microcode_blob.c
@@ -1,7 +1,5 @@
 unsigned char microcode[] __attribute__ ((aligned(16))) = {
 #if !CONFIG_K8_REV_F_SUPPORT
-	#include "microcode_rev_c.h"
-	#include "microcode_rev_d.h"
-	#include "microcode_rev_e.h"
+#include "../../../../3rdparty/blobs/cpu/amd/model_fxx/microcode.h"
 #endif
 };

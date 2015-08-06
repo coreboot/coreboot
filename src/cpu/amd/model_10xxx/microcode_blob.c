916e4085262461a362e7329e0d888b986dc77cc6
Stefan Reinauer <reinauer@chromium.org>
2015-08-06 15:46:32 -0700

    amd/model_10xxx: Move CPU microcode to 3rdparty/blobs
    Change-Id: Ib053bdec185eca2b45c95bec713cf0fb6d16c0bc
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Reviewed-on: http://review.coreboot.org/11130
    Tested-by: build bot (Jenkins)
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- src/cpu/amd/model_10xxx/microcode_blob.c
+++ src/cpu/amd/model_10xxx/microcode_blob.c
@@ -1,9 +1,3 @@
 unsigned char microcode[] __attribute__ ((aligned(16))) = {
-#include "mc_patch_010000c7.h"
-#include "mc_patch_010000c8.h"
-#include "mc_patch_010000d9.h"
-#include "mc_patch_010000db.h"
-#include "mc_patch_010000dc.h"
-#include "mc_patch_01000095.h"
-#include "mc_patch_01000096.h"
+#include "../../../../3rdparty/blobs/cpu/amd/model_10xxx/microcode.h"
 };

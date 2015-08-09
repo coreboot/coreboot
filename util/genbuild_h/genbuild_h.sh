f260f51834a8472bba72f4cb3c8af7141be1b697
Patrick Georgi <patrick@georgi-clan.de>
2015-08-09 20:53:16 +0200

    genbuild_h: actually make date(1) based timestamp locale independent
    This fixes the botched fix in commit d9bc2fadc4ea74ebca0d793aeef288fe9f1acf0c
    
    Change-Id: I0c4445af2851bc80fabb631864321a56123ce7b0
    Signed-off-by: Patrick Georgi <patrick@georgi-clan.de>
    Reviewed-on: http://review.coreboot.org/11146
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- util/genbuild_h/genbuild_h.sh
+++ util/genbuild_h/genbuild_h.sh
@@ -32,8 +32,8 @@ if [ -d "${top}/.git" ] && [ -f "$(command -v git)" ]; then
 	DATE=$(git log --pretty=format:%ct -1)
 else
 	GITREV=Unknown
-	TIMESOURCE="LANG=C LC_ALL=C TZ=UTC date"
-	DATE=$(date +%s)
+	TIMESOURCE="date"
+	DATE=$(LANG= LC_ALL=C TZ=UTC date +%s)
 fi
 
 our_date() {

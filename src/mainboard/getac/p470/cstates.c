21a78a88c6113eeeee565b610833e2dc9c5cfbaa
Patrick Georgi <patrick@georgi-clan.de>
2015-08-09 09:22:30 +0200

    getac/p470: Add C-State values
    Derived from what the vendor BIOS is doing.
    
    Change-Id: Ie2cba7b86b6bb3f1dcc4a5e1c189aa45d0aab109
    Signed-off-by: Patrick Georgi <patrick@georgi-clan.de>
    Found-by: fwts 15.08
    Reviewed-on: http://review.coreboot.org/11142
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/mainboard/getac/p470/cstates.c
+++ src/mainboard/getac/p470/cstates.c
@@ -1,7 +1,27 @@
 #include <device/device.h>
 #include <arch/x86/include/arch/acpigen.h>
+#include <southbridge/intel/i82801gx/i82801gx.h>
+
+static acpi_cstate_t cst_entries[] = {
+	{
+		/* acpi C1 / cpu C1 */
+		1, 0x01, 1000,
+		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, { 1 }, 0, 0 }
+	},
+	{
+		/* acpi C2 / cpu C2 */
+		2, 0x01,  500,
+		{ ACPI_ADDRESS_SPACE_IO, 8, 0, { 0 }, DEFAULT_PMBASE + LV2, 0 }
+	},
+	{
+		/* acpi C3 / cpu C2 */
+		2, 0x11,  250,
+		{ ACPI_ADDRESS_SPACE_IO, 8, 0, { 0 }, DEFAULT_PMBASE + LV3, 0 }
+	},
+};
 
 int get_cst_entries(acpi_cstate_t **entries)
 {
-	return 0;
+	*entries = cst_entries;
+	return ARRAY_SIZE(cst_entries);
 }

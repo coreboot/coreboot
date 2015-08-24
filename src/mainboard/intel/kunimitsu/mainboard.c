dfdd33eb53fd5abf90c8bbd2c66c2a187bd6a426
pchandri <preetham.chandrian@intel.com>
2015-08-24 13:44:19 -0700

    intel/kunimitsu: Adding mainboard init to enable SCI event
    BUG=chrome-os-partner:44470
    BRANCH=None
    TEST=Builds and Boots on FAB3 (Kunimitsu)
    
    Change-Id: I479fe60dcbdd51f4fa5bca857b4a166f958a54d5
    Signed-off-by: Patrick Georgi <patrick@georgi-clan.de>
    Original-Commit-Id: e88efdd8766e2846a650eb75709b29035c406bf8
    Original-Change-Id: I9fe5697d31e188fca48b14fb76e71631f2974c2d
    Original-Signed-off-by: pchandri <preetham.chandrian@intel.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/295218
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: http://review.coreboot.org/11433
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/intel/kunimitsu/mainboard.c
+++ src/mainboard/intel/kunimitsu/mainboard.c
@@ -20,6 +20,23 @@
  */
 
 #include <device/device.h>
+#include <stdlib.h>
+#include "ec.h"
+
+static void mainboard_init(device_t dev)
+{
+	mainboard_ec_init();
+}
+
+/*
+ * mainboard_enable is executed as first thing after
+ * enumerate_buses().
+ */
+static void mainboard_enable(device_t dev)
+{
+	dev->ops->init = mainboard_init;
+}
 
 struct chip_operations mainboard_ops = {
+	.enable_dev = mainboard_enable,
 };

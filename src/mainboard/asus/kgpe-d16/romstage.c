5f2bf6d02ddb82887a17c0b48ce8eb3a300f9c09
Timothy Pearson <tpearson@raptorengineeringinc.com>
2015-08-28 20:02:45 -0500

    mainboard/asus/kgpe-d16: Enable CBFS spinlocks
    Change-Id: I8f6226d3e74ac5c7f29f708128a7502ced1287bf
    Signed-off-by: Timothy Pearson <tpearson@raptorengineeringinc.com>
    Reviewed-on: https://review.coreboot.org/12062
    Tested-by: Raptor Engineering Automated Test Stand <noreply@raptorengineeringinc.com>
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/mainboard/asus/kgpe-d16/romstage.c
+++ src/mainboard/asus/kgpe-d16/romstage.c
@@ -324,6 +324,18 @@ void initialize_romstage_console_lock(void)
 	car_get_var(printk_spinlock) = SPIN_LOCK_UNLOCKED;
 }
 
+static spinlock_t nvram_cbfs_spinlock CAR_GLOBAL;
+
+spinlock_t* romstage_nvram_cbfs_lock(void)
+{
+	return car_get_var_ptr(&nvram_cbfs_spinlock);
+}
+
+void initialize_romstage_nvram_cbfs_lock(void)
+{
+	car_get_var(nvram_cbfs_spinlock) = SPIN_LOCK_UNLOCKED;
+}
+
 void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 {
 	uint32_t esp;
@@ -350,8 +362,9 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 		timestamp_init(timestamp_get());
 		timestamp_add_now(TS_START_ROMSTAGE);
 
-		/* Initialize the printk spinlock */
+		/* Initialize the printk and nvram CBFS spinlocks */
 		initialize_romstage_console_lock();
+		initialize_romstage_nvram_cbfs_lock();
 
 		/* Nothing special needs to be done to find bus 0 */
 		/* Allow the HT devices to be found */

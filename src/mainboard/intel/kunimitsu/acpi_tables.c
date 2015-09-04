5f3ea3b77ed733e59a83c0c77d36b8b9e7a322a1
Duncan Laurie <dlaurie@chromium.org>
2015-09-04 14:16:49 -0700

    kunimitsu: Remove functions from acpi_tables.c
    Remove the acpi_tables.c functions so these functions can move to
    SOC init code.  The file itself is included by x86/arch code and
    must exist for the build to succeed.
    
    BUG=chrome-os-partner:40635
    BRANCH=none
    TEST=emerge-kunimitsu coreboot
    
    Change-Id: Ia9657f4a39c30ed7a0fd7ca4815bb2614f049911
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 93ae87f2429af5cb9d497f8b5ef8b8dffe370df4
    Original-Change-Id: Ifc2f64dc1693e7bd3f5a43144d84ff033b2cfe8b
    Original-Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Original-Reviewed-on: https://chromium-review.googlesource.com/297759
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: http://review.coreboot.org/11580
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/intel/kunimitsu/acpi_tables.c
+++ src/mainboard/intel/kunimitsu/acpi_tables.c
@@ -17,25 +17,3 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc.
  */
-
-#include <arch/acpi.h>
-#include <arch/ioapic.h>
-#include <soc/acpi.h>
-#include <soc/nvs.h>
-
-void acpi_create_gnvs(global_nvs_t *gnvs)
-{
-	acpi_init_gnvs(gnvs);
-}
-
-unsigned long acpi_fill_madt(unsigned long current)
-{
-	/* Local APICs */
-	current = acpi_create_madt_lapics(current);
-
-	/* IOAPIC */
-	current += acpi_create_madt_ioapic((acpi_madt_ioapic_t *) current,
-				2, IO_APIC_ADDR, 0);
-
-	return acpi_madt_irq_overrides(current);
-}

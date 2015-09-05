cab71b638f6bd9d8c7e6d7955d4527c5236177cb
Timothy Pearson <tpearson@raptorengineeringinc.com>
2015-09-05 17:53:20 -0500

    drivers/i2c/w83795: Add full support for core functions
    Add full support for fan control, fan monitoring, and voltage
    monitoring.  Fan speeds and functions are configurable via
    each mainboard's devicetree.cb file.
    
    NOTE: This patch effectively rewrites large portions of
    the original driver.  You may need to re-verify correct
    operation on your hardware if you were using the old
    driver code.
    
    Change-Id: I3e246af0e398d65ee43ea708060885c67fd7d202
    Signed-off-by: Timothy Pearson <tpearson@raptorengineeringinc.com>
    Reviewed-on: http://review.coreboot.org/11936
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins)
    Reviewed-by: Peter Stuge <peter@stuge.se>

--- src/drivers/i2c/w83795/w83795.h
+++ src/drivers/i2c/w83795/w83795.h
@@ -2,6 +2,7 @@
  * This file is part of the coreboot project.
  *
  * Copyright (C) 2012 Advanced Micro Devices, Inc.
+ * Copyright (C) 2015 Raptor Engineering
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,8 +21,6 @@
 #ifndef _W83795_H_
 #define _W83795_H_
 
-#define W83795_DEV			0x2F /* Host I2c Addr (strap to addr1 addr0 1 1, 0x5E) */
-
 #define W83795_REG_I2C_ADDR		0xFC
 #define W83795_REG_BANKSEL		0x00
 #define W83795_REG_CONFIG		0x01
@@ -29,6 +28,8 @@
 #define W83795_REG_CONFIG_CONFIG48	0x04
 #define W83795_REG_CONFIG_INIT		0x80
 
+#define W83795_REG_VOLT_CTRL1		0x02
+#define W83795_REG_VOLT_CTRL2		0x03
 #define W83795_REG_TEMP_CTRL1		0x04 /* Temperature Monitoring Control Register */
 #define W83795_REG_TEMP_CTRL2		0x05 /* Temperature Monitoring Control Register */
 #define W83795_REG_FANIN_CTRL1		0x06
@@ -37,37 +38,58 @@
 #define DTS_SRC_INTEL_PECI		(0 << 0)
 #define DTS_SRC_AMD_SBTSI		(1 << 0)
 
-#define W83795_REG_TSS(n)		(0x209 + (n)) /* Temperature Source Selection Register */
 #define W83795_REG_TTTI(n)		(0x260 + (n)) /* Target temperature W83795G/ADG will try to tune the fan output to keep */
 #define W83795_REG_CTFS(n)		(0x268 + (n)) /* Critical Temperature to Full Speed all fan */
-#define W83795_REG_HT(n)		(0x270 + (n)) /* Hysteresis of Temperature */
 #define W83795_REG_DTSC			0x301 /* Digital Temperature Sensor Configuration */
 
 #define W83795_REG_DTSE			0x302 /* Digital Temperature Sensor Enable */
 #define W83795_REG_DTS(n)		(0x26 + (n))
 #define W83795_REG_VRLSB		0x3C
 
-#define W83795_TEMP_REG_TR1		0x21
-#define W83795_TEMP_REG_TR2		0x22
-#define W83795_TEMP_REG_TR3		0x23
-#define W83795_TEMP_REG_TR4		0x24
-#define W83795_TEMP_REG_TR5		0x1F
-#define W83795_TEMP_REG_TR6		0x20
+#define W83795_REG_TEMP_TR1		0x21
+#define W83795_REG_TEMP_TR2		0x22
+#define W83795_REG_TEMP_TR3		0x23
+#define W83795_REG_TEMP_TR4		0x24
+#define W83795_REG_TEMP_TR5		0x1F
+#define W83795_REG_TEMP_TR6		0x20
+
+#define W83795_REG_VOLT_LIM_HIGH(n)	(0x70 + (n * 2))	/* Voltage high limit (0 == VSEN1) */
+#define W83795_REG_VOLT_LIM_LOW(n)	(0x71 + (n * 2))	/* Voltage low limit (0 == VSEN1) */
+#define W83795_REG_VOLT_LIM_HIGH_2_M(n)	(0x96 + (n * 4))	/* Voltage high limit MSB (0 == VDSEN14) */
+#define W83795_REG_VOLT_LIM_LOW_2_M(n)	(0x97 + (n * 4))	/* Voltage low limit MSB (0 == VDSEN14)  */
+#define W83795_REG_VOLT_LIM_HIGH_2_L(n)	(0x98 + (n * 4))	/* Voltage high limit LSB (0 == VDSEN14) */
+#define W83795_REG_VOLT_LIM_LOW_2_L(n)	(0x99 + (n * 4))	/* Voltage low limit LSB (0 == VDSEN14)  */
+
+#define W83795_REG_TEMP_CRIT(n)		(0x96 + (n * 4))	/* Temperature critical limit */
+#define W83795_REG_TEMP_CRIT_HYSTER(n)	(0x97 + (n * 4))	/* Temperature critical limit hysteresis */
+#define W83795_REG_TEMP_WARN(n)		(0x98 + (n * 4))	/* Temperature warning limit */
+#define W83795_REG_TEMP_WARN_HYSTER(n)	(0x99 + (n * 4))	/* Temperature warning limit hysteresis */
+
+#define W83795_REG_DTS_CRIT		0xB2			/* Temperature critical limit */
+#define W83795_REG_DTS_CRIT_HYSTER	0xB3			/* Temperature critical limit hysteresis */
+#define W83795_REG_DTS_WARN		0xB4			/* Temperature warning limit */
+#define W83795_REG_DTS_WARN_HYSTER	0xB5			/* Temperature warning limit hysteresis */
 
 #define W83795_REG_FCMS1		0x201
 #define W83795_REG_FCMS2		0x208
-#define W83795_REG_TFMR(n)		(0x202 + (n)) /*temperature to fam mappig*/
+#define W83795_REG_TFMR(n)		(0x202 + (n))		/* Temperature to fan mapping */
+#define W83795_REG_T12TSS		0x209			/* Temperature Source Selection Register 1 */
+#define W83795_REG_T34TSS		0x20A			/* Temperature Source Selection Register 2 */
+#define W83795_REG_T56TSS		0x20B			/* Temperature Source Selection Register 3 */
+#define W83795_REG_FAN_MANUAL_SPEED(n)	(0x210 + n)
 #define W83795_REG_DFSP			0x20C
+
+#define W83795_REG_FAN_NONSTOP(n)	(0x228 + (n))	/* Fan Nonstop Value */
 
 #define W83795_REG_FTSH(n)		(0x240 + (n) * 2)
 #define W83795_REG_FTSL(n)		(0x241 + (n) * 2)
 #define W83795_REG_TFTS			0x250
 
 typedef enum w83795_fan_mode {
-	SPEED_CRUISE_MODE,	///< Fan Speed Cruise mode keeps the fan speed in a specified range
-	THERMAL_CRUISE_MODE,	///< Thermal Cruise mode is an algorithm to control the fan speed to keep the temperature source around the TTTI
-	SMART_FAN_MODE,		///< Smart Fan mode offers 6 slopes to control the fan speed
-	MANUAL_MODE,		///< control manually
+	SPEED_CRUISE_MODE = 0,		///< Fan Speed Cruise mode keeps the fan speed in a specified range
+	THERMAL_CRUISE_MODE = 1,	///< Thermal Cruise mode is an algorithm to control the fan speed to keep the temperature source around the TTTI
+	SMART_FAN_MODE = 2,		///< Smart Fan mode offers 6 slopes to control the fan speed
+	MANUAL_MODE = 3,		///< control manually
 } w83795_fan_mode_t;
 
 #endif

5d4735d140898ebb459a8a54f082568b8ffdb212
Pravin Angolkar <pravin.k.angolkar@intel.com>
2015-09-29 19:31:41 +0530

    intel/kunimitsu: Power gate Kepler device
    This patch power gates the Kepler module
    on skylake kunimitsu board. This is required
    to save power since this is consuming over 500mw
    of power in all active use cases.
    The device can be powered on later by using the
    kernel driver as required by setting the
    kepler enable gpio high.
    
    BRANCH=None
    BUG=chrome-os-partner:45962
    TEST=Build and Boot Kunimitsu and check lspci.
    The Kepler device should not be listed.
    Also power measurement of board should give
    approximately 300mW of reduction in power.
    
    Change-Id: I244a23385e20ef1431dc895536c8a47e1f5770d7
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 8d4fb7d01f32ac307a351c307b8461628c0e5414
    Original-Change-Id: Idafa74d7ff14d67a5b1e635f783efd84b5a7399c
    Original-Signed-off-by: Pravin Angolkar <pravin.k.angolkar@intel.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/302277
    Original-Commit-Ready: Naveenkrishna Ch <naveenkrishna.ch@intel.com>
    Original-Tested-by: Pravin K Angolkar <pravin.k.angolkar@intel.com>
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://review.coreboot.org/12964
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/mainboard/intel/kunimitsu/gpio.h
+++ src/mainboard/intel/kunimitsu/gpio.h
@@ -120,7 +120,7 @@ static const struct pad_config gpio_table[] = {
 /* UART0_RXD */		/* GPP_C8 */
 /* UART0_TXD */		/* GPP_C9 */
 /* NFC_RST* */		PAD_CFG_GPO(GPP_C10, 0, DEEP),
-/* EN_PP3300_KEPLER */	PAD_CFG_TERM_GPO(GPP_C11, 1, 20K_PD, DEEP),
+/* EN_PP3300_KEPLER */	PAD_CFG_TERM_GPO(GPP_C11, 0, 20K_PD, DEEP),
 /* PCH_MEM_CFG0 */	PAD_CFG_GPI(GPP_C12, NONE, DEEP),
 /* PCH_MEM_CFG1 */	PAD_CFG_GPI(GPP_C13, NONE, DEEP),
 /* PCH_MEM_CFG2 */	PAD_CFG_GPI(GPP_C14, NONE, DEEP),
@@ -230,8 +230,8 @@ static const struct pad_config gpio_table[] = {
 /* Early pad configuration in romstage. */
 static const struct pad_config early_gpio_table[] = {
 /* SRCCLKREQ2# */	PAD_CFG_NF(GPP_B7, NONE, DEEP, NF1), /* KEPLER */
-/* UART0_CTS# */	PAD_CFG_GPO(GPP_C11, 1, DEEP), /* EN_PP3300_KEPLER */
 /* SPI_WP_STATUS */	PAD_CFG_GPI(GPP_C23, 20K_PU, DEEP),
+/* UART0_CTS# */	PAD_CFG_GPO(GPP_C11, 0, DEEP), /* EN_PP3300_KEPLER */
 };
 
 #endif

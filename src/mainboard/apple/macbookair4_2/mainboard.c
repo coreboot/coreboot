d57fb2d97bd89a61b5e9c7797414f2c9096a4003
Vladimir Serbinenko <phcoder@gmail.com>
2015-10-10 14:21:36 +0200

    mba4,2: Fix requested vgabios output.
    Change-Id: I036c2c300b2aac38b2c30ab86623c9c46b3c5c98
    Signed-off-by: Vladimir Serbinenko <phcoder@gmail.com>
    Reviewed-on: http://review.coreboot.org/11850
    Tested-by: build bot (Jenkins)
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/mainboard/apple/macbookair4_2/mainboard.c
+++ src/mainboard/apple/macbookair4_2/mainboard.c
@@ -42,8 +42,7 @@ static void mainboard_enable(device_t dev)
 {
 	dev->ops->init = mainboard_init;
 
-	/* FIXME: fix those values*/
-	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_INT_LVDS, GMA_INT15_PANEL_FIT_DEFAULT, GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
+	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_EDP, GMA_INT15_PANEL_FIT_DEFAULT, GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
 }
 
 struct chip_operations mainboard_ops = {

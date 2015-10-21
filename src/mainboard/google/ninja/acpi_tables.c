0ca0fd893a3b5df46c36008eb5e3770549c48a62
Matt DeVillier <matt.devillier@gmail.com>
2015-10-20 19:36:09 -0500

    UPSTREAM: google/ninja: Upstream AOpen Chromebox Commerical
    Migrate google/ninja (AOpen Chromebox Commerical) from Chromium tree to
    upstream, using google/rambi as a reference.
    
    original source:
    branch firmware-ninja-5216.383.B
    commit 582a393 [Ninja, Sumo: Add SPD source for Hynix H5TC4G63CFR-PBA]
    
    TEST=built and booted Linux on ninja with full functionality
    
    blobs required for working image:
    VGA BIOS (vgabios.bin)
    firmware descriptor (ifd.bin)
    Intel ME firmware (me.bin)
    MRC (mrc.elf)
    external reference code (refcode.elf)
    
    Change-Id: I0f1892c24c08fa2d53185b2cf8b6f5a9001b2397
    Original-Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Original-Reviewed-on: https://review.coreboot.org/14950
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    (cherry-picked from commit a87fcabd2efe49c8035b76146401e190a0ea6593)
    Signed-off-by: Martin Roth <martinroth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/348412
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

---
+++ src/mainboard/google/ninja/acpi_tables.c
@@ -0,0 +1,64 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <types.h>
+#include <string.h>
+#include <cbmem.h>
+#include <console/console.h>
+#include <arch/acpi.h>
+#include <arch/ioapic.h>
+#include <arch/acpigen.h>
+#include <arch/smp/mpspec.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <device/pci_ids.h>
+#include <cpu/cpu.h>
+#include <cpu/x86/msr.h>
+#include <soc/acpi.h>
+#include <soc/nvs.h>
+#include <soc/iomap.h>
+
+void acpi_create_gnvs(global_nvs_t *gnvs)
+{
+	acpi_init_gnvs(gnvs);
+
+	/* Enable USB ports in S3 */
+	gnvs->s3u0 = 1;
+	gnvs->s3u1 = 1;
+
+	/* Disable USB ports in S5 */
+	gnvs->s5u0 = 0;
+	gnvs->s5u1 = 0;
+
+	/* TPM Present */
+	gnvs->tpmp = 1;
+
+	/* Enable DPTF */
+	gnvs->dpte = 1;
+}
+
+unsigned long acpi_fill_madt(unsigned long current)
+{
+	/* Local APICs */
+	current = acpi_create_madt_lapics(current);
+
+	/* IOAPIC */
+	current += acpi_create_madt_ioapic((acpi_madt_ioapic_t *) current,
+				2, IO_APIC_ADDR, 0);
+
+	current = acpi_madt_irq_overrides(current);
+
+	return current;
+}

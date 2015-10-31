40bf88eb120d1d41af2d8a4c97a7c6a786257ef2
Alexandru Gagniuc <alexandrux.gagniuc@intel.com>
2015-10-30 18:05:57 -0700

    UPSTREAM: drivers/intel/fsp2.0: Add semantic patch for FspUpdVpd.h header
    Previous FSP implementations in coreboot have included FspUpdVpd.h
    directly, along with with efi headers. Instead of taking that
    approach in FSP 2.0, we provide a semantic patch that, with minimal
    modifications, makes FspUpdVpd.h easier to include in coreboot, and
    eliminates reliance on external headers and definitions.
    
    Change-Id: I0c2a6f7baf6fb50ae22b64e08e653cfe1aefdaf9
    Original-Signed-off-by: Alexandru Gagniuc <alexandrux.gagniuc@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/13331
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    (cherry-picked from commit 6a587343a96167c21d63ace1857ee2d55cdebfa0)
    Signed-off-by: Martin Roth <martinroth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/350969
    Commit-Ready: Aaron Durbin <adurbin@chromium.org>
    Tested-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/drivers/intel/fsp2_0/header_util/fspupdvpd_sanitize.sh
@@ -0,0 +1,33 @@
+#
+# Convert the FspUpdVpd.h header file into a format usable by coreboot
+# Usage:
+#     fspupdvpd_sanitize.sh <path/to/FspUpdVpd.h>
+#
+# This file is part of the coreboot project.
+#
+# Copyright (C) 2015-2016 Intel Corp.
+# (Written by Alexandru Gagniuc <alexandrux.gagniuc@intel.com> for Intel Corp.)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+PWD=$(dirname "${BASH_SOURCE[0]}")
+
+SPATCH=spatch
+
+# Fix line terminations
+dos2unix $1
+
+# Clean up trailing whitespace
+sed -e "s, $,,g" -i $1
+
+# Now fix the actual coding style
+$SPATCH -sp_file ${PWD}/fspupdvpd.spatch \
+	-in_place $1

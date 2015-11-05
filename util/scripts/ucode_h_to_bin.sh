295358454aa214f1c915c2f340f8897bbd500b45
Martin Roth <martinroth@google.com>
2015-11-05 07:51:28 -0700

    utils/scripts: Add microcode conversion tool
    This is an update to the script in the blobs repo that converts
    individual or multiple files into a microcode binary.
    
    Change-Id: I66fb650bbfa334d1f07e8e3914ef6deb8e72bbb4
    Signed-off-by: Martin Roth <martinroth@google.com>
    Reviewed-on: http://review.coreboot.org/12332
    Tested-by: build bot (Jenkins)
    Reviewed-by: Werner Zeh <werner.zeh@siemens.com>

---
+++ util/scripts/ucode_h_to_bin.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# This file is part of the coreboot project.
+#
+# Copyright (C) 2015 Google Inc.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+	printf "Usage: %s <output file> \"<microcode .h files>\"\n" "$0"
+fi
+
+OUTFILE=$1
+TMPFILE=$(mktemp microcode_XXXX)
+cat > "${TMPFILE}.c" << EOF
+#include <stdio.h>
+unsigned int microcode[] = {
+EOF
+
+for UCODE in ${@:2}; do
+	echo "#include \"$UCODE\"" >> "${TMPFILE}.c"
+done
+
+cat >> "${TMPFILE}.c" << EOF
+};
+int main(void)
+{
+	FILE *f = fopen("$OUTFILE", "wb");
+	fwrite(microcode, sizeof(microcode), 1, f);
+	fclose(f);
+	return 0;
+}
+EOF
+
+gcc -o "$TMPFILE" "${TMPFILE}.c"
+"./$TMPFILE"
+rm "$TMPFILE" "${TMPFILE}.c"

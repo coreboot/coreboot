5f92a5a29540418de3338d274936cc7f7167ec86
Felix Durairaj <felixx.durairaj@intel.com>
2015-11-20 15:51:35 -0800

    lib: Implement framework for retrieving  WiFi regulatory domain
    Platforms that need to initialize WRDD package with the regulatory domain
    information should implement function wifi_regulatory_domain.
    A weak implementation is provided here.
    
    Original-Reviewed-on: https://chromium-review.googlesource.com/314384
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Original-Reviewed-by: Pratikkumar V Prajapati <pratikkumar.v.prajapati@intel.com>
    Original-Tested-by: Hannah Williams <hannah.williams@intel.com>
    
    Change-Id: I84e2acd748856437b40bbf997bf23f158c711712
    Signed-off-by: fdurairx <felixx.durairaj@intel.com>
    Signed-off-by: Hannah Williams <hannah.williams@intel.com>
    Reviewed-on: https://review.coreboot.org/12744
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

---
+++ src/include/wrdd.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 Google Inc.
+ * Copyright (C) 2015 Intel Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _WRDD_H_
+#define _WRDD_H_
+
+#include <stdint.h>
+
+/* Default regulatory domain ID */
+#define WRDD_DEFAULT_REGULATORY_DOMAIN 0x4150
+/* INDONESIA regulatory domain ID */
+#define WRDD_REGULATORY_DOMAIN_INDONESIA 0x4944
+
+/* Retrieve the regulatory domain information */
+uint16_t wifi_regulatory_domain(void);
+
+#endif /* _WRDD_H_ */
+

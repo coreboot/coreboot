7f761658a5b1ce6cdd48202cb1b0c5d7b3b59b91
Felix Durairaj <felixx.durairaj@intel.com>
2015-11-20 15:51:35 -0800

    lib: Implement framework for retrieving  WiFi regulatory domain
    Platforms that need to initialize WRDD package with the regulatory domain
    information should implement function wifi_regulatory_domain.
    A weak implementation is provided here.
    
    Signed-off-by: fdurairx <felixx.durairaj@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/314384
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Pratikkumar V Prajapati <pratikkumar.v.prajapati@intel.com>
    Commit-Queue: Hannah Williams <hannah.williams@intel.com>
    Tested-by: Hannah Williams <hannah.williams@intel.com>
    (cherry picked from commit c25d7221679d1fab830d614eeabfa3436bce6ac1)
    
    BUG=chrome-os-partner:50516
    BRANCH=glados
    TEST=build and boot on chell
    
    Change-Id: I1cbdf4e940b009c74ee8ed8f4fca85f4f5c943b2
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Original-Commit-Id: 27bba336e620a2d3d331e350d4f46164e337fabc
    Original-Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Original-Change-Id: I84e2acd748856437b40bbf997bf23f158c711712
    Original-Reviewed-on: https://chromium-review.googlesource.com/329291
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://review.coreboot.org/13836
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

---
+++ src/lib/wrdd.c
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 Google Inc.
+ * Copyright (C) 2015 Intel Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <wrdd.h>
+
+uint16_t __attribute__((weak)) wifi_regulatory_domain(void)
+{
+	return WRDD_DEFAULT_REGULATORY_DOMAIN;
+}

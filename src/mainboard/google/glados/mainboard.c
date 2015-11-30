5a18a0cb2061d653ab3277ac0445e0b2ed958267
Aaron Durbin <adurbin@chromium.org>
2015-11-30 15:01:39 -0600

    google/glados: add nhlt support
    Provide an option for including the NHLT blobs within the
    glados mainboard directory while also adding the ACPI NHLT
    table generation that the current hardware supports.
    
    BUG=chrome-os-partner:44481
    BRANCH=None
    TEST=Built and booted. Headphones, speakers, and mic on camera
         emits and creates sound albeit not the greatest.
    
    Change-Id: I6e36c0a99a73cdcb2bf6ccfbfc886a594f989a39
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 5b0383a93f054011dd7c18519ece4e6f1944366d
    Original-Change-Id: I6f8bd15c72fa89756382af99bddb6cb6abe89905
    Original-Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Original-Reviewed-on: https://chromium-review.googlesource.com/313794
    Original-Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://review.coreboot.org/12939
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/mainboard/google/glados/mainboard.c
+++ src/mainboard/google/glados/mainboard.c
@@ -15,13 +15,54 @@
  * GNU General Public License for more details.
  */
 
+#include <arch/acpi.h>
+#include <console/console.h>
 #include <device/device.h>
 #include <stdlib.h>
+#include <soc/nhlt.h>
 #include "ec.h"
 
 static void mainboard_init(device_t dev)
 {
 	mainboard_ec_init();
+}
+
+static unsigned long mainboard_write_acpi_tables(
+	device_t device, unsigned long current, acpi_rsdp_t *rsdp)
+{
+	uintptr_t start_addr;
+	uintptr_t end_addr;
+	struct nhlt *nhlt;
+
+	start_addr = current;
+
+	nhlt = nhlt_init();
+
+	if (nhlt == NULL)
+		return start_addr;
+
+	/* 2 Channel DMIC array. */
+	if (nhlt_soc_add_dmic_array(nhlt, 2))
+		printk(BIOS_ERR, "Couldn't add 2CH DMIC array.\n");
+
+	/* 4 Channel DMIC array. */
+	if (nhlt_soc_add_dmic_array(nhlt, 4))
+		printk(BIOS_ERR, "Couldn't add 4CH DMIC arrays.\n");
+
+	/* ADI Smart Amps for left and right. */
+	if (nhlt_soc_add_ssm4567(nhlt, AUDIO_LINK_SSP0))
+		printk(BIOS_ERR, "Couldn't add ssm4567.\n");
+
+	/* NAU88l25 Headset codec. */
+	if (nhlt_soc_add_nau88l25(nhlt, AUDIO_LINK_SSP1))
+		printk(BIOS_ERR, "Couldn't add headset codec.\n");
+
+	end_addr = nhlt_soc_serialize(nhlt, start_addr);
+
+	if (end_addr != start_addr)
+		acpi_add_table(rsdp, (void *)start_addr);
+
+	return end_addr;
 }
 
 /*
@@ -31,6 +72,7 @@ static void mainboard_init(device_t dev)
 static void mainboard_enable(device_t dev)
 {
 	dev->ops->init = mainboard_init;
+	dev->ops->write_acpi_tables = mainboard_write_acpi_tables;
 }
 
 struct chip_operations mainboard_ops = {

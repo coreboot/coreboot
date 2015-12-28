9074cec9b997f2be7083f96ef872e281e9194015
Damien Zammit <damien@zamaudio.com>
2015-12-28 23:34:00 +1100

    superio/it8718f: Add missing PNP info
    Change-Id: Id6d50d4d6af31e43f851645f09383121755291f6
    Signed-off-by: Damien Zammit <damien@zamaudio.com>
    Reviewed-on: https://review.coreboot.org/12815
    Tested-by: build bot (Jenkins)
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- src/superio/ite/it8718f/superio.c
+++ src/superio/ite/it8718f/superio.c
@@ -51,11 +51,21 @@ static struct device_operations ops = {
 	.init             = init,
 };
 
-/* TODO: FDC, PP, EC, KBCM, IR. */
+/* TODO: IR. */
 static struct pnp_info pnp_dev_info[] = {
+	{ &ops, IT8718F_FDC,  PNP_IO0 | PNP_IRQ0 | PNP_DRQ0
+			| PNP_MSC0 | PNP_MSC1, {0x0ff8, 0}, },
 	{ &ops, IT8718F_SP1,  PNP_IO0 | PNP_IRQ0, {0x07f8, 0}, },
-	{ &ops, IT8718F_SP2,  PNP_IO0 | PNP_IRQ0 | PNP_DRQ0 | PNP_DRQ1, {0x07f8, 0}, },
-	{ &ops, IT8718F_KBCK, PNP_IO0 | PNP_IO1 | PNP_IRQ0, {0x07f8, 0}, {0x07f8, 4}, },
+	{ &ops, IT8718F_SP2,  PNP_IO0 | PNP_IRQ0, {0x07f8, 0}, },
+	{ &ops, IT8718F_EC,   PNP_IO0 | PNP_IO1 | PNP_IRQ0
+			| PNP_MSC0 | PNP_MSC1 | PNP_MSC2 | PNP_MSC3
+			| PNP_MSC4 | PNP_MSC5 | PNP_MSC6,
+			{0x0ff8, 0}, {0x0ff8, 4}, },
+	{ &ops, IT8718F_KBCK, PNP_IO0 | PNP_IO1 | PNP_IRQ0
+			| PNP_MSC0, {0x07f8, 0}, {0x07f8, 4}, },
+	{ &ops, IT8718F_KBCM, PNP_IRQ0 | PNP_MSC0, },
+	{ &ops, IT8718F_PP,   PNP_IO0 | PNP_IO1 | PNP_IRQ0
+			| PNP_DRQ0 | PNP_MSC0, {0x0ff8, 0}, {0x0ff8, 4}, },
 };
 
 static void enable_dev(struct device *dev)

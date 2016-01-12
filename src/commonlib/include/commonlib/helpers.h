f4d015dbf842a07596e30fc933adeaecacb49d80
Martin Roth <martinroth@google.com>
2016-01-12 16:21:45 -0700

    commonlib/: Add missing license headers
    These files are original to coreboot and get the standard
    coreboot GPL header.
    
    Change-Id: I19565b0d2424a6f37a95ab4d7b16742d23122d1e
    Signed-off-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://review.coreboot.org/12919
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/commonlib/include/commonlib/helpers.h
+++ src/commonlib/include/commonlib/helpers.h
@@ -1,3 +1,16 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef COMMONLIB_HELPERS_H
 #define COMMONLIB_HELPERS_H
 /* This file is for helpers for both coreboot firmware and its utilities. */

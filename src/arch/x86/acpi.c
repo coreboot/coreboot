9df9e939146f6b715947754eb10c8d96ab7c5873
Martin Roth <martinroth@google.com>
2016-01-12 15:55:28 -0700

    arch/x86: add missing license headers
    These were mostly written as part of the coreboot project, so get
    the standard coreboot license header.
    
    memmove.c came from the linux kernel, so also gets the standard
    coreboot v2 license header, but gets the added attribution that it
    was derived from the linux kernel.  Unlike many coreboot files,
    this file may not be re-licensed as GPL V3.
    
    Change-Id: I1fdc26b543e059f7a42d4b886f7222f4c74b959d
    Signed-off-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://review.coreboot.org/12916
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

--- src/arch/x86/acpi.c
+++ src/arch/x86/acpi.c
@@ -14,6 +14,15 @@
  *
  * Copyright (C) 2005 ADVANCED MICRO DEVICES, INC. All Rights Reserved.
  * 2005.9 yhlu add SRAT table generation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 /*
--- src/arch/x86/memmove.c
+++ src/arch/x86/memmove.c
@@ -1,3 +1,19 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This file is derived from memcpy_32.c in the Linux kernel.
+ * Unlike many coreboot files, this file may not be re-licensed as GPL V3
+ */
+
 #include <string.h>
 
 void *memmove(void *dest, const void *src, size_t n)

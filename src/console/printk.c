4e7e987b242724392bb6fcf1c55ccbed57574bed
Damien Roth <yves.r.roth@gmail.com>
2016-01-16 18:59:51 -0700

    console/: add missing license headers
    These were copied from the linux kernel, so get the standard corboot GPL
    v2 header.
    
    Change-Id: I27ef3326cc42b7e005f94c8b4fd355012a89561d
    Signed-off-by: Damien Roth <yves.r.roth@gmail.com>
    Reviewed-on: https://review.coreboot.org/13023
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/console/printk.c
+++ src/console/printk.c
@@ -1,9 +1,20 @@
 /*
- *  blatantly copied from linux/kernel/printk.c
+ * This file is part of the coreboot project.
  *
- *  Copyright (C) 1991, 1992  Linus Torvalds
- *  Copyright (C) 2015 Timothy Pearson <tpearson@raptorengineeringinc.com>, Raptor Engineering
+ * Copyright (C) 1991, 1992  Linus Torvalds
+ * Copyright (C) 2015 Timothy Pearson <tpearson@raptorengineeringinc.com>,
+ *               Raptor Engineering
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * blatantly copied from linux/kernel/printk.c
  */
 
 #include <console/console.h>
--- src/console/vtxprintf.c
+++ src/console/vtxprintf.c
@@ -1,7 +1,18 @@
-/*  vtxprintf.c, from
- *    linux/lib/vsprintf.c
+/*
+ * This file is part of the coreboot project.
  *
  *  Copyright (C) 1991, 1992  Linus Torvalds
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * vtxprintf.c, originally from linux/lib/vsprintf.c
  */
 
 #include <console/console.h>

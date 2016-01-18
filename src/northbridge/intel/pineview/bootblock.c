2cfab90baa2eca04f6a522b65ce23650e5af56ff
Damien Zammit <damien@zamaudio.com>
2016-01-18 16:39:51 +1100

    nb/intel/pineview: Increase MMCONF decoding to 256 busses
    Linux kernel detects 256 busses but previously only 64 were
    allocated.  Removes warning in OS.
    
    Change-Id: Id83c85e60025a04acbe6a53dfea6878222d8791f
    Signed-off-by: Damien Zammit <damien@zamaudio.com>
    Reviewed-on: https://review.coreboot.org/13033
    Tested-by: build bot (Jenkins)
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- src/northbridge/intel/pineview/bootblock.c
+++ src/northbridge/intel/pineview/bootblock.c
@@ -1,8 +1,10 @@
 #include <arch/io.h>
 #define PCIEXBAR 0x60
+#define MMCONF_256_BUSSES 16
+#define ENABLE 1
 
 static void bootblock_northbridge_init(void)
 {
 	pci_io_write_config32(PCI_DEV(0,0,0), PCIEXBAR,
-		CONFIG_MMCONF_BASE_ADDRESS | 4 | 1);
+		CONFIG_MMCONF_BASE_ADDRESS | MMCONF_256_BUSSES | ENABLE);
 }

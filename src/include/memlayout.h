coreboot/coreboot

commit a8aef3acbc0aea2dd594f7133a7024f3babf764a

cbfs_spi: Initialize spi_flash when initializing cbfs_cache
Most devices do not use SPI before they initialize CBMEM. This change
initializes spi_flash in the CBMEM_INIT_HOOK to initialize the postram
cbfs cache so it is not overwritten when boot_device_init is called
later.

BUG=chromium:210230
BRANCH=none
TEST=confirm that the first cbfs access can occur before RAM initialized
and after on panther and jerry.

Change-Id: If3b6efc04082190e81c3773c0d3ce116bb12421f
Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
Original-Commit-Id: 0ab242786a16eba7fb423694f6b266e27d7660ec
Original-Change-Id: I5f884b473e51e6813fdd726bba06b56baf3841b0
Original-Signed-off-by: Mary Ruthven <mruthven@chromium.org>
Original-Reviewed-on: https://chromium-review.googlesource.com/314311
Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
Reviewed-on: https://review.coreboot.org/12601
Tested-by: build bot (Jenkins)
Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

src/include/memlayout.h
@@ -82,7 +82,7 @@
 #if ENV_ROMSTAGE
 	#define PRERAM_CBFS_CACHE(addr, size) CBFS_CACHE(addr, size)
 	#define POSTRAM_CBFS_CACHE(addr, size) \
-		REGION(dram_cbfs_cache, addr, size, 4)
+		REGION(postram_cbfs_cache, addr, size, 4)
 #elif defined(__PRE_RAM__)
 	#define PRERAM_CBFS_CACHE(addr, size) CBFS_CACHE(addr, size)
 	#define POSTRAM_CBFS_CACHE(addr, size) \

src/include/symbols.h
@@ -41,9 +41,9 @@ extern u8 _stack[];
 extern u8 _estack[];
 #define _stack_size (_estack - _stack)
 
-extern u8 _dram_cbfs_cache[];
-extern u8 _edram_cbfs_cache[];
-#define _dram_cbfs_cache_size (_edram_cbfs_cache - _dram_cbfs_cache)
+extern u8 _postram_cbfs_cache[];
+extern u8 _epostram_cbfs_cache[];
+#define _postram_cbfs_cache_size (_epostram_cbfs_cache - _postram_cbfs_cache)
 
 extern u8 _cbfs_cache[];
 extern u8 _ecbfs_cache[];

src/lib/cbfs_spi.c
@@ -45,7 +45,14 @@ static struct mmap_helper_region_device mdev =
 
 static void initialize_mdev(int unused)
 {
-	mmap_helper_device_init(&mdev, _dram_cbfs_cache, _dram_cbfs_cache_size);
+	/*
+	 * Call boot_device_init() to ensure spi_flash is initialized before
+	 * backing mdev with postram cache. This prevents the mdev backing from
+	 * being overwritten if spi_flash was not accessed before dram was up.
+	 */
+	boot_device_init();
+	mmap_helper_device_init(&mdev, _postram_cbfs_cache,
+				_postram_cbfs_cache_size);
 }
 ROMSTAGE_CBMEM_INIT_HOOK(initialize_mdev);
 

commit 757943c7d1ebdc109744aea37d3d999bf608a806

memlayout: Fix unified CBFS_CACHE macro
commit a8aef3ac (cbfs_spi: Initialize spi_flash when initializing
cbfs_cache) introduced a bug that makes the rarely-used unified
CBFS_CACHE() memlayout macro break when used in conjunction with
cbfs_spi.c (since that macro does not define a separate
postram_cbfs_cache region). This patch fixes the problem by making all
three region names always available for both the unified and split
macros in every stage (and adds code to ensure we don't reinitialize
the same buffer again in romstage, which might be a bad idea if
previous mappings are still in use).

BRANCH=None
BUG=None
TEST=Compiled for both kinds of macros, manually checked symbols in
disassembled stages.

Change-Id: I114933e93080c8eceab04bfdba3aabf0f75f8ef9
Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
Original-Commit-Id: 0f270f88e54b42afb8b5057b0773644c4ef357ef
Original-Change-Id: If172d9fa3d1fe587aa449bd4de7b5ca87d0f4915
Original-Signed-off-by: Julius Werner <jwerner@chromium.org>
Original-Reviewed-on: https://chromium-review.googlesource.com/318834
Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
Reviewed-on: https://review.coreboot.org/12933
Tested-by: build bot (Jenkins)
Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

src/include/memlayout.h
@@ -63,6 +63,10 @@
 		STR(name must be aligned to expected_align!)); \
 	SYMBOL(e##name, addr + size)
 
+#define ALIAS_REGION(name, alias) \
+	_##alias = _##name; \
+	_e##alias = _e##name;
+
 /* Declare according to SRAM/DRAM ranges in SoC hardware-defined address map. */
 #define SRAM_START(addr) SYMBOL(sram, addr)
 
@@ -77,20 +81,23 @@
 	REGION(preram_cbmem_console, addr, size, 4)
 
 /* Use either CBFS_CACHE (unified) or both (PRERAM|POSTRAM)_CBFS_CACHE */
-#define CBFS_CACHE(addr, size) REGION(cbfs_cache, addr, size, 4)
+#define CBFS_CACHE(addr, size) \
+	REGION(cbfs_cache, addr, size, 4) \
+	ALIAS_REGION(cbfs_cache, preram_cbfs_cache) \
+	ALIAS_REGION(cbfs_cache, postram_cbfs_cache)
 
-#if ENV_ROMSTAGE
-	#define PRERAM_CBFS_CACHE(addr, size) CBFS_CACHE(addr, size)
+#if defined(__PRE_RAM__)
+	#define PRERAM_CBFS_CACHE(addr, size) \
+		REGION(preram_cbfs_cache, addr, size, 4) \
+		ALIAS_REGION(preram_cbfs_cache, cbfs_cache)
 	#define POSTRAM_CBFS_CACHE(addr, size) \
 		REGION(postram_cbfs_cache, addr, size, 4)
-#elif defined(__PRE_RAM__)
-	#define PRERAM_CBFS_CACHE(addr, size) CBFS_CACHE(addr, size)
-	#define POSTRAM_CBFS_CACHE(addr, size) \
-		REGION(unused_cbfs_cache, addr, size, 4)
 #else
 	#define PRERAM_CBFS_CACHE(addr, size) \
-		REGION(unused_cbfs_cache, addr, size, 4)
-	#define POSTRAM_CBFS_CACHE(addr, size) CBFS_CACHE(addr, size)
+		REGION(preram_cbfs_cache, addr, size, 4)
+	#define POSTRAM_CBFS_CACHE(addr, size) \
+		REGION(postram_cbfs_cache, addr, size, 4) \
+		ALIAS_REGION(postram_cbfs_cache, cbfs_cache)
 #endif
 
 /* Careful: 'INCLUDE <filename>' must always be at the end of the output line */

src/include/symbols.h
@@ -41,6 +41,10 @@ extern u8 _stack[];
 extern u8 _estack[];
 #define _stack_size (_estack - _stack)
 
+extern u8 _preram_cbfs_cache[];
+extern u8 _epreram_cbfs_cache[];
+#define _preram_cbfs_cache_size (_epreram_cbfs_cache - _preram_cbfs_cache)
+
 extern u8 _postram_cbfs_cache[];
 extern u8 _epostram_cbfs_cache[];
 #define _postram_cbfs_cache_size (_epostram_cbfs_cache - _postram_cbfs_cache)

src/lib/cbfs_spi.c
@@ -43,7 +43,7 @@ static const struct region_device_ops spi_ops = {
 static struct mmap_helper_region_device mdev =
 	MMAP_HELPER_REGION_INIT(&spi_ops, 0, CONFIG_ROM_SIZE);
 
-static void initialize_mdev(int unused)
+static void switch_to_postram_cache(int unused)
 {
 	/*
 	 * Call boot_device_init() to ensure spi_flash is initialized before
@@ -51,10 +51,11 @@ static void initialize_mdev(int unused)
 	 * being overwritten if spi_flash was not accessed before dram was up.
 	 */
 	boot_device_init();
-	mmap_helper_device_init(&mdev, _postram_cbfs_cache,
-				_postram_cbfs_cache_size);
+	if (_preram_cbfs_cache != _postram_cbfs_cache)
+		mmap_helper_device_init(&mdev, _postram_cbfs_cache,
+					_postram_cbfs_cache_size);
 }
-ROMSTAGE_CBMEM_INIT_HOOK(initialize_mdev);
+ROMSTAGE_CBMEM_INIT_HOOK(switch_to_postram_cache);
 
 void boot_device_init(void)
 {
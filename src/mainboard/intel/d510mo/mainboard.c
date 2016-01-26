761c2942ef1a26dde2b1c9a126c62f091daef789
Damien Zammit <damien@zamaudio.com>
2016-01-26 13:55:43 +1100

    mb/intel/d510mo: Use native gfx initialization
    Change-Id: Ic4de7a762e90b379be3814afc61467e1cd099215
    Signed-off-by: Damien Zammit <damien@zamaudio.com>
    Reviewed-on: https://review.coreboot.org/13034
    Tested-by: build bot (Jenkins)
    Reviewed-by: Alexander Couzens <lynxis@fe80.eu>
    Reviewed-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- src/mainboard/intel/d510mo/mainboard.c
+++ src/mainboard/intel/d510mo/mainboard.c
@@ -18,10 +18,12 @@
 #include <device/pci_ops.h>
 #include <pc80/mc146818rtc.h>
 #include <device/pci.h>
+#include <drivers/intel/gma/int15.h>
 
 static void mainboard_enable(device_t dev)
 {
-	dev->ops->init = NULL;
+	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_NONE,
+		GMA_INT15_PANEL_FIT_DEFAULT, GMA_INT15_BOOT_DISPLAY_CRT, 0);
 }
 
 struct chip_operations mainboard_ops = {

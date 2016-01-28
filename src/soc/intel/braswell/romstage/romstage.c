lalilulelulz/Chromium-OS-Coreboot

Strago: Enable CA Mirror
Configuring UPD PcdCaMirrorEn. This is a board specific parameter.
CA mirror is the Command Address mirroring option that is enabled
on this board

BUG=chrome-os-partner:42267
BRANCH=firmware-strago-7287.B
TEST=Boots up to OS
CQ-DEPEND=CL:*236144

Change-Id: I05174e18d650332d838e5036c713e91c4840ee75
Signed-off-by: Divya Sasidharan <divya.s.sasidharan@intel.com>
Reviewed-on: https://chromium-review.googlesource.com/309190
Reviewed-by: Aaron Durbin <adurbin@chromium.org>
Tested-by: Hannah Williams <hannah.williams@intel.com>
Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
Commit-Queue: Hannah Williams <hannah.williams@intel.com>

src/soc/intel/braswell/romstage/romstage.c
@@ -228,6 +228,7 @@ void soc_memory_init_params(MEMORY_INIT_UPD *params)
 	params->PcdGttSize = config->PcdGttSize;
 	params->PcdLegacySegDecode = config->PcdLegacySegDecode;
 	params->PcdDvfsEnable = config->PcdDvfsEnable;
+	params->PcdCaMirrorEn = config->PcdCaMirrorEn;
 }
 
 void soc_display_memory_init_params(const MEMORY_INIT_UPD *old,

coreboot/coreboot

CQ-DEPEND=CL:13038
Original-Reviewed-on: https://chromium-review.googlesource.com/309190
Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
Original-Tested-by: Hannah Williams <hannah.williams@intel.com>
Original-Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
Signed-off-by: Hannah Williams <hannah.williams@intel.com>
Reviewed-on: https://review.coreboot.org/12749
Tested-by: build bot (Jenkins)
Reviewed-by: Martin Roth <martinroth@google.com>

src/soc/intel/braswell/romstage/romstage.c
@@ -212,6 +212,7 @@ void soc_memory_init_params(struct romstage_params *params,
 	upd->PcdGttSize = config->PcdGttSize;
 	upd->PcdLegacySegDecode = config->PcdLegacySegDecode;
 	upd->PcdDvfsEnable = config->PcdDvfsEnable;
+	upd->PcdCaMirrorEn = config->PcdCaMirrorEn;
 }
 
 void soc_display_memory_init_params(const MEMORY_INIT_UPD *old,
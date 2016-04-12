9943b8829bb63cc8c471efc6e4b1dd32eb742268
Martin Roth <martinroth@google.com>
2016-04-11 20:55:43 -0600

    southbridge/nvidia: Update license headers
    Update all of the license headers to make sure they are compliant
    with coreboot's license header policy.
    
    Change-Id: I7a19ed8cf16b9424190800940d2b8ec1a96c5ce9
    Signed-off-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://review.coreboot.org/14328
    Reviewed-by: Myles Watson <mylesgw@gmail.com>
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

--- src/southbridge/nvidia/ck804/fadt.c
+++ src/southbridge/nvidia/ck804/fadt.c
@@ -1,6 +1,20 @@
 /*
- * ACPI - create the Fixed ACPI Description Tables (FADT)
+ * This file is part of the coreboot project.
+ *
  * (C) Copyright 2005 Stefan Reinauer <stepan@openbios.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
+ * ACPI - create the Fixed ACPI Description Tables (FADT)
  */
 
 #include <string.h>

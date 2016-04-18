c056002141d4e7e36bc36011eeb0b58b33508081
Hannah Williams <hannah.williams@intel.com>
2016-04-18 13:47:08 -0700

    UPSTREAM: soc/apollolake: Add SOC specific c-state table
    Please refer Apollolake BIOS Writers Guide
    
    Change-Id: I5f82cdc4b34a53b5184ef1e918cae15a1df6cc5e
    Original-Signed-off-by: Hannah Williams <hannah.williams@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/15051
    Original-Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Original-Reviewed-by: Martin Roth <martinroth@google.com>
    (cherry-picked from commit 0f61da85820c518341240ce0b60ebfec70187fc0)
    Signed-off-by: Martin Roth <martinroth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/350968
    Commit-Ready: Aaron Durbin <adurbin@chromium.org>
    Tested-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/apollolake/chip.c
+++ src/soc/intel/apollolake/chip.c
@@ -16,6 +16,7 @@
  * GNU General Public License for more details.
  */
 
+#include <arch/acpi.h>
 #include <bootstate.h>
 #include <cbmem.h>
 #include <console/console.h>
@@ -56,6 +57,7 @@ static struct device_operations cpu_bus_ops = {
 	.enable_resources = DEVICE_NOOP,
 	.init = apollolake_init_cpus,
 	.scan_bus = NULL,
+	.acpi_fill_ssdt_generator = generate_cpu_entries,
 };
 
 static void enable_dev(device_t dev)

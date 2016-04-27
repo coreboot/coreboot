7f268eab7816c6521502c16695402f67cd18e7a7
Noah Glovsky <noah.glovsky@watershedschool.org>
2016-04-27 13:09:15 -0600

    mainboard/asus: Add license headers
    Change-Id: I71e461b91f981368d4bd13631b868430d1fc5774
    Signed-off-by: Noah Glovsky <noah.glovsky@watershedschool.org>
    Reviewed-on: https://review.coreboot.org/14530
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Philipp Deppenwiese <zaolin.daisuki@gmail.com>

--- src/mainboard/asus/a8n_e/acpi_tables.c
+++ src/mainboard/asus/a8n_e/acpi_tables.c
@@ -1,8 +1,18 @@
 /*
+ * This file is part of the coreboot project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * ACPI support
  * written by Stefan Reinauer <stepan@openbios.org>
  *  (C) 2005 Stefan Reinauer
- *
  *
  *  Copyright 2005 AMD
  *  2005.9 yhlu modify that to more dynamic for AMD Opteron Based MB

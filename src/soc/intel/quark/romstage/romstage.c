3f0fe68c4b29dd279d0169cda715c64e3000f963
Lee Leahy <leroy.p.leahy@intel.com>
2016-05-15 14:13:30 -0700

    soc/intel/quark: Perform GPIO initialization
    Set the base address and enable the GPIO and legacy GPIO controllers.
    Call the mainboard routine to initialize the GPIO controllers.
    
    TEST=Build and run on Galileo Gen2
    
    Change-Id: I06aed5903d6655d2a0948fb544cf9e0db68faa26
    Signed-off-by: Lee Leahy <leroy.p.leahy@intel.com>
    Reviewed-on: https://review.coreboot.org/14827
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/soc/intel/quark/romstage/romstage.c
+++ src/soc/intel/quark/romstage/romstage.c
@@ -28,6 +28,22 @@
 #include <soc/pci_devs.h>
 #include <soc/pm.h>
 #include <soc/romstage.h>
+#include <soc/reg_access.h>
+
+static const struct reg_script legacy_gpio_init[] = {
+	/* Temporarily enable the legacy GPIO controller */
+	REG_PCI_WRITE32(R_QNC_LPC_GBA_BASE, IO_ADDRESS_VALID
+		| LEGACY_GPIO_BASE_ADDRESS),
+	REG_PCI_OR8(PCI_COMMAND, PCI_COMMAND_IO),
+	REG_SCRIPT_END
+};
+
+static const struct reg_script i2c_gpio_controller_init[] = {
+	/* Temporarily enable the GPIO controller */
+	REG_PCI_WRITE32(PCI_BASE_ADDRESS_1, I2C_GPIO_BASE_ADDRESS),
+	REG_PCI_OR8(PCI_COMMAND, PCI_COMMAND_MEMORY),
+	REG_SCRIPT_END
+};
 
 void car_soc_pre_console_init(void)
 {
@@ -39,6 +55,11 @@ void car_soc_pre_console_init(void)
 void car_soc_post_console_init(void)
 {
 	report_platform_info();
+
+	/* Initialize the controllers */
+	reg_script_run_on_dev(I2CGPIO_BDF, i2c_gpio_controller_init);
+	reg_script_run_on_dev(LPC_BDF, legacy_gpio_init);
+	mainboard_gpio_init();
 };
 
 static struct chipset_power_state power_state CAR_GLOBAL;

2c51572435e8d7bded99ff4c83fc392cdae47772
Julius Werner <jwerner@chromium.org>
2016-05-20 18:06:22 -0700

    arm64: Add stack dump to exception handler
    Some exceptions (like from calling a NULL function pointer) are easier
    to narrow down with a dump of the call stack. Let's take a page out of
    ARM32's book and add that feature to ARM64 as well. Also change the
    output format to two register columns, to make it easier to fit a whole
    exception dump on one screen.
    
    Applying to both coreboot and libpayload and syncing the output format
    between both back up.
    
    Change-Id: I19768d13d8fa8adb84f0edda2af12f20508eb2db
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://review.coreboot.org/14931
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- payloads/libpayload/arch/arm64/exception.c
+++ payloads/libpayload/arch/arm64/exception.c
@@ -60,15 +60,35 @@ static struct exception_handler_info exceptions[EXC_COUNT] = {
 	[EXC_SERROR_ELX_32] = {"_serror_elx_32"},
 };
 
+static void dump_stack(uintptr_t addr, size_t bytes)
+{
+	int i, j;
+	const int words_per_line = 8;
+	uint64_t *ptr = (void *)ALIGN_DOWN(addr, words_per_line * sizeof(*ptr));
+
+	printf("Dumping stack:\n");
+	for (i = bytes / sizeof(*ptr); i >= 0; i -= words_per_line) {
+		printf("%p: ", ptr + i);
+		for (j = i; j < i + words_per_line; j++)
+			printf("%016llx ", *(ptr + j));
+		printf("\n");
+	}
+}
+
 static void print_regs(struct exception_state *state)
 {
 	int i;
 
-	printf("ELR = 0x%016llx\n",state->elr);
-	printf("ESR = 0x%08llx\n",state->esr);
-	for (i = 0; i < 31; i++) {
-		printf("X%02d = 0x%016llx\n", i, state->regs[i]);
+	printf("ELR = 0x%016llx         ESR = 0x%08llx\n",
+	       state->elr, state->esr);
+	printf("FAR = 0x%016llx        SPSR = 0x%08x\n",
+	       raw_read_far_current(), raw_read_spsr_current());
+	for (i = 0; i < 30; i += 2) {
+		printf("X%02d = 0x%016llx         X%02d = 0x%016llx\n",
+		       i, state->regs[i], i + 1, state->regs[i + 1]);
 	}
+	printf("X30 = 0x%016llx          SP = 0x%016llx\n",
+	       state->regs[30], raw_read_sp_el0());
 }
 
 void exception_dispatch(struct exception_state *state, int idx);
@@ -89,6 +109,8 @@ void exception_dispatch(struct exception_state *state, int idx)
 			printf("exception _not_used.\n");
 	}
 	print_regs(state);
+	/* Few words below SP in case we need state from a returned function. */
+	dump_stack(raw_read_sp_el0() - 32, 512);
 
 	if (test_exc) {
 		state->elr += 4;
--- src/arch/arm64/armv8/exception.c
+++ src/arch/arm64/armv8/exception.c
@@ -57,22 +57,38 @@ static const char *exception_names[NUM_EXC_VIDS] = {
 	[EXC_VID_LOW32_SERR] = "_serror_elx_32"
 };
 
+static void dump_stack(uintptr_t addr, size_t bytes)
+{
+	int i, j;
+	const int words_per_line = 8;
+	uint64_t *ptr = (void *)ALIGN_DOWN(addr, words_per_line * sizeof(*ptr));
+
+	printk(BIOS_DEBUG, "Dumping stack:\n");
+	for (i = bytes / sizeof(*ptr); i >= 0; i -= words_per_line) {
+		printk(BIOS_DEBUG, "%p: ", ptr + i);
+		for (j = i; j < i + words_per_line; j++)
+			printk(BIOS_DEBUG, "%016llx ", *(ptr + j));
+		printk(BIOS_DEBUG, "\n");
+	}
+}
+
 static void print_regs(struct exc_state *exc_state)
 {
 	int i;
 	struct elx_state *elx = &exc_state->elx;
 	struct regs *regs = &exc_state->regs;
 
-	uint64_t elx_esr = raw_read_esr_current();
-	uint64_t elx_far = raw_read_far_current();
-
-	printk(BIOS_DEBUG, "ELR = 0x%016llx\n", elx->elr);
-	printk(BIOS_DEBUG, "ESR = 0x%016llx\n", elx_esr);
-	printk(BIOS_DEBUG, "SPSR = 0x%08llx\n", elx->spsr);
-	printk(BIOS_DEBUG, "FAR = 0x%016llx\n", elx_far);
-	printk(BIOS_DEBUG, "SP = 0x%016llx\n", regs->sp);
-	for (i = X0_INDEX; i < XMAX_INDEX; i++)
-		printk(BIOS_DEBUG, "X%02d = 0x%016llx\n", i, regs->x[i]);
+	printk(BIOS_DEBUG, "ELR = 0x%016llx         ESR = 0x%08x\n",
+	       elx->elr, raw_read_esr_current());
+	printk(BIOS_DEBUG, "FAR = 0x%016llx        SPSR = 0x%08x\n",
+	       raw_read_far_current(), raw_read_spsr_current());
+	for (i = 0; i < 30; i += 2) {
+		printk(BIOS_DEBUG,
+		       "X%02d = 0x%016llx         X%02d = 0x%016llx\n",
+		       i, regs->x[i], i + 1, regs->x[i + 1]);
+	}
+	printk(BIOS_DEBUG, "X30 = 0x%016llx          SP = 0x%016llx\n",
+	       regs->x[30], regs->sp);
 }
 
 
@@ -119,6 +135,8 @@ static void print_exception_info(struct exc_state *state, uint64_t idx)
 		printk(BIOS_DEBUG, "exception %s\n", exception_names[idx]);
 
 	print_regs(state);
+	/* Few words below SP in case we need state from a returned function. */
+	dump_stack(state->regs.sp - 32, 512);
 }
 
 static void print_exception_and_die(struct exc_state *state, uint64_t idx)

17163755075149f36c37458af0cb5f398d2a6766
Kyösti Mälkki <kyosti.malkki@gmail.com>
2016-05-27 13:07:50 +0300

    pcengines/apu1: Rename Kconfig variables for pinmux
    Add APU1 prefix because Kconfig throws errors if we try to
    define the same variables as choice-entry for APU2 board.
    
    Change-Id: Ic071600dd88e391a8a278d63aad13abd01fd3c9d
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/14988
    Tested-by: build bot (Jenkins)
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/mainboard/pcengines/apu1/mainboard.c
+++ src/mainboard/pcengines/apu1/mainboard.c
@@ -146,16 +146,16 @@ static void config_gpio_mux(void)
 	uart = dev_find_slot_pnp(SIO_PORT, NCT5104D_SP3);
 	gpio = dev_find_slot_pnp(SIO_PORT, NCT5104D_GPIO0);
 	if (uart)
-		uart->enabled = CONFIG_PINMUX_UART_C;
+		uart->enabled = CONFIG_APU1_PINMUX_UART_C;
 	if (gpio)
-		gpio->enabled = CONFIG_PINMUX_GPIO0;
+		gpio->enabled = CONFIG_APU1_PINMUX_GPIO0;
 
 	uart = dev_find_slot_pnp(SIO_PORT, NCT5104D_SP4);
 	gpio = dev_find_slot_pnp(SIO_PORT, NCT5104D_GPIO1);
 	if (uart)
-		uart->enabled = CONFIG_PINMUX_UART_D;
+		uart->enabled = CONFIG_APU1_PINMUX_UART_D;
 	if (gpio)
-		gpio->enabled = CONFIG_PINMUX_GPIO1;
+		gpio->enabled = CONFIG_APU1_PINMUX_GPIO1;
 }
 
 /**

834561e7d67f35c5702954d8bfc76a5394b256e4
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2016-06-10 19:35:16 +0200

    UPSTREAM: riscv-spike: Replace custom UART with a memory-mapped 8250
    Since the HTIF is a non-standard interface, and coreboot already has a
    8250 driver, I started implementing an 8250 core for spike[1].
    
    [1]: https://github.com/riscv/riscv-isa-sim/pull/53
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Change-Id: I84adc1169474baa8cc5837358a8ad3d184cfa51b
    Original-Signed-off-by: Jonathan Neuschfer <j.neuschaefer@gmx.net>
    Original-Reviewed-on: https://review.coreboot.org/15150
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/352026
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

--- src/mainboard/emulation/spike-riscv/uart.c
+++ src/mainboard/emulation/spike-riscv/uart.c
@@ -19,39 +19,7 @@
 #include <boot/coreboot_tables.h>
 #include <spike_util.h>
 
-static uint8_t *buf = (void *)0x3f8;
 uintptr_t uart_platform_base(int idx)
 {
-	return (uintptr_t) buf;
+	return (uintptr_t) 0x40001000;
 }
-
-void uart_init(int idx)
-{
-}
-
-unsigned char uart_rx_byte(int idx)
-{
-	return *buf; // this does not work on spike, requires more implementation details
-}
-
-void uart_tx_byte(int idx, unsigned char data)
-{
-	mcall_console_putchar(data);
-}
-
-void uart_tx_flush(int idx)
-{
-}
-
-#ifndef __PRE_RAM__
-void uart_fill_lb(void *data)
-{
-	struct lb_serial serial;
-	serial.type = LB_SERIAL_TYPE_MEMORY_MAPPED;
-	serial.baseaddr = 0x3f8;
-	serial.baud = 115200;
-	serial.regwidth = 1;
-	lb_add_serial(&serial, data);
-        lb_add_console(LB_TAG_CONSOLE_SERIAL8250MEM, data);
-}
-#endif

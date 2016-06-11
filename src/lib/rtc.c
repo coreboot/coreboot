7903c214baaa8b8648a541d1147e702f4c6a7d80
Simon Glass <sjg@chromium.org>
2016-06-10 20:58:24 -0600

    UPSTREAM: lib: Add real-time-clock functions
    Add functions to convert between seconds and a struct rtc_time. Also
    add a function that can display the time on the console.
    
    BUG=chrome-os-partner:52220
    BRANCH=none
    TEST=(partial) with future commits and after setting RTC on the EC:
    boot on gru into linux shell, check firmware log:
    
    localhost ~ # grep Date: /sys/firmware/log
    Date: 2016-06-20 (Monday) Time: 18:01:44
    
    Then reboot ~10 seconds and check again:
    
    localhost ~ # grep Date: /sys/firmware/log
    Date: 2016-06-20 (Monday) Time: 18:01:54
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Change-Id: Id148ccb7a18a05865b903307358666ff6c7b4a3d
    Original-Signed-off-by: Martin Roth <martinroth@chromium.org>
    Original-Original-Commit-Id: 3b02dbcd7d9023ce0acabebcf904e70007428d27
    Original-Change-Id: I344c385e2e4cb995d3a374025c205f01c38b660d
    Original-Original-Signed-off-by: Simon Glass <sjg@chromium.org>
    Original-Original-Reviewed-on: https://chromium-review.googlesource.com/351782
    Original-Original-Commit-Ready: Vadim Bendebury <vbendeb@chromium.org>
    Original-Original-Tested-by: Vadim Bendebury <vbendeb@chromium.org>
    Original-Original-Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Original-Reviewed-on: https://review.coreboot.org/15301
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/358385
    Reviewed-by: Martin Roth <martinroth@chromium.org>

--- src/lib/rtc.c
+++ src/lib/rtc.c
@@ -1,8 +1,18 @@
 /*
- * (C) Copyright 2001
- * Wolfgang Denk,  DENX Software Engineering,  wd@denx.de.
+ * This file is part of the coreboot project.
  *
- * SPDX-License-Identifier:	GPL-2.0+
+ * (C) Copyright 2001 Wolfgang Denk, DENX Software Engineering, wd@denx.de.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * From U-Boot 2016.05
  */
 

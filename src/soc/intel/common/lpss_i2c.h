439e0e5449b319d2ed758259e815413bbeca537c
Duncan Laurie <dlaurie@chromium.org>
2016-06-13 10:28:36 -0700

    UPSTREAM: lpss_i2c: Set SDA hold and support custom speed config
    This I2C controller has separate registers for different speeds to set
    specific timing for SCL high and low times, and then a single register
    to configure the SDA hold time.
    
    For the most part these values can be generated based on the freq of
    the controller clock, which is SOC-specific.  The existing driver was
    generating SCL HCNT/LCNT values, but not the SDA hold time so that is
    added.
    
    Additionally a board may need custom values as the exact timing can
    depend on trace lengths and the number of devices on the I2C bus. This
    is a two-part customizaton, the first is to set the values for desired
    speed for use within firmware, and the second is to provide those
    values in ACPI for the OS driver to consume.
    
    And finally, recent upstream changes to the designware i2c driver in
    the Linux kernel now support passing custom timing values for high
    speed and fast-plus speed, so these are now supported as well.
    
    Since these custom speed configs will come from devicetree a macro is
    added to simplify the description:
    
    register "i2c[4].speed_config" = "{
    	 LPSS_I2C_SPEED_CONFIG(STANDARD, 432, 507, 30),
    	 LPSS_I2C_SPEED_CONFIG(FAST, 72, 160, 30),
    	 LPSS_I2C_SPEED_CONFIG(FAST_PLUS, 52, 120, 30),
    	 LPSS_I2C_SPEED_CONFIG(HIGH, 38, 90, 30),
    }"
    
    Which will result in the following speed config in \_SB.PCI0.I2C4:
    
    Name (SSCN, Package () { 432, 507, 30 })
    Name (FMCN, Package () { 72, 160, 30 })
    Name (FPCN, Package () { 52, 120, 30 })
    Name (HSCN, Package () { 38, 90, 30 })
    
    Change-Id: I18964426bb83fad0c956ad43a36ed9e04f3a66b5
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://review.coreboot.org/15163
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/354990
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>

--- src/soc/intel/common/lpss_i2c.h
+++ src/soc/intel/common/lpss_i2c.h
@@ -20,12 +20,91 @@
 #include <stdint.h>
 
 /*
+ * Timing values are in units of clock period, with the clock speed
+ * provided by the SOC in CONFIG_SOC_INTEL_COMMON_LPSS_I2C_CLOCK_MHZ.
+ * Automatic configuration is done based on requested speed, but the
+ * values may need tuned depending on the board and the number of
+ * devices present on the bus.
+ */
+struct lpss_i2c_speed_config {
+	enum i2c_speed speed;
+	/* SCL high and low period count */
+	uint16_t scl_lcnt;
+	uint16_t scl_hcnt;
+	/*
+	 * SDA hold time should be 300ns in standard and fast modes
+	 * and long enough for deterministic logic level change in
+	 * fast-plus and high speed modes.
+	 *
+	 *  [15:0] SDA TX Hold Time
+	 * [23:16] SDA RX Hold Time
+	 */
+	uint32_t sda_hold;
+};
+
+/*
+ * This I2C controller has support for 3 independent speed configs but can
+ * support both FAST_PLUS and HIGH speeds through the same set of speed
+ * config registers.  These are treated separately so the speed config values
+ * can be provided via ACPI to the OS.
+ */
+#define LPSS_I2C_SPEED_CONFIG_COUNT	4
+
+#define LPSS_I2C_SPEED_CONFIG(speedval,lcnt,hcnt,hold)	\
+	{						\
+		.speed = I2C_SPEED_ ## speedval,	\
+		.scl_lcnt = (lcnt),			\
+		.scl_hcnt = (hcnt),			\
+		.sda_hold = (hold),			\
+	}
+
+/*
  * Return the base address for this bus controller.
  *
  * This function *must* be implemented by the SOC and return the appropriate
  * base address for the I2C registers that correspond to the provided bus.
  */
 uintptr_t lpss_i2c_base_address(unsigned bus);
+
+/*
+ * Generate speed configuration for requested controller and bus speed.
+ *
+ * This allows a SOC or board to automatically generate speed config to use
+ * in firmware and provide to the OS.
+ */
+int lpss_i2c_gen_speed_config(enum i2c_speed speed,
+			      struct lpss_i2c_speed_config *config);
+
+/*
+ * Set raw speed configuration for given speed type.
+ *
+ * This allows a SOC or board to override the automatic bus speed calculation
+ * and provided specific values for the driver to use.
+ */
+int lpss_i2c_set_speed_config(unsigned bus,
+			      const struct lpss_i2c_speed_config *config);
+
+/*
+ * Write ACPI object to describe speed configuration.
+ *
+ * ACPI Object: Name ("xxxx", Package () { scl_lcnt, scl_hcnt, sda_hold }
+ *
+ * SSCN: I2C_SPEED_STANDARD
+ * FMCN: I2C_SPEED_FAST
+ * FPCN: I2C_SPEED_FAST_PLUS
+ * HSCN: I2C_SPEED_HIGH
+ */
+void lpss_i2c_acpi_write_speed_config(
+	const struct lpss_i2c_speed_config *config);
+
+/*
+ * Set I2C bus speed for this controller.
+ *
+ * This allows an SOC or board to set the basic I2C bus speed.  Values for the
+ * controller configuration registers will be calculated, for more specific
+ * control the raw configuration can be provided to lpss_i2c_set_speed_config().
+ */
+int lpss_i2c_set_speed(unsigned bus, enum i2c_speed speed);
 
 /*
  * Initialize this bus controller and set the speed.
@@ -36,6 +115,6 @@ uintptr_t lpss_i2c_base_address(unsigned bus);
  * The SOC *must* define CONFIG_SOC_INTEL_COMMON_LPSS_I2C_CLOCK for the
  * bus speed calculation to be correct.
  */
-void lpss_i2c_init(unsigned bus, enum i2c_speed speed);
+int lpss_i2c_init(unsigned bus, enum i2c_speed speed);
 
 #endif

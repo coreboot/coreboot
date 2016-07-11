c2710ebf1fa128a05972f74582be73934811379f
Paul Menzel <paulepanter@users.sourceforge.net>
2016-07-11 09:01:22 +0200

    UPSTREAM: lib/version: Correct whitespace alignment
    Remove and add spaces for a consistent alignment.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Change-Id: I612800cd60d97f50737c235465d7d0a87f2251a8
    Original-Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Original-Reviewed-on: https://review.coreboot.org/15596
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Jonathan Neuschfer <j.neuschaefer@gmx.net>
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/360818
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/lib/version.c
+++ src/lib/version.c
@@ -5,21 +5,21 @@
 #error CONFIG_MAINBOARD_VENDOR not defined
 #endif
 #ifndef CONFIG_MAINBOARD_PART_NUMBER
-#error  CONFIG_MAINBOARD_PART_NUMBER not defined
+#error CONFIG_MAINBOARD_PART_NUMBER not defined
 #endif
 
 #ifndef COREBOOT_VERSION
-#error  COREBOOT_VERSION not defined
+#error COREBOOT_VERSION not defined
 #endif
 #ifndef COREBOOT_BUILD
-#error  COREBOOT_BUILD not defined
+#error COREBOOT_BUILD not defined
 #endif
 
 #ifndef COREBOOT_COMPILE_TIME
-#error  COREBOOT_COMPILE_TIME not defined
+#error COREBOOT_COMPILE_TIME not defined
 #endif
 
-#ifndef  COREBOOT_EXTRA_VERSION
+#ifndef COREBOOT_EXTRA_VERSION
 #define COREBOOT_EXTRA_VERSION ""
 #endif
 
@@ -31,8 +31,8 @@ const char coreboot_extra_version[] = COREBOOT_EXTRA_VERSION;
 const char coreboot_build[] = COREBOOT_BUILD;
 const unsigned int coreboot_version_timestamp = COREBOOT_VERSION_TIMESTAMP;
 
-const char coreboot_compile_time[]   = COREBOOT_COMPILE_TIME;
-const char coreboot_dmi_date[]       = COREBOOT_DMI_DATE;
+const char coreboot_compile_time[] = COREBOOT_COMPILE_TIME;
+const char coreboot_dmi_date[] = COREBOOT_DMI_DATE;
 
 const struct bcd_date coreboot_build_date = {
 	.century = 0x20,

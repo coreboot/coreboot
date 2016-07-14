c126ce6e734d261d7ba13f37cfc83b5a787b4e5f
Aaron Durbin <adurbin@chromium.org>
2016-07-13 23:25:50 -0500

    UPSTREAM: southbridge/intel/fsp_rangeley: use common Intel ACPI hardware definitions
    Transition to using the common Intel ACPI hardware definitions
    generic ACPI definitions.
    
    BUG=chrome-os-partner:54977
    
    Change-Id: Ia113672fa3cb740cb193c23fd06181d9ce895ac3
    Original-Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Original-Reviewed-on: https://review.coreboot.org/15680
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Patrick Georgi <pgeorgi@google.com>
    Original-Reviewed-by: Furquan Shaikh <furquan@google.com>
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/360840
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/southbridge/intel/fsp_rangeley/soc.h
+++ src/southbridge/intel/fsp_rangeley/soc.h
@@ -18,6 +18,8 @@
 #ifndef SOUTHBRIDGE_INTEL_RANGELEY_SOC_H
 #define SOUTHBRIDGE_INTEL_RANGELEY_SOC_H
 
+#include <arch/acpi.h>
+
 /* SOC types */
 #define SOC_TYPE_RANGELEY	0x1F
 
@@ -316,13 +318,6 @@ void rangeley_sb_early_initialization(void);
 #define   GBL_EN	(1 << 5)
 #define   TMROF_EN	(1 << 0)
 #define PM1_CNT		0x04
-#define   SLP_EN	(1 << 13)
-#define   SLP_TYP	(7 << 10)
-#define    SLP_TYP_S0	0
-#define    SLP_TYP_S1	1
-#define    SLP_TYP_S3	5
-#define    SLP_TYP_S4	6
-#define    SLP_TYP_S5	7
 #define   GBL_RLS	(1 << 2)
 #define   BM_RLD	(1 << 1)
 #define   SCI_EN	(1 << 0)

bf80d9d15b513f64ec9929f0a14b9cc18807e8c5
Aaron Durbin <adurbin@chromium.org>
2016-07-13 23:21:12 -0500

    UPSTREAM: soc/intel/fsp_broadwell_de: use common Intel ACPI hardware definitions
    Transition to using the common Intel ACPI hardware definitions
    generic ACPI definitions.
    
    BUG=chrome-os-partner:54977
    
    Change-Id: Iecd94494cb568b20bdf6649b46a9a9586074bdc7
    Original-Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Original-Reviewed-on: https://review.coreboot.org/15672
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Furquan Shaikh <furquan@google.com>
    Original-Reviewed-by: York Yang <york.yang@intel.com>
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/360832
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/soc/intel/fsp_broadwell_de/include/soc/lpc.h
+++ src/soc/intel/fsp_broadwell_de/include/soc/lpc.h
@@ -17,6 +17,8 @@
 #ifndef _SOC_LPC_H_
 #define _SOC_LPC_H_
 
+#include <arch/acpi.h>
+
 /* LPC Interface Bridge PCI Configuration Registers */
 #define REVID			0x08
 #define PIRQ_RCR1		0x60
@@ -49,14 +51,6 @@
 #define   GBL_EN		(1 << 5)
 #define   TMROF_EN		(1 << 0)
 #define PM1_CNT			0x04
-#define   SLP_EN		(1 << 13)
-#define   SLP_TYP_SHIFT		10
-#define   SLP_TYP		(7 << SLP_TYP_SHIFT)
-#define    SLP_TYP_S0		0
-#define    SLP_TYP_S1		1
-#define    SLP_TYP_S3		5
-#define    SLP_TYP_S4		6
-#define    SLP_TYP_S5		7
 #define   GBL_RLS		(1 << 2)
 #define   BM_RLD		(1 << 1)
 #define   SCI_EN		(1 << 0)

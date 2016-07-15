64011880240cea5a3f8b1177853c7992a2d99ea8
Andrey Petrov <andrey.petrov@intel.com>
2016-07-15 13:31:09 -0700

    soc/intel/common: Add reset_prepare() for common reset
    Some Intel SoC may need preparation before reset can be properly
    handled. Add callback that chip/soc code can implement.
    
    BUG=chrome-os-partner:55055
    
    Change-Id: I45857838e1a306dbcb9ed262b55e7db88a8944e5
    Signed-off-by: Andrey Petrov <andrey.petrov@intel.com>
    Reviewed-on: https://review.coreboot.org/15720
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/include/reset.h
+++ src/include/reset.h
@@ -10,5 +10,6 @@ void soft_reset(void);
 void cpu_reset(void);
 /* Some Intel SoCs use a special reset that is specific to SoC */
 void global_reset(void);
-
+/* Some Intel SoCs may need to prepare/wait before reset */
+void reset_prepare(void);
 #endif

ec2947fb075838d1ea78754113aef6cf000cf522
Abhay Kumar <abhay.kumar@intel.com>
2016-07-14 18:43:54 -0700

    soc/intel/apollolake: Remove PEIM GFX from normal mode and S3 resume
    Do not pass VBT table to fsp in normal mode and S3 resume so that
    PEIM GFX will not get initialized.
    
    Change-Id: Iab7be3cceb0f80ae0273940b36fdd9c41bdb121e
    Signed-off-by: Abhay Kumar <abhay.kumar@intel.com>
    Reviewed-on: https://review.coreboot.org/14575
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/soc/intel/common/vbt.h
+++ src/soc/intel/common/vbt.h
@@ -21,4 +21,9 @@
 
 /* locate .vbt file */
 enum cb_err locate_vbt(struct region_device *rdev);
+/*
+ * Returns VBT pointer and mapping after checking prerequisites for Pre OS
+ * Graphics initialization
+ */
+void *vbt_get(struct region_device *rdev);
 #endif

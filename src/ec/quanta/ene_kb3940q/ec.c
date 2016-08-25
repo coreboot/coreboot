2b010b8795de84b6753c5e49d6a73c25fee96da1
Elyes HAOUAS <ehaouas@noos.fr>
2016-08-25 20:57:08 +0200

    src/ec: Add required space before opening parenthesis '('
    Change-Id: I013f71b702644ab337c3d76be1489530bad6e6cc
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/16322
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/ec/quanta/ene_kb3940q/ec.c
+++ src/ec/quanta/ene_kb3940q/ec.c
@@ -32,7 +32,7 @@
 static int ec_input_buffer_empty(u8 status_port)
 {
 	u32 timeout;
-	for(timeout = KBC_TIMEOUT_IN_MS; timeout && (inb(status_port) & KBD_IBF);
+	for (timeout = KBC_TIMEOUT_IN_MS; timeout && (inb(status_port) & KBD_IBF);
 	    timeout--) {
 		mdelay(1);
 	}
@@ -47,7 +47,7 @@ static int ec_input_buffer_empty(u8 status_port)
 static int ec_output_buffer_full(u8 status_port)
 {
 	u32 timeout;
-	for(timeout = KBC_TIMEOUT_IN_MS; timeout && ((inb(status_port)
+	for (timeout = KBC_TIMEOUT_IN_MS; timeout && ((inb(status_port)
 	    & KBD_OBF) == 0); timeout--) {
 		mdelay(1);
 	}

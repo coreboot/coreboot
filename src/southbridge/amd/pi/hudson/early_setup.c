1ac972804896db7925800dcf316eb463eb7b2972
Antonello Dettori <dev@dettori.io>
2016-09-03 10:45:33 +0200

    southbridge/amd/pi/hudson: transition away from device_t
    Replace the use of the old device_t definition inside
    southbridge/amd/pi/hudson.
    
    Change-Id: I8b22a8d9f0e90afaf0f218c5c0924a78883b7498
    Signed-off-by: Antonello Dettori <dev@dettori.io>
    Reviewed-on: https://review.coreboot.org/16475
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/southbridge/amd/pi/hudson/early_setup.c
+++ src/southbridge/amd/pi/hudson/early_setup.c
@@ -60,7 +60,7 @@ void configure_hudson_uart(void)
 void hudson_pci_port80(void)
 {
 	u8 byte;
-	device_t dev;
+	pci_devfn_t dev;
 
 	/* P2P Bridge */
 	dev = PCI_DEV(0, 0x14, 4);
@@ -105,7 +105,7 @@ void hudson_pci_port80(void)
 void hudson_lpc_port80(void)
 {
 	u8 byte;
-	device_t dev;
+	pci_devfn_t dev;
 
 	/* Enable LPC controller */
 	outb(0xEC, 0xCD6);
@@ -123,7 +123,7 @@ void hudson_lpc_port80(void)
 
 void hudson_lpc_decode(void)
 {
-	device_t dev;
+	pci_devfn_t dev;
 	u32 tmp = 0;
 
 	/* Enable I/O decode to LPC bus */

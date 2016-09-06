0b9ecb5831bdba869035e70f0773596881a97d27
Arthur Heymans <arthur@aheymans.xyz>
2016-09-06 23:03:04 +0200

    mb/intel/d945gclf: Allow use of native graphic init
    Add PCI device id to native graphic init and add the Native graphic init
    option in Kconfig.
    
    Change-Id: I136122daef70547830bcc87f568406be7162461f
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/16512
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/northbridge/intel/i945/gma.c
+++ src/northbridge/intel/i945/gma.c
@@ -726,7 +726,7 @@ static struct device_operations gma_func1_ops = {
 	.ops_pci		= &gma_pci_ops,
 };
 
-static const unsigned short pci_device_ids[] = { 0x27a2, 0x27ae, 0 };
+static const unsigned short pci_device_ids[] = { 0x27a2, 0x27ae, 0x2772, 0 };
 
 static const struct pci_driver i945_gma_func0_driver __pci_driver = {
 	.ops	= &gma_func0_ops,

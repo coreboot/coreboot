54573a018c7819d1bfb39df064182a695df79c1a
Aaron Durbin <adurbin@chromium.org>
2016-09-16 16:15:14 -0500

    UPSTREAM: arch/x86: move postcar main logic into C
    The console_init(), MTRR printing, and loading ramstage
    logic was previously all in assembly. Move that logic
    into C code so that future features can more easily be
    added into the postcar boot flow.
    
    BUG=chrome-os-partner:57513
    BRANCH=None
    TEST=None
    
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://review.coreboot.org/16618
    Tested-by: build bot (Jenkins)
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Andrey Petrov <andrey.petrov@intel.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    
    Change-Id: I332140f569caf0803570fd635d894295de8c0018
    Reviewed-on: https://chromium-review.googlesource.com/386985
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/arch/x86/postcar.c
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2016 Google, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <console/console.h>
+#include <main_decl.h>
+#include <program_loading.h>
+#include <soc/intel/common/util.h>
+
+void main(void)
+{
+	console_init();
+
+	/* Display the MTRRs */
+	if (IS_ENABLED(CONFIG_DISPLAY_MTRRS))
+		soc_display_mtrrs();
+
+	/* Load and run ramstage. */
+	run_ramstage();
+}

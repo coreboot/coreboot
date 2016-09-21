2cd67b72746d36186b42f068627e7d2c46e1aecf
Marc Jones <marcj303@gmail.com>
2016-09-20 20:14:15 -0600

    northbridge/amd: Copy 00660F01 directories to 00670F00
    Prepare for new 00670FF00 (StoneyRidge) support.
    
    Original-Signed-off-by: Marc Jones <marcj303@gmail.com>
    Original-Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Original-Tested-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    (cherry picked from commit 037cf16883fafd329a15f903ddf97e24a879bcce)
    
    Change-Id: I130d4f13beb2c1d71e4e4e9be5011f7993b34660
    Signed-off-by: Marc Jones <marcj303@gmail.com>
    Reviewed-on: https://review.coreboot.org/17142
    Tested-by: build bot (Jenkins)
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

---
+++ src/northbridge/amd/pi/00670F00/chip.h
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2013 Sage Electronic Engineering, LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _PI_FAM15CZ_CHIP_H_
+#define _PI_FAM15CZ_CHIP_H_
+
+struct northbridge_amd_pi_00660F01_config
+{
+	u8 spdAddrLookup[2][2][4];
+};
+
+#endif

db04fc55cc2a1c2a86c1a6d81d7beacc3256a411
Marc Jones <marcj303@gmail.com>
2016-09-20 20:06:43 -0600

    UPSTREAM: cpu/amd: Copy 00660F01 to 00670F00
    Prepare for new 00670F00 (StoneyRidge) support.
    
    Original-Signed-off-by: Marc Jones <marcj303@gmail.com>
    Original-Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Original-Tested-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    (cherry picked from commit 87d26e05189247685df0ca6492dc3181a1bad5e8)
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Marc Jones <marcj303@gmail.com>
    Reviewed-on: https://review.coreboot.org/17139
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    
    Change-Id: Ib296ad32a061669b28dae742cac08bb75fdd0de4
    Reviewed-on: https://chromium-review.googlesource.com/407181
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/cpu/amd/pi/00670F00/chip_name.c
@@ -0,0 +1,20 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2015 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <device/device.h>
+
+struct chip_operations cpu_amd_pi_00660F01_ops = {
+	CHIP_NAME("AMD CPU Family 15h")
+};

38bee539b12c11c3bba453858b748fd741097f76
Derek Basehore <dbasehore@chromium.org>
2016-09-21 17:42:26 -0700

    google/gru: change center logic voltage to 900mV
    It seems that we should only ever run at 900mV on center logic.
    Changing it to 950mV before might have just masked over problems that
    are now fixed.
    
    BRANCH=none
    BUG=chrome-os-partner:56940
    TEST=on kevin, run
    stressapptest -M 1536 -s 1000
    
    Change-Id: I5a09b1b403df800396bb2f2e8c76d14a4519d44a
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/391032
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Commit-Queue: Lin Huang <hl@rock-chips.com>
    Tested-by: Lin Huang <hl@rock-chips.com>
    Reviewed-on: https://review.coreboot.org/19356
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Tested-by: build bot (Jenkins)

--- src/mainboard/google/gru/romstage.c
+++ src/mainboard/google/gru/romstage.c
@@ -21,6 +21,7 @@
 #include <cbfs.h>
 #include <cbmem.h>
 #include <console/console.h>
+#include <delay.h>
 #include <program_loading.h>
 #include <romstage_handoff.h>
 #include <soc/addressmap.h>
@@ -37,7 +38,9 @@ static void init_dvs_outputs(void)
 {
 	pwm_regulator_configure(PWM_REGULATOR_GPU, 900);
 	pwm_regulator_configure(PWM_REGULATOR_BIG, 900);
-	pwm_regulator_configure(PWM_REGULATOR_CENTERLOG, 950);
+	pwm_regulator_configure(PWM_REGULATOR_CENTERLOG, 900);
+	/* Allow time for the regulators to settle */
+	udelay(500);
 }
 
 static void prepare_usb(void)

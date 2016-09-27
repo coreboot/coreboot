8f372d031b6b76405d6267e90b30829c1e8f9e2b
Elyes HAOUAS <ehaouas@noos.fr>
2016-09-27 21:36:55 +0200

    mainboard/technexion/tim5690: Use tabs for indents
    Change-Id: Icd1f145b3575c6d95dacceb9c0426fbdedcdd686
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/16777
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

--- src/mainboard/technexion/tim5690/tn_post_code.c
+++ src/mainboard/technexion/tim5690/tn_post_code.c
@@ -35,90 +35,90 @@
 // TechNexion's Post Code Initially.
 void technexion_post_code_init(void)
 {
-   uint8_t reg8_data;
-   device_t dev = 0;
+	uint8_t reg8_data;
+	device_t dev = 0;
 
-   // SMBus Module and ACPI Block (Device 20, Function 0)
-   dev = pci_locate_device(PCI_ID(PCI_VENDOR_ID_ATI, PCI_DEVICE_ID_ATI_SB600_SM), 0);
+	// SMBus Module and ACPI Block (Device 20, Function 0)
+	dev = pci_locate_device(PCI_ID(PCI_VENDOR_ID_ATI, PCI_DEVICE_ID_ATI_SB600_SM), 0);
 
-   // LED[bit0]:GPIO0
-   // This is reference SB600 RRG 4.1.1 GPIO
-   reg8_data = pmio_read(0x60);
-   reg8_data |= (1 << 7);  // 1: GPIO if not used by SATA
-   pmio_write(0x60, reg8_data);
+	// LED[bit0]:GPIO0
+	// This is reference SB600 RRG 4.1.1 GPIO
+	reg8_data = pmio_read(0x60);
+	reg8_data |= (1 << 7);  // 1: GPIO if not used by SATA
+	pmio_write(0x60, reg8_data);
 
-   reg8_data = pci_read_config8(dev, 0x80);
-   reg8_data = ((reg8_data | (1 << 0)) & ~(1 << 4));
-   pci_write_config8(dev, 0x80, reg8_data);
+	reg8_data = pci_read_config8(dev, 0x80);
+	reg8_data = ((reg8_data | (1 << 0)) & ~(1 << 4));
+	pci_write_config8(dev, 0x80, reg8_data);
 
-   // LED[bit1]:GPIO1
-   // This is reference SB600 RRG 4.1.1 GPIO
-   reg8_data = pci_read_config8(dev, 0x80);
-   reg8_data = ((reg8_data | (1 << 1)) & ~(1 << 5));
-   pci_write_config8(dev, 0x80, reg8_data);
+	// LED[bit1]:GPIO1
+	// This is reference SB600 RRG 4.1.1 GPIO
+	reg8_data = pci_read_config8(dev, 0x80);
+	reg8_data = ((reg8_data | (1 << 1)) & ~(1 << 5));
+	pci_write_config8(dev, 0x80, reg8_data);
 
-   // LED[bit2]:GPIO4
-   // This is reference SB600 RRG 4.1.1 GPIO
-   reg8_data = pmio_read(0x5e);
-   reg8_data &= ~(1 << 7); // 0: GPIO if not used by SATA
-   pmio_write(0x5e, reg8_data);
+	// LED[bit2]:GPIO4
+	// This is reference SB600 RRG 4.1.1 GPIO
+	reg8_data = pmio_read(0x5e);
+	reg8_data &= ~(1 << 7); // 0: GPIO if not used by SATA
+	pmio_write(0x5e, reg8_data);
 
-   reg8_data = pci_read_config8(dev, 0xa8);
-   reg8_data |= (1 << 0);
-   pci_write_config8(dev, 0xa8, reg8_data);
+	reg8_data = pci_read_config8(dev, 0xa8);
+	reg8_data |= (1 << 0);
+	pci_write_config8(dev, 0xa8, reg8_data);
 
-   reg8_data = pci_read_config8(dev, 0xa9);
-   reg8_data &= ~(1 << 0);
-   pci_write_config8(dev, 0xa9, reg8_data);
+	reg8_data = pci_read_config8(dev, 0xa9);
+	reg8_data &= ~(1 << 0);
+	pci_write_config8(dev, 0xa9, reg8_data);
 
-   // LED[bit3]:GPIO6
-   // This is reference SB600 RRG 4.1.1 GPIO
-   reg8_data = pmio_read(0x60);
-   reg8_data |= (1 << 7); // 1: GPIO if not used by SATA
-   pmio_write(0x60, reg8_data);
+	// LED[bit3]:GPIO6
+	// This is reference SB600 RRG 4.1.1 GPIO
+	reg8_data = pmio_read(0x60);
+	reg8_data |= (1 << 7); // 1: GPIO if not used by SATA
+	pmio_write(0x60, reg8_data);
 
-   reg8_data = pci_read_config8(dev, 0xa8);
-   reg8_data |= (1 << 2);
-   pci_write_config8(dev, 0xa8, reg8_data);
+	reg8_data = pci_read_config8(dev, 0xa8);
+	reg8_data |= (1 << 2);
+	pci_write_config8(dev, 0xa8, reg8_data);
 
-   reg8_data = pci_read_config8(dev, 0xa9);
-   reg8_data &= ~(1 << 2);
-   pci_write_config8(dev, 0xa9, reg8_data);
-   // LED[bit4]:GPIO7
-   // This is reference SB600 RRG 4.1.1 GPIO
-   reg8_data = pci_read_config8(dev, 0xa8);
-   reg8_data |= (1 << 3);
-   pci_write_config8(dev, 0xa8, reg8_data);
+	reg8_data = pci_read_config8(dev, 0xa9);
+	reg8_data &= ~(1 << 2);
+	pci_write_config8(dev, 0xa9, reg8_data);
+	// LED[bit4]:GPIO7
+	// This is reference SB600 RRG 4.1.1 GPIO
+	reg8_data = pci_read_config8(dev, 0xa8);
+	reg8_data |= (1 << 3);
+	pci_write_config8(dev, 0xa8, reg8_data);
 
-   reg8_data = pci_read_config8(dev, 0xa9);
-   reg8_data &= ~(1 << 3);
-   pci_write_config8(dev, 0xa9, reg8_data);
+	reg8_data = pci_read_config8(dev, 0xa9);
+	reg8_data &= ~(1 << 3);
+	pci_write_config8(dev, 0xa9, reg8_data);
 
-   // LED[bit5]:GPIO8
-   // This is reference SB600 RRG 4.1.1 GPIO
-   reg8_data = pci_read_config8(dev, 0xa8);
-   reg8_data |= (1 << 4);
-   pci_write_config8(dev, 0xa8, reg8_data);
+	// LED[bit5]:GPIO8
+	// This is reference SB600 RRG 4.1.1 GPIO
+	reg8_data = pci_read_config8(dev, 0xa8);
+	reg8_data |= (1 << 4);
+	pci_write_config8(dev, 0xa8, reg8_data);
 
-   reg8_data = pci_read_config8(dev, 0xa9);
-   reg8_data &= ~(1 << 4);
-   pci_write_config8(dev, 0xa9, reg8_data);
+	reg8_data = pci_read_config8(dev, 0xa9);
+	reg8_data &= ~(1 << 4);
+	pci_write_config8(dev, 0xa9, reg8_data);
 
-   // LED[bit6]:GPIO10
-   // This is reference SB600 RRG 4.1.1 GPIO
-   reg8_data = pci_read_config8(dev, 0xab);
-   reg8_data = ((reg8_data | (1 << 0)) & ~(1 << 1));
-   pci_write_config8(dev, 0xab, reg8_data);
+	// LED[bit6]:GPIO10
+	// This is reference SB600 RRG 4.1.1 GPIO
+	reg8_data = pci_read_config8(dev, 0xab);
+	reg8_data = ((reg8_data | (1 << 0)) & ~(1 << 1));
+	pci_write_config8(dev, 0xab, reg8_data);
 
-   // LED[bit7]:GPIO66
-   // This is reference SB600 RRG 4.1.1 GPIO
-   reg8_data = pmio_read(0x68);
-   reg8_data &= ~(1 << 5); // 0: GPIO
-   pmio_write(0x68, reg8_data);
+	// LED[bit7]:GPIO66
+	// This is reference SB600 RRG 4.1.1 GPIO
+	reg8_data = pmio_read(0x68);
+	reg8_data &= ~(1 << 5); // 0: GPIO
+	pmio_write(0x68, reg8_data);
 
-   reg8_data = pci_read_config8(dev, 0x7e);
-   reg8_data = ((reg8_data | (1 << 1)) & ~(1 << 5));
-   pci_write_config8(dev, 0x7e, reg8_data);
+	reg8_data = pci_read_config8(dev, 0x7e);
+	reg8_data = ((reg8_data | (1 << 1)) & ~(1 << 5));
+	pci_write_config8(dev, 0x7e, reg8_data);
 
 }
 
@@ -128,96 +128,96 @@ void technexion_post_code_init(void)
  */
 void technexion_post_code(uint8_t udata8)
 {
-   uint8_t u8_data;
-   device_t dev = 0;
+	uint8_t u8_data;
+	device_t dev = 0;
 
-   // SMBus Module and ACPI Block (Device 20, Function 0)
+	// SMBus Module and ACPI Block (Device 20, Function 0)
 #ifdef __PRE_RAM__
-   dev = pci_locate_device(PCI_ID(PCI_VENDOR_ID_ATI, PCI_DEVICE_ID_ATI_SB600_SM), 0);
+	dev = pci_locate_device(PCI_ID(PCI_VENDOR_ID_ATI, PCI_DEVICE_ID_ATI_SB600_SM), 0);
 #else
-   dev = dev_find_device(PCI_VENDOR_ID_ATI, PCI_DEVICE_ID_ATI_SB600_SM, 0);
+	dev = dev_find_device(PCI_VENDOR_ID_ATI, PCI_DEVICE_ID_ATI_SB600_SM, 0);
 #endif
 
-   udata8 = ~(udata8);
+	udata8 = ~(udata8);
 
-   // LED[bit0]:GPIO0
-   u8_data = pci_read_config8(dev, 0x80);
-   if (udata8 & 0x1) {
-      u8_data |= (1 << 0);
-   }
-   else {
-      u8_data &= ~(1 << 0);
-   }
-   pci_write_config8(dev, 0x80, u8_data);
+	// LED[bit0]:GPIO0
+	u8_data = pci_read_config8(dev, 0x80);
+	if (udata8 & 0x1) {
+		u8_data |= (1 << 0);
+	}
+	else {
+		u8_data &= ~(1 << 0);
+	}
+	pci_write_config8(dev, 0x80, u8_data);
 
-   // LED[bit1]:GPIO1
-   u8_data = pci_read_config8(dev, 0x80);
-   if (udata8 & 0x2) {
-      u8_data |= (1 << 1);
-   }
-   else {
-      u8_data &= ~(1 << 1);
-   }
-   pci_write_config8(dev, 0x80, u8_data);
+	// LED[bit1]:GPIO1
+	u8_data = pci_read_config8(dev, 0x80);
+	if (udata8 & 0x2) {
+		u8_data |= (1 << 1);
+	}
+	else {
+		u8_data &= ~(1 << 1);
+	}
+	pci_write_config8(dev, 0x80, u8_data);
 
-   // LED[bit2]:GPIO4
-   u8_data = pci_read_config8(dev, 0xa8);
-   if (udata8 & 0x4) {
-      u8_data |= (1 << 0);
-   }
-   else {
-      u8_data &= ~(1 << 0);
-   }
-   pci_write_config8(dev, 0xa8, u8_data);
+	// LED[bit2]:GPIO4
+	u8_data = pci_read_config8(dev, 0xa8);
+	if (udata8 & 0x4) {
+		u8_data |= (1 << 0);
+	}
+	else {
+		u8_data &= ~(1 << 0);
+	}
+	pci_write_config8(dev, 0xa8, u8_data);
 
-   // LED[bit3]:GPIO6
-   u8_data = pci_read_config8(dev, 0xa8);
-   if (udata8 & 0x8) {
-      u8_data |= (1 << 2);
-   }
-   else {
-      u8_data &= ~(1 << 2);
-   }
-   pci_write_config8(dev, 0xa8, u8_data);
+	// LED[bit3]:GPIO6
+	u8_data = pci_read_config8(dev, 0xa8);
+	if (udata8 & 0x8) {
+		u8_data |= (1 << 2);
+	}
+	else {
+		u8_data &= ~(1 << 2);
+	}
+	pci_write_config8(dev, 0xa8, u8_data);
 
-   // LED[bit4]:GPIO7
-   u8_data = pci_read_config8(dev, 0xa8);
-   if (udata8 & 0x10) {
-      u8_data |= (1 << 3);
-   }
-   else {
-      u8_data &= ~(1 << 3);
-   }
-   pci_write_config8(dev, 0xa8, u8_data);
+	// LED[bit4]:GPIO7
+	u8_data = pci_read_config8(dev, 0xa8);
+	if (udata8 & 0x10) {
+		u8_data |= (1 << 3);
+	}
+	else {
+		u8_data &= ~(1 << 3);
+	}
+	pci_write_config8(dev, 0xa8, u8_data);
 
-   // LED[bit5]:GPIO8
-   u8_data = pci_read_config8(dev, 0xa8);
-   if (udata8 & 0x20) {
-      u8_data |= (1 << 4);
-   }
-   else {
-      u8_data &= ~(1 << 4);
-   }
-   pci_write_config8(dev, 0xa8, u8_data);
+	// LED[bit5]:GPIO8
+	u8_data = pci_read_config8(dev, 0xa8);
+	if (udata8 & 0x20) {
+		u8_data |= (1 << 4);
+	}
+	else {
+		u8_data &= ~(1 << 4);
+	}
+	pci_write_config8(dev, 0xa8, u8_data);
 
-   // LED[bit6]:GPIO10
-   u8_data = pci_read_config8(dev, 0xab);
-   if (udata8 & 0x40) {
-      u8_data |= (1 << 0);
-   }
-   else {
-      u8_data &= ~(1 << 0);
-   }
-   pci_write_config8(dev, 0xab, u8_data);
+	// LED[bit6]:GPIO10
+	u8_data = pci_read_config8(dev, 0xab);
+	if (udata8 & 0x40) {
+		u8_data |= (1 << 0);
+	}
+	else {
+		u8_data &= ~(1 << 0);
+	}
+	pci_write_config8(dev, 0xab, u8_data);
 
-   // LED[bit7]:GPIO66
-   u8_data = pci_read_config8(dev, 0x7e);
-   if (udata8 & 0x80) {
-      u8_data |= (1 << 1);
-   }
-   else {
-      u8_data &= ~(1 << 1);
-   }
-   pci_write_config8(dev, 0x7e, u8_data);
+	// LED[bit7]:GPIO66
+	u8_data = pci_read_config8(dev, 0x7e);
+	if (udata8 & 0x80) {
+		u8_data |= (1 << 1);
+	}
+	else {
+		u8_data &= ~(1 << 1);
+	}
+	pci_write_config8(dev, 0x7e, u8_data);
 
 }
--- src/mainboard/technexion/tim5690/vgabios.c
+++ src/mainboard/technexion/tim5690/vgabios.c
@@ -30,39 +30,39 @@ static rs690_vbios_regs vbios_regs_local;
 /* Initialization interrupt function */
 static void vbios_fun_init(rs690_vbios_regs *vbios_regs)
 {
-        vbios_regs_local.int15_regs.fun00_panel_id = vbios_regs->int15_regs.fun00_panel_id;
-        vbios_regs_local.int15_regs.fun05_tv_standard = vbios_regs->int15_regs.fun05_tv_standard;
+	vbios_regs_local.int15_regs.fun00_panel_id = vbios_regs->int15_regs.fun00_panel_id;
+	vbios_regs_local.int15_regs.fun05_tv_standard = vbios_regs->int15_regs.fun05_tv_standard;
 }
 
 /* BIOS int15 function */
 int tim5690_int15_handler(void)
 {
-        int res = 0;
+	int res = 0;
 
-        printk(BIOS_DEBUG, "tim5690_int15_handler\n");
+	printk(BIOS_DEBUG, "tim5690_int15_handler\n");
 
-        switch (X86_EAX & 0xffff) {
-        case AMD_RS690_INT15:
-                switch (X86_EBX & 0xff) {
-                case 0x00:
-                        X86_EAX &= ~(0xff);
-                        X86_EBX = (X86_EBX & ~(0xff)) | vbios_regs_local.int15_regs.fun00_panel_id;
-                        res = 1;
-                        break;
-                case 0x05:
-                        X86_EAX &= ~(0xff);
-                        X86_EBX = (X86_EBX & ~(0xff)) | vbios_regs_local.int15_regs.fun05_tv_standard;
-                        res = 1;
-                        break;
-                }
-                break;
-        default:
-                printk(BIOS_DEBUG, "Unknown INT15 function %04x!\n",
-                                X86_EAX & 0xffff);
+	switch (X86_EAX & 0xffff) {
+	case AMD_RS690_INT15:
+		switch (X86_EBX & 0xff) {
+		case 0x00:
+			X86_EAX &= ~(0xff);
+			X86_EBX = (X86_EBX & ~(0xff)) | vbios_regs_local.int15_regs.fun00_panel_id;
+			res = 1;
+			break;
+		case 0x05:
+			X86_EAX &= ~(0xff);
+			X86_EBX = (X86_EBX & ~(0xff)) | vbios_regs_local.int15_regs.fun05_tv_standard;
+			res = 1;
+			break;
+		}
 		break;
-        }
+	default:
+		printk(BIOS_DEBUG, "Unknown INT15 function %04x!\n",
+				X86_EAX & 0xffff);
+		break;
+	}
 
-        return res;
+	return res;
 }
 
 /* Initialization VBIOS function */

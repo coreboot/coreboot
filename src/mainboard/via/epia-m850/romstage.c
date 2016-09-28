0c31a677b0dbc17b0909a5f6d5f0ab31294bd36b
Elyes HAOUAS <ehaouas@noos.fr>
2016-09-28 21:24:40 +0200

    mainboard/via/epia-m850/romstage.c: Remove unnecessary whitespace
    Change-Id: Ic5359f794c64a9a67ec9e059af337a65073baff4
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/16784
    Reviewed-by: Martin Roth <martinroth@google.com>
    Tested-by: Martin Roth <martinroth@google.com>

--- src/mainboard/via/epia-m850/romstage.c
+++ src/mainboard/via/epia-m850/romstage.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the coreboot project.
  *
-  * Copyright (C) 2011-2012  Alexandru Gagniuc <mr.nuke.me@gmail.com>
+ * Copyright (C) 2011-2012  Alexandru Gagniuc <mr.nuke.me@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

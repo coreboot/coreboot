9e71da4bb17bdc5640fcc17295a5b1a3f765cfc3
Elyes HAOUAS <ehaouas@noos.fr>
2016-09-29 21:00:13 +0200

    UPSTREAM: mainboard/kontron/ktqm77: Use tabs for indents
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/16822
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>
    
    Change-Id: I3040dc9a2534a77087805ee61c4f91d1bdb4d509
    Reviewed-on: https://chromium-review.googlesource.com/391799
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/kontron/ktqm77/mainboard.c
+++ src/mainboard/kontron/ktqm77/mainboard.c
@@ -148,7 +148,7 @@ static int int15_handler(void)
 		X86_EAX |= 0x015f;
 		res = 1;
 		break;
-        default:
+	default:
 		printk(BIOS_DEBUG, "Unknown INT15 function %04x!\n",
 		       X86_EAX & 0xffff);
 		break;

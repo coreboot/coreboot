885c289bba6554545ae21896a318f71e4ccb16a8
Arthur Heymans <arthur@aheymans.xyz>
2016-10-03 17:16:48 +0200

    nb/intel/i945: Make pci_mmio_size a devicetree parameter
    Instead of hardcoding pci_mmio_size in the raminit code,
    this makes it a parameter in the devicetree.
    
    A safe minimum of 768M is also defined since using anything
    less causes problems (if 4G of ram is used).
    
    Change-Id: If004c861464162d5dbbc61836a3a205d1619dfd5
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/16856
    Tested-by: build bot (Jenkins)
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/northbridge/intel/i945/chip.h
+++ src/northbridge/intel/i945/chip.h
@@ -8,6 +8,7 @@ struct northbridge_intel_i945_config {
 	u32 gpu_backlight;
 	int gpu_lvds_use_spread_spectrum_clock;
 	struct i915_gpu_controller_info gfx;
+	int pci_mmio_size;
 };
 
 #endif /* NORTHBRIDGE_INTEL_I945_CHIP_H */

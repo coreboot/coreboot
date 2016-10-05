ecc4ce723d498ecc2d9f5cd53c11291b0266eaae
Nico Huber <nico.huber@secunet.com>
2016-10-05 17:46:49 +0200

    UPSTREAM: Hook up libhwbase in ramstage
    It's hidden behind a configuration option `CONFIG_RAMSTAGE_LIBHWBASE`.
    
    This also adds some glue code to use the coreboot console for debug
    output and our monotonic timer framework as timer backend.
    
    v2: Also update 3rdparty/libhwbase to the latest master commit.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Change-Id: I8e8d50271b46aac1141f95ab55ad323ac0889a8d
    Signed-off-by: Nico Huber <nico.huber@secunet.com>
    Reviewed-on: https://review.coreboot.org/16951
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/422559
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/lib/hw-time-timer.adb
@@ -0,0 +1,48 @@
+--
+-- This file is part of the coreboot project.
+--
+-- Copyright (C) 2016 secunet Security Networks AG
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; version 2 of the License.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+
+with Interfaces.C;
+
+package body HW.Time.Timer
+   with Refined_State => (Timer_State => null,
+                          Abstract_Time => null)
+is
+
+   procedure Timer_Monotonic_Get (MT : out Interfaces.C.long);
+   pragma Import (C, Timer_Monotonic_Get, "timer_monotonic_get");
+
+   function Raw_Value_Min return T
+   with
+      SPARK_Mode => Off
+   is
+      Microseconds : Interfaces.C.long;
+   begin
+      Timer_Monotonic_Get (Microseconds);
+      return T (Microseconds);
+   end Raw_Value_Min;
+
+   function Raw_Value_Max return T
+   is
+   begin
+      return Raw_Value_Min + 1;
+   end Raw_Value_Max;
+
+   function Hz return T
+   is
+   begin
+      return 1_000_000;
+   end Hz;
+
+end HW.Time.Timer;

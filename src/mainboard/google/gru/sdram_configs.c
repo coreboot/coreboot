4ef9899bfe2f4cfdab0f57023b85f95fbf94d63a
Lin Huang <hl@rock-chips.com>
2016-10-13 20:14:42 -0700

    rockchip/rk3399: gru/kevin: drop unused sdram configs
    There are some sdram configurations that are no longer used. Drop them.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Change-Id: Ib6d2d58c3071147a3095bc1ed7fa7b02c748e1a5
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 111d375005ec6a3b91e47acdd676e8f1644c931c
    Original-Change-Id: I5f9278093f02e785b2894faa8e8cf09ecec20325
    Original-Signed-off-by: Lin Huang <hl@rock-chips.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/399122
    Original-Commit-Ready: Douglas Anderson <dianders@chromium.org>
    Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://review.coreboot.org/17103
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/mainboard/google/gru/sdram_configs.c
+++ src/mainboard/google/gru/sdram_configs.c
@@ -23,23 +23,17 @@
 #include <types.h>
 
 static const char *sdram_configs[] = {
-	"sdram-lpddr3-hynix-4GB-200",
 	"sdram-lpddr3-hynix-4GB-666",
 	"sdram-lpddr3-hynix-4GB-800",
-	"sdram-lpddr3-hynix-4GB-666-no-odt",
-	"sdram-lpddr3-hynix-4GB-800-no-odt",
 	"sdram-lpddr3-hynix-4GB-933",
 };
 
 static struct rk3399_sdram_params params;
 
 enum dram_speeds {
-	dram_200MHz = 0,
-	dram_666MHz = 1,
-	dram_800MHz = 2,
-	dram_666MHz_NO_ODT = 3,
-	dram_800MHz_NO_ODT = 4,
-	dram_933MHz = 5,
+	dram_666MHz = 0,
+	dram_800MHz = 1,
+	dram_933MHz = 2,
 };
 
 static enum dram_speeds get_sdram_index(void)
@@ -57,12 +51,7 @@ static enum dram_speeds get_sdram_index(void)
 		}
 
 	if (IS_ENABLED(CONFIG_BOARD_GOOGLE_GRU))
-		switch (id) {
-		case 0:
-			return dram_800MHz_NO_ODT;
-		default:
 			return dram_800MHz;
-		}
 }
 
 const struct rk3399_sdram_params *get_sdram_config()

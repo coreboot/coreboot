4a988001d1cc08817e840794887e94c154008c5d
Elyes HAOUAS <ehaouas@noos.fr>
2016-10-15 10:01:21 +0200

    UPSTREAM: util/msrtool: Use tabs for indents
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/17031
    Tested-by: build bot (Jenkins)
    Reviewed-by: Alexander Couzens <lynxis@fe80.eu>
    Reviewed-by: Anton Kochkov <anton.kochkov@gmail.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    
    Change-Id: Ib1aa4ad04dc8a584a751677aac5652cfa2e457df
    Reviewed-on: https://chromium-review.googlesource.com/400477
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- util/msrtool/sys.c
+++ util/msrtool/sys.c
@@ -26,12 +26,12 @@ struct cpuid_t *cpuid(void) {
 
 /* First, we need determine which vendor we have */
 #if defined(__DARWIN__) && !defined(__LP64__)
-        asm volatile (
-                "pushl %%ebx    \n"
-                "cpuid          \n"
-                "popl %%ebx     \n"
-                : "=b" (outebx) : "a" (0) : "%ecx", "%edx"
-        );
+	asm volatile (
+		"pushl %%ebx    \n"
+		"cpuid          \n"
+		"popl %%ebx     \n"
+		: "=b" (outebx) : "a" (0) : "%ecx", "%edx"
+	);
 #else
 	asm ("cpuid" : "=b" (outebx) : "a" (0) : "%ecx", "%edx");
 #endif
@@ -40,12 +40,12 @@ struct cpuid_t *cpuid(void) {
 
 /* Then, identificate CPU itself */
 #if defined(__DARWIN__) && !defined(__LP64__)
-        asm volatile (
-                "pushl %%ebx    \n"
-                "cpuid          \n"
-                "popl %%ebx     \n"
-                : "=a" (outeax) : "a" (1) : "%ecx", "%edx"
-        );
+	asm volatile (
+		"pushl %%ebx    \n"
+		"cpuid          \n"
+		"popl %%ebx     \n"
+		: "=a" (outeax) : "a" (1) : "%ecx", "%edx"
+	);
 #else
 	asm ("cpuid" : "=a" (outeax) : "a" (1) : "%ebx", "%ecx", "%edx");
 #endif

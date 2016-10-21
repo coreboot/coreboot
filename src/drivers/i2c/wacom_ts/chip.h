4f7690c6f3c22b1cc0807b12f44b796b5f3f90dd
Furquan Shaikh <furquan@chromium.org>
2016-10-21 16:42:45 -0700

    UPSTREAM: drivers/i2c/wacom_ts: Add support for WCOM touchscreen device driver
    BUG=chrome-os-partner:57846
    BRANCH=None
    TEST=None
    
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://review.coreboot.org/17092
    Tested-by: build bot (Jenkins)
    Reviewed-by: Jonathan Neuschfer <j.neuschaefer@gmx.net>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    
    Change-Id: Id6bd91b3fd6420994ad5811d362618b1a38a8afa
    Reviewed-on: https://chromium-review.googlesource.com/404109
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/drivers/i2c/wacom_ts/chip.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2016 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __I2C_WACOM_TS_CHIP_H__
+#define __I2C_WACOM_TS_CHIP_H__
+
+#include <drivers/i2c/generic/chip.h>
+
+#define WCOM50C1_HID		"WCOM50C1"
+#define PNP0C50_CID		"PNP0C50"
+#define WCOM_TS_DESC		"WCOM Touchscreen"
+
+struct drivers_i2c_wacom_ts_config {
+	struct drivers_i2c_generic_config generic;
+	uint8_t hid_desc_reg_offset;
+};
+
+#endif /* __I2C_WACOM_TS_CHIP_H__ */

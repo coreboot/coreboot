b4946a9e654599034adf4075c266bf529b8216a7
PH Hsu <ph.hsu@mediatek.com>
2016-10-25 15:03:23 +0800

    google/oak: Add more DRAM modules support
    Add support for following 3 modules.
    - Micro MT52L256M32D1PF / MT52L512M32D2PF
    - Hynix H9CCNNNBJTALAR
    
    Hana EVT was planed to add 4 DRAM modules but RAM_CODE=5 is not used
    in the end.
    This patch also unifies the naming of the RAM configurations.
    
    BUG=chrome-os-partner:58983
    TEST=verified on Hana EVT.
    
    Change-Id: I7dd44525de8e9dde01f210f4730fa8ccd4baef21
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 5dccd68149bcfd6fd0a83e310d43063bab645691
    Original-Change-Id: I7c245c8c24be159e152f4f3cca25bf970b58425c
    Original-Signed-off-by: Milton Chiang <milton.chiang@mediatek.com>
    Original-Signed-off-by: PH Hsu <ph.hsu@mediatek.com>
    Original-Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/402888
    Original-Reviewed-by: Pin-Huan Hsu <ph.hsu@mediatek.com>
    Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
    Original-Reviewed-by: Paris Yeh <pyeh@chromium.org>
    Reviewed-on: https://review.coreboot.org/17381
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/mainboard/google/oak/sdram_configs.c
+++ src/mainboard/google/oak/sdram_configs.c
@@ -19,22 +19,22 @@
 #include <stdlib.h>
 
 static const struct mt8173_sdram_params sdram_configs[] = {
-#include "sdram_inf/sdram-lpddr3-hynix-2GB.inc"      /* ram_code = 0000 */
-#include "sdram_inf/sdram-lpddr3-samsung-2GB.inc"    /* ram_code = 0001 */
-#include "sdram_inf/sdram-lpddr3-K4E6E304EE-4GB.inc" /* ram_code = 0010 */
-#include "sdram_inf/sdram-lpddr3-hynix-4GB.inc"      /* ram_code = 0011 */
-#include "sdram_inf/sdram-lpddr3-K4E6E304EB-4GB.inc" /* ram_code = 0100 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 0101 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 0110 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 0111 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 1000 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 1001 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 1010 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 1011 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 1100 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 1101 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 1110 */
-#include "sdram_inf/sdram-unused.inc"                /* ram_code = 1111 */
+#include "sdram_inf/sdram-lpddr3-H9CCNNN8GTMLAR-2GB.inc"  /* ram_code = 0000 */
+#include "sdram_inf/sdram-lpddr3-K4E8E304EE-2GB.inc"      /* ram_code = 0001 */
+#include "sdram_inf/sdram-lpddr3-K4E6E304EE-4GB.inc"      /* ram_code = 0010 */
+#include "sdram_inf/sdram-lpddr3-H9CCNNNBLTBLAR-4GB.inc"  /* ram_code = 0011 */
+#include "sdram_inf/sdram-lpddr3-K4E6E304EB-4GB.inc"      /* ram_code = 0100 */
+#include "sdram_inf/sdram-unused.inc"                     /* ram_code = 0101 */
+#include "sdram_inf/sdram-lpddr3-MT52L512M32D2PF-4GB.inc" /* ram_code = 0110 */
+#include "sdram_inf/sdram-lpddr3-H9CCNNNBJTALAR-4GB.inc"  /* ram_code = 0111 */
+#include "sdram_inf/sdram-lpddr3-MT52L256M32D1PF-2GB.inc" /* ram_code = 1000 */
+#include "sdram_inf/sdram-unused.inc"                     /* ram_code = 1001 */
+#include "sdram_inf/sdram-unused.inc"                     /* ram_code = 1010 */
+#include "sdram_inf/sdram-unused.inc"                     /* ram_code = 1011 */
+#include "sdram_inf/sdram-unused.inc"                     /* ram_code = 1100 */
+#include "sdram_inf/sdram-unused.inc"                     /* ram_code = 1101 */
+#include "sdram_inf/sdram-unused.inc"                     /* ram_code = 1110 */
+#include "sdram_inf/sdram-unused.inc"                     /* ram_code = 1111 */
 };
 
 const struct mt8173_sdram_params *get_sdram_config(void)

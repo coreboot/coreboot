46575fb1d4f00e0d1e69e9ad38acb4c1e4b38711
Naresh G Solanki <naresh.solanki@intel.com>
2016-10-27 21:18:25 +0530

    mainboard/intel/kblrvp: Update onboard memory specific configs
    1. Update dq, dqs map & Rcomp strength & Rcomp target.
    2. Fix rvp3.spd.hex byte 2 to 0x0F(JEDEC LPDDR3 memory type).
    
    Change-Id: I7efc3499b915d1e414cfe914830232993ef10ba2
    Signed-off-by: Naresh G Solanki <naresh.solanki@intel.com>
    Reviewed-on: https://review.coreboot.org/17162
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>

--- src/mainboard/intel/kblrvp/spd/spd_util.c
+++ src/mainboard/intel/kblrvp/spd/spd_util.c
@@ -28,8 +28,8 @@ void mainboard_fill_dq_map_data(void *dq_map_ptr)
 	const u8 dq_map[2][12] = {
 		  { 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
 		    0x0F, 0x00, 0xFF, 0x00, 0xFF, 0x00 },
-		  { 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
-		    0x0F, 0x00, 0xFF, 0x00, 0xFF, 0x00 } };
+		  { 0x33, 0xCC, 0x00, 0xCC, 0x33, 0xCC,
+		    0x33, 0x00, 0xFF, 0x00, 0xFF, 0x00 } };
 	memcpy(dq_map_ptr, dq_map, sizeof(dq_map));
 }
 
@@ -37,8 +37,8 @@ void mainboard_fill_dqs_map_data(void *dqs_map_ptr)
 {
 	/* DQS CPU<>DRAM map */
 	const u8 dqs_map[2][8] = {
-		{ 0, 1, 3, 2, 6, 5, 4, 7 },
-		{ 2, 3, 0, 1, 6, 7, 4, 5 } };
+		{ 0, 1, 3, 2, 4, 5, 6, 7 },
+		{ 1, 0, 4, 5, 2, 3, 6, 7 } };
 	memcpy(dqs_map_ptr, dqs_map, sizeof(dqs_map));
 }
 

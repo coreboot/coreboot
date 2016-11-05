f1395d825be59663913f88ddaf01cbfa6c46afa7
Nicola Corna <nicola@corna.info>
2016-11-05 16:06:59 +0100

    ec/lenovo/h8: Add USB Always On
    USB AO is the internal name for the dedicated charging port on
    ThinkPads when in S3 or lower.
    
    AOEN (bit 0) is internal name for enabling this feature while AOCF
    (bits 2 and 3) is the configuration field. According to Peter Stuge,
    AOCF can be configured in this way:
    
        00 => AC S3 S4 S4 USB on, battery S3 USB on, battery S4 S5 off
        11 => AC S3 S4 S4 USB on, battery S3 S4 S5 USB off
        10, 01 => equivalent to 00
    
    This commit also adds a new configuration field in the CMOS of the
    X220 and the X201 to activate this feature. It probably can be also
    added to all the ThinkPads that support this functionality.
    
    With this functionality USB devices are able to negotiate full power
    from the dedicated port (usually the yellow one) even in S3.
    
    Tested on a X201 and X220 with an Android smartphone: with this
    feature enabled it shows "Charging" when connected during S3, without
    it it shows "Charging slowly" (or it doesn't charge at all on the
    X201).
    
    For some reasons the "AC only" mode doesn't work, so it has been
    disabled.
    
    Change-Id: Ie1269a4357e2fbd608ad8b7b8262275914730f6e
    Signed-off-by: Nicola Corna <nicola@corna.info>
    Reviewed-on: https://review.coreboot.org/17252
    Tested-by: build bot (Jenkins)
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/ec/lenovo/h8/h8.c
+++ src/ec/lenovo/h8/h8.c
@@ -268,6 +268,8 @@ static void h8_enable(struct device *dev)
 	ec_write(0x1f, conf->eventf_enable);
 
 	ec_write(H8_FAN_CONTROL, H8_FAN_CONTROL_AUTO);
+	ec_write(H8_USB_ALWAYS_ON, ec_read(H8_USB_ALWAYS_ON) &
+			~H8_USB_ALWAYS_ON_ENABLE);
 
 	if (get_option(&val, "wlan") != CB_SUCCESS)
 		val = 1;

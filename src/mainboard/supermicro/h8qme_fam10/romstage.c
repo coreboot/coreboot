912dc7046bfb41c3759f3e24852bc7108ce383f0
Antonello Dettori <dev@dettori.io>
2016-11-08 18:44:46 +0100

    UPSTREAM: mainboard/supermicro/h8qme_fam10: transition away from device_t
    Replace the use of the old device_t definition inside
    mainboard/supermicro/h8qme_fam10.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Antonello Dettori <dev@dettori.io>
    Reviewed-on: https://review.coreboot.org/17306
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>
    
    Change-Id: Ia03c205ce498eadf8a34749a6a21fb2d0b29c840
    Reviewed-on: https://chromium-review.googlesource.com/410112
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/supermicro/h8qme_fam10/romstage.c
+++ src/mainboard/supermicro/h8qme_fam10/romstage.c
@@ -113,14 +113,14 @@ static const u8 spd_addr[] = {
 #define GPIO3_DEV PNP_DEV(0x2e, W83627HF_GPIO3)
 
 /* TODO: superio code should really not be in mainboard */
-static void pnp_enter_ext_func_mode(device_t dev)
+static void pnp_enter_ext_func_mode(pnp_devfn_t dev)
 {
 	u16 port = dev >> 8;
 	outb(0x87, port);
 	outb(0x87, port);
 }
 
-static void pnp_exit_ext_func_mode(device_t dev)
+static void pnp_exit_ext_func_mode(pnp_devfn_t dev)
 {
 	u16 port = dev >> 8;
 	outb(0xaa, port);

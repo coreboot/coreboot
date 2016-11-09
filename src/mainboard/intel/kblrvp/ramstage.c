974f258c08cd09060bd09680e2195fef28830459
Naresh G Solanki <naresh.solanki@intel.com>
2016-11-09 18:53:53 +0530

    UPSTREAM: intel/kblrvp: Program I/O expander
    Program I/O expander connected on I2C bus 4
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Naresh G Solanki <naresh.solanki@intel.com>
    Reviewed-on: https://review.coreboot.org/17338
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Martin Roth <martinroth@google.com>
    
    Change-Id: I1a431f50e7b06446399a7d7cb9490615818147e7
    Reviewed-on: https://chromium-review.googlesource.com/410116
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/intel/kblrvp/ramstage.c
+++ src/mainboard/intel/kblrvp/ramstage.c
@@ -13,20 +13,23 @@
  * GNU General Public License for more details.
  */
 
+#include <bootstate.h>
+#include <console/console.h>
+#include <device/i2c.h>
 #include <soc/ramstage.h>
 #include "gpio.h"
 
 void mainboard_silicon_init_params(FSP_SIL_UPD *params)
 {
-	u8 i;
+	size_t i;
 	/* Configure pads prior to SiliconInit() in case there's any
 	 * dependencies during hardware initialization. */
 	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 	params->CdClock = 3;
 
 	/* Set proper OC for various USB ports*/
-	u8 usb2_oc[] = { 0x0, 0x2, 0x8, 0x8, 0x2, 0x8, 0x8, 0x8, 0x1, 0x8};
-	u8 usb3_oc[] = { 0x0, 0x8, 0x8, 0x1, 0x8, 0x8 };
+	u8 usb2_oc[] = {0x0, 0x2, 0x8, 0x8, 0x2, 0x8, 0x8, 0x8, 0x1, 0x8};
+	u8 usb3_oc[] = {0x0, 0x8, 0x8, 0x1, 0x8, 0x8};
 
 	for (i = 0; i < ARRAY_SIZE(usb2_oc); i++)
 		params->Usb2OverCurrentPin[i] = usb2_oc[i];
@@ -34,3 +37,39 @@ void mainboard_silicon_init_params(FSP_SIL_UPD *params)
 	for (i = 0; i < ARRAY_SIZE(usb3_oc); i++)
 		params->Usb3OverCurrentPin[i] = usb3_oc[i];
 }
+
+static void ioexpander_init(void *unused)
+{
+	printk(BIOS_DEBUG, "Programming TCA6424A I/O expander\n");
+
+	/* I/O Expander 1, Port 0 Data */
+	i2c_writeb(IO_EXPANDER_BUS, IO_EXPANDER_0_ADDR, IO_EXPANDER_P0DOUT,
+		0xF7);
+	/* Port 0 Configuration */
+	i2c_writeb(IO_EXPANDER_BUS, IO_EXPANDER_0_ADDR, IO_EXPANDER_P0CONF,
+		0xE0);
+
+	/* Port 1 Data */
+	i2c_writeb(IO_EXPANDER_BUS, IO_EXPANDER_0_ADDR, IO_EXPANDER_P1DOUT,
+		0x9E);
+	/* Port 1 Configuration */
+	i2c_writeb(IO_EXPANDER_BUS, IO_EXPANDER_0_ADDR, IO_EXPANDER_P1CONF,
+		0x8C);
+
+	/* Port 2 Data */
+	i2c_writeb(IO_EXPANDER_BUS, IO_EXPANDER_0_ADDR, IO_EXPANDER_P2DOUT,
+		0xDA);
+	/* Port 2 Configuration */
+	i2c_writeb(IO_EXPANDER_BUS, IO_EXPANDER_0_ADDR, IO_EXPANDER_P2CONF,
+		0x08);
+
+	/* I/O Expander 2, Port 0 Data */
+	i2c_writeb(IO_EXPANDER_BUS, IO_EXPANDER_1_ADDR, IO_EXPANDER_P0DOUT,
+		0xFF);
+	/* Port 0 Configuration */
+	i2c_writeb(IO_EXPANDER_BUS, IO_EXPANDER_1_ADDR, IO_EXPANDER_P0CONF,
+		0x00);
+
+}
+
+BOOT_STATE_INIT_ENTRY(BS_POST_DEVICE, BS_ON_EXIT, ioexpander_init, NULL);

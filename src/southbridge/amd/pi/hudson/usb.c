fe7e0d779d5711d15580b8a488c9ed639d0547c8
Kyösti Mälkki <kyosti.malkki@gmail.com>
2016-11-19 22:38:13 +0200

    UPSTREAM: AMD binaryPI: Drop commented code with bad PCI IDs
    There is mismatch of VENDOR_ID_AMD with DEVICE_ID_ATI, also
    the device IDs have not been defined.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Kysti Mlkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/17508
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Edward O'Callaghan <edward.ocallaghan@koparo.com>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    
    Change-Id: I3076cb08e3181e7f86de38deb18f1661f037bc38
    Reviewed-on: https://chromium-review.googlesource.com/414555
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/southbridge/amd/pi/hudson/usb.c
+++ src/southbridge/amd/pi/hudson/usb.c
@@ -54,28 +54,3 @@ static const struct pci_driver usb_0_driver __pci_driver = {
 	.vendor = PCI_VENDOR_ID_AMD,
 	.devices = pci_device_ids,
 };
-
-/*
-static struct device_operations usb_ops2 = {
-	.read_resources = pci_ehci_read_resources,
-	.set_resources = pci_dev_set_resources,
-	.enable_resources = pci_dev_enable_resources,
-	.init = usb_init2,
-	.scan_bus = 0,
-	.ops_pci = &lops_pci,
-};
-*/
-/*
-static const struct pci_driver usb_5_driver __pci_driver = {
-	.ops = &usb_ops2,
-	.vendor = PCI_VENDOR_ID_AMD,
-	.device = PCI_DEVICE_ID_ATI_HUDSON_USB_18_2,
-};
-*/
-/*
- * static const struct pci_driver usb_5_driver __pci_driver = {
- * 	.ops = &usb_ops2,
- * 	.vendor = PCI_VENDOR_ID_AMD,
- * 	.device = PCI_DEVICE_ID_ATI_HUDSON_USB_19_2,
- * };
- */

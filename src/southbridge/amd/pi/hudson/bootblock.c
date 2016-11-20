04de114172781114db7167ba1feeccbda1b26e72
Kyösti Mälkki <kyosti.malkki@gmail.com>
2016-11-20 08:24:12 +0200

    UPSTREAM: AMD binaryPI: Use explicit PCI IO config access in bootblock
    This allows us to set MMCONF_SUPPORT_DEFAULT since we enable
    MMCONF early in romstage.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Kysti Mlkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/17532
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    
    Change-Id: I380cf483bfe4e2d64969110ae6d5d04c3ced2418
    Reviewed-on: https://chromium-review.googlesource.com/416152
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/southbridge/amd/pi/hudson/bootblock.c
+++ src/southbridge/amd/pi/hudson/bootblock.c
@@ -34,15 +34,15 @@ static void hudson_enable_rom(void)
 	dev = PCI_DEV(0, 0x14, 3);
 
 	/* Decode variable LPC ROM address ranges 1 and 2. */
-	reg8 = pci_read_config8(dev, 0x48);
+	reg8 = pci_io_read_config8(dev, 0x48);
 	reg8 |= (1 << 3) | (1 << 4);
-	pci_write_config8(dev, 0x48, reg8);
+	pci_io_write_config8(dev, 0x48, reg8);
 
 	/* LPC ROM address range 1: */
 	/* Enable LPC ROM range mirroring start at 0x000e(0000). */
-	pci_write_config16(dev, 0x68, 0x000e);
+	pci_io_write_config16(dev, 0x68, 0x000e);
 	/* Enable LPC ROM range mirroring end at 0x000f(ffff). */
-	pci_write_config16(dev, 0x6a, 0x000f);
+	pci_io_write_config16(dev, 0x6a, 0x000f);
 
 	/* LPC ROM address range 2: */
 	/*
@@ -52,9 +52,9 @@ static void hudson_enable_rom(void)
 	 * 0xffe0(0000): 2MB
 	 * 0xffc0(0000): 4MB
 	 */
-	pci_write_config16(dev, 0x6c, 0x10000 - (CONFIG_COREBOOT_ROMSIZE_KB >> 6));
+	pci_io_write_config16(dev, 0x6c, 0x10000 - (CONFIG_COREBOOT_ROMSIZE_KB >> 6));
 	/* Enable LPC ROM range end at 0xffff(ffff). */
-	pci_write_config16(dev, 0x6e, 0xffff);
+	pci_io_write_config16(dev, 0x6e, 0xffff);
 }
 
 static void bootblock_southbridge_init(void)

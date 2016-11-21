03a0a6f8fc0f93beb1ce99d506f1185e9d5c03ae
Arthur Heymans <arthur@aheymans.xyz>
2016-11-21 17:11:48 +0100

    UPSTREAM: mb/asus/p5gc-mx: Add mainboard
    Tested to work:
    * GPU (Nvidia gt210) in PCIe x16 slot;
    * SATA;
    * serial;
    * 800MHz and 1067MHz FSB Core 2 Duo CPUs;
    * ethernet;
    * native VGA graphic init.
    
    What does not work:
    * resume from s3 suspend;
    * superio hardware monitor (not initialised in coreboot).
    
    Quirks:
    * does not boot with just one dimm in slot B.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/17558
    Tested-by: build bot (Jenkins)
    Reviewed-by: Kysti Mlkki <kyosti.malkki@gmail.com>
    
    Change-Id: Ide5494be7f2f16d6b5cfd2ccf4ec438f0587add5
    Reviewed-on: https://chromium-review.googlesource.com/424867
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/mainboard/asus/p5gc-mx/hda_verb.c
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2016 Arthur Heymans <arthur@aheymans.xyz>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <device/azalia_device.h>
+
+const u32 cim_verb_data[] = {
+	/* coreboot specific header */
+	0x10ec0883,     /* Vendor ID */
+	0x104382c7,     /* Subsystem ID */
+	0x0000000c,     /* Number of entries */
+
+	/* Pin Widget Verb Table */
+	AZALIA_PIN_CFG(0, 0x14, 0x01014010),
+	AZALIA_PIN_CFG(0, 0x15, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x16, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x17, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x18, 0x01a19840),
+	AZALIA_PIN_CFG(0, 0x19, 0x02a19850),
+	AZALIA_PIN_CFG(0, 0x1a, 0x0181304f),
+	AZALIA_PIN_CFG(0, 0x1b, 0x02214020),
+	AZALIA_PIN_CFG(0, 0x1c, 0x593301f0),
+	AZALIA_PIN_CFG(0, 0x1d, 0x4005c603),
+	AZALIA_PIN_CFG(0, 0x1e, 0x18561130),
+	AZALIA_PIN_CFG(0, 0x1f, 0x411111f0),
+};
+
+const u32 pc_beep_verbs[0] = {};
+AZALIA_ARRAY_SIZES;

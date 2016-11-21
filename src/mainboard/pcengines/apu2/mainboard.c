532a9ec98d20953a5c7fc6c627b6fa0d4caf0d7b
Piotr Król <piotr.krol@3mdeb.com>
2016-11-21 23:45:47 +0100

    pcengines/apu2: add support for getting sku and serial number
    This commit implements smbios_mainboard_serial_number and
    smbios_mainboard_sku to provide correct information in SMBIOS tables.
    This was tested by running dmidecode in system.
    
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- src/mainboard/pcengines/apu2/mainboard.c
+++ src/mainboard/pcengines/apu2/mainboard.c
@@ -31,6 +31,9 @@
 #include <string.h>
 #include <cpu/x86/msr.h>
 #include <cpu/amd/mtrr.h>
+#include <cpu/amd/amdfam16.h>
+#include <cpuRegisters.h>
+#include <Fch/Fch.h>
 #include "gpio_ftns.h"
 
 #define SPD_SIZE  128
@@ -239,3 +242,54 @@ struct chip_operations mainboard_ops = {
 };
 
 
+const char *smbios_mainboard_serial_number(void)
+{
+    static char serial[10];
+    msr_t msr;
+    u32 mac_addr = 0;
+    device_t nic_dev;
+
+    // Allows the IO configuration space access method, IOCF8 and IOCFC, to be
+    // used to generate extended configuration cycles
+    msr = rdmsr(NB_CFG_MSR);
+    msr.hi |= (ENABLE_CF8_EXT_CFG);
+    wrmsr(NB_CFG_MSR, msr);
+
+    nic_dev = dev_find_slot(1, PCI_DEVFN(0, 0));
+
+    if ((serial[0] != 0) || !nic_dev)
+        return serial;
+
+    // Read 4 bytes starting from 0x144 offset
+    mac_addr = pci_read_config32(nic_dev, 0x144);
+    // MSB here is always 0xff
+    // Discard it so only bottom 3b of mac address are left
+    mac_addr &= 0x00ffffff;
+
+    // Set bit EnableCf8ExtCfg back to 0
+    msr.hi &= ~(ENABLE_CF8_EXT_CFG);
+    wrmsr(NB_CFG_MSR, msr);
+
+    // Calculate serial value
+    mac_addr /= 4;
+    mac_addr -= 64;
+
+    snprintf(serial, sizeof(serial), "%d", mac_addr);
+
+    return serial;
+}
+
+const char *smbios_mainboard_sku(void)
+{
+	u8 *memptr;
+	static char sku[5];
+	if (sku[0] != 0)
+		return sku;
+
+	memptr = (u8 *)(ACPI_MMIO_BASE + GPIO_OFFSET + GPIO_49);
+	if (!(*memptr & BIT0))
+		snprintf(sku, sizeof(sku), "2 GB");
+	else
+		snprintf(sku, sizeof(sku), "4 GB");
+	return sku;
+}

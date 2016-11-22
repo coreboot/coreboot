515a1c9357361052cd3ba04c779bb853cfb37b39
Kyösti Mälkki <kyosti.malkki@gmail.com>
2016-11-23 00:54:19 +0200

    UPSTREAM: x86 BIST: Declare function with inline in header file
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Kysti Mlkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/17572
    Tested-by: build bot (Jenkins)
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    
    Change-Id: Ieb5f1668a715ceadd5fe5ba0d121c865f1886038
    Reviewed-on: https://chromium-review.googlesource.com/415069
    Commit-Ready: Aaron Durbin <adurbin@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/include/cpu/x86/bist.h
+++ src/include/cpu/x86/bist.h
@@ -1,7 +1,7 @@
 #ifndef CPU_X86_BIST_H
 #define CPU_X86_BIST_H
 
-static void report_bist_failure(u32 bist)
+static inline void report_bist_failure(u32 bist)
 {
 	if (bist != 0) {
 		printk(BIOS_EMERG, "BIST failed: %08x", bist);

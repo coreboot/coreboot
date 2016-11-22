04b3632be83c25b6606cb1e5f54bc8bf6502db2e
Kyösti Mälkki <kyosti.malkki@gmail.com>
2016-11-22 08:37:15 +0200

    UPSTREAM: asus/f2a85-m msi/ms7721: Enable MMCONF early
    PCI MMCONF access only works after amd_initmmio() call.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Kysti Mlkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/17565
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    
    Change-Id: I5765604e178d09abdd6bb6ce7cc220bc5b35ed03
    Reviewed-on: https://chromium-review.googlesource.com/416148
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/msi/ms7721/romstage.c
+++ src/mainboard/msi/ms7721/romstage.c
@@ -128,14 +128,14 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	u8 byte;
 	pci_devfn_t dev;
 
+	amd_initmmio();
+
 #if IS_ENABLED(CONFIG_POST_DEVICE_PCI_PCIE)
 	hudson_pci_port80();
 #endif
 #if IS_ENABLED(CONFIG_POST_DEVICE_LPC)
 	hudson_lpc_port80();
 #endif
-
-	amd_initmmio();
 
 	if (!cpu_init_detectedx && boot_cpu()) {
 

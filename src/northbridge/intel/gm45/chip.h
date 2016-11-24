f56bfa23e2c4d4d58782992f0755805c1e7b4562
Arthur Heymans <arthur@aheymans.xyz>
2016-11-24 13:23:05 +0100

    UPSTREAM: nb/gm45/gma.c: Compute BLC_PWM_CTL value from PWM frequency
    This allows to set the backlight PWM frequency and the
    duty cycle in the devicetree instead of using a plain BLC_PWM_CTL
    value.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/17597
    Tested-by: build bot (Jenkins)
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    
    Change-Id: I4d9a555ac7ea5605712c1fcda994a6fcabf9acf3
    Reviewed-on: https://chromium-review.googlesource.com/415101
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/northbridge/intel/gm45/chip.h
+++ src/northbridge/intel/gm45/chip.h
@@ -26,6 +26,8 @@ struct northbridge_intel_gm45_config {
 	u16 gpu_panel_power_backlight_off_delay; /* Tx time sequence */
 	u8 gpu_panel_power_cycle_delay;          /* T4 time sequence */
 	struct i915_gpu_controller_info gfx;
+	u16 pwm_freq;
+	u8 duty_cycle;
 
 	/*
 	 * Maximum PCI mmio size in MiB.

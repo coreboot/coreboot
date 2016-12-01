3729181da31f182fbc6c0b1ea4860d11ae2743ee
Furquan Shaikh <furquan@chromium.org>
2016-12-01 07:25:31 -0800

    UPSTREAM: spi: Clean up SPI driver interface
    1. Add new structure spi_ctrlr_buses that allows platform to define a
    mapping from SPI controller to buses managed by the controller.
    2. Provide weak implementations of spi_init and spi_setup_slave that
    will be used by platforms using the new interface.
    
    BUG=chrome-os-partner:59832
    BRANCH=None
    TEST=Compiles successfully
    
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://review.coreboot.org/17561
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    
    Change-Id: Ia6f47941b786299f4d823895898ffb1b36e02f73
    Reviewed-on: https://chromium-review.googlesource.com/418363
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/drivers/spi/spi-generic.c
+++ src/drivers/spi/spi-generic.c
@@ -41,3 +41,38 @@ int spi_xfer(const struct spi_slave *slave, const void *dout, size_t bytesout,
 
 	return -1;
 }
+
+void __attribute__((weak)) spi_init(void)
+{
+	/* Default weak implementation - do nothing. */
+}
+
+const struct spi_ctrlr_buses spi_ctrlr_bus_map[0] __attribute__((weak));
+const size_t spi_ctrlr_bus_map_count __attribute__((weak));
+
+int __attribute__((weak)) spi_setup_slave(unsigned int bus, unsigned int cs,
+					  struct spi_slave *slave)
+{
+	size_t i;
+
+	memset(slave, 0, sizeof(*slave));
+
+	for (i = 0; i < spi_ctrlr_bus_map_count; i++) {
+		if ((spi_ctrlr_bus_map[i].bus_start <= bus) &&
+		    (spi_ctrlr_bus_map[i].bus_end >= bus)) {
+			slave->ctrlr = spi_ctrlr_bus_map[i].ctrlr;
+			break;
+		}
+	}
+
+	if (slave->ctrlr == NULL)
+		return -1;
+
+	slave->bus = bus;
+	slave->cs = cs;
+
+	if (slave->ctrlr->setup)
+		return slave->ctrlr->setup(slave);
+
+	return 0;
+}

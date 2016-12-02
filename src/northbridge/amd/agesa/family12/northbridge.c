b1c81b71d306371fcd5a03a0a56e8272f16cf9ac
Kyösti Mälkki <kyosti.malkki@gmail.com>
2016-12-02 16:02:30 +0200

    UPSTREAM: AMD fam10 binaryPI: Remove invalid PCI ops on CPU domain
    Device is of type CPU_CLUSTER, while pci_dev_set_resources()
    expects PCI_DOMAIN.
    
    BUG=None
    BRANCH=None
    TEST=None
    
    Signed-off-by: Kysti Mlkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/17697
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    
    Change-Id: Ib1add47d71071abb6e9c28e3a85dd0b671741b71
    Reviewed-on: https://chromium-review.googlesource.com/417957
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/northbridge/amd/agesa/family12/northbridge.c
+++ src/northbridge/amd/agesa/family12/northbridge.c
@@ -652,22 +652,6 @@ static void domain_enable_resources(device_t dev)
 
 /* Bus related code */
 
-
-static void cpu_bus_read_resources(device_t dev)
-{
-	printk(BIOS_DEBUG, "\nFam12h - northbridge.c - %s - Start.\n",__func__);
-
-	printk(BIOS_DEBUG, "Fam12h - northbridge.c - %s - End.\n",__func__);
-}
-
-static void cpu_bus_set_resources(device_t dev)
-{
-	printk(BIOS_DEBUG, "\nFam12h - northbridge.c - %s - Start.\n",__func__);
-	pci_dev_set_resources(dev);
-
-	printk(BIOS_DEBUG, "Fam12h - northbridge.c - %s - End.\n",__func__);
-}
-
 static void cpu_bus_init(device_t dev)
 {
 	printk(BIOS_DEBUG, "\nFam12h - northbridge.c - %s - Start.\n",__func__);
@@ -813,8 +797,8 @@ static struct device_operations pci_domain_ops = {
 
 
 static struct device_operations cpu_bus_ops = {
-	.read_resources   = cpu_bus_read_resources,
-	.set_resources    = cpu_bus_set_resources,
+	.read_resources   = DEVICE_NOOP,
+	.set_resources    = DEVICE_NOOP,
 	.enable_resources = DEVICE_NOOP,
 	.init             = cpu_bus_init,
 	.scan_bus         = 0,
--- src/northbridge/amd/amdfam10/northbridge.c
+++ src/northbridge/amd/amdfam10/northbridge.c
@@ -1955,14 +1955,9 @@ static void cpu_bus_init(device_t dev)
 #endif
 }
 
-static void cpu_bus_set_resources(struct device *dev)
-{
-	pci_dev_set_resources(dev);
-}
-
 static struct device_operations cpu_bus_ops = {
 	.read_resources	  = DEVICE_NOOP,
-	.set_resources	  = cpu_bus_set_resources,
+	.set_resources	  = DEVICE_NOOP,
 	.enable_resources = DEVICE_NOOP,
 	.init		  = cpu_bus_init,
 	.scan_bus	  = cpu_bus_scan,

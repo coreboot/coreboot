c895b7def185ddb623bbcc531048d4d936b814b7
Kyösti Mälkki <kyosti.malkki@gmail.com>
2016-12-02 08:06:55 +0200

    PCI ops: Remove pci_mmio_xx() in ramstage
    MMCONF operations are already the default so these
    would never be used.
    
    Change-Id: I671f3d2847742e400bc4ecfccc088e3b79d43070
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/17691
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/include/device/pci_ops.h
+++ src/include/device/pci_ops.h
@@ -13,14 +13,6 @@ void pci_write_config8(struct device *dev, unsigned int where, u8 val);
 void pci_write_config16(struct device *dev, unsigned int where, u16 val);
 void pci_write_config32(struct device *dev, unsigned int where, u32 val);
 
-#if CONFIG_MMCONF_SUPPORT
-u8 pci_mmio_read_config8(struct device *dev, unsigned int where);
-u16 pci_mmio_read_config16(struct device *dev, unsigned int where);
-u32 pci_mmio_read_config32(struct device *dev, unsigned int where);
-void pci_mmio_write_config8(struct device *dev, unsigned int where, u8 val);
-void pci_mmio_write_config16(struct device *dev, unsigned int where, u16 val);
-void pci_mmio_write_config32(struct device *dev, unsigned int where, u32 val);
-#endif
 #endif
 
 #endif /* PCI_OPS_H */

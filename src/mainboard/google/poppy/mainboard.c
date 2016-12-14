d81faf493c09a6f7898cd6882d4838f3f8ab2904
Furquan Shaikh <furquan@chromium.org>
2016-12-14 12:10:21 -0800

    UPSTREAM: google/poppy: Add new board
    Add poppy board files using kabylake and FSP 2.0.
    
    BUG=chrome-os-partner:60713
    BRANCH=None
    TEST=Compiles successfully
    
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://review.coreboot.org/17866
    Tested-by: build bot (Jenkins)
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    
    Change-Id: Ic9aa5093b319690ae893a21cab98d9b843000e6c
    Reviewed-on: https://chromium-review.googlesource.com/422948
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/mainboard/google/poppy/mainboard.c
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2016 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/acpi.h>
+#include <device/device.h>
+#include <ec/ec.h>
+#include <vendorcode/google/chromeos/chromeos.h>
+
+static void mainboard_init(device_t dev)
+{
+	mainboard_ec_init();
+}
+
+static void mainboard_enable(device_t dev)
+{
+	dev->ops->init = mainboard_init;
+	dev->ops->acpi_inject_dsdt_generator = chromeos_dsdt_generator;
+}
+
+struct chip_operations mainboard_ops = {
+	.enable_dev = mainboard_enable,
+};

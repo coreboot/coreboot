1e7911e8aa830fcdf86cbb72671d79ea4c2c0e2a
Paul Menzel <paulepanter@users.sourceforge.net>
2016-12-27 15:24:02 +0100

    util/intelmetool: Fix warning building with 32-bit
    On a 32-bit system, pointers are 32-bit wide, and not 64-bit, resulting
    in the warning below.
    
    ```
    mmap.c: In function ‘map_physical_exact’:
    mmap.c:26:20: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
       virt_addr = mmap((void*)mapto, len, PROT_WRITE | PROT_READ,
                        ^
    ```
    
    Fix this by using compatible types.
    
    Change-Id: I4ede26127efcbd5668b978e6880a0535607e373d
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-on: https://review.coreboot.org/17970
    Tested-by: build bot (Jenkins)
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- util/intelmetool/mmap.c
+++ util/intelmetool/mmap.c
@@ -19,33 +19,33 @@
 #ifndef __DARWIN__
 int fd_mem;
 
-void *map_physical_exact(uint64_t phys_addr, uint64_t mapto, size_t len) {
+void *map_physical_exact(off_t phys_addr, void *mapto, size_t len) {
   void *virt_addr;
   int err;
 
-  virt_addr = mmap((void*)mapto, len, PROT_WRITE | PROT_READ,
-                    MAP_SHARED | MAP_FIXED, fd_mem, (off_t) phys_addr);
+  virt_addr = mmap(mapto, len, PROT_WRITE | PROT_READ,
+                    MAP_SHARED | MAP_FIXED, fd_mem, phys_addr);
 
   if (virt_addr == MAP_FAILED) {
     err = errno;
-    printf("Error mapping physical memory 0x%016" PRIx64 "[0x%zx] ERRNO=%d\n",
-            phys_addr, len, err);
+    printf("Error mapping physical memory 0x%016jd [0x%zx] ERRNO=%d\n",
+            (intmax_t)phys_addr, len, err);
     return NULL;
   }
 
   return virt_addr;
 }
 
-void *map_physical(uint64_t phys_addr, size_t len) {
+void *map_physical(off_t phys_addr, size_t len) {
   void *virt_addr;
   int err;
 
-  virt_addr = mmap(NULL, len, PROT_WRITE | PROT_READ, MAP_SHARED, fd_mem, (off_t) phys_addr);
+  virt_addr = mmap(NULL, len, PROT_WRITE | PROT_READ, MAP_SHARED, fd_mem, phys_addr);
 
   if (virt_addr == MAP_FAILED) {
     err = errno;
-    printf("Error mapping physical memory 0x%016" PRIx64 "[0x%zx] ERRNO=%d\n",
-            phys_addr, len, err);
+    printf("Error mapping physical memory 0x%016jd [0x%zx] ERRNO=%d\n",
+            (intmax_t)phys_addr, len, err);
     return NULL;
   }
 

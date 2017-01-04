6cfd90563a08373c2a758c4bfdeac79609882b9a
Brenton Dong <brenton.m.dong@intel.com>
2017-01-04 15:12:27 -0700

    UPSTREAM: mainboard/intel: add leafhill board directory
    This commit adds the initial scaffolding for the Intel Leafhill CRB
    with Apollo Lake silicon.
    
    The google/reef directory is used as a template. This commit only
    makes the minimum changes to Kconfig and Kconfig.name needed for
    the build bot to not have issues.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I28d51ae70b98abafbbfd68b38a59b00074bc89ef
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 5f1f0538cf46cea122c49cc103771fd839d24b37
    Original-Change-Id: I088edee0e94ecfb4666fa31e08dbcfd24a81891b
    Original-Signed-off-by: Brenton Dong <brenton.m.dong@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/18038
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Werner Zeh <werner.zeh@siemens.com>
    Reviewed-on: https://chromium-review.googlesource.com/431978

---
+++ src/mainboard/intel/leafhill/romstage.c
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2016 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <baseboard/variants.h>
+#include <soc/meminit.h>
+#include <soc/romstage.h>
+
+void mainboard_memory_init_params(FSPM_UPD *memupd)
+{
+	meminit_lpddr4_by_sku(&memupd->FspmConfig,
+		variant_lpddr4_config(), variant_memory_sku());
+}
+
+void mainboard_save_dimm_info(void)
+{
+	save_lpddr4_dimm_info(variant_lpddr4_config(), variant_memory_sku());
+}

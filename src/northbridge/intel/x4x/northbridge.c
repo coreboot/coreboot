6d0c65ebc6c0f1e8b8d012dcfd42512b7d281515
Arthur Heymans <arthur@aheymans.xyz>
2017-01-05 17:07:33 +0100

    nb/intel/*/northbridge.c: Remove #include <device/hypertransport.h>
    Nothing from that header is used or even declared since
    CONFIG_HYPERTRANSPORT_PLUGIN_SUPPORT is not selected on Intel
    hardware.
    
    Change-Id: I9101eb6ffa6664a2ab45bc0b247279c916266537
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/18044
    Tested-by: build bot (Jenkins)
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-by: Idwer Vollering <vidwer@gmail.com>

--- src/northbridge/intel/x4x/northbridge.c
+++ src/northbridge/intel/x4x/northbridge.c
@@ -20,7 +20,6 @@
 #include <device/device.h>
 #include <device/pci.h>
 #include <device/pci_ids.h>
-#include <device/hypertransport.h>
 #include <stdlib.h>
 #include <string.h>
 #include <cpu/cpu.h>

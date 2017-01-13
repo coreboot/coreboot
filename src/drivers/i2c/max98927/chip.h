d9aa8dd73500157c2fd41baee92d7888f2288749
Rizwan Qureshi <rizwan.qureshi@intel.com>
2017-01-13 22:17:01 +0530

    UPSTREAM: driver/i2c/max98927: add i2c driver for Maxim 98927 codec
    Maxim 98927 kernel driver requires entries in the ACPI SSDT table,
    add a SSDT generator as part of this driver.
    
    BUG=chrome-os-partner:62051
    BRANCH=None
    TEST=After boot, dump and verify that the generated SSDT ACPI table has the
    required entries.
    
    Change-Id: I9043f9f0b66b45d04e8b8cbe8c99b77686fd5666
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 4979d7610e74b83f7b41b10ac30b4fb619b4e26d
    Original-Change-Id: Ic2d4d8449288bc00d085852220b2e1e7a208e9ef
    Original-Signed-off-by: Naresh G Solanki <naresh.solanki@intel.com>
    Original-Signed-off-by: Rizwan Qureshi <rizwan.qureshi@intel.com>
    Original-Signed-off-by: M Naveen <naveen.m@intel.com>
    Original-Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Original-Reviewed-on: https://review.coreboot.org/18211
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/445127

---
+++ src/drivers/i2c/max98927/chip.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2017 Intel Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
+ * Maxim MAX98927 audio codec devicetree bindings
+ */
+struct drivers_i2c_max98927_config {
+	/* I2C Bus Frequency in Hertz (default 400kHz) */
+	unsigned int bus_speed;
+	/* Set ‘1’ if I2S channel size is not 32bit. */
+	bool interleave_mode;
+	/* Identifier for chips */
+	unsigned int uid;
+	/* Device Description */
+	const char *desc;
+	/* ACPI Device Name */
+	const char *name;
+};
---
+++ src/drivers/i2c/max98927/max98927.c
@@ -0,0 +1,100 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2017 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/acpi.h>
+#include <arch/acpi_device.h>
+#include <arch/acpigen.h>
+#include <console/console.h>
+#include <device/i2c.h>
+#include <device/device.h>
+#include <device/path.h>
+#include <stdint.h>
+#include <string.h>
+#include "chip.h"
+
+#define MAX98927_ACPI_NAME	"MAXI"
+#define MAX98927_ACPI_HID	"MX98927"
+
+static void max98927_fill_ssdt(struct device *dev)
+{
+	struct drivers_i2c_max98927_config *config = dev->chip_info;
+	const char *scope = acpi_device_scope(dev);
+	struct acpi_i2c i2c = {
+		.address = dev->path.i2c.device,
+		.mode_10bit = dev->path.i2c.mode_10bit,
+		.speed = config->bus_speed ? : I2C_SPEED_FAST,
+		.resource = scope,
+	};
+	struct acpi_dp *dp;
+
+	if (!dev->enabled || !scope)
+		return;
+
+	/* Device */
+	acpigen_write_scope(scope);
+	acpigen_write_device(acpi_device_name(dev));
+	acpigen_write_name_string("_HID", MAX98927_ACPI_HID);
+	acpigen_write_name_integer("_UID", config->uid);
+	if (config->desc)
+		acpigen_write_name_string("_DDN", config->desc);
+	acpigen_write_STA(ACPI_STATUS_DEVICE_ALL_ON);
+
+	/* Resources */
+	acpigen_write_name("_CRS");
+	acpigen_write_resourcetemplate_header();
+	acpi_device_write_i2c(&i2c);
+	acpigen_write_resourcetemplate_footer();
+
+	/* Device Properties */
+	dp = acpi_dp_new_table("_DSD");
+
+	acpi_dp_add_integer(dp, "interleave_mode", config->interleave_mode);
+
+	acpi_dp_write(dp);
+
+	acpigen_pop_len(); /* Device */
+	acpigen_pop_len(); /* Scope */
+
+	printk(BIOS_INFO, "%s: %s address 0%xh\n", acpi_device_path(dev),
+			dev->chip_ops->name, dev->path.i2c.device);
+}
+
+static const char *max98927_acpi_name(struct device *dev)
+{
+	struct drivers_i2c_max98927_config *config = dev->chip_info;
+
+	if (config->name)
+		return config->name;
+
+	return MAX98927_ACPI_NAME;
+}
+
+static struct device_operations max98927_ops = {
+	.read_resources		  = DEVICE_NOOP,
+	.set_resources		  = DEVICE_NOOP,
+	.enable_resources	  = DEVICE_NOOP,
+	.acpi_name		  = &max98927_acpi_name,
+	.acpi_fill_ssdt_generator = &max98927_fill_ssdt,
+};
+
+static void max98927_enable(struct device *dev)
+{
+	dev->ops = &max98927_ops;
+}
+
+struct chip_operations drivers_i2c_max98927_ops = {
+	CHIP_NAME("Maxim MAX98927 Codec")
+	.enable_dev = &max98927_enable
+};

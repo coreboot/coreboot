da273286d92cce5ae15d7e3b8b3725c608685182
Nico Huber <nico.huber@secunet.com>
2017-01-24 11:18:07 +0100

    UPSTREAM: ec/roda/it8518: Do EC write manually with long timeout
    The EC may take very long for the first command on a cold boot (~180ms
    witnessed). Since this needs an incredibly long timeout, we do this
    single command manually.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I9671c6a3ad5127e41808247be13c99dae315ab5a
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: e223c3aee9ac7f3b91fbbb810e9f2b2acd3fc4f6
    Original-Change-Id: I3302622a845ac6651bc7f563370d8f0511836f94
    Original-Signed-off-by: Nico Huber <nico.huber@secunet.com>
    Original-Reviewed-on: https://review.coreboot.org/18707
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/472717

--- src/ec/roda/it8518/ec.c
+++ src/ec/roda/it8518/ec.c
@@ -19,6 +19,7 @@
 #include <pc80/keyboard.h>
 #include <ec/acpi/ec.h>
 #include <stdlib.h>
+#include <delay.h>
 
 #include "chip.h"
 
@@ -29,11 +30,20 @@ static void it8518_init(struct device *dev)
 	if (!dev->enabled)
 		return;
 
-	if (conf && conf->cpuhot_limit)
-		ec_write(0xb2, conf->cpuhot_limit);
-
 	printk(BIOS_DEBUG, "Roda IT8518: Initializing keyboard.\n");
 	pc_keyboard_init(NO_AUX_DEVICE);
+
+	if (conf && conf->cpuhot_limit) {
+		/* The EC may take very long for the first command on a
+		   cold boot (~180ms witnessed). Since we need an incre-
+		   dibly long timeout, we do this EC RAM write manually. */
+		int timeout = 50000;	/* 50,000 * 10us = 500ms */
+		send_ec_command(0x81);
+		while (ec_status() & EC_IBF && --timeout)
+			udelay(10);
+		send_ec_data(0xb2);
+		send_ec_data(conf->cpuhot_limit);
+	}
 }
 
 static struct device_operations ops = {

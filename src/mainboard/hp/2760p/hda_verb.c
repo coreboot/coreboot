03e96a8a9e2ba77c43567c61216e93a64b3e4120
Iru Cai <mytbk920423@gmail.com>
2017-01-26 14:51:47 +0800

    mainboard/hp: Add HP Elitebook 2760p
    The code is generated by autoport.
    
    The flash chip is socketed beside the WLAN slot. The EHCI debug port
    is on right side of the laptop beside the RJ11 connector.
    
    Things that work:
    - memory: 0+8G, 4G+8G
    - Linux (Linux Mint 18.1 with Linux 4.4)
    - native graphics init + SeaBIOS payload with SeaVGABIOS
    - all 3 USB ports
    - WLAN
    - WWAN
    - SD card reader
    - expresscard
    - S3 suspend and resume
    - internal flashing after IFD is unlocked and coreboot is flashed
    - keyboard, trackpoint and touchpad
    - fan control
    - AC and battery status
    
    Issues:
    - Wacom digitizer does not work (even after I add it in DSDT)
    - GRUB payload will freeze (in all Elitebooks, including chainloading GRUB
      from SeaBIOS)
    
    Things that are not tested:
    - smart card reader
    - cable modem
    
    (EC) blobs:
    
    This laptop uses SMSC KBC1126-NU as EC. It needs two blobs in the
    flash chip. You can use the tools in util/kbc1126 to extract them
    from OEM firmware, and use the following configuration to insert
    them to coreboot image:
    
      -> Chipset
        -> Add firmware images for KBC1126 firmware
    
    Change-Id: I3ffdb9f9c71f6c9a84e896abc3c424c8dd4bed0e
    Signed-off-by: Iru Cai <mytbk920423@gmail.com>
    Reviewed-on: https://review.coreboot.org/18241
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

---
+++ src/mainboard/hp/2760p/hda_verb.c
@@ -0,0 +1,82 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 The ChromiumOS Authors.  All rights reserved.
+ * Copyright (C) 2014 Vladimir Serbinenko
+ * Copyright (C) 2017 Iru Cai <mytbk920423@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <device/azalia_device.h>
+
+const u32 cim_verb_data[] = {
+	0x111d7605, /* Codec Vendor / Device ID: IDT */
+	0x103c162a, /* Subsystem ID */
+
+	0x0000000b, /* Number of 4 dword sets */
+	/* NID 0x01: Subsystem ID.  */
+	AZALIA_SUBVENDOR(0x0, 0x103c162a),
+
+	/* NID 0x0a.  */
+	AZALIA_PIN_CFG(0x0, 0x0a, 0x40f000f0),
+
+	/* NID 0x0b.  */
+	AZALIA_PIN_CFG(0x0, 0x0b, 0x0421401f),
+
+	/* NID 0x0c.  */
+	AZALIA_PIN_CFG(0x0, 0x0c, 0x04a11020),
+
+	/* NID 0x0d.  */
+	AZALIA_PIN_CFG(0x0, 0x0d, 0x90170110),
+
+	/* NID 0x0e.  */
+	AZALIA_PIN_CFG(0x0, 0x0e, 0x90a70130),
+
+	/* NID 0x0f.  */
+	AZALIA_PIN_CFG(0x0, 0x0f, 0x40f000f0),
+
+	/* NID 0x10.  */
+	AZALIA_PIN_CFG(0x0, 0x10, 0x40f000f0),
+
+	/* NID 0x11.  */
+	AZALIA_PIN_CFG(0x0, 0x11, 0x40f000f0),
+
+	/* NID 0x1f.  */
+	AZALIA_PIN_CFG(0x0, 0x1f, 0x40f000f0),
+
+	/* NID 0x20.  */
+	AZALIA_PIN_CFG(0x0, 0x20, 0x40f000f0),
+	0x11c11040, /* Codec Vendor / Device ID: LSI */
+	0x103c3066, /* Subsystem ID */
+
+	0x00000001, /* Number of 4 dword sets */
+	/* NID 0x01: Subsystem ID.  */
+	AZALIA_SUBVENDOR(0x1, 0x103c3066),
+	0x80862805, /* Codec Vendor / Device ID: Intel */
+	0x80860101, /* Subsystem ID */
+
+	0x00000004, /* Number of 4 dword sets */
+	/* NID 0x01: Subsystem ID.  */
+	AZALIA_SUBVENDOR(0x3, 0x80860101),
+
+	/* NID 0x05.  */
+	AZALIA_PIN_CFG(0x3, 0x05, 0x18560010),
+
+	/* NID 0x06.  */
+	AZALIA_PIN_CFG(0x3, 0x06, 0x58560020),
+
+	/* NID 0x07.  */
+	AZALIA_PIN_CFG(0x3, 0x07, 0x58560030),
+};
+
+const u32 pc_beep_verbs[0] = {};
+
+AZALIA_ARRAY_SIZES;

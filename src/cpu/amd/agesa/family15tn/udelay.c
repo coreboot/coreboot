b8676a582b5ab8a0556df4f26b80f7f6b9b53ab9
Martin Roth <martinroth@google.com>
2017-02-09 17:23:55 -0800

    UPSTREAM: src/cpu/amd: Update/Add license headers to all files
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: Icf521d160cb304ac03cb9dbd9a836f9f376d7fd0
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 869532264a20801a58ccfb4bbca1b52d015a959f
    Original-Change-Id: I1e0b2b9086db6b3c2f716d9400a83eb60b2ce222
    Original-Signed-off-by: Martin Roth <martinroth@google.com>
    Original-Reviewed-on: https://review.coreboot.org/18403
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://chromium-review.googlesource.com/445822

--- src/cpu/amd/agesa/family15tn/udelay.c
+++ src/cpu/amd/agesa/family15tn/udelay.c
@@ -1,10 +1,23 @@
 /*
- * udelay() impementation for SMI handlers
- * This is neat in that it never writes to hardware registers, and thus does not
- * modify the state of the hardware while servicing SMIs.
+ * This file is part of the coreboot project.
  *
  * Copyright (C) 2014 Alexandru Gagniuc <mr.nuke.me@gmail.com>
- * Subject to the GNU GPL v2, or (at your option) any later version.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
+ * udelay() impementation for SMI handlers
+ * This is neat in that it never writes to hardware registers, and thus does
+ *  not modify the state of the hardware while servicing SMIs.
  */
 
 #include <cpu/x86/msr.h>

6add44bd3c462ed25828657cbb329b996aa91c51
Martin Roth <martinroth@google.com>
2017-02-09 17:51:20 -0800

    src/cpu/x86: Update/Add license headers to all files
    Change-Id: I436bf0e7db008ea78e29eaeef10bea101e6c8922
    Signed-off-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://review.coreboot.org/18405
    Tested-by: build bot (Jenkins)
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/cpu/x86/pae/pgtbl.c
+++ src/cpu/x86/pae/pgtbl.c
@@ -1,6 +1,17 @@
 /*
-	2005.12 yhlu add ramstage cross the vga font buffer handling
-*/
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2005 Yinghai Lu
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 #include <console/console.h>
 #include <cpu/cpu.h>

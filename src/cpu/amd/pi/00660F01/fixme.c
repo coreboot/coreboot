f3f98c0ccebdb986a0822cd2b164c73810977b7a
Marshall Dawson <marshalldawson3rd@gmail.com>
2017-02-12 10:31:15 -0700

    UPSTREAM: amd/pi/hudson: Move APIC enable to CPU file
    Relocate the enabling of the LAPIC out of the southbridge source and
    surround it with a check for CONFIG_UDELAY_LAPIC (typical for AMD
    systems).  The LAPIC is now enabled for all cores; not only the BSP,
    and not only when the UART is used.
    
    This solves the problem of APs not having their APICs enabled when
    the timer is expected to be functional, e.g. verstage often uses
    do_printk_va_list() instead of do_printk() which exits early for
    APs when CONFIG_SQUELCH_EARLY_SMP=y.
    
    The changes were tested with two Gardenia builds, one using verstage
    and another with CONFIG_SQUELCH_EARLY_SMP=n.
    
    Original-Signed-off-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Original-Reviewed-by: Marc Jones <marcj303@gmail.com>
    (cherry picked from commit 93ffc311165f19d4192a5489051fa4264cd8e0ad)
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I3b2cf63f46cb63e46ddc916f399de7f9e76759e1
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 03e6a455a38a5ba0b8146085c215fb324d161f36
    Original-Change-Id: Ieaecc0bf921ee0d2691a8082f2431ea4d0c33749
    Original-Signed-off-by: Marc Jones <marcj303@gmail.com>
    Original-Signed-off-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Original-Reviewed-on: https://review.coreboot.org/18436
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Kysti Mlkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/451427

--- src/cpu/amd/pi/00660F01/fixme.c
+++ src/cpu/amd/pi/00660F01/fixme.c
@@ -84,4 +84,10 @@ void amd_initmmio(void)
 	LibAmdMsrWrite (0x20C, &MsrReg, &StdHeader);
 	MsrReg = ((1ULL << CONFIG_CPU_ADDR_BITS) - CACHE_ROM_SIZE) | 0x800ull;
 	LibAmdMsrWrite (0x20D, &MsrReg, &StdHeader);
+
+	if (IS_ENABLED(CONFIG_UDELAY_LAPIC)){
+		LibAmdMsrRead(0x1B, &MsrReg, &StdHeader);
+		MsrReg |= 1 << 11;
+		LibAmdMsrWrite(0x1B, &MsrReg, &StdHeader);
+	}
 }

18792314d7b8dde596a9c378b2d47516cbd871f4
Robbie Zhang <robbie.zhang@intel.com>
2017-02-13 13:44:14 -0800

    arch/x86: add functions to generate random numbers
    Using x86 RDRAND instruction, two functions are supplied to
    generate a 32bit or 64bit number.
    
    One potential usage is the sealing key generation for SGX.
    
    BUG=chrome-os-partner:62438
    BRANCH=NONE
    TEST=Tested on Eve to generate a 64bit random number.
    
    Change-Id: I50cbeda4de17ccf2fc5efc1fe04f6b1a31ec268c
    Signed-off-by: Robbie Zhang <robbie.zhang@intel.com>
    Reviewed-on: https://review.coreboot.org/18362
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/include/random.h
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2017 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _RANDOM_H_
+#define _RANDOM_H_
+
+#include <stdint.h>
+
+/*
+ * Generates a 32/64 bit random number respectively.
+ * return 0 on success and -1 on error.
+ */
+int get_random_number_32(uint32_t *rand);
+int get_random_number_64(uint64_t *rand);
+
+#endif /* _RANDOM_H_ */

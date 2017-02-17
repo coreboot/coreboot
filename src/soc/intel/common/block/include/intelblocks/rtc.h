e3f9bdb2d3014e93f980ad0bde4fdbe6809fcc7b
Barnali Sarkar <barnali.sarkar@intel.com>
2017-02-17 18:15:22 +0530

    UPSTREAM: soc/intel/common/block: Add Intel common RTC code support
    Create Intel Common RTC code. This code currently only
    contains the code for configuring RTC required in Bootblock phase
    which has the following programming -
    * Enable upper 128 bytes of CMOS.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I25d743418a00626e5fb199ce26c095acbf01902d
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 8e84723e02537229e079dcdb0795d2903eb1603d
    Original-Change-Id: Id9dfcdbc300c25f43936d1efb5d6f9d81d3c8453
    Original-Signed-off-by: Barnali Sarkar <barnali.sarkar@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/18558
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Original-Reviewed-by: Philippe Mathieu-Daud <philippe.mathieu.daude@gmail.com>
    Original-Tested-by: build bot (Jenkins)
    Reviewed-on: https://chromium-review.googlesource.com/474131

---
+++ src/soc/intel/common/block/include/intelblocks/rtc.h
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2017 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SOC_INTEL_COMMON_BLOCK_RTC_H
+#define SOC_INTEL_COMMON_BLOCK_RTC_H
+
+void enable_rtc_upper_bank(void);
+
+#endif	/* SOC_INTEL_COMMON_BLOCK_RTC_H */

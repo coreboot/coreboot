5492bfb55c5cec410a78a6050e8b27a2419ff914
Duncan Laurie <dlaurie@chromium.org>
2017-02-17 17:40:10 -0800

    google/eve: Add audio devices
    Add the audio devices to Eve mainboard:
    
    - Describe Maxim 98927 speaker amps and RT5663 headphone codec
    in ACPI so they can be enumerated by the OS.
    
    - Supply NHLT binaries for MAX98927, RT5663, and DMIC_4CH.
    
    BUG=chrome-os-partner:61009
    TEST=manual testing on Eve P1 with updated kernel to ensure that
    both speakers and headset are functional.  DMIC support is
    is still being worked on and is not yet functional.
    
    Change-Id: I5243e35d159a0ed15c6004e94ba5a50b28cff0a9
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://review.coreboot.org/18398
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins)

--- src/mainboard/google/eve/mainboard.c
+++ src/mainboard/google/eve/mainboard.c
@@ -15,12 +15,17 @@
  */
 
 #include <arch/acpi.h>
+#include <console/console.h>
 #include <device/device.h>
 #include <ec/ec.h>
 #include <vendorcode/google/chromeos/chromeos.h>
 #include <gpio.h>
 #include <soc/gpio.h>
+#include <soc/nhlt.h>
 #include "gpio.h"
+
+static const char *oem_id_maxim = "GOOGLE";
+static const char *oem_table_id_maxim = "EVEMAX";
 
 static void mainboard_init(device_t dev)
 {
@@ -28,10 +33,45 @@ static void mainboard_init(device_t dev)
 	gpio_configure_pads(late_gpio_table, ARRAY_SIZE(late_gpio_table));
 }
 
+static unsigned long mainboard_write_acpi_tables(
+	device_t device, unsigned long current, acpi_rsdp_t *rsdp)
+{
+	uintptr_t start_addr;
+	uintptr_t end_addr;
+	struct nhlt *nhlt;
+
+	start_addr = current;
+
+	nhlt = nhlt_init();
+	if (!nhlt)
+		return start_addr;
+
+	/* 4 Channel DMIC array. */
+	if (nhlt_soc_add_dmic_array(nhlt, 4))
+		printk(BIOS_ERR, "Couldn't add 4CH DMIC arrays.\n");
+
+	/* RT5663 Headset codec */
+	if (nhlt_soc_add_rt5663(nhlt, AUDIO_LINK_SSP1))
+		printk(BIOS_ERR, "Couldn't add headset codec.\n");
+
+	/* MAXIM98927 Smart Amps for left and right channel */
+	if (nhlt_soc_add_max98927(nhlt, AUDIO_LINK_SSP0))
+		printk(BIOS_ERR, "Couldn't add max98927\n");
+
+	end_addr = nhlt_soc_serialize_oem_overrides(nhlt, start_addr,
+				    oem_id_maxim, oem_table_id_maxim, 0);
+
+	if (end_addr != start_addr)
+		acpi_add_table(rsdp, (void *)start_addr);
+
+	return end_addr;
+}
+
 static void mainboard_enable(device_t dev)
 {
 	dev->ops->init = mainboard_init;
 	dev->ops->acpi_inject_dsdt_generator = chromeos_dsdt_generator;
+	dev->ops->write_acpi_tables = mainboard_write_acpi_tables;
 }
 
 struct chip_operations mainboard_ops = {

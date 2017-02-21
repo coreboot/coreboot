1f23e55aff697c249077db991b6215b51f55d042
Barnali Sarkar <barnali.sarkar@intel.com>
2017-02-21 16:24:49 +0530

    UPSTREAM: soc/intel/common: Save Memory DIMM Information in SMBIOS table
    Save SMBIOS memory information from FSP MEM_INFO_DATA_HOB in CBMEM.
    Add function dimm_info_fill() which populates SMBIOS memory
    information from FSP MEM_INFO_DATA_HOB data.
    
    BUG=chrome-os-partner:61729
    BRANCH=none
    TEST=Build and boot KBLRVP to verify the type 17 DIMM info coming in
    SMBIOS table from Kernel command "dmidecode".
    
    Change-Id: I489ff93622c18183115b9d7a0cb62a22a96bdc3e
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: e13b77564f71dd125f2e4691407ac39efe552b6e
    Original-Change-Id: I0fd7c9887076d3fdd320fcbdcc873cb1965b950c
    Original-Signed-off-by: Barnali Sarkar <barnali.sarkar@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/18418
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/449821

---
+++ src/soc/intel/common/smbios.h
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2017 Intel Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _COMMON_SMBIOS_H_
+#define _COMMON_SMBIOS_H_
+
+#include <stdint.h>
+#include <memory_info.h>
+
+/* Fill the SMBIOS memory information from FSP MEM_INFO_DATA_HOB in CBMEM.*/
+void dimm_info_fill(struct dimm_info *dimm, u32 dimm_capacity, u8 ddr_type,
+		u32 frequency, u8 channel_id, u8 dimm_id, const char *module_part_num,
+		u16 data_width);
+
+#endif /* _COMMON_SMBIOS_H_ */

bc5112029b633fffef2a0abca4a438bd6264f467
Iru Cai <mytbk920423@gmail.com>
2017-02-24 15:19:25 +0800

    autoport: add "-d" option to ectool to dump registers
    Change-Id: I7de37a026a0899c2d07ea17c9377c8d2283450ab
    Signed-off-by: Iru Cai <mytbk920423@gmail.com>
    Reviewed-on: https://review.coreboot.org/18481
    Tested-by: build bot (Jenkins)
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- util/autoport/log_maker.go
+++ util/autoport/log_maker.go
@@ -93,7 +93,7 @@ func MakeLogs(outDir string) {
 	}
 
 	RunAndSave(outDir+"/inteltool.log", "../inteltool/inteltool", inteltoolArgs)
-	RunAndSave(outDir+"/ectool.log", "../ectool/ectool")
+	RunAndSave(outDir+"/ectool.log", "../ectool/ectool", "-d")
 
 	SysDir := "/sys/class/sound/card0/"
 	files, _ := ioutil.ReadDir(SysDir)

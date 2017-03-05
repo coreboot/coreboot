7c414e78cc63a21a584ab844e2d6ba1dfa570a16
Paul Menzel <paulepanter@users.sourceforge.net>
2017-03-05 19:17:50 +0100

    northbridge/intel/i440bx: Align code
    Change-Id: Idd4127f7491524121b4b65c6fb9511e2c8159912
    Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-on: https://review.coreboot.org/18609
    Tested-by: build bot (Jenkins)
    Reviewed-by: Philippe Mathieu-Daudé <philippe.mathieu.daude@gmail.com>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/northbridge/intel/i440bx/northbridge.c
+++ src/northbridge/intel/i440bx/northbridge.c
@@ -78,7 +78,7 @@ static struct device_operations pci_domain_ops = {
 	.enable_resources	= NULL,
 	.init			= NULL,
 	.scan_bus		= pci_domain_scan_bus,
-	.ops_pci_bus	= pci_bus_default_ops,
+	.ops_pci_bus		= pci_bus_default_ops,
 };
 
 static void cpu_bus_init(device_t dev)

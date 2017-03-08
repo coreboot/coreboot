db469a689b7090e3cdec654367eb26636d38dad7
Lee Leahy <leroy.p.leahy@intel.com>
2017-03-08 10:05:16 -0800

    src/include: Fix indent for case labels
    Fix the following error detected by checkpatch.pl:
    
    ERROR: switch and case should be at the same indent
    
    TEST=Build and run on Galileo Gen2
    
    Change-Id: I92f00254c7fcb79a5ecd4ba5e19a757cfe5c11fa
    Signed-off-by: Lee Leahy <Leroy.P.Leahy@intel.com>
    Reviewed-on: https://review.coreboot.org/18683
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/include/cpu/x86/lapic.h
+++ src/include/cpu/x86/lapic.h
@@ -80,24 +80,24 @@ struct __xchg_dummy { unsigned long a[100]; };
 static inline unsigned long __xchg(unsigned long x, volatile void *ptr, int size)
 {
 	switch (size) {
-		case 1:
-			__asm__ __volatile__("xchgb %b0,%1"
-				:"=q" (x)
-				:"m" (*__xg(ptr)), "0" (x)
-				:"memory");
-			break;
-		case 2:
-			__asm__ __volatile__("xchgw %w0,%1"
-				:"=r" (x)
-				:"m" (*__xg(ptr)), "0" (x)
-				:"memory");
-			break;
-		case 4:
-			__asm__ __volatile__("xchgl %0,%1"
-				:"=r" (x)
-				:"m" (*__xg(ptr)), "0" (x)
-				:"memory");
-			break;
+	case 1:
+		__asm__ __volatile__("xchgb %b0,%1"
+			:"=q" (x)
+			:"m" (*__xg(ptr)), "0" (x)
+			:"memory");
+		break;
+	case 2:
+		__asm__ __volatile__("xchgw %w0,%1"
+			:"=r" (x)
+			:"m" (*__xg(ptr)), "0" (x)
+			:"memory");
+		break;
+	case 4:
+		__asm__ __volatile__("xchgl %0,%1"
+			:"=r" (x)
+			:"m" (*__xg(ptr)), "0" (x)
+			:"memory");
+		break;
 	}
 	return x;
 }

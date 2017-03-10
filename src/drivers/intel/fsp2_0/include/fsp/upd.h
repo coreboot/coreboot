f96a2cd575f5bf30ae686a844b27ca39b53c9cf0
Lee Leahy <leroy.p.leahy@intel.com>
2017-03-10 08:25:46 -0800

    UPSTREAM: drivers/intel/fsp2_0: Use tabs for indent
    Fix the following warning detected by checkpatch.pl:
    
    WARNING: please, no spaces at the start of a line
    
    TEST=Build and run on Galileo Gen2
    
    Change-Id: I1c318762b99d0a14520c2caa0e5a4a4cce28dd81
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 30bdb52e4c9041d988959eaa46de580899b2c922
    Original-Change-Id: I7cb35c8b5d7ff97849e666ce7f75d4e4763bb2a7
    Original-Signed-off-by: Lee Leahy <leroy.p.leahy@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/18742
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/453362

--- src/drivers/intel/fsp2_0/include/fsp/upd.h
+++ src/drivers/intel/fsp2_0/include/fsp/upd.h
@@ -13,52 +13,52 @@
 #define _FSP2_0_UPD_H_
 
 struct FSP_UPD_HEADER {
-  ///
-  /// UPD Region Signature. This signature will be
-  /// "XXXXXX_T" for FSP-T
-  /// "XXXXXX_M" for FSP-M
-  /// "XXXXXX_S" for FSP-S
-  /// Where XXXXXX is an unique signature
-  ///
-  uint64_t                      Signature;
-  ///
-  /// Revision of the Data structure. For FSP v2.0 value is 1.
-  ///
-  uint8_t                       Revision;
-  uint8_t                       Reserved[23];
+	///
+	/// UPD Region Signature. This signature will be
+	/// "XXXXXX_T" for FSP-T
+	/// "XXXXXX_M" for FSP-M
+	/// "XXXXXX_S" for FSP-S
+	/// Where XXXXXX is an unique signature
+	///
+	uint64_t                      Signature;
+	///
+	/// Revision of the Data structure. For FSP v2.0 value is 1.
+	///
+	uint8_t                       Revision;
+	uint8_t                       Reserved[23];
 } __attribute__((packed));
 
 struct FSPM_ARCH_UPD {
-  ///
-  /// Revision of the structure. For FSP v2.0 value is 1.
-  ///
-  uint8_t                       Revision;
-  uint8_t                       Reserved[3];
-  ///
-  /// Pointer to the non-volatile storage (NVS) data buffer.
-  /// If it is NULL it indicates the NVS data is not available.
-  ///
-  void                        *NvsBufferPtr;
-  ///
-  /// Pointer to the temporary stack base address to be
-  /// consumed inside FspMemoryInit() API.
-  ///
-  void                        *StackBase;
-  ///
-  /// Temporary stack size to be consumed inside
-  /// FspMemoryInit() API.
-  ///
-  uint32_t                      StackSize;
-  ///
-  /// Size of memory to be reserved by FSP below "top
-  /// of low usable memory" for bootloader usage.
-  ///
-  uint32_t                      BootLoaderTolumSize;
-  ///
-  /// Current boot mode.
-  ///
-  uint32_t                      BootMode;
-  uint8_t                       Reserved1[8];
+	///
+	/// Revision of the structure. For FSP v2.0 value is 1.
+	///
+	uint8_t                       Revision;
+	uint8_t                       Reserved[3];
+	///
+	/// Pointer to the non-volatile storage (NVS) data buffer.
+	/// If it is NULL it indicates the NVS data is not available.
+	///
+	void                        *NvsBufferPtr;
+	///
+	/// Pointer to the temporary stack base address to be
+	/// consumed inside FspMemoryInit() API.
+	///
+	void                        *StackBase;
+	///
+	/// Temporary stack size to be consumed inside
+	/// FspMemoryInit() API.
+	///
+	uint32_t                      StackSize;
+	///
+	/// Size of memory to be reserved by FSP below "top
+	/// of low usable memory" for bootloader usage.
+	///
+	uint32_t                      BootLoaderTolumSize;
+	///
+	/// Current boot mode.
+	///
+	uint32_t                      BootMode;
+	uint8_t                       Reserved1[8];
 } __attribute__((packed));
 
 #endif /* _FSP2_0_UPD_H_ */

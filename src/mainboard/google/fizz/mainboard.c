ae2c878e5405e18597d51d24758e736aa62deaee
Shelley Chen <shchen@chromium.org>
2017-03-10 17:44:00 -0800

    UPSTREAM: google/fizz: Remove poppy-specific configs
    - Remove spd files/directory
    - Remove audio blobs
    - Remove dptf.asl contents
    - Remove MKBP
    - Remove acpi table initialization
    
    BUG=b:35775024
    BRANCH=None
    TEST=Compiles successfully
    
    Change-Id: Id142b1ef38b108d4019e32b9466413913df0ba04
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 336d8c8cd09f8bd540bc7a0e0f52177c03c4c586
    Original-Change-Id: I5d717d23224956ee1653c5ded28abd05cd254c3a
    Original-Signed-off-by: Shelley Chen <shchen@chromium.org>
    Original-Reviewed-on: https://review.coreboot.org/18857
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/459666

--- src/mainboard/google/fizz/mainboard.c
+++ src/mainboard/google/fizz/mainboard.c
@@ -17,61 +17,17 @@
 #include <console/console.h>
 #include <device/device.h>
 #include <ec/ec.h>
-#include <soc/nhlt.h>
 #include <vendorcode/google/chromeos/chromeos.h>
-
-static const char *oem_id_maxim = "GOOGLE";
-static const char *oem_table_id_maxim = "FIZZMAX";
 
 static void mainboard_init(device_t dev)
 {
 	mainboard_ec_init();
 }
 
-static unsigned long mainboard_write_acpi_tables(device_t device,
-	unsigned long current, acpi_rsdp_t *rsdp)
-{
-	uintptr_t start_addr;
-	uintptr_t end_addr;
-	struct nhlt *nhlt;
-
-	start_addr = current;
-
-	nhlt = nhlt_init();
-
-	if (nhlt == NULL)
-		return start_addr;
-
-	/* 2 Channel DMIC array. */
-	if (nhlt_soc_add_dmic_array(nhlt, 2))
-		printk(BIOS_ERR, "Couldn't add 2CH DMIC array.\n");
-
-	/* 4 Channel DMIC array. */
-	if (nhlt_soc_add_dmic_array(nhlt, 4))
-		printk(BIOS_ERR, "Couldn't add 4CH DMIC arrays.\n");
-
-	/* Maxim MAX98927 Smart Amps for left and right channel */
-	if (nhlt_soc_add_max98927(nhlt, AUDIO_LINK_SSP0))
-		printk(BIOS_ERR, "Couldn't add Maxim MAX98927\n");
-
-	/* Realtek RT5663 Headset codec. */
-	if (nhlt_soc_add_rt5663(nhlt, AUDIO_LINK_SSP1))
-		printk(BIOS_ERR, "Couldn't add Realtek RT5663.\n");
-
-	end_addr = nhlt_soc_serialize_oem_overrides(nhlt, start_addr,
-			oem_id_maxim, oem_table_id_maxim, 0);
-
-	if (end_addr != start_addr)
-		acpi_add_table(rsdp, (void *)start_addr);
-
-	return end_addr;
-}
-
 static void mainboard_enable(device_t dev)
 {
 	dev->ops->init = mainboard_init;
 	dev->ops->acpi_inject_dsdt_generator = chromeos_dsdt_generator;
-	dev->ops->write_acpi_tables = mainboard_write_acpi_tables;
 }
 
 struct chip_operations mainboard_ops = {

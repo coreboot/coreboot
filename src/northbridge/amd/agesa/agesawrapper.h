fa420b49c5cbce160cfb4f46fc3542589a800a43
Kyösti Mälkki <kyosti.malkki@gmail.com>
2017-03-26 11:35:23 +0300

    AGESA: Simplify parameters for S3 support functions
    This save/restore facility operates on the same datablock.
    
    Change-Id: I6e1f176adc2addbf2659c724f94c1b8d46d4838f
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/19026
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/northbridge/amd/agesa/agesawrapper.h
+++ src/northbridge/amd/agesa/agesawrapper.h
@@ -52,8 +52,8 @@ struct OEM_HOOK
 extern const struct OEM_HOOK OemCustomize;
 
 /* For suspend-to-ram support. */
-AGESA_STATUS OemInitResume(AMD_RESUME_PARAMS *ResumeParams);
-AGESA_STATUS OemS3LateRestore(AMD_S3LATE_PARAMS *S3LateParams);
-AGESA_STATUS OemS3Save(AMD_S3SAVE_PARAMS *S3SaveParams);
+AGESA_STATUS OemInitResume(AMD_S3_PARAMS *dataBlock);
+AGESA_STATUS OemS3LateRestore(AMD_S3_PARAMS *dataBlock);
+AGESA_STATUS OemS3Save(AMD_S3_PARAMS *dataBlock);
 
 #endif /* _AGESAWRAPPER_H_ */

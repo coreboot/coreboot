c30fdafe26effdcee76ab16527758f76bce91025
Iru Cai <mytbk920423@gmail.com>
2017-03-26 12:05:32 +0800

    UPSTREAM: util: Add tools for dumping and inserting KBC1126 firmware images.
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: Iaa49e89d54dd9688581460fbf4f64d2f51c7c4f4
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Original-Commit-Id: 5fd00ce71a1bfd5cd36f9d42ab657a8e7d6c54f2
    Original-Change-Id: Ic521b177b9602ff042312cccaaa89371db7c5855
    Original-Signed-off-by: Iru Cai <mytbk920423@gmail.com>
    Original-Reviewed-on: https://review.coreboot.org/19071
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://chromium-review.googlesource.com/506186
    Commit-Ready: Patrick Georgi <pgeorgi@chromium.org>
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: Patrick Georgi <pgeorgi@chromium.org>

---
+++ util/kbc1126/kbc1126_ec_dump.c
@@ -0,0 +1,124 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2017 Iru Cai <mytbk920423@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void usage(const char *s)
+{
+	printf("%s <rom file>\n", s);
+	exit(1);
+}
+
+static void FseekEnd(FILE *fp, long o)
+{
+	if (fseek(fp, o, SEEK_END) != 0) {
+		puts("fseek() error!\n");
+		exit(1);
+	}
+}
+
+void dump_fw(FILE *dst, FILE *src, long offset)
+{
+	static unsigned char buf[65536];
+
+	if (offset > 0)
+		offset -= 0x1000000;
+
+	printf("Dumping firmware at -0x%lx...", -offset);
+
+	FseekEnd(src, offset);
+	unsigned short len;
+	unsigned short cksum;
+	unsigned short _cksum = 0;
+	fread(&len, 2, 1, src);
+	fread(&cksum, 2, 1, src);
+	fread(buf, len, 1, src);
+
+	for (size_t i = 0; i < len; i++) {
+		_cksum += buf[i];
+	}
+	if (_cksum == cksum) {
+		puts("checksum ok");
+	} else {
+		puts("checksum fail");
+		exit(1);
+	}
+
+	fwrite(&len, 2, 1, dst);
+	fwrite(&cksum, 2, 1, dst);
+	fwrite(buf, len, 1, dst);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+		usage(argv[0]);
+
+	FILE *fp = fopen(argv[1], "rb+");
+
+	if (fp == NULL) {
+		puts("Error opening file!");
+		exit(1);
+	}
+
+	char *basename = strrchr(argv[1], '/');
+	if (basename == NULL)
+		basename = argv[1];
+	else
+		basename = basename + 1;
+
+	int len = strlen(basename);
+	char fn1[len + 5], fn2[len + 5];
+	strcpy(fn1, basename);
+	strcpy(fn2, basename);
+	strcat(fn1, ".fw1");
+	strcat(fn2, ".fw2");
+
+	FILE *fw1 = fopen(fn1, "wb+");
+	FILE *fw2 = fopen(fn2, "wb+");
+
+	long romsz;
+	FseekEnd(fp, -1);
+	romsz = ftell(fp) + 1;
+	printf("size of %s: 0x%lx\n", argv[1], romsz);
+
+	if (romsz & 0xff) {
+		puts("The ROM size must be multiple of 0x100");
+		exit(1);
+	}
+
+	/* read offset of fw1 and fw2 */
+	char offs[8];
+	FseekEnd(fp, -0x100);
+	fread(offs, 8, 1, fp);
+
+	assert(offs[0] + offs[2] == '\xff');
+	assert(offs[1] + offs[3] == '\xff');
+	assert(offs[4] + offs[6] == '\xff');
+	assert(offs[5] + offs[7] == '\xff');
+	long offw1 = (offs[0] << 16) | (offs[1] << 8);
+	long offw2 = (offs[4] << 16) | (offs[5] << 8);
+
+	dump_fw(fw1, fp, offw1);
+	dump_fw(fw2, fp, offw2);
+
+	fclose(fp);
+	fclose(fw1);
+	fclose(fw2);
+	return 0;
+}

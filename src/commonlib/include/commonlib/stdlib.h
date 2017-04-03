5bc636506954b2eb1c5f1550ee3229e935e0442c
Lee Leahy <leroy.p.leahy@intel.com>
2017-04-03 07:38:20 -0700

    UPSTREAM: include: Add xmalloc, xzmalloc and dma routines
    Add a Kconfig value to indicate coreboot builds.
    
    Add prototypes and definitions for:
    * dma_coherent
    * dma_malloc
    * xmalloc
    * xzmalloc
    
    Move prototype for memset into stdlib.h from string.h to eliminate build
    breaks.
    
    TEST=Build and test on Galileo Gen2
    
    Change-Id: I43d69466a55f18f179b56bd9cd9d5ff8ea058ad8
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: bb70c40f2ef5b8a02f6c0490751cb732ed25fa3d
    Original-Change-Id: Ib2eb2ca143b0538bdd1863e628af4c1948bc0f8c
    Original-Signed-off-by: Lee Leahy <leroy.p.leahy@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/19207
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Reviewed-on: https://chromium-review.googlesource.com/486774

---
+++ src/commonlib/include/commonlib/stdlib.h
@@ -0,0 +1,74 @@
+/*
+ * This file is part of the libpayload project.
+ *
+ * Copyright (C) 2008 Advanced Micro Devices, Inc.
+ * Copyright 2013 Google Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef __COMMONLIB_STDLIB_H__
+#define __COMMONLIB_STDLIB_H__
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if IS_ENABLED(CONFIG_COREBOOT_BUILD)
+#include <console/console.h>
+#include <halt.h>
+#define printf(...)		printk(BIOS_ERR, __VA_ARGS__)
+#define HALT(x)			halt()
+#else
+#include <stdio.h>
+#define HALT(x)
+#endif
+
+static inline void *xmalloc_work(size_t size, const char *file,
+				 const char *func, int line)
+{
+	void *ret = malloc(size);
+	if (!ret && size) {
+		printf("%s/%s/line %d: Failed to malloc %zu bytes\n",
+			file, func, line, size);
+		while (1)
+			HALT(1);
+	}
+	return ret;
+}
+#define xmalloc(size) xmalloc_work((size), __FILE__, __FUNCTION__, __LINE__)
+
+static inline void *xzalloc_work(size_t size, const char *file,
+				 const char *func, int line)
+{
+	void *ret = xmalloc_work(size, file, func, line);
+	memset(ret, 0, size);
+	return ret;
+}
+#define xzalloc(size) xzalloc_work((size), __FILE__, __FUNCTION__, __LINE__)
+
+void *dma_malloc(size_t size);
+int dma_coherent(void *ptr);
+
+#endif /* __COMMONLIB_STDLIB_H__ */

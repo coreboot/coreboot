e20f3d02b579923ac9135521b98c2b9fd99acbe1
Martin Roth <martinroth@google.com>
2017-04-04 14:26:57 -0600

    src/drivers: Add license headers
    Change-Id: I1c4b30ab47e12ec35cb681ec5c6635ecd20aa2e5
    Signed-off-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://review.coreboot.org/19121
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- src/drivers/sil/3114/sil_sata.c
+++ src/drivers/sil/3114/sil_sata.c
@@ -1,7 +1,17 @@
-/* Copyright 2003-2004 Tyan Computer
- *  Yinghai Lu yhlu@tyan.com
+/*
+ * This file is part of the coreboot project.
  *
+ * Copyright (C) 2003-2004 Tyan Computer - Yinghai Lu <yhlu@tyan.com>
  * Copyright (C) 2010 Rudolf Marek <r.marek@assembler.cz>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #include <delay.h>

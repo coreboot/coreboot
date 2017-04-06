138b2a03bedb059f7a4064b4ff03d88083774302
Aamir Bohra <aamir.bohra@intel.com>
2017-04-06 20:21:58 +0530

    soc/intel/apollolake: Use LPSS common library
    Use lpss common library to program reset and
    clock register for lpss modules
    
    Change-Id: I75f9aebd60290fbf22684f8cc2ce8e8a4a4304b0
    Signed-off-by: Aamir Bohra <aamir.bohra@intel.com>
    Reviewed-on: https://review.coreboot.org/19154
    Tested-by: build bot (Jenkins)
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/apollolake/include/soc/uart.h
+++ src/soc/intel/apollolake/include/soc/uart.h
@@ -18,16 +18,6 @@
 #ifndef _SOC_APOLLOLAKE_UART_H_
 #define _SOC_APOLLOLAKE_UART_H_
 
-/* Clock is 100MHz * (M / N).*/
-#define UART_CLK		0x200
-# define UART_CLK_UPDATE	(1 << 31)
-# define UART_CLK_DIV_N(n)	(((n) & 0x7fff) << 16)
-# define UART_CLK_DIV_M(m)	(((m) & 0x7fff) << 1)
-# define UART_CLK_EN		(1 << 0)
-#define UART_RESET		0x204
-# define UART_RESET_DMA_EN	(1 << 2)
-# define UART_RESET_UART_EN	(3 << 0)
-
 void lpss_console_uart_init(void);
 
 /* Initialize the console UART including the pads for the configured UART. */

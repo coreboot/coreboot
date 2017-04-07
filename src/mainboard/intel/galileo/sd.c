a893e498f887d3f52c74d9e1ce5a38629ee790d0
Lee Leahy <leroy.p.leahy@intel.com>
2017-04-07 12:07:46 -0700

    UPSTREAM: mainboard/intel/galileo: Add SD controller configuration
    Configure the SD controller to handle the SD card slot.
    * Galileo supports a removable SD card slot.
    * Set SD card initialization frequency to 100 MHz.
    * Set default removable delays.
    * Build SD/MMC components by default
    
    TEST=Build and run on Galileo Gen2
    
    Change-Id: Icb6ca60d4f7ca9e7f6b387c622f7417713a2f9c7
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 1ea7cce8ae8fbc8a14a3ddfdee3f81a5e0e50031
    Original-Change-Id: Iaf4faa40fe01eca98abffa2681f61fd8e059f0c4
    Original-Signed-off-by: Lee Leahy <leroy.p.leahy@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/19212
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/501152

---
+++ src/mainboard/intel/galileo/sd.c
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2017 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <console/console.h>
+#include <device/sdhci.h>
+#include <device/storage.h>
+
+void soc_sd_mmc_controller_quirks(struct sd_mmc_ctrlr *ctrlr)
+{
+	/* Specify the additional driver support */
+	ctrlr->caps |= DRVR_CAP_REMOVABLE;
+
+	/* ADMA currently not working on Quark */
+	ctrlr->caps &= ~DRVR_CAP_AUTO_CMD12;
+
+	/* Set initialization clock frequency */
+	ctrlr->f_min = 100 * CLOCK_KHZ;
+
+	/* Set the initialization delays */
+	ctrlr->mdelay_before_cmd0 = 1;
+	ctrlr->mdelay_after_cmd0 = 2;
+}

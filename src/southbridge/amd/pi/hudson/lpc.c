3eec9dda1fa7aed3cd6a47232201c23303b3d686
Marc Jones <marcj303@gmail.com>
2017-04-09 18:00:40 -0600

    amd/pi/hudson: Add SERIRQ setup
    Enable SERIRQ in quiet or continuous mode based on Kconfig.
    Defaults to quite mode.
    
    Change-Id: Ib40a84719fcc3a5d6b3000c3c0412f1bcf629609
    Signed-off-by: Marc Jones <marcj303@gmail.com>
    Reviewed-on: https://review.coreboot.org/19234
    Tested-by: build bot (Jenkins)
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/southbridge/amd/pi/hudson/lpc.c
+++ src/southbridge/amd/pi/hudson/lpc.c
@@ -83,6 +83,13 @@ static void lpc_init(device_t dev)
 
 	/* Initialize i8254 timers */
 	setup_i8254 ();
+
+	/* Setup SERIRQ, enable continuous mode */
+	byte = (BIT(4) | BIT(7));
+	if (!IS_ENABLED(CONFIG_SERIRQ_CONTINUOUS_MODE))
+		byte |= BIT(6);
+
+	pm_write8(PM_SERIRQ_CONF, byte);
 }
 
 static void hudson_lpc_read_resources(device_t dev)

fd517514a1ee589ba275e43809947b8f3b7f66c7
Nico Huber <nico.h@gmx.de>
2017-04-18 21:44:50 +0200

    UPSTREAM: mb/sapphire/pureplatinumh61: Update GMA config
    This board was added while the latest libgfxinit changes were in review.
    Update to make it compile again and sanitize the port list.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: Ia0cfaad77bdda3b09771a4d7184f1fdb0f58f4af
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: b7f28700f4e114a12215842d31e29af734a191e2
    Original-Change-Id: I81b96e225945a8f8e47b64cefea91eb2747675ca
    Original-Signed-off-by: Nico Huber <nico.h@gmx.de>
    Original-Reviewed-on: https://review.coreboot.org/19353
    Original-Tested-by: build bot (Jenkins)
    Original-Reviewed-by: Nicola Corna <nicola@corna.info>
    Reviewed-on: https://chromium-review.googlesource.com/486749

--- src/mainboard/sapphire/pureplatinumh61/gma-mainboard.ads
+++ src/mainboard/sapphire/pureplatinumh61/gma-mainboard.ads
@@ -1,21 +1,25 @@
 with HW.GFX.GMA;
+with HW.GFX.GMA.Display_Probing;
 
 use HW.GFX.GMA;
+use HW.GFX.GMA.Display_Probing;
 
 private package GMA.Mainboard is
 
    -- For a three-pipe setup, bandwidth is shared between the 2nd and
-   -- the 3rd pipe (if it's not eDP). Thus, probe ports that likely
-   -- have a high-resolution display attached first, `Internal` last.
+   -- the 3rd pipe. Thus, probe ports that likely have a high-resolution
+   -- display attached first.
 
+   -- FIXME: Only one of the digital ports is implemented as DP
+   --        but it's unknown which.
    ports : constant Port_List :=
-     (DP2,
+     (DP1,
+      DP2,
       DP3,
-      Digital1,
-      Digital2,
-      Digital3,
+      HDMI1,
+      HDMI2,
+      HDMI3,
       Analog,
-      Internal,
       others => Disabled);
 
 end GMA.Mainboard;

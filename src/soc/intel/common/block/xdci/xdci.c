6aeb52667955fb8944d40ebcc4734f9549515677
Subrata Banik <subrata.banik@intel.com>
2017-04-24 11:28:40 +0530

    UPSTREAM: soc/intel/common/block: Add Intel XDCI code support
    XDCI MMIO offsets definitions are not alike between
    various SoCs hence provided "soc_xdci_init" function
    to implement SoC specific initialization.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I304637ef2633bbf1b0ba85f7fa46b26e07b486dc
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 23ccb0de3cb2177b10c23e334ca11f133c675564
    Original-Change-Id: I9cbc686a00c26b92be2847b6bd6c2e5aa5a690f7
    Original-Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/19428
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/498319

---
+++ src/soc/intel/common/block/xdci/xdci.c
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2014 Google Inc.
+ * Copyright (C) 2015-2017 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/io.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <device/pci_ids.h>
+#include <intelblocks/xdci.h>
+
+__attribute__((weak)) void soc_xdci_init(struct device *dev) { /* no-op */ }
+
+static struct device_operations usb_xdci_ops = {
+	.read_resources		= &pci_dev_read_resources,
+	.set_resources		= &pci_dev_set_resources,
+	.enable_resources	= &pci_dev_enable_resources,
+	.init			= soc_xdci_init,
+};
+
+static const unsigned short pci_device_ids[] = {
+	PCI_DEVICE_ID_INTEL_APOLLOLAKE_XDCI,
+	PCI_DEVICE_ID_INTEL_GLK_XDCI,
+	PCI_DEVICE_ID_INTEL_SPT_LP_XDCI,
+	0
+};
+
+static const struct pci_driver pch_usb_xdci __pci_driver = {
+	.ops	 = &usb_xdci_ops,
+	.vendor	 = PCI_VENDOR_ID_INTEL,
+	.devices	 = pci_device_ids,
+};

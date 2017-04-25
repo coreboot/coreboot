71352bf1c6f1c1649c4d3157a8f5edfaf245bbfb
Shamile Khan <shamile.khan@intel.com>
2017-04-24 22:32:58 -0700

    UPSTREAM: google/poppy: Enable PD MCU device
    In order for PD charge events to properly notify the OS when a charger is
    attached we need to enable the PD MCU device and event source from the EC.
    
    Without this change the charging still happens, but the OS does not notice
    and update the charge state icon in the Chrome OS UI.
    
    BUG=b:35586577
    BRANCH=none
    TEST=On a poppy board that has the VBUS rework applied, plug in a charger to
    either port and see charge status updated to indicate charging in the
    power_supply_info tool and the Chrome OS UI.
    
    Change-Id: I07ca5d7383e5e6b014a6b35c2b7c5ba6edd1234e
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: bb368db2a40418bce712803839395263c653477b
    Original-Change-Id: I59dcfc1cb5d11841f56cac7f4ffe461c2f9ec52a
    Original-Signed-off-by: Shamile Khan <shamile.khan@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/19441
    Original-Reviewed-by: Furquan Shaikh <furquan@google.com>
    Original-Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    Original-Tested-by: build bot (Jenkins)
    Reviewed-on: https://chromium-review.googlesource.com/490073
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>

--- src/mainboard/google/poppy/variants/baseboard/include/baseboard/ec.h
+++ src/mainboard/google/poppy/variants/baseboard/include/baseboard/ec.h
@@ -34,6 +34,7 @@
 	 EC_HOST_EVENT_MASK(EC_HOST_EVENT_THERMAL_OVERLOAD)  |\
 	 EC_HOST_EVENT_MASK(EC_HOST_EVENT_THROTTLE_START)    |\
 	 EC_HOST_EVENT_MASK(EC_HOST_EVENT_THROTTLE_STOP)     |\
+	 EC_HOST_EVENT_MASK(EC_HOST_EVENT_PD_MCU)            |\
 	 EC_HOST_EVENT_MASK(EC_HOST_EVENT_MKBP))
 
 #define MAINBOARD_EC_SMI_EVENTS \
@@ -65,6 +66,9 @@
 
 /* Enable EC backed ALS device in ACPI */
 #define EC_ENABLE_ALS_DEVICE
+
+/* Enable EC backed PD MCU device in ACPI */
+#define EC_ENABLE_PD_MCU_DEVICE
 
 /* Enable LID switch and provide wake pin for EC */
 #define EC_ENABLE_LID_SWITCH

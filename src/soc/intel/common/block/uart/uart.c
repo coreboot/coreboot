ffb0f1673d0201f335f1ae11fd8bb408f7ad7dc9
Aamir Bohra <aamir.bohra@intel.com>
2017-04-26 19:30:41 +0530

    UPSTREAM: soc/intel/common: Add PCI configuration code for UART
    Add PCI configuration code support for intel/common/
    block/uart module.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I967254c4f7860b671952f6bd5471c25deffafcb0
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 83f7baec308cffee0709e38c95e3b4726915e2ea
    Original-Change-Id: Ibce5623ffb879f2427b759106d1f350601837e4b
    Original-Signed-off-by: Aamir Bohra <aamir.bohra@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/19490
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/501160

--- src/soc/intel/common/block/uart/uart.c
+++ src/soc/intel/common/block/uart/uart.c
@@ -12,8 +12,10 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-
+#include <device/device.h>
+#include <device/pci.h>
 #include <device/pci_def.h>
+#include <device/pci_ids.h>
 #include <intelblocks/lpss.h>
 #include <intelblocks/uart.h>
 
@@ -33,3 +35,36 @@ void uart_common_init(device_t dev, uintptr_t baseaddr, uint32_t clk_m_val,
 	/* Set M and N divisor inputs and enable clock */
 	lpss_clk_update(baseaddr, clk_m_val, clk_n_val);
 }
+
+#if ENV_RAMSTAGE
+
+__attribute__((weak)) void pch_uart_read_resources(struct device *dev)
+{
+	pci_dev_read_resources(dev);
+}
+
+static struct device_operations device_ops = {
+	.read_resources		= &pch_uart_read_resources,
+	.set_resources		= &pci_dev_set_resources,
+	.enable_resources		= &pci_dev_enable_resources,
+};
+
+static const unsigned short pci_device_ids[] = {
+	PCI_DEVICE_ID_INTEL_SPT_UART0,
+	PCI_DEVICE_ID_INTEL_SPT_UART1,
+	PCI_DEVICE_ID_INTEL_SPT_UART2,
+	PCI_DEVICE_ID_INTEL_KBP_H_UART0,
+	PCI_DEVICE_ID_INTEL_KBP_H_UART1,
+	PCI_DEVICE_ID_INTEL_KBP_H_UART2,
+	PCI_DEVICE_ID_INTEL_APL_UART0,
+	PCI_DEVICE_ID_INTEL_APL_UART1,
+	PCI_DEVICE_ID_INTEL_APL_UART2,
+	PCI_DEVICE_ID_INTEL_APL_UART3,
+};
+
+static const struct pci_driver pch_uart __pci_driver = {
+	.ops	= &device_ops,
+	.vendor	= PCI_VENDOR_ID_INTEL,
+	.devices	= pci_device_ids,
+};
+#endif /* ENV_RAMSTAGE */

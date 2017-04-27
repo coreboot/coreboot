d6b416dbbe0b8bc15c8cebbd22eef34ea66742a1
Aamir Bohra <aamir.bohra@intel.com>
2017-04-27 10:49:04 +0530

    UPSTREAM: soc/intel/skylake: Use common/blocks/uart code
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: Id6f956580ce070eafa6892df4252f94537504e5d
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 06ef8897187cd08fbe13410a62c0302afd64c1ea
    Original-Change-Id: I53ed687dc49524e001889f091825b2cc530546a3
    Original-Signed-off-by: Aamir Bohra <aamir.bohra@intel.com>
    Original-Reviewed-on: https://review.coreboot.org/19492
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/501162

--- src/soc/intel/skylake/uart.c
+++ src/soc/intel/skylake/uart.c
@@ -14,20 +14,17 @@
  * GNU General Public License for more details.
  */
 
-#include <device/device.h>
 #include <device/pci.h>
-#include <device/pci_ids.h>
-#include <stdlib.h>
+#include <intelblocks/uart.h>
 #include <soc/iomap.h>
 #include <soc/pci_devs.h>
-#include <soc/ramstage.h>
 
 static int pch_uart_is_debug(struct device *dev)
 {
 	return dev->path.pci.devfn == PCH_DEVFN_UART2;
 }
 
-static void pch_uart_read_resources(struct device *dev)
+void pch_uart_read_resources(struct device *dev)
 {
 	pci_dev_read_resources(dev);
 
@@ -41,26 +38,3 @@ static void pch_uart_read_resources(struct device *dev)
 			IORESOURCE_FIXED;
 	}
 }
-
-static struct device_operations device_ops = {
-	.read_resources		= &pch_uart_read_resources,
-	.set_resources		= &pci_dev_set_resources,
-	.enable_resources	= &pci_dev_enable_resources,
-	.ops_pci		= &soc_pci_ops,
-};
-
-static const unsigned short pci_device_ids[] = {
-	0x9d27, /* UART0 */
-	0x9d28, /* UART1 */
-	0x9d66, /* UART2 */
-	0xa127, /* KBL-H UART0 */
-	0xa128, /* KBL-H UART1 */
-	0xa166, /* KBL-H UART2 */
-	0
-};
-
-static const struct pci_driver pch_uart __pci_driver = {
-	.ops	 = &device_ops,
-	.vendor	 = PCI_VENDOR_ID_INTEL,
-	.devices = pci_device_ids,
-};

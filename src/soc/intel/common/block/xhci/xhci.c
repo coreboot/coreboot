fc98c0195eb1e9ccf0f7757689a8070097ffd0e8
Subrata Banik <subrata.banik@intel.com>
2017-05-03 13:29:19 +0530

    common/block/xhci: Get XHCI PCI ID from device/pci_ids.h
    Change-Id: I33d92a173055ea18b8675c720f01dd5bc77befa3
    Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Reviewed-on: https://review.coreboot.org/19536
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/common/block/xhci/xhci.c
+++ src/soc/intel/common/block/xhci/xhci.c
@@ -30,10 +30,10 @@ static struct device_operations usb_xhci_ops = {
 };
 
 static const unsigned short pci_device_ids[] = {
-	0x5aa8, /* ApolloLake */
-	0x31a8, /* GLK */
-	0x9d2f, /* SunRisePoint LP */
-	0xa12f, /* KBL-H*/
+	PCI_DEVICE_ID_INTEL_APOLLOLAKE_XHCI,
+	PCI_DEVICE_ID_INTEL_GLK_XHCI,
+	PCI_DEVICE_ID_INTEL_SPT_LP_XHCI,
+	PCI_DEVICE_ID_INTEL_KBP_H_XHCI,
 	0
 };
 

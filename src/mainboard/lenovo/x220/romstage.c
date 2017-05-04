93eac6a89d6aa4367436610a690c5aa0f040fc3a
Patrick Rudolph <siro@das-labor.org>
2017-05-04 19:10:50 +0200

    mb/lenovo/*/romstage: Remove COM IO port
    All those boards do not have a serial port.
    
    Don't attempt to decode the COMA/COMB IO range.
    
    Change-Id: Ide7e818f87e70e3f559d0769ccde89c35da961d6
    Signed-off-by: Patrick Rudolph <siro@das-labor.org>
    Reviewed-on: https://review.coreboot.org/19571
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/mainboard/lenovo/x220/romstage.c
+++ src/mainboard/lenovo/x220/romstage.c
@@ -38,14 +38,11 @@ void pch_enable_lpc(void)
 	/* X230 EC Decode Range Port60/64, Port62/66 */
 	/* Enable EC, PS/2 Keyboard/Mouse */
 	pci_write_config16(PCH_LPC_DEV, LPC_EN,
-			   CNF2_LPC_EN | CNF1_LPC_EN | MC_LPC_EN | KBC_LPC_EN |
-			   COMA_LPC_EN);
+			   CNF2_LPC_EN | CNF1_LPC_EN | MC_LPC_EN | KBC_LPC_EN);
 
 	pci_write_config32(PCH_LPC_DEV, LPC_GEN1_DEC, 0x7c1601);
 	pci_write_config32(PCH_LPC_DEV, LPC_GEN2_DEC, 0xc15e1);
 	pci_write_config32(PCH_LPC_DEV, LPC_GEN4_DEC, 0x0c06a1);
-
-	pci_write_config16(PCH_LPC_DEV, LPC_IO_DEC, 0x10);
 
 	pci_write_config32(PCH_LPC_DEV, ETR3, 0x10000);
 }

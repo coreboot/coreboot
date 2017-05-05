d3e4c86fc3aa162425702759b70127ef00bd0472
Caesar Wang <wxt@rock-chips.com>
2017-05-05 17:37:42 +0800

    UPSTREAM: google/gru: support 800M/928M frequency for bob
    The coreboot had no supported the different frequency for gru yet.
    e.g:
    we can't support the bob to run ddr 800M for rev3 board and
    run 928M for rev4 board.
    
    So, in order to support the 800M and 928M ddr frequency for bob different
    boards. We will use the ram_id and board_id to select the board on bob.
    
    BRANCH=none
    BUG=b:36666655
    TEST=boot from bob, tested with memtester/s2r/reboot on bob.
    
    Change-Id: I613050292a09ff56f4636d7af285075e32259ef4
    Original-Change-Id: I613050292a09ff56f4636d7af285075e32259ef4
    Original-Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Original-Reviewed-on: https://review.coreboot.org/19558
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/488421
    Commit-Ready: Caesar Wang <wxt@rock-chips.com>
    Tested-by: Caesar Wang <wxt@rock-chips.com>
    Reviewed-by: Philip Chen <philipchen@chromium.org>

--- src/mainboard/google/gru/sdram_configs.c
+++ src/mainboard/google/gru/sdram_configs.c
@@ -23,23 +23,40 @@
 #include <types.h>
 
 static const char *sdram_configs[] = {
-    [0] = "sdram-lpddr3-hynix-4GB",
-    [3] = "sdram-lpddr3-samsung-2GB-24EB",
-    [4] = "sdram-lpddr3-micron-2GB",
-    [5] = "sdram-lpddr3-samsung-4GB-04EB",
-    [6] = "sdram-lpddr3-micron-4GB",
+	[0] = "sdram-lpddr3-hynix-4GB",
+	[3] = "sdram-lpddr3-samsung-2GB-24EB",
+	[4] = "sdram-lpddr3-micron-2GB",
+	[5] = "sdram-lpddr3-samsung-4GB-04EB",
+	[6] = "sdram-lpddr3-micron-4GB",
 };
 
 static struct rk3399_sdram_params params;
 
+enum dram_speeds {
+	dram_800MHz = 800,
+	dram_928MHz = 928,
+};
+
+static enum dram_speeds get_sdram_target_mhz(void)
+{
+	if (IS_ENABLED(CONFIG_BOARD_GOOGLE_BOB) && board_id() < 4)
+		return dram_800MHz;
+
+	return dram_928MHz;
+}
+
 const struct rk3399_sdram_params *get_sdram_config()
 {
+	char config_file[64];
 	uint32_t ramcode;
 
 	ramcode = ram_code();
-	if (ramcode >= ARRAY_SIZE(sdram_configs) || !sdram_configs[ramcode] ||
-	    (cbfs_boot_load_struct(sdram_configs[ramcode],
-				   &params, sizeof(params)) != sizeof(params)))
+	if (ramcode >= ARRAY_SIZE(sdram_configs) ||
+	    !snprintf(config_file, sizeof(config_file), "%s-%d",
+	    sdram_configs[ramcode], get_sdram_target_mhz()) ||
+	    (cbfs_boot_load_struct(config_file, &params,
+				   sizeof(params)) != sizeof(params)))
 		die("Cannot load SDRAM parameter file!");
+
 	return &params;
 }

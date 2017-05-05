c8970a09430cebbf465d0bac847546d05db1adbf
Aaron Durbin <adurbin@chromium.org>
2017-05-05 15:44:20 -0500

    UPSTREAM: soc/intel/skylake: remove unused SMI functions
    The southbridge_trigger_smi() and southbridge_clear_smi_status()
    functions are unused. Remove them.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I8c845999a08463974a5a6cf7f17f2f7faf7017d8
    Signed-off-by: Patrick Georgi <pgeorgi@chromium.org>
    Original-Commit-Id: 5c48b7e9f2d0249b07a673b4531a4781dd088616
    Original-Change-Id: I86994191a63cbf515bc330433ef7c3f79a39936e
    Original-Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Original-Reviewed-on: https://review.coreboot.org/19598
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Furquan Shaikh <furquan@google.com>
    Original-Reviewed-by: Philippe Mathieu-Daud <philippe.mathieu.daude@gmail.com>
    Original-Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-on: https://chromium-review.googlesource.com/498350

--- src/soc/intel/skylake/smi.c
+++ src/soc/intel/skylake/smi.c
@@ -70,38 +70,6 @@ void southbridge_smm_enable_smi(void)
 	enable_smi(APMC_EN | SLP_SMI_EN | GBL_SMI_EN | ESPI_SMI_EN | EOS);
 }
 
-void southbridge_trigger_smi(void)
-{
-	/*
-	 * There are several methods of raising a controlled SMI# via
-	 * software, among them:
-	 *  - Writes to io 0xb2 (APMC)
-	 *  - Writes to the Local Apic ICR with Delivery mode SMI.
-	 *
-	 * Using the local apic is a bit more tricky. According to
-	 * AMD Family 11 Processor BKDG no destination shorthand must be
-	 * used.
-	 * The whole SMM initialization is quite a bit hardware specific, so
-	 * I'm not too worried about the better of the methods at the moment
-	 */
-
-	/* raise an SMI interrupt */
-	printk(BIOS_SPEW, "  ... raise SMI#\n");
-	outb(0x00, 0xb2);
-}
-
-void southbridge_clear_smi_status(void)
-{
-	/* Clear SMI status */
-	clear_smi_status();
-
-	/* Clear PM1 status */
-	clear_pm1_status();
-
-	/* Set EOS bit so other SMIs can occur. */
-	enable_smi(EOS);
-}
-
 void smm_setup_structures(void *gnvs, void *tcg, void *smi1)
 {
 	/*

3178bdc345131f88a9ffd2c87a98d2ea332d52d7
Furquan Shaikh <furquan@chromium.org>
2017-05-27 17:52:32 -0700

    mainboard/google/poppy: Power down camera rails when suspending
    BUG=b:62147763
    
    Change-Id: Iba88fed972b847448e01fcfca8c7129d950244c2
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://review.coreboot.org/19953
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/google/poppy/smihandler.c
+++ src/mainboard/google/poppy/smihandler.c
@@ -15,8 +15,11 @@
 
 #include <cpu/x86/smm.h>
 #include <ec/google/chromeec/smm.h>
+#include <gpio.h>
+#include <soc/gpio.h>
 #include <soc/smm.h>
 
+#include <variant/gpio.h>
 #include <variant/ec.h>
 
 void mainboard_smi_espi_handler(void)
@@ -24,10 +27,18 @@ void mainboard_smi_espi_handler(void)
 	chromeec_smi_process_events();
 }
 
+static void mainboard_gpio_smi_sleep(void)
+{
+	/* Hold camera device in reset and then power it down. */
+	gpio_set(EN_CAM_PMIC_RST_L, 0);
+	gpio_set(EN_PP3300_DX_CAM, 0);
+}
+
 void mainboard_smi_sleep(u8 slp_typ)
 {
 	chromeec_smi_sleep(slp_typ, MAINBOARD_EC_S3_WAKE_EVENTS,
 			MAINBOARD_EC_S5_WAKE_EVENTS);
+	mainboard_gpio_smi_sleep();
 }
 
 int mainboard_smi_apmc(u8 apmc)

1318ea600b348cff43a66ffa6296a552b59e8888
Samuel Holland <samuel@sholland.org>
2017-06-03 03:52:57 -0500

    superio/ite/it8720f: add new IT8720F Super I/O
    This device is extremely similar to the IT8718F, so support is based on
    existing support for the IT8718F. The CIR device is only detected by
    Linux/Windows from the ACPI tables, so ACPI support is extended from the
    IT8783E/F (for ACPI). This Super I/O is used on the Foxconn G41S-K.
    
    Tested, working:
    * Serial port 1
    * Environment controller
      - Temperature monitoring
      - Voltage monitoring
      - Fan control (automatic and manual)
    * PS/2 keyboard and mouse
    
    Appears, OS driver loads, but otherwise untested:
    * Serial port 2
    * Consumer IR
    
    Untested:
    * Floppy controller
    * Parallel port
    * GPIO
    
    Change-Id: Ib9a6fe91a772d78f4d122a6c516feff8658ada0a
    Signed-off-by: Samuel Holland <samuel@sholland.org>
    Reviewed-on: https://review.coreboot.org/20026
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

---
+++ src/superio/ite/it8720f/it8720f.h
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2006 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 QingPei Wang <wangqingpei@gmail.com>
+ * Copyright (C) 2017 Samuel Holland <samuel@sholland.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SUPERIO_ITE_IT8720F_H
+#define SUPERIO_ITE_IT8720F_H
+
+/* Logical Device Numbers (LDN). */
+#define IT8720F_FDC  0x00 /* Floppy */
+#define IT8720F_SP1  0x01 /* Serial port 1 */
+#define IT8720F_SP2  0x02 /* Serial port 2 */
+#define IT8720F_PP   0x03 /* Parallel port */
+#define IT8720F_EC   0x04 /* Environment controller */
+#define IT8720F_KBCK 0x05 /* PS/2 keyboard */
+#define IT8720F_KBCM 0x06 /* PS/2 mouse */
+#define IT8720F_GPIO 0x07 /* GPIO (including SPI flash interface) */
+#define IT8720F_CIR  0x0a /* Consumer IR */
+
+#endif /* SUPERIO_ITE_IT8720F_H */

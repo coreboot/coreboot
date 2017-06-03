a2a34b8085b8e52c31a4f901b4efce5f6687c6af
Samuel Holland <samuel@sholland.org>
2017-06-03 03:53:33 -0500

    UPSTREAM: mb/foxconn/g41s-k: add new mainboard
    Based on the Intel G41 chipset, ICH7 southbridge, and IT8720F Super I/O.
    
    Tested, working:
    * Booting Linux 4.11.3 and Windows 8.1 from USB and HDD
    * Resume from S3 (Linux and Windows)
    * Native raminit (DDR2-800)
    * Native graphics init (SeaBIOS, Linux)
    * Graphics init with VGA BIOS (SeaBIOS, Windows)
    * PCI-E x16 PEG slot, PCI-E x1 slot from southbridge
    * Realtek ALC888 HD Audio (including front panel and jack detection)
    * Realtek R8168 Gigabit LAN
    * Both SATA ports
    * USB 1.1 and 2.0 devices (keyboard, mass storage)
    * PC speaker beep
    * COM header
    * Super I/O Environment controller (temps, voltage, fans)
    * PS/2 keyboard and mouse
    * Flashing with `flashrom -p internal`
    * 1MiB and 2MiB SPI flash chips
    * CMOS gfx_uma_size
    
    Appears, OS driver loads, but otherwise untested:
    * IrDA header
    * CIR header
    * TPM header
    
    Untested:
    * S/PDIF digital audio
    
    Tested, known broken:
    * CMOS power_on_after_fail
    * USB keyboard in secondary payloads
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: Iecb5ecf8a718755b5ce8f1ea52a803f609fea726
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Original-Commit-Id: 82651463e38aef32c5fc4012c5b9f3ac5b4e2104
    Original-Change-Id: Ifc4c8935b1a11e55f4bf6cfa484a8a8d09b1adda
    Original-Signed-off-by: Samuel Holland <samuel@sholland.org>
    Original-Reviewed-on: https://review.coreboot.org/20027
    Original-Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-on: https://chromium-review.googlesource.com/531728
    Commit-Ready: Patrick Georgi <pgeorgi@chromium.org>
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: Patrick Georgi <pgeorgi@chromium.org>

---
+++ src/mainboard/foxconn/g41s-k/hda_verb.c
@@ -0,0 +1,47 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2015 Damien Zammit <damien@zamaudio.com>
+ * Copyright (C) 2017 Samuel Holland <samuel@sholland.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <device/azalia_device.h>
+
+const u32 cim_verb_data[] = {
+	/* coreboot specific header */
+	0x10ec0888, /* Vendor ID */
+	0x105b0dda, /* Subsystem ID */
+	0x0000000e, /* Number of entries */
+
+	/* Pin Widget Verb Table */
+
+	AZALIA_PIN_CFG(0, 0x11, 0x99430140),
+	AZALIA_PIN_CFG(0, 0x12, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x14, 0x01014410),
+	AZALIA_PIN_CFG(0, 0x15, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x16, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x17, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x18, 0x01a19c50),
+	AZALIA_PIN_CFG(0, 0x19, 0x02a19c60),
+	AZALIA_PIN_CFG(0, 0x1a, 0x0181345f),
+	AZALIA_PIN_CFG(0, 0x1b, 0x02214c20),
+	AZALIA_PIN_CFG(0, 0x1c, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1d, 0x4004c601),
+	AZALIA_PIN_CFG(0, 0x1e, 0x01441130),
+	AZALIA_PIN_CFG(0, 0x1f, 0x411111f0),
+};
+
+const u32 pc_beep_verbs[0] = {};
+
+const u32 pc_beep_verbs_size = ARRAY_SIZE(pc_beep_verbs);
+const u32 cim_verb_data_size = ARRAY_SIZE(cim_verb_data);

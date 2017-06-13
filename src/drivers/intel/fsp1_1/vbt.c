7d14a192ef6022169d2cd96d19c99d9b69609415
Matt DeVillier <matt.devillier@gmail.com>
2017-06-13 16:54:00 -0500

    UPSTREAM: drivers/fsp1_1: decouple VBT from execution of GOP driver
    Commit 2e7f6cc introduced the 'no graphics init' option for
    FSP 1.1 SoCs using a GOP driver to init the display, but selecting
    that option while including a VBT breaks compilation for Braswell
    and Skylake devices because the VBT and GOP driver are intertwined.
    
    This patch decouples the VBT from the GOP driver execution,
    allowing the 'no graphics init' option to compile (and work)
    properly when CONFIG_ADD_VBT_DATA_FILE=y.
    
    BUG=none
    BRANCH=none
    TEST=none
    
    Change-Id: I25447b151e00c2505e7c6eff9411adbbadaa9848
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Original-Commit-Id: 660de34bbf0c9843120ffa2e861d26718e3205e3
    Original-Change-Id: Ifbcf32805177c290c4781b32bbcca679bcb0c297
    Original-Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Original-Reviewed-on: https://review.coreboot.org/20210
    Original-Reviewed-by: Nico Huber <nico.h@gmx.de>
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Original-Reviewed-by: Youness Alaoui <snifikino@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/538572
    Commit-Ready: Patrick Georgi <pgeorgi@chromium.org>
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: Stefan Reinauer <reinauer@google.com>

--- src/drivers/intel/fsp1_1/vbt.c
+++ src/drivers/intel/fsp1_1/vbt.c
@@ -15,9 +15,48 @@
  */
 
 #include <bootmode.h>
+#include <cbfs.h>
 #include <console/console.h>
 #include <fsp/ramstage.h>
 #include <fsp/util.h>
+
+/* Reading VBT table from flash */
+const optionrom_vbt_t *fsp_get_vbt(uint32_t *vbt_len)
+{
+	size_t vbt_size;
+	union {
+		const optionrom_vbt_t *data;
+		uint32_t *signature;
+	} vbt;
+
+	/* Locate the vbt file in cbfs */
+	vbt.data = cbfs_boot_map_with_leak("vbt.bin", CBFS_TYPE_RAW, &vbt_size);
+	if (!vbt.data) {
+		printk(BIOS_INFO,
+			"FSP_INFO: VBT data file (vbt.bin) not found in CBFS");
+		return NULL;
+	}
+
+	/* Validate the vbt file */
+	if (*vbt.signature != VBT_SIGNATURE) {
+		printk(BIOS_WARNING,
+			"FSP_WARNING: Invalid signature in VBT data file (vbt.bin)!\n");
+		return NULL;
+	}
+	*vbt_len = vbt_size;
+	printk(BIOS_DEBUG, "FSP_INFO: VBT found at %p, 0x%08x bytes\n",
+		vbt.data, *vbt_len);
+
+#if IS_ENABLED(CONFIG_DISPLAY_VBT)
+	/* Display the vbt file contents */
+	printk(BIOS_DEBUG, "VBT Data:\n");
+	hexdump(vbt.data, *vbt_len);
+	printk(BIOS_DEBUG, "\n");
+#endif
+
+	/* Return the pointer to the vbt file data */
+	return vbt.data;
+}
 
 /* Locate VBT and pass it to FSP GOP */
 void load_vbt(uint8_t s3_resume, SILICON_INIT_UPD *params)

a9d3d65a924ce765a481337ca22d36f5ab351ced
Marshall Dawson <marshalldawson3rd@gmail.com>
2017-06-23 13:05:09 -0600

    google/kahlee: Remove AMD IMC
    Kahlee does not use the AMD IMC. Remove the files and calls.
    
    Change-Id: Ia837551b592b4f473eb38c06c516586fb6c95c88
    Signed-off-by: Marc Jones <marcj303@gmail.com>
    Reviewed-on: https://review.coreboot.org/19832
    Reviewed-by: Martin Roth <martinroth@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/kahlee/BiosCallOuts.c
+++ src/mainboard/google/kahlee/BiosCallOuts.c
@@ -16,7 +16,6 @@
 #include <AGESA.h>
 #include <BiosCallOuts.h>
 #include <FchPlatform.h>
-#include <soc/imc.h>
 #include <soc/hudson.h>
 #include <stdlib.h>
 
@@ -27,8 +26,6 @@ static AGESA_STATUS fch_initenv(UINT32 Func, UINTN FchData, VOID *ConfigPtr)
 	if (StdHeader->Func == AMD_INIT_ENV) {
 		FCH_DATA_BLOCK *FchParams_env = (FCH_DATA_BLOCK *)FchData;
 		printk(BIOS_DEBUG, "Fch OEM config in INIT ENV ");
-		if (IS_ENABLED(CONFIG_STONEYRIDGE_IMC_FWM))
-			oem_fan_control(FchParams_env);
 
 		/* XHCI configuration */
 		if (IS_ENABLED(CONFIG_STONEYRIDGE_XHCI_ENABLE))

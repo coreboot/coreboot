9858796a1b3f755ee9f63d9864fe48595022f4ed
Elyes HAOUAS <ehaouas@noos.fr>
2017-07-03 21:43:18 +0200

    sb/intel/ibexpeak: Add whitespace around '<<'
    Change-Id: Ib3a69f45b48c19c61b12a992b23dad1693bf5f9e
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/20459
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/southbridge/intel/ibexpeak/smi.c
+++ src/southbridge/intel/ibexpeak/smi.c
@@ -181,9 +181,9 @@ static u32 reset_tco_status(void)
 
 	reg32 = inl(tcobase + 0x04);
 	/* set status bits are cleared by writing 1 to them */
-	outl(reg32 & ~(1<<18), tcobase + 0x04); //  Don't clear BOOT_STS before SECOND_TO_STS
+	outl(reg32 & ~(1 << 18), tcobase + 0x04); //  Don't clear BOOT_STS before SECOND_TO_STS
 	if (reg32 & (1 << 18))
-		outl(reg32 & (1<<18), tcobase + 0x04); // clear BOOT_STS
+		outl(reg32 & (1 << 18), tcobase + 0x04); // clear BOOT_STS
 
 	return reg32;
 }

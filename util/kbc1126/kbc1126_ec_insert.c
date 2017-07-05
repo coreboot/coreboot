4225493159bf1ad35e217d96de9c8222155e57fb
Iru Cai <mytbk920423@gmail.com>
2017-07-06 00:04:47 +0800

    util/kbc1126: Refactor kbc1126_ec_insert
    Add support for setting addresses of blobs only, and using address in
    memory when ROM is mapped
    
    Change-Id: I053c55253eee00558071d15923e3eb17b1e8410c
    Signed-off-by: Iru Cai <mytbk920423@gmail.com>
    Reviewed-on: https://review.coreboot.org/20474
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- util/kbc1126/kbc1126_ec_insert.c
+++ util/kbc1126/kbc1126_ec_insert.c
@@ -18,7 +18,18 @@
 
 static void usage(const char *s)
 {
-	printf("%s <rom file> <fw1> <fw2> <fw1 offset> <fw2 offset>\n", s);
+	printf("insert firmware blobs:\n\t"
+	       "%s <rom file> <fw1> <fw2> <fw1 offset> <fw2 offset>\n\n",
+	       s);
+	printf("set addresses of firmware blobs only:\n\t"
+	       "%s <rom file> <fw1 offset> <fw2 offset>\n\n",
+	       s);
+	printf(
+	    "offset can be (example is put it to 0x7ffa00 when ROM is 8MB):\n"
+	    "- file offset: 0x7ffa00\n"
+	    "- distance to the end of file: -0x600\n"
+	    "- the address when ROM is mapped to the end of memory: "
+	    "0xfffffa00\n");
 	exit(1);
 }
 
@@ -30,20 +41,48 @@ static void FseekEnd(FILE *fp, long o)
 	}
 }
 
+static long negoffset(long a, long romsz)
+{
+	if (a > 0) {
+		if (a & 0x80000000) /* the address in memory, and sizeof(long)
+				       is 8 */
+			return a - 0x100000000;
+		else /* the file offset */
+			return a - romsz;
+	} else {
+		return a;
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	if (argc < 6)
+	FILE *fp, *fw1, *fw2;
+	long offset1, offset2;
+
+	if (argc != 4 && argc != 6)
 		usage(argv[0]);
 
-	FILE *fp = fopen(argv[1], "rb+");
-	FILE *fw1 = fopen(argv[2], "rb");
-	FILE *fw2 = fopen(argv[3], "rb");
-	long offset1 = strtol(argv[4], NULL, 0);
-	long offset2 = strtol(argv[5], NULL, 0);
-
-	if (fp == NULL || fw1 == NULL || fw2 == NULL) {
-		puts("Error opening file!");
+	fp = fopen(argv[1], "rb+");
+	if (fp == NULL) {
+		puts("Error opening firmware image!");
 		exit(1);
+	}
+
+	if (argc == 6) {
+		fw1 = fopen(argv[2], "rb");
+		fw2 = fopen(argv[3], "rb");
+		offset1 = strtoul(argv[4], NULL, 0);
+		offset2 = strtoul(argv[5], NULL, 0);
+
+		if (fw1 == NULL || fw2 == NULL) {
+			puts("Error opening file!");
+			exit(1);
+		}
+	} else {
+		fw1 = NULL;
+		fw2 = NULL;
+		offset1 = strtoul(argv[2], NULL, 0);
+		offset2 = strtoul(argv[3], NULL, 0);
 	}
 
 	if ((offset1 & 0xff) || (offset2 & 0xff)) {
@@ -61,11 +100,8 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	if (offset1 > 0)
-		offset1 = offset1 - romsz;
-
-	if (offset2 > 0)
-		offset2 = offset2 - romsz;
+	offset1 = negoffset(offset1, romsz);
+	offset2 = negoffset(offset2, romsz);
 
 	/* write two offsets to $s-0x100 */
 	char offs[8];
@@ -80,29 +116,31 @@ int main(int argc, char *argv[])
 	offs[5] = os >> 8;
 	offs[6] = 0xff - offs[4];
 	offs[7] = 0xff - offs[5];
-	for (size_t i = 0; i < 8; i++) {
+	for (size_t i = 0; i < 8; i++)
 		printf("%02hhx ", offs[i]);
-	}
+
 	puts("");
 	FseekEnd(fp, -0x100);
 	printf("writing to 0x%lx\n", ftell(fp));
 	fwrite(offs, 1, 8, fp);
 
-	/* write fw1 and fw2 */
-	char c;
-	FseekEnd(fp, offset1);
-	printf("writing to 0x%lx\n", ftell(fp));
-	while (fread(&c, 1, 1, fw1) == 1) {
-		fwrite(&c, 1, 1, fp);
-	}
-	FseekEnd(fp, offset2);
-	printf("writing to 0x%lx\n", ftell(fp));
-	while (fread(&c, 1, 1, fw2) == 1) {
-		fwrite(&c, 1, 1, fp);
+	if (argc == 6) {
+		/* write fw1 and fw2 */
+		char c;
+		FseekEnd(fp, offset1);
+		printf("writing to 0x%lx\n", ftell(fp));
+		while (fread(&c, 1, 1, fw1) == 1)
+			fwrite(&c, 1, 1, fp);
+
+		FseekEnd(fp, offset2);
+		printf("writing to 0x%lx\n", ftell(fp));
+		while (fread(&c, 1, 1, fw2) == 1)
+			fwrite(&c, 1, 1, fp);
+
+		fclose(fw1);
+		fclose(fw2);
 	}
 
 	fclose(fp);
-	fclose(fw1);
-	fclose(fw2);
 	return 0;
 }

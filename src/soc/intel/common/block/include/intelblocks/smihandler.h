46e293ddff163cea3446ee4413a3c6d071119e60
Aaron Durbin <adurbin@chromium.org>
2017-08-09 11:50:03 -0600

    soc/intel/common: smi handler fix comilation error
    The device_t type is used in smihandler.h, but the header
    is not including a header which provides that type. Fix
    that by #includeing <device/device.h>
    
    Change-Id: I89af949b088bf569d330f2ea74b1b8ae97e1ed52
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://review.coreboot.org/20933
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Subrata Banik <subrata.banik@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/common/block/include/intelblocks/smihandler.h
+++ src/soc/intel/common/block/include/intelblocks/smihandler.h
@@ -16,6 +16,8 @@
 #ifndef SOC_INTEL_COMMON_BLOCK_SMI_HANDLER_H
 #define SOC_INTEL_COMMON_BLOCK_SMI_HANDLER_H
 
+#include <arch/io.h>
+#include <device/device.h>
 #include <stdint.h>
 
 struct gpi_status;

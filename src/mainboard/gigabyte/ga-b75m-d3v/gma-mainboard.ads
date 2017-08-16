e464ccd116fe51137d9068c5db2edd7275ae8c9d
Bill XIE <persmule@gmail.com>
2017-08-16 23:05:54 +0800

    mb/gigabyte/ga-b75m-d3v: add libgfxinit support
    On this device libgfxinit successfully initializes textmode and 
    framebuffer mode on the device connected to the HDMI1 port.
    
    VGA port is not tested, due to lack of VGA with valid EDID.
    
    Every ports works on GNU/Linux with kernel version 4.9+80+deb9u1.
    
    Due to limitations within the libgfxinit's current build system,
    it needs "select NORTHBRIDGE_INTEL_IVYBRIDGE" to work, even it is
    PCH instead of CPU for which libgfxinit should be configured here.
    
    Change-Id: Ib1513fc57b79eebc5d047c2e24585731c44bc215
    Signed-off-by: Bill XIE <persmule@gmail.com>
    Reviewed-on: https://review.coreboot.org/21071
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

---
+++ src/mainboard/gigabyte/ga-b75m-d3v/gma-mainboard.ads
@@ -0,0 +1,28 @@
+--
+-- Copyright (C) 2017 Bill XIE persmule@gmail.com
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+
+with HW.GFX.GMA;
+with HW.GFX.GMA.Display_Probing;
+
+use HW.GFX.GMA;
+use HW.GFX.GMA.Display_Probing;
+
+private package GMA.Mainboard is
+
+   ports : constant Port_List :=
+     (HDMI1,
+      Analog,
+      others => Disabled);
+
+end GMA.Mainboard;

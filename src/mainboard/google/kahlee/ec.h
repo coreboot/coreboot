f7cd2f5b940aabb79a2ed83cdbbb4fe311e04bff
Akshu Agrawal <akshu.agrawal@amd.com>
2017-08-21 14:21:30 +0530

    google/kahlee: Enable ALS connected to EC
    Kahlee has an ambient light sensor connected to the EC.
    
    TEST=Can see the device in /sys/bus/iio
    BUG=b:62030268
    
    Change-Id: Id1138a0fc5270489a734bdf8b1f4ac02d358c0df
    Signed-off-by: Akshu Agrawal <akshu.agrawal@amd.com>
    Reviewed-on: https://review.coreboot.org/21146
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/google/kahlee/ec.h
+++ src/mainboard/google/kahlee/ec.h
@@ -61,6 +61,9 @@
 /* Enable LID switch */
 #define EC_ENABLE_LID_SWITCH
 
+/* Enable EC backed ALS device in ACPI */
+#define EC_ENABLE_ALS_DEVICE
+
 #define SIO_EC_MEMMAP_ENABLE	/* EC Memory Map Resources */
 #define SIO_EC_HOST_ENABLE	/* EC Host Interface Resources */
 #define SIO_EC_ENABLE_PS2K	/* Enable PS/2 Keyboard */

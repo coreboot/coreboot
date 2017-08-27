7bb0325884593bc9d3aceb4533703acfeb17084a
Kevin Keijzer <kevin@quietlife.nl>
2017-08-27 22:05:52 +0200

    mb/lenovo/t400/blc.c: Add B141EW05 V3 to whitelist
    TESTED on Lenovo T400
    
    Change-Id: I365aeb7e997def225c23d3287558bdc4eefa4298
    Signed-off-by: Kevin Keijzer <kevin@quietlife.nl>
    Reviewed-on: https://review.coreboot.org/21230
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/lenovo/t400/blc.c
+++ src/mainboard/lenovo/t400/blc.c
@@ -21,6 +21,7 @@ static const struct blc_pwm_t blc_entries[] = {
 	/* Vendor BIOS seems to intend to set PWM freq at 220Hz */
 	/* T400 */
 	{"B141EW04 V7 ", 110},
+	{"B141EW05 V3 ", 450},
 	{"LP141WX3-TLR1", 110},
 	{"LP141WX5-TLP2", 450},
 	{"LTN141AT12401", 450},

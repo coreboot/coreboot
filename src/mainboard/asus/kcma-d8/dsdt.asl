coreboot/coreboot

commit 714709fde638acdaa31b8e49b1f12c43f3f6b3d6

AMD fam10 ACPI: Use common fixed sleepstates.asl
SSFG was meant to be used as a mask to enable sleepstates
_S1 thru _S4. However as a logical instead of bitwise 'and'
operation was used, all the states were enabled if only
one was marked available.

Note that all boards incorrectly had SSFG == 0x0D that previously
enabled ACPI S3 sleep state even when it was not available.

Change-Id: Ia948becff079383cbf861468da9e8a3ebbf213cb
Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
Reviewed-on: https://review.coreboot.org/21093
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Marc Jones <marc@marcjonesconsulting.com>

src/mainboard/asus/kcma-d8/dsdt.asl
@@ -50,13 +50,6 @@ DefinitionBlock (
 	/* HPET enable */
 	Name (HPTE, 0x1)
 
-	/* Define power states */
-	Name (\_S0, Package () { 0x00, 0x00, 0x00, 0x00 })	/* Normal operation */
-	Name (\_S1, Package () { 0x01, 0x01, 0x00, 0x00 })	/* Standby */
-	Name (\_S3, Package () { 0x03, 0x03, 0x00, 0x00 })	/* Suspend to RAM */
-	Name (\_S4, Package () { 0x04, 0x04, 0x00, 0x00 })	/* Suspend to disk */
-	Name (\_S5, Package () { 0x05, 0x05, 0x00, 0x00 })	/* Hard power off */
-
 	/* The _PIC method is called by the OS to choose between interrupt
 		* routing via the i8259 interrupt controller or the APIC.
 		*

src/mainboard/asus/kgpe-d16/dsdt.asl
@@ -50,13 +50,6 @@ DefinitionBlock (
 	/* HPET enable */
 	Name (HPTE, 0x1)
 
-	/* Define power states */
-	Name (\_S0, Package () { 0x00, 0x00, 0x00, 0x00 })	/* Normal operation */
-	Name (\_S1, Package () { 0x01, 0x01, 0x00, 0x00 })	/* Standby */
-	Name (\_S3, Package () { 0x03, 0x03, 0x00, 0x00 })	/* Suspend to RAM */
-	Name (\_S4, Package () { 0x04, 0x04, 0x00, 0x00 })	/* Suspend to disk */
-	Name (\_S5, Package () { 0x05, 0x05, 0x00, 0x00 })	/* Hard power off */
-
 	/* The _PIC method is called by the OS to choose between interrupt
 		* routing via the i8259 interrupt controller or the APIC.
 		*

src/mainboard/advansus/a785e-i/dsdt.asl
@@ -35,8 +35,6 @@ DefinitionBlock (
 	Name(PCBA, 0xE0000000)	/* Base address of PCIe config space */
 	Name(HPBA, 0xFED00000)	/* Base address of HPET table */
 
-	Name(SSFG, 0x0D)		/* S1 support: bit 0, S2 Support: bit 1, etc. S0 & S5 assumed */
-
 	/* USB overcurrent mapping pins.   */
 	Name(UOM0, 0)
 	Name(UOM1, 2)
@@ -796,27 +794,7 @@ DefinitionBlock (
 
 	}   /* End Scope(_SB)  */
 
-
-	/* Supported sleep states: */
-	Name(\_S0, Package () {0x00, 0x00, 0x00, 0x00} )	/* (S0) - working state */
-
-	If (LAnd(SSFG, 0x01)) {
-		Name(\_S1, Package () {0x01, 0x01, 0x00, 0x00} )	/* (S1) - sleeping w/CPU context */
-	}
-	If (LAnd(SSFG, 0x02)) {
-		Name(\_S2, Package () {0x02, 0x02, 0x00, 0x00} )	/* (S2) - "light" Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x04)) {
-		Name(\_S3, Package () {0x03, 0x03, 0x00, 0x00} )	/* (S3) - Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x08)) {
-		Name(\_S4, Package () {0x04, 0x04, 0x00, 0x00} )	/* (S4) - Suspend to Disk */
-	}
-
-	Name(\_S5, Package () {0x05, 0x05, 0x00, 0x00} )	/* (S5) - Soft Off */
-
-	Name(\_SB.CSPS ,0)				/* Current Sleep State (S0, S1, S2, S3, S4, S5) */
-	Name(CSMS, 0)			/* Current System State */
+	#include <southbridge/amd/common/acpi/sleepstates.asl>
 
 	/* Wake status package */
 	Name(WKST,Package(){Zero, Zero})

src/mainboard/amd/bimini_fam10/dsdt.asl
@@ -35,8 +35,6 @@ DefinitionBlock (
 	Name(PCBA, 0xE0000000)	/* Base address of PCIe config space */
 	Name(HPBA, 0xFED00000)	/* Base address of HPET table */
 
-	Name(SSFG, 0x0D)		/* S1 support: bit 0, S2 Support: bit 1, etc. S0 & S5 assumed */
-
 	/* USB overcurrent mapping pins.   */
 	Name(UOM0, 0)
 	Name(UOM1, 2)
@@ -796,27 +794,7 @@ DefinitionBlock (
 
 	}   /* End Scope(_SB)  */
 
-
-	/* Supported sleep states: */
-	Name(\_S0, Package () {0x00, 0x00, 0x00, 0x00} )	/* (S0) - working state */
-
-	If (LAnd(SSFG, 0x01)) {
-		Name(\_S1, Package () {0x01, 0x01, 0x00, 0x00} )	/* (S1) - sleeping w/CPU context */
-	}
-	If (LAnd(SSFG, 0x02)) {
-		Name(\_S2, Package () {0x02, 0x02, 0x00, 0x00} )	/* (S2) - "light" Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x04)) {
-		Name(\_S3, Package () {0x03, 0x03, 0x00, 0x00} )	/* (S3) - Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x08)) {
-		Name(\_S4, Package () {0x04, 0x04, 0x00, 0x00} )	/* (S4) - Suspend to Disk */
-	}
-
-	Name(\_S5, Package () {0x05, 0x05, 0x00, 0x00} )	/* (S5) - Soft Off */
-
-	Name(\_SB.CSPS ,0)				/* Current Sleep State (S0, S1, S2, S3, S4, S5) */
-	Name(CSMS, 0)			/* Current System State */
+	#include <southbridge/amd/common/acpi/sleepstates.asl>
 
 	/* Wake status package */
 	Name(WKST,Package(){Zero, Zero})

src/mainboard/amd/dbm690t/dsdt.asl
@@ -36,8 +36,6 @@ DefinitionBlock (
 
 	Name(PCBA, 0xE0000000)	/* Base address of PCIe config space */
 
-	Name(SSFG, 0x0D)		/* S1 support: bit 0, S2 Support: bit 1, etc. S0 & S5 assumed */
-
 	/* USB overcurrent mapping pins.   */
 	Name(UOM0, 0)
 	Name(UOM1, 2)
@@ -763,27 +761,7 @@ DefinitionBlock (
 
 	}   /* End Scope(_SB)  */
 
-
-	/* Supported sleep states: */
-	Name(\_S0, Package () {0x00, 0x00, 0x00, 0x00} )	/* (S0) - working state */
-
-	If (LAnd(SSFG, 0x01)) {
-		Name(\_S1, Package () {0x01, 0x01, 0x00, 0x00} )	/* (S1) - sleeping w/CPU context */
-	}
-	If (LAnd(SSFG, 0x02)) {
-		Name(\_S2, Package () {0x02, 0x02, 0x00, 0x00} )	/* (S2) - "light" Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x04)) {
-		Name(\_S3, Package () {0x03, 0x03, 0x00, 0x00} )	/* (S3) - Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x08)) {
-		Name(\_S4, Package () {0x04, 0x04, 0x00, 0x00} )	/* (S4) - Suspend to Disk */
-	}
-
-	Name(\_S5, Package () {0x05, 0x05, 0x00, 0x00} )	/* (S5) - Soft Off */
-
-	Name(\_SB.CSPS ,0)				/* Current Sleep State (S0, S1, S2, S3, S4, S5) */
-	Name(CSMS, 0)			/* Current System State */
+	#include <southbridge/amd/common/acpi/sleepstates.asl>
 
 	/* Wake status package */
 	Name(WKST,Package(){Zero, Zero})

src/mainboard/amd/mahogany/dsdt.asl
@@ -35,8 +35,6 @@ DefinitionBlock (
 	Name(PCBA, 0xE0000000)	/* Base address of PCIe config space */
 	Name(HPBA, 0xFED00000)	/* Base address of HPET table */
 
-	Name(SSFG, 0x0D)		/* S1 support: bit 0, S2 Support: bit 1, etc. S0 & S5 assumed */
-
 	/* USB overcurrent mapping pins.   */
 	Name(UOM0, 0)
 	Name(UOM1, 2)
@@ -759,27 +757,7 @@ DefinitionBlock (
 
 	}   /* End Scope(_SB)  */
 
-
-	/* Supported sleep states: */
-	Name(\_S0, Package () {0x00, 0x00, 0x00, 0x00} )	/* (S0) - working state */
-
-	If (LAnd(SSFG, 0x01)) {
-		Name(\_S1, Package () {0x01, 0x01, 0x00, 0x00} )	/* (S1) - sleeping w/CPU context */
-	}
-	If (LAnd(SSFG, 0x02)) {
-		Name(\_S2, Package () {0x02, 0x02, 0x00, 0x00} )	/* (S2) - "light" Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x04)) {
-		Name(\_S3, Package () {0x03, 0x03, 0x00, 0x00} )	/* (S3) - Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x08)) {
-		Name(\_S4, Package () {0x04, 0x04, 0x00, 0x00} )	/* (S4) - Suspend to Disk */
-	}
-
-	Name(\_S5, Package () {0x05, 0x05, 0x00, 0x00} )	/* (S5) - Soft Off */
-
-	Name(\_SB.CSPS ,0)				/* Current Sleep State (S0, S1, S2, S3, S4, S5) */
-	Name(CSMS, 0)			/* Current System State */
+	#include <southbridge/amd/common/acpi/sleepstates.asl>
 
 	/* Wake status package */
 	Name(WKST,Package(){Zero, Zero})

src/mainboard/amd/mahogany_fam10/dsdt.asl
@@ -35,8 +35,6 @@ DefinitionBlock (
 	Name(PCBA, CONFIG_MMCONF_BASE_ADDRESS)	/* Base address of PCIe config space */
 	Name(HPBA, 0xFED00000)	/* Base address of HPET table */
 
-	Name(SSFG, 0x0D)		/* S1 support: bit 0, S2 Support: bit 1, etc. S0 & S5 assumed */
-
 	/* USB overcurrent mapping pins.   */
 	Name(UOM0, 0)
 	Name(UOM1, 2)
@@ -801,27 +799,7 @@ DefinitionBlock (
 
 	}   /* End Scope(_SB)  */
 
-
-	/* Supported sleep states: */
-	Name(\_S0, Package () {0x00, 0x00, 0x00, 0x00} )	/* (S0) - working state */
-
-	If (LAnd(SSFG, 0x01)) {
-		Name(\_S1, Package () {0x01, 0x01, 0x00, 0x00} )	/* (S1) - sleeping w/CPU context */
-	}
-	If (LAnd(SSFG, 0x02)) {
-		Name(\_S2, Package () {0x02, 0x02, 0x00, 0x00} )	/* (S2) - "light" Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x04)) {
-		Name(\_S3, Package () {0x03, 0x03, 0x00, 0x00} )	/* (S3) - Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x08)) {
-		Name(\_S4, Package () {0x04, 0x04, 0x00, 0x00} )	/* (S4) - Suspend to Disk */
-	}
-
-	Name(\_S5, Package () {0x05, 0x05, 0x00, 0x00} )	/* (S5) - Soft Off */
-
-	Name(\_SB.CSPS ,0)				/* Current Sleep State (S0, S1, S2, S3, S4, S5) */
-	Name(CSMS, 0)			/* Current System State */
+	#include <southbridge/amd/common/acpi/sleepstates.asl>
 
 	/* Wake status package */
 	Name(WKST,Package(){Zero, Zero})

src/mainboard/amd/pistachio/dsdt.asl
@@ -36,8 +36,6 @@ DefinitionBlock (
 
 	Name(PCBA, 0xE0000000)	/* Base address of PCIe config space */
 
-	Name(SSFG, 0x0D)		/* S1 support: bit 0, S2 Support: bit 1, etc. S0 & S5 assumed */
-
 	/* USB overcurrent mapping pins.   */
 	Name(UOM0, 0)
 	Name(UOM1, 2)
@@ -763,27 +761,7 @@ DefinitionBlock (
 
 	}   /* End Scope(_SB)  */
 
-
-	/* Supported sleep states: */
-	Name(\_S0, Package () {0x00, 0x00, 0x00, 0x00} )	/* (S0) - working state */
-
-	If (LAnd(SSFG, 0x01)) {
-		Name(\_S1, Package () {0x01, 0x01, 0x00, 0x00} )	/* (S1) - sleeping w/CPU context */
-	}
-	If (LAnd(SSFG, 0x02)) {
-		Name(\_S2, Package () {0x02, 0x02, 0x00, 0x00} )	/* (S2) - "light" Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x04)) {
-		Name(\_S3, Package () {0x03, 0x03, 0x00, 0x00} )	/* (S3) - Suspend to RAM */
-	}
-	If (LAnd(SSFG, 0x08)) {
-		Name(\_S4, Package () {0x04, 0x04, 0x00, 0x00} )	/* (S4) - Suspend to Disk */
-	}
-
-	Name(\_S5, Package () {0x05, 0x05, 0x00, 0x00} )	/* (S5) - Soft Off */
-
-	Name(\_SB.CSPS ,0)				/* Current Sleep State (S0, S1, S2, S3, S4, S5) */
-	Name(CSMS, 0)			/* Current System State */
+	#include <southbridge/amd/common/acpi/sleepstates.asl>
 
 	/* Wake status package */
 	Name(WKST,Package(){Zero, Zero})

commit a26377b063d9fe33ddf36dec04b7ce7ec9defba5

asus/kcma-d8 kgpe/d16: Fix regression for shutdown
Fix regression caused by commit:

  714709f AMD fam10 ACPI: Use common fixed sleepstates.asl

Adding common sleepstates.asl got lost in rebase process.

Change-Id: I4f22ee950ae5637113db8e79ca238cb1b81002aa
Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
Reviewed-on: https://review.coreboot.org/21413
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Iru Cai <mytbk920423@gmail.com>
Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

src/mainboard/asus/kcma-d8/dsdt.asl
@@ -50,6 +50,8 @@ DefinitionBlock (
 	/* HPET enable */
 	Name (HPTE, 0x1)
 
+	#include <southbridge/amd/common/acpi/sleepstates.asl>
+
 	/* The _PIC method is called by the OS to choose between interrupt
 		* routing via the i8259 interrupt controller or the APIC.
 		*

src/mainboard/asus/kgpe-d16/dsdt.asl
@@ -50,6 +50,8 @@ DefinitionBlock (
 	/* HPET enable */
 	Name (HPTE, 0x1)
 
+	#include <southbridge/amd/common/acpi/sleepstates.asl>
+
 	/* The _PIC method is called by the OS to choose between interrupt
 		* routing via the i8259 interrupt controller or the APIC.
 		*
1d24b9631048d3e130fc72a4def6f6ec61d39fac
Kyösti Mälkki <kyosti.malkki@gmail.com>
2017-09-10 07:27:50 +0300

    amd/inagua: Drop unused Broadcom 5785 support code
    Remove sample configuration code for internal
    Broadcom GbE device in AMD A55E aka Hudson-E1.
    
    Change-Id: Ib0262805aafc62513d9237019ade473cb1efbf1c
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/21478
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/mainboard/amd/inagua/mainboard.c
+++ src/mainboard/amd/inagua/mainboard.c
@@ -19,8 +19,6 @@
 #include <southbridge/amd/sb800/sb800.h>
 #include "SBPLATFORM.h" 	/* Platfrom Specific Definitions */
 
-void broadcom_init(void);
-
 static void init_gpios(void)
 {
 	/**
@@ -68,10 +66,6 @@ static void mainboard_enable(device_t dev)
 	 */
 	pm_iowrite(0x29, 0x80);
 	pm_iowrite(0x28, 0x61);
-
-	/* Upload AMD A55E GbE 'NV'RAM contents.  Still untested on Inagua.
-	 * After anyone can confirm it works please uncomment the call. */
-	//broadcom_init();
 }
 
 struct chip_operations mainboard_ops = {

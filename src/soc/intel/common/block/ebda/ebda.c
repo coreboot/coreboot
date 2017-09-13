ef059c5a092f898f9cecd3f2d94acc263b30b48e
Subrata Banik <subrata.banik@intel.com>
2017-09-13 19:06:28 +0530

    soc/intel/common: Add intel common EBDA support
    This patch provides EBDA library for soc usage.
    
    Change-Id: I8355a1dd528b111f1391e6d28a9b174edddc9ca0
    Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Reviewed-on: https://review.coreboot.org/21538
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/soc/intel/common/block/ebda/ebda.c
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2017 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/ebda.h>
+#include <intelblocks/ebda.h>
+
+/*
+ * Mainboard Override function
+ *
+ * Mainboard directory may implement below functionality for romstage.
+ */
+
+/* Fill up EBDA structure inside Mainboard directory */
+__attribute__((weak)) void create_mainboard_ebda(struct ebda_config *cfg)
+{
+	/* no-op */
+}
+
+static void create_soc_ebda(struct ebda_config *cfg)
+{
+	/* Create EBDA header */
+	cfg->signature = EBDA_SIGNATURE;
+	/* Fill up memory layout information */
+	fill_soc_memmap_ebda(cfg);
+}
+
+void fill_ebda_area(void)
+{
+	struct ebda_config ebda_cfg;
+	struct ebda_config *cfg = &ebda_cfg;
+
+	/* Initialize EBDA area early during romstage. */
+	setup_default_ebda();
+	create_soc_ebda(cfg);
+	create_mainboard_ebda(cfg);
+	write_ebda_data(cfg, sizeof(*cfg));
+}
---
+++ src/soc/intel/common/block/include/intelblocks/ebda.h
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2017 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SOC_INTEL_COMMON_BLOCK_EBDA_H
+#define SOC_INTEL_COMMON_BLOCK_EBDA_H
+
+#include <soc/ebda.h>
+
+#define EBDA_SIGNATURE 0xebda
+
+/*
+ * Mainboard Override function
+ *
+ * Mainboard directory may implement below functionality for romstage.
+ */
+
+/* Fill up EBDA structure inside Mainboard directory */
+void create_mainboard_ebda(struct ebda_config *cfg);
+
+/*
+ * SoC overrides
+ *
+ * All new SoC must implement below functionality for romstage.
+ */
+void fill_soc_memmap_ebda(struct ebda_config *cfg);
+
+/*
+ * API to perform below operation
+ * 1. Initialize EBDA area
+ * 2. Fill up EBDA structure inside SOC directory
+ * 3. Fill up EBDA structure inside Mainboard directory
+ * 4. Store EBDA structure into EBDA area
+ */
+void fill_ebda_area(void);
+
+/*
+ * EBDA structure
+ *
+ * SOC should implement EBDA structure as per need
+ * as below.
+ *
+ * Note: First 4 bytes should be reserved for signature as
+ * 0xEBDA
+ *
+ * struct ebda_config {
+ *	uint32_t signature;
+ *	<Required variables..>
+ * };
+ */
+
+#endif

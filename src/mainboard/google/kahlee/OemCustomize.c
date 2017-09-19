aa51cd5c12596b0b6d5f61eb1108091bf3a4fd3f
Marc Jones <marcj303@gmail.com>
2017-09-18 19:58:47 -0600

    google/kahlee: Prevent AGESA memory clear
    The Linux Pstore area must not be cleared on a reboot. Set the option
    to not clear the memory in AGESA.
    
    BUG=b:64193190
    BRANCH=none
    TEST=Memory clear isn't called in AGESA.
    
    Change-Id: I9b8286ade718fa80bf3badd478ab9a7df643ab98
    Signed-off-by: Marc Jones <marcj303@gmail.com>
    Reviewed-on: https://review.coreboot.org/21596
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/kahlee/OemCustomize.c
+++ src/mainboard/google/kahlee/OemCustomize.c
@@ -36,4 +36,6 @@ void OemPostParams(AMD_POST_PARAMS *PostParams)
 {
 	PostParams->MemConfig.PlatformMemoryConfiguration =
 				(PSO_ENTRY *)DDR4PlatformMemoryConfiguration;
+	/* disable memory clear for pstore memory storage and boot time */
+	PostParams->MemConfig.EnableMemClr = FALSE;
 }

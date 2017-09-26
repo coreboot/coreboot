972d95cd7f9e02059ba3fd3b16f731094cdb51a6
Kyösti Mälkki <kyosti.malkki@gmail.com>
2017-09-26 13:56:30 +0300

    binaryPI: Move agesawrapper.h header
    Change-Id: Ib8d4d3ee490188ce171188d859549da586683e26
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/21695
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- src/northbridge/amd/pi/agesawrapper.h
+++ src/northbridge/amd/pi/agesawrapper.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the coreboot project.
  *
- * Copyright (C) 2012 Advanced Micro Devices, Inc.
+ * Copyright (C) 2011 Advanced Micro Devices, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,5 +13,49 @@
  * GNU General Public License for more details.
  */
 
-/* Use a common header file until references get removed. */
-#include <../agesa/agesawrapper.h>
+#ifndef _AGESAWRAPPER_H_
+#define _AGESAWRAPPER_H_
+
+#if IS_ENABLED(CONFIG_BINARYPI_LEGACY_WRAPPER)
+
+#include <stdint.h>
+#include "Porting.h"
+#include "AGESA.h"
+
+AGESA_STATUS agesawrapper_amdinitreset(void);
+AGESA_STATUS agesawrapper_amdinitearly(void);
+AGESA_STATUS agesawrapper_amdinitenv(void);
+AGESA_STATUS agesawrapper_amdinitlate(void);
+AGESA_STATUS agesawrapper_amdinitpost(void);
+AGESA_STATUS agesawrapper_amdinitmid(void);
+
+AGESA_STATUS agesawrapper_amdinitresume(void);
+AGESA_STATUS agesawrapper_amdS3Save(void);
+AGESA_STATUS agesawrapper_amds3laterestore(void);
+
+AGESA_STATUS agesawrapper_fchs3earlyrestore(void);
+AGESA_STATUS agesawrapper_fchs3laterestore(void);
+
+#define AGESA_EVENTLOG(status, stdheader) \
+	agesawrapper_trace(status, stdheader, __func__)
+
+#else
+
+/* Defined to make unused agesa_main() build. */
+static inline int agesawrapper_amdinitreset(void) { return -1; }
+static inline int agesawrapper_amdinitearly(void) { return -1; }
+static inline int agesawrapper_amdinitenv(void) { return -1; }
+static inline int agesawrapper_amdinitpost(void) { return -1; }
+static inline int agesawrapper_amdinitresume(void) { return -1; }
+static inline int agesawrapper_amds3laterestore(void) { return -1; }
+
+#endif
+
+#if IS_ENABLED(CONFIG_BINARYPI_LEGACY_WRAPPER)
+const void *agesawrapper_locate_module (const CHAR8 name[8]);
+
+VOID OemCustomizeInitEarly (IN OUT AMD_EARLY_PARAMS *InitEarly);
+void OemPostParams(AMD_POST_PARAMS *PostParams);
+#endif
+
+#endif /* _AGESAWRAPPER_H_ */

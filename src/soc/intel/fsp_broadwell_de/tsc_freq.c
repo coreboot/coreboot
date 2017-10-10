0ccc3c49e47d46b6f2ad8d93182414850284ae70
Werner Zeh <werner.zeh@siemens.com>
2017-10-10 12:29:53 +0200

    intel/fsp_broadwell_de: Add timestamp functionality
    Add a little code to enable timestamps on FSP based implementation
    of Broadwell-DE. I have tested it by reading back the timestamps
    with cbmem utility once the board has booted into Lubuntu.
    
    Change-Id: Idaa65a22a00382bf0c37acf2f5a1e07c6b1b42d9
    Signed-off-by: Werner Zeh <werner.zeh@siemens.com>
    Reviewed-on: https://review.coreboot.org/21932
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/fsp_broadwell_de/tsc_freq.c
+++ src/soc/intel/fsp_broadwell_de/tsc_freq.c
@@ -1,9 +1,7 @@
 /*
  * This file is part of the coreboot project.
  *
- * Copyright (C) 2013 Google, Inc.
- * Copyright (C) 2015-2016 Intel Corp.
- * Copyright (C) 2017 Siemens AG
+ * Copyright (C) 2014 Google Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,18 +13,16 @@
  * GNU General Public License for more details.
  */
 
-#ifndef _SOC_BROADWELL_DE_H_
-#define _SOC_BROADWELL_DE_H_
+#include <stdint.h>
+#include <cpu/x86/msr.h>
+#include <cpu/x86/tsc.h>
+#include <soc/msr.h>
+#include <soc/broadwell_de.h>
 
-#define VTBAR_OFFSET		0x180
-#define VTBAR_MASK		0xffffe000
+unsigned long tsc_freq_mhz(void)
+{
+	msr_t platform_info;
 
-#define SMM_FEATURE_CONTROL	0x58
-#define  SMM_CPU_SAVE_EN	(1 << 1)
-#define TSEG_BASE		0xa8	/* TSEG base */
-#define TSEG_LIMIT		0xac	/* TSEG limit */
-
-/* CPU bus clock is fixed at 100MHz */
-#define CPU_BCLK		100
-
-#endif /* _SOC_BROADWELL_DE_H_ */
+	platform_info = rdmsr(MSR_PLATFORM_INFO);
+	return CPU_BCLK * ((platform_info.lo >> 8) & 0xff);
+}

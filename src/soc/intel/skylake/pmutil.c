3efeeb2c8f64ce741c49745bf237528744c0b2f5
Furquan Shaikh <furquan@chromium.org>
2017-10-14 19:24:43 -0700

    soc/intel/skylake: Move power_state functions to pmutil.c
    This change moves soc_fill_power_state and soc_prev_sleep_state to
    pmutil.c. It allows the functions to be used across romstage and smm.
    
    BUG=b:67874513
    
    Change-Id: I375ac029520c2cdd52926f3ab3c2d5559936dd8c
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://review.coreboot.org/22085
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/skylake/pmutil.c
+++ src/soc/intel/skylake/pmutil.c
@@ -21,6 +21,7 @@
 
 #define __SIMPLE_DEVICE__
 
+#include <arch/acpi.h>
 #include <arch/io.h>
 #include <device/device.h>
 #include <device/pci.h>
@@ -246,4 +247,71 @@ int rtc_failure(void)
 int vbnv_cmos_failed(void)
 {
 	return rtc_failure();
+}
+
+/* Return 0, 3, or 5 to indicate the previous sleep state. */
+int soc_prev_sleep_state(const struct chipset_power_state *ps,
+						int prev_sleep_state)
+{
+	/*
+	 * Check for any power failure to determine if this a wake from
+	 * S5 because the PCH does not set the WAK_STS bit when waking
+	 * from a true G3 state.
+	 */
+	if (!(ps->pm1_sts & WAK_STS) &&
+	    (ps->gen_pmcon_b & (PWR_FLR | SUS_PWR_FLR)))
+		prev_sleep_state = ACPI_S5;
+
+	/*
+	 * If waking from S3 determine if deep S3 is enabled. If not,
+	 * need to check both deep sleep well and normal suspend well.
+	 * Otherwise just check deep sleep well.
+	 */
+	if (prev_sleep_state == ACPI_S3) {
+		/* PWR_FLR represents deep sleep power well loss. */
+		uint32_t mask = PWR_FLR;
+
+		/* If deep s3 isn't enabled check the suspend well too. */
+		if (!deep_s3_enabled())
+			mask |= SUS_PWR_FLR;
+
+		if (ps->gen_pmcon_b & mask)
+			prev_sleep_state = ACPI_S5;
+	}
+	return prev_sleep_state;
+}
+
+void soc_fill_power_state(struct chipset_power_state *ps)
+{
+	uint16_t tcobase;
+	uint8_t *pmc;
+
+	tcobase = smbus_tco_regs();
+
+	ps->tco1_sts = inw(tcobase + TCO1_STS);
+	ps->tco2_sts = inw(tcobase + TCO2_STS);
+
+	printk(BIOS_DEBUG, "TCO_STS:   %04x %04x\n",
+	       ps->tco1_sts, ps->tco2_sts);
+
+	ps->gen_pmcon_a = pci_read_config32(PCH_DEV_PMC, GEN_PMCON_A);
+	ps->gen_pmcon_b = pci_read_config32(PCH_DEV_PMC, GEN_PMCON_B);
+
+	pmc = pmc_mmio_regs();
+	ps->gblrst_cause[0] = read32(pmc + GBLRST_CAUSE0);
+	ps->gblrst_cause[1] = read32(pmc + GBLRST_CAUSE1);
+
+	printk(BIOS_DEBUG, "GEN_PMCON: %08x %08x\n",
+	       ps->gen_pmcon_a, ps->gen_pmcon_b);
+
+	printk(BIOS_DEBUG, "GBLRST_CAUSE: %08x %08x\n",
+	       ps->gblrst_cause[0], ps->gblrst_cause[1]);
+}
+
+int acpi_get_sleep_type(void)
+{
+	struct chipset_power_state *ps;
+
+	ps = pmc_get_power_state();
+	return ps->prev_sleep_state;
 }

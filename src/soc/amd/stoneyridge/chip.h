de897a6dba1bc6ce157aed8c00cc20642c5d6c59
Martin Roth <martinroth@google.com>
2017-10-16 11:06:44 -0600

    soc/amd/stoneyridge: clean up chip.h
    Remove obsolete register entries.
    
    BUG=None
    TEST=build
    
    Change-Id: Ia9beb9d42f0ee5d63d9e9073507fc606a9d45c46
    Signed-off-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://review.coreboot.org/22050
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/amd/stoneyridge/chip.h
+++ src/soc/amd/stoneyridge/chip.h
@@ -20,12 +20,6 @@
 
 struct soc_amd_stoneyridge_config {
 	u8 spdAddrLookup[1][1][2];
-	u32 ide0_enable : 1;
-	u32 sata0_enable : 1;
-	u32 boot_switch_sata_ide : 1;
-	u32 hda_viddid;
-	u8  gpp_configuration;
-	u8  sd_mode;
 };
 
 typedef struct soc_amd_stoneyridge_config config_t;

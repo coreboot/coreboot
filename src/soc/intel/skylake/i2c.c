ebd7c44d7b0b3f44fd0f5b5351ee7e8169a31312
Lijian Zhao <lijian.zhao@intel.com>
2017-11-01 10:40:02 -0700

    soc/intel/skylake: Update coding style for i2c
    From comment from https://review.coreboot.org/#/c/22238/, the coding
    style need to be update.
    
    TEST=N/A
    
    Change-Id: Id022648951c0f11216aa32f422b5095476f82f8c
    Signed-off-by: Lijian Zhao <lijian.zhao@intel.com>
    Reviewed-on: https://review.coreboot.org/22278
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/soc/intel/skylake/i2c.c
+++ src/soc/intel/skylake/i2c.c
@@ -43,12 +43,18 @@ uintptr_t i2c_get_soc_early_base(unsigned int bus)
 int i2c_soc_devfn_to_bus(unsigned int devfn)
 {
 	switch (devfn) {
-	case PCH_DEVFN_I2C0: return 0;
-	case PCH_DEVFN_I2C1: return 1;
-	case PCH_DEVFN_I2C2: return 2;
-	case PCH_DEVFN_I2C3: return 3;
-	case PCH_DEVFN_I2C4: return 4;
-	case PCH_DEVFN_I2C5: return 5;
+	case PCH_DEVFN_I2C0:
+		return 0;
+	case PCH_DEVFN_I2C1:
+		return 1;
+	case PCH_DEVFN_I2C2:
+		return 2;
+	case PCH_DEVFN_I2C3:
+		return 3;
+	case PCH_DEVFN_I2C4:
+		return 4;
+	case PCH_DEVFN_I2C5:
+		return 5;
 	}
 	return -1;
 }
@@ -56,12 +62,18 @@ int i2c_soc_devfn_to_bus(unsigned int devfn)
 int i2c_soc_bus_to_devfn(unsigned int bus)
 {
 	switch (bus) {
-	case 0: return PCH_DEVFN_I2C0;
-	case 1: return PCH_DEVFN_I2C1;
-	case 2: return PCH_DEVFN_I2C2;
-	case 3: return PCH_DEVFN_I2C3;
-	case 4: return PCH_DEVFN_I2C4;
-	case 5: return PCH_DEVFN_I2C5;
+	case 0:
+		return PCH_DEVFN_I2C0;
+	case 1:
+		return PCH_DEVFN_I2C1;
+	case 2:
+		return PCH_DEVFN_I2C2;
+	case 3:
+		return PCH_DEVFN_I2C3;
+	case 4:
+		return PCH_DEVFN_I2C4;
+	case 5:
+		return PCH_DEVFN_I2C5;
 	}
 	return -1;
 }

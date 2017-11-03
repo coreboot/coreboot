coreboot/coreboot

commit 4a51ea8470eecabe401b22142fd1bbcb263953f7

google/kahlee: Add ASL for Elan touchpad
Add ASL for the Elan touchpad driver connection in ChromeOS.
This is based on the Auron and Rambi ASL. The AMD ACPI code
doesn't have the auto table generation the newer Intel
Chrome SOC use.

Device visible to OS: /sys/bus/acpi/devices/ELAN0000

Change-Id: Id3fc8c8855b0296f43a502e81143498d663468ec
Signed-off-by: Ivy Jian <ivy_jian@compal.com>
Signed-off-by: Marc Jones <marcj303@gmail.com>
Reviewed-on: https://review.coreboot.org/19844
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Aaron Durbin <adurbin@chromium.org>

src/mainboard/google/kahlee/acpi/mainboard.asl
@@ -34,3 +34,27 @@ Name (SSFG, 0x0D)
 Name (OSVR, 3)		/* WinXp = 1, Vista = 2, Linux = 3, WinCE = 4 */
 Name (OSV, Ones)	/* Assume nothing */
 Name (PMOD, One)	/* Assume APIC */
+
+Device (ETPA)
+{
+	Name (_HID, "ELAN0000")
+	Name (_DDN, "Elan Touchpad")
+	Name (_UID, 1)
+	Name (ISTP, 1)	/* Touchpad */
+
+	Name (_CRS, ResourceTemplate()
+	{
+		I2cSerialBus (
+			0x15,			/* SlaveAddress */
+			ControllerInitiated,	/* SlaveMode */
+			400000,			/* ConnectionSpeed */
+			AddressingMode7Bit,	/* AddressingMode */
+			"\\_SB.I2CD",		/* ResourceSource */
+		)
+		GpioInt (Level, ActiveLow, ExclusiveAndWake, PullNone,,
+			"\\_SB.GPIO") { 0x5 }
+	})
+
+	/* Allow device to power off in S0 */
+	Name (_S0W, 3)
+}

commit 427feecbf0003d968ac804edaac7cb25ac67b397

google/kahlee/acpi: Serialize method _CRS
ASL+ Optimizing Compiler/Disassembler version 20170831 shows the remark
below.

```
dsdt.aml     87:  Method (_CRS, 0x0, NotSerialized)
Remark   2120 -             ^ Control Method should be made Serialized \
        (due to creation of named objects within)
```

So, serialize the method.

Fixes: commit 4a51ea8470 (google/kahlee: Add ASL for Elan touchpad)
Change-Id: I664f493318cbfd80d91565c0d29ec918278c4906
Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>
Reviewed-on: https://review.coreboot.org/21901
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Patrick Rudolph <siro@das-labor.org>
Reviewed-by: Marc Jones <marc@marcjonesconsulting.com>
Reviewed-by: Martin Roth <martinroth@google.com>

src/mainboard/google/kahlee/acpi/mainboard.asl
@@ -72,7 +72,7 @@ Device (RTEK)   /* Audio Codec driver I2CS*/
 		Name (_CID, "AMDI1002")
 	}
 
-	Method (_CRS, 0x0, NotSerialized)
+	Method (_CRS, 0x0, Serialized)
 	{
 		Name (SBUF, ResourceTemplate ()
 		{
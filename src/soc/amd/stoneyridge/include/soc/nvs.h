coreboot/coreboot

commit 90ebf96df59bfe9d60721ac881c07c00df29c418

soc/intel/skylake: Add GNVS variables and include SGX ASL
- Add GNVS variables for SGX
- Include SGX ASL if CONFIG_SOC_INTEL_COMMON_BLOCK_SGX is set
- With this patch SGX ACPI device would get created and kernel SGX
  driver would let loaded

Change-Id: Ie95eb79a01e1c0005e0f137b015b7fe000c1ab2a
Signed-off-by: Pratik Prajapati <pratikkumar.v.prajapati@intel.com>
Reviewed-on: https://review.coreboot.org/21971
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Aaron Durbin <adurbin@chromium.org>

src/soc/intel/skylake/include/soc/nvs.h
@@ -58,6 +58,9 @@ typedef struct {
 	u16	u2we; /* 0x3f - USB2 Wake Enable Bitmap */
 	u8	u3we; /* 0x41 - USB3 Wake Enable Bitmap */
 	u8	uior; /* 0x42 - UART debug controller init on S3 resume */
+	u8	ecps; /* 0x43 - SGX Enabled status */
+	u64	emna; /* 0x44 - 0x4B EPC base address */
+	u64	elng; /* 0x4C - 0x53 EPC Length */
 	u8	unused[189];
 
 	/* ChromeOS specific (0x100 - 0xfff) */

src/soc/intel/skylake/acpi/globalnvs.asl
@@ -67,6 +67,9 @@ Field (GNVS, ByteAcc, NoLock, Preserve)
 	U2WE,	16,	// 0x3f - USB2 Wake Enable Bitmap
 	U3WE,	8,	// 0x41 - USB3 Wake Enable Bitmap
 	UIOR,	8,	// 0x42 - UART debug controller init on S3 resume
+	EPCS,	8,	// 0x43 - SGX Enabled status
+	EMNA,	64,	// 0x44 - 0x4B EPC base address
+	ELNG,	64,	// 0x4C - 0x53 EPC Length
 
 	/* ChromeOS specific */
 	Offset (0x100),

src/soc/intel/skylake/acpi/pch.asl
@@ -72,3 +72,8 @@ Method (_OSC, 4)
 		Return (Arg3)
 	}
 }
+
+/* SGX */
+#if IS_ENABLED(CONFIG_SOC_INTEL_COMMON_BLOCK_SGX)
+#include <soc/intel/common/acpi/sgx.asl>
+#endif

commit 0781cbe1d33e62a26a234962bc271209cc22c931

sb and soc: Enforce correct offset of member "chromeos" in global_nvs_t
The padding has recently been broken in commit 90ebf96df5
("soc/intel/skylake: Add GNVS variables and include SGX ASL") and fixed
again in commit af88398887 ("soc/intel/skylake: Fix broken GNVS offset
for chromeos").  Avoid this bug in the future.

Change-Id: I1bf3027bba239c8747ad26a3130a7e047d3b8c94
Signed-off-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>
Reviewed-on: https://review.coreboot.org/22229
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Aaron Durbin <adurbin@chromium.org>
Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

src/soc/amd/stoneyridge/include/soc/nvs.h
@@ -24,6 +24,7 @@
 #ifndef __SOC_STONEYRIDGE_NVS_H__
 #define __SOC_STONEYRIDGE_NVS_H__
 
+#include <commonlib/helpers.h>
 #include <compiler.h>
 #include <stdint.h>
 #include <vendorcode/google/chromeos/gnvs.h>
@@ -48,5 +49,6 @@ typedef struct global_nvs_t {
 	/* ChromeOS specific (0x100 - 0xfff) */
 	chromeos_acpi_t chromeos;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 #endif /* __SOC_STONEYRIDGE_NVS_H__ */

src/soc/intel/apollolake/include/soc/nvs.h
@@ -24,6 +24,7 @@
 #ifndef _SOC_APOLLOLAKE_NVS_H_
 #define _SOC_APOLLOLAKE_NVS_H_
 
+#include <commonlib/helpers.h>
 #include <compiler.h>
 #include <vendorcode/google/chromeos/gnvs.h>
 
@@ -52,5 +53,6 @@ typedef struct global_nvs_t {
 	/* ChromeOS specific (0x100 - 0xfff) */
 	chromeos_acpi_t chromeos;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 #endif	/* _SOC_APOLLOLAKE_NVS_H_ */

src/soc/intel/baytrail/include/soc/nvs.h
@@ -17,11 +17,12 @@
 #ifndef _BAYTRAIL_NVS_H_
 #define _BAYTRAIL_NVS_H_
 
+#include <commonlib/helpers.h>
 #include <compiler.h>
 #include <vendorcode/google/chromeos/gnvs.h>
 #include <soc/device_nvs.h>
 
-typedef struct {
+typedef struct global_nvs_t {
 	/* Miscellaneous */
 	u16	osys; /* 0x00 - Operating System */
 	u8	smif; /* 0x02 - SMI function call ("TRAP") */
@@ -66,6 +67,7 @@ typedef struct {
 	/* Baytrail LPSS (0x1000) */
 	device_nvs_t dev;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 void acpi_create_gnvs(global_nvs_t *gnvs);
 #ifdef __SMM__

src/soc/intel/braswell/include/soc/nvs.h
@@ -18,12 +18,13 @@
 #ifndef _SOC_NVS_H_
 #define _SOC_NVS_H_
 
-#include <rules.h>
+#include <commonlib/helpers.h>
 #include <compiler.h>
-#include <vendorcode/google/chromeos/gnvs.h>
+#include <rules.h>
 #include <soc/device_nvs.h>
+#include <vendorcode/google/chromeos/gnvs.h>
 
-typedef struct {
+typedef struct global_nvs_t {
 	/* Miscellaneous */
 	u16	osys; /* 0x00 - Operating System */
 	u8	smif; /* 0x02 - SMI function call ("TRAP") */
@@ -70,6 +71,7 @@ typedef struct {
 	/* LPSS (0x1000) */
 	device_nvs_t dev;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 void acpi_create_gnvs(global_nvs_t *gnvs);
 #if ENV_SMM

src/soc/intel/broadwell/include/soc/nvs.h
@@ -17,11 +17,12 @@
 #ifndef _BROADWELL_NVS_H_
 #define _BROADWELL_NVS_H_
 
+#include <commonlib/helpers.h>
 #include <compiler.h>
-#include <vendorcode/google/chromeos/gnvs.h>
 #include <soc/device_nvs.h>
+#include <vendorcode/google/chromeos/gnvs.h>
 
-typedef struct {
+typedef struct global_nvs_t {
 	/* Miscellaneous */
 	u16	osys; /* 0x00 - Operating System */
 	u8	smif; /* 0x02 - SMI function call ("TRAP") */
@@ -58,6 +59,7 @@ typedef struct {
 	/* Device specific (0x1000) */
 	device_nvs_t dev;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 void acpi_create_gnvs(global_nvs_t *gnvs);
 #ifdef __SMM__

src/soc/intel/cannonlake/include/soc/nvs.h
@@ -18,6 +18,7 @@
 #ifndef _SOC_NVS_H_
 #define _SOC_NVS_H_
 
+#include <commonlib/helpers.h>
 #include <compiler.h>
 #include <vendorcode/google/chromeos/gnvs.h>
 
@@ -45,6 +46,7 @@ typedef struct global_nvs_t {
 	/* ChromeOS specific (0x100 - 0xfff) */
 	chromeos_acpi_t chromeos;
 }  __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 
 #endif

src/soc/intel/skylake/include/soc/nvs.h
@@ -18,11 +18,12 @@
 #ifndef _SOC_NVS_H_
 #define _SOC_NVS_H_
 
-#include <rules.h>
+#include <commonlib/helpers.h>
 #include <compiler.h>
+#include <rules.h>
 #include <vendorcode/google/chromeos/gnvs.h>
 
-typedef struct {
+typedef struct global_nvs_t {
 	/* Miscellaneous */
 	u16	osys; /* 0x00 - Operating System */
 	u8	smif; /* 0x02 - SMI function call ("TRAP") */
@@ -66,6 +67,7 @@ typedef struct {
 	/* ChromeOS specific (0x100 - 0xfff) */
 	chromeos_acpi_t chromeos;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 #if ENV_SMM
 /* Used in SMM to find the ACPI GNVS address */

src/southbridge/intel/bd82x6x/nvs.h
@@ -14,10 +14,12 @@
  * GNU General Public License for more details.
  */
 
-#include <stdint.h>
+#include <commonlib/helpers.h>
 #include <compiler.h>
+#include <stdint.h>
 #include "vendorcode/google/chromeos/gnvs.h"
-typedef struct {
+
+typedef struct global_nvs_t {
 	/* Miscellaneous */
 	u16	osys; /* 0x00 - Operating System */
 	u8	smif; /* 0x02 - SMI function call ("TRAP") */
@@ -152,6 +154,7 @@ typedef struct {
 	/* ChromeOS specific (starts at 0x100)*/
 	chromeos_acpi_t chromeos;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 #ifdef __SMM__
 /* Used in SMM to find the ACPI GNVS address */

src/southbridge/intel/fsp_bd82x6x/nvs.h
@@ -14,9 +14,11 @@
  * GNU General Public License for more details.
  */
 
+#include <commonlib/helpers.h>
 #include <compiler.h>
 #include "vendorcode/google/chromeos/gnvs.h"
-typedef struct {
+
+typedef struct global_nvs_t {
 	/* Miscellaneous */
 	u16	osys; /* 0x00 - Operating System */
 	u8	smif; /* 0x02 - SMI function call ("TRAP") */
@@ -148,6 +150,7 @@ typedef struct {
 	/* ChromeOS specific (starts at 0x100)*/
 	chromeos_acpi_t chromeos;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 #ifdef __SMM__
 /* Used in SMM to find the ACPI GNVS address */

src/southbridge/intel/fsp_i89xx/nvs.h
@@ -14,9 +14,11 @@
  * GNU General Public License for more details.
  */
 
+#include <commonlib/helpers.h>
 #include <compiler.h>
 #include "vendorcode/google/chromeos/gnvs.h"
-typedef struct {
+
+typedef struct global_nvs_t {
 	/* Miscellaneous */
 	u16	osys; /* 0x00 - Operating System */
 	u8	smif; /* 0x02 - SMI function call ("TRAP") */
@@ -148,6 +150,7 @@ typedef struct {
 	/* ChromeOS specific (starts at 0x100)*/
 	chromeos_acpi_t chromeos;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 #ifdef __SMM__
 /* Used in SMM to find the ACPI GNVS address */

src/southbridge/intel/ibexpeak/nvs.h
@@ -14,9 +14,11 @@
  * GNU General Public License for more details.
  */
 
+#include <commonlib/helpers.h>
 #include <compiler.h>
 #include "vendorcode/google/chromeos/gnvs.h"
-typedef struct {
+
+typedef struct global_nvs_t {
 	/* Miscellaneous */
 	u16	osys; /* 0x00 - Operating System */
 	u8	smif; /* 0x02 - SMI function call ("TRAP") */
@@ -150,6 +152,7 @@ typedef struct {
 	/* ChromeOS specific (starts at 0x100)*/
 	chromeos_acpi_t chromeos;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 #ifdef __SMM__
 /* Used in SMM to find the ACPI GNVS address */

src/southbridge/intel/lynxpoint/nvs.h
@@ -14,9 +14,11 @@
  * GNU General Public License for more details.
  */
 
+#include <commonlib/helpers.h>
 #include <compiler.h>
 #include "vendorcode/google/chromeos/gnvs.h"
-typedef struct {
+
+typedef struct global_nvs_t {
 	/* Miscellaneous */
 	u16	osys; /* 0x00 - Operating System */
 	u8	smif; /* 0x02 - SMI function call ("TRAP") */
@@ -126,6 +128,7 @@ typedef struct {
 	/* ChromeOS specific (starts at 0x100)*/
 	chromeos_acpi_t chromeos;
 } __packed global_nvs_t;
+check_member(global_nvs_t, chromeos, 0x100);
 
 #ifdef __SMM__
 /* Used in SMM to find the ACPI GNVS address */
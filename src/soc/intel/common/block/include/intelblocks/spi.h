6c4b5916fcd4844410a709320ab7bf5a06a45596
Subrata Banik <subrata.banik@intel.com>
2017-11-07 16:10:05 +0530

    soc/intel/common/block: Add Intel common SPI support
    SOC need to select specific macros need to compile
    common SPI code.
    
    Change-Id: I82f7d1852d12ca37f386b64a613a676753da959c
    Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Reviewed-on: https://review.coreboot.org/22360
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/soc/intel/common/block/include/intelblocks/spi.h
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2017 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SOC_INTEL_COMMON_BLOCK_SPI_H
+#define SOC_INTEL_COMMON_BLOCK_SPI_H
+
+/*
+ * SoC overrides
+ *
+ * All new SoC must implement below functionality.
+ */
+
+/* Function to convert input device function to bus number
+ * Input: Device Function number
+ * Output: -1 translate to Error, >=0 is bus number
+ */
+int spi_soc_devfn_to_bus(unsigned int devfn);
+
+/* Function to convert input bus number to device function
+ * Input: Bus number
+ * Output: -1 translate to Error, >=0 is function number
+ */
+int spi_soc_bus_to_devfn(unsigned int bus);
+
+#endif	/* SOC_INTEL_COMMON_BLOCK_SPI_H */

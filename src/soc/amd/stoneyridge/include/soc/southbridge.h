3e4aeec5f45b741158770c6dc6ea0c47404d35ff
Richard Spiegel <richard.spiegel@silverbackltd.com>
2017-11-08 10:57:24 -0700

    src/soc/amd/stoneyridge/southbridge.h: Remove unused prototypes
    Remove unused s3_resume_init_data prototype from southbridge,h
    
    BUG=b:68007655
    
    Change-Id: If022f873813070aac6cc9090c2212178a4e66354
    Signed-off-by: Richard Spiegel <richard.spiegel@silverbackltd.com>
    Reviewed-on: https://review.coreboot.org/22376
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/amd/stoneyridge/include/soc/southbridge.h
+++ src/soc/amd/stoneyridge/include/soc/southbridge.h
@@ -263,7 +263,6 @@ void smi_write16(u8 reg, u16 value);
 void smi_write32(u8 reg, u32 value);
 uint16_t pm_acpi_pm_cnt_blk(void);
 int s3_load_nvram_early(int size, u32 *old_dword, int nvram_pos);
-void s3_resume_init_data(void *FchParams);
 int s3_save_nvram_early(u32 dword, int size, int  nvram_pos);
 void bootblock_fch_early_init(void);
 

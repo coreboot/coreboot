3417ee9a3fc4fcb6e758962781c9b8ff0a353f6d
Naveen Manohar <naveen.m@intel.com>
2017-11-14 00:35:21 +0530

    mb/google/poppy/variants/nautilus: add SPK DMIC nhlt support
    Nautilus board uses max989357a speaker codec and 4CH DMIC.
    Select the appropriate NHLT blob to be packaged in CBFS.
    Also generate the required ACPI NHLT table for codec
    and the supported topology in nautilus.
    
    BUG=b:68686020
    TEST=With the required driver support in kernel verify that
    the Audio plays on Speaker and captures on 4CH DMIC
    
    Change-Id: Ie90af02e0935029f53f9020bd78027b6eb31a187
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>
    Reviewed-on: https://review.coreboot.org/22457
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/mainboard/google/poppy/variants/nautilus/nhlt.c
+++ src/mainboard/google/poppy/variants/nautilus/nhlt.c
@@ -20,7 +20,16 @@
 
 void variant_nhlt_init(struct nhlt *nhlt)
 {
+	/* 4 Channel DMIC array. */
+	if (nhlt_soc_add_dmic_array(nhlt, 4))
+		printk(BIOS_ERR, "Couldn't add 4CH DMIC array.\n");
+
 	/* Dialog DA7219 Headset codec. */
 	if (nhlt_soc_add_da7219(nhlt, AUDIO_LINK_SSP1))
 		printk(BIOS_ERR, "Couldn't add Dialog DA7219.\n");
+
+	/* MAXIM Smart Amps for left and right speakers. */
+	if (nhlt_soc_add_max98357(nhlt, AUDIO_LINK_SSP0))
+		printk(BIOS_ERR, "Couldn't add  Maxim_98357 codec.\n");
+
 }

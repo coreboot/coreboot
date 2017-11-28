4a1ee4b53ec2eed6c9e8129381982c300fa87031
N, Harshapriya <harshapriya.n@intel.com>
2017-11-28 14:29:26 -0800

    mainboard/intel/cannonlake_rvp: Add support for MAX98373 speaker amp
    Add NHLT and dt support for max98373 amp
    
    BUG=None
    TEST=check SSDT and verify entries for max98373
    TEST=check NHLT ACPI tables included blobs for max98373
    
    Change-Id: I0b402f89f1ece9e62a394f713c4b0feff29bd1e5
    Signed-off-by: N, Harshapriya <harshapriya.n@intel.com>
    Signed-off-by: Sathyanarayana Nujella <sathyanarayana.nujella@intel.com>
    Reviewed-on: https://review.coreboot.org/22674
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/mainboard/intel/cannonlake_rvp/variants/baseboard/nhlt.c
+++ src/mainboard/intel/cannonlake_rvp/variants/baseboard/nhlt.c
@@ -33,16 +33,21 @@ void __attribute__((weak)) variant_nhlt_init(struct nhlt *nhlt)
 		!nhlt_soc_add_dmic_array(nhlt, 4))
 		printk(BIOS_ERR, "Added 4CH DMIC array.\n");
 
-#if IS_ENABLED(CONFIG_INCLUDE_SND_MAX98357_DA7219_NHLT)
-	/* Dialog for Headset codec.
-	 * Headset codec is bi-directional but uses the same configuration
-	 * settings for render and capture endpoints.
-	 */
-	if (!nhlt_soc_add_da7219(nhlt, AUDIO_LINK_SSP2))
-		printk(BIOS_ERR, "Added Dialog_7219 codec.\n");
+	if (IS_ENABLED(CONFIG_INCLUDE_SND_MAX98357_DA7219_NHLT))
+	{
+		/* Dialog for Headset codec.
+		 * Headset codec is bi-directional but uses the same configuration
+		 * settings for render and capture endpoints.
+		 */
+		if (!nhlt_soc_add_da7219(nhlt, AUDIO_LINK_SSP2))
+			printk(BIOS_ERR, "Added Dialog_7219 codec.\n");
 
-	/* MAXIM Smart Amps for left and right speakers. */
-	if (!nhlt_soc_add_max98357(nhlt, AUDIO_LINK_SSP1))
-		printk(BIOS_ERR, "Added Maxim_98357 codec.\n");
-#endif
+		/* MAXIM Smart Amps for left and right speakers. */
+		if (!nhlt_soc_add_max98357(nhlt, AUDIO_LINK_SSP1))
+			printk(BIOS_ERR, "Added Maxim_98357 codec.\n");
+	}
+
+	if (IS_ENABLED(CONFIG_INCLUDE_SND_MAX98373_NHLT) &&
+		!nhlt_soc_add_max98373(nhlt, AUDIO_LINK_SSP1))
+		printk(BIOS_ERR, "Added Maxim_98373 codec.\n");
 }
--- src/soc/intel/cannonlake/nhlt.c
+++ src/soc/intel/cannonlake/nhlt.c
@@ -180,6 +180,60 @@ static const struct nhlt_endp_descriptor max98357_descriptors[] = {
 	},
 };
 
+static const struct nhlt_format_config max98373_render_formats[] = {
+	/* 48 KHz 24-bits per sample. */
+	{
+		.num_channels = 2,
+		.sample_freq_khz = 48,
+		.container_bits_per_sample = 32,
+		.valid_bits_per_sample = 24,
+		.speaker_mask = SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT,
+		.settings_file = "max98373-render-2ch-48khz-24b.bin",
+	},
+	/* 48 KHz 16-bits per sample. */
+	{
+		.num_channels = 2,
+		.sample_freq_khz = 48,
+		.container_bits_per_sample = 16,
+		.valid_bits_per_sample = 16,
+		.speaker_mask = SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT,
+		.settings_file = "max98373-render-2ch-48khz-16b.bin",
+	}
+};
+
+static const struct nhlt_format_config max98373_capture_formats[] = {
+	/* 48 KHz 16-bits per sample. */
+	{
+		.num_channels = 2,
+		.sample_freq_khz = 48,
+		.container_bits_per_sample = 16,
+		.valid_bits_per_sample = 16,
+		.speaker_mask = SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT,
+		.settings_file = "max98373-render-2ch-48khz-16b.bin",
+	},
+};
+
+static const struct nhlt_endp_descriptor max98373_descriptors[] = {
+	{
+		.link = NHLT_LINK_SSP,
+		.device = NHLT_SSP_DEV_I2S,
+		.direction = NHLT_DIR_RENDER,
+		.vid = NHLT_VID,
+		.did = NHLT_DID_SSP,
+		.formats = max98373_render_formats,
+		.num_formats = ARRAY_SIZE(max98373_render_formats),
+	},
+	{
+		.link = NHLT_LINK_SSP,
+		.device = NHLT_SSP_DEV_I2S,
+		.direction = NHLT_DIR_CAPTURE,
+		.vid = NHLT_VID,
+		.did = NHLT_DID_SSP,
+		.formats = max98373_capture_formats,
+		.num_formats = ARRAY_SIZE(max98373_capture_formats),
+       },
+};
+
 int nhlt_soc_add_dmic_array(struct nhlt *nhlt, int num_channels)
 {
 	switch (num_channels) {
@@ -209,4 +263,11 @@ int nhlt_soc_add_max98357(struct nhlt *nhlt, int hwlink)
 	/* Virtual bus id of SSP links are the hardware port ids proper. */
 	return nhlt_add_ssp_endpoints(nhlt, hwlink, max98357_descriptors,
 					ARRAY_SIZE(max98357_descriptors));
+}
+
+int nhlt_soc_add_max98373(struct nhlt *nhlt, int hwlink)
+{
+	/* Virtual bus id of SSP links are the hardware port ids proper. */
+	return nhlt_add_ssp_endpoints(nhlt, hwlink, max98373_descriptors,
+					ARRAY_SIZE(max98373_descriptors));
 }

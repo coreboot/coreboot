2ca4ca3f2192f067680ad9f3d99917658de74045
Alex Thiessen <alex.thiessen.de+coreboot@gmail.com>
2018-01-13 18:48:38 +0000

    util/gitconfig: Fix too long lines in gitconfig.sh
    Change-Id: Iaff0852259f0a91fb4c906e1a01d77b92f8a49f1
    Signed-off-by: Alex Thiessen <alex.thiessen.de+coreboot@gmail.com>
    Reviewed-on: https://review.coreboot.org/23248
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- util/gitconfig/gitconfig.sh
+++ util/gitconfig/gitconfig.sh
@@ -51,7 +51,18 @@ for d in 3rdparty/{blobs,libhwbase,libgfxinit}; do
 		 git config remote.origin.push HEAD:refs/for/master)
 	fi
 done
+
 git config remote.origin.push HEAD:refs/for/master
-git config alias.sup '!git submodule update --remote --rebase && git submodule update --init --checkout'
-git config alias.sup-destroy '!git submodule deinit --force "$(git rev-parse --show-toplevel)"; git submodule init && git submodule update --checkout'
-(git config --includes user.name >/dev/null && git config --includes user.email >/dev/null) || (printf 'Please configure your name and email in git:\n\n git config --global user.name "Your Name Comes Here"\n git config --global user.email your.email@example.com\n'; exit 1)
+git config alias.sup "!git submodule update --remote --rebase && \
+git submodule update --init --checkout"
+
+git config alias.sup-destroy "!git submodule deinit --force \
+\"\$(git rev-parse --show-toplevel)\"; \
+git submodule init && git submodule update --checkout"
+
+(git config --includes user.name >/dev/null && \
+	git config --includes user.email >/dev/null) || \
+	(printf "Please configure your name and email in git:\n\n\
+ git config --global user.name \"Your Name Comes Here\"\n\
+ git config --global user.email your.email@example.com\n"; \
+exit 1)

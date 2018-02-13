b424caa25af59d33c37bea319c33594915e63ba5
Julien Viard de Galbert <jviarddegalbert@online.net>
2018-02-13 22:19:30 +0100

    mb/scaleway/tagada: Remove eMMC configuration
    The board does no support eMMC so no need to configure it.
    
    Change-Id: If29009a09f39484b1da16fb650b4f9cbee2a6d19
    Signed-off-by: Julien Viard de Galbert <jviarddegalbert@online.net>
    Reviewed-on: https://review.coreboot.org/23739
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/mainboard/scaleway/tagada/ramstage.c
+++ src/mainboard/scaleway/tagada/ramstage.c
@@ -2,6 +2,7 @@
  * This file is part of the coreboot project.
  *
  * Copyright (C) 2014 - 2017 Intel Corporation
+ * Copyright (C) 2017 - 2018 Online SAS.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,40 +18,9 @@
 #include <console/console.h>
 #include <fsp/api.h>
 #include <soc/ramstage.h>
-#include "emmc.h"
-
-static int get_emmc_dll_info(uint16_t signature, size_t num_of_entry,
-			     BL_EMMC_INFORMATION **config)
-{
-	uint8_t entry;
-
-	if ((signature == 0) || (num_of_entry == 0) || (*config == NULL))
-		return 1;
-
-	for (entry = 0; entry < num_of_entry; entry++) {
-		if ((*config)[entry].Signature == signature) {
-			*config = &(*config)[entry];
-			return 0;
-		}
-	}
-
-	return 1;
-}
 
 void mainboard_silicon_init_params(FSPS_UPD *params)
 {
-	size_t num;
-	uint16_t emmc_dll_sign;
-	BL_EMMC_INFORMATION *emmc_config;
-
-	/* Configure eMMC DLL PCD */
-	emmc_dll_sign = DEFAULT_EMMC_DLL_SIGN;
-	num = ARRAY_SIZE(harcuvar_emmc_config);
-	emmc_config = harcuvar_emmc_config;
-
-	if (get_emmc_dll_info(emmc_dll_sign, num, &emmc_config))
-		die("eMMC DLL Configuration is invalid, please correct it!");
-
-	params->FspsConfig.PcdEMMCDLLConfigPtr =
-		(uint32_t)&emmc_config->eMMCDLLConfig;
+	/* Disable eMMC */
+	params->FspsConfig.PcdEnableEmmc = 0;
 }

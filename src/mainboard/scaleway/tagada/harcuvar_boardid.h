4f13640572801323bdcaef408abfed1ec78ee4f0
Julien Viard de Galbert <jviarddegalbert@online.net>
2018-02-16 14:40:53 +0100

    mb/scaleway/tagada: Copy intel/harcuvar and rename
    Change-Id: I6fc1f8393ce3f5ba6f52edad7cf8efa5524d2704
    Signed-off-by: Julien Viard de Galbert <jviarddegalbert@online.net>
    Reviewed-on: https://review.coreboot.org/23735
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>

---
+++ src/mainboard/scaleway/tagada/harcuvar_boardid.h
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2015 - 2017 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef HARCUVAR_MAINBOARD_BOARD_H
+#define HARCUVAR_MAINBOARD_BOARD_H
+
+#include <stdint.h>
+
+#define BoardIdHarcuvar 0x52
+
+uint8_t board_id(void);
+
+#endif /* MAINBOARD_BOARD_H */
---
+++ src/mainboard/scaleway/tagada/hsio.c
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2017 Intel Corporation.
+ * Copyright (C) 2017 Online SAS.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <harcuvar_boardid.h>
+#include <hsio.h>
+#include <soc/fiamux.h>
+
+size_t mainboard_get_hsio_config(BL_HSIO_INFORMATION **p_hsio_config)
+{
+    uint8_t boardid = board_id();
+    size_t num;
+    switch (boardid) {
+    case BoardIdHarcuvar:
+        num = ARRAY_SIZE(harcuvar_hsio_config);
+        (*p_hsio_config) = (BL_HSIO_INFORMATION *)harcuvar_hsio_config;
+        break;
+    default:
+        num = 0;
+        (*p_hsio_config) = NULL;
+        break;
+    }
+    return num;
+}

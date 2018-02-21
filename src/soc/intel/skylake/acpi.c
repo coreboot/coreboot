95b4d0c25d337546a26821f9c1c0d6b25d1a69ff
Furquan Shaikh <furquan@chromium.org>
2018-02-20 21:33:00 -0800

    soc/intel/skylake: Do not set ACPI_FADT_LEGACY_DEVICES
    SKL/KBL PCH does not support legacy devices. This change removes the
    setting of ACPI_FADT_LEGACY_DEVICES flag in FADT for SKL/KBL.
    
    It helps Linux kernel to disable controllers required to support legacy 
    devices only e.g. i8237 DMA controller.
    
    BUG=b:72679357
    
    Change-Id: Ie2a85a719997157f52b0eab7254689f5a56ba05b
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://review.coreboot.org/23833
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>
    Reviewed-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@intel.corp-partner.google.com>
    Reviewed-by: Rizwan Qureshi <rizwan.qureshi@intel.com>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/skylake/acpi.c
+++ src/soc/intel/skylake/acpi.c
@@ -267,7 +267,7 @@ void acpi_fill_fadt(acpi_fadt_t *fadt)
 	fadt->day_alrm = 0xd;
 	fadt->mon_alrm = 0x00;
 	fadt->century = 0x00;
-	fadt->iapc_boot_arch = ACPI_FADT_LEGACY_DEVICES;
+	fadt->iapc_boot_arch = 0;
 	if (!IS_ENABLED(CONFIG_NO_FADT_8042))
 		fadt->iapc_boot_arch |= ACPI_FADT_8042;
 

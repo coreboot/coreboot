7693c94ecfce6253917745513a8f933ccb31b5ff
Bill XIE <persmule@gmail.com>
2018-03-15 20:05:35 +0800

    mainboard/hp: Add Elitebook Folio 9470m
    The code is based on autoport and that for revolve_810g1
    
    Tested:
    - CPU i5-3437U
    - Slotted DIMM 8GiB
    - Onboard USB2 interfaces (wlan slot, wwan slot, camera, smart card)
    - Mini pci-e on wlan slot
    - On board SDHCI connected to pci-e
    - USB3 ports
    - USB3 hub on dock (connected to USB3 port 1)
    - NVRAM options for North and South bridges
    - S3
    - TPM1 on LPC
    - Linux 4.13.17-1 within Debian GNU/Linux testing, loaded from
      SeaBIOS, or Linux payload (Heads)
    
    Not tested:
    - Fingerprint reader on USB2
    
    Not working well:
    - EHCI debug on port SSP2,(The USB port on the left, wired to ehci
      before OS) it has always-on enabled by default (maybe via EC),
      which disturbs FT232H's own power up, requiring a very critical
      timing to plug it in for it to work.
    
    Change-Id: I52e549ec18e8aa661a506a16dbc7f83417c0da78
    Signed-off-by: Bill XIE <persmule@gmail.com>
    Reviewed-on: https://review.coreboot.org/25218
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/hp/folio_9470m/mainboard.c
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2017 Bill Xie <persmule@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <device/device.h>
+#include <drivers/intel/gma/int15.h>
+
+static void mainboard_enable(device_t dev)
+{
+	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_INT_LVDS,
+			GMA_INT15_PANEL_FIT_DEFAULT,
+			GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
+}
+
+struct chip_operations mainboard_ops = {
+	.enable_dev = mainboard_enable,
+};

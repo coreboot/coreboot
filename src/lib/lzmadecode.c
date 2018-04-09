coreboot/coreboot

commit 35af5c47b030c0a1837cb34bc9ae4d50734fefb0

src/lib: Fix spacing
Fix the following errors and warnings detected by checkpatch.pl:

ERROR: spaces required around that '?' (ctx:WxV)
ERROR: spaces required around that '=' (ctx:VxV)
ERROR: spaces required around that '<' (ctx:VxV)
ERROR: spaces required around that '+=' (ctx:VxV)
ERROR: space required after that ',' (ctx:VxV)
ERROR: space required before the open brace '{'
ERROR: space required after that close brace '}'
ERROR: need consistent spacing around '+' (ctx:WxV)
ERROR: need consistent spacing around '*' (ctx:WxV)
ERROR: need consistent spacing around '&' (ctx:VxW)
ERROR: spaces required around that '?' (ctx:VxW)
ERROR: spaces required around that ':' (ctx:VxW)
ERROR: trailing whitespace
ERROR: space prohibited before that '++' (ctx:WxO)
ERROR: space prohibited before that ',' (ctx:WxW)
ERROR: space prohibited after that '!' (ctx:BxW)
ERROR: spaces prohibited around that '->' (ctx:VxW)
ERROR: space prohibited after that '-' (ctx:WxW)
WARNING: space prohibited before semicolon
WARNING: unnecessary whitespace before a quoted newline
WARNING: missing space after return type

Note that lib/libgcov.c and lib/lzmadecode.c are providing false
positives for ERROR: need consistent spacing around '*' (ctx:WxV)
An example is:
void __gcov_merge_add(gcov_type *counters  __attribute__ ((unused)),
      unsigned int n_counters __attribute__ ((unused))) {}

TEST=Build and run on Galileo Gen2

Change-Id: I0016327a5754018eaeb25bedf42338291632c7c1
Signed-off-by: Lee Leahy <Leroy.P.Leahy@intel.com>
Reviewed-on: https://review.coreboot.org/18733
Tested-by: build bot (Jenkins)
Reviewed-by: Martin Roth <martinroth@google.com>

src/lib/lzmadecode.c
@@ -37,9 +37,15 @@
 		      : ((((uintptr_t) Buffer & 3) || ((SizeT) (BufferLim - Buffer) <= 4)) ? (*Buffer++) \
 	   : ((look_ahead.dw = *(UInt32 *)Buffer), (Buffer += 4), (look_ahead_ptr = 1), look_ahead.raw[0])))
 
-#define RC_INIT2 Code = 0; Range = 0xFFFFFFFF; \
-	{ int i; for (i = 0; i < 5; i++) { RC_TEST; Code = (Code << 8) \
-		| RC_READ_BYTE; }}
+#define RC_INIT2 Code = 0; Range = 0xFFFFFFFF;		\
+{							\
+	int i;						\
+							\
+	for (i = 0; i < 5; i++) {			\
+		RC_TEST;				\
+		Code = (Code << 8) | RC_READ_BYTE;	\
+	}						\
+}
 
 
 #define RC_TEST { if (Buffer == BufferLim) return LZMA_RESULT_DATA_ERROR; }
@@ -57,7 +63,7 @@
 	{ UpdateBit0(p); mi <<= 1; A0; } else \
 	{ UpdateBit1(p); mi = (mi + mi) + 1; A1; }
 
-#define RC_GET_BIT(p, mi) RC_GET_BIT2(p, mi, ; , ;)
+#define RC_GET_BIT(p, mi) RC_GET_BIT2(p, mi, ;, ;)
 
 #define RangeDecoderBitTreeDecode(probs, numLevels, res)	\
 {								\
@@ -192,7 +198,7 @@ int LzmaDecode(CLzmaDecoderState *vs,
 	while (nowPos < outSize) {
 		CProb *prob;
 		UInt32 bound;
-		int posState = (int)((nowPos)& posStateMask);
+		int posState = (int)((nowPos)&posStateMask);
 
 		prob = p + IsMatch + (state << kNumPosBitsMax) + posState;
 		IfBit0(prob) {
@@ -346,7 +352,7 @@ int LzmaDecode(CLzmaDecoderState *vs,
 						int mi = 1;
 						do {
 							CProb *prob3 = prob + mi;
-							RC_GET_BIT2(prob3, mi, ; , rep0 |= i);
+							RC_GET_BIT2(prob3, mi, ;, rep0 |= i);
 							i <<= 1;
 						} while (--numDirectBits != 0);
 					}

src/lib/edid.c
@@ -553,10 +553,10 @@ detailed_block(struct edid *result_edid, unsigned char *x, int in_extension,
 	       out->mode.va + out->mode.vso + out->mode.vspw,
 	       out->mode.va + out->mode.vbl, out->mode.vborder,
 	       out->mode.phsync, out->mode.pvsync,
-	       extra_info.syncmethod, x[17] & 0x80 ?" interlaced" : "",
+	       extra_info.syncmethod, x[17] & 0x80 ? " interlaced" : "",
 	       extra_info.stereo);
 
-	if (! c->did_detailed_timing) {
+	if (!c->did_detailed_timing) {
 		printk(BIOS_SPEW, "Did detailed timing\n");
 		c->did_detailed_timing = 1;
 		*result_edid = *out;
@@ -654,7 +654,7 @@ cea_video_block(unsigned char *x)
 	int length = x[0] & 0x1f;
 
 	for (i = 1; i < length; i++)
-		printk(BIOS_SPEW,"    VIC %02d %s\n", x[i] & 0x7f,
+		printk(BIOS_SPEW, "    VIC %02d %s\n", x[i] & 0x7f,
 		       x[i] & 0x80 ? "(native)" : "");
 }
 
@@ -995,7 +995,7 @@ static void dump_breakdown(unsigned char *edid)
 	printk(BIOS_SPEW, "Extracted contents:\n");
 	print_subsection("header", edid, 0, 7);
 	print_subsection("serial number", edid, 8, 17);
-	print_subsection("version", edid,18, 19);
+	print_subsection("version", edid, 18, 19);
 	print_subsection("basic params", edid, 20, 24);
 	print_subsection("chroma info", edid, 25, 34);
 	print_subsection("established", edid, 35, 37);
@@ -1610,7 +1610,7 @@ void set_vbe_mode_info_valid(const struct edid *edid, uintptr_t fb_addr)
 	edid_fb.bits_per_pixel = edid->framebuffer_bits_per_pixel;
 	edid_fb.reserved_mask_pos = 0;
 	edid_fb.reserved_mask_size = 0;
-	switch (edid->framebuffer_bits_per_pixel){
+	switch (edid->framebuffer_bits_per_pixel) {
 	case 32:
 	case 24:
 		/* packed into 4-byte words */

src/lib/gcov-glue.c
@@ -129,7 +129,7 @@ static void setbuf(FILE *stream, char *buf)
 static void coverage_init(void *unused)
 {
 	extern long __CTOR_LIST__;
-	typedef void (*func_ptr)(void) ;
+	typedef void (*func_ptr)(void);
 	func_ptr *ctor = (func_ptr *) &__CTOR_LIST__;
 	if (ctor == NULL)
 		return;

src/lib/gcov-io.h
@@ -300,7 +300,7 @@ typedef HOST_WIDEST_INT gcov_type;
 #include "gcov-iov.h"
 
 /* Convert a magic or version number to a 4 character string.  */
-#define GCOV_UNSIGNED2STRING(ARRAY,VALUE)	\
+#define GCOV_UNSIGNED2STRING(ARRAY, VALUE)	\
 	((ARRAY)[0] = (char)((VALUE) >> 24),	\
 	(ARRAY)[1] = (char)((VALUE) >> 16),	\
 	(ARRAY)[2] = (char)((VALUE) >> 8),	\
@@ -387,12 +387,12 @@ typedef HOST_WIDEST_INT gcov_type;
 #define GCOV_TAG_MASK(TAG) (((TAG) - 1) ^ (TAG))
 
 /* Return nonzero if SUB is an immediate subtag of TAG.  */
-#define GCOV_TAG_IS_SUBTAG(TAG,SUB)				\
+#define GCOV_TAG_IS_SUBTAG(TAG, SUB)				\
 	(GCOV_TAG_MASK(TAG) >> 8 == GCOV_TAG_MASK(SUB)		\
 	 && !(((SUB) ^ (TAG)) & ~GCOV_TAG_MASK(TAG)))
 
 /* Return nonzero if SUB is at a sublevel to TAG.  */
-#define GCOV_TAG_IS_SUBLEVEL(TAG,SUB)				\
+#define GCOV_TAG_IS_SUBLEVEL(TAG, SUB)				\
 	(GCOV_TAG_MASK(TAG) > GCOV_TAG_MASK(SUB))
 
 /* Basic block flags.  */

src/lib/imd.c
@@ -636,7 +636,7 @@ int imd_entry_remove(const struct imd *imd, const struct imd_entry *entry)
 	imdr = imd_entry_to_imdr(imd, entry);
 
 	if (imdr == NULL)
-		return - 1;
+		return -1;
 
 	r = imdr_root(imdr);
 

src/lib/jpeg.c
@@ -600,10 +600,10 @@ static void dec_makehuff(struct dec_hufftbl *hu, int *hufflen, unsigned char *hu
 							  i);
 						if (v && x < (1 << (v - 1)))
 							x += (-1 << v) + 1;
-						x = x << 16 | (hu-> vals[k] & 0xf0) << 4 |
+						x = x << 16 | (hu->vals[k] & 0xf0) << 4 |
 							(DECBITS - (i + 1 + v)) | 128;
 					} else
-						x = v << 16 | (hu-> vals[k] & 0xf0) << 4 |
+						x = v << 16 | (hu->vals[k] & 0xf0) << 4 |
 							(DECBITS - (i + 1));
 					hu->llvals[c | d] = x;
 				}
@@ -633,11 +633,11 @@ static void dec_makehuff(struct dec_hufftbl *hu, int *hufflen, unsigned char *hu
 #define C5IC1 ((PREC)IFIX(0.566454497))	/* c5/c1 */
 #define C7IC1 ((PREC)IFIX(0.198912367))	/* c7/c1 */
 
-#define XPP(a,b) (t = a + b, b = a - b, a = t)
-#define XMP(a,b) (t = a - b, b = a + b, a = t)
-#define XPM(a,b) (t = a + b, b = b - a, a = t)
+#define XPP(a, b) (t = a + b, b = a - b, a = t)
+#define XMP(a, b) (t = a - b, b = a + b, a = t)
+#define XPM(a, b) (t = a + b, b = b - a, a = t)
 
-#define ROT(a,b,s,c) (t = IMULT(a + b, s),	\
+#define ROT(a, b, s, c) (t = IMULT(a + b, s),	\
 			a = IMULT(a, c - s) + t,	\
 			b = IMULT(b, c + s) - t)
 
@@ -810,7 +810,7 @@ static void initcol(PREC q[][64])
 }
 
 /* This is optimized for the stupid sun SUNWspro compiler. */
-#define STORECLAMP(a,x)				\
+#define STORECLAMP(a, x)			\
 (						\
 	(a) = (x),				\
 	(unsigned int)(x) >= 256 ?		\
@@ -825,8 +825,8 @@ static void initcol(PREC q[][64])
 
 #define CBCRCG(yin, xin)			\
 (						\
-	cb = outc[0 +yin*8+xin],		\
-	cr = outc[64+yin*8+xin],		\
+	cb = outc[0  + yin * 8 + xin],		\
+	cr = outc[64 + yin * 8 + xin],		\
 	cg = (50 * cb + 130 * cr + 128) >> 8	\
 )
 
@@ -834,8 +834,8 @@ static void initcol(PREC q[][64])
 
 #define CBCRCG(yin, xin)			\
 (						\
-	cb = outc[0 +yin*8+xin],		\
-	cr = outc[64+yin*8+xin],		\
+	cb = outc[0  + yin*8 + xin],		\
+	cr = outc[64 + yin*8 + xin],		\
 	cg = (3 * cb + 8 * cr) >> 4		\
 )
 

src/lib/libgcov.c
@@ -248,7 +248,7 @@ buffer_fn_data(const char *filename, const struct gcov_info *gi_ptr,
 	return &fn_buffer->next;
 
 fail:
-	fprintf(stderr, "profiling:%s:Function %u %s %u \n", filename, fn_ix,
+	fprintf(stderr, "profiling:%s:Function %u %s %u\n", filename, fn_ix,
 		len ? "cannot allocate" : "counter mismatch", len ? len : ix);
 
 	return (struct gcov_fn_buffer **)free_fn_data(gi_ptr, fn_buffer, ix);
@@ -289,7 +289,7 @@ gcov_version(struct gcov_info *ptr, gcov_unsigned_t version,
 
 		fprintf(stderr,
 			"profiling:%s:Version mismatch - expected %.4s got %.4s\n",
-			filename? filename : ptr->filename, e, v);
+			filename ? filename : ptr->filename, e, v);
 		return 0;
 	}
 	return 1;
@@ -603,7 +603,7 @@ gcov_exit(void)
 
 read_error:;
 		fprintf(stderr, "profiling:%s:%s merging\n", gi_filename,
-			error < 0 ? "Overflow": "Error");
+			error < 0 ? "Overflow" : "Error");
 
 		goto read_fatal;
 
@@ -986,7 +986,7 @@ void
 __gcov_average_profiler(gcov_type *counters, gcov_type value)
 {
 	counters[0] += value;
-	counters[1] ++;
+	counters[1]++;
 }
 #endif
 

src/lib/lzmadecode.h
@@ -42,7 +42,7 @@ typedef struct _CLzmaProperties {
 	int lc;
 	int lp;
 	int pb;
-}CLzmaProperties;
+} CLzmaProperties;
 
 int LzmaDecodeProperties(CLzmaProperties *propsRes, const unsigned char *propsData, int size);
 

commit d538dd1fe70a333c929a2eb7c4106608bcd05257

lib/lzmadecode: Add block around `UpdateBit1()`
Fix the error below.

```
src/lib/lzmadecode.c: In function 'LzmaDecode':
src/lib/lzmadecode.c:77:2: error: macro expands to multiple statements \
[-Werror=multistatement-macros]
  Range -= bound;    \
  ^~~~~
src/lib/lzmadecode.c:300:7: note: in expansion of macro 'UpdateBit1'
       UpdateBit1(prob);
       ^~~~~~~~~~
src/lib/lzmadecode.c:299:8: note: some parts of macro expansion are not \
guarded by this 'else' clause
      } else
        ^~~~
cc1: all warnings being treated as errors
```

The macro is defined as below.

    #define UpdateBit1(p)                           \
            Range -= bound;                         \
            Code -= bound;                          \
            *(p) -= (*(p)) >> kNumMoveBits

Found-by: gcc-8 (Debian 8-20180402-1) 8.0.1 20180402 (experimental) [trunk revision 259004]
Fixes: 35af5c47 (src/lib: Fix spacing)
Change-Id: Ife0688541e23c05e26e429a6d8caee7e2d425b1b
Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/25549
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Julius Werner <jwerner@chromium.org>
Reviewed-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

src/lib/lzmadecode.c
@@ -296,8 +296,9 @@ int LzmaDecode(CLzmaDecoderState *vs,
 							previousByte;
 
 						continue;
-					} else
+					} else {
 						UpdateBit1(prob);
+					}
 				} else {
 					UInt32 distance;
 					UpdateBit1(prob);
ed76908e4a8bea3781a442fe0a3a557a15ebeed9
Richard Spiegel <richard.spiegel@amd.corp-partner.google.com>
2018-04-12 16:17:49 -0700

    include/memory_info.h: Change part number field from 19 bytes to 21
    DDR4 reserves 20 bytes for part number without termination. Therefor
    coreboot buffer for part number has to be increased to 21 bytes, to
    account termination.
    
    BUG=b:77943312
    TEST=Build kahlee.
    
    Change-Id: I57aa8e06d907a1e6b11ce88f612a0d6c14b7c65b
    Signed-off-by: Richard Spiegel <richard.spiegel@silverbackltd.com>
    Reviewed-on: https://review.coreboot.org/25650
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/include/memory_info.h
+++ src/include/memory_info.h
@@ -20,7 +20,7 @@
 #include <compiler.h>
 
 #define DIMM_INFO_SERIAL_SIZE		4
-#define DIMM_INFO_PART_NUMBER_SIZE	19
+#define DIMM_INFO_PART_NUMBER_SIZE	21
 #define DIMM_INFO_TOTAL			8	/* Maximum num of dimm is 8 */
 
 /**

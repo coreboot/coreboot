2b1d353f1be104452ddb0def22a108b712c74e9b
Bartek Pastudzki <Bartek.Pastudzki@3mdeb.com>
2018-04-16 22:31:25 +0200

    use template for option checking functions

--- src/mainboard/pcengines/apu2/bios_knobs.c
+++ src/mainboard/pcengines/apu2/bios_knobs.c
@@ -17,6 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
 #include <stdint.h>
 #include <console/console.h>
 #include <program_loading.h>
@@ -41,143 +42,47 @@ static char *after_str(const char *s, const char *pattern)
 	return NULL;
 }
 
+#define map_cbfs_file(...) cbfs_boot_map_with_leak (__VA_ARGS__);
+#define fail(...) {printk (__VA_ARGS__); return -1;}
+
 static u8 check_knob_value(const char *s)
 {
 	const char *boot_file = NULL;
 	size_t boot_file_len = 0;
 	char * token = NULL;
 
-	//
-	// This function locates a file in cbfs, maps it to memory and returns
-	// a void* pointer
-	//
-	boot_file = cbfs_boot_map_with_leak(BOOTORDER_FILE, CBFS_TYPE_RAW, &boot_file_len);
+	boot_file = map_cbfs_file (BOOTORDER_FILE, CBFS_TYPE_RAW, &boot_file_len);
 	if (boot_file == NULL)
-		printk(BIOS_ALERT, "file [%s] not found in CBFS\n", BOOTORDER_FILE);
+		fail(BIOS_ALERT, "file [%s] not found in CBFS\n", BOOTORDER_FILE);
 	if (boot_file_len < 4096)
-		printk(BIOS_ALERT, "Missing bootorder data.\n");
-	if (boot_file == NULL || boot_file_len < 4096)
-		return -1;
+		fail(BIOS_ALERT, "Missing bootorder data.\n");
 
-	token = after_str( boot_file, s );
-
-	if (token) {
+	if ((token = after_str (boot_file, s)) != NULL) {
 		if (*token == '0') return 0;
 		if (*token == '1') return 1;
+        else return -2;
 	}
 
 	return -1;
 }
 
-bool check_console(void)
-{
-	u8 scon;
+#define opt_function(NAME, PAR_NAME, DEFAULT) \
+    bool NAME(void) \
+    { \
+        u8 val = check_knob_value (PAR_NAME); \
+        if (val == 0) \
+            return false; \
+        else if (val == 1) \
+            return true; \
+        else { \
+		    printk(BIOS_EMERG, "Missing or invalid " PAR_NAME \
+                  " knob, default: " #DEFAULT ".\n"); \
+            return DEFAULT; \
+        }\
+    }
 
-	//
-	// Find the serial console item
-	//
-	scon = check_knob_value("scon");
-
-	switch (scon) {
-	case 0:
-		return false;
-		break;
-	case 1:
-		return true;
-		break;
-	default:
-		printk(BIOS_EMERG, "Missing or invalid scon knob, enable console.\n");
-		break;
-	}
-
-	return true;
-}
-
-static bool check_uart(char uart_letter)
-{
-	u8 uarten;
-
-	switch (uart_letter) {
-	case 'c':
-		uarten = check_knob_value("uartc");
-		break;
-	case 'd':
-		uarten = check_knob_value("uartd");
-		break;
-	default:
-		uarten = -1;
-		break;
-	}
-
-	switch (uarten) {
-	case 0:
-		return false;
-		break;
-	case 1:
-		return true;
-		break;
-	default:
-		printk(BIOS_EMERG, "Missing or invalid uart knob, disable port.\n");
-		break;
-	}
-
-	return false;
-}
-
-inline bool check_uartc(void)
-{
-	return check_uart('c');
-}
-
-inline bool check_uartd(void)
-{
-	return check_uart('d');
-}
-
-bool check_ehci0(void)
-{
-	u8 ehci0;
-
-	//
-	// Find the serial console item
-	//
-	ehci0 = check_knob_value("ehcien");
-
-	switch (ehci0) {
-	case 0:
-		return false;
-		break;
-	case 1:
-		return true;
-		break;
-	default:
-		printk(BIOS_EMERG, "Missing or invalid ehci0 knob, enable ehci0.\n");
-		break;
-	}
-
-	return true;
-}
-
-bool check_mpcie2_clk(void)
-{
-	u8 mpcie2_clk;
-
-	//
-	// Find the serial console item
-	//
-	mpcie2_clk = check_knob_value("mpcie2_clk");
-
-	switch (mpcie2_clk) {
-	case 0:
-		return false;
-		break;
-	case 1:
-		return true;
-		break;
-	default:
-		printk(BIOS_EMERG, "Missing or invalid mpcie2_clk knob, forcing CLK of mPCIe2 slot is not enabled .\n");
-		break;
-	}
-
-	return false;
-}
+opt_function (check_console, "scon", true)
+opt_function (check_uartc, "uartc", false)
+opt_function (check_uartd, "uartd", false)
+opt_function (check_ehci0, "ehcien", true)
+opt_function (check_mpcie2_clk, "mpcie2_clk", false)

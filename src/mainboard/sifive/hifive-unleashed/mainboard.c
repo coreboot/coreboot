1c09cfa37b167d64da2a182058b04780789f6201
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2018-04-19 16:23:55 +0200

    mb/sifive: Add HiFive Unleashed mainboard
    Change-Id: I52ef2da9148809923c90178a00ba94babba8d2f8
    Signed-off-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>
    Reviewed-on: https://review.coreboot.org/25790
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ src/mainboard/sifive/hifive-unleashed/mainboard.c
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2018 Jonathan Neuschäfer
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <device/device.h>
+#include <bootblock_common.h>
+#include <timestamp.h>
+
+static void mainboard_enable(device_t dev)
+{
+}
+
+struct chip_operations mainboard_ops = {
+	.name = CONFIG_MAINBOARD_PART_NUMBER,
+	.enable_dev = mainboard_enable,
+};

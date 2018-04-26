f03daf7d29d78bc3f11babf9930a3cf498081fb2
Elyes HAOUAS <ehaouas@noos.fr>
2018-04-26 14:23:16 +0200

    drivers/i2c/lm63: Remove unneeded headers
    Change-Id: Id13eef7f0fca0b929372490859aa4734ea7a9f23
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/25868
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/drivers/i2c/lm63/lm63.c
+++ src/drivers/i2c/lm63/lm63.c
@@ -1,10 +1,6 @@
 #include <console/console.h>
 #include <device/device.h>
 #include <device/smbus.h>
-#include <device/pci.h>
-#include <device/pci_ids.h>
-#include <device/pci_ops.h>
-#include <cpu/x86/msr.h>
 
 static void lm63_init(struct device *dev)
 {

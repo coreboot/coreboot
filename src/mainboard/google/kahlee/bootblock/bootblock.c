a44900a24ba1c6730caaa8940b01de2dcccebe3e
Marc Jones <marc.jones@scarletltd.com>
2018-05-01 14:30:48 -0600

    google/kahlee: Set SPI 100 MHz and SPI Dual Read IO mode
    Set SPI Fast Read to 100MHz and Dual Read IO mode to speed up
    the boot process by over a half second. Also, increase the Normal
    Read speed to 33MHz as supported by the W25Q128FW.
    
    BUG=b:70558952
    TEST=Run cbmem -t to get boot times.
    
    Change-Id: I616a96526ed90bb4ab0c9c6b78787799faa02633
    Signed-off-by: Marc Jones <marc.jones@scarletltd.com>
    Reviewed-on: https://review.coreboot.org/25970
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/mainboard/google/kahlee/bootblock/bootblock.c
+++ src/mainboard/google/kahlee/bootblock/bootblock.c
@@ -34,9 +34,17 @@ void bootblock_mainboard_early_init(void)
 
 void bootblock_mainboard_init(void)
 {
+	/*
+	 * W25Q128FW Setup
+	 * Normal Read 40MHz
+	 * Fast Read 104MHz
+	 * Dual Read IO (1-2-2)
+	 */
+	sb_read_mode(SPI_READ_MODE_DUAL122);
+
 	/* Set SPI speeds before verstage. Needed for TPM */
-	sb_set_spi100(SPI_SPEED_22M,		/* Normal */
-			SPI_SPEED_66M,		/* Fast   */
+	sb_set_spi100(SPI_SPEED_33M,		/* Normal */
+			SPI_SPEED_100M,		/* Fast   */
 			SPI_SPEED_66M,		/* AltIO  */
 			SPI_SPEED_66M);		/* TPM    */
 

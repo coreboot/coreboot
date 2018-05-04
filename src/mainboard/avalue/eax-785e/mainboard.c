477a516ec3e97bd419d41f6b432eff19b722b9f2
Elyes HAOUAS <ehaouas@noos.fr>
2018-05-04 20:02:58 +0200

    mb/avalue: Get rid of device_t
    Use of device_t has been abandoned in ramstage.
    
    Change-Id: I7f1276ee593928956913eaeecd62fd3018cc9ae2
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/26077
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>

--- src/mainboard/avalue/eax-785e/mainboard.c
+++ src/mainboard/avalue/eax-785e/mainboard.c
@@ -64,7 +64,7 @@ u8 is_dev3_present(void)
 * enable the dedicated function in EAX-785E board.
 * This function called early than rs780_enable.
 *************************************************/
-static void mainboard_enable(device_t dev)
+static void mainboard_enable(struct device *dev)
 {
 	printk(BIOS_INFO, "Mainboard " CONFIG_MAINBOARD_PART_NUMBER " Enable.\n");
 

ae27430a3cb10c72c75b9552ca78dfe079322446
Elyes HAOUAS <ehaouas@noos.fr>
2018-05-04 20:20:23 +0200

    mb/gizmosphere: Get rid of device_t
    Use of device_t has been abandoned in ramstage.
    
    Change-Id: I75258ecc5f3881012c3f767c8b970a1f10c6abbd
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/26085
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>

--- src/mainboard/gizmosphere/gizmo/mainboard.c
+++ src/mainboard/gizmosphere/gizmo/mainboard.c
@@ -27,7 +27,7 @@
 /**********************************************
  * Enable the dedicated functions of the board.
  **********************************************/
-static void mainboard_enable(device_t dev)
+static void mainboard_enable(struct device *dev)
 {
 	printk(BIOS_INFO, "Mainboard " CONFIG_MAINBOARD_PART_NUMBER " Enable.\n");
 
@@ -49,7 +49,7 @@ static void mainboard_enable(device_t dev)
 
 static void mainboard_final(void *chip_info)
 {
-	device_t ahci_dev;
+	struct device *ahci_dev;
 	uintptr_t ABAR;
 	u8 *memptr;
 

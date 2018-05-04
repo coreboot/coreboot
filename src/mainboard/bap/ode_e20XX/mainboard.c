5a0757cf74f01b719508df600b03faf444fd1283
Elyes HAOUAS <ehaouas@noos.fr>
2018-05-04 20:05:33 +0200

    mb/bap: Get rid of device_t
    Use of device_t has been abandoned in ramstage.
    
    Change-Id: I6fc056acb8ff16a943352342b99a9ede6558d438
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/26078
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

--- src/mainboard/bap/ode_e20XX/mainboard.c
+++ src/mainboard/bap/ode_e20XX/mainboard.c
@@ -109,7 +109,7 @@ static void pirq_setup(void)
 /**********************************************
  * enable the dedicated function in mainboard.
  **********************************************/
-static void mainboard_enable(device_t dev)
+static void mainboard_enable(struct device *dev)
 {
 	printk(BIOS_INFO, "Mainboard " CONFIG_MAINBOARD_PART_NUMBER " Enable.\n");
 
--- src/mainboard/bap/ode_e20XX/mptable.c
+++ src/mainboard/bap/ode_e20XX/mptable.c
@@ -106,7 +106,7 @@ static void *smp_write_config_table(void *v)
 	PCI_INT(0x2, 0x0, 0x0, intr_data_ptr[PIRQ_F]);
 
 	/* PCI slots */
-	device_t dev = dev_find_slot(0, PCI_DEVFN(0x14, 4));
+	struct device *dev = dev_find_slot(0, PCI_DEVFN(0x14, 4));
 	if (dev && dev->enabled) {
 		u8 bus_pci = dev->link_list->secondary;
 		/* PCI_SLOT 0 */

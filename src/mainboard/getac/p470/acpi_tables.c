5bb159a6cf588e0408aa3c56d61242a9f6e803d5
Elyes HAOUAS <ehaouas@noos.fr>
2018-05-04 20:17:55 +0200

    mb/getac: Get rid of device_t
    Use of device_t has been abandoned in ramstage.
    
    Change-Id: Ie5ed621423315388e2b8eb3d5433ef2a7a47d602
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/26083
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>

--- src/mainboard/getac/p470/acpi_tables.c
+++ src/mainboard/getac/p470/acpi_tables.c
@@ -81,7 +81,7 @@ static long acpi_create_ecdt(acpi_ecdt_t * ecdt)
 	return header->length;
 }
 
-unsigned long mainboard_write_acpi_tables(device_t device,
+unsigned long mainboard_write_acpi_tables(struct device *device,
 					  unsigned long start,
 					  acpi_rsdp_t *rsdp)
 {

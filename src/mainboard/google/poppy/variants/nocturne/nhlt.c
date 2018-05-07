9146ccd7e38dc5140daf8316eb866aa6b84994e8
Sathyanarayana Nujella <sathyanarayana.nujella@intel.com>
2018-05-07 16:06:59 -0700

    mb/google/poppy/variants/nocturne: update Audio configuration
    This patch updates the below:
    1)
    Nocturne board has only Max98373 speaker amp.
    Update both NHLT and DT entries to include only Max98373
    and not include DA7219.
    
    2) I2S2 is used for Boot Beep.
       So, update GPP_F0 ~ F2 pins accordingly.
    
    3) Include DMIC-4ch configuration.
    
    BUG=b:79362472
    TEST=None [Waiting for HW to verify]
    
    Change-Id: I0e9b3a564c22de6e84e96e5e937a3aca4ae73d75
    Signed-off-by: Sathyanarayana Nujella <sathyanarayana.nujella@intel.com>
    Reviewed-on: https://review.coreboot.org/26143
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/poppy/variants/nocturne/nhlt.c
+++ src/mainboard/google/poppy/variants/nocturne/nhlt.c
@@ -20,13 +20,9 @@
 
 void variant_nhlt_init(struct nhlt *nhlt)
 {
-	/* 2 Channel DMIC array. */
-	if (nhlt_soc_add_dmic_array(nhlt, 2))
-		printk(BIOS_ERR, "Couldn't add 2CH DMIC array.\n");
-
-	/* Dialog DA7219 Headset codec. */
-	if (nhlt_soc_add_da7219(nhlt, AUDIO_LINK_SSP1))
-		printk(BIOS_ERR, "Couldn't add Dialog DA7219 codec.\n");
+	/* 4 Channel DMIC array. */
+	if (nhlt_soc_add_dmic_array(nhlt, 4))
+		printk(BIOS_ERR, "Couldn't add 4CH DMIC array.\n");
 
 	/* MAXIM Smart Amps for left and right speakers. */
 	if (nhlt_soc_add_max98373(nhlt, AUDIO_LINK_SSP0))
@@ -37,6 +33,6 @@ void variant_nhlt_oem_overrides(const char **oem_id, const char **oem_table_id,
 				uint32_t *oem_revision)
 {
 	*oem_id = "GOOGLE";
-	*oem_table_id = "NOCTURNEMAX";
+	*oem_table_id = "NOCTURNE";
 	*oem_revision = 0;
 }

a78e66e5f4cc032968dc8e6f8e76e550d87dafc8
Patrick Rudolph <siro@das-labor.org>
2018-05-13 16:06:51 +0200

    Documentation: Add static CSS file to fix tables
    Add a static CSS file to remove annoying scrollbars on rst code tables.
    
    Change-Id: I436b36fb7ee9856c7d6ad8534cd0610b7f071b17
    Signed-off-by: Patrick Rudolph <siro@das-labor.org>
    Reviewed-on: https://review.coreboot.org/26263
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Philipp Deppenwiese <zaolin.daisuki@gmail.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -58,7 +58,13 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
+
+html_context = {
+      'css_files': [
+        '_static/theme_overrides.css',  # override wide tables in RTD theme
+      ],
+}
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'corebootdoc'

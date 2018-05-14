13f89980263fc546848478e97f20eab6e4b303a1
Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
2018-05-14 16:08:06 +0800

    mb/google/poppy: Disable one ALS node
    Since there are two ALS device nodes on Nami, need to remove one.
    
    BUG=b:79227879
    BRANCH=master
    TEST=Verify if only one ALS node is found in /sys/bus/iio/devices
    
    Change-Id: I850af06bec833739afa0c8c516d351d81952ce2c
    Signed-off-by: Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/26271
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/poppy/variants/nami/include/variant/ec.h
+++ src/mainboard/google/poppy/variants/nami/include/variant/ec.h
@@ -61,13 +61,6 @@
 	 EC_HOST_EVENT_MASK(EC_HOST_EVENT_BATTERY_SHUTDOWN) |\
 	 EC_HOST_EVENT_MASK(EC_HOST_EVENT_PANIC))
 
-/*
- * ACPI related definitions for ASL code.
- */
-
-/* Enable EC backed ALS device in ACPI */
-#define EC_ENABLE_ALS_DEVICE
-
 /* Enable EC backed PD MCU device in ACPI */
 #define EC_ENABLE_PD_MCU_DEVICE
 

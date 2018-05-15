coreboot/coreboot

commit 4f5bed5210a82eb5f422b72514f9c123a2234876

cbfs: Rename CBFS_TYPE_PAYLOAD to CBFS_TYPE_SELF
In preparation of having FIT payloads, which aren't converted to simple ELF,
rename the CBFS type payload to actually show the format the payload is
encoded in.

Another type CBFS_TYPE_FIT will be added to have two different payload
formats. For now this is only a cosmetic change.

Change-Id: I39ee590d063b3e90f6153fe655aa50e58d45e8b0
Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
Reviewed-on: https://review.coreboot.org/25986
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>
Reviewed-by: Aaron Durbin <adurbin@chromium.org>
Reviewed-by: Julius Werner <jwerner@chromium.org>

payloads/coreinfo/cbfs_module.c
@@ -28,7 +28,7 @@
 #define COMPONENT_BOOTBLOCK	0x01
 #define COMPONENT_CBFSHEADER	0x02
 #define COMPONENT_STAGE		0x10
-#define COMPONENT_PAYLOAD	0x20
+#define COMPONENT_SELF		0x20
 #define COMPONENT_OPTIONROM	0x30
 #define COMPONENT_RAW		0x50
 #define COMPONENT_MICROCODE	0x53
@@ -187,8 +187,8 @@ static int cbfs_module_redraw(WINDOW * win)
 	case COMPONENT_STAGE:
 		mvwprintw(win, row++, 38, "stage");
 		break;
-	case COMPONENT_PAYLOAD:
-		mvwprintw(win, row++, 38, "payload");
+	case COMPONENT_SELF:
+		mvwprintw(win, row++, 38, "simple ELF");
 		break;
 	case COMPONENT_OPTIONROM:
 		mvwprintw(win, row++, 38, "optionrom");

util/cbfstool/cbfs.h
@@ -171,7 +171,7 @@ struct cbfs_payload {
 #define CBFS_COMPONENT_BOOTBLOCK  0x01
 #define CBFS_COMPONENT_CBFSHEADER 0x02
 #define CBFS_COMPONENT_STAGE      0x10
-#define CBFS_COMPONENT_PAYLOAD    0x20
+#define CBFS_COMPONENT_SELF       0x20
 #define CBFS_COMPONENT_OPTIONROM  0x30
 #define CBFS_COMPONENT_BOOTSPLASH 0x40
 #define CBFS_COMPONENT_RAW        0x50
@@ -204,7 +204,7 @@ static struct typedesc_t filetypes[] unused = {
 	{CBFS_COMPONENT_BOOTBLOCK, "bootblock"},
 	{CBFS_COMPONENT_CBFSHEADER, "cbfs header"},
 	{CBFS_COMPONENT_STAGE, "stage"},
-	{CBFS_COMPONENT_PAYLOAD, "payload"},
+	{CBFS_COMPONENT_SELF, "simple elf"},
 	{CBFS_COMPONENT_OPTIONROM, "optionrom"},
 	{CBFS_COMPONENT_BOOTSPLASH, "bootsplash"},
 	{CBFS_COMPONENT_RAW, "raw"},

util/nvramtool/cbfs.h
@@ -67,7 +67,7 @@ typedef uint8_t u8;
     components */
 
 #define CBFS_TYPE_STAGE      0x10
-#define CBFS_TYPE_PAYLOAD    0x20
+#define CBFS_TYPE_SELF       0x20
 #define CBFS_TYPE_OPTIONROM  0x30
 #define CBFS_TYPE_BOOTSPLASH 0x40
 #define CBFS_TYPE_RAW        0x50

payloads/libpayload/include/cbfs_core.h
@@ -67,7 +67,7 @@
     components */
 
 #define CBFS_TYPE_STAGE      0x10
-#define CBFS_TYPE_PAYLOAD    0x20
+#define CBFS_TYPE_SELF       0x20
 #define CBFS_TYPE_OPTIONROM  0x30
 #define CBFS_TYPE_BOOTSPLASH 0x40
 #define CBFS_TYPE_RAW        0x50

payloads/libpayload/libcbfs/cbfs.c
@@ -158,7 +158,7 @@ int cbfs_execute_stage(struct cbfs_media *media, const char *name)
 void *cbfs_load_payload(struct cbfs_media *media, const char *name)
 {
 	return (struct cbfs_payload *)cbfs_get_file_content(
-		media, name, CBFS_TYPE_PAYLOAD, NULL);
+		media, name, CBFS_TYPE_SELF, NULL);
 }
 
 struct cbfs_file *cbfs_find(const char *name) {

src/commonlib/include/commonlib/cbfs_serialized.h
@@ -67,7 +67,7 @@
 #define CBFS_TYPE_DELETED    0x00000000
 #define CBFS_TYPE_DELETED2   0xffffffff
 #define CBFS_TYPE_STAGE      0x10
-#define CBFS_TYPE_PAYLOAD    0x20
+#define CBFS_TYPE_SELF       0x20
 #define CBFS_TYPE_OPTIONROM  0x30
 #define CBFS_TYPE_BOOTSPLASH 0x40
 #define CBFS_TYPE_RAW        0x50

util/cbfstool/cbfs_image.c
@@ -1328,7 +1328,7 @@ int cbfs_export_entry(struct cbfs_image *image, const char *entry_name,
 			buffer_delete(&buffer);
 			return -1;
 		}
-	} else if (ntohl(entry->type) == CBFS_COMPONENT_PAYLOAD) {
+	} else if (ntohl(entry->type) == CBFS_COMPONENT_SELF) {
 		if (cbfs_payload_make_elf(&buffer, arch)) {
 			buffer_delete(&buffer);
 			return -1;
@@ -1522,7 +1522,7 @@ int cbfs_print_entry_info(struct cbfs_image *image, struct cbfs_file *entry,
 					      CBFS_SUBHEADER(entry), fp);
 			break;
 
-		case CBFS_COMPONENT_PAYLOAD:
+		case CBFS_COMPONENT_SELF:
 			payload = (struct cbfs_payload_segment *)
 					CBFS_SUBHEADER(entry);
 			while (payload) {

util/cbfstool/cbfstool.c
@@ -410,7 +410,7 @@ static int cbfs_add_component(const char *filename,
 			if (type == CBFS_COMPONENT_STAGE)
 				attrs->position = htonl(offset +
 					sizeof(struct cbfs_stage));
-			else if (type == CBFS_COMPONENT_PAYLOAD)
+			else if (type == CBFS_COMPONENT_SELF)
 				attrs->position = htonl(offset +
 					sizeof(struct cbfs_payload));
 			else
@@ -737,7 +737,7 @@ static int cbfs_add_payload(void)
 {
 	return cbfs_add_component(param.filename,
 				  param.name,
-				  CBFS_COMPONENT_PAYLOAD,
+				  CBFS_COMPONENT_SELF,
 				  param.baseaddress,
 				  param.headeroffset,
 				  cbfstool_convert_mkpayload);
@@ -757,7 +757,7 @@ static int cbfs_add_flat_binary(void)
 	}
 	return cbfs_add_component(param.filename,
 				  param.name,
-				  CBFS_COMPONENT_PAYLOAD,
+				  CBFS_COMPONENT_SELF,
 				  param.baseaddress,
 				  param.headeroffset,
 				  cbfstool_convert_mkflatpayload);

commit 717ba748366cda19b7532897a5b8d59fc2cd25d9

cbfs/payload type: Fix build warning and whitespace in name
Currently, adding a payload to CBFS using the build system, the warning
below is shown.

    W: Unknown type 'payload' ignored

Update payload type from "simple elf" to "simple_elf" and rename the
word "payload" to "simple_elf" in all Makefiles.

Fixes: 4f5bed52 (cbfs: Rename CBFS_TYPE_PAYLOAD to CBFS_TYPE_SELF)
Change-Id: Iccf6cc889b7ddd0c6ae04bda194fe5f9c00e495d
Signed-off-by: Paul Menzel <paulepanter@users.sourceforge.net>
Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
Reviewed-on: https://review.coreboot.org/26240
Reviewed-by: Julius Werner <jwerner@chromium.org>
Reviewed-by: Nico Huber <nico.h@gmx.de>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

payloads/Makefile.inc
@@ -15,12 +15,12 @@
 
 cbfs-files-$(CONFIG_COREINFO_SECONDARY_PAYLOAD) += img/coreinfo
 img/coreinfo-file := payloads/coreinfo/build/coreinfo.elf
-img/coreinfo-type := payload
+img/coreinfo-type := simple_elf
 img/coreinfo-compression := $(CBFS_SECONDARY_PAYLOAD_COMPRESS_FLAG)
 
 cbfs-files-$(CONFIG_NVRAMCUI_SECONDARY_PAYLOAD) += img/nvramcui
 img/nvramcui-file := payloads/nvramcui/nvramcui.elf
-img/nvramcui-type := payload
+img/nvramcui-type := simple_elf
 img/nvramcui-compression := $(CBFS_SECONDARY_PAYLOAD_COMPRESS_FLAG)
 
 PAYLOADS_LIST=\

payloads/coreinfo/cbfs_module.c
@@ -188,7 +188,7 @@ static int cbfs_module_redraw(WINDOW * win)
 		mvwprintw(win, row++, 38, "stage");
 		break;
 	case COMPONENT_SELF:
-		mvwprintw(win, row++, 38, "simple ELF");
+		mvwprintw(win, row++, 38, "simple_elf");
 		break;
 	case COMPONENT_OPTIONROM:
 		mvwprintw(win, row++, 38, "optionrom");

payloads/external/Makefile.inc
@@ -48,7 +48,7 @@ $(CONFIG_CBFS_PREFIX)/payload-file := $(CONFIG_PAYLOAD_FILE)
 ifeq ($(CONFIG_PAYLOAD_IS_FLAT_BINARY),y)
 $(CONFIG_CBFS_PREFIX)/payload-type := flat-binary
 else
-$(CONFIG_CBFS_PREFIX)/payload-type := payload
+$(CONFIG_CBFS_PREFIX)/payload-type := simple_elf
 endif
 $(CONFIG_CBFS_PREFIX)/payload-compression := $(CBFS_PAYLOAD_COMPRESS_FLAG)
 $(CONFIG_CBFS_PREFIX)/payload-options := $(ADDITIONAL_PAYLOAD_CONFIG)
@@ -184,14 +184,14 @@ payloads/external/tint/tint/tint.elf tint:
 
 cbfs-files-$(CONFIG_TINT_SECONDARY_PAYLOAD) += img/tint
 img/tint-file := payloads/external/tint/tint/tint.elf
-img/tint-type := payload
+img/tint-type := simple_elf
 img/tint-compression := $(CBFS_SECONDARY_PAYLOAD_COMPRESS_FLAG)
 
 # Memtest86+
 
 cbfs-files-$(CONFIG_MEMTEST_SECONDARY_PAYLOAD) += img/memtest
 img/memtest-file := payloads/external/Memtest86Plus/memtest86plus/memtest
-img/memtest-type := payload
+img/memtest-type := simple_elf
 img/memtest-compression := $(CBFS_SECONDARY_PAYLOAD_COMPRESS_FLAG)
 
 ifeq ($(CONFIG_CONSOLE_SERIAL)$(CONFIG_DRIVERS_UART_8250IO),yy)

src/arch/arm64/Makefile.inc
@@ -194,7 +194,7 @@ $(BL31): $(obj)/build.h
 
 BL31_CBFS := $(CONFIG_CBFS_PREFIX)/bl31
 $(BL31_CBFS)-file := $(BL31)
-$(BL31_CBFS)-type := payload
+$(BL31_CBFS)-type := simple_elf
 $(BL31_CBFS)-compression := $(CBFS_COMPRESS_FLAG)
 cbfs-files-y += $(BL31_CBFS)
 

util/cbfstool/cbfs.h
@@ -204,7 +204,7 @@ static struct typedesc_t filetypes[] unused = {
 	{CBFS_COMPONENT_BOOTBLOCK, "bootblock"},
 	{CBFS_COMPONENT_CBFSHEADER, "cbfs header"},
 	{CBFS_COMPONENT_STAGE, "stage"},
-	{CBFS_COMPONENT_SELF, "simple elf"},
+	{CBFS_COMPONENT_SELF, "simple_elf"},
 	{CBFS_COMPONENT_OPTIONROM, "optionrom"},
 	{CBFS_COMPONENT_BOOTSPLASH, "bootsplash"},
 	{CBFS_COMPONENT_RAW, "raw"},
86c92ba0422a7075af2f0f1e6da791fc0c6f7aad
Elyes HAOUAS <ehaouas@noos.fr>
2018-05-19 12:07:10 +0200

    sb/broadcom/bcm5780: Get rid of device_t
    Use of device_t has been abandoned in ramstage.
    
    Change-Id: Ia46b909c78086d9417cabc1cd65e16d264a8df8e
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/26402
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

--- src/southbridge/broadcom/bcm5780/nic.c
+++ src/southbridge/broadcom/bcm5780/nic.c
@@ -21,7 +21,8 @@
 #include <device/pci_ops.h>
 
 
-static void lpci_set_subsystem(device_t dev, unsigned vendor, unsigned device)
+static void lpci_set_subsystem(struct device *dev, unsigned vendor,
+			       unsigned device)
 {
 	pci_write_config32(dev, 0x40,
 		((device & 0xffff) << 16) | (vendor & 0xffff));

b28f466a7bde9d5b928982e6e6f27005966711ad
Martin Roth <martinroth@google.com>
2018-05-26 17:58:47 -0600

    src/mainboard: Add and update license headers
    This change adds and updates headers in all of the mainboard files that
    had missing or unrecognized headers.  After this goes in, we can turn on
    lint checking for headers in all mainboard directories.
    
    Change-Id: Ibe038a8f7468253b21fd2ac90c045d0c9cc89dfc
    Signed-off-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://review.coreboot.org/26568
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/mainboard/cubietech/cubieboard/bootblock.c
+++ src/mainboard/cubietech/cubieboard/bootblock.c
@@ -1,9 +1,22 @@
 /*
- * Minimal bootblock for Cubieboard
- * It sets up CPU clock, and enables the bootblock console.
+ * This file is part of the coreboot project.
  *
  * Copyright (C) 2013  Alexandru Gagniuc <mr.nuke.me@gmail.com>
- * Subject to the GNU GPL v2, or (at your option) any later version.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
+ * Minimal bootblock for Cubieboard
+ * It sets up CPU clock, and enables the bootblock console.
  */
 
 #include <arch/io.h>
--- src/mainboard/lenovo/g505s/mainboard.h
+++ src/mainboard/lenovo/g505s/mainboard.h
@@ -1,8 +1,21 @@
 /*
- * "The way things are connected" and a few setup options
+ * This file is part of the coreboot project.
  *
  * Copyright (C) 2014 Alexandru Gagniuc <mr.nuke.me@gmail.com>
- * Subject to the GNU GPL v2, or (at your option) any later version.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
+ * "The way things are connected" and a few setup options
  */
 
 #ifndef _MAINBOARD_LENOVO_G505S_MAINBOARD_H

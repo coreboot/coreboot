85c34750cf4d70ac0b6277950cb428185c962af7
Tristan Shieh <tristan.shieh@mediatek.com>
2018-06-05 12:20:00 +0800

    mediatek/mt8183: Add mtcmos init support
    Using common mtcmos code to power on audio and display modules in SOC.
    
    BUG=b:80501386
    BRANCH=none
    TEST=Boots correctly on Kukui. Passes the status check at the end of
         mtcmos_power_on()
    
    Change-Id: I41f16ba36432a8bbc47793cec2979753c9f84b43
    Signed-off-by: Tristan Shieh <tristan.shieh@mediatek.com>
    Reviewed-on: https://review.coreboot.org/27030
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- src/soc/mediatek/mt8183/mtcmos.c
+++ src/soc/mediatek/mt8183/mtcmos.c
@@ -13,12 +13,22 @@
  * GNU General Public License for more details.
  */
 
-#ifndef __SOC_MEDIATEK_COMMON_MTCMOS_H__
-#define __SOC_MEDIATEK_COMMON_MTCMOS_H__
+#include <arch/io.h>
+#include <soc/infracfg.h>
+#include <soc/smi.h>
+#include <soc/mtcmos.h>
 
-void mtcmos_audio_power_on(void);
-void mtcmos_display_power_on(void);
+enum {
+	DISP_PROT_STEP1_0_MASK	= 0x3 << 16,
+	DISP_PROT_STEP2_0_MASK	= 0x3 << 10,
+	DISP_PROT_STEP2_1_MASK	= 0xff,
+};
 
-void mtcmos_protect_display_bus(void);
-
-#endif /* __SOC_MEDIATEK_COMMON_MTCMOS_H__ */
+void mtcmos_protect_display_bus(void)
+{
+	write32(&mt8183_infracfg->infra_topaxi_protecten_clr,
+		DISP_PROT_STEP2_0_MASK);
+	write32(&mt8183_smi->smi_common_clamp_en_clr, DISP_PROT_STEP2_1_MASK);
+	write32(&mt8183_infracfg->infra_topaxi_protecten_1_clr,
+		DISP_PROT_STEP1_0_MASK);
+}

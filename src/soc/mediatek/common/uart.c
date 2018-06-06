362a73409103ae076172ea58f3f1c1a8fbdc6b00
Tristan Shieh <tristan.shieh@mediatek.com>
2018-06-06 14:03:22 +0800

    mediatek: Move uart, timer and cbmem code to a common directory.
    This patch moves uart, timer and cbmem code which can be reused into a
    common directory under soc/mediatek.
    
    BUG=b:80501386
    BRANCH=none
    TEST=the refactored code works fine on the new platform (with the rest
         of the patches applied) and Elm platform
    
    Change-Id: I5210149b324947ee90f1a481b42f0e2e1f7cfc25
    Signed-off-by: Tristan Shieh <tristan.shieh@mediatek.com>
    Reviewed-on: https://review.coreboot.org/26658
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- src/soc/mediatek/common/uart.c
+++ src/soc/mediatek/common/uart.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the coreboot project.
  *
- * Copyright 2015 MediaTek Inc.
+ * Copyright 2018 MediaTek Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

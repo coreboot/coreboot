faad9684a9882c6521913872d39222221e6c871a
Furquan Shaikh <furquan@google.com>
2018-06-19 08:40:19 -0700

    mb/google/octopus: Configure EC_IN_RW correctly
    This change fixes the following issues with EC_IN_RW signal:
    1. EC_IN_RW is an input signal to the SoC. Configure it accordingly in
    GPIO table for baseboard and bip.
    2. GPIO_EC_IN_RW is passed in coreboot tables so that payload can
    re-sample the GPIO at runtime.
    
    BUG=b:110084012
    TEST=Verified that EC_IN_RW signal is read correctly in depthcharge.
    
    Change-Id: I1c5f5b4b914ced98e89a571dc398df5ba1fe8460
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/27166
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>

--- src/mainboard/google/octopus/chromeos.c
+++ src/mainboard/google/octopus/chromeos.c
@@ -29,7 +29,8 @@ void fill_lb_gpios(struct lb_gpios *gpios)
 		{-1, ACTIVE_HIGH, get_lid_switch(), "lid"},
 		{-1, ACTIVE_HIGH, 0, "power"},
 		{-1, ACTIVE_HIGH, gfx_get_init_done(), "oprom"},
-		{-1, ACTIVE_HIGH, gpio_get(GPIO_EC_IN_RW), "EC in RW"},
+		{GPIO_EC_IN_RW, ACTIVE_HIGH, gpio_get(GPIO_EC_IN_RW),
+		 "EC in RW"},
 	};
 	lb_add_gpios(gpios, chromeos_gpios, ARRAY_SIZE(chromeos_gpios));
 }

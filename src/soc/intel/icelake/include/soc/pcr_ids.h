c7267631e23ec113de898bf07b5f6f212acc3629
Aamir Bohra <aamir.bohra@intel.com>
2018-06-30 12:38:43 +0530

    soc/intel/icelake: Add PID based on Icelake EDS
    Change-Id: I2d9e06f06a39dc76a3c1351d7976505d7bd92d10
    Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Signed-off-by: Aamir Bohra <aamir.bohra@intel.com>
    Reviewed-on: https://review.coreboot.org/29436
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/soc/intel/icelake/include/soc/pcr_ids.h
+++ src/soc/intel/icelake/include/soc/pcr_ids.h
@@ -20,11 +20,14 @@
  */
 #define PID_EMMC	0x52
 #define PID_SDX		0x53
-#define PID_GPIOCOM4	0x6a
-#define PID_GPIOCOM3	0x6b
-#define PID_GPIOCOM2	0x6c
-#define PID_GPIOCOM1	0x6d
+
 #define PID_GPIOCOM0	0x6e
+#define PID_GPIOCOM1	0x6d
+#define PID_GPIOCOM2	0x6c
+#define PID_GPIOCOM3	0x6b
+#define PID_GPIOCOM4	0x6a
+#define PID_GPIOCOM5	0x69
+
 #define PID_DMI		0x88
 #define PID_PSTH	0x89
 #define PID_CSME0	0x90
@@ -35,7 +38,7 @@
 #define PID_PSF4	0xbd
 #define PID_SCS		0xc0
 #define PID_RTC		0xc3
-#define PID_ITSS	0xc2
+#define PID_ITSS	0xc4
 #define PID_LPC		0xc7
 #define PID_SERIALIO	0xcb
 

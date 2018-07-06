131ff8ccec9677144cbed19dba3f4ef1851cce8f
Tristan Shieh <tristan.shieh@mediatek.com>
2018-07-06 15:37:05 +0800

    google/kukui: Add SPI NOR support
    This patch sets SPI flash related configs and inits SPI bus 1 to
    support SPI NOR flash.
    
    BUG=b:80501386
    BRANCH=none
    TEST=Boots correctly on Kukui
    
    Change-Id: I1a18a456f41a7c7daec954e961c9fbee3650493d
    Signed-off-by: Tristan Shieh <tristan.shieh@mediatek.com>
    Reviewed-on: https://review.coreboot.org/27499
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

---
+++ src/mainboard/google/kukui/bootblock.c
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <bootblock_common.h>
+#include <soc/spi.h>
+
+void bootblock_mainboard_init(void)
+{
+	mtk_spi_init(CONFIG_BOOT_DEVICE_SPI_FLASH_BUS, SPI_PAD0_MASK, 26 * MHz);
+}

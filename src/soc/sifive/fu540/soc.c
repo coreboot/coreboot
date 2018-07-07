c014ef59191c512100a0596f998ffb9926fc8eb0
Philipp Hug <philipp@hug.cx>
2018-07-07 14:23:01 +0200

    soc/sifive/fu540: create ram_resource with actual memory size
    Change-Id: If6af6f679e24e56c79b995de0970d4e6f455e40a
    Signed-off-by: Philipp Hug <philipp@hug.cx>
    Reviewed-on: https://review.coreboot.org/28592
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ src/soc/sifive/fu540/soc.c
@@ -0,0 +1,20 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Philipp Hug <philipp@hug.cx>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+static void soc_enable(struct device *dev)
+{
+	ram_resource(dev, 0, (uintptr_t)_dram/KiB,
+		sdram_size_mb()*KiB);
+}

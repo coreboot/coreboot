94d79a338bb7f035257d3e3137c315eb1b9ea3ae
Arthur Heymans <arthur@aheymans.xyz>
2018-07-17 16:43:16 +0200

    mb/lenovo/x201: Allow use of libgfxinit
    Tested on LVDS (internal), VGA and HDMI (on the dock DP++ connector) output with
    both native resolution and textmode.
    
    Change-Id: Ibfcb586d7b4cac7f1d22540bb96c288704a277a0
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/27513
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/lenovo/x201/gma-mainboard.ads
@@ -0,0 +1,30 @@
+--
+-- This file is part of the coreboot project.
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+
+with HW.GFX.GMA;
+with HW.GFX.GMA.Display_Probing;
+
+use HW.GFX.GMA;
+use HW.GFX.GMA.Display_Probing;
+
+private package GMA.Mainboard is
+
+   ports : constant Port_List :=
+     (DP2, -- DP++ connector on the dock
+      HDMI2,
+      Analog,
+      Internal,
+      others => Disabled);
+
+end GMA.Mainboard;

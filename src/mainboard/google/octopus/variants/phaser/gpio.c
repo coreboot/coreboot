0a144fa583b270d7b96a3b96599567d52336c958
Furquan Shaikh <furquan@google.com>
2018-07-25 15:04:56 -0700

    mb/google/octopus/variants/phaser: Provide override GPIO config
    This change provides override GPIO table for variant phaser depending
    upon the board id. Additionally, early_gpio_table is also provided for
    phaser since EN_PP3300_WLAN needs to be handled differently based on
    board id.
    
    BUG=b:111743717
    TEST=Verified that GPIO configuration for the override GPIOs is
    different than before with this change.
    
    Change-Id: I4d2e829e1b886299442c17cecc069854b742b43c
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/27642
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/mainboard/google/octopus/variants/phaser/gpio.c
@@ -0,0 +1,113 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <boardid.h>
+#include <gpio.h>
+#include <soc/gpio.h>
+
+static const struct pad_config default_override_table[] = {
+	PAD_NC(GPIO_52, UP_20K),
+	PAD_NC(GPIO_53, UP_20K),
+	PAD_NC(GPIO_67, UP_20K),
+	PAD_NC(GPIO_117, UP_20K),
+	PAD_NC(GPIO_143, UP_20K),
+
+	/* EN_PP3300_TOUCHSCREEN */
+	PAD_CFG_GPO_IOSSTATE_IOSTERM(GPIO_146, 0, DEEP, NONE, Tx0RxDCRx0,
+					DISPUPD),
+
+	PAD_NC(GPIO_161, DN_20K),
+
+	/* EN_PP3300_WLAN_L */
+	PAD_CFG_GPO(GPIO_178, 0, DEEP),
+
+	PAD_NC(GPIO_213, DN_20K),
+	PAD_NC(GPIO_214, DN_20K),
+};
+
+static const struct pad_config bid0_override_table[] = {
+	PAD_NC(GPIO_52, UP_20K),
+	PAD_NC(GPIO_53, UP_20K),
+	PAD_NC(GPIO_67, UP_20K),
+	PAD_NC(GPIO_117, UP_20K),
+	PAD_NC(GPIO_143, UP_20K),
+	PAD_NC(GPIO_161, DN_20K),
+	PAD_NC(GPIO_214, DN_20K),
+};
+
+const struct pad_config *variant_override_gpio_table(size_t *num)
+{
+	const struct pad_config *c;
+
+	switch (board_id()) {
+	case 0:
+	case UNDEFINED_STRAPPING_ID:
+		c = bid0_override_table;
+		*num = ARRAY_SIZE(bid0_override_table);
+		break;
+	default:
+		c = default_override_table;
+		*num = ARRAY_SIZE(default_override_table);
+	}
+
+	return c;
+}
+
+/* GPIOs needed prior to ramstage. */
+static const struct pad_config early_gpio_table[] = {
+	/* PCH_WP_OD */
+	PAD_CFG_GPI(GPIO_190, NONE, DEEP),
+	/* H1_PCH_INT_ODL */
+	PAD_CFG_GPI_APIC_IOS(GPIO_63, NONE, DEEP, EDGE_SINGLE, INVERT, TxDRxE,
+		DISPUPD),
+	/* H1_SLAVE_SPI_CLK_R */
+	PAD_CFG_NF(GPIO_79, NONE, DEEP, NF1),
+	/* H1_SLAVE_SPI_CS_L_R */
+	PAD_CFG_NF(GPIO_80, NONE, DEEP, NF1),
+	/* H1_SLAVE_SPI_MISO */
+	PAD_CFG_NF(GPIO_82, NONE, DEEP, NF1),
+	/* H1_SLAVE_SPI_MOSI_R */
+	PAD_CFG_NF(GPIO_83, NONE, DEEP, NF1),
+
+	/* WLAN_PE_RST */
+	PAD_CFG_GPO(GPIO_164, 0, DEEP),
+
+	/*
+	 * ESPI_IO1 acts as ALERT# (which is open-drain) and requies a weak
+	 * pull-up for proper operation. Since there is no external pull present
+	 * on this platform, configure an internal weak pull-up.
+	 */
+	PAD_CFG_NF_IOSSTATE_IOSTERM(GPIO_151, UP_20K, DEEP, NF2, HIZCRx1,
+					ENPU),
+};
+
+const struct pad_config *variant_early_gpio_table(size_t *num)
+{
+	/*
+	 * This is a hack to configure EN_PP3300_WLAN based on board id. Once
+	 * board id 0 is deprecated, we can get rid of this.
+	 */
+	uint32_t bid = board_id();
+
+	if (bid == UNDEFINED_STRAPPING_ID || bid < 1)
+		gpio_output(GPIO_178, 1);
+	else
+		gpio_output(GPIO_178, 0);
+
+	*num = ARRAY_SIZE(early_gpio_table);
+	return early_gpio_table;
+}

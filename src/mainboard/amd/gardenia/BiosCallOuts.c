90b2cca81fcb38e2bda94c8715519d1aa4a9d8c1
Richard Spiegel <richard.spiegel@amd.corp-partner.google.com>
2018-07-26 10:16:50 -0700

    mb/amd/gardenia: Remove IMC support
    Per AMD, the Integrated Micro Controller is not a supported feature of the
    Stoney Ridge APU.  Systems are expected to implement an external EC for
    desired features. Remove IMC files and functions from gardenia.
    
    BUG=b:111780177
    TEST=Build gardenia
    
    Change-Id: I570b7f8e364b0c2937592590cc033d5a6c9fade0
    Signed-off-by: Richard Spiegel <richard.spiegel@silverbackltd.com>
    Reviewed-on: https://review.coreboot.org/27650
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Reviewed-by: Martin Roth <martinroth@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/amd/gardenia/BiosCallOuts.c
+++ src/mainboard/amd/gardenia/BiosCallOuts.c
@@ -15,32 +15,9 @@
 
 #include <amdblocks/agesawrapper.h>
 #include <amdblocks/BiosCallOuts.h>
-#include <soc/imc.h>
 #include <soc/southbridge.h>
 #include <stdlib.h>
 #include <string.h>
-
-/* Hardware Monitor Fan Control
- * Hardware limitation:
- *  HWM will fail to read the input temperature via I2C if other
- *  software switches the I2C address.  AMD recommends using IMC
- *  to control fans, instead of HWM.
- */
-void oem_fan_control(FCH_DATA_BLOCK *FchParams)
-{
-	/* Enable IMC fan control. the recommand way */
-	imc_reg_init();
-
-	FchParams->Imc.ImcEnable = TRUE;
-
-	/* 1 IMC, 0 HWM */
-	FchParams->Hwm.HwmControl = 1;
-
-	/* 2 disable IMC, 1 enable IMC, 0 following hw strap setting */
-	FchParams->Imc.ImcEnableOverWrite = 1;
-
-	memset(&FchParams->Imc.EcStruct, 0, sizeof(FCH_EC));
-}
 
 void platform_FchParams_env(FCH_DATA_BLOCK *FchParams_env)
 {

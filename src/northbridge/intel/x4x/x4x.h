6cd2c2f6ff792d1a170cd090e3347cfe2e14ac15
Felix Held <felix-coreboot@felixheld.de>
2018-07-29 18:04:14 +0200

    northbridge/x4x: add MCHBAR AND/OR/AND_OR access macros
    Change-Id: Ie95321f3eb6fb17b17eb25e8a54670654c373706
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/27716
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/northbridge/intel/x4x/x4x.h
+++ src/northbridge/intel/x4x/x4x.h
@@ -85,6 +85,18 @@
 #define MCHBAR8(x) (*((volatile u8 *)(DEFAULT_MCHBAR + (x))))
 #define MCHBAR16(x) (*((volatile u16 *)(DEFAULT_MCHBAR + (x))))
 #define MCHBAR32(x) (*((volatile u32 *)(DEFAULT_MCHBAR + (x))))
+#define MCHBAR8_AND(x, and) (MCHBAR8(x) = MCHBAR8(x) & (and))
+#define MCHBAR8_OR(x, or) (MCHBAR8(x) = MCHBAR8(x) | (or))
+#define MCHBAR8_AND_OR(x, and, or) \
+	(MCHBAR8(x) = (MCHBAR8(x) & (and)) | (or))
+#define MCHBAR16_AND(x, and) (MCHBAR16(x) = MCHBAR16(x) & (and))
+#define MCHBAR16_OR(x, or) (MCHBAR16(x) = MCHBAR16(x) | (or))
+#define MCHBAR16_AND_OR(x, and, or) \
+	(MCHBAR16(x) = (MCHBAR16(x) & (and)) | (or))
+#define MCHBAR32_AND(x, and) (MCHBAR32(x) = MCHBAR32(x) & (and))
+#define MCHBAR32_OR(x, or) (MCHBAR32(x) = MCHBAR32(x) | (or))
+#define MCHBAR32_AND_OR(x, and, or) \
+	(MCHBAR32(x) = (MCHBAR32(x) & (and)) | (or))
 
 #define CHDECMISC	0x111
 #define STACKED_MEM	(1 << 1)

2dd7b6b2f9432141e182738f8f779016aaa805b5
Nick Vaccaro <nvaccaro@google.com>
2018-08-09 16:18:02 -0700

    mb/google/poppy/variant/nocturne: update PL2 based on CPU sku
    This patch adds a function to overwrite PL2 setting based on CPU
    sku. From doc #594883, PL2 is 18W for AML-Y.
    
    BUG=b:110890675
    BRANCH=None
    TEST=emerge-nocturne coreboot chromeos-bootimage & test with AML-Y
         and KBL-Y skus.
    
    Change-Id: Idfdc0c2434fdef56a7c25df05e640837a5096973
    Signed-off-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-on: https://review.coreboot.org/27997
    Reviewed-by: Caveh Jalali <caveh@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/poppy/variants/nocturne/mainboard.c
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2018 Google Inc.
+ * Copyright (C) 2018 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <baseboard/variants.h>
+#include <chip.h>
+#include <device/device.h>
+#include <device/pci_ids.h>
+#include <device/pci_ops.h>
+
+/* PL2 limit in watts for AML and KBL */
+#define PL2_AML	18
+#define PL2_KBL	15
+
+static uint32_t get_pl2(void)
+{
+	uint16_t id;
+	id = pci_read_config16(SA_DEV_IGD, PCI_DEVICE_ID);
+	/* Assume we only have KLB-Y and AML-Y SKUs */
+	if (id == PCI_DEVICE_ID_INTEL_KBL_GT2_SULXM)
+		return PL2_KBL;
+
+	return PL2_AML;
+}
+
+/* Override dev tree settings per board */
+void variant_devtree_update(void)
+{
+	struct device *root = SA_DEV_ROOT;
+	config_t *cfg = root->chip_info;
+
+	/* Update PL2 based on CPU */
+	cfg->tdp_pl2_override = get_pl2();
+}

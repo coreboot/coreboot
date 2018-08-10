ff23d21e33233d2e1d6cb23467fcff2a894b328b
Furquan Shaikh <furquan@google.com>
2018-08-10 11:30:18 -0700

    mb/google/octopus/var/fleex: Update GPIO config for fleex bid >= 1
    This change updates GPIO configuration for fleex boards with id >= 1
    This follows the same model as phaser:
    a. Dynamically update touchscreen power enable GPIO in devicetree.
    b. Provide default and bid0 tables for GPIO configuration in ramstage.
    c. Configure WLAN enable GPIO differently in bootblock based on
    boardid.
    d. Disable unused I2C devices in devicetree.
    
    BUG=b:112458032
    TEST=No errors observed on boot-up on fleex.
    
    Change-Id: Ib4c449168b08e2393e2395d6b49469be5599c2ce
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/28015
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

---
+++ src/mainboard/google/octopus/variants/fleex/variant.c
@@ -0,0 +1,73 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/acpi_device.h>
+#include <baseboard/variants.h>
+#include <boardid.h>
+#include <device/device.h>
+#include <drivers/i2c/generic/chip.h>
+#include <drivers/i2c/hid/chip.h>
+#include <soc/gpio.h>
+#include <soc/pci_devs.h>
+#include <string.h>
+
+extern struct chip_operations drivers_i2c_generic_ops;
+extern struct chip_operations drivers_i2c_hid_ops;
+
+void variant_update_devtree(struct device *dev)
+{
+	uint32_t bid;
+	struct device *touchscreen_i2c_host;
+	struct device *child;
+	const struct bus *children_bus;
+	static const struct acpi_gpio new_enable_gpio =
+		ACPI_GPIO_OUTPUT_ACTIVE_HIGH(GPIO_146);
+
+	bid = board_id();
+
+	/* Nothing to update. */
+	if (bid == UNDEFINED_STRAPPING_ID || bid < 1)
+		return;
+
+	touchscreen_i2c_host = dev_find_slot(0, PCH_DEVFN_I2C7);
+
+	if (touchscreen_i2c_host == NULL)
+		return;
+
+	children_bus = touchscreen_i2c_host->link_list;
+	child = NULL;
+
+	/* Find all children on bus to update touchscreen enable gpio. */
+	while ((child = dev_bus_each_child(children_bus, child)) != NULL) {
+		struct drivers_i2c_generic_config *cfg;
+
+		/* No configration to change. */
+		if (child->chip_info == NULL)
+			continue;
+
+		if (child->chip_ops == &drivers_i2c_generic_ops)
+			cfg = child->chip_info;
+		else if (child->chip_ops == &drivers_i2c_hid_ops) {
+			struct drivers_i2c_hid_config *hid_cfg;
+			hid_cfg = child->chip_info;
+			cfg = &hid_cfg->generic;
+		} else
+			continue;
+
+		/* Update the enable gpio. */
+		memcpy(&cfg->enable_gpio, &new_enable_gpio,
+			sizeof(new_enable_gpio));
+	}
+}

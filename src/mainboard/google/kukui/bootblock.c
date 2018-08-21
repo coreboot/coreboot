990d825196227190d3625c3b44a082816748bc3b
Tristan Shieh <tristan.shieh@mediatek.com>
2018-08-21 13:40:23 +0800

    google/kukui: Init SPI bus for EC
    Set EC SPI bus config and init SPI bus according to the config.
    
    BUG=b:80501386
    BRANCH=none
    TEST=EC is not working yet. This makes depthcharge go forward a little.
    
    Change-Id: Id9209b6429417430cfcf7f5a5a1659e7e4bc7866
    Signed-off-by: Tristan Shieh <tristan.shieh@mediatek.com>
    Reviewed-on: https://review.coreboot.org/28251
    Reviewed-by: Joel Kitching <kitching@google.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/kukui/bootblock.c
+++ src/mainboard/google/kukui/bootblock.c
@@ -29,5 +29,6 @@ void bootblock_mainboard_init(void)
 	/* Turn on real eMMC. */
 	gpio_output(BOOTBLOCK_EN_L, 1);
 
+	mtk_spi_init(CONFIG_EC_GOOGLE_CHROMEEC_SPI_BUS, SPI_PAD0_MASK, 6 * MHz);
 	mtk_spi_init(CONFIG_BOOT_DEVICE_SPI_FLASH_BUS, SPI_PAD0_MASK, 26 * MHz);
 }

fcd70085c272678c35cea1d44a0a79f1304590b2
Elyes HAOUAS <ehaouas@noos.fr>
2018-08-23 18:19:52 +0200

    util/nvramtool: Fix typos & remove unneeded whitespace
    Change-Id: I0a704cba80d0439ae95db34a6b73df7be5b3b862
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/28290
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- util/nvramtool/coreboot_tables.h
+++ util/nvramtool/coreboot_tables.h
@@ -8,35 +8,35 @@
 #include <stdint.h>
 
 /* Note: The contents of this file were borrowed from the coreboot source
- *       code which may be obtained from https://www.coreboot.org/.
- *       Specifically, this code was obtained from LinuxBIOS version 1.1.8.
+ * code which may be obtained from https://www.coreboot.org/.
+ * Specifically, this code was obtained from LinuxBIOS version 1.1.8.
  */
 
 /* The coreboot table information is for conveying information
- * from the firmware to the loaded OS image.  Primarily this
+ * from the firmware to the loaded OS image. Primarily this
  * is expected to be information that cannot be discovered by
- * other means, such as quering the hardware directly.
+ * other means, such as querying the hardware directly.
  *
  * All of the information should be Position Independent Data.
- * That is it should be safe to relocated any of the information
- * without it's meaning/correctnes changing.   For table that
+ * That is, it should be safe to relocate any of the information
+ * without changing its meaning/correctness. For tables that
  * can reasonably be used on multiple architectures the data
- * size should be fixed.  This should ease the transition between
+ * size should be fixed. This should ease the transition between
  * 32 bit and 64 bit architectures etc.
  *
  * The completeness test for the information in this table is:
  * - Can all of the hardware be detected?
- * - Are the per motherboard constants available?
+ * - Are the per-motherboard constants available?
  * - Is there enough to allow a kernel to run that was written before
  *   a particular motherboard is constructed? (Assuming the kernel
  *   has drivers for all of the hardware but it does not have
  *   assumptions on how the hardware is connected together).
  *
- * With this test it should be straight forward to determine if a
- * table entry is required or not.  This should remove much of the
- * long term compatibility burden as table entries which are
+ * With this test it should be straightforward to determine if a
+ * table entry is required or not. This should remove much of the
+ * long-term compatibility burden as table entries which are
  * irrelevant or have been replaced by better alternatives may be
- * dropped.  Of course it is polite and expidite to include extra
+ * dropped. Of course it is polite and expedite to include extra
  * table entries and be backwards compatible, but it is not required.
  */
 
@@ -85,10 +85,10 @@ struct lb_header {
 	uint32_t table_entries;
 };
 
-/* Every entry in the boot enviroment list will correspond to a boot
- * info record.  Encoding both type and size.  The type is obviously
- * so you can tell what it is.  The size allows you to skip that
- * boot enviroment record if you don't know what it easy.  This allows
+/* Every entry in the boot environment list will correspond to a boot
+ * info record, encoding both type and size. The type is obviously
+ * so you can tell what it is. The size allows you to skip that
+ * boot environment record if you don't know what it is. This allows
  * forward compatibility with records not yet defined.
  */
 struct lb_record {
@@ -96,17 +96,17 @@ struct lb_record {
 	uint32_t size;		/* size of record (in bytes) */
 };
 
-#define LB_TAG_UNUSED        0x0000
+#define LB_TAG_UNUSED		0x0000
 
-#define LB_TAG_MEMORY        0x0001
+#define LB_TAG_MEMORY		0x0001
 
 struct lb_memory_range {
 	struct lb_uint64 start;
 	struct lb_uint64 size;
 	uint32_t type;
-#define LB_MEM_RAM       1	/* Memory anyone can use */
-#define LB_MEM_RESERVED  2	/* Don't use this memory region */
-#define LB_MEM_TABLE     16	/* Ram configuration tables are kept in */
+#define LB_MEM_RAM	1	/* Memory anyone can use */
+#define LB_MEM_RESERVED	2	/* Don't use this memory region */
+#define LB_MEM_TABLE	16	/* RAM configuration tables are kept in */
 };
 
 struct lb_memory {
@@ -115,14 +115,14 @@ struct lb_memory {
 	struct lb_memory_range map[0];
 };
 
-#define LB_TAG_HWRPB            0x0002
+#define LB_TAG_HWRPB		0x0002
 struct lb_hwrpb {
 	uint32_t tag;
 	uint32_t size;
 	uint64_t hwrpb;
 };
 
-#define LB_TAG_MAINBOARD        0x0003
+#define LB_TAG_MAINBOARD	0x0003
 struct lb_mainboard {
 	uint32_t tag;
 	uint32_t size;
@@ -131,16 +131,16 @@ struct lb_mainboard {
 	uint8_t strings[0];
 };
 
-#define LB_TAG_VERSION          0x0004
-#define LB_TAG_EXTRA_VERSION    0x0005
-#define LB_TAG_BUILD            0x0006
-#define LB_TAG_COMPILE_TIME     0x0007
-#define LB_TAG_COMPILE_BY       0x0008
-#define LB_TAG_COMPILE_HOST     0x0009
-#define LB_TAG_COMPILE_DOMAIN   0x000a
-#define LB_TAG_COMPILER         0x000b
-#define LB_TAG_LINKER           0x000c
-#define LB_TAG_ASSEMBLER        0x000d
+#define LB_TAG_VERSION		0x0004
+#define LB_TAG_EXTRA_VERSION	0x0005
+#define LB_TAG_BUILD		0x0006
+#define LB_TAG_COMPILE_TIME	0x0007
+#define LB_TAG_COMPILE_BY	0x0008
+#define LB_TAG_COMPILE_HOST	0x0009
+#define LB_TAG_COMPILE_DOMAIN	0x000a
+#define LB_TAG_COMPILER		0x000b
+#define LB_TAG_LINKER		0x000c
+#define LB_TAG_ASSEMBLER	0x000d
 struct lb_string {
 	uint32_t tag;
 	uint32_t size;
@@ -155,22 +155,22 @@ struct lb_forward {
 	uint64_t forward;
 };
 
-/* The following structures are for the cmos definitions table */
+/* The following structures are for the CMOS definitions table */
 #define LB_TAG_CMOS_OPTION_TABLE 200
-/* cmos header record */
+/* CMOS header record */
 struct cmos_option_table {
 	uint32_t tag;		/* CMOS definitions table type */
 	uint32_t size;		/* size of the entire table */
 	uint32_t header_length;	/* length of header */
 };
 
-/* cmos entry record
-        This record is variable length.  The name field may be
-        shorter than CMOS_MAX_NAME_LENGTH. The entry may start
-        anywhere in the byte, but can not span bytes unless it
-        starts at the beginning of the byte and the length is
-        fills complete bytes.
-*/
+/* CMOS entry record
+ * This record has a variable length. The name field may be
+ * shorter than CMOS_MAX_NAME_LENGTH. The entry may start
+ * anywhere in the byte, but can not span bytes unless it
+ * starts at the beginning of the byte and the length
+ * fills complete bytes.
+ */
 #define LB_TAG_OPTION 201
 struct cmos_entries {
 	uint32_t tag;		/* entry type */
@@ -184,10 +184,10 @@ struct cmos_entries {
 						   variable length int aligned */
 };
 
-/* cmos enumerations record
-        This record is variable length.  The text field may be
-        shorter than CMOS_MAX_TEXT_LENGTH.
-*/
+/* CMOS enumerations record
+ * This record has a variable length. The text field may be
+ * shorter than CMOS_MAX_TEXT_LENGTH.
+ */
 #define LB_TAG_OPTION_ENUM 202
 struct cmos_enums {
 	uint32_t tag;		/* enumeration type */
@@ -199,9 +199,9 @@ struct cmos_enums {
 						   variable length int aligned */
 };
 
-/* cmos defaults record
-        This record contains default settings for the cmos ram.
-*/
+/* CMOS default record
+ * This record contains default settings for the CMOS RAM.
+ */
 #define LB_TAG_OPTION_DEFAULTS 203
 struct cmos_defaults {
 	uint32_t tag;		/* default type */

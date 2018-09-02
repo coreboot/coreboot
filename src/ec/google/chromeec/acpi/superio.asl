lalilulelulz/Chromium-OS-Coreboot

chromeec: PS2K node can't be under SIO node
Some operating systems won't find the keyboard if it is under
the SIO node.

Signed-off-by: Stefan Reinauer <reinauer@google.com>
BRANCH=none
BUG=none
TEST=Boot Windows, observe that keyboard is working

Change-Id: I76b1ca9bf9243ffa861bed9c356a45377e7f43ef
Reviewed-on: https://chromium-review.googlesource.com/827785
Reviewed-by: Duncan Laurie <dlaurie@google.com>
Reviewed-by: Furquan Shaikh <furquan@chromium.org>
Commit-Queue: Duncan Laurie <dlaurie@google.com>
Tested-by: Duncan Laurie <dlaurie@google.com>
Trybot-Ready: Duncan Laurie <dlaurie@google.com>

src/ec/google/chromeec/acpi/superio.asl
@@ -131,21 +131,34 @@ Device (SIO) {
 		})
 	}
 #endif
+}
 
 #ifdef SIO_EC_ENABLE_PS2K
-	Device (PS2K)		// Keyboard
+Device (PS2K)		// Keyboard
+{
+	Name (_UID, 0)
+	Name (_ADR, 0)
+	Name (_HID, EISAID("PNP0303"))
+	Name (_CID, EISAID("PNP030B"))
+
+	Method (_STA, 0, NotSerialized) {
+		Return (0x0F)
+	}
+
+	Name (_CRS, ResourceTemplate()
 	{
-		Name (_UID, 0)
-		Name (_ADR, 0)
-		Name (_HID, EISAID("PNP0303"))
-		Name (_CID, EISAID("PNP030B"))
+		IO (Decode16, 0x60, 0x60, 0x01, 0x01)
+		IO (Decode16, 0x64, 0x64, 0x01, 0x01)
+#ifdef SIO_EC_PS2K_IRQ
+		SIO_EC_PS2K_IRQ
+#else
+		IRQ (Edge, ActiveHigh, ExclusiveAndWake) {1}
+#endif
+	})
 
-		Method (_STA, 0, NotSerialized) {
-			Return (0x0F)
-		}
-
-		Name (_CRS, ResourceTemplate()
-		{
+	Name (_PRS, ResourceTemplate()
+	{
+		StartDependentFn (0, 0) {
 			IO (Decode16, 0x60, 0x60, 0x01, 0x01)
 			IO (Decode16, 0x64, 0x64, 0x01, 0x01)
 #ifdef SIO_EC_PS2K_IRQ
@@ -153,21 +166,8 @@ Device (SIO) {
 #else
 			IRQ (Edge, ActiveHigh, ExclusiveAndWake) {1}
 #endif
-		})
-
-		Name (_PRS, ResourceTemplate()
-		{
-			StartDependentFn (0, 0) {
-				IO (Decode16, 0x60, 0x60, 0x01, 0x01)
-				IO (Decode16, 0x64, 0x64, 0x01, 0x01)
-#ifdef SIO_EC_PS2K_IRQ
-				SIO_EC_PS2K_IRQ
-#else
-				IRQ (Edge, ActiveHigh, ExclusiveAndWake) {1}
-#endif
-			}
-			EndDependentFn ()
-		})
-	}
-#endif
+		}
+		EndDependentFn ()
+	})
 }
+#endif

coreboot/coreboot

Original-Signed-off-by: Stefan Reinauer <reinauer@google.com>
Original-Change-Id: I76b1ca9bf9243ffa861bed9c356a45377e7f43ef
Original-Reviewed-on: https://chromium-review.googlesource.com/895364
Change-Id: If99e15bef2173c44cecaa8fdeaa69381bd0e499a
Signed-off-by: Martin Roth <martinroth@chromium.org>
Reviewed-on: https://review.coreboot.org/28386
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Furquan Shaikh <furquan@google.com>

src/ec/google/chromeec/acpi/superio.asl
@@ -131,21 +131,34 @@ Device (SIO) {
 		})
 	}
 #endif
+}
 
 #ifdef SIO_EC_ENABLE_PS2K
-	Device (PS2K)		// Keyboard
+Device (PS2K)		// Keyboard
+{
+	Name (_UID, 0)
+	Name (_ADR, 0)
+	Name (_HID, "GOOG000A")
+	Name (_CID, Package() { EISAID("PNP0303"), EISAID("PNP030B") } )
+
+	Method (_STA, 0, NotSerialized) {
+		Return (0x0F)
+	}
+
+	Name (_CRS, ResourceTemplate()
 	{
-		Name (_UID, 0)
-		Name (_ADR, 0)
-		Name (_HID, "GOOG000A")
-		Name (_CID, Package() { EISAID("PNP0303"), EISAID("PNP030B") } )
+		IO (Decode16, 0x60, 0x60, 0x01, 0x01)
+		IO (Decode16, 0x64, 0x64, 0x01, 0x01)
+#ifdef SIO_EC_PS2K_IRQ
+		SIO_EC_PS2K_IRQ
+#else
+		IRQ (Edge, ActiveHigh, Exclusive) {1}
+#endif
+	})
 
-		Method (_STA, 0, NotSerialized) {
-			Return (0x0F)
-		}
-
-		Name (_CRS, ResourceTemplate()
-		{
+	Name (_PRS, ResourceTemplate()
+	{
+		StartDependentFn (0, 0) {
 			IO (Decode16, 0x60, 0x60, 0x01, 0x01)
 			IO (Decode16, 0x64, 0x64, 0x01, 0x01)
 #ifdef SIO_EC_PS2K_IRQ
@@ -153,21 +166,8 @@ Device (SIO) {
 #else
 			IRQ (Edge, ActiveHigh, Exclusive) {1}
 #endif
-		})
-
-		Name (_PRS, ResourceTemplate()
-		{
-			StartDependentFn (0, 0) {
-				IO (Decode16, 0x60, 0x60, 0x01, 0x01)
-				IO (Decode16, 0x64, 0x64, 0x01, 0x01)
-#ifdef SIO_EC_PS2K_IRQ
-				SIO_EC_PS2K_IRQ
-#else
-				IRQ (Edge, ActiveHigh, Exclusive) {1}
-#endif
-			}
-			EndDependentFn ()
-		})
-	}
-#endif
+		}
+		EndDependentFn ()
+	})
 }
+#endif
199b75f58a0ffc2ad0871eb4853ca425c78b4893
Philipp Hug <philipp@hug.cx>
2018-09-13 18:11:56 +0200

    arch/riscv: provide a monotonic timer
    The RISC-V Privileged Architecture specification defines the Machine
    Time Registers (mtime and mtimecmp) in section 3.1.15.
    
    Makes it possible to use the generic udelay.
    The timer is enabled using RISCV_USE_ARCH_TIMER for the lowrisc,
    sifive and ucb soc.
    
    Change-Id: I5139601226e6f89da69e302a10f2fb56b4b24f38
    Signed-off-by: Philipp Hug <philipp@hug.cx>
    Reviewed-on: https://review.coreboot.org/27434
    Reviewed-by: Ronald G. Minnich <rminnich@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/arch/riscv/arch_timer.c
+++ src/arch/riscv/arch_timer.c
@@ -1,6 +1,8 @@
 /*
  * This file is part of the coreboot project.
  *
+ * Copyright 2018 Philipp Hug <philipp@hug.cx>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 2 of the License.
@@ -11,8 +13,16 @@
  * GNU General Public License for more details.
  */
 
-#include <delay.h>
+#include <arch/io.h>
+#include <arch/encoding.h>
+#include <console/console.h>
+#include <stddef.h>
+#include <timer.h>
+#include <mcall.h>
 
-void init_timer(void)
+void timer_monotonic_get(struct mono_time *mt)
 {
+	if (HLS()->time == NULL)
+		die("time not set in HLS");
+	mono_time_set_usecs(mt, (long)read64((void *)(HLS()->time)));
 }
--- src/arch/riscv/include/arch/io.h
+++ src/arch/riscv/include/arch/io.h
@@ -33,6 +33,11 @@ static __always_inline uint32_t read32(const volatile void *addr)
 	return *((volatile uint32_t *)(addr));
 }
 
+static __always_inline uint64_t read64(const volatile void *addr)
+{
+	return *((volatile uint64_t *)(addr));
+}
+
 static __always_inline void write8(volatile void *addr, uint8_t value)
 {
 	*((volatile uint8_t *)(addr)) = value;
@@ -46,6 +51,11 @@ static __always_inline void write16(volatile void *addr, uint16_t value)
 static __always_inline void write32(volatile void *addr, uint32_t value)
 {
 	*((volatile uint32_t *)(addr)) = value;
+}
+
+static __always_inline void write64(volatile void *addr, uint64_t value)
+{
+	*((volatile uint64_t *)(addr)) = value;
 }
 
 #endif

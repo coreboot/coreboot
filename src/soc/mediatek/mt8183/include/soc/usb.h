b2c136d9601b6a8a93a2214018df1961a8c2c75c
Jumin Li <jumin.li@mediatek.com>
2018-09-20 17:40:45 +0800

    mediatek/mt8183: Add USB support
    This patch implements SoC-specific defines of mt8183 and links the
    common code to support USB.
    
    BUG=b:80501386
    BRANCH=none
    TEST=Boots correctly on Kukui
    
    Change-Id: I1224cf24f92b07f3c1814f1cbfef96aafa5a992b
    Signed-off-by: Jumin Li <jumin.li@mediatek.com>
    Reviewed-on: https://review.coreboot.org/28787
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Joel Kitching <kitching@google.com>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/soc/mediatek/mt8183/include/soc/usb.h
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SOC_MEDIATEK_MT8183_USB_H
+#define SOC_MEDIATEK_MT8183_USB_H
+
+#include <soc/usb_common.h>
+
+struct ssusb_sif_port {
+	struct sif_u2_phy_com u2phy;
+	u32 reserved0[64*5];
+	struct sif_u3phyd u3phyd;
+	u32 reserved1[64];
+	struct sif_u3phya u3phya;
+	struct sif_u3phya_da u3phya_da;
+	u32 reserved2[64 * 3];
+};
+check_member(ssusb_sif_port, u3phyd, 0x600);
+check_member(ssusb_sif_port, u3phya, 0x800);
+check_member(ssusb_sif_port, u3phya_da, 0x900);
+check_member(ssusb_sif_port, reserved2, 0xa00);
+
+#define USB_PORT_NUMBER 1
+
+#endif

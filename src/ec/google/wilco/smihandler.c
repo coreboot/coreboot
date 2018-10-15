d978174d1dec9a4afba0510933eb34aee63355d5
Duncan Laurie <dlaurie@google.com>
2018-10-15 15:18:01 +0000

    ec/google/wilco: Add SMM handlers
    Add EC handlers for specific SMM actions:
    
    - on entry to sleep state tell the EC to save state and to prepare for
    the host to enter sleep
    - on ACPI enable/disable send command to the EC
    - add a function to print SMI reasons when eSPI SMI is received
    
    These need to be called by the mainboard handlers which will be done
    when a board is added that uses this EC.
    
    Change-Id: Ibabdc1462e0a8df405f9520244b83684e2ccf2f5
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Reviewed-on: https://review.coreboot.org/29119
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/ec/google/wilco/smihandler.c
@@ -0,0 +1,59 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/acpi.h>
+#include <console/console.h>
+#include <cpu/x86/smm.h>
+
+#include "commands.h"
+#include "ec.h"
+#include "smm.h"
+
+void wilco_ec_smi_sleep(int slp_type)
+{
+	switch (slp_type) {
+	case ACPI_S3:
+		wilco_ec_slp_en();
+		break;
+	case ACPI_S5:
+		wilco_ec_power_off(EC_SWOFF_ACPI);
+		break;
+	}
+}
+
+void wilco_ec_smi_apmc(int apmc)
+{
+	switch (apmc) {
+	case APM_CNT_ACPI_ENABLE:
+		wilco_ec_send(KB_ACPI, ACPI_ON);
+		break;
+	case APM_CNT_ACPI_DISABLE:
+		wilco_ec_send(KB_ACPI, ACPI_OFF);
+		break;
+	}
+}
+
+void wilco_ec_smi_espi(void)
+{
+	struct ec_pm_event_state pm;
+
+	if (!wilco_ec_get_pm(&pm, true)) {
+		printk(BIOS_INFO,
+		       "EC SMI: %02x_%02x-%02x_%02x_%02x_%02x_%02x_%02x-%02x",
+		       pm.event[0], pm.event[1],
+		       pm.state[0], pm.state[1], pm.state[2],
+		       pm.state[3], pm.state[4], pm.state[5], pm.hotkey);
+	}
+}
---
+++ src/ec/google/wilco/smm.h
@@ -0,0 +1,47 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef EC_GOOGLE_WILCO_SMM_H
+#define EC_GOOGLE_WILCO_SMM_H
+
+/* Handlers for SMI events */
+
+/**
+ * wilco_ec_smi_sleep
+ *
+ * Perform EC actions before suspend or power-off.
+ *
+ * @slp_type:	Requested sleep type
+ */
+void wilco_ec_smi_sleep(int slp_type);
+
+/**
+ * wilco_ec_smi_apmc
+ *
+ * Perform EC actions on ACPI OS commands.
+ * This is used when the OS enables or disables ACPI.
+ *
+ * @apmc:	OS command
+ */
+void wilco_ec_smi_apmc(int apmc);
+
+/**
+ * wilco_ec_smi_espi
+ *
+ * Perform EC actions on EC eSPI SMI interrupt.
+ */
+void wilco_ec_smi_espi(void);
+
+#endif /* EC_GOOGLE_WILCO_SMM_H */

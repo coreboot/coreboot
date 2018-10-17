d5f23cecd94929487fdb71c8cc7f3ef4f6c64903
Richard Spiegel <richard.spiegel@amd.corp-partner.google.com>
2018-10-17 10:06:01 -0700

    soc/amd/stoneyridge: Remove double definition for wideio
    WIDEIO_RANGE_ERROR and TOTAL_WIDEIO_PORTS are defined twice. Remove the
    definitions within MMIO definitions, as wideio is not related to MMIO.
    
    BUG=b:117814228
    TEST=Build grunt.
    
    Change-Id: I370a5b387b908fe7a840eb7579d45c1a6a9ca615
    Signed-off-by: Richard Spiegel <richard.spiegel@silverbackltd.com>
    Reviewed-on: https://review.coreboot.org/29168
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>

--- src/soc/amd/stoneyridge/include/soc/southbridge.h
+++ src/soc/amd/stoneyridge/include/soc/southbridge.h
@@ -158,9 +158,6 @@
 #define   DEBUG_PORT_ENABLE		  BIT(18)
 #define   DEBUG_PORT_MASK		(BIT(16) | BIT(17) | BIT(18))
 
-#define WIDEIO_RANGE_ERROR		-1
-#define TOTAL_WIDEIO_PORTS		3
-
 /* FCH AOAC Registers 0xfed81e00 */
 #define FCH_AOAC_D3_CONTROL_CLK_GEN	0x40
 #define FCH_AOAC_D3_CONTROL_I2C0	0x4a

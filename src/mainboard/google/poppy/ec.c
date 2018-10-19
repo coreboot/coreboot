2ce56f152367de3b717658a1e848c4c1da2e23f0
Zhuohao Lee <zhuohao@chromium.org>
2018-10-19 16:50:02 +0800

    mb/google/poppy: add the smi_events back
    Before entering the OS, the AP relies on the smi handler to shutdown the
    system when the lid closes. Without the smi_events setting, the AP will
    not receive the smi handler. As a result, the AP won't shutdown and will
    always keep in S0.
    This problem is caused by the https://review.coreboot.org/c/coreboot/+/28983
    and this patch adds the smi_events back to support the smi handler for
    the lid close.
    
    BRANCH=master
    BUG=b:115572596
    TEST=test_that -b ${BOARD} ${IP} firmware_ECLidShutdown
    
    Change-Id: Id82311a8ccd109f9c26516f59a45bdf34da98529
    Signed-off-by: Zhuohao Lee <zhuohao@chromium.org>
    Reviewed-on: https://review.coreboot.org/29191
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>

--- src/mainboard/google/poppy/ec.c
+++ src/mainboard/google/poppy/ec.c
@@ -24,6 +24,7 @@ __weak const struct google_chromeec_event_info *variant_get_event_info(void)
 	static const struct google_chromeec_event_info info = {
 		.log_events = MAINBOARD_EC_LOG_EVENTS,
 		.sci_events = MAINBOARD_EC_SCI_EVENTS,
+		.smi_events = MAINBOARD_EC_SMI_EVENTS,
 		.s3_wake_events = MAINBOARD_EC_S3_WAKE_EVENTS,
 		.s5_wake_events = MAINBOARD_EC_S5_WAKE_EVENTS,
 		.s0ix_wake_events = MAINBOARD_EC_S0IX_WAKE_EVENTS,

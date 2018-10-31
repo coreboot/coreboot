1956a00953d8eac277b0eb508fcfe60c8f4e1141
Martin Roth <martinroth@google.com>
2018-10-30 22:31:40 -0600

    soc/amd/stoneyridge: Fix get_cpu_count()
    In commit 41baf0c3ff (soc/amd/stoneyridge: Remove dev_find_slot where
    possible), the register being read was changed accidentally from
    HT_DEV (Device 18h, Func 0) to NB_DEV (Device 18h, Func 5)
    
    This doesn't return the correct value, and causes Grunt to reboot.
    
    BUG=b:118721473
    TEST=Boot grunt
    
    Change-Id: I7b73358a074dd27639aafead7c8b39f0fad5685f
    Signed-off-by: Martin Roth <martinroth@google.com>
    Reviewed-on: https://review.coreboot.org/29367
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Daniel Kurtz <djkurtz@google.com>
    Reviewed-by: Richard Spiegel <richard.spiegel@silverbackltd.com>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/soc/amd/stoneyridge/cpu.c
+++ src/soc/amd/stoneyridge/cpu.c
@@ -55,7 +55,7 @@ static void pre_mp_init(void)
 
 static int get_cpu_count(void)
 {
-	return (pci_read_config16(SOC_NB_DEV, D18F0_CPU_CNT) & CPU_CNT_MASK)
+	return (pci_read_config16(SOC_HT_DEV, D18F0_CPU_CNT) & CPU_CNT_MASK)
 									+ 1;
 }
 

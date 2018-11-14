be11d9369b364253a29c8c4a7bc9a6288ff7df65
Seunghwan Kim <sh_.kim@samsung.com>
2018-11-14 11:30:09 +0900

    mb/google/poppy/variants/nautilus: Control GPP_D0 in 2nd SKU only
    GPP_D0 is NC in 1st SKU board design, so we should control GPP_D0
    for only 2nd SKU.
    
    BUG=none
    BRANCH=poppy
    TEST=emerge-nautilus coreboot
    
    Change-Id: Ifd85693c9155ed960f0c794d4b83fe8863b77134
    Signed-off-by: Seunghwan Kim <sh_.kim@samsung.com>
    Reviewed-on: https://review.coreboot.org/29631
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/mainboard/google/poppy/variants/nautilus/smihandler.c
+++ src/mainboard/google/poppy/variants/nautilus/smihandler.c
@@ -16,13 +16,24 @@
 #include <arch/acpi.h>
 #include <baseboard/variants.h>
 #include <gpio.h>
+#include <variant/sku.h>
 
 #define LTE3_PWROFF_L	GPP_D0
 
 void variant_smi_sleep(u8 slp_typ)
 {
+	uint32_t sku_id;
+
 	if (slp_typ == ACPI_S5) {
-		/* Turn off LTE module */
-		gpio_set(LTE3_PWROFF_L, 0);
+		sku_id = variant_board_sku();
+
+		switch (sku_id) {
+		case SKU_1_NAUTILUS_LTE:
+			/* Turn off LTE module */
+			gpio_set(LTE3_PWROFF_L, 0);
+			break;
+		default:
+			break;
+		}
 	}
 }

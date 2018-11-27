5d7f5bc973f4aac3e2833baba7e6e386c602f5b4
Julius Werner <jwerner@chromium.org>
2018-11-26 17:16:24 -0800

    cheza: Add board reset via Chrome EC
    This patch implements board reset on the Cheza board. The real board
    reset used by the operating system uses the PMIC, but unfortunately the
    PMIC needs to be configured right for that to work. The PMIC
    configuration currently happens in the Qualcomm blob (QcLib) that is run
    from romstage, but vboot needs to be able to reboot during verstage
    already. Porting all the PMIC initialization code to run in the
    bootblock seems excessive (and at odds with the goal of doing as little
    as possible before verification), so we'll just do a little hack and ask
    the EC to perform a cold reset instead. For vboot purposes, this should
    work just as well.
    
    BUG=b:118501305
    TEST=Hacked vboot code to call vboot_reboot(), confirmed that board
    reset and came back up as expected.
    
    Change-Id: I3858d95f481884a87c243d4fa3d6369c1e8a5a2c
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/29849
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/cheza/reset.c
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <ec/google/chromeec/ec.h>
+#include <reset.h>
+
+/* Can't do a "real" reset before the PMIC is initialized in QcLib (romstage),
+   but this works well enough for our purposes. */
+void do_board_reset(void)
+{
+	google_chromeec_reboot(0, EC_REBOOT_COLD, 0);
+}

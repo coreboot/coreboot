042772a6bd66cd09add08da40785406e34e92d0a
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2018-11-30 00:06:51 +0100

    mb/emulation/spike-riscv: Implement mtime_init
    This patch lets spike boot to "Payload not loaded" again.
    
    Because soc/ucb/riscv/ does not represent a real SoC, but is a dummy
    directory for emulators, and different emulators might have different
    memory maps, I moved mtime_init to the mainboard-specific directories
    for Spike and QEMU.
    
    Change-Id: I080f7f81df752e25478bd277637bf894bbee4cb2
    Signed-off-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>
    Reviewed-on: https://review.coreboot.org/c/28873
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Philipp Hug <philipp@hug.cx>

---
+++ src/mainboard/emulation/spike-riscv/clint.c
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2018 HardenedLinux
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <mcall.h>
+
+#define SPIKE_CLINT_BASE	0x02000000
+
+/* This function is used to initialize HLS()->time/HLS()->timecmp  */
+void mtime_init(void)
+{
+	long hart_id = read_csr(mhartid);
+	HLS()->time = (uint64_t *)(SPIKE_CLINT_BASE + 0xbff8);
+	HLS()->timecmp = (uint64_t *)(SPIKE_CLINT_BASE + 0x4000 + 8 * hart_id);
+}

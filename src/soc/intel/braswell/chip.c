b6fa7a28a4bf53608e789cbbe92a80a765c2ff5c
Elyes HAOUAS <ehaouas@noos.fr>
2018-12-07 12:21:18 +0100

    src/soc/intel/braswell: Use DEVICE_NOOP
    Use already defined DEVICE_NOOP instead.
    
    Change-Id: Ie6182f273cba3073c84a502c34a002dee6122c2f
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/29857
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/braswell/chip.c
+++ src/soc/intel/braswell/chip.c
@@ -37,12 +37,10 @@ static struct device_operations pci_domain_ops = {
 	.scan_bus         = pci_domain_scan_bus,
 };
 
-static void cpu_bus_noop(struct device *dev) { }
-
 static struct device_operations cpu_bus_ops = {
-	.read_resources   = cpu_bus_noop,
-	.set_resources    = cpu_bus_noop,
-	.enable_resources = cpu_bus_noop,
+	.read_resources   = DEVICE_NOOP,
+	.set_resources    = DEVICE_NOOP,
+	.enable_resources = DEVICE_NOOP,
 	.init             = soc_init_cpus
 };
 

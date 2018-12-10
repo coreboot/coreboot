b81dcc6c4d046392835002fcf6f1661437d0f31c
Frans Hendriks <fhendriks@eltan.com>
2018-12-10 10:30:37 +0100

    soc/intel/braswell/northcluster.c: Fix typo
    Correct typo of 'resource'
    
    BUG=N/A
    TEST=N/A
    
    Change-Id: I79dde87007759b7cab92061df37fd3a19d5e3d1f
    Signed-off-by: Frans Hendriks <fhendriks@eltan.com>
    Reviewed-on: https://review.coreboot.org/c/30125
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tristan Corrick <tristan@corrick.kiwi>

--- src/soc/intel/braswell/northcluster.c
+++ src/soc/intel/braswell/northcluster.c
@@ -53,7 +53,7 @@
  * +--------------------------+ 0
  *
  * Note that there are really only a few regions that need to enumerated w.r.t.
- * coreboot's resrouce model:
+ * coreboot's resource model:
  *
  * +--------------------------+ BMBOUND_HI
  * |     Cacheable/Usable     |

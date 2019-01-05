646edd18e6627eb34554c6ba8e918fbbc4cf76ca
Kyösti Mälkki <kyosti.malkki@gmail.com>
2019-01-05 12:43:16 +0200

    southbridge/nvidia/ck804: Drop leftover code
    Code was for romcc romstage.
    
    Change-Id: If368610651ce950169ef71d9ed768a509c2a5c5c
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/30736
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/southbridge/nvidia/ck804/ck804.h
+++ src/southbridge/nvidia/ck804/ck804.h
@@ -26,11 +26,6 @@
 #define CK804B_BUSN 0x80
 #define CK804B_DEVN_BASE (!CONFIG_SB_HT_CHAIN_UNITID_OFFSET_ONLY ? CK804_DEVN_BASE : 1)
 
-#if CONFIG_CK804_NUM > 1
-#define CK804B_ANACTRL_IO_BASE (ANACTRL_IO_BASE + 0x8000)
-#define CK804B_SYSCTRL_IO_BASE (SYSCTRL_IO_BASE + 0x8000)
-#endif
-
 #ifdef __PRE_RAM__
 void enable_fid_change_on_sb(unsigned sbbusn, unsigned sbdn);
 #endif

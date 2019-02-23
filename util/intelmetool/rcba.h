f0a9128424f2c610756b97ef77f8fcd49a98f563
Damien Zammit <damien@zamaudio.com>
2019-02-23 12:38:15 +1100

    intelmetool: Use GPLv2+ where possible
    Upstream intelmetool is out of date; I suggest I archive it
    instead of trying to merge coreboot's changes into it.
    
    However I would like to preserve the licensing of files in the tool
    as GPLv2+ where possible instead of GPLv2-only.
    
    Change-Id: I47b1ff2734f54c65f4214b39244bd868ef44b83c
    Signed-off-by: Damien Zammit <damien@zamaudio.com>
    Reviewed-on: https://review.coreboot.org/c/31587
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Philipp Deppenwiese <zaolin.daisuki@gmail.com>

--- util/intelmetool/rcba.h
+++ util/intelmetool/rcba.h
@@ -1,9 +1,10 @@
 /*
  * Copyright (C) 2017  Patrick Rudolph <siro@das-labor.org>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of

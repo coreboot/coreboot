0eb4db185cfef44ddfdbd91d4fe69a48c127fa84
santhosh hassan <sahassan@google.com>
2019-03-09 02:28:43 -0800

    google/mistral: Implement board reset
    Implement reset using PSHOLD.
    
    Change-Id: I472bf73cc7b227187b284a3730ec5dea5373695c
    Signed-off-by: Santhosh Hassan <sahassan@google.com>
    Signed-off-by: Nitheesh Sekar <nsekar@codeaurora.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/31827
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

---
+++ src/mainboard/google/mistral/reset.c
@@ -0,0 +1,35 @@
+/*
+ *
+ * This file is part of the coreboot project.
+ *
+ * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ * Copyright 2014 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <device/mmio.h>
+#include <reset.h>
+
+#define GCNT_PSHOLD			((void *)0x004AB000u)
+#define TCSR_BOOT_MISC_DETECT		((void *)0x0193D100)
+#define TCSR_RESET_DEBUG_SW_ENTRY	((void *)0x01940000)
+
+void do_board_reset(void)
+{
+	/*
+	 * At boot time the boot loaders would have set a magic cookie
+	 * here to detect watchdog reset. However, since this is a
+	 * normal reset clear the magic numbers.
+	 */
+	write32(TCSR_BOOT_MISC_DETECT, 0);
+	write32(TCSR_RESET_DEBUG_SW_ENTRY, 0);
+	write32(GCNT_PSHOLD, 0);
+}

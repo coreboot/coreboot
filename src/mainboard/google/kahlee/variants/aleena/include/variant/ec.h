301d47fdd6547125f8e0418f2c05b3b586bd03cf
Edward Hill <ecgh@chromium.org>
2019-03-13 15:31:15 -0600

    mb/google/kahlee/aleena: Add EC_ENABLE_TBMC_DEVICE
    Enable ACPI TBMC notification on tablet mode change to support
    convertible Aleena devices.
    
    BUG=b:124132058
    BRANCH=grunt
    TEST=evtest shows tablet mode events
    
    Change-Id: Iaf8ef031d4660f0791b5f664880437e6dfa58dc8
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/31893
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/mainboard/google/kahlee/variants/aleena/include/variant/ec.h
+++ src/mainboard/google/kahlee/variants/aleena/include/variant/ec.h
@@ -16,6 +16,9 @@
 /* Enable EC backed Keyboard Backlight in ACPI */
 #define EC_ENABLE_KEYBOARD_BACKLIGHT
 
+/* Enable Tablet switch */
+#define EC_ENABLE_TBMC_DEVICE
+
 /*
  * Enable EC sync interrupt via GPIO controller, EC_SYNC_IRQ is defined in
  * variant/gpio.h

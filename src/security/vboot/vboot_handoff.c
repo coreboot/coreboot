f7f41a663f11f57a463177d4f7c9df165830c821
Joel Kitching <kitching@google.com>
2019-03-18 17:50:19 +0800

    vboot_handoff: do not set VBSD_HONOR_VIRT_DEV_SWITCH
    As part of chromium:942901, physical dev switch functionality
    is being deprecated.
    
    This flag is no longer read after CL:1526070, and thus
    does not need to be set here.
    
    coreboot's vboot subrepository needs to be updated to include
    CL:1526070 before this CL can be merged.
    
    BUG=b:124141368, b:124192753, chromium:942901
    TEST=Build and deploy to eve
    TEST=util/lint/checkpatch.pl -g origin/master..HEAD
    TEST=make clean && make test-abuild
    CQ-DEPEND=CL:1526070
    BRANCH=none
    
    Change-Id: Ie5849f9e0fcb8e4e6d35d542a141bf635e751af4
    Signed-off-by: Joel Kitching <kitching@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/31952
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/security/vboot/vboot_handoff.c
+++ src/security/vboot/vboot_handoff.c
@@ -79,7 +79,6 @@ static void fill_vboot_handoff(struct vboot_handoff *vboot_handoff,
 		vb_sd->flags |= VBSD_LF_DEV_SWITCH_ON;
 	}
 	/* TODO: Set these in depthcharge */
-	vb_sd->flags |= VBSD_HONOR_VIRT_DEV_SWITCH;
 	if (!CONFIG(VBOOT_PHYSICAL_REC_SWITCH))
 		vb_sd->flags |= VBSD_BOOT_REC_SWITCH_VIRTUAL;
 	if (CONFIG(VBOOT_OPROM_MATTERS)) {

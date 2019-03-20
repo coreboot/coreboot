b618b04992158fd86ac96e725225e095b74acf55
Kyösti Mälkki <kyosti.malkki@gmail.com>
2019-03-20 10:24:11 +0200

    sb/amd8111: Drop unused code
    Change-Id: I2b1f46865aa380c2a31e05e55418b27296c72136
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/31988
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

--- src/southbridge/amd/amd8111/usb2.c
+++ src/southbridge/amd/amd8111/usb2.c
@@ -21,21 +21,6 @@
 #include <device/pci_ops.h>
 #include "amd8111.h"
 
-#if 0
-
-static void lpci_set_subsystem(struct device *dev, unsigned int vendor,
-			       unsigned int device)
-{
-	pci_write_config32(dev, 0x70,
-		((device & 0xffff) << 16) | (vendor & 0xffff));
-}
-
-static struct pci_operations lops_pci = {
-	.set_subsystem = lpci_set_subsystem,
-};
-
-#endif
-
 static void amd8111_usb2_enable(struct device *dev)
 {
 	// Due to buggy USB2 we force it to disable.
@@ -50,7 +35,6 @@ static struct device_operations usb2_ops  = {
 	.enable_resources = pci_dev_enable_resources,
 	.scan_bus         = 0,
 	.enable           = amd8111_usb2_enable,
-	// .ops_pci          = &lops_pci,
 };
 
 static const struct pci_driver usb2_driver __pci_driver = {

coreboot/coreboot

commit 74aa99a5435ce3a1b984a3e0e5a0b696d6f6165d

src: Drop unused '#include <halt.h>'
Change-Id: Ie7afe77053a21bcf6a1bf314570f897d1791a620
Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/31921
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>

src/soc/nvidia/tegra124/lp0/tegra_lp0_resume.c
@@ -14,6 +14,7 @@
  * GNU General Public License for more details.
  */
 
+#include <halt.h>
 #include <stdint.h>
 
 /* Function unit addresses. */

src/soc/nvidia/tegra210/lp0/tegra_lp0_resume.c
@@ -14,6 +14,7 @@
  * GNU General Public License for more details.
  */
 
+#include <halt.h>
 #include <stdint.h>
 
 /* Function unit addresses. */

src/commonlib/storage/bouncebuf.c
@@ -17,7 +17,6 @@
 
 #include <arch/cache.h>
 #include "bouncebuf.h"
-#include <halt.h>
 #include "storage.h"
 #include <string.h>
 #include <commonlib/stdlib.h>

src/commonlib/storage/sdhci.c
@@ -24,7 +24,6 @@
 #include <commonlib/storage.h>
 #include <delay.h>
 #include <endian.h>
-#include <halt.h>
 #include "sdhci.h"
 #include "sd_mmc.h"
 #include "storage.h"

src/cpu/intel/haswell/romstage.c
@@ -21,7 +21,6 @@
 #include <cpu/x86/bist.h>
 #include <cpu/x86/msr.h>
 #include <cpu/x86/mtrr.h>
-#include <halt.h>
 #include <lib.h>
 #include <timestamp.h>
 #include <device/pci_def.h>

src/ec/google/chromeec/ec.c
@@ -23,7 +23,6 @@
 #include <bootstate.h>
 #include <delay.h>
 #include <elog.h>
-#include <halt.h>
 #include <reset.h>
 #include <rtc.h>
 #include <stdlib.h>

src/mainboard/google/butterfly/romstage.c
@@ -25,7 +25,6 @@
 #include <northbridge/intel/sandybridge/raminit_native.h>
 #include <southbridge/intel/bd82x6x/pch.h>
 #include <southbridge/intel/common/gpio.h>
-#include <halt.h>
 #if CONFIG(CHROMEOS)
 #include <vendorcode/google/chromeos/chromeos.h>
 #endif

src/mainboard/google/link/romstage.c
@@ -27,7 +27,6 @@
 #include <northbridge/intel/sandybridge/raminit_native.h>
 #include <southbridge/intel/common/gpio.h>
 #include "ec/google/chromeec/ec.h"
-#include <halt.h>
 #include <cbfs.h>
 
 #include <southbridge/intel/bd82x6x/chip.h>

commit a9273b5015f1cbcf3fc5db048663147c25c76f83

soc/nvidia/tegra{124,210}: Remove unneeded 'include <halt.h>'
Commit 74aa99a (src: Drop unused '#include <halt.h>')
accidentally added '#include <halt.h>', however tegra_lp0 directory
is not linked into the rest of coreboot. So we can't use generic halt()
from halt.c file.

Change-Id: I3a67abb77846172597b8ebde779878b9aa2ff8d7
Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/31979
Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
Reviewed-by: Julius Werner <jwerner@chromium.org>
Reviewed-by: Nico Huber <nico.h@gmx.de>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/soc/nvidia/tegra124/lp0/tegra_lp0_resume.c
@@ -14,7 +14,6 @@
  * GNU General Public License for more details.
  */
 
-#include <halt.h>
 #include <stdint.h>
 
 /* Function unit addresses. */

src/soc/nvidia/tegra210/lp0/tegra_lp0_resume.c
@@ -14,7 +14,6 @@
  * GNU General Public License for more details.
  */
 
-#include <halt.h>
 #include <stdint.h>
 
 /* Function unit addresses. */
e556f716e43daac7a8f553213eadbe352e19a79b
Joel Kitching <kitching@google.com>
2019-04-01 21:49:59 +0800

    vboot: remove Kconfig option VBOOT_PHYSICAL_REC_SWITCH
    This option is duplicated in depthcharge:
    https://crrev.com/c/1545144
    
    BUG=b:124141368, b:124192753, chromium:943150
    TEST=make clean && make test-abuild
    CQ-DEPEND=CL:1545144
    BRANCH=none
    
    Change-Id: I48e20ad21cdcb948a23387d3e5fcf142723b0c82
    Signed-off-by: Joel Kitching <kitching@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/32135
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- src/security/vboot/vboot_handoff.c
+++ src/security/vboot/vboot_handoff.c
@@ -79,8 +79,6 @@ static void fill_vboot_handoff(struct vboot_handoff *vboot_handoff,
 		vb_sd->flags |= VBSD_LF_DEV_SWITCH_ON;
 	}
 	/* TODO: Set these in depthcharge */
-	if (!CONFIG(VBOOT_PHYSICAL_REC_SWITCH))
-		vb_sd->flags |= VBSD_BOOT_REC_SWITCH_VIRTUAL;
 	if (CONFIG(VBOOT_OPROM_MATTERS)) {
 		vb_sd->flags |= VBSD_OPROM_MATTERS;
 		/*

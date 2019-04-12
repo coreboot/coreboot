2efee9df8581a5b1898d58c6d8f7ba3552ce1ed0
peichao.wang <peichao.wang@bitland.corp-partner.google.com>
2019-04-12 15:14:15 +0800

    mb/google/octopus: Add custom SAR values for Laser
    Laser would prefer to use different SAR values. Since Laser
    sku id is 5.
    
    BUG=b:130381493
    BRANCH=octopus
    TEST=build
    
    Change-Id: I5cce38a191edfb235e274db3c788c58b65e0ebe1
    Signed-off-by: peichao.wang <peichao.wang@bitland.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/32296
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/octopus/variants/phaser/mainboard.c
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <boardid.h>
+#include <ec/google/chromeec/ec.h>
+#include <sar.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	const char *filename = NULL;
+	uint32_t sku_id;
+
+	if (google_chromeec_cbi_get_sku_id(&sku_id))
+		return NULL;
+
+	if (sku_id == 5)
+		filename = "wifi_sar-laser.hex";
+
+	return filename;
+}

4a402feebf6d8754a8ca8d2207607efd93ef8fe4
Arthur Heymans <arthur@aheymans.xyz>
2019-05-25 10:24:16 +0200

    drivers/emulation/qemu_debugcon: Don't use CAR_GLOBAL
    This platform uses NO_CAR_GLOBAL_MIGRATION.
    
    Change-Id: Idc9434e5a1a8bc5ed76a9f80c9a7cfba2fd474c0
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/33000
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/drivers/emulation/qemu/qemu_debugcon.c
+++ src/drivers/emulation/qemu/qemu_debugcon.c
@@ -17,14 +17,13 @@
 #include <console/console.h>
 #include <console/qemu_debugcon.h>
 #include <arch/io.h>
-#include <arch/early_variables.h>
 
-static int qemu_debugcon_detected CAR_GLOBAL;
+static int qemu_debugcon_detected;
 
 void qemu_debugcon_init(void)
 {
 	int detected = (inb(CONFIG_CONSOLE_QEMU_DEBUGCON_PORT) == 0xe9);
-	car_set_var(qemu_debugcon_detected, detected);
+	qemu_debugcon_detected = detected;
 	printk(BIOS_INFO, "QEMU debugcon %s [port 0x%x]\n",
 	       detected ? "detected" : "not found",
 	       CONFIG_CONSOLE_QEMU_DEBUGCON_PORT);
@@ -32,6 +31,6 @@ void qemu_debugcon_init(void)
 
 void qemu_debugcon_tx_byte(unsigned char data)
 {
-	if (car_get_var(qemu_debugcon_detected) != 0)
+	if (qemu_debugcon_detected != 0)
 		outb(data, CONFIG_CONSOLE_QEMU_DEBUGCON_PORT);
 }

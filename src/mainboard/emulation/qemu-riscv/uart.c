efc39cbec787b9447669c29e664e9ac74c476783
Patrick Rudolph <patrick.rudolph@9elements.com>
2019-06-23 19:23:30 +0200

    mb/emulation/qemu-riscv: Use generic 8250 uart driver
    Drop hacked uart code and use the generic 8250 uart driver for ns16550a.
    
    Tested on qemu-system-riscv64:
    * The UART is still working.
    
    Change-Id: I6efda913fa39e0cfa466b52c570572aca90dacdf
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/33735
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-by: Philipp Hug <philipp@hug.cx>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/emulation/qemu-riscv/uart.c
+++ src/mainboard/emulation/qemu-riscv/uart.c
@@ -15,42 +15,9 @@
 
 #include <types.h>
 #include <console/uart.h>
-#include <boot/coreboot_tables.h>
 #include <mainboard/addressmap.h>
 
-static uint8_t *buf = (void *)QEMU_VIRT_UART0;
 uintptr_t uart_platform_base(int idx)
 {
-	return (uintptr_t) buf;
+	return (uintptr_t) QEMU_VIRT_UART0;
 }
-
-void uart_init(int idx)
-{
-}
-
-unsigned char uart_rx_byte(int idx)
-{
-	return *buf;
-}
-
-void uart_tx_byte(int idx, unsigned char data)
-{
-	*buf = data;
-}
-
-void uart_tx_flush(int idx)
-{
-}
-
-#ifndef __PRE_RAM__
-void uart_fill_lb(void *data)
-{
-	struct lb_serial serial;
-	serial.type = LB_SERIAL_TYPE_MEMORY_MAPPED;
-	serial.baseaddr = QEMU_VIRT_UART0;
-	serial.baud = 115200;
-	serial.regwidth = 1;
-	lb_add_serial(&serial, data);
-        lb_add_console(LB_TAG_CONSOLE_SERIAL8250MEM, data);
-}
-#endif

640ca69c0589b2337d2f319c59dd937767be6036
Huayang Duan <huayang.duan@mediatek.com>
2019-06-27 15:33:20 +0800

    mediatek/mt8183: support more EMCP LPDDR4X DDR bootup
    Support SANDISK SDADA4CR-128G, SAMSUNG KMDP6001DA-B425, KMDV6001DA-B620
    EMCP LPDDR4X DDR bootup.
    
    BUG=b:80501386
    BRANCH=none
    TEST=Boots correctly on EMCP DRAM
    
    Change-Id: I7de4c9a27282d3d00f51adf46dcb3d2f3984bfff
    Signed-off-by: Huayang Duan <huayang.duan@mediatek.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/33838
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- src/mainboard/google/kukui/sdram_configs.c
+++ src/mainboard/google/kukui/sdram_configs.c
@@ -21,8 +21,11 @@
 static const char *const sdram_configs[] = {
 	[1] = "sdram-lpddr4x-H9HCNNNCPMALHR-4GB",
 	[2] = "sdram-lpddr4x-MT53E1G32D4NQ-4GB",
-	[3] = "sdram-lpddr4x-KMDP6001DA-B425-4GB",
+	[3] = "sdram-lpddr4x-KMDH6001DA-B422-4GB",
+	[4] = "sdram-lpddr4x-KMDP6001DA-B425-4GB",
 	[5] = "sdram-lpddr4x-MT29VZZZAD8DQKSL-4GB",
+	[6] = "sdram-lpddr4x-KMDV6001DA-B620-4GB",
+	[7] = "sdram-lpddr4x-SDADA4CR-128G-4GB",
 };
 
 static struct sdram_params params;

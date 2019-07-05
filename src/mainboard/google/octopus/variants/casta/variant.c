0921962e4410577ffb1b15109ec6dc2deb015d37
Seunghwan Kim <sh_.kim@samsung.com>
2019-07-05 14:56:41 +0900

    mb/google/octopus: Add custom SAR values for Bluebird
    Bluebird needs to use different SAR values than Casta.
    Bluebird sku id is 2.
    
    CQ-DEPEND=CL:*1435310
    BUG=b:129725065
    BRANCH=octopus
    TEST=build
    
    Change-Id: I107a8519832fcf906b94f958a3dc508d19bb4727
    Signed-off-by: Seunghwan Kim <sh_.kim@samsung.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/34080
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/octopus/variants/casta/variant.c
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2019 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <baseboard/variants.h>
+#include <sar.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	const char *filename = NULL;
+	uint32_t sku_id = get_board_sku();
+
+	if (sku_id == 2)
+		filename = "wifi_sar-bluebird.hex";
+
+	return filename;
+}

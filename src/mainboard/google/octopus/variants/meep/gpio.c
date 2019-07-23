a0a83e1a6c526d5877c83122a1682704c6ec178d
Wisley Chen <wisley.chen@quantatw.com>
2019-07-23 14:33:04 +0800

    mb/google/octopus: Override DDI1 DDC SDA/SCL for HDMI
    The device Dorp uses the variant Meep, and supports HDMI.
    
    -sku33 (HDMI)
    -sku34 (HDMI + keyboard backlight)
    -sku35 (HDMI + Touchscreen)
    -sku36 (HDMI + keyboard backlight + Touchscreen)
    
    BUG=b:136522841
    BRANCH=octopus
    TEST=emerge-octopus coreboot
    
    Change-Id: I59ba2e56cf2f83ca9d533454570bcdd39c0a2e7c
    Signed-off-by: Wisley Chen <wisley.chen@quantatw.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/34509
    Reviewed-by: Marco Chen <marcochen@google.com>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/octopus/variants/meep/gpio.c
+++ src/mainboard/google/octopus/variants/meep/gpio.c
@@ -17,6 +17,7 @@
 #include <baseboard/variants.h>
 #include <gpio.h>
 #include <soc/gpio.h>
+#include <variant/sku.h>
 
 static const struct pad_config default_override_table[] = {
 	PAD_NC(GPIO_104, UP_20K),
@@ -26,8 +27,34 @@ static const struct pad_config default_override_table[] = {
 				     DISPUPD),
 };
 
+static const struct pad_config hdmi_sku_override_table[] = {
+	PAD_NC(GPIO_104, UP_20K),
+
+	/* HV_DDI1_DDC_SDA */
+	PAD_CFG_NF_IOSSTATE_IOSTERM(GPIO_126, NONE, DEEP, NF1, HIZCRx1,
+					DISPUPD),
+	/* HV_DDI1_DDC_SCL */
+	PAD_CFG_NF_IOSSTATE_IOSTERM(GPIO_127, NONE, DEEP, NF1, HIZCRx1,
+					DISPUPD),
+	/* EN_PP3300_TOUCHSCREEN */
+	PAD_CFG_GPO_IOSSTATE_IOSTERM(GPIO_146, 0, DEEP, NONE, Tx0RxDCRx0,
+					DISPUPD),
+};
+
 const struct pad_config *variant_override_gpio_table(size_t *num)
 {
-	*num = ARRAY_SIZE(default_override_table);
-	return default_override_table;
+	uint32_t sku_id;
+	sku_id = get_board_sku();
+
+	switch (sku_id) {
+	case SKU_33_DORP:
+	case SKU_34_DORP:
+	case SKU_35_DORP:
+	case SKU_36_DORP:
+		*num = ARRAY_SIZE(hdmi_sku_override_table);
+		return hdmi_sku_override_table;
+	default:
+		*num = ARRAY_SIZE(default_override_table);
+		return default_override_table;
+	}
 }

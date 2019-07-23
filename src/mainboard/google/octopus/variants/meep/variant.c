3b3417783099f65cea3ae0130c34fa897052327e
Wisley Chen <wisley.chen@quantatw.com>
2019-07-23 17:30:59 +0800

    mb/google/octopus: Override VBT selection for Dorp
    For dorp HDMI sku, select VBT which enables HDMI output.
    
    -sku33 (HDMI)
    -sku34 (HDMI + keyboard backlight)
    -sku35 (HDMI + Touchscreen)
    -sku36 (HDMI + keyboard backlight + Touchscreen)
    
    Cq-Depend: chrome-internal:1502253
    BUG=b:136522841
    BRANCH=octopus
    TEST=emerge-octopus coreboot
    
    Change-Id: I62262378f85bb899073ffac7804be876e649e429
    Signed-off-by: Wisley Chen <wisley.chen@quantatw.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/34512
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marco Chen <marcochen@google.com>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>

---
+++ src/mainboard/google/octopus/variants/meep/variant.c
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2019 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <ec/google/chromeec/ec.h>
+#include <drivers/intel/gma/opregion.h>
+#include <baseboard/variants.h>
+#include <variant/sku.h>
+
+const char *mainboard_vbt_filename(void)
+{
+	uint32_t sku_id;
+
+	sku_id = get_board_sku();
+
+	switch (sku_id) {
+	case SKU_33_DORP:
+	case SKU_34_DORP:
+	case SKU_35_DORP:
+	case SKU_36_DORP:
+		return "vbt_dorp_hdmi.bin";
+	default:
+		return "vbt.bin";
+	}
+}

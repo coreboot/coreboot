aa9aff7b81348dff5de91e16ec54d36d6e74dffe
David Wu <david_wu@quanta.corp-partner.google.com>
2019-08-08 11:16:30 +0800

    mb/google/hatch/var/kindred: Disable SATA controller for eMMC SKUs
    Disable SATA controller and SATA port 1 for eMMC SKUs
    
    BUG=b:132918661
    TEST=Verify SSD is disabled when SKU ID = 2/4/21/22
    
    Change-Id: I6d95ff94b079a564f74c19739370101899843f00
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/34789
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/hatch/variants/kindred/variant.c
+++ src/mainboard/google/hatch/variants/kindred/variant.c
@@ -22,16 +22,28 @@ void variant_devtree_update(void)
 {
 	uint32_t sku_id;
 	struct device *emmc_host;
+	struct device *ssd_host;
 	config_t *cfg = config_of_path(SA_DEVFN_ROOT);
 	emmc_host = pcidev_path_on_root(PCH_DEVFN_EMMC);
-
-	if (emmc_host == NULL)
-		return;
+	ssd_host = pcidev_path_on_root(PCH_DEVFN_SATA);
 
 	/* SKU ID 1/3/23/24 doesn't have a eMMC device, hence disable it. */
 	sku_id = get_board_sku();
 	if (sku_id == 1 || sku_id == 3 || sku_id == 23 || sku_id == 24) {
+		if (emmc_host == NULL)
+			return;
 		emmc_host->enabled = 0;
 		cfg->ScsEmmcHs400Enabled = 0;
+	}
+	/* SKU ID 2/4/21/22 doesn't have a SSD device, hence disable it. */
+	if (sku_id == 2 || sku_id == 4 || sku_id == 21 || sku_id == 22) {
+		if (ssd_host == NULL)
+			return;
+		ssd_host->enabled = 0;
+		cfg->SataSalpSupport = 0;
+		cfg->SataMode = 0;
+		cfg->SataPortsEnable[1] = 0;
+		cfg->SataPortsDevSlp[1] = 0;
+		cfg->satapwroptimize = 0;
 	}
 }

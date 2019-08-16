0c1cf9f5a24dbb7d38d9d9de82d3488a57e55462
Chris Wang <chris.wang@amd.corp-partner.google.com>
2019-08-16 18:47:38 +0800

    mb/google/kahlee/treeya: remove keyboard backlight support
    Treeya doesn't support the keyboard backlight.
    
    BUG=b:135551210
    BRANCH=grunt
    TEST=emerge-grunt coreboot
    
    Signed-off-by: Chris Wang <chris.wang@amd.corp-partner.google.com>
    Change-Id: I02dfc77d3cb7ac00b3f10d577d92775db99c1bdf
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/34903
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Edward Hill <ecgh@chromium.org>

--- src/mainboard/google/kahlee/variants/treeya/include/variant/ec.h
+++ src/mainboard/google/kahlee/variants/treeya/include/variant/ec.h
@@ -15,9 +15,6 @@
 
 #include <baseboard/ec.h>
 
-/* Enable EC backed Keyboard Backlight in ACPI */
-#define EC_ENABLE_KEYBOARD_BACKLIGHT
-
 /* Enable Tablet switch */
 #define EC_ENABLE_TBMC_DEVICE
 

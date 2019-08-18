85cd7c245cb9ccf085ed198d89fc2a3413e86d12
Kyösti Mälkki <kyosti.malkki@gmail.com>
2019-08-18 22:21:25 +0300

    device/oprom: Drop unnecessary AMD headers
    Change-Id: I548dfa053f195b1ea87568240bf0041bb193d825
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/34986
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>

--- src/device/oprom/realmode/x86.c
+++ src/device/oprom/realmode/x86.c
@@ -19,8 +19,6 @@
 #include <arch/registers.h>
 #include <boot/coreboot_tables.h>
 #include <console/console.h>
-#include <cpu/amd/lxdef.h>
-#include <cpu/amd/vr.h>
 #include <delay.h>
 #include <device/pci.h>
 #include <device/pci_ids.h>

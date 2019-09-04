934ae21b52492c9c730dc5accd2900b32c5c1492
Philipp Hug <philipp@hug.cx>
2019-09-04 09:24:45 -0700

    mb/emulation/qemu-riscv: Implement ipi using clint to enable smp in qemu/spike.
    TEST=Set MAX_CPUS=2 and run qemu with -smp 2
    
    Signed-off-by: Philipp Hug <philipp@hug.cx>
    Change-Id: I94fb25fad103e3cb5db676eb4caead11d54ae0ae
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/35246
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Xiang Wang <merle@hardenedlinux.org>

--- src/mainboard/emulation/qemu-riscv/clint.c
+++ src/mainboard/emulation/qemu-riscv/clint.c
@@ -14,6 +14,7 @@
  */
 
 #include <mcall.h>
+#include <device/mmio.h>
 #include <mainboard/addressmap.h>
 
 /* This function is used to initialize HLS()->time/HLS()->timecmp  */
@@ -22,4 +23,9 @@ void mtime_init(void)
 	long hart_id = read_csr(mhartid);
 	HLS()->time = (uint64_t *)(QEMU_VIRT_CLINT + 0xbff8);
 	HLS()->timecmp = (uint64_t *)(QEMU_VIRT_CLINT + 0x4000 + 8 * hart_id);
+}
+
+void set_msip(int hartid, int val)
+{
+	write32((void *)(QEMU_VIRT_CLINT + 4 * (uintptr_t)hartid), !!val);
 }
--- src/mainboard/emulation/spike-riscv/clint.c
+++ src/mainboard/emulation/spike-riscv/clint.c
@@ -14,6 +14,7 @@
  */
 
 #include <mcall.h>
+#include <device/mmio.h>
 
 #define SPIKE_CLINT_BASE	0x02000000
 
@@ -23,4 +24,9 @@ void mtime_init(void)
 	long hart_id = read_csr(mhartid);
 	HLS()->time = (uint64_t *)(SPIKE_CLINT_BASE + 0xbff8);
 	HLS()->timecmp = (uint64_t *)(SPIKE_CLINT_BASE + 0x4000 + 8 * hart_id);
+}
+
+void set_msip(int hartid, int val)
+{
+	write32((void *)(SPIKE_CLINT_BASE + 4 * (uintptr_t)hartid), !!val);
 }

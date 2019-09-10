20667c59d81312fab4c99dd0725f2e0bb6396de8
Himanshu Sahdev <himanshusah@hcl.com>
2019-09-10 16:03:13 +0530

    emulation/qemu-i440fx/fw_cfg_if.h: replace macro with enum
    replace multiple existing FW_CFG_* defines with enum fw_cfg_enum.
    
    Change-Id: I9699df4aeb2d8b18f933bb9aaed16008d10158ad
    Signed-off-by: Himanshu Sahdev <himanshusah@hcl.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/35310
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>

--- src/mainboard/emulation/qemu-i440fx/fw_cfg_if.h
+++ src/mainboard/emulation/qemu-i440fx/fw_cfg_if.h
@@ -13,40 +13,43 @@
  */
 
 /*
- * This are the qemu firmware config interface defines and structs.
- * Copyed over from qemu soure tree, include/hw/nvram/fw_cfg.h
+ * These are the qemu firmware config interface defines and structs.
+ * Copied over from qemu soure tree,
+ * include/standard-headers/linux/qemu_fw_cfg.h and modified accordingly.
  */
 #ifndef FW_CFG_IF_H
 #define FW_CFG_IF_H
 
 #include <stdint.h>
 
-#define FW_CFG_SIGNATURE        0x00
-#define FW_CFG_ID               0x01
-#define FW_CFG_UUID             0x02
-#define FW_CFG_RAM_SIZE         0x03
-#define FW_CFG_NOGRAPHIC        0x04
-#define FW_CFG_NB_CPUS          0x05
-#define FW_CFG_MACHINE_ID       0x06
-#define FW_CFG_KERNEL_ADDR      0x07
-#define FW_CFG_KERNEL_SIZE      0x08
-#define FW_CFG_KERNEL_CMDLINE   0x09
-#define FW_CFG_INITRD_ADDR      0x0a
-#define FW_CFG_INITRD_SIZE      0x0b
-#define FW_CFG_BOOT_DEVICE      0x0c
-#define FW_CFG_NUMA             0x0d
-#define FW_CFG_BOOT_MENU        0x0e
-#define FW_CFG_MAX_CPUS         0x0f
-#define FW_CFG_KERNEL_ENTRY     0x10
-#define FW_CFG_KERNEL_DATA      0x11
-#define FW_CFG_INITRD_DATA      0x12
-#define FW_CFG_CMDLINE_ADDR     0x13
-#define FW_CFG_CMDLINE_SIZE     0x14
-#define FW_CFG_CMDLINE_DATA     0x15
-#define FW_CFG_SETUP_ADDR       0x16
-#define FW_CFG_SETUP_SIZE       0x17
-#define FW_CFG_SETUP_DATA       0x18
-#define FW_CFG_FILE_DIR         0x19
+enum fw_cfg_enum {
+	FW_CFG_SIGNATURE,
+	FW_CFG_ID,
+	FW_CFG_UUID,
+	FW_CFG_RAM_SIZE,
+	FW_CFG_NOGRAPHIC,
+	FW_CFG_NB_CPUS,
+	FW_CFG_MACHINE_ID,
+	FW_CFG_KERNEL_ADDR,
+	FW_CFG_KERNEL_SIZE,
+	FW_CFG_KERNEL_CMDLINE,
+	FW_CFG_INITRD_ADDR,
+	FW_CFG_INITRD_SIZE,
+	FW_CFG_BOOT_DEVICE,
+	FW_CFG_NUMA,
+	FW_CFG_BOOT_MENU,
+	FW_CFG_MAX_CPUS,
+	FW_CFG_KERNEL_ENTRY,
+	FW_CFG_KERNEL_DATA,
+	FW_CFG_INITRD_DATA,
+	FW_CFG_CMDLINE_ADDR,
+	FW_CFG_CMDLINE_SIZE,
+	FW_CFG_CMDLINE_DATA,
+	FW_CFG_SETUP_ADDR,
+	FW_CFG_SETUP_SIZE,
+	FW_CFG_SETUP_DATA,
+	FW_CFG_FILE_DIR
+};
 
 #define FW_CFG_FILE_FIRST       0x20
 #define FW_CFG_FILE_SLOTS       0x10

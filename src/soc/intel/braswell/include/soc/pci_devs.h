0b4298c24288f65a6b315864bf5b44ef54cfb30b
Kyösti Mälkki <kyosti.malkki@gmail.com>
2019-09-29 21:17:46 +0300

    intel/pci_devs: Regroup PCI xx_DEVID entries
    Change-Id: I953e9a7746232b4c40deca55eb6cb3bd7af91496
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/35727
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/braswell/include/soc/pci_devs.h
+++ src/soc/intel/braswell/include/soc/pci_devs.h
@@ -23,99 +23,76 @@
 /* SoC transaction router */
 #define SOC_DEV 0x0
 #define SOC_FUNC 0
-# define SOC_DEVID 0x2280
 
 /* Graphics and Display */
 #define GFX_DEV 0x2
 #define GFX_FUNC 0
-# define GFX_DEVID 0x22b1
 
 /* P-Unit DPTF */
 #define PUNIT_DEV 0xB
 #define PUNIT_FUNC 0
-#define PUNIT_DEVID 0x22DC
 
 /* MMC Port */
 #define MMC_DEV 0x10
 #define MMC_FUNC 0
-# define MMC_DEVID 0x2294
 
 /* SDIO Port */
 #define SDIO_DEV 0x11
 #define SDIO_FUNC 0
-# define SDIO_DEVID 0x2295
 
 /* SD Port */
 #define SD_DEV 0x12
 #define SD_FUNC 0
-# define SD_DEVID 0x2296
 
 /* SATA */
 #define SATA_DEV 0x13
 #define SATA_FUNC 0
-#define AHCI1_DEVID 0x22a3
 
 /* xHCI */
 #define XHCI_DEV 0x14
 #define XHCI_FUNC 0
-#define XHCI_DEVID 0x22b5
 
 /* LPE Audio */
 #define LPE_DEV 0x15
 #define LPE_FUNC 0
-# define LPE_DEVID 0x22a8
 
 /* Serial IO 1 */
 #define SIO1_DEV 0x18
 # define SIO_DMA1_DEV SIO1_DEV
 # define SIO_DMA1_FUNC 0
-# define SIO_DMA1_DEVID 0x22c0
 # define I2C1_DEV SIO1_DEV
 # define I2C1_FUNC 1
-# define I2C1_DEVID 0x22c1
 # define I2C2_DEV SIO1_DEV
 # define I2C2_FUNC 2
-# define I2C2_DEVID 0x22c2
 # define I2C3_DEV SIO1_DEV
 # define I2C3_FUNC 3
-# define I2C3_DEVID 0x22c3
 # define I2C4_DEV SIO1_DEV
 # define I2C4_FUNC 4
-# define I2C4_DEVID 0x22c4
 # define I2C5_DEV SIO1_DEV
 # define I2C5_FUNC 5
-# define I2C5_DEVID 0x22c5
 # define I2C6_DEV SIO1_DEV
 # define I2C6_FUNC 6
-# define I2C6_DEVID 0x22c6
 # define I2C7_DEV SIO1_DEV
 # define I2C7_FUNC 7
-# define I2C7_DEVID 0x22c7
 
 /* Trusted Execution Engine */
 #define TXE_DEV 0x1a
 #define TXE_FUNC 0
-# define TXE_DEVID 0x2298
 
 /* HD Audio */
 #define HDA_DEV 0x1b
 #define HDA_FUNC 0
-# define HDA_DEVID 0x2284
 
 /* PCIe Ports */
 #define PCIE_DEV 0x1c
 # define PCIE_PORT1_DEV PCIE_DEV
 # define PCIE_PORT1_FUNC 0
-# define PCIE_PORT1_DEVID 0x22c8
 # define PCIE_PORT2_DEV PCIE_DEV
 # define PCIE_PORT2_FUNC 1
-# define PCIE_PORT2_DEVID 0x22ca
 # define PCIE_PORT3_DEV PCIE_DEV
 # define PCIE_PORT3_FUNC 2
-# define PCIE_PORT3_DEVID 0x22cc
 # define PCIE_PORT4_DEV PCIE_DEV
 # define PCIE_PORT4_FUNC 3
-# define PCIE_PORT4_DEVID 0x22ce
 /* Total number of ROOT PORTS */
 #define MAX_ROOT_PORTS_BSW 4
 
@@ -123,34 +100,59 @@
 #define SIO2_DEV 0x1e
 # define SIO_DMA2_DEV SIO2_DEV
 # define SIO_DMA2_FUNC 0
-# define SIO_DMA2_DEVID 0x2286
 # define PWM1_DEV SIO2_DEV
 # define PWM1_FUNC 1
-# define PWM1_DEVID 0x2288
 # define PWM2_DEV SIO2_DEV
 # define PWM2_FUNC 2
-# define PWM2_DEVID 0x2289
 # define HSUART1_DEV SIO2_DEV
 # define HSUART1_FUNC 3
-# define HSUART1_DEVID 0x228a
 # define HSUART2_DEV SIO2_DEV
 # define HSUART2_FUNC 4
-# define HSUART2_DEVID 0x228c
 # define SPI_DEV SIO2_DEV
 # define SPI_FUNC 5
-# define SPI_DEVID 0x228e
 
 /* Platform Controller Unit */
 #define PCU_DEV 0x1f
 # define LPC_DEV PCU_DEV
 # define LPC_FUNC 0
-# define LPC_DEVID 0x229c
 # define SMBUS_DEV PCU_DEV
 # define SMBUS_FUNC 3
-# define SMBUS_DEVID 0x0f12
 
 /* PCH SCC Device Modes */
 #define PCH_DISABLED 0
 #define PCH_PCI_MODE 1
 #define PCH_ACPI_MODE 2
+
+#define SOC_DEVID 0x2280
+#define GFX_DEVID 0x22b1
+#define PUNIT_DEVID 0x22DC
+#define MMC_DEVID 0x2294
+#define SDIO_DEVID 0x2295
+#define SD_DEVID 0x2296
+#define AHCI1_DEVID 0x22a3
+#define XHCI_DEVID 0x22b5
+#define LPE_DEVID 0x22a8
+#define SIO_DMA1_DEVID 0x22c0
+#define I2C1_DEVID 0x22c1
+#define I2C2_DEVID 0x22c2
+#define I2C3_DEVID 0x22c3
+#define I2C4_DEVID 0x22c4
+#define I2C5_DEVID 0x22c5
+#define I2C6_DEVID 0x22c6
+#define I2C7_DEVID 0x22c7
+#define TXE_DEVID 0x2298
+#define HDA_DEVID 0x2284
+#define PCIE_PORT1_DEVID 0x22c8
+#define PCIE_PORT2_DEVID 0x22ca
+#define PCIE_PORT3_DEVID 0x22cc
+#define PCIE_PORT4_DEVID 0x22ce
+#define SIO_DMA2_DEVID 0x2286
+#define PWM1_DEVID 0x2288
+#define PWM2_DEVID 0x2289
+#define HSUART1_DEVID 0x228a
+#define HSUART2_DEVID 0x228c
+#define SPI_DEVID 0x228e
+#define LPC_DEVID 0x229c
+#define SMBUS_DEVID 0x0f12
+
 #endif /* _SOC_PCI_DEVS_H_ */
--- src/soc/intel/fsp_baytrail/include/soc/pci_devs.h
+++ src/soc/intel/fsp_baytrail/include/soc/pci_devs.h
@@ -26,48 +26,38 @@
 /* SoC transaction router */
 #define SOC_DEV 0x0
 #define SOC_FUNC 0
-# define SOC_DEVID 0x0f00
 # define SOC_DEV_FUNC PCI_DEVFN(SOC_DEV,SOC_FUNC)
 
 
 /* Graphics and Display */
 #define GFX_DEV 0x2
 #define GFX_FUNC 0
-# define GFX_DEVID 0x0f31
 # define GFX_DEV_FUNC PCI_DEVFN(GFX_DEV,GFX_FUNC)
 
 /* MIPI */
 #define MIPI_DEV 0x3
 #define MIPI_FUNC 0
-# define MIPI_DEVID 0x0f38
 # define MIPI_DEV_FUNC PCI_DEVFN(MIPI_DEV,MIPI_FUNC)
 
 
 /* SDIO Port */
 #define EMMC_DEV 0x10
 #define EMMC_FUNC 0
-# define EMMC_DEVID 0x0f14
 # define EMMC_DEV_FUNC PCI_DEVFN(EMMC_DEV,EMMC_FUNC)
 
 /* SDIO Port */
 #define SDIO_DEV 0x11
 #define SDIO_FUNC 0
-# define SDIO_DEVID 0x0f15
 # define SDIO_DEV_FUNC PCI_DEVFN(SDIO_DEV,SDIO_FUNC)
 
 /* SD Port */
 #define SD_DEV 0x12
 #define SD_FUNC 0
-# define SD_DEVID 0x0f16
 # define SD_DEV_FUNC PCI_DEVFN(SD_DEV,SD_FUNC)
 
 /* SATA */
 #define SATA_DEV 0x13
 #define SATA_FUNC 0
-# define IDE1_DEVID 0x0f20
-# define IDE2_DEVID 0x0f21
-# define AHCI1_DEVID 0x0f22
-# define AHCI2_DEVID 0x0f23
 # define SATA_MA 0x84
 # define SATA_MAP 0x90
 # define SATA_PSC 0x92
@@ -84,7 +74,6 @@
 /* xHCI */
 #define XHCI_DEV 0x14
 #define XHCI_FUNC 0
-# define XHCI_DEVID 0x0f35
 # define XHCI_FUS_REG	0xE0
 # define XHCI_FUNC_DISABLE	(1 << 0)
 # define XHCI_USB2PR_REG 0xD0
@@ -93,47 +82,36 @@
 /* LPE Audio */
 #define LPE_DEV 0x15
 #define LPE_FUNC 0
-# define LPE_DEVID 0x0f28
 # define LPE_DEV_FUNC PCI_DEVFN(LPE_DEV,LPE_FUNC)
 
 /* OTG */
 #define OTG_DEV 0x16
 #define OTG_FUNC 0
-# define OTG_DEVID 0x0f37
 # define OTG_DEV_FUNC PCI_DEVFN(LPE_DEV,LPE_FUNC)
 
 /* MMC Port */
 #define MMC45_DEV 0x17
 #define MMC45_FUNC 0
-# define MMC45_DEVID 0x0f50
 # define MMC45_DEV_FUNC PCI_DEVFN(MMC45_DEV,MMC45_FUNC)
 
 /* Serial IO 1 */
 #define SIO1_DEV 0x18
 # define SIO_DMA1_DEV SIO1_DEV
 # define SIO_DMA1_FUNC 0
-# define SIO_DMA1_DEVID 0x0f40
 # define I2C1_DEV SIO1_DEV
 # define I2C1_FUNC 1
-# define I2C1_DEVID 0x0f41
 # define I2C2_DEV SIO1_DEV
 # define I2C2_FUNC 2
-# define I2C2_DEVID 0x0f42
 # define I2C3_DEV SIO1_DEV
 # define I2C3_FUNC 3
-# define I2C3_DEVID 0x0f43
 # define I2C4_DEV SIO1_DEV
 # define I2C4_FUNC 4
-# define I2C4_DEVID 0x0f44
 # define I2C5_DEV SIO1_DEV
 # define I2C5_FUNC 5
-# define I2C5_DEVID 0x0f45
 # define I2C6_DEV SIO1_DEV
 # define I2C6_FUNC 6
-# define I2C6_DEVID 0x0f46
 # define I2C7_DEV SIO1_DEV
 # define I2C7_FUNC 7
-# define I2C7_DEVID 0x0f47
 # define SIO_DMA1_DEV_FUNC PCI_DEVFN(SIO_DMA1_DEV,SIO_DMA1_FUNC)
 # define I2C1_DEV_FUNC PCI_DEVFN(I2C1_DEV,I2C1_FUNC)
 # define I2C2_DEV_FUNC PCI_DEVFN(I2C2_DEV,I2C2_FUNC)
@@ -146,13 +124,11 @@
 /* Trusted Execution Engine */
 #define TXE_DEV 0x1a
 #define TXE_FUNC 0
-# define TXE_DEVID 0x0f18
 # define TXE_DEV_FUNC PCI_DEVFN(TXE_DEV,TXE_FUNC)
 
 /* HD Audio */
 #define HDA_DEV 0x1b
 #define HDA_FUNC 0
-# define HDA_DEVID 0x0f04
 # define HDA_DEV_FUNC PCI_DEVFN(HDA_DEV,HDA_FUNC)
 # define HDA_AZUBAR 0x14
 # define HDA_MMLA 0x64
@@ -165,16 +141,12 @@
 #define PCIE_DEV 0x1c
 # define PCIE_PORT1_DEV PCIE_DEV
 # define PCIE_PORT1_FUNC 0
-# define PCIE_PORT1_DEVID 0x0f48
 # define PCIE_PORT2_DEV PCIE_DEV
 # define PCIE_PORT2_FUNC 1
-# define PCIE_PORT2_DEVID 0x0f4a
 # define PCIE_PORT3_DEV PCIE_DEV
 # define PCIE_PORT3_FUNC 2
-# define PCIE_PORT3_DEVID 0x0f4c
 # define PCIE_PORT4_DEV PCIE_DEV
 # define PCIE_PORT4_FUNC 3
-# define PCIE_PORT4_DEVID 0x0f4e
 # define PCIE_PORT1_DEV_FUNC PCI_DEVFN(PCIE_DEV,PCIE_PORT1_FUNC)
 # define PCIE_PORT2_DEV_FUNC PCI_DEVFN(PCIE_DEV,PCIE_PORT2_FUNC)
 # define PCIE_PORT3_DEV_FUNC PCI_DEVFN(PCIE_DEV,PCIE_PORT3_FUNC)
@@ -183,29 +155,22 @@
 /* EHCI */
 #define EHCI_DEV 0x1d
 #define EHCI_FUNC 0
-# define EHCI_DEVID 0x0f34
 # define EHCI_DEV_FUNC PCI_DEVFN(EHCI_DEV,EHCI_FUNC)
 
 /* Serial IO 2 */
 #define SIO2_DEV 0x1e
 # define SIO_DMA2_DEV SIO2_DEV
 # define SIO_DMA2_FUNC 0
-# define SIO_DMA2_DEVID 0x0f06
 # define PWM1_DEV SIO2_DEV
 # define PWM1_FUNC 1
-# define PWM1_DEVID 0x0f08
 # define PWM2_DEV SIO2_DEV
 # define PWM2_FUNC 2
-# define PWM2_DEVID 0x0f09
 # define HSUART1_DEV SIO2_DEV
 # define HSUART1_FUNC 3
-# define HSUART1_DEVID 0x0f0a
 # define HSUART2_DEV SIO2_DEV
 # define HSUART2_FUNC 4
-# define HSUART2_DEVID 0x0f0c
 # define SPI_DEV SIO2_DEV
 # define SPI_FUNC 5
-# define SPI_DEVID 0xf0e
 # define SIO_DMA2_DEV_FUNC PCI_DEVFN(SIO_DMA2_DEV,SIO_DMA2_FUNC)
 # define PWM1_DEV_FUNC PCI_DEVFN(PWM1_DEV,PWM1_FUNC)
 # define PWM2_DEV_FUNC PCI_DEVFN(PWM2_DEV,PWM2_FUNC)
@@ -218,10 +183,8 @@
 #define PCU_DEV 0x1f
 # define LPC_DEV PCU_DEV
 # define LPC_FUNC 0
-# define LPC_DEVID 0x0f1c
 # define SMBUS_DEV PCU_DEV
 # define SMBUS_FUNC 3
-# define SMBUS_DEVID 0x0f12
 # define LPC_DEV_FUNC PCI_DEVFN(LPC_DEV,LPC_FUNC)
 # define LPC_BDF		PCI_DEV(0, LPC_DEV, LPC_FUNC)
 # define SMBUS_DEV_FUNC PCI_DEVFN(SMBUS_DEV,SMBUS_FUNC)
@@ -233,5 +196,44 @@
 #define ASM_AFTER_CAR_TSC_LOC				(0x80 << 24 | SATA_DEV_FUNC << 8 | SATA_BIST2)
 #define START_ROMSTAGE_TIMESTAMP_LOCATION	HDA_MMLA_BDFO
 #define BEFORE_RAMINIT_TIMESTAMP_LOCATION	SATA_MA_BDFO
+
+
+#define SOC_DEVID 0x0f00
+#define GFX_DEVID 0x0f31
+#define MIPI_DEVID 0x0f38
+#define EMMC_DEVID 0x0f14
+#define SDIO_DEVID 0x0f15
+#define SD_DEVID 0x0f16
+#define IDE1_DEVID 0x0f20
+#define IDE2_DEVID 0x0f21
+#define AHCI1_DEVID 0x0f22
+#define AHCI2_DEVID 0x0f23
+#define XHCI_DEVID 0x0f35
+#define LPE_DEVID 0x0f28
+#define OTG_DEVID 0x0f37
+#define MMC45_DEVID 0x0f50
+#define SIO_DMA1_DEVID 0x0f40
+#define I2C1_DEVID 0x0f41
+#define I2C2_DEVID 0x0f42
+#define I2C3_DEVID 0x0f43
+#define I2C4_DEVID 0x0f44
+#define I2C5_DEVID 0x0f45
+#define I2C6_DEVID 0x0f46
+#define I2C7_DEVID 0x0f47
+#define TXE_DEVID 0x0f18
+#define HDA_DEVID 0x0f04
+#define PCIE_PORT1_DEVID 0x0f48
+#define PCIE_PORT2_DEVID 0x0f4a
+#define PCIE_PORT3_DEVID 0x0f4c
+#define PCIE_PORT4_DEVID 0x0f4e
+#define EHCI_DEVID 0x0f34
+#define SIO_DMA2_DEVID 0x0f06
+#define PWM1_DEVID 0x0f08
+#define PWM2_DEVID 0x0f09
+#define HSUART1_DEVID 0x0f0a
+#define HSUART2_DEVID 0x0f0c
+#define SPI_DEVID 0xf0e
+#define LPC_DEVID 0x0f1c
+#define SMBUS_DEVID 0x0f12
 
 #endif /* _BAYTRAIL_PCI_DEVS_H_ */

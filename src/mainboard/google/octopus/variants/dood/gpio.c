42cad6c1b6fcf00fb675849a731911c876c9028b
Tim Chen <tim-chen@quanta.corp-partner.google.com>
2019-10-03 14:22:52 +0800

    mb/google/octopus: Create Dood variant
    This commit creates a dood variant for Octopus. The initial settings
    override the baseboard was copied from variant bobba.
    
    BUG=b:141960652
    BRANCH=octopus
    TEST=emerge-octopus coreboot
    
    Change-Id: Id8852e1f04f4356fac5445f6da6d56d430c88ad0
    Signed-off-by: Tim Chen <tim-chen@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/35768
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marco Chen <marcochen@google.com>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Martin Roth <martinroth@google.com>

---
+++ src/mainboard/google/octopus/variants/dood/gpio.c
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2019 The coreboot project Authors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <boardid.h>
+#include <gpio.h>
+#include <soc/gpio.h>
+
+static const struct pad_config default_override_table[] = {
+	PAD_NC(GPIO_104, UP_20K),
+
+	/* EN_PP3300_TOUCHSCREEN */
+	PAD_CFG_GPO_IOSSTATE_IOSTERM(GPIO_146, 0, DEEP, NONE, Tx0RxDCRx0,
+				     DISPUPD),
+
+	PAD_NC(GPIO_213, DN_20K),
+};
+
+const struct pad_config *variant_override_gpio_table(size_t *num)
+{
+	*num = ARRAY_SIZE(default_override_table);
+
+	return default_override_table;
+}

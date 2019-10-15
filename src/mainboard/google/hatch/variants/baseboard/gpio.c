4d77bf2a23d23301f889a5ffa4616d460712ab85
Paul Fagerburg <pfagerburg@chromium.org>
2019-10-15 11:01:28 -0600

    hatch: refactor gpio table into baseboard, allow empty SPDs
    Each variant needed to define variant_early_gpio_table(), even if
    it didn't need to make any changes. Added a __weak version of the
    function into baseboard/gpio.c.
    
    Certain upcoming Hatch variants will not use SPD files. Allow
    SPD_SOURCES in spd/Makefile.inc to be empty.
    
    BUG=None
    BRANCH=None
    TEST=Build coreboot and see that it builds without error
    
    Change-Id: Ie946cfd7c071824168faa38fd53bd338a5a451e1
    Signed-off-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/36068
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/hatch/variants/baseboard/gpio.c
+++ src/mainboard/google/hatch/variants/baseboard/gpio.c
@@ -443,3 +443,10 @@ const struct pad_config *__weak override_gpio_table(size_t *num)
 	*num = 0;
 	return NULL;
 }
+
+/* Weak implementation of early gpio */
+const struct pad_config *__weak variant_early_gpio_table(size_t *num)
+{
+	*num = 0;
+	return NULL;
+}

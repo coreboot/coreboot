626a53776b2915d7f27a8a97f9d84200b20f3079
T Michael Turney <mturney@codeaurora.org>
2019-10-15 07:40:30 -0700

    trogdor: Add mainboard gpio support
    Change-Id: I06cdb8eaaf7f74b47e1d1283dcaa765674ceaa45
    Signed-off-by: T Michael Turney <mturney@codeaurora.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/36070
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/trogdor/board.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2018, The Linux Foundation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _COREBOOT_SRC_MAINBOARD_GOOGLE_TROGDOR_BOARD_H_
+#define _COREBOOT_SRC_MAINBOARD_GOOGLE_TROGDOR_BOARD_H_
+
+#include <gpio.h>
+#include <soc/gpio.h>
+
+#define GPIO_EC_IN_RW	GPIO(118)
+#define GPIO_AP_EC_INT	GPIO(94)
+#define GPIO_AP_SUSPEND	GPIO(20)
+#define GPIO_WP_STATE	GPIO(42)
+#define GPIO_H1_AP_INT	GPIO(21)
+
+void setup_chromeos_gpios(void);
+
+#endif /* _COREBOOT_SRC_MAINBOARD_GOOGLE_TROGDOR_BOARD_H_ */
---
+++ src/mainboard/google/trogdor/boardid.c
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC
+ * Copyright 2019 The Linux Foundation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <boardid.h>
+#include <gpio.h>
+
+uint32_t board_id(void)
+{
+	static uint32_t id = UNDEFINED_STRAPPING_ID;
+
+	const gpio_t pins[] = {[2] = GPIO(31), [1] = GPIO(93), [0] = GPIO(33)};
+
+	if (id == UNDEFINED_STRAPPING_ID)
+		id = gpio_base2_value(pins, ARRAY_SIZE(pins));
+
+	return id;
+}
+
+uint32_t ram_code(void)
+{
+	static uint32_t id = UNDEFINED_STRAPPING_ID;
+
+	const gpio_t pins[] = {[1] = GPIO(91), [0] = GPIO(29)};
+
+	if (id == UNDEFINED_STRAPPING_ID)
+		id = gpio_base2_value(pins, ARRAY_SIZE(pins));
+
+	return id;
+}
+
+uint32_t sku_id(void)
+{
+	static uint32_t id = UNDEFINED_STRAPPING_ID;
+
+	const gpio_t pins[] = {[1] = GPIO(90), [0] = GPIO(114)};
+
+	if (id == UNDEFINED_STRAPPING_ID)
+		id = gpio_base2_value(pins, ARRAY_SIZE(pins));
+
+	return id;
+}

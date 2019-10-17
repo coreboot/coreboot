9f9656817119fdf481ff7fdde6382f4c06a67aa5
Marshall Dawson <marshalldawson3rd@gmail.com>
2019-10-16 19:40:07 -0600

    superio/smsc: Restore sio1036
    Change d3a1a417 "src/superio: Remove unused superio chips" removed
    all unused devices except for ones used on mainboards still under
    review.  The SMSC 1036 was inadvertenly also removed as well.  This
    device is used in debug cards that may be connected to AMD CRBs.
    
    This patch restores the smsc1036 directory as-is and then corrects
    the following lint messages.
     * WARNING: Prefer 'unsigned int' to bare use of 'unsigned'
     * ERROR: else should follow close brace '}'
     * WARNING: braces {} are not necessary for single statement blocks
    
    Change-Id: I851826e12032f802b9b2ff86d5a0eb99871bee6d
    Signed-off-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/36119
    Reviewed-by: Martin Roth <martinroth@google.com>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/superio/smsc/sio1036/sio1036.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SUPERIO_SMSC_SIO1306_H
+#define SUPERIO_SMSC_SIO1306_H
+
+#define SIO1036_SP1		0 /* Com1 */
+
+#define UART_POWER_DOWN		(1 << 7)
+#define LPT_POWER_DOWN		(1 << 2)
+#define IR_OUTPUT_MUX		(1 << 6)
+
+#include <device/pnp_type.h>
+#include <stdint.h>
+
+void sio1036_enable_serial(pnp_devfn_t dev, u16 iobase);
+
+#endif /* SUPERIO_SMSC_SIO1306_H */
---
+++ src/superio/smsc/sio1036/sio1036_early_init.c
@@ -0,0 +1,95 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2012 Advanced Micro Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/* Pre-RAM driver for the SMSC KBC1100 Super I/O chip */
+
+#include <arch/io.h>
+#include <device/pnp_ops.h>
+#include <stdint.h>
+
+#include "sio1036.h"
+
+static inline void sio1036_enter_conf_state(pnp_devfn_t dev)
+{
+	u8 port = dev >> 8;
+	outb(0x55, port);
+}
+
+static inline void sio1036_exit_conf_state(pnp_devfn_t dev)
+{
+	u8 port = dev >> 8;
+	outb(0xaa, port);
+}
+
+/* Detect SMSC SIO1036 LPC Debug Card status */
+static u8 detect_sio1036_chip(unsigned int port)
+{
+	pnp_devfn_t dev = PNP_DEV(port, SIO1036_SP1);
+	u8 data;
+
+	sio1036_enter_conf_state(dev);
+	data = pnp_read_config(dev, 0x0D);
+	sio1036_exit_conf_state(dev);
+
+	/* Detect SMSC SIO1036 chip */
+	if (data == 0x82) {
+		/* Found SMSC SIO1036 chip */
+		return 0;
+	} else {
+		return 1;
+	};
+}
+
+void sio1036_enable_serial(pnp_devfn_t dev, u16 iobase)
+{
+	unsigned int port = dev >> 8;
+
+	if (detect_sio1036_chip(port) != 0)
+		return;
+
+	sio1036_enter_conf_state(dev);
+
+	/* Enable SMSC UART 0 */
+	/* Valid configuration cycle */
+	pnp_write_config(dev, 0x00, 0x28);
+
+	/* PP power/mode/cr lock */
+	pnp_write_config(dev, 0x01, 0x98 | LPT_POWER_DOWN);
+	pnp_write_config(dev, 0x02, 0x08 | UART_POWER_DOWN);
+
+	/*Auto power management*/
+	pnp_write_config(dev, 0x07, 0x00);
+
+	/*ECP FIFO threhod */
+	pnp_write_config(dev, 0x0A, 0x00 | IR_OUTPUT_MUX);
+
+	/*GPIO direction register 2 */
+	pnp_write_config(dev, 0x033, 0x00);
+
+	/*UART Mode */
+	pnp_write_config(dev, 0x0C, 0x02);
+
+	/* GPIO polarity regisgter 2 */
+	pnp_write_config(dev, 0x034, 0x00);
+
+	/* Enable SMSC UART 0 */
+	/*Set base io address */
+	pnp_write_config(dev, 0x25, (u8)(iobase >> 2));
+
+	/* Set UART IRQ onto 0x04 */
+	pnp_write_config(dev, 0x28, 0x04);
+
+	sio1036_exit_conf_state(dev);
+}

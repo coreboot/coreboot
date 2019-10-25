776da0872efcc58543afbe32fc55202521ef0014
Patrick Rudolph <siro@das-labor.org>
2019-10-25 08:09:33 +0200

    arch/x86/*.S: use defines instead of hardcoded values
    As preparation for x86_64 clean the assembly code and introduce
    arch/ram_segs.h similar to existing arch/rom_segs.h.
    
    Replace open coded segment values with the defines from the new
    header.
    
    Change-Id: Ib006cd4df59951335506b8153e9347450ec3403e
    Signed-off-by: Patrick Rudolph <siro@das-labor.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/36321
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/arch/x86/include/arch/ram_segs.h
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef RAM_SEGS_H
+#define RAM_SEGS_H
+
+#define RAM_CODE_SEG            0x10
+#define RAM_DATA_SEG            0x18
+#define RAM_CODE16_SEG          0x28
+#define RAM_DATA16_SEG          0x30
+#define RAM_CODE_ACPI_SEG       0x38
+#define RAM_DATA_ACPI_SEG       0x40
+#define RAM_CODE_SEG64          0x48
+
+#endif /* RAM_SEGS_H */

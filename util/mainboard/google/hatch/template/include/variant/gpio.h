f1ca63ca40d27287b0b187d9763f06ce8ef56efb
Paul Fagerburg <pfagerburg@chromium.org>
2019-10-28 11:27:04 -0600

    automation: add GPIOs and version number, change branch name
    * Add defines for GPIO_MEM_CONFIG_0:3 in the template file, so
    that code that relies on these defines can compile. Because they
    are preprocessor symbols, there is no way to define them as
    __weak in the baseboard header and allow the variant to override
    as needed, so they need to be defined here and changed if needed.
    * Add a version number for the script and an "auto-generated by"
    line in the git commit message.
    * Change the branch name so that it's not the same as the ones
    that the other scripts will create, so that repo upload on those
    CLs won't affect this one.
    
    BUG=b:140261109
    BRANCH=None
    TEST=Create and build the "sushi" variant:
    $ util/mainboard/google/hatch/create_coreboot_variant.sh sushi
    $ util/abuild/abuild -p none -t google/hatch -x -a
    
    Prior to this CL, you would get an error message that SPD_SOURCES is
    not set. If you fixed that, then you would get failures for
    GPIO_MEM_CONFIG_0, _1, _2, and _3 not defined, and/or gpio_table[]
    and early_gpio_table[] not defined. After the CL, the build proceeds.
    
    Change-Id: I0f48d6bb9544cad6d419d3a6fbb17f57200938b2
    Signed-off-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/36408
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- util/mainboard/google/hatch/template/include/variant/gpio.h
+++ util/mainboard/google/hatch/template/include/variant/gpio.h
@@ -16,4 +16,11 @@
 
 #include <baseboard/gpio.h>
 
+/* Memory configuration board straps */
+/* Copied from baseboard and may need to change for the new variant. */
+#define GPIO_MEM_CONFIG_0	GPP_F20
+#define GPIO_MEM_CONFIG_1	GPP_F21
+#define GPIO_MEM_CONFIG_2	GPP_F11
+#define GPIO_MEM_CONFIG_3	GPP_F22
+
 #endif

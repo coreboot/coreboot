ab0d687fc5ab7fb07e7c383c10902b586415c9bf
Michael Niewöhner <foss@mniewoehner.de>
2019-11-03 22:43:58 +0100

    mb/supermicro/x11-lga1151-series: drop console guard in bootblock
    To make debugging possible in a fallback setup, the serial console must
    be set up in bootblock, thus drop the guard.
    
    Change-Id: If0dd3c03ba52b4936eb234e6b2b61bb5ce044fcd
    Signed-off-by: Michael Niewöhner <foss@mniewoehner.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/36602
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/supermicro/x11-lga1151-series/bootblock.c
+++ src/mainboard/supermicro/x11-lga1151-series/bootblock.c
@@ -32,8 +32,7 @@ static void early_config_gpio(void)
 static void early_config_superio(void)
 {
 	const pnp_devfn_t serial_dev = PNP_DEV(0x2e, AST2400_SUART1);
-	if (CONFIG(CONSOLE_SERIAL))
-		aspeed_enable_serial(serial_dev, CONFIG_TTYS0_BASE);
+	aspeed_enable_serial(serial_dev, CONFIG_TTYS0_BASE);
 }
 
 void bootblock_mainboard_early_init(void)

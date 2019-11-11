37bf996efb16aa48fb0270b38b3c83633c3bf784
Jeff Chase <jnchase@google.com>
2019-11-11 18:05:08 -0500

    mb/google/fizz: Add Endeavour variant
    Use the existing Karma variant as a base.
    
    BUG=b:144307303
    TEST=build
    
    Change-Id: I09a10e99877d18361b31b36bed703b02508ccd05
    Signed-off-by: Jeff Chase <jnchase@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/36791
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Shelley Chen <shchen@google.com>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

---
+++ src/mainboard/google/fizz/variants/endeavour/nhlt.c
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2019 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#include <baseboard/variants.h>
+#include <console/console.h>
+#include <nhlt.h>
+#include <soc/nhlt.h>
+
+void variant_nhlt_oem_overrides(const char **oem_id,
+					const char **oem_table_id,
+					uint32_t *oem_revision)
+{
+	*oem_id = "GOOGLE";
+	*oem_table_id = "ENDEAVOUR";
+	*oem_revision = 0;
+}

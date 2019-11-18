322635a955ed6133508dd56b85f65ad448fcb1c5
Bill XIE <persmule@hardenedlinux.org>
2019-11-19 00:29:27 +0800

    mb/gigabyte/ga-b75m-d3h: Add ga-b75-d3v as a variant
    It is an ATX board similar to existing ga-b75* boards. The major
    difference is the configuration of pci-e ports on PCH, and on-board
    pci-e NIC. (see below)
    
    Tested:
        - CPU i5 3570T
        - Slotted DIMM 8GiB*4 from Kingston
        - usb2 and usb3
        - pci and pci-e ports
        - sata
        - Sound
        - S3
        - AR8161 NIC connected to 1c.2 with mac address burnt in efuse
        - libgfxinit-based graphic init
        - NVRAM options for North and South bridges
        - tpm 1.2 on lpc (similar to ga-b75m-d3h)
        - Linux 4.19.67-2 within Debian GNU/Linux stable, loaded from
          SeaBIOS.
    
    Change-Id: I1a969880e4da02abf8ba73aac60ee1296fe0abf2
    Signed-off-by: Bill XIE <persmule@hardenedlinux.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/36992
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

---
+++ src/mainboard/gigabyte/ga-b75m-d3h/variants/ga-b75-d3v/include/variant/hda_verb.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef GA_B75_D3V_HDA_VERB_H
+#define GA_B75_D3V_HDA_VERB_H
+
+const u32 cim_verb_data[] = {
+	/* coreboot specific header */
+	0x10ec0887,	// Realtek 887
+	0x1458a002,	// Subsystem ID
+	0x0000000e,	// Number of entries
+
+	/* NID 0x01: Subsystem ID.  */
+	AZALIA_SUBVENDOR(0x2, 0x1458a002),
+
+	/* Pin Widget Verb Table */
+	AZALIA_PIN_CFG(0x2, 0x11, 0x99430130),
+	AZALIA_PIN_CFG(0x2, 0x12, 0x411111f0),
+	AZALIA_PIN_CFG(0x2, 0x14, 0x01014410),
+	AZALIA_PIN_CFG(0x2, 0x15, 0x411111f0),
+	AZALIA_PIN_CFG(0x2, 0x16, 0x411111f0),
+	AZALIA_PIN_CFG(0x2, 0x17, 0x411111f0),
+	AZALIA_PIN_CFG(0x2, 0x18, 0x01a19c50),
+	AZALIA_PIN_CFG(0x2, 0x19, 0x02a19c60),
+	AZALIA_PIN_CFG(0x2, 0x1a, 0x0181345f),
+	AZALIA_PIN_CFG(0x2, 0x1b, 0x02214c20),
+	AZALIA_PIN_CFG(0x2, 0x1c, 0x411111f0),
+	AZALIA_PIN_CFG(0x2, 0x1d, 0x4004c601),
+	AZALIA_PIN_CFG(0x2, 0x1e, 0x411111f0),
+	AZALIA_PIN_CFG(0x2, 0x1f, 0x411111f0),
+};
+
+#endif

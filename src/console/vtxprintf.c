coreboot/coreboot

commit f96d9051c2b39544300d35f64ce92502e1e230c0

Remove MIPS architecture
The MIPS architecture port has been added 5+ years ago in order to
support a Chrome OS project that ended up going nowhere. No other board
has used it since and nobody is still willing or has the expertise and
hardware to maintain it. We have decided that it has become too much of
a mainenance burden and the chance of anyone ever reviving it seems too
slim at this point. This patch eliminates all MIPS code and
MIPS-specific hacks.

Change-Id: I5e49451cd055bbab0a15dcae5f53e0172e6e2ebe
Signed-off-by: Julius Werner <jwerner@chromium.org>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/34919
Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
Reviewed-by: Hung-Te Lin <hungte@chromium.org>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/console/vtxprintf.c
@@ -20,10 +20,6 @@
 
 #define call_tx(x) tx_byte(x, data)
 
-#if !CONFIG(ARCH_MIPS)
-#define SUPPORT_64BIT_INTS
-#endif
-
 #define ZEROPAD	1		/* pad with zero */
 #define SIGN	2		/* unsigned/signed long */
 #define PLUS	4		/* show plus */

commit 540b2adb6165a5a86bc575065445df63cf1516cf

src/console: Bring back support for printf'ing 64bit ints
commit f96d9051c2 (Remove MIPS Architecture) accidentally enabled
a MIPS special case to not support 64bit integers in printf for
all platforms.

This removes that MIPS-only special case entirely.

Change-Id: I5245bb32b45f9bd37bd012a7b15a64fba24a4cb7
Signed-off-by: Patrick Georgi <pgeorgi@google.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/37113
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Jacob Garber <jgarber1@ualberta.ca>
Reviewed-by: Julius Werner <jwerner@chromium.org>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>

src/console/vtxprintf.c
@@ -36,20 +36,8 @@ static int number(void (*tx_byte)(unsigned char byte, void *data),
 	const char *digits = "0123456789abcdef";
 	int i;
 	int count = 0;
-#ifdef SUPPORT_64BIT_INTS
 	unsigned long long num = inum;
 	long long snum = num;
-#else
-	unsigned long num = (unsigned long)inum;
-	long snum = (long)num;
-
-	if (num != inum) {
-		/* Alert user to an incorrect result by printing #^!. */
-		call_tx('#');
-		call_tx('^');
-		call_tx('!');
-	}
-#endif
 
 	if (type & LARGE)
 		digits = "0123456789ABCDEF";
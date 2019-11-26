41fe62b6dccd6be84e9d3685c73f1d8683af78de
Hash.Hung <hash1.hung@lcfc.corp-partner.google.com>
2019-11-26 23:36:40 +0800

    mb/google/octopus: Create Lick variant
    Create new variant for Lick that is copied from phaser variant.
    Remove unnecessary code, due to not support touchscreen and stylus.
    Set to default_override_table.
    Remove variant.c.
    
    BUG=b:145181137
    BRANCH=octopus
    TEST=./util/abuild/abuild -p none -t google/octopus -x -a
    
    Change-Id: If732d94194defb9f5ee9c847ee93dd58aef01174
    Signed-off-by: Hash.Hung <hash1.hung@lcfc.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/37247
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-by: Henry Sun <henrysun@google.com>
    Reviewed-by: Marco Chen <marcochen@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/octopus/variants/lick/gpio.c
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2019 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <boardid.h>
+#include <gpio.h>
+#include <soc/gpio.h>
+#include <ec/google/chromeec/ec.h>
+
+#define SKU_UNKNOWN            0xFFFFFFFF
+
+static const struct pad_config default_override_table[] = {
+       /* disable I2C7 SCL and SDA */
+	PAD_NC(GPIO_114, UP_20K),     /* LPSS_I2C7_SDA */
+	PAD_NC(GPIO_115, UP_20K),     /* LPSS_I2C7_SCL */
+
+	PAD_NC(GPIO_52, UP_20K),
+	PAD_NC(GPIO_53, UP_20K),
+	PAD_NC(GPIO_67, UP_20K),
+	PAD_NC(GPIO_117, UP_20K),
+	PAD_NC(GPIO_143, UP_20K),
+
+	/* EN_PP3300_TOUCHSCREEN */
+	PAD_CFG_GPO_IOSSTATE_IOSTERM(GPIO_146, 0, DEEP, NONE, Tx0RxDCRx0, DISPUPD),
+
+	PAD_NC(GPIO_161, DN_20K),
+
+	/* EN_PP3300_WLAN_L */
+	PAD_CFG_GPO_IOSSTATE_IOSTERM(GPIO_178, 0, DEEP, NONE, Tx0RxDCRx0, DISPUPD),
+
+	PAD_NC(GPIO_213, DN_20K),
+	PAD_NC(GPIO_214, DN_20K),
+};
+
+const struct pad_config *variant_override_gpio_table(size_t *num)
+{
+	*num = ARRAY_SIZE(default_override_table);
+	return default_override_table;
+}

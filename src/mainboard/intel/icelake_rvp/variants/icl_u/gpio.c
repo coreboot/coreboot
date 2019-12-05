b5ba8b6d1a57f8b57b3a6aec80ed6a47618d433d
Aamir Bohra <aamir.bohra@intel.com>
2019-12-05 17:49:39 +0530

    mb/intel/icelake_rvp: Remove baseboard gpio configuration support
    Remove baseboard gpio.c and rely on variant override.
    
    Change-Id: I4657b1aa2c81a990b750e163e948b8495d8b97c7
    Signed-off-by: Aamir Bohra <aamir.bohra@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/37512
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Rizwan Qureshi <rizwan.qureshi@intel.com>
    Reviewed-by: Maulik V Vaghela <maulik.v.vaghela@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/intel/icelake_rvp/variants/icl_u/gpio.c
+++ src/mainboard/intel/icelake_rvp/variants/icl_u/gpio.c
@@ -114,3 +114,13 @@ const struct pad_config *variant_early_gpio_table(size_t *num)
 	*num = ARRAY_SIZE(early_gpio_table);
 	return early_gpio_table;
 }
+
+static const struct cros_gpio cros_gpios[] = {
+	CROS_GPIO_REC_AL(CROS_GPIO_VIRTUAL, CROS_GPIO_DEVICE_NAME),
+};
+
+const struct cros_gpio *variant_cros_gpios(size_t *num)
+{
+	*num = ARRAY_SIZE(cros_gpios);
+	return cros_gpios;
+}

07e6098d487fc9135fc47e316a6d7e0332d814a0
Frans Hendriks <fhendriks@eltan.com>
2019-12-09 09:56:44 +0100

    mb/facebook/fbg1701: Move verified items to board_verified_boot.h
    Items in onboard.h are related to verified or measured boot.
    Move the items to board_verified_boot.h and remove onboard.h.
    
    BUG=N/A
    TEST=build
    
    Change-Id: Icfc8d6d8351f0654c277e81c7f3cc2b0a947866a
    Signed-off-by: Wim Vervoorn <wvervoorn@eltan.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/37614
    Reviewed-by: Frans Hendriks <fhendriks@eltan.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/facebook/fbg1701/board_mboot.h
+++ src/mainboard/facebook/fbg1701/board_mboot.h
@@ -14,6 +14,7 @@
  */
 
 #include <mboot.h>
+#include "board_verified_boot.h"
 
 const mboot_measure_item_t mb_log_list[] = {
 	{ "config", CBFS_TYPE_RAW, MBOOT_PCR_INDEX_0, EV_NO_ACTION, NULL },
--- src/mainboard/facebook/fbg1701/board_verified_boot.h
+++ src/mainboard/facebook/fbg1701/board_verified_boot.h
@@ -17,6 +17,15 @@
 #define BOARD_VERIFIED_BOOT_H
 
 #include <vboot_check.h>
-#include "onboard.h"
+
+/* Define the items to be measured or verified */
+#define FSP (const char *)"fsp.bin"
+#define CMOS_LAYOUT (const char *)"cmos_layout.bin"
+#define RAMSTAGE (const char *)CONFIG_CBFS_PREFIX"/ramstage"
+#define ROMSTAGE (const char *)CONFIG_CBFS_PREFIX"/romstage"
+#define PAYLOAD (const char *)CONFIG_CBFS_PREFIX"/payload"
+#define POSTCAR (const char *)CONFIG_CBFS_PREFIX"/postcar"
+#define OP_ROM_VBT (const char *)"vbt.bin"
+#define MICROCODE (const char *)"cpu_microcode_blob.bin"
 
 #endif

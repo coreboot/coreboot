b6796be8e0936569c9eaf89e479549457dfb06ec
Kyösti Mälkki <kyosti.malkki@gmail.com>
2019-12-19 06:45:56 +0200

    aopen/dxplplusu: Fix IOAPIC in ASL
    Change-Id: I8a2abe2cf20952491d181766a3ca492de355fc88
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/52814
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/aopen/dxplplusu/acpi_tables.c
+++ src/mainboard/aopen/dxplplusu/acpi_tables.c
@@ -44,7 +44,7 @@ unsigned long acpi_fill_madt(unsigned long current)
 
 	/* P64H2 Bus A IOAPIC */
 	if (bdev)
-		dev = pcidev_path_behind(bdev->link_list, PCI_DEVFN(28, 0));
+		dev = pcidev_path_behind(bdev->link_list, PCI_DEVFN(30, 0));
 	if (dev) {
 		res = find_resource(dev, PCI_BASE_ADDRESS_0);
 		current += acpi_create_madt_ioapic((acpi_madt_ioapic_t *)current,

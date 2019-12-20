6bb9aaf93f40c8852bab933c968d5aef8bb68b32
Jamie Chen <jamie.chen@intel.com>
2019-12-20 19:30:33 +0800

    soc/intel/cannonlake: Add Comet Lake H SA 4+2 Device ID
    This patch adds CML-H 4+2 SA DID into systemagent.c and report
    platform.
    According to doc #605546:
        CML-H (4+2) R1: 9B64h
    
    BUG:none
    BRANCH:none
    TEST:build no error
    
    Change-Id: I5bac6173a84a11abd2ce17f82854fbb14fb8558b
    Signed-off-by: Jamie Chen <jamie.chen@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/37873
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-by: Edward Hill <ecgh@chromium.org>

--- src/soc/intel/cannonlake/bootblock/report_platform.c
+++ src/soc/intel/cannonlake/bootblock/report_platform.c
@@ -84,6 +84,7 @@ static struct {
 	{ PCI_DEVICE_ID_INTEL_CML_S_P0P1_8_2, "CometLake-S P0/P1 (8+2)" },
 	{ PCI_DEVICE_ID_INTEL_CML_S_P0P1_10_2, "CometLake-S P0/P1 (10+2)" },
 	{ PCI_DEVICE_ID_INTEL_CML_H, "CometLake-H (6+2)" },
+	{ PCI_DEVICE_ID_INTEL_CML_H_4_2, "CometLake-H (4+2)" },
 	{ PCI_DEVICE_ID_INTEL_CML_H_8_2, "CometLake-H (8+2)" },
 };
 

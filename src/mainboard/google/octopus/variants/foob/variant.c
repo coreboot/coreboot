325fd3462e0b4088f915a7583295334779f5270e
Tommie <tong.lin@bitland.corp-partner.google.com>
2019-12-23 13:33:34 +0800

    mb/google/octopus: Add two new sku IDs for foob
    Declare these sku IDs:
        -SKU: 1 Foob, 1-cam, no touch, no pen.
        -SKU: 9 Foob360, 2-cam, touch, pen.
    
    BUG=b:145837644
    BRANCH=octopus
    TEST=emerge-octopus coreboot
    
    Signed-off-by: tong.lin <tong.lin@bitland.corp-partner.google.com>
    Change-Id: Iffcbb3f6f945ea299ff687a383a82b88dcd11ea1
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/37906
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marco Chen <marcochen@google.com>

---
+++ src/mainboard/google/octopus/variants/foob/variant.c
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2019 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <baseboard/variants.h>
+#include <soc/pci_devs.h>
+#include <ec/google/chromeec/ec.h>
+
+#define SKU_UNKNOWN     0xFFFFFFFF
+
+void variant_update_devtree(struct device *dev)
+{
+	uint32_t sku_id = SKU_UNKNOWN;
+	struct device *touchscreen_i2c_host;
+
+	touchscreen_i2c_host = pcidev_path_on_root(PCH_DEVFN_I2C7);
+
+	if (touchscreen_i2c_host == NULL)
+		return;
+
+	/* SKU ID 1 does not have a touchscreen device, hence disable it. */
+	sku_id = get_board_sku();
+	if (no_touchscreen_sku(sku_id))
+		touchscreen_i2c_host->enabled = 0;
+}

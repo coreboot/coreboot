d4f39abebf069517321b5fc6157ad65318b13cf3
Ren Kuo <ren.kuo@quanta.corp-partner.google.com>
2019-12-25 14:18:08 +0800

    mb/google/octopus/variants/dood: support LTE module
    related LTE GPIOs:
      GPIO_67  - EN_PP3300
      GPIO_117 - FULL_CARD_POWER_ON_OFF
      GPIO_161 - PLT_RST_LTE_L
    1. Power on: GPIO_67 -> 0ms -> GPIO_117 -> 30ms -> GPIO_161
    2. Power off: GPIO_161 -> 30ms -> GPIO_117 -> 100ms -> GPIO_67
    3. Power reset:
      - keep GPIO_67 and GPIO_117 high and
      - pull down GPIO_161 for 30ms then release it.
    
    BUG=b:146843935
    BRANCH=octopus
    TEST=build and verify on the DUT with LTE
    
    Signed-off-by: Ren Kuo <ren.kuo@quanta.corp-partner.google.com>
    Change-Id: Ief6c993ede2bb4b3effbb05cfe22b7af4fcf7faf
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/37931
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marco Chen <marcochen@google.com>

--- src/mainboard/google/octopus/variants/dood/gpio.c
+++ src/mainboard/google/octopus/variants/dood/gpio.c
@@ -19,6 +19,11 @@
 #include <gpio.h>
 #include <soc/gpio.h>
 
+enum {
+	SKU_1_LTE  = 1, /* Wifi + LTE */
+	SKU_2_WIFI = 2, /* Wifi */
+};
+
 static const struct pad_config default_override_table[] = {
 	PAD_NC(GPIO_104, UP_20K),
 
@@ -29,9 +34,56 @@ static const struct pad_config default_override_table[] = {
 	PAD_NC(GPIO_213, DN_20K),
 };
 
+static const struct pad_config lte_override_table[] = {
+	/* Default override table. */
+	PAD_NC(GPIO_104, UP_20K),
+
+	/* EN_PP3300_TOUCHSCREEN */
+	PAD_CFG_GPO_IOSSTATE_IOSTERM(GPIO_146, 0, DEEP, NONE, Tx0RxDCRx0,
+				     DISPUPD),
+
+	PAD_NC(GPIO_213, DN_20K),
+
+	/* Be specific to LTE SKU */
+	/* UART2-CTS_B -- EN_PP3300_DX_LTE_SOC */
+	PAD_CFG_GPO(GPIO_67, 1, PWROK),
+
+	/* PCIE_WAKE1_B -- FULL_CARD_POWER_OFF */
+	PAD_CFG_GPO(GPIO_117, 1, PWROK),
+
+	/* AVS_I2S1_MCLK -- PLT_RST_LTE_L */
+	PAD_CFG_GPO(GPIO_161, 1, DEEP),
+};
+
 const struct pad_config *variant_override_gpio_table(size_t *num)
 {
-	*num = ARRAY_SIZE(default_override_table);
+	uint32_t sku_id;
+	sku_id = get_board_sku();
 
-	return default_override_table;
+	switch (sku_id) {
+	case SKU_1_LTE:
+		*num = ARRAY_SIZE(lte_override_table);
+		return lte_override_table;
+	default:
+		*num = ARRAY_SIZE(default_override_table);
+		return default_override_table;
+	}
+}
+
+static const struct pad_config lte_early_override_table[] = {
+	/* UART2-CTS_B -- EN_PP3300_DX_LTE_SOC */
+	PAD_CFG_GPO(GPIO_67, 1, PWROK),
+
+	/* PCIE_WAKE1_B -- FULL_CARD_POWER_OFF */
+	PAD_CFG_GPO(GPIO_117, 1, PWROK),
+
+	/* AVS_I2S1_MCLK -- PLT_RST_LTE_L */
+	PAD_CFG_GPO(GPIO_161, 0, DEEP),
+};
+
+const struct pad_config *variant_early_override_gpio_table(size_t *num)
+{
+	*num = ARRAY_SIZE(lte_early_override_table);
+
+	return lte_early_override_table;
 }
---
+++ src/mainboard/google/octopus/variants/dood/variant.c
@@ -0,0 +1,71 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2019 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/acpi.h>
+#include <boardid.h>
+#include <sar.h>
+#include <baseboard/variants.h>
+#include <delay.h>
+#include <gpio.h>
+
+enum {
+	SKU_1_LTE  = 1, /* Wifi + LTE */
+	SKU_2_WIFI = 2, /* Wifi */
+};
+
+struct gpio_with_delay {
+	gpio_t gpio;
+	unsigned int delay_msecs;
+};
+
+static void power_off_lte_module(u8 slp_typ)
+{
+	const struct gpio_with_delay lte_power_off_gpios[] = {
+		{
+			GPIO_161, /* AVS_I2S1_MCLK -- PLT_RST_LTE_L */
+			30,
+		},
+		{
+			GPIO_117, /* PCIE_WAKE1_B -- FULL_CARD_POWER_OFF */
+			100
+		},
+		{
+			GPIO_67, /* UART2-CTS_B -- EN_PP3300_DX_LTE_SOC */
+			0
+		}
+	};
+
+	for (int i = 0; i < ARRAY_SIZE(lte_power_off_gpios); i++) {
+		gpio_output(lte_power_off_gpios[i].gpio, 0);
+		mdelay(lte_power_off_gpios[i].delay_msecs);
+	}
+}
+
+
+void variant_smi_sleep(u8 slp_typ)
+{
+	/* Currently use cases here all target to S5 therefore we do early return
+	 * here for saving one transaction to the EC for getting SKU ID. */
+	if (slp_typ != ACPI_S5)
+		return;
+
+	switch (get_board_sku()) {
+	case SKU_1_LTE:
+		power_off_lte_module(slp_typ);
+		return;
+	default:
+		return;
+	}
+}

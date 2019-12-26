9fadd9a917c15d76067825073afb683e90ac19b1
Peter Lemenkov <lemenkov@gmail.com>
2019-12-26 21:02:24 +0300

    mb/lenovo/*/acpi_tables: Don't zero out gnvs again
    The gnvs structure was zeroed out already in the following files:
    
    * src/southbridge/intel/i82801ix/lpc.c (t400 and x200)
    * src/southbridge/intel/i82801gx/lpc.c (thinkcentre_a58)
    
    Change-Id: Id7d552e1c4084a0b36b98f9627a85a75c8b90e81
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/37937
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>

--- src/mainboard/lenovo/thinkcentre_a58/acpi_tables.c
+++ src/mainboard/lenovo/thinkcentre_a58/acpi_tables.c
@@ -20,8 +20,6 @@
 
 void acpi_create_gnvs(global_nvs_t *gnvs)
 {
-	memset((void *)gnvs, 0, sizeof(*gnvs));
-
 	gnvs->pwrs = 1;    /* Power state (AC = 1) */
 	gnvs->cmap = 0x01; /* Enable COM 1 port */
 }

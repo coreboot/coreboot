533e4399f10940796fedd6056ada60426bcc673f
Angel Pons <th3fanbus@gmail.com>
2020-01-02 00:11:30 +0100

    mb/hp/8770w: Transform into variant
    Since this board does not have integrated graphics, do not install the
    INT15 handler if it is not selected in Kconfig.
    
    NOTE: Since cmos options are not very flexible, this board ends up with
    a spurious gfx_uma_size option. Other than that, everything is the same.
    
    Tested with BUILD_TIMELESS=1, binary does not change when ignoring the
    cmos options.
    
    Change-Id: I2ebcfd5160773bf98a3d23e797a89e290063d112
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38094
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/mainboard/hp/snb_ivb_laptops/mainboard.c
+++ src/mainboard/hp/snb_ivb_laptops/mainboard.c
@@ -18,9 +18,11 @@
 
 static void mainboard_enable(struct device *dev)
 {
-	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_INT_LVDS,
-			GMA_INT15_PANEL_FIT_DEFAULT,
-			GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
+	if (CONFIG(INTEL_INT15)) {
+		install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_INT_LVDS,
+						GMA_INT15_PANEL_FIT_DEFAULT,
+						GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
+	}
 }
 
 struct chip_operations mainboard_ops = {

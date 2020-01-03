473d97940f8f42e641930b9c77ab2fca9a1e9877
Kyösti Mälkki <kyosti.malkki@gmail.com>
2020-01-03 10:12:43 +0200

    device/smbus: Drop SMBUS_HAS_AUX_CHANNELS
    The guarded prototypes are no longer implemented in the
    tree.
    
    Change-Id: I5bfedde2aaf691826e7537eceb8578a855800ea2
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38153
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Frans Hendriks <fhendriks@eltan.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/include/device/smbus.h
+++ src/include/device/smbus.h
@@ -53,9 +53,4 @@ static inline int smbus_write_byte(struct device *const dev, u8 addr, u8 val)
 int smbus_block_read(struct device *dev, u8 cmd, u8 bytes, u8 *buffer);
 int smbus_block_write(struct device *dev, u8 cmd, u8 bytes, const u8 *buffer);
 
-#if CONFIG(SMBUS_HAS_AUX_CHANNELS)
-void smbus_switch_to_channel(uint8_t channel_number);
-uint8_t smbus_get_current_channel(void);
-#endif
-
 #endif /* DEVICE_SMBUS_H */

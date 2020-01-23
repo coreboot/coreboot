977b8e83cb0a71af1143d3ab56fd0f95bf8d6c70
Patrick Rudolph <patrick.rudolph@9elements.com>
2020-01-23 13:32:08 +0100

    mb/emulation/qemu-aarch64: Add MMU support
    Enable MMU in bootblock. Makes qemu look more similar to real hardware.
    There's no real need to activate the MMU.
    
    Tested on qemu-system-aarch64: 5 page entries are used out of 32.
    
    Change-Id: Ifaed9d3cc11520f180a732d51adce634621b5844
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38534
    Reviewed-by: Philipp Deppenwiese <zaolin.daisuki@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/emulation/qemu-aarch64/bootblock.c
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/mmu.h>
+#include <bootblock_common.h>
+#include <symbols.h>
+
+void bootblock_mainboard_init(void)
+{
+	mmu_init();
+
+	/* Everything below DRAM is device memory */
+	mmu_config_range((void *)0, (uintptr_t)_dram, MA_DEV | MA_RW);
+	/* Set a dummy value for DRAM. ramstage should update the mapping. */
+	mmu_config_range(_dram, 1 * GiB, MA_MEM | MA_RW);
+
+	mmu_config_range(_ttb, REGION_SIZE(ttb), MA_MEM | MA_S | MA_RW);
+	mmu_config_range(_bootblock, REGION_SIZE(bootblock), MA_MEM | MA_S | MA_RW);
+	mmu_config_range(_romstage, REGION_SIZE(romstage), MA_MEM | MA_S | MA_RW);
+	mmu_config_range(_ramstage, REGION_SIZE(ramstage), MA_MEM | MA_S | MA_RW);
+
+	mmu_enable();
+}

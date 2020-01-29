e7601b5d6c6c3a0fdf0d779cfe12b9a381f0fba4
Craig Hesling <hesling@chromium.org>
2020-01-28 21:16:18 -0800

    hatch/mushu: Fix FPMCU pwr/rst gpio handling
    Asserting reset in RO instead of in RW has no impact on security or
    performance, but it does limit improvements to this process later.
    This fix removes reset line control from RO and makes these variants
    consistent with other hatch variants.
    
    This fix reinforces the concept from commit fcd8c9e99e
    (hatch: Fix FPMCU pwr/rst gpio handling).
    
    BUG=b:148457345
    TEST=None
    
    Change-Id: I12dc0c3bead7672e2d3207771212efb0d246973a
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38623
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

---
+++ src/mainboard/google/hatch/variants/mushu/ramstage.c
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2020 The coreboot project Authors.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include <delay.h>
+#include <gpio.h>
+#include <baseboard/variants.h>
+#include <soc/gpio.h>
+
+void variant_ramstage_init(void)
+{
+	/*
+	 * Enable power to FPMCU, wait for power rail to stabilize,
+	 * and then deassert FPMCU reset.
+	 * Waiting for the power rail to stabilize can take a while,
+	 * a minimum of 400us on Kohaku.
+	 */
+	gpio_output(GPP_C11, 1);
+	mdelay(1);
+	gpio_output(GPP_A12, 1);
+}

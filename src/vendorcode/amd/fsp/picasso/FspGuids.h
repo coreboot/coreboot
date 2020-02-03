74bee3c8adb70c8987ea1c174d58682bbf05350a
Marshall Dawson <marshalldawson3rd@gmail.com>
2020-02-03 16:01:10 -0700

    vc/amd/fsp/picasso: Add file for GUIDs
    Begin a file for GUIDs used by the FSP.
    
    Signed-off-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Change-Id: Ied5c5085ea8ed55439192be8a44fa401aeb559a7
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38697
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/vendorcode/amd/fsp/picasso/FspGuids.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
+
+#ifndef __FSP_GUIDS__
+#define __FSP_GUIDS__
+
+#include <uuid.h>
+
+#define AMD_FSP_TSEG_HOB_GUID \
+		GUID_INIT(0x5fc7897a, 0x5aff, 0x4c61, \
+		0xaa, 0x7a, 0xdd, 0xcf, 0xa9, 0x18, 0x43, 0x0c)
+
+#endif /* __FSP_GUIDS__ */

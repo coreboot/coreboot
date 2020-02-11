fa36d0b79fb9fdc9747bb653398c1893eb7221c8
Casper Chang <casper.chang@bitland.corp-partner.google.com>
2020-02-11 11:34:26 +0800

    mb/google/kukui: Add panel for Kakadu
    Declare the following panel for Kakadu:
    - BOE_TV105WUM_NW0
    
    BUG=b:148997748
    TEST=build Kakadu image passed
    BRANCH=kukui
    
    Signed-off-by: Casper Chang <casper_chang@bitand.corp-partner.google.com>
    Change-Id: I394b8cafa8be40e5fd6bf8ceb81b520df73718a4
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38822
    Reviewed-by: Peichao Li <peichao.wang@bitland.corp-partner.google.com>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/kukui/panel_kakadu.c
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2020 Bitland Tech Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "panel.h"
+
+static struct panel_description kakadu_panels[] = {
+	[1] = { .name = "BOE_TV105WUM_NW0", },
+};
+
+struct panel_description *get_panel_description(int panel_id)
+{
+	if (panel_id < 0 || panel_id >= ARRAY_SIZE(kakadu_panels))
+		return NULL;
+
+	return get_panel_from_cbfs(&kakadu_panels[panel_id]);
+}

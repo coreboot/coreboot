96eceba31424a97c183a8111a039fba342565dba
Karthikeyan Ramasubramanian <kramasub@google.com>
2020-02-12 16:28:08 -0700

    mb/google/dedede: Add waddledoo variant
    Add initial support for waddledoo board.
    
    BUG=None
    TEST=Build the mainboard and variant board.
    
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Change-Id: I8ab4d52c97b1cfb5549d2fce4b931748a1b1ff1a
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38857
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

---
+++ src/mainboard/google/dedede/variants/waddledoo/include/variant/ec.h
@@ -0,0 +1,14 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2020 The coreboot project Authors.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef MAINBOARD_EC_H
+#define MAINBOARD_EC_H
+
+#include <baseboard/ec.h>
+
+#endif
---
+++ src/mainboard/google/dedede/variants/waddledoo/include/variant/gpio.h
@@ -0,0 +1,14 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2020 The coreboot project Authors.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef MAINBOARD_GPIO_H
+#define MAINBOARD_GPIO_H
+
+#include <baseboard/gpio.h>
+
+#endif /* MAINBOARD_GPIO_H */

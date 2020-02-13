a185665de442be7ce6d498c9ae2359f938d173cf
Mike Banon <mikebdp2@gmail.com>
2020-02-13 16:07:54 +0000

    mb/lippert/frontrunner-af: Switch away from ROMCC_BOOTBLOCK
    Warning: Not tested on hardware.
    
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Signed-off-by: Mike Banon <mikebdp2@gmail.com>
    Change-Id: I63dd15ade28acb06da8d320edc8ae1fd433aa0e0
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38872
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

--- src/mainboard/lippert/frontrunner-af/bootblock.c
+++ src/mainboard/lippert/frontrunner-af/bootblock.c
@@ -13,14 +13,13 @@
  * GNU General Public License for more details.
  */
 
+#include <bootblock_common.h>
 #include <northbridge/amd/agesa/state_machine.h>
 #include <superio/smsc/smscsuperio/smscsuperio.h>
-#include <sb_cimx.h>
 
 #define SERIAL_DEV PNP_DEV(0x4e, SMSCSUPERIO_SP1)
 
-void board_BeforeAgesa(struct sysinfo *cb)
+void bootblock_mainboard_early_init(void)
 {
-	sb_Poweron_Init();
 	smscsuperio_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
 }

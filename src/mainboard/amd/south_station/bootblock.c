0dcbcd31918cd0dd7e4f1086649dfe4a04397633
Mike Banon <mikebdp2@gmail.com>
2020-02-13 16:05:35 +0000

    mb/amd/south_station: Switch away from ROMCC_BOOTBLOCK
    Warning: Not tested on hardware.
    
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Signed-off-by: Mike Banon <mikebdp2@gmail.com>
    Change-Id: Iba1d020b9e565e3c6c89a97114084d72a00b2a55
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38871
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/amd/south_station/bootblock.c
+++ src/mainboard/amd/south_station/bootblock.c
@@ -13,15 +13,13 @@
  * GNU General Public License for more details.
  */
 
-#include <northbridge/amd/agesa/state_machine.h>
+#include <bootblock_common.h>
 #include <superio/fintek/common/fintek.h>
 #include <superio/fintek/f81865f/f81865f.h>
-#include <sb_cimx.h>
 
 #define SERIAL_DEV PNP_DEV(0x4e, F81865F_SP1)
 
-void board_BeforeAgesa(struct sysinfo *cb)
+void bootblock_mainboard_early_init(void)
 {
-	sb_Poweron_Init();
 	fintek_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
 }

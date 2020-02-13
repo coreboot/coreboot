bb45f38eb9d0ecc6f4a1d0ca37c8c52212360c56
Mike Banon <mikebdp2@gmail.com>
2020-02-13 15:45:05 +0000

    mb/bap/ode_e20XX: Switch away from ROMCC_BOOTBLOCK
    Warning: not tested on hardware.
    
    Signed-off-by: Mike Banon <mikebdp2@gmail.com>
    Change-Id: I37a1a95bdf07d99916247095a5bc3ac5349cd98f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38869
    Reviewed-by: HAOUAS Elyes <ehaouas@noos.fr>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/bap/ode_e20XX/bootblock.c
+++ src/mainboard/bap/ode_e20XX/bootblock.c
@@ -1,10 +1,6 @@
 /*
  * This file is part of the coreboot project.
  *
- * Copyright (C) 2012 Advanced Micro Devices, Inc.
- * Copyright (C) 2015 BAP - Bruhnspace Advanced Projects
- * (Written by Fabian Kunkel <fabi@adv.bruhnspace.com> for BAP)
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 2 of the License.
@@ -15,26 +11,17 @@
  * GNU General Public License for more details.
  */
 
-#include <arch/io.h>
 #include <amdblocks/acpimmio.h>
-#include <device/pci_ops.h>
-#include <southbridge/amd/agesa/hudson/hudson.h>
-
-#include <northbridge/amd/agesa/state_machine.h>
+#include <bootblock_common.h>
 #include <superio/fintek/common/fintek.h>
 #include <superio/fintek/f81866d/f81866d.h>
 
-
 #define SERIAL_DEV1 PNP_DEV(0x4e, F81866D_SP1)
 
-void board_BeforeAgesa(struct sysinfo *cb)
+void bootblock_mainboard_early_init(void)
 {
 	/* Disable PCI-PCI bridge and release GPIO32/33 for other uses. */
-	pm_io_write(0xea, 1);
-
-	/* Set LPC decode enables. */
-	pci_devfn_t dev = PCI_DEV(0, 0x14, 3);
-	pci_write_config32(dev, 0x44, 0xff03ffd5);
+	pm_write8(0xea, 0x1);
 
 	fintek_enable_serial(SERIAL_DEV1, CONFIG_TTYS0_BASE);
 }

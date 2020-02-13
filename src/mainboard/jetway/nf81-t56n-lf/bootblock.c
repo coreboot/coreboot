c896df7f158cf759906f4f164330fb552bbe0fec
Mike Banon <mikebdp2@gmail.com>
2020-02-13 16:16:01 +0000

    mb/jetway/nf81-t56n-lf: Switch away from ROMCC_BOOTBLOCK
    Warning: Not tested on hardware.
    
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Signed-off-by: Mike Banon <mikebdp2@gmail.com>
    Change-Id: I2ccdb10b7e06e4c159b5a0203131f6ac4c37aacf
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/38874
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/jetway/nf81-t56n-lf/bootblock.c
+++ src/mainboard/jetway/nf81-t56n-lf/bootblock.c
@@ -14,16 +14,14 @@
  * GNU General Public License for more details.
  */
 
-#include <northbridge/amd/agesa/state_machine.h>
+#include <bootblock_common.h>
 #include <superio/fintek/common/fintek.h>
 #include <superio/fintek/f71869ad/f71869ad.h>
-#include <sb_cimx.h>
 
 /* Ensure Super I/O config address (i.e., 0x2e or 0x4e) matches that of devicetree.cb */
 #define SERIAL_DEV PNP_DEV(0x2e, F71869AD_SP1)
 
-void board_BeforeAgesa(struct sysinfo *cb)
+void bootblock_mainboard_early_init(void)
 {
-	sb_Poweron_Init();
 	fintek_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
 }

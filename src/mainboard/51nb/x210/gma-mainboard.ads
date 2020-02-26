6d6fb6bdd2c3fe5f198bb37c51609b4768c7fd74
Matt DeVillier <matt.devillier@gmail.com>
2020-02-26 12:55:49 -0600

    mb/51nb/x210: add libgfxinit support
    Derived from x210_test branch of HarryKipper's repo:
    https://github.com/harrykipper/coreboot
    
    Test: build/boot x210, test eDP, MiniDP, VGA outputs
    
    Change-Id: Ie2b79b236a458ebd243c992d6e615e41930eeb50
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39106
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

---
+++ src/mainboard/51nb/x210/gma-mainboard.ads
@@ -0,0 +1,18 @@
+-- SPDX-License-Identifier: GPL-2.0-or-later
+-- This file is part of the coreboot project.
+
+with HW.GFX.GMA;
+with HW.GFX.GMA.Display_Probing;
+
+use HW.GFX.GMA;
+use HW.GFX.GMA.Display_Probing;
+
+private package GMA.Mainboard is
+
+   ports : constant Port_List :=
+     (DP1,
+      DP2,
+      eDP,
+      Others => Disabled);
+
+end GMA.Mainboard;

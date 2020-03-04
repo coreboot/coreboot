d1e50f9e9fb8f8dd9726ec838ba4d7c1ca0d265a
Patrick Georgi <pgeorgi@google.com>
2020-03-04 15:00:05 +0100

    src/arch/riscv: Convert to SPDX license header
    This also drops individual copyright notices, all mentioned authors in
    that part of the tree are listed in AUTHORS.
    
    Change-Id: I770c1afd9b68a40ec0e69818f24b5ef3ad4f1d35
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39283
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: HAOUAS Elyes <ehaouas@noos.fr>

--- src/arch/riscv/fit_payload.c
+++ src/arch/riscv/fit_payload.c
@@ -1,18 +1,5 @@
-/*
- * Copyright 2013 Google Inc.
- * Copyright 2018 Facebook, Inc.
- * Copyright 2019 9elements Agency GmbH <patrick.rudolph@9elements.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* This file is part of the coreboot project. */
 
 #include <cbfs.h>
 #include <commonlib/bsd/compression.h>
--- src/arch/riscv/tables.c
+++ src/arch/riscv/tables.c
@@ -1,19 +1,5 @@
-/*
- * This file is part of the coreboot project.
- *
- * Copyright (C) 2003 Eric Biederman
- * Copyright (C) 2005 Steve Magnani
- * Copyright (C) 2008-2009 coresystems GmbH
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
 
 #include <bootmem.h>
 #include <boot/tables.h>

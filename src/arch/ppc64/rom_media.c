0a2a670502b2a3404fdaf0708b06a96581880aa6
Patrick Georgi <pgeorgi@google.com>
2020-03-04 14:43:23 +0100

    src/arch/ppc64: Convert to SPDX license header
    This also drops individual copyright notices, all mentioned authors in
    that part of the tree are already listed in AUTHORS.
    
    Change-Id: I19b1c379b474dd011e2d0f8c8202ff1351c9290d
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39281
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: HAOUAS Elyes <ehaouas@noos.fr>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/arch/ppc64/rom_media.c
+++ src/arch/ppc64/rom_media.c
@@ -1,16 +1,6 @@
-/*
- * This file is part of the coreboot project.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; version 2 of
- * the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
+
 #include <boot_device.h>
 
 /* This assumes that the CBFS resides at 0x0, which is true for the default

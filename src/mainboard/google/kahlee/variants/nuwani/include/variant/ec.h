dbc958495d3b7c94046b3b8f826f9316ee528e48
Peichao Wang <peichao.wang@bitland.corp-partner.google.com>
2020-03-05 17:02:58 +0800

    mb/google/kahlee/nuwani: Create Nuwani variant
    This commit creates a nuwani variant for Grunt. The initial settings
    override the baseboard was copied from variant treeya.
    
    BUG=b:144890301
    TEST=emerge-grunt coreboot
    
    Signed-off-by: Peichao Wang <peichao.wang@bitland.corp-partner.google.com>
    Change-Id: Id3a7fc890340e5a88ebc4b516dc2c0b085654999
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39316
    Reviewed-by: Martin Roth <martinroth@google.com>
    Reviewed-by: Mathew King <mathewk@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/kahlee/variants/nuwani/include/variant/ec.h
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2020 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <baseboard/ec.h>
+
+/* Enable Tablet switch */
+#define EC_ENABLE_TBMC_DEVICE
+
+/*
+ * Enable EC sync interrupt via GPIO controller, EC_SYNC_IRQ is defined in
+ * variant/gpio.h
+ */
+#define EC_ENABLE_SYNC_IRQ_GPIO

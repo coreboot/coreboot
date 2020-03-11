6e5693386ba94ebf05015e7cbcc7dd482119c8e4
Paul Fagerburg <pfagerburg@google.com>
2020-03-11 14:48:30 -0700

    coreboot: add Volteer template files
    Add template files for making a new barebones-copy of Volteer.
    
    BUG=b:147483699
    BRANCH=None
    TEST=N/A
    
    Change-Id: I8cc69b8ce7dbc6809de058019bdc466a060069e7
    Signed-off-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39462
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>

---
+++ util/mainboard/google/volteer/template/include/variant/ec.h
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2020 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef __VARIANT_EC_H__
+#define __VARIANT_EC_H__
+
+#include <baseboard/ec.h>
+
+#endif

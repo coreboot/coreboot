2677e2dbf6a2733de6a7d6c4ff0975d8a2650e13
Matt DeVillier <matt.devillier@gmail.com>
2020-03-14 16:22:01 -0500

    ec/51nb: add support for NPCE985LA0DX EC
    Add support for the NPCE985LA0DX, as used on the 51NB X210
    (to be added in a follow-on commit, and from which this was extracted).
    
    Original source: https://review.coreboot.org/c/coreboot/+/32531/37
    
    Change-Id: I5798fad7fd18083cde1aa647fd91ca9c5ce963b7
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Signed-off-by: Matthew Garrett <mjg59@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39567
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

---
+++ src/ec/51nb/npce985la0dx/npce985la0dx.c
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later  */
+/* This file is part of the coreboot project. */
+
+#include <device/pnp.h>
+
+/*
+ * This embedded controller looks awfully like a Super I/O chip. LDNs 5 and 6
+ * need to be enabled to turn on the keyboard and mouse controller, and LDN
+ * 0x11 needs to be enabled to turn on ACPI embedded controller functionality.
+ */
+static struct pnp_info dev_infos[] = {
+	{ NULL, 0x05 }, { NULL, 0x06 }, { NULL, 0x11 }
+};
+
+static void ec_51nb_npce985la0dx_ops_enable(struct device *dev)
+{
+	pnp_enable_devices(dev, &pnp_ops, ARRAY_SIZE(dev_infos), dev_infos);
+}
+
+struct chip_operations ec_51nb_npce985la0dx_ops = {
+	CHIP_NAME("51NB EC")
+	.enable_dev = ec_51nb_npce985la0dx_ops_enable,
+};

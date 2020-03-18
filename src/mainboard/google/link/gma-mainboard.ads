c67e4db2dc33642a7c854507968a94eda740e161
Angel Pons <th3fanbus@gmail.com>
2020-03-18 23:26:07 +0100

    mb/**/gma-mainboard.ads: Use SPDX for GPL-2.0-or-later
    Change-Id: I78f06b54a6a03d565cf86f1d7bdf37965c3f6ad0
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39660
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/mainboard/google/link/gma-mainboard.ads
+++ src/mainboard/google/link/gma-mainboard.ads
@@ -1,12 +1,5 @@
---
--- This program is free software; you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation; either version 2 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
---
+-- SPDX-License-Identifier: GPL-2.0-or-later
+-- This file is part of the coreboot project.
 
 with HW.GFX.GMA;
 with HW.GFX.GMA.Display_Probing;

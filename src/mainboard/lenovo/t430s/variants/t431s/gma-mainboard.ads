610b3d7a33e7950e118f92588d08e8ae40cfe7da
Angel Pons <th3fanbus@gmail.com>
2020-03-18 23:20:17 +0100

    mb/**/gma-mainboard.ads: Remove copyright statements
    They are already in AUTHORS.
    
    Change-Id: I315c0c57babfa239e3d7c501a4183b8996999e6e
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39659
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/mainboard/lenovo/t430s/variants/t431s/gma-mainboard.ads
+++ src/mainboard/lenovo/t430s/variants/t431s/gma-mainboard.ads
@@ -1,6 +1,4 @@
 --
--- Copyright (C) 2017 Bill XIE persmule@gmail.com
---
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation; either version 2 of the License, or

1e40a115778865e60a13cb71fdf18eb3ddf1751f
David Wu <david_wu@quanta.corp-partner.google.com>
2020-03-25 13:37:07 +0800

    mb/google/hatch/var/kindred: set wifi sar for kled
    Enable wifi sar feature and set wifi sar name for kled sku.
    
    BUG=b:152277272
    TEST=emerge-hatch coreboot chromeos-bootimage and
         verify wifi SAR load by sku-id
    
    Change-Id: I9ee242773fd05cc2bcd7bde07da8176022827677
    Signed-off-by: David Wu <David_Wu@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39813
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>

--- src/mainboard/google/hatch/variants/kindred/variant.c
+++ src/mainboard/google/hatch/variants/kindred/variant.c
@@ -16,6 +16,7 @@
 #include <chip.h>
 #include <soc/pci_devs.h>
 #include <ec/google/chromeec/ec.h>
+#include <sar.h>
 
 void variant_devtree_update(void)
 {
@@ -45,4 +46,14 @@ void variant_devtree_update(void)
 		cfg->SataPortsDevSlp[1] = 0;
 		cfg->satapwroptimize = 0;
 	}
+}
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	const char *filename = NULL;
+	uint32_t sku_id = google_chromeec_get_board_sku();
+
+	if (sku_id == 1 || sku_id == 2 || sku_id == 3 || sku_id == 4)
+		filename = "wifi_sar-kled.hex";
+	return filename;
 }

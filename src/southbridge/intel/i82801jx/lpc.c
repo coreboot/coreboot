93fcf37017d0c4fe39d3f9b52f551c5e1ea907b3
Matt DeVillier <matt.devillier@gmail.com>
2020-03-30 21:56:52 -0500

    sb/intel/i82801jx: drop IGD-related NVS variables
    NDID/DID entries are no longer used by the GMA SSDT generator, so
    drop them. SSDT generation will be simplified in a subsequent commit.
    
    Change-Id: Iecd8559f660cc748c417ec94b7a822e16603cbd8
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/39955
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/southbridge/intel/i82801jx/lpc.c
+++ src/southbridge/intel/i82801jx/lpc.c
@@ -35,7 +35,6 @@
 #include "nvs.h"
 #include <southbridge/intel/common/pciehp.h>
 #include <southbridge/intel/common/acpi_pirq_gen.h>
-#include <drivers/intel/gma/i915.h>
 
 #define NMI_OFF	0
 
@@ -647,14 +646,8 @@ static void southbridge_inject_dsdt(struct device *dev)
 	global_nvs_t *gnvs = cbmem_add (CBMEM_ID_ACPI_GNVS, sizeof(*gnvs));
 
 	if (gnvs) {
-		const struct i915_gpu_controller_info *gfx = intel_gma_get_controller_info();
 		memset(gnvs, 0, sizeof(*gnvs));
 		acpi_create_gnvs(gnvs);
-
-		if (gfx) {
-			gnvs->ndid = gfx->ndid;
-			memcpy(gnvs->did, gfx->did, sizeof(gnvs->did));
-		}
 
 		/* And tell SMI about it */
 		smm_setup_structures(gnvs, NULL, NULL);

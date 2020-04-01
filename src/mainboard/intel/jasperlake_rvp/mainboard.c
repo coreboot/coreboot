979c8c7caefdb0a41d18ddd3f1e3473edf6b7861
Meera Ravindranath <meera.ravindranath@intel.com>
2020-04-01 16:04:39 +0530

    mb/intel/jasperlake_rvp: Update SMBIOS data for Jslrvp
    1)Change Mainboard Part Number to jslrvp
    2)Change Mainboard Family to Intel_jslrvp
    3)Generate SMBIOS table and fill sku id information in SMBIOS
    
    BUG=None
    BRANCH=None
    TEST=Mosys works on jslrvp and Sku ID info is generated
    
    Change-Id: Iad0b394fea017223a5b98fff0cb4c2bd1d5a7bd7
    Signed-off-by: Meera Ravindranath <meera.ravindranath@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40011
    Reviewed-by: Frans Hendriks <fhendriks@eltan.com>
    Reviewed-by: Maulik V Vaghela <maulik.v.vaghela@intel.com>
    Reviewed-by: Subrata Banik <subrata.banik@intel.com>
    Reviewed-by: Ronak Kanabar <ronak.kanabar@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/intel/jasperlake_rvp/mainboard.c
+++ src/mainboard/intel/jasperlake_rvp/mainboard.c
@@ -5,6 +5,7 @@
 #include <baseboard/variants.h>
 #include <device/device.h>
 #include <soc/gpio.h>
+#include <smbios.h>
 #include <vendorcode/google/chromeos/chromeos.h>
 
 static void mainboard_init(void *chip_info)
@@ -19,6 +20,12 @@ static void mainboard_init(void *chip_info)
 static void mainboard_enable(struct device *dev)
 {
 	dev->ops->acpi_inject_dsdt = chromeos_dsdt_generator;
+}
+
+const char *smbios_system_sku(void)
+{
+	static const char *sku_str = "sku2147483647"; /* sku{0-1} */
+	return sku_str;
 }
 
 struct chip_operations mainboard_ops = {

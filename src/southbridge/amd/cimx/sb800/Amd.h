182dbdeac4b2489f8d03a47a79ab28ad556d9d21
Angel Pons <th3fanbus@gmail.com>
2020-04-02 23:49:05 +0200

    src/southbridge: Use SPDX for GPL-2.0-only files
    Done with sed and God Lines. Only done for C-like code for now.
    
    Change-Id: I5b00b3e38edda90f35f0679cd4171a3499288f24
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40059
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: HAOUAS Elyes <ehaouas@noos.fr>

--- src/southbridge/amd/cimx/sb800/Amd.h
+++ src/southbridge/amd/cimx/sb800/Amd.h
@@ -8,24 +8,8 @@
  * Contains AMD AGESA/CIMx core interface
  *
  */
-/*
- *****************************************************************************
- *
- * This file is part of the coreboot project.
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * ***************************************************************************
- *
- */
-
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
 
 #ifndef _AMD_H_
 #define _AMD_H_
--- src/southbridge/intel/i82801ix/smi.c
+++ src/southbridge/intel/i82801ix/smi.c
@@ -1,18 +1,7 @@
 /*
  * This file is part of the coreboot project.
  *
- *               2012 secunet Security Networks AG
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; version 2 of
- * the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+ *               2012 secunet Security Networks AG SPDX-License-Identifier: GPL-2.0-only */
 
 
 #include <device/device.h>

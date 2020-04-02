09481b1b86592201e855e525d96f72c2031c5b06
Angel Pons <th3fanbus@gmail.com>
2020-04-03 01:21:13 +0200

    mainboard/asrock: Use SPDX for GPL-2.0-only files
    Done with sed and God Lines. Only done for C-like code for now.
    
    Change-Id: I32c5f5e865b5455ddb7034612ecea1383932cef9
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40066
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Maxim Polyakov <max.senia.poliak@gmail.com>

--- src/mainboard/asrock/b85m_pro4/bootblock.c
+++ src/mainboard/asrock/b85m_pro4/bootblock.c
@@ -1,19 +1,5 @@
-/*
- * This file is part of the coreboot project.
- *
- * Copyright (C) 2007-2010 coresystems GmbH
- * Copyright (C) 2012 Google Inc.
- * Copyright (C) 2018 Tristan Corrick <tristan@corrick.kiwi>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
 
 #include <device/pnp_ops.h>
 #include <superio/nuvoton/common/nuvoton.h>

236c637180c93efd15dd4c661671195849e79411
Angel Pons <th3fanbus@gmail.com>
2020-04-03 01:21:05 +0200

    mainboard/aopen: Use SPDX for GPL-2.0-only files
    Done with sed and God Lines. Only done for C-like code for now.
    
    Change-Id: I9617d289ac09defc337631c4fd6adf6c8df5df66
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40064
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/aopen/dxplplusu/acpi_tables.c
+++ src/mainboard/aopen/dxplplusu/acpi_tables.c
@@ -1,18 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
+
 /*
- * This file is part of the coreboot project.
- *
- *
  * Ported to Intel XE7501DEVKIT by Agami Aruma
  * Ported to AOpen DXPL Plus-U by Kyösti Mälkki
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #include <arch/acpi.h>

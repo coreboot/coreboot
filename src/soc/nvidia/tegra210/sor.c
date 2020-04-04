a2ee761bfbb4daaf38a990a97f473b98ad357298
Angel Pons <th3fanbus@gmail.com>
2020-04-04 18:51:15 +0200

    soc/nvidia: Use SPDX for GPL-2.0-only files
    Done with sed and God Lines. Only done for C-like code for now.
    
    Change-Id: Id987662ba96ad7e78e76aa5a66a59b313e82f724
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40133
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/nvidia/tegra210/sor.c
+++ src/soc/nvidia/tegra210/sor.c
@@ -1,18 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
+
 /*
- * This file is part of the coreboot project.
- *
  * drivers/video/tegra/dc/sor.c
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
  */
 
 #include <console/console.h>

5f249e60f95f8aaaff74c72c7e3a728c192e9092
Angel Pons <th3fanbus@gmail.com>
2020-04-04 18:51:01 +0200

    soc/cavium: Use SPDX for GPL-2.0-only files
    Done with sed and God Lines. Only done for C-like code for now.
    
    Change-Id: I3d872f63d56711d39c8320ace2642cea2a23f545
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40130
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/soc/cavium/common/include/soc/bootblock.h
+++ src/soc/cavium/common/include/soc/bootblock.h
@@ -1,17 +1,5 @@
-/*
- * This file is part of the coreboot project.
- *
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; version 2 of
- * the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
 #ifndef SRC_SOC_CAVIUM_COMMON_INCLUDE_SOC_BOOTBLOCK_H_
 #define SRC_SOC_CAVIUM_COMMON_INCLUDE_SOC_BOOTBLOCK_H_
 

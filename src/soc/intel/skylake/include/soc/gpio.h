3bd1e3db9c5ba6b156142841efefee55c3985c2e
Angel Pons <th3fanbus@gmail.com>
2020-04-05 15:47:17 +0200

    soc/intel/skylake: Use SPDX for GPL-2.0-only files
    Done with sed and God Lines. Only done for C-like code for now.
    
    Change-Id: I7354edb15ca9cbe181739bc2a148f16bb85ab118
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40218
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by:  Felix Singer <felixsinger@posteo.net>

--- src/soc/intel/skylake/include/soc/gpio.h
+++ src/soc/intel/skylake/include/soc/gpio.h
@@ -1,16 +1,5 @@
-/*
- * This file is part of the coreboot project.
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
 
 #ifndef _SOC_GPIO_H_
 #define _SOC_GPIO_H_

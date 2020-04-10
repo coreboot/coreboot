2016d8157c72fc2b32219d067ac4b759f3deebb0
CK Hu <ck.hu@mediatek.com>
2020-04-10 11:34:29 +0800

    mb/google/asurada: Add USB support
    Change-Id: I35dc4be65f0843c3c74695c443dd958676e6c12c
    Signed-off-by: CK Hu <ck.hu@mediatek.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45397
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/mainboard/google/asurada/mainboard.c
+++ src/mainboard/google/asurada/mainboard.c
@@ -1,9 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <console/console.h>
 #include <device/device.h>
+#include <soc/usb.h>
 
 static void mainboard_init(struct device *dev)
 {
+	setup_usb_host();
 }
 
 static void mainboard_enable(struct device *dev)

coreboot/coreboot

commit b77cbbe1b0b4448ca9d493cd71b8eeffcd5be499

mb/google/poppy/variants/nami: Update DPTF table
Update dptf.asl from tuning of the thermal team.

BUG=b:72974136
TEST=Match the result from DPTF UI.

Change-Id: I21ddc337359c3e11ad9756e61ba174b33dfc3c75
Signed-off-by: John Su <john_su@compal.corp-partner.google.com>
Reviewed-on: https://review.coreboot.org/26209
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Furquan Shaikh <furquan@google.com>
Reviewed-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>

src/mainboard/google/poppy/variants/nami/include/variant/acpi/dptf.asl
@@ -15,26 +15,31 @@
 
 #define DPTF_CPU_PASSIVE	98
 #define DPTF_CPU_CRITICAL	125
-#define DPTF_CPU_ACTIVE_AC0	71
-#define DPTF_CPU_ACTIVE_AC1	69
-#define DPTF_CPU_ACTIVE_AC2	67
-#define DPTF_CPU_ACTIVE_AC3	65
-#define DPTF_CPU_ACTIVE_AC4	60
+#define DPTF_CPU_ACTIVE_AC0	87
+#define DPTF_CPU_ACTIVE_AC1	85
+#define DPTF_CPU_ACTIVE_AC2	83
+#define DPTF_CPU_ACTIVE_AC3	80
+#define DPTF_CPU_ACTIVE_AC4	75
 
 #define DPTF_TSR0_SENSOR_ID	0
 #define DPTF_TSR0_SENSOR_NAME	"Thermal_Sensor_Remote_CPU"
 #define DPTF_TSR0_PASSIVE	81
 #define DPTF_TSR0_CRITICAL	125
-#define DPTF_TSR0_ACTIVE_AC0	62
-#define DPTF_TSR0_ACTIVE_AC1	60
-#define DPTF_TSR0_ACTIVE_AC2	55
-#define DPTF_TSR0_ACTIVE_AC3	50
-#define DPTF_TSR0_ACTIVE_AC4	45
+#define DPTF_TSR0_ACTIVE_AC0	50
+#define DPTF_TSR0_ACTIVE_AC1	47
+#define DPTF_TSR0_ACTIVE_AC2	45
+#define DPTF_TSR0_ACTIVE_AC3	43
+#define DPTF_TSR0_ACTIVE_AC4	41
 
 #define DPTF_TSR1_SENSOR_ID	1
 #define DPTF_TSR1_SENSOR_NAME	"Thermal_Sensor_Remote_PMIC"
 #define DPTF_TSR1_PASSIVE	78
 #define DPTF_TSR1_CRITICAL	125
+#define DPTF_TSR1_ACTIVE_AC0    50
+#define DPTF_TSR1_ACTIVE_AC1    47
+#define DPTF_TSR1_ACTIVE_AC2    45
+#define DPTF_TSR1_ACTIVE_AC3    43
+#define DPTF_TSR1_ACTIVE_AC4    41
 
 #define DPTF_ENABLE_CHARGER
 #define DPTF_ENABLE_FAN_CONTROL
@@ -57,10 +62,10 @@ Name (DFPS, Package () {
 	 */
 	/* Control, Trip Point, Speed, NoiseLevel, Power */
 	Package () {90,		0xFFFFFFFF,	6700,	220,	2200},
-	Package () {72,		0xFFFFFFFF,	5800,	180,	1800},
-	Package () {59,		0xFFFFFFFF,	5000,	145,	1450},
-	Package () {57,		0xFFFFFFFF,	4900,	115,	1150},
-	Package () {40,		0xFFFFFFFF,	3900,	90,	900}
+	Package () {69,		0xFFFFFFFF,	5800,	180,	1800},
+	Package () {52,		0xFFFFFFFF,	5000,	145,	1450},
+	Package () {46,		0xFFFFFFFF,	4900,	115,	1150},
+	Package () {36,		0xFFFFFFFF,	3900,	90,	900}
 })
 
 Name (DART, Package () {
@@ -71,15 +76,15 @@ Name (DART, Package () {
 		 * Source, Target, Weight, AC0, AC1, AC2, AC3, AC4, AC5, AC6,
 		 *	AC7, AC8, AC9
 		 */
-		\_SB.DPTF.TFN1, \_SB.PCI0.B0D4, 100, 100, 90, 72, 59, 40, 0, 0,
+		\_SB.DPTF.TFN1, \_SB.PCI0.B0D4, 100, 90, 69, 52, 46, 36, 0, 0,
 			0, 0, 0
 	},
 	Package () {
-		\_SB.DPTF.TFN1, \_SB.DPTF.TSR0, 100, 100, 90, 72, 59, 40, 0, 0,
+		\_SB.DPTF.TFN1, \_SB.DPTF.TSR0, 100, 90, 69, 52, 46, 36, 0, 0,
 			0, 0, 0
 	},
 	Package () {
-		\_SB.DPTF.TFN1, \_SB.DPTF.TSR1, 100, 100, 90, 72, 59, 40, 0, 0,
+		\_SB.DPTF.TFN1, \_SB.DPTF.TSR1, 100, 90, 69, 52, 46, 36, 0, 0,
 			0, 0, 0
 	}
 })
@@ -111,7 +116,7 @@ Name (MPPC, Package ()
 	Package () {	/* Power Limit 2 */
 		1,	/* PowerLimitIndex, 1 for Power Limit 2 */
 		15000,	/* PowerLimitMinimum */
-		25000,	/* PowerLimitMaximum */
+		44000,	/* PowerLimitMaximum */
 		28000,	/* TimeWindowMinimum */
 		32000,	/* TimeWindowMaximum */
 		100	/* StepSize */

commit 71e2b2903aaf8a25d6cffda128201b46a6728014

mb/google/poppy/variants/nami: Use tabs for alignment
Change-Id: Ia707295c55ce2e18eb8970506be10b7b0f3fbc39
Fixes: b77cbbe1b0 ("mb/google/poppy/variants/nami: Update DPTF table")
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/40305
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>

src/mainboard/google/poppy/variants/nami/include/variant/acpi/dptf.asl
@@ -23,11 +23,11 @@
 #define DPTF_TSR1_SENSOR_NAME	"Thermal_Sensor_Remote_PMIC"
 #define DPTF_TSR1_PASSIVE	75
 #define DPTF_TSR1_CRITICAL	125
-#define DPTF_TSR1_ACTIVE_AC0    50
-#define DPTF_TSR1_ACTIVE_AC1    47
-#define DPTF_TSR1_ACTIVE_AC2    45
-#define DPTF_TSR1_ACTIVE_AC3    42
-#define DPTF_TSR1_ACTIVE_AC4    39
+#define DPTF_TSR1_ACTIVE_AC0	50
+#define DPTF_TSR1_ACTIVE_AC1	47
+#define DPTF_TSR1_ACTIVE_AC2	45
+#define DPTF_TSR1_ACTIVE_AC3	42
+#define DPTF_TSR1_ACTIVE_AC4	39
 
 #define DPTF_TSR2_SENSOR_ID	2
 #define DPTF_TSR2_SENSOR_NAME	"Thermal_Sensor_Remote_CPU"
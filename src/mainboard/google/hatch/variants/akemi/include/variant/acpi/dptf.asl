coreboot/coreboot

commit e2ac5b7a36cd0583a6b62dac4f8dae5529f44b74

mb/google/hatch/variants: Add DPTF based Fan control
This adds DPTF based Fan speed control for CML based Hatch system.

BUG=None
BRANCH=None
TEST=Built and tested fan speed with different temperatures

Change-Id: I3c2a679dc67eecb17098ce0f0c9703c679473a2d
Signed-off-by: Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/31980
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Furquan Shaikh <furquan@google.com>

src/mainboard/google/hatch/variants/baseboard/include/baseboard/acpi/dptf.asl
@@ -15,18 +15,34 @@
 
 #define DPTF_CPU_PASSIVE	95
 #define DPTF_CPU_CRITICAL	105
+#define DPTF_CPU_ACTIVE_AC0	87
+#define DPTF_CPU_ACTIVE_AC1	85
+#define DPTF_CPU_ACTIVE_AC2	83
+#define DPTF_CPU_ACTIVE_AC3	80
+#define DPTF_CPU_ACTIVE_AC4	75
 
 #define DPTF_TSR0_SENSOR_ID	1
 #define DPTF_TSR0_SENSOR_NAME	"Thermal Sensor 1"
 #define DPTF_TSR0_PASSIVE	65
 #define DPTF_TSR0_CRITICAL	75
+#define DPTF_TSR0_ACTIVE_AC0	50
+#define DPTF_TSR0_ACTIVE_AC1	47
+#define DPTF_TSR0_ACTIVE_AC2	45
+#define DPTF_TSR0_ACTIVE_AC3	42
+#define DPTF_TSR0_ACTIVE_AC4	39
 
 #define DPTF_TSR1_SENSOR_ID	2
 #define DPTF_TSR1_SENSOR_NAME	"Thermal Sensor 2"
 #define DPTF_TSR1_PASSIVE	65
 #define DPTF_TSR1_CRITICAL	75
+#define DPTF_TSR1_ACTIVE_AC0    50
+#define DPTF_TSR1_ACTIVE_AC1    47
+#define DPTF_TSR1_ACTIVE_AC2    45
+#define DPTF_TSR1_ACTIVE_AC3    42
+#define DPTF_TSR1_ACTIVE_AC4    39
 
 #define DPTF_ENABLE_CHARGER
+#define DPTF_ENABLE_FAN_CONTROL
 
 /* Charger performance states, board-specific values from charger and EC */
 Name (CHPS, Package () {
@@ -34,6 +50,47 @@ Name (CHPS, Package () {
 	Package () { 0, 0, 0, 0, 24, 0x600, "mA", 0 },	/* 1.5A */
 	Package () { 0, 0, 0, 0, 16, 0x400, "mA", 0 },	/* 1.0A */
 	Package () { 0, 0, 0, 0, 8, 0x200, "mA", 0 },	/* 0.5A */
+})
+
+/* DFPS: Fan Performance States */
+Name (DFPS, Package () {
+	0,	// Revision
+	/*
+	 * TODO : Need to update this Table after characterization.
+	 *	  These are initial reference values.
+	 */
+	/* Control, Trip Point, Speed, NoiseLevel, Power */
+	Package () {90,		0xFFFFFFFF,	6700,	220,	2200},
+	Package () {80,		0xFFFFFFFF,	5800,	180,	1800},
+	Package () {70,		0xFFFFFFFF,	5000,	145,	1450},
+	Package () {60,		0xFFFFFFFF,	4900,	115,	1150},
+	Package () {50,		0xFFFFFFFF,	3838,	90,	900},
+	Package () {40,		0xFFFFFFFF,	2904,	55,	550},
+	Package () {30,		0xFFFFFFFF,	2337,	30,	300},
+	Package () {20,		0xFFFFFFFF,	1608,	15,	150},
+	Package () {10,		0xFFFFFFFF,	800,	10,	100},
+	Package () {0,		0xFFFFFFFF,	0,	0,	50}
+})
+
+Name (DART, Package () {
+	/* Fan effect on CPU */
+	0,	// Revision
+	Package () {
+		/*
+		 * Source, Target, Weight, AC0, AC1, AC2, AC3, AC4, AC5, AC6,
+		 *	AC7, AC8, AC9
+		 */
+		\_SB.DPTF.TFN1, \_SB.PCI0.TCPU, 100, 90, 69, 56, 46, 36, 0, 0,
+			0, 0, 0
+	},
+	Package () {
+		\_SB.DPTF.TFN1, \_SB.DPTF.TSR0, 100, 90, 69, 56, 46, 36, 0, 0,
+			0, 0, 0
+	},
+	Package () {
+		\_SB.DPTF.TFN1, \_SB.DPTF.TSR1, 100, 90, 69, 56, 46, 36, 0, 0,
+			0, 0, 0
+	}
 })
 
 Name (DTRT, Package () {

commit aecbe7a988265c0621a118368d7c189b12779dd4

mb/google/hatch: Use tabs for alignment
Change-Id: I38d429245810f64a03253b5076391af843f8d0de
Fixes: e2ac5b7a36 ("mb/google/hatch/variants: Add DPTF based Fan control")
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/40307
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>

src/mainboard/google/hatch/variants/akemi/include/variant/acpi/dptf.asl
@@ -25,9 +25,9 @@
 #define DPTF_TSR1_SENSOR_NAME	"Thermal Sensor 2"
 #define DPTF_TSR1_PASSIVE	38
 #define DPTF_TSR1_CRITICAL	75
-#define DPTF_TSR1_ACTIVE_AC0    42
-#define DPTF_TSR1_ACTIVE_AC1    40
-#define DPTF_TSR1_ACTIVE_AC2    38
+#define DPTF_TSR1_ACTIVE_AC0	42
+#define DPTF_TSR1_ACTIVE_AC1	40
+#define DPTF_TSR1_ACTIVE_AC2	38
 
 #define DPTF_TSR2_SENSOR_ID	2
 #define DPTF_TSR2_SENSOR_NAME	"Thermal Sensor - CPU"

src/mainboard/google/hatch/variants/baseboard/include/baseboard/acpi/dptf.asl
@@ -23,11 +23,11 @@
 #define DPTF_TSR1_SENSOR_NAME	"Thermal Sensor 2"
 #define DPTF_TSR1_PASSIVE	65
 #define DPTF_TSR1_CRITICAL	75
-#define DPTF_TSR1_ACTIVE_AC0    50
-#define DPTF_TSR1_ACTIVE_AC1    47
-#define DPTF_TSR1_ACTIVE_AC2    45
-#define DPTF_TSR1_ACTIVE_AC3    42
-#define DPTF_TSR1_ACTIVE_AC4    39
+#define DPTF_TSR1_ACTIVE_AC0	50
+#define DPTF_TSR1_ACTIVE_AC1	47
+#define DPTF_TSR1_ACTIVE_AC2	45
+#define DPTF_TSR1_ACTIVE_AC3	42
+#define DPTF_TSR1_ACTIVE_AC4	39
 
 #define DPTF_ENABLE_CHARGER
 #define DPTF_ENABLE_FAN_CONTROL
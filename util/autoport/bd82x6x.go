f04b2627105cb01bc480325498b265a3208df6f4
Peter Lemenkov <lemenkov@gmail.com>
2020-04-17 10:36:41 +0200

    autoport: Don't initialize already initialized fields in acpi_tables
    Don't initialize fields with zeroes since gnvs structs were zeroed out
    in southbridge already. Also add some comments.
    
    See also these commits:
    
    * Commit a76cf28 with Change-Id
      I2ccf4699ba3ed3f5b9402c0340153d4a5bf82682 ("mb/lenovo/*/acpi_tables:
    Don't initialize already initialized fields").
    * Commit 0c52638 with Change-Id
      I71f092ed7582b4931122d72f41d0b42a7569b96e ("mb/lenovo: Remove
    thermal.h header").
    
    Change-Id: I1a0042bc93a2b30babcb896b3df23faf37998f3c
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40479
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- util/autoport/bd82x6x.go
+++ util/autoport/bd82x6x.go
@@ -364,18 +364,12 @@ void mainboard_get_spd(spd_raw_data *spd, bool id_only)
 /* FIXME: check this function.  */
 void acpi_create_gnvs(struct global_nvs *gnvs)
 {
-	/* Disable USB ports in S3 by default */
-	gnvs->s3u0 = 0;
-	gnvs->s3u1 = 0;
-
-	/* Disable USB ports in S5 by default */
-	gnvs->s5u0 = 0;
-	gnvs->s5u1 = 0;
-
 	/* The lid is open by default. */
 	gnvs->lids = 1;
 
+	/* Temperature at which OS will shutdown */
 	gnvs->tcrt = 100;
+	/* Temperature at which OS will throttle CPU */
 	gnvs->tpsv = 90;
 }
 `)

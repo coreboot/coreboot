edd38465a58d47b737f1e656a8055f64a3b0c421
Keith Hui <buurin@gmail.com>
2020-04-19 00:55:48 -0400

    mainboard/asus/p3b-f: Reintroduce as variant of p2b
    Fold this last ASUS 440BX board into the P2B family, while bringing in
    some changes:
    
    - Devicetree becomes overridetree.
    - Remove non-existent IR device and disable ACPI device on Super I/O to
      match OEM firmware.
    - Add SB GPO settings from OEM firmware to devicetree. This disables
      the SPD enabling magic this board needs. By moving the enabling part
      to bootblock the hacky enable_spd hook can be eliminated.
    - Initialize the serial port in bootblock, like the other boards.
    
    Boot tested on hardware.
    
    Change-Id: I65f2cb9d1bd4c82550de43889e3502526a46bd18
    Signed-off-by: Keith Hui <buurin@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41047
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/asus/p2b/variants/p3b-f/romstage.c
+++ src/mainboard/asus/p2b/variants/p3b-f/romstage.c
@@ -4,12 +4,6 @@
 #include <arch/io.h>
 #include <southbridge/intel/i82371eb/i82371eb.h>
 #include <northbridge/intel/i440bx/raminit.h>
-#include <superio/winbond/common/winbond.h>
-/* FIXME: The ASUS P3B-F has a Winbond W83977EF, actually. */
-#include <superio/winbond/w83977tf/w83977tf.h>
-
-/* FIXME: The ASUS P3B-F has a Winbond W83977EF, actually. */
-#define SERIAL_DEV PNP_DEV(0x3f0, W83977TF_SP1)
 
 /*
  * ASUS P3B-F specific SPD enable magic.
@@ -40,9 +34,4 @@ void enable_spd(void)
 void disable_spd(void)
 {
 	outb(0x67, PM_IO_BASE + 0x37);
-}
-
-void mainboard_enable_serial(void)
-{
-	winbond_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
 }

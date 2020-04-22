fdbdca2ec3a3a28142791cd331fcf42da59e9d38
Peichao Wang <peichao.wang@bitland.corp-partner.google.com>
2020-04-22 13:50:46 +0800

    mb/google/dedede: add new variant for wheelie
    Add initial support for wheelie variant board.
    
    BUG=b:154664137
    BRANCH=None
    TEST=build
    
    Change-Id: Id638e987f45c247dae824f221a38ccf32626572f
    Signed-off-by: peichao.wang <peichao.wang@bitland.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40587
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marco Chen <marcochen@google.com>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

---
+++ src/mainboard/google/dedede/variants/wheelie/include/variant/ec.h
@@ -0,0 +1,13 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef MAINBOARD_EC_H
+#define MAINBOARD_EC_H
+
+#include <baseboard/ec.h>
+
+#endif

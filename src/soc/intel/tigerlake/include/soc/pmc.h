e685107dd61461f91d3fdbf722cf378e121e2551
derek.huang <derek.huang@intel.corp-partner.google.com>
2020-04-23 14:55:24 +0800

    soc/intel/tigerlake: Print HPR_CAUSE0 register
    In addition to GBLRST_CAUSE0 and GBLRST_CAUSE1, print the value
    of HPR_CAUSE0.
    
    Change-Id: Idc57c3cd6a8d156c5544640898e8e7147d34c535
    Signed-off-by: derek.huang <derek.huang@intel.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40648
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/soc/intel/tigerlake/include/soc/pmc.h
+++ src/soc/intel/tigerlake/include/soc/pmc.h
@@ -119,6 +119,10 @@
 #define GBLRST_CAUSE0			0x1924
 #define   GBLRST_CAUSE0_THERMTRIP	(1 << 5)
 #define GBLRST_CAUSE1			0x1928
+#define HPR_CAUSE0			0x192C
+#define   HPR_CAUSE0_MI_HRPD		(1 << 10)
+#define   HPR_CAUSE0_MI_HRPC		(1 << 9)
+#define   HPR_CAUSE0_MI_HR		(1 << 8)
 
 #define CPPMVRIC			0x1B1C
 #define   XTALSDQDIS			(1 << 22)

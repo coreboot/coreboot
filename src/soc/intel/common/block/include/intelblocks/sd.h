3b54fdf282797ca2950341cdba32bb6f451ef53d
Furquan Shaikh <furquan@google.com>
2020-04-24 21:52:27 -0700

    soc/intel: Constify struct device *param to sd_fill_soc_gpio_info
    sd_fill_soc_gpio_info() does not need to modify device
    structure. Hence, this change makes the struct device * parameter to
    this function as const.
    
    Change-Id: I237ee9640ec64061aa9ed7c65ea21740c40b6ae2
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40708
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/soc/intel/common/block/include/intelblocks/sd.h
+++ src/soc/intel/common/block/include/intelblocks/sd.h
@@ -10,6 +10,6 @@
  * Fill the GPIO Interrupt or I/O information that will be used for the
  * GPIO Connection Descriptor.
  */
-int sd_fill_soc_gpio_info(struct acpi_gpio* gpio, struct device *dev);
+int sd_fill_soc_gpio_info(struct acpi_gpio* gpio, const struct device *dev);
 
 #endif /* SOC_INTEL_COMMON_BLOCK_SD_H */

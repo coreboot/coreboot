0025f777ed3e231e93e06fdebf520496804a6097
Kevin Chiu <Kevin.Chiu@quantatw.com>
2020-04-28 17:00:41 +0800

    mainboard/google/kahlee: move specific setting to variant
    Separate specific setting to variant from baseboard.
    baseboard/romstage.c in current release is only utilized by
    careena, we could remove it from the rest of variant build.
    
    BUG=b:154357210,b:154848243
    BRANCH=master
    TEST=emerge-grunt coreboot
    
    Change-Id: I658526e44aadc47bdc5538f506a1bfe2e5f20f63
    Signed-off-by: Kevin Chiu <Kevin.Chiu@quantatw.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40796
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>

---
+++ src/mainboard/google/kahlee/variants/careena/include/variant/sku.h
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2017 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/* SKU ID enumeration */
+enum careena_sku {
+	SKU_UNKNOWN = -1,
+	SKU_CAREENA_KB_NO_BACKLIGHT16 = 16,
+	SKU_CAREENA_KB_BACKLIGHT18 = 18,
+	SKU_CAREENA_KB_BACKLIGHT19 = 19,
+	SKU_CAREENA_KB_BACKLIGHT22 = 22,
+	SKU_CAREENA_KB_BACKLIGHT23 = 23,
+};
---
+++ src/mainboard/google/kahlee/variants/careena/variant.c
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2020 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <ec/google/chromeec/ec.h>
+#include <baseboard/variants.h>
+#include <variant/sku.h>
+
+void variant_romstage_entry(int s3_resume)
+{
+	uint32_t sku = google_chromeec_get_sku_id();
+
+	if (!s3_resume) {
+		/* Based on SKU, turn on keyboard backlight */
+		switch (sku) {
+		default:
+			google_chromeec_kbbacklight(75);
+			break;
+		case SKU_CAREENA_KB_BACKLIGHT18:
+		case SKU_CAREENA_KB_BACKLIGHT19:
+		case SKU_CAREENA_KB_BACKLIGHT22:
+		case SKU_CAREENA_KB_BACKLIGHT23:
+			google_chromeec_kbbacklight(10);
+			break;
+		}
+	}
+}

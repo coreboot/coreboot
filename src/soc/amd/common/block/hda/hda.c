f9e6d3e0508b1f9494e996b9aef4fd7fe23dcc05
Furquan Shaikh <furquan@google.com>
2020-04-27 17:59:25 -0700

    soc/amd/common/block/hda: Use default pci_dev_ops_pci
    This change sets ops_pci for hda_audio_ops to default pci_dev_ops_pci
    and removes the custom lops_pci since the driver does not really need
    a custom ops_pci.
    
    BUG=b:153858769
    
    Change-Id: I4b46e22ef556c0f49152c41a07f3c54c513ae37a
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40779
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/common/block/hda/hda.c
+++ src/soc/amd/common/block/hda/hda.c
@@ -14,15 +14,11 @@ static const unsigned short pci_device_ids[] = {
 	0
 };
 
-static struct pci_operations lops_pci = {
-	.set_subsystem = pci_dev_set_subsystem,
-};
-
 static struct device_operations hda_audio_ops = {
 	.read_resources = pci_dev_read_resources,
 	.set_resources = pci_dev_set_resources,
 	.enable_resources = pci_dev_enable_resources,
-	.ops_pci = &lops_pci,
+	.ops_pci = &pci_dev_ops_pci,
 };
 
 static const struct pci_driver hdaaudio_driver __pci_driver = {

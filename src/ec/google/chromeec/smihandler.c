b468f9b9ea03edc33ff959aa1f42fc06a124c1b6
Victor Ding <victording@google.com>
2020-04-30 13:23:42 +1000

    ec/google/chromeec: Fix incorrect diag message
    The expected error code observed in clear_pending_events() should
    be EC_RES_UNAVAILABLE(9), not EC_RES_INVALID_COMMAND(1).
    
    BUG=b:153896701
    
    Change-Id: I609490ceef675267760d34b5e9775211da93347c
    Signed-off-by: Victor Ding <victording@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40900
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/ec/google/chromeec/smihandler.c
+++ src/ec/google/chromeec/smihandler.c
@@ -41,7 +41,7 @@ static void clear_pending_events(void)
 	while (google_chromeec_get_event() != 0)
 		;
 
-	printk(BIOS_DEBUG,"Clearing pending EC events. Error code 1 is expected.\n");
+	printk(BIOS_DEBUG, "Clearing pending EC events. Error code EC_RES_UNAVAILABLE(9) is expected.\n");
 	while (google_chromeec_get_mkbp_event(&mkbp_event) == 0)
 		;
 }

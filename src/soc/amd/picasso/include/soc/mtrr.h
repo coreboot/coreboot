e04c2c4527d07583201003aa95f1ebc596c11e00
Raul E Rangel <rrangel@chromium.org>
2020-04-30 16:26:30 -0600

    src/soc/amd/picasso: Add methods to save and restore MTRRs
    FSP AGESA overrides the MTRRs that coreboot set up. Until this is fixed
    we need to save and restore the MTRRs to undo what AGESA did.
    
    Once AGESA is fixed, we can delete these files.
    
    BUG=b:155426691, b:147042464
    TEST=Boot trembyle and see MTRRs being modified
    Saving Variable MTRR 0: Base: 0x00000000 0xff000005, Mask: 0x0000ffff 0xff000800
    Saving Variable MTRR 1: Base: 0x00000000 0x08070006, Mask: 0x0000ffff 0xffff0800
    Saving Variable MTRR 2: Base: 0x00000000 0x00000000, Mask: 0x00000000 0x00000000
    Saving Variable MTRR 3: Base: 0x00000000 0x00000000, Mask: 0x00000000 0x00000000
    Saving Variable MTRR 4: Base: 0x00000000 0x00000000, Mask: 0x00000000 0x00000000
    Saving Variable MTRR 5: Base: 0x00000000 0x00000000, Mask: 0x00000000 0x00000000
    Saving Variable MTRR 6: Base: 0x00000000 0x00000000, Mask: 0x00000000 0x00000000
    Saving Variable MTRR 7: Base: 0x00000000 0x00000000, Mask: 0x00000000 0x00000000
    Saving Fixed MTRR 0: 0x00000000 0x00000000
    Saving Fixed MTRR 1: 0x00000000 0x00000000
    Saving Fixed MTRR 2: 0x00000000 0x00000000
    Saving Fixed MTRR 3: 0x00000000 0x00000000
    Saving Fixed MTRR 4: 0x00000000 0x00000000
    Saving Fixed MTRR 5: 0x00000000 0x00000000
    Saving Fixed MTRR 6: 0x00000000 0x00000000
    Saving Fixed MTRR 7: 0x00000000 0x00000000
    Saving Fixed MTRR 8: 0x00000000 0x00000000
    Saving Fixed MTRR 9: 0x00000000 0x00000000
    Saving Fixed MTRR 10: 0x00000000 0x00000000
    Saving Default Type MTRR: 0x00000000 0x00000800
    Saving SYS_CFG: 0x00000000 0x00000800
    ...
    MSR 0x200 was modified: 0x00000000 0x00000006
    MSR 0x201 was modified: 0x0000ffff 0x80000800
    MSR 0x202 was modified: 0x00000000 0x80000006
    MSR 0x203 was modified: 0x0000ffff 0xc0000800
    MSR 0x204 was modified: 0x00000000 0xc0000006
    MSR 0x205 was modified: 0x0000ffff 0xf0000800
    MSR 0x250 was modified: 0x06060606 0x06060606
    MSR 0x258 was modified: 0x06060606 0x06060606
    SYS_CFG was modified: 0x00000000 0x00740000
    
    
    Signed-off-by: Raul E Rangel <rrangel@chromium.org>
    Change-Id: I6048b25bd8a32904031ca23953f9726754b5a294
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40922
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/soc/amd/picasso/include/soc/mtrr.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* This file is part of the coreboot project. */
+
+#ifndef __PICASSO_MTRR_H__
+#define __PICASSO_MTRR_H__
+
+void picasso_save_mtrrs(void);
+void picasso_restore_mtrrs(void);
+
+#endif /* __PICASSO_MTRR_H__ */

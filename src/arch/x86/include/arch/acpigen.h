00367b2243064268062cd094f43e1271fca09c5e
Furquan Shaikh <furquan@google.com>
2020-04-30 23:00:41 -0700

    acpi: Move ACPI table support out of arch/x86 (4/5)
    This change moves all ACPI table support in coreboot currently living
    under arch/x86 into common code to make it architecture
    independent. ACPI table generation is not really tied to any
    architecture and hence it makes sense to move this to its own
    directory.
    
    In order to make it easier to review, this change is being split into
    multiple CLs. This is change 4/5 which gets rid of the placeholder
    header files that were added to temporarily include acpi/ header files
    from arch/header files.
    
    BUG=b:155428745
    
    Change-Id: If6e8580c3c6433f9239e06a1dc7ba661b3f597e5
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/40939
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: HAOUAS Elyes <ehaouas@noos.fr>
    Reviewed-by: Frans Hendriks <fhendriks@eltan.com>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/arch/x86/include/arch/acpigen.h
+++
@@ -1,4 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-/* This file is part of the coreboot project. */
-
-#include <acpi/acpigen.h>
--- src/arch/x86/include/arch/acpigen_ps2_keybd.h
+++
@@ -1,7 +0,0 @@
-/*
- * This file is part of the coreboot project.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-
-#include <acpi/acpigen_ps2_keybd.h>

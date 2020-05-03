b92a4d682f146b7864d8e697af297f42de0d5c5d
James Ye <jye836@gmail.com>
2020-05-03 23:04:07 +1000

    sb/intel/bd82x6x: Add missing ID for PCI bridge
    The PCI device ID 0x244e for the "Intel Corporation 82801 PCI Bridge"
    for desktop platforms was missing.
    
    Change-Id: I22cdbcf518d86af7b93de7731d175088a81bbc1f
    Signed-off-by: James Ye <jye836@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41015
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/southbridge/intel/bd82x6x/pci.c
+++ src/southbridge/intel/bd82x6x/pci.c
@@ -45,8 +45,14 @@ static struct device_operations device_ops = {
 	.ops_pci		= &pci_dev_ops_pci,
 };
 
+static const unsigned short pci_device_ids[] = {
+	0x2448, /* Mobile */
+	0x244e, /* Desktop */
+	0
+};
+
 static const struct pci_driver pch_pci __pci_driver = {
-	.ops	= &device_ops,
-	.vendor	= PCI_VENDOR_ID_INTEL,
-	.device	= 0x2448,
+	.ops		= &device_ops,
+	.vendor		= PCI_VENDOR_ID_INTEL,
+	.devices	= pci_device_ids,
 };

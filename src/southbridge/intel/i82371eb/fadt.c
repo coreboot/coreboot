132384aa4c71b5e8443834d872af89cfbff89376
Elyes HAOUAS <ehaouas@noos.fr>
2020-05-07 07:19:50 +0200

    sb/intel/i82371eb: Replace GPLv2 long form headers with SPDX header
    Change-Id: If54234ec2d80d5a6502400eb1c6f02dd9bba73c5
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41129
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>

--- src/southbridge/intel/i82371eb/fadt.c
+++ src/southbridge/intel/i82371eb/fadt.c
@@ -1,17 +1,8 @@
 /* This file is part of the coreboot project. */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
 /*
  * Based on src/southbridge/via/vt8237r/vt8237_fadt.c
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #include <string.h>

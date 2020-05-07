e4fc65bf746fe79babd03ce4d8efc54219b471b4
Elyes HAOUAS <ehaouas@noos.fr>
2020-05-07 07:26:54 +0200

    soc/intel: Replace GPLv2 long form headers with SPDX header
    Change-Id: I468d2ba85033c41ba53333ebbfd6f4108a36e407
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41130
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/soc/intel/apollolake/include/soc/gpio_apl.h
+++ src/soc/intel/apollolake/include/soc/gpio_apl.h
@@ -1,21 +1,11 @@
+/* This file is part of the coreboot project. */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
 /*
  * Definitions for the GPIO subsystem on Apollolake
  *
  * Placed in a separate file since some of these definitions can be used from
  * assembly code
- *
- * This file is part of the coreboot project.
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #ifndef _SOC_APOLLOLAKE_GPIO_H_
--- src/soc/intel/apollolake/include/soc/gpio_glk.h
+++ src/soc/intel/apollolake/include/soc/gpio_glk.h
@@ -1,20 +1,9 @@
+/* This file is part of the coreboot project. */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
 /*
- *
  * Placed in a separate file since some of these definitions can be used from
  * assembly code
- *
- * This file is part of the coreboot project.
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #ifndef _SOC_GLK_GPIO_H_

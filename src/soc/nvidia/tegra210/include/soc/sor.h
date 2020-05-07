16bc46c7adb23fd0cec8eadc014bb1b0adbd7916
Elyes HAOUAS <ehaouas@noos.fr>
2020-05-07 07:42:04 +0200

    soc/nvidia: Replace GPLv2 long form headers with SPDX header
    Change-Id: I7cc9adc95af5a8fc3cd69462d49efb1550e30295
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41133
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/soc/nvidia/tegra210/include/soc/sor.h
+++ src/soc/nvidia/tegra210/include/soc/sor.h
@@ -1,17 +1,8 @@
 /* This file is part of the coreboot project. */
+/* SPDX-License-Identifier: GPL-2.0-only */
+
 /*
  * drivers/video/tegra/dc/sor_regs.h
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
- * GNU General Public License for more details.
- *
  */
 
 #ifndef __TEGRA210_SOR_H__

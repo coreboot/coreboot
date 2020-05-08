e8d1bef8cbd78c00065381848030655d34d0ecd3
Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
2020-05-08 21:31:44 +0530

    jasperlake: update processor power limits configuration
    Update processor power limit configuration parameters based on
    common code base support for Intel Jasperlake SoC based platforms.
    
    BRANCH=None
    BUG=None
    TEST=Built for jasperlake system
    
    Change-Id: I9b725d041dcb8847f83ec103e58b9571b4c596ac
    Signed-off-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41237
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/soc/intel/jasperlake/include/soc/cpu.h
+++ src/soc/intel/jasperlake/include/soc/cpu.h
@@ -30,7 +30,4 @@
 	C_STATE_LATENCY_MICRO_SECONDS(C_STATE_LATENCY_CONTROL_ ##reg## _LIMIT, \
 				      (IRTL_1024_NS >> 10))
 
-/* Configure power limits for turbo mode */
-void set_power_limits(u8 power_limit_1_time);
-
 #endif

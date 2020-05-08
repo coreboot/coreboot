2faeb11fcda8791e94fee804a920adfd6272e4d7
Patrick Georgi <pgeorgi@google.com>
2020-05-08 18:32:38 +0200

    treewide: more SPDX header work
    Change-Id: Ib78c322730ec6dfa9dcaafa16e5741cd3d351b8d
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41174
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>
    Reviewed-by: HAOUAS Elyes <ehaouas@noos.fr>

--- Documentation/mainboard_io_trap_handler_sample.c
+++ Documentation/mainboard_io_trap_handler_sample.c
@@ -1,18 +1,5 @@
-/*
- * This file is part of the coreboot project.
- *
- * Copyright (C) 2008-2009 coresystems GmbH
- * Copyright 2013 Google Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+/* This file is part of the coreboot project. */
+/* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <arch/io.h>
 #include <console/console.h>

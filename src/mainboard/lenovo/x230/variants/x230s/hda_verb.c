6b95507ec5b087658178a325bdc68570bc48bb20
Bill XIE <persmule@hardenedlinux.org>
2020-05-08 16:40:48 +0800

    mainboard/lenovo/x230: Add ThinkPad x230s as a variant
    The code is based on autoport and that for X230. Major differences are:
        - Only one DDR3 slot
        - HM77 PCH
        - M.2 socket instead of mini pci-e
        - no docking
        - no tpm
    
    Tested:
        - CPU i5-3337U
        - Slotted DIMM 8GiB
        - Camera
        - pci-e and usb2 on M.2 slot with A key for wlan
        - sata and usb2  (no superspeed components) on M.2 slot with B key for wwan
        - On board SDHCI connected to pci-e
        - USB3 ports
        - libgfxinit-based graphic init
        - NVRAM options for North and South bridges
        - Sound
        - Thinkpad EC
        - S3
        - Linux 4.9 within Debian GNU/Linux stable, loaded from
          Seabios.
    
    Untested:
        - Touch screen, which is said to work under ubuntu but not debian.
    
    Change-Id: Ie537645d5ffaee799e79af2f821f80c3ebd2dfec
    Signed-off-by: Bill XIE <persmule@hardenedlinux.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41168
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Patrick Georgi <pgeorgi@google.com>

---
+++ src/mainboard/lenovo/x230/variants/x230s/hda_verb.c
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/azalia_device.h>
+
+const u32 cim_verb_data[] = {
+	0x10ec0269,	/* Codec Vendor / Device ID: Realtek */
+	0x17aa2209,	/* Subsystem ID */
+	11,		/* Number of 4 dword sets */
+	AZALIA_SUBVENDOR(0, 0x17aa2209),
+	AZALIA_PIN_CFG(0, 0x12, 0x90a60140),
+	AZALIA_PIN_CFG(0, 0x14, 0x90170110),
+	AZALIA_PIN_CFG(0, 0x15, 0x03211020),
+	AZALIA_PIN_CFG(0, 0x17, 0x40008000),
+	AZALIA_PIN_CFG(0, 0x18, 0x03a11030),
+	AZALIA_PIN_CFG(0, 0x19, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1a, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1b, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1d, 0x40f38205),
+	AZALIA_PIN_CFG(0, 0x1e, 0x411111f0),
+
+	0x80862806,	/* Codec Vendor / Device ID: Intel */
+	0x80860101,	/* Subsystem ID */
+	4,		/* Number of 4 dword sets */
+	AZALIA_SUBVENDOR(3, 0x80860101),
+	AZALIA_PIN_CFG(3, 0x05, 0x18560010),
+	AZALIA_PIN_CFG(3, 0x06, 0x58560020),
+	AZALIA_PIN_CFG(3, 0x07, 0x58560030),
+
+};
+
+const u32 pc_beep_verbs[0] = {};
+
+AZALIA_ARRAY_SIZES;

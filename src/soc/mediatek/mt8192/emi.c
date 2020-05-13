ad700565efe2edfb1d4ca812e00e941d16a5b5ba
CK Hu <ck.hu@mediatek.com>
2020-05-13 10:21:02 +0800

    soc/mediatek/mt8192: Add a placeholder for the EMI driver
    Add minimal function to report SDRAM size.
    
    Signed-off-by: CK Hu <ck.hu@mediatek.com>
    Change-Id: If74b6b52dd6e91d1ff40cf8460b6a03b2f3bb6f6
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/43961
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/soc/mediatek/mt8192/emi.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <soc/emi.h>
+
+size_t sdram_size(void)
+{
+	size_t dram_size = 0x100000000;
+
+	return dram_size;
+}
---
+++ src/soc/mediatek/mt8192/include/soc/emi.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SOC_MEDIATEK_MT8192_EMI_H
+#define SOC_MEDIATEK_MT8192_EMI_H
+
+#include <types.h>
+
+size_t sdram_size(void);
+
+#endif /* SOC_MEDIATEK_MT8192_EMI_H */

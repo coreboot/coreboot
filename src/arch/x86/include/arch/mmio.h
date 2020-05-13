2b79203bdbccb3e807fa59acc9f7b841ca747e69
Elyes HAOUAS <ehaouas@noos.fr>
2020-05-13 14:08:24 +0200

    x86/include/arch/mmio.h: Convert to 96 characters line length
    Change-Id: I93d0ef6db417904c345fe7b76730bcb70ba25089
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41361
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/arch/x86/include/arch/mmio.h
+++ src/arch/x86/include/arch/mmio.h
@@ -5,50 +5,42 @@
 
 #include <stdint.h>
 
-static __always_inline uint8_t read8(
-	const volatile void *addr)
+static __always_inline uint8_t read8(const volatile void *addr)
 {
 	return *((volatile uint8_t *)(addr));
 }
 
-static __always_inline uint16_t read16(
-	const volatile void *addr)
+static __always_inline uint16_t read16(const volatile void *addr)
 {
 	return *((volatile uint16_t *)(addr));
 }
 
-static __always_inline uint32_t read32(
-	const volatile void *addr)
+static __always_inline uint32_t read32(const volatile void *addr)
 {
 	return *((volatile uint32_t *)(addr));
 }
 
-static __always_inline uint64_t read64(
-	const volatile void *addr)
+static __always_inline uint64_t read64(const volatile void *addr)
 {
 	return *((volatile uint64_t *)(addr));
 }
 
-static __always_inline void write8(volatile void *addr,
-	uint8_t value)
+static __always_inline void write8(volatile void *addr, uint8_t value)
 {
 	*((volatile uint8_t *)(addr)) = value;
 }
 
-static __always_inline void write16(volatile void *addr,
-	uint16_t value)
+static __always_inline void write16(volatile void *addr, uint16_t value)
 {
 	*((volatile uint16_t *)(addr)) = value;
 }
 
-static __always_inline void write32(volatile void *addr,
-	uint32_t value)
+static __always_inline void write32(volatile void *addr, uint32_t value)
 {
 	*((volatile uint32_t *)(addr)) = value;
 }
 
-static __always_inline void write64(volatile void *addr,
-	uint64_t value)
+static __always_inline void write64(volatile void *addr, uint64_t value)
 {
 	*((volatile uint64_t *)(addr)) = value;
 }

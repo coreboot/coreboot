c1c6c51ec00906038a7add6de54ca59561842ba6
Angel Pons <th3fanbus@gmail.com>
2020-05-14 11:19:55 +0200

    drivers/xgi: Remove dead code
    This was used by the now-gone Asus KFSN4-DRE mainboard. Drop it.
    
    Change-Id: Id00c883ed0f80e7af96fdf3f6e2985dd5b227831
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41402
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: HAOUAS Elyes <ehaouas@noos.fr>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/drivers/xgi/common/vb_init.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-
-/* File taken from the Linux xgifb driver (v3.18.5) */
-
-#ifndef _VBINIT_
-#define _VBINIT_
-extern unsigned char XGIInitNew(struct pci_dev *pdev);
-extern void XGIRegInit(struct vb_device_info *, unsigned long);
-#endif

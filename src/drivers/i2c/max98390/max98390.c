8952de55cb4949969b09219fdf6b9b14f3b59b0c
Seunghwan Kim <sh_.kim@samsung.corp-partner.google.com>
2020-05-18 15:01:06 +0900

    driver/i2c/max98390: Fix build error related to max98390 driver
    Fix coreboot build error with adding this driver
    
    BUG=b:149443429
    BRANCH=None
    TEST=built without errors
    
    Signed-off-by: Seunghwan Kim <sh_.kim@samsung.corp-partner.google.com>
    Change-Id: I2d76ec72ca6ae9ac54ab05f15ea92beb645acd5c
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41496
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/drivers/i2c/max98390/max98390.c
+++ src/drivers/i2c/max98390/max98390.c
@@ -15,7 +15,7 @@
 
 #define MAX98390_DP_INT(key, val) acpi_dp_add_integer(dp, "maxim," key, (val))
 
-static void max98390_fill_ssdt(struct device *dev)
+static void max98390_fill_ssdt(const struct device *dev)
 {
 	struct drivers_i2c_max98390_config *config = dev->chip_info;
 	const char *scope = acpi_device_scope(dev);
@@ -86,7 +86,7 @@ static struct device_operations max98390_ops = {
 	.read_resources = noop_read_resources,
 	.set_resources = noop_set_resources,
 	.acpi_name = max98390_acpi_name,
-	.acpi_fill_ssdt_generator = max98390_fill_ssdt,
+	.acpi_fill_ssdt = max98390_fill_ssdt,
 };
 
 static void max98390_enable(struct device *dev)

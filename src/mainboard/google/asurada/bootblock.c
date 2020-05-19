5ed4d63fe12b9dc744a15a76c136132cc42308db
Nicolas Boichat <drinkcat@chromium.org>
2020-05-19 14:24:33 +0800

    mb/google/asurada: enable SPI devices
    Configure and initialize EC and TPM on Asurada.
    
    BUG=none
    TEST=boot asurada
    
    Change-Id: I0f169407d1726899fd0c42e144d907024f036c6a
    Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/46385
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/asurada/bootblock.c
+++ src/mainboard/google/asurada/bootblock.c
@@ -1,7 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootblock_common.h>
+#include <soc/spi.h>
 
 void bootblock_mainboard_init(void)
 {
+	mtk_spi_init(CONFIG_EC_GOOGLE_CHROMEEC_SPI_BUS, SPI_PAD0_MASK, 1 * MHz, 0);
+	mtk_spi_init(CONFIG_DRIVER_TPM_SPI_BUS, SPI_PAD0_MASK, 1 * MHz, 0);
 }

b4ab1e78cdda18fffb4dffd611300e3537315c17
David Wu <david_wu@quanta.corp-partner.google.com>
2020-05-26 15:28:09 +0800

    mb/google/volteer: Create terrador variant
    Create the terrador variant of the volteer reference board
    
    BUG=b:156435028
    BRANCH=None
    TEST=util/abuild/abuild -p none -t google/volteer -x -a
    make sure the build includes GOOGLE_TERRADOR
    
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Change-Id: I088861d1f8b7b4ee8de1e5ab6c7d3109ffd0531b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41718
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>

---
+++ src/mainboard/google/volteer/variants/terrador/gpio.c
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <commonlib/helpers.h>
+
+/* Pad configuration in ramstage */
+static const struct pad_config gpio_table[] = {
+
+};
+
+const struct pad_config *variant_base_gpio_table(size_t *num)
+{
+	*num = ARRAY_SIZE(gpio_table);
+	return gpio_table;
+}
+
+/* Early pad configuration in bootblock */
+static const struct pad_config early_gpio_table[] = {
+
+};
+
+const struct pad_config *variant_early_gpio_table(size_t *num)
+{
+	*num = ARRAY_SIZE(early_gpio_table);
+	return early_gpio_table;
+}
+
+static const struct cros_gpio cros_gpios[] = {
+};
+
+const struct cros_gpio *variant_cros_gpios(size_t *num)
+{
+	*num = ARRAY_SIZE(cros_gpios);
+	return cros_gpios;
+}
---
+++ src/mainboard/google/volteer/variants/terrador/include/variant/ec.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef __VARIANT_EC_H__
+#define __VARIANT_EC_H__
+
+#include <baseboard/ec.h>
+
+#endif

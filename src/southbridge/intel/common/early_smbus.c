90e9f5472680bbc44fe4e23b390a9323f8c59643
Angel Pons <th3fanbus@gmail.com>
2020-06-01 19:31:53 +0200

    ironlake/ibexpeak: Move early_smbus.c to common code
    We will update the other platforms to use this common code in
    susbsequent commits. While we are at it, reflow a broken line,
    define the SMBus PCI device in the header and fix whitespace.
    
    Change-Id: I1fdff2feead4165f02b24cb948d8c03318969014
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41999
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/southbridge/intel/common/early_smbus.c
+++ src/southbridge/intel/common/early_smbus.c
@@ -1,10 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <device/pci_def.h>
 #include <device/pci_ids.h>
 #include <device/pci_ops.h>
-#include <device/pci_def.h>
 #include <device/smbus_host.h>
-#include "pch.h"
+#include "early_smbus.h"
 
 uintptr_t smbus_base(void)
 {
@@ -14,15 +14,14 @@ uintptr_t smbus_base(void)
 int smbus_enable_iobar(uintptr_t base)
 {
 	/* Set the SMBus device statically. */
-	pci_devfn_t dev = PCI_DEV(0x0, 0x1f, 0x3);
+	const pci_devfn_t dev = PCI_DEV_SMBUS;
 
 	/* Check to make sure we've got the right device. */
 	if (pci_read_config16(dev, PCI_VENDOR_ID) != PCI_VENDOR_ID_INTEL)
 		return -1;
 
 	/* Set SMBus I/O base. */
-	pci_write_config32(dev, SMB_BASE,
-			   base | PCI_BASE_ADDRESS_SPACE_IO);
+	pci_write_config32(dev, SMB_BASE, base | PCI_BASE_ADDRESS_SPACE_IO);
 
 	/* Set SMBus enable. */
 	pci_write_config8(dev, HOSTC, HST_EN);
---
+++ src/southbridge/intel/common/early_smbus.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SOUTHBRIDGE_INTEL_COMMON_EARLY_SMBUS_H
+#define SOUTHBRIDGE_INTEL_COMMON_EARLY_SMBUS_H
+
+#include <device/pci_def.h>
+
+#define PCI_DEV_SMBUS		PCI_DEV(0, 0x1f, 3)
+
+#define SMB_BASE		PCI_BASE_ADDRESS_4
+#define HOSTC			0x40
+
+/* HOSTC bits */
+#define I2C_EN			(1 << 2)
+#define SMB_SMI_EN		(1 << 1)
+#define HST_EN			(1 << 0)
+
+#endif /* SOUTHBRIDGE_INTEL_COMMON_EARLY_SMBUS_H */

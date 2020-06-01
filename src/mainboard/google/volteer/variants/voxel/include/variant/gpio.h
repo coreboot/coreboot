fba0ad84d18d5509598b7f047a16d038e6cb8f42
David Wu <david_wu@quanta.corp-partner.google.com>
2020-06-01 19:45:54 +0800

    mb/google/volteer: Create voxel variant
    Create the voxel variant of the volteer reference board
    
    BUG=b:157879197
    BRANCH=None
    TEST=util/abuild/abuild -p none -t google/volteer -x -a
    make sure the build includes GOOGLE_VOXEL
    
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Change-Id: I8ba5412be211730db84675927c500238cb20ff3d
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/41968
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>

---
+++ src/mainboard/google/volteer/variants/voxel/include/variant/gpio.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef VARIANT_GPIO_H
+#define VARIANT_GPIO_H
+
+#include <baseboard/gpio.h>
+
+/* Copied from baseboard and may need to change for the new variant. */
+
+#endif

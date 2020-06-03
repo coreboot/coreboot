coreboot/coreboot

commit a1c82c5ebee830fa28a1962618bba4946e68f3ba

drivers/generic/max98357a: Allow custom _HID from config
Add HID field in max98357a_config and allow mainboards to set it.

Signed-off-by: Aamir Bohra <aamir.bohra@intel.com>
Change-Id: I22d2d078a9a4eb6ab330da8439737ff5133086d4
Reviewed-on: https://review.coreboot.org/c/coreboot/+/39286
Reviewed-by: Furquan Shaikh <furquan@google.com>
Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/drivers/generic/max98357a/max98357a.c
@@ -23,7 +23,6 @@
 #if CONFIG(HAVE_ACPI_TABLES)
 
 #define MAX98357A_ACPI_NAME	"MAXM"
-#define MAX98357A_ACPI_HID	"MX98357A"
 
 static void max98357a_fill_ssdt(struct device *dev)
 {
@@ -42,7 +41,13 @@ static void max98357a_fill_ssdt(struct device *dev)
 	/* Device */
 	acpigen_write_scope(scope);
 	acpigen_write_device(name);
-	acpigen_write_name_string("_HID", MAX98357A_ACPI_HID);
+
+	if (!config->hid) {
+		printk(BIOS_ERR, "%s: ERROR: _HID required\n", dev_path(dev));
+		return;
+	}
+
+	acpigen_write_name_string("_HID", config->hid);
 	acpigen_write_name_integer("_UID", 0);
 	acpigen_write_name_string("_DDN", dev->chip_ops->name);
 	acpigen_write_STA(acpi_device_status(dev));

src/drivers/generic/max98357a/chip.h
@@ -14,6 +14,10 @@
 #include <arch/acpi_device.h>
 
 struct drivers_generic_max98357a_config {
+
+	/* ACPI _HID */
+	const char *hid;
+
 	/* SDMODE GPIO */
 	struct acpi_gpio sdmode_gpio;
 

src/mainboard/google/fizz/variants/karma/overridetree.cb
@@ -58,6 +58,7 @@ chip soc/intel/skylake
 		end # USB xHCI
 		device pci 19.1 on
 			chip drivers/generic/max98357a
+				register "hid" = ""MX98357A""
 				register "sdmode_gpio" =  "ACPI_GPIO_OUTPUT_ACTIVE_HIGH(GPP_A23)"
 				register "sdmode_delay" = "5"
 				device generic 0 on end

src/mainboard/google/glados/variants/asuka/devicetree.cb
@@ -269,6 +269,7 @@ chip soc/intel/skylake
 		device pci 1f.2 on  end # Power Management Controller
 		device pci 1f.3 on
 			chip drivers/generic/max98357a
+				register "hid" = ""MX98357A""
 				register "sdmode_gpio" = "ACPI_GPIO_OUTPUT_ACTIVE_HIGH(GPP_E3)"
 				register "sdmode_delay" = "5"
 				device generic 0 on end

src/mainboard/google/glados/variants/cave/devicetree.cb
@@ -284,6 +284,7 @@ chip soc/intel/skylake
 		device pci 1f.2 on  end # Power Management Controller
 		device pci 1f.3 on
 			chip drivers/generic/max98357a
+				register "hid" = ""MX98357A""
 				register "sdmode_gpio" = "ACPI_GPIO_OUTPUT_ACTIVE_HIGH(GPP_E3)"
 				register "sdmode_delay" = "5"
 				device generic 0 on end

src/mainboard/google/glados/variants/lars/devicetree.cb
@@ -267,6 +267,7 @@ chip soc/intel/skylake
 		device pci 1f.2 on  end # Power Management Controller
 		device pci 1f.3 on
 			chip drivers/generic/max98357a
+				register "hid" = ""MX98357A""
 				register "sdmode_gpio" = "ACPI_GPIO_OUTPUT_ACTIVE_HIGH(GPP_B2)"
 				register "sdmode_delay" = "5"
 				device generic 0 on end

src/mainboard/google/glados/variants/sentry/devicetree.cb
@@ -290,6 +290,7 @@ chip soc/intel/skylake
 		device pci 1f.2 on  end # Power Management Controller
 		device pci 1f.3 on
 			chip drivers/generic/max98357a
+				register "hid" = ""MX98357A""
 				register "sdmode_gpio" = "ACPI_GPIO_OUTPUT_ACTIVE_HIGH(GPP_E3)"
 				register "device_present_gpio" = "GPP_E3"
 				register "device_present_gpio_invert" = "1"

src/mainboard/google/hatch/variants/akemi/overridetree.cb
@@ -236,6 +236,7 @@ chip soc/intel/cannonlake
 		device pci 1e.3 off end # GSPI #1
 		device pci 1f.3 on
 			chip drivers/generic/max98357a
+				register "hid" = ""MX98357A""
 				register "sdmode_gpio" =  "ACPI_GPIO_OUTPUT_ACTIVE_HIGH(GPP_H3)"
 				register "sdmode_delay" = "5"
 				device generic 0 on end

commit 066a9af02c3eff812193597f7c32b0be1d8f6e3b

drivers/generic/max98357a: Don't write device if HID is missing
If the device is missing the HID, the code would previously leave an
open scope and device on the stack.

BUG=b:154756391
TEST=Verify stack ACPI stack does not exceed limit on Trembyle.

Fixes: a1c82c5ebee ("drivers/generic/max98357a: Allow custom _HID from config")
Signed-off-by: Raul E Rangel <rrangel@chromium.org>
Change-Id: I798ed08ef0a0575def12937a66a7ce99f743e60d
Reviewed-on: https://review.coreboot.org/c/coreboot/+/42014
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Furquan Shaikh <furquan@google.com>

src/drivers/generic/max98357a/max98357a.c
@@ -26,14 +26,14 @@ static void max98357a_fill_ssdt(const struct device *dev)
 	if (!scope || !name)
 		return;
 
-	/* Device */
-	acpigen_write_scope(scope);
-	acpigen_write_device(name);
-
 	if (!config->hid) {
 		printk(BIOS_ERR, "%s: ERROR: _HID required\n", dev_path(dev));
 		return;
 	}
+
+	/* Device */
+	acpigen_write_scope(scope);
+	acpigen_write_device(name);
 
 	acpigen_write_name_string("_HID", config->hid);
 	acpigen_write_name_integer("_UID", 0);
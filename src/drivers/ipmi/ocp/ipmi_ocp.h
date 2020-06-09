6b297c07c3da6002941a365ecb89d2709e44f5d6
Tim Chu <Tim.Chu@quantatw.com>
2020-06-08 22:52:58 -0700

    drivers/ipmi/ocp: Add ipmi set processor information
    Implement setting processor information to BMC based on
    document YosemiteV3_BMC_Feature_Spec_v1.7.
    
    TEST=Use get command in OpenBMC to check.
    Command and information are shown as below:
    
    root@bmc-oob:~# ipmi-util 1 0xd8 0x11 0x4c 0x1c 0x00 0 1
    DC 11 00 47 65 6E 75 69 6E 65 20 49 6E 74 65 6C 28 52 29
    20 43 50 55 20 30 30 30 30 25 40 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00
    root@bmc-oob:~# ipmi-util 1 0xd8 0x11 0x4c 0x1c 0x00 0 2
    DC 11 00 1A 34 00 DC 05 41 30
    root@bmc-oob:~#
    
    Signed-off-by: Tim Chu <Tim.Chu@quantatw.com>
    Change-Id: I3d53ac241a11ca962572816283a0c653fcde9f9e
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/42242
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/drivers/ipmi/ocp/ipmi_ocp.h
+++ src/drivers/ipmi/ocp/ipmi_ocp.h
@@ -3,4 +3,34 @@
 #ifndef __IPMI_OCP_H
 #define __IPMI_OCP_H
 
+#include <cpu/x86/msr.h>
+#include <cpu/x86/name.h>
+#include "drivers/ipmi/ipmi_kcs.h"
+
+#define IPMI_NETFN_OEM_COMMON			0x36
+#define  IPMI_BMC_SET_PROCESSOR_INFORMATION     0x10
+#define  IPMI_BMC_GET_PROCESSOR_INFORMATION     0x11
+
+#define MSR_CORE_THREAD_COUNT	0x35
+#define MSR_PLATFORM_INFO	0xce
+
+struct ipmi_processor_info_req {
+	uint8_t manufacturer_id[3];
+	uint8_t index;
+	uint8_t parameter_selector;
+} __packed;
+
+struct ipmi_processor_info_param1_req {
+	struct ipmi_processor_info_req data;
+	char product_name[48];
+} __packed;
+
+struct ipmi_processor_info_param2_req {
+	struct ipmi_processor_info_req data;
+	uint8_t core_number;
+	uint16_t thread_number;
+	uint16_t processor_freq;
+	char revision[2];
+} __packed;
+
 #endif

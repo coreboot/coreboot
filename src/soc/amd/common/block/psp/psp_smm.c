722e610fbc287a44112db3cef8996ebba0343440
Elyes HAOUAS <ehaouas@noos.fr>
2020-06-13 12:52:24 +0200

    soc/amd/common/block/psp/psp_smm.c: Add missing <string.h>
    'memset' needs <string.h>.
    
    Change-Id: Idc1d72e92c97cd5139ae7439aadb575ef011129a
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/42342
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/common/block/psp/psp_smm.c
+++ src/soc/amd/common/block/psp/psp_smm.c
@@ -9,6 +9,8 @@
 #include <console/console.h>
 #include <amdblocks/psp.h>
 #include <soc/iomap.h>
+#include <string.h>
+
 #include "psp_def.h"
 
 #define C2P_BUFFER_MAXSIZE 0xc00 /* Core-to-PSP buffer */

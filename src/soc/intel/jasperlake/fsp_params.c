f4a940c23613b988512f46acf45b4e0eb338e2c5
Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
2020-06-18 18:53:23 +0530

    jasperlake: enable tcc_offset functionality
    This enables Thermal Control Circuit (TCC) activation feature to set
    tcc_offset value to new value in devicetree.
    
    BUG=None
    BRANCH=None
    TEST=Built for dedede platform and verified the MSR value
    
    Change-Id: I58e4fa362f20efeef84e06e64d70ee7c4f9554d6
    Signed-off-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/42515
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/soc/intel/jasperlake/fsp_params.c
+++ src/soc/intel/jasperlake/fsp_params.c
@@ -190,6 +190,9 @@ void platform_fsp_silicon_init_params_cb(FSPS_UPD *supd)
 
 	params->Device4Enable = config->Device4Enable;
 
+	/* Set TccActivationOffset */
+	params->TccActivationOffset = config->tcc_offset;
+
 	/* eMMC configuration */
 	dev = pcidev_path_on_root(PCH_DEVFN_EMMC);
 	if (!dev) {

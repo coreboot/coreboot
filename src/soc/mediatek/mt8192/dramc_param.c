7452a2fdeffeef460483ee41ad74c9f90553a5e3
Huayang Duan <huayang.duan@mediatek.com>
2020-06-22 17:44:01 +0800

    soc/mediatek/mt8192: Add dramc param struct
    Signed-off-by: Huayang Duan <huayang.duan@mediatek.com>
    Change-Id: I3bae57e6777ab6fc46c771a034f814dd1175be95
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/44566
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ src/soc/mediatek/mt8192/dramc_param.c
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <console/console.h>
+#include <string.h>
+#include <soc/dramc_param.h>
+
+#define print(_x_...) printk(BIOS_INFO, _x_)
+
+struct dramc_param *get_dramc_param_from_blob(void *blob)
+{
+	return (struct dramc_param *)blob;
+}
+
+void dump_param_header(const void *blob)
+{
+	const struct dramc_param *dparam = blob;
+	const struct dramc_param_header *header = &dparam->header;
+
+	print("header.status = %#x\n", header->status);
+	print("header.version = %#x (expected: %#x)\n",
+	       header->version, DRAMC_PARAM_HEADER_VERSION);
+	print("header.size = %#x (expected: %#lx)\n",
+	       header->size, sizeof(*dparam));
+	print("header.flags = %#x\n", header->flags);
+	print("header.checksum = %#x\n", header->checksum);
+}
+
+int validate_dramc_param(const void *blob)
+{
+	const struct dramc_param *param = blob;
+	const struct dramc_param_header *hdr = &param->header;
+
+	if (hdr->version != DRAMC_PARAM_HEADER_VERSION)
+		return DRAMC_ERR_INVALID_VERSION;
+
+	if (hdr->size != sizeof(*param))
+		return DRAMC_ERR_INVALID_SIZE;
+
+	return DRAMC_SUCCESS;
+}
+
+int is_valid_dramc_param(const void *blob)
+{
+	return validate_dramc_param(blob) == DRAMC_SUCCESS;
+}
+
+int initialize_dramc_param(void *blob)
+{
+	struct dramc_param *param = blob;
+	struct dramc_param_header *hdr = &param->header;
+
+	memset(hdr, 0, sizeof(*hdr));
+	hdr->version = DRAMC_PARAM_HEADER_VERSION;
+	hdr->size = sizeof(*param);
+	return 0;
+}
---
+++ src/soc/mediatek/mt8192/include/soc/dramc_common_mt8192.h
@@ -0,0 +1,82 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef __SOC_MEDIATEK_MT8192_DRAMC_COMMON_MT8192_H__
+#define __SOC_MEDIATEK_MT8192_DRAMC_COMMON_MT8192_H__
+
+enum {
+	CHANNEL_A = 0,
+	CHANNEL_B,
+	CHANNEL_MAX
+};
+
+enum {
+	RANK_0 = 0,
+	RANK_1,
+	RANK_MAX
+};
+
+enum {
+	FSP_0 = 0,
+	FSP_1,
+	FSP_MAX,
+};
+
+typedef enum {
+	DRAM_DFS_SHU0 = 0,
+	DRAM_DFS_SHU1,
+	DRAM_DFS_SHU2,
+	DRAM_DFS_SHU3,
+	DRAM_DFS_SHU4,
+	DRAM_DFS_SHU5,
+	DRAM_DFS_SHU6,
+	DRAM_DFS_SHU_MAX
+} dram_dfs_shu;
+
+typedef enum {
+	ODT_OFF = 0,
+	ODT_ON,
+	ODT_MAX
+} dram_odt_state;
+
+typedef enum {
+	DBI_OFF = 0,
+	DBI_ON
+} dbi_mode;
+
+enum {
+	CKE_FIXOFF = 0,
+	CKE_FIXON,
+	CKE_DYNAMIC
+};
+
+enum {
+	CA_NUM_LP4 = 6,
+	DQ_DATA_WIDTH = 16,
+	DQS_BIT_NUMBER = 8,
+	DQS_NUMBER = (DQ_DATA_WIDTH / DQS_BIT_NUMBER),
+};
+#define BYTE_NUM		DQS_NUMBER
+#define DQS_NUMBER_LP4		DQS_NUMBER
+#define DQ_DATA_WIDTH_LP4	DQ_DATA_WIDTH
+
+typedef enum {
+	CBT_NORMAL_MODE = 0,
+	CBT_BYTE_MODE1
+} dram_cbt_mode;
+
+/* DONOT change the sequence of pinmux */
+typedef enum {
+	PINMUX_DSC = 0,
+	PINMUX_LPBK,
+	PINMUX_EMCP,
+	PINMUX_MAX
+} dram_pinmux_type;
+
+enum {
+	CBT_R0_R1_NORMAL = 0,
+	CBT_R0_R1_BYTE,
+	CBT_R0_NORMAL_R1_BYTE,
+	CBT_R0_BYTE_R1_NORMAL
+};
+
+#endif	/* __SOC_MEDIATEK_MT8192_DRAMC_COMMON_MT8192_H__ */

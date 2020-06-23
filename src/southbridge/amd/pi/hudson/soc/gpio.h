b8cb142ccd1922f8f0a245923eabaed5030048e5
Kyösti Mälkki <kyosti.malkki@gmail.com>
2020-06-23 21:36:14 +0300

    sb/amd/pi/hudson: Enable use of common GPIO API
    The code in soc/amd/common has an implementation of
    GPIO register space that is compatible with the hardware
    sb/amd/pi/hudson supports.
    
    Change-Id: I86ae40a3cdf335263d7e9e3dcfdd588947cdd9b1
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/42733
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

---
+++ src/southbridge/amd/pi/hudson/soc/gpio.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SOC_GPIO_H
+#define SOC_GPIO_H
+
+/* <soc/gpio.h> must provide gpio_t. */
+#include <amdblocks/gpio_banks.h>
+
+#endif /* SOC_GPIO_H */
---
+++ src/southbridge/amd/pi/hudson/soc/smi.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SOC_SMI_H
+#define SOC_SMI_H
+
+#define SMI_SCI_TRIG			0x08
+#define SMI_SCI_LEVEL			0x0c
+#define SMI_SCI_STATUS			0x10
+#define SMI_SCI_EN			0x14
+
+#endif /* SOC_SMI_H */

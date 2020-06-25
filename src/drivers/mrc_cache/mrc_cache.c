6d412d738c48c5e65da4e2054109726ba3f558d7
Paul Menzel <pmenzel@molgen.mpg.de>
2020-06-25 09:11:56 +0200

    drivers/mrc_cache: Avoid unused variable assignment
    Fix the scan-build warning below:
    
            CC         romstage/drivers/mrc_cache/mrc_cache.o
        src/drivers/mrc_cache/mrc_cache.c:450:26: warning: Value stored to 'flash' during its initialization is never read
                const struct spi_flash *flash = boot_device_spi_flash();
                                        ^~~~~   ~~~~~~~~~~~~~~~~~~~~~~~
        1 warning generated.
    
    The function can return early before the value is read. Fix this, by
    getting rid of the variable, as the value is only read once.
    
    Change-Id: I3c94b123f4994eed9d7568b63971fd5b1d94bc09
    Found-by: scan-build (clang-tools-9 1:9.0.1-12)
    Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/42798
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>

--- src/drivers/mrc_cache/mrc_cache.c
+++ src/drivers/mrc_cache/mrc_cache.c
@@ -447,15 +447,13 @@ static int nvm_is_write_protected(void)
 /* Apply protection to a range of flash */
 static int nvm_protect(const struct region *r)
 {
-	const struct spi_flash *flash = boot_device_spi_flash();
-
 	if (!CONFIG(MRC_SETTINGS_PROTECT))
 		return 0;
 
 	if (!CONFIG(BOOT_DEVICE_SPI_FLASH))
 		return 0;
 
-	return spi_flash_ctrlr_protect_region(flash, r, WRITE_PROTECT);
+	return spi_flash_ctrlr_protect_region(boot_device_spi_flash(), r, WRITE_PROTECT);
 }
 
 /* Protect mrc region with a Protected Range Register */

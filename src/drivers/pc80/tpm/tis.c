56fdafbaff33aee9f016ff04e0d69f5477729039
Patrick Rudolph <siro@das-labor.org>
2020-07-01 20:07:08 +0200

    drivers/pc80/tpm/tis: Add x86_64 support
    Fix integer with different size to pointer conversion on x86_64.
    
    Change-Id: Ic06a32d549b694310f4c724246f28fed15acf83f
    Signed-off-by: Patrick Rudolph <siro@das-labor.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/42983
    Reviewed-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Christian Walter <christian.walter@9elements.com>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/drivers/pc80/tpm/tis.c
+++ src/drivers/pc80/tpm/tis.c
@@ -44,7 +44,7 @@
 
 /* the macro accepts the locality value, but only locality 0 is operational */
 #define TIS_REG(LOCALITY, REG) \
-	(void *)(CONFIG_TPM_TIS_BASE_ADDRESS + (LOCALITY << 12) + REG)
+	(void *)(uintptr_t)(CONFIG_TPM_TIS_BASE_ADDRESS + (LOCALITY << 12) + REG)
 
 /* hardware registers' offsets */
 #define TIS_REG_ACCESS                 0x0

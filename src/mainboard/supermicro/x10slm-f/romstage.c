e8ecabca73eea669abd453462b80eb10ed536ac3
Angel Pons <th3fanbus@gmail.com>
2020-07-03 18:29:38 +0200

    mb/supermicro/x10slm-f: Factor out common MRC settings
    There's no need to redefine common settings.
    
    Change-Id: Iee5ca6188de4cea9393efb66baa089969353b4e6
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/43111
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tristan Corrick <tristan@corrick.kiwi>

--- src/mainboard/supermicro/x10slm-f/romstage.c
+++ src/mainboard/supermicro/x10slm-f/romstage.c
@@ -22,51 +22,51 @@ void mainboard_config_rcba(void)
 
 void mainboard_fill_pei_data(struct pei_data *pei_data)
 {
-	struct pei_data mainboard_pei_data = {
-		.pei_version = PEI_VERSION,
-		.mchbar = (uintptr_t)DEFAULT_MCHBAR,
-		.dmibar = (uintptr_t)DEFAULT_DMIBAR,
-		.epbar = DEFAULT_EPBAR,
-		.pciexbar = CONFIG_MMCONF_BASE_ADDRESS,
-		.smbusbar = SMBUS_IO_BASE,
-		.hpet_address = HPET_ADDR,
-		.rcba = (uintptr_t)DEFAULT_RCBA,
-		.pmbase = DEFAULT_PMBASE,
-		.gpiobase = DEFAULT_GPIOBASE,
-		.temp_mmio_base = 0xfed08000,
-		.system_type = 1, /* desktop/server */
-		.tseg_size = CONFIG_SMM_TSEG_SIZE,
-		.spd_addresses = { 0xa0, 0xa2, 0xa4, 0xa6 },
-		.ec_present = 0,
-		.ddr_refresh_2x = 1,
-		.max_ddr3_freq = 1600,
-		.usb2_ports = {
-			/* Length, Enable, OCn#, Location */
-			{ 0x0040, 1, 0, USB_PORT_INTERNAL },
-			{ 0x0040, 1, 0, USB_PORT_BACK_PANEL },
-			{ 0x0110, 1, 1, USB_PORT_BACK_PANEL },
-			{ 0x0110, 1, 1, USB_PORT_BACK_PANEL },
-			{ 0x0110, 1, 2, USB_PORT_BACK_PANEL },
-			{ 0x0110, 1, 2, USB_PORT_BACK_PANEL },
-			{ 0x0040, 0, USB_OC_PIN_SKIP, USB_PORT_SKIP },
-			{ 0x0040, 0, USB_OC_PIN_SKIP, USB_PORT_SKIP },
-			{ 0x0040, 1, 4, USB_PORT_BACK_PANEL },
-			{ 0x0040, 1, 4, USB_PORT_BACK_PANEL },
-			{ 0x0040, 0, USB_OC_PIN_SKIP, USB_PORT_SKIP },
-			{ 0x0040, 0, USB_OC_PIN_SKIP, USB_PORT_SKIP },
-			{ 0x0040, 1, 6, USB_PORT_BACK_PANEL },
-			{ 0x0040, 1, 6, USB_PORT_BACK_PANEL },
-		},
-		.usb3_ports = {
-			/* Enable, OCn# */
-			{ 1, 1 },
-			{ 1, 1 },
-			{ 0, USB_OC_PIN_SKIP },
-			{ 0, USB_OC_PIN_SKIP },
-			{ 1, 3 },
-			{ 1, 3 },
-		},
+	pei_data->system_type = 1; /* Desktop/Server */
+	pei_data->spd_addresses[0] = 0xa0;
+	pei_data->spd_addresses[1] = 0xa2;
+	pei_data->spd_addresses[2] = 0xa4;
+	pei_data->spd_addresses[3] = 0xa6;
+	pei_data->ec_present = 0;
+	/*
+	 * 0 = leave channel enabled
+	 * 1 = disable dimm 0 on channel
+	 * 2 = disable dimm 1 on channel
+	 * 3 = disable dimm 0+1 on channel
+	 */
+	pei_data->dimm_channel0_disabled = 0;
+	pei_data->dimm_channel1_disabled = 0;
+	pei_data->ddr_refresh_2x = 1;
+	pei_data->max_ddr3_freq = 1600;
+
+	struct usb2_port_setting usb2_ports[MAX_USB2_PORTS] = {
+		/* Length, Enable, OCn#, Location */
+		{ 0x0040, 1, 0, USB_PORT_INTERNAL },
+		{ 0x0040, 1, 0, USB_PORT_BACK_PANEL },
+		{ 0x0110, 1, 1, USB_PORT_BACK_PANEL },
+		{ 0x0110, 1, 1, USB_PORT_BACK_PANEL },
+		{ 0x0110, 1, 2, USB_PORT_BACK_PANEL },
+		{ 0x0110, 1, 2, USB_PORT_BACK_PANEL },
+		{ 0x0040, 0, USB_OC_PIN_SKIP, USB_PORT_SKIP },
+		{ 0x0040, 0, USB_OC_PIN_SKIP, USB_PORT_SKIP },
+		{ 0x0040, 1, 4, USB_PORT_BACK_PANEL },
+		{ 0x0040, 1, 4, USB_PORT_BACK_PANEL },
+		{ 0x0040, 0, USB_OC_PIN_SKIP, USB_PORT_SKIP },
+		{ 0x0040, 0, USB_OC_PIN_SKIP, USB_PORT_SKIP },
+		{ 0x0040, 1, 6, USB_PORT_BACK_PANEL },
+		{ 0x0040, 1, 6, USB_PORT_BACK_PANEL },
 	};
 
-	*pei_data = mainboard_pei_data; /* FIXME: Do not overwrite everything */
+	struct usb3_port_setting usb3_ports[MAX_USB3_PORTS] = {
+		/* Enable, OCn# */
+		{ 1, 1 },
+		{ 1, 1 },
+		{ 0, USB_OC_PIN_SKIP },
+		{ 0, USB_OC_PIN_SKIP },
+		{ 1, 3 },
+		{ 1, 3 },
+	};
+
+	memcpy(pei_data->usb2_ports, usb2_ports, sizeof(usb2_ports));
+	memcpy(pei_data->usb3_ports, usb3_ports, sizeof(usb3_ports));
 }

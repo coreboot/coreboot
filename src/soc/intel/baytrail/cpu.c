f2c13bd905ae62508f2f929d047822d4b234a303
Mate Kukri <kukri.mate@gmail.com>
2020-07-07 17:23:18 +0200

    soc/intel/baytrail: Add new CPUID 0x30679
    This ID is reported by newer mfg date SOCs. Needed for newer GBYT4 boards.
    
    Change-Id: I6af746d66a15f67553de1dc1c925e5cb0b181898
    Signed-off-by: Mate Kukri <kukri.mate@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/43180
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/soc/intel/baytrail/cpu.c
+++ src/soc/intel/baytrail/cpu.c
@@ -60,6 +60,7 @@ static struct device_operations cpu_dev_ops = {
 static const struct cpu_device_id cpu_table[] = {
 	{ X86_VENDOR_INTEL, 0x30673 },
 	{ X86_VENDOR_INTEL, 0x30678 },
+	{ X86_VENDOR_INTEL, 0x30679 },
 	{ 0, 0 },
 };
 

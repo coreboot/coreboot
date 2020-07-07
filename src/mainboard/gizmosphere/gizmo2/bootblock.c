1b85f18a977589ca084f13ee1136311802b6a495
Angel Pons <th3fanbus@gmail.com>
2020-07-07 23:40:48 +0200

    mb/gizmosphere/gizmo2: Drop dead code
    This code is not even being build-tested. Drop it before it grows moss.
    
    Change-Id: Icc3f9a4f71001547ef3d1efe6fc7551b5c690f92
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/43237
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Michael Niewöhner

--- src/mainboard/gizmosphere/gizmo2/bootblock.c
+++ src/mainboard/gizmosphere/gizmo2/bootblock.c
@@ -5,16 +5,4 @@
 
 void bootblock_mainboard_early_init(void)
 {
-#if 0
-	volatile u32 i, val;
-
-	/* LPC clock? Should happen before enable_serial. */
-
-	/*
-	* On Larne, after LpcClkDrvSth is set, it needs some time to be stable,
-	* because of the buffer ICS551M
-	*/
-	for (i = 0; i < 200000; i++)
-		val = inb(0xcd6);
-#endif
 }

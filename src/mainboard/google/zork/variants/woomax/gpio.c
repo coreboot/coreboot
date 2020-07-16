bfd6521ce71b67a619da221dce7070db3959533b
Kane Chen <kane_chen@pegatron.corp-partner.google.com>
2020-07-16 13:36:20 +0800

    mb/google/zork: Modify Woomax variant
    Update Woomax configuration including GPIO, memory SPD table, I2C devices
    and USB type C.
    
    BUG=b:158343602
    BRANCH=None
    TEST=emerge-zork coreboot
    
    Signed-off-by: Kane Chen <kane_chen@pegatron.corp-partner.google.com>
    Change-Id: I55ba995d9438551d45cb9e17f92b5089ccf4a5fd
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/43497
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

---
+++ src/mainboard/google/zork/variants/woomax/gpio.c
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/variants.h>
+#include <soc/gpio.h>
+
+static const struct soc_amd_gpio woomax_gpio_set_stage_ram[] = {
+	/* GPIO_4 NC */
+	PAD_NC(GPIO_4),
+	/* GPIO_5 NC */
+	PAD_NC(GPIO_5),
+	/* GPIO_6 NC */
+	PAD_NC(GPIO_6),
+	/* GPIO_11 NC */
+	PAD_NC(GPIO_11),
+	/* GPIO_32 NC */
+	PAD_NC(GPIO_32),
+	/* GPIO_69 NC */
+	PAD_NC(GPIO_69),
+	/* RAM_ID_4  */
+	PAD_NC(GPIO_84),
+	/* GPIO_141 NC */
+	PAD_NC(GPIO_141),
+	/* GPIO_143 NC */
+	PAD_NC(GPIO_143),
+};
+
+const struct soc_amd_gpio *variant_override_gpio_table(size_t *size)
+{
+	*size = ARRAY_SIZE(woomax_gpio_set_stage_ram);
+	return woomax_gpio_set_stage_ram;
+}

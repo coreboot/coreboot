4907e62893fb70e26f2d92b8de572b4767771812
Paul Menzel <pmenzel@molgen.mpg.de>
2020-07-19 10:01:04 +0200

    ec/lenovo/h8: Align macro values in one column
    Change-Id: I5691a582d9a195317994413fff4fd3273413b5fe
    Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/43586
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/ec/lenovo/h8/h8.h
+++ src/ec/lenovo/h8/h8.h
@@ -69,16 +69,16 @@ void h8_mb_init(void);
 #define H8_TRACKPOINT_OFF		0x02
 #define H8_TRACKPOINT_ON		0x03
 
-#define H8_LED_CONTROL		0x0c
+#define H8_LED_CONTROL			0x0c
 #define H8_LED_CONTROL_OFF		0x00
 #define H8_LED_CONTROL_ON		0x80
 #define H8_LED_CONTROL_PULSE		0xa0	/* Some models, power LED only*/
 #define H8_LED_CONTROL_BLINK		0xc0
 
 #define H8_LED_CONTROL_POWER_LED	0x00
-#define H8_LED_CONTROL_BAT0_LED	0x01
-#define H8_LED_CONTROL_BAT1_LED	0x02
-#define H8_LED_CONTROL_UBAY_LED	0x04
+#define H8_LED_CONTROL_BAT0_LED		0x01
+#define H8_LED_CONTROL_BAT1_LED		0x02
+#define H8_LED_CONTROL_UBAY_LED		0x04
 #define H8_LED_CONTROL_SUSPEND_LED	0x07
 #define H8_LED_CONTROL_DOCK_LED1	0x08
 #define H8_LED_CONTROL_DOCK_LED2	0x09
@@ -95,51 +95,51 @@ void h8_mb_init(void);
 #define H8_VOLUME_CONTROL		0x30
 #define H8_VOLUME_CONTROL_MUTE		0x40
 /* Embedded controller events */
-#define H8_EVENT_FN_F1	0x10
-#define H8_EVENT_FN_F2	0x11
-#define H8_EVENT_FN_F3	0x12
-#define H8_EVENT_FN_F4	0x13
-#define H8_EVENT_FN_HOME	0x14
-#define H8_EVENT_FN_END	0x15
-#define H8_EVENT_FN_F7	0x16
-#define H8_EVENT_FN_F8	0x17
-#define H8_EVENT_FN_F9	0x18
+#define H8_EVENT_FN_F1			0x10
+#define H8_EVENT_FN_F2			0x11
+#define H8_EVENT_FN_F3			0x12
+#define H8_EVENT_FN_F4			0x13
+#define H8_EVENT_FN_HOME		0x14
+#define H8_EVENT_FN_END			0x15
+#define H8_EVENT_FN_F7			0x16
+#define H8_EVENT_FN_F8			0x17
+#define H8_EVENT_FN_F9			0x18
 #define H8_EVENT_FN_THINKVANTAGE	0x19
-#define H8_EVENT_FN_F11	0x1a
-#define H8_EVENT_FN_F12	0x1b
-#define H8_EVENT_FN_1		0x1c
-#define H8_EVENT_FN_2		0x1d
-#define H8_EVENT_FN_PGUP	0x1f
+#define H8_EVENT_FN_F11			0x1a
+#define H8_EVENT_FN_F12			0x1b
+#define H8_EVENT_FN_1			0x1c
+#define H8_EVENT_FN_2			0x1d
+#define H8_EVENT_FN_PGUP		0x1f
 
-#define H8_EVENT_AC_ON	0x26
-#define H8_EVENT_AC_OFF	0x27
+#define H8_EVENT_AC_ON			0x26
+#define H8_EVENT_AC_OFF			0x27
 
-#define H8_EVENT_PWRSW_PRESS	0x28
-#define H8_EVENT_PWRSW_RELEASE 0x29
+#define H8_EVENT_PWRSW_PRESS		0x28
+#define H8_EVENT_PWRSW_RELEASE		0x29
 
-#define H8_EVENT_LIDSW_CLOSE	0x2a
-#define H8_EVENT_LIDSW_PUSH	0x2b
+#define H8_EVENT_LIDSW_CLOSE		0x2a
+#define H8_EVENT_LIDSW_PUSH		0x2b
 
-#define H8_EVENT_UBAY_UNLOCK	0x2c
-#define H8_EVENT_UBAY_LOCK	0x2d
+#define H8_EVENT_UBAY_UNLOCK		0x2c
+#define H8_EVENT_UBAY_LOCK		0x2d
 
-#define H8_EVENT_KEYPRESS	0x33
+#define H8_EVENT_KEYPRESS		0x33
 
-#define H8_EVENT_FN_PRESS	0x39
+#define H8_EVENT_FN_PRESS		0x39
 
-#define H8_STATUS0		0x46
-#define H8_STATUS0_FN_KEY_DOWN	0x01
-#define H8_STATUS1		0x47
-#define H8_STATUS2		0x48
-#define H8_STATUS3		0x49
+#define H8_STATUS0			0x46
+#define H8_STATUS0_FN_KEY_DOWN		0x01
+#define H8_STATUS1			0x47
+#define H8_STATUS2			0x48
+#define H8_STATUS3			0x49
 
-#define H8_EVENT_BAT0		0x4a
-#define H8_EVENT_BAT0_STATE	0x4b
+#define H8_EVENT_BAT0			0x4a
+#define H8_EVENT_BAT0_STATE		0x4b
 
-#define H8_EVENT_BAT1		0x4c
-#define H8_EVENT_BAT1_STATE	0x4d
+#define H8_EVENT_BAT1			0x4c
+#define H8_EVENT_BAT1_STATE		0x4d
 
-#define H8_EVENT_FN_F5 0x64
-#define H8_EVENT_FN_F6 0x65
+#define H8_EVENT_FN_F5			0x64
+#define H8_EVENT_FN_F6			0x65
 
 #endif /* EC_LENOVO_H8_H */

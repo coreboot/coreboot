81bebc8374607e2376efdba09553184db1a9957d
Elyes HAOUAS <ehaouas@noos.fr>
2020-07-22 13:17:18 +0200

    payloads/libpayload/curses/menu/eti.h: Remove unneeded whitespaces
    Change-Id: Ia77c5be4b22740f88fb9c11bff95036adbd8145f
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/43715
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- payloads/libpayload/curses/menu/eti.h
+++ payloads/libpayload/curses/menu/eti.h
@@ -36,19 +36,19 @@
 #define NCURSES_ETI_H_incl 1
 
 #define	E_OK			(0)
-#define	E_SYSTEM_ERROR	 	(-1)
-#define	E_BAD_ARGUMENT	 	(-2)
-#define	E_POSTED	 	(-3)
-#define	E_CONNECTED	 	(-4)
-#define	E_BAD_STATE	 	(-5)
-#define	E_NO_ROOM	 	(-6)
+#define	E_SYSTEM_ERROR		(-1)
+#define	E_BAD_ARGUMENT		(-2)
+#define	E_POSTED		(-3)
+#define	E_CONNECTED		(-4)
+#define	E_BAD_STATE		(-5)
+#define	E_NO_ROOM		(-6)
 #define	E_NOT_POSTED		(-7)
 #define	E_UNKNOWN_COMMAND	(-8)
 #define	E_NO_MATCH		(-9)
 #define	E_NOT_SELECTABLE	(-10)
-#define	E_NOT_CONNECTED	        (-11)
+#define	E_NOT_CONNECTED		(-11)
 #define	E_REQUEST_DENIED	(-12)
-#define	E_INVALID_FIELD	        (-13)
+#define	E_INVALID_FIELD		(-13)
 #define	E_CURRENT		(-14)
 
 #endif

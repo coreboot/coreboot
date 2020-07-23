6c0ed003672acb49d819f264ef1135894722d55e
Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
2020-07-23 12:03:17 +0800

    mb/google/dedede/var/madoo: Configure GPIO for Madoo
    Follow schematic to modify some GPIO pins.
    
    GPP_D12 - NC Pin
    GPP_D13 - NC Pin
    GPP_D14 - NC Pin
    GPP_D15 - NC Pin
    GPP_E0  - NC Pin
    GPP_E2  - NC Pin
    GPP_H6  - NC Pin
    GPP_H7  - NC Pin
    GPP_S02 - NC Pin
    GPP_S03 - NC Pin
    
    BUG=b:161407664
    BRANCH=NONE
    TEST=Build the coreboot image on madoo board.
    
    Signed-off-by: Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
    Change-Id: I85aadfb0d020055eec921c7646c16ae6c95a606f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/43745
    Reviewed-by: EricR Lai <ericr_lai@compal.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/dedede/variants/madoo/gpio.c
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+
+/* Pad configuration in ramstage */
+static const struct pad_config override_gpio_table[] = {
+	/* D12 : WCAM_RST_L */
+	PAD_NC(GPP_D12, NONE),
+	/* D13 : EN_PP2800_CAMERA */
+	PAD_NC(GPP_D13, NONE),
+	/* D14 : EN_PP1200_CAMERA */
+	PAD_NC(GPP_D14, NONE),
+	/* D15 : UCAM_RST_L */
+	PAD_NC(GPP_D15, NONE),
+	/* E0  : CLK_24M_UCAM */
+	PAD_NC(GPP_E0, NONE),
+	/* E2  : CLK_24M_WCAM */
+	PAD_NC(GPP_E2, NONE),
+	/* H6  : AP_I2C_CAM_SDA */
+	PAD_NC(GPP_H6, NONE),
+	/* H7  : AP_I2C_CAM_SCL */
+	PAD_NC(GPP_H7, NONE),
+	/* S2  : DMIC1_CLK */
+	PAD_NC(GPP_S2, NONE),
+	/* S3  : DMIC1_DATA */
+	PAD_NC(GPP_S3, NONE),
+};
+
+const struct pad_config *variant_override_gpio_table(size_t *num)
+{
+	*num = ARRAY_SIZE(override_gpio_table);
+	return override_gpio_table;
+}

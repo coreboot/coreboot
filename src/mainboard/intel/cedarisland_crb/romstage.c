a9713c11c07590eb0ba6afd7bd341f349b695d12
Maxim Polyakov <max.senia.poliak@gmail.com>
2020-07-26 13:12:49 +0300

    mb/intel/cedarisland: Remove duplicated code
    Some UPD options are already set in `xeon_sp/cpx/romstage.c`. Remove
    them from the board configuration to avoid duplicating this code.
    
    Change-Id: Ic79245103c33427e06c7ea881be778e3d219c45f
    Signed-off-by: Maxim Polyakov <max.senia.poliak@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/43924
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/intel/cedarisland_crb/romstage.c
+++ src/mainboard/intel/cedarisland_crb/romstage.c
@@ -6,9 +6,5 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
 {
 	FSP_M_CONFIG *m_cfg = &mupd->FspmConfig;
 
-	m_cfg->BoardId = 0x1d;
-	m_cfg->BoardTypeBitmask = 0x11111111;
-	m_cfg->DebugPrintLevel = 8;
 	m_cfg->KtiLinkSpeedMode = 0;
-	m_cfg->KtiPrefetchEn = 2;
 }

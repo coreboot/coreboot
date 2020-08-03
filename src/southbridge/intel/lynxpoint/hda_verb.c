eaf2ae764e77991844e6d1f0b2f2111a5e7c9621
Elyes HAOUAS <ehaouas@noos.fr>
2020-08-03 15:33:49 +0200

    sb/intel/lynxpoint: Use <device/azalia_device.h> registers
    Change-Id: Ib4929e3213676056ff3f8116d226fd38132baa28
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/44126
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/southbridge/intel/lynxpoint/hda_verb.c
+++ src/southbridge/intel/lynxpoint/hda_verb.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <console/console.h>
+#include <device/azalia_device.h>
 #include <device/mmio.h>
 #include <delay.h>
 #include "pch.h"
--- src/southbridge/intel/lynxpoint/hda_verb.h
+++ src/southbridge/intel/lynxpoint/hda_verb.h
@@ -3,15 +3,7 @@
 #ifndef LYNXPOINT_HDA_VERB_H
 #define LYNXPOINT_HDA_VERB_H
 
-#define HDA_GCAP_REG		0x00
-#define HDA_GCTL_REG		0x08
-#define   HDA_GCTL_CRST		(1 << 0)
-#define HDA_STATESTS_REG	0x0e
-#define HDA_IC_REG		0x60
-#define HDA_IR_REG		0x64
-#define HDA_ICII_REG		0x68
-#define   HDA_ICII_BUSY		(1 << 0)
-#define   HDA_ICII_VALID	(1 << 1)
+#include <stdint.h>
 
 int hda_codec_detect(u8 *base);
 int hda_codec_write(u8 *base, u32 size, const u32 *data);

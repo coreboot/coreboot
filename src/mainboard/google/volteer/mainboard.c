1d8494116f652c2dd833b8495b24066c99d4b59d
Tim Wawrzynczak <twawrzynczak@chromium.org>
2020-08-25 16:50:29 -0600

    mb/google/volteer: Add probed fw_configs to SMBIOS OEM strings
    Some Linux kernel drivers bind to "DMI quirks." In this case, the audio
    fw_config is added as an OEM string, e.g., "AUDIO-MAX98357_ALC5682I_I2S"
    so the audio topology can be correctly discovered.
    But add all successfully probed fw_config items as well, because this
    makes it easier to view what is selected from userspace.
    
    BUG=b:161963281
    TEST=With CBI FW_CONFIG field set to 0x201:
    
    localhost ~ # dmidecode -t 11
    # dmidecode 3.2
    Getting SMBIOS data from sysfs.
    SMBIOS 3.0 present.
    
    Handle 0x0009, DMI type 11, 5 bytes
    OEM Strings
            String 1: DB_USB-USB4_GEN2
            String 2: AUDIO-MAX98373_ALC5682I_I2S
    
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Change-Id: I7b7586b0ebfe7b2fd888f448a50ae086364fa718
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/44783
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/mainboard/google/volteer/mainboard.c
+++ src/mainboard/google/volteer/mainboard.c
@@ -5,6 +5,7 @@
 #include <baseboard/variants.h>
 #include <device/device.h>
 #include <ec/ec.h>
+#include <fw_config.h>
 #include <soc/gpio.h>
 #include <vendorcode/google/chromeos/chromeos.h>
 #include <variant/gpio.h>
@@ -14,10 +15,27 @@ static void mainboard_init(struct device *dev)
 	mainboard_ec_init();
 }
 
+static void add_fw_config_oem_string(const struct fw_config *config, void *arg)
+{
+	struct smbios_type11 *t;
+	char buffer[64];
+
+	t = (struct smbios_type11 *)arg;
+
+	snprintf(buffer, sizeof(buffer), "%s-%s", config->field_name, config->option_name);
+	t->count = smbios_add_string(t->eos, buffer);
+}
+
+static void mainboard_smbios_strings(struct device *dev, struct smbios_type11 *t)
+{
+	fw_config_for_each_found(add_fw_config_oem_string, t);
+}
+
 static void mainboard_enable(struct device *dev)
 {
 	dev->ops->init = mainboard_init;
 	dev->ops->acpi_inject_dsdt = chromeos_dsdt_generator;
+	dev->ops->get_smbios_strings = mainboard_smbios_strings;
 }
 
 static void mainboard_chip_init(void *chip_info)

b91e5180626bcbb90fe69f23c08511aeff58c5d7
Kevin Chiu <Kevin.Chiu@quantatw.com>
2020-08-28 11:56:31 +0800

    mb/google/zork: update GPIO config for dirinboz
    dirinboz does not support stylus, config AGPIO4/5 to NC
    to prevent unexpected wake event for s3.
    
    BUG=none
    BRANCH=zork
    TEST=emerge-zork coreboot chromeos-bootimage
    
    Change-Id: I3cfdeb326c3d3775148b2a00732c7d848dab35cb
    Signed-off-by: Kevin Chiu <kevin.chiu@quantatw.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/44894
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/zork/variants/dirinboz/gpio.c
+++ src/mainboard/google/zork/variants/dirinboz/gpio.c
@@ -8,12 +8,23 @@
 #include <ec/google/chromeec/ec.h>
 
 static const struct soc_amd_gpio bid_1_gpio_set_stage_ram[] = {
+	/* PEN_DETECT_ODL - no used */
+	PAD_NC(GPIO_4),
+	/* PEN_POWER_EN - no used */
+	PAD_NC(GPIO_5),
 	/* TP */
 	PAD_NC(GPIO_32),
 	/* EN_DEV_BEEP_L */
 	PAD_GPO(GPIO_89, HIGH),
 	/* USI_RESET */
 	PAD_GPO(GPIO_140, HIGH),
+};
+
+static const struct soc_amd_gpio dirinboz_gpio_set_stage_ram[] = {
+	/* PEN_DETECT_ODL - no used */
+	PAD_NC(GPIO_4),
+	/* PEN_POWER_EN - no used */
+	PAD_NC(GPIO_5),
 };
 
 const struct soc_amd_gpio *variant_override_gpio_table(size_t *size)
@@ -33,6 +44,6 @@ const struct soc_amd_gpio *variant_override_gpio_table(size_t *size)
 		return bid_1_gpio_set_stage_ram;
 	}
 
-	*size = 0;
-	return NULL;
+	*size = ARRAY_SIZE(dirinboz_gpio_set_stage_ram);
+	return dirinboz_gpio_set_stage_ram;
 }

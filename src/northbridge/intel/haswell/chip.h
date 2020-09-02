d04957970cc55743db3d896b3975570b42f05d95
Angel Pons <th3fanbus@gmail.com>
2020-09-02 19:01:24 +0200

    nb/intel/haswell: Drop `gpu_panel_port_select`
    The corresponding bits in PP_ON_DELAYS are reserved MBZ.
    
    Change-Id: Icd2554c928a5908dfb354b81d3e6c5b5f242f1d1
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45034
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/northbridge/intel/haswell/chip.h
+++ src/northbridge/intel/haswell/chip.h
@@ -17,7 +17,6 @@ struct northbridge_intel_haswell_config {
 	u8 gpu_dp_c_hotplug; /* Digital Port C Hotplug Config */
 	u8 gpu_dp_d_hotplug; /* Digital Port D Hotplug Config */
 
-	u8 gpu_panel_port_select; /* 0=LVDS 1=DP_B 2=DP_C 3=DP_D */
 	u8 gpu_panel_power_cycle_delay;          /* T4 time sequence */
 	u16 gpu_panel_power_up_delay;            /* T1+T2 time sequence */
 	u16 gpu_panel_power_down_delay;          /* T3 time sequence */

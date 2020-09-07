9209817acedd6db0369249ce094761df252f786d
Subrata Banik <subrata.banik@intel.com>
2020-09-07 16:20:53 +0530

    pci_ids: Add Alder Lake DTT PCI IDs
    Add PCI IDs for Intel's Dynamic Tuning Technology (DTT) for ADL.
    
    Also add NULL terminator at end of pci_device_ids.
    
    Change-Id: If25b1f562567a833683b0b8796bd1d6cac0bd490
    Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45140
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/common/block/dtt/dtt.c
+++ src/soc/intel/common/block/dtt/dtt.c
@@ -8,6 +8,8 @@ static const unsigned short pci_device_ids[] = {
 	PCI_DEVICE_ID_INTEL_CML_DTT,
 	PCI_DEVICE_ID_INTEL_TGL_DTT,
 	PCI_DEVICE_ID_INTEL_JSL_DTT,
+	PCI_DEVICE_ID_INTEL_ADL_DTT,
+	0
 };
 
 static struct device_operations dptf_dev_ops = {

12beaea5e21e0e4db2608840cc78b8bee04320c8
Angel Pons <th3fanbus@gmail.com>
2020-09-10 01:56:03 +0200

    superio/nuvoton: Inline `nuvoton_hwm_select_bank`
    There's no need to place a single-line function in its own compilation
    unit, and then guard it behind a Kconfig symbol. This also allows using
    this function in stages other than ramstage.
    
    Change-Id: I103a4ea4cef24844d382854c9358bbb37d229e04
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/42130
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/superio/nuvoton/common/hwm.h
+++ src/superio/nuvoton/common/hwm.h
@@ -6,7 +6,13 @@
 /* Nuvoton is a Winbond spin-off, so this code is for both */
 
 #include <stdint.h>
+#include <superio/hwm5_conf.h>
 
-void nuvoton_hwm_select_bank(const u16 base, const u8 bank);
+#define HWM_BANK_SELECT	0x4e
+
+static inline void nuvoton_hwm_select_bank(const u16 base, const u8 bank)
+{
+	pnp_write_hwm5_index(base, HWM_BANK_SELECT, bank);
+}
 
 #endif /* SUPERIO_NUVOTON_COMMON_HWM_H */

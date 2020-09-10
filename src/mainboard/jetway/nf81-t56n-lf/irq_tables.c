4865983a70f6ba46c4acb8ffb660bd78a848f840
Elyes HAOUAS <ehaouas@noos.fr>
2020-09-10 11:39:15 +0200

    mb/jetway: Drop unneeded empty lines
    Change-Id: Id9837eda69d725539a82b3c98f63a57240051c5b
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45243
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>

--- src/mainboard/jetway/nf81-t56n-lf/irq_tables.c
+++ src/mainboard/jetway/nf81-t56n-lf/irq_tables.c
@@ -27,7 +27,6 @@ static void write_pirq_info(struct irq_info *pirq_info, u8 bus, u8 devfn,
 
 unsigned long write_pirq_routing_table(unsigned long addr)
 {
-
 	struct irq_routing_table *pirq;
 	struct irq_info *pirq_info;
 	u32 slot_num;

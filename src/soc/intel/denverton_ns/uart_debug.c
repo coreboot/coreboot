4b58d14fa7f20e037e58ef11850784aa97a1cd96
Felix Held <felix-coreboot@felixheld.de>
2020-09-11 15:54:30 +0200

    soc/intel/denverton_ns/uart_debug: include header for uart_platform_base
    Include console/uart.h for the declaration of uart_platform_base instead
    of declaring the function in the source file.
    
    Change-Id: Ib72d8884f27e93cec058dbcda404dd6908de1981
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45295
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/soc/intel/denverton_ns/uart_debug.c
+++ src/soc/intel/denverton_ns/uart_debug.c
@@ -1,14 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <stdint.h>
+#include <console/uart.h>
 #include <device/pci_def.h>
 #include <device/pci_ops.h>
 #include <soc/uart.h>
 
 #define MY_PCI_DEV(SEGBUS, DEV, FN) \
 	((((SEGBUS)&0xFFF) << 20) | (((DEV)&0x1F) << 15) | (((FN)&0x07) << 12))
-
-uintptr_t uart_platform_base(unsigned int idx);
 
 uintptr_t uart_platform_base(unsigned int idx)
 {

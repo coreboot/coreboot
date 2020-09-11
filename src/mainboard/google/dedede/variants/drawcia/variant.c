aa03f30e6ecd999cff5b0ad0f166d2ed7471b62b
Karthikeyan Ramasubramanian <kramasub@google.com>
2020-09-11 10:12:31 -0600

    mb/google/dedede/var/drawcia: Remove debug statement with NULL pointer
    The debug statement to print WiFi SAR file can potentially have a NULL
    pointer. Also the debug statement does not add much value. Hence remove the
    debug statement.
    
    BUG=b:165613510
    TEST=Build and boot the drawcia board to OS.
    
    Change-Id: I710240f5e965f523fb8ac55a67880e1cbf9abd48
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45298
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/mainboard/google/dedede/variants/drawcia/variant.c
+++ src/mainboard/google/dedede/variants/drawcia/variant.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootstate.h>
-#include <console/console.h>
 #include <fw_config.h>
 #include <sar.h>
 
@@ -12,6 +11,5 @@ const char *get_wifi_sar_cbfs_filename(void)
 	if (fw_config_probe(FW_CONFIG(TABLETMODE, TABLETMODE_ENABLED)))
 		filename = "wifi_sar-drawcia.hex";
 
-	printk(BIOS_INFO, "SAR file name: %s\n", filename);
 	return filename;
 }

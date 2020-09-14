362a1568670851cf25117d5660504dfb1d1b6331
Sam Lewis <sam.vr.lewis@gmail.com>
2020-09-14 21:04:35 +1000

    arch/arm: Add support for loading Linux kernels
    Adds support for loading Linux kernels through FIT payloads. This has
    been implemented as an assembly function in order to simplify dealing
    with some of the intricacies of loading a kernel (such as needing to
    jump to the kernel in ARM mode and the kernel ABI).
    
    TEST: Booted a FIT image containing a 5.4 kernel and initramfs on the
    Beaglebone Black.
    
    Change-Id: I7dbf9467665ec17447cec73676763844b4be4764
    Signed-off-by: Sam Lewis <sam.vr.lewis@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45335
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- src/arch/arm/boot.c
+++ src/arch/arm/boot.c
@@ -1,7 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <cbfs.h>
 #include <arch/cache.h>
 #include <program_loading.h>
+
+void boot_linux(void *kernel_ptr, void *fdt_ptr);
 
 void arch_prog_run(struct prog *prog)
 {
@@ -9,6 +12,21 @@ void arch_prog_run(struct prog *prog)
 
 	cache_sync_instructions();
 
-	doit = prog_entry(prog);
-	doit(prog_entry_arg(prog));
+	switch (prog_cbfs_type(prog)) {
+	case CBFS_TYPE_FIT:
+		/*
+		 * We only load Linux payloads from the ramstage, so provide a hint to
+		 * the linker that the below functions do not need to be included in
+		 * earlier stages.
+		 */
+		if (!ENV_RAMSTAGE)
+			break;
+
+		dcache_mmu_disable();
+		boot_linux(prog_entry(prog), prog_entry_arg(prog));
+		break;
+	default:
+		doit = prog_entry(prog);
+		doit(prog_entry_arg(prog));
+	}
 }

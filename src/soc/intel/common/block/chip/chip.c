7938ebc7d7f1ee8a048b76f24d812a64f1bb5324
Subrata Banik <subrata.banik@intel.com>
2020-09-15 16:10:25 +0530

    soc/intel/common/block/chip: Refactor chip_get_common_soc_structure()
    Found-by: Klocwork, Pointer soc_config is used uninitialized.
    
    Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Change-Id: I7e2aa4ef23a68a2ec2ba9d55cf890a7f81e3e278
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45416
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/soc/intel/common/block/chip/chip.c
+++ src/soc/intel/common/block/chip/chip.c
@@ -1,15 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <soc/pci_devs.h>
 #include <soc/soc_chip.h>
 
 const struct soc_intel_common_config *chip_get_common_soc_structure(void)
 {
-	const struct soc_intel_common_config *soc_config;
-	const config_t *config;
-
-	config = config_of_soc();
-	soc_config = &config->common_soc_config;
-
-	return soc_config;
+	return &((config_t *)config_of_soc())->common_soc_config;
 }

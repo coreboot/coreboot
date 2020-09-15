b566ce4aea2473274e1e59ac9d25b316080323f3
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-09-15 17:01:57 +0200

    mb/raptor-cs/talos-2: add basic mainboard structure
    Change-Id: I0c4f74c7b27c8bb5599d68305adf369ddc6fcc70
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/67063
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>

---
+++ src/mainboard/raptor-cs/talos-2/mainboard.c
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <console/console.h>
+#include <device/device.h>
+
+static void mainboard_enable(struct device *dev)
+{
+	if (!dev)
+		die("No dev0; die\n");
+}
+
+struct chip_operations mainboard_ops = {
+	.enable_dev = mainboard_enable,
+};

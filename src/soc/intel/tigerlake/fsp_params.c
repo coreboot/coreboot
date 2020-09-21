c16fc8a49c4bae8da2b496dd8a59056ac57483f9
John Zhao <john.zhao@intel.com>
2020-09-21 13:10:11 -0700

    soc/intel/tigerlake: Add support for CnviBtCore and CnviBtAudioOffload
    This change adds configuration support for both of CnviBtCore and
    CnviBtAudioOffload.
    
    BUG=b:169045123
    TEST=Built and boot up to kernel on Volteer.
    
    Signed-off-by: John Zhao <john.zhao@intel.com>
    Change-Id: Id4bf41f07c4a53de17e9eb91a8ddfb1083cbf83e
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45585
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Sukumar Ghorai <sukumar.ghorai@intel.com>
    Reviewed-by: Naveen M <naveen.m@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/tigerlake/fsp_params.c
+++ src/soc/intel/tigerlake/fsp_params.c
@@ -270,6 +270,13 @@ void platform_fsp_silicon_init_params_cb(FSPS_UPD *supd)
 	dev = pcidev_path_on_root(PCH_DEVFN_CNVI_WIFI);
 	params->CnviMode = is_dev_enabled(dev);
 
+	/* CNVi BT Core */
+	dev = pcidev_path_on_root(PCH_DEVFN_CNVI_BT);
+	params->CnviBtCore = is_dev_enabled(dev);
+
+	/* CNVi BT Audio Offload */
+	params->CnviBtAudioOffload = config->CnviBtAudioOffload;
+
 	/* VMD */
 	dev = pcidev_path_on_root(SA_DEVFN_VMD);
 	params->VmdEnable = is_dev_enabled(dev);

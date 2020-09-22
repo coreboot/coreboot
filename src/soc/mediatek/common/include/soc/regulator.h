cbbdcb330d7885824623014e6e589a4eebe9d9d4
Yidi Lin <yidi.lin@mediatek.com>
2020-09-22 19:45:41 +0800

    soc/mediatek/mt8192: Add board-specific regulator APIs
    To enable DVFS, DRAM driver needs to access four different
    regulators that SoC can't access directly and need board-specific
    implementations.
    
    To support that we need to define the getter and setter APIs for
    those regulators.
    
    BUG=b:147789962
    BRANCH=none
    TEST=verified with DRAM driver
    
    Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Change-Id: I0c2d471a7f8628735af90c5b5a5ab3012831e442
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/46405
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/soc/mediatek/common/include/soc/regulator.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SOC_MEDIATEK_COMMON_REGULATOR_H
+#define SOC_MEDIATEK_COMMON_REGULATOR_H
+
+#include <stdint.h>
+
+enum mtk_regulator {
+	MTK_REGULATOR_VDD1,
+	MTK_REGULATOR_VDD2,
+	MTK_REGULATOR_VDDQ,
+	MTK_REGULATOR_VMDDR,
+};
+
+void mainboard_set_regulator_vol(enum mtk_regulator regulator,
+				 uint32_t voltage_uv);
+uint32_t mainboard_get_regulator_vol(enum mtk_regulator regulator);
+
+#endif /* SOC_MEDIATEK_COMMON_REGULATOR_H */

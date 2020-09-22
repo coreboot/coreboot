1bdbcd751028799105c4288e86d600d2d0aa9428
Felix Singer <felix.singer@secunet.com>
2020-09-22 12:55:25 +0200

    azalia_device.h: Add new macro to configure pins as NC
    Change-Id: I740d0d756599688165458a9c6e925d5d94754bb2
    Signed-off-by: Felix Singer <felix.singer@secunet.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45604
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/include/device/azalia_device.h
+++ src/include/device/azalia_device.h
@@ -128,6 +128,8 @@ enum azalia_pin_location_2 {
 	(((codec) << 28) | ((pin) << 20) | (0x71f << 8)	\
 		| (((val) >> 24) & 0xff))
 
+#define AZALIA_PIN_CFG_NC(n)   (0x411111f0 | (n & 0xf))
+
 #define AZALIA_RESET(pin)					\
 	(((pin) << 20) | 0x7ff00), (((pin) << 20) | 0x7ff00),	\
 	(((pin) << 20) | 0x7ff00), (((pin) << 20) | 0x7ff00)

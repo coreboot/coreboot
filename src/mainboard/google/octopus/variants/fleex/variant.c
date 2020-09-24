5f43369bec670067a7199a80f982ec8f9bcc69b0
Eric Lai <ericr_lai@compal.corp-partner.google.com>
2020-09-24 20:38:40 +0800

    mb/google/octopus/variants/fleex: Only do LTE power off for LTE sku
    Only do LTE power off for LTE sku in order to save extra 130ms delay
    for non-LTE sku.
    
    BUG=b:168075958
    BRANCH=octopus
    TEST=build image and verify on the DUT with LTE DB.
    
    Signed-off-by: Eric Lai <ericr_lai@compal.corp-partner.google.com>
    Change-Id: If983185ff2f09fb1b2553c6ff1a1473d3254de4b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45687
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marco Chen <marcochen@google.com>

--- src/mainboard/google/octopus/variants/fleex/variant.c
+++ src/mainboard/google/octopus/variants/fleex/variant.c
@@ -2,10 +2,15 @@
 
 #include <acpi/acpi.h>
 #include <baseboard/variants.h>
-#include <delay.h>
 #include <ec/google/chromeec/ec.h>
-#include <gpio.h>
 #include <sar.h>
+
+#define LTE_SKU		4
+
+static bool is_lte_sku(void)
+{
+	return (google_chromeec_get_board_sku() == LTE_SKU);
+}
 
 void variant_smi_sleep(u8 slp_typ)
 {
@@ -13,16 +18,15 @@ void variant_smi_sleep(u8 slp_typ)
 	 * here for saving one transaction to the EC for getting SKU ID. */
 	if (slp_typ != ACPI_S5)
 		return;
-
-	power_off_lte_module();
+	if (is_lte_sku())
+		power_off_lte_module();
 }
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
 	const char *filename = NULL;
-	uint32_t sku_id = google_chromeec_get_board_sku();
 
-	if (sku_id != 4)
+	if (!is_lte_sku())
 		filename = "wifi_sar-fleex.hex";
 
 	return filename;

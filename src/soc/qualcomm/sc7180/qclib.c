69ba8f186339600bef53facdbc5fd03c36dfbe6a
mkurumel <mkurumel@codeaurora.org>
2020-09-24 22:24:23 +0530

    sc7180: Remove LIMITS_CFG loading in romstage.
    LIMITS_CFG is not used/required by trogdor. Supporting this requires an FMAP
    partition as well as code, removing this support saves space and maintenance
    headaches.
    
    Change-Id: I9f57f5b520599ba6d708c91df9851e0e86b4b6c0
    Signed-off-by: Manideep Kurumella <mkurumel@codeaurora.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45704
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/qualcomm/sc7180/qclib.c
+++ src/soc/qualcomm/sc7180/qclib.c
@@ -9,7 +9,6 @@
 int qclib_soc_blob_load(void)
 {
 	size_t size;
-	ssize_t ssize;
 
 	/* Attempt to load PMICCFG Blob */
 	size = cbfs_boot_load_file(CONFIG_CBFS_PREFIX "/pmiccfg",
@@ -24,14 +23,6 @@ int qclib_soc_blob_load(void)
 	if (!size)
 		return -1;
 	qclib_add_if_table_entry(QCLIB_TE_DCB_SETTINGS, _dcb, size, 0);
-
-	/* Attempt to load Limits Config Blob */
-	ssize = fmap_read_area(QCLIB_FR_LIMITS_CFG_DATA, _limits_cfg,
-		REGION_SIZE(limits_cfg));
-	if (ssize < 0)
-		return -1;
-	qclib_add_if_table_entry(QCLIB_TE_LIMITS_CFG_DATA,
-				 _limits_cfg, ssize, 0);
 
 	return 0;
 }

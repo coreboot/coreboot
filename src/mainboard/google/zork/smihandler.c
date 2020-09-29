0cf0849cff485d635028e92ed9677f1ddccf9c92
Martin Roth <martinroth@chromium.org>
2020-09-29 17:04:16 -0600

    mb/google/zork: Initialize the backlight in the OS
    This fix needs to go into ACPI in the long-term, but this
    should suffice in the short-term.
    
    BUG=b:158087989
    TEST=Boot berknip, verify backlight is enabled.  Test suspend
    & resume sequence, backlight is still enabled.
    BRANCH=Zork
    
    
    Signed-off-by: Martin Roth <martinroth@chromium.org>
    Change-Id: I6ecc3c9e397c9756a78e480d3f639c507879a0ea
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45854
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/zork/smihandler.c
+++ src/mainboard/google/zork/smihandler.c
@@ -33,5 +33,11 @@ int mainboard_smi_apmc(u8 apmc)
 	if (CONFIG(EC_GOOGLE_CHROMEEC))
 		chromeec_smi_apmc(apmc, MAINBOARD_EC_SCI_EVENTS,
 					MAINBOARD_EC_SMI_EVENTS);
+
+	/* Temporary fix - Needs to go into ACPI instead */
+	/* Turn on the backlight when we go to ACPI mode */
+	if (apmc == APM_CNT_ACPI_ENABLE)
+		gpio_set(GPIO_85, 0);
+
 	return 0;
 }

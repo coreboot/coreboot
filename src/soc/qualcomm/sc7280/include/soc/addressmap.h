e689378d883c31ee367285340fe5780842cd912a
Taniya Das <tdas@codeaurora.org>
2020-09-29 13:12:12 +0530

    sc7280: Add target specific GPIO pin definitions
    Add GPIO pin details specific to SC7280 chipset
    for the consumers to be able to request for the
    gpio functionality as per their requirement.
    
    BUG=b:182963902
    TEST=Validated on qualcomm sc7280 development board
    
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    Change-Id: I63bcaed78a6eeb0e6fad857b89d40181613e50cc
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55077
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Shelley Chen <shchen@google.com>

---
+++ src/soc/qualcomm/sc7280/include/soc/addressmap.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _SOC_QUALCOMM_SC7280_ADDRESS_MAP_H_
+#define _SOC_QUALCOMM_SC7280_ADDRESS_MAP_H_
+
+#include <stdint.h>
+
+#define TLMM_TILE_BASE			0x0F100000
+
+#endif /*  __SOC_QUALCOMM_SC7280_ADDRESS_MAP_H__ */

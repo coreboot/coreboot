f710bf65dcd824854a5d0c2020ebd636dd08a861
Paul Menzel <pmenzel@molgen.mpg.de>
2020-10-03 08:57:57 +0200

    superio/nuvoton/common: Collapse two if statements
    There are more devices requiring this code, so avoid duplicating the if
    block over and over.
    
    Change-Id: Ib4f787e3c883b1fec941de77bc8e19ccf0d5224c
    Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/45970
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/superio/nuvoton/common/early_serial.c
+++ src/superio/nuvoton/common/early_serial.c
@@ -52,11 +52,8 @@ void nuvoton_enable_serial(pnp_devfn_t dev, u16 iobase)
 
 	nuvoton_pnp_enter_conf_state(dev);
 
-	if (CONFIG(SUPERIO_NUVOTON_NCT5539D_COM_A))
-		/* Route COM A to GPIO8 pin group */
-		pnp_write_config(dev, 0x2a, 0x40);
-
-	if (CONFIG(SUPERIO_NUVOTON_NCT6776_COM_A))
+	if (CONFIG(SUPERIO_NUVOTON_NCT5539D_COM_A) ||
+	    CONFIG(SUPERIO_NUVOTON_NCT6776_COM_A))
 		/* Route COM A to GPIO8 pin group */
 		pnp_write_config(dev, 0x2a, 0x40);
 

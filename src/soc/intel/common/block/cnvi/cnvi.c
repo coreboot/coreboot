23e88135bb86361cbd4c260a1a38bb7fda2b2338
Furquan Shaikh <furquan@google.com>
2020-10-08 23:44:20 -0700

    soc/intel: Add a driver for CNVi WiFi/BT controllers
    This change adds a common block driver for CNVi WiFi/BT controllers in
    Intel SoCs. This driver uses the common PCI dev operations in addition
    to generating ACPI device node and returning ACPI name for the
    controller device.
    
    This change also selects this driver for CML, GLK, ICL, JSL and TGL.
    
    Change-Id: I69a832be918d4b9f4fbe3a40913d4542a457a77c
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/46864
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

---
+++ src/soc/intel/common/block/cnvi/cnvi.c
@@ -0,0 +1,74 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <acpi/acpi_device.h>
+#include <device/device.h>
+#include <device/pci.h>
+#include <device/pci_ids.h>
+
+static const char *cnvi_wifi_acpi_name(const struct device *dev)
+{
+	return "CNVW";
+}
+
+static struct device_operations cnvi_wifi_ops = {
+	.read_resources		= pci_dev_read_resources,
+	.set_resources		= pci_dev_set_resources,
+	.enable_resources	= pci_dev_enable_resources,
+	.ops_pci		= &pci_dev_ops_pci,
+	.scan_bus		= scan_static_bus,
+	.acpi_name		= cnvi_wifi_acpi_name,
+	.acpi_fill_ssdt		= acpi_device_write_pci_dev,
+};
+
+static const unsigned short wifi_pci_device_ids[] = {
+	PCI_DEVICE_ID_INTEL_CML_LP_CNVI_WIFI,
+	PCI_DEVICE_ID_INTEL_CML_H_CNVI_WIFI,
+	PCI_DEVICE_ID_INTEL_CNL_LP_CNVI_WIFI,
+	PCI_DEVICE_ID_INTEL_CNL_H_CNVI_WIFI,
+	PCI_DEVICE_ID_INTEL_GLK_CNVI_WIFI,
+	PCI_DEVICE_ID_INTEL_ICL_CNVI_WIFI,
+	PCI_DEVICE_ID_INTEL_JSL_CNVI_WIFI_0,
+	PCI_DEVICE_ID_INTEL_JSL_CNVI_WIFI_1,
+	PCI_DEVICE_ID_INTEL_JSL_CNVI_WIFI_2,
+	PCI_DEVICE_ID_INTEL_JSL_CNVI_WIFI_3,
+	PCI_DEVICE_ID_INTEL_TGL_CNVI_WIFI_0,
+	PCI_DEVICE_ID_INTEL_TGL_CNVI_WIFI_1,
+	PCI_DEVICE_ID_INTEL_TGL_CNVI_WIFI_2,
+	PCI_DEVICE_ID_INTEL_TGL_CNVI_WIFI_3,
+	0
+};
+
+static const struct pci_driver pch_cnvi_wifi __pci_driver = {
+	.ops			= &cnvi_wifi_ops,
+	.vendor			= PCI_VENDOR_ID_INTEL,
+	.devices		= wifi_pci_device_ids,
+};
+
+static const char *cnvi_bt_acpi_name(const struct device *dev)
+{
+	return "CNVB";
+}
+
+static struct device_operations cnvi_bt_ops = {
+	.read_resources		= pci_dev_read_resources,
+	.set_resources		= pci_dev_set_resources,
+	.enable_resources	= pci_dev_enable_resources,
+	.ops_pci		= &pci_dev_ops_pci,
+	.scan_bus		= scan_static_bus,
+	.acpi_name		= cnvi_bt_acpi_name,
+	.acpi_fill_ssdt		= acpi_device_write_pci_dev,
+};
+
+static const unsigned short bt_pci_device_ids[] = {
+	PCI_DEVICE_ID_INTEL_TGL_CNVI_BT_0,
+	PCI_DEVICE_ID_INTEL_TGL_CNVI_BT_1,
+	PCI_DEVICE_ID_INTEL_TGL_CNVI_BT_2,
+	PCI_DEVICE_ID_INTEL_TGL_CNVI_BT_3,
+	0
+};
+
+static const struct pci_driver pch_cnvi_bt __pci_driver = {
+	.ops			= &cnvi_bt_ops,
+	.vendor			= PCI_VENDOR_ID_INTEL,
+	.devices		= bt_pci_device_ids,
+};

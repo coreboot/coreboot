coreboot/coreboot

commit f95daa510d6c344199be3309afb86e8030521d05

superio/nuvoton: Add back Nuvoton NCT6776 support
Revert commit 53552cc0 (Drop SuperIO nuvoton/nct6776),
removing the code as no other mainboard uses it.

The board Intel Saddle Brook uses this device, so add the
code back with minor adaptations.

Change-Id: I546879285ad8336e81798d0fbdf94f72e1fa61a2
Signed-off-by: Teo Boon Tiong <boon.tiong.teo@intel.com>
Reviewed-on: https://review.coreboot.org/16519
Tested-by: build bot (Jenkins)
Reviewed-by: Martin Roth <martinroth@google.com>

src/superio/nuvoton/common/early_serial.c
@@ -63,6 +63,9 @@ static void pnp_exit_conf_state(pnp_devfn_t dev)
 void nuvoton_enable_serial(pnp_devfn_t dev, u16 iobase)
 {
 	pnp_enter_conf_state(dev);
+	if (IS_ENABLED(CONFIG_SUPERIO_NUVOTON_NCT6776_COM_A))
+		/* Route GPIO8 pin group to COM A */
+		pnp_write_config(dev, 0x2a, 0x40);
 	pnp_set_logical_device(dev);
 	pnp_set_enable(dev, 0);
 	pnp_set_iobase(dev, PNP_IDX_IO0, iobase);

src/superio/nuvoton/nct6776/Kconfig
@@ -1,7 +1,8 @@
 ##
 ## This file is part of the coreboot project.
 ##
-## Copyright (C) 2011 Advanced Micro Devices, Inc.
+## Copyright (C) 2011 - 2012 Advanced Micro Devices, Inc.
+## Copyright (C) 2014 Felix Held <felix-coreboot@felixheld.de>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -13,12 +14,10 @@
 ## GNU General Public License for more details.
 ##
 
-## include generic nuvoton pre-ram stage driver
-romstage-$(CONFIG_SUPERIO_NUVOTON_COMMON_ROMSTAGE) += common/early_serial.c
+config SUPERIO_NUVOTON_NCT6776
+	bool
+	select SUPERIO_NUVOTON_COMMON_ROMSTAGE
 
-subdirs-$(CONFIG_SUPERIO_NUVOTON_WPCM450) += wpcm450
-subdirs-$(CONFIG_SUPERIO_NUVOTON_NCT5104D) += nct5104d
-subdirs-$(CONFIG_SUPERIO_NUVOTON_NCT5572D) += nct5572d
-subdirs-$(CONFIG_SUPERIO_NUVOTON_NCT6776) += nct6776
-subdirs-$(CONFIG_SUPERIO_NUVOTON_NCT6779D) += nct6779d
-subdirs-$(CONFIG_SUPERIO_NUVOTON_NCT6791D) += nct6791d
+config SUPERIO_NUVOTON_NCT6776_COM_A
+	bool
+	default n

src/superio/nuvoton/nct6776/Makefile.inc
@@ -1,0 +1,18 @@
+##
+## This file is part of the coreboot project.
+##
+## Copyright (C) 2011 - 2012 Advanced Micro Devices, Inc.
+## Copyright (C) 2014 Felix Held <felix-coreboot@felixheld.de>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+
+ramstage-$(CONFIG_SUPERIO_NUVOTON_NCT6776) += superio.c

src/superio/nuvoton/nct6776/nct6776.h
@@ -1,0 +1,58 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2014 Felix Held <felix-coreboot@felixheld.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/* Both NCT6776D and NCT6776F package variants are supported. */
+
+#ifndef SUPERIO_NUVOTON_NCT6776_H
+#define SUPERIO_NUVOTON_NCT6776_H
+
+/* Logical Device Numbers (LDN). */
+#define NCT6776_FDC		0x00 /* Floppy */
+#define NCT6776_PP		0x01 /* Parallel port */
+#define NCT6776_SP1		0x02 /* Com1 */
+#define NCT6776_SP2		0x03 /* Com2 & IR */
+#define NCT6776_KBC		0x05 /* PS/2 keyboard and mouse */
+#define NCT6776_CIR		0x06
+#define NCT6776_GPIO6789_V	0x07
+#define NCT6776_WDT1_GPIO01A_V	0x08
+#define NCT6776_GPIO1234567_V	0x09
+#define NCT6776_ACPI		0x0A
+#define NCT6776_HWM_FPLED	0x0B /* Hardware monitor & front LED */
+#define NCT6776_VID		0x0D
+#define NCT6776_CIRWKUP		0x0E /* CIR wakeup */
+#define NCT6776_GPIO_PP_OD	0x0F /* GPIO Push-Pull/Open drain select */
+#define NCT6776_SVID		0x14
+#define NCT6776_DSLP		0x16 /* Deep sleep */
+#define NCT6776_GPIOA_LDN	0x17
+
+/* virtual LDN for GPIO and WDT */
+#define NCT6776_WDT1		((0 << 8) | NCT6776_WDT1_GPIO01A_V)
+
+#define NCT6776_GPIOBASE	((0 << 8) | NCT6776_WDT1_GPIO01A_V) //?
+
+#define NCT6776_GPIO0		((1 << 8) | NCT6776_WDT1_GPIO01A_V)
+#define NCT6776_GPIO1		((1 << 8) | NCT6776_GPIO1234567_V)
+#define NCT6776_GPIO2		((2 << 8) | NCT6776_GPIO1234567_V)
+#define NCT6776_GPIO3		((3 << 8) | NCT6776_GPIO1234567_V)
+#define NCT6776_GPIO4		((4 << 8) | NCT6776_GPIO1234567_V)
+#define NCT6776_GPIO5		((5 << 8) | NCT6776_GPIO1234567_V)
+#define NCT6776_GPIO6		((6 << 8) | NCT6776_GPIO1234567_V)
+#define NCT6776_GPIO7		((7 << 8) | NCT6776_GPIO1234567_V)
+#define NCT6776_GPIO8		((0 << 8) | NCT6776_GPIO6789_V)
+#define NCT6776_GPIO9		((1 << 8) | NCT6776_GPIO6789_V)
+#define NCT6776_GPIOA		((2 << 8) | NCT6776_WDT1_GPIO01A_V)
+
+#endif /* SUPERIO_NUVOTON_NCT6776_H */

src/superio/nuvoton/nct6776/superio.c
@@ -1,0 +1,99 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2011 Advanced Micro Devices, Inc.
+ * Copyright (C) 2014 Felix Held <felix-coreboot@felixheld.de>
+ * Copyright (C) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/io.h>
+#include <device/device.h>
+#include <device/pnp.h>
+#include <pc80/keyboard.h>
+#include <stdlib.h>
+#include <superio/conf_mode.h>
+
+#include "nct6776.h"
+
+/* Both NCT6776D and NCT6776F package variants are supported. */
+
+static void nct6776_init(struct device *dev)
+{
+	if (!dev->enabled)
+		return;
+
+	switch (dev->path.pnp.device) {
+	/* TODO: Might potentially need code for HWM or FDC etc. */
+	case NCT6776_KBC:
+		pc_keyboard_init(NO_AUX_DEVICE);
+		break;
+	}
+}
+
+static struct device_operations ops = {
+	.read_resources   = pnp_read_resources,
+	.set_resources    = pnp_set_resources,
+	.enable_resources = pnp_enable_resources,
+	.enable           = pnp_alt_enable,
+	.init             = nct6776_init,
+	.ops_pnp_mode     = &pnp_conf_mode_8787_aa,
+};
+
+static struct pnp_info pnp_dev_info[] = {
+	{ &ops, NCT6776_FDC, PNP_IO0 | PNP_IRQ0 | PNP_DRQ0,
+		{0x0ff8, 0}, },
+	{ &ops, NCT6776_PP, PNP_IO0 | PNP_IRQ0 | PNP_DRQ0,
+		{0x0ff8, 0}, },
+	{ &ops, NCT6776_SP1, PNP_IO0 | PNP_IRQ0,
+		{0x0ff8, 0}, },
+	{ &ops, NCT6776_SP2, PNP_IO0 | PNP_IRQ0,
+		{0x0ff8, 0}, },
+	{ &ops, NCT6776_KBC, PNP_IO0 | PNP_IO1 | PNP_IRQ0 | PNP_IRQ1,
+		{0x0fff, 0}, {0x0fff, 4}, },
+	{ &ops, NCT6776_CIR, PNP_IO0 | PNP_IRQ0,
+		{0x0ff8, 0}, },
+	{ &ops, NCT6776_ACPI},
+	{ &ops, NCT6776_HWM_FPLED, PNP_IO0 | PNP_IO1 | PNP_IRQ0,
+		{0x0ffe, 0}, {0x0ffe, 4}, },
+	{ &ops, NCT6776_VID},
+	{ &ops, NCT6776_CIRWKUP, PNP_IO0 | PNP_IRQ0,
+		{0x0ff8, 0}, },
+	{ &ops, NCT6776_GPIO_PP_OD},
+	{ &ops, NCT6776_SVID},
+	{ &ops, NCT6776_DSLP},
+	{ &ops, NCT6776_GPIOA_LDN},
+	{ &ops, NCT6776_WDT1},
+	{ &ops, NCT6776_GPIOBASE, PNP_IO0,
+		{0x0ff8, 0}, },
+	{ &ops, NCT6776_GPIO0},
+	{ &ops, NCT6776_GPIO1},
+	{ &ops, NCT6776_GPIO2},
+	{ &ops, NCT6776_GPIO3},
+	{ &ops, NCT6776_GPIO4},
+	{ &ops, NCT6776_GPIO5},
+	{ &ops, NCT6776_GPIO6},
+	{ &ops, NCT6776_GPIO7},
+	{ &ops, NCT6776_GPIO8},
+	{ &ops, NCT6776_GPIO9},
+	{ &ops, NCT6776_GPIOA},
+};
+
+static void enable_dev(struct device *dev)
+{
+	pnp_enable_devices(dev, &ops, ARRAY_SIZE(pnp_dev_info), pnp_dev_info);
+}
+
+struct chip_operations superio_nuvoton_nct6776_ops = {
+	CHIP_NAME("NUVOTON NCT6776 Super I/O")
+	.enable_dev = enable_dev,
+};

commit 138c1b864bf9745f084e2b7035204aaa3b5b983c

superio/nuvoton: Only set bit 7 of global CR 0x2a for COM A
Currently, when selecting SUPERIO_NUVOTON_NCT*_COM_A, the whole global
control register 0x2a is written to 0x40. CR 0x2a defaults to 0xc0, so
indeed bit 7 is cleared, but the device early init code might have set
other bits in that control register, so setting it to 0x40 might
override already set bits. So, only clear bit 7 and leave the other bits
untouched.

Fixes: f95daa510d ("superio/nuvoton: Add back Nuvoton NCT6776 support")
Change-Id: I9ded9dab3985c4c8e5c45af354ef44af482e18c2
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/46286
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>
Reviewed-by: Nico Huber <nico.h@gmx.de>

src/superio/nuvoton/common/early_serial.c
@@ -55,7 +55,7 @@ void nuvoton_enable_serial(pnp_devfn_t dev, u16 iobase)
 	if (CONFIG(SUPERIO_NUVOTON_NCT5539D_COM_A) ||
 	    CONFIG(SUPERIO_NUVOTON_NCT6776_COM_A))
 		/* Route COM A to GPIO8 pin group */
-		pnp_write_config(dev, 0x2a, 0x40);
+		pnp_unset_and_set_config(dev, 0x2a, 1 << 7, 0);
 
 	if (CONFIG(SUPERIO_NUVOTON_NCT6791D_COM_A))
 		/* Route COM A to GPIO8 pin group */
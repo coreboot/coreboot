coreboot/coreboot

commit 6ff10789909c86dadd61dadefa7e961990a7b6cf

superio: Log if mouse controller is disabled
It’s useful to know, if the mouse controller is disabled or not, so
convert the comment to a log message.

Change-Id: Ic3f7d5b7b98cf8c258a6a601f4a44ce403f4a576
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/22285
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

src/superio/nuvoton/nct5572d/superio.c
@@ -57,7 +57,8 @@ static void nct5572d_init(struct device *dev)
 		mouse_detected = pc_keyboard_init(PROBE_AUX_DEVICE);
 
 		if (!mouse_detected && !acpi_is_wakeup_s3()) {
-			/* Disable mouse controller */
+			printk(BIOS_INFO, "%s: Disable mouse controller.",
+					__func__);
 			pnp_enter_conf_mode_8787(dev);
 			byte = pnp_read_config(dev, 0x2a);
 			byte |= 0x1 << 1;

src/superio/winbond/w83667hg-a/superio.c
@@ -57,7 +57,8 @@ static void w83667hg_a_init(struct device *dev)
 		mouse_detected = pc_keyboard_init(PROBE_AUX_DEVICE);
 
 		if (!mouse_detected && !acpi_is_wakeup_s3()) {
-			/* Disable mouse controller */
+			printk(BIOS_INFO, "%s: Disable mouse controller.",
+					__func__);
 			pnp_enter_conf_mode_8787(dev);
 			byte = pnp_read_config(dev, 0x2a);
 			byte |= 0x1 << 1;

commit e1194ae6b6b9b02371d1e9e9c3b00aa658f30213

superio: Add newline to log message about disabled mouse controller
A newline is missing at the end of the informational message.

    PNP: 002e.5 init
    nct5572d_init: Disable mouse controller.PNP: 002e.5 init finished in 0 msecs
    PNP: 002e.307 init

Change-Id: Ic73ed97be0993637be1e97040784d5a8e70a22ae
Fixes: 6ff1078990 ("superio: Log if mouse controller is disabled")
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/45805
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>

src/superio/nuvoton/nct5572d/superio.c
@@ -36,7 +36,7 @@ static void nct5572d_init(struct device *dev)
 		mouse_detected = pc_keyboard_init(PROBE_AUX_DEVICE);
 
 		if (!mouse_detected) {
-			printk(BIOS_INFO, "%s: Disable mouse controller.",
+			printk(BIOS_INFO, "%s: Disable mouse controller.\n",
 					__func__);
 			pnp_enter_conf_mode(dev);
 			byte = pnp_read_config(dev, 0x2a);

src/superio/winbond/w83667hg-a/superio.c
@@ -36,7 +36,7 @@ static void w83667hg_a_init(struct device *dev)
 		mouse_detected = pc_keyboard_init(PROBE_AUX_DEVICE);
 
 		if (!mouse_detected) {
-			printk(BIOS_INFO, "%s: Disable mouse controller.",
+			printk(BIOS_INFO, "%s: Disable mouse controller.\n",
 					__func__);
 			pnp_enter_conf_mode(dev);
 			byte = pnp_read_config(dev, 0x2a);
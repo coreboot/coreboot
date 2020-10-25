c1f58e68fb93bf3da9c220b48b45519fb8fb0bf9
Wisley Chen <wisley.chen@quantatw.com>
2020-10-26 01:34:16 +0800

    mb/google/volteer: correct memory id for elemi
    BUG=b:170604353
    BRANCH=volteer
    TEST=emerge-volteer coreboot, and boot into kernel.
    
    Change-Id: If354aa158f3ad60193268f38278a44f9c99bf3db
    Signed-off-by: Wisley Chen <wisley.chen@quantatw.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/46770
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/volteer/variants/elemi/memory.c
+++ src/mainboard/google/volteer/variants/elemi/memory.c
@@ -21,10 +21,10 @@ const struct ddr_memory_cfg *variant_memory_params(void)
 int variant_memory_sku(void)
 {
 	gpio_t spd_gpios[] = {
-		GPIO_MEM_CONFIG_3,
-		GPIO_MEM_CONFIG_2,
-		GPIO_MEM_CONFIG_1,
 		GPIO_MEM_CONFIG_0,
+		GPIO_MEM_CONFIG_1,
+		GPIO_MEM_CONFIG_2,
+		GPIO_MEM_CONFIG_3,
 	};
 
 	return gpio_base2_value(spd_gpios, ARRAY_SIZE(spd_gpios));

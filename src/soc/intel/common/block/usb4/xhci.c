0f5a17e98092dd6f340582ede5d481e53f90a06c
Duncan Laurie <dlaurie@google.com>
2020-10-27 10:26:18 -0700

    soc/intel/common/block/usb4: Add TCSS XHCI driver for SSDT generation
    In order to generate ACPI entries for USB devices attached to the
    USB4/TBT/TCSS/North XHCI device it needs to have a driver that will
    enumerate static devices on the bus.  This driver does that and nothing
    else.
    
    BUG=b:151731851
    TEST=boot on volteer and check for USB devices on \_SB.PCI0.TXHC.RHUB
    
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Change-Id: I5a2ff1cd1bed557e793d45119232cf87032ddd7b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/46851
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/soc/intel/common/block/usb4/xhci.c
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/device.h>
+#include <device/pci.h>
+#include <device/pci_ids.h>
+#include <intelblocks/acpi.h>
+#include <soc/pci_devs.h>
+
+static const char *usb4_xhci_acpi_name(const struct device *dev)
+{
+	if (dev->path.type != DEVICE_PATH_PCI)
+		return NULL;
+
+	return "TXHC";
+}
+
+static struct device_operations usb4_xhci_ops = {
+	.read_resources		= pci_dev_read_resources,
+	.set_resources		= pci_dev_set_resources,
+	.enable_resources	= pci_dev_enable_resources,
+	.ops_pci		= &pci_dev_ops_pci,
+	.scan_bus		= scan_static_bus,
+#if CONFIG(HAVE_ACPI_TABLES)
+	.acpi_name		= usb4_xhci_acpi_name,
+#endif
+};
+
+static const unsigned short pci_device_ids[] = {
+	PCI_DEVICE_ID_INTEL_TGP_TCSS_XHCI,
+	0
+};
+
+static const struct pci_driver usb4_xhci __pci_driver = {
+	.ops	 = &usb4_xhci_ops,
+	.vendor	 = PCI_VENDOR_ID_INTEL,
+	.devices = pci_device_ids,
+};

2736c82956d4c4969fd896688b8be0d16f3ab272
Furquan Shaikh <furquan@google.com>
2020-10-27 19:46:11 -0700

    drivers/wifi/generic: Move ACPI functions to a separate file
    This change reorganizes the WiFi generic driver to move the ACPI
    functions to a separate file. This change is done to reduce the noise
    in generic.c file and improve readability of the file.
    
    Change-Id: If5fafb5452fb5bad327be730fcfc43d8a5d3b8ec
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/46860
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/drivers/wifi/generic/wifi_private.h
+++ src/drivers/wifi/generic/wifi_private.h
@@ -5,4 +5,7 @@
 
 int smbios_write_wifi(struct device *dev, int *handle, unsigned long *current);
 
+const char *wifi_generic_acpi_name(const struct device *dev);
+void wifi_generic_fill_ssdt(const struct device *dev);
+
 #endif

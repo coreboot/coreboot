7ba377511420e99575b3ae74d67e2c63e7700ecd
Yidi Lin <yidi.lin@mediatek.com>
2020-10-29 13:01:28 +0800

    soc/mediatek/common: Add common API for loading firmwares
    Add mtk_init_mcu to load the firmware to the specified memory address
    and run the firmware. This function also measures the load time and the
    blob size. For example:
    
    mtk_init_mcu: Loaded (and reset) dpm.pm in 15 msecs (14004 bytes)
    
    Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Change-Id: Ie94001bbda25fe015f43172e92a1006e059de223
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/46930
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ src/soc/mediatek/common/include/soc/mcu_common.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SOC_MEDIATEK_MTLIB_COMMON_H
+#define SOC_MEDIATEK_MTLIB_COMMON_H
+
+struct mtk_mcu {
+	const char *firmware_name;	/* The firmware file name in CBFS */
+	void *run_address;		/* The address for running the firmware */
+	size_t run_size;		/* The buffer for loading the firmware */
+	void *load_buffer;		/* The buffer size */
+	size_t buffer_size;		/* The firmware real size */
+	void *priv;			/* The additional data required by the reset callback */
+	void (*reset)(struct mtk_mcu *mcu);	/* The reset callback */
+};
+
+int mtk_init_mcu(struct mtk_mcu *mcu);
+
+#endif /* SOC_MEDIATEK_MTLIB_COMMON_H */
---
+++ src/soc/mediatek/common/mcu.c
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <arch/barrier.h>
+#include <cbfs.h>
+#include <console/console.h>
+#include <soc/mcu_common.h>
+#include <soc/symbols.h>
+#include <timer.h>
+
+int mtk_init_mcu(struct mtk_mcu *mcu)
+{
+	struct stopwatch sw;
+
+	if (!mcu)
+		return CB_ERR_ARG;
+
+	stopwatch_init(&sw);
+
+	mcu->run_size = cbfs_load(mcu->firmware_name, mcu->load_buffer, mcu->buffer_size);
+	if (mcu->run_size == 0) {
+		printk(BIOS_ERR, "%s: Failed to load %s\n", __func__, mcu->firmware_name);
+		return CB_ERR;
+	}
+
+	if (mcu->run_address) {
+		memcpy(mcu->run_address, mcu->load_buffer, mcu->run_size);
+		/* Memory barrier to ensure data is flushed before resetting MCU. */
+		mb();
+	}
+
+	if (mcu->reset)
+		mcu->reset(mcu);
+
+	printk(BIOS_DEBUG, "%s: Loaded (and reset) %s in %ld msecs (%zd bytes)\n",
+	       __func__, mcu->firmware_name, stopwatch_duration_msecs(&sw), mcu->run_size);
+
+	return CB_SUCCESS;
+}

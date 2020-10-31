2b2ade96384791d7320d87bc0e29445002b6d246
Subrata Banik <subrata.banik@intel.com>
2020-10-31 21:07:16 +0530

    soc/intel/common: Create common Intel FSP reset code block
    Create SOC_INTEL_COMMON_FSP_RESET Kconfig to have IA common code block
    to handle platform reset request raised by FSP. The FSP will use the
    FSP EAS v2.0 section 12.2.2 (OEM Status Code) to indicate that a reset
    is required.
    
    Make FSP_STATUS_GLOBAL_RESET depends on SOC_INTEL_COMMON_FSP_RESET.
    
    Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Change-Id: I934b41affed7bb146f53ff6a4654fdbc6626101b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/47017
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

---
+++ src/soc/intel/common/fsp_reset.c
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <console/console.h>
+#include <fsp/util.h>
+#include <soc/intel/common/reset.h>
+
+void chipset_handle_reset(uint32_t status)
+{
+	if (status == CONFIG_FSP_STATUS_GLOBAL_RESET) {
+		printk(BIOS_DEBUG, "GLOBAL RESET!\n");
+		global_reset();
+	}
+
+	printk(BIOS_ERR, "unhandled reset type %x\n", status);
+	die("unknown reset type");
+}

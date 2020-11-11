1de2dd25f72b2cc425acd3f83ecf0eb0a1486702
Shaik Sajida Bhanu <sbhanu@codeaurora.org>
2020-11-11 19:33:42 +0530

    mainboard/google/herobrine: Configure SDCC clock
    Configure 384MHz for eMMC clock and 50MHz for SD card clock.
    
    BUG=b:182963902
    TEST=Validated on qualcomm sc7280 development board
    
    Change-Id: I8acbce58614add0228adc39289762da10937cbe2
    Signed-off-by: Shaik Sajida Bhanu <sbhanu@codeaurora.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50585
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Shelley Chen <shchen@google.com>

--- src/mainboard/google/herobrine/mainboard.c
+++ src/mainboard/google/herobrine/mainboard.c
@@ -1,11 +1,20 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include "board.h"
+#include <boardid.h>
+#include <bootblock_common.h>
+#include <soc/clock.h>
+#include <console/console.h>
 #include <device/device.h>
 #include <bootblock_common.h>
+#include <soc/clock.h>
 
 static void mainboard_init(struct device *dev)
 {
-
+	/* Configure clock for eMMC */
+	clock_configure_sdcc(1, 384 * MHz);
+	/* Configure clock for SD card */
+	clock_configure_sdcc(2, 50 * MHz);
 }
 
 static void mainboard_enable(struct device *dev)

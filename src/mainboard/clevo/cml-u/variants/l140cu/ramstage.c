50650427dc3a0b144a3a06a9936d8c85819dc059
Angel Pons <th3fanbus@gmail.com>
2020-11-13 12:32:55 +0100

    mb/clevo/cml-u: Fix up comment style
    Change-Id: Idefc15fec1d62d68d88e91c76f59e1a60a3996b6
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/47564
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>
    Reviewed-by:  Felix Singer <felixsinger@posteo.net>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/clevo/cml-u/variants/l140cu/ramstage.c
+++ src/mainboard/clevo/cml-u/variants/l140cu/ramstage.c
@@ -6,6 +6,6 @@
 void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 {
 	/* Configure pads prior to SiliconInit() in case there's any
-	 * dependencies during hardware initialization. */
+	   dependencies during hardware initialization. */
 	cnl_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 }

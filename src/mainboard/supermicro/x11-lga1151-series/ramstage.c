33b92efa5168ab8687b6bbc5c653ba8913d4e8a4
Angel Pons <th3fanbus@gmail.com>
2020-11-13 12:34:47 +0100

    mb/supermicro/x11-lga1151-series: Fix up comment style
    Change-Id: I5b6191d2b5f6fd6d127934eda56e3c24181b0c4c
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/47565
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>
    Reviewed-by:  Felix Singer <felixsinger@posteo.net>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/supermicro/x11-lga1151-series/ramstage.c
+++ src/mainboard/supermicro/x11-lga1151-series/ramstage.c
@@ -6,6 +6,6 @@
 void mainboard_silicon_init_params(FSP_SIL_UPD *params)
 {
 	/* Configure pads prior to SiliconInit() in case there's any
-	 * dependencies during hardware initialization. */
+	   dependencies during hardware initialization. */
 	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 }

79c42635ae9f5be496ead2d760a63d0ab40a5f14
Felix Singer <felixsinger@posteo.net>
2020-11-21 13:45:34 +0000

    mb/clevo/cml-u: Get rid of cnl_configure_pads()
    Get rid of cnl_configure_pads() since it is a hack for the FSP. Instead,
    hook up to the mainboard_ops driver and configure the GPIOs using .init.
    
    Tested on clevo/l140cu and it still boots.
    
    Change-Id: I75dd15ab6d2b3b72b3ad0398df87b349fd00bc3c
    Signed-off-by: Felix Singer <felixsinger@posteo.net>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/47835
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>

--- src/mainboard/clevo/cml-u/ramstage.c
+++ src/mainboard/clevo/cml-u/ramstage.c
@@ -1,11 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <soc/ramstage.h>
+#include <device/device.h>
 #include <variant/gpio.h>
 
-void mainboard_silicon_init_params(FSP_S_CONFIG *params)
+static void init_mainboard(void *chip_info)
 {
-	/* Configure pads prior to SiliconInit() in case there's any
-	   dependencies during hardware initialization. */
-	cnl_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
+	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 }
+
+struct chip_operations mainboard_ops = {
+	.init = init_mainboard,
+};

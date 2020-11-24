d328934c9b9d1454a83971182627565a5d116cb0
Michael Niewöhner <foss@mniewoehner.de>
2020-11-24 13:24:47 +0100

    mb/supermicro/x11ssm-f: drop NMI overrides
    Drop the NMI overrides, since NMI now gets configured in gpio common
    code. Also remove the variant init mechanism, which is unused now.
    
    Signed-off-by: Michael Niewöhner <foss@mniewoehner.de>
    Change-Id: I02e0c679f9aafe33108320a8dfc62dcb278202ef
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/48092
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/supermicro/x11-lga1151-series/mainboard.c
+++ src/mainboard/supermicro/x11-lga1151-series/mainboard.c
@@ -1,20 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <mainboard.h>
 #include <device/device.h>
 #include <mainboard/gpio.h>
 
-__weak void variant_mainboard_init(void *chip_info)
-{
-}
-
 static void mainboard_chip_init(void *chip_info)
 {
-	/* do common init */
 	mainboard_configure_gpios();
-
-	/* do variant init */
-	variant_mainboard_init(chip_info);
 }
 
 struct chip_operations mainboard_ops = {

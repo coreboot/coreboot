dc811c9ea3937b4265ce94c72e7dcba9fb122105
Michael Niewöhner <foss@mniewoehner.de>
2020-11-24 01:01:05 +0100

    mb/supermicro/x11ssm-f: drop unneeded ITSS override
    The ITSS override is not needed for LPC_CLKOUT* pads. Drop it.
    
    Signed-off-by: Michael Niewöhner <foss@mniewoehner.de>
    Change-Id: I3dbbc8944751779151dcd4f92fb870d937801d69
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/48084
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/supermicro/x11-lga1151-series/variants/x11ssm-f/mainboard.c
+++ src/mainboard/supermicro/x11-lga1151-series/variants/x11ssm-f/mainboard.c
@@ -2,21 +2,11 @@
 
 #include <mainboard.h>
 #include <device/device.h>
-#include <intelblocks/itss.h>
 #include <intelblocks/pcr.h>
-#include <soc/itss.h>
 #include <soc/pcr_ids.h>
 
 void variant_mainboard_init(struct device *dev)
 {
-	/* TODO:
-	* Find out why the polarities from gpio.h gets overwritten by FSP.
-	* This sets irq polarity to the same values as vendor
-	* but I do not know if this is really needed....
-	*/
-	itss_set_irq_polarity(33, 0);
-	itss_set_irq_polarity(34, 0);
-
 	// TODO: NMI; is this needed? vendor sets it
 	pcr_write32(0xae, 0x01e4, 0x00000004);
 	pcr_write32(0xae, 0x01e8, 0x00000040);

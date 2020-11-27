7d83309eb2bd96764a688be45c4aeb7df3f13e09
Tan, Lean Sheng <lean.sheng.tan@intel.com>
2020-11-27 04:33:34 -0800

    mb/intel/ehlcrb: Remove JSL sku id info in SMBIOS
    Remove JSL specific SMBIOS sku id info as it is not required by
    EHL.
    
    Signed-off-by: Tan, Lean Sheng <lean.sheng.tan@intel.com>
    Change-Id: Ib672eb456ba62f2eb7f941630c4fbb34823664f5
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/48123
    Reviewed-by: Frans Hendriks <fhendriks@eltan.com>
    Reviewed-by: Werner Zeh <werner.zeh@siemens.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/intel/elkhartlake_crb/mainboard.c
+++ src/mainboard/intel/elkhartlake_crb/mainboard.c
@@ -3,7 +3,6 @@
 #include <baseboard/variants.h>
 #include <device/device.h>
 #include <soc/gpio.h>
-#include <smbios.h>
 
 static void mainboard_init(void *chip_info)
 {
@@ -12,12 +11,6 @@ static void mainboard_init(void *chip_info)
 
 	pads = variant_gpio_table(&num);
 	gpio_configure_pads(pads, num);
-}
-
-const char *smbios_system_sku(void)
-{
-	static const char *sku_str = "sku2147483647"; /* sku{0-1} */
-	return sku_str;
 }
 
 struct chip_operations mainboard_ops = {

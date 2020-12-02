c221d56478ddb1a364048ea2eb74cb26284eedf8
Yidi Lin <yidi.lin@mediatek.com>
2020-12-02 16:43:43 +0800

    soc/mediatek/mt8183: Add DRAM_DMA section
    mtk_init_mcu uses DRAM_DMA section as CBFS buffer.
    The change "mediatek/mt8183: Remove DRAM_DMA section" is reverted
    for using mtk_init_mcu.
    
    On mt8173 and mt8192, this region is used by DMA hardware and is
    marked as non-cacheable resource. On mt8183, this region is reserved
    as CBFS buffer, so it is not necessary to be marked as non-cacheable
    resource.
    
    Change-Id: I7ce9f68883e2787ee7f3c5066f4c47c5ca315633
    Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/48232
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

---
+++ src/soc/mediatek/mt8183/include/soc/symbols.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _SOC_MEDIATEK_MT8183_SYMBOLS_H_
+#define _SOC_MEDIATEK_MT8183_SYMBOLS_H_
+#include <symbols.h>
+
+DECLARE_REGION(dram_dma)
+
+#endif /* _SOC_MEDIATEK_MT8183_SYMBOLS_H_ */

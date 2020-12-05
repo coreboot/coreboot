c8272783db8b8a8fe66e1bd1fe60a37532b6b071
Felix Held <felix-coreboot@felixheld.de>
2020-12-05 01:39:28 +0100

    soc/amd/cezanne: add config.c and minimal chip.h
    Change-Id: I89f08c201bd7d9a11b186ef960abe9714a76fb97
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/48317
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>

--- src/soc/amd/cezanne/chip.c
+++ src/soc/amd/cezanne/chip.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <device/device.h>
+#include "chip.h"
 
 struct chip_operations soc_amd_cezanne_ops = { NULL };
---
+++ src/soc/amd/cezanne/chip.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef CEZANNE_CHIP_H
+#define CEZANNE_CHIP_H
+
+#include <amdblocks/chip.h>
+
+struct soc_amd_cezanne_config {
+	struct soc_amd_common_config common_config;
+};
+
+#endif /* CEZANNE_CHIP_H */

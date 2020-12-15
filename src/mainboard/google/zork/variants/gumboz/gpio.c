d800d5e3ac740df608bc4dd2c0d16720aed45285
Kevin Chiu <Kevin.Chiu@quantatw.com>
2020-12-15 17:01:16 +0800

    zork: update gumboz variant
    gumboz is the dalboz/dirinboz follower.
    update gumboz variant to align dirinboz settings.
    
    BUG=b:174277853,b:173662179
    BRANCH=zork
    TEST=emerge-zork coreboot
    
    Signed-off-by: Kevin Chiu <kevin.chiu@quantatw.com>
    Change-Id: I80c03d531761c02b68bd127d889c3ace2dd9e99e
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/48642
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>
    Reviewed-by: Kangheui Won <khwon@chromium.org>

---
+++ src/mainboard/google/zork/variants/gumboz/gpio.c
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <boardid.h>
+#include <gpio.h>
+#include <soc/gpio.h>
+
+static const struct soc_amd_gpio dirinboz_gpio_set_stage_ram[] = {
+	/* PEN_DETECT_ODL - no used */
+	PAD_NC(GPIO_4),
+	/* PEN_POWER_EN - no used */
+	PAD_NC(GPIO_5),
+};
+
+const struct soc_amd_gpio *variant_override_gpio_table(size_t *size)
+{
+	*size = ARRAY_SIZE(dirinboz_gpio_set_stage_ram);
+	return dirinboz_gpio_set_stage_ram;
+}
---
+++ src/mainboard/google/zork/variants/gumboz/variant.c
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/variants.h>
+#include <console/console.h>
+#include <device/device.h>
+#include <drivers/i2c/generic/chip.h>
+#include <soc/pci_devs.h>
+#include <ec/google/chromeec/ec.h>
+#include <ec/google/chromeec/i2c_tunnel/chip.h>
+#include <string.h>
+
+/* FIXME: Comments seem to suggest these are not entirely correct. */
+static const fsp_ddi_descriptor non_hdmi_ddi_descriptors[] = {
+	{
+		// DDI0, DP0, eDP
+		.connector_type = EDP,
+		.aux_index = AUX1,
+		.hdp_index = HDP1
+	},
+	{
+		// DDI1, DP1, DB OPT2 USB-C1 / DB OPT3 MST hub
+		.connector_type = DP,
+		.aux_index = AUX2,
+		.hdp_index = HDP2
+	},
+	{
+		// DP2 pins not connected on Dali
+		// DDI2, DP3, USB-C0
+		.connector_type = DP,
+		.aux_index = AUX4,
+		.hdp_index = HDP4,
+	}
+};
+
+void variant_get_dxio_ddi_descriptors(const fsp_dxio_descriptor **dxio_descs,
+				      size_t *dxio_num,
+				      const fsp_ddi_descriptor **ddi_descs,
+				      size_t *ddi_num)
+{
+
+	*dxio_descs = baseboard_get_dxio_descriptors(dxio_num);
+	*ddi_descs = &non_hdmi_ddi_descriptors[0];
+	*ddi_num = ARRAY_SIZE(non_hdmi_ddi_descriptors);
+}

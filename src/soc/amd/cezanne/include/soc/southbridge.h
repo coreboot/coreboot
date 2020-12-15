02a5dddb01a6629aaf64fdc196b996883602a293
Zheng Bao <fishbaozi@gmail.com>
2020-12-15 22:16:51 +0800

    soc/amd/cezanne: Add SMI support
    Change-Id: I83b9a91cbab297d032292997a4d5768b89fe97dd
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Signed-off-by: Zheng Bao <fishbaozi@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/48645
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/cezanne/include/soc/southbridge.h
+++ src/soc/amd/cezanne/include/soc/southbridge.h
@@ -8,6 +8,7 @@
 /* Power management registers:  0xfed80300 or index/data at IO 0xcd6/cd7 */
 #define PWR_RESET_CFG			0x10
 #define   TOGGLE_ALL_PWR_GOOD		(1 << 1)
+#define PM_ACPI_SMI_CMD			0x6a
 
 /* IO 0xf0 NCP Error */
 #define   NCP_WARM_BOOT			(1 << 7) /* Write-once */

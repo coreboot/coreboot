b2a749d4acc25ddf07f754f8c95e48e809b7f425
Felix Singer <felixsinger@posteo.net>
2020-12-21 03:58:13 +0000

    Revert "mb/clevo/cml-u: drop duplicated configuration of UART pads"
    This reverts commit 1a0071c7115819302c7df3fa2c07b1ca971e515d.
    
    Reason for revert:
    UART pad configuration should not be done in common code, since it
    could cause short circuits if the user configures a wrong UART index.
    
    Change-Id: I6022935eaab748f82c6330be0729ff72f4880493
    Signed-off-by: Felix Singer <felixsinger@posteo.net>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/48328
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>

---
+++ src/mainboard/clevo/cml-u/bootblock.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <bootblock_common.h>
+#include <gpio.h>
+#include <variant/gpio.h>
+
+void bootblock_mainboard_init(void)
+{
+	variant_configure_early_gpios();
+}

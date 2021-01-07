cb18524b4a789195c78490fea9398178d4d043fe
Matt DeVillier <matt.devillier@puri.sm>
2021-01-07 09:46:48 -0600

    mb/purism/librem_cnl: Update HDA verbs for Librem Mini
    Disable all NIDs other than those for the front combo jack.
    Adjust attributes to match jack physical location, appearance, etc.
    Correct group number for verbs for HDMI output.
    
    Test: run hdajackretask, verify NID characteristics correct for each
    verb. Verify headphone detection and output functional.
    
    Change-Id: If9fca5d9795d56bd38c8ea47f8de985c14ac8fab
    Signed-off-by: Matt DeVillier <matt.devillier@puri.sm>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49464
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/purism/librem_cnl/variants/librem_mini/hda_verb.c
+++ src/mainboard/purism/librem_cnl/variants/librem_mini/hda_verb.c
@@ -5,18 +5,19 @@
 const u32 cim_verb_data[] = {
 	0x10ec0269,	/* Codec Vendor/Device ID: Realtek ALC293 */
 	0x10ec0000,	/* Subsystem ID */
-	10,		/* Number of entries */
+	11,		/* Number of entries */
 
 	AZALIA_SUBVENDOR(0, 0x10ec0000),
-	AZALIA_PIN_CFG(0, 0x12, 0x40000000),
-	AZALIA_PIN_CFG(0, 0x15, 0x01214010),
-	AZALIA_PIN_CFG(0, 0x17, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x18, 0x01a19130),
-	AZALIA_PIN_CFG(0, 0x19, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x1a, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x1b, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x1d, 0x40422201),
-	AZALIA_PIN_CFG(0, 0x1e, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x12, 0x411111f0), /* NC */
+	AZALIA_PIN_CFG(0, 0x14, 0x411111f0), /* NC */
+	AZALIA_PIN_CFG(0, 0x15, 0x02211010), /* Jack analog out */
+	AZALIA_PIN_CFG(0, 0x17, 0x411111f0), /* NC */
+	AZALIA_PIN_CFG(0, 0x18, 0x02a11020), /* Jack analog mic */
+	AZALIA_PIN_CFG(0, 0x19, 0x411111f0), /* NC */
+	AZALIA_PIN_CFG(0, 0x1a, 0x411111f0), /* NC */
+	AZALIA_PIN_CFG(0, 0x1b, 0x411111f0), /* NC */
+	AZALIA_PIN_CFG(0, 0x1d, 0x411111f0), /* NC */
+	AZALIA_PIN_CFG(0, 0x1e, 0x411111f0), /* NC */
 
 	0x8086280b,	/* Codec Vendor/Device ID: Intel CannonPoint HDMI */
 	0x80860101,	/* Subsystem ID */
@@ -24,8 +25,8 @@ const u32 cim_verb_data[] = {
 
 	AZALIA_SUBVENDOR(2, 0x80860101),
 	AZALIA_PIN_CFG(2, 0x05, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x06, 0x18560020),
-	AZALIA_PIN_CFG(2, 0x07, 0x18560030),
+	AZALIA_PIN_CFG(2, 0x06, 0x18560010),
+	AZALIA_PIN_CFG(2, 0x07, 0x18560010),
 };
 
 const u32 pc_beep_verbs[] = {};

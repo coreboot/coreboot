91ef92525d8a9a0e83be8d91eb5e83b1cab58008
Felix Held <felix-coreboot@felixheld.de>
2021-01-12 23:44:05 +0100

    soc/amd/stoneyridge: use SOC_AMD_COMMON_BLOCK_UART
    Since the functions that get called by the coreboot console
    initialization code aren't in the SOC-specific code anymore, the SOC's
    uart.c can be included unconditionally in the build now. This also
    replaces the STONEYRIDGE_UART Kconfig option with the common
    AMD_SOC_CONSOLE_UART one.
    
    Change-Id: I09c15566a402895d6388715e8e5a802dc3c94fdd
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49375
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/stoneyridge/bootblock.c
+++ src/soc/amd/stoneyridge/bootblock.c
@@ -89,7 +89,7 @@ void bootblock_soc_early_init(void)
 
 void bootblock_soc_init(void)
 {
-	if (CONFIG(STONEYRIDGE_UART))
+	if (CONFIG(AMD_SOC_CONSOLE_UART))
 		assert(CONFIG_UART_FOR_CONSOLE >= 0
 					&& CONFIG_UART_FOR_CONSOLE <= 1);
 
--- src/soc/amd/stoneyridge/uart.c
+++ src/soc/amd/stoneyridge/uart.c
@@ -1,17 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <console/uart.h>
+#include <amdblocks/uart.h>
 #include <soc/southbridge.h>
+#include <types.h>
 
-uintptr_t uart_platform_base(unsigned int idx)
+uintptr_t get_uart_base(unsigned int idx)
 {
 	if (CONFIG_UART_FOR_CONSOLE < 0 || CONFIG_UART_FOR_CONSOLE > 1)
 		return 0;
 
 	return (uintptr_t)(APU_UART0_BASE + 0x2000 * (idx & 1));
-}
-
-unsigned int uart_platform_refclk(void)
-{
-	return 48000000;
 }

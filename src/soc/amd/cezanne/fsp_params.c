8d0a609e6d1bfb48de781e7223f73ff979d0ce2e
Felix Held <felix-coreboot@felixheld.de>
2021-01-14 01:40:50 +0100

    soc,vendorcode/amd/cezanne: add basic FSP integration
    This is a trimmed-down version of the Cezanne FSP integration code, so
    for example the UPD definitions are empty, which will be addressed
    later. Since coreboot just leaves the UPD values at their default, this
    is not a problem during the initial platform bring-up.
    
    Change-Id: Ie0fc30120c2455aa2160708251e9d2f229984305
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49445
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

---
+++ src/soc/amd/cezanne/fsp_params.c
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <fsp/api.h>
+
+void platform_fsp_silicon_init_params_cb(FSPS_UPD *supd)
+{
+}
--- src/soc/amd/cezanne/romstage.c
+++ src/soc/amd/cezanne/romstage.c
@@ -1,6 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <arch/cpu.h>
+#include <fsp/api.h>
+
+void platform_fsp_memory_init_params_cb(FSPM_UPD *mupd, uint32_t version)
+{
+}
 
 asmlinkage void car_stage_entry(void)
 {

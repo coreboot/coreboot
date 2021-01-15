5e6e5c11c7841d5a1df8e7a33466df1a8a2a6314
Wisley Chen <wisley.chen@quantatw.com>
2021-01-15 13:48:38 +0800

    mb/google/dedede/var/drawcia: Add support to handle pen detection
    For board version 6 afterward, it will have external pull-up for
    GPP_C12, and remove internal pull-up.
    
    BUG=b:177618684
    TEST=emerge-dedede coreboot, check evtest if SW_PEN_INSERTED event
    (value:1/0) when insert/eject pen, and eject pen to wake system from s0ix
    
    Signed-off-by: Wisley Chen <wisley.chen@quantatw.com>
    Change-Id: I503873afb48384168dcd8a822c7246655898356e
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49469
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Henry Sun <henrysun@google.com>

---
+++ src/mainboard/google/dedede/variants/drawcia/gpio.c
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <fw_config.h>
+#include <console/console.h>
+#include <ec/google/chromeec/ec.h>
+
+/* Pad configuration in ramstage*/
+static const struct pad_config pre_board6_gpio_table[] = {
+
+	/* C12 : AP_PEN_DET_ODL */
+	PAD_CFG_GPI_GPIO_DRIVER(GPP_C12, UP_20K, DEEP),
+};
+
+/* bid6: Pad configuration for board version 6 afterward in ramstage*/
+static const struct pad_config board6_gpio_table[] = {
+
+	/* C12 : AP_PEN_DET_ODL has an external pull-up and hence no pad termination.*/
+	PAD_CFG_GPI_GPIO_DRIVER(GPP_C12, NONE, DEEP),
+};
+
+const struct pad_config *variant_override_gpio_table(size_t *num)
+{
+	uint32_t board_version;
+	*num = ARRAY_SIZE(pre_board6_gpio_table);
+
+	if (!google_chromeec_cbi_get_board_version(&board_version)) {
+		if (board_version >= 6) {
+			*num = ARRAY_SIZE(board6_gpio_table);
+			return board6_gpio_table;
+		}
+	}
+
+	return pre_board6_gpio_table;
+}

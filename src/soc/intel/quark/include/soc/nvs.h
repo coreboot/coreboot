e05f81a3e0e51e7e1d8b907399a6ac887cee37fe
Kyösti Mälkki <kyosti.malkki@gmail.com>
2021-01-15 22:31:33 +0200

    soc/intel/quark: Add pwrs in <soc/nvs.h>
    For the time being every soc/intel selects ACPI_SOC_NVS
    and pwrs is a required field for the common initialisation
    implementation of followup work.
    
    Change-Id: I4a0c7eb35f0646898e49fad15c6448607c398731
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49493
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/quark/include/soc/nvs.h
+++ src/soc/intel/quark/include/soc/nvs.h
@@ -7,6 +7,7 @@
 
 struct __packed global_nvs {
 	uint32_t	cbmc; /* 0x00 - 0x03 - coreboot Memory Console */
+	uint8_t		pwrs; /* 0x4 - Power state (AC = 1) */
 };
 
 #endif /* SOC_INTEL_QUARK_NVS_H */

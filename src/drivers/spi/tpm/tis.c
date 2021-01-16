d0a62c667da913e84b092fa5174b90687e739863
Elyes HAOUAS <ehaouas@noos.fr>
2021-01-16 17:30:33 +0100

    drivers/spi/tpm/tis.c: Use __func__
    Change-Id: I2941d4480a7c88b6c020a9da584135a0030fccfe
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49551
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Christian Walter <christian.walter@9elements.com>

--- src/drivers/spi/tpm/tis.c
+++ src/drivers/spi/tpm/tis.c
@@ -31,7 +31,7 @@ static const char *tis_get_dev_name(struct tpm2_info *info)
 int tis_open(void)
 {
 	if (tpm_is_open) {
-		printk(BIOS_ERR, "tis_open() called twice.\n");
+		printk(BIOS_ERR, "%s() called twice.\n", __func__);
 		return -1;
 	}
 	return 0;

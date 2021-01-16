e167c387758264f82ace017f68abaa79baa3c3d4
Elyes HAOUAS <ehaouas@noos.fr>
2021-01-16 17:31:07 +0100

    mainboard/intel/strago/ec.c: Use __func__
    Change-Id: Ifa30e9d2a71eae9a438e84367fd8b4f8bd920983
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49555
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by:  Felix Singer <felixsinger@posteo.net>
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>

--- src/mainboard/intel/strago/ec.c
+++ src/mainboard/intel/strago/ec.c
@@ -15,7 +15,7 @@ void mainboard_ec_init(void)
 		.s5_wake_events = MAINBOARD_EC_S5_WAKE_EVENTS,
 	};
 
-	printk(BIOS_DEBUG, "mainboard_ec_init\n");
+	printk(BIOS_DEBUG, "%s\n", __func__);
 	post_code(0xf0);
 
 	google_chromeec_events_init(&info, acpi_is_wakeup_s3());

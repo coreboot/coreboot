b45219e720dce03b9815e82023245331b222753f
Elyes HAOUAS <ehaouas@noos.fr>
2021-01-16 17:30:19 +0100

    drivers/i2c/tpm/tpm.c: Use __func__
    Change-Id: I28f976118a380ef05a98257e9d57aadc26b69cb5
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49549
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Christian Walter <christian.walter@9elements.com>

--- src/drivers/i2c/tpm/tpm.c
+++ src/drivers/i2c/tpm/tpm.c
@@ -365,7 +365,7 @@ static int tpm_tis_i2c_recv(struct tpm_chip *chip, uint8_t *buf, size_t count)
 	/* read first 10 bytes, including tag, paramsize, and result */
 	size = recv_data(chip, buf, TPM_HEADER_SIZE);
 	if (size < TPM_HEADER_SIZE) {
-		printk(BIOS_DEBUG, "tpm_tis_i2c_recv: Unable to read header\n");
+		printk(BIOS_DEBUG, "%s: Unable to read header\n", __func__);
 		goto out;
 	}
 
@@ -379,15 +379,14 @@ static int tpm_tis_i2c_recv(struct tpm_chip *chip, uint8_t *buf, size_t count)
 	size += recv_data(chip, &buf[TPM_HEADER_SIZE],
 				expected - TPM_HEADER_SIZE);
 	if (size < expected) {
-		printk(BIOS_DEBUG, "tpm_tis_i2c_recv: Unable to "
-			"read remainder of result\n");
+		printk(BIOS_DEBUG, "%s: Unable to read remainder of result\n", __func__);
 		size = -1;
 		goto out;
 	}
 
 	wait_for_stat(chip, TPM_STS_VALID, &status);
 	if (status & TPM_STS_DATA_AVAIL) {	/* retry? */
-		printk(BIOS_DEBUG, "tpm_tis_i2c_recv: Error left over data\n");
+		printk(BIOS_DEBUG, "%s: Error left over data\n", __func__);
 		size = -1;
 		goto out;
 	}

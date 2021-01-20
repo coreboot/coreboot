84a37b9d55e8da6137bfeb9d2e866ae4273983a8
Angel Pons <th3fanbus@gmail.com>
2021-01-20 01:33:47 +0100

    nb/intel/ironlake: Avoid pointer arithmetics
    Drop casts to prevent pointer arithmetic and for consistency with other
    platforms. These macros will be factored out in a subsequent commit.
    
    Change-Id: I959e7378a8bf46fd1772192090a751d7a2f6f470
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49747
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/northbridge/intel/ironlake/ironlake.h
+++ src/northbridge/intel/ironlake/ironlake.h
@@ -88,9 +88,9 @@
  * MCHBAR
  */
 
-#define MCHBAR8(x)			(*((volatile u8  *)((u8 *)DEFAULT_MCHBAR + (x))))
-#define MCHBAR16(x)			(*((volatile u16 *)((u8 *)DEFAULT_MCHBAR + (x))))
-#define MCHBAR32(x)			(*((volatile u32 *)((u8 *)DEFAULT_MCHBAR + (x))))
+#define MCHBAR8(x)			(*((volatile u8  *)(DEFAULT_MCHBAR + (x))))
+#define MCHBAR16(x)			(*((volatile u16 *)(DEFAULT_MCHBAR + (x))))
+#define MCHBAR32(x)			(*((volatile u32 *)(DEFAULT_MCHBAR + (x))))
 #define MCHBAR8_AND(x,  and)		(MCHBAR8(x)  = MCHBAR8(x)  & (and))
 #define MCHBAR16_AND(x, and)		(MCHBAR16(x) = MCHBAR16(x) & (and))
 #define MCHBAR32_AND(x, and)		(MCHBAR32(x) = MCHBAR32(x) & (and))
@@ -104,9 +104,9 @@
  * EPBAR - Egress Port Root Complex Register Block
  */
 
-#define EPBAR8(x)	(*((volatile u8  *)((u8 *)DEFAULT_EPBAR + (x))))
-#define EPBAR16(x)	(*((volatile u16 *)((u8 *)DEFAULT_EPBAR + (x))))
-#define EPBAR32(x)	(*((volatile u32 *)((u8 *)DEFAULT_EPBAR + (x))))
+#define EPBAR8(x)	(*((volatile u8  *)(DEFAULT_EPBAR + (x))))
+#define EPBAR16(x)	(*((volatile u16 *)(DEFAULT_EPBAR + (x))))
+#define EPBAR32(x)	(*((volatile u32 *)(DEFAULT_EPBAR + (x))))
 
 #include "registers/epbar.h"
 
@@ -114,9 +114,9 @@
  * DMIBAR
  */
 
-#define DMIBAR8(x)	(*((volatile u8  *)((u8 *)DEFAULT_DMIBAR + (x))))
-#define DMIBAR16(x)	(*((volatile u16 *)((u8 *)DEFAULT_DMIBAR + (x))))
-#define DMIBAR32(x)	(*((volatile u32 *)((u8 *)DEFAULT_DMIBAR + (x))))
+#define DMIBAR8(x)	(*((volatile u8  *)(DEFAULT_DMIBAR + (x))))
+#define DMIBAR16(x)	(*((volatile u16 *)(DEFAULT_DMIBAR + (x))))
+#define DMIBAR32(x)	(*((volatile u32 *)(DEFAULT_DMIBAR + (x))))
 
 #include "registers/dmibar.h"
 

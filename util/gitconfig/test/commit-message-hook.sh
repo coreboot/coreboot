e3ecc2964ed494c8e020d7bc67254cdc050360e9
Patrick Georgi <pgeorgi@google.com>
2021-01-27 12:18:04 +0100

    gitconfig/test: Adapt test to current tree layout
    The test expects a README file to exist under revision control, but we
    converted it to markdown, together with a rename over 2 years ago in
    commit ee8780eb7880477.
    
    Change-Id: I7768e116a10cb373ca35fa1c874a5949dabaa111
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/49982
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- util/gitconfig/test/commit-message-hook.sh
+++ util/gitconfig/test/commit-message-hook.sh
@@ -55,7 +55,7 @@ git clone "$(git rev-parse --show-toplevel)" "${clone_dir}" \
 	# test
 	echo "good case..."
 	log_file="${base_dir}/good_case.log"
-	echo "this is a test" >> README
+	echo "this is a test" >> README.md
 	timeout 4m git commit --all --signoff --message="good case" \
 		1>"${log_file}" 2>&1 \
 		|| check_exit_code positive "${log_file}"

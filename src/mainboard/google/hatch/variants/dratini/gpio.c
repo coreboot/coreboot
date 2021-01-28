fa21c922c6d963e93db5aeeec77229c4772f33be
Ricardo Ribalda <ribalda@chromium.org>
2021-01-28 13:17:47 +0000

    Revert "mb/google/hatch/dratini: Describe the privacy_gpio"
    This reverts commit f41645c34d8b22a3d887abd56138ae794fc2dfa5.
    
    In Dragonair, during the MP stage, one of the resistors needed
    for this functionality has been removed. This results in the
    privacy-switch not readable back by the system.
    
    BUG=b:178458332
    
    Change-Id: I0781f338d5ecd89fccee613fb13ea25c59385625
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50044
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/google/hatch/variants/dratini/gpio.c
+++ src/mainboard/google/hatch/variants/dratini/gpio.c
@@ -16,8 +16,6 @@ static const struct pad_config gpio_table[] = {
 	PAD_NC(GPP_A19, NONE),
 	/* C12 : FPMCU_PCH_BOOT1 */
 	PAD_CFG_GPO(GPP_C12, 0, DEEP),
-	/* D4  : Camera Privacy Status */
-	PAD_CFG_GPI_INT(GPP_D4, NONE, PLTRST, EDGE_BOTH),
 	/* F1  : NC */
 	PAD_NC(GPP_F1, NONE),
 	/* F3  : MEM_STRAP_3 */

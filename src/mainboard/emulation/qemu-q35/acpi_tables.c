2399adaeb0000966c620cf17df82797290d47ef4
Angel Pons <th3fanbus@gmail.com>
2021-01-28 12:06:39 +0100

    mb/emulation/qemu-q35: Use common MADT
    Select SOUTHBRIDGE_INTEL_COMMON_ACPI_MADT and drop the `acpi_fill_madt`
    function definition, which is redundant. Tested, still boots to payload.
    
    Change-Id: I6ba448f264a478e7ef060ea1dfbf5016a310d528
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50026
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/mainboard/emulation/qemu-q35/acpi_tables.c
+++ src/mainboard/emulation/qemu-q35/acpi_tables.c
@@ -1,12 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <types.h>
 #include <acpi/acpi.h>
-#include <arch/ioapic.h>
-#include <arch/smp/mpspec.h>
-#include <device/device.h>
-#include <device/pci_ops.h>
-#include <version.h>
 
 #include "../qemu-i440fx/fw_cfg.h"
 #include "../qemu-i440fx/acpi.h"
@@ -17,24 +11,6 @@ void mainboard_fill_fadt(acpi_fadt_t *fadt)
 	fadt->smi_cmd = 0;
 	fadt->acpi_enable = 0;
 	fadt->acpi_disable = 0;
-}
-
-unsigned long acpi_fill_madt(unsigned long current)
-{
-	/* Local APICs */
-	current = acpi_create_madt_lapics(current);
-
-	/* IOAPIC */
-	current += acpi_create_madt_ioapic((acpi_madt_ioapic_t *) current,
-				2, IO_APIC_ADDR, 0);
-
-	/* INT_SRC_OVR */
-	current += acpi_create_madt_irqoverride((acpi_madt_irqoverride_t *)
-		 current, 0, 0, 2, 0);
-	current += acpi_create_madt_irqoverride((acpi_madt_irqoverride_t *)
-		 current, 0, 9, 9, MP_IRQ_TRIGGER_LEVEL | MP_IRQ_POLARITY_HIGH);
-
-	return current;
 }
 
 unsigned long acpi_fill_mcfg(unsigned long current)

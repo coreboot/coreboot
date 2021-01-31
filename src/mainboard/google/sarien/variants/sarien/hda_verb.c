6d7a6d291d6dd04c2fc5ca0dba2b3ccb032c9ff4
Angel Pons <th3fanbus@gmail.com>
2021-01-31 13:09:01 +0100

    mb/google/sarien: Turn `hda_verb.h` into `hda_verb.c`
    Change-Id: I40c8145fdddf9605bc3cc66ae8075e52dca4e539
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50175
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/mainboard/google/sarien/variants/sarien/hda_verb.c
+++ src/mainboard/google/sarien/variants/sarien/hda_verb.c
@@ -1,8 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#ifndef MAINBOARD_HDA_VERB_H
-#define MAINBOARD_HDA_VERB_H
-
 #include <device/azalia_device.h>
 
 const u32 cim_verb_data[] = {
@@ -135,5 +132,3 @@ const u32 pc_beep_verbs[] = {
 };
 
 AZALIA_ARRAY_SIZES;
-
-#endif

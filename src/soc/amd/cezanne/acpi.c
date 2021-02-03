86024954dfa06af9a9a7e5f19ab5aae32eb14909
Felix Held <felix-coreboot@felixheld.de>
2021-02-03 23:44:28 +0100

    soc/amd/cezanne: select ACPI support and make the compiler happy
    Follow-up patches will add more functionality.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Signed-off-by: Raul E Rangel <rrangel@chromium.org>
    Change-Id: I9b806569154e46418fa7d4fa35575a0acfec9132
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50273
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>

---
+++ src/soc/amd/cezanne/acpi.c
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/* ACPI - create the Fixed ACPI Description Tables (FADT) */
+
+#include <acpi/acpi.h>
+
+unsigned long acpi_fill_madt(unsigned long current)
+{
+	/* TODO */
+	return current;
+}
+
+/*
+ * Reference section 5.2.9 Fixed ACPI Description Table (FADT)
+ * in the ACPI 3.0b specification.
+ */
+void acpi_fill_fadt(acpi_fadt_t *fadt)
+{
+	/* TODO */
+}

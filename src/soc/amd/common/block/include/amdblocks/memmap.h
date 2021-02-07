b743178ab3012aacc8210667d664c1098244cec9
Felix Held <felix-coreboot@felixheld.de>
2021-02-07 20:25:54 +0100

    soc/amd/common/memmap: add comment about types in memmap_early_dram
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reported-by: Angel Pons <th3fanbus@gmail.com>
    Change-Id: I295bfcb05571492adbe81ffc579a835be4abffe1
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50370
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/soc/amd/common/block/include/amdblocks/memmap.h
+++ src/soc/amd/common/block/include/amdblocks/memmap.h
@@ -9,6 +9,7 @@
 DECLARE_REGION(early_reserved_dram)
 
 struct memmap_early_dram {
+	/* fixed size types, so the layout in CBMEM won't change for 32 vs. 64 bit stages */
 	uint32_t base;
 	uint32_t size;
 };

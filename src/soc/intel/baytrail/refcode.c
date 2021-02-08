coreboot/coreboot

commit 0322bc5ed858c65f7d42340d9af144d4838151d4

src: Remove unused <cbmem.h>
Change-Id: I2279e2d7e6255a88953b2485c1f1a3b51a72c65e
Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/50182
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Patrick Georgi <pgeorgi@google.com>

src/soc/intel/baytrail/refcode.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <acpi/acpi.h>
-#include <cbmem.h>
 #include <console/console.h>
 #include <console/streams.h>
 #include <cpu/x86/tsc.h>

src/soc/intel/broadwell/refcode.c
@@ -2,7 +2,6 @@
 
 #include <string.h>
 #include <acpi/acpi.h>
-#include <cbmem.h>
 #include <console/console.h>
 #include <console/streams.h>
 #include <program_loading.h>

src/lib/metadata_hash.c
@@ -2,7 +2,6 @@
 /* This file is part of the coreboot project. */
 
 #include <assert.h>
-#include <cbmem.h>
 #include <metadata_hash.h>
 #include <symbols.h>
 

src/security/vboot/vboot_loader.c
@@ -2,7 +2,6 @@
 
 #include <boot_device.h>
 #include <cbfs.h>
-#include <cbmem.h>
 #include <commonlib/bsd/cbfs_private.h>
 #include <console/console.h>
 #include <ec/google/chromeec/ec.h>

src/soc/amd/common/block/lpc/lpc.c
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <cbmem.h>
 #include <console/console.h>
 #include <device/device.h>
 #include <device/pci.h>

src/soc/amd/common/block/pi/refcode_loader.c
@@ -2,7 +2,6 @@
 
 #include <acpi/acpi.h>
 #include <cbfs.h>
-#include <cbmem.h>
 #include <console/console.h>
 #include <rmodule.h>
 #include <stage_cache.h>

src/soc/intel/alderlake/acpi.c
@@ -5,7 +5,6 @@
 #include <acpi/acpigen.h>
 #include <device/mmio.h>
 #include <arch/smp/mpspec.h>
-#include <cbmem.h>
 #include <console/console.h>
 #include <device/device.h>
 #include <device/pci_ops.h>

src/soc/intel/apollolake/acpi.c
@@ -9,7 +9,6 @@
 #include <arch/smp/mpspec.h>
 #include <assert.h>
 #include <device/pci_ops.h>
-#include <cbmem.h>
 #include <gpio.h>
 #include <intelblocks/acpi.h>
 #include <intelblocks/pmclib.h>

commit e4c8dc8bb14e4831ba037fefb566e947e792baea

soc/intel/{baytrail,broadwell}: Add missing <cbmem.h>
Change-Id: Ic4e55f8233e5cb5cea575ad0581adf457a45ba9a
Fixes: commit 0322bc5ed8 (src: Remove unused <cbmem.h>)
Signed-off-by: Angel Pons <th3fanbus@gmail.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/50375
Reviewed-by: Patrick Georgi <pgeorgi@google.com>
Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/soc/intel/baytrail/refcode.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <acpi/acpi.h>
+#include <cbmem.h>
 #include <console/console.h>
 #include <console/streams.h>
 #include <cpu/x86/tsc.h>

src/soc/intel/broadwell/refcode.c
@@ -2,6 +2,7 @@
 
 #include <string.h>
 #include <acpi/acpi.h>
+#include <cbmem.h>
 #include <console/console.h>
 #include <console/streams.h>
 #include <program_loading.h>
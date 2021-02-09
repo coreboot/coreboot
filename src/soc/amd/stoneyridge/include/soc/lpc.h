f87427f1a4303d6cab26f29641831b4d3dec8451
Raul E Rangel <rrangel@chromium.org>
2021-02-09 10:54:57 -0700

    soc/amd/stoneyridge: Add SPI registers
    This is a copy/paste of amdblocks/lpc.h. The registers are different for
    picasso and cezanne, so I'm moving them to soc.
    
    Signed-off-by: Raul E Rangel <rrangel@chromium.org>
    Change-Id: I4dfadcdc025d3581cb1423e9793a9b2181742b9e
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50443
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>

---
+++ src/soc/amd/stoneyridge/include/soc/lpc.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef AMD_STONEYRIDGE_LPC_H
+#define AMD_STONEYRIDGE_LPC_H
+
+#define SPIROM_BASE_ADDRESS_REGISTER	0xa0
+#define   SPI_BASE_ALIGNMENT		BIT(6)
+#define   SPI_BASE_RESERVED		(BIT(4) | BIT(5))
+#define   ROUTE_TPM_2_SPI		BIT(3)
+#define   SPI_ABORT_ENABLE		BIT(2)
+#define   SPI_ROM_ENABLE		BIT(1)
+#define   SPI_ROM_ALT_ENABLE		BIT(0)
+#define   SPI_PRESERVE_BITS		(BIT(0) | BIT(1) | BIT(2) | BIT(3))
+
+#endif /* AMD_STONEYRIDGE_LPC_H */

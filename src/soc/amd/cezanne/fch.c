a6529e789f5c460c1b378b3194e795ceb32a5171
Raul E Rangel <rrangel@chromium.org>
2021-02-09 14:38:36 -0700

    soc/amd/cezanne: Add PCI IRQ Router definitions
    These definitions were identical to picasso. The only thing I changed
    was that I renamed Misc1 and Misc2 to HPET_L and HPET_H.
    
    This change still doesn't write the PCI_IRQ register for all the PCI
    devices. We need to refactor the picasso pci_gpp code first.
    
    TEST=Boot majolica and see FCH IRQs being programmed.
    
    Signed-off-by: Raul E Rangel <rrangel@chromium.org>
    Change-Id: Ic7e637f234d3af426959a9bbd82a0dcf25bb3c8e
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50451
    Reviewed-by: Mathew King <mathewk@chromium.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/cezanne/fch.c
+++ src/soc/amd/cezanne/fch.c
@@ -1,12 +1,72 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <amdblocks/acpimmio.h>
+#include <amdblocks/amd_pci_util.h>
 #include <amdblocks/smi.h>
 #include <assert.h>
+#include <bootstate.h>
 #include <cpu/x86/smm.h>
+#include <soc/amd_pci_int_defs.h>
 #include <soc/iomap.h>
 #include <soc/smi.h>
 #include <soc/southbridge.h>
+
+/*
+ * Table of APIC register index and associated IRQ name. Using IDX_XXX_NAME
+ * provides a visible association with the index, therefore helping
+ * maintainability of table. If a new index/name is defined in
+ * amd_pci_int_defs.h, just add the pair at the end of this table.
+ * Order is not important.
+ */
+const static struct irq_idx_name irq_association[] = {
+	{ PIRQ_A,	"INTA#" },
+	{ PIRQ_B,	"INTB#" },
+	{ PIRQ_C,	"INTC#" },
+	{ PIRQ_D,	"INTD#" },
+	{ PIRQ_E,	"INTE#" },
+	{ PIRQ_F,	"INTF#/GENINT2" },
+	{ PIRQ_G,	"INTG#" },
+	{ PIRQ_H,	"INTH#" },
+	{ PIRQ_MISC,	"Misc" },
+	{ PIRQ_MISC0,	"Misc0" },
+	{ PIRQ_HPET_L,	"HPET_L" },
+	{ PIRQ_HPET_H,	"HPET_H" },
+	{ PIRQ_SIRQA,	"Ser IRQ INTA" },
+	{ PIRQ_SIRQB,	"Ser IRQ INTB" },
+	{ PIRQ_SIRQC,	"Ser IRQ INTC" },
+	{ PIRQ_SIRQD,	"Ser IRQ INTD" },
+	{ PIRQ_SCI,	"SCI" },
+	{ PIRQ_SMBUS,	"SMBUS" },
+	{ PIRQ_ASF,	"ASF" },
+	{ PIRQ_PMON,	"PerMon" },
+	{ PIRQ_SD,	"SD" },
+	{ PIRQ_SDIO,	"SDIO" },
+	{ PIRQ_CIR,	"CIR" },
+	{ PIRQ_GPIOA,	"GPIOa" },
+	{ PIRQ_GPIOB,	"GPIOb" },
+	{ PIRQ_GPIOC,	"GPIOc" },
+	{ PIRQ_SATA,	"SATA" },
+	{ PIRQ_EMMC,	"eMMC" },
+	{ PIRQ_GPP0,	"GPP0" },
+	{ PIRQ_GPP1,	"GPP1" },
+	{ PIRQ_GPP2,	"GPP2" },
+	{ PIRQ_GPP3,	"GPP3" },
+	{ PIRQ_GPIO,	"GPIO" },
+	{ PIRQ_I2C0,	"I2C0" },
+	{ PIRQ_I2C1,	"I2C1" },
+	{ PIRQ_I2C2,	"I2C2" },
+	{ PIRQ_I2C3,	"I2C3" },
+	{ PIRQ_UART0,	"UART0" },
+	{ PIRQ_UART1,	"UART1" },
+	{ PIRQ_I2C4,	"I2C4" },
+	{ PIRQ_I2C5,	"I2C5" },
+};
+
+const struct irq_idx_name *sb_get_apic_reg_association(size_t *size)
+{
+	*size = ARRAY_SIZE(irq_association);
+	return irq_association;
+}
 
 static void fch_init_acpi_ports(void)
 {
@@ -57,3 +117,15 @@ void fch_init(void *chip_info)
 void fch_final(void *chip_info)
 {
 }
+
+static void set_pci_irqs(void *unused)
+{
+	/* Write PCI_INTR regs 0xC00/0xC01 */
+	write_pci_int_table();
+}
+
+/*
+ * Hook this function into the PCI state machine
+ * on entry into BS_DEV_ENABLE.
+ */
+BOOT_STATE_INIT_ENTRY(BS_DEV_ENABLE, BS_ON_ENTRY, set_pci_irqs, NULL);

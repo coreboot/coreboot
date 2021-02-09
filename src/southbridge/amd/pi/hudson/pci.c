c64465f2123c6e68a1b1a0210890d02826139579
Angel Pons <th3fanbus@gmail.com>
2021-02-09 11:18:39 +0100

    sb/amd/pi/hudson/pci.c: Remove empty init operation
    Change-Id: I5620867b3044936be8ad1bf95255be5a3565bb51
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50422
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/southbridge/amd/pi/hudson/pci.c
+++ src/southbridge/amd/pi/hudson/pci.c
@@ -7,10 +7,6 @@
 #include <southbridge/amd/common/amd_pci_util.h>
 #include <bootstate.h>
 
-static void pci_init(struct device *dev)
-{
-}
-
 /*
  * Update the PCI devices with a valid IRQ number
  * that is set in the mainboard PCI_IRQ structures.
@@ -38,7 +34,6 @@ static struct device_operations pci_ops = {
 	.read_resources = pci_bus_read_resources,
 	.set_resources = pci_dev_set_resources,
 	.enable_resources = pci_bus_enable_resources,
-	.init = pci_init,
 	.scan_bus = pci_scan_bridge,
 	.reset_bus = pci_bus_reset,
 	.ops_pci = &lops_pci,

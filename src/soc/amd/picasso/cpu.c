coreboot/coreboot

commit 5dbe45e0f5608794e03634aed28530ddb2ab9ac5

soc/amd/picasso: Skip SmmInfo to PSP on S3 resume
The PSP does not accept the SmmInfo command during a resume so
remove the call.

BUG=b:163017485
TEST=Run SST on trembyle, verify error message goes away
BRANCH=Zork

Signed-off-by: Marshall Dawson <marshalldawson3rd@gmail.com>
Change-Id: Ib75a20c9594bc331aa7abf77be95196085a3dbc6
Reviewed-on: https://review.coreboot.org/c/coreboot/+/44398
Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/soc/amd/picasso/smi.c
@@ -4,6 +4,7 @@
  * Utilities for SMM setup
  */
 
+#include <acpi/acpi.h>
 #include <arch/io.h>
 #include <console/console.h>
 #include <cpu/x86/smm.h>
@@ -19,5 +20,6 @@ void global_smi_enable(void)
 	reg |= SMITRG0_EOS;	/* Set EOS bit */
 	smi_write32(SMI_REG_SMITRIG0, reg);
 
-	outb(APM_CNT_SMMINFO, APM_CNT);
+	if (!acpi_is_wakeup_s3())
+		outb(APM_CNT_SMMINFO, APM_CNT);
 }

commit 2fec394913d2ef721623d796ac1db82d19ab5195

soc/amd/picasso: Move APM_CNT_SMMINFO callsite
Triggering SMI is not part of the semantics of global_smi_enable(), so
move it to the post_mp_init handler. Even without the !acpi_is_wakeup_s3
check we don't get PSP warnings/errors during resume, so we can drop the
workaround introduced in commit 5dbe45e0f5608794e03634aed28530ddb2ab9ac5
in this patch.

Change-Id: Id0e7723c2bb9811f80fe36c38199a01445dc1d7d
Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/42987
Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/soc/amd/picasso/cpu.c
@@ -81,12 +81,18 @@ static void relocation_handler(int cpu, uintptr_t curr_smbase,
 	smm_state->smbase = staggered_smbase;
 }
 
+static void post_mp_init(void)
+{
+	global_smi_enable();
+	apm_control(APM_CNT_SMMINFO);
+}
+
 static const struct mp_ops mp_ops = {
 	.pre_mp_init = pre_mp_init,
 	.get_cpu_count = get_cpu_count,
 	.get_smm_info = get_smm_info,
 	.relocation_handler = relocation_handler,
-	.post_mp_init = global_smi_enable,
+	.post_mp_init = post_mp_init,
 };
 
 void mp_init_cpus(struct bus *cpu_bus)

src/soc/amd/picasso/smi.c
@@ -4,8 +4,6 @@
  * Utilities for SMM setup
  */
 
-#include <acpi/acpi.h>
-#include <arch/io.h>
 #include <console/console.h>
 #include <cpu/x86/smm.h>
 #include <amdblocks/acpimmio.h>
@@ -19,7 +17,4 @@ void global_smi_enable(void)
 	reg &= ~SMITRG0_SMIENB;	/* Enable SMI generation */
 	reg |= SMITRG0_EOS;	/* Set EOS bit */
 	smi_write32(SMI_REG_SMITRIG0, reg);
-
-	if (!acpi_is_wakeup_s3())
-		outb(APM_CNT_SMMINFO, APM_CNT);
 }
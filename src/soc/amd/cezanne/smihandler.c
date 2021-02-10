3c4fd70d6168a664055245c4b4aead29006b968a
Felix Held <felix-coreboot@felixheld.de>
2021-02-10 03:47:38 +0100

    soc/amd/cezanne/smihandler: add missing southbridge_io_trap_handler
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I4140fbf93e84a2620ffb88e5c65df17b23135553
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50465
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Reviewed-by: Martin Roth <martinroth@google.com>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/cezanne/smihandler.c
+++ src/soc/amd/cezanne/smihandler.c
@@ -1,6 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <amdblocks/smm.h>
+#include <cpu/x86/smm.h>
+
+int southbridge_io_trap_handler(int smif)
+{
+	return 0;
+}
 
 void *get_smi_source_handler(int source)
 {

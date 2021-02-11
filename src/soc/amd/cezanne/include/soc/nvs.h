428a682abe7a0aaf1414d6c9d4f6c862f4c2b5fe
Felix Held <felix-coreboot@felixheld.de>
2021-02-11 20:05:40 +0100

    soc/amd/cezanne: drop PWRS from GNVS
    A copy of Picasso's include/nvs.h was added to Cezanne right before the
    commit d6ccbb9d48f97dd3bbd4b947fe3bc4857216a363 that removed it for the
    other mainboards and SoCs, so apply the equivalent change here as well
    to keep everything in sync.
    
    Change-Id: I76b551c05b3c3028a3afb3bc3b77df2401aed7a8
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50563
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/soc/amd/cezanne/include/soc/nvs.h
+++ src/soc/amd/cezanne/include/soc/nvs.h
@@ -15,7 +15,7 @@ struct __packed global_nvs {
 	/* Miscellaneous */
 	uint8_t		unused_was_pcnt; /* 0x00 - Processor Count */
 	uint8_t		lids; /* 0x01 - LID State */
-	uint8_t		pwrs; /* 0x02 - AC Power State */
+	uint8_t		unused_was_pwrs; /* 0x02 - AC Power State */
 	uint32_t	cbmc; /* 0x03 - 0x06 - coreboot Memory Console */
 	uint64_t	pm1i; /* 0x07 - 0x0e - System Wake Source - PM1 Index */
 	uint64_t	gpei; /* 0x0f - 0x16 - GPE Wake Source */

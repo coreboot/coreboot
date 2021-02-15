486eabce80ef8e17706d0e28d467b05b92d97ee0
Subrata Banik <subrata.banik@intel.com>
2021-02-15 21:45:20 +0530

    mb/intel/adlrvp: Early program SMBUS CLOCK and DATA
    TEST=Ensure SMBUS CLK/DATA GPIO is in NF1.
    
    Change-Id: Id615462cc21fc24e7ec6ef16274d784d41bd9bd4
    Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50746
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/mainboard/intel/adlrvp/early_gpio.c
+++ src/mainboard/intel/adlrvp/early_gpio.c
@@ -11,6 +11,10 @@ static const struct pad_config early_gpio_table[] = {
 	PAD_CFG_GPO(GPP_F14, 0, PLTRST),
 	/* WWAN_PWR_EN */
 	PAD_CFG_GPO(GPP_F21, 1, DEEP),
+	/* SMB_CLK */
+	PAD_CFG_NF(GPP_C0, NONE, DEEP, NF1),
+	/* SMB_DATA */
+	PAD_CFG_NF(GPP_C1, NONE, DEEP, NF1),
 };
 
 void variant_configure_early_gpio_pads(void)

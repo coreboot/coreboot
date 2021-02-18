30cb21811b0dc89f0bfe15cfb37a855e44efea14
Johnny Lin <johnny_lin@wiwynn.com>
2021-02-18 11:29:41 +0800

    vc/intel/fsp/fsp2_0/cooperlake_sp: Update memory map hob for WW06 FSP
    Change-Id: Id534e1b73e73bbb9d944c988d1ef66bc1f463eff
    Signed-off-by: Johnny Lin <johnny_lin@wiwynn.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50867
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Deomid "rojer" Ryabkov <rojer9@fb.com>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/vendorcode/intel/fsp/fsp2_0/cooperlake_sp/hob_memmap.h
+++ src/vendorcode/intel/fsp/fsp2_0/cooperlake_sp/hob_memmap.h
@@ -160,7 +160,7 @@ typedef struct SystemMemoryMapHob {
   UINT8    NumChPerMC;
   UINT8    numberEntries;                         // Number of Memory Map Elements
   SYSTEM_MEMORY_MAP_ELEMENT Element[(MAX_SOCKET * MAX_DRAM_CLUSTERS * MAX_SAD_RULES) + MAX_FPGA_REMOTE_SAD_RULES];
-  UINT8    reserved5[2249];
+  UINT8    reserved5[2253];
   MEMMAP_SOCKET Socket[MAX_SOCKET];
   UINT8    reserved6[1603];
 

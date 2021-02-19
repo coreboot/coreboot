43026ba81966a9f065435a68a6ca8bd69005fe19
Angel Pons <th3fanbus@gmail.com>
2021-02-19 21:57:31 +0100

    soc/intel/cannonlake: Drop unnecessary guard
    The MRC cache driver assumes BOOT_DEVICE_MEMORY_MAPPED=y already. This
    is to ease factoring out common code across seven Intel platforms.
    
    Change-Id: I0598cb18b456e10789b2a42792fbfa2639cdd2c4
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50951
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/cannonlake/bootblock/cpu.c
+++ src/soc/intel/cannonlake/bootblock/cpu.c
@@ -6,7 +6,5 @@
 void bootblock_cpu_init(void)
 {
 	/* Temporarily cache the memory-mapped boot media. */
-	if (CONFIG(BOOT_DEVICE_MEMORY_MAPPED) &&
-		CONFIG(BOOT_DEVICE_SPI_FLASH))
-		fast_spi_cache_bios_region();
+	fast_spi_cache_bios_region();
 }

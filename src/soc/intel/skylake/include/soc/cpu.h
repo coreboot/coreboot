ba4cfb504ca1e8246d1ea135dfb566c3db5835cb
Angel Pons <th3fanbus@gmail.com>
2021-02-20 00:06:58 +0100

    soc/intel/skylake: Remove unused macro in cpu.h
    Change-Id: I92c9c06c606215a4bd9b44b3b4b1f0acced8a252
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50962
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/skylake/include/soc/cpu.h
+++ src/soc/intel/skylake/include/soc/cpu.h
@@ -5,9 +5,6 @@
 
 #include <device/device.h>
 
-/* CPU types */
-#define SKYLAKE_FAMILY_ULT	0x406e0
-
 /* Latency times in units of 1024ns. */
 #define C_STATE_LATENCY_CONTROL_0_LIMIT	0x4e
 #define C_STATE_LATENCY_CONTROL_1_LIMIT	0x76

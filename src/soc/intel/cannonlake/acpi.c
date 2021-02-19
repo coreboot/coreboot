98f672a5ea793a86e0f66317893a3706fbd66102
Angel Pons <th3fanbus@gmail.com>
2021-02-19 19:42:10 +0100

    soc/intel: Factor out identical acpigen GPIO helpers
    Change-Id: I27f198d403f6ba05ba72ae0652da224d4cbf323a
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/50938
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/cannonlake/acpi.c
+++ src/soc/intel/cannonlake/acpi.c
@@ -206,43 +206,6 @@ int soc_madt_sci_irq_polarity(int sci)
 	return MP_IRQ_POLARITY_HIGH;
 }
 
-static int acpigen_soc_gpio_op(const char *op, unsigned int gpio_num)
-{
-	/* op (gpio_num) */
-	acpigen_emit_namestring(op);
-	acpigen_write_integer(gpio_num);
-	return 0;
-}
-
-static int acpigen_soc_get_gpio_state(const char *op, unsigned int gpio_num)
-{
-	/* Store (op (gpio_num), Local0) */
-	acpigen_write_store();
-	acpigen_soc_gpio_op(op, gpio_num);
-	acpigen_emit_byte(LOCAL0_OP);
-	return 0;
-}
-
-int acpigen_soc_read_rx_gpio(unsigned int gpio_num)
-{
-	return acpigen_soc_get_gpio_state("\\_SB.PCI0.GRXS", gpio_num);
-}
-
-int acpigen_soc_get_tx_gpio(unsigned int gpio_num)
-{
-	return acpigen_soc_get_gpio_state("\\_SB.PCI0.GTXS", gpio_num);
-}
-
-int acpigen_soc_set_tx_gpio(unsigned int gpio_num)
-{
-	return acpigen_soc_gpio_op("\\_SB.PCI0.STXS", gpio_num);
-}
-
-int acpigen_soc_clear_tx_gpio(unsigned int gpio_num)
-{
-	return acpigen_soc_gpio_op("\\_SB.PCI0.CTXS", gpio_num);
-}
-
 static unsigned long soc_fill_dmar(unsigned long current)
 {
 	struct device *const igfx_dev = pcidev_path_on_root(SA_DEVFN_IGD);

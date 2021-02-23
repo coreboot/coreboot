011e1b3fbc4d31feaa19ef34d3093f2d289357eb
chenzanxi <chenzanxi@huaqin.corp-partner.google.com>
2021-02-23 11:29:26 +0800

    mb/google/dedede/var/storo: Support LTE module
    Add LTE module support into devicetree and associated GPIO configuartion.
    
    BUG=b:177955524
    BRANCH=dedede
    TEST=LTE function is OK
    
    Change-Id: I9aff9608e08eae00ab5ac8547f63bc83b62fea78
    Signed-off-by: Zanxi Chen <chenzanxi@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51017
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

--- src/mainboard/google/dedede/variants/storo/gpio.c
+++ src/mainboard/google/dedede/variants/storo/gpio.c
@@ -5,8 +5,17 @@
 
 /* Pad configuration in ramstage*/
 static const struct pad_config gpio_table[] = {
+	/* A10 : WWAN_EN */
+	PAD_CFG_GPO(GPP_A10, 1, PWROK),
+
+	/* B7  : PCIE_CLKREQ2_N ==> WWAN_SAR_DETECT_ODL*/
+	PAD_CFG_GPI_IRQ_WAKE(GPP_B7, NONE, DEEP, LEVEL, INVERT),
+
 	/* D15 : EN_PP3300_CAMERA */
 	PAD_CFG_GPO(GPP_D15, 1, PLTRST),
+
+	/* H17 : WWAN_RST_L */
+	PAD_CFG_GPO(GPP_H17, 0, PLTRST),
 };
 
 const struct pad_config *variant_override_gpio_table(size_t *num)

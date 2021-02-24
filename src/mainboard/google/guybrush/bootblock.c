1037e341233a7ac87dbaf9e2544571b892a3262c
Mathew King <mathewk@chromium.org>
2021-02-24 14:36:54 -0700

    mb/google/guybrush: Add option to toggle GPIO for sign of life
    Enabling the GPIO_SIGN_OF_LIFE option will allow for early boot testing.
    
    BUG=b:180721202
    TEST=builds
    
    Change-Id: I069623ae76a4e4d1e43a47dd95fdfcece398ebfb
    Signed-off-by: Mathew King <mathewk@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51069
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: EricR Lai <ericr_lai@compal.corp-partner.google.com>

--- src/mainboard/google/guybrush/bootblock.c
+++ src/mainboard/google/guybrush/bootblock.c
@@ -2,6 +2,9 @@
 
 #include <bootblock_common.h>
 #include <baseboard/variants.h>
+#include <delay.h>
+#include <gpio.h>
+#include <soc/gpio.h>
 
 void bootblock_mainboard_early_init(void)
 {
@@ -10,4 +13,13 @@ void bootblock_mainboard_early_init(void)
 
 	gpios = variant_bootblock_gpio_table(&num_gpios);
 	program_gpios(gpios, num_gpios);
+
+	if (CONFIG(GPIO_SIGN_OF_LIFE)) {
+		for (int x = 0; x < 20; x++) {
+			gpio_set(GPIO_31, 1);
+			mdelay(10);
+			gpio_set(GPIO_31, 0);
+			mdelay(10);
+		}
+	}
 }

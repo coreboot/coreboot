16b34ed8f85896157aa7eccd67b81d0ef4c5552e
Eric Lai <ericr_lai@compal.corp-partner.google.com>
2021-02-24 16:28:17 +0800

    drivers/generic/alc1015: add ALC1015 AMP driver
    Add ALC1015 AMP support.
    ALC1015Q-VB Datasheet Rev 0.1
    
    BUG=b:177971830
    TEST: ALC1015P driver can probe properly.
    
    Signed-off-by: Eric Lai <ericr_lai@compal.corp-partner.google.com>
    Change-Id: Id93845024aa2cded69acc88d594c222f2f821f79
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51051
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/drivers/generic/alc1015/alc1015.c
@@ -0,0 +1,71 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <acpi/acpi_device.h>
+#include <acpi/acpigen.h>
+#include <console/console.h>
+#include <device/device.h>
+#include <device/path.h>
+#include "chip.h"
+
+static void alc1015_fill_ssdt(const struct device *dev)
+{
+	struct drivers_generic_alc1015_config *config = dev->chip_info;
+	const char *path;
+	struct acpi_dp *dp;
+
+	if (!config)
+		return;
+
+	const char *scope = acpi_device_scope(dev);
+	const char *name = acpi_device_name(dev);
+	if (!scope || !name)
+		return;
+
+	/* Device */
+	acpigen_write_scope(scope);
+	acpigen_write_device(name);
+
+	acpigen_write_name_string("_HID", "ALCP1015");
+	acpigen_write_name_integer("_UID", 0);
+	acpigen_write_name_string("_DDN", dev->chip_ops->name);
+	acpigen_write_STA(acpi_device_status(dev));
+
+	/* Resources */
+	acpigen_write_name("_CRS");
+	acpigen_write_resourcetemplate_header();
+	acpi_device_write_gpio(&config->sdb);
+	acpigen_write_resourcetemplate_footer();
+
+	/* _DSD for devicetree properties */
+	/* This points to the first pin in the first gpio entry in _CRS */
+	path = acpi_device_path(dev);
+	dp = acpi_dp_new_table("_DSD");
+	acpi_dp_add_gpio(dp, "sdb", path, 0, 0, config->sdb.active_low);
+
+	acpigen_pop_len(); /* Device */
+	acpigen_pop_len(); /* Scope */
+
+	printk(BIOS_INFO, "%s: %s\n", path, dev->chip_ops->name);
+}
+
+static const char *alc1015_acpi_name(const struct device *dev)
+{
+	return "ALCP";
+}
+
+static struct device_operations alc1015_ops = {
+	.read_resources		= noop_read_resources,
+	.set_resources		= noop_set_resources,
+	.acpi_name		= alc1015_acpi_name,
+	.acpi_fill_ssdt		= alc1015_fill_ssdt,
+};
+
+static void alc1015_enable(struct device *dev)
+{
+	dev->ops = &alc1015_ops;
+}
+
+struct chip_operations drivers_generic_alc1015_ops = {
+	CHIP_NAME("ASoC RT1015P Amplifier driver")
+	.enable_dev = alc1015_enable
+};
---
+++ src/drivers/generic/alc1015/chip.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <acpi/acpi_device.h>
+
+struct drivers_generic_alc1015_config {
+	/* SDMODE GPIO */
+	struct acpi_gpio sdb;
+};

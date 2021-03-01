54d59ef7b772374ae9e30826a2e85b9fccafa2e4
Angel Pons <th3fanbus@gmail.com>
2021-03-01 01:06:52 +0100

    drivers/intel/gma/gma.ads: Uniformize casing
    Use lowercase `port` in both the spec and the body.
    
    Change-Id: I3d1e2abe03eedcaf57716af444a3e3b8a61b60d4
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51146
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/drivers/intel/gma/gma.ads
+++ src/drivers/intel/gma/gma.ads
@@ -8,7 +8,7 @@ package GMA is
 
    function read_edid
      (raw_edid :    out HW.GFX.EDID.Raw_EDID_Data;
-      Port     : in     Interfaces.C.int)
+      port     : in     Interfaces.C.int)
       return Interfaces.C.int
    with
       Export, Convention => C, External_Name => "gma_read_edid";

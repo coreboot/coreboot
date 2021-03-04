b9239315ed1c00d59a93c4ad98d0edfd94f2e037
Jitao Shi <jitao.shi@mediatek.com>
2021-03-04 21:52:24 +0800

    mb/google/kukui: Add flag for MIPI_DSI_MODE_LINE_END ANX7625
    Config ANX7625 line data end same time on all line.
    
    BUG=b:173603645
    BRANCH=kukui
    TEST=Display is normal on Kukui
    
    Signed-off-by: Jitao Shi <jitao.shi@mediatek.com>
    Change-Id: Ia1dc217138a98a79ef2f31225b52ba2b1aaf8672
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51435
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- src/mainboard/google/kukui/mainboard.c
+++ src/mainboard/google/kukui/mainboard.c
@@ -162,7 +162,8 @@ static bool configure_display(void)
 			      MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
 			      MIPI_DSI_MODE_LPM);
 	if (CONFIG(DRIVER_ANALOGIX_ANX7625))
-		mipi_dsi_flags |= MIPI_DSI_MODE_EOT_PACKET;
+		mipi_dsi_flags |= MIPI_DSI_MODE_EOT_PACKET |
+				  MIPI_DSI_MODE_LINE_END;
 	if (mtk_dsi_init(mipi_dsi_flags, MIPI_DSI_FMT_RGB888, 4, edid,
 			 panel->s->init) < 0) {
 		printk(BIOS_ERR, "%s: Failed in DSI init.\n", __func__);

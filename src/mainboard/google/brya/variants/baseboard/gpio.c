885e84eb4dd78b7bed47fd5bb88e7da134134c3a
Tim Wawrzynczak <twawrzynczak@chromium.org>
2021-03-05 15:44:42 -0700

    mb/google/brya: Define ChromeOS GPIO support in ACPI tbales
    Define the ChromeOS GPIOs (physical write-protect and virtual recovery
    mode) in ACPI tables so the OS knows which physical pad is used for them.
    
    BUG=b:181887865
    TEST=flashrom_tester is able to "see" the WP GPIO
    
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Change-Id: I3feed366afd6507894a1d31304891cc785a4d314
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51347
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: EricR Lai <ericr_lai@compal.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/baseboard/gpio.c
+++ src/mainboard/google/brya/variants/baseboard/gpio.c
@@ -3,6 +3,7 @@
 #include <baseboard/gpio.h>
 #include <baseboard/variants.h>
 #include <commonlib/helpers.h>
+#include <soc/gpio.h>
 #include <vendorcode/google/chromeos/chromeos.h>
 
 /* Pad configuration in ramstage */
@@ -397,6 +398,8 @@ const struct pad_config *__weak variant_early_gpio_table(size_t *num)
 }
 
 static const struct cros_gpio cros_gpios[] = {
+	CROS_GPIO_REC_AL(CROS_GPIO_VIRTUAL, CROS_GPIO_DEVICE_NAME),
+	CROS_GPIO_WP_AH(GPIO_PCH_WP, CROS_GPIO_DEVICE_NAME),
 };
 
 const struct cros_gpio *__weak variant_cros_gpios(size_t *num)

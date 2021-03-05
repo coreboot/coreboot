0e351c9607290b72bd024ceef69afe848e0ed6d5
David Wu <david_wu@quanta.corp-partner.google.com>
2021-03-05 18:08:18 +0800

    mb/google/dedede/var/magolor: Support VBT for Magister
    Default VBT supports only integrated Display port. Magister supports a
    HDMI port and hence support a separate VBT for Magister.
    
    BUG=b:180666608
    BRANCH=dedede
    TEST=Build and boot to OS.
    
    Cq-Depend: chrome-internal:3661227
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Change-Id: I52c10452887312959f68cfc4e25d5897dae388f8
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51279
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/dedede/variants/magolor/variant.c
+++ src/mainboard/google/dedede/variants/magolor/variant.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <drivers/intel/gma/opregion.h>
 #include <fw_config.h>
 #include <sar.h>
 
@@ -9,4 +10,12 @@ const char *get_wifi_sar_cbfs_filename(void)
 		return "wifi_sar-magolor.hex";
 
 	return WIFI_SAR_CBFS_DEFAULT_FILENAME;
+}
+
+const char *mainboard_vbt_filename(void)
+{
+	if (fw_config_probe(FW_CONFIG(DB_PORTS, DB_PORTS_1A_HDMI)))
+		return "vbt_magister.bin";
+
+	return "vbt.bin";
 }

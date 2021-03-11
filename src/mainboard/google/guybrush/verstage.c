7a8108deb9dab63aa77447f35ba0fe8201eca266
Mathew King <mathewk@chromium.org>
2021-03-11 08:24:40 -0700

    mb/google/guybrush: Enable verstage
    BUG=b:181961514
    TEST=builds
    
    Signed-off-by: Mathew King <mathewk@chromium.org>
    Change-Id: I289a2ad1adc5dcc33c5863d6138f66b9b6dc6590
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51294
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>

---
+++ src/mainboard/google/guybrush/verstage.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <security/vboot/vboot_common.h>
+
+void verstage_mainboard_early_init(void)
+{
+}
+
+void verstage_mainboard_init(void)
+{
+}

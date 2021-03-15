51f3b3225504707ce6df81b12e373ce8ff1ee58c
Stanley Wu <stanley1.wu@lcfc.corp-partner.google.com>
2021-03-15 22:48:25 +0800

    mb/google/dedede/var/boten: Add custom Wifi SAR for botenflex
    Add wifi sar for botenflex.
    Due to fw-config cannot distinguish between boten and botenflex.
    Using sku_id to decide to load botenflex custom wifi sar.
    Detail reason for using sku_id in b:182433707.
    
    BUG=b:182433707
    TEST=build and test on boten/botenflex
    
    Cq-Depend: chrome-internal:3686313
    Change-Id: Id3f2529a7ad56ff306df98f77cda556656da52a5
    Signed-off-by: Stanley Wu <stanley1.wu@lcfc.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51501
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

--- src/mainboard/google/dedede/variants/boten/variant.c
+++ src/mainboard/google/dedede/variants/boten/variant.c
@@ -3,7 +3,11 @@
 #include <acpi/acpi.h>
 #include <baseboard/variants.h>
 #include <delay.h>
+#include <ec/google/chromeec/ec.h>
 #include <gpio.h>
+#include <sar.h>
+
+#define SKU_ID_BOTENFLEX 0x90000
 
 static void power_off_lte_module(void)
 {
@@ -20,4 +24,14 @@ void variant_smi_sleep(u8 slp_typ)
 	 */
 	if (slp_typ == ACPI_S5)
 		power_off_lte_module();
+}
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	uint32_t sku_id = google_chromeec_get_board_sku();
+
+	if (sku_id == SKU_ID_BOTENFLEX)
+		return "wifi_sar-botenflex.hex";
+
+	return "wifi_sar-boten.hex";
 }

16e1fd5ae99c4939f0778fe417e8575e249ccadb
Zheng Bao <fishbaozi@gmail.com>
2021-03-16 17:33:46 +0800

    mb/amd/majolica: Set IOMUX to enable I2C2 & I2C3
    Change-Id: I142c06c150214d58acc04b8c6b3b027fff0256db
    Signed-off-by: Zheng Bao <fishbaozi@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51516
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Mathew King <mathewk@chromium.org>
    Reviewed-by: Jason Glenesk <jason.glenesk@gmail.com>

--- src/mainboard/amd/majolica/early_gpio.c
+++ src/mainboard/amd/majolica/early_gpio.c
@@ -6,6 +6,10 @@
 /* GPIO pins used by coreboot should be initialized in bootblock */
 
 static const struct soc_amd_gpio gpio_set_stage_reset[] = {
+	PAD_NF(GPIO_113, I2C2_SCL, PULL_NONE),
+	PAD_NF(GPIO_114, I2C2_SDA, PULL_NONE),
+	PAD_NF(GPIO_19, I2C3_SCL, PULL_NONE),
+	PAD_NF(GPIO_20, I2C3_SDA, PULL_NONE),
 };
 
 void mainboard_program_early_gpios(void)

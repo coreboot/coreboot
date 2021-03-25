6c2f80c4a2ea39679c80368387d8a20854a985b2
Boris Mittelberg <bmbm@google.com>
2021-03-25 22:34:28 +0000

    mainboard/google/brya: Enable tight timestamp
    This change exposes the PCH_INT_ODL line in GPP_F17 as interrupt resource for
    CREC device
    
    BUG=none
    TEST=manual test
    
    Signed-off-by: Boris Mittelberg <bmbm@google.com>
    Change-Id: I0c05160cb7894b5f7beee93a0c93776f973eae56
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/51830
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/mainboard/google/brya/variants/baseboard/include/baseboard/ec.h
+++ src/mainboard/google/brya/variants/baseboard/include/baseboard/ec.h
@@ -64,4 +64,6 @@
 #define SIO_EC_HOST_ENABLE       /* EC Host Interface Resources */
 #define SIO_EC_ENABLE_PS2K       /* Enable PS/2 Keyboard */
 
+#define EC_ENABLE_SYNC_IRQ      /* Enable tight timestamp / wake support */
+
 #endif /* __BASEBOARD_EC_H__ */
--- src/mainboard/google/brya/variants/baseboard/include/baseboard/gpio.h
+++ src/mainboard/google/brya/variants/baseboard/include/baseboard/gpio.h
@@ -16,5 +16,7 @@
 #define GPIO_EC_IN_RW		GPP_F18
 /* Used to gate SoC's SLP_S0# signal */
 #define GPIO_SLP_S0_GATE	GPP_F9
+/* GPIO IRQ for tight timestamps / wake support */
+#define EC_SYNC_IRQ		GPP_F17_IRQ
 
 #endif /* __BASEBOARD_GPIO_H__ */

a435c3e2eabc59b8a2194f125391e836dda9c05f
Aaron Durbin <adurbin@chromium.org>
2021-04-01 13:26:06 -0600

    mb/google/dedede: add lalala variant
    The Lalala variant is a design that differs only
    in replacing Cr50 with a discrete TPM part.
    
    BUG=b:184151664
    
    Change-Id: I2f7abb9637cd5a13ac896396781b19feb156c948
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/52031
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Aseda Aboagye <aaboagye@google.com>

---
+++ src/mainboard/google/dedede/variants/lalala/variant.c
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <fw_config.h>
+#include <sar.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	if (fw_config_probe(FW_CONFIG(TABLETMODE, TABLETMODE_ENABLED)))
+		return "wifi_sar-lalala.hex";
+
+	return WIFI_SAR_CBFS_DEFAULT_FILENAME;
+}

c48cf110dd38f6dbbbd146aaec67daa81a56c043
Kevin Chang <kevin.chang@lcfc.corp-partner.google.com>
2021-04-07 15:18:25 +0800

    lillipup: provide additional VBT for lillipup OLED sku
    Lillipup add two sku for OLED panel.
    
    Additional VBT is necessary to modify PWM source from VESA eDP AUX
    interface
    
    BUG=b:183630802
    TEST=emerge-volteer coreboot-private-files-baseboard-volteer
    check vbt_oled.bin is under build folder and check in CPU log.
    
    Cq-Depend: chrome-internal:3744227
    Signed-off-by: Kevin Chang <kevin.chang@lcfc.corp-partner.google.com>
    Change-Id: I576297b8296def3c37a01ae0223fa332aa9f02b1
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/52150
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Zhuohao Lee <zhuohao@google.com>
    Reviewed-by: YH Lin <yueherngl@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/volteer/variants/lindar/variant.c
+++ src/mainboard/google/volteer/variants/lindar/variant.c
@@ -2,6 +2,7 @@
 
 #include <fw_config.h>
 #include <sar.h>
+#include <drivers/intel/gma/opregion.h>
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
@@ -11,4 +12,11 @@ const char *get_wifi_sar_cbfs_filename(void)
 		return "wifi_sar_1.hex";
 
 	return WIFI_SAR_CBFS_DEFAULT_FILENAME;
+}
+
+const char *mainboard_vbt_filename(void)
+{
+	if (fw_config_probe(FW_CONFIG(OLED_SCREEN, OLED_PRESENT)))
+		return "vbt-oled.bin";
+	return "vbt.bin";
 }

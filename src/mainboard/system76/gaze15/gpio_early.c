d3c490ef0db36e13447df40e39b4666b4c8d598c
Tim Crawford <tcrawford@system76.com>
2021-04-14 10:45:21 -0600

    mb/system76/gaze15: Leave NC GPIOs unterminated
    Remove the unneeded pull up, as leaving them unterminated disconnects
    them from internal logic.
    
    Also replace use of PAD_CFG_TERM_GPO with PAD_CFG_GPO, as none configure
    termination.
    
    Change-Id: I28549a89a885598ba2d5111a9974356562a03cde
    Signed-off-by: Tim Crawford <tcrawford@system76.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/52387
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/system76/gaze15/gpio_early.c
+++ src/mainboard/system76/gaze15/gpio_early.c
@@ -7,8 +7,8 @@ static const struct pad_config early_gpio_table[] = {
 	PAD_CFG_NF(GPP_C20, NONE, DEEP, NF1), // UART2_RXD
 	PAD_CFG_NF(GPP_C21, NONE, DEEP, NF1), // UART2_TXD
 	PAD_CFG_NF(GPP_F19, NONE, DEEP, NF1), // NB_ENAVDD
-	PAD_CFG_TERM_GPO(GPP_F22, 0, NONE, DEEP), // DGPU_RST#_PCH
-	PAD_CFG_TERM_GPO(GPP_F23, 0, NONE, DEEP), // DGPU_PWR_EN
+	PAD_CFG_GPO(GPP_F22, 0, DEEP), // DGPU_RST#_PCH
+	PAD_CFG_GPO(GPP_F23, 0, DEEP), // DGPU_PWR_EN
 };
 
 void mainboard_configure_early_gpios(void)

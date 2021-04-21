8ac6e09a1056f9fea56387219d1b867cdd117445
FrankChu <frank_chu@pegatron.corp-partner.google.com>
2021-04-21 16:26:56 +0800

    mb/google/dedede/var/galith: Support Wifi SAR for DVT phase
    Because galith/gallop both non-suport tablet mode,
    remove un-use fw_config conditional.
    
    BUG=b:176206495
    TEST=enable CHROMEOS_WIFI_SAR in config of coreboot,
    emerge-dedede coreboot-private-files-baseboard-dedede coreboot chromeos-bootimage.
    
    Signed-off-by: FrankChu <frank_chu@pegatron.corp-partner.google.com>
    Change-Id: Ic9bb76c207ef033f81ecdd57849535b8ac8d13ae
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/52565
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/dedede/variants/galtic/variant.c
+++ src/mainboard/google/dedede/variants/galtic/variant.c
@@ -5,7 +5,5 @@
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
-	if (fw_config_probe(FW_CONFIG(TABLETMODE, TABLETMODE_ENABLED)))
-		return "wifi_sar-galtic.hex";
-	return WIFI_SAR_CBFS_DEFAULT_FILENAME;
+	return "wifi_sar-galtic.hex";
 }

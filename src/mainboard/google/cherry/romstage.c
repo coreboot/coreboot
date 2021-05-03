0575778667e9802a3af9766f459f43f833330d88
Rex-BC Chen <rex-bc.chen@mediatek.com>
2021-05-03 16:15:05 +0800

    mb/google/cherry: Initialize pmif/spmi/pmic in romstage
    Signed-off-by: Henry Chen <henryc.chen@mediatek.com>
    Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Change-Id: I2eeddb44b5495d05602c995a6103a56b09cf126a
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/52849
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

---
+++ src/mainboard/google/cherry/romstage.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <arch/stages.h>
+#include <soc/mt6315.h>
+#include <soc/mt6359p.h>
+#include <soc/pmif.h>
+
+void platform_romstage_main(void)
+{
+	mtk_pmif_init();
+	mt6359p_init();
+	mt6315_init();
+}

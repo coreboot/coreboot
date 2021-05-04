8143d03d514377a67f520f0a66c4344ffcb6d35d
Felix Held <felix-coreboot@felixheld.de>
2021-05-04 21:20:06 +0200

    soc/amd/cezanne/agesa_acpi: add add_agesa_fsp_acpi_table call
    this adds the ALIB SSDT that gets passed from the FSP to coreboot via a
    HOB.
    
    BUG=b:185481298
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I8a7dae5789eee442b321ddf276494eb53fc5f499
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/52904
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Matt Papageorge <matthewpapa07@gmail.com>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/soc/amd/cezanne/agesa_acpi.c
+++ src/soc/amd/cezanne/agesa_acpi.c
@@ -3,11 +3,15 @@
 #include <acpi/acpi.h>
 #include <amdblocks/acpi.h>
 #include <device/device.h>
+#include <FspGuids.h>
 #include <soc/acpi.h>
 #include <types.h>
 
 uintptr_t agesa_write_acpi_tables(const struct device *device, uintptr_t current,
 				  acpi_rsdp_t *rsdp)
 {
+	/* add ALIB SSDT from HOB */
+	current = add_agesa_fsp_acpi_table(AMD_FSP_ACPI_ALIB_HOB_GUID, "ALIB", rsdp, current);
+
 	return current;
 }

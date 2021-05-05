9575c08345e96ee906d0dd694f39ffac71aa8418
Arthur Heymans <arthur@aheymans.xyz>
2021-05-05 13:43:15 +0200

    sb/intel/common/pmclib: Use pmbase functions
    Change-Id: Ic0be2c1ffaadcc6212c548332d60d40b405abbda
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/52939
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>

--- src/southbridge/intel/common/pmclib.c
+++ src/southbridge/intel/common/pmclib.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <bootmode.h>
 #include <stdint.h>
 #include <acpi/acpi.h>
 #include <console/console.h>
@@ -8,31 +9,24 @@
 #include "pmbase.h"
 #include "pmutil.h"
 
+static void clear_power_state(void)
+{
+	uint32_t pm1_cnt = read_pmbase32(PM1_CNT);
+	write_pmbase32(PM1_CNT, pm1_cnt & ~SLP_TYP);
+}
+
 int southbridge_detect_s3_resume(void)
 {
-	u32 pm1_cnt;
-	u16 pm1_sts;
-	int is_s3 = 0;
-
-	/* Check PM1_STS[15] to see if we are waking from Sx */
-	pm1_sts = read_pmbase16(PM1_STS);
-	if (pm1_sts & WAK_STS) {
-		/* Read PM1_CNT[12:10] to determine which Sx state */
-		pm1_cnt = read_pmbase32(PM1_CNT);
-		if (((pm1_cnt >> 10) & 7) == SLP_TYP_S3) {
-			/* Clear SLP_TYPE. */
-			write_pmbase32(PM1_CNT, pm1_cnt & ~(7 << 10));
-			is_s3 = 1;
-		}
-	}
-	if (is_s3) {
+	if (platform_is_resuming()) {
+		clear_power_state();
 		if (!acpi_s3_resume_allowed()) {
 			printk(BIOS_DEBUG, "SB: Resume from S3 detected, but disabled.\n");
 			return 0;
 		}
 
 		printk(BIOS_DEBUG, "SB: Resume from S3 detected.\n");
+		return 1;
 	}
 
-	return is_s3;
+	return 0;
 }

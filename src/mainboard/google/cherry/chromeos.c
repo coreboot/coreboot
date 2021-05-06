fb5fa1abe7a17caf29c387cb6864b85b1479822b
Trevor Wu <trevor.wu@mediatek.com>
2021-05-06 14:07:12 +0800

    mb/google/cherry: Support audio
    Add GPIO "beep enable" for switching on and off.
    
    Signed-off-by: Trevor Wu <trevor.wu@mediatek.com>
    Change-Id: Iddb781e30fa90f05767cceeb83e623432540dcc0
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/54739
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/mainboard/google/cherry/chromeos.c
+++ src/mainboard/google/cherry/chromeos.c
@@ -12,6 +12,7 @@ void setup_chromeos_gpios(void)
 	gpio_input(GPIO_WP);
 	gpio_input_pullup(GPIO_EC_AP_INT);
 	gpio_input_pullup(GPIO_SD_CD);
+	gpio_output(GPIO_BEEP_ON, 0);
 	gpio_input_pullup(GPIO_EC_IN_RW);
 	gpio_input_pullup(GPIO_GSC_AP_INT);
 	gpio_output(GPIO_EN_SPK, 0);
@@ -23,6 +24,7 @@ void fill_lb_gpios(struct lb_gpios *gpios)
 	struct lb_gpio chromeos_gpios[] = {
 		{GPIO_EC_AP_INT.id, ACTIVE_LOW, -1, "EC interrupt"},
 		{GPIO_SD_CD.id, ACTIVE_LOW, -1, "SD card detect"},
+		{GPIO_BEEP_ON.id, ACTIVE_HIGH, -1, "beep enable"},
 		{GPIO_EC_IN_RW.id, ACTIVE_LOW, -1, "EC in RW"},
 		{GPIO_GSC_AP_INT.id, ACTIVE_LOW, -1, "TPM interrupt"},
 		{GPIO_EN_SPK.id, ACTIVE_HIGH, -1, "speaker enable"},

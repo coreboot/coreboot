ad9270db221bfc31daec30f1e056104d12cfaf2b
Angel Pons <th3fanbus@gmail.com>
2021-05-07 18:08:59 +0200

    mb/gigabyte/ga-d510ud: Fix HDA codec configuration
    The values were copied from Foxconn D41S, which uses a different codec.
    Adjust the codec config as per the settings dumped from vendor firmware.
    
    Change-Id: If6a4c41b5d424adb23ebef402d2d2ad21269fe25
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/53914
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/mainboard/gigabyte/ga-d510ud/hda_verb.c
+++ src/mainboard/gigabyte/ga-d510ud/hda_verb.c
@@ -3,22 +3,24 @@
 #include <device/azalia_device.h>
 
 const u32 cim_verb_data[] = {
-	/* coreboot specific header */
-	0x10ec0662,
-	0x105b0d55,	/* Subsystem ID */
-	10,		/* Number of entries */
-
-	/* Pin Widget Verb Table */
-	AZALIA_PIN_CFG(0, 0x14, 0x01014c10),
-	AZALIA_PIN_CFG(0, 0x15, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x16, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x18, 0x01a19c30),
-	AZALIA_PIN_CFG(0, 0x19, 0x02a19c31),
-	AZALIA_PIN_CFG(0, 0x1a, 0x0181343f),
-	AZALIA_PIN_CFG(0, 0x1b, 0x02214c1f),
-	AZALIA_PIN_CFG(0, 0x1c, 0x411111f0),
-	AZALIA_PIN_CFG(0, 0x1d, 0x4005c603),
-	AZALIA_PIN_CFG(0, 0x1e, 0x99430120),
+	0x10ec0887,	/* Realtek ALC887 */
+	0x1458a002,	/* Subsystem ID */
+	15,		/* Number of 4 dword sets */
+	AZALIA_SUBVENDOR(2, 0x1458a002),
+	AZALIA_PIN_CFG(2, 0x11, 0x411111f0),
+	AZALIA_PIN_CFG(2, 0x12, 0x411111f0),
+	AZALIA_PIN_CFG(2, 0x14, 0x01014410),
+	AZALIA_PIN_CFG(2, 0x15, 0x411111f0),
+	AZALIA_PIN_CFG(2, 0x16, 0x411111f0),
+	AZALIA_PIN_CFG(2, 0x17, 0x411111f0),
+	AZALIA_PIN_CFG(2, 0x18, 0x01a19c30),
+	AZALIA_PIN_CFG(2, 0x19, 0x02a19c50),
+	AZALIA_PIN_CFG(2, 0x1a, 0x0181344f),
+	AZALIA_PIN_CFG(2, 0x1b, 0x02214c20),
+	AZALIA_PIN_CFG(2, 0x1c, 0x593301f0),
+	AZALIA_PIN_CFG(2, 0x1d, 0x4005c603),
+	AZALIA_PIN_CFG(2, 0x1e, 0x411111f0),
+	AZALIA_PIN_CFG(2, 0x1f, 0x411111f0),
 };
 
 const u32 pc_beep_verbs[] = {};

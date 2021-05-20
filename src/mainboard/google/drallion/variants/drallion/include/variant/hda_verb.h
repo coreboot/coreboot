coreboot/coreboot

commit e3443d87ccaa3a845b595d3f056317f549ccdf6b

mb/google/drallion: Add new mainboard
Drallion is a new mainboard using Intel Comet Lake SOC. As a starting
point, I took mainboard/sarien as the reference code and modified WHL
to Comet Lake.

BUG=b:138098572
Test=compiles

Signed-off-by: Thejaswani Putta <thejaswani.putta@intel.com>
Change-Id: I541952a4ef337e7277a85f02d25979f12ec075c4
Reviewed-on: https://review.coreboot.org/c/coreboot/+/34497
Reviewed-by: Mathew King <mathewk@chromium.org>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/mainboard/google/drallion/variants/drallion/include/variant/hda_verb.h
@@ -1,0 +1,209 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef MAINBOARD_HDA_VERB_H
+#define MAINBOARD_HDA_VERB_H
+
+#include <device/azalia_device.h>
+
+const u32 cim_verb_data[] = {
+	/* coreboot specific header */
+	0x10ec0295,	// Codec Vendor / Device ID: Realtek ALC3204
+	0xffffffff,	// Subsystem ID
+	0x0000002b,	// Number of jacks (NID entries)
+
+	/* Rest Codec First */
+	AZALIA_RESET(0x1),
+	/* NID 0x01, HDA Codec Subsystem ID Verb Table */
+	AZALIA_SUBVENDOR(0x0, 0x102808b6),
+
+	/* Pin Widget Verb Table */
+	AZALIA_PIN_CFG(0x0, 0x12, 0xb7a60130),
+	AZALIA_PIN_CFG(0x0, 0x13, 0x411111f0),
+	AZALIA_PIN_CFG(0x0, 0x14, 0x90170110),
+	AZALIA_PIN_CFG(0x0, 0x16, 0x40000000),
+	AZALIA_PIN_CFG(0x0, 0x17, 0x411111f0),
+	AZALIA_PIN_CFG(0x0, 0x18, 0x411111f0),
+	AZALIA_PIN_CFG(0x0, 0x19, 0x04a11030),
+	AZALIA_PIN_CFG(0x0, 0x1a, 0x411111f0),
+	AZALIA_PIN_CFG(0x0, 0x1b, 0x411111f0),
+	AZALIA_PIN_CFG(0x0, 0x1d, 0x40c00001),
+	AZALIA_PIN_CFG(0x0, 0x1e, 0x421212f2),
+	AZALIA_PIN_CFG(0x0, 0x21, 0x04211020),
+
+	/* D reset */
+	0x0205003C,
+	0x0204F254,
+	0x0205003C,
+	0x0204F214,
+	/* JD1 - 2port JD mode */
+	0x02050009,
+	0x0204E003,
+	0x0205000A,
+	0x02047770,
+	/* Set TRS type-1 */
+	0x02050045,
+	0x02045289,
+	0x02050049,
+	0x02040049,
+	/* Set TRS type-2 + Set UAJ Line2 vref(ALC3254) */
+	0x0205004A,
+	0x0204A830,
+	0x02050063,
+	0x0204CF00,
+	/* NID 0x20 set class-D to 2W@4ohm (+12dB gain)
+	 * + Set sine tone gain(0x34) */
+	0x02050038,
+	0x02043909,
+	0x05C50000,
+	0x05C43482,
+	/* AGC-1 Disable + (Front Gain=0dB ) */
+	0x05D50006,
+	0x05D44C50,
+	0x05D50002,
+	0x05D44004,
+	/* AGC-2   (Backt Boost Gain= -0.375dB  ,Limiter = -3dB) */
+	0x05D50003,
+	0x05D45E5E,
+	0x05D50001,
+	0x05D4D788,
+	/* AGC-3 + AGC Enable */
+	0x05D50009,
+	0x05D451FF,
+	0x05D50006,
+	0x05D44E50,
+	/* HP-JD Enable +Nokia type */
+	0x0205004A,
+	0x02042010,
+	0x02050008,
+	0x02046A0C,
+	/* EAPD set to verb-control + I2C Un-use+ DVDD3.3V */
+	0x02050010,
+	0x02040020,
+	0x02050034,
+	0x0204A23D,
+	/* Class D silent detection Enable -84dB threshold */
+	0x02050030,
+	0x02049000,
+	0x02050037,
+	0x0204FE15,
+	/* Disable EQ + set 250Hz 3rd High Pass filter */
+	0x05350000,
+	0x0534203A,
+	0x05350000,
+	0x0534203A,
+	/* Left Channel-1 */
+	0x0535001d,
+	0x05340800,
+	0x0535001e,
+	0x05340800,
+	/* Left Channel-2 */
+	0x05350003,
+	0x05341EF8,
+	0x05350004,
+	0x05340000,
+	/* Left Channel-3 */
+	0x05350005,
+	0x053403EE,
+	0x05350006,
+	0x0534FA60,
+	/* Left Channel-4 */
+	0x05350007,
+	0x05341E10,
+	0x05350008,
+	0x05347B86,
+	/* Left Channel-5 */
+	0x05350009,
+	0x053401F7,
+	0x0535000A,
+	0x05349FB6,
+	/* Left Channel-6 */
+	0x0535000B,
+	0x05341C00,
+	0x0535000C,
+	0x05340000,
+	/* Left Channel-7 */
+	0x0535000D,
+	0x05340200,
+	0x0535000E,
+	0x05340000,
+	/* Right Channel-1 */
+	0x05450000,
+	0x05442000,
+	0x0545001d,
+	0x05440800,
+	/* Right Channel-2 */
+	0x0545001e,
+	0x05440800,
+	0x05450003,
+	0x05441EF8,
+	/* Right Channel-3 */
+	0x05450004,
+	0x05440000,
+	0x05450005,
+	0x054403EE,
+	/* Right Channel-4 */
+	0x05450006,
+	0x0544FA60,
+	0x05450007,
+	0x05441E10,
+	/* Right Channel-5 */
+	0x05450008,
+	0x05447B86,
+	0x05450009,
+	0x054401F7,
+	/* Right Channel-6 */
+	0x0545000A,
+	0x05449FB6,
+	0x0545000B,
+	0x05441C00,
+	/* Right Channel-7 */
+	0x0545000C,
+	0x05440000,
+	0x0545000D,
+	0x05440200,
+	/* Right Channel-8 + EQ Update & Enable */
+	0x0545000E,
+	0x05440000,
+	0x05350000,
+	0x0534E03A,
+	/* Enable all Microphone */
+	0x0205000D,
+	0x0204A023,
+	0x0205000D,
+	0x0204A023,
+	/* Enable Internal Speaker (NID14) */
+	0x0205000F,
+	0x02040000,
+	0x0205000F,
+	0x02040000,
+};
+
+const u32 pc_beep_verbs[] = {
+	/* PCBeep pass through to NID14 for ePSA test-1 */
+	0x02050036,
+	0x020477D7,
+	0x0143B000,
+	0x01470740,
+	/* PCBeep pass through to NID14 for ePSA test-2 */
+	0x01470C02,
+	0x01470C02,
+	0x01470C02,
+	0x01470C02,
+};
+
+AZALIA_ARRAY_SIZES;
+
+#endif

src/mainboard/google/drallion/Kconfig
@@ -1,0 +1,106 @@
+
+config BOARD_GOOGLE_BASEBOARD_DRALLION
+	def_bool n
+	select BOARD_ROMSIZE_KB_32768
+	select DRIVERS_I2C_GENERIC
+	select DRIVERS_I2C_HID
+	select DRIVERS_INTEL_ISH
+	select DRIVERS_SPI_ACPI
+	select DRIVERS_USB_ACPI
+	select EC_GOOGLE_WILCO
+	select GENERIC_SPD_BIN
+	select GOOGLE_SMBIOS_MAINBOARD_VERSION
+	select HAVE_ACPI_RESUME
+	select HAVE_ACPI_TABLES
+	select INTEL_LPSS_UART_FOR_CONSOLE
+	select MAINBOARD_HAS_CHROMEOS
+	select MAINBOARD_HAS_I2C_TPM_CR50
+	select MAINBOARD_HAS_TPM2
+	select SOC_INTEL_COMETLAKE
+	select SOC_INTEL_COMMON_BLOCK_HDA_VERB
+	select SOC_INTEL_COMMON_BLOCK_SMM_ESPI_DISABLE
+	select SPD_READ_BY_WORD
+	select SYSTEM_TYPE_LAPTOP
+	select TPM2
+	select MAINBOARD_USES_IFD_EC_REGION
+	select MAINBOARD_USES_IFD_GBE_REGION
+	select USE_SAR
+	select SAR_ENABLE
+
+if BOARD_GOOGLE_BASEBOARD_DRALLION
+
+config CHROMEOS
+	bool
+	default y
+	select GBB_FLAG_FORCE_DEV_SWITCH_ON
+	select GBB_FLAG_FORCE_DEV_BOOT_USB
+	select GBB_FLAG_FORCE_DEV_BOOT_LEGACY
+	select GBB_FLAG_DISABLE_EC_SOFTWARE_SYNC
+
+config DIMM_MAX
+	int
+	default 2
+
+config DIMM_SPD_SIZE
+	int
+	default 512
+
+config DRIVER_TPM_I2C_BUS
+	hex
+	default 0x4
+
+config DRIVER_TPM_I2C_ADDR
+	hex
+	default 0x50
+
+config TPM_TIS_ACPI_INTERRUPT
+	int
+	default 82  # GPE0_DW2_18 (GPP_D18)
+
+config POWER_OFF_ON_CR50_UPDATE
+	bool
+	default n
+
+config GBB_HWID
+	string
+	depends on CHROMEOS
+	default "DRALLION TEST 3556"
+
+config MAINBOARD_DIR
+	string
+	default "google/drallion"
+
+config MAINBOARD_FAMILY
+	string
+	default "Google_Drallion"
+
+config MAINBOARD_PART_NUMBER
+	string
+	default "Drallion"
+
+config MAINBOARD_VENDOR
+	string
+	default "Google"
+
+config MAX_CPUS
+	int
+	default 8
+
+config UART_FOR_CONSOLE
+	int
+	default 2
+
+config VARIANT_DIR
+	string
+	default "drallion"
+
+config DEVICETREE
+	string
+	default "variants/$(CONFIG_VARIANT_DIR)/devicetree.cb"
+
+config VBOOT
+	select HAS_RECOVERY_MRC_CACHE
+	select MRC_CLEAR_NORMAL_CACHE_ON_RECOVERY_RETRAIN
+	select VBOOT_LID_SWITCH
+
+endif # BOARD_GOOGLE_BASEBOARD_DRALLION

src/mainboard/google/drallion/Kconfig.name
@@ -1,0 +1,5 @@
+comment "Drallion"
+
+config BOARD_GOOGLE_DRALLION
+	bool "->  Drallion"
+	select BOARD_GOOGLE_BASEBOARD_DRALLION

src/mainboard/google/drallion/Makefile.inc
@@ -1,0 +1,38 @@
+##
+## This file is part of the coreboot project.
+##
+## Copyright 2018 Google LLC
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; version 2 of the License.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+
+bootblock-y += bootblock.c
+
+ramstage-y += ramstage.c
+ramstage-y += sku.c
+
+romstage-y += romstage.c
+
+smm-y += smihandler.c
+
+bootblock-$(CONFIG_CHROMEOS) += chromeos.c
+ramstage-$(CONFIG_CHROMEOS) += chromeos.c
+romstage-$(CONFIG_CHROMEOS) += chromeos.c
+verstage-$(CONFIG_CHROMEOS) += chromeos.c
+
+ramstage-$(CONFIG_SOC_INTEL_COMMON_BLOCK_HDA_VERB) += hda_verb.c
+
+bootblock-y += ec.c
+ramstage-y += ec.c
+romstage-y += ec.c
+verstage-y += ec.c
+
+subdirs-y += variants/$(VARIANT_DIR)
+CPPFLAGS_common += -I$(src)/mainboard/$(MAINBOARDDIR)/variants/$(VARIANT_DIR)/include

src/mainboard/google/drallion/bootblock.c
@@ -1,0 +1,34 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <bootblock_common.h>
+#include <ec/google/wilco/bootblock.h>
+#include <soc/gpio.h>
+#include <variant/gpio.h>
+
+static void early_config_gpio(void)
+{
+	const struct pad_config *early_gpio_table;
+	size_t num_gpios = 0;
+
+	early_gpio_table = variant_early_gpio_table(&num_gpios);
+	gpio_configure_pads(early_gpio_table, num_gpios);
+}
+
+void bootblock_mainboard_init(void)
+{
+	early_config_gpio();
+	wilco_ec_early_init();
+}

src/mainboard/google/drallion/chromeos.c
@@ -1,0 +1,127 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/acpi.h>
+#include <boot/coreboot_tables.h>
+#include <gpio.h>
+#include <soc/gpio.h>
+#include <variant/gpio.h>
+#include <vendorcode/google/chromeos/chromeos.h>
+#include <security/tpm/tss.h>
+#include <device/device.h>
+#include <intelblocks/pmclib.h>
+#include <soc/pmc.h>
+#include <soc/pci_devs.h>
+
+enum rec_mode_state {
+	REC_MODE_UNINITIALIZED,
+	REC_MODE_NOT_REQUESTED,
+	REC_MODE_REQUESTED,
+};
+
+void fill_lb_gpios(struct lb_gpios *gpios)
+{
+	struct lb_gpio chromeos_gpios[] = {
+		{GPIO_PCH_WP, ACTIVE_HIGH, get_write_protect_state(),
+		 "write protect"},
+		{-1, ACTIVE_HIGH, get_lid_switch(), "lid"},
+		{-1, ACTIVE_HIGH, 0, "power"},
+		{-1, ACTIVE_HIGH, gfx_get_init_done(), "oprom"},
+		{-1, ACTIVE_HIGH, 0, "EC in RW"},
+	};
+	lb_add_gpios(gpios, chromeos_gpios, ARRAY_SIZE(chromeos_gpios));
+}
+
+static int cros_get_gpio_value(int type)
+{
+	const struct cros_gpio *cros_gpios;
+	size_t i, num_gpios = 0;
+
+	cros_gpios = variant_cros_gpios(&num_gpios);
+
+	for (i = 0; i < num_gpios; i++) {
+		const struct cros_gpio *gpio = &cros_gpios[i];
+		if (gpio->type == type) {
+			int state = gpio_get(gpio->gpio_num);
+			if (gpio->polarity == CROS_GPIO_ACTIVE_LOW)
+				return !state;
+			else
+				return state;
+		}
+	}
+	return 0;
+}
+
+void mainboard_chromeos_acpi_generate(void)
+{
+	const struct cros_gpio *cros_gpios;
+	size_t num_gpios = 0;
+
+	cros_gpios = variant_cros_gpios(&num_gpios);
+
+	chromeos_acpi_gpio_generate(cros_gpios, num_gpios);
+}
+
+int get_write_protect_state(void)
+{
+	return cros_get_gpio_value(CROS_GPIO_WP);
+}
+
+int get_recovery_mode_switch(void)
+{
+	static enum rec_mode_state saved_rec_mode = REC_MODE_UNINITIALIZED;
+	enum rec_mode_state state = REC_MODE_NOT_REQUESTED;
+	uint8_t cr50_state = 0;
+
+	/* Check cached state, since TPM will only tell us the first time */
+	if (saved_rec_mode != REC_MODE_UNINITIALIZED)
+		return saved_rec_mode == REC_MODE_REQUESTED;
+
+	/*
+	 * Read one-time recovery request from cr50 in verstage only since
+	 * the TPM driver won't be set up in time for other stages like romstage
+	 * and the value from the TPM would be wrong anyway since the verstage
+	 * read would have cleared the value on the TPM.
+	 *
+	 * The TPM recovery request is passed between stages through the
+	 * vboot_get_shared_data or cbmem depending on stage.
+	 */
+	if (ENV_VERSTAGE &&
+	    tlcl_cr50_get_recovery_button(&cr50_state) == TPM_SUCCESS &&
+	    cr50_state)
+		state = REC_MODE_REQUESTED;
+
+	/* Read state from the GPIO controlled by servo. */
+	if (cros_get_gpio_value(CROS_GPIO_REC))
+		state = REC_MODE_REQUESTED;
+
+	/* Store the state in case this is called again in verstage. */
+	saved_rec_mode = state;
+
+	return state == REC_MODE_REQUESTED;
+}
+
+int get_lid_switch(void)
+{
+	return 1;
+}
+
+void mainboard_prepare_cr50_reset(void)
+{
+#if ENV_RAMSTAGE
+	/* Ensure system powers up after CR50 reset */
+	pmc_set_afterg3(MAINBOARD_POWER_STATE_ON);
+#endif
+}

src/mainboard/google/drallion/chromeos.fmd
@@ -1,0 +1,49 @@
+FLASH@0xfe000000 0x2000000 {
+	SI_ALL@0x0 0x400000 {
+		SI_DESC@0x0 0x1000
+		SI_EC@0x1000 0x100000
+		SI_GBE(PRESERVE)@0x101000 0x2000
+		SI_ME@0x103000 0x2f9000
+		SI_PDR(PRESERVE)@0x3fc000 0x4000
+	}
+	SI_BIOS@0x400000 0x1c00000 {
+		RW_DIAG@0x0 0x12d0000 {
+			RW_LEGACY(CBFS)@0x0 0x12c0000
+			DIAG_NVRAM@0x12c0000 0x10000
+		}
+		RW_SECTION_A@0x12d0000 0x280000 {
+			VBLOCK_A@0x0 0x10000
+			FW_MAIN_A(CBFS)@0x10000 0x26ffc0
+			RW_FWID_A@0x27ffc0 0x40
+		}
+		RW_SECTION_B@0x1550000 0x280000 {
+			VBLOCK_B@0x0 0x10000
+			FW_MAIN_B(CBFS)@0x10000 0x26ffc0
+			RW_FWID_B@0x27ffc0 0x40
+		}
+		RW_MISC@0x17d0000 0x30000 {
+			UNIFIED_MRC_CACHE@0x0 0x20000 {
+				RECOVERY_MRC_CACHE@0x0 0x10000
+				RW_MRC_CACHE@0x10000 0x10000
+			}
+			RW_ELOG(PRESERVE)@0x20000 0x4000
+			RW_SHARED@0x24000 0x4000 {
+				SHARED_DATA@0x0 0x2000
+				VBLOCK_DEV@0x2000 0x2000
+			}
+			RW_VPD(PRESERVE)@0x28000 0x2000
+			RW_NVRAM(PRESERVE)@0x2a000 0x6000
+		}
+		WP_RO@0x1800000 0x400000 {
+			RO_VPD(PRESERVE)@0x0 0x4000
+			RO_UNUSED@0x4000 0xc000
+			RO_SECTION@0x10000 0x3f0000 {
+				FMAP@0x0 0x800
+				RO_FRID@0x800 0x40
+				RO_FRID_PAD@0x840 0x7c0
+				GBB@0x1000 0xef000
+				COREBOOT(CBFS)@0xf0000 0x300000
+			}
+		}
+	}
+}

src/mainboard/google/drallion/dsdt.asl
@@ -1,0 +1,81 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright 2018 Google LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <variant/ec.h>
+
+#include <arch/acpi.h>
+DefinitionBlock(
+	"dsdt.aml",
+	"DSDT",
+	0x02,		/* DSDT revision: ACPI v2.0 and up */
+	OEM_ID,
+	ACPI_TABLE_CREATOR,
+	0x20110725	/* OEM revision */
+)
+{
+	/* Some generic macros */
+	#include <soc/intel/cannonlake/acpi/platform.asl>
+
+	/* global NVS and variables */
+	#include <soc/intel/cannonlake/acpi/globalnvs.asl>
+
+	/* CPU */
+	#include <cpu/intel/common/acpi/cpu.asl>
+
+	Scope (\_SB) {
+		Device (PCI0)
+		{
+			#include <soc/intel/cannonlake/acpi/northbridge.asl>
+			#include <soc/intel/cannonlake/acpi/southbridge.asl>
+		}
+		/* Per board variant mainboard hooks. */
+		#include <variant/acpi/mainboard.asl>
+	}
+
+#if CONFIG(CHROMEOS)
+	/* Chrome OS specific */
+	#include <vendorcode/google/chromeos/acpi/chromeos.asl>
+	/* VPD support */
+	#include <vendorcode/google/chromeos/acpi/vpd.asl>
+	/* MAC address passthru */
+	#include <vendorcode/google/chromeos/acpi/amac.asl>
+#endif
+
+	/* Chipset specific sleep states */
+	#include <soc/intel/cannonlake/acpi/sleepstates.asl>
+
+	/* Low power idle table */
+	#include <soc/intel/cannonlake/acpi/lpit.asl>
+
+	/* Chrome OS Embedded Controller */
+	Scope (\_SB.PCI0.LPCB)
+	{
+		/* ACPI code for EC SuperIO functions */
+		#include <ec/google/wilco/acpi/superio.asl>
+		/* ACPI code for EC functions */
+		#include <ec/google/wilco/acpi/ec.asl>
+	}
+
+	/* Dynamic Platform Thermal Framework */
+	Scope (\_SB)
+	{
+		/* Per board variant specific definitions. */
+		#include <variant/acpi/dptf.asl>
+		/* Include soc specific DPTF changes */
+		#include <soc/intel/cannonlake/acpi/dptf.asl>
+		/* Include common dptf ASL files */
+		#include <soc/intel/common/acpi/dptf/dptf.asl>
+	}
+}

commit 682eb5fe4da889079ff62f07c47a4cb91f4e5caa

mb/google/drallion/var/drallion/hda_verb: Correct codec name in comment
Correct the Realtek ALC3254 codec name in the comment. The name is used
in the original commit message, and is also present in the Linux kernel
(`sound/pci/hda/patch_realtek.c`).

The file was an exact copy of
`src/mainboard/google/sarien/variants/arcada/include/variant/hda_verb.h`
added in commit 95370e1f (mb/google/sarien: Add HD Audio verb table).

Change-Id: I43cd73a14e07eb4518e3d44b6f81dff5016da721
Fixes: e3443d87 ("mb/google/drallion: Add new mainboard")
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/54590
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

src/mainboard/google/drallion/variants/drallion/include/variant/hda_verb.h
@@ -7,7 +7,7 @@
 
 const u32 cim_verb_data[] = {
 	/* coreboot specific header */
-	0x10ec0295,	// Codec Vendor / Device ID: Realtek ALC3204
+	0x10ec0295,	// Codec Vendor / Device ID: Realtek ALC3254
 	0xffffffff,	// Subsystem ID
 	0x0000002b,	// Number of jacks (NID entries)
 
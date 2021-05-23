0cda8d2c5081b012795491354971f5cbda025ac8
Angel Pons <th3fanbus@gmail.com>
2021-05-23 12:01:53 +0200

    mb/lenovo/t430: Do not set unused GNVS fields
    ACPI code for this mainboard uses none of these values.
    
    Change-Id: I429bf8dc229fd830ae662034a8b733c9ee669140
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/54851
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/lenovo/t430/acpi_tables.c
+++ src/mainboard/lenovo/t430/acpi_tables.c
@@ -8,14 +8,8 @@ void mainboard_fill_gnvs(struct global_nvs *gnvs)
 	/* The lid is open by default */
 	gnvs->lids = 1;
 
-	/* Config TDP Down */
-	gnvs->f0of = 80;
-	gnvs->f0on = 90;
-
 	/* Temperature at which OS will shutdown */
 	gnvs->tcrt = 100;
 	/* Temperature at which OS will throttle CPU */
 	gnvs->tpsv = 90;
-	/* Tj_max value for calculating PECI CPU temperature */
-	gnvs->tmax = 105;
 }

29228286a20b377d77e17249d1a968b40edf90a7
Sheng-Liang Pan <sheng-liang.pan@quanta.corp-partner.google.com>
2021-06-01 19:32:11 +0800

    mb/google/volteer/var/volet: Update gpio and devicetree settings
    Based on schematic and gpio table of volet, update gpio and
    devicetree settings for volet Proto.
    
    BUG=b:186334008
    TEST=FW_NAME=volet emerge-volteer coreboot chromeos-bootimage
    
    Signed-off-by: Sheng-Liang Pan <sheng-liang.pan@quanta.corp-partner.google.com>
    Change-Id: Ia0e9557e01ce1e7a49a3dddf6da3e4a29587a8b7
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55113
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/volteer/variants/volet/ramstage.c
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <delay.h>
+#include <gpio.h>
+#include <baseboard/variants.h>
+#include <soc/gpio.h>
+
+void variant_ramstage_init(void)
+{
+	/*
+	 * Assert FPMCU reset and enable power to FPMCU,
+	 * wait for power rail to stabilize,
+	 * and then deassert FPMCU reset.
+	 * Waiting for the power rail to stabilize can take a while.
+	 */
+	gpio_output(GPP_C23, 0);
+	gpio_output(GPP_A21, 1);
+	mdelay(1);
+	gpio_output(GPP_C23, 1);
+}

36572cade47a0e03aa379d7505958253c90ddb44
Sheng-Liang Pan <sheng-liang.pan@quanta.corp-partner.google.com>
2021-06-09 14:00:29 +0800

    mb/google/volteer/var/chronicler:  add chronicler memory configuration and gpio and devicetree settings
    add memory configuration for chronicler,
    based on schematic and gpio table, update gpio and devicetree settings for chronicler.
    
    BUG=b:187318819
    BRANCH=None
    TEST=FW_NAME=chronicler emerge-volteer coreboot chromeos-bootimage
    verify bootable with chronicler
    
    Signed-off-by: Sheng-Liang Pan <sheng-liang.pan@quanta.corp-partner.google.com>
    Change-Id: Id5524b97a236dcc64d18ab1cd2ce13f6bb2d998f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55340
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/volteer/variants/chronicler/memory.c
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <gpio.h>
+
+static const struct mb_cfg baseboard_memcfg = {
+	.type = MEM_TYPE_DDR4,
+};
+
+const struct mb_cfg *variant_memory_params(void)
+{
+	return &baseboard_memcfg;
+}
+
+int variant_memory_sku(void)
+{
+	gpio_t spd_gpios[] = {
+		GPIO_MEM_CONFIG_0,
+		GPIO_MEM_CONFIG_1,
+		GPIO_MEM_CONFIG_2,
+		GPIO_MEM_CONFIG_3,
+	};
+
+	return gpio_base2_value(spd_gpios, ARRAY_SIZE(spd_gpios));
+}

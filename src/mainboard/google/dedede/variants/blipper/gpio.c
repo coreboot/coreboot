1c43d92efc004795e4f2198d19414b07c082ccd1
Zanxi Chen <chenzanxi@huaqin.corp-partner.google.com>
2021-06-10 20:29:05 +0800

    mb/google/dedede/var/blipper: Update devicetree and gpio setting
    To reduce power load, set unused GPIOs to NC and close
    unused interface in devicetree. GPIOs and
    interfaces are as below:
    GPIO: GPP_C18/C19/D12/D14/D15/D19/D20/D21/E00/E02/H06/H07
    Interface: I2C1/I2C3/I2C5
    USB: port2_3/2_4/2_6
    
    BUG=b:185044041
    BRANCH=dedede
    TEST=Built bios and test, it reduces power load without affecting
    device function.
    
    Change-Id: Ib5999f0e129bf3e660fe293eda7af3e8e1426151
    Signed-off-by: Zanxi Chen <chenzanxi@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55385
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Ben Kao <ben.kao@intel.com>

--- src/mainboard/google/dedede/variants/blipper/gpio.c
+++ src/mainboard/google/dedede/variants/blipper/gpio.c
@@ -7,8 +7,35 @@
 
 /* Pad configuration in ramstage*/
 static const struct pad_config gpio_table[] = {
+	/* C18 : AP_I2C_EMR_SDA */
+	PAD_NC(GPP_C18, NONE),
+	/* C19 : AP_I2C_EMR_SCL */
+	PAD_NC(GPP_C19, NONE),
+
+	/* D12 : WCAM_RST_L */
+	PAD_NC(GPP_D12, NONE),
 	/* D13 : EN_PP2800_CAMERA */
 	PAD_CFG_GPO(GPP_D13, 1, PLTRST),
+	/* D14 : EN_PP1200_CAMERA */
+	PAD_NC(GPP_D14, NONE),
+	/* D15 : UCAM_RST_L */
+	PAD_NC(GPP_D15, NONE),
+	/* D19 : WWAN_WLAN_COEX1 */
+	PAD_NC(GPP_D19, NONE),
+	/* D20 : WWAN_WLAN_COEX2 */
+	PAD_NC(GPP_D20, NONE),
+	/* D21 : WWAN_WLAN_COEX3 */
+	PAD_NC(GPP_D21, NONE),
+
+	/* E0  : CLK_24M_UCAM */
+	PAD_NC(GPP_E0, NONE),
+	/* E2  : CLK_24M_WCAM */
+	PAD_NC(GPP_E2, NONE),
+
+	/* H6  : AP_I2C_CAM_SDA */
+	PAD_NC(GPP_H6, NONE),
+	/* H7  : AP_I2C_CAM_SCL */
+	PAD_NC(GPP_H7, NONE),
 };
 
 const struct pad_config *variant_override_gpio_table(size_t *num)

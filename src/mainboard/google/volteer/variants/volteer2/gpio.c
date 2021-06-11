1a36211a839affd2ba4b9e5cf494ade81f99cdad
Nick Vaccaro <nvaccaro@google.com>
2021-06-11 11:32:45 -0700

    mb/google/volteer/var/volteer2: change GPP_B2 to PLTRST
    Change GPP_B2 (EN_PP3300_SSD) to PLTRST to avoid S3 resume hang.
    Add GPP_B2 to the early_gpio_table.
    
    BUG=b:174776411
    BRANCH=none
    TEST=none
    
    Change-Id: I07be950096aef42dbf4f067134e56c5849dfa02d
    Signed-off-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55433
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Furquan Shaikh <furquan@google.com>

--- src/mainboard/google/volteer/variants/volteer2/gpio.c
+++ src/mainboard/google/volteer/variants/volteer2/gpio.c
@@ -29,7 +29,7 @@ static const struct pad_config override_gpio_table[] = {
 	PAD_CFG_NF(GPP_A23, NONE, DEEP, NF1),
 
 	/* B2  : VRALERT# ==> EN_PP3300_SSD */
-	PAD_CFG_GPO(GPP_B2, 1, DEEP),
+	PAD_CFG_GPO(GPP_B2, 1, PLTRST),
 	/* B3  : CPU_GP2 ==> PEN_DET_ODL */
 	PAD_CFG_GPI(GPP_B3, NONE, DEEP),
 	/* B5  : ISH_I2C0_CVF_SDA */
@@ -220,6 +220,8 @@ static const struct pad_config early_gpio_table[] = {
 	/* A17 : DDSP_HPDC ==> MEM_CH_SEL */
 	PAD_CFG_GPI(GPP_A17, NONE, DEEP),
 
+	/* B2  : VRALERT# ==> EN_PP3300_SSD */
+	PAD_CFG_GPO(GPP_B2, 1, PLTRST),
 	/* B11 : PMCALERT# ==> PCH_WP_OD */
 	PAD_CFG_GPI_GPIO_DRIVER(GPP_B11, NONE, DEEP),
 	/* B15 : GSPI0_CS0# ==> PCH_GSPI0_H1_TPM_CS_L */

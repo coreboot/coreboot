f8986a94b175d8703e18265c8c079fc73e8bcbc5
Angel Pons <th3fanbus@gmail.com>
2021-06-14 11:47:06 +0200

    soc/intel/broadwell: Drop unused PSS macros
    These macros were used to generate ACPI P-state entries, but Broadwell
    now uses Haswell CPU code. These macros are unused and can be removed.
    
    Tested with BUILD_TIMELESS=1, Purism Librem 13 v1 remains identical.
    
    Change-Id: Ib2baca2964d9177e7ab6630d4ced22c5d332fb6e
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55487
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/soc/intel/broadwell/include/soc/acpi.h
+++ src/soc/intel/broadwell/include/soc/acpi.h
@@ -5,12 +5,6 @@
 
 #include <acpi/acpi.h>
 
-/* P-state configuration */
-#define PSS_MAX_ENTRIES			8
-#define PSS_RATIO_STEP			2
-#define PSS_LATENCY_TRANSITION		10
-#define PSS_LATENCY_BUSMASTER		10
-
 unsigned long northbridge_write_acpi_tables(const struct device *dev,
 	unsigned long current, struct acpi_rsdp *rsdp);
 #endif

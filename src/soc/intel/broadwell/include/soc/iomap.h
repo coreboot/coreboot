74fdd1a5b28c08fec0b07129069f860f82fcdb44
Angel Pons <th3fanbus@gmail.com>
2021-06-14 11:03:46 +0200

    soc/intel/broadwell: Use Haswell memmap.h in iomap.h
    Include Haswell memmap.h from Broadwell iomap.h to deduplicate identical
    definitions. This also prevents the definitions from falling out of sync
    while the unification process is ongoing.
    
    Tested with BUILD_TIMELESS=1, Purism Librem 13 v1 remains identical.
    
    Change-Id: I850e5521effba3818f4e2a13b94281bf07857d50
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55483
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/soc/intel/broadwell/include/soc/iomap.h
+++ src/soc/intel/broadwell/include/soc/iomap.h
@@ -3,25 +3,9 @@
 #ifndef _BROADWELL_IOMAP_H_
 #define _BROADWELL_IOMAP_H_
 
-#define MCH_BASE_SIZE		0x8000
-
-#define DMI_BASE_SIZE		0x1000
-
-#define EP_BASE_SIZE		0x1000
-
-#define EDRAM_BASE_ADDRESS	0xfed80000
-#define EDRAM_BASE_SIZE		0x4000
-
-#define GDXC_BASE_ADDRESS	0xfed84000
-#define GDXC_BASE_SIZE		0x1000
+#include <northbridge/intel/haswell/memmap.h>
 
 #define HPET_BASE_ADDRESS	0xfed00000
-
-#define GFXVT_BASE_ADDRESS	0xfed90000ULL
-#define GFXVT_BASE_SIZE		0x1000
-
-#define VTVC0_BASE_ADDRESS	0xfed91000ULL
-#define VTVC0_BASE_SIZE		0x1000
 
 #define ACPI_BASE_ADDRESS	0x1000
 #define ACPI_BASE_SIZE		0x100

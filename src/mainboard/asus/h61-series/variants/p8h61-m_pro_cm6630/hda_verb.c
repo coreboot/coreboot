cebf1e8c463bc8614877c524a19ecf0eedae5f47
Alice Sell <devfusediboot@protonmail.com>
2021-06-17 17:09:00 -0700

    mb/asus/p8h61-m_pro_cm6630: Add initial support
    This motherboard is somewhat similar to the p8h61-m_pro, but also different.
    It has two exposed RAM slots with a pinout for four, and it's an OEM
    variant used in PCs ASUS sold in stores.
    
    Signed-off-by: Hunter Sell <alicelyralain@gmail.com>
    Change-Id: Id08349feb0aeaf21406f814f6d19bbe0d9312a4d
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55661
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

---
+++ src/mainboard/asus/h61-series/variants/p8h61-m_pro_cm6630/hda_verb.c
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/azalia_device.h>
+
+const u32 cim_verb_data[] = {
+	0x10ec0887,	/* Codec Vendor / Device ID: Realtek ALC887 */
+	0x10438444,	/* Subsystem ID */
+	15,		/* Number of 4 dword sets */
+	AZALIA_SUBVENDOR(0, 0x10438444),
+	AZALIA_PIN_CFG(0, 0x11, 0x99430140),
+	AZALIA_PIN_CFG(0, 0x12, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x14, 0x01014010),
+	AZALIA_PIN_CFG(0, 0x15, 0x01011012),
+	AZALIA_PIN_CFG(0, 0x16, 0x01016011),
+	AZALIA_PIN_CFG(0, 0x17, 0x01012014),
+	AZALIA_PIN_CFG(0, 0x18, 0x01a19850),
+	AZALIA_PIN_CFG(0, 0x19, 0x02a19c60),
+	AZALIA_PIN_CFG(0, 0x1a, 0x0181305f),
+	AZALIA_PIN_CFG(0, 0x1b, 0x02214c20),
+	AZALIA_PIN_CFG(0, 0x1c, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1d, 0x4005e601),
+	AZALIA_PIN_CFG(0, 0x1e, 0x01456130),
+	AZALIA_PIN_CFG(0, 0x1f, 0x411111f0),
+
+	0x80862805,	/* Codec Vendor / Device ID: Intel HDMI */
+	0x80860101,	/* Subsystem ID */
+	4,		/* Number of 4 dword sets */
+	AZALIA_SUBVENDOR(3, 0x80860101),
+	AZALIA_PIN_CFG(3, 0x05, 0x58560010),
+	AZALIA_PIN_CFG(3, 0x06, 0x58560020),
+	AZALIA_PIN_CFG(3, 0x07, 0x18560030),
+};
+
+const u32 pc_beep_verbs[0] = {};
+
+AZALIA_ARRAY_SIZES;

d5b0000856f22c79f23333e3589b3566a5aa1447
Ryan Chuang <ryan.chuang@mediatek.corp-partner.google.com>
2021-06-23 09:44:57 +0800

    mb/google/cherry: Add mt6360 driver for PMIC access
    Add initialization of mt6360 drvier used by DRAM calibration test.
    
    Signed-off-by: Ryan Chuang <ryan.chuang@mediatek.corp-partner.google.com>
    Change-Id: Id74835d8395afac9e7e2c987a0a033f1b524fbfb
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55750
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/mainboard/google/cherry/romstage.c
+++ src/mainboard/google/cherry/romstage.c
@@ -3,17 +3,23 @@
 #include <arch/stages.h>
 #include <soc/clkbuf.h>
 #include <soc/emi.h>
+#include <soc/i2c.h>
 #include <soc/mt6315.h>
 #include <soc/mt6359p.h>
+#include <soc/mt6360.h>
 #include <soc/pmif.h>
 #include <soc/rtc.h>
 #include <soc/scp.h>
+
+#define I2C_BUS 7
 
 void platform_romstage_main(void)
 {
 	mtk_pmif_init();
 	mt6359p_init();
 	mt6315_init();
+	mtk_i2c_bus_init(I2C_BUS);
+	mt6360_init(I2C_BUS);
 	clk_buf_init();
 	rtc_boot();
 	mtk_dram_init();

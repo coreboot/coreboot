ae626d30355b4744762d2c434e159ba9c3998783
Angel Pons <th3fanbus@gmail.com>
2021-06-23 15:53:22 +0200

    broadwell boards: Do not set `ddr_refresh_2x` again
    The `ddr_refresh_2x` setting is already set in chipset code.
    
    Change-Id: I76478689b3aa27c369a0413d9fbde03674d5e528
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55810
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/jecht/spd/spd.c
+++ src/mainboard/google/jecht/spd/spd.c
@@ -8,6 +8,4 @@ void mainboard_fill_spd_data(struct pei_data *pei_data)
 {
 	pei_data->spd_addresses[0] = 0xa0;
 	pei_data->spd_addresses[2] = 0xa4;
-	// Enable 2x refresh mode
-	pei_data->ddr_refresh_2x = 1;
 }

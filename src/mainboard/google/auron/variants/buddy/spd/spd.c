887b779c11363b5e9b9be0d2260598fa12a92c43
Angel Pons <th3fanbus@gmail.com>
2021-06-23 14:16:10 +0200

    Broadwell boards: Do not assign unused SPD addresses
    The `pei_data` struct is already zero-initialised.
    
    Change-Id: If539cddc007f32a04389bc3b3b06c43cb5c86e10
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/55804
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/auron/variants/buddy/spd/spd.c
+++ src/mainboard/google/auron/variants/buddy/spd/spd.c
@@ -7,9 +7,7 @@
 void mainboard_fill_spd_data(struct pei_data *pei_data)
 {
 	pei_data->spd_addresses[0] = 0xa0;
-	pei_data->spd_addresses[1] = 0x00;
 	pei_data->spd_addresses[2] = 0xa4;
-	pei_data->spd_addresses[3] = 0x00;
 	pei_data->dimm_channel0_disabled = 2;
 	pei_data->dimm_channel1_disabled = 2;
 	/* Enable 2x refresh mode */

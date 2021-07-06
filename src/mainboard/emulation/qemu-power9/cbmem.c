bcbcdf73943d8bc81f2e35c4a67eebcbde716eda
Yaroslav Kurlaev <yaroslav.kurlaev@3mdeb.com>
2021-07-06 22:30:28 +0700

    src/mainboard/emulation/qemu-power9: add RAM detection
    Change-Id: Ie333294c7a311f6d47bdfbd1fc3cec0128cf63e7
    Signed-off-by: Yaroslav Kurlaev <yaroslav.kurlaev@3mdeb.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/57081
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/emulation/qemu-power9/cbmem.c
+++ src/mainboard/emulation/qemu-power9/cbmem.c
@@ -1,11 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <cbmem.h>
+#include <ramdetect.h>
 
 void *cbmem_top_chipset(void)
 {
-	/* Top of cbmem is at lowest usable DRAM address below 4GiB. */
-	/* For now, last 1M of 4G */
-	void *ptr = (void *) (4ULL * GiB - 1 * MiB);
-	return ptr;
+	return (void *)(probe_ramsize(0, CONFIG_DRAM_SIZE_MB) * MiB);
 }

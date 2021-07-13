2ecf1561b4f7682bec22be9943270cbd518a1552
Felix Held <felix-coreboot@felixheld.de>
2021-07-14 00:30:53 +0200

    soc/amd/*/mca: factor out BERT entry generation to soc/amd/common
    Change-Id: I960a2f384f11e4aa5aa2eb0645b6046f9f2f8847
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/56283
    Reviewed-by: Martin Roth <martinroth@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/common/block/include/amdblocks/mca.h
+++ src/soc/amd/common/block/include/amdblocks/mca.h
@@ -3,7 +3,15 @@
 #ifndef AMD_BLOCK_MCA_H
 #define AMD_BLOCK_MCA_H
 
+#include <cpu/x86/msr.h>
+
+struct mca_bank_status {
+	unsigned int bank;
+	msr_t sts;
+};
+
 void check_mca(void);
 void mca_check_all_banks(void);
+void build_bert_mca_error(struct mca_bank_status *mci);
 
 #endif /* AMD_BLOCK_MCA_H */

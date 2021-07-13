dc970fc0399b96e42671770fad6a7da5a40de461
Felix Held <felix-coreboot@felixheld.de>
2021-07-13 23:38:22 +0200

    soc/amd/cezanne/mca: add empty mca_check_all_banks function
    This will allow factoring out and moving check_mca() to soc/amd/common.
    
    Change-Id: I92c7657baef17c248a5aef1eda268e9647502837
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/56280
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/cezanne/mca.c
+++ src/soc/amd/cezanne/mca.c
@@ -3,11 +3,15 @@
 #include <amdblocks/mca.h>
 #include <cpu/x86/msr.h>
 
+static void mca_check_all_banks(void)
+{
+	/* TODO: Implement MCAX register checking and BERT table generation. */
+}
+
 /* Check the Machine Check Architecture Extension registers */
 void check_mca(void)
 {
-	/* TODO: Implement MCAX register checking and BERT table generation. */
-
+	mca_check_all_banks();
 	/* mca_clear_status uses the MCA registers and not the MCAX ones. Since they are
 	   aliases, we can use either set of registers. */
 	mca_clear_status();

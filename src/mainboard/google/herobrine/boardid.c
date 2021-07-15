1158f712dc99c8fe4dc4c70009cad0c3fdb7c80d
Philip Chen <philipchen@google.com>
2021-07-15 15:31:51 -0700

    mb/google/herobrine: Retrieve SKU ID from EC
    BUG=b:186264627
    BRANCH=none
    TEST=build herobrine
    
    Signed-off-by: Philip Chen <philipchen@google.com>
    Change-Id: Id3faf7af64c0129ec646a01085adc43b561225d2
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/56354
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Shelley Chen <shchen@google.com>

--- src/mainboard/google/herobrine/boardid.c
+++ src/mainboard/google/herobrine/boardid.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <boardid.h>
+#include <ec/google/chromeec/ec.h>
 #include <gpio.h>
 
 uint32_t board_id(void)
@@ -19,7 +20,5 @@ uint32_t ram_code(void)
 
 uint32_t sku_id(void)
 {
-	static uint32_t id = UNDEFINED_STRAPPING_ID;
-
-	return id;
+	return google_chromeec_get_board_sku();
 }

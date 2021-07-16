73193cf7b77592d52bb0c60da887af227ac0ace7
Raul E Rangel <rrangel@chromium.org>
2021-07-16 13:53:29 -0600

    soc/amd/{common,cezanne}: Implement HAVE_PAYLOAD_PRELOAD_CACHE
    This change allows preloading the payload.
    
    BUG=b:179699789
    TEST=Boot guybrush and see payload read/decompress drop by 20 ms. We
    now spend 7ms decompression from RAM. By switching to LZ4 we drop that
    to 500us.
    
    Signed-off-by: Raul E Rangel <rrangel@chromium.org>
    Change-Id: I3ec78e628f24f2ba0c9fcf2a9e3bde64687eec44
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/56053
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>

--- src/soc/amd/cezanne/fsp_s_params.c
+++ src/soc/amd/cezanne/fsp_s_params.c
@@ -1,8 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <acpi/acpi.h>
 #include <amdblocks/apob_cache.h>
 #include <device/pci.h>
 #include <fsp/api.h>
+#include <program_loading.h>
 
 static void fsp_assign_vbios_upds(FSP_S_CONFIG *scfg)
 {
@@ -22,4 +24,18 @@ void platform_fsp_silicon_init_params_cb(FSPS_UPD *supd)
 	 * no SPI operations, we can read the APOB while FSP-S executes.
 	 */
 	start_apob_cache_read();
+	/*
+	 * We enqueue the payload to be loaded after the APOB. This might cause a bit of
+	 * bus contention when loading uCode and OPROMs, but since those calls happen at
+	 * different points in the boot state machine it's a little harder to sequence all the
+	 * async loading correctly. So in order to keep the complexity down, we enqueue the
+	 * payload preload here. The end goal will be to add uCode and OPROM preloading
+	 * before the payload so that the sequencing is correct.
+	 *
+	 * While FSP-S is executing, it's not currently possible to enqueue other transactions
+	 * because FSP-S doesn't call `thread_yield()`. So the payload will start loading
+	 * right after FSP-S completes.
+	 */
+	if (!acpi_is_wakeup_s3())
+		payload_preload();
 }

d59c950379e54d9c0a42fddeac38a2108c5ee953
Zhi Li <lizhi7@huaqin.corp-partner.google.com>
2021-08-03 21:35:49 +0800

    mb/google/dedede/var/sasukette: Add FW_CONFIG probe for EXT_VR
    commit df520855 (soc/intel/jsl: Add disable_external_bypass_vr config)
    Add FW_CONFIG probe for don't stuffing ANPEC APW8738BQBI IC.
    
    BUG=b:190727416
    BRANCH=dedede
    TEST=test for enter S0ix and resume normally by powerd_dbus_suspend
    
    Signed-off-by: Zhi Li <lizhi7@huaqin.corp-partner.google.com>
    Change-Id: I15ab30f14df9dc02157009091aa8398e2fa75188
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/56804
    Reviewed-by: Weimin Wu <wuweimin@huaqin.corp-partner.google.com>
    Reviewed-by: Henry Sun <henrysun@google.com>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/dedede/variants/sasukette/ramstage.c
+++ src/mainboard/google/dedede/variants/sasukette/ramstage.c
@@ -6,6 +6,7 @@
 #include <fw_config.h>
 #include <soc/pci_devs.h>
 #include <drivers/i2c/generic/chip.h>
+#include <soc/soc_chip.h>
 
 extern struct chip_operations drivers_i2c_generic_ops;
 
@@ -29,7 +30,16 @@ static void audio_codec_update(void)
 		config->hid = "RTL5682";
 }
 
+static void ext_vr_update(void)
+{
+	struct soc_intel_jasperlake_config *cfg = config_of_soc();
+
+	if (fw_config_probe(FW_CONFIG(EXT_VR, EXT_VR_ABSENT)))
+		cfg->disable_external_bypass_vr = 1;
+}
+
 void variant_devtree_update(void)
 {
 	audio_codec_update();
+	ext_vr_update();
 }

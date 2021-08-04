0c78fffa54d54249854f83b4711c587e007e85ea
Sunwei Li <lisunwei@huaqin.corp-partner.google.com>
2021-08-04 10:28:34 +0800

    mb/google/dedede/var/cappy2: Add fw_config probe for multi audio codec
    Compatible headphone codec "Realtek ALC5682I-VD" and "cirrus CS42L42"
    Compatible AMP codec "ALC1015Q-VB" and "MAX98360"
    
    BUG=b:193373320
    BRANCH=dedede
    TEST=Both realtek and cirrus audio codec can work normally
    
    Signed-off-by: Sunwei Li <lisunwei@huaqin.corp-partner.google.com>
    Change-Id: I9121e75eaf46b43e6dc5ef2e31029a153c7a807d
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/56795
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Weimin Wu <wuweimin@huaqin.corp-partner.google.com>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>

--- src/mainboard/google/dedede/variants/cappy2/gpio.c
+++ src/mainboard/google/dedede/variants/cappy2/gpio.c
@@ -2,11 +2,15 @@
 
 #include <baseboard/gpio.h>
 #include <baseboard/variants.h>
+#include <bootstate.h>
+#include <fw_config.h>
 
 /* Pad configuration in ramstage */
 static const struct pad_config gpio_table[] = {
 	/* D15 : EN_PP3300_CAMERA */
 	PAD_CFG_GPO(GPP_D15, 1, PLTRST),
+	/* H16 : AP_SUB_IO_L ==> HP_RST_ODL */
+	PAD_CFG_GPO(GPP_H16, 1, PWROK),
 };
 
 const struct pad_config *variant_override_gpio_table(size_t *num)
@@ -14,3 +18,21 @@ const struct pad_config *variant_override_gpio_table(size_t *num)
 	*num = ARRAY_SIZE(gpio_table);
 	return gpio_table;
 }
+
+static const struct pad_config codec_rt5682_pads[] = {
+	PAD_NC(GPP_H16, NONE),
+};
+
+static const struct pad_config codec_cs42l42_pads[] = {
+	PAD_NC(GPP_D18, NONE),
+};
+
+static void fw_config_handle(void *unused)
+{
+	if (fw_config_probe(FW_CONFIG(AUDIO_CODEC_SOURCE, AUDIO_CODEC_ALC5682)))
+		gpio_configure_pads(codec_rt5682_pads, ARRAY_SIZE(codec_rt5682_pads));
+	if (fw_config_probe(FW_CONFIG(AUDIO_CODEC_SOURCE, AUDIO_CODEC_CS42l42)))
+		gpio_configure_pads(codec_cs42l42_pads, ARRAY_SIZE(codec_cs42l42_pads));
+}
+
+BOOT_STATE_INIT_ENTRY(BS_DEV_ENABLE, BS_ON_ENTRY, fw_config_handle, NULL);

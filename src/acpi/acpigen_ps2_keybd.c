coreboot/coreboot

commit ffd80fd2b7193d1c10eba11116904863cc0ea978

arch/x86/acpi: Add code for KEY_MICMUTE and KEY_KBDILLUMTOGGLE
Chromebook have some platform need support MICMUTE and KBDILLUMTOGGLE.

BUG=b:194146863
BRANCH=none
TEST=check on evtest
type 4 (EV_MSC), code 4 (MSC_SCAN), value ab
type 1 (EV_KEY), code 228 (KEY_KBDILLUMTOGGLE), 1

type 4 (EV_MSC), code 4 (MSC_SCAN), value 9b
type 1 (EV_KEY), code 248 (KEY_MICMUTE), value 1

Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
Change-Id: Ic39ecb3118e885c9e6d84d7b78bf435cb903d17e
Reviewed-on: https://review.coreboot.org/c/coreboot/+/56709
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Lance Zhao

src/acpi/acpigen_ps2_keybd.c
@@ -54,6 +54,8 @@ static const uint32_t action_keymaps[] = {
 	[PS2_KEY_KBD_BKLIGHT_UP] = KEYMAP(0x98, KEY_KBDILLUMUP),     /* e018 */
 	[PS2_KEY_PRIVACY_SCRN_TOGGLE] = KEYMAP(0x96,		     /* e016 */
 					  KEY_PRIVACY_SCREEN_TOGGLE),
+	[PS2_KEY_MICMUTE] = KEYMAP(0x9b, KEY_MICMUTE),       /* e01b */
+	[PS2_KEY_KBDILLUMTOGGLE] = KEYMAP(0xab, KEY_KBDILLUMTOGGLE), /* e02b */
 };
 
 /* Keymap for numeric keypad keys */

src/include/acpi/acpigen_ps2_keybd.h
@@ -24,6 +24,8 @@ enum ps2_action_key {
 	PS2_KEY_PLAY_PAUSE,
 	PS2_KEY_NEXT_TRACK,
 	PS2_KEY_PREV_TRACK,
+	PS2_KEY_KBDILLUMTOGGLE,
+	PS2_KEY_MICMUTE,
 };
 
 #define PS2_MIN_TOP_ROW_KEYS		10

commit 9e34060d52533b5f0891d2a31beb65946d27e082

acpi/acpigen_ps2_keyb: Align comments using tabs
Fixes: ffd80fd2 ("arch/x86/acpi: Add code for KEY_MICMUTE and KEY_KBDILLUMTOGGLE")
Change-Id: I2cca8cdbbd3607acca88da7b273ca6b080db9e7c
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/56749
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Lance Zhao
Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

src/acpi/acpigen_ps2_keybd.c
@@ -48,14 +48,14 @@ static const uint32_t action_keymaps[] = {
 	[PS2_KEY_NEXT_TRACK] = KEYMAP(0x99, KEY_NEXTSONG),	/* e019 */
 	[PS2_KEY_PREV_TRACK] = KEYMAP(0x90, KEY_PREVIOUSSONG),	/* e010 */
 	[PS2_KEY_SNAPSHOT] = KEYMAP(0x93, KEY_SYSRQ),		/* e013 */
-	[PS2_KEY_BRIGHTNESS_DOWN] = KEYMAP(0x94, KEY_BRIGHTNESSDOWN),/* e014 */
-	[PS2_KEY_BRIGHTNESS_UP] = KEYMAP(0x95, KEY_BRIGHTNESSUP),    /* e015 */
-	[PS2_KEY_KBD_BKLIGHT_DOWN] = KEYMAP(0x97, KEY_KBDILLUMDOWN), /* e017 */
-	[PS2_KEY_KBD_BKLIGHT_UP] = KEYMAP(0x98, KEY_KBDILLUMUP),     /* e018 */
-	[PS2_KEY_PRIVACY_SCRN_TOGGLE] = KEYMAP(0x96,		     /* e016 */
+	[PS2_KEY_BRIGHTNESS_DOWN] = KEYMAP(0x94, KEY_BRIGHTNESSDOWN),	/* e014 */
+	[PS2_KEY_BRIGHTNESS_UP] = KEYMAP(0x95, KEY_BRIGHTNESSUP),	/* e015 */
+	[PS2_KEY_KBD_BKLIGHT_DOWN] = KEYMAP(0x97, KEY_KBDILLUMDOWN),	/* e017 */
+	[PS2_KEY_KBD_BKLIGHT_UP] = KEYMAP(0x98, KEY_KBDILLUMUP),	/* e018 */
+	[PS2_KEY_PRIVACY_SCRN_TOGGLE] = KEYMAP(0x96,			/* e016 */
 					  KEY_PRIVACY_SCREEN_TOGGLE),
-	[PS2_KEY_MICMUTE] = KEYMAP(0x9b, KEY_MICMUTE),       /* e01b */
-	[PS2_KEY_KBDILLUMTOGGLE] = KEYMAP(0xab, KEY_KBDILLUMTOGGLE), /* e02b */
+	[PS2_KEY_MICMUTE] = KEYMAP(0x9b, KEY_MICMUTE),			/* e01b */
+	[PS2_KEY_KBDILLUMTOGGLE] = KEYMAP(0xab, KEY_KBDILLUMTOGGLE),	/* e02b */
 };
 
 /* Keymap for numeric keypad keys */
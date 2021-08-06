6b744344144cde7156b68fe499c499785f82ba0b
stanley.wu <stanley1.wu@lcfc.corp-partner.google.com>
2021-08-07 01:23:44 +0800

    mb/google/dedede: add gooey variant
    gooey is the same design as boten, and differs only in replacing Cr50
    with discrete TPM.
    
    BUG=b:193366710, 197247706
    TEST=FW_NAME=gooey emerge-keeby coreboot
    
    Signed-off-by: stanley.wu <stanley1.wu@lcfc.corp-partner.google.com>
    Change-Id: I2a54f872a7d5c0bee76a9e6e309613d9357b380b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/56858
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

---
+++ src/mainboard/google/dedede/variants/gooey/Makefile.inc
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+ramstage-y += gpio.c
+
+smm-y += variant.c
+
+ramstage-y += variant.c
---
+++ src/mainboard/google/dedede/variants/gooey/gpio.c
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <commonlib/helpers.h>
+#include <vendorcode/google/chromeos/chromeos.h>
+
+/* Pad configuration in ramstage*/
+static const struct pad_config gpio_table[] = {
+	/* A10 : WWAN_EN => LTE_PWR_OFF_ODL */
+	PAD_CFG_GPO(GPP_A10, 1, PWROK),
+
+	/* B7  : WWAN_SAR_DETECT_R_ODL */
+	PAD_CFG_GPO(GPP_B7, 1, DEEP),
+
+	/* C12 : AP_PEN_DET_ODL */
+	PAD_CFG_GPI_GPIO_DRIVER(GPP_C12, UP_20K, DEEP),
+	/* C18 : AP_I2C_EMR_SDA */
+	PAD_NC(GPP_C18, NONE),
+	/* C19 : AP_I2C_EMR_SCL */
+	PAD_NC(GPP_C19, NONE),
+	/* C22 : UART2_RTS_N */
+	PAD_NC(GPP_C22, NONE),
+	/* C23 : UART2_CTS_N */
+	PAD_NC(GPP_C23, NONE),
+
+	/* D12 : WCAM_RST_L */
+	PAD_NC(GPP_D12, NONE),
+	/* D13 : EN_PP2800_CAMERA */
+	PAD_CFG_GPO(GPP_D13, 1, PLTRST),
+	/* D14 : EN_PP1200_CAMERA */
+	PAD_NC(GPP_D14, NONE),
+	/* D15 : UCAM_RST_L */
+	PAD_NC(GPP_D15, NONE),
+	/* D19 : WWAN_WLAN_COEX1 */
+	PAD_NC(GPP_D19, NONE),
+	/* D20 : WWAN_WLAN_COEX2 */
+	PAD_NC(GPP_D20, NONE),
+	/* D21 : WWAN_WLAN_COEX3 */
+	PAD_NC(GPP_D21, NONE),
+	/* D22 : AP_I2C_SUB_SDA*/
+	PAD_CFG_NF(GPP_D22, NONE, DEEP, NF1),
+	/* D23 : AP_I2C_SUB_SCL */
+	PAD_CFG_NF(GPP_D23, NONE, DEEP, NF1),
+
+	/* E0  : CLK_24M_UCAM */
+	PAD_NC(GPP_E0, NONE),
+	/* E2  : CLK_24M_WCAM */
+	PAD_NC(GPP_E2, NONE),
+	/* E11 : AP_I2C_SUB_INT_ODL */
+	PAD_CFG_GPI_APIC(GPP_E11, NONE, PLTRST, LEVEL, NONE),
+
+	/* H6  : AP_I2C_CAM_SDA */
+	PAD_NC(GPP_H6, NONE),
+	/* H7  : AP_I2C_CAM_SCL */
+	PAD_NC(GPP_H7, NONE),
+	/* H17 : WWAN_RST_L => LTE_RESET_R_ODL */
+	PAD_CFG_GPO(GPP_H17, 0, PLTRST),
+};
+
+const struct pad_config *variant_override_gpio_table(size_t *num)
+{
+	*num = ARRAY_SIZE(gpio_table);
+	return gpio_table;
+}

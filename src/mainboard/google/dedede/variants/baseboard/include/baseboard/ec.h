71a0bb57d7752192d6b78dc6e44505bebb3aedb2
Boris Mittelberg <bmbm@google.com>
2021-08-05 17:00:58 -0700

    mb/google/dedede: allow MKBP devices and disable TBMC device
    Enable MKBP (Matrix Keyboard Protocol) interface for all dedede family
    to use for buttons and switches. Disable TBMC (Tablet Mode Switch
    device), as it is not needed anymore.
    
    BUG=b:170966461
    TEST=manual test on Madoo:
    Volume Up/Down and Power buttons, Tablet Mode switch
    
    Cq-Depend: chromium:3069163
    Signed-off-by: Boris Mittelberg <bmbm@google.com>
    Change-Id: I9d1f43e4dd56318af4c1d5f5c1c3a2c237a05c5f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/56840
    Reviewed-by: Furquan Shaikh <furquan@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/dedede/variants/baseboard/include/baseboard/ec.h
+++ src/mainboard/google/dedede/variants/baseboard/include/baseboard/ec.h
@@ -62,12 +62,12 @@
 /* Enable EC backed ALS device in ACPI */
 #define EC_ENABLE_ALS_DEVICE
 
+/* Enable MKBP for buttons and switches */
+#define EC_ENABLE_MKBP_DEVICE
+
 /* Enable LID switch and provide wake pin for EC */
 #define EC_ENABLE_LID_SWITCH
 #define EC_ENABLE_WAKE_PIN      GPE_EC_WAKE
-
-/* Enable Tablet switch */
-#define EC_ENABLE_TBMC_DEVICE
 
 /* Enable EC backed PD MCU device in ACPI */
 #define EC_ENABLE_PD_MCU_DEVICE

f343ed42ebeac7d28fb751635a6771731baae554
Mario Scheithauer <mario.scheithauer@siemens.com>
2021-08-25 10:26:17 +0200

    mb/siemens/mc_ehl2: Set coreboot ready LED
    This mainboard has its own coreboot ready LED. The LED is switched
    on via GPIO GPP_F20.
    
    Change-Id: I3570d691e90d2cb6e11b856b876f0327da118522
    Signed-off-by: Mario Scheithauer <mario.scheithauer@siemens.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/58293
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Werner Zeh <werner.zeh@siemens.com>

--- src/mainboard/siemens/mc_ehl/variants/mc_ehl2/mainboard.c
+++ src/mainboard/siemens/mc_ehl/variants/mc_ehl2/mainboard.c
@@ -1,8 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <baseboard/variants.h>
+#include <bootstate.h>
 #include <console/console.h>
+#include <gpio.h>
 #include <intelblocks/pcr.h>
+#include <soc/gpio.h>
 #include <soc/pcr_ids.h>
 
 void variant_mainboard_final(void)
@@ -11,3 +14,11 @@ void variant_mainboard_final(void)
 	   INTA#->PIRQC#, INTB#->PIRQD#, INTC#->PIRQA#, INTD#-> PIRQB# */
 	pcr_write16(PID_ITSS, 0x3150, 0x1032);
 }
+
+static void finalize_boot(void *unused)
+{
+	/* Set coreboot ready LED. */
+	gpio_output(GPP_F20, 1);
+}
+
+BOOT_STATE_INIT_ENTRY(BS_PAYLOAD_BOOT, BS_ON_ENTRY, finalize_boot, NULL);

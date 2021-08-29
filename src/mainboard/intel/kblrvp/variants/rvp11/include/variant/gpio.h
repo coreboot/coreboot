6649b1750eba2b7c2f26d5c6e70571718eec3785
Angel Pons <th3fanbus@gmail.com>
2021-08-29 11:26:41 +0200

    mb/intel: Drop unused `GPIO_MEM_CONFIG_.` defines
    These defines are copy-paste leftovers from Kunimitsu. However, neither
    Saddle Brook nor KBLRVP use memory-down, so drop the unneeded defines.
    
    Change-Id: I396aeaa634f619be7be0ee97c0cab1c682f53ff2
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/57231
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>

--- src/mainboard/intel/kblrvp/variants/rvp11/include/variant/gpio.h
+++ src/mainboard/intel/kblrvp/variants/rvp11/include/variant/gpio.h
@@ -24,12 +24,6 @@
 /* BIOS Flash Write Protect */
 #define GPIO_PCH_WP		GPP_C23
 
-/* Memory configuration board straps */
-#define GPIO_MEM_CONFIG_0	GPP_C12
-#define GPIO_MEM_CONFIG_1	GPP_C13
-#define GPIO_MEM_CONFIG_2	GPP_C14
-#define GPIO_MEM_CONFIG_3	GPP_C15
-
 /* EC wake is LAN_WAKE# which is a special DeepSX wake pin */
 #define GPE_EC_WAKE		GPE0_LAN_WAK
 

cd807213d80612356510f973ea3e6c60dbdcc4f1
Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
2021-08-31 18:27:13 +0600

    soc/intel/alderlake: Add igd device
    Add igd device name in soc_acpi_name(), and src/drivers/gfx/generic
    can generate device in GFX0 scope in ssdt.
    
    BUG=b:198188272
    TEST=emerge-brya coreboot and check ssdt.
    
    Change-Id: Id0c50254a8a25b47368e932c99243f4f02250b82
    Signed-off-by: Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/57288
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/soc/intel/alderlake/chip.c
+++ src/soc/intel/alderlake/chip.c
@@ -61,6 +61,7 @@ const char *soc_acpi_name(const struct device *dev)
 
 	switch (dev->path.pci.devfn) {
 	case SA_DEVFN_ROOT:		return "MCHC";
+	case SA_DEVFN_IGD:		return "GFX0";
 	case SA_DEVFN_TCSS_XHCI:	return "TXHC";
 	case SA_DEVFN_TCSS_XDCI:	return "TXDC";
 	case SA_DEVFN_TCSS_DMA0:	return "TDM0";

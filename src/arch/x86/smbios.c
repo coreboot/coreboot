12b809bf00f2eaf23ef45c7dfb16233ce0ef517f
Angel Pons <th3fanbus@gmail.com>
2021-09-03 11:58:04 +0200

    smbios.c: Rename two local functions
    Rename two functions that have `walk` in their name but do not perform
    any walk. The new names are derived from the comments just above these
    functions' definitions. Also, remove these now-redundant comments.
    
    Change-Id: I380a5b60b3f4e820e8f6d6f960826de97c0446be
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/57361
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/arch/x86/smbios.c
+++ src/arch/x86/smbios.c
@@ -1132,9 +1132,8 @@ static u8 smbios_get_device_type_from_dev(struct device *dev)
 	}
 }
 
-/* Generate Type41 entries from devicetree */
-static int smbios_walk_device_tree_type41(struct device *dev, int *handle,
-					  unsigned long *current)
+static int smbios_generate_type41_from_devtree(struct device *dev, int *handle,
+					       unsigned long *current)
 {
 	static u8 type41_inst_cnt[SMBIOS_DEVICE_TYPE_COUNT + 1] = {};
 
@@ -1164,9 +1163,8 @@ static int smbios_walk_device_tree_type41(struct device *dev, int *handle,
 					device_type);
 }
 
-/* Generate Type9 entries from devicetree */
-static int smbios_walk_device_tree_type9(struct device *dev, int *handle,
-					 unsigned long *current)
+static int smbios_generate_type9_from_devtree(struct device *dev, int *handle,
+					      unsigned long *current)
 {
 	enum misc_slot_usage usage;
 	enum slot_data_bus_bandwidth bandwidth;
@@ -1225,8 +1223,8 @@ static int smbios_walk_device_tree(struct device *tree, int *handle, unsigned lo
 			printk(BIOS_INFO, "%s (%s)\n", dev_path(dev), dev_name(dev));
 			len += dev->ops->get_smbios_data(dev, handle, current);
 		}
-		len += smbios_walk_device_tree_type9(dev, handle, current);
-		len += smbios_walk_device_tree_type41(dev, handle, current);
+		len += smbios_generate_type9_from_devtree(dev, handle, current);
+		len += smbios_generate_type41_from_devtree(dev, handle, current);
 	}
 	return len;
 }

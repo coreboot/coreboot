coreboot/coreboot

commit cc50770cd0eeab0794264f2e6bccdfa7c117f2b9

wifi: Add support for wifi time average SAR config
Add support for the WTAS ACPI BIOS configuration table as per the
connectivity document:
559910_Intel_Connectivity_Platforms_BIOS_Guidelines_Rev6_4.pdf

BUG=b:193665559
TEST=Generated SAR file with the WTAS related configuration values and
verified that the SSDT has the WTAS ACPI table.

Change-Id: I42cf3cba7974e6db0e05de30846ef103a15fd584
Signed-off-by: Sugnan Prabhu S <sugnan.prabhu.s@intel.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/57061
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

src/drivers/wifi/generic/acpi.c
@@ -267,6 +267,58 @@ static void sar_emit_ppag(struct gain_profile *ppag)
 	acpigen_write_package_end();
 }
 
+static void sar_emit_wtas(struct avg_profile *wtas)
+{
+	int i;
+	size_t package_size;
+
+	if (wtas == NULL)
+		return;
+
+	/*
+	 * Name (WTAS, Package() {
+	 * {
+	 *   Revision,
+	 *   Package()
+	 *   {
+	 *     DomainType,            // 0x7:WiFi
+	 *     WifiTASSelection,      // Enable/Disable the TAS feature
+	 *     WifiTASListEntries,    // No. of blocked countries not approved by OEM to
+	 *     BlockedListEntry1,        support this feature
+	 *     BlockedListEntry2,
+	 *     BlockedListEntry3,
+	 *     BlockedListEntry4,
+	 *     BlockedListEntry5,
+	 *     BlockedListEntry6,
+	 *     BlockedListEntry7,
+	 *     BlockedListEntry8,
+	 *     BlockedListEntry9,
+	 *     BlockedListEntry10,
+	 *     BlockedListEntry11,
+	 *     BlockedListEntry12,
+	 *     BlockedListEntry13,
+	 *     BlockedListEntry14,
+	 *     BlockedListEntry15,
+	 *     BlockedListEntry16,
+	 *   }
+	 * })
+	 */
+	package_size = 1 + 1 + 1 + MAX_DENYLIST_ENTRY;
+
+	acpigen_write_name("WTAS");
+	acpigen_write_package(2);
+	acpigen_write_dword(wtas->revision);
+	acpigen_write_package(package_size);
+	acpigen_write_dword(DOMAIN_TYPE_WIFI);
+	acpigen_write_dword(wtas->tas_selection);
+	acpigen_write_dword(wtas->tas_list_size);
+	for (i = 0; i < MAX_DENYLIST_ENTRY; i++)
+		acpigen_write_byte(wtas->deny_list_entry[i]);
+
+	acpigen_write_package_end();
+	acpigen_write_package_end();
+}
+
 static void emit_sar_acpi_structures(const struct device *dev)
 {
 	union wifi_sar_limits sar_limits;
@@ -288,6 +340,7 @@ static void emit_sar_acpi_structures(const struct device *dev)
 	sar_emit_ewrd(sar_limits.sar);
 	sar_emit_wgds(sar_limits.wgds);
 	sar_emit_ppag(sar_limits.ppag);
+	sar_emit_wtas(sar_limits.wtas);
 
 	free(sar_limits.sar);
 }

src/include/sar.h
@@ -5,9 +5,10 @@
 #include <stdint.h>
 
 #define MAX_ANT_GAINS_REVISION	2
+#define MAX_DENYLIST_ENTRY	16
 #define MAX_DSAR_SET_COUNT	3
 #define MAX_GEO_OFFSET_REVISION	3
-#define MAX_PROFILE_COUNT	3
+#define MAX_PROFILE_COUNT	4
 #define MAX_SAR_REVISION	2
 #define REVISION_SIZE		1
 #define SAR_REV0_CHAINS_COUNT	2
@@ -39,6 +40,13 @@ struct gain_profile {
 	uint8_t ppag_table[0];
 } __packed;
 
+struct avg_profile {
+	uint8_t revision;
+	uint8_t tas_selection;
+	uint8_t tas_list_size;
+	uint8_t deny_list_entry[MAX_DENYLIST_ENTRY];
+} __packed;
+
 struct sar_header {
 	char marker[SAR_STR_PREFIX_SIZE];
 	uint8_t version;
@@ -51,6 +59,7 @@ union wifi_sar_limits {
 		struct sar_profile *sar;
 		struct geo_profile *wgds;
 		struct gain_profile *ppag;
+		struct avg_profile *wtas;
 	};
 	void *profile[MAX_PROFILE_COUNT];
 };

src/vendorcode/google/chromeos/sar.c
@@ -76,6 +76,14 @@ static int gain_table_size(const struct gain_profile *gain)
 	return sizeof(struct gain_profile) + (gain->chains_count * gain->bands_count);
 }
 
+static int sar_avg_table_size(const struct avg_profile *sar_avg)
+{
+	if (sar_avg == NULL)
+		return 0;
+
+	return sizeof(struct avg_profile);
+}
+
 static bool valid_legacy_length(size_t bin_len)
 {
 	if (bin_len == LEGACY_SAR_WGDS_BIN_SIZE)
@@ -125,6 +133,7 @@ static int fill_wifi_sar_limits(union wifi_sar_limits *sar_limits, const uint8_t
 	expected_sar_bin_size += sar_table_size(sar_limits->sar);
 	expected_sar_bin_size += wgds_table_size(sar_limits->wgds);
 	expected_sar_bin_size += gain_table_size(sar_limits->ppag);
+	expected_sar_bin_size += sar_avg_table_size(sar_limits->wtas);
 
 	if (sar_bin_size != expected_sar_bin_size) {
 		printk(BIOS_ERR, "ERROR: Invalid SAR size, expected: %ld, obtained: %ld\n",
@@ -188,6 +197,7 @@ static int fill_wifi_sar_limits_legacy(union wifi_sar_limits *sar_limits,
  * [SAR_REVISION,DSAR_SET_COUNT,CHAINS_COUNT,SUBBANDS_COUNT <WRDD>[EWRD]]
  * [WGDS_REVISION,CHAINS_COUNT,SUBBANDS_COUNT<WGDS_DATA>]
  * [PPAG_REVISION,MODE,CHAINS_COUNT,SUBBANDS_COUNT<PPAG_DATA>]
+ * [WTAS_REVISION, WTAS_DATA]
  *
  * The configuration data will always have the revision added in the file for each of the
  * block, based on the revision number and validity, size of the specific block will be
@@ -222,6 +232,11 @@ static int fill_wifi_sar_limits_legacy(union wifi_sar_limits *sar_limits,
  *              [Antenna gain used for 6525-6705MHz frequency]
  *              [Antenna gain used for 6705-6865MHz frequency]
  *              [Antenna gain used for 6865-7105MHz frequency]
+ *
+ * [WTAS_DATA] =
+ *      [Enable/disable the TAS feature]
+ *      [Number of blocked countries that are not approved by the OEM to support this feature]
+ *      [deny_list_entry_<1-16>: ISO country code to block]
  */
 int get_wifi_sar_limits(union wifi_sar_limits *sar_limits)
 {

commit 627c8443a3ddaded2c1cf4ac0811164eb1943815

wifi: Update deny list entry size to uint16_t
As per the connectivity document deny list entry size should be uint16
559910_Intel_Connectivity_Platforms_BIOS_Guidelines_Rev6_4.pdf

Fixes: cc50770cd0("wifi: Add support for wifi time average SAR config")
Change-Id: I045c21350cf4c2266df108eede6350d090322ba0
Signed-off-by: Sugnan Prabhu S <sugnan.prabhu.s@intel.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/57407
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

src/drivers/wifi/generic/acpi.c
@@ -450,10 +450,10 @@ static void sar_emit_wtas(struct avg_profile *wtas)
 	acpigen_write_dword(wtas->revision);
 	acpigen_write_package(package_size);
 	acpigen_write_dword(DOMAIN_TYPE_WIFI);
-	acpigen_write_dword(wtas->tas_selection);
-	acpigen_write_dword(wtas->tas_list_size);
+	acpigen_write_byte(wtas->tas_selection);
+	acpigen_write_byte(wtas->tas_list_size);
 	for (i = 0; i < MAX_DENYLIST_ENTRY; i++)
-		acpigen_write_byte(wtas->deny_list_entry[i]);
+		acpigen_write_word(wtas->deny_list_entry[i]);
 
 	acpigen_write_package_end();
 	acpigen_write_package_end();

src/include/sar.h
@@ -44,7 +44,7 @@ struct avg_profile {
 	uint8_t revision;
 	uint8_t tas_selection;
 	uint8_t tas_list_size;
-	uint8_t deny_list_entry[MAX_DENYLIST_ENTRY];
+	uint16_t deny_list_entry[MAX_DENYLIST_ENTRY];
 } __packed;
 
 struct dsm_profile {
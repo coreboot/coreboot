522174ba382c4a75a49fe1116b320efbe9faa122
Furquan Shaikh <furquan@google.com>
2021-09-16 16:54:04 -0700

    mb/google/volteer: Switch to using device pointers using alias names
    This change replaces the device tree walks with device pointers by
    using alias names for the following devices:
    
    1. PMC MUX connector
    2. SPI TPM
    3. I2C TPM
    
    Change-Id: I38f87d3a90a7253f2a29aba7db9a9f9744985494
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/57740
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/mainboard/google/volteer/variants/volteer2/variant.c
+++ src/mainboard/google/volteer/variants/volteer2/variant.c
@@ -4,22 +4,14 @@
 #include <soc/pci_devs.h>
 #include <acpi/acpi_device.h>
 
-extern struct chip_operations drivers_i2c_tpm_ops;
-
-static bool match_i2c_tpm(DEVTREE_CONST struct device *dev)
-{
-	return dev->chip_ops == &drivers_i2c_tpm_ops;
-}
-
 /*
  * This function runs only on the volteer_ti50 variant, which has the GSC on a
  * reworked I2C bus.
  */
 static void devtree_enable_i2c_tpm(void)
 {
-	struct device *spi_tpm = pcidev_path_on_root(PCH_DEVFN_GSPI0)->link_list->children;
-	struct device *i2c_tpm = dev_find_matching_device_on_bus(
-		pcidev_path_on_root(PCH_DEVFN_I2C1)->link_list, match_i2c_tpm);
+	struct device *spi_tpm = DEV_PTR(spi_tpm);
+	struct device *i2c_tpm = DEV_PTR(i2c_tpm);
 	if (!i2c_tpm || !spi_tpm)
 		return;
 	spi_tpm->enabled = 0;

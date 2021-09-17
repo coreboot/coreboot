e7821e8de00fa673f75d620fa320ce7629ef2ce1
Furquan Shaikh <furquan@google.com>
2021-09-16 19:58:20 -0700

    mb/google/dedede: Clean up LTE device enabling
    On some dedede variants, USB port 2.3/3.3 might be connected to either
    LTE device or Type-A external port depending upon FW_CONFIG. Commit
    856b579 ("mb/google/dedede/var/kracko: Update LTE USB port
    configuration") enabled Type-A external port by default in override
    tree and updated the config dynamically for LTE USB device if
    FW_CONFIG indicated support for it. This was required because sconfig
    lacked the support for multiple override devices. Commit
    b9c22e0 ("util/sconfig: Compare probe conditions for override device
    match") fixed this behavior in sconfig and now we can add multiple
    override devices using different FW_CONFIG probe statements in
    override tree. Hence, this change moves the LTE USB device to override
    tree for metaknight, kracko and drawcia variants.
    
    In addition to that, drawcia needs to be update reset_gpio depending
    upon board_id. Thus, alias `lte_usb2` is used in drawcia override tree
    to fix the reset_gpio for older boards i.e. board_id <= 9.
    
    Change-Id: Ie5b205594680d9c2b8543c5c99325d95620cafd2
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/57742
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

--- src/mainboard/google/dedede/variants/drawcia/ramstage.c
+++ src/mainboard/google/dedede/variants/drawcia/ramstage.c
@@ -2,19 +2,23 @@
 
 #include <baseboard/variants.h>
 #include <boardid.h>
-
-static struct acpi_gpio lte_reset_gpio = ACPI_GPIO_OUTPUT_ACTIVE_LOW(GPP_H0);
-static struct acpi_gpio lte_enable_gpio = ACPI_GPIO_OUTPUT_ACTIVE_HIGH(GPP_A10);
-/* New lte reset for drapwer DVT*/
-static struct acpi_gpio lte_new_reset_gpio =  ACPI_GPIO_OUTPUT_ACTIVE_LOW(GPP_H17);
+#include <device/device.h>
+#include <drivers/usb/acpi/chip.h>
 
 void variant_devtree_update(void)
 {
-
 	uint32_t board_version = board_id();
+	struct device *lte_usb2 = DEV_PTR(lte_usb2);
+	struct drivers_usb_acpi_config *config;
+	struct acpi_gpio lte_reset_gpio = ACPI_GPIO_OUTPUT_ACTIVE_LOW(GPP_H0);
 
-	if (board_version <= 9) /* board version 9 is drawper EVT */
-		update_lte_device(&lte_reset_gpio, &lte_enable_gpio);
-	else
-		update_lte_device(&lte_new_reset_gpio, &lte_enable_gpio);
+	/* board version 9 is drawper EVT */
+	if (board_version > 9)
+		return;
+
+	if (lte_usb2 == NULL)
+		return;
+
+	config = config_of(lte_usb2);
+	config->reset_gpio = lte_reset_gpio;
 }

1724b57729795f4eed9fb401572f634aa286ab0c
Tim Crawford <tcrawford@system76.com>
2021-09-21 21:50:49 -0600

    soc/intel/tigerlake: Hook up GMA ACPI brightness controls
    Add function needed to generate ACPI backlight control SSDT, along with
    Kconfig values for accessing the registers.
    
    Tested by adding gfx register on system76/gaze16 and booting Windows.
    Display settings has a brightness setting, and can change the brightness
    level.
    
    Change-Id: Id8b14c0b4a7a681dc6cb95778c12a006a7e31373
    Signed-off-by: Tim Crawford <tcrawford@system76.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/57823
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/soc/intel/tigerlake/chip.h
+++ src/soc/intel/tigerlake/chip.h
@@ -4,6 +4,7 @@
 #define _SOC_CHIP_H_
 
 #include <drivers/i2c/designware/dw_i2c.h>
+#include <drivers/intel/gma/gma.h>
 #include <intelblocks/cfg.h>
 #include <intelblocks/gpio.h>
 #include <intelblocks/gspi.h>
@@ -532,6 +533,9 @@ struct soc_intel_tigerlake_config {
 	 *
 	 */
 	bool external_bypass;
+
+	/* i915 struct for GMA backlight control */
+	struct i915_gpu_controller_info gfx;
 };
 
 typedef struct soc_intel_tigerlake_config config_t;

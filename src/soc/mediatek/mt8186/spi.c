a6b3af927cb7005cf664e2065dbfcc49b0b295ca
Rex-BC Chen <rex-bc.chen@mediatek.corp-partner.google.com>
2021-09-23 20:21:18 +0800

    soc/mediatek/mt8186: Add NOR-Flash support
    Add NOR-Flash drivers to pass verification of flash at verstage.
    
    TEST=boot to romstage
    BUG=b:202871018
    
    Signed-off-by: Rex-BC Chen <rex-bc.chen@mediatek.com>
    Change-Id: If51d765e1fd4895f97898710ec6fa1374e1048fb
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/58837
    Reviewed-by: Rex-BC Chen <rex-bc.chen@mediatek.corp-partner.google.com>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/mediatek/mt8186/spi.c
+++ src/soc/mediatek/mt8186/spi.c
@@ -7,15 +7,19 @@
 
 #include <device/mmio.h>
 #include <soc/addressmap.h>
+#include <soc/flash_controller_common.h>
 #include <soc/spi.h>
 
 static const struct spi_ctrlr spi_flash_ctrlr = {
 	.max_xfer_size = 65535,
+	.flash_probe = mtk_spi_flash_probe,
 };
 
 const struct spi_ctrlr_buses spi_ctrlr_bus_map[] = {
 	{
 		.ctrlr = &spi_flash_ctrlr,
+		.bus_start = CONFIG_BOOT_DEVICE_SPI_FLASH_BUS,
+		.bus_end = CONFIG_BOOT_DEVICE_SPI_FLASH_BUS,
 	},
 };
 

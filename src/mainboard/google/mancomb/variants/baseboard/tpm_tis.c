f42fca18cb9e19096621588d9699ca98d332af7d
Karthikeyan Ramasubramanian <kramasub@google.com>
2021-09-27 16:26:29 -0600

    mb/google/mancomb: Delete board support
    Mancomb mainboard has been cancelled. Hence delete the board support.
    
    BUG=b:190404616
    TEST=None
    
    Cq-Depend: chromium:3188634
    Change-Id: I3ce02efb1fa5ea488447099abe08da6051fb6fc6
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/57996
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/mainboard/google/mancomb/variants/baseboard/tpm_tis.c
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#include <security/tpm/tis.h>
-#include <soc/gpio.h>
-
-int tis_plat_irq_status(void)
-{
-	return gpio_interrupt_status(GPIO_3);
-}

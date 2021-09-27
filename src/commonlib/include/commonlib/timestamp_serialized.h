c1a55f73ebd97b9675e968ef49b426e612250a3c
Bora Guvendik <bora.guvendik@intel.com>
2021-09-26 17:09:12 -0700

    commonlib: Add new TS for IA pre-cpu reset entities
    The idea here is to capture the various boot entities prior
    to IA cpu reset.
    
    BUG=b:182575295
    TEST=Boot to OS, check cbmem -t output
    
    Signed-off-by: Bora Guvendik <bora.guvendik@intel.com>
    Change-Id: If89befa362d7852a2c0743d05155a0b6c1634672
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/57969
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/commonlib/include/commonlib/timestamp_serialized.h
+++ src/commonlib/include/commonlib/timestamp_serialized.h
@@ -113,6 +113,11 @@ enum timestamp_id {
 	TS_ME_INFORM_DRAM_DONE = 941,
 	TS_ME_BEFORE_END_OF_POST = 942,
 	TS_ME_AFTER_END_OF_POST = 943,
+	TS_ME_BOOT_STALL_DONE = 944,
+	TS_ME_ICC_CONFIG_START = 945,
+	TS_ME_HOST_BOOT_PREP_DONE = 946,
+	TS_ME_RECEIVED_CRDA_FROM_PMC = 947,
+	TS_FIT_UCODE_LOADED = 948,
 
 	/* 950+ reserved for vendorcode extensions (950-999: intel/fsp) */
 	TS_FSP_MEMORY_INIT_START = 950,
@@ -267,6 +272,11 @@ static const struct timestamp_id_to_name {
 	{ TS_ME_INFORM_DRAM_DONE,	"finished waiting for ME response"},
 	{ TS_ME_BEFORE_END_OF_POST,	"before sending EOP to ME"},
 	{ TS_ME_AFTER_END_OF_POST,	"after sending EOP to ME"},
+	{ TS_ME_BOOT_STALL_DONE,	"CSE sent 'Boot Stall Done' to PMC"},
+	{ TS_ME_ICC_CONFIG_START,	"CSE started to handle ICC configuration"},
+	{ TS_ME_HOST_BOOT_PREP_DONE,	"CSE sent 'Host BIOS Prep Done' to PMC"},
+	{ TS_ME_RECEIVED_CRDA_FROM_PMC,	"CSE received 'CPU Reset Done Ack sent' from PMC"},
+	{ TS_FIT_UCODE_LOADED,		"CPU has loaded UCODE/PCODE from FIT"},
 
 	/* FSP related timestamps */
 	{ TS_FSP_MEMORY_INIT_START, "calling FspMemoryInit" },

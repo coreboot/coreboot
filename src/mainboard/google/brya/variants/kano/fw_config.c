5a76c79791e4787702b22d1cc7edf534dbf664af
David Wu <david_wu@quanta.corp-partner.google.com>
2021-10-18 17:32:53 +0800

    mb/google/brya/var/kano: Enable BT offload support
    Enable the CnviBtAudioOffload UPD and program the corresponding
    BT VPGIOs.
    
    BUG=b:202913826
    TEST=emerge-brya coreboot and verify it builds without error.
    
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Change-Id: Id81cba82742f552c098ec3719a0b453b752dc5c5
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/58415
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/mainboard/google/brya/variants/kano/fw_config.c
+++ src/mainboard/google/brya/variants/kano/fw_config.c
@@ -41,12 +41,35 @@ static const struct pad_config i2s_disable_pads[] = {
 	PAD_NC(GPP_S3, NONE),
 };
 
+static const struct pad_config bt_i2s_enable_pads[] = {
+	PAD_CFG_NF(GPP_VGPIO_30, NONE, DEEP, NF3),	/* BT_I2S_BCLK */
+	PAD_CFG_NF(GPP_VGPIO_31, NONE, DEEP, NF3),	/* BT_I2S_SYNC */
+	PAD_CFG_NF(GPP_VGPIO_32, NONE, DEEP, NF3),	/* BT_I2S_SDO */
+	PAD_CFG_NF(GPP_VGPIO_33, NONE, DEEP, NF3),	/* BT_I2S_SDI */
+	PAD_CFG_NF(GPP_VGPIO_34, NONE, DEEP, NF1),	/* SSP2_SCLK */
+	PAD_CFG_NF(GPP_VGPIO_35, NONE, DEEP, NF1),	/* SSP2_SFRM */
+	PAD_CFG_NF(GPP_VGPIO_36, NONE, DEEP, NF1),	/* SSP_TXD */
+	PAD_CFG_NF(GPP_VGPIO_37, NONE, DEEP, NF1),	/* SSP_RXD */
+};
+
+static const struct pad_config bt_i2s_disable_pads[] = {
+	PAD_NC(GPP_VGPIO_30, NONE),
+	PAD_NC(GPP_VGPIO_31, NONE),
+	PAD_NC(GPP_VGPIO_32, NONE),
+	PAD_NC(GPP_VGPIO_33, NONE),
+	PAD_NC(GPP_VGPIO_34, NONE),
+	PAD_NC(GPP_VGPIO_35, NONE),
+	PAD_NC(GPP_VGPIO_36, NONE),
+	PAD_NC(GPP_VGPIO_37, NONE),
+};
+
 static void fw_config_handle(void *unused)
 {
 	if (!fw_config_is_provisioned() || fw_config_probe(FW_CONFIG(AUDIO, AUDIO_UNKNOWN))) {
 		printk(BIOS_INFO, "Disable audio related GPIO pins.\n");
 		gpio_configure_pads(i2s_disable_pads, ARRAY_SIZE(i2s_disable_pads));
 		gpio_configure_pads(dmic_disable_pads, ARRAY_SIZE(dmic_disable_pads));
+		gpio_configure_pads(bt_i2s_disable_pads, ARRAY_SIZE(bt_i2s_disable_pads));
 		return;
 	}
 
@@ -54,6 +77,11 @@ static void fw_config_handle(void *unused)
 		printk(BIOS_INFO, "Configure audio over I2S with MAX98373 NAU88L25B.\n");
 		gpio_configure_pads(dmic_enable_pads, ARRAY_SIZE(dmic_enable_pads));
 		gpio_configure_pads(i2s_enable_pads, ARRAY_SIZE(i2s_enable_pads));
+		printk(BIOS_INFO, "BT offload enabled\n");
+		gpio_configure_pads(bt_i2s_enable_pads, ARRAY_SIZE(bt_i2s_enable_pads));
+	} else {
+		printk(BIOS_INFO, "BT offload disabled\n");
+		gpio_configure_pads(bt_i2s_disable_pads, ARRAY_SIZE(bt_i2s_disable_pads));
 	}
 }
 BOOT_STATE_INIT_ENTRY(BS_DEV_ENABLE, BS_ON_ENTRY, fw_config_handle, NULL);

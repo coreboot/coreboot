0c39cd7dab5e7444c61d078968722124e1bfba48
Seunghwan Kim <sh_.kim@samsung.corp-partner.google.com>
2021-10-19 15:26:54 +0900

    mb/google/dedede/var/bugzzy: Enable/disable LTE function based on FW_CONFIG
    Enable/disable LTE function based on DB_PORTS field of FW_CONFIG.
    - GPIO control
    - USB port setting
    
    BUG=None
    BRANCH=dedede
    TEST=FW_NAME=bugzzy emerge-dedede coreboot
    
    Signed-off-by: Seunghwan Kim <sh_.kim@samsung.corp-partner.google.com>
    Change-Id: I8363f8e7052ff9cfa423063a7e8f5a0f9ce1df2c
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/58442
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Henry Sun <henrysun@google.com>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

--- src/mainboard/google/dedede/variants/bugzzy/gpio.c
+++ src/mainboard/google/dedede/variants/bugzzy/gpio.c
@@ -2,6 +2,8 @@
 
 #include <baseboard/gpio.h>
 #include <baseboard/variants.h>
+#include <bootstate.h>
+#include <fw_config.h>
 
 /* Pad configuration in ramstage*/
 static const struct pad_config gpio_table[] = {
@@ -53,8 +55,22 @@ static const struct pad_config gpio_table[] = {
 	PAD_CFG_GPO(GPP_H17, 0, PLTRST),
 };
 
+static const struct pad_config lte_disable_pads[] = {
+	PAD_NC(GPP_A10, NONE),
+	PAD_NC(GPP_D0, NONE),
+	PAD_NC(GPP_H17, NONE),
+};
+
 const struct pad_config *variant_override_gpio_table(size_t *num)
 {
 	*num = ARRAY_SIZE(gpio_table);
 	return gpio_table;
 }
+
+static void fw_config_handle(void *unused)
+{
+	if (!fw_config_probe(FW_CONFIG(DB_PORTS, DB_PORTS_1C_1A_LTE)))
+		gpio_configure_pads(lte_disable_pads, ARRAY_SIZE(lte_disable_pads));
+}
+
+BOOT_STATE_INIT_ENTRY(BS_DEV_ENABLE, BS_ON_ENTRY, fw_config_handle, NULL);

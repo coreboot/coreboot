371cc15a89170bcb8ab2bcdbc03ccb7909641537
Felix Held <felix-coreboot@felixheld.de>
2021-11-02 19:22:27 +0100

    soc/amd/cezanne/include/cppc: use AMD_CEZANNE_CPPC_H as include guard
    This makes this header file consistent with the rest.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: Ice2872b4a24032d3a65777795943602cd2595de7
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/58864
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/soc/amd/cezanne/include/soc/cppc.h
+++ src/soc/amd/cezanne/include/soc/cppc.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#ifndef _CPU_AMD_COMMON_H
-#define _CPU_AMD_COMMON_H
+#ifndef AMD_CEZANNE_CPPC_H
+#define AMD_CEZANNE_CPPC_H
 
 #include <types.h>
 #include <acpi/acpigen.h>
@@ -10,4 +10,4 @@ struct cppc_config;
 void cpu_init_cppc_config(struct cppc_config *config, u32 version);
 void generate_cppc_entries(unsigned int core_id);
 
-#endif
+#endif /* AMD_CEZANNE_CPPC_H */

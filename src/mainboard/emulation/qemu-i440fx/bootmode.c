085fdd85592715c77cc35585f9155c25d2344e1a
Kyösti Mälkki <kyosti.malkki@gmail.com>
2021-11-03 06:02:57 +0200

    emulation/qemu-i440fx,q35: Split chromeos.c
    This drops VBOOT_NO_BOARD_SUPPORT.
    
    There is little impact of always having recovery_mode_switch()
    implemented in bootmode.c. A weak write_protect_state() is not
    necessary as there is no BOOT_DEVICE_SPI_FLASH with the emulation.
    Call to fill_lb_gpios() is already guarded with CONFIG(CHROMEOS)
    so the weak implementation would not be referenced.
    
    Change-Id: I3c00b30c5233ae3556b7622f97c3166668c8ab12
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/58946
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

---
+++ src/mainboard/emulation/qemu-i440fx/bootmode.c
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <bootmode.h>
+#include <console/console.h>
+#include "fw_cfg.h"
+
+/*
+ * Enable recovery mode with fw_cfg option to qemu:
+ *   -fw_cfg name=opt/cros/recovery,string=1
+ */
+int get_recovery_mode_switch(void)
+{
+	FWCfgFile f;
+
+	if (!fw_cfg_check_file(&f, "opt/cros/recovery")) {
+		uint8_t rec_mode;
+		if (f.size != 1) {
+			printk(BIOS_ERR, "opt/cros/recovery invalid size %d\n", f.size);
+			return 0;
+		}
+		fw_cfg_get(f.select, &rec_mode, f.size);
+		if (rec_mode == '1') {
+			printk(BIOS_INFO, "Recovery is enabled.\n");
+			return 1;
+		}
+	}
+
+	return 0;
+}
--- src/mainboard/emulation/qemu-q35/chromeos.c
+++ src/mainboard/emulation/qemu-q35/chromeos.c
@@ -2,9 +2,7 @@
 
 #include <bootmode.h>
 #include <boot/coreboot_tables.h>
-#include <console/console.h>
 #include <vendorcode/google/chromeos/chromeos.h>
-#include "../qemu-i440fx/fw_cfg.h"
 
 void fill_lb_gpios(struct lb_gpios *gpios)
 {
@@ -15,35 +13,6 @@ void fill_lb_gpios(struct lb_gpios *gpios)
 		{-1, ACTIVE_HIGH, 0, "EC in RW"},
 	};
 	lb_add_gpios(gpios, chromeos_gpios, ARRAY_SIZE(chromeos_gpios));
-}
-
-int get_write_protect_state(void)
-{
-	return 0;
-}
-
-/*
- * Enable recovery mode with fw_cfg option to qemu:
- *   -fw_cfg name=opt/cros/recovery,string=1
- */
-int get_recovery_mode_switch(void)
-{
-	FWCfgFile f;
-
-	if (!fw_cfg_check_file(&f, "opt/cros/recovery")) {
-		uint8_t rec_mode;
-		if (f.size != 1) {
-			printk(BIOS_ERR, "opt/cros/recovery invalid size %d\n", f.size);
-			return 0;
-		}
-		fw_cfg_get(f.select, &rec_mode, f.size);
-		if (rec_mode == '1') {
-			printk(BIOS_INFO, "Recovery is enabled.\n");
-			return 1;
-		}
-	}
-
-	return 0;
 }
 
 static const struct cros_gpio cros_gpios[] = {

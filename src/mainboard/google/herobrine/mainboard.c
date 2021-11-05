eaac053d2391d65b8e23f624c4c08a83fdbaae0f
Ravi Kumar Bokka <rbokka@codeaurora.org>
2021-11-05 17:27:46 +0530

    mb/google/herobrine: Add senor support QUP FW for I2C and SPI
    Add senor board to QUP FW load for: APPS I2C, ESIM SPI & fingerprint SPI.
    
    BUG=b:182963902
    TEST=Validated on qualcomm sc7280 development board.
    
    Signed-off-by: Ravi Kumar Bokka <rbokka@codeaurora.org>
    Change-Id: I6fdd09bb437547e6d12eb60c4b2917d2a3074618
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/59556
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Shelley Chen <shchen@google.com>

--- src/mainboard/google/herobrine/mainboard.c
+++ src/mainboard/google/herobrine/mainboard.c
@@ -77,7 +77,8 @@ static void mainboard_init(struct device *dev)
 		qupv3_se_fw_load_and_init(QUPV3_0_SE2, SE_PROTOCOL_I2C, MIXED);
 		/* Fingerprint SPI */
 		qupv3_se_fw_load_and_init(QUPV3_1_SE3, SE_PROTOCOL_SPI, MIXED);
-	} else if (CONFIG(BOARD_GOOGLE_PIGLIN) || CONFIG(BOARD_GOOGLE_HOGLIN)) {
+	} else if (CONFIG(BOARD_GOOGLE_SENOR) || CONFIG(BOARD_GOOGLE_PIGLIN) ||
+			CONFIG(BOARD_GOOGLE_HOGLIN)) {
 		/* APPS I2C */
 		qupv3_se_fw_load_and_init(QUPV3_0_SE1, SE_PROTOCOL_I2C, GSI);
 		/* ESIM SPI */

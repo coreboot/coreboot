c75d846971c2fd96c989a7f8a14011bb70866409
Ian Feng <ian_feng@compal.corp-partner.google.com>
2021-11-29 14:27:11 +0800

    mb/google/brya/var/felwinter: Add WiFi SAR table for felwinter
    Add WiFi SAR table for felwinter.
    
    BUG=b:206901900
    TEST=emerge-brya chromeos-config chromeos-config-bsp-private
    coreboot-private-files-baseboard-brya coreboot chromeos-bootimage
    and checked SAR table can load by WiFi driver.
    
    Signed-off-by: Ian Feng <ian_feng@compal.corp-partner.google.com>
    Change-Id: I0de710f4447302ee545a67cbd79373bdd2077637
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/59718
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: EricR Lai <ericr_lai@compal.corp-partner.google.com>

--- src/mainboard/google/brya/variants/felwinter/variant.c
+++ src/mainboard/google/brya/variants/felwinter/variant.c
@@ -3,6 +3,12 @@
 #include <chip.h>
 #include <fw_config.h>
 #include <baseboard/variants.h>
+#include <sar.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return "wifi_sar_0.hex";
+}
 
 void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
 {

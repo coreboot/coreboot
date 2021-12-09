ee56998a373e452bc1898a7a7fe6da61646f21fd
Rex-BC Chen <rex-bc.chen@mediatek.corp-partner.google.com>
2021-12-09 11:35:02 +0800

    mb/google/corsola: set up open-drain ChromeOS pins
    Set open-drain GPIOs for ChromeOS as input and high-z mode.
    After applying this patch, we can measure these pins from 1.0V to
    correct voltage (1.8V) to prevent wrong judgement of low/high.
    
    Reference document:
    MT8186_SoC_Pinmux_V1_1
    
    BUG=b:209342636
    TEST=measure pins voltage 1.8V on kingler board
    
    Signed-off-by: Rex-BC Chen <rex-bc.chen@mediatek.com>
    Change-Id: Ib55a773bb63404a1b952f7e7645eb7aba6638b00
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/60002
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/mainboard/google/corsola/chromeos.c
+++ src/mainboard/google/corsola/chromeos.c
@@ -9,20 +9,35 @@
 
 void setup_chromeos_gpios(void)
 {
-	gpio_input(GPIO_WP);
-	gpio_input_pullup(GPIO_EC_AP_INT);
-	gpio_input_pullup(GPIO_EC_IN_RW);
-	gpio_input_pullup(GPIO_GSC_AP_INT);
-	gpio_output(GPIO_EN_SPK, 0);
+	/* Set up open-drain pins */
+	gpio_input(GPIO_SAR_INT_ODL);
+	gpio_input(GPIO_BT_WAKE_AP_ODL);
+	gpio_input(GPIO_WIFI_INT_ODL);
+	gpio_input(GPIO_DPBRDG_INT_ODL);
+	gpio_input(GPIO_EDPBRDG_INT_ODL);
+	gpio_input(GPIO_EC_AP_HPD_OD);
+	gpio_input(GPIO_TCHPAD_INT_ODL);
+	gpio_input(GPIO_TCHSCR_INT_1V8_ODL);
+	gpio_input(GPIO_EC_AP_INT_ODL);
+	gpio_input(GPIO_EC_IN_RW_ODL);
+	gpio_input(GPIO_GSC_AP_INT_ODL);
+	gpio_input(GPIO_AP_WP_ODL);
+	gpio_input(GPIO_HP_INT_ODL);
+	gpio_input(GPIO_PEN_EJECT_OD);
+	gpio_input(GPIO_UCAM_DET_ODL);
+
+	/* Set up GPIOs */
 	gpio_output(GPIO_RESET, 0);
+	gpio_output(GPIO_XHCI_DONE, 0);
+	gpio_output(GPIO_EN_SPK, 0);
 }
 
 void fill_lb_gpios(struct lb_gpios *gpios)
 {
 	struct lb_gpio chromeos_gpios[] = {
-		{GPIO_EC_AP_INT.id, ACTIVE_LOW, -1, "EC interrupt"},
-		{GPIO_EC_IN_RW.id, ACTIVE_LOW, -1, "EC in RW"},
-		{GPIO_GSC_AP_INT.id, ACTIVE_HIGH, -1, "TPM interrupt"},
+		{GPIO_EC_AP_INT_ODL.id, ACTIVE_LOW, -1, "EC interrupt"},
+		{GPIO_EC_IN_RW_ODL.id, ACTIVE_LOW, -1, "EC in RW"},
+		{GPIO_GSC_AP_INT_ODL.id, ACTIVE_HIGH, -1, "TPM interrupt"},
 		{GPIO_EN_SPK.id, ACTIVE_HIGH, -1, "speaker enable"},
 	};
 	lb_add_gpios(gpios, chromeos_gpios, ARRAY_SIZE(chromeos_gpios));
@@ -31,10 +46,10 @@ void fill_lb_gpios(struct lb_gpios *gpios)
 int get_ec_is_trusted(void)
 {
 	/* EC is trusted if not in RW. This is active low. */
-	return !!gpio_get(GPIO_EC_IN_RW);
+	return !!gpio_get(GPIO_EC_IN_RW_ODL);
 }
 
 int tis_plat_irq_status(void)
 {
-	return gpio_eint_poll(GPIO_GSC_AP_INT);
+	return gpio_eint_poll(GPIO_GSC_AP_INT_ODL);
 }

138f547c8bff06989089a8317badf43f0733acd6
Shon Wang <shon.wang@quanta.corp-partner.google.com>
2021-12-14 20:41:48 +0800

    mb/google/brya/var/vell: Swap TPM I2C with touchscreen I2C
    According to the latest schematic for the next build phase, exchange I2C port for TPM/touchscreen.
    TPM: I2C3 -> I2C1
    Touchscreen: I2C1 -> I2C3
    
    BUG=b:210572663
    TEST=FW_NAME=vell emerge-brya coreboot
    
    Change-Id: If72717a2c073f5b871c3109399f466a04a9d2484
    Signed-off-by: Shon Wang <shon.wang@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/60106
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/mainboard/google/brya/variants/vell/gpio.c
+++ src/mainboard/google/brya/variants/vell/gpio.c
@@ -92,10 +92,10 @@ static const struct pad_config early_gpio_table[] = {
 	PAD_CFG_GPI_APIC(GPP_A13, NONE, PLTRST, LEVEL, INVERT),
 	/* B4  : PROC_GP3 ==> SSD_PERST_L */
 	PAD_CFG_GPO(GPP_B4, 0, DEEP),
-	/* B7  : ISH_12C1_SDA ==> PCH_I2C_TPM_SDA */
-	PAD_CFG_NF(GPP_B7, NONE, DEEP, NF2),
-	/* B8  : ISH_12C1_SCL ==> PCH_I2C_TPM_SCL */
-	PAD_CFG_NF(GPP_B8, NONE, DEEP, NF2),
+	/* H6  : I2C1_SDA ==> PCH_I2C_TPM_SDA */
+	PAD_CFG_NF(GPP_H6, NONE, DEEP, NF1),
+	/* H7  : IC1_SCL ==> PCH_I2C_TPM_SCL */
+	PAD_CFG_NF(GPP_H7, NONE, DEEP, NF1),
 	/*
 	 * D1  : ISH_GP1 ==> FP_RST_ODL
 	 * FP_RST_ODL comes out of reset as hi-z and does not have an external pull-down.

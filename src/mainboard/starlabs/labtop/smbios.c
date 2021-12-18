a2c10a25391a739186caa43799657a4bf8959c3b
Angel Pons <th3fanbus@gmail.com>
2021-12-18 13:26:45 +0100

    mb/starlabs/labtop: Rename some files
    Rename `mainboard.c` to `smbios.c` as it only contains SMBIOS functions.
    Rename `ramstage.c` to `mainboard.c` as it contains the mainboard chip
    operations struct.
    
    Change-Id: I9548ca72a7583de98f5b154e3381825bba00d5cf
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/60211
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Sean Rhodes <admin@starlabs.systems>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

---
+++ src/mainboard/starlabs/labtop/smbios.c
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <chip.h>
+#include <console/console.h>
+#include <device/device.h>
+#include <device/pci_def.h>
+#include <ec/starlabs/merlin/ec.h>
+#include <limits.h>
+#include <option.h>
+#include <smbios.h>
+#include <types.h>
+#include <uuid.h>
+#include <variants.h>
+
+const char *smbios_mainboard_bios_version(void)
+{
+	return "8";
+}
+
+/* Get the Embedded Controller firmware version */
+void smbios_ec_revision(uint8_t *ec_major_revision, uint8_t *ec_minor_revision)
+{
+	u16 ec_version = it_get_version();
+
+	*ec_major_revision = ec_version >> 8;
+	*ec_minor_revision = ec_version & 0xff;
+}
+
+const char *smbios_system_sku(void)
+{
+	return CONFIG_MAINBOARD_FAMILY;
+}
+
+u8 smbios_mainboard_feature_flags(void)
+{
+	return SMBIOS_FEATURE_FLAGS_HOSTING_BOARD | SMBIOS_FEATURE_FLAGS_REPLACEABLE;
+}
+
+const char *smbios_chassis_version(void)
+{
+	return smbios_mainboard_version();
+}
+
+const char *smbios_chassis_serial_number(void)
+{
+	return smbios_mainboard_serial_number();
+}

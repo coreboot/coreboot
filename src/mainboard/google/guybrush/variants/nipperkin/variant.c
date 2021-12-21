c067e4a6e5de73e410f3f496d9b93fc6f8a4d4ad
Kevin Chiu <kevin.chiu.17802@gmail.com>
2021-12-21 09:07:29 +0800

    mb/google/guybrush/var/nipperkin: turn on WLAN ASPM L1ss
    BUG=b:198258604
    BRANCH=guybrush
    TEST=emerge-guybrush coreboot
         WLAN works properly in OS
    
    Change-Id: Ie1f295eaa57af7c2942e1807b3a0c4dcd89cd696
    Signed-off-by: Kevin Chiu <kevin.chiu.17802@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/60265
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/mainboard/google/guybrush/variants/nipperkin/variant.c
+++ src/mainboard/google/guybrush/variants/nipperkin/variant.c
@@ -7,6 +7,13 @@
 
 void variant_update_dxio_descriptors(fsp_dxio_descriptor *dxio_descriptors)
 {
-	dxio_descriptors[WLAN].link_aspm_L1_1 = false;
-	dxio_descriptors[WLAN].link_aspm_L1_2 = false;
+	uint32_t board_version = board_id();
+
+	if (board_version >= 3) {
+		dxio_descriptors[WLAN].link_aspm_L1_1 = true;
+		dxio_descriptors[WLAN].link_aspm_L1_2 = true;
+	} else {
+		dxio_descriptors[WLAN].link_aspm_L1_1 = false;
+		dxio_descriptors[WLAN].link_aspm_L1_2 = false;
+	}
 }

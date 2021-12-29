60570f41344b10c06d42ec39c2aa5a119889ed14
Yu-Ping Wu <yupingso@chromium.org>
2021-12-29 15:44:33 +0800

    soc/mediatek: Fix include guard naming for emi.h
    Fix the name of the include guard for
    soc/mediatek/common/include/soc/emi.h.
    
    BUG=none
    TEST=emerge-corsola coreboot
    BRANCH=none
    
    Change-Id: Iddac3467959545b7db141545aaa2a135536f44f1
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/60440
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Xi Chen <xixi.chen@mediatek.com>
    Reviewed-by: Rex-BC Chen <rex-bc.chen@mediatek.com>

--- src/soc/mediatek/common/include/soc/emi.h
+++ src/soc/mediatek/common/include/soc/emi.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#ifndef SOC_MEDIATEK_EMI_H
-#define SOC_MEDIATEK_EMI_H
+#ifndef SOC_MEDIATEK_COMMON_EMI_H
+#define SOC_MEDIATEK_COMMON_EMI_H
 
 #include <soc/dramc_param.h>
 
@@ -16,4 +16,4 @@ u32 get_ddr_geometry(void);
 u32 get_ddr_type(void);
 void init_dram_by_params(struct dramc_param *dparam);
 
-#endif /* SOC_MEDIATEK_MT8192_EMI_H */
+#endif /* SOC_MEDIATEK_COMMON_EMI_H */

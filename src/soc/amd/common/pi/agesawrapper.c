b0cda4b1697d862673e4025dc66eddca819635d9
Elyes HAOUAS <ehaouas@noos.fr>
2022-01-01 19:06:22 +0100

    soc/amd/common/pi/agesawrapper.c: Include <string.h>
    "memcpy" needs <string.h>
    
    Change-Id: I5b7b3a94acbb7e4f9614fcf3f06d68e6ac72f4f1
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/60641
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/soc/amd/common/pi/agesawrapper.c
+++ src/soc/amd/common/pi/agesawrapper.c
@@ -11,6 +11,7 @@
 #include <soc/pci_devs.h>
 #include <soc/northbridge.h>
 #include <soc/cpu.h>
+#include <string.h>
 
 void __weak SetMemParams(AMD_POST_PARAMS *PostParams) {}
 void __weak OemPostParams(AMD_POST_PARAMS *PostParams) {}

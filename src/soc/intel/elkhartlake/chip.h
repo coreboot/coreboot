6fb126773f538ea4467b1abfde6cb8c6fc3cc9bb
Subrata Banik <subratabanik@google.com>
2022-01-03 18:49:35 +0000

    soc/intel/ehl: Replace dt `HeciEnabled` by `HECI1 disable` config
    The only option to make HECI1 function disable on Elkhart Lake SoC
    platform is using SBI under SMM mode.
    
    List of changes:
    1. Drop `HeciEnabled` from dt and dt chip configuration.
    2. Replace all logic that disables HECI1 based on the `HeciEnabled`
    chip config with `DISABLE_HECI1_AT_PRE_BOOT` config.
    
    Mainboards that choose to make HECI1 enable during boot don't override
    `heci1 disable` config.
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: I76c625e6221fdef1343599e7dbc7739caa91bf98
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/60727
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Sheng Lean Tan <sheng.tan@9elements.com>

--- src/soc/intel/elkhartlake/chip.h
+++ src/soc/intel/elkhartlake/chip.h
@@ -226,10 +226,6 @@ struct soc_intel_elkhartlake_config {
 
 	uint8_t Device4Enable;
 
-	/* HeciEnabled decides the state of Heci1 at end of boot
-	 * Setting to 0 (default) disables Heci1 and hides the device from OS */
-	uint8_t HeciEnabled;
-
 	/* Enable/Disable EIST. 1b:Enabled, 0b:Disabled */
 	uint8_t eist_enable;
 

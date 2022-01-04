8a4a89fdcb58655de09e4a2688c8f4d912bacc6c
Anil Kumar <anil.kumar.k@intel.com>
2022-01-04 15:03:03 -0800

    efi_datatype: Add typedef for EFI_PHYSICAL_ADDRESS
    Signed-off-by: Anil Kumar <anil.kumar.k@intel.com>
    Change-Id: Ie09e337ee1790a06689681fca087edcfd89d215f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/60778
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

--- src/include/efi/efi_datatype.h
+++ src/include/efi/efi_datatype.h
@@ -5,6 +5,7 @@
 #define __EFI_DATATYPE_H__
 #include <Base.h>
 #include <PiPei.h>
+#include <Uefi/UefiBaseType.h>
 
 /* Basic Data types */
 /* 8-byte unsigned value. */
@@ -38,6 +39,8 @@ typedef EFI_STATUS efi_return_status_t;
 /* Data structure */
 /* Data structure for EFI_PEI_SERVICE. */
 typedef EFI_PEI_SERVICES efi_pei_services;
+/* Data structure for EFI_PHYSICAL_ADDRESS */
+typedef EFI_PHYSICAL_ADDRESS efi_physical_address;
 
 /*  Structure that describes information about a logical CPU. */
 typedef EFI_PROCESSOR_INFORMATION efi_processor_information;

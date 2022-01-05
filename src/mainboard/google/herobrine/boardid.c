coreboot/coreboot

commit 8b63dac06184e56ce40d82d982e983ac79163551

google/herobrine: configure gpio to detect board ID
BUG=b:182963902, b:193807794
TEST=Validated on qualcomm sc7280 development board

Signed-off-by: Ravi Kumar Bokka <rbokka@codeaurora.org>
Change-Id: I6de2a7e7b11ecce8325e0fd44dc7221d73729390
Reviewed-on: https://review.coreboot.org/c/coreboot/+/56642
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Shelley Chen <shchen@google.com>

src/mainboard/google/herobrine/boardid.c
@@ -10,19 +10,25 @@
 uint32_t board_id(void)
 {
 	static uint32_t id = UNDEFINED_STRAPPING_ID;
-	const gpio_t pins[] = {[2] = GPIO(50), [1] = GPIO(49), [0] = GPIO(48)};
+	gpio_t pins[3];
+	if (CONFIG(BOARD_GOOGLE_HEROBRINE)) {
+		pins[2] = GPIO(75);
+		pins[1] = GPIO(74);
+		pins[0] = GPIO(73);
+	} else if (CONFIG(BOARD_GOOGLE_PIGLIN) || CONFIG(BOARD_GOOGLE_HOGLIN)
+			|| CONFIG(BOARD_GOOGLE_SENOR)) {
+		pins[2] = GPIO(50);
+		pins[1] = GPIO(49);
+		pins[0] = GPIO(48);
+	}
 
 	if (id == UNDEFINED_STRAPPING_ID)
 		id = gpio_base3_value(pins, ARRAY_SIZE(pins));
 
-	if ((id == QCOM_SC7280_SKU1) || (id == QCOM_SC7280_SKU2) ||
-						(id == QCOM_SC7280_SKU3))
-		printk(BIOS_INFO, "BoardID :%d - "
+	printk(BIOS_INFO, "BoardID :%d - "
 				"Machine model: "
 				"Qualcomm Technologies, Inc. "
-				"sc7280 IDP SKU%d platform\n", id, (id+1));
-	else
-		printk(BIOS_ERR, "Invalid BoardId : %d\n", id);
+				"sc7280 platform\n", id);
 
 	return id;
 }

src/mainboard/google/herobrine/board.h
@@ -18,7 +18,6 @@
 
 #define QCOM_SC7280_SKU1 0x0
 #define QCOM_SC7280_SKU2 0x1
-#define QCOM_SC7280_SKU3 0x2
 
 /* Fingerprint-specific GPIOs. Only for fingerprint-enabled devices. */
 #if CONFIG(HEROBRINE_HAS_FINGERPRINT)

commit eb14a979f9447da09c5203d83376157e418a8bec

mb/google/herobrine: Initialize `pins` to fix the compilation issue
Fix compilation issue introduced with commit 8b63dac0
(google/herobrine: configure gpio to detect board ID) by initialising
the gpio pins.

Signed-off-by: Subrata Banik <subratabanik@google.com>
Change-Id: I084fec777b56f402efb3b04a1d358cd5b0891846
Reviewed-on: https://review.coreboot.org/c/coreboot/+/60784
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: EricR Lai <ericr_lai@compal.corp-partner.google.com>
Reviewed-by: Patrick Georgi <patrick@coreboot.org>

src/mainboard/google/herobrine/boardid.c
@@ -10,7 +10,7 @@
 uint32_t board_id(void)
 {
 	static uint32_t id = UNDEFINED_STRAPPING_ID;
-	gpio_t pins[3];
+	gpio_t pins[3] = { 0 };
 	if (CONFIG(BOARD_GOOGLE_HEROBRINE)) {
 		pins[2] = GPIO(75);
 		pins[1] = GPIO(74);
03824a6b2e5ee4f5e1bf29e230c05f1500059049
Elyes HAOUAS <ehaouas@noos.fr>
2022-01-07 22:21:12 +0100

    src/mainboard/{elmex,gizmosphere}: Remove unused <console/console.h>
    Found using:
    diff <(git grep -l '#include <console/console.h>' -- src/) <(git grep -l 'console_time_report\|console_time_get_and_reset\|do_putchar\|vprintk\|printk\|console_log_level\|console_init\|get_log_level\|CONSOLE_ENABLE\|get_console_loglevel\|die_notify\|die_with_post_code\|die\|arch_post_code\|mainboard_post\|post_code\|RAM_SPEW\|RAM_DEBUG\|BIOS_EMERG\|BIOS_ALERT\|BIOS_CRIT\|BIOS_ERR\|BIOS_WARNING\|BIOS_NOTICE\|BIOS_INFO\|BIOS_DEBUG\|BIOS_SPEW\|BIOS_NEVER' -- src/) |grep "<"
    
    Change-Id: I656ca220180261b7af10297ab2c3d2d8693666c7
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/60921
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/mainboard/elmex/pcm205400/mainboard.c
+++ src/mainboard/elmex/pcm205400/mainboard.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <amdblocks/acpimmio.h>
-#include <console/console.h>
 #include <device/device.h>
 #include <device/mmio.h>
 #include <southbridge/amd/common/amd_pci_util.h>

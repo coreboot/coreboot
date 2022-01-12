f8a4580c0297419112110514d775dbdcb9e60a48
Elyes HAOUAS <ehaouas@noos.fr>
2022-01-12 21:13:23 +0100

    device: Remove unused <cbmem.h>
    Change-Id: I81bf00af1b56a0181c376db92f5f85297b6993ed
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/61050
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/device/dram/lpddr4.c
+++ src/device/dram/lpddr4.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <console/console.h>
-#include <cbmem.h>
 #include <device/device.h>
 #include <device/dram/lpddr4.h>
 #include <memory_info.h>

039be8c7c7daf3f26b70b52c4c6355851d3e01ef
Felix Held <felix-coreboot@felixheld.de>
2022-01-12 23:26:42 +0100

    soc/amd/sabrina/include/data_fabric: update IOMS0_FABRIC_ID
    The data fabric ID table in PPR #57243 Rev 1.50 has a different IOMS0
    fabric ID than Cezanne.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I32890b5c03219f6ebf8180929d71ef726d382483
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/61089
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Reviewed-by: Fred Reitberger <reitbergerfred@gmail.com>

--- src/soc/amd/sabrina/include/soc/data_fabric.h
+++ src/soc/amd/sabrina/include/soc/data_fabric.h
@@ -1,7 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-/* TODO: Check if this is still correct */
-
 #ifndef AMD_SABRINA_DATA_FABRIC_H
 #define AMD_SABRINA_DATA_FABRIC_H
 
@@ -10,7 +8,7 @@
 /* SoC-specific bits in D18F0_MMIO_CTRL0 */
 #define   DF_MMIO_NP			BIT(16)
 
-#define IOMS0_FABRIC_ID			10
+#define IOMS0_FABRIC_ID			9
 
 #define NUM_NB_MMIO_REGS		8
 

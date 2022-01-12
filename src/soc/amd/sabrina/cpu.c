283999ad539f67fe9bb92907fe82ccb591f19b43
Felix Held <felix-coreboot@felixheld.de>
2022-01-12 23:22:19 +0100

    soc/amd/sabrina/cpu: update CPUID
    Sabrina is family 17h model A0h.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I01e02e3491fb90941c767058986da876bdf7ca1d
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/61087
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Fred Reitberger <reitbergerfred@gmail.com>
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>

--- src/soc/amd/sabrina/cpu.c
+++ src/soc/amd/sabrina/cpu.c
@@ -75,7 +75,7 @@ static struct device_operations cpu_dev_ops = {
 };
 
 static struct cpu_device_id cpu_table[] = {
-	{ X86_VENDOR_AMD, CEZANNE_A0_CPUID},
+	{ X86_VENDOR_AMD, SABRINA_A0_CPUID},
 	{ 0, 0 },
 };
 

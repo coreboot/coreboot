51ede8af2e95734c7f7e820f7a29409fbb3c0605
Joey Peng <joey.peng@lcfc.corp-partner.google.com>
2022-01-18 15:22:05 +0800

    mb/google/brya/var/taniks: swap TPM i2c with TS i2c for next build
    Taniks is going to exchange i2c port for touchscreen and cr50.
    
    BUG=b:215039999
    TEST=emerge-brya coreboot
    
    Cq-Depend:chromium:3397562
    Signed-off-by: Joey Peng <joey.peng@lcfc.corp-partner.google.com>
    Change-Id: I179949887f6d8f4bbdff7d806319e2ac368ebc2c
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/61169
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/mainboard/google/brya/variants/taniks/gpio.c
+++ src/mainboard/google/brya/variants/taniks/gpio.c
@@ -158,10 +158,6 @@ static const struct pad_config override_gpio_table[] = {
 static const struct pad_config early_gpio_table[] = {
 	/* A13 : PMC_I2C_SCL ==> GSC_PCH_INT_ODL */
 	PAD_CFG_GPI_APIC(GPP_A13, NONE, PLTRST, LEVEL, INVERT),
-	/* B7  : ISH_12C1_SDA ==> PCH_I2C_TPM_SDA */
-	PAD_CFG_NF(GPP_B7, NONE, DEEP, NF2),
-	/* B8  : ISH_12C1_SCL ==> PCH_I2C_TPM_SCL */
-	PAD_CFG_NF(GPP_B8, NONE, DEEP, NF2),
 	/* D11 : ISH_SPI_MISO ==> EN_PP3300_SSD */
 	PAD_CFG_GPO(GPP_D11, 1, DEEP),
 	/* E13 : THC0_SPI1_IO2 ==> MEM_CH_SEL */
@@ -170,6 +166,10 @@ static const struct pad_config early_gpio_table[] = {
 	PAD_CFG_GPI_GPIO_DRIVER(GPP_E15, NONE, DEEP),
 	/* F18 : THC1_SPI2_INT# ==> EC_IN_RW_OD */
 	PAD_CFG_GPI(GPP_F18, NONE, DEEP),
+	/* H6  : I2C1_SDA ==> PCH_I2C_TPM_SDA */
+	PAD_CFG_NF(GPP_H6, NONE, DEEP, NF1),
+	/* H7  : I2C1_SCL ==> PCH_I2C_TPM_SCL */
+	PAD_CFG_NF(GPP_H7, NONE, DEEP, NF1),
 	/* H10 : UART0_RXD ==> UART_PCH_RX_DBG_TX */
 	PAD_CFG_NF(GPP_H10, NONE, DEEP, NF2),
 	/* H11 : UART0_TXD ==> UART_PCH_TX_DBG_RX */

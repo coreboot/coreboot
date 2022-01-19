0ddb47048d064087bf8c3e206bead1fa55f4e918
Felix Held <felix-coreboot@felixheld.de>
2022-01-19 22:29:29 +0100

    soc/amd/picasso/bootblock: drop unused includes
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I837e1f8727adefb9227ac7df2ff715245957be2c
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/61258
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: HAOUAS Elyes <ehaouas@noos.fr>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/picasso/bootblock.c
+++ src/soc/amd/picasso/bootblock.c
@@ -1,16 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <stdint.h>
-#include <symbols.h>
 #include <amdblocks/cpu.h>
-#include <amdblocks/reset.h>
 #include <bootblock_common.h>
 #include <console/console.h>
 #include <cpu/x86/tsc.h>
-#include <pc80/mc146818rtc.h>
 #include <soc/psp_transfer.h>
 #include <soc/southbridge.h>
-#include <soc/i2c.h>
 #include <amdblocks/amd_pci_mmconf.h>
 
 asmlinkage void bootblock_c_entry(uint64_t base_timestamp)

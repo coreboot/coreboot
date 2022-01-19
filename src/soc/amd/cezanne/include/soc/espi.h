cf4bba82cbb62237609b03ba2933c81558df4785
Felix Held <felix-coreboot@felixheld.de>
2022-01-19 17:19:56 +0100

    soc/amd/cezanne/include/espi.h: add missing include guards
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I393feab8550a7124ab2982ff3d256e3491d27b4b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/61213
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/soc/amd/cezanne/include/soc/espi.h
+++ src/soc/amd/cezanne/include/soc/espi.h
@@ -1,4 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#ifndef AMD_CEZANNE_ESPI_H
+#define AMD_CEZANNE_ESPI_H
+
 void espi_disable_lpc_ldrq(void);
 void espi_switch_to_spi2_pads(void);
+
+#endif /* AMD_CEZANNE_ESPI_H */

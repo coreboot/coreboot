8fe9e541adfcf5f9ae3539e3fe19603605e36476
Elyes HAOUAS <ehaouas@noos.fr>
2022-01-24 07:22:03 +0100

    soc/ti/am335x/cbmem.c: Use MiB macro
    Use "* MiB" instead of "<< 20".
    
    Change-Id: Iab6592804961a34fae6dc8012bfbc70023421a49
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/61332
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/soc/ti/am335x/cbmem.c
+++ src/soc/ti/am335x/cbmem.c
@@ -1,9 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <cbmem.h>
+#include <commonlib/bsd/helpers.h>
 #include <symbols.h>
 
 void *cbmem_top_chipset(void)
 {
-	return _dram + (CONFIG_DRAM_SIZE_MB << 20);
+	return _dram + CONFIG_DRAM_SIZE_MB * MiB;
 }

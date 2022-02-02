a84d4f2312319ac7e1213d8e8d255d189e38316f
Karthikeyan Ramasubramanian <kramasub@google.com>
2022-02-02 10:10:03 -0700

    mb/google/skyrim: Add new mainboard
    Skyrim is a new Google mainboard with AMD Sabrina SOC.
    
    BUG=b:214413553
    TEST=util/abuild/abuild -t GOOGLE_SKYRIM --clean
    
    Change-Id: I008fea4aa163b8aa66e86735b29b3fdc4e08a327
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/61565
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Jon Murphy <jpmurphy@google.com>

---
+++ src/mainboard/google/skyrim/port_descriptors.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <soc/platform_descriptors.h>
+#include <types.h>
+
+void mainboard_get_dxio_ddi_descriptors(
+		const fsp_dxio_descriptor **dxio_descs, size_t *dxio_num,
+		const fsp_ddi_descriptor **ddi_descs, size_t *ddi_num)
+{
+	/* TODO: Initialize DXIO and DDI descriptors */
+}

d40e8b6cb542c652dc5e5a69c4b7e8075ec90d49
Felix Held <felix-coreboot@felixheld.de>
2022-02-07 17:25:44 +0100

    soc/amd/sabrina: change MAX_CPUS to 8
    The Sabrina APU has a maximum configuration of 4 physical cores with 2
    threads each, so a total of 8 CPU cores.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I627ed78ffba6098726c9c8ec55b60665503240ea
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65068
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/sabrina/cpu.c
+++ src/soc/amd/sabrina/cpu.c
@@ -19,7 +19,7 @@
 #include <soc/iomap.h>
 #include <types.h>
 
-_Static_assert(CONFIG_MAX_CPUS == 16, "Do not override MAX_CPUS. To reduce the number of "
+_Static_assert(CONFIG_MAX_CPUS == 8, "Do not override MAX_CPUS. To reduce the number of "
 	"available cores, use the downcore_mode and disable_smt devicetree settings instead.");
 
 /* MP and SMM loading initialization */

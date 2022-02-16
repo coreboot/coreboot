af025d6ee124a6bafb23302f616b4839de3e120e
Jon Murphy <jpmurphy@google.com>
2022-02-16 06:44:53 -0700

    mb/google/skyrim: CONFIG_CHROMEOS
    BUG=b:214415401
    TEST=builds
    BRANCH=none
    
    Signed-off-by: Jon Murphy <jpmurphy@google.com>
    Change-Id: I045f76c366a1a72814536a2be984b7ad5a438a5a
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/62043
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/skyrim/chromeos.c
+++ src/mainboard/google/skyrim/chromeos.c
@@ -1,7 +1,19 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <baseboard/gpio.h>
+#include <boot/coreboot_tables.h>
+#include <bootmode.h>
+#include <gpio.h>
 #include <vendorcode/google/chromeos/chromeos.h>
+
+void fill_lb_gpios(struct lb_gpios *gpios)
+{
+	struct lb_gpio chromeos_gpios[] = {
+		/* TODO Enable lid switch with EC */
+		{-1, ACTIVE_HIGH, 0, "power"},
+	};
+	lb_add_gpios(gpios, chromeos_gpios, ARRAY_SIZE(chromeos_gpios));
+}
 
 static const struct cros_gpio cros_gpios[] = {
 	CROS_GPIO_REC_AL(CROS_GPIO_VIRTUAL, GPIO_DEVICE_NAME),

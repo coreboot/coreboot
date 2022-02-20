66f99f7fa7e7644e2549619b375e758492b601e3
Michał Żygowski <michal.zygowski@3mdeb.com>
2022-02-20 23:30:43 +0100

    mb/dell/snb_ivb_workstations: Add Precision T1650 support
    Precision is a Mid Tower chassis platform with very similar mainboard
    to OptiPlex 9010. It has one more PCIe port and a PCI port. It also
    incorporates C216 chipset instead of Q77 and enables DRAM ECC support.
    Other changes are related to subsystem ID and fan control
    initialization.
    
    TEST=Boot Dell Precision T1650 and launch Debian 10.
    
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Change-Id: I4ec2013d5f53af36cab0d1def19272f5ef1a9516
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/62212
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martinroth@google.com>

---
+++ src/mainboard/dell/snb_ivb_workstations/variants/precision_t1650/hda_verb.c
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/azalia_device.h>
+
+const u32 cim_verb_data[] = {
+	0x10ec0269,	/* Codec Vendor / Device ID: Realtek */
+	0x1028053a,	/* Subsystem ID */
+
+	11,		/* Number of 4 dword sets */
+
+	AZALIA_SUBVENDOR(0, 0x1028053a),
+	AZALIA_PIN_CFG(0, 0x12, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x14, 0x99130110),
+	AZALIA_PIN_CFG(0, 0x17, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x18, 0x02a19830),
+	AZALIA_PIN_CFG(0, 0x19, 0x01a19840),
+	AZALIA_PIN_CFG(0, 0x1a, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1b, 0x01014020),
+	AZALIA_PIN_CFG(0, 0x1d, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1e, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x21, 0x0221402f),
+
+	0x80862806,	/* Codec Vendor / Device ID: Intel */
+	0x80860101,	/* Subsystem ID */
+
+	4,		/* Number of 4 dword sets */
+
+	AZALIA_SUBVENDOR(3, 0x80860101),
+	AZALIA_PIN_CFG(3, 0x05, 0x18560010),
+	AZALIA_PIN_CFG(3, 0x06, 0x18560020),
+	AZALIA_PIN_CFG(3, 0x07, 0x58560030),
+};
+
+const u32 pc_beep_verbs[0] = {};
+
+AZALIA_ARRAY_SIZES;

d402fdff5dd08d0db6136e004ef1ba99a3ea6713
Frank Chu <Frank_Chu@pegatron.corp-partner.google.com>
2022-03-09 16:52:04 +0800

    mb/google/dedede/var/galtic: update Wifi SAR for for galnat
    Add wifi sar for galnat/galnat360
    Use SKU ID to load wifi table.
    
    Each Project and SKU ID correspond as below
    galtic (sku id:0x120000)
    galith (sku id:0x130000)
    galnat (sku id:0x140000)*
    gallop (sku id:0x150000)
    galtic360 (sku id:0x260000)
    galith360 (sku id:0x270000)
    galnat360 (sku id:0x2B0000)*
    
    BUG=b:222008376
    TEST=emerge-dedede coreboot chromeos-bootimage \
         coreboot-private-files-baseboard-dedede
         verify the SAR table is correct in each project
    
    Signed-off-by: Frank Chu <Frank_Chu@pegatron.corp-partner.google.com>
    Change-Id: I868a7416a002732736cabea48ce80548ea75e517
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/62704
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Frank Chu <frank_chu@pegatron.corp-partner.google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

--- src/mainboard/google/dedede/variants/galtic/variant.c
+++ src/mainboard/google/dedede/variants/galtic/variant.c
@@ -9,12 +9,16 @@ enum {
 	GALTIC_SKU_END      = 0x12ffff,
 	GALITH_SKU_START    = 0x130000,
 	GALITH_SKU_END      = 0x13ffff,
+	GALNAT_SKU_START    = 0x140000,
+	GALNAT_SKU_END      = 0x14ffff,
 	GALLOP_SKU_START    = 0x150000,
 	GALLOP_SKU_END      = 0x15ffff,
 	GALTIC360_SKU_START = 0x260000,
 	GALTIC360_SKU_END   = 0x26ffff,
 	GALITH360_SKU_START = 0x270000,
 	GALITH360_SKU_END   = 0x27ffff,
+	GALNAT360_SKU_START = 0x2B0000,
+	GALNAT360_SKU_END   = 0x2Bffff,
 };
 const char *get_wifi_sar_cbfs_filename(void)
 {
@@ -22,10 +26,14 @@ const char *get_wifi_sar_cbfs_filename(void)
 
 	if (sku_id >= GALTIC_SKU_START && sku_id <= GALTIC_SKU_END)
 		return "wifi_sar-galtic.hex";
+	if (sku_id >= GALNAT_SKU_START && sku_id <= GALNAT_SKU_END)
+		return "wifi_sar-galnat.hex";
 	if (sku_id >= GALTIC360_SKU_START && sku_id <= GALTIC360_SKU_END)
 		return "wifi_sar-galtic360.hex";
 	if (sku_id >= GALITH360_SKU_START && sku_id <= GALITH360_SKU_END)
 		return "wifi_sar-galith360.hex";
+	if (sku_id >= GALNAT360_SKU_START && sku_id <= GALNAT360_SKU_END)
+		return "wifi_sar-galnat360.hex";
 
 	return WIFI_SAR_CBFS_DEFAULT_FILENAME;
 }

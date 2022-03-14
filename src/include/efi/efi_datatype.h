242e2665d005576a081908873fa7652895e8e60d
Subrata Banik <subratabanik@google.com>
2022-03-14 16:33:34 +0530

    include/efi: Clean up efi_datatype.h file
    This patch cleans up `efi_datatype.h` to allow other SoC and/or driver
    code to use this file.
    
    `PiPei.h` file only gets added from UDK2017 hence, the platform with the
    older EDK2 base (prior to UDK2017) is unable to resolve this file
    dependency.
    
    This CL removed the `PiPei.h` header and only added the required header
    file `PiPeiCis.h` for platforms with UDK base >= 2017.
    
    BUG=b:200113959
    TEST=Able to build Brya.
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: Ie9177814fcf41e5950ace94050356f0273f765c5
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/62787
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Anil Kumar K <anil.kumar.k@intel.com>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/include/efi/efi_datatype.h
+++ src/include/efi/efi_datatype.h
@@ -4,8 +4,16 @@
 #ifndef __EFI_DATATYPE_H__
 #define __EFI_DATATYPE_H__
 #include <Base.h>
-#include <PiPei.h>
 #include <Uefi/UefiBaseType.h>
+
+#if CONFIG_UDK_VERSION >= CONFIG_UDK_2017_VERSION
+#include <Pi/PiPeiCis.h>
+
+/* Data structure for EFI_PEI_SERVICE. */
+typedef EFI_PEI_SERVICES efi_pei_services;
+/*  Structure that describes information about a logical CPU. */
+typedef EFI_PROCESSOR_INFORMATION efi_processor_information;
+#endif
 
 /* Basic Data types */
 /* 8-byte unsigned value. */
@@ -36,14 +44,9 @@ typedef UINTN efi_uintn_t;
 typedef INTN efi_intn_t;
 /* Status codes common to all execution phases */
 typedef EFI_STATUS efi_return_status_t;
-/* Data structure */
-/* Data structure for EFI_PEI_SERVICE. */
-typedef EFI_PEI_SERVICES efi_pei_services;
 /* Data structure for EFI_PHYSICAL_ADDRESS */
 typedef EFI_PHYSICAL_ADDRESS efi_physical_address;
 
-/*  Structure that describes information about a logical CPU. */
-typedef EFI_PROCESSOR_INFORMATION efi_processor_information;
 /*
  * The function prototype for invoking a function on an
  * Application Processor.

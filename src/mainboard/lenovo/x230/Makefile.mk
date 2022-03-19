a88dd4b6fbac8748a6f4580ad6363da47f5b3bc2
Alexander Couzens <lynxis@fe80.eu>
2022-03-19 13:42:33 +0000

    mb/lenovo/x230: introduce EDP variant
    There is a modification for the x230 which uses the 2nd DP from the
    dock as the integrated panel's connection, which allows using a custom
    eDP panel instead of the stock LVDS display.
    
    There are several adapter boards present on the market and all of them
    use the same method of enabling the custom eDP panel.
    
    To make this work with coreboot, the internal LVDS connector should be
    disabled in libgfxinit. Additionally, VBT has been modified to keep
    brightness controls functional on the adapter boards that use LVDS for
    the job.
    
    The modifications done to the VBT are:
    - Remove the LVDS port entry.
    - Move the DP-3 (which is the 2nd DP on the dock) entry to the first
      position on the list.
    - Set the DP-3 as internally connected.
    
    This has been reported to work with the following panels:
    - LP125WF2-SPB4 (1920*1080, 12.5")
    - LQ125T1JW02 (2560*1440, 12.5")
    - LQ133M1JW21 (1920*1080, 13.3")
    - LTN133HL10-201 (1920*1080, 13.3")
    - B133HAN04.6 (1920*1080, 13.3")
    - B133QAN02.0 (2560*1600, 13.3")
    
    Other eDP panels not on this list should work as well.
    
    Change-Id: I0355d39a61956792e69bccd5274cfc2749d72bf0
    Signed-off-by: Alexander Couzens <lynxis@fe80.eu>
    Signed-off-by: Felix Singer <felixsinger@posteo.net>
    Signed-off-by: Alexei Sorokin <sor.alexei@meowr.ru>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/28950
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>

--- src/mainboard/lenovo/x230/Makefile.mk
+++ src/mainboard/lenovo/x230/Makefile.mk
@@ -5,4 +5,9 @@ bootblock-y += variants/$(VARIANT_DIR)/gpio.c
 romstage-y += variants/$(VARIANT_DIR)/early_init.c
 romstage-y += variants/$(VARIANT_DIR)/gpio.c
 ramstage-y += variants/$(VARIANT_DIR)/hda_verb.c
+
+ifeq ($(CONFIG_BOARD_LENOVO_X230_EDP),y)
+ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += variants/x230_edp/gma-mainboard.ads
+else
 ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += variants/$(VARIANT_DIR)/gma-mainboard.ads
+endif

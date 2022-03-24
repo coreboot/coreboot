879a2789eeca2f054f23a8d19f2afcf8479c7559
Raul E Rangel <rrangel@chromium.org>
2022-03-24 17:06:47 -0600

    mb/google/skyrim: Call espi_switch_to_spi1_pads
    We are using the second SPI pads for eSPI.
    
    BUG=b:226635441
    TEST=Build skyrim
    
    Signed-off-by: Raul E Rangel <rrangel@chromium.org>
    Change-Id: I43713d7376a28ced2be635668836464ceec46392
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63096
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/skyrim/bootblock.c
+++ src/mainboard/google/skyrim/bootblock.c
@@ -3,6 +3,7 @@
 #include <amdblocks/espi.h>
 #include <bootblock_common.h>
 #include <baseboard/variants.h>
+#include <soc/espi.h>
 
 void mb_set_up_early_espi(void)
 {
@@ -11,6 +12,8 @@ void mb_set_up_early_espi(void)
 
 	variant_espi_gpio_table(&gpios, &num_gpios);
 	gpio_configure_pads(gpios, num_gpios);
+
+	espi_switch_to_spi1_pads();
 }
 
 void bootblock_mainboard_early_init(void)

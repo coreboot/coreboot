08617e3f61715e89cc177b614362d1165bb6e8da
Matt DeVillier <matt.devillier@gmail.com>
2022-03-28 21:50:50 -0500

    mb/google/octopus: Demote NHLT log messages from error to info
    Change-Id: Ib2d0c6a23b66e6e61cc8ea09a443e19a4b37c66d
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63287
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Reviewed-by: Sean Rhodes <sean@starlabs.systems>

--- src/mainboard/google/octopus/variants/baseboard/nhlt.c
+++ src/mainboard/google/octopus/variants/baseboard/nhlt.c
@@ -12,11 +12,11 @@ void __weak variant_nhlt_init(struct nhlt *nhlt)
 
 	/* 2 Channel DMIC array. */
 	if (!nhlt_soc_add_dmic_array(nhlt, 2))
-		printk(BIOS_ERR, "Added 2CH DMIC array.\n");
+		printk(BIOS_INFO, "Added 2CH DMIC array.\n");
 
 	/* 4 Channel DMIC array. */
 	if (!nhlt_soc_add_dmic_array(nhlt, 4))
-		printk(BIOS_ERR, "Added 4CH DMIC arrays.\n");
+		printk(BIOS_INFO, "Added 4CH DMIC arrays.\n");
 
 	/*
 	 * Headset codec is bi-directional but uses the same configuration
@@ -25,7 +25,7 @@ void __weak variant_nhlt_init(struct nhlt *nhlt)
 	if (CONFIG(NHLT_DA7219) && codec == SSFC_AUDIO_CODEC_DA7219) {
 		/* Dialog for Headset codec */
 		if (!nhlt_soc_add_da7219(nhlt, AUDIO_LINK_SSP2))
-			printk(BIOS_ERR, "Added Dialog_7219 codec.\n");
+			printk(BIOS_INFO, "Added Dialog_7219 codec.\n");
 	}
 
 	if (CONFIG(NHLT_RT5682) &&
@@ -33,10 +33,10 @@ void __weak variant_nhlt_init(struct nhlt *nhlt)
 			codec == SSFC_AUDIO_CODEC_RT5682_VS)) {
 		/* Realtek for Headset codec */
 		if (!nhlt_soc_add_rt5682(nhlt, AUDIO_LINK_SSP2))
-			printk(BIOS_ERR, "Added ALC5682 codec.\n");
+			printk(BIOS_INFO, "Added ALC5682 codec.\n");
 	}
 
 	/* MAXIM Smart Amps for left and right speakers. */
 	if (!nhlt_soc_add_max98357(nhlt, AUDIO_LINK_SSP1))
-		printk(BIOS_ERR, "Added Maxim_98357 codec.\n");
+		printk(BIOS_INFO, "Added Maxim_98357 codec.\n");
 }

0c6298761b3800c88d781770286aea7194fd5bc0
Arthur Heymans <arthur@aheymans.xyz>
2022-03-30 19:34:10 +0200

    arch/x86/Kconfig: Drop obsolete fixed ramstage symbols
    On x86 ramstage is always relocated at runtime in cbmem so there is no
    need to have this configurable in Kconfig.
    
    Change-Id: I01b2335d0b82bea8f885ee5ca9814351bbf2aa3c
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63215
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/arch/x86/include/arch/memlayout.h
+++ src/arch/x86/include/arch/memlayout.h
@@ -3,10 +3,6 @@
 #ifndef __ARCH_MEMLAYOUT_H
 #define __ARCH_MEMLAYOUT_H
 
-#if (CONFIG_RAMTOP == 0)
-# error "CONFIG_RAMTOP not configured"
-#endif
-
 /* Intel386 psABI requires a 16 byte aligned stack. */
 #define ARCH_STACK_ALIGN_SIZE 16
 

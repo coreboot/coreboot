coreboot/coreboot

commit 41faa22c532def4093a65366764d12298c6a10d3

soc/mediatek: Add early_init for passing data across stages
Add support for "early_init_data" region, which can be used to store
data initialized in an early stage (such as bootblock), and retrieve it
in later stages (such as ramstage).

TEST=Build pass and boot up to kernel successfully via SSD on Dojo
board, here is the SSD information in boot log:
 == NVME IDENTIFY CONTROLLER DATA ==
    PCI VID   : 0x15b7
    PCI SSVID : 0x15b7
    SN        : 21517J440114
    MN        : WDC PC SN530 SDBPTPZ-256G-1006
    RAB       : 0x4
    AERL      : 0x7
    SQES      : 0x66
    CQES      : 0x44
    NN        : 0x1
Identified NVMe model WDC PC SN530 SDBPTPZ-256G-1006

BUG=b:178565024
BRANCH=cherry

Signed-off-by: Jianjun Wang <jianjun.wang@mediatek.com>
Change-Id: I01f91b7fe2cbe4f73b5c616bb7aae778dee27d9a
Reviewed-on: https://review.coreboot.org/c/coreboot/+/63019
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Yu-Ping Wu <yupingso@google.com>

src/soc/mediatek/common/early_init.c
@@ -1,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <assert.h>
+#include <soc/early_init.h>
+#include <stdlib.h>
+#include <string.h>
+
+static struct early_init_data *find_early_init(void)
+{
+	assert(sizeof(struct early_init_data) <= REGION_SIZE(early_init_data));
+	return (struct early_init_data *)_early_init_data;
+}
+
+void early_init_clear(void)
+{
+	struct early_init_data *data = find_early_init();
+
+	if (!data)
+		return;
+
+	memset(data, 0, sizeof(*data));
+}
+
+void early_init_save_time(enum early_init_type init_type)
+{
+	struct early_init_data *data = find_early_init();
+
+	if (!data)
+		return;
+
+	timer_monotonic_get(&data->init_time[init_type]);
+}
+
+uint64_t early_init_get_elapsed_time_us(enum early_init_type init_type)
+{
+	struct early_init_data *data = find_early_init();
+	struct mono_time cur_time;
+
+	if (!data)
+		return 0;
+
+	memset(&cur_time, 0, sizeof(cur_time));
+
+	/* If early init data was never saved */
+	if (!memcmp(&data->init_time[init_type], &cur_time, sizeof(cur_time)))
+		return 0;
+
+	timer_monotonic_get(&cur_time);
+
+	return mono_time_diff_microseconds(&data->init_time[init_type],
+					   &cur_time);
+}

src/soc/mediatek/common/include/soc/early_init.h
@@ -1,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SOC_MEDIATEK_EARLY_INIT_H
+#define SOC_MEDIATEK_EARLY_INIT_H
+
+#include <soc/symbols.h>
+#include <stdint.h>
+#include <symbols.h>
+#include <timer.h>
+
+DECLARE_REGION(early_init_data);
+
+enum early_init_type {
+	EARLY_INIT_PCIE,
+	EARLY_INIT_MAX,
+};
+
+struct early_init_data {
+	struct mono_time init_time[EARLY_INIT_MAX];
+};
+
+void early_init_clear(void);
+void early_init_save_time(enum early_init_type init_type);
+uint64_t early_init_get_elapsed_time_us(enum early_init_type init_type);
+
+#endif /* SOC_MEDIATEK_EARLY_INIT_H */

commit 79b35ca4812ab839ff12bfd7036ea90ea8f24f59

soc/mediatek/early_init: Fix function return type
Fix return type of early_init_get_elapsed_time_us() to comply with the
data type of return value.

Also replace memset() with struct initializer.

Signed-off-by: Jianjun Wang <jianjun.wang@mediatek.com>
Fixes: commit 41faa22 (soc/mediatek: Add early_init for passing data
       across stages)
Change-Id: I7c361828362c2dfec91358ad8a420f5360243da0
Reviewed-on: https://review.coreboot.org/c/coreboot/+/63190
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Yu-Ping Wu <yupingso@google.com>
Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

src/soc/mediatek/common/early_init.c
@@ -31,15 +31,13 @@ void early_init_save_time(enum early_init_type init_type)
 	timer_monotonic_get(&data->init_time[init_type]);
 }
 
-uint64_t early_init_get_elapsed_time_us(enum early_init_type init_type)
+long early_init_get_elapsed_time_us(enum early_init_type init_type)
 {
 	struct early_init_data *data = find_early_init();
-	struct mono_time cur_time;
+	struct mono_time cur_time = {0};
 
 	if (!data)
 		return 0;
-
-	memset(&cur_time, 0, sizeof(cur_time));
 
 	/* If early init data was never saved */
 	if (!memcmp(&data->init_time[init_type], &cur_time, sizeof(cur_time)))

src/soc/mediatek/common/include/soc/early_init.h
@@ -21,6 +21,6 @@ struct early_init_data {
 
 void early_init_clear(void);
 void early_init_save_time(enum early_init_type init_type);
-uint64_t early_init_get_elapsed_time_us(enum early_init_type init_type);
+long early_init_get_elapsed_time_us(enum early_init_type init_type);
 
 #endif /* SOC_MEDIATEK_EARLY_INIT_H */

src/soc/mediatek/common/pcie.c
@@ -215,7 +215,7 @@ void mtk_pcie_domain_enable(struct device *dev)
 	const mtk_soc_config_t *config = config_of(dev);
 	const struct mtk_pcie_config *conf = &config->pcie_config;
 	const char *ltssm_state;
-	uint64_t perst_time_us;
+	long perst_time_us;
 	size_t tries = 0;
 	uint32_t val;
 
@@ -236,7 +236,7 @@ void mtk_pcie_domain_enable(struct device *dev)
 	write32p(conf->base + PCIE_INT_ENABLE_REG, val);
 
 	perst_time_us = early_init_get_elapsed_time_us(EARLY_INIT_PCIE);
-	printk(BIOS_DEBUG, "%s: %lld us elapsed since assert PERST#\n",
+	printk(BIOS_DEBUG, "%s: %ld us elapsed since assert PERST#\n",
 	       __func__, perst_time_us);
 
 	/*
@@ -245,7 +245,7 @@ void mtk_pcie_domain_enable(struct device *dev)
 	 * The deassertion of PERST# should be delayed 100ms (TPVPERL)
 	 * for the power and clock to become stable.
 	 */
-	const uint64_t min_perst_time_us = 100000; /* 100 ms */
+	const long min_perst_time_us = 100000; /* 100 ms */
 	if (perst_time_us < min_perst_time_us) {
 		if (!perst_time_us) {
 			printk(BIOS_WARNING,
@@ -254,7 +254,7 @@ void mtk_pcie_domain_enable(struct device *dev)
 			mtk_pcie_reset(conf->base + PCIE_RST_CTRL_REG, true);
 		} else {
 			printk(BIOS_WARNING,
-			       "%s: Need an extra %lld us delay to meet PERST# deassertion requirement\n",
+			       "%s: Need an extra %ld us delay to meet PERST# deassertion requirement\n",
 			       __func__, min_perst_time_us - perst_time_us);
 		}
 
239267478069f83cea3c54f6cc600b2aa760edfd
Arthur Heymans <arthur@aheymans.xyz>
2022-04-06 23:17:19 +0200

    mb/qemu-armv7: Initialize cbmem
    Change-Id: I607205a0d44c71eb26031ced7a8af303efacd6f2
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63421
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- src/mainboard/emulation/qemu-armv7/romstage.c
+++ src/mainboard/emulation/qemu-armv7/romstage.c
@@ -1,10 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <cbmem.h>
 #include <console/console.h>
 #include <program_loading.h>
 
 void main(void)
 {
 	console_init();
+
+	cbmem_initialize_empty();
+
 	run_ramstage();
 }

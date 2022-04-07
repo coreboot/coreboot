a833d194413b985dca5d855180f8f1ba26dc65df
Michał Kopeć <michal.kopec@3mdeb.com>
2022-04-07 10:24:52 +0200

    mb/msi/ms7d25: add GPIO configuration
    Based on the output of:
    - inteltool from CB:63374
    - intelp2m from CB:63403
    
    TEST=Build coreboot binary for msi/ms7d5 and boot the board.
    
    Change-Id: If37eaf875f8fcfc64299227744a8c40d304a0214
    Signed-off-by: Michał Kopeć <michal.kopec@3mdeb.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63490
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- src/mainboard/msi/ms7d25/mainboard.c
+++ src/mainboard/msi/ms7d25/mainboard.c
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <device/device.h>
+#include <soc/ramstage.h>
+#include "gpio.h"
 
 static void mainboard_init(void *chip_info)
 {
@@ -10,6 +12,11 @@ static void mainboard_init(void *chip_info)
 static void mainboard_enable(struct device *dev)
 {
 
+}
+
+void mainboard_silicon_init_params(FSP_S_CONFIG *params)
+{
+	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 }
 
 struct chip_operations mainboard_ops = {

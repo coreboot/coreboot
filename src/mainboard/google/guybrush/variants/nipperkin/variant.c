4d4c0a5f277916ba5e4826aa952f379039a7e222
Kevin Chiu <kevin.chiu.17802@gmail.com>
2022-04-11 11:33:00 +0800

    mb/google/guybrush/var/nipperkin: turn off WLAN ASPM L1ss
    BUG=b:227296841
    BRANCH=guybrush
    TEST=emerge-guybrush coreboot chromeos-bootimage
         pass PLT criteria: S0 > 600ms, s0i3 > 14 days
    
    Change-Id: I9c61e1d0f3db8b9885040255d6de266616768b68
    Signed-off-by: Kevin Chiu <kevin.chiu.17802@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63537
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Jon Murphy <jpmurphy@google.com>

--- src/mainboard/google/guybrush/variants/nipperkin/variant.c
+++ src/mainboard/google/guybrush/variants/nipperkin/variant.c
@@ -1,23 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <baseboard/variants.h>
-#include <boardid.h>
-#include <device/device.h>
-#include <soc/gpio.h>
 #include <string.h>
 
 void variant_update_dxio_descriptors(fsp_dxio_descriptor *dxio_descriptors)
 {
-	uint32_t board_version = board_id();
-
-	if (board_version >= 3) {
-		dxio_descriptors[WLAN].link_aspm_L1_1 = true;
-		dxio_descriptors[WLAN].link_aspm_L1_2 = true;
-		/* Disable PSPP to avoid S0ix hangs - b/228830362 */
-		memset(dxio_descriptors[WLAN].port_params, 0,
-		       sizeof(dxio_descriptors[WLAN].port_params));
-	} else {
-		dxio_descriptors[WLAN].link_aspm_L1_1 = false;
-		dxio_descriptors[WLAN].link_aspm_L1_2 = false;
-	}
+	dxio_descriptors[WLAN].link_aspm_L1_1 = false;
+	dxio_descriptors[WLAN].link_aspm_L1_2 = false;
+	/* Disable PSPP to avoid S0ix hangs - b/228830362 */
+	memset(dxio_descriptors[WLAN].port_params, 0,
+	       sizeof(dxio_descriptors[WLAN].port_params));
 }

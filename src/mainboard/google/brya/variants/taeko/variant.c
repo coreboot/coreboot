37d93dc4883b738ed54804d0e030b05cec8ab8d5
Joey Peng <joey.peng@lcfc.corp-partner.google.com>
2022-04-15 14:27:20 +0800

    mb/google/brya/var/taeko: Add WIFI SAR support for tarlo
    Taeko/Tarlo uses the WIFI_SAR_ID field in FW_CONFIG to pick which
    SAR table to load.
    
    BUG=b:226684990
    TEST=emerge-brya coreboot
    
    Cq-Depend: chrome-internal:4676926, chrome-internal:4686953
    Signed-off-by: Joey Peng <joey.peng@lcfc.corp-partner.google.com>
    Change-Id: I9852553f5c91494db845d45a94e2566248538bba
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63644
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>

--- src/mainboard/google/brya/variants/taeko/variant.c
+++ src/mainboard/google/brya/variants/taeko/variant.c
@@ -1,8 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <fw_config.h>
 #include <sar.h>
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
-	return "wifi_sar_0.hex";
+	if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, WIFI_SAR_ID_0)))
+		return "wifi_sar_0.hex";
+	else if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, WIFI_SAR_ID_1)))
+		return "wifi_sar_1.hex";
+	return WIFI_SAR_CBFS_DEFAULT_FILENAME;
 }

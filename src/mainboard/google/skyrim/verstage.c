43f51a041d9f8ee5867031d043d541812b7fcab9
Karthikeyan Ramasubramanian <kramasub@google.com>
2022-04-19 14:34:59 -0600

    mb/google/skyrim: Prepare for enabling PSP verstage
    Add various verstage init functions to prepare for enable PSP verstage.
    
    BUG=None
    TEST=Build Skyrim BIOS image with PSP verstage enabled.
    
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Change-Id: I0d0dba05d4d083e2c6860078676e59cf8f487c87
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63731
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

---
+++ src/mainboard/google/skyrim/verstage.c
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <amdblocks/acpimmio.h>
+#include <amdblocks/gpio.h>
+#include <arch/io.h>
+#include <baseboard/variants.h>
+#include <psp_verstage.h>
+#include <security/vboot/vboot_common.h>
+#include <soc/southbridge.h>
+
+void verstage_mainboard_early_init(void)
+{
+	const struct soc_amd_gpio *gpios, *override_gpios;
+	size_t num_gpios, override_num_gpios;
+
+	variant_early_gpio_table(&gpios, &num_gpios);
+	variant_early_override_gpio_table(&override_gpios, &override_num_gpios);
+	gpio_configure_pads_with_override(gpios, num_gpios, override_gpios, override_num_gpios);
+}
+
+void verstage_mainboard_espi_init(void)
+{
+	const struct soc_amd_gpio *gpios;
+	size_t num_gpios;
+
+	variant_espi_gpio_table(&gpios, &num_gpios);
+	gpio_configure_pads(gpios, num_gpios);
+}
+
+void verstage_mainboard_tpm_init(void)
+{
+	const struct soc_amd_gpio *gpios;
+	size_t num_gpios;
+
+	variant_tpm_gpio_table(&gpios, &num_gpios);
+	gpio_configure_pads(gpios, num_gpios);
+}

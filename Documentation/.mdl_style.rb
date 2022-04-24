3144be76f403b14d2eff255e7ef93dec1303908a
Martin Roth <gaumless@gmail.com>
2022-04-24 11:02:25 -0600

    Documentation: Add mdl style file
    This adds a style file for the markdown linter mdl.
    
    The tool can be found on archive.org at the URL:
    https://web.archive.org/web/20220407032312/https://github.com/markdownlint/markdownlint
    
    This does 2 things:
    - Sets that line length limit to 72 characters as requested in the docs
    about writing the documentation.
    
    - Excludes several rules that were added for a particular markdown
    parser. My opinion is that these rules make the text versions of the
    markdown harder to read.
    
    To use this style file, run:
    $ mdl -s Documentation/.mdl_style.rb
    
    Signed-off-by: Martin Roth <gaumless@gmail.com>
    Change-Id: I98289492ae3e920d440f0e5c308a3590fb89d9fd
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63805
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Georgi <patrick@coreboot.org>

---
+++ Documentation/.mdl_style.rb
@@ -0,0 +1,9 @@
+# See one of the following URLs for explanations of all the rules
+# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+# https://web.archive.org/web/20220424164542/https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+
+all
+exclude_rule 'no-multiple-blanks'
+exclude_rule 'blanks-around-headers'
+exclude_rule 'blanks-around-lists'
+rule 'line-length', :line_length => 72

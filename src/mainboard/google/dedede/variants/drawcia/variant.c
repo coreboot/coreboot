a618e11f1a170db9a2bc2d7556c6cb38db303e3c
Elyes Haouas <ehaouas@noos.fr>
2022-04-25 20:00:38 +0200

    mb/{google,ocp}: Remove unused <bootstate.h>
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Change-Id: Id4550842a31f89e7eb6c1543512794eeb5e24937
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63833
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin L Roth <gaumless@tutanota.com>

--- src/mainboard/google/dedede/variants/drawcia/variant.c
+++ src/mainboard/google/dedede/variants/drawcia/variant.c
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <bootstate.h>
 #include <acpi/acpi.h>
 #include <baseboard/variants.h>
 #include <delay.h>
--- src/mainboard/ocp/tiogapass/ramstage.c
+++ src/mainboard/ocp/tiogapass/ramstage.c
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include <bootstate.h>
 #include <drivers/ipmi/ipmi_ops.h>
 #include <drivers/ocp/dmi/ocp_dmi.h>
 #include <soc/ramstage.h>

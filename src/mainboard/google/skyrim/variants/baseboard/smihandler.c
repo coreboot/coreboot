e6c677ce94960f983c9246d190a28dc405152d36
Raul E Rangel <rrangel@chromium.org>
2022-04-25 13:45:26 -0600

    mb/google/skyrim: Include smm handler
    We need to include the SMM handler to enable SCI events when ACPI is
    enabled.
    
    With this enabled we now see we have EC timeout problems while in SMI:
    
    [SPEW ]  SMI# #1
    [WARN ]  SMIx88 => 0x800
    [DEBUG]  Chrome EC: Set SMI mask to 0x0000000000000000
    [DEBUG]  Chrome EC: UHEPI supported
    [ERROR]  Timeout waiting for EC QUERY_EVENT!
    [DEBUG]  Clearing pending EC events. Error code EC_RES_UNAVAILABLE(9) is expected.
    [ERROR]  EC returned error result code 1
    [DEBUG]  Chrome EC: Set SCI mask to 0x00000000186601fb
    
    We still need to debug that. I suspect we have problems reading from
    the ACPI IO decodes 0x62 or 0x66.
    
    BUG=none
    TEST=Verify SMI handler runs
    
    Signed-off-by: Raul E Rangel <rrangel@chromium.org>
    Change-Id: Ida0fcd634e620274e124a8669836f3974e0a2bf4
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63844
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Jon Murphy <jpmurphy@google.com>

--- src/mainboard/google/skyrim/variants/baseboard/smihandler.c
+++ src/mainboard/google/skyrim/variants/baseboard/smihandler.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <amdblocks/gpio.h>
 #include <baseboard/variants.h>
 #include <console/console.h>
 #include <cpu/x86/smm.h>
@@ -16,7 +17,7 @@ void mainboard_smi_sleep(u8 slp_typ)
 	chromeec_smi_sleep(slp_typ, MAINBOARD_EC_S3_WAKE_EVENTS, MAINBOARD_EC_S5_WAKE_EVENTS);
 
 	variant_sleep_gpio_table(&gpios, &num_gpios);
-	program_gpios(gpios, num_gpios);
+	gpio_configure_pads(gpios, num_gpios);
 }
 
 int mainboard_smi_apmc(u8 apmc)

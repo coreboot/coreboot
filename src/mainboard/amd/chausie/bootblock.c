6e184e6bdf2d4662aa110affbf4f78208bceea2f
Fred Reitberger <reitbergerfred@gmail.com>
2022-04-26 10:22:15 -0400

    md/amd/chausie: call espi_switch_to_spi1_pads
    Chausie uses the spi1 pads for eSPI
    
    Change-Id: Iee9b92dd9b4e84764568ec3cc8d1fce731e0d1a7
    Signed-off-by: Fred Reitberger <reitbergerfred@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63866
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/amd/chausie/bootblock.c
+++ src/mainboard/amd/chausie/bootblock.c
@@ -1,9 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootblock_common.h>
+#include <soc/espi.h>
 #include "gpio.h"
 
 void bootblock_mainboard_early_init(void)
 {
 	mainboard_program_early_gpios();
+
+	espi_switch_to_spi1_pads();
 }

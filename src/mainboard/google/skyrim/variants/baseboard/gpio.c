4852c114063e80b27a7a1ea4b5afcc19ec4d632b
Ian Feng <ian_feng@compal.corp-partner.google.com>
2022-05-03 14:52:20 +0800

    mb/google/skyrim: Fix SD card power sequence
    Fix power sequence according to datasheet:GL9750S-OIY04 rev1.24.
    
    BUG=b:229181624
    TEST=Build and boot to OS in Skyrim. Ensure that the SD Controller
    and SD Card are enumerated fine.
    
    Signed-off-by: Ian Feng <ian_feng@compal.corp-partner.google.com>
    Change-Id: Iea729d43d10a3f8353b4fe540146d00975f4d422
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/64023
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/mainboard/google/skyrim/variants/baseboard/gpio.c
+++ src/mainboard/google/skyrim/variants/baseboard/gpio.c
@@ -195,6 +195,8 @@ static const struct soc_amd_gpio pcie_gpio_table[] = {
 	PAD_GPO(GPIO_7, HIGH),
 	/* PCIE_RST0_L */
 	PAD_NFO(GPIO_26, PCIE_RST0_L, HIGH),
+	/* SD_AUX_RESET_L  */
+	PAD_NFO(GPIO_27, PCIE_RST1_L, HIGH),
 };
 
 __weak void variant_pcie_gpio_table(const struct soc_amd_gpio **gpio, size_t *size)

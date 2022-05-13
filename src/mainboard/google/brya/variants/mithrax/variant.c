af092ac6ec48a7562ef6301b6fa492f0ec07d835
John Su <john_su@compal.corp-partner.google.com>
2022-05-13 17:06:42 +0800

    mb/google/brya/var/mithrax: update overridetree and Kconfig
    1. Update override devicetree based on schematics.
    2. Update Kconfig based on schematics.
    
    BUG=b:229191897
    TEST=emerge-brya coreboot chromeos-bootimage
    
    Signed-off-by: John Su <john_su@compal.corp-partner.google.com>
    Change-Id: Ia28ae16f609fda6d90558e69b2d41139dbe533fd
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/64329
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Frank Wu <frank_wu@compal.corp-partner.google.com>

---
+++ src/mainboard/google/brya/variants/mithrax/variant.c
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <chip.h>
+#include <fw_config.h>
+#include <baseboard/variants.h>
+
+void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
+{
+	if (fw_config_probe(FW_CONFIG(DB_USB, USB3_PS8815))) {
+		config->typec_aux_bias_pads[2].pad_auxp_dc = GPP_A19;
+		config->typec_aux_bias_pads[2].pad_auxn_dc = GPP_A20;
+		config->tcss_aux_ori = 0x10;
+	}
+}

coreboot/coreboot

commit f89cb241eecdf70d9e52c852833c6ed1e3b9632d

mb/google/glados: port to FSP 2.0
This patch is part of the patch series to drop support for FSP 1.1 in
soc/intel/skylake.

The following modifications have been done to migrate the board(s) from
FSP 1.1 to FSP 2.0:

- remove deprecated devicetree VR_RING domain (only 4 domains in FSP 2.0)
- switch to using the FSP default VBT

TODO:
- testing

Change-Id: Id747ef484dfdcb2d346f817976f52073912468d0
Signed-off-by: Michael Niewöhner <foss@mniewoehner.de>
Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/35921
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/mainboard/google/glados/variants/lars/variant.c
@@ -28,9 +28,11 @@
 #define MEM_SINGLE_CHANB	0xb
 #define MEM_SINGLE_CHANC	0xc
 
-void variant_memory_init_params(
-		MEMORY_INIT_UPD *const params, const int spd_index)
+void variant_memory_init_params(FSPM_UPD *mupd, const int spd_index)
 {
+	FSP_M_CONFIG *mem_cfg;
+	mem_cfg = &mupd->FspmConfig;
+
 	/* DQ byte map */
 	const u8 dq_map[2][12] = {
 		  { 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
@@ -58,18 +60,12 @@ void variant_memory_init_params(
 	if (spd_index == K4E6E304EB_MEM_ID)
 		targeted_rcomp = StrengthendRcompTarget;
 
-	memcpy(params->DqByteMapCh0, dq_map[0],
-			sizeof(params->DqByteMapCh0));
-	memcpy(params->DqByteMapCh1, dq_map[1],
-			sizeof(params->DqByteMapCh1));
-	memcpy(params->DqsMapCpu2DramCh0, dqs_map[0],
-			sizeof(params->DqsMapCpu2DramCh0));
-	memcpy(params->DqsMapCpu2DramCh1, dqs_map[1],
-			sizeof(params->DqsMapCpu2DramCh1));
-	memcpy(params->RcompResistor, RcompResistor,
-			sizeof(params->RcompResistor));
-	memcpy(params->RcompTarget, targeted_rcomp,
-			sizeof(params->RcompTarget));
+	memcpy(mem_cfg->DqByteMapCh0, dq_map[0], sizeof(mem_cfg->DqByteMapCh0));
+	memcpy(mem_cfg->DqByteMapCh1, dq_map[1], sizeof(mem_cfg->DqByteMapCh1));
+	memcpy(mem_cfg->DqsMapCpu2DramCh0, dqs_map[0], sizeof(mem_cfg->DqsMapCpu2DramCh0));
+	memcpy(mem_cfg->DqsMapCpu2DramCh1, dqs_map[1], sizeof(mem_cfg->DqsMapCpu2DramCh1));
+	memcpy(mem_cfg->RcompResistor, RcompResistor, sizeof(mem_cfg->RcompResistor));
+	memcpy(mem_cfg->RcompTarget, RcompTarget, sizeof(mem_cfg->RcompTarget));
 }
 
 int is_dual_channel(const int spd_index)

src/mainboard/google/glados/variants/sentry/variant.c
@@ -21,9 +21,11 @@
 
 #define K4E6E304EE_MEM_ID	0x3
 
-void variant_memory_init_params(
-		MEMORY_INIT_UPD *const memory_params, const int spd_index)
+void variant_memory_init_params(FSPM_UPD *mupd, const int spd_index)
 {
+	FSP_M_CONFIG *mem_cfg;
+	mem_cfg = &mupd->FspmConfig;
+
 	/* DQ byte map */
 	const u8 dq_map[2][12] = {
 		  { 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
@@ -51,16 +53,10 @@ void variant_memory_init_params(
 	if (spd_index == K4E6E304EE_MEM_ID)
 		targeted_rcomp = StrengthendRcompTarget;
 
-	memcpy(memory_params->DqByteMapCh0, dq_map[0],
-			sizeof(memory_params->DqByteMapCh0));
-	memcpy(memory_params->DqByteMapCh1, dq_map[1],
-			sizeof(memory_params->DqByteMapCh1));
-	memcpy(memory_params->DqsMapCpu2DramCh0, dqs_map[0],
-			sizeof(memory_params->DqsMapCpu2DramCh0));
-	memcpy(memory_params->DqsMapCpu2DramCh1, dqs_map[1],
-			sizeof(memory_params->DqsMapCpu2DramCh1));
-	memcpy(memory_params->RcompResistor, RcompResistor,
-			sizeof(memory_params->RcompResistor));
-	memcpy(memory_params->RcompTarget, targeted_rcomp,
-			sizeof(memory_params->RcompTarget));
+	memcpy(mem_cfg->DqByteMapCh0, dq_map[0], sizeof(mem_cfg->DqByteMapCh0));
+	memcpy(mem_cfg->DqByteMapCh1, dq_map[1], sizeof(mem_cfg->DqByteMapCh1));
+	memcpy(mem_cfg->DqsMapCpu2DramCh0, dqs_map[0], sizeof(mem_cfg->DqsMapCpu2DramCh0));
+	memcpy(mem_cfg->DqsMapCpu2DramCh1, dqs_map[1], sizeof(mem_cfg->DqsMapCpu2DramCh1));
+	memcpy(mem_cfg->RcompResistor, RcompResistor, sizeof(mem_cfg->RcompResistor));
+	memcpy(mem_cfg->RcompTarget, RcompTarget, sizeof(mem_cfg->RcompTarget));
 }

src/mainboard/google/glados/variants/asuka/variant.c
@@ -18,10 +18,13 @@
 #include <string.h>
 #include <baseboard/variant.h>
 #include <fsp/soc_binding.h>
+#include <soc/romstage.h>
 
-void variant_memory_init_params(
-		MEMORY_INIT_UPD *const memory_params, const int spd_index)
+void variant_memory_init_params(FSPM_UPD *mupd, const int spd_index)
 {
+	FSP_M_CONFIG *mem_cfg;
+	mem_cfg = &mupd->FspmConfig;
+
 	/* DQ byte map */
 	const u8 dq_map[2][12] = {
 		{ 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
@@ -39,18 +42,12 @@ void variant_memory_init_params(
 	/* Rcomp target */
 	const u16 RcompTarget[5] = { 100, 40, 40, 23, 40 };
 
-	memcpy(memory_params->DqByteMapCh0, dq_map[0],
-			sizeof(memory_params->DqByteMapCh0));
-	memcpy(memory_params->DqByteMapCh1, dq_map[1],
-			sizeof(memory_params->DqByteMapCh1));
-	memcpy(memory_params->DqsMapCpu2DramCh0, dqs_map[0],
-			sizeof(memory_params->DqsMapCpu2DramCh0));
-	memcpy(memory_params->DqsMapCpu2DramCh1, dqs_map[1],
-			sizeof(memory_params->DqsMapCpu2DramCh1));
-	memcpy(memory_params->RcompResistor, RcompResistor,
-			sizeof(memory_params->RcompResistor));
-	memcpy(memory_params->RcompTarget, RcompTarget,
-			sizeof(memory_params->RcompTarget));
+	memcpy(mem_cfg->DqByteMapCh0, dq_map[0], sizeof(mem_cfg->DqByteMapCh0));
+	memcpy(mem_cfg->DqByteMapCh1, dq_map[1], sizeof(mem_cfg->DqByteMapCh1));
+	memcpy(mem_cfg->DqsMapCpu2DramCh0, dqs_map[0], sizeof(mem_cfg->DqsMapCpu2DramCh0));
+	memcpy(mem_cfg->DqsMapCpu2DramCh1, dqs_map[1], sizeof(mem_cfg->DqsMapCpu2DramCh1));
+	memcpy(mem_cfg->RcompResistor, RcompResistor, sizeof(mem_cfg->RcompResistor));
+	memcpy(mem_cfg->RcompTarget, RcompTarget, sizeof(mem_cfg->RcompTarget));
 }
 
 int is_dual_channel(const int spd_index)

src/mainboard/google/glados/variants/caroline/variant.c
@@ -21,9 +21,11 @@
 #include <string.h>
 #include <variant/gpio.h>
 
-void variant_memory_init_params(
-		MEMORY_INIT_UPD *const memory_params, const int spd_index)
+void variant_memory_init_params(FSPM_UPD *mupd, const int spd_index)
 {
+	FSP_M_CONFIG *mem_cfg;
+	mem_cfg = &mupd->FspmConfig;
+
 	/* DQ byte map */
 	const u8 dq_map[2][12] = {
 		  { 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
@@ -41,19 +43,12 @@ void variant_memory_init_params(
 	/* Rcomp target */
 	const u16 RcompTarget[5] = { 100, 40, 40, 23, 40 };
 
-	memcpy(memory_params->DqByteMapCh0, dq_map[0],
-			sizeof(memory_params->DqByteMapCh0));
-	memcpy(memory_params->DqByteMapCh1, dq_map[1],
-			sizeof(memory_params->DqByteMapCh1));
-	memcpy(memory_params->DqsMapCpu2DramCh0, dqs_map[0],
-			sizeof(memory_params->DqsMapCpu2DramCh0));
-	memcpy(memory_params->DqsMapCpu2DramCh1, dqs_map[1],
-			sizeof(memory_params->DqsMapCpu2DramCh1));
-	memcpy(memory_params->RcompResistor, RcompResistor,
-			sizeof(memory_params->RcompResistor));
-	memcpy(memory_params->RcompTarget, RcompTarget,
-			sizeof(memory_params->RcompTarget));
-	memory_params->DdrFreqLimit = 1600;
+	memcpy(mem_cfg->DqByteMapCh0, dq_map[0], sizeof(mem_cfg->DqByteMapCh0));
+	memcpy(mem_cfg->DqByteMapCh1, dq_map[1], sizeof(mem_cfg->DqByteMapCh1));
+	memcpy(mem_cfg->DqsMapCpu2DramCh0, dqs_map[0], sizeof(mem_cfg->DqsMapCpu2DramCh0));
+	memcpy(mem_cfg->DqsMapCpu2DramCh1, dqs_map[1], sizeof(mem_cfg->DqsMapCpu2DramCh1));
+	memcpy(mem_cfg->RcompResistor, RcompResistor, sizeof(mem_cfg->RcompResistor));
+	memcpy(mem_cfg->RcompTarget, RcompTarget, sizeof(mem_cfg->RcompTarget));
 }
 
 void mainboard_gpio_smi_sleep(void)

src/mainboard/google/glados/variants/cave/variant.c
@@ -21,9 +21,11 @@
 #include <string.h>
 #include <variant/gpio.h>
 
-void variant_memory_init_params(
-		MEMORY_INIT_UPD *const memory_params, const int spd_index)
+void variant_memory_init_params(FSPM_UPD *mupd, const int spd_index)
 {
+	FSP_M_CONFIG *mem_cfg;
+	mem_cfg = &mupd->FspmConfig;
+
 	/* DQ byte map */
 	const u8 dq_map[2][12] = {
 		  { 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
@@ -41,18 +43,12 @@ void variant_memory_init_params(
 	/* Rcomp target */
 	const u16 RcompTarget[5] = { 100, 40, 40, 23, 40 };
 
-	memcpy(memory_params->DqByteMapCh0, dq_map[0],
-			sizeof(memory_params->DqByteMapCh0));
-	memcpy(memory_params->DqByteMapCh1, dq_map[1],
-			sizeof(memory_params->DqByteMapCh1));
-	memcpy(memory_params->DqsMapCpu2DramCh0, dqs_map[0],
-			sizeof(memory_params->DqsMapCpu2DramCh0));
-	memcpy(memory_params->DqsMapCpu2DramCh1, dqs_map[1],
-			sizeof(memory_params->DqsMapCpu2DramCh1));
-	memcpy(memory_params->RcompResistor, RcompResistor,
-			sizeof(memory_params->RcompResistor));
-	memcpy(memory_params->RcompTarget, RcompTarget,
-			sizeof(memory_params->RcompTarget));
+	memcpy(mem_cfg->DqByteMapCh0, dq_map[0], sizeof(mem_cfg->DqByteMapCh0));
+	memcpy(mem_cfg->DqByteMapCh1, dq_map[1], sizeof(mem_cfg->DqByteMapCh1));
+	memcpy(mem_cfg->DqsMapCpu2DramCh0, dqs_map[0], sizeof(mem_cfg->DqsMapCpu2DramCh0));
+	memcpy(mem_cfg->DqsMapCpu2DramCh1, dqs_map[1], sizeof(mem_cfg->DqsMapCpu2DramCh1));
+	memcpy(mem_cfg->RcompResistor, RcompResistor, sizeof(mem_cfg->RcompResistor));
+	memcpy(mem_cfg->RcompTarget, RcompTarget, sizeof(mem_cfg->RcompTarget));
 }
 
 void mainboard_gpio_smi_sleep(void)

src/mainboard/google/glados/variants/chell/variant.c
@@ -21,15 +21,18 @@
 #include <string.h>
 #include <variant/gpio.h>
 
-void variant_memory_init_params(
-		MEMORY_INIT_UPD *const memory_params, const int spd_index)
+void variant_memory_init_params(FSPM_UPD *mupd, const int spd_index)
 {
+	FSP_M_CONFIG *mem_cfg;
+	mem_cfg = &mupd->FspmConfig;
+
 	/* DQ byte map */
 	const u8 dq_map[2][12] = {
 		  { 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
 		    0x0F, 0x00, 0xFF, 0x00, 0xFF, 0x00 },
 		  { 0x33, 0xCC, 0x00, 0xCC, 0x33, 0xCC,
 		    0x33, 0x00, 0xFF, 0x00, 0xFF, 0x00 } };
+
 	/* DQS CPU<>DRAM map */
 	const u8 dqs_map[2][8] = {
 		{ 0, 3, 1, 2, 4, 5, 6, 7 },
@@ -41,18 +44,12 @@ void variant_memory_init_params(
 	/* Rcomp target */
 	const u16 RcompTarget[5] = { 100, 40, 40, 23, 40 };
 
-	memcpy(memory_params->DqByteMapCh0, dq_map[0],
-			sizeof(memory_params->DqByteMapCh0));
-	memcpy(memory_params->DqByteMapCh1, dq_map[1],
-			sizeof(memory_params->DqByteMapCh1));
-	memcpy(memory_params->DqsMapCpu2DramCh0, dqs_map[0],
-			sizeof(memory_params->DqsMapCpu2DramCh0));
-	memcpy(memory_params->DqsMapCpu2DramCh1, dqs_map[1],
-			sizeof(memory_params->DqsMapCpu2DramCh1));
-	memcpy(memory_params->RcompResistor, RcompResistor,
-			sizeof(memory_params->RcompResistor));
-	memcpy(memory_params->RcompTarget, RcompTarget,
-			sizeof(memory_params->RcompTarget));
+	memcpy(mem_cfg->DqByteMapCh0, dq_map[0], sizeof(mem_cfg->DqByteMapCh0));
+	memcpy(mem_cfg->DqByteMapCh1, dq_map[1], sizeof(mem_cfg->DqByteMapCh1));
+	memcpy(mem_cfg->DqsMapCpu2DramCh0, dqs_map[0], sizeof(mem_cfg->DqsMapCpu2DramCh0));
+	memcpy(mem_cfg->DqsMapCpu2DramCh1, dqs_map[1], sizeof(mem_cfg->DqsMapCpu2DramCh1));
+	memcpy(mem_cfg->RcompResistor, RcompResistor, sizeof(mem_cfg->RcompResistor));
+	memcpy(mem_cfg->RcompTarget, RcompTarget, sizeof(mem_cfg->RcompTarget));
 }
 
 void mainboard_gpio_smi_sleep(void)

src/mainboard/google/glados/variants/glados/variant.c
@@ -21,9 +21,11 @@
 #include <string.h>
 #include <variant/gpio.h>
 
-void variant_memory_init_params(
-		MEMORY_INIT_UPD *const memory_params, const int spd_index)
+void variant_memory_init_params(FSPM_UPD *mupd, const int spd_index)
 {
+	FSP_M_CONFIG *mem_cfg;
+	mem_cfg = &mupd->FspmConfig;
+
 	/* DQ byte map */
 	const u8 dq_map[2][12] = {
 		  { 0x0F, 0xF0, 0x00, 0xF0, 0x0F, 0xF0,
@@ -41,18 +43,12 @@ void variant_memory_init_params(
 	/* Rcomp target */
 	const u16 RcompTarget[5] = { 100, 40, 40, 23, 40 };
 
-	memcpy(memory_params->DqByteMapCh0, dq_map[0],
-			sizeof(memory_params->DqByteMapCh0));
-	memcpy(memory_params->DqByteMapCh1, dq_map[1],
-			sizeof(memory_params->DqByteMapCh1));
-	memcpy(memory_params->DqsMapCpu2DramCh0, dqs_map[0],
-			sizeof(memory_params->DqsMapCpu2DramCh0));
-	memcpy(memory_params->DqsMapCpu2DramCh1, dqs_map[1],
-			sizeof(memory_params->DqsMapCpu2DramCh1));
-	memcpy(memory_params->RcompResistor, RcompResistor,
-			sizeof(memory_params->RcompResistor));
-	memcpy(memory_params->RcompTarget, RcompTarget,
-			sizeof(memory_params->RcompTarget));
+	memcpy(mem_cfg->DqByteMapCh0, dq_map[0], sizeof(mem_cfg->DqByteMapCh0));
+	memcpy(mem_cfg->DqByteMapCh1, dq_map[1], sizeof(mem_cfg->DqByteMapCh1));
+	memcpy(mem_cfg->DqsMapCpu2DramCh0, dqs_map[0], sizeof(mem_cfg->DqsMapCpu2DramCh0));
+	memcpy(mem_cfg->DqsMapCpu2DramCh1, dqs_map[1], sizeof(mem_cfg->DqsMapCpu2DramCh1));
+	memcpy(mem_cfg->RcompResistor, RcompResistor, sizeof(mem_cfg->RcompResistor));
+	memcpy(mem_cfg->RcompTarget, RcompTarget, sizeof(mem_cfg->RcompTarget));
 }
 
 void mainboard_gpio_smi_sleep(void)

src/mainboard/google/glados/Kconfig
@@ -17,6 +17,7 @@ config BOARD_GOOGLE_BASEBOARD_GLADOS
 	select MAINBOARD_HAS_CHROMEOS
 	select MAINBOARD_HAS_LPC_TPM
 	select MAINBOARD_HAS_TPM1
+	select MAINBOARD_USES_FSP2_0
 	select SOC_INTEL_SKYLAKE
 	select SYSTEM_TYPE_LAPTOP
 
@@ -61,6 +62,9 @@ config VARIANT_DIR
 config DEVICETREE
 	string
 	default "variants/$(CONFIG_VARIANT_DIR)/devicetree.cb"
+
+config INTEL_GMA_VBT_FILE
+	default "3rdparty/fsp/KabylakeFspBinPkg/SampleCode/Vbt/Vbt.bin"
 
 config MAX_CPUS
 	int

commit 522c92ed35dd8f7f145aabbe7b3e6ddc4992b4f9

mb/google/glados: Fix unused variable
Commit f89cb241eec introduced a regression where the RcompTarget was
not updated according to the SPD.

Change-Id: I07715224b11937604b107e370d957745b245ddd9
Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/64239
Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/mainboard/google/glados/variants/lars/variant.c
@@ -51,7 +51,7 @@ void variant_memory_init_params(FSPM_UPD *mupd, const int spd_index)
 	memcpy(mem_cfg->DqsMapCpu2DramCh0, dqs_map[0], sizeof(mem_cfg->DqsMapCpu2DramCh0));
 	memcpy(mem_cfg->DqsMapCpu2DramCh1, dqs_map[1], sizeof(mem_cfg->DqsMapCpu2DramCh1));
 	memcpy(mem_cfg->RcompResistor, RcompResistor, sizeof(mem_cfg->RcompResistor));
-	memcpy(mem_cfg->RcompTarget, RcompTarget, sizeof(mem_cfg->RcompTarget));
+	memcpy(mem_cfg->RcompTarget, targeted_rcomp, sizeof(mem_cfg->RcompTarget));
 }
 
 int is_dual_channel(const int spd_index)

src/mainboard/google/glados/variants/sentry/variant.c
@@ -44,5 +44,5 @@ void variant_memory_init_params(FSPM_UPD *mupd, const int spd_index)
 	memcpy(mem_cfg->DqsMapCpu2DramCh0, dqs_map[0], sizeof(mem_cfg->DqsMapCpu2DramCh0));
 	memcpy(mem_cfg->DqsMapCpu2DramCh1, dqs_map[1], sizeof(mem_cfg->DqsMapCpu2DramCh1));
 	memcpy(mem_cfg->RcompResistor, RcompResistor, sizeof(mem_cfg->RcompResistor));
-	memcpy(mem_cfg->RcompTarget, RcompTarget, sizeof(mem_cfg->RcompTarget));
+	memcpy(mem_cfg->RcompTarget, targeted_rcomp, sizeof(mem_cfg->RcompTarget));
 }
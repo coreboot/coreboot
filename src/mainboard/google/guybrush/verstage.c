ac040552fc865fe0c166818bf8902b0b0ea594c5
Jon Murphy <jpmurphy@google.com>
2022-05-20 14:45:00 -0600

    mb/google/guybrush: Remove TODO for ESPI functions
    The feature request was moved to Skyrim in the interest of time
    and effort.  The bug was updated to reflect this, and the comment
    should be removed from the monkey island code base
    
    BUG=b:232952508
    TEST=Builds
    
    Signed-off-by: Jon Murphy <jpmurphy@google.com>
    Change-Id: Id4ca43692aa56b6dba2f7acc1f924b30c1e966ab
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/64558
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/mainboard/google/guybrush/verstage.c
+++ src/mainboard/google/guybrush/verstage.c
@@ -27,11 +27,6 @@ void verstage_mainboard_espi_init(void)
 	gpios = variant_espi_gpio_table(&num_gpios);
 	gpio_configure_pads(gpios, num_gpios);
 
-	/*
-	* TODO : Make common function in cezanne code and just call it
-	* when PCI access is fixed in the PSP (b/186602472).
-	* For now the PSP doesn't configure LPC so it should be fine.
-	*/
 	dword = pm_io_read32(PM_SPI_PAD_PU_PD);
 	dword |= PM_ESPI_CS_USE_DATA2;
 	pm_io_write32(PM_SPI_PAD_PU_PD, dword);

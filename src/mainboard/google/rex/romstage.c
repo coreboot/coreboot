f6c52f46846b4d6157bc1cddbadbbece8a872afc
Eric Lai <eric_lai@quanta.corp-partner.google.com>
2022-05-23 15:47:20 +0800

    mb/google/rex: Add entry stubs of each stage
    Add entry point stubs of each stage for Rex. More functionalities will
    be added later.
    
    BUG=b:224325352
    TEST=util/abuild/abuild -p none -t google/rex -a -c max
    
    Signed-off-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Change-Id: I2310e58ab92bdb0ce86a9f7284cc0b3e04a2889f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/64591
    Reviewed-by: Tarun Tuli <taruntuli@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/rex/romstage.c
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <fsp/api.h>
+#include <soc/romstage.h>
+
+void mainboard_memory_init_params(FSPM_UPD *memupd)
+{
+	/* TODO : Fill FSP-M memory params */
+}

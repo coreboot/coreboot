469d4908c66813cc51e7dae3dccb6bb5a6d75e62
Jon Murphy <jpmurphy@google.com>
2022-05-24 12:18:25 -0600

    mb/google/skyrim: Remove unused GPIO table
    On Skyrim, the power and lid switches are managed by the EC
    and coreboot and the AP have no control over them within this
    context.  Remove unused GPIO's to prevent coreboot warnings
    about resampling at boot.
    
    BUG=b:233771163
    TEST=Builds
    
    Signed-off-by: Jon Murphy <jpmurphy@google.com>
    Change-Id: Ie369bb7d430bd0dd1f1c1f41bf543a9b18e34db1
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/64644
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/skyrim/chromeos.c
+++ src/mainboard/google/skyrim/chromeos.c
@@ -8,11 +8,6 @@
 
 void fill_lb_gpios(struct lb_gpios *gpios)
 {
-	struct lb_gpio chromeos_gpios[] = {
-		{-1, ACTIVE_HIGH, get_lid_switch(), "lid"},
-		{-1, ACTIVE_HIGH, 0, "power"},
-	};
-	lb_add_gpios(gpios, chromeos_gpios, ARRAY_SIZE(chromeos_gpios));
 }
 
 static const struct cros_gpio cros_gpios[] = {

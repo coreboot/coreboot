ccaafdfa5a9834a43134f0b5d0bafd8a0748eec6
Guodong Liu <guodong.liu@mediatek.corp-partner.google.com>
2022-06-11 10:37:47 +0800

    soc/mediatek/mt8188: Add GPIO drivers
    Add GPIO drivers to let other module control GPIOs.
    
    TEST=build pass
    BUG=b:233720142
    
    Signed-off-by: Guodong Liu <guodong.liu@mediatek.corp-partner.google.com>
    Change-Id: I0a2a5178949e9ad3e033ac332e0f1e8565e39b3d
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65619
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-by: Yidi Lin <yidilin@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/soc/mediatek/mt8188/gpio.c
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/*
+ * This file is created based on MT8188 Functional Specification
+ * Chapter number: 5.2
+ */
+
+#include <device/mmio.h>
+#include <gpio.h>
+
+void *gpio_find_reg_addr(gpio_t gpio)
+{
+	void *reg_addr;
+	switch (gpio.base & 0x0f) {
+	case 1:
+		reg_addr = (void *)IOCFG_RM_BASE;
+		break;
+	case 2:
+		reg_addr = (void *)IOCFG_LT_BASE;
+		break;
+	case 3:
+		reg_addr = (void *)IOCFG_LM_BASE;
+		break;
+	case 4:
+		reg_addr = (void *)IOCFG_RT_BASE;
+		break;
+	default:
+		reg_addr = NULL;
+		break;
+	}
+
+	return reg_addr;
+}
---
+++ src/soc/mediatek/mt8188/include/soc/gpio_base.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/*
+ * This file is created based on MT8188 Functional Specification
+ * Chapter number: 5.2
+ */
+
+#ifndef SOC_MEDIATEK_MT8188_GPIO_BASE_H
+#define SOC_MEDIATEK_MT8188_GPIO_BASE_H
+
+#include <stdint.h>
+
+typedef union {
+	u32 raw;
+	struct {
+		u32 id     : 8;
+		u32 flag   : 3;
+		u32 bit    : 5;
+		u32 base   : 8;
+		u32 offset : 8;
+	};
+} gpio_t;
+
+#endif

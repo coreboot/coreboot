244ecad52cd9ab4d0c6f1786a34a97b687ef53d0
eddylu@ami.corp-partner.google.com <eddylu@ami.corp-partner.google.com>
2022-06-14 13:03:34 +0800

    mb/google/brya/vell: Implement variant_devtree_update() for audio
    Different board versions have different audio layouts, therefore
    support both layouts by enabling only the appropriate devices
    in the devicetree via board_id().
    
    BUG=b:207333035
    BRANCH=none
    TEST='FW_NAME=vell emerge-brya coreboot'
    
    Change-Id: If053b8f85933f8fc75589ae175e225cc9c1e3991
    Signed-off-by: Eddy Lu <eddylu@ami.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65124
    Reviewed-by: Shon Wang <shon.wang@quanta.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/mainboard/google/brya/variants/vell/variant.c
+++ src/mainboard/google/brya/variants/vell/variant.c
@@ -1,8 +1,33 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <baseboard/variants.h>
+#include <boardid.h>
 #include <sar.h>
+#include <soc/pci_devs.h>
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
 	return "wifi_sar_0.hex";
+}
+
+void variant_devtree_update(void)
+{
+	struct device *i2c0_cs35l53_0 = DEV_PTR(i2c0_cs35l53_0);
+	struct device *i2c0_cs35l53_1 = DEV_PTR(i2c0_cs35l53_1);
+	struct device *i2c7_cs35l53_0 = DEV_PTR(i2c7_cs35l53_0);
+	struct device *i2c7_cs35l53_1 = DEV_PTR(i2c7_cs35l53_1);
+
+	uint32_t board_ver = board_id();
+
+	if (board_ver >= 2) {
+		i2c0_cs35l53_0->enabled = 0;
+		i2c0_cs35l53_1->enabled = 0;
+		i2c7_cs35l53_0->enabled = 1;
+		i2c7_cs35l53_1->enabled = 1;
+	} else {
+		i2c0_cs35l53_0->enabled = 1;
+		i2c0_cs35l53_1->enabled = 1;
+		i2c7_cs35l53_0->enabled = 0;
+		i2c7_cs35l53_1->enabled = 0;
+	}
 }

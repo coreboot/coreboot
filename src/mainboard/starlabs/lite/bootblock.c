ae64b6e5db2e35b45e76ee4ef9ec416a6f09384e
Sean Rhodes <sean@starlabs.systems>
2022-06-14 12:29:40 +0100

    mb/starlabs/lite: Configure MMIO window for EC
    The Nuvoton EC requires a window to be opened for updates, so open
    this window only if the Nuvoton EC is present.
    
    Change-Id: Iaa45aa58749c4d0bfc77e60b52eab2bcb270f3ee
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65130
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/starlabs/lite/bootblock.c
+++ src/mainboard/starlabs/lite/bootblock.c
@@ -2,6 +2,7 @@
 
 #include <bootblock_common.h>
 #include <console/console.h>
+#include <intelblocks/lpc_lib.h>
 #include <soc/gpio.h>
 #include <variants.h>
 
@@ -12,4 +13,7 @@ void bootblock_mainboard_init(void)
 
 	pads = variant_early_gpio_table(&num);
 	gpio_configure_pads(pads, num);
+
+	if (CONFIG(EC_STARLABS_NUVOTON))
+		lpc_open_mmio_window(0xfe800000, 0x10000);
 }

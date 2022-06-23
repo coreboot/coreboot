d3f859d5e530d9fdd56e928247a14b09a256410d
Kshitiz Godara <quic_kgodara@quicinc.com>
2022-06-23 17:18:24 +0530

    soc/qualcomm/sc7180: Update hardware watchdog logging
    Move watchdog functionality to common folder.
    
    BUG=b:221393157
    TEST=None
    
    Signed-off-by: Kshitiz Godara <quic_kgodara@quicinc.com>
    Change-Id: Ib2f7f21ce991fd8193329e7b8260e58e47bf39c4
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65358
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

---
+++ src/soc/qualcomm/common/include/soc/watchdog.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _SOC_QUALCOMM_WDOG_COMMON_H__
+#define _SOC_QUALCOMM_WDOG_COMMON_H__
+
+void check_wdog(void);
+
+#endif  /* _SOC_QUALCOMM_WDOG_COMMON_H__ */
--- src/soc/qualcomm/sc7180/include/soc/watchdog.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-
-#ifndef _SOC_QUALCOMM_SC7180_WDOG_H__
-#define _SOC_QUALCOMM_SC7180_WDOG_H__
-
-void check_wdog(void);
-
-#endif  /* _SOC_QUALCOMM_SC7180_WDOG_H__ */

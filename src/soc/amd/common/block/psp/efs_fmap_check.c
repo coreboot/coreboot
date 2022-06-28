dcbdedd8277af4cb3ce65b908ff9811d925eb40d
Fred Reitberger <reitbergerfred@gmail.com>
2022-06-28 17:13:47 -0400

    soc/amd/common/psp: Revert AMD_SOC_SEPARATE_EFS_SECTION
    Reverting commit 1e25fd426ad8 ("soc/amd/common/block/psp: introduce
    AMD_SOC_SEPARATE_EFS_SECTION").
    
    A better solution was used in commit c17330c1dddb ("mb/amd/chausie: Add
    EC blob into CBFS"), and this is no longer necessary.
    
    TEST: Boot chausie
    
    Signed-off-by: Fred Reitberger <reitbergerfred@gmail.com>
    Change-Id: I27a8622a1f0d871690b181a79adca225a20996ea
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65492
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/soc/amd/common/block/psp/efs_fmap_check.c
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-
-#include <amdblocks/psp_efs.h>
-#include <assert.h>
-#include <fmap_config.h>
-#include <soc/iomap.h>
-
-_Static_assert(FMAP_SECTION_EFS_START == (FLASH_BASE_ADDR + EFS_OFFSET),
-	"FMAP EFS Offset does not match EFS Offset - check your config");

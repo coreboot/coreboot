d4e07090ffb3420f5aa11d8a92dd61c8074412b4
Jon Murphy <jpmurphy@google.com>
2022-06-29 11:56:20 -0600

    soc/amd/sabrina,vc/amd/fsp/sabrina: Add UART support for Sabrina
    Sabrina previously didn't support UART mapping in psp verstage.  Now that it has been enabled, add the relevant uart code here.
    
    BUG=b:218709292
    TEST=Set serial soft fuse, boot to kernel, check logs
    
    Signed-off-by: Jon Murphy <jpmurphy@google.com>
    Change-Id: I591fa69b6e722929839babfff62e9d56c68e1112
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65532
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/sabrina/psp_verstage/uart.c
+++ src/soc/amd/sabrina/psp_verstage/uart.c
@@ -4,8 +4,23 @@
 #include <amdblocks/uart.h>
 #include <types.h>
 
+static void *uart_bars[FCH_UART_ID_MAX];
+
 uintptr_t get_uart_base(unsigned int idx)
 {
-	/* Mapping the UART is not supported. */
-	return 0;
+	uint32_t err;
+
+	if (idx >= ARRAY_SIZE(uart_bars))
+		return 0;
+
+	if (uart_bars[idx])
+		return (uintptr_t)uart_bars[idx];
+
+	err = svc_map_fch_dev(FCH_IO_DEVICE_UART, idx, 0, &uart_bars[idx]);
+	if (err) {
+		svc_debug_print("Failed to map UART\n");
+		return 0;
+	}
+
+	return (uintptr_t)uart_bars[idx];
 }

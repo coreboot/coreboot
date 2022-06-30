9d11cd70810f5879482a10b67007e3b05e754f2d
Bo-Chen Chen <rex-bc.chen@mediatek.com>
2022-06-30 13:40:49 +0800

    mb/google/geralt: Initialize PMICs in romstage
    TEST=build pass
    BUG=b:233720142
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: I71cc69c74dd618f441140790af351095ead3f6f9
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65759
    Reviewed-by: Yidi Lin <yidilin@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/geralt/romstage.c
+++ src/mainboard/google/geralt/romstage.c
@@ -1,8 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <arch/stages.h>
+#include <soc/mt6315.h>
+#include <soc/mt6359p.h>
+#include <soc/pmif.h>
 
 void platform_romstage_main(void)
 {
-	/* TODO: add romstage main function */
+	mtk_pmif_init();
+	mt6315_init();
+	mt6359p_init();
 }

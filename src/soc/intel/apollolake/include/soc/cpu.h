61b66e9a81658d8f178dec1ad003649b2e3139bc
Sean Rhodes <sean@starlabs.systems>
2022-07-06 19:53:22 +0100

    soc/intel/apollolake: Add function to clear MCA in Bank 4
    Change-Id: Ic3b88e336a1cb3be5a9281ce266c7d9fb2465799
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65576
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/apollolake/include/soc/cpu.h
+++ src/soc/intel/apollolake/include/soc/cpu.h
@@ -17,4 +17,14 @@ static inline void flush_l1d_to_l2(void)
 	msr.lo |= FLUSH_DL1_L2;
 	wrmsr(MSR_POWER_MISC, msr);
 }
+
+/* Clear MCA in Bank 4 */
+static inline void clear_mca_bank4(void)
+{
+	msr_t msr;
+	msr.hi = 0;
+	msr.lo = 0;
+	wrmsr(IA32_MC_STATUS(4), msr);
+}
+
 #endif /* _SOC_APOLLOLAKE_CPU_H_ */

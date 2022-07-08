b57d172fbb5265d632c031532fcc2aec156e065a
John Zhao <john.zhao@intel.com>
2022-07-08 16:21:00 -0700

    soc/intel/meteorlake: Align TCSS functions through SBI
    This change aligns the Meteor Lake TCSS functions of pad configuration
    and Thunderbolt authentication through the sideband access.
    
    BUG=b:213574324
    TEST=Build platforms coreboot images successfully.
    
    Change-Id: I393f6e1c7d322878cbb684cd95bfa2477195b23a
    Signed-off-by: John Zhao <john.zhao@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/65724
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>

--- src/soc/intel/meteorlake/tcss.c
+++ src/soc/intel/meteorlake/tcss.c
@@ -3,6 +3,6 @@
 #include <intelblocks/tcss.h>
 
 const struct soc_tcss_ops tcss_ops = {
-	.configure_aux_bias_pads = tcss_configure_aux_bias_pads_regbar,
-	.valid_tbt_auth = tcss_valid_tbt_auth,
+	.configure_aux_bias_pads = ioe_tcss_configure_aux_bias_pads_sbi,
+	.valid_tbt_auth = ioe_tcss_valid_tbt_auth,
 };

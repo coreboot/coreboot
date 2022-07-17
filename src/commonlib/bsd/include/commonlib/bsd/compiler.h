coreboot/coreboot

commit ac136250b26ddcb54d61ed8428f110b607cb3c88

commonlib: Substitude macro "__unused" in compiler.h
Since there are many identifiers whose name contain "__unused" in
headers of musl libc, introducing a macro which expands "__unused" to
the source of a util may have disastrous effect during its compiling
under a musl-based platform.

However, it is hard to detect musl at build time as musl is notorious
for having explicitly been refusing to add a macro like "__MUSL__" to
announce its own presence.

Using __always_unused and __maybe_unused for everything may be a good
idea. This is how it works in the Linux kernel, so that would at least
make us match some other standard rather than doing our own thing
(especially since the other compiler.h shorthand macros are also
inspired by Linux).

Signed-off-by: Bill XIE <persmule@hardenedlinux.org>
Change-Id: I547ae3371d7568f5aed732ceefe0130a339716a9
Reviewed-on: https://review.coreboot.org/c/coreboot/+/65717
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Felix Singer <felixsinger@posteo.net>
Reviewed-by: Julius Werner <jwerner@chromium.org>

src/commonlib/bsd/include/commonlib/bsd/compiler.h
@@ -15,8 +15,21 @@
 #define __aligned(x) __attribute__((__aligned__(x)))
 #endif
 
-#ifndef __unused
-#define __unused __attribute__((__unused__))
+/* Because there may be variables/parameters whose name contains "__unused" in
+   header files of libc, namely musl, these names consistent with ones in the
+   Linux kernel may be a better choice. */
+
+/* This is used to mark identifiers unused in all conditions, e.g. a parameter
+   completely unused in all code branch, only present to fit an API. */
+#ifndef __always_unused
+#define __always_unused __attribute__((__unused__))
+#endif
+
+/* This is used to mark identifiers unused in some conditions, e.g. a parameter
+   only unused in some code branches, a global variable only accessed with code
+   being conditionally preprocessed, etc. */
+#ifndef __maybe_unused
+#define __maybe_unused __attribute__((__unused__))
 #endif
 
 #ifndef __must_check

src/arch/riscv/sbi.c
@@ -59,7 +59,7 @@ void handle_sbi(trapframe *tf)
 {
 	uintptr_t ret = 0;
 	uintptr_t arg0 = tf->gpr[10];
-	__unused uintptr_t arg1 = tf->gpr[11];
+	__maybe_unused uintptr_t arg1 = tf->gpr[11];
 	uintptr_t which = tf->gpr[17];
 
 	switch (which) {

src/commonlib/region.c
@@ -222,7 +222,7 @@ void xlate_window_init(struct xlate_window *window, const struct region_device *
 }
 
 static void *mdev_mmap(const struct region_device *rd, size_t offset,
-			size_t size __unused)
+			size_t size __always_unused)
 {
 	const struct mem_region_device *mdev;
 
@@ -231,8 +231,8 @@ static void *mdev_mmap(const struct region_device *rd, size_t offset,
 	return &mdev->base[offset];
 }
 
-static int mdev_munmap(const struct region_device *rd __unused,
-			void *mapping __unused)
+static int mdev_munmap(const struct region_device *rd __always_unused,
+			void *mapping __always_unused)
 {
 	return 0;
 }
@@ -368,7 +368,8 @@ static void *xlate_mmap(const struct region_device *rd, size_t offset,
 	return rdev_mmap(xlwindow->access_dev, offset, size);
 }
 
-static int xlate_munmap(const struct region_device *rd __unused, void *mapping __unused)
+static int xlate_munmap(const struct region_device *rd __always_unused,
+			void *mapping __always_unused)
 {
 	/*
 	 * xlate_region_device does not keep track of the access device that was used to service

src/drivers/crb/tis.c
@@ -127,7 +127,7 @@ static const char *crb_tpm_acpi_name(const struct device *dev)
 	return "TPM";
 }
 
-static struct device_operations __unused crb_ops = {
+static struct device_operations __maybe_unused crb_ops = {
 	.read_resources = noop_read_resources,
 	.set_resources = noop_set_resources,
 #if CONFIG(HAVE_ACPI_TABLES)

src/drivers/spi/spi_sdcard.c
@@ -210,7 +210,7 @@ static int lookup_response_length(int response_type)
 static int response_resolve(int response_type, uint8_t *response,
 	uint32_t *out_register)
 {
-	__unused static const char * const sd_err[] = {
+	__maybe_unused static const char * const sd_err[] = {
 		"Card is locked",
 		"wp erase skip | lock/unlok cmd failed",
 		"error",

src/ec/google/chromeec/ec.c
@@ -219,7 +219,7 @@ static int google_chromeec_clear_mask(uint8_t type, uint64_t mask)
 						EC_HOST_EVENT_CLEAR, &mask);
 }
 
-static int __unused google_chromeec_set_mask(uint8_t type, uint64_t mask)
+static int google_chromeec_set_mask(uint8_t type, uint64_t mask)
 {
 	if (google_chromeec_is_uhepi_supported())
 		return google_chromeec_uhepi_cmd(type,

src/soc/amd/common/block/lpc/spi_dma.c
@@ -26,7 +26,8 @@ struct spi_dma_transaction {
 	size_t remaining;
 };
 
-static void *spi_dma_mmap(const struct region_device *rd, size_t offset, size_t size __unused)
+static void *spi_dma_mmap(const struct region_device *rd, size_t offset,
+			  size_t size __always_unused)
 {
 	const struct mem_region_device *mdev;
 
@@ -35,7 +36,8 @@ static void *spi_dma_mmap(const struct region_device *rd, size_t offset, size_t 
 	return &mdev->base[offset];
 }
 
-static int spi_dma_munmap(const struct region_device *rd __unused, void *mapping __unused)
+static int spi_dma_munmap(const struct region_device *rd __always_unused,
+			  void *mapping __always_unused)
 {
 	return 0;
 }

src/soc/amd/common/psp_verstage/boot_dev.c
@@ -11,7 +11,8 @@
 
 #define DEST_BUF_ALIGNMENT 16
 
-static void *boot_dev_mmap(const struct region_device *rd, size_t offset, size_t size __unused)
+static void *boot_dev_mmap(const struct region_device *rd, size_t offset,
+			   size_t size __always_unused)
 {
 	const struct mem_region_device *mdev;
 
@@ -20,7 +21,8 @@ static void *boot_dev_mmap(const struct region_device *rd, size_t offset, size_t
 	return &(mdev->base[offset]);
 }
 
-static int boot_dev_munmap(const struct region_device *rd __unused, void *mapping __unused)
+static int boot_dev_munmap(const struct region_device *rd __always_unused,
+			   void *mapping __always_unused)
 {
 	return 0;
 }

commit b6daf297b308961dc22d7ffdcc27d4398ffd6aca

commonlib: compiler.h: Improve wording in comment
It probably was supposed to be *making these names conistent …*, but
short that a little, and add a missing article.

Change-Id: If88ff6d7b0a61aa83d5822b5e1c0b5b4c9d3bb3c
Fixes: ac136250b26d ("commonlib: Substitude macro "__unused" in compiler.h")
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/65884
Reviewed-by: Elyes Haouas <ehaouas@noos.fr>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/commonlib/bsd/include/commonlib/bsd/compiler.h
@@ -16,7 +16,7 @@
 #endif
 
 /* Because there may be variables/parameters whose name contains "__unused" in
-   header files of libc, namely musl, these names consistent with ones in the
+   header files of libc, namely musl, names consistent with the ones in the
    Linux kernel may be a better choice. */
 
 /* This is used to mark identifiers unused in all conditions, e.g. a parameter
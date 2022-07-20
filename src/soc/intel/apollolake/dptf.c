9d1c9ee2121171f38d651ef4ffa8f7eef274b41d
Sean Rhodes <sean@starlabs.systems>
2022-07-20 15:20:27 +0100

    soc/apollolake: Add DPTF HIDs
    Add the HIDs that Windows uses for the DPTF driver.
    
    Change-Id: Ic0cb4a45b5ebaf777a09bed1e5836e8afd873657
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66013
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/soc/intel/apollolake/dptf.c
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <drivers/intel/dptf/dptf.h>
+
+static const struct dptf_platform_info apl_dptf_platform_info = {
+	.use_eisa_hids = true,
+	/* _HID for the toplevel DPTF device, typically \_SB.DPTF */
+	.dptf_device_hid = "INT3400",
+	/* _HID for Intel DPTF Generic Device (these require PTYP as well) */
+	.generic_hid = "INT3403",
+	/* _HID for Intel DPTF Fan Device */
+	.fan_hid = "INT3404",
+};
+
+const struct dptf_platform_info *get_dptf_platform_info(void)
+{
+	return &apl_dptf_platform_info;
+}

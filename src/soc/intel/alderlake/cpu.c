cd6a2ad1b2d3444e815fb9b1a6ec1c98e0403c23
Jeremy Compostella <jeremy.compostella@intel.com>
2022-07-21 14:08:08 -0700

    soc/intel/alderlake: Set Energy Perf Bias appropriate default value
    The current "normal" EPB (six) setting resulted in the desired out of
    box power and performance for several CPU generations.
    
    However, a power and performance analysis on Alder Lake and Raptor
    Lake CPUs demonstrates that this value results in undesirable higher
    uncore power and that seven is a more appropriate value.
    
    Note: the Linux kernel "4ecc933b x86: intel_epb: Allow model specific
          normal EPB value" patch sets the EPB to 7 for Alder Lake.
    
    BRANCH=firmware-brya-14505.B
    BUG=b:239853069
    TEST=verify that EPB is set by coreboot
    
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Change-Id: I5784656903d4c58bedc5063ee3ef310a99711050
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66059
    Reviewed-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Reviewed-by: Cliff Huang <cliff.huang@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/alderlake/cpu.c
+++ src/soc/intel/alderlake/cpu.c
@@ -129,8 +129,9 @@ void soc_core_init(struct device *cpu)
 	/* Enable Direct Cache Access */
 	configure_dca_cap();
 
-	/* Set energy policy */
-	set_energy_perf_bias(ENERGY_POLICY_NORMAL);
+	/* Set energy policy.  The "normal" EPB (6) is not suitable for Alder
+	 * Lake or Raptor Lake CPUs, as this results in higher uncore power. */
+	set_energy_perf_bias(7);
 
 	const config_t *conf = config_of_soc();
 	/* Set energy-performance preference */

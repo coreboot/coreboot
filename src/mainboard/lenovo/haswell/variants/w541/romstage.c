c5f705574615c307e3e098b2fbd396eb3cc84206
Felix Singer <felixsinger@posteo.net>
2022-07-23 21:40:22 +0200

    mb/lenovo: Integrate W541 into haswell mainboard
    Lots of code from lenovo/haswell can be reused for lenovo/w541. Thus,
    integrate it into lenovo/haswell and make it a variant.
    
    Change-Id: If99d842cff777fe27ff63baabc447e69b9d0333c
    Signed-off-by: Felix Singer <felixsinger@posteo.net>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/63514
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Michael Niewöhner <foss@mniewoehner.de>

--- src/mainboard/lenovo/haswell/variants/w541/romstage.c
+++ src/mainboard/lenovo/haswell/variants/w541/romstage.c
@@ -8,33 +8,6 @@
 #include <ec/lenovo/pmh7/pmh7.h>
 #include <device/pci_ops.h>
 
-void mainboard_config_rcba(void)
-{
-	RCBA16(D31IR) = DIR_ROUTE(PIRQA, PIRQD, PIRQC, PIRQA);
-	RCBA16(D29IR) = DIR_ROUTE(PIRQH, PIRQD, PIRQA, PIRQC);
-	RCBA16(D28IR) = DIR_ROUTE(PIRQA, PIRQA, PIRQA, PIRQA);
-	RCBA16(D27IR) = DIR_ROUTE(PIRQG, PIRQB, PIRQC, PIRQD);
-	RCBA16(D26IR) = DIR_ROUTE(PIRQA, PIRQF, PIRQC, PIRQD);
-	RCBA16(D25IR) = DIR_ROUTE(PIRQE, PIRQF, PIRQG, PIRQH);
-	RCBA16(D22IR) = DIR_ROUTE(PIRQA, PIRQB, PIRQC, PIRQD);
-	RCBA16(D20IR) = DIR_ROUTE(PIRQA, PIRQB, PIRQC, PIRQD);
-}
-
-void mb_late_romstage_setup(void)
-{
-	u8 enable_peg = get_uint_option("enable_dual_graphics", 0);
-
-	bool power_en = pmh7_dgpu_power_state();
-
-	if (enable_peg != power_en)
-		pmh7_dgpu_power_enable(!power_en);
-
-	if (!enable_peg) {
-		// Hide disabled dGPU device
-		pci_and_config32(HOST_BRIDGE, DEVEN, ~DEVEN_D1F0EN);
-	}
-}
-
 void mb_get_spd_map(struct spd_info *spdi)
 {
 	spdi->addresses[0] = 0x50;
--- src/mainboard/lenovo/w541/acpi/ec.asl
+++
@@ -1,4 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#include <ec/lenovo/h8/acpi/ec.asl>
-#include <ec/lenovo/h8/acpi/thinkpad_bat_thresholds_b0.asl>

d699de071fee0572971170637fe3bd81dbc463c9
Rex-BC Chen <rex-bc.chen@mediatek.com>
2022-07-25 20:26:52 +0800

    mb/google/geralt: Initialize RTC and clk_buf in romstage
    TEST=build pass.
    BUG=b:233720142
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: I869c0879d09e00cf66882adb728c9ccb6ac57e03
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66183
    Reviewed-by: Yidi Lin <yidilin@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/geralt/romstage.c
+++ src/mainboard/google/geralt/romstage.c
@@ -1,13 +1,17 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <arch/stages.h>
+#include <soc/clkbuf.h>
 #include <soc/mt6315.h>
 #include <soc/mt6359p.h>
 #include <soc/pmif.h>
+#include <soc/rtc.h>
 
 void platform_romstage_main(void)
 {
 	mtk_pmif_init();
 	mt6315_init();
 	mt6359p_init();
+	clk_buf_init();
+	rtc_boot();
 }

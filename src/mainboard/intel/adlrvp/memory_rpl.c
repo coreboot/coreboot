778c7af37ae399cfd72178f4585f3ba626ed84cf
Arthur Heymans <arthur@aheymans.xyz>
2022-07-29 10:55:04 +0200

    mb/intel/adlrvp: Fix expected statement
    Switch cases expect a statement so move the default label.
    
    TEST: With BUILD_TIMELESS=1 binary remains identical.
    
    Change-Id: I9a5d39bb3cbde64f82fc90186b0f2fb64bcde595
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66266
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/intel/adlrvp/memory_rpl.c
+++ src/mainboard/intel/adlrvp/memory_rpl.c
@@ -18,11 +18,11 @@ void rpl_memory_params(FSPM_UPD *memupd)
 	case ADL_P_DDR4_2:
 	case ADL_P_DDR5_1:
 	case ADL_P_DDR5_2:
+	default:
 		return;
 	case ADL_P_LP5_1:
 	case ADL_P_LP5_2:
 		mem_cfg->Lp5BankMode = 1;
 		return;
-	default:
 	}
 }

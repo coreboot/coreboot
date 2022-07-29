85c9a7320f36a9b67fbc79aa19fad333c3e77ef0
Bora Guvendik <bora.guvendik@intel.com>
2022-07-28 20:49:00 -0700

    vendorcode/intel/fsp: Fix wrong license
    Fix the license in header file.
    
    BUG=none
    BRANCH=firmware-brya-14505.B
    TEST=none
    
    Signed-off-by: Bora Guvendik <bora.guvendik@intel.com>
    Change-Id: I025f7c571d09e4cc63a659279e63d17c098c01cf
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66253
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Sridhar Siricilla <sridhar.siricilla@intel.com>

--- src/vendorcode/intel/fsp/fsp2_0/raptorlake/MemInfoHob.h
+++ src/vendorcode/intel/fsp/fsp2_0/raptorlake/MemInfoHob.h
@@ -3,34 +3,15 @@
   Memory S3 Save data, Memory Info data and Memory Platform
   data hobs.
 
-@copyright
-  INTEL CONFIDENTIAL
-  Copyright 1999 - 2021 Intel Corporation.
+  @copyright
+  Copyright (c) 1999 - 2022, Intel Corporation. All rights reserved.<BR>
+  This program and the accompanying materials are licensed and made available under
+  the terms and conditions of the BSD License that accompanies this distribution.
+  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php.
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
 
-  The source code contained or described herein and all documents related to the
-  source code ("Material") are owned by Intel Corporation or its suppliers or
-  licensors. Title to the Material remains with Intel Corporation or its suppliers
-  and licensors. The Material may contain trade secrets and proprietary and
-  confidential information of Intel Corporation and its suppliers and licensors,
-  and is protected by worldwide copyright and trade secret laws and treaty
-  provisions. No part of the Material may be used, copied, reproduced, modified,
-  published, uploaded, posted, transmitted, distributed, or disclosed in any way
-  without Intel's prior express written permission.
-
-  No license under any patent, copyright, trade secret or other intellectual
-  property right is granted to or conferred upon you by disclosure or delivery
-  of the Materials, either expressly, by implication, inducement, estoppel or
-  otherwise. Any license under such intellectual property rights must be
-  express and approved by Intel in writing.
-
-  Unless otherwise agreed by Intel in writing, you may not remove or alter
-  this notice or any other notice embedded in Materials by Intel or
-  Intel's suppliers or licensors in any way.
-
-  This file contains an 'Intel Peripheral Driver' and is uniquely identified as
-  "Intel Reference Module" and is licensed for Intel CPUs and chipsets under
-  the terms of your license agreement with Intel or your vendor. This file may
-  be modified by the user, subject to additional terms of the license agreement.
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 @par Specification Reference:
 **/

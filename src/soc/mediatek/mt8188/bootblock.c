1dcc669aca5c098fc8e407302f8d99c08ea7019f
Rex-BC Chen <rex-bc.chen@mediatek.com>
2022-08-04 15:21:48 +0800

    soc/mediatek/mt8188: Add tracker dump
    Tracker is a debugging tool. When bus timeout occurs, the system will
    reboot and latch some values of tracker registers which could be used
    for debugging.
    
    This function will be triggered only when it encounters the bus
    hanging issue.
    
    TEST=build pass
    BUG=b:236331724
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: I78f676c08ea44e9bb10bd99bbfed70e3e8ece993
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66584
    Reviewed-by: Yidi Lin <yidilin@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/soc/mediatek/mt8188/bootblock.c
+++ src/soc/mediatek/mt8188/bootblock.c
@@ -3,11 +3,13 @@
 #include <bootblock_common.h>
 #include <soc/mmu_operations.h>
 #include <soc/pll.h>
+#include <soc/tracker_common.h>
 #include <soc/wdt.h>
 
 void bootblock_soc_init(void)
 {
 	mtk_mmu_init();
+	bustracker_init();
 	mtk_wdt_init();
 	mt_pll_init();
 }
---
+++ src/soc/mediatek/mt8188/include/soc/tracker.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SOC_MEDIATEK_MT8188_TRACKER_H
+#define SOC_MEDIATEK_MT8188_TRACKER_H
+
+#include <soc/tracker_v2.h>
+
+#endif

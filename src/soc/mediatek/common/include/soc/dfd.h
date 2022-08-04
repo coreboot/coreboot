d6cea76dfa8d0cb65c22ef55c4947e7d1068dbc3
Rex-BC Chen <rex-bc.chen@mediatek.com>
2022-08-04 15:46:33 +0800

    soc/mediatek: Move common definition of DFD to common folder
    We use the same dump address and size for DFD in all MediaTek SoCs, so
    we move them to dfd_common.h and rename dfd_common.h to dfd.h.
    
    TEST=build pass
    BUG=b:236331724
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: I162bbb0a82e3b55c8cfbbd20e28a54ad01fd6b0d
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66585
    Reviewed-by: Yidi Lin <yidilin@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/mediatek/common/include/soc/dfd.h
+++ src/soc/mediatek/common/include/soc/dfd.h
@@ -6,6 +6,10 @@
 #define CPC_FLOW_CTRL_CFG	0x0C53A814
 #define RESET_ON_KEEP_EN	BIT(17)
 
+/* DFD dump address and size need to be the same as defined in Kernel DTS. */
+#define DFD_DUMP_ADDRESS        0x6A000000
+#define DFD_DUMP_SIZE           (1 * MiB)
+
 static u32 *const dfd_cfg = (void *)CPC_FLOW_CTRL_CFG;
 
 void dfd_init(void);

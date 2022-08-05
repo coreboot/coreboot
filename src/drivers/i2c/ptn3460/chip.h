f7319225e95de40e8fc7a8ce1e5442dd03408cd3
Martin Roth <gaumless@gmail.com>
2022-08-04 20:33:53 -0600

    drivers: Add SPDX identifiers to files missing them
    This adds SPDX identifiers to the remaining source files in the
    drivers directory that don't already have them.
    
    Signed-off-by: Martin Roth <martin.roth@amd.corp-partner.google.com>
    Change-Id: I97f96de857515214069c3b77f3c781f7f0555c6e
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66499
    Reviewed-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/drivers/i2c/ptn3460/chip.h
+++ src/drivers/i2c/ptn3460/chip.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
 struct drivers_i2c_ptn3460_config {
 
 };
--- src/drivers/intel/gma/edid.h
+++ src/drivers/intel/gma/edid.h
@@ -1,2 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
 void intel_gmbus_read_edid(u8 *gmbus_mmio, u8 bus, u8 slave, u8 *edid, u32 edid_size);
 void intel_gmbus_stop(u8 *gmbus_mmio);

7a0eff6b8e423ab87403869cda92142baa3de869
Mark Hsieh <mark_hsieh@wistron.corp-partner.google.com>
2022-08-08 17:37:35 +0800

    mb/google/nissa/var/joxer: Add WiFi SAR table
    Add WiFi SAR table for joxer.
    
    BUG=b:239788985
    TEST=build FW and checked SAR table can load by WiFi driver.
    
    Signed-off-by: Mark Hsieh <mark_hsieh@wistron.corp-partner.google.com>
    Change-Id: Ia8dddf454e441840233fa4405704ee1f0a8ed86c
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66522
    Reviewed-by: Reka Norman <rekanorman@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/joxer/variant.c
+++ src/mainboard/google/brya/variants/joxer/variant.c
@@ -4,6 +4,12 @@
 #include <console/console.h>
 #include <fw_config.h>
 #include <soc/bootblock.h>
+#include <sar.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return "wifi_sar_0.hex";
+}
 
 /*
  * TODO(b/229022567): This is a workaround which will be removed once we

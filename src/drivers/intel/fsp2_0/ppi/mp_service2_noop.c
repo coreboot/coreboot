3befdf1161f9ee11646f52e0878c756c146d44a4
Subrata Banik <subratabanik@google.com>
2022-08-14 11:18:47 +0530

    drivers: Implement EFI_PEI_MP_SERVICES_PPI with FSP_UNSUPPORTED type
    This patch implements EFI_PEI_MP_SERVICES_PPI structure definitions
    with APIs that return mp_api_unsupported().
    
    The reason behind this change is to fix an FSP issue where FSP assumes
    ownership of the APs (Application Processors) upon passing a `NULL`
    pointer to the CpuMpPpi FSP-S UPD.Hence, this patch implements
    `MP_SERVICES_PPI_DEFAULT` config to fill EFI_PEI_MP_SERVICES_PPI with
    `mp_api_unsupported` APIs.
    
    Later this data structure can be passed to the CpuMpPpi UPD to avoid
    APs from getting hijacked by FSP while coreboot decides to set
    SkipMpInit UPD.
    
    TEST=Able to build and boot Google/Taeko with this patch.
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: I31fcaa2aa633071b6d6bfa05dbe891ef87978d2c
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66708
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

---
+++ src/drivers/intel/fsp2_0/ppi/mp_service2_noop.c
@@ -0,0 +1,89 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <fsp/api.h>
+#include <fsp/ppi/mp_service_ppi.h>
+#include <Ppi/MpServices2.h>
+
+typedef EDKII_PEI_MP_SERVICES2_PPI efi_pei_mp_services_ppi;
+
+static efi_return_status_t mps2_noop_get_number_of_processors(
+	efi_pei_mp_services_ppi *ignored1,
+	efi_uintn_t *ignored2,
+	efi_uintn_t *ignored3)
+{
+	return mp_api_unsupported();
+}
+
+static efi_return_status_t mps2_noop_get_processor_info(
+	efi_pei_mp_services_ppi *ignored1,
+	efi_uintn_t ignored2,
+	efi_processor_information *ignored3)
+{
+	return mp_api_unsupported();
+}
+
+static efi_return_status_t mps2_noop_startup_all_aps(
+	efi_pei_mp_services_ppi *ignored1,
+	efi_ap_procedure ignored2, efi_boolean_t ignored3,
+	efi_uintn_t ignored4, void *ignored5)
+{
+	return mp_api_unsupported();
+}
+
+static efi_return_status_t mps2_noop_startup_all_cpus(
+	efi_pei_mp_services_ppi *ignored1,
+	efi_ap_procedure ignored2,
+	efi_uintn_t ignored3, void *ignored4)
+{
+	return mp_api_unsupported();
+}
+
+static efi_return_status_t mps2_noop_startup_this_ap(
+	efi_pei_mp_services_ppi *ignored1,
+	efi_ap_procedure ignored2, efi_uintn_t ignored3,
+	efi_uintn_t ignored4, void *ignored5)
+{
+	return mp_api_unsupported();
+}
+
+static efi_return_status_t mps2_noop_switch_bsp(
+	efi_pei_mp_services_ppi *ignored1, efi_uintn_t ignored2,
+	efi_boolean_t ignored3)
+{
+	return mp_api_unsupported();
+}
+
+static efi_return_status_t mps2_noop_enable_disable_ap(
+	efi_pei_mp_services_ppi *ignored1,
+	efi_uintn_t ignored2, efi_boolean_t ignored3, efi_uint32_t *ignored4)
+{
+	return mp_api_unsupported();
+}
+
+static efi_return_status_t mps2_noop_identify_processor(
+	efi_pei_mp_services_ppi *ignored1,
+	efi_uintn_t *ignored2)
+{
+	return mp_api_unsupported();
+}
+
+/*
+ * Default MP Services data structure adhering to the EDK2 UEFIPKG Open
+ * Source version 2 specification
+ */
+
+static efi_pei_mp_services_ppi mp_service2_noop_ppi = {
+	mps2_noop_get_number_of_processors,
+	mps2_noop_get_processor_info,
+	mps2_noop_startup_all_aps,
+	mps2_noop_startup_this_ap,
+	mps2_noop_switch_bsp,
+	mps2_noop_enable_disable_ap,
+	mps2_noop_identify_processor,
+	mps2_noop_startup_all_cpus,
+};
+
+void *mp_fill_ppi_services_data(void)
+{
+	return (void *)&mp_service2_noop_ppi;
+}

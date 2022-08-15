38155a1549a177145d5d4a52552d58d20db9d17c
Stanley Wu <stanley1.wu@lcfc.corp-partner.google.com>
2022-08-15 16:51:11 +0800

    mb/google/nissa/var/pujjo: Add FW_CONFIG probe for supported devices
    Add FW_CONFIG probe based on pujjoteen boxster of below devices:
    LTE, SD card, stylus, WFC camera, AUDIO
    
    BUG=b:236158122
    TEST=Boot to OS and verify that above devices are set based on
    fw_cofnig.
    
    Signed-off-by: Stanley Wu <stanley1.wu@lcfc.corp-partner.google.com>
    Change-Id: I49fc5461e7affba68a6b89bf166c84598fbfa088
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66741
    Reviewed-by: Kangheui Won <khwon@chromium.org>
    Reviewed-by: Reka Norman <rekanorman@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/pujjo/Makefile.inc
+++ src/mainboard/google/brya/variants/pujjo/Makefile.inc
@@ -3,4 +3,5 @@ bootblock-y += gpio.c
 
 romstage-y += gpio.c
 
+ramstage-$(CONFIG_FW_CONFIG) += fw_config.c
 ramstage-y += gpio.c
---
+++ src/mainboard/google/brya/variants/pujjo/fw_config.c
@@ -0,0 +1,51 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <console/console.h>
+#include <fw_config.h>
+
+static const struct pad_config lte_enable_pads[] = {
+	/* A8  : WWAN_RF_DISABLE_ODL */
+	PAD_CFG_GPO(GPP_A8, 1, DEEP),
+	/* H19 : SOC_I2C_SUB_INT_ODL */
+	PAD_CFG_GPI_APIC(GPP_H19, NONE, PLTRST, LEVEL, NONE),
+	/* H23 : WWAN_SAR_DETECT_ODL */
+	PAD_CFG_GPO(GPP_H23, 1, DEEP),
+};
+
+static const struct pad_config lte_disable_pads[] = {
+	/* D6  : WWAN_EN */
+	PAD_NC(GPP_D6, NONE),
+	/* F12 : WWAN_RST_L */
+	PAD_NC_LOCK(GPP_F12, NONE, LOCK_CONFIG),
+};
+
+static const struct pad_config sd_disable_pads[] = {
+	/* D8  : SD_CLKREQ_ODL */
+	PAD_NC(GPP_D8, NONE),
+	/* H12 : SD_PERST_L */
+	PAD_NC_LOCK(GPP_H12, NONE, LOCK_CONFIG),
+	/* H13 : EN_PP3300_SD_X */
+	PAD_NC_LOCK(GPP_H13, NONE, LOCK_CONFIG),
+};
+
+void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
+{
+	if (fw_config_probe(FW_CONFIG(LTE, LTE_PRESENT))) {
+		printk(BIOS_INFO, "Enable LTE-related GPIO pins.\n");
+		gpio_padbased_override(padbased_table, lte_enable_pads,
+						ARRAY_SIZE(lte_enable_pads));
+	} else {
+		printk(BIOS_INFO, "Disable LTE-related GPIO pins.\n");
+		gpio_padbased_override(padbased_table, lte_disable_pads,
+						ARRAY_SIZE(lte_disable_pads));
+	}
+
+	if (fw_config_probe(FW_CONFIG(SD_CARD, SD_ABSENT))) {
+		printk(BIOS_INFO, "Disable SD card GPIO pins.\n");
+		gpio_padbased_override(padbased_table, sd_disable_pads,
+						ARRAY_SIZE(sd_disable_pads));
+	}
+
+}

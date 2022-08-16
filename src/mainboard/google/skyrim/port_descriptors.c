9c63fd5ad28a6b32397d61bcfc5f5779c10c794a
Felix Held <felix-coreboot@felixheld.de>
2022-08-17 00:28:46 +0200

    mb/google/skyrim/port_descriptors: replace sbna acronym with mdn
    Since the SoC that was upstreamed as Sabrina was finally renamed to
    Mendocino, also adjust the abbreviation used for the DXIO/DDI descriptor
    struct array names.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I14ecf98e4a94376a70e783774c8f7b8701581220
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66815
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/skyrim/port_descriptors.c
+++ src/mainboard/google/skyrim/port_descriptors.c
@@ -5,7 +5,7 @@
 #include <soc/platform_descriptors.h>
 #include <types.h>
 
-static const fsp_dxio_descriptor skyrim_sbna_dxio_descriptors[] = {
+static const fsp_dxio_descriptor skyrim_mdn_dxio_descriptors[] = {
 	{ /* WLAN */
 		.engine_type = PCIE_ENGINE,
 		.port_present = true,
@@ -47,7 +47,7 @@ static const fsp_dxio_descriptor skyrim_sbna_dxio_descriptors[] = {
 	},
 };
 
-static const fsp_ddi_descriptor skyrim_sbna_ddi_descriptors[] = {
+static const fsp_ddi_descriptor skyrim_mdn_ddi_descriptors[] = {
 	{ /* DDI0 - eDP */
 		.connector_type = DDI_EDP,
 		.aux_index = DDI_AUX1,
@@ -79,8 +79,8 @@ void mainboard_get_dxio_ddi_descriptors(
 		const fsp_dxio_descriptor **dxio_descs, size_t *dxio_num,
 		const fsp_ddi_descriptor **ddi_descs, size_t *ddi_num)
 {
-	*dxio_descs = skyrim_sbna_dxio_descriptors;
-	*dxio_num = ARRAY_SIZE(skyrim_sbna_dxio_descriptors);
-	*ddi_descs = skyrim_sbna_ddi_descriptors;
-	*ddi_num = ARRAY_SIZE(skyrim_sbna_ddi_descriptors);
+	*dxio_descs = skyrim_mdn_dxio_descriptors;
+	*dxio_num = ARRAY_SIZE(skyrim_mdn_dxio_descriptors);
+	*ddi_descs = skyrim_mdn_ddi_descriptors;
+	*ddi_num = ARRAY_SIZE(skyrim_mdn_ddi_descriptors);
 }

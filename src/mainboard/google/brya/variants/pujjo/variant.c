50b45d35f097d017e18e27d43c65f7e458df7e4e
Leo Chou <leo.chou@lcfc.corp-partner.google.com>
2022-08-18 18:17:57 +0800

    mb/google/nissa/var/pujjo: Add FW_CONFIG probe for Pujjoteen disable bypass power
    Add FW_CONFIG probe to separate ext fivr settings for Pujjoteen
    and others(Pujjo and Pujjoflex)
    
    BUG=b:242663554
    TEST=Boot to OS and verify that ext_fivr_settings are set based on
    fw_config.
    
    Signed-off-by: Leo Chou <leo.chou@lcfc.corp-partner.google.com>
    Change-Id: I6bb6d1701c55459cf331dd2f3ffe07f91bca2fa5
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/66863
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kangheui Won <khwon@chromium.org>
    Reviewed-by: Reka Norman <rekanorman@chromium.org>

---
+++ src/mainboard/google/brya/variants/pujjo/variant.c
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <fw_config.h>
+#include <baseboard/variants.h>
+
+void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
+{
+	// Configure external V1P05/Vnn/VnnSx Rails for Pujjo, Pujjoflex
+	if (fw_config_probe(FW_CONFIG(EXT_VR, EXT_VR_PRESENT))) {
+		config->ext_fivr_settings.configure_ext_fivr = 1;
+
+		config->ext_fivr_settings.v1p05_enable_bitmap =
+				FIVR_ENABLE_ALL_SX;
+
+		config->ext_fivr_settings.vnn_enable_bitmap =
+				FIVR_ENABLE_ALL_SX;
+
+		config->ext_fivr_settings.vnn_sx_enable_bitmap =
+				FIVR_ENABLE_ALL_SX;
+
+		config->ext_fivr_settings.v1p05_supported_voltage_bitmap =
+				FIVR_VOLTAGE_NORMAL;
+
+		config->ext_fivr_settings.vnn_supported_voltage_bitmap =
+				FIVR_VOLTAGE_MIN_ACTIVE;
+
+		config->ext_fivr_settings.v1p05_voltage_mv = 1050;
+		config->ext_fivr_settings.vnn_voltage_mv = 780;
+		config->ext_fivr_settings.vnn_sx_voltage_mv = 1050;
+		config->ext_fivr_settings.v1p05_icc_max_ma = 500;
+		config->ext_fivr_settings.vnn_icc_max_ma = 500;
+	}
+}

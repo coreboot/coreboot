be738e5d242f81a59569305519b837c02a869eb1
Tarun Tuli <taruntuli@google.com>
2022-08-25 17:59:01 -0400

    mb/google/rex: Add mapping for EC_SYNC_IRQ
    The define EC_SYNC_IRQ needs to be mapped to A17 based on
    the Rex schematics 24/6/2022.
    
    BUG=b:243781237
    TEST=Successfully build rex and tested to ensure EC is now functional.
    
    Signed-off-by: Tarun Tuli <taruntuli@google.com>
    
    Change-Id: Ib61ddc9f73dd7b817d3b990bef8f0169f7cafbcd
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/67082
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/mainboard/google/rex/variants/baseboard/rex/include/baseboard/gpio.h
+++ src/mainboard/google/rex/variants/baseboard/rex/include/baseboard/gpio.h
@@ -12,7 +12,7 @@
 /* EC in RW or RO */
 #define GPIO_EC_IN_RW	0
 /* GPIO IRQ for tight timestamps / wake support */
-#define EC_SYNC_IRQ	0
+#define EC_SYNC_IRQ	GPP_A17_IRQ
 /* eSPI virtual wire reporting */
 #define EC_SCI_GPI	GPE0_ESPI
 /* EC wake is LAN_WAKE# which is a special DeepSX wake pin */

a53772c5d6d0a25799938cc6f9999e1163e70996
EricKY Cheng <ericky_cheng@compal.corp-partner.google.com>
2022-08-30 19:54:57 +0800

    mb/google/skyrim/var/winterhold: Add gpio override settings
    Follow FT6_SOC_GPIO_PM&Strap_20220815A.XLSX
    update Gpio setting
    
    BUG=b:240824497
    BRANCH=None
    
    Signed-off-by: EricKY Cheng <ericky_cheng@compal.corp-partner.google.com>
    Change-Id: I2086c326cbf46ba6378d18d37dcbbe9fafa6b2bc
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/67209
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Frank Wu <frank_wu@compal.corp-partner.google.com>
    Reviewed-by: Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

---
+++ src/mainboard/google/skyrim/variants/winterhold/gpio.c
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <commonlib/helpers.h>
+#include <soc/gpio.h>
+
+/* GPIO configuration in ramstage */
+static const struct soc_amd_gpio override_gpio_table[] = {
+
+	/* SOC_PEN_DETECT_ODL */
+	PAD_NC(GPIO_3),
+
+	/* EN_PWR_WWAN_X */
+	PAD_NC(GPIO_8),
+
+	/* SD_AUX_RST_SOC_L */
+	PAD_NC(GPIO_27),
+
+	/* WWAN_RST_L */
+	PAD_NC(GPIO_42),
+
+};
+
+void variant_override_gpio_table(const struct soc_amd_gpio **gpio, size_t *size)
+{
+	*size = ARRAY_SIZE(override_gpio_table);
+	*gpio = override_gpio_table;
+}

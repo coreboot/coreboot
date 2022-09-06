bc18fb3e1a47dbed229d3737b2200246fd46e7ee
Bo-Chen Chen <rex-bc.chen@mediatek.com>
2022-09-06 14:41:57 +0800

    mb/google/geralt: Pass reset gpio parameter to BL31
    Pass the reset gpio parameter to BL31 to support SoC reset.
    
    TEST=build pass.
    BUG=b:233720142
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: Ifdfbd6bd82f64b084f6349cb617443053c89a3f1
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/67357
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/geralt/mainboard.c
+++ src/mainboard/google/geralt/mainboard.c
@@ -1,14 +1,20 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <device/device.h>
+#include <soc/bl31.h>
 #include <soc/msdc.h>
 #include <soc/usb.h>
+
+#include "gpio.h"
 
 static void mainboard_init(struct device *dev)
 {
 	mtk_msdc_configure_emmc(true);
 	mtk_msdc_configure_sdcard();
 	setup_usb_host();
+
+	if (CONFIG(ARM64_USE_ARM_TRUSTED_FIRMWARE))
+		register_reset_to_bl31(GPIO_AP_EC_WARM_RST_REQ.id, true);
 }
 
 static void mainboard_enable(struct device *dev)

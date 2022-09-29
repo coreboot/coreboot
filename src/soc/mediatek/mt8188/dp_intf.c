8a604bdbae9d5ddb1ba4ae699d978ebb38746663
Bo-Chen Chen <rex-bc.chen@mediatek.com>
2022-09-29 17:31:23 +0800

    soc/mediatek/mt8188: Add eDP support for firmware display
    MT8188 supports eDP as internal display interface.
    
    BUG=b:244208960
    TEST=emerge-geralt coreboot.
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: I6441a36557b097e041bc081b907eb60b56c9fbe6
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/68488
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-by: Yidi Lin <yidilin@google.com>

---
+++ src/soc/mediatek/mt8188/dp_intf.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR MIT */
+
+#include <soc/addressmap.h>
+#include <soc/dp_intf.h>
+
+const struct mtk_dpintf dpintf_data = {
+	.regs = (void *)(DP_INTF0_BASE),
+	.color_format = MTK_DPINTF_COLOR_FORMAT_RGB,
+	.yc_map = MTK_DPINTF_OUT_YC_MAP_RGB,
+	.bit_num = MTK_DPINTF_OUT_BIT_NUM_8BITS,
+	.channel_swap = MTK_DPINTF_OUT_CHANNEL_SWAP_RGB,
+	.input_mode = MTK_DPINTF_INPUT_MODE_1P,
+};

45ad511a8acba8a6ed4b9757ca39fd55ff613a30
Matt DeVillier <matt.devillier@gmail.com>
2022-10-02 13:05:15 -0500

    mb/google/reef: Demote NHLT log messages from error to info
    Change-Id: I1c00322d2b22e1fafffd6ebd66f2df14bcedbd89
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/68461
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/reef/variants/baseboard/nhlt.c
+++ src/mainboard/google/reef/variants/baseboard/nhlt.c
@@ -12,23 +12,23 @@ void __weak variant_nhlt_init(struct nhlt *nhlt)
 	/* 1-dmic configuration */
 	if (CONFIG(NHLT_DMIC_1CH_16B) &&
 			(!nhlt_soc_add_dmic_array(nhlt, 1)))
-		printk(BIOS_ERR, "Added 1CH DMIC array.\n");
+		printk(BIOS_INFO, "Added 1CH DMIC array.\n");
 	/* 2-dmic configuration */
 	if (CONFIG(NHLT_DMIC_2CH_16B) &&
 			(!nhlt_soc_add_dmic_array(nhlt, 2)))
-		printk(BIOS_ERR, "Added 2CH DMIC array.\n");
+		printk(BIOS_INFO, "Added 2CH DMIC array.\n");
 	/* 4-dmic configuration */
 	if (CONFIG(NHLT_DMIC_4CH_16B) &&
 			(!nhlt_soc_add_dmic_array(nhlt, 4)))
-		printk(BIOS_ERR, "Added 4CH DMIC array.\n");
+		printk(BIOS_INFO, "Added 4CH DMIC array.\n");
 	/* Dialog for Headset codec.
 	 * Headset codec is bi-directional but uses the same configuration
 	 * settings for render and capture endpoints.
 	 */
 	if (!nhlt_soc_add_da7219(nhlt, AUDIO_LINK_SSP1))
-		printk(BIOS_ERR, "Added Dialog_7219 codec.\n");
+		printk(BIOS_INFO, "Added Dialog_7219 codec.\n");
 
 	/* MAXIM Smart Amps for left and right speakers. */
 	if (!nhlt_soc_add_max98357(nhlt, AUDIO_LINK_SSP5))
-		printk(BIOS_ERR, "Added Maxim_98357 codec.\n");
+		printk(BIOS_INFO, "Added Maxim_98357 codec.\n");
 }

7b7b8e48d8a70216863e8e5331cbee05cab7f48b
Elyes Haouas <ehaouas@noos.fr>
2022-10-02 12:42:54 +0200

    cpu/intel/common/fsb.c: Sorte includes and add <stdint.h>
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Change-Id: I9b85836ac21da5b885a97f05e3973fb23a052fd5
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/68041
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin L Roth <gaumless@gmail.com>

--- src/cpu/intel/common/fsb.c
+++ src/cpu/intel/common/fsb.c
@@ -1,13 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <arch/cpu.h>
+#include <commonlib/helpers.h>
+#include <console/console.h>
+#include <cpu/intel/fsb.h>
+#include <cpu/intel/speedstep.h>
 #include <cpu/x86/msr.h>
 #include <cpu/x86/tsc.h>
-#include <cpu/intel/speedstep.h>
-#include <cpu/intel/fsb.h>
-#include <console/console.h>
-#include <commonlib/helpers.h>
 #include <delay.h>
+#include <stdint.h>
 
 static u32 timer_fsb;
 static u32 timer_tsc;

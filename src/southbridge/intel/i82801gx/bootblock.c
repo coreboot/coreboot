b6ad6b66a85e68eaefa1cd400650531e00f25f4b
Elyes Haouas <ehaouas@noos.fr>
2022-10-03 11:07:45 +0200

    sb/intel/i82801gx/bootblock.c: Clean up includes
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Change-Id: I61d4a188dc9526b71277c05dd317255fc9727414
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/68080
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin L Roth <gaumless@gmail.com>

--- src/southbridge/intel/i82801gx/bootblock.c
+++ src/southbridge/intel/i82801gx/bootblock.c
@@ -1,8 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <arch/bootblock.h>
-#include <device/pci_ops.h>
 #include <southbridge/intel/common/early_spi.h>
+#include <southbridge/intel/common/rcba.h>
+
 #include "i82801gx.h"
 
 void bootblock_early_southbridge_init(void)

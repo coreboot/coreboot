b3dcb96dc57ade665306e9c03f41353fd11737aa
Arthur Heymans <arthur@aheymans.xyz>
2022-10-05 21:54:29 +0200

    soc/amd/*: Hook up IOMMU ops in devicetree
    This removed the need to maintain a PCI driver.
    
    Change-Id: I43def81d615749008fcc9de8734fa2aca752aa9d
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/68146
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martin.roth@amd.corp-partner.google.com>

--- src/soc/amd/common/block/iommu/iommu.c
+++ src/soc/amd/common/block/iommu/iommu.c
@@ -2,7 +2,6 @@
 
 #include <device/device.h>
 #include <device/pci.h>
-#include <device/pci_ids.h>
 #include <lib.h>
 
 static void iommu_read_resources(struct device *dev)
@@ -28,7 +27,7 @@ static const char *iommu_acpi_name(const struct device *dev)
 }
 #endif
 
-static struct device_operations iommu_ops = {
+struct device_operations amd_iommu_ops = {
 	.read_resources = iommu_read_resources,
 	.set_resources = pci_dev_set_resources,
 	.enable_resources = pci_dev_enable_resources,
@@ -36,19 +35,4 @@ static struct device_operations iommu_ops = {
 #if CONFIG(HAVE_ACPI_TABLES)
 	.acpi_name = iommu_acpi_name,
 #endif
-};
-
-static const unsigned short pci_device_ids[] = {
-	PCI_DID_AMD_15H_MODEL_303F_NB_IOMMU,
-	PCI_DID_AMD_15H_MODEL_707F_NB_IOMMU,
-	PCI_DID_AMD_17H_MODEL_1020_NB_IOMMU,
-	PCI_DID_AMD_17H_MODEL_606F_NB_IOMMU,
-	PCI_DID_AMD_17H_MODEL_A0AF_NB_IOMMU,
-	0
-};
-
-static const struct pci_driver iommu_driver __pci_driver = {
-	.ops = &iommu_ops,
-	.vendor = PCI_VID_AMD,
-	.devices = pci_device_ids,
 };

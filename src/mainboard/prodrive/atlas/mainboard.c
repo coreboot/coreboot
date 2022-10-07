97a86734d2d148def0e4ac52447d534a42cab5d8
Maximilian Brune <maximilian.brune@9elements.com>
2022-10-07 18:29:29 +0200

    mb/prodrive/atlas: Print HSID
    Signed-off-by: Maximilian Brune <maximilian.brune@9elements.com>
    Change-Id: Ibb7aac1204bc297d16797cac5b32b119d0a9204b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/68224
    Reviewed-by: Lean Sheng Tan <sheng.tan@9elements.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/prodrive/atlas/mainboard.c
+++ src/mainboard/prodrive/atlas/mainboard.c
@@ -1,13 +1,28 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <device/device.h>
+#include <console/console.h>
 #include <stdint.h>
+#include <gpio.h>
+#include <arch/io.h>
 
 #include "gpio.h"
+
+static uint8_t get_hsid(void)
+{
+	const gpio_t hsid_gpios[] = {
+		GPP_A8,
+		GPP_F19,
+		GPP_H23,
+		GPP_H19,
+	};
+	return gpio_base2_value(hsid_gpios, ARRAY_SIZE(hsid_gpios));
+}
 
 static void mainboard_init(void *chip_info)
 {
 	configure_gpio_pads();
+	printk(BIOS_INFO, "HSID: 0x%x\n", get_hsid());
 }
 
 struct chip_operations mainboard_ops = {

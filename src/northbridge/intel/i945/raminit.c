coreboot/coreboot

commit e8bb6d2b16acbe4c583905d17c32172938006100

nb/intel/i945/raminit.c: Fix formatted print
Change-Id: I7122988a1c88175a2e72c11bb95bfa434ce48ff2
Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/68104
Reviewed-by: Angel Pons <th3fanbus@gmail.com>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/northbridge/intel/i945/raminit.c
@@ -74,7 +74,7 @@ static __attribute__((noinline)) void do_ram_command(u32 command)
 
 static void ram_read32(uintptr_t offset)
 {
-	PRINTK_DEBUG("   RAM read: %08x\n", offset);
+	PRINTK_DEBUG("   RAM read: %" PRIxPTR "\n", offset);
 
 	read32p(offset);
 }

commit 8917ab4346034c1794f0486aee4e6ba16684f283

nb/intel/i945/raminit: Include `inttypes.h` for PRIxPTR
Selecting *Output verbose RAM init debug messages*
(`CONFIG_DEBUG_RAM_SETUP=y`) the build fails due to the missing header.

        CC         romstage/northbridge/intel/i945/raminit.o
    src/northbridge/intel/i945/raminit.c: In function 'ram_read32':
    src/northbridge/intel/i945/raminit.c:77:39: error: expected ')' before 'PRIxPTR'
       77 |         PRINTK_DEBUG("   RAM read: %" PRIxPTR "\n", offset);
          |                                       ^~~~~~~
    src/northbridge/intel/i945/raminit.c:25:52: note: in definition of macro 'PRINTK_DEBUG'
       25 | #define PRINTK_DEBUG(x...)      printk(BIOS_DEBUG, x)
          |                                                    ^
    src/northbridge/intel/i945/raminit.c:22:1: note: 'PRIxPTR' is defined in header '<inttypes.h>'; did you forget to '#include <inttypes.h>'?
       21 | #include "chip.h"
      +++ |+#include <inttypes.h>
       22 |
    src/northbridge/intel/i945/raminit.c:25:39: note: to match this '('
       25 | #define PRINTK_DEBUG(x...)      printk(BIOS_DEBUG, x)
          |                                       ^
    src/northbridge/intel/i945/raminit.c:77:9: note: in expansion of macro 'PRINTK_DEBUG'
       77 |         PRINTK_DEBUG("   RAM read: %" PRIxPTR "\n", offset);
          |         ^~~~~~~~~~~~
    src/northbridge/intel/i945/raminit.c:77:22: error: spurious trailing '%' in format [-Werror=format=]
       77 |         PRINTK_DEBUG("   RAM read: %" PRIxPTR "\n", offset);
          |                      ^~~~~~~~~~~~~~~~
    src/northbridge/intel/i945/raminit.c:25:52: note: in definition of macro 'PRINTK_DEBUG'
       25 | #define PRINTK_DEBUG(x...)      printk(BIOS_DEBUG, x)
          |                                                    ^
    src/northbridge/intel/i945/raminit.c:77:36: note: format string is defined here
       77 |         PRINTK_DEBUG("   RAM read: %" PRIxPTR "\n", offset);
          |                                    ^

Include `inttypes.h` to fix it.

Fixes: e8bb6d2b16ac ("Output verbose RAM init debug messages")

src/northbridge/intel/i945/raminit.c
@@ -9,6 +9,7 @@
 #include <device/pci_ops.h>
 #include <device/pci_type.h>
 #include <device/smbus_host.h>
+#include <inttypes.h>
 #include <lib.h>
 #include <pc80/mc146818rtc.h>
 #include <spd.h>
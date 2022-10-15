0923c6244845d2a3742e85313771c44e0f50d561
Matt DeVillier <matt.devillier@gmail.com>
2022-10-15 12:04:08 -0500

    util/chromeos/extract_blobs: try using RW_MAIN_A region first
    Since the RW firmware may contain newer/additional blobs than the
    RO COREBOOT region, try using it first, then fall back to
    COREBOOT and eventually BOOT_STUB if necessary.
    
    TEST=extract blobs from dedede and brya firmware images
    
    Change-Id: Ia01b37f8c410685de8a17ea4105ca671931a47c5
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/68453
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martin.roth@amd.corp-partner.google.com>

--- util/chromeos/extract_blobs.sh
+++ util/chromeos/extract_blobs.sh
@@ -33,20 +33,24 @@ if [[ "$IFDTOOL" = "" ]]; then
 	exit 1
 fi
 
-# ensure valid coreboot image / get list of main COREBOOT CBFS contents
-REGION=""
-if ! $CBFSTOOL $IMAGE print >$DIR/cbfs.txt 2>/dev/null; then
-	# try using BOOT_STUB region
-	if ! $CBFSTOOL $IMAGE print -r BOOT_STUB >$DIR/cbfs.txt; then
-		echo "Error reading CBFS: $IMAGE is not a valid coreboot image"
-		exit 1
-	else
-		REGION="-r BOOT_STUB"
-	fi
+# ensure valid coreboot image / get list of CBFS contents
+# try using RW_MAIN_A region first as it may contain newer
+# files / files not present in the COREBOOT region
+if $CBFSTOOL $IMAGE print -r FW_MAIN_A >$DIR/cbfs.txt 2>/dev/null; then
+	REGION="FW_MAIN_A"
+elif $CBFSTOOL $IMAGE print -r COREBOOT >$DIR/cbfs.txt; then
+	# use COREBOOT region
+	REGION="COREBOOT"
+elif $CBFSTOOL $IMAGE print -r BOOT_STUB >$DIR/cbfs.txt; then
+	# use BOOT_STUB region
+	REGION="BOOT_STUB"
+else
+	echo "Error reading CBFS: $IMAGE is not a valid coreboot image"
+	exit 1
 fi
 
 echo ""
-echo "Extracting blobs..."
+echo "Extracting blobs from region $REGION..."
 echo ""
 
 # extract flash regions
@@ -60,39 +64,39 @@ mv flashregion_0_flashdescriptor.bin $DIR/flashdescriptor.bin
 rm flashregion_*.bin
 
 # extract microcode
-$CBFSTOOL $IMAGE extract $REGION -n cpu_microcode_blob.bin -f $DIR/cpu_microcode_blob.bin
+$CBFSTOOL $IMAGE extract -r $REGION -n cpu_microcode_blob.bin -f $DIR/cpu_microcode_blob.bin
 
 # extract VGA BIOS
 VGA=$(grep pci $DIR/cbfs.txt | cut -f1 -d\ )
 if [ "$VGA" != "" ]; then
-	$CBFSTOOL $IMAGE extract $REGION -n $VGA -f $DIR/vgabios.bin
+	$CBFSTOOL $IMAGE extract -r $REGION -n $VGA -f $DIR/vgabios.bin
 fi
 
 # extract MRC.bin
 MRC=$(grep mrc.bin $DIR/cbfs.txt | cut -f1 -d\ )
 if [ "$MRC" != "" ]; then
-	$CBFSTOOL $IMAGE extract $REGION -n "$MRC" -f "$DIR/$MRC"
+	$CBFSTOOL $IMAGE extract -r $REGION -n "$MRC" -f "$DIR/$MRC"
 fi
 
 # extract refcode
 REF=$(grep refcode $DIR/cbfs.txt | cut -f1 -d\ )
 if [ "$REF" != "" ]; then
-	$CBFSTOOL $IMAGE extract $REGION -n fallback/refcode -f "$DIR/refcode.elf" -m x86
+	$CBFSTOOL $IMAGE extract -r $REGION -n fallback/refcode -f "$DIR/refcode.elf" -m x86
 fi
 
 # extract FSP blobs
 for FSP in $(grep fsp $DIR/cbfs.txt | cut -f1 -d\ ); do
-	$CBFSTOOL $IMAGE extract $REGION -n $FSP -f $DIR/$FSP
+	$CBFSTOOL $IMAGE extract -r $REGION -n $FSP -f $DIR/$FSP
 done
 
 # extract audio blobs
 for AUD in $(grep -e "-2ch-" -e "-4ch-" $DIR/cbfs.txt | cut -f1 -d\ ); do
-	$CBFSTOOL $IMAGE extract $REGION -n $AUD -f $DIR/$AUD
+	$CBFSTOOL $IMAGE extract -r $REGION -n $AUD -f $DIR/$AUD
 done
 
 # extract VBTs
 for VBT in $(grep vbt $DIR/cbfs.txt | cut -f1 -d\ ); do
-	$CBFSTOOL $IMAGE extract $REGION -n $VBT -f $DIR/$VBT
+	$CBFSTOOL $IMAGE extract -r $REGION -n $VBT -f $DIR/$VBT
 done
 
 # extract IFWI

6c6c35f20cfa105852f717db7284a2a464791b8c
Felix Held <felix-coreboot@felixheld.de>
2022-10-18 16:25:31 +0200

    soc/amd/picasso/uart: separate data and code
    The goal of this is to be able to move most of the code over to the
    common AMD blocks.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: Ia496a4b29b25d4438ed8fc09bfe6f83e3fb768d9
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/68524
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Fred Reitberger <reitbergerfred@gmail.com>
    Reviewed-by: Jason Glenesk <jason.glenesk@amd.corp-partner.google.com>

--- src/soc/amd/picasso/uart.c
+++ src/soc/amd/picasso/uart.c
@@ -15,24 +15,26 @@
 
 static const struct {
 	uintptr_t base;
+	unsigned int aoac_device;
+	const char *acpi_name;
 	struct soc_amd_gpio mux[2];
 } uart_info[] = {
-	[0] = { APU_UART0_BASE, {
+	[0] =	{ APU_UART0_BASE, FCH_AOAC_DEV_UART0, "FUR0", {
 			PAD_NF(GPIO_138, UART0_TXD, PULL_NONE),
 			PAD_NF(GPIO_136, UART0_RXD, PULL_NONE),
-	} },
-	[1] = { APU_UART1_BASE, {
+		} },
+	[1] =	{ APU_UART1_BASE, FCH_AOAC_DEV_UART1, "FUR1", {
 			PAD_NF(GPIO_143, UART1_TXD, PULL_NONE),
 			PAD_NF(GPIO_141, UART1_RXD, PULL_NONE),
-	} },
-	[2] = { APU_UART2_BASE, {
+		} },
+	[2] =	{ APU_UART2_BASE, FCH_AOAC_DEV_UART2, "FUR2", {
 			PAD_NF(GPIO_137, UART2_TXD, PULL_NONE),
 			PAD_NF(GPIO_135, UART2_RXD, PULL_NONE),
-	} },
-	[3] = { APU_UART3_BASE, {
+		} },
+	[3] =	{ APU_UART3_BASE, FCH_AOAC_DEV_UART3, "FUR3", {
 			PAD_NF(GPIO_140, UART3_TXD, PULL_NONE),
 			PAD_NF(GPIO_142, UART3_RXD, PULL_NONE),
-	} },
+		} },
 };
 
 uintptr_t get_uart_base(unsigned int idx)
@@ -41,6 +43,28 @@ uintptr_t get_uart_base(unsigned int idx)
 		return 0;
 
 	return uart_info[idx].base;
+}
+
+static enum cb_err get_uart_idx(uintptr_t base, unsigned int *idx)
+{
+	unsigned int i;
+	for (i = 0; i < ARRAY_SIZE(uart_info); i++) {
+		if (base == uart_info[i].base) {
+			*idx = i;
+			return CB_SUCCESS;
+		}
+	}
+	return CB_ERR;
+}
+
+static enum cb_err get_uart_aoac_device(uintptr_t base, unsigned int *aoac_dev)
+{
+	unsigned int idx;
+	if (get_uart_idx(base, &idx) == CB_ERR)
+		return CB_ERR;
+
+	*aoac_dev = uart_info[idx].aoac_device;
+	return CB_SUCCESS;
 }
 
 void clear_uart_legacy_config(void)
@@ -58,18 +82,11 @@ void set_uart_config(unsigned int idx)
 
 static const char *uart_acpi_name(const struct device *dev)
 {
-	switch (dev->path.mmio.addr) {
-	case APU_UART0_BASE:
-		return "FUR0";
-	case APU_UART1_BASE:
-		return "FUR1";
-	case APU_UART2_BASE:
-		return "FUR2";
-	case APU_UART3_BASE:
-		return "FUR3";
-	default:
+	unsigned int idx;
+	if (get_uart_idx(dev->path.mmio.addr, &idx) == CB_SUCCESS)
+		return uart_info[idx].acpi_name;
+	else
 		return NULL;
-	}
 }
 
 /* Even though this is called enable, it gets called for both enabled and disabled devices. */
@@ -77,20 +94,7 @@ static void uart_enable(struct device *dev)
 {
 	unsigned int dev_id;
 
-	switch (dev->path.mmio.addr) {
-	case APU_UART0_BASE:
-		dev_id = FCH_AOAC_DEV_UART0;
-		break;
-	case APU_UART1_BASE:
-		dev_id = FCH_AOAC_DEV_UART1;
-		break;
-	case APU_UART2_BASE:
-		dev_id = FCH_AOAC_DEV_UART2;
-		break;
-	case APU_UART3_BASE:
-		dev_id = FCH_AOAC_DEV_UART3;
-		break;
-	default:
+	if (get_uart_aoac_device(dev->path.mmio.addr, &dev_id) == CB_ERR) {
 		printk(BIOS_ERR, "%s: Unknown device: %s\n", __func__, dev_path(dev));
 		return;
 	}

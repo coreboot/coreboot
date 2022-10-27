833bb448c5213b6b9051aa7c55f9b9a2585b8c49
Werner Zeh <werner.zeh@siemens.com>
2022-10-27 10:23:29 +0200

    mb/siemens/mc_ehl: Remove spd.bin from CBFS
    The SPD data for DRAM init has moved into the hwinfo data structure and
    is therefore not used from spd.bin anymore. spd.bin will not receive any
    updates, changes will only be done in hwinfo. There is no reason to keep
    spd.bin around so remove it for both variants.
    
    Change-Id: Ie6091b655ba7ff2e01b684266ce34b85593b8623
    Signed-off-by: Werner Zeh <werner.zeh@siemens.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/68908
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/mainboard/siemens/mc_ehl/romstage_fsp_params.c
+++ src/mainboard/siemens/mc_ehl/romstage_fsp_params.c
@@ -27,9 +27,7 @@ void mainboard_memory_init_params(FSPM_UPD *memupd)
 		spd_info.spd_spec.spd_data_ptr_info.spd_data_len = CONFIG_DIMM_SPD_SIZE;
 		spd_info.read_type = READ_SPD_MEMPTR;
 	} else {
-		printk(BIOS_WARNING, "SPD in HW-Info not valid, fall back to spd.bin!\n");
-		spd_info.read_type = READ_SPD_CBFS;
-		spd_info.spd_spec.spd_index = 0x00;
+		die("SPD in HW-Info not valid!\n");
 	}
 	/* Initialize variant specific configurations */
 	memcfg_init(&memupd->FspmConfig, board_cfg, &spd_info, false);

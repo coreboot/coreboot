cdac3aeb1168223ff218b7dc6dd0a0075667f429
Fred Reitberger <reitbergerfred@gmail.com>
2022-10-31 15:52:39 -0400

    soc/amd/mendocino/data_fabric: Add register bitslice struct
    Add structs to define the data_fabric register bitfields.
    
    Signed-off-by: Fred Reitberger <reitbergerfred@gmail.com>
    Change-Id: I01dcea783542ecc0a761191907c1273016f854c2
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/69070
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/soc/amd/mendocino/include/soc/data_fabric.h
+++ src/soc/amd/mendocino/include/soc/data_fabric.h
@@ -12,4 +12,32 @@
 
 #define NUM_NB_MMIO_REGS		8
 
+union df_mmio_control {
+	struct {
+		uint32_t re        :  1; /* [ 0.. 0] */
+		uint32_t we        :  1; /* [ 1.. 1] */
+		uint32_t           :  2; /* [ 3.. 2] */
+		uint32_t fabric_id :  4; /* [ 7.. 4] */
+		uint32_t           :  8; /* [15.. 8] */
+		uint32_t np        :  1; /* [16..16] */
+		uint32_t           : 15; /* [31..17] */
+	};
+	uint32_t raw;
+};
+
+union df_ficaa {
+	struct {
+		uint32_t cfg_inst_acc_en : 1; /* [ 0.. 0] */
+		uint32_t                 : 1; /* [ 1.. 1] */
+		uint32_t reg_num         : 9; /* [10.. 2] */
+		uint32_t func_num        : 3; /* [13..11] */
+		uint32_t b64_en          : 1; /* [14..14] */
+		uint32_t                 : 1; /* [15..15] */
+		uint32_t inst_id         : 8; /* [23..16] */
+		uint32_t                 : 8; /* [31..24] */
+	};
+	uint32_t raw;
+};
+
+
 #endif /* AMD_MENDOCINO_DATA_FABRIC_H */

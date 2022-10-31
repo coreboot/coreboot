a9b09547d853e58419d5093b9044ea776d024891
Fred Reitberger <reitbergerfred@gmail.com>
2022-10-31 15:51:39 -0400

    soc/amd/picasso/data_fabric: Add register bitslice struct
    Add structs to define the data_fabric register bitfields.
    
    Signed-off-by: Fred Reitberger <reitbergerfred@gmail.com>
    Change-Id: If7cc94681cd5e282e09455c0ac7d3675884c3cf9
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/69069
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/soc/amd/picasso/include/soc/data_fabric.h
+++ src/soc/amd/picasso/include/soc/data_fabric.h
@@ -14,6 +14,33 @@
 
 #define NUM_NB_MMIO_REGS		8
 
+union df_mmio_control {
+	struct {
+		uint32_t re        :  1; /* [ 0.. 0] */
+		uint32_t we        :  1; /* [ 1.. 1] */
+		uint32_t           :  2; /* [ 3.. 2] */
+		uint32_t fabric_id :  8; /* [11.. 4] */
+		uint32_t np        :  1; /* [12..12] */
+		uint32_t           : 19; /* [31..13] */
+	};
+	uint32_t raw;
+};
+
+union df_ficaa {
+	struct {
+		uint32_t cfg_inst_acc_en : 1; /* [ 0.. 0] */
+		uint32_t                 : 1; /* [ 1.. 1] */
+		uint32_t reg_num         : 9; /* [10.. 2] */
+		uint32_t func_num        : 3; /* [13..11] */
+		uint32_t b64_en          : 1; /* [14..14] */
+		uint32_t                 : 1; /* [15..15] */
+		uint32_t inst_id         : 8; /* [23..16] */
+		uint32_t                 : 8; /* [31..24] */
+	};
+	uint32_t raw;
+};
+
+
 #define D18F0_VGAEN			0x80
 #define   VGA_ADDR_ENABLE		BIT(0)
 

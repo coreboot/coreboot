dbdf170dcdf9edbc860766606e53c6d647d89d38
Arthur Heymans <arthur@aheymans.xyz>
2022-11-01 23:28:54 +0100

    {cpu/nb}/amd/family14: Remove platform
    This platform use the LEGACY_SMP_INIT which is to be deprecated after
    release 4.18.
    
    Change-Id: Ieaac0a32e71d208b66fd2c4e26f5349abc921d4f
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/69115
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/cpu/amd/agesa/family14/chip_name.c
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-
-#include <device/device.h>
-
-struct chip_operations cpu_amd_agesa_family14_ops = {
-	CHIP_NAME("AMD CPU Family 14h Model 00h-0Fh")
-};

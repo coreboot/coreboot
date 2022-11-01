9a458e4e58edbfc154dce007961514b5c31cf7aa
Arthur Heymans <arthur@aheymans.xyz>
2022-11-01 23:30:21 +0100

    {cpu/nb}/amd/family15tn: Remove platform
    This platform use the LEGACY_SMP_INIT which is to be deprecated after
    release 4.18.
    
    Change-Id: I18eb1c1ccad16980a4e57318dec411b82c45b25a
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/69116
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/cpu/amd/agesa/family15tn/chip_name.c
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-
-#include <device/device.h>
-
-struct chip_operations cpu_amd_agesa_family15tn_ops = {
-	CHIP_NAME("AMD CPU Family 15h Model 10h-1Fh")
-};

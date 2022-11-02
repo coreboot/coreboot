c16a7fc7179e0811a26bcfa44214f88f64793f04
Mario Scheithauer <mario.scheithauer@siemens.com>
2022-11-02 16:00:27 +0100

    soc/intel/ehl: Add MDIO operation to TSN GbE device
    This patch refactors the MDIO access for the TSN GbE device by placing
    the MDIO read and write functions into mdio_bus_operations struct which
    is assigned to the .ops_mdio member of the PCI device struct. In this
    way the MDIO interface of the TSN GbE device is exposed and can be used
    by other drivers if needed.
    
    Change-Id: I5d1b9dd2f2ba8c18291fff314c13f0c3851784aa
    Signed-off-by: Mario Scheithauer <mario.scheithauer@siemens.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/69383
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Werner Zeh <werner.zeh@siemens.com>

--- src/soc/intel/elkhartlake/tsn_gbe.c
+++ src/soc/intel/elkhartlake/tsn_gbe.c
@@ -1,8 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <console/console.h>
+#include <delay.h>
+#include <device/mdio.h>
 #include <device/pci.h>
 #include <device/pci_ids.h>
+#include <device/pci_ops.h>
 #include <intelblocks/lpss.h>
 #include <soc/soc_chip.h>
 #include <soc/tsn_gbe.h>
@@ -29,74 +32,21 @@ static void program_mac_address(struct device *dev, void *base)
 			(mac[3] << 24) | (mac[2] << 16) | (mac[1] << 8) | mac[0]);
 }
 
-enum cb_err phy_gmii_ready(void *base)
-{
-	struct stopwatch sw;
 
-	stopwatch_init_msecs_expire(&sw, TSN_GMII_TIMEOUT_MS);
-	do {
-		if (!(read32((base + TSN_MAC_MDIO_ADR)) & TSN_MAC_GMII_BUSY))
-			return CB_SUCCESS;
-
-	} while (!stopwatch_expired(&sw));
-
-	printk(BIOS_ERR, "%s Timeout after %lld msec\n", __func__,
-			stopwatch_duration_msecs(&sw));
-	return CB_ERR;
-}
-
-uint16_t tsn_mdio_read(void *base, uint8_t phy_adr, uint8_t reg_adr)
-{
-	uint16_t data = 0;
-	enum cb_err status;
-
-	clrsetbits32(base + TSN_MAC_MDIO_ADR, TSN_MAC_MDIO_ADR_MASK,
-			TSN_MAC_PHYAD(phy_adr) | TSN_MAC_REGAD(reg_adr)
-			| TSN_MAC_CLK_TRAIL_4 | TSN_MAC_CSR_CLK_DIV_102
-			| TSN_MAC_OP_CMD_READ | TSN_MAC_GMII_BUSY);
-
-	/* Wait for MDIO frame transfer to complete before reading MDIO DATA register */
-	status = phy_gmii_ready(base);
-	if (status == CB_ERR) {
-		printk(BIOS_ERR, "%s TSN GMII busy. PHY Adr: 0x%x, Reg 0x%x\n",
-				__func__, phy_adr, reg_adr);
-	} else {
-		data = read16(base + TSN_MAC_MDIO_DATA);
-		printk(BIOS_DEBUG, "%s PHY Adr: 0x%x, Reg: 0x%x , Data: 0x%x\n",
-				__func__, phy_adr, reg_adr, data);
-	}
-	return data;
-}
-
-void tsn_mdio_write(void *base, uint8_t phy_adr, uint8_t reg_adr, uint16_t data)
-{
-	enum cb_err status;
-
-	write16(base + TSN_MAC_MDIO_DATA, data);
-	clrsetbits32(base + TSN_MAC_MDIO_ADR, TSN_MAC_MDIO_ADR_MASK,
-			TSN_MAC_PHYAD(phy_adr) | TSN_MAC_REGAD(reg_adr)
-			| TSN_MAC_CLK_TRAIL_4 | TSN_MAC_CSR_CLK_DIV_102
-			| TSN_MAC_OP_CMD_WRITE | TSN_MAC_GMII_BUSY);
-
-	/* Wait for MDIO frame transfer to complete before do next */
-	status = phy_gmii_ready(base);
-	if (status == CB_ERR)
-		printk(BIOS_ERR, "%s TSN GMII busy. PHY Adr: 0x%x, Reg 0x%x\n",
-				__func__, phy_adr, reg_adr);
-	else
-		printk(BIOS_DEBUG, "%s PHY Adr: 0x%x, Reg: 0x%x , Data: 0x%x\n",
-				__func__, phy_adr, reg_adr, data);
-}
-
-static void tsn_set_phy2mac_irq_polarity(void *base, enum tsn_phy_irq_polarity pol)
+static void tsn_set_phy2mac_irq_polarity(struct device *dev, enum tsn_phy_irq_polarity pol)
 {
 	uint16_t gcr_reg;
+	const struct mdio_bus_operations *mdio_ops;
+
+	mdio_ops = dev_get_mdio_ops(dev);
+	if (!mdio_ops)
+		return;
 
 	if (pol == RISING_EDGE) {
 		/* Read TSN adhoc PHY sublayer register - global configuration register */
-		gcr_reg = tsn_mdio_read(base, TSN_MAC_MDIO_ADHOC_ADR, TSN_MAC_MDIO_GCR);
+		gcr_reg = mdio_ops->read(dev, TSN_MAC_MDIO_ADHOC_ADR, TSN_MAC_MDIO_GCR);
 		gcr_reg |= TSN_MAC_PHY2MAC_INTR_POL;
-		tsn_mdio_write(base, TSN_MAC_MDIO_ADHOC_ADR, TSN_MAC_MDIO_GCR, gcr_reg);
+		mdio_ops->write(dev, TSN_MAC_MDIO_ADHOC_ADR, TSN_MAC_MDIO_GCR, gcr_reg);
 	}
 }
 
@@ -120,16 +70,86 @@ static void gbe_tsn_init(struct device *dev)
 	/* Set PHY-to-MAC IRQ polarity according to devicetree */
 	switch (dev->path.pci.devfn) {
 	case PCH_DEVFN_GBE:
-		tsn_set_phy2mac_irq_polarity(io_mem_base, config->pch_tsn_phy_irq_edge);
+		tsn_set_phy2mac_irq_polarity(dev, config->pch_tsn_phy_irq_edge);
 		break;
 	case PCH_DEVFN_PSEGBE0:
-		tsn_set_phy2mac_irq_polarity(io_mem_base, config->pse_tsn_phy_irq_edge[0]);
+		tsn_set_phy2mac_irq_polarity(dev, config->pse_tsn_phy_irq_edge[0]);
 		break;
 	case PCH_DEVFN_PSEGBE1:
-		tsn_set_phy2mac_irq_polarity(io_mem_base, config->pse_tsn_phy_irq_edge[1]);
+		tsn_set_phy2mac_irq_polarity(dev, config->pse_tsn_phy_irq_edge[1]);
 		break;
 	}
 }
+
+static enum cb_err phy_gmii_ready(void *base)
+{
+	struct stopwatch sw;
+
+	stopwatch_init_msecs_expire(&sw, GMII_TIMEOUT_MS);
+	do {
+		if (!(read32((base + MAC_MDIO_ADR)) & MAC_GMII_BUSY))
+			return CB_SUCCESS;
+		mdelay(1);
+	} while (!stopwatch_expired(&sw));
+
+	printk(BIOS_ERR, "%s Timeout after %lld msec\n", __func__,
+			stopwatch_duration_msecs(&sw));
+	return CB_ERR;
+}
+
+static uint16_t tsn_mdio_read(struct device *dev, uint8_t phy_adr, uint8_t reg_adr)
+{
+	uint16_t data = 0;
+	struct resource *gbe_tsn_res = find_resource(dev, PCI_BASE_ADDRESS_0);
+	void *mmio_base = res2mmio(gbe_tsn_res, 0, 0);
+
+	if (!mmio_base)
+		return data;
+
+	clrsetbits32(mmio_base + MAC_MDIO_ADR, MAC_MDIO_ADR_MASK,
+			MAC_PHYAD(phy_adr) | MAC_REGAD(reg_adr)
+			| MAC_CLK_TRAIL_4 | MAC_CSR_CLK_DIV_102
+			| MAC_OP_CMD_READ | MAC_GMII_BUSY);
+
+	/* Wait for MDIO frame transfer to complete before reading MDIO DATA register. */
+	if (phy_gmii_ready(mmio_base) != CB_SUCCESS) {
+		printk(BIOS_ERR, "%s TSN GMII busy. PHY Adr: 0x%x, Reg 0x%x\n",
+				__func__, phy_adr, reg_adr);
+	} else {
+		data = read16(mmio_base + MAC_MDIO_DATA);
+		printk(BIOS_SPEW, "%s PHY Adr: 0x%x, Reg: 0x%x , Data: 0x%x\n",
+				__func__, phy_adr, reg_adr, data);
+	}
+	return data;
+}
+
+static void tsn_mdio_write(struct device *dev, uint8_t phy_adr, uint8_t reg_adr, uint16_t data)
+{
+	struct resource *gbe_tsn_res = find_resource(dev, PCI_BASE_ADDRESS_0);
+	void *mmio_base = res2mmio(gbe_tsn_res, 0, 0);
+
+	if (!mmio_base)
+		return;
+
+	write16(mmio_base + MAC_MDIO_DATA, data);
+	clrsetbits32(mmio_base + MAC_MDIO_ADR, MAC_MDIO_ADR_MASK,
+			MAC_PHYAD(phy_adr) | MAC_REGAD(reg_adr)
+			| MAC_CLK_TRAIL_4 | MAC_CSR_CLK_DIV_102
+			| MAC_OP_CMD_WRITE | MAC_GMII_BUSY);
+
+	/* Wait for MDIO frame transfer to complete before exit. */
+	if (phy_gmii_ready(mmio_base) != CB_SUCCESS)
+		printk(BIOS_ERR, "%s TSN GMII busy. PHY Adr: 0x%x, Reg 0x%x\n",
+				__func__, phy_adr, reg_adr);
+	else
+		printk(BIOS_SPEW, "%s PHY Adr: 0x%x, Reg: 0x%x , Data: 0x%x\n",
+				__func__, phy_adr, reg_adr, data);
+}
+
+static struct mdio_bus_operations mdio_ops = {
+	.read			= tsn_mdio_read,
+	.write			= tsn_mdio_write,
+};
 
 static struct device_operations gbe_tsn_ops  = {
 	.read_resources   = pci_dev_read_resources,
@@ -138,6 +158,7 @@ static struct device_operations gbe_tsn_ops  = {
 	.scan_bus         = scan_generic_bus,
 	.enable           = gbe_tsn_enable,
 	.init             = gbe_tsn_init,
+	.ops_mdio         = &mdio_ops,
 };
 
 static const unsigned short gbe_tsn_device_ids[] = {

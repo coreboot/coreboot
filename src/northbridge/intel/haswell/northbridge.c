600fa266bdc8740126420e63579a5b9d103ca960
Arthur Heymans <arthur@aheymans.xyz>
2022-11-07 08:04:59 +0100

    nb/intel/haswell: Hook up PCI domain and CPU cluster ops to devicetree
    Change-Id: I955274bc6bda587201f130762c0735c36f5501d1
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/69289
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/northbridge/intel/haswell/northbridge.c
+++ src/northbridge/intel/haswell/northbridge.c
@@ -34,7 +34,7 @@ static const char *northbridge_acpi_name(const struct device *dev)
 	return NULL;
 }
 
-static struct device_operations pci_domain_ops = {
+struct device_operations haswell_pci_domain_ops = {
 	.read_resources    = pci_domain_read_resources,
 	.set_resources     = pci_domain_set_resources,
 	.scan_bus          = pci_domain_scan_bus,
@@ -557,23 +557,12 @@ static const struct pci_driver mc_driver_hsw __pci_driver = {
 	.devices = mc_pci_device_ids,
 };
 
-static struct device_operations cpu_bus_ops = {
+struct device_operations haswell_cpu_bus_ops = {
 	.read_resources   = noop_read_resources,
 	.set_resources    = noop_set_resources,
 	.init             = mp_cpu_bus_init,
 };
 
-static void enable_dev(struct device *dev)
-{
-	/* Set the operations if it is a special bus type. */
-	if (dev->path.type == DEVICE_PATH_DOMAIN) {
-		dev->ops = &pci_domain_ops;
-	} else if (dev->path.type == DEVICE_PATH_CPU_CLUSTER) {
-		dev->ops = &cpu_bus_ops;
-	}
-}
-
 struct chip_operations northbridge_intel_haswell_ops = {
 	CHIP_NAME("Intel Haswell integrated Northbridge")
-	.enable_dev = enable_dev,
 };

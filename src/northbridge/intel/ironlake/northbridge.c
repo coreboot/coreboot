62eb94c9d3fe66b8fc0160d4a1290f6223e2f27a
Arthur Heymans <arthur@aheymans.xyz>
2022-11-07 08:34:41 +0100

    nb/intel/ironlake: Hook up PCI domain and CPU ops to devicetree
    Change-Id: I9dd254eddc12966154776d8a2d43f002567e758f
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/69290
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/northbridge/intel/ironlake/northbridge.c
+++ src/northbridge/intel/ironlake/northbridge.c
@@ -75,7 +75,7 @@ static const char *northbridge_acpi_name(const struct device *dev)
 }
 #endif
 
-static struct device_operations pci_domain_ops = {
+struct device_operations ironlake_pci_domain_ops = {
 	.read_resources	= pci_domain_read_resources,
 	.set_resources	= pci_domain_set_resources,
 	.scan_bus	= pci_domain_scan_bus,
@@ -217,24 +217,13 @@ static const struct pci_driver mc_driver_ilk __pci_driver = {
 	.devices = pci_device_ids,
 };
 
-static struct device_operations cpu_bus_ops = {
+struct device_operations ironlake_cpu_bus_ops = {
 	.read_resources = noop_read_resources,
 	.set_resources = noop_set_resources,
 	.init = mp_cpu_bus_init,
 };
 
-static void enable_dev(struct device *dev)
-{
-	/* Set the operations if it is a special bus type */
-	if (dev->path.type == DEVICE_PATH_DOMAIN) {
-		dev->ops = &pci_domain_ops;
-	} else if (dev->path.type == DEVICE_PATH_CPU_CLUSTER) {
-		dev->ops = &cpu_bus_ops;
-	}
-}
-
 struct chip_operations northbridge_intel_ironlake_ops = {
 	CHIP_NAME("Intel Ironlake integrated Northbridge")
-	.enable_dev = enable_dev,
 	.init = ironlake_init,
 };

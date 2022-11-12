fa775b7651e4488f0535c17a5e04974e7d1a40b2
Arthur Heymans <arthur@aheymans.xyz>
2022-11-12 16:50:08 +0100

    cpu/cpu.h: Remove unused functions prototypes
    These were dropped with LEGACY_SMP_INIT.
    
    Change-Id: Iecaf9ba3d31d22311557b885b31e98a0edd74d96
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/69503
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>

--- src/include/cpu/cpu.h
+++ src/include/cpu/cpu.h
@@ -13,8 +13,6 @@ uintptr_t cpu_get_lapic_addr(void);
 /* Function to keep track of cpu default apic_id */
 void cpu_add_map_entry(unsigned int index);
 struct bus;
-void initialize_cpus(struct bus *cpu_bus);
-asmlinkage void secondary_cpu_init(unsigned int cpu_index);
 int cpu_phys_address_size(void);
 
 #if ENV_RAMSTAGE

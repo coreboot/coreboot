8894a55fc830683a09ec1eae813a24180b275d7f
Ashish Kumar Mishra <ashish.k.mishra@intel.com>
2022-11-17 14:48:26 +0530

    mb/intel/mtlrvp: Add romstage and configure LP5 memory parts
    This patch adds initial romstage code and spd data for LP5 memory
    parts for MTL-RVP. This also configures memory based on the board id.
    
    Memory - x32 LPDDR5
    Vendor/Model - Micron/MT62F2G32D8DR-031 WT:B
    Board ID -
             0b0000 - Empty spd hex file
             0b0001 - DDR5 (Empty spd hex file)
             0b0010 - LPDDR5 (MT62F2G32D8DR-031 WT:B)
    
    BUG=b:224325352
    TEST=Able to boot intel/mtlrvp (LP5 SKU) to ChromeOS
    
    Signed-off-by: Ashish Kumar Mishra <ashish.k.mishra@intel.com>
    Change-Id: I15b352eb246aed23da273e56490c7094eae9d176
    Signed-off-by: Harsha B R <harsha.b.r@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/69741
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Sridhar Siricilla <sridhar.siricilla@intel.com>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>

--- src/mainboard/intel/mtlrvp/romstage.c
+++ src/mainboard/intel/mtlrvp/romstage.c
@@ -5,11 +5,31 @@
 #include <ec/intel/board_id.h>
 #include <soc/romstage.h>
 
+#define SPD_ID_MASK 0x7
+
+static size_t get_spd_index(void)
+{
+	uint8_t board_id = get_rvp_board_id();
+	size_t spd_index;
+
+	printk(BIOS_INFO, "board id is 0x%x\n", board_id);
+
+	spd_index = board_id & SPD_ID_MASK;
+
+	printk(BIOS_INFO, "SPD index is 0x%x\n", (unsigned int)spd_index);
+	return spd_index;
+}
+
 void mainboard_memory_init_params(FSPM_UPD *memupd)
 {
 	const struct mb_cfg *mem_config = variant_memory_params();
 	int board_id = get_rvp_board_id();
 	const bool half_populated = false;
+
+	const struct mem_spd memory_down_spd_info = {
+		.topo = MEM_TOPO_MEMORY_DOWN,
+		.cbfs_index = get_spd_index(),
+	};
 
 	const struct mem_spd dimm_module_spd_info = {
 		.topo = MEM_TOPO_DIMM_MODULE,
@@ -36,6 +56,10 @@ void mainboard_memory_init_params(FSPM_UPD *memupd)
 	switch (board_id) {
 	case MTLP_DDR5_RVP:
 		memcfg_init(memupd, mem_config, &dimm_module_spd_info, half_populated);
+		break;
+	case MTLP_LP5_T3_RVP:
+	case MTLP_LP5_T4_RVP:
+		memcfg_init(memupd, mem_config, &memory_down_spd_info, half_populated);
 		break;
 	default:
 		die("Unknown board id = 0x%x\n", board_id);
--- src/mainboard/intel/mtlrvp/variants/baseboard/mtlrvp_p/memory/memory.c
+++ src/mainboard/intel/mtlrvp/variants/baseboard/mtlrvp_p/memory/memory.c
@@ -5,6 +5,68 @@
 #include <ec/intel/board_id.h>
 #include <soc/romstage.h>
 
+static const struct mb_cfg lp5_mem_config = {
+	.type = MEM_TYPE_LP5X,
+
+	/* DQ byte map as per doc #573387 */
+	.lpx_dq_map = {
+		.ddr0 = {
+			.dq0 = {  0,  3,  1,  2,  4,  5,  6,  7, },
+			.dq1 = {  12,  13,  14,  15,  11,  9,  10,  8 },
+		},
+		.ddr1 = {
+			.dq0 = {  3,  0,  1,  2,  5,  7,  6,  4, },
+			.dq1 = {  13,  10,  12,  15,  9,  11,  8,  14 },
+		},
+		.ddr2 = {
+			.dq0 = {  2,  1,  3,  0,  7,  5,  6,  4, },
+			.dq1 = {  12,  8,  13,  15,  11,  10,  9,  14 },
+		},
+		.ddr3 = {
+			.dq0 = {  4,  3,  0,  1,  5,  2,  6,  7, },
+			.dq1 = {  8,  15,  12,  14,  10,  13,  9,  11 },
+		},
+		.ddr4 = {
+			.dq0 = {  1,  3,  2,  6,  7,  5,  4,  0, },
+			.dq1 = {  14,  13,  12,  15,  11,  10,  8,  9 },
+		},
+		.ddr5 = {
+			.dq0 = {  0,  7,  3,  6,  2,  5,  1,  4, },
+			.dq1 = {  9,  10,  11,  8,  13,  14,  15,  12 },
+		},
+		.ddr6 = {
+			.dq0 = {  3,  0,  2,  1,  6,  5,  4,  7, },
+			.dq1 = {  15,  13,  12,  14,  8,  11,  10,  9 },
+		},
+		.ddr7 = {
+			.dq0 = {  2,  1,  3,  0,  6,  4,  7,  5, },
+			.dq1 = {  14,  12,  13,  15,  9,  10,  11,  8 },
+		},
+	},
+
+	/* DQS CPU<>DRAM map as per doc #573387 */
+	.lpx_dqs_map = {
+		.ddr0 = { .dqs0 = 0, .dqs1 = 1 },
+		.ddr1 = { .dqs0 = 0, .dqs1 = 1 },
+		.ddr2 = { .dqs0 = 0, .dqs1 = 1 },
+		.ddr3 = { .dqs0 = 0, .dqs1 = 1 },
+		.ddr4 = { .dqs0 = 0, .dqs1 = 1 },
+		.ddr5 = { .dqs0 = 0, .dqs1 = 1 },
+		.ddr6 = { .dqs0 = 0, .dqs1 = 1 },
+		.ddr7 = { .dqs0 = 0, .dqs1 = 1 }
+	},
+
+	.ect = false, /* Early Command Training */
+
+	.LpDdrDqDqsReTraining = 1,
+
+	.UserBd = BOARD_TYPE_ULT_ULX,
+
+	.lp5x_config = {
+		.ccc_config = 0,
+	},
+};
+
 static const struct mb_cfg ddr5_mem_config = {
 	.type = MEM_TYPE_DDR5,
 
@@ -31,6 +93,9 @@ const struct mb_cfg *variant_memory_params(void)
 	switch (board_id) {
 	case MTLP_DDR5_RVP:
 		return &ddr5_mem_config;
+	case MTLP_LP5_T3_RVP:
+	case MTLP_LP5_T4_RVP:
+		return &lp5_mem_config;
 	default:
 		die("Unknown board id = 0x%x\n", board_id);
 		break;

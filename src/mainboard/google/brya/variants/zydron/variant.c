50a32650172dfedc2a6c5335af94fb2d3c7d71e6
David Wu <david_wu@quanta.corp-partner.google.com>
2022-11-30 17:05:09 +0800

    mb/google/brya/var/zydron: Add WiFi SAR table
    Add WiFi SAR table for zydron.
    
    BUG=b:260770999
    TEST=build FW and checked SAR table can load by WiFi driver.
    
    Change-Id: I8d5f966c7af3ac6d9923d4f6c851bfb340f31fab
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/70173
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>

--- src/mainboard/google/brya/variants/zydron/variant.c
+++ src/mainboard/google/brya/variants/zydron/variant.c
@@ -2,7 +2,13 @@
 
 #include <chip.h>
 #include <fw_config.h>
+#include <sar.h>
 #include <baseboard/variants.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return "wifi_sar_0.hex";
+}
 
 void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
 {

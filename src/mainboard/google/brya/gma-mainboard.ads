a439406bdcb00e5d039a695a051afcdd13d33a4b
Jeremy Compostella <jeremy.compostella@intel.com>
2022-12-02 11:07:09 -0700

    mb/google/brya: Add romstage early graphics for brya
    BUG=b:252792591
    BRANCH=firmware-brya-14505.B
    TEST=On-screen text message seen during MRC training on skolas
         with a few extra patches
    
    Change-Id: I41c9cccb09dea52e2318f8f9ebeeda3697a7b514
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/70304
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tarun Tuli <taruntuli@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/71696

---
+++ src/mainboard/google/brya/gma-mainboard.ads
@@ -0,0 +1,13 @@
+-- SPDX-License-Identifier: GPL-2.0-or-later
+
+with HW.GFX.GMA;
+with HW.GFX.GMA.Display_Probing;
+
+use HW.GFX.GMA;
+use HW.GFX.GMA.Display_Probing;
+
+private package GMA.Mainboard is
+   ports : constant Port_List :=
+     (eDP,
+      others => Disabled);
+end GMA.Mainboard;

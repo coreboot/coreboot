f251a6a439e70d7003ee6a5fbfae79eab3971e75
Subrata Banik <subratabanik@google.com>
2022-12-11 16:39:05 +0530

    soc/intel/meteorlake: Implement MultiPhase SI Init Index 2 callback
    The details about how the CPU multiprocessor init (MP) has migrated
    from coreboot to FSP can be found in
    https://doc.coreboot.org/soc/intel/mp_init/mp_init.html.
    
    The major reason behind this migration is to support the Intel
    proprietary and restricted CPU feature programming which can't be
    performed if coreboot sets the BIOS_DONE or BIOS Reset CPL as part
    of coreboot MP Init flow (prior to calling FSP-S). Hence, the new
    flow introduced with Tiger Lake platform forced having monolithic
    MP Init peformed by FSP (using coreboot MP PPI wrapper code).
    
    The last 3-4 years of FSP doing MP Init has demonstrated ample
    issues during platform bringup which is specific to UEFI MP Service
    implementation and not relevant to open source coreboot. This new
    flow makes the debug and validation aspect complicated where
    any FSP MP Init code changes should have been validated with coreboot
    MP PPI wrapper else might cause some failure, unfortunately,
    the validation commitment has never been met, hence, issue debugging
    is the only solution that remains in practice.
    
    Most importantly, the restricted feature programming which demanded
    closed source MP Init (for features like SGX and C6DRAM) has never
    been enabled in coreboot (starting with Alder Lake, the SGX feature
    has been dropped).
    
    This patch attempts to decouple FSP-S doing MP Init from the rest
    of the FSP-S silicon init and introduces 2nd MultiPhase SI init
    which allows bootloader to perform the mandatory SoC programming
    before FSP-S has done with PM programming (a.k.a set the reset CPL).
    
    The core/uncore BWG suggests the minimum SoC programming before
    BIOS Reset CPL is set. coreboot uses the MultiPhaseSI Init Index 2
    to perform the required CPU programming before enabling the BIOS
    Reset CPL.
    
    This implementation would allow us to get rid of FSP running CPU
    feature programming and additionally make several EDK2 MP service
    modules optional (those are packed to create FSP-S blob).
    
    In summary, this change would allow coreboot to utilize open source
    MP init without running into FSP-S related code blocks.
    
    Note: At present, Intel Meteor Lake FSP doesn't have support for
    MultiPhase SI Init, Index 2 (submitted a FSP code changes over
    chrome-internal to enable this feature to decouple MP Init from
    FSP-S init).
    
    This patch is backported from
    commit b6c3a0325b9b0462cca81ea4134efb6b73756577 (soc/intel/alderlake:
    Implement MultiPhase SI Init Index 2 callback).
    
    BUG=b:233199592
    TEST=Build and boot google/taeko to ChromeOS.
    Perform several thousands cycles of suspend test and power cycle
    without running into any issue.
    
    Change-Id: I2ea1a8bb2b142e39c2bc9d248b7fd0041366c0db
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/70558
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Christian Walter <christian.walter@9elements.com>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/soc/intel/meteorlake/fsp_params.c
+++ src/soc/intel/meteorlake/fsp_params.c
@@ -14,6 +14,8 @@
 #include <option.h>
 #include <intelblocks/cse.h>
 #include <intelblocks/lpss.h>
+#include <intelblocks/mp_init.h>
+#include <intelblocks/systemagent.h>
 #include <intelblocks/xdci.h>
 #include <intelpch/lockdown.h>
 #include <security/vboot/vboot_common.h>
@@ -488,6 +490,7 @@ void platform_fsp_silicon_init_params_cb(FSPS_UPD *supd)
  * Phase   |  FSP return point                                |  Purpose
  * ------- + ------------------------------------------------ + -------------------------------
  *   1     |  After TCSS initialization completed             |  for TCSS specific init
+ *   2     |  Before BIOS Reset CPL is set by FSP-S           |  for CPU specific init
  */
 void platform_fsp_multi_phase_init_cb(uint32_t phase_index)
 {
@@ -501,6 +504,14 @@ void platform_fsp_multi_phase_init_cb(uint32_t phase_index)
 			const config_t *config = config_of_soc();
 			tcss_configure(config->typec_aux_bias_pads);
 		}
+		break;
+	case 2:
+		/* CPU specific initialization here */
+		printk(BIOS_DEBUG, "FSP MultiPhaseSiInit %s/%s called\n",
+			__FILE__, __func__);
+		before_post_cpus_init();
+		/* Enable BIOS Reset CPL */
+		enable_bios_reset_cpl();
 		break;
 	default:
 		break;

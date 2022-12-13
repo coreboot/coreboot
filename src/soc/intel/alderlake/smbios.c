d34364bdea12fd73ce5e83b44bc6fa347f3359d3
Subrata Banik <subratabanik@google.com>
2022-12-13 13:43:17 +0530

    soc/intel/alderlake: Utilize `CPU_BCLK_MHZ` over dedicated macro
    This patch drops the redundant macro to define CPU BCLK and instead
    uses `CPU_BCLK_MHZ` config to calculate the
    `smbios_cpu_get_max_speed_mhz`.
    
    TEST=Able to see max cpu speed is correct in smbios table while trying
    on Google/Kano.
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: I5167f3a513c074b9e6986c960e1bcced65f1264c
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/70676
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Dinesh Gehlot <digehlot@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Ivy Jian <ivy.jian@quanta.corp-partner.google.com>

--- src/soc/intel/alderlake/smbios.c
+++ src/soc/intel/alderlake/smbios.c
@@ -4,11 +4,8 @@
 #include <intelblocks/msr.h>
 #include <smbios.h>
 
-/* AlderLake bus clock is fixed at 100MHz */
-#define ADL_BCLK		100
-
 /* Provide the max turbo frequency of the CPU */
 unsigned int smbios_cpu_get_max_speed_mhz(void)
 {
-	return cpu_get_max_turbo_ratio() * ADL_BCLK;
+	return cpu_get_max_turbo_ratio() * CONFIG_CPU_BCLK_MHZ;
 }

coreboot/coreboot

commit cdb26fd011285124a6ed5e6be6dddc806ca5b3a8

cpu/intel/model_206ax: Remove fake lapic device
Instead of using a fake lapic device hook up the cpu cluster to chip
cpu/intel/model_206ax.

The lapic device is also not needed as the mp init will allocate it for
the BSP at runtime.

Change-Id: Id3b1c4ca027e2905535e137691c3e3e60417dbf3
Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/59316
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

src/cpu/intel/model_206ax/acpi.c
@@ -95,18 +95,9 @@ static int get_logical_cores_per_package(void)
 	return msr.lo & 0xffff;
 }
 
-static void generate_C_state_entries(void)
+static void generate_C_state_entries(const struct device *dev)
 {
-	struct device *lapic;
-	struct cpu_intel_model_206ax_config *conf = NULL;
-
-	/* Find the SpeedStep CPU in the device tree using magic APIC ID */
-	lapic = dev_find_lapic(SPEEDSTEP_APIC_MAGIC);
-	if (!lapic)
-		return;
-	conf = lapic->chip_info;
-	if (!conf)
-		return;
+	struct cpu_intel_model_206ax_config *conf = dev->bus->dev->chip_info;
 
 	const int acpi_cstates[3] = { conf->acpi_c1, conf->acpi_c2, conf->acpi_c3 };
 
@@ -324,7 +315,7 @@ void generate_cpu_entries(const struct device *device)
 				cpuID-1, cores_per_package);
 
 			/* Generate C-state tables */
-			generate_C_state_entries();
+			generate_C_state_entries(device);
 
 			/* Generate T-state tables */
 			generate_T_state_entries(

src/cpu/intel/model_206ax/chip.h
@@ -1,8 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-/* Magic value used to locate this chip in the device tree */
-#define SPEEDSTEP_APIC_MAGIC 0xACAC
-
 /* Keep this in sync with acpi.c */
 enum cpu_acpi_level {
 	CPU_ACPI_DISABLED = 0,

src/cpu/intel/model_206ax/model_206ax_init.c
@@ -211,17 +211,10 @@ static void configure_c_states(void)
 	wrmsr(MSR_PP1_CURRENT_CONFIG, msr);
 }
 
-static void configure_thermal_target(void)
+static void configure_thermal_target(struct device *dev)
 {
-	struct cpu_intel_model_206ax_config *conf;
-	struct device *lapic;
+	struct cpu_intel_model_206ax_config *conf = dev->bus->dev->chip_info;
 	msr_t msr;
-
-	/* Find pointer to CPU configuration */
-	lapic = dev_find_lapic(SPEEDSTEP_APIC_MAGIC);
-	if (!lapic || !lapic->chip_info)
-		return;
-	conf = lapic->chip_info;
 
 	/* Set TCC activation offset if supported */
 	msr = rdmsr(MSR_PLATFORM_INFO);
@@ -346,7 +339,7 @@ static void model_206ax_init(struct device *cpu)
 	configure_misc();
 
 	/* Thermal throttle activation offset */
-	configure_thermal_target();
+	configure_thermal_target(cpu);
 
 	set_aesni_lock();
 

src/mainboard/dell/snb_ivb_workstations/variants/baseboard/devicetree.cb
@@ -1,10 +1,8 @@
 chip northbridge/intel/sandybridge
-	device cpu_cluster 0 on
-		chip cpu/intel/model_206ax
-			register "tcc_offset" = "5" # TCC of 95C
-			device lapic 0 on end
-			device lapic 0xacac off end
-		end
+	chip cpu/intel/model_206ax
+		device cpu_cluster 0 on end
+
+		register "tcc_offset" = "5" # TCC of 95C
 	end
 	device domain 0 on
 

src/mainboard/google/stout/devicetree.cb
@@ -19,14 +19,10 @@ chip northbridge/intel/sandybridge
 
 	register "max_mem_clock_mhz" = "666"
 
-	device cpu_cluster 0 on
-		chip cpu/intel/model_206ax
-			# Magic APIC ID to locate this chip
-			device lapic 0 on end
-			device lapic 0xacac off end
+	chip cpu/intel/model_206ax
+		device cpu_cluster 0 on end
 
-			register "tcc_offset" = "5"	# TCC of 95C
-		end
+		register "tcc_offset" = "5"	# TCC of 95C
 	end
 
 	device domain 0 on

src/mainboard/intel/emeraldlake2/devicetree.cb
@@ -11,15 +11,12 @@ chip northbridge/intel/sandybridge
 	# Enable DVI Hotplug with 6ms pulse
 	register "gpu_dp_b_hotplug" = "0x06"
 
-	device cpu_cluster 0 on
-		chip cpu/intel/model_206ax
-			# Magic APIC ID to locate this chip
-			device lapic 0 on end
-			device lapic 0xacac off end
 
-			register "acpi_c1" = "CPU_ACPI_C3"
-			register "acpi_c2" = "CPU_ACPI_C6"
-		end
+	chip cpu/intel/model_206ax
+		device cpu_cluster 0 on end
+
+		register "acpi_c1" = "CPU_ACPI_C3"
+		register "acpi_c2" = "CPU_ACPI_C6"
 	end
 
 	device domain 0 on

src/mainboard/kontron/ktqm77/devicetree.cb
@@ -2,15 +2,11 @@ chip northbridge/intel/sandybridge
 	# IGD Displays
 	register "gfx" = "GMA_STATIC_DISPLAYS(0)"
 
-	device cpu_cluster 0 on
-		chip cpu/intel/model_206ax
-			# Magic APIC ID to locate this chip
-			device lapic 0 on end
-			device lapic 0xacac off end
+	chip cpu/intel/model_206ax
+		device cpu_cluster 0 on end
 
-			register "acpi_c2" = "CPU_ACPI_C6"
-			register "acpi_c3" = "CPU_ACPI_DISABLED"
-		end
+		register "acpi_c2" = "CPU_ACPI_C6"
+		register "acpi_c3" = "CPU_ACPI_DISABLED"
 	end
 
 	device domain 0 on

src/mainboard/roda/rv11/variants/rv11/devicetree.cb
@@ -17,15 +17,11 @@ chip northbridge/intel/sandybridge
 	register "gpu_cpu_backlight" = "0x0000001a"
 	register "gpu_pch_backlight" = "0x002e0000"
 
-	device cpu_cluster 0 on
-		chip cpu/intel/model_206ax
-			# Magic APIC ID to locate this chip
-			device lapic 0 on end
-			device lapic 0xacac off end
+	chip cpu/intel/model_206ax
+		device cpu_cluster 0 on end
 
-			register "acpi_c2" = "CPU_ACPI_C6"
-			register "acpi_c3" = "CPU_ACPI_DISABLED"
-		end
+		register "acpi_c2" = "CPU_ACPI_C6"
+		register "acpi_c3" = "CPU_ACPI_DISABLED"
 	end
 
 	device domain 0 on

commit e64b8ac1e73c75d7b55f9f80c3fb7e2927bdbd0e

cpu/intel/206ax: Fix generating C state entries
The struct device passed to this function is the cpu cluster and not
individual lapic. This fixes a regression introduced by
cdb26fd (cpu/intel/model_206ax: Remove fake lapic device)

Change-Id: I586e13a723303b8d639d526a175bd6828465a607
Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/70665
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Christian Walter <christian.walter@9elements.com>
Reviewed-by: Angel Pons <th3fanbus@gmail.com>
Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

src/cpu/intel/model_206ax/acpi.c
@@ -97,7 +97,7 @@ static int get_logical_cores_per_package(void)
 
 static void generate_C_state_entries(const struct device *dev)
 {
-	struct cpu_intel_model_206ax_config *conf = dev->bus->dev->chip_info;
+	struct cpu_intel_model_206ax_config *conf = dev->chip_info;
 
 	const int acpi_cstates[3] = { conf->acpi_c1, conf->acpi_c2, conf->acpi_c3 };
 
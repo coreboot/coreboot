fbdccebb66e0684dd460f62d47130ad1e506d1bc
Subrata Banik <subratabanik@google.com>
2022-12-19 18:09:56 +0530

    soc/intel/tigerlake: Move DPTF ACPI Device IDs into header file
    This patch moves DPTF ACPI Device IDs into the header file (soc/dptf.h)
    so that upcoming patches in this patch train can achieve more
    common code.
    
    TEST=Able to build and boot Google/Volteer.
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: I111fa9b2672ad01268bb2620b47a53a7a5b00f3f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/71107
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/soc/intel/tigerlake/dptf.c
+++ src/soc/intel/tigerlake/dptf.c
@@ -1,16 +1,17 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <drivers/intel/dptf/dptf.h>
+#include <soc/dptf.h>
 #include <stdbool.h>
 
 static const struct dptf_platform_info tgl_dptf_platform_info = {
 	.use_eisa_hids = false,
 	/* _HID for the toplevel DPTF device, typically \_SB.DPTF */
-	.dptf_device_hid = "INTC1040",
+	.dptf_device_hid = DPTF_DPTF_DEVICE,
 	/* _HID for Intel DPTF Generic Device (these require PTYP as well) */
-	.generic_hid = "INTC1043",
+	.generic_hid = DPTF_GEN_DEVICE,
 	/* _HID for Intel DPTF Fan Device */
-	.fan_hid = "INTC1044",
+	.fan_hid = DPTF_FAN_DEVICE,
 };
 
 const struct dptf_platform_info *soc_get_dptf_platform_info(void)

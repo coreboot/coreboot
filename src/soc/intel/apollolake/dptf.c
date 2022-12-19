4b0c8ccb141982ca47387046a09a604fe655a6e2
Subrata Banik <subratabanik@google.com>
2022-12-19 17:45:25 +0530

    soc/intel/apollolake: Move DPTF ACPI Device IDs into header file
    This patch moves DPTF ACPI Device IDs into the header file (soc/dptf.h)
    so that upcoming patches in this patch train can achieve more
    common code.
    
    TEST=Able to build and boot Google/Reef.
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: I0ce956351afc06871c465b67f51cba8786ce52db
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/71104
    Reviewed-by: Sean Rhodes <sean@starlabs.systems>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/soc/intel/apollolake/dptf.c
+++ src/soc/intel/apollolake/dptf.c
@@ -1,15 +1,16 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <drivers/intel/dptf/dptf.h>
+#include <soc/dptf.h>
 
 static const struct dptf_platform_info apl_dptf_platform_info = {
 	.use_eisa_hids = true,
 	/* _HID for the toplevel DPTF device, typically \_SB.DPTF */
-	.dptf_device_hid = "INT3400",
+	.dptf_device_hid = DPTF_DPTF_DEVICE,
 	/* _HID for Intel DPTF Generic Device (these require PTYP as well) */
-	.generic_hid = "INT3403",
+	.generic_hid = DPTF_GEN_DEVICE,
 	/* _HID for Intel DPTF Fan Device */
-	.fan_hid = "INT3404",
+	.fan_hid = DPTF_FAN_DEVICE,
 };
 
 const struct dptf_platform_info *soc_get_dptf_platform_info(void)
---
+++ src/soc/intel/apollolake/include/soc/dptf.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _SOC_DPTF_H_
+#define _SOC_DPTF_H_
+
+/* Below are the unique ACPI Device IDs for thermal/dptf on Apollo Lake SoC. */
+/* DPTF ACPI Device ID */
+#define DPTF_DPTF_DEVICE	"INT3400"
+/* Generic ACPI Device ID for TSR0/1/2/3 and charger */
+#define DPTF_GEN_DEVICE		"INT3403"
+/* Fan ACPI Device ID */
+#define DPTF_FAN_DEVICE		"INT3404"
+
+#endif /* _SOC_DPTF_H_ */

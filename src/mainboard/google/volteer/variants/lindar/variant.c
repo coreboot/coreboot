3c53300002c771c26a8dc597cf950e6b077193be
Eric Lai <eric_lai@quanta.corp-partner.google.com>
2022-12-20 13:11:47 +0800

    mb/google/volteer/var/lindar: Use get_wifi_sar_fw_config_filename
    Use get_wifi_sar_fw_config_filename to remove the duplicate code.
    WIFI_SAR_CBFS_DEFAULT_FILENAME is not exist, so return the non-exist
    id has the same outcome.
    
    Signed-off-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Change-Id: Ib34e6b4f435880d62936ae54f19ba2ec752eced2
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/71123
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Ivy Jian <ivy.jian@quanta.corp-partner.google.com>

--- src/mainboard/google/volteer/variants/lindar/variant.c
+++ src/mainboard/google/volteer/variants/lindar/variant.c
@@ -6,12 +6,7 @@
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
-	if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, WIFI_SAR_ID_0)))
-		return "wifi_sar_0.hex";
-	else if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, WIFI_SAR_ID_1)))
-		return "wifi_sar_1.hex";
-
-	return WIFI_SAR_CBFS_DEFAULT_FILENAME;
+	return get_wifi_sar_fw_config_filename(FW_CONFIG_FIELD(WIFI_SAR_ID));
 }
 
 const char *mainboard_vbt_filename(void)

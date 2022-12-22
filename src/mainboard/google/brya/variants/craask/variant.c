beb2826ffd6877031ed4de8ec8b2990fe9bc8c4b
Eric Lai <eric_lai@quanta.corp-partner.google.com>
2022-12-22 11:56:50 +0800

    mb/google/nissa/var/craask: Use get_wifi_sar_fw_config_filename
    Use get_wifi_sar_fw_config_filename to remove the duplicate code.
    
    Signed-off-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Change-Id: I04176fee373e534d42c72506df73a092ad55e65b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/71218
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kangheui Won <khwon@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Ivy Jian <ivy.jian@quanta.corp-partner.google.com>

--- src/mainboard/google/brya/variants/craask/variant.c
+++ src/mainboard/google/brya/variants/craask/variant.c
@@ -6,14 +6,5 @@
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
-	if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, ID_0))) {
-		printk(BIOS_INFO, "Use wifi_sar_0.hex.\n");
-		return "wifi_sar_0.hex";
-	}
-
-	else if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, UNUSED))) {
-		printk(BIOS_INFO, "Wi-Fi SAR not used, return NULL!\n");
-	}
-
-	return NULL;
+	return get_wifi_sar_fw_config_filename(FW_CONFIG_FIELD(WIFI_SAR_ID));
 }

872079656b9b47795e778bf86a074b7ed185327a
David Wu <david_wu@quanta.corp-partner.google.com>
2022-12-26 00:07:42 +0800

    mb/google/brya/var/kuldax: Add wifi sar table
    Add wifi sar table for kuldax
    
    BUG=b:248367859
    BRANCH=firmware-brya-14505.B
    TEST=emerge-brask coreboot-private-files-baseboard-brya coreboot
    chromeos-bootimage
    
    Change-Id: I5ade590c739aae391e47e8bb66ee03c086e8d56e
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/71270
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/kuldax/variant.c
+++ src/mainboard/google/brya/variants/kuldax/variant.c
@@ -1,8 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <baseboard/variants.h>
 #include <chip.h>
 #include <fw_config.h>
-#include <baseboard/variants.h>
+#include <sar.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return "wifi_sar_0.hex";
+}
 
 void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
 {

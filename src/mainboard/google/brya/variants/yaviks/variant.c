cdf7ec6faa60f8f6a5650214a6bc9bb30f32e13e
Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
2022-12-28 14:15:53 +0600

    mb/google/nissa/var/yaviks: Disable external fivr
    In next phase, yaviks will remove external fivr. Use the board version
    to config external fivr for backward compatibility and show message.
    
    BUG=b:263842258
    TEST=build, boot to OS, suspend/resume work normally.
    
    Signed-off-by: Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
    Change-Id: Id85570046c5b8e9d90a112793c1ec8604e6bf533
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/71560
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kangheui Won <khwon@chromium.org>

--- src/mainboard/google/brya/variants/yaviks/variant.c
+++ src/mainboard/google/brya/variants/yaviks/variant.c
@@ -1,5 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <baseboard/variants.h>
+#include <boardid.h>
 #include <fw_config.h>
 #include <sar.h>
 
@@ -9,4 +11,29 @@ const char *get_wifi_sar_cbfs_filename(void)
 		return "wifi_sar_0.hex";
 
 	return NULL;
+}
+
+void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
+{
+	const uint32_t id = board_id();
+	if (id == BOARD_ID_UNKNOWN || id < 4) { // proto, EVT
+		config->ext_fivr_settings.configure_ext_fivr = 1;
+
+		config->ext_fivr_settings.v1p05_enable_bitmap =
+				FIVR_ENABLE_ALL_SX & ~FIVR_ENABLE_S0;
+		config->ext_fivr_settings.vnn_enable_bitmap =
+				FIVR_ENABLE_ALL_SX;
+		config->ext_fivr_settings.vnn_sx_enable_bitmap =
+				FIVR_ENABLE_ALL_SX;
+		config->ext_fivr_settings.v1p05_supported_voltage_bitmap =
+				FIVR_VOLTAGE_NORMAL;
+		config->ext_fivr_settings.vnn_supported_voltage_bitmap =
+				FIVR_VOLTAGE_MIN_ACTIVE;
+		config->ext_fivr_settings.v1p05_voltage_mv = 1050;
+		config->ext_fivr_settings.vnn_voltage_mv = 780;
+		config->ext_fivr_settings.vnn_sx_voltage_mv = 1050;
+		config->ext_fivr_settings.v1p05_icc_max_ma = 500;
+		config->ext_fivr_settings.vnn_icc_max_ma = 500;
+		printk(BIOS_INFO, "Configured External FIVR\n");
+	}
 }

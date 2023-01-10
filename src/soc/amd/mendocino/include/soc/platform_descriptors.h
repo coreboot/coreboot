65335ffa48ba3adde22f16d50f67b24dd4ba111e
Felix Held <felix-coreboot@felixheld.de>
2023-01-11 00:23:10 +0100

    soc/amd/mendocino/include/platform_descriptors: remove TODO after review
    This header file is correct for Mendocino, so remove the TODO.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I85b47491863bff731b86cf0523253cb547dbb76a
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/71794
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Fred Reitberger <reitbergerfred@gmail.com>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/mendocino/include/soc/platform_descriptors.h
+++ src/soc/amd/mendocino/include/soc/platform_descriptors.h
@@ -1,7 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-/* TODO: Check if this is still correct */
-
 #ifndef AMD_MENDOCINO_PLATFORM_DESCRIPTORS_H
 #define AMD_MENDOCINO_PLATFORM_DESCRIPTORS_H
 

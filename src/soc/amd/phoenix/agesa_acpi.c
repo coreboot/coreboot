192945e61cef3f90b0a7464b98801b4bd0150ff7
Felix Held <felix-coreboot@felixheld.de>
2023-01-17 02:50:06 +0100

    soc/amd/phoenix/agesa_acpi: use acpi_align_current to align IVRS & ALIB
    This changes the alignment of the IVRS table from 8 bytes to 16 bytes
    and aligns the ALIB table to a 16 byte boundary.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I766260aefcac6876609d6b45202b41a3e9e44385
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/72025
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Fred Reitberger <reitbergerfred@gmail.com>

--- src/soc/amd/phoenix/agesa_acpi.c
+++ src/soc/amd/phoenix/agesa_acpi.c
@@ -16,10 +16,11 @@ uintptr_t agesa_write_acpi_tables(const struct device *device, uintptr_t current
 	acpi_ivrs_t *ivrs;
 
 	/* add ALIB SSDT from HOB */
+	current = acpi_align_current(current);
 	current = add_agesa_fsp_acpi_table(AMD_FSP_ACPI_ALIB_HOB_GUID, "ALIB", rsdp, current);
 
 	/* IVRS */
-	current = ALIGN_UP(current, 8);
+	current = acpi_align_current(current);
 	ivrs = (acpi_ivrs_t *)current;
 	acpi_create_ivrs(ivrs, acpi_fill_ivrs);
 	current += ivrs->header.length;

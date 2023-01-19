b2394e853bf7652fa70295d34892a8385b9a5153
Felix Held <felix-coreboot@felixheld.de>
2023-01-19 18:15:17 +0100

    soc/amd/cezanne: clean up global NVS
    From Cezanne on, the TMPS, TCRT and TPSV fields are unused in both the C
    and ACPI code, so they can be removed. Also remove the unused fields
    that were previously used for PCNT and PWRS. The LIDS field is only used
    in the ACPI code, but keep if for now, since it would require a bigger
    rework to remove it from the global NVS.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: Ib4034e959d167fb1e08ee5b15e21fb93bc89db8a
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/72093
    Reviewed-by: Matt DeVillier <matt.devillier@amd.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/cezanne/include/soc/nvs.h
+++ src/soc/amd/cezanne/include/soc/nvs.h
@@ -13,15 +13,10 @@
 
 struct __packed global_nvs {
 	/* Miscellaneous */
-	uint8_t		unused_was_pcnt; /* 0x00 - Processor Count */
-	uint8_t		lids; /* 0x01 - LID State */
-	uint8_t		unused_was_pwrs; /* 0x02 - AC Power State */
-	uint32_t	cbmc; /* 0x03 - 0x06 - coreboot Memory Console */
-	uint64_t	pm1i; /* 0x07 - 0x0e - System Wake Source - PM1 Index */
-	uint64_t	gpei; /* 0x0f - 0x16 - GPE Wake Source */
-	uint8_t		tmps; /* 0x17 - Temperature Sensor ID */
-	uint8_t		tcrt; /* 0x18 - Critical Threshold */
-	uint8_t		tpsv; /* 0x19 - Passive Threshold */
+	uint8_t		lids; /* 0x00 - LID State */
+	uint32_t	cbmc; /* 0x01 - 0x04 - coreboot Memory Console */
+	uint64_t	pm1i; /* 0x05 - 0x0c - System Wake Source - PM1 Index */
+	uint64_t	gpei; /* 0x0d - 0x14 - GPE Wake Source */
 };
 
 #endif /* AMD_CEZANNE_NVS_H */

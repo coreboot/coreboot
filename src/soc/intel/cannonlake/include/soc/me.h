ce79ae00fb96abb759165f20fe77711f52c7d811
Dinesh Gehlot <digehlot@google.com>
2023-01-24 11:16:40 +0000

    soc/intel/cnl: Move ME FSR structures to pertinent header
    This patch moves ME host firmware status register structures to ME
    header file. It also marks unused structure fields to reserved.
    
    The idea here is to decouple ME specification defined structures from
    the source file `.c` and keep those into header files so that in future
    those spec defined header can move into common code.
    
    The current and future SoC platform will be able to select the correct
    ME spec header based on the applicable config. It might be also
    beneficial if two different SoC platforms would like to use the same
    ME specification and not necessarily share the same SoC directory.
    
    BUG=b:260309647
    Test=Able to build and boot.
    
    Signed-off-by: Dinesh Gehlot <digehlot@google.com>
    Change-Id: I34d3c4a60653fe0c1766cd50c96b8d3fe63637d1
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/72412
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Lean Sheng Tan <sheng.tan@9elements.com>

--- src/soc/intel/cannonlake/include/soc/me.h
+++ src/soc/intel/cannonlake/include/soc/me.h
@@ -5,43 +5,71 @@
 
 /* ME Host Firmware Status register 1 */
 union me_hfsts1 {
-	u32 data;
+	uint32_t data;
 	struct {
-		u32 working_state: 4;
-		u32 mfg_mode: 1;
-		u32 fpt_bad: 1;
-		u32 operation_state: 3;
-		u32 fw_init_complete: 1;
-		u32 ft_bup_ld_flr: 1;
-		u32 update_in_progress: 1;
-		u32 error_code: 4;
-		u32 operation_mode: 4;
-		u32 reset_count: 4;
-		u32 boot_options_present: 1;
-#if CONFIG(SOC_INTEL_COMETLAKE)
-		u32 invoke_enhance_dbg_mode:1;
-#else
-		u32 reserved0: 1;
-#endif
-		u32 bist_test_state: 1;
-		u32 bist_reset_request: 1;
-		u32 current_power_source: 2;
-		u32 reserved1: 1;
-		u32 d0i3_support_valid: 1;
+		uint32_t working_state		: 4;
+		uint32_t mfg_mode		: 1;
+		uint32_t fpt_bad		: 1;
+		uint32_t operation_state	: 3;
+		uint32_t fw_init_complete	: 1;
+		uint32_t ft_bup_ld_flr		: 1;
+		uint32_t update_in_progress	: 1;
+		uint32_t error_code		: 4;
+		uint32_t operation_mode		: 4;
+		uint32_t reserved_0		: 4;
+		uint32_t boot_options_present	: 1;
+		uint32_t reserved_1		: 6;
+		uint32_t d0i3_support_valid	: 1;
+	} __packed fields;
+};
+
+/* Host Firmware Status Register 2 */
+union me_hfsts2 {
+	uint32_t data;
+	struct {
+		uint32_t reserved_0		: 4;
+		uint32_t cpu_replaced		: 1;
+		uint32_t reserved_1		: 3;
+		uint32_t cpu_replaced_valid	: 1;
+		uint32_t low_power_state	: 1;
+		uint32_t reserved_2		: 22;
 	} __packed fields;
 };
 
 /* ME Host Firmware Status Register 3 */
 union me_hfsts3 {
-	u32 data;
+	uint32_t data;
 	struct {
-		u32 reserved_0: 4;
-		u32 fw_sku: 3;
-		u32 reserved_7: 2;
-		u32 reserved_9: 2;
-		u32 resered_11: 3;
-		u32 resered_14: 16;
-		u32 reserved_30: 2;
+		uint32_t reserved_0		: 4;
+		uint32_t fw_sku			: 3;
+		uint32_t reserved_1		: 25;
+	} __packed fields;
+};
+
+/* Host Firmware Status Register 4 */
+union me_hfsts4 {
+	uint32_t data;
+	struct {
+		uint32_t reserved_0;
+	} __packed fields;
+};
+
+/* Host Firmware Status Register 5 */
+union me_hfsts5 {
+	uint32_t data;
+	struct {
+		uint32_t reserved_0;
+	} __packed fields;
+};
+
+/* Host Firmware Status Register 6 */
+union me_hfsts6 {
+	uint32_t data;
+	struct {
+		uint32_t reserved_0		: 1;
+		uint32_t cpu_debug_disable	: 1;
+		uint32_t reserved_1		: 29;
+		uint32_t txt_support		: 1;
 	} __packed fields;
 };
 

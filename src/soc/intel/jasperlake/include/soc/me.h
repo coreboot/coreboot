c08bacab05584275ec03eae8a94ab1ddb2b795e6
Dinesh Gehlot <digehlot@google.com>
2023-01-24 11:48:16 +0000

    soc/intel/jsk: Move ME FSR structures to pertinent header
    This patch moves ME host firmware status register structures to ME
    header file. It also marks unused structure fields to reserved.
    
    The idea here is to decouple ME specification defined structures from
    the source file `.c` and keep those into header files so that in future
    those spec defined header can move into common code.
    
    The current and future SoC platform will be able to select the correct
    ME spec header based on the applicable config. It might be also
    beneficial if two different SoC platforms would like to use the same
    ME specification and not necessarily share the same SoC directory.
    
    BUG=b:260309647
    Test=Able to build and boot.
    
    Signed-off-by: Dinesh Gehlot <digehlot@google.com>
    Change-Id: I58faed286718f5eab714cd39001177e50feb4f8b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/72414
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Lean Sheng Tan <sheng.tan@9elements.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

--- src/soc/intel/jasperlake/include/soc/me.h
+++ src/soc/intel/jasperlake/include/soc/me.h
@@ -7,23 +7,19 @@
 union me_hfsts1 {
 	uint32_t data;
 	struct {
-		uint32_t working_state: 4;
-		uint32_t mfg_mode: 1;
-		uint32_t fpt_bad: 1;
-		uint32_t operation_state: 3;
-		uint32_t fw_init_complete: 1;
-		uint32_t ft_bup_ld_flr: 1;
-		uint32_t update_in_progress: 1;
-		uint32_t error_code: 4;
-		uint32_t operation_mode: 4;
-		uint32_t reset_count: 4;
-		uint32_t boot_options_present: 1;
-		uint32_t reserved1: 1;
-		uint32_t bist_test_state: 1;
-		uint32_t bist_reset_request: 1;
-		uint32_t current_power_source: 2;
-		uint32_t reserved: 1;
-		uint32_t d0i3_support_valid: 1;
+		uint32_t working_state		: 4;
+		uint32_t mfg_mode		: 1;
+		uint32_t fpt_bad		: 1;
+		uint32_t operation_state	: 3;
+		uint32_t fw_init_complete	: 1;
+		uint32_t ft_bup_ld_flr		: 1;
+		uint32_t update_in_progress	: 1;
+		uint32_t error_code		: 4;
+		uint32_t operation_mode		: 4;
+		uint32_t reserved_0		: 4;
+		uint32_t boot_options_present	: 1;
+		uint32_t reserved_1		: 6;
+		uint32_t d0i3_support_valid	: 1;
 	} __packed fields;
 };
 
@@ -31,23 +27,12 @@ union me_hfsts1 {
 union me_hfsts2 {
 	uint32_t data;
 	struct {
-		uint32_t nftp_load_failure	: 1;
-		uint32_t icc_prog_status	: 2;
-		uint32_t invoke_mebx		: 1;
+		uint32_t reserved_0		: 4;
 		uint32_t cpu_replaced		: 1;
-		uint32_t rsvd0			: 1;
-		uint32_t mfs_failure		: 1;
-		uint32_t warm_reset_rqst	: 1;
+		uint32_t reserved_1		: 3;
 		uint32_t cpu_replaced_valid	: 1;
 		uint32_t low_power_state	: 1;
-		uint32_t me_power_gate		: 1;
-		uint32_t ipu_needed		: 1;
-		uint32_t forced_safe_boot	: 1;
-		uint32_t rsvd1			: 2;
-		uint32_t listener_change	: 1;
-		uint32_t status_data		: 8;
-		uint32_t current_pmevent	: 4;
-		uint32_t phase			: 4;
+		uint32_t reserved_2		: 22;
 	} __packed fields;
 };
 
@@ -55,9 +40,9 @@ union me_hfsts2 {
 union me_hfsts3 {
 	uint32_t data;
 	struct {
-		uint32_t reserved_0: 4;
-		uint32_t fw_sku: 3;
-		uint32_t reserved: 25;
+		uint32_t reserved_0		: 4;
+		uint32_t fw_sku			: 3;
+		uint32_t reserved_1		: 25;
 	} __packed fields;
 };
 
@@ -65,15 +50,7 @@ union me_hfsts3 {
 union me_hfsts4 {
 	uint32_t data;
 	struct {
-		uint32_t rsvd0			: 9;
-		uint32_t enforcement_flow	: 1;
-		uint32_t sx_resume_type		: 1;
-		uint32_t rsvd1			: 1;
-		uint32_t tpms_disconnected	: 1;
-		uint32_t rvsd2			: 1;
-		uint32_t fwsts_valid		: 1;
-		uint32_t boot_guard_self_test	: 1;
-		uint32_t rsvd3			: 16;
+		uint32_t reserved_0;
 	} __packed fields;
 };
 
@@ -81,18 +58,7 @@ union me_hfsts4 {
 union me_hfsts5 {
 	uint32_t data;
 	struct {
-		uint32_t acm_active		: 1;
-		uint32_t valid			: 1;
-		uint32_t result_code_source	: 1;
-		uint32_t error_status_code	: 5;
-		uint32_t acm_done_sts		: 1;
-		uint32_t timeout_count		: 7;
-		uint32_t scrtm_indicator	: 1;
-		uint32_t inc_boot_guard_acm	: 4;
-		uint32_t inc_key_manifest	: 4;
-		uint32_t inc_boot_policy	: 4;
-		uint32_t rsvd0			: 2;
-		uint32_t start_enforcement	: 1;
+		uint32_t reserved_0;
 	} __packed fields;
 };
 
@@ -100,23 +66,9 @@ union me_hfsts5 {
 union me_hfsts6 {
 	uint32_t data;
 	struct {
-		uint32_t force_boot_guard_acm	: 1;
+		uint32_t reserved_0		: 1;
 		uint32_t cpu_debug_disable	: 1;
-		uint32_t bsp_init_disable	: 1;
-		uint32_t protect_bios_env	: 1;
-		uint32_t rsvd0			: 2;
-		uint32_t error_enforce_policy	: 2;
-		uint32_t measured_boot		: 1;
-		uint32_t verified_boot		: 1;
-		uint32_t boot_guard_acmsvn	: 4;
-		uint32_t kmsvn			: 4;
-		uint32_t bpmsvn			: 4;
-		uint32_t key_manifest_id	: 4;
-		uint32_t boot_policy_status	: 1;
-		uint32_t error			: 1;
-		uint32_t boot_guard_disable	: 1;
-		uint32_t fpf_disable		: 1;
-		uint32_t fpf_soc_lock		: 1;
+		uint32_t reserved_1		: 29;
 		uint32_t txt_support		: 1;
 	} __packed fields;
 };

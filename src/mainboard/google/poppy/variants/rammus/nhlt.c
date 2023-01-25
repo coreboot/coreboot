84aa9a74e8aa70167ad5ee82e06a435d731c573a
Matt DeVillier <matt.devillier@gmail.com>
2023-01-25 16:36:39 -0600

    mb/google/poppy/rammus: Fix NHLT init
    Commit bf3c648fa7f6 ("soc/intel/skl; mb/google/eve,poppy: Update NHLT
    methods") contained a copy/paste error for rammus, swapping the max98373
    entry for the correct max98927 one. Change it back.
    
    TEST=build/boot Windows on rammus, verify audio functional with
    coolstar's AVS audio drivers.
    
    Change-Id: Ibcd4b752e01866a3dd54997f1d2a6c079b07b7a3
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/72473
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: CoolStar <coolstarorganization@gmail.com>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/google/poppy/variants/rammus/nhlt.c
+++ src/mainboard/google/poppy/variants/rammus/nhlt.c
@@ -17,7 +17,7 @@ void variant_nhlt_init(struct nhlt *nhlt)
 
 	/* Maxim MAX98927 Smart Amps for left and right channel */
 	/* Render time_slot is 0 and feedback time_slot is 2 */
-	if (nhlt_soc_add_max98373(nhlt, AUDIO_LINK_SSP0, 0, 2))
+	if (nhlt_soc_add_max98927(nhlt, AUDIO_LINK_SSP0, 0, 2))
 		printk(BIOS_ERR, "Couldn't add Maxim MAX98927\n");
 
 }

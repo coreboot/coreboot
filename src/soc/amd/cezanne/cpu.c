4e812bd29753e7f8b40c6bbcdc584c0f033a8606
Felix Held <felix-coreboot@felixheld.de>
2023-02-06 17:21:50 +0100

    soc/amd/cezanne: use CPUID_ALL_STEPPINGS_MASK to support all steppings
    Use CPUID_ALL_STEPPINGS_MASK as CPUID match mask to support all Cezanne
    steppings. This adds support for Cezanne stepping A1 and possible future
    steppings.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: Idb020052685d9369109f391797fdd8f8790a91d1
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/72849
    Reviewed-by: Fred Reitberger <reitbergerfred@gmail.com>
    Reviewed-by: Martin Roth <martin.roth@amd.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/cezanne/cpu.c
+++ src/soc/amd/cezanne/cpu.c
@@ -47,7 +47,7 @@ static struct device_operations cpu_dev_ops = {
 };
 
 static struct cpu_device_id cpu_table[] = {
-	{ X86_VENDOR_AMD, CEZANNE_A0_CPUID, CPUID_EXACT_MATCH_MASK },
+	{ X86_VENDOR_AMD, CEZANNE_A0_CPUID, CPUID_ALL_STEPPINGS_MASK },
 	{ 0, 0, 0 },
 };
 

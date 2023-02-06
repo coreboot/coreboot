286c00347d784da6d486953c2e082b575aaa50ec
Felix Held <felix-coreboot@felixheld.de>
2023-02-06 17:32:48 +0100

    soc/amd/glinda/cpu: use CPUID_ALL_STEPPINGS_MASK
    Use CPUID_ALL_STEPPINGS_MASK as CPUID match mask to support all Glinda
    steppings once GLINDA_A0_CPUID is updated.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: Ic9b8cbb2dc925a8258db6a4eb0d1b00b2745637f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/72852
    Reviewed-by: Fred Reitberger <reitbergerfred@gmail.com>
    Reviewed-by: Martin Roth <martin.roth@amd.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/glinda/cpu.c
+++ src/soc/amd/glinda/cpu.c
@@ -50,7 +50,7 @@ static struct device_operations cpu_dev_ops = {
 };
 
 static struct cpu_device_id cpu_table[] = {
-	{ X86_VENDOR_AMD, GLINDA_A0_CPUID, CPUID_EXACT_MATCH_MASK }, /* TODO: Update for Glinda */
+	{ X86_VENDOR_AMD, GLINDA_A0_CPUID, CPUID_ALL_STEPPINGS_MASK }, /* TODO: Update for Glinda */
 	{ 0, 0, 0 },
 };
 

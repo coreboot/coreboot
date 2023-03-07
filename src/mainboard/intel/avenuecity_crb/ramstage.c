61d61af6ea305c56ee189c68d35cc04e99987f22
Li, Jincheng <jincheng.li@intel.com>
2023-03-07 10:16:35 +0800

    mb/intel/avenuecity_crb: Update SMBIOS info for type 0,1,2,3
    Update wake-up type, mainboard feature flags and enclosure type.
    All other info are used from src/lib/smbios_defaults.c
    
    Change-Id: I8e68c057fefa1d408fb8d69fef066cb573c929a4
    Signed-off-by: Li, Jincheng <jincheng.li@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83328
    Reviewed-by: Lean Sheng Tan <sheng.tan@9elements.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/intel/avenuecity_crb/ramstage.c
+++ src/mainboard/intel/avenuecity_crb/ramstage.c
@@ -1,11 +1,30 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <smbios.h>
 #include <soc/dimm_slot.h>
 #include <soc/ramstage.h>
 
 void mainboard_silicon_init_params(FSPS_UPD *params)
 {
 
+}
+
+/*
+ * TODO: SMBIOS info in src/lib/smbios_defaults.c could be overridden based on needs
+ */
+smbios_wakeup_type smbios_system_wakeup_type(void)
+{
+	return SMBIOS_WAKEUP_TYPE_POWER_SWITCH;
+}
+
+u8 smbios_mainboard_feature_flags(void)
+{
+	return SMBIOS_FEATURE_FLAGS_HOSTING_BOARD | SMBIOS_FEATURE_FLAGS_REPLACEABLE;
+}
+
+smbios_enclosure_type smbios_mainboard_enclosure_type(void)
+{
+	return SMBIOS_ENCLOSURE_RACK_MOUNT_CHASSIS;
 }
 
 void smbios_fill_dimm_locator(const struct dimm_info *dimm, struct smbios_type17 *t)

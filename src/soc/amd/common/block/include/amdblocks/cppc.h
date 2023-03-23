797894d0cca170a49c3773d212c0688b2e58b702
Felix Held <felix-coreboot@felixheld.de>
2023-03-23 22:52:20 +0100

    soc/amd/common/include/cppc: rename include guard
    When the code was made common in commit 8f7f4bf87a23 ("soc/amd/cezanne,
    common: factor out CPPC code to common AMD SoC code"), the include guard
    wasn't renamed accordingly, so do that now.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I9eefe2065fae31e97aa4e6710008a6f9712bed40
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/73992
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Fred Reitberger <reitbergerfred@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/common/block/include/amdblocks/cppc.h
+++ src/soc/amd/common/block/include/amdblocks/cppc.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#ifndef AMD_CEZANNE_CPPC_H
-#define AMD_CEZANNE_CPPC_H
+#ifndef AMD_BLOCK_CPPC_H
+#define AMD_BLOCK_CPPC_H
 
 #include <types.h>
 #include <acpi/acpigen.h>
@@ -11,4 +11,4 @@ void generate_cppc_entries(unsigned int core_id);
 enum cb_err get_ccx_cppc_min_frequency(uint32_t *freq);
 enum cb_err get_ccx_cppc_nom_frequency(uint32_t *freq);
 
-#endif /* AMD_CEZANNE_CPPC_H */
+#endif /* AMD_BLOCK_CPPC_H */

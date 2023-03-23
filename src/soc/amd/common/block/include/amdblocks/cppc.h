1f1ae8ef05ba52aa8484f8a08d4410d142974291
Felix Held <felix-coreboot@felixheld.de>
2023-03-23 22:47:26 +0100

    soc/amd/common/include/cppc: remove cppc_config forward declaration
    The included acpi/acpigen.h provides the cppc_config struct and nothing
    in this header file is using the cppc_config struct.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: Ia91fd4105e6872d812f595447783d02a0dd1568b
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/73993
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Fred Reitberger <reitbergerfred@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/common/block/include/amdblocks/cppc.h
+++ src/soc/amd/common/block/include/amdblocks/cppc.h
@@ -6,7 +6,6 @@
 #include <types.h>
 #include <acpi/acpigen.h>
 
-struct cppc_config;
 void generate_cppc_entries(unsigned int core_id);
 enum cb_err get_ccx_cppc_min_frequency(uint32_t *freq);
 enum cb_err get_ccx_cppc_nom_frequency(uint32_t *freq);

30c9a10c216b0cf12e37680277569f0fce795494
Jamie Chen <jamie_chen@compal.corp-partner.google.com>
2023-03-23 19:17:43 +0800

    mb/google/brya/var/omnigul: Add ADL and RPL dptf settings
    Add Alder Lake (ADL) and Raptor Lake (RPL) dptf settings for omnigul
    
    BUG=b:273415170
    BRANCH=firmware-brya-14505.B
    TEST=FW_NAME=omnigul emerge-brya coreboot
    
    Change-Id: I8280f82ff1534ea63bcb448da231712bb4abd6d3
    Signed-off-by: Jamie Chen <jamie_chen@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/73945
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Reviewed-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

---
+++ src/mainboard/google/brya/variants/omnigul/ramstage.c
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/variants.h>
+#include <device/pci_ids.h>
+
+const struct cpu_power_limits limits[] = {
+	/* SKU_ID, TDP (Watts), pl1_min, pl1_max, pl2_min, pl2_max, pl4 */
+	{ PCI_DID_INTEL_ADL_P_ID_7, 15, 15000, 25000, 55000, 55000, 123000 },
+	{ PCI_DID_INTEL_ADL_P_ID_6, 15, 15000, 25000, 55000, 55000, 123000 },
+	{ PCI_DID_INTEL_RPL_P_ID_3, 15, 15000, 25000, 55000, 55000, 114000 },
+	{ PCI_DID_INTEL_RPL_P_ID_4, 15, 15000, 25000, 55000, 55000, 114000 },
+};
+
+void variant_devtree_update(void)
+{
+	size_t total_entries = ARRAY_SIZE(limits);
+	variant_update_power_limits(limits, total_entries);
+}

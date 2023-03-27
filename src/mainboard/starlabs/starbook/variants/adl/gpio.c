ae4b184ee0cf4551fe3d47da80caa3394a1dc87b
Sean Rhodes <sean@starlabs.systems>
2023-03-27 12:22:01 +0100

    mb/starlabs/starbook/adl: Enable OverCurrent 3 GPIO
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Change-Id: I9971209539aa7b74e55673141902b6ad0d698e4f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/73985
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/starlabs/starbook/variants/adl/gpio.c
+++ src/mainboard/starlabs/starbook/variants/adl/gpio.c
@@ -69,7 +69,7 @@ const struct pad_config gpio_table[] = {
 	/* A15:		Test Point 52					*/
 	PAD_NC(GPP_A15, NONE),
 	/* A16:		USB OverCurrent 3				*/
-	PAD_NC(GPP_A16, NONE),
+	PAD_CFG_NF(GPP_A16, NONE, DEEP, NF1),
 	/* A17:		Not Connected					*/
 	PAD_NC(GPP_A17, NONE),
 	/* A18:		DDI B DP HPD					*/

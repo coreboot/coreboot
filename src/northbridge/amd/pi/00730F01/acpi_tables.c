3454367d6401ba7d468e8915706fd022b3b5d59f
Kyösti Mälkki <kyosti.malkki@gmail.com>
2023-04-08 23:07:57 +0300

    AMD binaryPI: Use ACPI_COMMON_MADT_IOAPIC
    Change-Id: I799f61d13f7ae3ea753869ded282c14ed566793a
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/74359
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/northbridge/amd/pi/00730F01/acpi_tables.c
+++ src/northbridge/amd/pi/00730F01/acpi_tables.c
@@ -6,20 +6,8 @@
 
 unsigned long acpi_fill_madt(unsigned long current)
 {
-	/* Write SB800 IOAPIC, only one */
-	current += acpi_create_madt_ioapic_from_hw((acpi_madt_ioapic_t *)current, IO_APIC_ADDR);
-
 	/* TODO: Remove the hardcode */
 	current += acpi_create_madt_ioapic_from_hw((acpi_madt_ioapic_t *)current, IO_APIC2_ADDR);
-
-	current += acpi_create_madt_irqoverride((acpi_madt_irqoverride_t *)
-						current, 0, 0, 2, 0);
-	current += acpi_create_madt_irqoverride((acpi_madt_irqoverride_t *)
-						current, 0, 9, 9, 0xF);
-	/* 0: mean bus 0--->ISA */
-	/* 0: PIC 0 */
-	/* 2: APIC 2 */
-	/* 5 mean: 0101 --> Edge-triggered, Active high */
 
 	return current;
 }

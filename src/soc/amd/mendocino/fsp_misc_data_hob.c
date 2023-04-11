coreboot/coreboot

commit b9caac74a320a58ff0a60b33ec541f667082fd27

soc/amd/mendocino: Reinterpret smu_power_and_thm_limit
The FSP will return the TDP in the format 0xX0000, where 'X' is the
value we're interested in. For example: 0xF0000 (15W), 0x60000 (6W).
Re-interpret the value so the caller just sees the TDP directly, without
needing to re-interpret things themselves.

BUG=b:253301653
TEST=Manually verify value is correct

Change-Id: I632e702d986a4ac85605040e09c1afab2bbdc59d
Signed-off-by: Tim Van Patten <timvp@google.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/74126
Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/soc/amd/mendocino/fsp_misc_data_hob.c
@@ -43,7 +43,14 @@ enum cb_err get_amd_smu_reported_tdp(uint32_t *tdp)
 	if (get_amd_misc_data_hob(&fsp_misc_data, AMD_MISC_DATA_VERSION) != CB_SUCCESS)
 		return CB_ERR;
 
-	*tdp = fsp_misc_data->smu_power_and_thm_limit;
+	/*
+	 * The FSP will return the TDP in the format 0xX0000, where 'X' is the value
+	 * we're interested in. For example: 0xF0000 (15W), 0x60000 (6W). Re-interpret
+	 * the value so the caller just sees the TDP.
+	 */
+	printk(BIOS_INFO, "fsp_misc_data->smu_power_and_thm_limit = 0x%08X\n",
+		fsp_misc_data->smu_power_and_thm_limit);
+	*tdp = fsp_misc_data->smu_power_and_thm_limit >> 16;
 
 	return CB_SUCCESS;
 }

commit 6e4102bc9dd71aa68c4f96ed3482a315095f2318

soc/amd/mendocino: Lower log level for TDP value to DEBUG
Printing the value of a variable is not informative for a normal user,
so decrease the value from BIOS_INFO to BIOS_DEBUG.

Fixes: b9caac74a320 ("soc/amd/mendocino: Reinterpret smu_power_and_thm_limit")
Change-Id: I22f6293fd47633dfdbdae37b7257f47a5a4bb29c
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/74271
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Tim Van Patten <timvp@google.com>

src/soc/amd/mendocino/fsp_misc_data_hob.c
@@ -48,7 +48,7 @@ enum cb_err get_amd_smu_reported_tdp(uint32_t *tdp)
 	 * we're interested in. For example: 0xF0000 (15W), 0x60000 (6W). Re-interpret
 	 * the value so the caller just sees the TDP.
 	 */
-	printk(BIOS_INFO, "fsp_misc_data->smu_power_and_thm_limit = 0x%08X\n",
+	printk(BIOS_DEBUG, "fsp_misc_data->smu_power_and_thm_limit = 0x%08X\n",
 		fsp_misc_data->smu_power_and_thm_limit);
 	*tdp = fsp_misc_data->smu_power_and_thm_limit >> 16;
 
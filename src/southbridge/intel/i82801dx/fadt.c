79b864958338ac682f27d74f0fac2ae26b9a7a20
Kyösti Mälkki <kyosti.malkki@gmail.com>
2023-04-14 12:24:59 +0300

    aopen/dxplplusu: Drop ACPI C-states support
    C0 clock throttling was disabled, no need to add _PTC.
    C2/C3 latency values were copy-paste from different CPUs.
    
    TBD: Check IO-trap
    
    Change-Id: Ia0e35e28f0df8b0f8fc58f70c7d792487ee4f7f3
    Signed-off-by: Kyösti Mälkki <kyosti.malkki@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/74439
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>

--- src/southbridge/intel/i82801dx/fadt.c
+++ src/southbridge/intel/i82801dx/fadt.c
@@ -25,16 +25,12 @@ void acpi_fill_fadt(acpi_fadt_t *fadt)
 	fadt->pm1_cnt_len = 2;
 	fadt->pm_tmr_len = 4;
 	fadt->gpe0_blk_len = 8;
-	fadt->p_lvl2_lat = 1;
-	fadt->p_lvl3_lat = 85;
-	fadt->duty_offset = 1;
-	fadt->duty_width = 0;
+
 	fadt->day_alrm = 0xd;
 	fadt->mon_alrm = 0x00;
 	fadt->iapc_boot_arch = ACPI_FADT_LEGACY_DEVICES | ACPI_FADT_8042;
 
-	fadt->flags |= ACPI_FADT_WBINVD | ACPI_FADT_C1_SUPPORTED |
-			ACPI_FADT_C2_MP_SUPPORTED | ACPI_FADT_SLEEP_BUTTON |
+	fadt->flags |= ACPI_FADT_WBINVD | ACPI_FADT_C1_SUPPORTED | ACPI_FADT_SLEEP_BUTTON |
 			ACPI_FADT_S4_RTC_WAKE | ACPI_FADT_PLATFORM_CLOCK;
 
 	fadt->x_pm1a_evt_blk.space_id = ACPI_ADDRESS_SPACE_IO;

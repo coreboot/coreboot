a77c7ef758a3d9e7d802ed343a833e36a1bea3f6
Sean Rhodes <sean@starlabs.systems>
2023-04-14 21:04:27 +0100

    ec/starlabs/merlin: Update the merlin variant
    Merlin was the name for the open-source variant of the EC. It
    ended up getting entirely rewritten to work with SDCC, and is
    currently being used on starbook/adl. The source code isn't
    available at the time of this commit due to some old ITE XLT
    code being used.
    
    Add the latest version of the code, replacing the old code, so
    the boards can be migrated over.
    
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Change-Id: Ib8384fc9322058297e8219ac8e483ac37a70bd33
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/74443
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/ec/starlabs/merlin/variants/merlin/ecdefs.h
+++ src/ec/starlabs/merlin/variants/merlin/ecdefs.h
@@ -1,5 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <assert.h>
+#include <stdint.h>
+
 /*
  * EC communication interface for ITE Embedded Controller
  */
@@ -11,15 +14,15 @@
 #define ITE_CHIPID_VAL		0x5570
 
 /* EC RAM offsets */
-#define ECRAM_FN_CTRL_REVERSE	0x30
-#define ECRAM_FN_LOCK_STATE	0x31
-#define ECRAM_TRACKPAD_STATE	0x32
-#define ECRAM_KBL_BRIGHTNESS	0x33
-#define ECRAM_KBL_STATE		0x34
-#define ECRAM_KBL_TIMEOUT	0x35
-#define ECRAM_FAN_MODE		0x50
-#define ECRAM_MAX_CHARGE	0x51
-#define ECRAM_FAST_CHARGE       0x52
+#define ECRAM_KBL_BRIGHTNESS	0x09
+#define ECRAM_KBL_TIMEOUT	0x10
+#define ECRAM_KBL_STATE		0x0a
+#define ECRAM_TRACKPAD_STATE	0x0c
+#define ECRAM_FN_LOCK_STATE	0x0f
+#define ECRAM_FN_CTRL_REVERSE	0x17
+#define ECRAM_MAX_CHARGE	0x1a
+#define ECRAM_FAN_MODE		0x1b
+#define ECRAM_FAST_CHARGE	dead_code_t(uint8_t)
 #define ECRAM_MIRROR_FLAG	0x05
 
 #endif

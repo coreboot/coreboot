f7bc25f1bcd75b387f159225a127485e6494378a
Felix Held <felix-coreboot@felixheld.de>
2023-04-21 16:29:37 +0200

    soc/amd/phoenix/include/xhci: add USB4 XHCI device pointers
    Beware that there's no XHCI2 controller and the USB4 controller device
    pointers were added right after the xhci_0 and xhci_1 controller device
    pointers.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I14725d4b546ffcca42e21bbe7756babaaff8fea3
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/74658
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Martin Roth <martin.roth@amd.corp-partner.google.com>

--- src/soc/amd/phoenix/include/soc/xhci.h
+++ src/soc/amd/phoenix/include/soc/xhci.h
@@ -7,8 +7,9 @@
 
 #define SOC_XHCI_0 DEV_PTR(xhci_0)
 #define SOC_XHCI_1 DEV_PTR(xhci_1)
-#define SOC_XHCI_2 NULL
-#define SOC_XHCI_3 NULL
+/* Beware that there is no XHCI 2 on Phoenix  */
+#define SOC_XHCI_2 DEV_PTR(usb4_xhci_0) /* XHCI 3 */
+#define SOC_XHCI_3 DEV_PTR(usb4_xhci_1) /* XHCI 4 */
 #define SOC_XHCI_4 NULL
 #define SOC_XHCI_5 NULL
 #define SOC_XHCI_6 NULL

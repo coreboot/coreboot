946d17a2a53e6d2de8387ba3cd3d94ef91e36f59
Frans Hendriks <fhendriks@eltan.com>
2023-04-25 11:30:31 +0200

    mb/facebook/fbg1701/board_mboot.h: Remove config from mb_log_list
    Error message ´Cannot map compressed file config without cbfs_cache' is reported.
    
    Compressed parts of CBFS can not be used during bootblock stage.
    Remove config from mb_log_list.
    Will be added to verified items by CB:74752.
    
    BUG=NA
    TEST=booting and verify log on facebook FBG1701
    
    Change-Id: Iacf023bc8b9c2ebc66137c4ea683589751a30d2f
    Signed-off-by: Frans Hendriks <fhendriks@eltan.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/74750
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Erik van den Bogaert <ebogaert@eltan.com>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/facebook/fbg1701/board_mboot.h
+++ src/mainboard/facebook/fbg1701/board_mboot.h
@@ -4,7 +4,6 @@
 #include "board_verified_boot.h"
 
 const mboot_measure_item_t mb_log_list[] = {
-	{ "config", CBFS_TYPE_RAW, MBOOT_PCR_INDEX_0, EV_NO_ACTION, NULL },
 	{ "revision", CBFS_TYPE_RAW, MBOOT_PCR_INDEX_0, EV_NO_ACTION, NULL },
 	{ "cmos_layout.bin", CBFS_TYPE_CMOS_LAYOUT, MBOOT_PCR_INDEX_0,
 		EV_NO_ACTION, NULL },

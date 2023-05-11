75388534295e9f811f5960e579a17976c8345530
Leo Chou <leo.chou@lcfc.corp-partner.google.com>
2023-05-11 15:36:48 +0800

    mb/google/nissa/var/pujjo: Enable PCIe port 3 for WWAN_5G
    Pujjoteen5 support WWAN 5G device, enable PCIe port 3 for WWAN 5G device
    
    BUG=b:281943398
    TEST=Build and boot on pujjo
    
    Signed-off-by: Leo Chou <leo.chou@lcfc.corp-partner.google.com>
    Change-Id: I6d2e8eaecae968ed51095d9497beab492ba7e0c8
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/75153
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Derek Huang <derekhuang@google.com>

--- src/mainboard/google/brya/variants/pujjo/fw_config.c
+++ src/mainboard/google/brya/variants/pujjo/fw_config.c
@@ -8,12 +8,20 @@
 static const struct pad_config lte_disable_pads[] = {
 	/* A8  : WWAN_RF_DISABLE_ODL */
 	PAD_NC(GPP_A8, NONE),
+	/* A12 : WWAN_PCIE_WAKE_ODL */
+	PAD_NC(GPP_A12, NONE),
+	/* D5  : SRCCLKREQ0# ==> WWAN_CLKREQ_ODL */
+	PAD_NC(GPP_D5, NONE),
 	/* D6  : WWAN_EN */
 	PAD_NC(GPP_D6, NONE),
+	/* D15  : EN_PP2800_WCAM_X ==> WWAN_SAR_DETECT_2_ODL */
+	PAD_NC(GPP_D15, NONE),
 	/* F12 : WWAN_RST_L */
 	PAD_NC_LOCK(GPP_F12, NONE, LOCK_CONFIG),
 	/* H19 : SOC_I2C_SUB_INT_ODL */
 	PAD_NC(GPP_H19, NONE),
+	/* H21  : WCAM_MCLK_R ==> WWAN_PERST_L */
+	PAD_NC_LOCK(GPP_H21, NONE, LOCK_CONFIG),
 	/* H23 : WWAN_SAR_DETECT_ODL */
 	PAD_NC(GPP_H23, NONE),
 };
@@ -36,7 +44,8 @@ static const struct pad_config stylus_disable_pads[] = {
 
 void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
 {
-	if (!fw_config_probe(FW_CONFIG(LTE, LTE_PRESENT))) {
+	if (fw_config_probe(FW_CONFIG(LTE, LTE_ABSENT)) && fw_config_probe(FW_CONFIG(WWAN_5G, WWAN_5G_ABSENT))
+	) {
 		printk(BIOS_INFO, "Disable LTE-related GPIO pins.\n");
 		gpio_padbased_override(padbased_table, lte_disable_pads,
 						ARRAY_SIZE(lte_disable_pads));
--- src/mainboard/google/brya/variants/pujjo/include/variant/gpio.h
+++ src/mainboard/google/brya/variants/pujjo/include/variant/gpio.h
@@ -7,6 +7,8 @@
 
 #define WWAN_FCPO	GPP_D6
 #define WWAN_RST	GPP_F12
+#define WWAN_PERST      GPP_H21
+#define T1_OFF_MS	20
 #define T2_OFF_MS	20
 
 #endif

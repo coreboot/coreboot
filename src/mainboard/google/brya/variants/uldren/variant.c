a6d337badf0dbc6e56c9debc3062ce7f85ce863b
Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
2023-05-18 17:28:42 +0800

    mb/google/nissa/var/uldren: Add fw_config probe for touchscreen
    Use fw_config to probe touchscreen.
    
    BUG=b:283199751
    BRANCH=firmware-nissa-15217.B
    TEST=emerge-nissa coreboot chromeos-bootimage
    
    Change-Id: I5d8129b3af3aa09e5bc31160de82d9ef7af0dd59
    Signed-off-by: Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/75335
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Jakub Czapiga <jacz@semihalf.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>

--- src/mainboard/google/brya/variants/uldren/variant.c
+++ src/mainboard/google/brya/variants/uldren/variant.c
@@ -1,8 +1,31 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <console/console.h>
 #include <sar.h>
+
+static const struct pad_config touchscreen_disable_pads[] = {
+	/* C0  : SMBCLK ==> EN_PP3300_TCHSCR_X */
+	PAD_NC(GPP_C0, NONE),
+	/* C1  : SMBDATA ==> TCHSCR_RST_L */
+	PAD_NC(GPP_C1, NONE),
+	/* H6  : I2C1_SDA ==> SOC_I2C_TCHSCR_SDA */
+	PAD_NC(GPP_H6, NONE),
+	/* H7  : I2C1_SCL ==> SOC_I2C_TCHSCR_SCL */
+	PAD_NC(GPP_H7, NONE),
+};
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
 	return "wifi_sar_0.hex";
+}
+
+void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
+{
+	if (fw_config_probe(FW_CONFIG(TOUCHSCREEN, TOUCHSCREEN_NONE))) {
+		printk(BIOS_INFO, "Disable touchscreen GPIO pins.\n");
+		gpio_padbased_override(padbased_table, touchscreen_disable_pads,
+						ARRAY_SIZE(touchscreen_disable_pads));
+	}
 }

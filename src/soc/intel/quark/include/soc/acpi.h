531023285ea4108d5b847625739d76992ce39200
Felix Singer <felixsinger@posteo.net>
2023-05-19 15:32:35 +0200

    soc/intel/quark: Drop support
    As announced in the 4.20 release notes, support for the Intel Quark SoC
    is moved to the 4.20 branch and dropped from master.
    
    Change-Id: I8a1ca7a2092aaeaea9c72eac5a8dd8f7d72e8f09
    Signed-off-by: Felix Singer <felixsinger@posteo.net>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/75341
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: ron minnich <rminnich@gmail.com>
    Reviewed-by: Martin L Roth <gaumless@gmail.com>

--- src/soc/intel/quark/include/soc/acpi.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-
-#ifndef _SOC_ACPI_H_
-#define _SOC_ACPI_H_
-
-#include <acpi/acpi.h>
-#include <acpi/acpigen.h>
-
-#endif /* _SOC_ACPI_H_ */
--- src/soc/intel/quark/tsc_freq.c
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-
-#include <cpu/x86/tsc.h>
-
-unsigned long tsc_freq_mhz(void)
-{
-	/* CPU freq = 400 MHz */
-	return 400;
-}

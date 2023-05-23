ce3c77c30536649ba2a6ab81979a7fa371128c31
Naresh Solanki <Naresh.Solanki@9elements.com>
2023-05-23 18:52:24 +0200

    mb/ibm/sbp1: Set coreboot ready GPIO in BS_PAYLOAD_BOOT
    Set coreboot ready gpio. This gpio is used to indicate to BMC of BIOS
    completion.
    
    Change-Id: Iaed8bec12e593cf1687d973765b0117bdc115cb8
    Signed-off-by: Naresh Solanki <naresh.solanki@9elements.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76404
    Reviewed-by: Patrick Rudolph <siro@das-labor.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/ibm/sbp1/ramstage.c
+++ src/mainboard/ibm/sbp1/ramstage.c
@@ -3,9 +3,19 @@
 #include <soc/ramstage.h>
 
 #include "include/spr_sbp1_gpio.h"
+#include <bootstate.h>
 
 void mainboard_silicon_init_params(FSPS_UPD *params)
 {
 	/* configure Emmitsburg PCH GPIO controller after FSP-M */
 	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
 }
+
+static void finalize_boot(void *unused)
+{
+	printk(BIOS_DEBUG, "FM_BIOS_POST_CMPLT_N cleared.\n");
+	/* Clear FM_BIOS_POST_CMPLT_N */
+	gpio_output(GPPC_C17, 0);
+}
+
+BOOT_STATE_INIT_ENTRY(BS_PAYLOAD_BOOT, BS_ON_ENTRY, finalize_boot, NULL);

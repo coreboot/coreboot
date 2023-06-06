2813c7c10c54e176ea0cd5413a8eb78f1664a1c9
Eric Lai <eric_lai@quanta.corp-partner.google.com>
2023-06-06 10:58:33 +0800

    mb/google/myst: Correct CROS_WP_GPIO to active high
    HW has invert the signal, set it to active high.
    
    BUG=b:285964562
    TEST=check crossystem wpsw_cur change as expected.
    
    Signed-off-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Change-Id: I54c578e5df5f1b24743cc9506e1e31b0b18bfb25
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/75628
    Reviewed-by: Jon Murphy <jpmurphy@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Tim Van Patten <timvp@google.com>
    Reviewed-by: Ivy Jian <ivy.jian@quanta.corp-partner.google.com>

--- src/mainboard/google/myst/chromeos.c
+++ src/mainboard/google/myst/chromeos.c
@@ -8,6 +8,6 @@ void fill_lb_gpios(struct lb_gpios *gpios) {}
 
 static const struct cros_gpio cros_gpios[] = {
 	CROS_GPIO_REC_AL(CROS_GPIO_VIRTUAL, GPIO_DEVICE_NAME),
-	CROS_GPIO_WP_AL(CROS_WP_GPIO, GPIO_DEVICE_NAME),
+	CROS_GPIO_WP_AH(CROS_WP_GPIO, GPIO_DEVICE_NAME),
 };
 DECLARE_CROS_GPIOS(cros_gpios);

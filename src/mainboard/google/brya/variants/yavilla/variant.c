204a8a4a64f6940c1800407df95b90018df4c1ac
Shon Wang <shon.wang@quanta.corp-partner.google.com>
2023-06-07 09:47:40 +0800

    mb/google/nissa/var/yavilla: Enable wifi SAR
    Enable wifi sar function for yavilla/yavilly/yavijo.
    Use the fw_config to separate SAR setting for different wifi card.
    
    BUG=b:286141046
    BRANCH=firmware-nissa-15217.B
    TEST=build, enabled iwlwifi debug, and check dmesg
    
    Change-Id: I1bd111a734a250df49535a07ef056d5b68fccb33
    Signed-off-by: Shon Wang <shon.wang@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/75673
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Derek Huang <derekhuang@google.com>

--- src/mainboard/google/brya/variants/yavilla/variant.c
+++ src/mainboard/google/brya/variants/yavilla/variant.c
@@ -4,6 +4,7 @@
 #include <boardid.h>
 #include <device/device.h>
 #include <fw_config.h>
+#include <sar.h>
 
 void variant_devtree_update(void)
 {
@@ -26,4 +27,9 @@ void variant_devtree_update(void)
 		ufs->enabled = 0;
 		ish->enabled = 0;
 	}
+}
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return get_wifi_sar_fw_config_filename(FW_CONFIG_FIELD(WIFI_SAR_ID2));
 }

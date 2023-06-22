f3aa88a51cefc12da3304cd827bbfd6e7fcbfe76
Arthur Heymans <arthur@aheymans.xyz>
2023-06-22 21:41:51 +0200

    arch/arm64: Hook up ACPI table generation
    Linux v6.3.5 is able to detect and use ACPI tables on an out of tree
    target using hacked version of u-boot to pass ACPI through UEFI.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Change-Id: I4f60c546ec262ffb4d447fe6476844cf5a1b756d
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76071
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Lean Sheng Tan <sheng.tan@9elements.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>

--- src/arch/arm64/tables.c
+++ src/arch/arm64/tables.c
@@ -1,12 +1,27 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <bootmem.h>
-#include <boot/tables.h>
+#include <acpi/acpi.h>
+#include <assert.h>
 #include <boot/coreboot_tables.h>
+#include <boot/tables.h>
+#include <bootmem.h>
+#include <cbmem.h>
 #include <symbols.h>
+
+static void write_acpi_table(void)
+{
+	const size_t max_acpi_size = CONFIG_MAX_ACPI_TABLE_SIZE_KB * KiB;
+	const uintptr_t acpi_start = (uintptr_t)cbmem_add(CBMEM_ID_ACPI, max_acpi_size);
+	assert(IS_ALIGNED(acpi_start, 16));
+	const uintptr_t acpi_end = write_acpi_tables(acpi_start);
+	assert(acpi_end < acpi_start + max_acpi_size);
+	printk(BIOS_DEBUG, "ACPI tables: %ld bytes.\n", acpi_end - acpi_start);
+}
 
 void arch_write_tables(uintptr_t coreboot_table)
 {
+	if (CONFIG(HAVE_ACPI_TABLES))
+		write_acpi_table();
 }
 
 void bootmem_arch_add_ranges(void)

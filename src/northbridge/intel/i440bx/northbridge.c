a4b391bf816aec20a3f175ee89b54ea9fa851110
Arthur Heymans <arthur@aheymans.xyz>
2023-07-05 09:25:38 +0200

    nb/i440bx: Use newer function for resource declarations
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Change-Id: I61ae378867f8c0d9e86092ebe8deec53800c4717
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76271
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Lean Sheng Tan <sheng.tan@9elements.com>
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/northbridge/intel/i440bx/northbridge.c
+++ src/northbridge/intel/i440bx/northbridge.c
@@ -61,8 +61,8 @@ static void i440bx_domain_read_resources(struct device *dev)
 
 		/* Report the memory regions. */
 		idx = 10;
-		ram_resource_kb(dev, idx++, 0, 640);
-		ram_resource_kb(dev, idx++, 768, tolmk - 768);
+		ram_range(dev, idx++, 0, 0xa0000);
+		ram_from_to(dev, idx++, 0xc0000, tolmk * KiB);
 	}
 }
 

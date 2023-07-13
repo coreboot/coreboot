2118fb1f69009d65abc0d0ab7faad0b7fc5a0bdc
Karthikeyan Ramasubramanian <kramasub@google.com>
2023-07-13 11:12:16 -0600

    soc/amd/phoenix: Disable APOB Cache
    There is a data abort in ABL when the memory training data is used from
    APOB Cache. Disable APOB Cache until the cause is identified. The
    downside of this change is that the memory training happens in every
    boot cycle.
    
    BUG=b:290763369
    TEST=Build BIOS image and boot to OS in Myst. Trigger a reboot from AP
    console and ensure that the system boots to OS.
    
    Change-Id: I20f4f40cdaac68bca6e121e3a238d13fe80d0d3c
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76422
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin Roth <martin.roth@amd.corp-partner.google.com>
    Reviewed-by: Matt DeVillier <matt.devillier@amd.corp-partner.google.com>

--- src/soc/amd/common/block/include/amdblocks/apob_cache.h
+++ src/soc/amd/common/block/include/amdblocks/apob_cache.h
@@ -3,8 +3,15 @@
 #ifndef AMD_BLOCK_APOB_CACHE_H
 #define AMD_BLOCK_APOB_CACHE_H
 
+#include <stddef.h>
+
+#if CONFIG(SOC_AMD_COMMON_BLOCK_APOB)
 /* Start loading the APOB as soon as possible so it is ready by the time we need it. */
 void start_apob_cache_read(void);
 void *soc_fill_apob_cache(void);
+#else /* CONFIG(SOC_AMD_COMMON_BLOCK_APOB) */
+static inline void start_apob_cache_read(void) {}
+static inline void *soc_fill_apob_cache(void) { return NULL; }
+#endif /* CONFIG(SOC_AMD_COMMON_BLOCK_APOB) */
 
 #endif /* AMD_BLOCK_APOB_CACHE_H */

22c616e6f5420dc902a62163a48305c17dab8087
Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
2023-07-13 11:22:54 +0800

    mb/google/nissa/var/uldren: Add FW_CONFIG probe for fivr
    Uldren will support internal fivr in next phase and using fw_config to
    decide the board with internal or external fivr.
    
    BUG=b:287379760
    BRANCH=firmware-nissa-15217.B
    TEST=boot to ChromeOS, cold reboot/suspend/recovery mode/install OS
    work normally.
    
    Change-Id: I8a1ac60f599f2895654946d9fa1c4e1f2657fd10
    Signed-off-by: Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76419
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Ian Feng <ian_feng@compal.corp-partner.google.com>

--- src/mainboard/google/brya/variants/uldren/variant.c
+++ src/mainboard/google/brya/variants/uldren/variant.c
@@ -43,3 +43,26 @@ void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
 						ARRAY_SIZE(lte_disable_pads));
 	}
 }
+
+void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
+{
+	if (fw_config_probe(FW_CONFIG(EXT_VR, EXT_VR_PRESENT))) {
+		config->ext_fivr_settings.configure_ext_fivr = 1;
+		config->ext_fivr_settings.v1p05_enable_bitmap =
+				FIVR_ENABLE_ALL_SX & ~FIVR_ENABLE_S0;
+		config->ext_fivr_settings.vnn_enable_bitmap =
+				FIVR_ENABLE_ALL_SX;
+		config->ext_fivr_settings.vnn_sx_enable_bitmap =
+				FIVR_ENABLE_ALL_SX;
+		config->ext_fivr_settings.v1p05_supported_voltage_bitmap =
+				FIVR_VOLTAGE_NORMAL;
+		config->ext_fivr_settings.vnn_supported_voltage_bitmap =
+				FIVR_VOLTAGE_MIN_ACTIVE;
+		config->ext_fivr_settings.v1p05_voltage_mv = 1050;
+		config->ext_fivr_settings.vnn_voltage_mv = 780;
+		config->ext_fivr_settings.vnn_sx_voltage_mv = 1050;
+		config->ext_fivr_settings.v1p05_icc_max_ma = 500;
+		config->ext_fivr_settings.vnn_icc_max_ma = 500;
+		printk(BIOS_INFO, "Configured External FIVR\n");
+	}
+}

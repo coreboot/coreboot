0f5f2ceb550c7c830950cd3e5b8f277c37dda185
Felix Held <felix-coreboot@felixheld.de>
2023-07-14 19:36:20 +0200

    sb/amd/pi/hudson/enable_usbdebug: use pm_io_write8
    Use pm_io_write8 instead of open coding the same functionality.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I1d9397f2d85e48883f961adbbca0e1e71e825ce0
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76462
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin L Roth <gaumless@gmail.com>

--- src/southbridge/amd/pi/hudson/enable_usbdebug.c
+++ src/southbridge/amd/pi/hudson/enable_usbdebug.c
@@ -4,7 +4,7 @@
 #define __SIMPLE_DEVICE__
 
 #include <stdint.h>
-#include <arch/io.h>
+#include <amdblocks/acpimmio.h>
 #include <device/mmio.h>
 #include <device/pci_ehci.h>
 #include <device/pci_def.h>
@@ -15,8 +15,7 @@
 pci_devfn_t pci_ehci_dbg_dev(unsigned int hcd_idx)
 {
 	/* Enable all of the USB controllers */
-	outb(0xEF, PM_INDEX);
-	outb(0x7F, PM_DATA);
+	pm_io_write8(0xef, 0x7f);
 
 	if (hcd_idx == 3)
 		return PCI_DEV(0, 0x16, 0);

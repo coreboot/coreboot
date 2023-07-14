2e2f1661bb306d7b00b4f121148e9613c5d14e72
Arthur Heymans <arthur@aheymans.xyz>
2023-07-14 22:58:49 +0200

    soc/amd/genoa: Hook SMP and SMM init
    All CPUs properly come out of reset and relocate SMM.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Change-Id: I8c2d976addacd5a2ba70eb629510128853b9f847
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76523
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

---
+++ src/soc/amd/genoa/cpu.c
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <amdblocks/cpu.h>
+#include <cpu/cpu.h>
+#include <device/device.h>
+#include <soc/cpu.h>
+
+static void model_19_init(struct device *dev)
+{
+	set_cstate_io_addr();
+}
+
+static struct device_operations cpu_dev_ops = {
+	.init = model_19_init,
+};
+
+static struct cpu_device_id cpu_table[] = {
+	{ X86_VENDOR_AMD, GENOA_A0_CPUID, CPUID_ALL_STEPPINGS_MASK },
+	{ X86_VENDOR_AMD, GENOA_B0_CPUID, CPUID_ALL_STEPPINGS_MASK },
+	CPU_TABLE_END
+};
+
+static const struct cpu_driver model_19 __cpu_driver = {
+	.ops      = &cpu_dev_ops,
+	.id_table = cpu_table,
+};

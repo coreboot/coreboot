7bb960d7dfc4a90d7e007070c668e5cd95dccabf
Fred Reitberger <reitbergerfred@gmail.com>
2023-07-17 08:26:02 -0400

    soc/amd/common/block/apob: Add Kconfig option to disable APOB NV
    Add Kconfig option to disable the non-volatile APOB cache for a
    mainboard using an SOC that supports APOB.
    
    BUG=b:290763369
    TEST=verify APOB cache is disabled when selected
    
    Signed-off-by: Fred Reitberger <reitbergerfred@gmail.com>
    Change-Id: I0170355bbf29ea6386fa69a318e61f057b9a9a3f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76566
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin L Roth <gaumless@gmail.com>

--- src/soc/amd/common/block/include/amdblocks/apob_cache.h
+++ src/soc/amd/common/block/include/amdblocks/apob_cache.h
@@ -5,13 +5,13 @@
 
 #include <stddef.h>
 
-#if CONFIG(SOC_AMD_COMMON_BLOCK_APOB)
+#if CONFIG(SOC_AMD_COMMON_BLOCK_APOB) && !CONFIG(SOC_AMD_COMMON_BLOCK_APOB_NV_DISABLE)
 /* Start loading the APOB as soon as possible so it is ready by the time we need it. */
 void start_apob_cache_read(void);
 void *soc_fill_apob_cache(void);
-#else /* CONFIG(SOC_AMD_COMMON_BLOCK_APOB) */
+#else /* CONFIG(SOC_AMD_COMMON_BLOCK_APOB) && !CONFIG(SOC_AMD_COMMON_BLOCK_APOB_NV_DISABLE) */
 static inline void start_apob_cache_read(void) {}
 static inline void *soc_fill_apob_cache(void) { return NULL; }
-#endif /* CONFIG(SOC_AMD_COMMON_BLOCK_APOB) */
+#endif /* CONFIG(SOC_AMD_COMMON_BLOCK_APOB) && !CONFIG(SOC_AMD_COMMON_BLOCK_APOB_NV_DISABLE) */
 
 #endif /* AMD_BLOCK_APOB_CACHE_H */

6e0de5d9cc8ba6aefc5407f91a6c00781d19ab8e
Patrick Rudolph <patrick.rudolph@9elements.com>
2023-07-20 08:56:54 +0200

    mb/ibm/sbp1: Drop SuperIO code
    The SuperIO is not used so don't enable decoding of 0xE2 and
    drop all code using it. It's not even required for the virtual
    UART on 0x3f8 to work.
    
    Add the virtual UART on 0x3f8 as ACPI device.
    
    TEST: Verified on SBP1 that serial still works.
    
    Change-Id: I8e431a0c8417435cc6e3ba16f97ff080e1656a7b
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76676
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kyösti Mälkki <kyosti.malkki@gmail.com>

--- src/mainboard/ibm/sbp1/acpi/platform.asl
+++ src/mainboard/ibm/sbp1/acpi/platform.asl
@@ -21,6 +21,19 @@ Field (DBG0, ByteAcc, Lock, Preserve)
 	IO81, 8
 }
 
+/* Virtual UART on 0x3f8 */
+Scope(\_SB) {
+	Device(COM1) {
+		Name(_HID, EisaId("PNP0501"))
+		Name(_UID, 0x01)
+		Name(_STA,0x0F)
+		Name(_CRS, ResourceTemplate() {
+			IO(Decode16, 0x03F8, 0x03F8, 0x00, 0x08)
+			IRQNoFlags() { 4 }
+		})
+	}
+}
+
 /*
  * The _PTS method (Prepare To Sleep) is called before the OS is
  * entering a sleep state. The sleep state number is passed in Arg0
--- src/mainboard/ibm/sbp1/bootblock.c
+++ src/mainboard/ibm/sbp1/bootblock.c
@@ -8,24 +8,21 @@
 #include <soc/intel/common/block/lpc/lpc_def.h>
 #include <soc/pci_devs.h>
 #include <soc/pcr_ids.h>
-#include <superio/aspeed/ast2400/ast2400.h>
-#include <superio/aspeed/common/aspeed.h>
 
-#define ASPEED_SIO_PORT 0x2E
 #define PCR_DMI_LPCIOD  0x2770
 #define PCR_DMI_LPCIOE  0x2774
 
 void bootblock_mainboard_early_init(void)
 {
 	uint16_t lpciod = LPC_IOD_COMA_RANGE;
-	uint16_t lpcioe = (LPC_IOE_SUPERIO_2E_2F | LPC_IOE_COMA_EN);
+	uint16_t lpcioe = LPC_IOE_COMA_EN;
 
 	/* Open IO windows: 0x3f8 for com1 */
 	pcr_or32(PID_DMI, PCR_DMI_LPCIOD, lpciod);
 	/* LPC I/O enable: com1 */
 	pcr_or32(PID_DMI, PCR_DMI_LPCIOE, lpcioe);
 
-	/* Enable com1 (0x3f8) and superio (0x2e) */
+	/* Enable com1 (0x3f8) */
 	pci_write_config16(PCH_DEV_LPC, LPC_IO_DECODE, lpciod);
 	pci_write_config16(PCH_DEV_LPC, LPC_IO_ENABLES, lpcioe);
 }

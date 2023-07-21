b4a8937045daec20bdea78221f7178a71339e8a5
Keith Hui <buurin@gmail.com>
2023-07-21 19:26:13 -0400

    mb/asus/p8z77-m_pro: Drop useless early init code
    Drop code that puts Super I/O into config mode, select serial device,
    then leave config mode right away having done nothing.
    
    I'll also take this chance to revise its #includes based on
    include-what-you-use results.
    
    Change-Id: I304fc1610740375b59121b6b8784122440795838
    Signed-off-by: Keith Hui <buurin@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/73693
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/asus/p8x7x-series/variants/p8z77-m_pro/early_init.c
+++ src/mainboard/asus/p8x7x-series/variants/p8z77-m_pro/early_init.c
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootblock_common.h>
-#include <device/pnp_ops.h>
+#include <device/pnp_type.h>
 #include <southbridge/intel/bd82x6x/pch.h>
 
 #include <superio/nuvoton/common/nuvoton.h>
@@ -12,7 +12,6 @@
 #include <northbridge/intel/sandybridge/raminit.h>
 #include <northbridge/intel/sandybridge/pei_data.h>
 
-#define GLOBAL_DEV PNP_DEV(0x2e, 0)
 #define SERIAL_DEV PNP_DEV(0x2e, NCT6779D_SP2)
 
 const struct southbridge_usb_port mainboard_usb_ports[] = {
@@ -35,13 +34,6 @@ const struct southbridge_usb_port mainboard_usb_ports[] = {
 
 void bootblock_mainboard_early_init(void)
 {
-	/* Setup COM/UART */
-	nuvoton_pnp_enter_conf_state(GLOBAL_DEV);
-
-	/* TODO / FIXME: Setup Multifuncion/SIO pins for COM */
-
-	pnp_set_logical_device(SERIAL_DEV);
-	nuvoton_pnp_exit_conf_state(GLOBAL_DEV);
 	nuvoton_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
 }
 

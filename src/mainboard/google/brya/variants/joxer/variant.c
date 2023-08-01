7333901701c4549a6bbe3d85f7ed7834cb9f63c8
Mark Hsieh <mark_hsieh@wistron.corp-partner.google.com>
2023-08-01 19:16:36 +0800

    mb/google/nissa/var/joxer: support DPTF oem_variables
    1. Joxer uses dptf.dv to distinguish 6W/15W by setting OEM variable.
    2. Update passive policy and critical policy.
    
    BUG=b:285477026, b:293540179
    TEST=emerge-nissa coreboot and check the OEM variable.
    
    Signed-off-by: Mark Hsieh <mark_hsieh@wistron.corp-partner.google.com>
    Change-Id: I4e52ac624f7d7628cce3035a2bac67fc527bc167
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76773
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>

--- src/mainboard/google/brya/variants/joxer/variant.c
+++ src/mainboard/google/brya/variants/joxer/variant.c
@@ -3,9 +3,33 @@
 #include <baseboard/variants.h>
 #include <console/console.h>
 #include <device/device.h>
+#include <device/pci.h>
+#include <drivers/intel/dptf/chip.h>
 #include <fw_config.h>
 #include <soc/bootblock.h>
 #include <sar.h>
+
+WEAK_DEV_PTR(dptf_policy);
+
+static void update_oem_variables(void)
+{
+	const struct device *policy_dev = DEV_PTR(dptf_policy);
+	struct drivers_intel_dptf_config *config = policy_dev->chip_info;
+
+	if (fw_config_probe(FW_CONFIG(THERMAL, THERMAL_FANLESS))) {
+		config->oem_data.oem_variables[0] = 0;
+		config->oem_data.oem_variables[1] = 1;
+		printk(BIOS_INFO, "Set OEM Variable [0] to 0.\n");
+		printk(BIOS_INFO, "Set OEM Variable [1] to 1.\n");
+	}
+
+	if (fw_config_probe(FW_CONFIG(THERMAL, THERMAL_FAN))) {
+		config->oem_data.oem_variables[0] = 1;
+		config->oem_data.oem_variables[1] = 0;
+		printk(BIOS_INFO, "Set OEM Variable [0] to 1.\n");
+		printk(BIOS_INFO, "Set OEM Variable [1] to 0.\n");
+	}
+}
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
@@ -17,6 +41,8 @@ void variant_devtree_update(void)
 	struct device *emmc = DEV_PTR(emmc);
 	struct device *ufs = DEV_PTR(ufs);
 	struct device *ish = DEV_PTR(ish);
+
+	update_oem_variables();
 
 	if (!fw_config_is_provisioned()) {
 		printk(BIOS_INFO, "fw_config unprovisioned so enable all storage devices\n");

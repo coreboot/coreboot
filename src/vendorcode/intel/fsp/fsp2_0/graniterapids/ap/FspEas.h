619535778cf7c6b141f8c13b967995327108a50a
Jincheng Li <jincheng.li@intel.com>
2023-08-01 09:47:48 +0800

    vc/intel/fsp/fsp2_0: Add GNR N-1 FSP headers
    GNR N-1 FSP headers are a set of stub headers used to fulfill
    build sanity check for GNR SoC and CRB codes before the formal
    FSP headers are published. The N-1 headers are forward compatible
    with the later formal headers.
    
    Change-Id: I1c8125dd64e5a9619073c2f17aeade1d33607870
    Signed-off-by: Shuo Liu <shuo.liu@intel.com>
    Signed-off-by: Jincheng Li <jincheng.li@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/80633
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Lean Sheng Tan <sheng.tan@9elements.com>

---
+++ src/vendorcode/intel/fsp/fsp2_0/graniterapids/ap/FspEas.h
@@ -0,0 +1,17 @@
+/** @file
+  Intel FSP definition from Intel Firmware Support Package External
+  Architecture Specification v2.2.
+ @copyright
+  Copyright (c) 2015 - 2021, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef _FSP_EAS_H_
+#define _FSP_EAS_H_
+
+#include <Uefi.h>
+#include <Guid/GuidHobFspEas.h>
+#include <Guid/FspHeaderFile.h>
+#include <FspEas/FspApi.h>
+
+#endif // _FSP_EAS_H_

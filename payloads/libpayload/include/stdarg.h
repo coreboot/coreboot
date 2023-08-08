99a8287f08424d00d0cf09886b967983789c2368
Maximilian Brune <maximilian.brune@9elements.com>
2023-08-08 21:43:18 +0200

    libpayload/include/stdarg.h: Add va_copy builtin
    Add the builtin to copy variadic lists/arguments.
    
    Signed-off-by: Maximilian Brune <maximilian.brune@9elements.com>
    Change-Id: I4507b901bdce052c5d1701fdf825eb8a96a5b55a
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77097
    Reviewed-by: Martin L Roth <gaumless@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- payloads/libpayload/include/stdarg.h
+++ payloads/libpayload/include/stdarg.h
@@ -40,6 +40,7 @@
 #define va_start(v,l)		__builtin_va_start(v,l)
 #define va_end(v)		__builtin_va_end(v)
 #define va_arg(v,l)		__builtin_va_arg(v,l)
+#define va_copy(d,s)		__builtin_va_copy(d,s)
 typedef __builtin_va_list	va_list;
 #else
 #include_next <stdarg.h>

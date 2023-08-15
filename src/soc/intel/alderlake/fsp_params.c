03dfc212579e16898de5107bf8e9fc03e51ef669
Subrata Banik <subratabanik@google.com>
2023-08-16 02:50:16 +0530

    soc/intel/alderlake: Add provision to show pre-boot splash screen
    This patch adds the ability to show a pre-boot splash screen on
    Meteor Lake systems using FSP-S.
    
    The patch calls into `fsp_convert_bmp_to_gop_blt()` when the
    `BMP_LOGO` config is enabled. This function converts a BMP
    file to a BLT buffer, which is then used by FSP-S to render the splash
    screen.
    
    Additionally, increase the heap size (malloc'able size) upto 512KB
    (when BMP_LOGO config is enabled) to accommodate high
    resolution logo file.
    
    BUG=b:284799726
    TEST=Able to see splash screen while booting google/marasov
    with BMP_LOGO config enable.
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: I9f4d1bc0aa991e784624ca19ba96a259ab8ddfa6
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77233
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>

--- src/soc/intel/alderlake/fsp_params.c
+++ src/soc/intel/alderlake/fsp_params.c
@@ -12,6 +12,7 @@
 #include <drivers/intel/gma/i915_reg.h>
 #include <fsp/api.h>
 #include <fsp/fsp_debug_event.h>
+#include <fsp/fsp_gop_blt.h>
 #include <fsp/ppi/mp_service_ppi.h>
 #include <fsp/util.h>
 #include <gpio.h>
@@ -1371,5 +1372,10 @@ __weak void mainboard_silicon_init_params(FSP_S_CONFIG *s_cfg)
 /* Handle FSP logo params */
 void soc_load_logo(FSPS_UPD *supd)
 {
-	bmp_load_logo(&supd->FspsConfig.LogoPtr, &supd->FspsConfig.LogoSize);
+	fsp_convert_bmp_to_gop_blt(&supd->FspsConfig.LogoPtr,
+			 &supd->FspsConfig.LogoSize,
+			 &supd->FspsConfig.BltBufferAddress,
+			 &supd->FspsConfig.BltBufferSize,
+			 &supd->FspsConfig.LogoPixelHeight,
+			 &supd->FspsConfig.LogoPixelWidth);
 }

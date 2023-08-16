b6392ef4d7ffe2dbef91c5f2c0d705c8d6acca17
Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
2023-08-16 14:18:40 +0800

    mb/google/brask/var/constitution: Separate wifi sar table
    Separate constitution and intrepid wifi sar table in variant.c
    
    BUG=b:291859402
    BRANCH=firmware-brya-14505.B
    TEST=emerge-constitution coreboot chromeos-bootimage
    
    Change-Id: I0f89b3d5f5252a2b55bad4d91ad4ab9ec7519c50
    Signed-off-by: Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77242
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Martin L Roth <gaumless@gmail.com>
    Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/constitution/variant.c
+++ src/mainboard/google/brya/variants/constitution/variant.c
@@ -1,8 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <console/console.h>
+#include <fw_config.h>
 #include <sar.h>
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
-	return "wifi_sar_0.hex";
+	if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, ID_0))) {
+		return "wifi_sar_0.hex";
+	}
+	return NULL;
 }

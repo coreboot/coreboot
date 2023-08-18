ff153965cd07410ff97630ed2ac086c54d17f7e5
Robert Chen <robert.chen@quanta.corp-partner.google.com>
2023-08-18 04:38:03 -0400

    mb/google/nissa/var/quandiso: Update initial files based on yavilla
    Update files copied from yavilla
    - fw_config setting
    - GPIO setting
    - Kconfig setting
    - overridetree setting
    - SPD memory parts
    - variant setting
    
    BUG=b:296506936
    BRANCH=firmware-nissa-15217.B
    TEST=emerge-nissa coreboot chromeos-bootimage
    flash bin file in DUT
    
    Change-Id: Ibbef42a1f891d0cf0309aa76edd7ec5dd664588e
    Signed-off-by: Robert Chen <robert.chen@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77361
    Reviewed-by: Martin L Roth <gaumless@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/brya/variants/quandiso/fw_config.c
@@ -0,0 +1,101 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <console/console.h>
+#include <fw_config.h>
+
+
+static const struct pad_config lte_disable_pads[] = {
+	/* A8  : WWAN_RF_DISABLE_ODL */
+	PAD_NC(GPP_A8, NONE),
+	/* D6  : WWAN_EN */
+	PAD_NC(GPP_D6, NONE),
+	/* F12 : WWAN_RST_L */
+	PAD_NC(GPP_F12, NONE),
+	/* H23 : WWAN_SAR_DETECT_ODL */
+	PAD_NC(GPP_H23, NONE),
+};
+
+static const struct pad_config wfc_disable_pads[] = {
+	/* D3  : WCAM_RST_L */
+	PAD_NC_LOCK(GPP_D3, NONE, LOCK_CONFIG),
+	/* D15 : EN_PP2800_WCAM_X */
+	PAD_NC_LOCK(GPP_D15, NONE, LOCK_CONFIG),
+	/* D16 : EN_PP1800_PP1200_WCAM_X */
+	PAD_NC_LOCK(GPP_D16, NONE, LOCK_CONFIG),
+	/* H22 : WCAM_MCLK_R */
+	PAD_NC(GPP_H22, NONE),
+	/* R6 : DMIC_WCAM_CLK_R */
+	PAD_NC(GPP_R6, NONE),
+	/* R7 : DMIC_WCAM_DATA */
+	PAD_NC(GPP_R7, NONE),
+};
+
+static const struct pad_config emmc_disable_pads[] = {
+	/* I7  : EMMC_CMD */
+	PAD_NC(GPP_I7, NONE),
+	/* I8  : EMMC_D0 */
+	PAD_NC(GPP_I8, NONE),
+	/* I9  : EMMC_D1 */
+	PAD_NC(GPP_I9, NONE),
+	/* I10 : EMMC_D2 */
+	PAD_NC(GPP_I10, NONE),
+	/* I11 : EMMC_D3 */
+	PAD_NC(GPP_I11, NONE),
+	/* I12 : EMMC_D4 */
+	PAD_NC(GPP_I12, NONE),
+	/* I13 : EMMC_D5 */
+	PAD_NC(GPP_I13, NONE),
+	/* I14 : EMMC_D6 */
+	PAD_NC(GPP_I14, NONE),
+	/* I15 : EMMC_D7 */
+	PAD_NC(GPP_I15, NONE),
+	/* I16 : EMMC_RCLK */
+	PAD_NC(GPP_I16, NONE),
+	/* I17 : EMMC_CLK */
+	PAD_NC(GPP_I17, NONE),
+	/* I18 : EMMC_RST_L */
+	PAD_NC(GPP_I18, NONE),
+};
+
+static const struct pad_config stylus_disable_pads[] = {
+	/* F13 : SOC_PEN_DETECT_R_ODL */
+	PAD_NC_LOCK(GPP_F13, NONE, LOCK_CONFIG),
+	/* F15 : SOC_PEN_DETECT_ODL */
+	PAD_NC_LOCK(GPP_F15, NONE, LOCK_CONFIG),
+};
+
+static const struct pad_config disable_wifi_pch_susclk[] = {
+	/* GPD8 ==> NC */
+	PAD_NC(GPD8, NONE),
+};
+
+void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
+{
+	if (!fw_config_probe(FW_CONFIG(DB_USB, DB_1C_LTE))) {
+		printk(BIOS_INFO, "Disable LTE-related GPIO pins.\n");
+		gpio_padbased_override(padbased_table, lte_disable_pads,
+				       ARRAY_SIZE(lte_disable_pads));
+	}
+	if (fw_config_probe(FW_CONFIG(WFC, WFC_ABSENT))) {
+		printk(BIOS_INFO, "Disable MIPI WFC GPIO pins.\n");
+		gpio_padbased_override(padbased_table, wfc_disable_pads,
+				       ARRAY_SIZE(wfc_disable_pads));
+	}
+	if (fw_config_is_provisioned() && !fw_config_probe(FW_CONFIG(STORAGE, STORAGE_EMMC))) {
+		printk(BIOS_INFO, "Disable eMMC GPIO pins.\n");
+		gpio_padbased_override(padbased_table, emmc_disable_pads,
+				       ARRAY_SIZE(emmc_disable_pads));
+	}
+	if (fw_config_probe(FW_CONFIG(STYLUS, STYLUS_ABSENT))) {
+		printk(BIOS_INFO, "Disable Stylus GPIO pins.\n");
+		gpio_padbased_override(padbased_table, stylus_disable_pads,
+				       ARRAY_SIZE(stylus_disable_pads));
+	}
+	if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, SAR_ID_3))) {
+		printk(BIOS_INFO, "Disable PCH SUSCLK.\n");
+		gpio_padbased_override(padbased_table, disable_wifi_pch_susclk,
+					ARRAY_SIZE(disable_wifi_pch_susclk));
+	}
+}

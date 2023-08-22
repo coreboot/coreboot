7687e7767f2d2321b57fa8eab68b7e954e57ad42
Martin Roth <gaumless@gmail.com>
2023-08-22 16:32:20 -0600

    vc/amd/pi: Add SPDX headers to all files that don't have them
    License classifiers are much better about classifying files with SPDX
    headers than they are at classifying the general text licenses due to
    minor variations in the text. To help with classification, add the
    SPDX headers to the files.
    
    To see the current state of coreboot's licensing, see:
    https://coreboot.org/fossology/
    
    Signed-off-by: Martin Roth <gaumless@gmail.com>
    Change-Id: If490f6705e7862d9ad02c925104113b355434101
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77380
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/vendorcode/amd/pi/00670F00/Include/Filecode.h
+++ src/vendorcode/amd/pi/00670F00/Include/Filecode.h
@@ -1,3 +1,6 @@
+/* SPDX-License-Identifier: CC-PDDC */
+/* Please update the license if adding licensable material. */
+
 /*
  * Empty file.
  * coreboot doesn't need the filecodes, but some of the AGESA source need
--- src/vendorcode/amd/pi/00670F00/gcc-intrin.h
+++ src/vendorcode/amd/pi/00670F00/gcc-intrin.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
 /*
  * Copyright (c) 2011, Advanced Micro Devices, Inc. All rights reserved.
  * Copyright (c) 2014, Edward O'Callaghan <eocallaghan@alterapraxis.com>

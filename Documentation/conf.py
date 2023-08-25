60be9fe7cad1b22efc0a166cd52c29dd1dd799d6
Felix Singer <felixsinger@posteo.net>
2023-08-25 15:40:25 +0000

    Documentation: Remove unused build targets
    The options in conf.py for the following build targets are either
    commented out or contain example values, which suggests that there was
    no interest in them recently. Their comments also seem more like
    generated examples.
    
      * LaTeX
      * man pages
      * Texinfo
    
    In order to clean up our configs and scripts for the documentation,
    remove the configuration options from conf.py for these build targets.
    
    Also, remove the build targets responsible for generating a PDF file
    from Makefile. Don't touch Makefile.sphinx for now though as we usually
    wrap around it.
    
    We may bring these build targets back if there is real interest in
    them, but it seems only the HTML target was really used.
    
    Change-Id: I7df8ea886f94d9b25e8eeb0ccbc2a7392b96a575
    Signed-off-by: Felix Singer <felixsinger@posteo.net>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77439
    Reviewed-by: Martin L Roth <gaumless@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -94,92 +94,6 @@ html_css_files = [
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'corebootdoc'
 
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-     # The paper size ('letterpaper' or 'a4paper').
-     #
-     # 'papersize': 'letterpaper',
-
-     # The font size ('10pt', '11pt' or '12pt').
-     #
-     # 'pointsize': '10pt',
-
-     # Additional stuff for the LaTeX preamble.
-     #
-     # 'preamble': '',
-
-     # Latex figure (float) alignment
-     #
-     # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'coreboot.tex', u'coreboot Documentation',
-     u'the coreboot project', 'manual'),
-]
-
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#
-# latex_logo = None
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#
-# latex_use_parts = False
-
-# If true, show page references after internal links.
-#
-# latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-#
-# latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-#
-# latex_appendices = []
-
-# If false, will not define \strong, \code,	itleref, \crossref ... but only
-# \sphinxstrong, ..., \sphinxtitleref, ... To help avoid clash with user added
-# packages.
-#
-# latex_keep_old_macro_names = True
-
-# If false, no module index is generated.
-#
-# latex_domain_indices = True
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'coreboot', u'coreboot Documentation',
-     [author], 1)
-]
-
-# If true, show URL addresses after external links.
-#
-# man_show_urls = False
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'coreboot', u'coreboot Documentation',
-     author, 'coreboot', 'One line description of project.',
-     'Miscellaneous'),
-]
-
 enable_auto_toc_tree = True
 
 class MyCommonMarkParser(CommonMarkParser):
@@ -188,23 +102,6 @@ class MyCommonMarkParser(CommonMarkParser):
         from docutils import nodes
         n = nodes.literal(mdnode.literal, mdnode.literal)
         self.current_node.append(n)
-
-# Documents to append as an appendix to all manuals.
-#
-# texinfo_appendices = []
-
-# If false, no module index is generated.
-#
-# texinfo_domain_indices = True
-
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#
-# texinfo_show_urls = 'footnote'
-
-# If true, do not generate a @detailmenu in the "Top" node's menu.
-#
-# texinfo_no_detailmenu = False
-
 
 def setup(app):
     from recommonmark.transform import AutoStructify

fd6908a748ad00aa6557e08309b9aad9abcdceb2
YH Lin <yueherngl@google.com>
2023-08-31 09:41:31 -0700

    mb/google/rex/var/screebo: add hook for WiFi SAR table
    As a preparation for WiFi SAR table addition, adding hook for it.
    
    BUG=b:291155207
    TEST=emerge-rex coreboot
    
    Change-Id: Ia313cfddec278e6bf8498407b242c027a5891deb
    Signed-off-by: YH Lin <yueherngl@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77598
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/rex/variants/screebo/variant.c
+++ src/mainboard/google/rex/variants/screebo/variant.c
@@ -3,6 +3,12 @@
 #include <chip.h>
 #include <fw_config.h>
 #include <baseboard/variants.h>
+#include <sar.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return get_wifi_sar_fw_config_filename(FW_CONFIG_FIELD(WIFI_SAR_ID));
+}
 
 void variant_update_soc_chip_config(struct soc_intel_meteorlake_config *config)
 {

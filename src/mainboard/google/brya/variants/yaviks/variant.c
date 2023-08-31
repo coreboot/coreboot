208cbdb6af078851915a491b8415c914646eb0c8
Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
2023-08-31 13:06:08 +0800

    mb/google/nissa/var/yaviks: Add wifi sar for yahiko
    Add intel wifi sar table for yahiko
    
    BUG=b:298280621
    BRANCH=firmware-nissa-15217.B
    TEST=build, enable iwlwifi debug option, and check dmesg
    
    Change-Id: I38d2e640fc2f7cbde3986474ca1bf7de9b2d25b4
    Signed-off-by: Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77585
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/yaviks/variant.c
+++ src/mainboard/google/brya/variants/yaviks/variant.c
@@ -12,6 +12,8 @@ const char *get_wifi_sar_cbfs_filename(void)
 {
 	if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, SAR_ID_0)))
 		return "wifi_sar_0.hex";
+	if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, SAR_ID_2)))
+		return "wifi_sar_2.hex";
 
 	return NULL;
 }

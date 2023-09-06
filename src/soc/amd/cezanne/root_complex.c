50ba957b176d7b9da7ceb9e167460d8c26d33699
Matt DeVillier <matt.devillier@gmail.com>
2023-09-06 08:57:40 -0500

    soc/amd/cezanne: Add ACPI opregion to root complex
    Add an ACPI OperationRegion to access an SMN access index/data register
    pair in the root complex. To access the PCI config space registers, the
    ECAM MMCONF MMIO region is used which matches the UEFI reference
    implementation.
    
    TEST=The AML code sequence written by this decompiles to the expected
    ASL code.
    
    Change-Id: I4d00c86647e51e5cae621fe788f0a1b06471a443
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77682
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/cezanne/root_complex.c
+++ src/soc/amd/cezanne/root_complex.c
@@ -59,6 +59,52 @@ static void root_complex_fill_ssdt(const struct device *device)
 {
 	if (CONFIG(SOC_AMD_COMMON_BLOCK_ACPI_DPTC))
 		acipgen_dptci();
+
+	/* Add ACPI device, opregion to host bridge needed for ACP driver.
+	 *
+	 * This is used by an ACPI method in the ACP's ACPI code to access different mailbox
+	 * interfaces in th hardware. Some ACP drivers will use that to both notify the PSP
+	 * that the DSP firmware has been loaded, so that the PSP can validate the firmware
+	 * and set the qualifier bit to enable running it, and to configure the ACP's clock
+	 * source.
+	 *
+	 * As this SMN access is not arbitrated and there may be other drivers or parts of
+	 * the firmware attempting to use the SMN access register pair, there is a risk of
+	 * conflict / incorrect data, but given the frequency and duration of accesses, the
+	 * risk is deemed to be quite low.
+	 *
+	 *	Scope (\_SB.PCI0)
+	 *	{
+	 *		Device (GNB)
+	 *		{
+	 *			Name (_ADR, 0x0000000000000000)
+	 *			Method (_STA, 0, NotSerialized)
+	 *			{
+	 *				Return (0x0F)
+	 *			}
+	 *		}
+	 *	}
+	 *	Scope (\_SB.PCI0.GNB)
+	 *	{
+	 *		OperationRegion(SMN, SystemMemory , 0xF80000B8, 0x8)
+	 *		Field(SMN, AnyAcc, NoLock, Preserve) {
+	 *			SMNA,   32,
+	 *			SMND,   32,
+	 *		}
+	 *	}
+	 */
+	acpi_device_write_pci_dev(device);
+	acpigen_write_scope(acpi_device_path(device));
+	struct opregion opreg = OPREGION("SMN", SYSTEMMEMORY,
+					 CONFIG_ECAM_MMCONF_BASE_ADDRESS + 0xb8, 0x8);
+	acpigen_write_opregion(&opreg);
+	static const struct fieldlist list[] = {
+		FIELDLIST_NAMESTR("SMNA", 32),
+		FIELDLIST_NAMESTR("SMND", 32),
+	};
+	acpigen_write_field(opreg.name, list, ARRAY_SIZE(list),
+			FIELD_ANYACC | FIELD_NOLOCK | FIELD_PRESERVE);
+	acpigen_write_scope_end();
 }
 
 static const char *gnb_acpi_name(const struct device *dev)

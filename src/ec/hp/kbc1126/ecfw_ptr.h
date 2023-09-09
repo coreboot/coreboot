c0b35ccb510259a783dce234e633e788fb76b5c2
Elyes Haouas <ehaouas@noos.fr>
2023-09-09 08:00:59 +0200

    ec/hp/kbc1126: Use packed over attrbute__((packed))
    Change-Id: Ia4b142a5eac2aab7e4fa6e32ed68c96934ec6c32
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77731
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>

--- src/ec/hp/kbc1126/ecfw_ptr.h
+++ src/ec/hp/kbc1126/ecfw_ptr.h
@@ -5,14 +5,14 @@
 
 #include <stdint.h>
 
-struct __attribute__((__packed__)) ecfw_addr {
+struct __packed ecfw_addr {
 	/* 8-byte offset of firmware blob in big endian */
 	uint16_t off;
 	/* bitwise inverse of "off", for error checking */
 	uint16_t inv;
 };
 
-struct __attribute__((__packed__)) ecfw_ptr {
+struct __packed ecfw_ptr {
 	struct ecfw_addr fw1;
 	struct ecfw_addr fw2;
 };

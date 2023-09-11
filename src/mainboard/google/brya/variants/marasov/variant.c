ed62dbaf674d102d45b84b38c5de62134e5bca4c
Daniel Peng <Daniel_Peng@pegatron.corp-partner.google.com>
2023-09-11 17:29:54 +0800

    mb/google/brya/var/marasov: Enable Wi-Fi sar table for Intel module
    1.In contrast to the MediaTek Wi-Fi module, the Intel Wi-Fi module needs to load a SAR table.
    
    2.Describe the FW_CONFIG probe for the settings on marasov.
    - WIFI_SAR_ID_0 for MTK Wi-Fi module MT7921L
    - WIFI_SAR_ID_1 for Intel Wi-Fi module AX211NGW
    
    BUG=b:300045956
    BRANCH=firmware-brya-14505.B
    TEST=emerge-brya coreboot chromeos-bootimage
    
    Change-Id: I5b5c6bea6c2c916fb682044218ec7b3a5d2659f6
    Signed-off-by: Daniel Peng <Daniel_Peng@pegatron.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77789
    Reviewed-by: Daniel Peng <daniel_peng@pegatron.corp-partner.google.com>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

--- src/mainboard/google/brya/variants/marasov/variant.c
+++ src/mainboard/google/brya/variants/marasov/variant.c
@@ -2,7 +2,21 @@
 
 #include <acpi/acpigen.h>
 #include <baseboard/variants.h>
+#include <console/console.h>
+#include <fw_config.h>
+#include <sar.h>
 #include <variant/gpio.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, WIFI_SAR_ID_1))) {
+		printk(BIOS_INFO, "Use wifi_sar_1.hex for Intel Wi-Fi module AX211NGW.\n");
+		return "wifi_sar_1.hex";
+	}
+
+	printk(BIOS_INFO, "Intel Wi-Fi SAR not used, return NULL!\n");
+	return NULL;
+}
 
 void variant_generate_s0ix_hook(enum s0ix_entry entry)
 {

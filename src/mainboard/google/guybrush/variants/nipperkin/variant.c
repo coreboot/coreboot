00864fdba65b4b466bbc7ba599b84b6ac8e8b1c6
Tim Van Patten <timvp@google.com>
2023-09-15 10:48:35 -0600

    mb/google/guybrush: Disable WLAN ASPM
    ASPM on the WLAN PCIe bus introduces large latency spikes, which can be
    measured with cyclictest:
    
    $ cyclictest --policy=rr --priority=12 --interval=10000 --threads=1 --loops=6000
    
    Disabling ASPM for WLAN reduces the latency spikes from 2,500-3,000 usec
    down to 35-65 usec. These latency spikes can impact the user when
    real-time processes like Audio (cras) are starved of CPU time, leading
    to buffer underruns resulting in crackling/distorted audio.
    
    ASPM is already disabled for Nipperkin devices (CB:63537), so this CL
    disables it for both in the shared declaration of
    guybrush_czn_dxio_descriptors.
    
    Power impact for Dewatt:
    
    * ASPM enabled
    
        power_VideoCall.FDO_25min_webrtc
        w_energy_rate 7.425043688811071
    
        power_Idle.default20min
        wh_energy_used 1.4164200000000022
    
    * ASPM disabled
    
        power_VideoCall.FDO_25min_webrtc
        w_energy_rate 8.779998551703423
    
        power_Idle.default20min
        wh_energy_used 1.4860800000000012
    
    When using Google Meet over WiFi, power increases by ~1.5W.
    
    BUG=b:297970318
    TEST=cyclictest --policy=rr --priority=12 --interval=10000 --threads=1 --loops=6000
    
    Change-Id: I16940987d598943bd5d6ace8b4008eba4d4a177c
    Signed-off-by: Tim Van Patten <timvp@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77963
    Reviewed-by: Martin L Roth <gaumless@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

--- src/mainboard/google/guybrush/variants/nipperkin/variant.c
+++ src/mainboard/google/guybrush/variants/nipperkin/variant.c
@@ -1,13 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <baseboard/variants.h>
-#include <string.h>
 
 void variant_update_dxio_descriptors(fsp_dxio_descriptor *dxio_descriptors)
 {
-	dxio_descriptors[WLAN].link_aspm_L1_1 = false;
-	dxio_descriptors[WLAN].link_aspm_L1_2 = false;
-
 	/* Fix link speed to GEN2 - b/228830362 */
 	dxio_descriptors[WLAN].link_speed_capability = GEN2;
 	dxio_descriptors[WLAN].port_params[0] = PP_PSPP_AC;

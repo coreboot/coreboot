coreboot/coreboot

commit 17cea380d9856ed5765e5233aa2d6edeaa3a16c5

commonlib: Add CBMEM ID to store CSE Boot Partition Info
PSR data is created and stored in CSE data partition. In platforms that
employ CSE Lite SKU firmware, a firmware downgrade involves clearing of
CSE data partition which results in PSR data being lost. In order to
backup PSR data before initiating firmware downgrade, CSE Lite firmware
supports a command to do this. This command works only after memory has
been initialized. So the CSE firmware downgrade can be done only in
post-RAM stage. CSE firmware sync actions will be moved to early
ramstage to support this.

Moving CSE firmware sync actions to ramstage results in cse_get_bp_info
command taking additional boot time of ~45-55ms. To avoid this,
cse_get_bp_info will be sent in early romstage and the response will be
stored in cbmem to avoid sending the command again, and re-use in
ramstage.

This patch adds a CBMEM ID to store this CSE Boot Partition Info
response in cbmem.

BUG=b:273207144

Change-Id: I914befadab4ad0ac197435e2a2c4343a796b2b1b
Signed-off-by: Krishna Prasad Bhat <krishna.p.bhat.d@intel.com>
Signed-off-by: Rizwan Qureshi <rizwan.qureshi@intel.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/78052
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: sridhar siricilla <siricillasridhar@gmail.com>

src/commonlib/bsd/include/commonlib/bsd/cbmem_id.h
@@ -88,6 +88,7 @@
 #define CBMEM_ID_AMD_STB	0x5f425453
 #define CBMEM_ID_AMD_MP2	0x5f32504d
 #define CBMEM_ID_CSE_INFO	0x4553435F
+#define CBMEM_ID_CSE_BP_INFO	0x43534542
 
 #define CBMEM_ID_TO_NAME_TABLE				 \
 	{ CBMEM_ID_ACPI,		"ACPI       " }, \
@@ -168,5 +169,6 @@
 	{ CBMEM_ID_MEM_CHIP_INFO,       "MEM CHIP INFO"},\
 	{ CBMEM_ID_AMD_STB,		"AMD STB"},\
 	{ CBMEM_ID_AMD_MP2,		"AMD MP2 BUFFER"},\
-	{ CBMEM_ID_CSE_INFO,		"CSE SPECIFIC INFO"}
+	{ CBMEM_ID_CSE_INFO,		"CSE SPECIFIC INFO"},\
+	{ CBMEM_ID_CSE_INFO,		"CSE BP INFO"}
 #endif /* _CBMEM_ID_H_ */

commit 8e2e33a044262aa5b74cd2cb330fb4943d14b59f

commonlib: Make CBMEM_ID_CSE_BP_INFO little endian, fix id for string
This patch fixes the mistake introduced with 'commit 17cea380d985
("commonlib: Add CBMEM ID to store CSE Boot Partition Info")' where
single CBMEM ID name `CBMEM_ID_CSE_INFO` is associated with two
different name description.

Additionally, use little endian format for `CBMEM_ID_CSE_INFO` cbmem id.

TEST=Build and boot google/rex. Able to fix the issue introduced in
commit 17cea380d985 while running cbmem --list and verify that the
associated name string is proper.

Change-Id: I4235f1f6881ab86ccb252065e922d5d526f7f1f7
Signed-off-by: Subrata Banik <subratabanik@google.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/78110
Reviewed-by: Patrick Georgi <patrick@coreboot.org>
Reviewed-by: Dinesh Gehlot <digehlot@google.com>
Reviewed-by: sridhar siricilla <siricillasridhar@gmail.com>
Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Krishna P Bhat D <krishna.p.bhat.d@intel.com>

src/commonlib/bsd/include/commonlib/bsd/cbmem_id.h
@@ -88,7 +88,7 @@
 #define CBMEM_ID_AMD_STB	0x5f425453
 #define CBMEM_ID_AMD_MP2	0x5f32504d
 #define CBMEM_ID_CSE_INFO	0x4553435F
-#define CBMEM_ID_CSE_BP_INFO	0x43534542
+#define CBMEM_ID_CSE_BP_INFO	0x42455343
 
 #define CBMEM_ID_TO_NAME_TABLE				 \
 	{ CBMEM_ID_ACPI,		"ACPI       " }, \
@@ -170,5 +170,5 @@
 	{ CBMEM_ID_AMD_STB,		"AMD STB"},\
 	{ CBMEM_ID_AMD_MP2,		"AMD MP2 BUFFER"},\
 	{ CBMEM_ID_CSE_INFO,		"CSE SPECIFIC INFO"},\
-	{ CBMEM_ID_CSE_INFO,		"CSE BP INFO"}
+	{ CBMEM_ID_CSE_BP_INFO,		"CSE BP INFO"}
 #endif /* _CBMEM_ID_H_ */
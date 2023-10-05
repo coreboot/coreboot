2651351805bba1d2051423ba844b26057556c55f
Kenneth Chan <kenneth.chan@quanta.corp-partner.google.com>
2023-10-05 11:45:56 +0800

    mb/google/dedede: Create dexi variant
    Create the dexi variant of the taranza project by
    copying the files to a new directory named for the variant.
    
    BUG=b:303533815
    BRANCH=dedede
    TEST=util/abuild/abuild -p none -t google/dedede -x -a
    make sure the build includes GOOGLE_DEXI
    
    Signed-off-by: Kenneth Chan <kenneth.chan@quanta.corp-partner.google.com>
    Change-Id: I708a16cb864dca7309cb0201e7887af7456a4885
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/78249
    Reviewed-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/google/dedede/variants/dexi/Makefile.inc
@@ -0,0 +1,6 @@
+## SPDX-License-Identifier: GPL-2.0-or-later
+
+ramstage-y += gpio.c
+
+ramstage-y += variant.c
+ramstage-y += ramstage.c
---
+++ src/mainboard/google/dedede/variants/dexi/include/variant/gpio.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef MAINBOARD_GPIO_H
+#define MAINBOARD_GPIO_H
+
+#include <baseboard/gpio.h>
+
+#endif /* MAINBOARD_GPIO_H */

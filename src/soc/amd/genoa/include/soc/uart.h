970d7701c77f7d2f3b9691bc2644b2812caa1228
Varshit Pandya <pandyavarshit@gmail.com>
2023-10-06 18:14:02 +0530

    soc/amd/genoa: Enable uart
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Signed-off-by: Varshit Pandya <pandyavarshit@gmail.com>
    Change-Id: I1529657f30b6e228c2e3cd7e0438255522381367
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/76507
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/soc/amd/genoa/include/soc/uart.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef AMD_GENOA_UART_H
+#define AMD_GENOA_UART_H
+
+#include <types.h>
+
+void clear_uart_legacy_config(void); /* disable legacy I/O decode for FCH UART */
+
+#endif /* AMD_GENOA_UART_H */

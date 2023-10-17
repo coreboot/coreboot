7d3ababd71031fdb6f0cf08af41b26616efc5006
Jamie Chen <jamie_chen@compal.corp-partner.google.com>
2023-10-17 10:55:58 +0800

    mb/google/brya/var/omnigul: Add fingerprint SPI
    Add fingerprint SPI, and power off FPMCU during romstage.
    
    BUG=b:305860604, b:306320063
    BRANCH=firmware-brya-14505.B
    TEST=emerge-brya coreboot, measure evtest can detect and check device probed in kernel log
    
    Change-Id: Ic7b9e29ca3cb9352fe098156924fde2719399a79
    Signed-off-by: Jamie Chen <jamie_chen@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/78427
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Matt DeVillier <matt.devillier@amd.corp-partner.google.com>

--- src/mainboard/google/brya/variants/omnigul/gpio.c
+++ src/mainboard/google/brya/variants/omnigul/gpio.c
@@ -316,12 +316,15 @@ static const struct pad_config romstage_gpio_table[] = {
 	 * B4 is programmed here so that it is sequenced after EN_PP3300_SSD.
 	 */
 	PAD_CFG_GPO(GPP_B4, 1, DEEP),
-
 	/* Enable touchscreen, hold in reset */
 	/* C0  : SMBCLK ==> EN_PP3300_TCHSCR */
 	PAD_CFG_GPO(GPP_C0, 1, DEEP),
 	/* C1  : SMBDATA ==> USI_RST_L */
 	PAD_CFG_GPO(GPP_C1, 0, DEEP),
+	// D1  : ISH_GP1 ==> FP_RST_ODL /
+	PAD_CFG_GPO(GPP_D1, 0, DEEP),
+	// D2  : ISH_GP2 ==> EN_FP_PWR /
+	PAD_CFG_GPO(GPP_D2, 0, DEEP),
 };
 
 const struct pad_config *variant_gpio_override_table(size_t *num)

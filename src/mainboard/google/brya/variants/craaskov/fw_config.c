3c83995ff99fd6aa4e20249e7e34da0b32a89a5f
Rex Chou <rex_chou@compal.corp-partner.google.com>
2023-10-31 15:01:28 +0800

    mb/google/nissa/var/craaskov: Disable storage devices based on fw_config
    Disable devices in variant.c instead of adding probe statements to
    devicetree because storage devices need to be enabled when fw_config is
    unprovisioned, and devicetree does not currently support this (it
    disables all probed devices when fw_config is unprovisioned).
    
    BUG=b:305887856
    BRANCH=firmware-nissa-15217.B
    TEST=emerge-nissa coreboot chromeos-bootimage
    
    Change-Id: I5993049ac63520c4dfd057c38b566fc69502d825
    Signed-off-by: Rex Chou <rex_chou@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/78843
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>

---
+++ src/mainboard/google/brya/variants/craaskov/fw_config.c
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <baseboard/gpio.h>
+#include <baseboard/variants.h>
+#include <console/console.h>
+#include <fw_config.h>
+
+static const struct pad_config emmc_disable_pads[] = {
+	/* I7  : EMMC_CMD */
+	PAD_NC(GPP_I7, NONE),
+	/* I8  : EMMC_D0 */
+	PAD_NC(GPP_I8, NONE),
+	/* I9  : EMMC_D1 */
+	PAD_NC(GPP_I9, NONE),
+	/* I10 : EMMC_D2 */
+	PAD_NC(GPP_I10, NONE),
+	/* I11 : EMMC_D3 */
+	PAD_NC(GPP_I11, NONE),
+	/* I12 : EMMC_D4 */
+	PAD_NC(GPP_I12, NONE),
+	/* I13 : EMMC_D5 */
+	PAD_NC(GPP_I13, NONE),
+	/* I14 : EMMC_D6 */
+	PAD_NC(GPP_I14, NONE),
+	/* I15 : EMMC_D7 */
+	PAD_NC(GPP_I15, NONE),
+	/* I16 : EMMC_RCLK */
+	PAD_NC(GPP_I16, NONE),
+	/* I17 : EMMC_CLK */
+	PAD_NC(GPP_I17, NONE),
+	/* I18 : EMMC_RST_L */
+	PAD_NC(GPP_I18, NONE),
+};
+
+void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
+{
+	if (fw_config_is_provisioned() && !fw_config_probe(FW_CONFIG(STORAGE, STORAGE_EMMC))) {
+		printk(BIOS_INFO, "Disable eMMC GPIO pins.\n");
+		gpio_padbased_override(padbased_table, emmc_disable_pads,
+				       ARRAY_SIZE(emmc_disable_pads));
+	}
+}
--- src/mainboard/google/brya/variants/craaskov/variant.c
+++ src/mainboard/google/brya/variants/craaskov/variant.c
@@ -1,8 +1,33 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <baseboard/variants.h>
+#include <device/device.h>
 #include <sar.h>
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
 	return "wifi_sar_0.hex";
+}
+
+void variant_devtree_update(void)
+{
+	struct device *emmc = DEV_PTR(emmc);
+	struct device *ufs = DEV_PTR(ufs);
+	struct device *ish = DEV_PTR(ish);
+
+	if (!fw_config_is_provisioned()) {
+		printk(BIOS_INFO, "fw_config unprovisioned so enable all storage devices\n");
+		return;
+	}
+
+	if (!fw_config_probe(FW_CONFIG(STORAGE, STORAGE_EMMC))) {
+		printk(BIOS_INFO, "eMMC disabled by fw_config\n");
+		emmc->enabled = 0;
+	}
+
+	if (!fw_config_probe(FW_CONFIG(STORAGE, STORAGE_UFS))) {
+		printk(BIOS_INFO, "UFS disabled by fw_config\n");
+		ufs->enabled = 0;
+		ish->enabled = 0;
+	}
 }

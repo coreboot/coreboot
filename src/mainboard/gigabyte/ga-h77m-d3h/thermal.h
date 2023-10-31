7d8e105420c3d8fc7385cd9ad634fd4e130030b7
Fabian Groffen <grobian@gentoo.org>
2023-10-31 09:00:10 +0100

    mb/gigabyte/ga-h77m-d3h: Add Sandy/Ivy Bridge board GA-H77M-D3H
    This board is based off ga-b75m-d3h, which uses the same SuperIO chip.
    It doesn't have the ASMedia SATA3 controller, the H77 chipset comes with
    2 SATA3 ports next to the 4 SATA2 ports.
    
    Flashing notes:
      These boards come with dual-BIOS feature.  This is set of two
      unremovable what appears to be identical chips marked M_BIOS and
      B_BIOS.  Flash the B_BIOS chip, and boot the system.  Ensure you have
      a payload and setup ready to boot a Linux system with iomem=relaxed or
      similar.  Immediately use flashrom -p internal to flash the same
      firmware again.  If you skip this step your next boot will show weird
      exception traces in either coreboot or your payload.  Flashing from
      there via the chip is very difficult (you have to try many times in
      order to get a booting run), which can all be remedied by doing a
      flash from internal.  I suppose the dual-BIOS feature is somewhat in
      the way here.
    
    Tested with:
    - CPU Core i7-3770S
    - RAM single bank 4GB CL11, two banks 4+4GB CL11
    - OS Gentoo Linux LiveUSB, KDE desktop (Linux 5.15.72)
    
    Working:
    - GRUB2 payload
    - Intel ME stripped
    - Integrated graphics with libgfxinit
    - (boot from) SATA2, SATA3 ports
    - Rear and mainboard connector USB ports, supporting boot
    - Atheros GbE NIC
    - 2.0 channel audio via lineout jack output
    - ACPI (power button triggers OS events)
    - S3 suspend/resume
    - PWM FAN control, FAN speed readings
    - Temperature sensor readings
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>
    Change-Id: Icb3e74326a0a7aaf770d1917a2a0931feadd7eab
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/77046
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/gigabyte/ga-h77m-d3h/thermal.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef GAB75MD3H_THERMAL_H
+#define GAB75MD3H_THERMAL_H
+
+/* Temperature which OS will shutdown at */
+#define CRITICAL_TEMPERATURE	100
+
+/* Temperature which OS will throttle CPU */
+#define PASSIVE_TEMPERATURE	90
+
+#endif

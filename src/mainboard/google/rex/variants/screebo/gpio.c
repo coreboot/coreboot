f281de82a74df9d077326dbe0c98d16550a17bf8
Kun Liu <liukun11@huaqin.corp-partner.google.com>
2023-11-28 19:21:34 +0800

    mb/google/rex/var/screebo: Change GPP_B14 from NC to NF
    Change GPP_B14 from NC to NF
    
    BUG=b:272447747
    TEST=enable usb OC2 function to ensure USBA work normal
    
    Change-Id: Ie0f112bcf183870869d0c1b9a223d4231600a300
    Signed-off-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/79308
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Rui Zhou <zhourui@huaqin.corp-partner.google.com>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/mainboard/google/rex/variants/screebo/gpio.c
+++ src/mainboard/google/rex/variants/screebo/gpio.c
@@ -75,8 +75,8 @@ static const struct pad_config gpio_table[] = {
 	PAD_CFG_NF(GPP_B12, NONE, DEEP, NF1),
 	/* GPP_B13 : [] ==> PLT_RST_L */
 	PAD_CFG_NF(GPP_B13, NONE, DEEP, NF1),
-	/* GPP_B14 : GPP_B14_STRAP ==> Component NC */
-	PAD_NC(GPP_B14, NONE),
+	/* GPP_B14 : GPP_B14_STRAP ==> USB_OC2 */
+	PAD_CFG_NF_LOCK(GPP_B14, UP_20K, NF1, LOCK_CONFIG),
 	/* GPP_B15 : [] ==> USB_OC3# */
 	PAD_CFG_NF_LOCK(GPP_B15, NONE, NF1, LOCK_CONFIG),
 	/* GPP_B16 : [] ==> SOC_HDMI_HPD_L */

coreboot/coreboot

commit 3ab13a8691cbe0e96c0741cb9bdf0e0acfa6efaf

ec/lenovo/h8/acpi/thermal: Add support for passive cooling
The ACPI spec requires _TSP, _TC1, _TC2 and _PSL for passive cooling.
_TSP already has been added in a previous commit.
Copy the coefficients used on google devices to activate the feature.

Tested on Lenovo T430:
 The CPU is throttled once the passive threshold has been reached.
Tested on Lenovo T500:
 The CPU is throttled once the passive threshold has been reached.

Change-Id: I922923a9029de77158988ac254bab4aad9536935
Signed-off-by: Patrick Rudolph <siro@das-labor.org>
Reviewed-on: https://review.coreboot.org/21223
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Vasya Boytsov <vasiliy.boytsov@phystech.edu>
Reviewed-by: Nico Huber <nico.h@gmx.de>

src/ec/lenovo/h8/acpi/thermal.asl
@@ -40,20 +40,36 @@ Scope(\_TZ)
 		/* Thermal sampling period for passive cooling: 10 seconds */
 		Name (_TSP, 100)
 
-		Method(_CRT, 0, NotSerialized) {
-			Store (\TCRT, Local0)
-			if (LGreater(Local0, Zero)) {
-				Return (C2K(Local0))
-			}
-			Return (C2K(127))
+		/* Coefficients for passive cooling */
+		Name (_TC1, 0x02)
+		Name (_TC2, 0x05)
+
+#if !IS_ENABLED(CONFIG_CPU_INTEL_MODEL_206AX) && \
+	!IS_ENABLED(CONFIG_CPU_INTEL_MODEL_2065X) && \
+	!IS_ENABLED(CONFIG_CPU_INTEL_MODEL_306AX)
+	/* Generated by acpigen */
+External (\PPKG, MethodObj)
+#endif
+		/* Processors used for passive cooling */
+		Method (_PSL, 0, Serialized)
+		{
+			Return (\PPKG ())
 		}
 
-		Method(_PSV, 0, NotSerialized) {
+		Method (_CRT, 0, NotSerialized) {
+			Store (\TCRT, Local0)
+			if (LGreater (Local0, 0)) {
+				Return (C2K (Local0))
+			}
+			Return (C2K (127))
+		}
+
+		Method (_PSV, 0, NotSerialized) {
 			Store (\TPSV, Local0)
-			if (LGreater(Local0, Zero)) {
+			if (LGreater (Local0, 0)) {
 				Return (C2K(Local0))
 			}
-			Return (C2K(95))
+			Return (C2K (95))
 		}
 
 		Method(_TMP) {
@@ -76,12 +92,22 @@ Scope(\_TZ)
 		/* Thermal sampling period for passive cooling: 10 seconds */
 		Name (_TSP, 100)
 
-		Method(_CRT, 0, NotSerialized) {
-			Return (C2K(99))
+		/* Coefficients for passive cooling */
+		Name (_TC1, 0x02)
+		Name (_TC2, 0x05)
+
+		/* Processors used for passive cooling */
+		Method (_PSL, 0, Serialized)
+		{
+			Return (\PPKG ())
 		}
 
-		Method(_PSV, 0, NotSerialized) {
-			Return (C2K(94))
+		Method (_CRT, 0, NotSerialized) {
+			Return (C2K (99))
+		}
+
+		Method (_PSV, 0, NotSerialized) {
+			Return (C2K (94))
 		}
 
 		Method(_TMP) {

commit 87425775484d8381b31ff3323c290b1862698220

ec/lenovo/h8/acpi/thermal: Make NameSeg FPWR all upper case
Building the Lenovo T60/T60p, iasl 20230628 shows the remark below:

    dsdt.asl   2099:   PowerResource (FPwR, 0, 0)
    Remark   2182 -                     ^ At least one lower case letter found in NameSeg, ASL is case insensitive - converting to upper case (FPWR)

    dsdt.asl   2118:    Name (_PR0, Package () { FPwR })
    Remark   2182 -                                ^ At least one lower case letter found in NameSeg, ASL is case insensitive - converting to upper case (FPWR)

Address it by making it all upper case.

Change-Id: Ia7924b015e76c43818d2d82da35ce0013d721c26
Fixes: 3ab13a8691cb ("ec/lenovo/h8/acpi/thermal: Add support for passive cooling")
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/79367
Reviewed-by: Nico Huber <nico.h@gmx.de>
Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/ec/lenovo/h8/acpi/thermal.asl
@@ -97,7 +97,7 @@ External (\PPKG, MethodObj)
 
 		Name (_AL0, Package () { FAN })
 
-		PowerResource (FPwR, 0, 0)
+		PowerResource (FPWR, 0, 0)
 		{
 			/*
 			 * WINDOWS BUG: Don't read from EmbeddedControl
@@ -127,7 +127,7 @@ External (\PPKG, MethodObj)
 		Device (FAN)
 		{
 			Name (_HID, EISAID ("PNP0C0B"))
-			Name (_PR0, Package () { FPwR })
+			Name (_PR0, Package () { FPWR })
 		}
 	}
 
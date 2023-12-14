6759ad39ad7d669f8fe712b6dcbb927092043497
Felix Held <felix-coreboot@felixheld.de>
2023-12-14 20:49:59 +0100

    arch/x86: introduce HAVE_CONFIGURABLE_APMC_SMI_PORT
    Introduce the HAVE_CONFIGURABLE_APMC_SMI_PORT Kconfig option that when
    not selected will result in a default implementation of
    pm_acpi_smi_cmd_port to be included in the build that returns APM_CNT.
    SoCs that provide their own pm_acpi_smi_cmd_port implementation, need to
    select this Kconfig option.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: Iaceb61b0f2a630d7afe2e0780b6a2a9806ea62f9
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/79566
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- src/arch/x86/apmc_smi_port.c
+++ src/arch/x86/apmc_smi_port.c
@@ -2,7 +2,7 @@
 
 #include <cpu/x86/smm.h>
 
-/* The X86 qemu target uses AMD64 save states but the APM port is not configurable. */
+/* default implementation of the !HAVE_CONFIGURABLE_APMC_SMI_PORT case */
 uint16_t pm_acpi_smi_cmd_port(void)
 {
 	return APM_CNT;

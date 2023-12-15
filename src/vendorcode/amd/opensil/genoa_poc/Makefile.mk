ede452fb9936a55bddffe9c7ca3e557e9b081f26
Arthur Heymans <arthur@aheymans.xyz>
2023-12-15 14:32:39 +0100

    vendorcode/amd/opensil: Add CPP args to all stages
    It does not hurt to do this and makes it possible to link romstage
    sources into bootblock.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Change-Id: Ic7edfdac43c2d71ee3dcbd9d8f59c9799595e7f8
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/79576
    Reviewed-by: Martin L Roth <gaumless@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/vendorcode/amd/opensil/genoa_poc/Makefile.mk
+++ src/vendorcode/amd/opensil/genoa_poc/Makefile.mk
@@ -2,8 +2,7 @@
 
 subdirs-y += mpio
 
-CPPFLAGS_ramstage += -I$(opensil_dir)/Include -I$(opensil_dir)/xUSL -I$(opensil_dir)/xUSL/Include -I$(opensil_dir)/xUSL/FCH -I$(opensil_dir)/xUSL/FCH/Common -I$(opensil_dir)/xSIM -I$(opensil_dir)/xPRF
-CPPFLAGS_romstage += -I$(opensil_dir)/Include -I$(opensil_dir)/xUSL -I$(opensil_dir)/xUSL/Include -I$(opensil_dir)/xSIM -I$(opensil_dir)/xPRF
+CPPFLAGS_common += -I$(opensil_dir)/Include -I$(opensil_dir)/xUSL -I$(opensil_dir)/xUSL/Include -I$(opensil_dir)/xUSL/FCH -I$(opensil_dir)/xUSL/FCH/Common -I$(opensil_dir)/xSIM -I$(opensil_dir)/xPRF
 
 romstage-y += opensil_console.c
 romstage-y += romstage.c

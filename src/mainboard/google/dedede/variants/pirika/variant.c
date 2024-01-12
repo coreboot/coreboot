189c576cdc3988c43fc54762a0eee7404f5367dc
Daniel Peng <Daniel_Peng@pegatron.corp-partner.google.com>
2024-01-12 18:52:40 +0800

    mb/google/dedede/var/pirika: Add initial fw_config configuration setting
    1. Describe the FW_CONFIG probe for the settings for Palutena.
        - WIFI_SAR_ID_0 for AW Wi-Fi module AW-CM421NF
        - WIFI_SAR_ID_1 for Intel Wi-Fi module AX211NGW
    
    2. In contrast to the AW Wi-Fi module, the Intel Wi-Fi module needs
    to load a SAR table in dedede platform.
    
    3. For Palutena project, the SKU ID segment of Palutena is set for
    "0x350000~0x35FFFF".
    
    BUG=b:319792428
    BRANCH=firmware-dedede-13606.B
    TEST=build pass
    
    Change-Id: Ic4f38928d24c4398d90df226cfe0788a30075bf2
    Signed-off-by: Daniel Peng <Daniel_Peng@pegatron.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/79930
    Reviewed-by: Daniel Peng <daniel_peng@pegatron.corp-partner.google.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Shou-Chieh Hsu <shouchieh@google.com>

--- src/mainboard/google/dedede/variants/pirika/variant.c
+++ src/mainboard/google/dedede/variants/pirika/variant.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <ec/google/chromeec/ec.h>
+#include <fw_config.h>
 #include <sar.h>
 
 enum {
@@ -10,6 +11,8 @@ enum {
 	PIRIKA_SKU_END      = 0x1Effff,
 	PASARA_SKU_START    = 0x2A0000,
 	PASARA_SKU_END      = 0x2Affff,
+	PALUTENA_SKU_START  = 0x350000,
+	PALUTENA_SKU_END    = 0x35ffff,
 };
 const char *get_wifi_sar_cbfs_filename(void)
 {
@@ -17,6 +20,11 @@ const char *get_wifi_sar_cbfs_filename(void)
 
 	if (sku_id >= PASARA_SKU_START && sku_id <= PASARA_SKU_END)
 		return "wifi_sar-pasara.hex";
+
+	if (sku_id >= PALUTENA_SKU_START && sku_id <= PALUTENA_SKU_END) {
+		if (fw_config_probe(FW_CONFIG(WIFI_SAR_ID, WIFI_SAR_ID_1)))
+			return "wifi_sar-palutena_INTEL_AX201.hex";
+	}
 
 	return WIFI_SAR_CBFS_DEFAULT_FILENAME;
 }

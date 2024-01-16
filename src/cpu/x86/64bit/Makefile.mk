ecbc243a45de3b7894e2fe6c8e22b5d07172274b
Ashish Kumar Mishra <ashish.k.mishra@intel.com>
2024-01-16 16:23:03 +0530

    cpu/x86: Add 1GiB pages for memory access up to 512GiB
    Current pagetable implementation allows memory access up to 4GiB using
    2MiB pages. If user wants to access more than 4GiB with a 2MiB page it
    will require more pagetable entries. By using a 1GiB page table, users
    can access more than 4GiB of memory while reducing the number of
    pagetable entries. This patch enables memory access up to 512GiB through
    1GiB pages by selecting USE_1G_PAGES_TLB in Kconfig.
    
    TEST: Verified in 64bit mode boot and access above 4GiB
    
    Change-Id: Id569ae5b50abf5b72e4db33b5e4cd802399e76ec
    Signed-off-by: Ashish Kumar Mishra <ashish.k.mishra@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/80088
    Reviewed-by: Wonkyu Kim <wonkyu.kim@intel.com>
    Reviewed-by: Jérémy Compostella <jeremy.compostella@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/cpu/x86/64bit/Makefile.mk
+++ src/cpu/x86/64bit/Makefile.mk
@@ -3,8 +3,14 @@
 all_x86-y += mode_switch.S
 all_x86-y += mode_switch2.S
 
+ifeq ($(CONFIG_USE_1G_PAGETABLES),y)
+PAGETABLE_SRC := pt1G.S
+else
+PAGETABLE_SRC := pt.S
+endif
+
 # Add --defsym=_start=0 to suppress a linker warning.
-$(objcbfs)/pt: $(dir)/pt.S $(obj)/config.h
+$(objcbfs)/pt: $(dir)/$(PAGETABLE_SRC) $(obj)/config.h
 	$(CC_bootblock) $(CFLAGS_bootblock) $(CPPFLAGS_bootblock) -o $@.tmp $< -Wl,--section-start=.rodata=$(CONFIG_ARCH_X86_64_PGTBL_LOC),--defsym=_start=0
 	$(OBJCOPY_ramstage) -Obinary -j .rodata $@.tmp $@
 	rm $@.tmp

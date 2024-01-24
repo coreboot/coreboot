6aa0da7b7398ea050f571ada3848b74e424bbbb2
jamie_chen <jamie_chen@compal.corp-partner.google.com>
2024-01-24 11:46:21 +0800

    mb/google/brya/var/omniknight: Add WIFI SAR table
    Add WIFI SAR table for omniknight.
    
    BUG=b:320172979
    TEST=FW_NAME=omnigul emerge-brya coreboot
    
    Change-Id: I70e79577612b3d5c4dc0f92211f87cbea0532d5d
    Signed-off-by: jamie_chen <jamie_chen@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/80152
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: Sumeet R Pawnikar <sumeet.r.pawnikar@intel.com>

--- src/mainboard/google/brya/variants/omnigul/variant.c
+++ src/mainboard/google/brya/variants/omnigul/variant.c
@@ -1,8 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <sar.h>
+#include <fw_config.h>
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
-	return "wifi_sar_0.hex";
+	if (fw_config_probe(FW_CONFIG(WIFI_SAR_TABLE, OMNIGUL_WIFI_SAR_0)))
+		return "wifi_sar_0.hex";
+	if (fw_config_probe(FW_CONFIG(WIFI_SAR_TABLE, OMNIKNIGHT_WIFI_SAR_1)))
+		return "wifi_sar_1.hex";
+
+	return NULL;
 }

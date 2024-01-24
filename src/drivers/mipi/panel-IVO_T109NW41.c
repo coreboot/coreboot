coreboot/coreboot

commit 520137fab0b750590da1dc6eaa00ba3f3a8bdd33

drivers/mipi: Add support for IVO_T109NW41 panel
Add IVO_T109NW41 serializable data to CBFS.
Datasheet: T109NW41 R0 Tentative Product Specification.docx

BUG=b:319025360
TEST=build and check the CBFS include the panel
BRANCH=None

Change-Id: Id740e3a21f72bbcd6e5c2b56b31ac90f4990d475
Signed-off-by: Ruihai Zhou <zhouruihai@huaqin.corp-partner.google.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/79844
Reviewed-by: Yidi Lin <yidilin@google.com>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: cong yang <yangcong5@huaqin.corp-partner.google.com>
Reviewed-by: Yu-Ping Wu <yupingso@google.com>

src/drivers/mipi/panel-IVO_T109NW41.c
@@ -1,0 +1,111 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <mipi/panel.h>
+
+struct panel_serializable_data IVO_T109NW41 = {
+	.edid = {
+		.ascii_string = "T109NW41",
+		.manufacturer_name = "IVO",
+		.panel_bits_per_color = 8,
+		.panel_bits_per_pixel = 24,
+		.mode = {
+			.pixel_clock = 158400,
+			.lvds_dual_channel = 0,
+			.refresh = 60,
+			.ha = 1200, .hbl = 120, .hso = 60, .hspw = 20,
+			.va = 1920, .vbl = 80, .vso = 60, .vspw = 8,
+			.phsync = '-', .pvsync = '-',
+			.x_mm = 147, .y_mm = 235,
+		},
+	},
+	.init = {
+		PANEL_DELAY(60),
+		PANEL_DCS(0xB9, 0x83, 0x10, 0x21, 0x55, 0x00),
+		PANEL_DCS(0xB1, 0x2C, 0xED, 0xED, 0x27, 0xE7, 0x52, 0xF5, 0x39,
+			0x36, 0x36, 0x36, 0x36, 0x32, 0x8B, 0x11, 0x65, 0x00, 0x88,
+			0xFA, 0xFF, 0xFF, 0x8F, 0xFF, 0x08, 0xD6, 0x33),
+		PANEL_DCS(0xB2, 0x00, 0x47, 0xB0, 0x80, 0x00, 0x12, 0x3A, 0x3C,
+			0xA3, 0x22, 0x20, 0x00, 0x00, 0x88, 0x01),
+		PANEL_DCS(0xB4, 0x35, 0x35, 0x43, 0x43, 0x35, 0x35, 0x30, 0x7A,
+			0x30, 0x7A, 0x00, 0x9D),
+		PANEL_DCS(0xE9, 0xCD),
+		PANEL_DCS(0xBA, 0x84),
+		PANEL_DCS(0xE9, 0x3F),
+		PANEL_DCS(0xBC, 0x1B, 0x04),
+		PANEL_DCS(0xBE, 0x20),
+		PANEL_DCS(0xBF, 0xFC, 0xC4),
+		PANEL_DCS(0xC0, 0x34, 0x34, 0x22, 0x11, 0x22, 0xA0, 0x31, 0x08,
+			0xF5, 0x03),
+		PANEL_DCS(0xE9, 0xCC),
+		PANEL_DCS(0xC7, 0x80),
+		PANEL_DCS(0xE9, 0x3F),
+		PANEL_DCS(0xE9, 0xC9),
+		PANEL_DCS(0xC8, 0x00),
+		PANEL_DCS(0xE9, 0x3F),
+		PANEL_DCS(0xC9, 0x00, 0x1E, 0x13, 0x88, 0x01),
+		PANEL_DCS(0xCB, 0x08, 0x13, 0x07, 0x00, 0x0F, 0x34),
+		PANEL_DCS(0xCC, 0x02, 0x03, 0x44),
+		PANEL_DCS(0xE9, 0xC4),
+		PANEL_DCS(0xD0, 0x03),
+		PANEL_DCS(0xE9, 0x3F),
+		PANEL_DCS(0xD1, 0x37, 0x06, 0x00, 0x02, 0x04, 0x2C, 0xFF),
+		PANEL_DCS(0xD3, 0x06, 0x00, 0x00, 0x00, 0x00, 0x08, 0x08, 0x08,
+			0x08, 0x37, 0x07, 0x64, 0x7C, 0x11, 0x11, 0x03, 0x03, 0x32,
+			0x10, 0x0E, 0x00, 0x0E, 0x32, 0x17, 0x97, 0x07, 0x97, 0x32,
+			0x00, 0x02, 0x00, 0x02, 0x00, 0x00),
+		PANEL_DCS(0xD5, 0x25, 0x24, 0x25, 0x24, 0x18, 0x18, 0x18, 0x18,
+			0x07, 0x06, 0x07, 0x06, 0x05, 0x04, 0x05, 0x04, 0x03, 0x02,
+			0x03, 0x02, 0x01, 0x00, 0x01, 0x00, 0xA8, 0xA8, 0xA8, 0xA8,
+			0x29, 0x29, 0x29, 0x29, 0x21, 0x20, 0x21, 0x20, 0x18, 0x18,
+			0x18, 0x18, 0x18, 0x18, 0x18, 0x18),
+		PANEL_DCS(0xD8, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xA0, 0xAA, 0xAA,
+			0xAA, 0xAA, 0xAA, 0xA0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00),
+		PANEL_DCS(0xE7, 0x07, 0x10, 0x10, 0x1A, 0x26, 0x9E, 0x00, 0x4F,
+			0xA0, 0x14, 0x14, 0x00, 0x00, 0x00, 0x00, 0x12, 0x0A, 0x02,
+			0x02, 0x00, 0x33, 0x02, 0x04, 0x18, 0x01),
+		PANEL_DCS(0xBD, 0x01),
+		PANEL_DCS(0xB1, 0x01, 0x7F, 0x11, 0xFD),
+		PANEL_DCS(0xCB, 0x86),
+		PANEL_DCS(0xD8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xA0,
+			0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xA0, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00),
+		PANEL_DCS(0xE7, 0x02, 0x00, 0x2B, 0x01, 0x7E, 0x0F, 0x7E, 0x10,
+			0xA0, 0x00, 0x00, 0x77, 0x00, 0x00, 0x00),
+		PANEL_DCS(0xBD, 0x02),
+		PANEL_DCS(0xBF, 0xF2),
+		PANEL_DCS(0xCB, 0x03, 0x07, 0x00, 0x10, 0x79),
+		PANEL_DCS(0xD8, 0xFF, 0xFF, 0xFF, 0xFF, 0xFA, 0xA0, 0xFF, 0xFF,
+			0xFF, 0xFF, 0xFA, 0xA0),
+		PANEL_DCS(0xE7, 0xFE, 0x01, 0xFE, 0x01, 0xFE, 0x01, 0x00, 0x00,
+			0x00, 0x23, 0x00, 0x23, 0x81, 0x02, 0x40, 0x00, 0x20, 0x6E,
+			0x02, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00),
+		PANEL_DCS(0xBD, 0x03),
+		PANEL_DCS(0xD8, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xA0, 0xAA, 0xAA,
+			0xAA, 0xAA, 0xAA, 0xA0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFA, 0xA0,
+			0xFF, 0xFF, 0xFF, 0xFF, 0xFA, 0xA0, 0xAA, 0xAA, 0xAA, 0xAA,
+			0xAA, 0xA0, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xA0, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00),
+		PANEL_DCS(0xE9, 0xC6),
+		PANEL_DCS(0xB4, 0x03, 0xFF, 0xF8),
+		PANEL_DCS(0xE9, 0x3F),
+		PANEL_DCS(0xE1, 0x00),
+		PANEL_DCS(0xBD, 0x00),
+		PANEL_DCS(0xD2, 0x35, 0x35, 0x35, 0x35, 0x35, 0x35),
+		PANEL_DCS(0xE9, 0xC4),
+		PANEL_DCS(0xBA, 0x96),
+		PANEL_DCS(0xE9, 0x3F),
+		PANEL_DCS(0xBD, 0x01),
+		PANEL_DCS(0xE9, 0xC5),
+		PANEL_DCS(0xBA, 0x4F),
+		PANEL_DCS(0xE9, 0x3F),
+		PANEL_DCS(0xBD, 0x00),
+		PANEL_DCS(0x11),
+		PANEL_DELAY(120),
+		PANEL_DCS(0x29),
+		PANEL_DELAY(20),
+		PANEL_END,
+	},
+};

src/drivers/mipi/Kconfig
@@ -42,6 +42,9 @@ config MIPI_PANEL_INX_HJ110IZ_01A_B2
 config MIPI_PANEL_INX_OTA7290D10P
 	bool
 
+config MIPI_PANEL_IVO_T109NW41
+	bool
+
 config MIPI_PANEL_STA_2081101QFH032011_53G
 	bool
 

src/drivers/mipi/Makefile.inc
@@ -21,6 +21,8 @@ panel-params-$(CONFIG_MIPI_PANEL_CMN_P097PFG_SSD2858) += panel-CMN_P097PFG_SSD28
 panel-params-$(CONFIG_MIPI_PANEL_INX_HJ110IZ_01A_B2) += panel-INX_HJ110IZ_01A_B2
 panel-params-$(CONFIG_MIPI_PANEL_INX_OTA7290D10P) += panel-INX_OTA7290D10P
 
+panel-params-$(CONFIG_MIPI_PANEL_IVO_T109NW41) += panel-IVO_T109NW41
+
 panel-params-$(CONFIG_MIPI_PANEL_STA_2081101QFH032011_53G) += panel-STA_2081101QFH032011_53G
 panel-params-$(CONFIG_MIPI_PANEL_STA_HIMAX83102_J02) += panel-STA_HIMAX83102_J02
 panel-params-$(CONFIG_MIPI_PANEL_STA_ILI9882T) += panel-STA_ILI9882T

commit e81109ac97a0102484d2bd4aa3f60b2b63156435

drivers/mipi: Fine tune VFP, CLK and init code for IVO_T109NW41 panel
1. Adjust VFP and CLK to meet 60 +- 0.01 Hz
2. Fine tune init code for panel internal circuit

Fixes: 520137f("drivers/mipi: Add support for IVO_T109NW41 panel")

BUG=b:320892589
TEST=boot ciri with IVO_T109NW41 panel and see firmware screen

Signed-off-by: Ruihai Zhou <zhouruihai@huaqin.corp-partner.google.com>
Change-Id: I4d7c7bd4d79301fbb6d555117d190c358bceafcc
Reviewed-on: https://review.coreboot.org/c/coreboot/+/80086
Reviewed-by: cong yang <yangcong5@huaqin.corp-partner.google.com>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Yidi Lin <yidilin@google.com>
Reviewed-by: Yu-Ping Wu <yupingso@google.com>

src/drivers/mipi/panel-IVO_T109NW41.c
@@ -9,11 +9,11 @@ struct panel_serializable_data IVO_T109NW41 = {
 		.panel_bits_per_color = 8,
 		.panel_bits_per_pixel = 24,
 		.mode = {
-			.pixel_clock = 158400,
+			.pixel_clock = 162600,
 			.lvds_dual_channel = 0,
 			.refresh = 60,
 			.ha = 1200, .hbl = 120, .hso = 60, .hspw = 20,
-			.va = 1920, .vbl = 80, .vso = 60, .vspw = 8,
+			.va = 1920, .vbl = 135, .vso = 115, .vspw = 8,
 			.phsync = '-', .pvsync = '-',
 			.x_mm = 147, .y_mm = 235,
 		},
@@ -21,13 +21,14 @@ struct panel_serializable_data IVO_T109NW41 = {
 	.init = {
 		PANEL_DELAY(60),
 		PANEL_DCS(0xB9, 0x83, 0x10, 0x21, 0x55, 0x00),
-		PANEL_DCS(0xB1, 0x2C, 0xED, 0xED, 0x27, 0xE7, 0x52, 0xF5, 0x39,
+		PANEL_DCS(0xB1, 0x2C, 0xED, 0xED, 0x27, 0xE7, 0x42, 0xF5, 0x39,
 			0x36, 0x36, 0x36, 0x36, 0x32, 0x8B, 0x11, 0x65, 0x00, 0x88,
 			0xFA, 0xFF, 0xFF, 0x8F, 0xFF, 0x08, 0xD6, 0x33),
-		PANEL_DCS(0xB2, 0x00, 0x47, 0xB0, 0x80, 0x00, 0x12, 0x3A, 0x3C,
+		PANEL_DCS(0xB2, 0x00, 0x47, 0xB0, 0x80, 0x00, 0x12, 0x71, 0x3C,
 			0xA3, 0x22, 0x20, 0x00, 0x00, 0x88, 0x01),
 		PANEL_DCS(0xB4, 0x35, 0x35, 0x43, 0x43, 0x35, 0x35, 0x30, 0x7A,
-			0x30, 0x7A, 0x00, 0x9D),
+			0x30, 0x7A, 0x01, 0x9D),
+		PANEL_DCS(0xB6, 0x34, 0x34, 0x03),
 		PANEL_DCS(0xE9, 0xCD),
 		PANEL_DCS(0xBA, 0x84),
 		PANEL_DCS(0xE9, 0x3F),
@@ -39,8 +40,8 @@ struct panel_serializable_data IVO_T109NW41 = {
 		PANEL_DCS(0xE9, 0xCC),
 		PANEL_DCS(0xC7, 0x80),
 		PANEL_DCS(0xE9, 0x3F),
-		PANEL_DCS(0xE9, 0xC9),
-		PANEL_DCS(0xC8, 0x00),
+		PANEL_DCS(0xE9, 0xC6),
+		PANEL_DCS(0xC8, 0x97),
 		PANEL_DCS(0xE9, 0x3F),
 		PANEL_DCS(0xC9, 0x00, 0x1E, 0x13, 0x88, 0x01),
 		PANEL_DCS(0xCB, 0x08, 0x13, 0x07, 0x00, 0x0F, 0x34),
@@ -48,7 +49,7 @@ struct panel_serializable_data IVO_T109NW41 = {
 		PANEL_DCS(0xE9, 0xC4),
 		PANEL_DCS(0xD0, 0x03),
 		PANEL_DCS(0xE9, 0x3F),
-		PANEL_DCS(0xD1, 0x37, 0x06, 0x00, 0x02, 0x04, 0x2C, 0xFF),
+		PANEL_DCS(0xD1, 0x07, 0x06, 0x00, 0x02, 0x04, 0x2C, 0xFF),
 		PANEL_DCS(0xD3, 0x06, 0x00, 0x00, 0x00, 0x00, 0x08, 0x08, 0x08,
 			0x08, 0x37, 0x07, 0x64, 0x7C, 0x11, 0x11, 0x03, 0x03, 0x32,
 			0x10, 0x0E, 0x00, 0x0E, 0x32, 0x17, 0x97, 0x07, 0x97, 0x32,
@@ -93,7 +94,6 @@ struct panel_serializable_data IVO_T109NW41 = {
 		PANEL_DCS(0xE9, 0x3F),
 		PANEL_DCS(0xE1, 0x00),
 		PANEL_DCS(0xBD, 0x00),
-		PANEL_DCS(0xD2, 0x35, 0x35, 0x35, 0x35, 0x35, 0x35),
 		PANEL_DCS(0xE9, 0xC4),
 		PANEL_DCS(0xBA, 0x96),
 		PANEL_DCS(0xE9, 0x3F),
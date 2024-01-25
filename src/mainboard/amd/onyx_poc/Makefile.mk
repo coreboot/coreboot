4687325448c7e283ebb54ca967274590587f6d26
Varshit Pandya <pandyavarshit@gmail.com>
2024-01-25 13:58:47 +0530

    mb/amd/onyx_poc: Add and use APCB configuration data
    This patch adds APCB blobs to the mainboard directory and it replaces
    CB:76445 Also this brings onyx_poc mainboard inline with how APCB are
    included in other AMD mainboard: commit 95d05d830162 ("mb/google/zork:
    Add and use APCB configuration data"), commit I352f58e0d39 ("mb/google/
    skyrim: Add and use APCB configuration data") and commit I1c34528fa0f
    ("mb/amd/onyx_poc: Add and use APCB configuration data").
    
    BUG=none
    TEST=build/boot onyx_poc
    
    Change-Id: I1c34528fa0fd15b847c22c995713078c60ac3873
    Signed-off-by: Varshit Pandya <pandyavarshit@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/80204
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/amd/onyx_poc/Makefile.mk
+++ src/mainboard/amd/onyx_poc/Makefile.mk
@@ -1,11 +1,11 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
-ifneq ($(wildcard $(MAINBOARD_BLOBS_DIR)/Typex60_0_0_0_Apcb.bin),)
-APCB_SOURCES = $(MAINBOARD_BLOBS_DIR)/Typex60_0_0_0_Apcb.bin
-APCB_SOURCES1 = $(MAINBOARD_BLOBS_DIR)/Typex60_0_1_0_Apcb.bin
-APCB_SOURCES_RECOVERY = $(MAINBOARD_BLOBS_DIR)/Typex68_0_0_0_ApcbRec.bin
-APCB_SOURCES_RECOVERY1 = $(MAINBOARD_BLOBS_DIR)/Typex68_0_8_0_ApcbRec.bin
-APCB_SOURCES_RECOVERY2 = $(MAINBOARD_BLOBS_DIR)/Typex68_0_9_0_ApcbRec.bin
+ifneq ($(wildcard $(src)/mainboard/$(MAINBOARDDIR)/data.apcb),)
+APCB_SOURCES = $(src)/mainboard/$(MAINBOARDDIR)/data.apcb
+APCB_SOURCES1 = $(src)/mainboard/$(MAINBOARDDIR)/data1.apcb
+APCB_SOURCES_RECOVERY = $(src)/mainboard/$(MAINBOARDDIR)/data_rec.apcb
+APCB_SOURCES_RECOVERY1 = $(src)/mainboard/$(MAINBOARDDIR)/data_rec1.apcb
+APCB_SOURCES_RECOVERY2 = $(src)/mainboard/$(MAINBOARDDIR)/data_rec2.apcb
 else
 show_notices:: warn_no_apcb
 endif

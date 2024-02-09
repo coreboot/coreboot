a2acdce8d1f1b761bc2ddc575baac16439d035b4
Varshit Pandya <pandyavarshit@gmail.com>
2024-02-09 18:34:15 +0530

    soc/amd/picasso: Use gpp_clk_setup_common function
    In follow up to CB:80285 use gpp_clk_setup_common for picasso as well.
    
    Change-Id: I68d498d08d5975037086c84ff2f7fdb265ee84d9
    Signed-off-by: Varshit Pandya <pandyavarshit@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/80414
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/picasso/include/soc/southbridge.h
+++ src/soc/amd/picasso/include/soc/southbridge.h
@@ -72,21 +72,6 @@
 #define GPE0_LIMIT			32
 #define TOTAL_BITS(a)			(8 * sizeof(a))
 
-/* FCH MISC Registers 0xfed80e00 */
-#define GPP_CLK_CNTRL			0x00
-#define   GPP_CLK0_REQ_SHIFT		0
-#define   GPP_CLK1_REQ_SHIFT		2
-#define   GPP_CLK4_REQ_SHIFT		4
-#define   GPP_CLK2_REQ_SHIFT		6
-#define   GPP_CLK3_REQ_SHIFT		8
-#define   GPP_CLK5_REQ_SHIFT		10
-#define   GPP_CLK6_REQ_SHIFT		12
-#define     GPP_CLK_OUTPUT_COUNT	7
-#define   GPP_CLK_REQ_MASK(clk_shift)	(0x3 << (clk_shift))
-#define   GPP_CLK_REQ_ON(clk_shift)	(0x3 << (clk_shift))
-#define   GPP_CLK_REQ_EXT(clk_shift)	(0x1 << (clk_shift))
-#define   GPP_CLK_REQ_OFF(clk_shift)	(0x0 << (clk_shift))
-
 #define MISC_CGPLL_CONFIG1		0x08
 #define   CG1PLL_SPREAD_SPECTRUM_ENABLE	BIT(0)
 #define MISC_CLK_CNTL1			0x40

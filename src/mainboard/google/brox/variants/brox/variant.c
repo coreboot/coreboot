63f0ebf1d7b69f4fdb9f067a4be95b6d0f7ea680
Ashish Kumar Mishra <ashish.k.mishra@intel.com>
2024-02-15 16:52:10 +0530

    mb/google/brox: Handle bluetooth enable on devices
    For devices that require CNVi Bluetooth select WIFI_BT_CNVI
    in FW_CONFIG. Discrete Bluetooth devices need to select
    WIFI_BT_PCIE.
    
    BUG=b:319188820,b:325084796
    BRANCH=None
    TEST=Boot image on SKU1,SKU2 and check BT devices enumerate.
    
    Change-Id: Iba008682fcfa7ddc1ec400649c8742c721666f1d
    Signed-off-by: Ashish Kumar Mishra <ashish.k.mishra@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/80564
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Shelley Chen <shchen@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brox/variants/brox/variant.c
+++ src/mainboard/google/brox/variants/brox/variant.c
@@ -8,9 +8,13 @@
 
 void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
 {
+	if (fw_config_probe(FW_CONFIG(WIFI_BT, WIFI_BT_CNVI))) {
+		printk(BIOS_INFO, "CNVi bluetooth enabled by fw_config\n");
+		config->cnvi_bt_core = true;
+	}
 }
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
-	return get_wifi_sar_fw_config_filename(FW_CONFIG_FIELD(WIFI));
+	return get_wifi_sar_fw_config_filename(FW_CONFIG_FIELD(WIFI_BT));
 }

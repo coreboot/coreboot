10291e800ca45c722ac43d205c0b8ed46633ab56
Martin Roth <gaumless@gmail.com>
2024-02-22 19:04:26 -0700

    amdfwtool: Use Makefile.mk for Makefile settings
    When updating the Makefiles, to keep from having to update two files at
    the same time, import Makefile.mk into the external Makefile. This
    allows the bulk of the settings to be in a single location.
    
    While I'm here, I adjusted the print statements to match the rest of
    coreboot.
    
    Change-Id: Id5b869f49b34b22e6a02fc086e7b42975141a87e
    Signed-off-by: Martin Roth <gaumless@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/80715
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>

--- util/amdfwtool/Makefile.mk
+++ util/amdfwtool/Makefile.mk
@@ -4,12 +4,18 @@ ifneq ($(BUILD_ALL_TOOLS)$(CONFIG_USE_AMDFWTOOL),)
 
 amdfwtoolobj = amdfwtool.o data_parse.o signed_psp.o handle_file.o
 amdfwreadobj = amdfwread.o
+amdfwheader = amdfwtool.h
 
-AMDFWTOOLCFLAGS :=-O2 -Wall -Wextra -Wshadow -Werror
+WERROR ?= -Werror
+AMDFWTOOLCFLAGS :=-O2 -Wall -Wextra -Wshadow $(WERROR)
 AMDFWTOOLCFLAGS += -I $(top)/src/commonlib/bsd/include
 AMDFWTOOLCFLAGS += -D_GNU_SOURCE # memmem() from string.h
 
+ifneq ($(PKG_CONFIG),)
+HOSTPKGCONFIG ?= $(PKG_CONFIG)
+else
 HOSTPKGCONFIG ?= pkg-config
+endif
 
 AMDFWTOOLCFLAGS += $(shell $(HOSTPKGCONFIG) --cflags libcrypto)
 ifneq ($(.SHELLSTATUS),0)
@@ -17,15 +23,16 @@ $(error "Ensure that pkg-config is installed.")
 endif
 LDFLAGS += $(shell $(HOSTPKGCONFIG) --libs libcrypto)
 
-$(objutil)/amdfwtool/%.o: $(top)/util/amdfwtool/%.c # $(HEADER)
+$(objutil)/amdfwtool/%.o: $(top)/util/amdfwtool/%.c $(dir)/$(amdfwheader)
+	printf "    AMDFW      $@\n"
 	$(HOSTCC) $(AMDFWTOOLCFLAGS) $(HOSTCFLAGS) -c -o $@ $<
 
-$(objutil)/amdfwtool/amdfwtool: $(addprefix $(objutil)/amdfwtool/,$(amdfwtoolobj))
-	printf "   AMDFWTOOL\n"
+$(objutil)/amdfwtool/amdfwtool: $(addprefix $(objutil)/amdfwtool/,$(amdfwtoolobj)) $(dir)/$(amdfwheader)
+	printf "    AMDFWTOOL  $@\n"
 	$(HOSTCC) $(addprefix $(objutil)/amdfwtool/,$(amdfwtoolobj)) $(LDFLAGS) -o $@
 
-$(objutil)/amdfwtool/amdfwread: $(addprefix $(objutil)/amdfwtool/,$(amdfwreadobj))
-	printf "   AMDFWREAD\n"
+$(objutil)/amdfwtool/amdfwread: $(addprefix $(objutil)/amdfwtool/,$(amdfwreadobj)) $(dir)/$(amdfwheader)
+	printf "    AMDFWREAD  $@\n"
 	$(HOSTCC) $(addprefix $(objutil)/amdfwtool/,$(amdfwreadobj)) $(LDFLAGS) -o $@
 
 endif # ifneq ($(BUILD_ALL_TOOLS)$(CONFIG_USE_AMDFWTOOL),)

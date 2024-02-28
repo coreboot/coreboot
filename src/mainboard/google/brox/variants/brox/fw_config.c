55a7d90a500782d861214ab743d860148465535a
Li Feng <li1.feng@intel.com>
2024-02-27 21:47:15 -0800

    mb/google/brox: support ISH
    Set FW_CONFIG bit 21 to enable ISH PCI device and define ISH main
    firmware name so ISH shim loader can load firmware from file system.
    
    ISH also need to be enabled if STORAGE_UFS is set.
    
    BUG=b:280329972
    TEST= Set bit CBI FW_CONFIG bit 21
          Boot Brox board, check that ISH is enabled and loaded
          lspci shows: 00:12.0 Serial controller: Intel Corporation Alder
    Lake-P Integrated Sensor Hub (rev 01).
    
    Change-Id: Iadc5108c62737d27642a6948c00b5c122541aaba
    Signed-off-by: Li Feng <li1.feng@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/80773
    Reviewed-by: Tanu Malhotra <tanu.malhotra@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Shelley Chen <shchen@google.com>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Yuval Peress <peress@google.com>

--- src/mainboard/google/brox/variants/brox/fw_config.c
+++ src/mainboard/google/brox/variants/brox/fw_config.c
@@ -1,8 +1,35 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootstate.h>
+#include <fw_config.h>
+#include <gpio.h>
+
+#define GPIO_PADBASED_OVERRIDE(b, a) gpio_padbased_override(b, a, ARRAY_SIZE(a))
+
+static const struct pad_config ish_enable_pads[] = {
+	/* GPP_B5 : ISH I2C0_SDA */
+	PAD_CFG_NF_IOSTANDBY_IGNORE(GPP_B5, NONE, DEEP, NF1),
+	/* GPP_B6 : ISH_I2C0_SCL */
+	PAD_CFG_NF_IOSTANDBY_IGNORE(GPP_B6, NONE, DEEP, NF1),
+	/* GPP_D13 : [NF1: ISH_UART0_RXD ==> UART0_ISH_RX_DBG_TX */
+	PAD_CFG_NF(GPP_D13, NONE, DEEP, NF1),
+	/* GPP_D14 : [NF1: ISH_UART0_TXD ==> UART0_ISH_TX_DBG_RX */
+	PAD_CFG_NF(GPP_D14, NONE, DEEP, NF1),
+	/* GPP_D2 : ISH_GP2, SOC_ISH_ACCEL_INT_L */
+	PAD_CFG_NF(GPP_D2, NONE, DEEP, NF1),
+	/* GPP_D3 : ISH_GP3, SOC_ISH_IMU_INT_L */
+	PAD_CFG_NF(GPP_D3, NONE, DEEP, NF1),
+	/* GPP_B15 : ISH_GP7, LID_OPEN_1V8 */
+	PAD_CFG_NF(GPP_B15, NONE, DEEP, NF5),
+	/* GPP_A16 : ISH_GP5, TABLET_MODE_ODL */
+	PAD_CFG_NF(GPP_A16, NONE, DEEP, NF4),
+};
 
 static void fw_config_handle(void *unused)
 {
+	if (fw_config_probe(FW_CONFIG(ISH, ISH_ENABLE))) {
+		printk(BIOS_INFO, "Configure GPIOs for ISH.\n");
+		gpio_configure_pads(ish_enable_pads, ARRAY_SIZE(ish_enable_pads));
+	}
 }
 BOOT_STATE_INIT_ENTRY(BS_DEV_ENABLE, BS_ON_ENTRY, fw_config_handle, NULL);

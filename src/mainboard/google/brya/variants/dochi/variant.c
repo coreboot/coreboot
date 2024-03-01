3266dcbff0df9d36c77d0a3ba9b96f104bd80ef5
Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
2024-03-01 18:11:33 +0800

    mb/google/brya/var/dochi: Add wifi sar table
    Add wifi sar table for dochi
    
    BUG=b:326137130
    BRANCH=firmware-brya-14505.B
    TEST=emerge-brya coreboot chromeos-bootimage
    
    Change-Id: Iaf90756eb318bef1ffcda9368a976c0ca209a100
    Signed-off-by: Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/80809
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: Bob Moragues <moragues@google.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

--- src/mainboard/google/brya/variants/dochi/variant.c
+++ src/mainboard/google/brya/variants/dochi/variant.c
@@ -1,11 +1,17 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <baseboard/variants.h>
 #include <chip.h>
 #include <fw_config.h>
-#include <baseboard/variants.h>
+#include <sar.h>
 
 void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
 {
 	if (fw_config_probe(FW_CONFIG(FP_MCU, FP_ABSENT)))
 		config->serial_io_gspi_mode[PchSerialIoIndexGSPI1] = PchSerialIoDisabled;
+}
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return "wifi_sar_0.hex";
 }

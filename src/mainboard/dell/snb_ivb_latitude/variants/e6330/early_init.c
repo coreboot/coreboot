189a59edbc7889041d165b267dd0e4544263359a
Nicholas Chin <nic.c3.14@gmail.com>
2024-03-08 09:33:03 -0700

    mb/dell: Add Latitude E6330 (Ivy Bridge)
    Mainboard is QAL70/LA-7741P. I do not physically have this system;
    someone with physical access to one sent me the output of autoport which
    I then modified to produce this port. I was also sent the VBT binary,
    which was obtained from `/sys/kernel/debug/dri/0/i915_vbt` while running
    version A21 of the vendor firmware. This port has not been tested.
    
    The EC is the SMSC MEC5055, which seems to be compatible with the
    existing MEC5035 code. As with the other Dell systems with this EC, this
    board is assumed to be internally flashable using an EC command that
    tells it to pull the FDO pin low on the next boot, which also tells the
    vendor firmware to disable all write protections to the flash [1].
    
    [1] https://gitlab.com/nic3-14159/dell-flash-unlock
    
    Change-Id: I827826e9ff8a9a534c50250458b399104478e06c
    Signed-off-by: Nicholas Chin <nic.c3.14@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/82152
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>

---
+++ src/mainboard/dell/snb_ivb_latitude/variants/e6330/early_init.c
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <bootblock_common.h>
+#include <device/pci_ops.h>
+#include <ec/dell/mec5035/mec5035.h>
+#include <southbridge/intel/bd82x6x/pch.h>
+
+void bootblock_mainboard_early_init(void)
+{
+	pci_write_config16(PCH_LPC_DEV, LPC_EN, CNF1_LPC_EN | MC_LPC_EN
+			| KBC_LPC_EN | FDD_LPC_EN | LPT_LPC_EN
+			| COMB_LPC_EN | COMA_LPC_EN);
+	mec5035_early_init();
+}

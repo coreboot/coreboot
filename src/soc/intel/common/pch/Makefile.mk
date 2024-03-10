96d7524ee69f07cc54ecf5527953380c8c5f5c48
Shuo Liu <shuo.liu@intel.com>
2024-03-11 04:56:19 +0800

    intel/common/pch: Add Kconfig SOC_INTEL_COMMON_IBL_BASE
    IBL (Integrated Boot Logic) provides a subset of server
    PCH logics for no-PCH solution. IBL is with limited features
    and registers exposed, PCIe root ports/USB/SATA/LAN support are
    removed.
    
    Change-Id: I8f3d64a2dd3b79ec5a9e4306f40b012b00387259
    Signed-off-by: Shuo Liu <shuo.liu@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/81314
    Reviewed-by: Lean Sheng Tan <sheng.tan@9elements.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/common/pch/Makefile.mk
+++ src/soc/intel/common/pch/Makefile.mk
@@ -1,6 +1,11 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 subdirs-$(CONFIG_SOC_INTEL_COMMON_PCH_BASE) += ./*
+subdirs-$(CONFIG_SOC_INTEL_COMMON_IBL_BASE) += ./*
 
 ifeq ($(CONFIG_SOC_INTEL_COMMON_PCH_BASE),y)
+CPPFLAGS_common += -I$(src)/soc/intel/common/pch/include/
+endif
+
+ifeq ($(CONFIG_SOC_INTEL_COMMON_IBL_BASE),y)
 CPPFLAGS_common += -I$(src)/soc/intel/common/pch/include/
 endif

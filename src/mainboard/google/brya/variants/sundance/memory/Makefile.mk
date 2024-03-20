f2492c383c62a3ae011ea5aa5569b4d96d7364ca
Leo Chou <leo.chou@lcfc.corp-partner.google.com>
2024-03-20 11:42:09 +0800

    mb/google/nissa: Create sundance variant
    Create the sundance variant of nissa reference board by copying the
    template files to a new directory named for the variant.
    
    Due to new_variant.py limitation that repo can no longer be used in
    inside, created this CL manually following google suggestion.
    
    BUG=b:328505938
    TEST=util/abuild/abuild -p none -t google/brya -x -a
    make sure the build includes GOOGLE_SUNDANCE
    
    Change-Id: Ia8ba318f18d2cac69898687311631778e61bf2ea
    Signed-off-by: Leo Chou <leo.chou@lcfc.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/81347
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: Shou-Chieh Hsu <shouchieh@google.com>

---
+++ src/mainboard/google/brya/variants/sundance/memory/Makefile.mk
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# This is an auto-generated file. Do not edit!!
+# Add memory parts in mem_parts_used.txt and run spd_tools to regenerate.
+
+SPD_SOURCES = placeholder

d2d7933b488b846448c7dc7daf9bd14acee2e1ef
Nicholas Chin <nic.c3.14@gmail.com>
2024-03-21 09:41:45 -0600

    src: Add missing SPDX license headers
    Other files in the commits that added these files were licensed under
    GPL-2.0-only, and the project as a whole is GPL-2.0-only, so use that
    as the license.
    
    Change-Id: I6c1a7ba582f61f98069ebf3857a8b5bdc8588c3e
    Signed-off-by: Nicholas Chin <nic.c3.14@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/81421
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/myst/variants/baseboard/include/baseboard/port_descriptors.h
+++ src/mainboard/google/myst/variants/baseboard/include/baseboard/port_descriptors.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
 #ifndef __BASEBOARD_PORT_DESCRIPTORS_H__
 #define __BASEBOARD_PORT_DESCRIPTORS_H__
 
--- src/soc/sifive/fu740/include/soc/gpio.h
+++ src/soc/sifive/fu740/include/soc/gpio.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
 #ifndef _SOC_SIFIVE_FU740_GPIO_H_
 #define _SOC_SIFIVE_FU740_GPIO_H_
 

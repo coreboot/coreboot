5d57af9a36d8ef1c1a531034f7d7e5cdb182fb77
Elyes Haouas <ehaouas@noos.fr>
2024-03-23 15:46:00 +0100

    mb/acer: Remove blank lines before '}' and after '{'
    Change-Id: I335487ca1b17ab958c0d0238f425b92b430014ca
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/81494
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>

--- src/mainboard/acer/aspire_vn7_572g/gpio.c
+++ src/mainboard/acer/aspire_vn7_572g/gpio.c
@@ -20,7 +20,6 @@
 
 /* Pad configuration was generated automatically using intelp2m utility */
 static const struct pad_config gpio_table[] = {
-
 	/* ------- GPIO Community 0 ------- */
 
 	/* ------- GPIO Group GPP_A ------- */

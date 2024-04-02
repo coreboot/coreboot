0368e43e626de299e1bbca78370b78c1ae530889
Dinesh Gehlot <digehlot@google.com>
2024-04-02 11:50:46 +0530

    mb/google/brya: Add new baseboard trulo
    This patch adds a new baseboard trulo. This commit is a stub which
    only adds the minimum code needed for a successful build.
    
    BUG=b:333314089
    TEST=abuild -a -x -p none -t google/brya
    
    Change-Id: Iad6230064c6b8359698d37c3e0440614cc7b073d
    Signed-off-by: Dinesh Gehlot <digehlot@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/81626
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

---
+++ src/mainboard/google/brya/variants/baseboard/trulo/include/baseboard/gpio.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef __BASEBOARD_GPIO_H__
+#define __BASEBOARD_GPIO_H__
+
+#include <soc/gpe.h>
+#include <soc/gpio.h>
+
+/* TODO: Set the correct values */
+#define EC_SCI_GPI		0
+#define GPIO_PCH_WP		0
+#define GPIO_EC_IN_RW		0
+#define GPIO_SLP_S0_GATE	0
+
+#endif /* __BASEBOARD_GPIO_H__ */

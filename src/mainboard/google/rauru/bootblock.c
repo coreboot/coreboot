35852c97a31895836cb1bd2cc0be609f00c67a7e
Yidi Lin <yidilin@chromium.org>
2024-04-08 15:30:23 +0800

    mb/google/rauru: Reset USB hub in bootblock
    We have to reset the USB hub as early as possible. Otherwise the USB3
    hub may not be usable in the payload. This design has been introduced
    since Cherry.
    
    TEST=build pass.
    BUG=b:317009620
    
    Change-Id: Iea793b4b04bd009d0354e2331604bccf30466a23
    Signed-off-by: Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84024
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/rauru/bootblock.c
+++ src/mainboard/google/rauru/bootblock.c
@@ -3,7 +3,15 @@
 #include <bootblock_common.h>
 #include <soc/spi.h>
 
+#include "gpio.h"
+
+static void usb3_hub_reset(void)
+{
+	gpio_output(GPIO(USB_RST), 1);
+}
+
 void bootblock_mainboard_init(void)
 {
 	mtk_snfc_init();
+	usb3_hub_reset();
 }

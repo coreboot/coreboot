e437cb5f87a022c5e219ce1aadfd16a2078e0e86
Maximilian Brune <maximilian.brune@9elements.com>
2024-04-15 12:58:29 +0200

    soc/sifive/fu540/chip.c: Add RAM resources
    Add RAM region so that the payload can be placed in there without
    coreboot complaining that the payload doesn't target a RAM region.
    
    Signed-off-by: Maximilian Brune <maximilian.brune@9elements.com>
    Change-Id: Id07eae3560ce69cd8a6a695702fa0b4463c50855
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/81909
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/soc/sifive/fu540/chip.c
+++ src/soc/sifive/fu540/chip.c
@@ -1,6 +1,18 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <cbmem.h>
 #include <device/device.h>
+#include <soc/addressmap.h>
+
+static void fu540_read_resources(struct device *dev)
+{
+	int index = 0;
+	ram_from_to(dev, index++, FU540_DRAM, (uintptr_t)cbmem_top());
+}
+
+struct device_operations fu540_cpu_ops = {
+	.read_resources = fu540_read_resources,
+};
 
 struct chip_operations soc_sifive_fu540_ops = {
 	.name = "SIFIVE FU540",

97be4e7209bfdaa6c49a21c63f4cb0a18f77bce2
Jianjun Wang <jianjun.wang@mediatek.com>
2024-04-17 10:38:53 +0800

    soc/mediatek/mt8196: Add PCIe driver and early init support
    Add PCIe driver for MT8196 platform.
    
    According to the PCIe CEM specification, the deassertion of PERST#
    should occur at least 100ms after the assertion. To ensure the 100ms
    delay requirement is met and to save delay time in the ramstage, add
    an early init data region to store the elapsed time since assertion.
    This will speed up the boot time by 100ms.
    
    PCIe port 1 and port 2 share the same PCIe resources, but PCIe port 2 is
    not used. Therefore, in mtk_pcie_pre_init(), make sure PCIe port 2 is
    reset to prevent interference with PCIe port 1.
    
    TEST=Build pass, show pcie init pass log:
    mtk_pcie_domain_enable: PCIe link up success (1)
    BUG=b:317009620
    
    Signed-off-by: Jianjun Wang <jianjun.wang@mediatek.com>
    Change-Id: I826a96822e88972bcd4966b6681797a646adf3d6
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84696
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- src/soc/mediatek/mt8196/Makefile.mk
+++ src/soc/mediatek/mt8196/Makefile.mk
@@ -10,7 +10,9 @@ all-y += timer.c timer_prepare.c
 all-y += ../common/uart.c
 
 bootblock-y += bootblock.c
+bootblock-y += ../common/early_init.c
 bootblock-y += ../common/mmu_operations.c
+bootblock-$(CONFIG_PCI) += ../common/pcie.c pcie.c
 bootblock-y += ../common/wdt.c ../common/wdt_req.c wdt.c
 
 romstage-y += ../common/cbmem.c
@@ -18,9 +20,11 @@ romstage-y += emi.c
 romstage-y += l2c_ops.c
 romstage-y += ../common/mmu_operations.c ../common/mmu_cmops.c
 
+ramstage-y += ../common/early_init.c
 ramstage-y += emi.c
 ramstage-y += l2c_ops.c
 ramstage-y += ../common/mmu_operations.c ../common/mmu_cmops.c
+ramstage-$(CONFIG_PCI) += ../common/pcie.c pcie.c
 ramstage-y += soc.c
 ramstage-y += ../common/usb.c usb.c
 
---
+++ src/soc/mediatek/mt8196/include/soc/soc_chip.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR MIT */
+
+#ifndef SOC_MEDIATEK_SOC_CHIP_H
+#define SOC_MEDIATEK_SOC_CHIP_H
+
+#include "../../chip.h"
+
+#endif /* SOC_MEDIATEK_SOC_CHIP_H */

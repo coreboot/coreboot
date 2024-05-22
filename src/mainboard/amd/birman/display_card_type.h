452055565605e171f20749c127db0fa756b0e79a
Felix Held <felix-coreboot@felixheld.de>
2024-05-22 17:51:59 +0200

    mb/amd/birman/display_card_type.h: add missing include guards
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: Iaf4478814e672fb8cfae5ffc4fa89c475f5bb0b2
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/82607
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-by: Matt DeVillier <matt.devillier@amd.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/amd/birman/display_card_type.h
+++ src/mainboard/amd/birman/display_card_type.h
@@ -1,3 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#ifndef MAINBOARD_DISPLAY_CARD_TYPE_H
+#define MAINBOARD_DISPLAY_CARD_TYPE_H
+
 uint8_t get_ddi1_type(void);
+
+#endif /* MAINBOARD_DISPLAY_CARD_TYPE_H */

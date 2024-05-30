b0692f65b6513592b5792125493b2848ce6e642a
Leo Chou <leo.chou@lcfc.corp-partner.google.com>
2024-05-30 15:54:11 +0800

    mb/google/nissa/var/pujjoga: Add wifi sar table
    Add AX211 and AX203 wifi sar table for pujjoga wifi sar config.
    Use fw_config to separate different wifi card settings.
    
    WIFI_SAR_TABLE_AX211:	0
    WIFI_SAR_TABLE_AX203:	1
    
    BUG=b:336167281
    Test=emerge-nissa coreboot
    
    Change-Id: If0f542cb13e93e99960bf65d616b26cee7617a43
    Signed-off-by: Leo Chou <leo.chou@lcfc.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/82702
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>

---
+++ src/mainboard/google/brya/variants/pujjoga/variant.c
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <fw_config.h>
+#include <sar.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return get_wifi_sar_fw_config_filename(FW_CONFIG_FIELD(WIFI_SAR_ID));
+}

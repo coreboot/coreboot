74472453ed2165e9664de92f65f502a5bf7ae696
Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
2024-06-11 16:02:19 +0800

    mb/google/trulo/var/orisa: Disable storage devices based on fw_config
    Disable devices in variant.c instead of adding probe statements to
    devicetree because storage devices need to be enabled when fw_config is
    unprovisioned.
    
    BUG=b:345112878
    BRANCH=firmware-nissa-15217.B
    TEST=emerge-nissa coreboot chromeos-bootimage
    
    Change-Id: I1e5e49c1baa8d2b00134c26cc3b69aa15712b512
    Signed-off-by: Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/82907
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Eric Lai <ericllai@google.com>

--- src/mainboard/google/brya/variants/orisa/Makefile.mk
+++ src/mainboard/google/brya/variants/orisa/Makefile.mk
@@ -1,7 +1,11 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
 bootblock-y += gpio.c
+
 romstage-y += gpio.c
 romstage-y += memory.c
+
+ramstage-$(CONFIG_FW_CONFIG) += fw_config.c
 ramstage-y += gpio.c
 ramstage-$(CONFIG_SOC_INTEL_COMMON_BLOCK_HDA_VERB) += hda_verb.c
+ramstage-y += variant.c

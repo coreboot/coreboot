0395b4b5f274e2cfd1d1073f5d221393d8d8e253
Patrick Rudolph <patrick.rudolph@9elements.com>
2024-06-14 17:07:29 +0200

    mb/emulation/qemu: Configure TSEG size
    Configure TSEG size by reading CONFIG_SMM_TSEG_SIZE in romstage.
    The remaining Qemu code can already handle the bigger TSEG region.
    
    TEST: Increased TSEG to 8MiB.
    
    Change-Id: I1ae5ac93ecca83ae9c319c666aac844bbd5b259f
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83114
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>

--- src/mainboard/emulation/qemu-q35/romstage.c
+++ src/mainboard/emulation/qemu-q35/romstage.c
@@ -7,12 +7,29 @@
 
 #include "q35.h"
 
+#define TSEG_SZ_MASK	(3 << 1)
+
 void mainboard_romstage_entry(void)
 {
 	i82801ix_early_init();
 
 	if (!CONFIG(BOOTBLOCK_CONSOLE))
 		mainboard_machine_check();
+
+	/* Configure requested TSEG size */
+	switch (CONFIG_SMM_TSEG_SIZE) {
+	case 1 * MiB:
+		pci_update_config8(HOST_BRIDGE, ESMRAMC, ~TSEG_SZ_MASK, 0 << 1);
+		break;
+	case 2 * MiB:
+		pci_update_config8(HOST_BRIDGE, ESMRAMC, ~TSEG_SZ_MASK, 1 << 1);
+		break;
+	case 8 * MiB:
+		pci_update_config8(HOST_BRIDGE, ESMRAMC, ~TSEG_SZ_MASK, 2 << 1);
+		break;
+	default:
+		printk(BIOS_WARNING, "%s: Unsupported TSEG size: 0x%x\n", __func__, CONFIG_SMM_TSEG_SIZE);
+	}
 
 	cbmem_recovery(0);
 }

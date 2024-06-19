96d48fe3d2daf71266df2dc36c3bd92317c08e01
Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
2024-06-19 14:18:12 +0800

    mb/google/brask/var/constitution: Generate SPD ID for supported parts
    Add supported memory part in mem_parts_used.txt, then generate.
    
    H54G56CYRBX247
    
    BUG=b:199645942
    TEST=run part_id_gen to generate SPD id
    
    Change-Id: I2169d71695d8d133d26cafe5c7be33b976dd8603
    Signed-off-by: Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83127
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/constitution/memory/Makefile.mk
+++ src/mainboard/google/brya/variants/constitution/memory/Makefile.mk
@@ -5,4 +5,4 @@
 
 SPD_SOURCES =
 SPD_SOURCES += spd/lp4x/set-0/spd-1.hex      # ID = 0(0b0000)  Parts = K4U6E3S4AB-MGCL
-SPD_SOURCES += spd/lp4x/set-0/spd-3.hex      # ID = 1(0b0001)  Parts = MT53E1G32D2NP-046 WT:B, K4UBE3D4AB-MGCL
+SPD_SOURCES += spd/lp4x/set-0/spd-3.hex      # ID = 1(0b0001)  Parts = MT53E1G32D2NP-046 WT:B, K4UBE3D4AB-MGCL, H54G56CYRBX247

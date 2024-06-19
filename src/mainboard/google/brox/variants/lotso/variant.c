daea4e7934d5fe67bed1f7544e3ce6263091a209
Jing Tong <tongjian@huaqin.corp-partner.google.com>
2024-06-19 11:34:51 +0800

    mb/google/brox/var/lotso: enable CNVi bluetooth
    Lotso's WIFI_BT is same design as brox, copy from brox.
    
    BUG=b:339612353
    TEST=emerge-brox coreboot chromeos-bootimage and boot on
    
    Change-Id: I030e306dc5d9d3fcb6314bc491dbf5c9ae60bcb7
    Signed-off-by: Jing Tong <tongjian@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83126
    Reviewed-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Wentao Qin <qinwentao@huaqin.corp-partner.google.com>

---
+++ src/mainboard/google/brox/variants/lotso/variant.c
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <assert.h>
+#include <baseboard/variants.h>
+#include <chip.h>
+#include <fw_config.h>
+#include <sar.h>
+
+void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
+{
+	if (fw_config_probe(FW_CONFIG(WIFI_BT, WIFI_CNVI_WIFI6E))) {
+		printk(BIOS_INFO, "CNVi bluetooth enabled by fw_config\n");
+		config->cnvi_bt_core = true;
+	}
+}
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return get_wifi_sar_fw_config_filename(FW_CONFIG_FIELD(WIFI_BT));
+}

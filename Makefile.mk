d05fe9fd3ce9c741ff802a32229c85ab14420702
Felix Held <felix-coreboot@felixheld.de>
2024-07-02 20:47:53 +0000

    Revert "Makefile.mk: Use Walloc-size GCC option"
    This reverts commit 6ab188ee6c99b1d9924b607d7e939d91e35014ec.
    
    This breaks the build using a slightly older toolchain that doesn't know
    this option yet.
    
    Signed-off-by: Felix Held <felix-coreboot@felixheld.de>
    Change-Id: I0bdc909c0e53b5353743dca521c963bbec792f7e
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83311
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-by: Nicholas Chin <nic.c3.14@gmail.com>

--- Makefile.mk
+++ Makefile.mk
@@ -535,7 +535,6 @@ CFLAGS_common += -fno-pie
 CFLAGS_common += -Wstring-compare
 ifeq ($(CONFIG_COMPILER_GCC),y)
 CFLAGS_common += -Wold-style-declaration
-CFLAGS_common += -Walloc-size
 CFLAGS_common += -Wcast-function-type
 # Don't add these GCC specific flags when running scan-build
 ifeq ($(CCC_ANALYZER_OUTPUT_FORMAT),)

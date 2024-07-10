188909aad4ba3eeadba2c7ad6fc08858800b8f7d
zengqinghong <zengqinghong@huaqin.corp-partner.google.com>
2024-07-10 20:54:41 +0800

    mb/google/nissa: Create teliks variant
    Create the teliks variant of the nissa reference board by copying
    the anraggar files to a new directory named for the variant.
    
    BUG=b:352263941
    BRANCH=None
    TEST=1. util/abuild/abuild -p none -t google/brya -x -a
            make sure the build includes GOOGLE_TELIKS
         2. Run part_id_gen tool without any errors
    
    Change-Id: I744f4d7c2d35544d3a8a8f76e24bad3298442768
    Signed-off-by: zengqinghong <zengqinghong@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83408
    Reviewed-by: Weimin Wu <wuweimin@huaqin.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

---
+++ src/mainboard/google/brya/variants/teliks/include/variant/gpio.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef VARIANT_GPIO_H
+#define VARIANT_GPIO_H
+
+#include <baseboard/gpio.h>
+
+#define WWAN_FCPO	GPP_F11 /* FULL_CARD_POWER_OFF# */
+#define WWAN_RST	GPP_F12
+#define T2_OFF_MS	20
+
+#endif
---
+++ src/mainboard/google/brya/variants/teliks/variant.c
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <baseboard/variants.h>
+#include <chip.h>
+#include <fw_config.h>
+#include <sar.h>
+#include <soc/gpio_soc_defs.h>
+
+const char *get_wifi_sar_cbfs_filename(void)
+{
+	return get_wifi_sar_fw_config_filename(FW_CONFIG_FIELD(WIFI));
+}
+
+static const struct pad_config wifi_pcie_enable_pad[] = {
+	/* H20 : IMGCLKOUT1 ==> WLAN_PERST_L */
+	PAD_CFG_GPO(GPP_H20, 1, DEEP),
+	/* B11 : NC ==> EN_PP3300_WLAN_X*/
+	PAD_CFG_GPO(GPP_B11, 1, DEEP),
+};
+
+void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
+{
+	if (!fw_config_probe(FW_CONFIG(WIFI, WIFI_6E))) {
+		printk(BIOS_INFO, "CNVi bluetooth disabled by fw_config\n");
+		config->cnvi_bt_core = false;
+		printk(BIOS_INFO, "CNVi bluetooth audio offload disabled by fw_config\n");
+		config->cnvi_bt_audio_offload = false;
+	}
+}
+
+void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
+{
+	if (fw_config_probe(FW_CONFIG(WIFI, WIFI_6))) {
+		printk(BIOS_INFO, "Enable PCie based Wifi GPIO pins.\n");
+		gpio_padbased_override(padbased_table, wifi_pcie_enable_pad,
+						ARRAY_SIZE(wifi_pcie_enable_pad));
+	}
+}

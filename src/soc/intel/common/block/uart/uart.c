661382960fc16e65ca6d8cfbe206da795b004e0d
Subrata Banik <subratabanik@google.com>
2024-07-17 10:23:58 +0000

    device/pci_ids: Add new Intel PTL device IDs for UARTx
    This patch adds new UARTx PCI device IDs for Intel PTL-U
    and PTL-H.
    
    Additionally, updates the UART driver's `pci_device_ids` list to
    include these new IDs.
    
    Source: Intel PTL-EDS vol 1. Document Number 815002, Rev 0.51 Chapter 2
    
    BUG=b:347669091
    TEST=Able to build google/fatcat.
    
    Change-Id: I384a753f08ae5a752cef6009d07104e8ff4b4a6e
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83510
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/soc/intel/common/block/uart/uart.c
+++ src/soc/intel/common/block/uart/uart.c
@@ -308,7 +308,8 @@ static const char *uart_acpi_hid(const struct device *dev)
 static const char *uart_acpi_name(const struct device *dev)
 {
 	switch (dev->device) {
-	case PCI_DID_INTEL_PTL_UART0:
+	case PCI_DID_INTEL_PTL_H_UART0:
+	case PCI_DID_INTEL_PTL_U_H_UART0:
 	case PCI_DID_INTEL_LNL_UART0:
 	case PCI_DID_INTEL_ADP_P_UART0:
 	case PCI_DID_INTEL_APL_UART0:
@@ -317,7 +318,8 @@ static const char *uart_acpi_name(const struct device *dev)
 	case PCI_DID_INTEL_SPT_H_UART0:
 	case PCI_DID_INTEL_CNP_H_UART0:
 		return "UAR0";
-	case PCI_DID_INTEL_PTL_UART1:
+	case PCI_DID_INTEL_PTL_H_UART1:
+	case PCI_DID_INTEL_PTL_U_H_UART1:
 	case PCI_DID_INTEL_LNL_UART1:
 	case PCI_DID_INTEL_ADP_P_UART1:
 	case PCI_DID_INTEL_APL_UART1:
@@ -326,7 +328,8 @@ static const char *uart_acpi_name(const struct device *dev)
 	case PCI_DID_INTEL_SPT_H_UART1:
 	case PCI_DID_INTEL_CNP_H_UART1:
 		return "UAR1";
-	case PCI_DID_INTEL_PTL_UART2:
+	case PCI_DID_INTEL_PTL_H_UART2:
+	case PCI_DID_INTEL_PTL_U_H_UART2:
 	case PCI_DID_INTEL_LNL_UART2:
 	case PCI_DID_INTEL_ADP_P_UART2:
 	case PCI_DID_INTEL_APL_UART2:
@@ -354,9 +357,12 @@ struct device_operations uart_ops = {
 };
 
 static const unsigned short pci_device_ids[] = {
-	PCI_DID_INTEL_PTL_UART0,
-	PCI_DID_INTEL_PTL_UART1,
-	PCI_DID_INTEL_PTL_UART2,
+	PCI_DID_INTEL_PTL_H_UART0,
+	PCI_DID_INTEL_PTL_H_UART1,
+	PCI_DID_INTEL_PTL_H_UART2,
+	PCI_DID_INTEL_PTL_U_H_UART0,
+	PCI_DID_INTEL_PTL_U_H_UART1,
+	PCI_DID_INTEL_PTL_U_H_UART2,
 	PCI_DID_INTEL_LNL_UART0,
 	PCI_DID_INTEL_LNL_UART1,
 	PCI_DID_INTEL_LNL_UART2,

e5e683e84a21dc37f8b4a74e59470dfb4d7a49a2
Kun Liu <liukun11@huaqin.corp-partner.google.com>
2024-07-17 14:09:33 +0800

    mb/google/rex/variants/screebo: Generate RAM IDs
    Generate 3 Samsung RAM IDs
    K3KL9L90CM-MGCT  Samsung
    K3KL6L60GM-MGCT  Samsung
    K3KL8L80CM-MGCT  Samsung
    
    BUG=b:331539447,b:333145301,b:333220620
    TEST=Run part_id_gen tool without any errors
    
    Change-Id: I4ba0fb409015c24446b2ae8e224fbce3910715e3
    Signed-off-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83501
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Dinesh Gehlot <digehlot@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/rex/variants/screebo/memory/Makefile.mk
+++ src/mainboard/google/rex/variants/screebo/memory/Makefile.mk
@@ -5,4 +5,6 @@
 
 SPD_SOURCES =
 SPD_SOURCES += spd/lp5/set-0/spd-1.hex      # ID = 0(0b0000)  Parts = MT62F512M32D2DR-031 WT:B, H9JCNNNBK3MLYR-N6E
-SPD_SOURCES += spd/lp5/set-0/spd-7.hex      # ID = 1(0b0001)  Parts = MT62F1G32D2DS-026 WT:B, H58G56BK7BX068
+SPD_SOURCES += spd/lp5/set-0/spd-7.hex      # ID = 1(0b0001)  Parts = MT62F1G32D2DS-026 WT:B, H58G56BK7BX068, K3KL8L80CM-MGCT
+SPD_SOURCES += spd/lp5/set-0/spd-8.hex      # ID = 2(0b0010)  Parts = K3KL9L90CM-MGCT
+SPD_SOURCES += spd/lp5/set-0/spd-9.hex      # ID = 3(0b0011)  Parts = K3KL6L60GM-MGCT

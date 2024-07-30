1fdeabe507d30b5ff22ffa264dde23c00b69faef
Weimin Wu <wuweimin@huaqin.corp-partner.google.com>
2024-07-30 17:31:46 +0800

    mb/google/dedede/var/awasuki: Enable ELAN touchscreen with fw_config
    1. Change driver form i2c/hid to i2c/generic.
    2. Add fw_config for touchscreen.
    
    BUG=b:351968527
    TEST=ectool cbi set 6 0x0x10200a0;
         touchscreen functions normally;
    
    Change-Id: Ifd6330be8924d4873f0efab3ce404168a62099eb
    Signed-off-by: Weimin Wu <wuweimin@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83704
    Reviewed-by: zhongtian wu <wuzhongtian@huaqin.corp-partner.google.com>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/dedede/variants/awasuki/ramstage.c
+++ src/mainboard/google/dedede/variants/awasuki/ramstage.c
@@ -3,18 +3,35 @@
 #include <baseboard/variants.h>
 #include <fw_config.h>
 #include <soc/soc_chip.h>
+#include <soc/gpio.h>
 
-static void ext_vr_update(void)
+static const struct pad_config ts_disable_pad[] = {
+	/* A11 : TOUCH_RPT_EN */
+	PAD_NC(GPP_A11, NONE),
+
+	/* D4  : TOUCH_INT_ODL */
+	PAD_NC(GPP_D4, NONE),
+	/* D5  : TOUCH_RESET_L */
+	PAD_NC(GPP_D5, NONE),
+	/* D6  : EN_PP3300_TOUCH_S0 */
+	PAD_NC(GPP_D6, NONE),
+
+	/* H4  : AP_I2C_TS_SDA */
+	PAD_NC(GPP_H4, NONE),
+	/* H5  : AP_I2C_TS_SCL */
+	PAD_NC(GPP_H5, NONE),
+};
+
+void variant_devtree_update(void)
 {
 	struct soc_intel_jasperlake_config *cfg = config_of_soc();
 
 	if (fw_config_probe(FW_CONFIG(EXT_VR, EXT_VR_ABSENT))) {
-		printk(BIOS_INFO, "Device config for EXT_VR_ABSENT.\n");
 		cfg->disable_external_bypass_vr = 1;
 	}
-}
 
-void variant_devtree_update(void)
-{
-	ext_vr_update();
+	if (!fw_config_probe(FW_CONFIG(TOUCHSCREEN, TOUCHSCREEN_PRESENT))) {
+		cfg->SerialIoI2cMode[PchSerialIoIndexI2C2] = PchSerialIoDisabled;
+		gpio_configure_pads(ts_disable_pad, ARRAY_SIZE(ts_disable_pad));
+	}
 }

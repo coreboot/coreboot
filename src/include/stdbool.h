37dcaf86039ef3b4f06a87fb0e4b210c83c73c53
Elyes Haouas <ehaouas@noos.fr>
2024-08-14 18:07:27 +0200

    include/stdbool: Don't unconditionally typedef bool
    When compiling with the C23 standard, bool, true, and false are
    pre-defined by the language, so defining them in stdbool.h isn't allowed.
    This fixes the following error:
    src/include/stdbool.h:6:17: error: two or more data types in declaration specifiers
        6 | typedef _Bool   bool;
          |                 ^~~~
    src/include/stdbool.h:6:1: error: useless type name in empty declaration [-Werror]
        6 | typedef _Bool   bool;
          | ^~~~~~~
    
    Change-Id: Iec9b4e3f308008ece773ce3460daff97370161ea
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83909
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/include/stdbool.h
+++ src/include/stdbool.h
@@ -3,8 +3,10 @@
 #ifndef __STDBOOL_H__
 #define __STDBOOL_H__
 
+#if __STDC_VERSION__ <= 201710L
 typedef _Bool   bool;
-#define true    1
-#define false   0
+#define true	1
+#define false	0
+#endif
 
 #endif /* __STDBOOL_H__ */

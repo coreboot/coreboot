2f3d534eeaf8683ca60de3118c820bc32b03d070
David Wu <david_wu@quanta.corp-partner.google.com>
2024-08-19 16:35:06 +0800

    mb/google/nissa/var/riven: Set VccIn Aux Imon IccMax to 25A
    Iccmax of VccIn_Aux is 25A with MBVR design.
    
    BUG=b:348258637
    TEST=Local build successfully and boot to OS normally.
    
    Change-Id: I59c420c03a8f01d185f616a2212798266b4251e0
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83986
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Sheng-Liang Pan <sheng-liang.pan@quanta.corp-partner.google.com>
    Reviewed-by: Tyler Wang <tyler.wang@quanta.corp-partner.google.com>

---
+++ src/mainboard/google/brya/variants/riven/ramstage.c
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <console/console.h>
+#include <soc/ramstage.h>
+
+void mainboard_silicon_init_params(FSP_S_CONFIG *params)
+{
+	/* Refer to FSP Silicon (soc/intel/alderlake/fsp_params.c)
+	   VccIn Aux Imon IccMax. Values are in 1/4 Amp increments */
+	params->VccInAuxImonIccImax = 100; /* 25000(25A) * 4 / 1000 */
+	printk(BIOS_INFO, "Override VccInAuxImonIccImax = %d\n", params->VccInAuxImonIccImax);
+}

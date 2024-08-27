425e6bc1a97111363d2dc418bf3c146774959818
Simon Yang <simon1.yang@intel.com>
2024-08-27 11:24:50 +0800

    mb/google/nissa/var/anraggar: Force audio mute to avoid screen flick
    Panel CSOT MNB601LS1-3 will flicker once during enter Chrome login
    screen, it is because it inserts 12 blank frames if it receives the
    unmute in VB-ID.
    
    Always override the mute in VB-ID to avoid Tcon EC detected the
    audiomute_flag change.
    
    BUG:b=357764688
    BRANCH=firmware-nissa-15217.B
    TEST:Verfied on Anraggar and cannot reproduce the issue
    
    Change-Id: I711dfd0803440e4b04f02849fed529c3872e023d
    Signed-off-by: Simon Yang <simon1.yang@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84098
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

--- src/mainboard/google/brya/variants/anraggar/variant.c
+++ src/mainboard/google/brya/variants/anraggar/variant.c
@@ -5,6 +5,11 @@
 #include <fw_config.h>
 #include <sar.h>
 #include <soc/gpio_soc_defs.h>
+#include <intelblocks/graphics.h>
+
+/* Per-pipe DDI Function Control 2 */
+#define TRANS_DDI_FUNC_CTL2_A					0x60404
+#define TRANS_DDI_AUDIO_MUTE_OVERRIDE_BITS_FIELDS		(3 << 6)
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
@@ -36,4 +41,17 @@ void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
 		config->typec_aux_bias_pads[1].pad_auxp_dc = GPP_E22;
 		config->typec_aux_bias_pads[1].pad_auxn_dc = GPP_E23;
 	}
+}
+
+void variant_finalize(void)
+{
+	/*
+	 * Panel CSOT MNB601LS1-3 will flicker once during enter Chrome login screen,
+	 * it is because it inserts 12 blank frames if it receives the unmute in VB-ID.
+	 *
+	 * Always override the mute in VB-ID to avoid Tcon EC detected the
+	 * audiomute_flag change.
+	 */
+	graphics_gtt_rmw(TRANS_DDI_FUNC_CTL2_A, ~TRANS_DDI_AUDIO_MUTE_OVERRIDE_BITS_FIELDS,
+		TRANS_DDI_AUDIO_MUTE_OVERRIDE_BITS_FIELDS);
 }

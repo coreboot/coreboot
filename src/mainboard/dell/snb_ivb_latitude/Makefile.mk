d380ca64d04c39055988c0dac2ea00e2b67a794a
Nicholas Chin <nic.c3.14@gmail.com>
2024-08-26 19:36:30 -0600

    mb/dell/snb_ivb_latitude: Move early_init.c out of variants
    Now that the USB configs are in the devicetree, only the
    bootblock_mainboard_early_init function remains in early_init.c. It is
    identical between every variant except the E6230, which enabled fewer
    decode ranges in the LPC_EN register. Enabling the additional decode
    ranges probably shouldn't cause issues, so go with the majority.
    
    TEST=Timeless builds do not change with the exception of the E6230.
    
    Change-Id: Ic43915888f5893652991b7402ebab3bd3a2cf278
    Signed-off-by: Nicholas Chin <nic.c3.14@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84097
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Reviewed-by: Nico Huber <nico.h@gmx.de>

--- src/mainboard/dell/snb_ivb_latitude/Makefile.mk
+++ src/mainboard/dell/snb_ivb_latitude/Makefile.mk
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
-bootblock-y += variants/$(VARIANT_DIR)/early_init.c
+bootblock-y += early_init.c
 bootblock-y += variants/$(VARIANT_DIR)/gpio.c
 
-romstage-y += variants/$(VARIANT_DIR)/early_init.c
+romstage-y += early_init.c
 romstage-y += variants/$(VARIANT_DIR)/gpio.c
 
 ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads

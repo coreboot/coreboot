d4a759a068a4631d73f96a2e16f6f8ee878d1235
Tim Crawford <tcrawford@system76.com>
2024-08-30 10:43:33 -0600

    mb/system76/mtl: darp10: Add TCSS configs
    Fixes using USB3 devices at USB3 speeds in all ports.
    
    This fix requires `EnableTcssCovTypeA`, which is not available in the
    coreboot FSP headers and not available upstream as Intel will not make a
    Client FSP release.
    
    Change-Id: I9bc6c5fc4c13bfa2e31ee1ce334b91e151373b6e
    Signed-off-by: Tim Crawford <tcrawford@system76.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/83696
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Jeremy Soller <jeremy@system76.com>

--- src/mainboard/system76/mtl/variants/darp10/ramstage.c
+++ src/mainboard/system76/mtl/variants/darp10/ramstage.c
@@ -4,6 +4,14 @@
 
 void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 {
+	// Enable TCP1 USB-A conversion
+	// BIT 0:3 is mapping to PCH XHCI USB2 port
+	// BIT 4:5 is reserved
+	// BIT 6 is orientational
+	// BIT 7 is enable
+	// TODO: Add to coreboot FSP headers as no Client FSP release will be made.
+	//params->EnableTcssCovTypeA[1] = 0x82;
+
 	// XXX: Enabling C10 reporting causes system to constantly enter and
 	// exit opportunistic suspend when idle.
 	params->PchEspiHostC10ReportEnable = 0;

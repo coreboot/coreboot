ce546192a2bf00759a8178e6dfb6cbb7a846b10c
Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
2024-09-17 02:18:31 +0000

    mb/msi/ms7e06: Correct number of jacks in hda_verb.c
    This was found due to the `_Static_assert()` from CB:84360 failing.
    
    Change-Id: I5cf34d8c4e27835d126eb66f2015d2e9d93b700f
    Signed-off-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84396
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- src/mainboard/msi/ms7e06/hda_verb.c
+++ src/mainboard/msi/ms7e06/hda_verb.c
@@ -26,7 +26,7 @@ const u32 cim_verb_data[] = {
 	/* Alderlake HDMI */
 	0x80862818, /* Vendor ID */
 	0x80860101, /* Subsystem ID */
-	2, /* Number of entries */
+	10, /* Number of entries */
 	AZALIA_SUBVENDOR(2, 0x80860101),
 	AZALIA_PIN_CFG(2, 0x04, 0x18560010),
 	AZALIA_PIN_CFG(2, 0x06, 0x18560010),

d842e94d35de386cd27fcb3bfca9dd05767c2c77
Sean Rhodes <sean@starlabs.systems>
2024-10-09 20:38:27 +0100

    soc/intel/*: Add debug prints for misaligned FSP and driver settings
    Print a warning when the FSP UPD for CNVi Audio Offload is enabled
    without the corresponding USB ACPI driver being enabled.
    
    Throw an error when the USB ACPI driver is enabled without the
    corresponding UPD being enabled.
    
    Change-Id: I449c43998dd379dc68a33db47a2fe51cfe5cda2f
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84716
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin L Roth <gaumless@gmail.com>

--- src/soc/intel/meteorlake/fsp_params.c
+++ src/soc/intel/meteorlake/fsp_params.c
@@ -9,6 +9,7 @@
 #include <cpu/intel/microcode.h>
 #include <device/device.h>
 #include <device/pci.h>
+#include <drivers/usb/acpi/chip.h>
 #include <fsp/api.h>
 #include <fsp/fsp_debug_event.h>
 #include <fsp/fsp_gop_blt.h>
@@ -576,6 +577,23 @@ static void fill_fsps_lan_params(FSP_S_CONFIG *s_cfg,
 static void fill_fsps_cnvi_params(FSP_S_CONFIG *s_cfg,
 		const struct soc_intel_meteorlake_config *config)
 {
+	struct device *port = NULL;
+	struct drivers_usb_acpi_config *usb_cfg;
+	bool usb_audio_offload = false;
+
+	/* Search through the devicetree for matching USB devices */
+	while ((port = dev_find_path(port, DEVICE_PATH_USB)) != NULL) {
+		/* Skip ports that are not enabled or not of USB type */
+		if (!port->enabled || port->path.type != DEVICE_PATH_USB)
+			continue;
+
+		usb_cfg = port->chip_info;
+		if (usb_cfg && usb_cfg->cnvi_bt_audio_offload) {
+			usb_audio_offload = true;
+			break;
+		}
+	}
+
 	/* CNVi */
 	s_cfg->CnviMode = is_devfn_enabled(PCI_DEVFN_CNVI_WIFI);
 	s_cfg->CnviWifiCore = config->cnvi_wifi_core;
@@ -593,6 +611,13 @@ static void fill_fsps_cnvi_params(FSP_S_CONFIG *s_cfg,
 		printk(BIOS_ERR, "CNVi BT is enabled without CNVi being enabled\n");
 		s_cfg->CnviBtCore = 0;
 		s_cfg->CnviBtAudioOffload = 0;
+	}
+	if (s_cfg->CnviBtAudioOffload && !usb_audio_offload) {
+		printk(BIOS_WARNING, "CNVi BT Audio offload enabled but not in USB driver\n");
+	}
+	if (!s_cfg->CnviBtAudioOffload && usb_audio_offload) {
+		printk(BIOS_ERR, "USB BT Audio offload enabled but CNVi BT offload disabled\n");
+		usb_cfg->cnvi_bt_audio_offload = 0;
 	}
 }
 

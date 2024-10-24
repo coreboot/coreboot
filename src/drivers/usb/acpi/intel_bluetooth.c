c3f9dd3af34146a83db1d7cea30e291d60d83127
Sean Rhodes <sean@starlabs.systems>
2024-10-24 14:06:47 +0100

    drivers/usb/intel_bluetooth: Change the Power Resource to S0
    This resource is applicable to S0, as it can be used to reset
    the wireless and for RTD3, so change it from S5.
    
    Change-Id: I9ae710ef452c717ec414324d2847bf3218fd62d3
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84988
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>

--- src/drivers/usb/acpi/intel_bluetooth.c
+++ src/drivers/usb/acpi/intel_bluetooth.c
@@ -98,7 +98,7 @@ void acpi_device_intel_bt(unsigned int reset_gpio, unsigned int enable_gpio, boo
 
 	acpigen_write_dsm_uuid_arr(uuid_callbacks, ARRAY_SIZE(uuid_callbacks));
 /*
- *	PowerResource (BTRT, 0x05, 0x0000)
+ *	PowerResource (BTRT, 0, 0)
  *	{
  *		Method (_STA, 0, NotSerialized)
  *		{
@@ -126,7 +126,7 @@ void acpi_device_intel_bt(unsigned int reset_gpio, unsigned int enable_gpio, boo
  *		}
  *	}
  */
-	acpigen_write_power_res("BTRT", 5, 0, NULL, 0);
+	acpigen_write_power_res("BTRT", 0, 0, NULL, 0);
 	{
 		acpigen_write_method("_STA", 0);
 		{

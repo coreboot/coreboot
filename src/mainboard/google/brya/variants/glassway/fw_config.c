55e7baff922830d0d5fbcda61345e612f3e6750f
Daniel Peng <Daniel_Peng@pegatron.corp-partner.google.com>
2024-10-30 16:55:05 +0800

    mb/google/nissa/var/glassway: Add initial LTE related settings
    1. Add DB_1C_LTE 4 on DB_USB fw_config.
    2. Implement WWAN power sequencing.
    3. Disable LTE-related GPIOs based on fw_config.
    4. Add I2C SX9324 (P-sensor) support.
       Refer Schematic file: CA31AC_R10_MB_SUB_240903A_P.pdf
    
    BUG=b:374666995
    BRANCH=firmware-nissa-15217.B
    TEST=emerge-nissa coreboot chromeos-bootimage
         Confirm the device node i2c-STH9324:00 created correctly,
         and command for # i2cdump -f -y 11 0x28 is workable.
    
    Change-Id: Ida56ff338d82f48aef419a65830a3380c83123d5
    Signed-off-by: Daniel Peng <Daniel_Peng@pegatron.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84925
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>

--- src/mainboard/google/brya/variants/glassway/fw_config.c
+++ src/mainboard/google/brya/variants/glassway/fw_config.c
@@ -5,6 +5,19 @@
 #include <console/console.h>
 #include <fw_config.h>
 
+static const struct pad_config lte_disable_pads[] = {
+	/* A8  : WWAN_RF_DISABLE_ODL */
+	PAD_NC(GPP_A8, NONE),
+	/* D6  : WWAN_EN */
+	PAD_NC(GPP_D6, NONE),
+	/* F12 : WWAN_RST_L */
+	PAD_NC_LOCK(GPP_F12, NONE, LOCK_CONFIG),
+	/* H19 : SOC_I2C_SUB_INT_ODL */
+	PAD_NC(GPP_H19, NONE),
+	/* H23 : WWAN_SAR_DETECT_ODL */
+	PAD_NC(GPP_H23, NONE),
+};
+
 static const struct pad_config stylus_disable_pads[] = {
 	/* F13 : SOC_PEN_DETECT_R_ODL */
 	PAD_NC_LOCK(GPP_F13, NONE, LOCK_CONFIG),
@@ -14,6 +27,12 @@ static const struct pad_config stylus_disable_pads[] = {
 
 void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
 {
+	if (!fw_config_probe(FW_CONFIG(DB_USB, DB_1C_LTE))) {
+		printk(BIOS_INFO, "Disable LTE-related GPIO pins on Glassway serial projects.\n");
+		gpio_padbased_override(padbased_table, lte_disable_pads,
+						ARRAY_SIZE(lte_disable_pads));
+	}
+
 	if (fw_config_probe(FW_CONFIG(STYLUS, STYLUS_ABSENT))) {
 		printk(BIOS_INFO, "Disable Stylus GPIO pins.\n");
 		gpio_padbased_override(padbased_table, stylus_disable_pads,

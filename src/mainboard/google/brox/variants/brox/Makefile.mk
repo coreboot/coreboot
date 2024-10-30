2ba74b8c188b8234131dff7e12284becf34a6f2f
Karthikeyan Ramasubramanian <kramasub@google.com>
2024-10-30 10:27:10 -0600

    mb/google/brox: Hint romstage init about upcoming reset
    Add support for the mainboard to check for any potential firmware
    component update and hence the assosicated reset. This indication can be
    used to avoid any redundant resets during the boot flow.
    
    BUG=b:375444631
    TEST=Build Brox BIOS image and boot to OS. Ensure that the hints are
    provided correctly and 2 redundant resets are filtered out.
    
    Change-Id: Ieed3f9013dee9aa501a3f0403f3a28722a3878f1
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84937
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

--- src/mainboard/google/brox/variants/brox/Makefile.mk
+++ src/mainboard/google/brox/variants/brox/Makefile.mk
@@ -2,6 +2,7 @@
 
 bootblock-y += gpio.c
 romstage-y += gpio.c
+romstage-$(CONFIG_FW_CONFIG) += variant.c
 ramstage-$(CONFIG_FW_CONFIG) += fw_config.c
 ramstage-$(CONFIG_FW_CONFIG) += variant.c
 ramstage-y += gpio.c
--- src/mainboard/google/brox/variants/jubilant/Makefile.mk
+++ src/mainboard/google/brox/variants/jubilant/Makefile.mk
@@ -4,6 +4,7 @@ bootblock-y += gpio.c
 
 romstage-y += gpio.c
 romstage-y += memory.c
+romstage-$(CONFIG_FW_CONFIG) += variant.c
 
 ramstage-$(CONFIG_FW_CONFIG) += fw_config.c
 ramstage-$(CONFIG_FW_CONFIG) += variant.c

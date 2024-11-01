6a8e8459a89adcbdbb12b6ebb430a7ddafe20752
Robert Chen <robert.chen@quanta.corp-partner.google.com>
2024-11-01 07:26:59 -0400

    mb/google/dedede/var/drawcia: Update ext_vr for board version > 0xb
    ext_vr_update should be run after board version 0xb, but skipped by
    return. Drawper LTE board version was set after 0x9, but there are more
    board added after that. Specific Drawper board version as 0xa, 0xb and
    0xf.
    
    BUG=b:376828839
    BRANCH=firmware-dedede-13606.B
    TEST=emerge-dedede coreboot chromeos-bootimage and test on DUTs.
    
    Change-Id: I13f4709b6f490169f69054cf2b26430b4de0746a
    Signed-off-by: Robert Chen <robert.chen@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/84953
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>

--- src/mainboard/google/dedede/variants/drawcia/ramstage.c
+++ src/mainboard/google/dedede/variants/drawcia/ramstage.c
@@ -15,21 +15,27 @@ static void ext_vr_update(void)
 		cfg->disable_external_bypass_vr = 1;
 }
 
-void variant_devtree_update(void)
+static void update_lte_device_drawcia(void)
 {
-	uint32_t board_version = board_id();
 	struct device *lte_usb2 = DEV_PTR(lte_usb2);
 	struct drivers_usb_acpi_config *config;
 	struct acpi_gpio lte_reset_gpio = ACPI_GPIO_OUTPUT_ACTIVE_LOW(GPP_H0);
-
-	/* board version 9 is drawper EVT */
-	if (board_version > 9)
-		return;
 
 	if (lte_usb2 == NULL)
 		return;
 
 	config = config_of(lte_usb2);
 	config->reset_gpio = lte_reset_gpio;
-	ext_vr_update();
+}
+
+void variant_devtree_update(void)
+{
+	uint32_t board_version = board_id();
+
+	/* Remove power IC after board version 0xb for Draw* and Oscino units */
+	if (board_version > 0xb)
+		ext_vr_update();
+	/* Board version 0xa, 0xb and 0xf are for Drawper units */
+	if (board_version != 0xa && board_version != 0xb && board_version != 0xf)
+		update_lte_device_drawcia();
 }

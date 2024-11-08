8bada5dcb026b0ac27624b9547020b145fee1831
Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
2024-11-08 20:11:38 +0800

    mb/google/rauru: Fully calibrate DRAM
    Initialize and calibrate DRAM in romstage.
    
    DRAM full calibration logs:
    dram_init: dram init end (result: 0)
    DRAM-K:
    Fast calibration passed in 1119 msecs
    
    TEST=Full calibration pass.
    BUG=b:317009620
    
    Change-Id: Ibb18675caa11a828d27860eeab48c49acf6b938d
    Signed-off-by: Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/85120
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- src/mainboard/google/rauru/romstage.c
+++ src/mainboard/google/rauru/romstage.c
@@ -1,12 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <arch/stages.h>
+#include <soc/emi.h>
 #include <soc/irq2axi.h>
 #include <soc/pcie.h>
 
 void platform_romstage_main(void)
 {
 	irq2axi_disable();
+	mtk_dram_init();
 
 	if (CONFIG(PCI))
 		mtk_pcie_deassert_perst();

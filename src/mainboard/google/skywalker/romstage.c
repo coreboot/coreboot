12d6d0606cd038d83c6e24fbb4ed33fcb95b3efe
Zhigang Qin <zhigang.qin@mediatek.corp-partner.google.com>
2024-11-26 18:46:46 +0800

    mb/google/skywalker: Initialize PMIC in romstage
    Add PMIC function support.
    
    BUG=b:379008996
    BRANCH=none
    TEST=build pass and driver log is normal:
    [DEBUG]  [pmif_ulposc_check] calibration done: cur=260M, CAL_RATE=40, target=260
    [INFO ]  [Pass] dly:1, pol:0, sampl:0x2
    [INFO ]  [Pass] dly:1, pol:0, sampl:0x2
    [DEBUG]  pmic_efuse_setting: Set efuses in 10 msecs
    
    Signed-off-by: Zhigang Qin <zhigang.qin@mediatek.corp-partner.google.com>
    Change-Id: I4f2601bbb15807a70348bd2aa9246630adf6e0aa
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87700
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/mainboard/google/skywalker/romstage.c
+++ src/mainboard/google/skywalker/romstage.c
@@ -2,8 +2,14 @@
 
 #include <arch/stages.h>
 #include <soc/emi.h>
+#include <soc/mt6315.h>
+#include <soc/mt6359p.h>
+#include <soc/pmif.h>
 
 void platform_romstage_main(void)
 {
+	mtk_pmif_init();
+	mt6315_init();
+	mt6359p_init();
 	mtk_dram_init();
 }

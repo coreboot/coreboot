cda1e7e55397ac8fe9a9b78e261ec0911078001d
Roger Wang <roger2.wang@lcfc.corp-partner.google.com>
2024-11-27 10:26:42 +0800

    mb/google/nissa: Create pujjogatwin variant
    Create the pujjogatwin variant of nissa reference board by copying the
    pujjoga files to a new directory named for the variant.
    
    The difference between pujjoga and pujjogatwin is that pujjogatwin use
    Twin Lake CPU and firmware config, so copying the pujjoga setting files
    to create new variant.
    
    BUG=b:381152086
    TEST=util/abuild/abuild -p none -t google/brya -x -a
    make sure the build includes GOOGLE_PUJJOGATWIN
    
    Change-Id: I345ce463699840b10016555bb104a16968e7a8c1
    Signed-off-by: Roger Wang <roger2.wang@lcfc.corp-partner.google.com>
    Signed-off-by: Luca Lai <luca.lai@lcfc.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/85329
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/pujjoga/memory/Makefile.mk
+++ src/mainboard/google/brya/variants/pujjoga/memory/Makefile.mk
@@ -7,3 +7,4 @@ SPD_SOURCES =
 SPD_SOURCES += spd/lp5/set-0/spd-7.hex      # ID = 0(0b0000)  Parts = H58G56BK7BX068, K3KL8L80CM-MGCT, MT62F1G32D2DS-026 WT:B
 SPD_SOURCES += spd/lp5/set-0/spd-1.hex      # ID = 1(0b0001)  Parts = H9JCNNNBK3MLYR-N6E
 SPD_SOURCES += spd/lp5/set-0/spd-9.hex      # ID = 2(0b0010)  Parts = K3KL6L60GM-MGCT
+SPD_SOURCES += spd/lp5/set-0/spd-11.hex      # ID = 3(0b0011)  Parts = H58G56CK8BX146

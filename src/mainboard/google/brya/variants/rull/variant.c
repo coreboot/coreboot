0ce153c8dfe51d6537cdf0adedca2bdd45842758
Rui Zhou <zhourui@huaqin.corp-partner.google.com>
2024-12-12 17:24:39 +0800

    mb/google/nissa/var/rull: For probe, change unprovisioned to unknown
    The nissa project fw branch is more suitable for compatibility with unknown, so the compatible methods of wifi and ssd are replaced.
    
    BUG=b:383675207
    BRANCH=None
    TEST=emerge-nissa coreboot chromeos-bootimage
    
    Change-Id: Id2415d18c635dece94500f511bccdc9d2b69d1b4
    Signed-off-by: Rui Zhou <zhourui@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/85566
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/mainboard/google/brya/variants/rull/variant.c
+++ src/mainboard/google/brya/variants/rull/variant.c
@@ -18,7 +18,7 @@ const char *get_wifi_sar_cbfs_filename(void)
 
 void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
 {
-	if (!fw_config_is_provisioned() || fw_config_probe(FW_CONFIG(WIFI, WIFI_CNVI_WIFI6E))) {
+	if (fw_config_probe(FW_CONFIG(WIFI, WIFI_CNVI_WIFI6E)) || fw_config_probe(FW_CONFIG(WIFI, WIFI_UNKNOWN))) {
 		printk(BIOS_INFO, "CNVi bluetooth enabled by fw_config\n");
 		config->cnvi_bt_core = true;
 		config->cnvi_bt_audio_offload = true;

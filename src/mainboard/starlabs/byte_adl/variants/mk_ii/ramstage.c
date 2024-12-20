91f6d0377ed7a48230b83d819cd1ddc62538a433
Sean Rhodes <sean@starlabs.systems>
2024-12-20 10:10:54 +0000

    mb/starlabs/*: Configure GPIO UPDs for eSPI
    FSP defaults to using pins that are used for LPC; given that
    coreboot and these boards only supports eSPI, set these pins
    accordingly.
    
    If this is not done, FSP will assert and not boot.
    
    Change-Id: Ide4d92211fa7ab496c38ce1c4e895337c269d247
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/85691
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/starlabs/byte_adl/variants/mk_ii/ramstage.c
+++ src/mainboard/starlabs/byte_adl/variants/mk_ii/ramstage.c
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <option.h>
 #include <soc/ramstage.h>
 
 void mainboard_silicon_init_params(FSP_S_CONFIG *supd)
@@ -9,14 +8,7 @@ void mainboard_silicon_init_params(FSP_S_CONFIG *supd)
 	 * FSP defaults to pins that are used for LPC; given that
 	 * coreboot only supports eSPI, set these pins accordingly.
 	 */
-	supd->PchSerialIoI2cSdaPinMux[0]	= 0x1947c404;	// GPP_H4
-	supd->PchSerialIoI2cSclPinMux[0]	= 0x1947a405;	// GPP_H5
+	supd->CnviRfResetPinMux			= 0x194ce404;	// GPP_F4
+	supd->CnviClkreqPinMux			= 0x294ce605;	// GPP_F5
 	supd->SataPortDevSlpPinMux[1]		= 0x5967400d;	// GPP_H13
-
-	/*
-	 * Enable Hot Plug on RP5 to slow down coreboot so that
-	 * third-party drives are detected.
-	 */
-	if (get_uint_option("pci_hot_plug", 0) == 1)
-		supd->PcieRpHotPlug[8] = 1;
 }

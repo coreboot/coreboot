b3edaa7b10af69f9e3cbcf7c86b450ba2ed4e698
Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
2024-12-23 17:17:17 +0800

    mb/google/rauru: Implement SKU ID
    Retrieve the SKU ID for Rauru via CBI interface. If that failed
    (or no data found), fall back to ADC channels for SKU ID.
    
    TEST=Build pass, boot ok, log show:
    SKU Code: 0x2
    BUG=b:317009620
    
    Change-Id: I49ba6f428f55d3aae1b84a4d5ce06bec765caece
    Signed-off-by: Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/85666
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- src/mainboard/google/rauru/boardid.c
+++ src/mainboard/google/rauru/boardid.c
@@ -3,12 +3,17 @@
 #include <assert.h>
 #include <boardid.h>
 #include <console/console.h>
+#include <device/mmio.h>
 #include <ec/google/chromeec/ec.h>
 #include <soc/mt6363_sdmadc.h>
 
 #include "storage.h"
 
 #define ADC_LEVELS 8
+
+DEFINE_BITFIELD(STORAGE_TYPE, 11, 9);
+DEFINE_BITFIELD(CPU_TYPE, 8, 8);
+DEFINE_BITFIELD(PANEL_TYPE, 7, 0);
 
 enum {
 	/* Storage IDs */
@@ -74,4 +79,23 @@ enum ufs_type storage_type(uint32_t index)
 		printk(BIOS_DEBUG, "unsupported type %d\n", index);
 	}
 	return UFS_UNKNOWN;
+}
+
+uint32_t sku_id(void)
+{
+	static uint32_t cached_sku_code = BOARD_ID_INIT;
+
+	if (cached_sku_code == BOARD_ID_INIT) {
+		cached_sku_code = google_chromeec_get_board_sku();
+
+		if (cached_sku_code == CROS_SKU_UNKNOWN ||
+		    cached_sku_code == CROS_SKU_UNPROVISIONED) {
+			printk(BIOS_WARNING, "Failed to get SKU code from EC\n");
+			cached_sku_code = CROS_SKU_UNPROVISIONED;
+			SET32_BITFIELDS(&cached_sku_code, STORAGE_TYPE, storage_id());
+		}
+		printk(BIOS_DEBUG, "SKU Code: %#02x\n", cached_sku_code);
+	}
+
+	return cached_sku_code;
 }

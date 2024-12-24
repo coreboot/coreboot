af3076b1f923a1defba52bd8bf173079e5074f9a
Mike Lin <mike.lin@mediatek.corp-partner.google.com>
2024-12-24 15:44:27 +0800

    soc/mediatek/mt8196: Move TX TRACKING from MT8196 to common folder
    The configuration method of the reserved DRAM buffer used for HW TX
    TRACING on MT8196 is also applicable to other SoCs, such as MT8189.
    
    To facilitate reuse, we move the relevant files to the common directory.
    
    BUG=b:379008996
    BRANCH=none
    TEST=Reserve memory ok
    Firmware shows the following log with 12GB DDR board:
    00000001ffff0000-00000001ffffffff: RESERVED
    000000037fff0000-000000037fffffff: RESERVED
    
    Change-Id: I3fdd9d2f7ab1bbdcc097510556929da2134f7d95
    Signed-off-by: Mike Lin <mike.lin@mediatek.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87036
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yidi Lin <yidilin@google.com>

---
+++ src/soc/mediatek/common/include/soc/dramc_info.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef __SOC_MEDIATEK_COMMON_DRAMC_INFO_H__
+#define __SOC_MEDIATEK_COMMON_DRAMC_INFO_H__
+
+void reserve_buffer_for_dramc(void);
+
+#endif	/* __SOC_MEDIATEK_COMMON_DRAMC_INFO_H__ */
--- src/soc/mediatek/mt8196/include/soc/dramc_info.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-
-#ifndef __SOC_MEDIATEK_MT8196_DRAMC_INFO_H__
-#define __SOC_MEDIATEK_MT8196_DRAMC_INFO_H__
-
-void reserve_buffer_for_dramc(void);
-
-#endif	/* __SOC_MEDIATEK_MT8196_DRAMC_INFO_H__ */

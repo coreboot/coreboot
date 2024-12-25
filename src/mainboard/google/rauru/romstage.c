044017b4cd9ca508baaeba7c5a942d44170866c6
Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
2024-12-25 18:44:08 +0800

    mb/google/rauru: Initialize PMICs in romstage
    BUG=b:317009620
    TEST=Build pass, boot ok.
    
    Change-Id: Ia220db1d8d6d20e508f5e4d47054922012f6c417
    Signed-off-by: Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/85755
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/mainboard/google/rauru/romstage.c
+++ src/mainboard/google/rauru/romstage.c
@@ -3,17 +3,25 @@
 #include <arch/stages.h>
 #include <soc/emi.h>
 #include <soc/irq2axi.h>
+#include <soc/mt6316.h>
 #include <soc/mt6363.h>
+#include <soc/mt6373.h>
+#include <soc/mt6685.h>
 #include <soc/mtk_pwrsel.h>
 #include <soc/pcie.h>
+#include <soc/pmif.h>
 
 void platform_romstage_main(void)
 {
 	irq2axi_disable();
 	pwrsel_init();
-	mt6363_init_pmif_arb();
+	mtk_pmif_init();
+	mt6363_init();
 	mt6363_enable_vtref18(true);
 	mt6363_set_vtref18_voltage(1800000);
+	mt6316_init();
+	mt6373_init();
+	mt6685_init();
 	mtk_dram_init();
 
 	if (CONFIG(PCI))

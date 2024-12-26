f2ad73b5d170c21f1d1ce83176b1f6cec6af1b6f
Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
2024-12-26 14:23:13 +0800

    mb/google/rauru: Raise little core CPU frequency from 700MHz to 2.4GHz
    To improve boot time, raise little CPU from 700MHz to 2.4GHz at romstage
    (before DRAM calibration).
    
    FW logs:
    Check CPU freq: 2400120 KHz
    
    BUG=b:317009620
    TEST=Build pass, boot ok.
    
    Change-Id: I14a31f3a51ca246b842cc0ef740c43ff5d857310
    Signed-off-by: Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/85773
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/mainboard/google/rauru/romstage.c
+++ src/mainboard/google/rauru/romstage.c
@@ -9,7 +9,16 @@
 #include <soc/mt6685.h>
 #include <soc/mtk_pwrsel.h>
 #include <soc/pcie.h>
+#include <soc/pll.h>
 #include <soc/pmif.h>
+
+static void raise_little_cpu_freq(void)
+{
+	mt6316_buck_set_voltage(SPMI_SLAVE_8, MT6316_BUCK_3, 1050000);
+	printk(BIOS_INFO, "%s: little core voltage: %d\n",
+	       __func__, mt6316_buck_get_voltage(SPMI_SLAVE_8, MT6316_BUCK_3));
+	mt_pll_raise_little_cpu_freq(2400U * MHz);
+}
 
 void platform_romstage_main(void)
 {
@@ -20,6 +29,7 @@ void platform_romstage_main(void)
 	mt6363_enable_vtref18(true);
 	mt6363_set_vtref18_voltage(1800000);
 	mt6316_init();
+	raise_little_cpu_freq();
 	mt6373_init();
 	mt6685_init();
 	mtk_dram_init();

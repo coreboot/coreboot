c63f91f4dc20bdc40fbdf629949cfb8f5ff3e2ed
Yidi Lin <yidilin@chromium.org>
2025-01-07 12:18:32 +0800

    soc/mediatek/common/dp: Initialize dptx_misc
    Initialize dptx_misc to prevent unexpected value set to
    REG_3034_DP_ENCODER0_P0.
    
    TEST=emerge-geralt coreboot && emerge-rauru coreboot
    
    Change-Id: I80e0f83f238d9b4c1ed0e1d1b219f4fb89a6cd22
    Signed-off-by: Yidi Lin <yidilin@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/85877
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/soc/mediatek/common/dp/dptx_common.c
+++ src/soc/mediatek/common/dp/dptx_common.c
@@ -333,7 +333,7 @@ static void dptx_set_tu(struct mtk_dp *mtk_dp)
 static void dptx_set_misc(struct mtk_dp *mtk_dp)
 {
 	u8 format, depth;
-	union misc_t dptx_misc;
+	union misc_t dptx_misc = { .cmisc = {0} };
 
 	format = mtk_dp->info.format;
 	depth = mtk_dp->info.depth;

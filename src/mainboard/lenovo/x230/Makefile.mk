a781c9037d6f8fdd1540e79b59915041ca8989ee
Keith Hui <buurin@gmail.com>
2025-01-11 07:13:01 -0500

    mb/lenovo/x230: Remove old USB configurations
    As of commit a911b7584820 ("mb/*: Remove old USB configurations from
    SNB/IVB boards") USB configurations are drawn exclusively from devicetree.
    These stuff should have been removed then.
    
    Drop early_init.c that only contains these old configurations.
    
    Change-Id: I3c92ab408219291fef355c9462134dbbd2e4ea87
    Signed-off-by: Keith Hui <buurin@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/85941
    Reviewed-by: Nicholas Chin <nic.c3.14@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>

--- src/mainboard/lenovo/x230/Makefile.mk
+++ src/mainboard/lenovo/x230/Makefile.mk
@@ -1,8 +1,6 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
-bootblock-y += variants/$(VARIANT_DIR)/early_init.c
 bootblock-y += variants/$(VARIANT_DIR)/gpio.c
-romstage-y += variants/$(VARIANT_DIR)/early_init.c
 romstage-y += variants/$(VARIANT_DIR)/gpio.c
 ramstage-y += variants/$(VARIANT_DIR)/hda_verb.c
 

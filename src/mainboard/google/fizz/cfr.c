dc19824e56b039a8a152d5ce24b4adc4eab013db
Matt DeVillier <matt.devillier@gmail.com>
2025-01-13 14:07:31 -0600

    mb/google/fizz: Use CFR setup menu to manage options
    Enable support for managing system options via CFR, and select it by
    default when using edk2 with SMMSTORE.
    
    TEST=build/boot fizz w/edk2 payload
    
    Change-Id: I211a91f16622b048d15ebe373106b0f70b429312
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87565
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

---
+++ src/mainboard/google/fizz/cfr.c
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <boot/coreboot_tables.h>
+#include <drivers/option/cfr_frontend.h>
+#include <intelblocks/cfr.h>
+#include <soc/cfr.h>
+
+static struct sm_obj_form system = {
+	.ui_name = "System",
+	.obj_list = (const struct sm_object *[]) {
+		&hyper_threading,
+		&igd_dvmt,
+		&igd_aperture,
+		&legacy_8254_timer,
+		&me_state,
+		&me_state_counter,
+		&pciexp_aspm,
+		&pciexp_clk_pm,
+		&pciexp_l1ss,
+		&pciexp_speed,
+		&s0ix_enable,
+		&vtd,
+		NULL
+	},
+};
+
+static struct sm_obj_form power = {
+	.ui_name = "Power",
+	.obj_list = (const struct sm_object *[]) {
+		&power_on_after_fail,
+		NULL
+	},
+};
+
+static struct sm_obj_form *sm_root[] = {
+	&system,
+	&power,
+	NULL
+};
+
+void mb_cfr_setup_menu(struct lb_cfr *cfr_root)
+{
+	cfr_write_setup_menu(cfr_root, sm_root);
+}

56d60167ff593fa5a28a012b1a897155dd42893b
Cong Yang <yangcong5@huaqin.corp-partner.google.com>
2025-01-17 12:49:04 +0800

    mb/google/skywalker: Pass XHCI_INIT_DONE to the payload
    Configure GPIO (XHCI_INIT_DONE) as output, so that payloads (for example
    depthcharge) can assert it to notify EC to enable USB VBUS.
    
    BUG=b:390357201
    BRANCH=none
    TEST=detect USB devices in depthcharge, and the log is like "Added USB
    disk 2."
    
    Change-Id: I99760ace3e87626f55c52dc4f8a30bab27cba345
    Signed-off-by: Cong Yang <yangcong5@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87347
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/mainboard/google/skywalker/chromeos.c
+++ src/mainboard/google/skywalker/chromeos.c
@@ -1,8 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only OR MIT */
 
 #include <boot/coreboot_tables.h>
+#include <gpio.h>
+
+#include "gpio.h"
 
 void fill_lb_gpios(struct lb_gpios *gpios)
 {
-	/* TODO: add ChromeOS specific gpios */
+	struct lb_gpio chromeos_gpios[] = {
+		{GPIO_XHCI_INIT_DONE.id, ACTIVE_HIGH, -1, "XHCI init done"},
+	};
+	lb_add_gpios(gpios, chromeos_gpios, ARRAY_SIZE(chromeos_gpios));
 }
---
+++ src/mainboard/google/skywalker/gpio.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef __MAINBOARD_GOOGLE_SKYWALKER_GPIO_H__
+#define __MAINBOARD_GOOGLE_SKYWALKER_GPIO_H__
+
+#include <soc/gpio.h>
+
+#define GPIO_XHCI_INIT_DONE	GPIO(GBE_MDC)
+
+#endif

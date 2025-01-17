cf7460f6ea72327d509951f50c712b4870eb253a
Cong Yang <yangcong5@huaqin.corp-partner.google.com>
2025-01-17 12:51:12 +0800

    mb/google/skywalker: Enable USB3_HUB_RST
    We have to reset the USB hub as early as possible. Otherwise the USB3
    hub may not be usable in the payload. This design has been introduced
    since Cherry.
    
    BUG=b:390357201
    BRANCH=none
    TEST=detect USB devices in depthcharge, and the log is like "Added USB
    disk 2."
    
    Change-Id: I4ee24aef2a887c8a30738912a8bf90f830a72bed
    Signed-off-by: Cong Yang <yangcong5@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87348
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/mainboard/google/skywalker/bootblock.c
+++ src/mainboard/google/skywalker/bootblock.c
@@ -1,9 +1,18 @@
 /* SPDX-License-Identifier: GPL-2.0-only OR MIT */
 
 #include <bootblock_common.h>
+#include <gpio.h>
 #include <soc/spi.h>
+
+#include "gpio.h"
+
+static void usb3_hub_reset(void)
+{
+	gpio_output(GPIO_USB3_HUB_RST_L, 1);
+}
 
 void bootblock_mainboard_init(void)
 {
 	mtk_snfc_init();
+	usb3_hub_reset();
 }
--- src/mainboard/google/skywalker/gpio.h
+++ src/mainboard/google/skywalker/gpio.h
@@ -6,5 +6,6 @@
 #include <soc/gpio.h>
 
 #define GPIO_XHCI_INIT_DONE	GPIO(GBE_MDC)
+#define GPIO_USB3_HUB_RST_L	GPIO(GPIO07)
 
 #endif

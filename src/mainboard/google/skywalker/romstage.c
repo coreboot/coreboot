24ab31f477cf2ae6ab29ea6123d5afc4b0c9aade
Shunxi Zhang <ot_shunxi.zhang@mediatek.com>
2025-01-23 18:28:37 +0800

    mb/google/skywalker: Enable RTC boot init
    Enable RTC to get correct timestamp.
    
    BUG=b:379008996
    BRANCH=none
    TEST=build passed and check RTC time is increasing by command 'cat
    /proc/driver/rtc'
    
    skywalker-rev1 ~ # cat /proc/driver/rtc
    rtc_time        : 12:36:25
    skywalker-rev1 ~ # cat /proc/driver/rtc
    rtc_time        : 12:36:28
    
    Change-Id: Idfe6185b9e2ad8d116da454c8d95ddeb32a5998d
    Signed-off-by: Shunxi Zhang <ot_shunxi.zhang@mediatek.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87707
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/skywalker/romstage.c
+++ src/mainboard/google/skywalker/romstage.c
@@ -5,11 +5,13 @@
 #include <soc/mt6315.h>
 #include <soc/mt6359p.h>
 #include <soc/pmif.h>
+#include <soc/rtc.h>
 
 void platform_romstage_main(void)
 {
 	mtk_pmif_init();
 	mt6315_init();
 	mt6359p_init();
+	rtc_boot();
 	mtk_dram_init();
 }

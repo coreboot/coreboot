3dabaf88683cb59fa34e7cfe0acb9bf591fbfc4b
John Su <john_su@compal.corp-partner.google.com>
2025-01-24 21:10:09 +0800

    mb/google/trulo/var/uldrenite: Add fw_config probe for touchscreen
    Use fw_config to probe touchscreen.
    
    BUG=b:392040004
    BRANCH=firmware-trulo-15217.771.B
    TEST=emerge-nissa coreboot chromeos-bootimage
    
    Change-Id: I63512e85052bce974ffb0b738164bea440bd413c
    Signed-off-by: John Su <john_su@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/86144
    Reviewed-by: Jayvik Desai <jayvik@google.com>
    Reviewed-by: Ian Feng <ian_feng@compal.corp-partner.google.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Frank Wu <frank_wu@compal.corp-partner.google.com>

--- src/mainboard/google/brya/variants/uldrenite/variant.c
+++ src/mainboard/google/brya/variants/uldrenite/variant.c
@@ -4,6 +4,7 @@
 #include <baseboard/variants.h>
 #include <console/console.h>
 #include <delay.h>
+#include <fw_config.h>
 
 #define RW350R_RST_DELAY_MS 20
 #define RW350R_PERST_DELAY_MS 30
@@ -20,6 +21,30 @@ static const struct pad_config rw350r_perst_pad[] = {
 	/* F13 : PLTRST_WWAN# */
 	PAD_CFG_GPO(GPP_F13, 1, DEEP),
 };
+
+static const struct pad_config touchscreen_disable_pads[] = {
+	/* A21  : TCHSCR_REPORT_EN */
+	PAD_NC(GPP_A21, NONE),
+	/* B16  : SOC_I2C_TCHSCR_SDA */
+	PAD_NC(GPP_B16, NONE),
+	/* B17  : SOC_I2C_TCHSCR_SCL */
+	PAD_NC(GPP_B17, NONE),
+	/* D15  : TCHSCR_RST_L */
+	PAD_NC(GPP_D15, NONE),
+	/* D16  : TCHSCR_INT_ODL */
+	PAD_NC(GPP_D16, NONE),
+	/* E17  : EN_PP3300_TCHSCR_X */
+	PAD_NC(GPP_E17, NONE),
+};
+
+void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
+{
+	if (fw_config_probe(FW_CONFIG(TOUCHSCREEN, TOUCHSCREEN_NONE))) {
+		printk(BIOS_INFO, "Disable touchscreen GPIO pins.\n");
+		gpio_padbased_override(padbased_table, touchscreen_disable_pads,
+				ARRAY_SIZE(touchscreen_disable_pads));
+	}
+}
 
 void variant_init(void)
 {

273e84976bdad6ac60682746e7069857019d1b6a
Keith Hui <buurin@gmail.com>
2025-01-28 10:11:40 -0500

    mb/asus/p8z77-v: Apply vendor PCH interrupt mapping
    Got this information from Bill Xie while troubleshooting CB:85413.
    Apply the differences from vendor to coreboot.
    
    Change-Id: I56f4314eca101cdfcac12594115d373472d1e1db
    Signed-off-by: Keith Hui <buurin@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/86191
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Bill XIE <persmule@hardenedlinux.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/asus/p8x7x-series/variants/p8z77-v/early_init.c
+++ src/mainboard/asus/p8x7x-series/variants/p8z77-v/early_init.c
@@ -2,12 +2,28 @@
 
 #include <bootblock_common.h>
 #include <device/pnp_ops.h>
+#include <southbridge/intel/bd82x6x/pch.h>
 #include <superio/nuvoton/common/nuvoton.h>
 #include <superio/nuvoton/nct6779d/nct6779d.h>
 
 #define GLOBAL_DEV PNP_DEV(CONFIG_SUPERIO_NUVOTON_PNP_BASE, 0)
 #define SERIAL_DEV PNP_DEV(CONFIG_SUPERIO_NUVOTON_PNP_BASE, NCT6779D_SP1)
 #define ACPI_DEV   PNP_DEV(CONFIG_SUPERIO_NUVOTON_PNP_BASE, NCT6779D_ACPI)
+
+void mainboard_late_rcba_config(void)
+{
+	RCBA32(D31IP) = (INTC << D31IP_TTIP) | (INTB << D31IP_SIP2) |
+			(INTC << D31IP_SMIP) | (INTB << D31IP_SIP);
+	RCBA32(D22IP) = (INTB << D22IP_KTIP)   | (INTC << D22IP_IDERIP) |
+			(INTB << D22IP_MEI2IP) | (INTA << D22IP_MEI1IP);
+
+	DIR_ROUTE(D31IR, PIRQA, PIRQC, PIRQD, PIRQA);
+	DIR_ROUTE(D29IR, PIRQH, PIRQD, PIRQA, PIRQC);
+	DIR_ROUTE(D27IR, PIRQG, PIRQB, PIRQC, PIRQD);
+	DIR_ROUTE(D26IR, PIRQA, PIRQF, PIRQC, PIRQD);
+	DIR_ROUTE(D25IR, PIRQE, PIRQF, PIRQG, PIRQH);
+	DIR_ROUTE(D22IR, PIRQA, PIRQD, PIRQC, PIRQB);
+}
 
 void bootblock_mainboard_early_init(void)
 {

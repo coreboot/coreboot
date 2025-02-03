1f0d3db4f4257a727f6ccc79822be87df3802360
Matt DeVillier <matt.devillier@gmail.com>
2025-02-03 15:24:40 -0600

    soc/intel/{adl,tgl}: Set PMC ACPI device to hidden/on
    This prevents Windows from displaying the PMC device in Device Manager
    as an unknown device with no driver available, and brings Alderlake
    and Tigerlake in line with Meteorlake and Pantherlake.
    
    TEST=build/boot Win11 on starlabs/starlite_adl, verify PMC device
    not shown as unknown device in Device Manager.
    
    Change-Id: I4bd62d113455fab7fcb272d85f70e6a185e53b74
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/86256
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Jérémy Compostella <jeremy.compostella@intel.com>
    Reviewed-by: Sean Rhodes <sean@starlabs.systems>

--- src/soc/intel/alderlake/pmc.c
+++ src/soc/intel/alderlake/pmc.c
@@ -108,7 +108,7 @@ static void soc_pmc_fill_ssdt(const struct device *dev)
 
 	acpigen_write_name_string("_HID", PMC_HID);
 	acpigen_write_name_string("_DDN", "Intel(R) Alder Lake IPC Controller");
-	acpigen_write_STA(ACPI_STATUS_DEVICE_ALL_ON);
+	acpigen_write_STA(ACPI_STATUS_DEVICE_HIDDEN_ON);
 
 	/*
 	 * Part of the PCH's reserved 32 MB MMIO range (0xFC800000 - 0xFE7FFFFF).

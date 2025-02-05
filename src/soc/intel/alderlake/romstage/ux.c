cf6f4920995ac2424065daa102023d870e0af496
Subrata Banik <subratabanik@google.com>
2025-02-05 09:52:18 +0000

    soc/intel/alderlake: Use helper for UX messages
    This commit introduces `ux_inform_user_of_operation` to reduce code
    duplication when displaying UX messages.
    
    `ux_inform_user_of_update_operation` now calls this helper, passing
    the message ID. This allows easier addition of other UX messages.
    
    BUG=b:339673254
    TEST=Built and booted google/brox. Verified display eSOL.
    
    Change-Id: Ib31f7633e7b3f84122419e4ce39e2b5044cb9a96
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/86278
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

--- src/soc/intel/alderlake/romstage/ux.c
+++ src/soc/intel/alderlake/romstage/ux.c
@@ -8,23 +8,27 @@
 
 #include "ux.h"
 
-bool ux_inform_user_of_update_operation(const char *name)
+static bool ux_inform_user_of_operation(const char *name, enum ux_locale_msg id)
 {
 	timestamp_add_now(TS_ESOL_START);
 
-	if (!CONFIG(CHROMEOS_ENABLE_ESOL) ||
-	    !early_graphics_init()) {
+	if (!CONFIG(CHROMEOS_ENABLE_ESOL) || !early_graphics_init()) {
 		timestamp_add_now(TS_ESOL_END);
 		return false;
 	}
 
 	printk(BIOS_INFO, "Informing user on-display of %s.\n", name);
 
-	const char *text = ux_locales_get_text(UX_LOCALE_MSG_MEMORY_TRAINING);
+	const char *text = ux_locales_get_text(id);
 
 	vga_write_text(VGA_TEXT_CENTER, VGA_TEXT_HORIZONTAL_MIDDLE,
 		       (const unsigned char *)text);
 	ux_locales_unmap();
 	timestamp_add_now(TS_ESOL_END);
 	return true;
+}
+
+bool ux_inform_user_of_update_operation(const char *name)
+{
+	return ux_inform_user_of_operation(name, UX_LOCALE_MSG_MEMORY_TRAINING);
 }

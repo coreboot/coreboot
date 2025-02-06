71b62486022073a7e129baaa7546a516bbc197a3
John Su <john_su@compal.corp-partner.google.com>
2025-02-07 00:26:33 +0800

    mb/google/trulo/var/uldrenite: Add FW_CONFIG probe for fivr
    Uldrenite will support internal fivr in next phase and using fw_config
    to decide the board with internal or external fivr.
    
    BUG=b:394752422
    BRANCH=firmware-trulo-15217.771.B
    TEST=boot to ChromeOS, cold reboot/suspend/recovery mode/install OS
    work normally
    
    Change-Id: I14233090f2445461cf422c1257f21556fd745b43
    Signed-off-by: John Su <john_su@compal.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/86303
    Reviewed-by: Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/uldrenite/variant.c
+++ src/mainboard/google/brya/variants/uldrenite/variant.c
@@ -83,3 +83,27 @@ void variant_init(void)
 	mdelay(RW350R_PERST_DELAY_MS);
 	gpio_configure_pads(rw350r_perst_pad, ARRAY_SIZE(rw350r_perst_pad));
 }
+
+void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
+{
+	if (fw_config_probe(FW_CONFIG(EXT_VR, EXT_VR_PRESENT))) {
+		/* # Configure external V1P05/Vnn/VnnSx Rails */
+		config->ext_fivr_settings.configure_ext_fivr = 1;
+		config->ext_fivr_settings.v1p05_enable_bitmap =
+				FIVR_ENABLE_ALL_SX & ~FIVR_ENABLE_S0;
+		config->ext_fivr_settings.vnn_enable_bitmap =
+				FIVR_ENABLE_ALL_SX;
+		config->ext_fivr_settings.vnn_sx_enable_bitmap =
+				FIVR_ENABLE_ALL_SX;
+		config->ext_fivr_settings.v1p05_supported_voltage_bitmap =
+				FIVR_VOLTAGE_NORMAL;
+		config->ext_fivr_settings.vnn_supported_voltage_bitmap =
+				FIVR_VOLTAGE_MIN_ACTIVE;
+		config->ext_fivr_settings.v1p05_voltage_mv = 1050;
+		config->ext_fivr_settings.vnn_voltage_mv = 780;
+		config->ext_fivr_settings.vnn_sx_voltage_mv = 1050;
+		config->ext_fivr_settings.v1p05_icc_max_ma = 500;
+		config->ext_fivr_settings.vnn_icc_max_ma = 500;
+		printk(BIOS_INFO, "Configured External FIVR\n");
+	}
+}

bcbe17dea3c004f85abd0e56b51f0df0aab6bd67
Wentao Qin <qinwentao@huaqin.corp-partner.google.com>
2025-02-11 14:44:54 +0800

    mb/google/skywalker: Configure TPM
    Initialize I2C bus 3 for TPM control and enable vboot secdata.
    
    BUG=b:395723580
    BRANCH=none
    TEST=check boot log
    
    Change-Id: I34da1a494e71bdaac0223d1db918fffe12f68df4
    Signed-off-by: Wentao Qin <qinwentao@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87772
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/skywalker/chromeos.c
+++ src/mainboard/google/skywalker/chromeos.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only OR MIT */
 
 #include <boot/coreboot_tables.h>
+#include <drivers/tpm/cr50.h>
 #include <ec/google/chromeec/ec.h>
 #include <gpio.h>
 
@@ -9,6 +10,7 @@
 void setup_chromeos_gpios(void)
 {
 	gpio_input(GPIO_EC_AP_INT_ODL);
+	gpio_input(GPIO_GSC_AP_INT_ODL);
 	gpio_output(GPIO_AP_EC_WARM_RST_REQ, 0);
 	gpio_output(GPIO_XHCI_INIT_DONE, 0);
 }
@@ -18,6 +20,12 @@ void fill_lb_gpios(struct lb_gpios *gpios)
 	struct lb_gpio chromeos_gpios[] = {
 		{GPIO_XHCI_INIT_DONE.id, ACTIVE_HIGH, -1, "XHCI init done"},
 		{GPIO_EC_AP_INT_ODL.id, ACTIVE_LOW, -1, "EC interrupt"},
+		{GPIO_GSC_AP_INT_ODL.id, ACTIVE_HIGH, -1, "TPM interrupt"},
 	};
 	lb_add_gpios(gpios, chromeos_gpios, ARRAY_SIZE(chromeos_gpios));
+}
+
+int cr50_plat_irq_status(void)
+{
+	return gpio_eint_poll(GPIO_GSC_AP_INT_ODL);
 }
--- src/mainboard/google/skywalker/gpio.h
+++ src/mainboard/google/skywalker/gpio.h
@@ -9,6 +9,7 @@
 #define GPIO_USB3_HUB_RST_L	GPIO(GPIO07)
 #define GPIO_EC_AP_INT_ODL	GPIO(GBE_MDIO)
 #define GPIO_AP_EC_WARM_RST_REQ	GPIO(GBE_AUX_PPS0)
+#define GPIO_GSC_AP_INT_ODL	GPIO(GBE_AUX_PPS1)
 
 void setup_chromeos_gpios(void);
 

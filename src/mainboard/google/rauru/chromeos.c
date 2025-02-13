74efa4e3968ceeeea58602e9de3d34e1df5dae1d
Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
2025-02-13 13:32:03 +0800

    mb/google/rauru: Correct GPIO name for BEEP_ON
    Rename GPIO_BEEP_ON_OD to GPIO_BEEP_ON to match the schematics.
    
    BRANCH=rauru
    TEST=Build pass
    BUG=b:317009620
    
    Change-Id: Ica48ed4f4e2cd6a159203ba1f8c17ac371a08f87
    Signed-off-by: Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/86391
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>

--- src/mainboard/google/rauru/chromeos.c
+++ src/mainboard/google/rauru/chromeos.c
@@ -14,7 +14,7 @@ void setup_chromeos_gpios(void)
 	gpio_input(GPIO_SD_CD_AP_ODL);
 	gpio_output(GPIO_AP_EC_WARM_RST_REQ, 0);
 	gpio_output(GPIO_AP_FP_FW_UP_STRAP, 0);
-	gpio_output(GPIO_BEEP_ON_OD, 0);
+	gpio_output(GPIO_BEEP_ON, 0);
 	gpio_output(GPIO_EN_PWR_FP, 0);
 	gpio_output(GPIO_EN_SPKR, 0);
 	gpio_output(GPIO_FP_RST_1V8_S3_L, 0);
@@ -39,7 +39,7 @@ void fill_lb_gpios(struct lb_gpios *gpios)
 
 	struct lb_gpio nau8318_gpios[] = {
 		{GPIO_EN_SPKR.id, ACTIVE_HIGH, -1, "speaker enable"},
-		{GPIO_BEEP_ON_OD.id, ACTIVE_HIGH, -1, "beep enable"},
+		{GPIO_BEEP_ON.id, ACTIVE_HIGH, -1, "beep enable"},
 	};
 
 	struct lb_gpio smartamp_gpios[] = {

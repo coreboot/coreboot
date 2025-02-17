c4fe5e2483706544fa964d6a21db4c7897033e59
Vince Liu <vince-wl.liu@mediatek.corp-partner.google.com>
2025-02-17 15:07:10 +0800

    mb/google/skywalker: Pass reset GPIO parameter to BL31
    Pass the reset GPIO parameter to BL31 to support SoC reset.
    
    BUG=b:395795640
    BRANCH=none
    TEST=run reboot command in depthcharge
    
    Signed-off-by: Vince Liu <vince-wl.liu@mediatek.corp-partner.google.com>
    Change-Id: I87063d58d04ea6437195a59abab9c54f2da7eac0
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87814
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- src/mainboard/google/skywalker/mainboard.c
+++ src/mainboard/google/skywalker/mainboard.c
@@ -1,15 +1,21 @@
 /* SPDX-License-Identifier: GPL-2.0-only OR MIT */
 
 #include <device/device.h>
+#include <soc/bl31.h>
 #include <soc/dpm_v2.h>
 #include <soc/spm_common.h>
 #include <soc/usb.h>
+
+#include "gpio.h"
 
 static void mainboard_init(struct device *dev)
 {
 	dpm_init();
 	setup_usb_host();
 	spm_init();
+
+	if (CONFIG(ARM64_USE_ARM_TRUSTED_FIRMWARE))
+		register_reset_to_bl31(GPIO_AP_EC_WARM_RST_REQ.id, true);
 }
 
 static void mainboard_enable(struct device *dev)

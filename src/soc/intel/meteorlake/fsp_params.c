02ca72b2d43ec6d79496f0455b6b815774af3379
Sean Rhodes <sean@starlabs.systems>
2025-03-06 13:52:52 +0000

    soc/intel/meteorlake: Hook up Pch Sleep Assertion widths
    Hook up devicetree to the assertion width UPDs, in the same way
    that Tiger Lake does - specifically, only setting the UPDs if a
    non-default value is set via devicetree; otherwise, use the
    FSP default value.
    
    Change-Id: Ifd92ef8217055eb7b558bc494a6586b35403c368
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/86754
    Reviewed-by: Martin L Roth <gaumless@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/meteorlake/fsp_params.c
+++ src/soc/intel/meteorlake/fsp_params.c
@@ -22,6 +22,7 @@
 #include <intelblocks/irq.h>
 #include <intelblocks/lpss.h>
 #include <intelblocks/mp_init.h>
+#include <intelblocks/pmclib.h>
 #include <intelblocks/systemagent.h>
 #include <intelblocks/xdci.h>
 #include <intelpch/lockdown.h>
@@ -658,6 +659,24 @@ static void fill_fsps_misc_power_params(FSP_S_CONFIG *s_cfg,
 	/* Enable the energy efficient turbo mode */
 	s_cfg->EnergyEfficientTurbo = 1;
 	s_cfg->PmcLpmS0ixSubStateEnableMask = get_supported_lpm_mask();
+
+	/* Apply minimum assertion width settings if non-zero */
+	if (config->pch_slp_s3_min_assertion_width)
+		s_cfg->PchPmSlpS3MinAssert = config->pch_slp_s3_min_assertion_width;
+	if (config->pch_slp_s4_min_assertion_width)
+		s_cfg->PchPmSlpS4MinAssert = config->pch_slp_s4_min_assertion_width;
+	if (config->pch_slp_sus_min_assertion_width)
+		s_cfg->PchPmSlpSusMinAssert = config->pch_slp_sus_min_assertion_width;
+	if (config->pch_slp_a_min_assertion_width)
+		s_cfg->PchPmSlpAMinAssert = config->pch_slp_a_min_assertion_width;
+
+	/* Set Power Cycle Duration */
+	if (config->pch_reset_power_cycle_duration)
+		s_cfg->PchPmPwrCycDur = get_pm_pwr_cyc_dur(config->pch_slp_s4_min_assertion_width,
+							   config->pch_slp_s3_min_assertion_width,
+							   config->pch_slp_a_min_assertion_width,
+							   config->pch_reset_power_cycle_duration);
+
 	/* Un-Demotion from Demoted C1 need to be disable when
 	 * C1 auto demotion is disabled */
 	s_cfg->C1StateUnDemotion = !config->disable_c1_state_auto_demotion;

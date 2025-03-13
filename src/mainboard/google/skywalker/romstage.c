0f2942b513ab38e06cb29c740d36756159e572d7
Zhigang Qin <zhigang.qin@mediatek.corp-partner.google.com>
2025-03-13 16:29:32 +0800

    mb/google/skywalker: Raise little core CPU frequency to 2.0 GHz
    Increase the CPU little core frequency from 1.6 GHz to 2.0 GHz to
    speed up the boot process.
    
    BUG=b:379008996
    BRANCH=none
    TEST=check little core cpu frequency is 2GHz in kernel by commands
    clkdbg() { echo $@ > /proc/clkdbg ; cat /proc/clkdbg ; }
    clkdbg fmeter
    
    See the little core CPU frequency:
    fm_armpll_ll_ck          	: 1999968
    
    Signed-off-by: Zhigang Qin <zhigang.qin@mediatek.corp-partner.google.com>
    Change-Id: I979f44e9340ea5bd733dc7f0fe47af47a4f403b7
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87795
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/skywalker/romstage.c
+++ src/mainboard/google/skywalker/romstage.c
@@ -1,17 +1,30 @@
 /* SPDX-License-Identifier: GPL-2.0-only OR MIT */
 
 #include <arch/stages.h>
+#include <delay.h>
 #include <soc/emi.h>
 #include <soc/mt6315.h>
 #include <soc/mt6359p.h>
+#include <soc/pll.h>
 #include <soc/pmif.h>
+#include <soc/regulator.h>
 #include <soc/rtc.h>
+
+static void raise_little_cpu_freq(void)
+{
+	mainboard_set_regulator_voltage(MTK_REGULATOR_VSRAM_PROC12, 1050000);
+	mainboard_set_regulator_voltage(MTK_REGULATOR_VPROC12, 1050000);
+	udelay(200);
+	mt_pll_raise_little_cpu_freq(2000 * MHz);
+	mt_pll_raise_cci_freq(1540 * MHz);
+}
 
 void platform_romstage_main(void)
 {
 	mtk_pmif_init();
 	mt6315_init();
 	mt6359p_init();
+	raise_little_cpu_freq();
 	rtc_boot();
 	mtk_dram_init();
 }

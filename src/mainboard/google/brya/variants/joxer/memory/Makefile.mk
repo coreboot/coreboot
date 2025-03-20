a3862fa8c30ed0d998f6759fe12a9682591276d9
Mark Hsieh <mark_hsieh@wistron.corp-partner.google.com>
2025-03-20 10:09:27 +0800

    mb/google/nissa/var/joxer: Include SPD for Hynix H58G56CK8BX146
    Add joxer new supported memory part in mem_parts_used.txt.
    
    DRAM Part Name                 ID to assign
    H58G56CK8BX146                 5 (0101)
    
    BUG=b:236576115
    TEST=USE="project_joxer emerge-nissa coreboot"
    
    Change-Id: I4045e895694b940748b5f221ebcabaa4be064b95
    Signed-off-by: Mark Hsieh <mark_hsieh@wistron.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/86933
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>

--- src/mainboard/google/brya/variants/joxer/memory/Makefile.mk
+++ src/mainboard/google/brya/variants/joxer/memory/Makefile.mk
@@ -9,3 +9,4 @@ SPD_SOURCES += spd/lp5/set-0/spd-2.hex      # ID = 1(0b0001)  Parts = MT62F1G32D
 SPD_SOURCES += spd/lp5/set-0/spd-3.hex      # ID = 2(0b0010)  Parts = H58G56AK6BX069, K3LKBKB0BM-MGCP
 SPD_SOURCES += spd/lp5/set-0/spd-9.hex      # ID = 3(0b0011)  Parts = K3KL6L60GM-MGCT
 SPD_SOURCES += spd/lp5/set-0/spd-7.hex      # ID = 4(0b0100)  Parts = H58G56BK7BX068, MT62F1G32D2DS-026 WT:B, K3KL8L80CM-MGCT
+SPD_SOURCES += spd/lp5/set-0/spd-11.hex      # ID = 5(0b0101)  Parts = H58G56CK8BX146

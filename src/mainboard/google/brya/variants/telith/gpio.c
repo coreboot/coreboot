ef61d4d9254e9b9733933a124012e853cc56864d
Kun Liu <liukun11@huaqin.corp-partner.google.com>
2025-04-08 20:58:51 +0800

    mb/google/nissa/var/telith: Support x32 memory configuration
    Use GPP_E19 level to determine whether x32 memory configuration is
    supported.
    
    BUG=b:405303038
    TEST=emerge-nissa coreboot chromeos-bootimage
    
    Change-Id: I969fea2aba858f76870c1a31ad4bd884ec9b6ff3
    Signed-off-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87212
    Reviewed-by: Jayvik Desai <jayvik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Qinghong Zeng <zengqinghong@huaqin.corp-partner.google.com>
    Reviewed-by: Dinesh Gehlot <digehlot@google.com>

--- src/mainboard/google/brya/variants/telith/gpio.c
+++ src/mainboard/google/brya/variants/telith/gpio.c
@@ -69,6 +69,9 @@ static const struct pad_config override_gpio_table[] = {
 	/* R7 : DMIC_DATA_1A ==> NC */
 	PAD_NC_LOCK(GPP_R7, NONE, LOCK_CONFIG),
 
+	/* E19 : DDP1_CTRLDATA ==> GPP_E19_STRAP */
+	PAD_CFG_GPI_LOCK(GPP_E19, DN_20K, LOCK_CONFIG),
+
 	/* Configure the virtual CNVi Bluetooth I2S GPIO pads */
 	/* BT_I2S_BCLK */
 	PAD_CFG_NF(GPP_VGPIO_30, NONE, DEEP, NF3),
@@ -112,6 +115,8 @@ static const struct pad_config early_gpio_table[] = {
 	PAD_CFG_NF(GPP_H10, NONE, DEEP, NF2),
 	/* H11 : UART0_TXD ==> UART_SOC_TX_DBG_RX */
 	PAD_CFG_NF(GPP_H11, NONE, DEEP, NF2),
+	/* E19 : DDP1_CTRLDATA ==> GPP_E19_STRAP */
+	PAD_CFG_GPI_LOCK(GPP_E19, DN_20K, LOCK_CONFIG),
 };
 
 const struct pad_config *variant_gpio_override_table(size_t *num)

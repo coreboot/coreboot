fe881c990c97354297473314c2d38941b96a1e89
Kun Liu <liukun11@huaqin.corp-partner.google.com>
2025-04-09 10:18:27 +0800

    mb/google/brya: Create pujjocento variant
    Create the pujjocento variant of the nissa reference board by
    copying the template files to a new directory named for the variant.
    
    (Auto-Generated by create_coreboot_variant.sh version 4.5.0)
    
    BUG=b:409254508
    BRANCH=None
    TEST=util/abuild/abuild -p none -t google/brya -x -a
    make sure the build includes GOOGLE_PUJJOCENTO
    
    Change-Id: I33b0642624ba67f53126e92976096ba53750ae89
    Signed-off-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87419
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Jayvik Desai <jayvik@google.com>

---
+++ src/mainboard/google/brya/variants/pujjocento/Makefile.mk
@@ -0,0 +1,8 @@
+## SPDX-License-Identifier: GPL-2.0-only
+
+bootblock-y += gpio.c
+romstage-y += gpio.c
+romstage-y += memory.c
+ramstage-y += gpio.c
+ramstage-$(CONFIG_SOC_INTEL_COMMON_BLOCK_HDA_VERB) += hda_verb.c
+ramstage-y += variant.c
---
+++ src/mainboard/google/brya/variants/pujjocento/include/variant/gpio.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef VARIANT_GPIO_H
+#define VARIANT_GPIO_H
+
+#include <baseboard/gpio.h>
+
+#define WWAN_RST	GPP_E17
+#define T2_OFF_MS	20
+#define SLP_S0_HDMI_EN	GPP_A7
+#endif

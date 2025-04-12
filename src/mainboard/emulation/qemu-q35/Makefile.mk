b3b7b7a027517aaf238be4b6cc1ebf24c2ed3beb
Maximilian Brune <maximilian.brune@9elements.com>
2025-04-12 15:36:13 +0200

    mb/emulation/qemu-q35/Makefile.mk: Use all-y for memmap.c
    Signed-off-by: Maximilian Brune <maximilian.brune@9elements.com>
    Change-Id: I14cd19c6c0f458a8d9a4034bb6faff0b42577f54
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87290
    Reviewed-by: Elyes Haouas <ehaouas@noos.fr>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Naresh <naresh.solanki.2011@gmail.com>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/emulation/qemu-q35/Makefile.mk
+++ src/mainboard/emulation/qemu-q35/Makefile.mk
@@ -1,22 +1,19 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
 bootblock-y += bootblock.c
-bootblock-y += memmap.c
 
 romstage-y += ../qemu-i440fx/memmap.c
-romstage-y += memmap.c
 
 postcar-y += ../qemu-i440fx/memmap.c
 postcar-y += ../qemu-i440fx/exit_car.S
-postcar-y += memmap.c
 
 ramstage-y += ../qemu-i440fx/memmap.c
 ramstage-y += ../qemu-i440fx/northbridge.c
 ramstage-y += ../qemu-i440fx/rom_media.c
-ramstage-y += memmap.c
 ramstage-y += cpu.c
 
 all-y += ../qemu-i440fx/bootmode.c
+all-y += memmap.c
 
 ramstage-$(CONFIG_CHROMEOS) += chromeos.c
 

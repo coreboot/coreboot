ee5256bfe5b73560a5503a8028c12357f7338543
David Wu <david_wu@quanta.corp-partner.google.com>
2025-04-14 16:21:41 +0800

    mb/google/fatcat: Create kinmen variant
    Create the kinmen variant of the fatcat reference board by copying
    the fatcat files to a new directory named for the variant.
    
    BUG=b:409148565
    TEST=1. util/abuild/abuild -p none -t google/fatcat -x -a
            make sure the build includes GOOGLE_KINMEN
         2. Run part_id_gen tool without any errors
    
    Change-Id: I51e388e61f102216f6ce9233c87c1915596602be
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87317
    Reviewed-by: Bob Moragues <moragues@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Reviewed-by: YH Lin <yueherngl@google.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Jayvik Desai <jayvik@google.com>

--- src/mainboard/google/fatcat/variants/baseboard/fatcat/include/baseboard/gpio.h
+++ src/mainboard/google/fatcat/variants/baseboard/fatcat/include/baseboard/gpio.h
@@ -30,6 +30,10 @@
  #define GPIO_PCH_WP	0 /* TODO */
 /* Used to gate SoC's SLP_S0# signal */
 #define GPIO_SLP_S0_GATE	GPP_D03
+#elif CONFIG(BOARD_GOOGLE_KINMEN)
+ #define EC_SYNC_IRQ	GPP_E07_IRQ
+ #define GPIO_PCH_WP	GPP_D02
+#define GPIO_SLP_S0_GATE	0 /* Not Connected */
 #endif
 
 #endif /* __BASEBOARD_GPIO_H__ */

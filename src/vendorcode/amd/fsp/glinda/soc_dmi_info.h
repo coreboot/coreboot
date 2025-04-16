66873a38125902eca5093f7ca602b936cc11e9b8
Maximilian Brune <maximilian.brune@9elements.com>
2025-04-16 20:55:21 +0200

    vc/amd/fsp/glinda: Update SMBIOS Type 17 information
    Update values for glinda SOC.
    
    Signed-off-by: Maximilian Brune <maximilian.brune@9elements.com>
    Change-Id: I02110647db86d7ff23f3ea3640dd531c7954ca42
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87585
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/vendorcode/amd/fsp/glinda/soc_dmi_info.h
+++ src/vendorcode/amd/fsp/glinda/soc_dmi_info.h
@@ -8,9 +8,9 @@
 #ifndef SOC_DMI_INFO_H
 #define SOC_DMI_INFO_H
 
-#define AGESA_STRUCT_SOCKET_COUNT         2   ///< Number of sockets in AGESA FSP DMI T17 table
-#define AGESA_STRUCT_CHANNELS_PER_SOCKET  8   ///< Channels per socket in AGESA FSP DMI T17 table
-#define AGESA_STRUCT_DIMMS_PER_CHANNEL    4   ///< DIMMs per channel in AGESA FSP DMI T17 table
+#define AGESA_STRUCT_SOCKET_COUNT         4   ///< Number of sockets in AGESA FSP DMI T17 table
+#define AGESA_STRUCT_CHANNELS_PER_SOCKET  16   ///< Channels per socket in AGESA FSP DMI T17 table
+#define AGESA_STRUCT_DIMMS_PER_CHANNEL    2   ///< DIMMs per channel in AGESA FSP DMI T17 table
 #define AGESA_STRUCT_PART_NUMBER_SIZE     31
 
 #define SMBIOS_3_2_3_3_SUPPORT            1

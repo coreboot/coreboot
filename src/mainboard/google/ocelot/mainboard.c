effd1ffdad73e23e23ffbab8fbd0f7de052126ff
Avi Uday <aviuday@google.com>
2025-04-17 17:34:12 +0530

    mb/google/ocelot: Update Ocelot board
    Syncing ocelot code to fatcat so that it has the recent changes made to fatcat. This is done by manually copying the fatcat code, similar to https://review.coreboot.org/c/coreboot/+/86692
    
    List of commits -
    86702: mb/google/fatcat/var/fatcat: Update GSPI0 CS pin for FPS | https://review.coreboot.org/c/coreboot/+/86702
    86479: mb/google/fatcat/var/fatcat: Update THC Interrupt for Touchpad Development | https://review.coreboot.org/c/coreboot/+/86479
    86938: mb/google/fatcat/var/fatcat: Add Write Protect GPIO to cros_gpios | https://review.coreboot.org/c/coreboot/+/86938
    86981: mb/google/fatcat: Implement S0ix hooks aka `MS0X` method | https://review.coreboot.org/c/coreboot/+/86981
    85662: mb/google/fatcat: Rationalize Wi-Fi and Bluetooth combinations | https://review.coreboot.org/c/coreboot/+/85662
    87089: mb/google/fatcat: Allow board-specific FSP-M UPD override | https://review.coreboot.org/c/coreboot/+/87089
    87090: mb/google/fatcat: Enable HDA SDI based on FW config | https://review.coreboot.org/c/coreboot/+/87090
    85200: mb/google/fatcat: Add Intel Touch support for touchscreen and touchpad | https://review.coreboot.org/c/coreboot/+/85200
    87154: mb/google/fatcat/variants/fatcat: Refactor X1 PCIe power enable GPIO | https://review.coreboot.org/c/coreboot/+/87154
    87191: mb/google/fatcat: Perform early post-memory GPIO configuration | https://review.coreboot.org/c/coreboot/+/87191
    87183: mb/google/fatcat: Introduce variant-specific barrel jack presence check | https://review.coreboot.org/c/coreboot/+/87183
    87207: mb/google/fatcat/var/fatcat: Implement barrel jack presence check | https://review.coreboot.org/c/coreboot/+/87207
    87236: mb/google/fatcat: Enable FSP_UGOP_EARLY_SIGN_OF_LIFE | https://review.coreboot.org/c/coreboot/+/87236
    
    BUG=b:412736286
    BRANCH=None
    TEST=Build AP firmware image.
    
    Change-Id: Icb8ec9ff26abd71931e0689f94ea6af260f7618c
    Signed-off-by: Avi Uday <aviuday@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87351
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/mainboard/google/ocelot/mainboard.c
+++ src/mainboard/google/ocelot/mainboard.c
@@ -4,6 +4,7 @@
 #include <acpi/acpigen.h>
 #include <baseboard/gpio.h>
 #include <baseboard/variants.h>
+#include <bootstate.h>
 #include <device/device.h>
 #include <ec/ec.h>
 #include <soc/ramstage.h>
@@ -27,6 +28,12 @@ void __weak variant_update_soc_chip_config(struct soc_intel_pantherlake_config *
 
 static void mainboard_init(void *chip_info)
 {
+	baseboard_devtree_update();
+}
+
+/* Must happen before MPinit */
+static void mainboard_early(void *unused)
+{
 	struct pad_config *padbased_table;
 	const struct pad_config *base_pads;
 	size_t base_num;
@@ -37,8 +44,9 @@ static void mainboard_init(void *chip_info)
 	fw_config_gpio_padbased_override(padbased_table);
 	gpio_configure_pads_with_padbased(padbased_table);
 	free(padbased_table);
-	baseboard_devtree_update();
 }
+
+BOOT_STATE_INIT_ENTRY(BS_PRE_DEVICE, BS_ON_EXIT, mainboard_early, NULL);
 
 void __weak baseboard_devtree_update(void)
 {
@@ -58,6 +66,32 @@ void __weak variant_generate_s0ix_hook(enum s0ix_entry entry)
 	 */
 }
 
+static void mainboard_generate_s0ix_hook(void)
+{
+	acpigen_write_if_lequal_op_int(ARG0_OP, 1);
+	{
+		if (CONFIG(HAVE_SLP_S0_GATE))
+			acpigen_soc_clear_tx_gpio(GPIO_SLP_S0_GATE);
+		variant_generate_s0ix_hook(S0IX_ENTRY);
+	}
+	acpigen_write_else();
+	{
+		if (CONFIG(HAVE_SLP_S0_GATE))
+			acpigen_soc_set_tx_gpio(GPIO_SLP_S0_GATE);
+		variant_generate_s0ix_hook(S0IX_EXIT);
+	}
+	acpigen_write_if_end();
+}
+
+static void mainboard_fill_ssdt(const struct device *dev)
+{
+	acpigen_write_scope("\\_SB");
+	acpigen_write_method_serialized("MS0X", 1);
+	mainboard_generate_s0ix_hook();
+	acpigen_write_method_end(); /* Method */
+	acpigen_write_scope_end(); /* Scope */
+}
+
 static void mainboard_dev_init(struct device *dev)
 {
 	mainboard_ec_init();
@@ -66,6 +100,7 @@ static void mainboard_dev_init(struct device *dev)
 static void mainboard_enable(struct device *dev)
 {
 	dev->ops->init = mainboard_dev_init;
+	dev->ops->acpi_fill_ssdt = mainboard_fill_ssdt;
 }
 
 struct chip_operations mainboard_ops = {
--- src/mainboard/google/ocelot/variants/ocelot/variant.c
+++ src/mainboard/google/ocelot/variants/ocelot/variant.c
@@ -1,9 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <acpi/acpigen.h>
 #include <baseboard/variants.h>
+#include <ec/google/chromeec/ec.h>
+#include <fsp/api.h>
 #include <fw_config.h>
 #include <sar.h>
+#include <soc/gpio_soc_defs.h>
+#include <drivers/intel/touch/chip.h>
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
@@ -12,8 +15,6 @@ const char *get_wifi_sar_cbfs_filename(void)
 
 void variant_update_soc_chip_config(struct soc_intel_pantherlake_config *config)
 {
-	config->cnvi_wifi_core = false;
-	config->cnvi_bt_core = false;
 	/* CNVi */
 	if (fw_config_probe(FW_CONFIG(WIFI, WIFI_CNVI_6)) ||
 			fw_config_probe(FW_CONFIG(WIFI, WIFI_CNVI_7))) {
@@ -25,4 +26,66 @@ void variant_update_soc_chip_config(struct soc_intel_pantherlake_config *config)
 			config->cnvi_bt_audio_offload = true;
 		}
 	}
+
+    /* Touchscreen and touchpad WOT support:
+     * +===================+==================+=================+============================+
+     * | touchsreen        | touchpad         | PMC_GPE0_DW0    | WOT                        |
+     * +===================+==================+==============================================+
+     * | THC-SPI/THC-I2C   | LPSS-I2C         | GPP_F           | TS, TP                     |
+     * +-------------------+------------------+----------------------------------------------+
+     * | THC-SPI/THC-I2C   | not used or      | GPP_A (default) | TS                         |
+     * |                   | with WOT disabled|                 |                            |
+     * +-------------------+------------------+----------------------------------------------+
+     * | THC-SPI/THC-I2C   | THC-I2C          | GPP_A (default) | TS, TP                     |
+     * +-------------------+------------------+----------------------------------------------+
+     * | LPSS-SPI/LPSS-I2C | LPSS-I2C         | GPP_F           | TS via PMC_GPE0_DW2: GPP_E,|
+     * |                   |                  |                 | TP                         |
+     * +-------------------+------------------+----------------------------------------------+
+     * | LPSS-SPI/LPSS-I2C | not used or      | GPP_A (default) | TS via PMC_GPE0_DW2: GPP_E |
+     * |                   | with WOT disabled|                 |                            |
+     * +-------------------+------------------+----------------------------------------------+
+     * | not used          | not used or      | GPP_A (default) | NA                         |
+     * | with WOT disabled | with WOT disabled|                 |                            |
+     * +===================+==================+=================+============================+
+     */
+	if (fw_config_probe(FW_CONFIG(TOUCHSCREEN, TOUCHSCREEN_THC_I2C))) {
+		config->thc_mode[0] = THC_HID_I2C_MODE;
+	} else if (fw_config_probe(FW_CONFIG(TOUCHSCREEN, TOUCHSCREEN_THC_SPI))) {
+		config->thc_mode[0] = THC_HID_SPI_MODE;
+	}
+
+	if (fw_config_probe(FW_CONFIG(TOUCHPAD, TOUCHPAD_LPSS_I2C))) {
+		/* touchpad: GPP_F18: GPE0_DW0_18 */
+		if (config->thc_wake_on_touch[1])
+			config->pmc_gpe0_dw0 = GPP_F;
+	} else if (fw_config_probe(FW_CONFIG(TOUCHPAD, TOUCHPAD_THC_I2C))) {
+		config->thc_mode[1] = THC_HID_I2C_MODE;
+
+		if (fw_config_probe(FW_CONFIG(TOUCHSCREEN, TOUCHSCREEN_NONE)))
+			/* When THC0 is only enabled due to THC1 is enabled, we force THC0
+			 * to I2C mode so that kernel THC I2C driver will find it and put
+			 * THC0 to low power state because no connected device is found.
+			 */
+			config->thc_mode[0] = THC_HID_I2C_MODE;
+	}
+}
+
+void variant_update_soc_memory_init_params(FSPM_UPD *memupd)
+{
+	FSP_M_CONFIG *m_cfg = &memupd->FspmConfig;
+
+	/* HDA Audio */
+	if (fw_config_probe(FW_CONFIG(AUDIO, AUDIO_ALC256_HDA))) {
+		printk(BIOS_INFO, "Overriding HDA SDI lanes.\n");
+		m_cfg->PchHdaSdiEnable[0] = true;
+		m_cfg->PchHdaSdiEnable[1] = false;
+	}
+}
+
+bool variant_is_barrel_charger_present(void)
+{
+	if (fw_config_probe(FW_CONFIG(PSU, PSU_BJ)))
+		return google_chromeec_is_barrel_charger_present();
+	else
+		return false;
 }

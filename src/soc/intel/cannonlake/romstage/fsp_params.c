b830fdc2d7d9f9b275c6024db2730d64ca87a677
Matt DeVillier <matt.devillier@gmail.com>
2025-04-21 10:25:40 -0500

    soc/intel/cannonlake: Hook up IGD config to option API
    Hook up the IGD config options IgdDvmt50PreAlloc and ApertureSize to the
    option API, so they can be configured at runtime without recompilation.
    
    The Aperture size falls back to the FSP default value, so no change if
    unconfigured.
    
    Change-Id: Idad22ca79c10d575320b4360ec24c2019a837446
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87398
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Maxim Polyakov <max.senia.poliak@gmail.com>

--- src/soc/intel/cannonlake/romstage/fsp_params.c
+++ src/soc/intel/cannonlake/romstage/fsp_params.c
@@ -37,7 +37,8 @@ void platform_fsp_memory_init_params_cb(FSPM_UPD *mupd, uint32_t version)
 	if (igd_on && pci_read_config16(SA_DEV_IGD, PCI_VENDOR_ID) != 0xffff) {
 		/* Set IGD stolen size to 64MB. */
 		m_cfg->InternalGfx = 1;
-		m_cfg->IgdDvmt50PreAlloc = 2;
+		m_cfg->IgdDvmt50PreAlloc = get_uint_option("igd_dvmt_prealloc", 2);
+		m_cfg->ApertureSize = get_uint_option("igd_aperture_size", 1);
 	} else {
 		m_cfg->InternalGfx = 0;
 		m_cfg->IgdDvmt50PreAlloc = 0;

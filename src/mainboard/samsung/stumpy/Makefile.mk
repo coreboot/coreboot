204aae207ddd7b68e5279b9dc95cbe52500f39cd
Matt DeVillier <matt.devillier@gmail.com>
2025-04-21 14:02:36 -0500

    mb/samsung/stumpy: Clean up makefile
    Organize according to stage, alphabetize, and guard inclusion of
    ChromeOS-specific code.
    
    TEST=build/boot stumpy
    
    Change-Id: Id6ea4e685ff3e098f6c619e1cab48a5a9320c29b
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87405
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/samsung/stumpy/Makefile.mk
+++ src/mainboard/samsung/stumpy/Makefile.mk
@@ -1,12 +1,12 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
-romstage-y += chromeos.c
-ramstage-y += chromeos.c
+bootblock-y += early_init.c
 bootblock-y += gpio.c
+
+romstage-$(CONFIG_CHROMEOS) += chromeos.c
+romstage-y += early_init.c
 romstage-y += gpio.c
 
-ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads
-bootblock-y += early_init.c
-romstage-y += early_init.c
-
 ramstage-$(CONFIG_DRIVERS_OPTION_CFR) += cfr.c
+ramstage-$(CONFIG_CHROMEOS) += chromeos.c
+ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads

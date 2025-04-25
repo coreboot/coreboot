1015d4332fc9c8b0e3417eafab8154603b14b758
Subrata Banik <subratabanik@google.com>
2025-04-25 12:11:11 +0530

    mainboard/google/bluey: Add fingerprint sensor GPIO entries
    This commit introduces Kconfig options and GPIO definitions to support
    onboard fingerprint sensors for the Bluey mainboard.
    
    Source: Bluey schematics (dated 04/15).
    
    BUG=b:404985109
    TEST=Successfully built google/bluey with the Qualcomm x1p42100 SoC.
    
    Change-Id: Ia8f1a7b2dab3bef1a9332d8c0020489c06bd4f99
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87448
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Jayvik Desai <jayvik@google.com>

--- src/mainboard/google/bluey/board.h
+++ src/mainboard/google/bluey/board.h
@@ -10,6 +10,20 @@
 #define GSC_AP_INT(x)		GPIO(x)
 #define GPIO_GSC_AP_INT		GSC_AP_INT(CONFIG_MAINBOARD_GPIO_PIN_FOR_GSC_AP_INTERRUPT)
 
+/* Fingerprint-specific GPIOs. Only for fingerprint-enabled devices. */
+#if CONFIG(MAINBOARD_HAS_FINGERPRINT)
+#define GPIO_FP_RST_L		GPIO(25)
+#if CONFIG(MAINBOARD_HAS_FINGERPRINT_VIA_SPI)
+#define GPIO_FPMCU_BOOT0	GPIO(24)
+#define GPIO_FPMCU_INT		GPIO(23)
+#define GPIO_EN_FP_RAILS	GPIO(22)
+#else
+#define GPIO_FPMCU_BOOT0	dead_code_t(gpio_t)
+#define GPIO_FPMCU_INT		dead_code_t(gpio_t)
+#define GPIO_EN_FP_RAILS	dead_code_t(gpio_t)
+#endif
+#endif
+
 void setup_chromeos_gpios(void);
 
 #endif /* MAINBOARD_GOOGLE_BLUEY_BOARD_H */

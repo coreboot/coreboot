3cc728110d05a745c2c3cd18c82b91fe10ad4754
Matt DeVillier <matt.devillier@gmail.com>
2025-04-30 12:47:38 -0500

    soc/intel/alderlake: Hook up S0ix setting to option API
    Hook up the s0ix_enable setting to the option API, so it can be changed
    at runtime without recompilation. Default to the value set by the
    mainboard.
    
    Change-Id: I8f55bb7b31936c098da64f65b76965a09981ff73
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87498
    Reviewed-by: Sean Rhodes <sean@starlabs.systems>
    Reviewed-by: Jayvik Desai <jayvik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/alderlake/fsp_params.c
+++ src/soc/intel/alderlake/fsp_params.c
@@ -1249,6 +1249,9 @@ static void soc_silicon_init_params(FSP_S_CONFIG *s_cfg,
 	/* Override settings per board if required. */
 	mainboard_update_soc_chip_config(config);
 
+	/* Runtime configuration of S0ix */
+	config->s0ix_enable = get_uint_option("s0ix_enable", config->s0ix_enable);
+
 	void (*const fill_fsps_params[])(FSP_S_CONFIG *s_cfg,
 			const struct soc_intel_alderlake_config *config) = {
 		fill_fsps_lpss_params,

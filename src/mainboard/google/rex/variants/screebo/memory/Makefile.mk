6c830088da6ed51bd7c634125fca9eb43eef2aa3
Zhongtian Wu <wuzhongtian@huaqin.corp-partner.google.com>
2025-05-09 14:48:39 +0800

    mb/google/rex/var/screebo: Generate RAM IDs
    Generate RAM IDs for K3KL9L90EM-MGCU
    
    BUG=b:416632273
    BRANCH=None
    TEST=Run part_id_gen tool without any errors
    
    Change-Id: I8b19e768ad81b0ef46bf1f5f6ec83952d784c3f2
    Signed-off-by: Zhongtian Wu <wuzhongtian@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87579
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: YH Lin <yueherngl@google.com>

--- src/mainboard/google/rex/variants/screebo/memory/Makefile.mk
+++ src/mainboard/google/rex/variants/screebo/memory/Makefile.mk
@@ -9,3 +9,4 @@ SPD_SOURCES += spd/lp5/set-0/spd-7.hex      # ID = 1(0b0001)  Parts = MT62F1G32D
 SPD_SOURCES += spd/lp5/set-0/spd-8.hex      # ID = 2(0b0010)  Parts = K3KL9L90CM-MGCT
 SPD_SOURCES += spd/lp5/set-0/spd-9.hex      # ID = 3(0b0011)  Parts = K3KL6L60GM-MGCT
 SPD_SOURCES += spd/lp5/set-0/spd-11.hex      # ID = 4(0b0100)  Parts = K3KL8L80EM-MGCU
+SPD_SOURCES += spd/lp5/set-0/spd-10.hex      # ID = 5(0b0101)  Parts = K3KL9L90EM-MGCU

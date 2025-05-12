d93f7f01a6a1c6d703e3972155b09c87466bf022
Alicja Michalska <alicja.michalska@9elements.com>
2025-05-13 00:44:08 +0200

    mb/topton/adl: Use CFR setup menu to manage options
    Much like ongoing CFR work, this patch adds support for configuring
    certain options (such as iGPU memory allocation, ASPM, S3/s0ix, VT-d) at
    runtime, using EDK2 payload.
    
    TEST=Build/boot/toggle coreboot+edk2 on the firewall, test results by
    booting Linux.
    
    Change-Id: Id51e704750fd9aa4a8df72804d9205974747d708
    Signed-off-by: Alicja Michalska <alicja.michalska@9elements.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87652
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/topton/adl/Makefile.mk
+++ src/mainboard/topton/adl/Makefile.mk
@@ -1,5 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 bootblock-y += bootblock.c
+
 romstage-y += romstage_fsp_params.c
+
 ramstage-y += mainboard.c
+ramstage-$(CONFIG_DRIVERS_OPTION_CFR) += cfr.c

dc3d524d19c11a076c9751927e05e09802e6c558
Matt DeVillier <matt.devillier@gmail.com>
2025-05-12 15:22:14 -0500

    mb/starlabs/starlite_adl: Use SoC common CFR forms
    Use SoC common CFR forms; select CSE_DEFAULT_OPTION_STATE_DISABLED
    to keep existing behavior.
    
    Change-Id: I63cea721e5678a979bdb51c935b36d3149e067c9
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87987
    Reviewed-by: Sean Rhodes <sean@starlabs.systems>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/starlabs/starlite_adl/cfr.c
+++ src/mainboard/starlabs/starlite_adl/cfr.c
@@ -1,14 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <boot/coreboot_tables.h>
-#include <commonlib/coreboot_tables.h>
 #include <console/cfr.h>
 #include <drivers/option/cfr_frontend.h>
 #include <ec/starlabs/merlin/cfr.h>
-#include <inttypes.h>
-#include <intelblocks/pcie_rp.h>
-#include <string.h>
-#include <types.h>
+#include <intelblocks/cfr.h>
 #include <variants.h>
 
 static const struct sm_object accelerometer = SM_DECLARE_BOOL({
@@ -27,24 +23,6 @@ static const struct sm_object gna = SM_DECLARE_BOOL({
 });
 #endif
 
-static const struct sm_object me_state = SM_DECLARE_ENUM({
-	.opt_name	= "me_state",
-	.ui_name	= "Intel Management Engine",
-	.ui_helptext	= "Enable or disable the Intel Management Engine",
-	.default_value	= 1,
-	.values		= (const struct sm_enum_value[]) {
-				{ "Disabled",		1		},
-				{ "Enabled",		0		},
-				SM_ENUM_VALUE_END			},
-});
-
-static const struct sm_object me_state_counter = SM_DECLARE_NUMBER({
-	.opt_name	= "me_state_counter",
-	.ui_name	= "ME State Counter",
-	.flags		= CFR_OPTFLAG_SUPPRESS,
-	.default_value	= 0,
-});
-
 static const struct sm_object memory_speed = SM_DECLARE_ENUM({
 	.opt_name	= "memory_speed",
 	.ui_name	= "Memory Speed",
@@ -56,13 +34,6 @@ static const struct sm_object memory_speed = SM_DECLARE_ENUM({
 		{	"7500MT/s",	2		},
 		SM_ENUM_VALUE_END,
 	}
-});
-
-static const struct sm_object power_on_after_fail = SM_DECLARE_BOOL({
-	.opt_name	= "power_on_after_fail",
-	.ui_name	= "Power on after failure",
-	.ui_helptext	= "Automatically turn on after a power failure",
-	.default_value	= false,
 });
 
 static const struct sm_object power_profile = SM_DECLARE_ENUM({
@@ -83,48 +54,6 @@ static const struct sm_object microphone = SM_DECLARE_BOOL({
 	.ui_helptext	= "Enable or disable the built-in microphone",
 	.default_value	= true,
 });
-
-#if CONFIG(SOC_INTEL_ALDERLAKE)
-static const struct sm_object pciexp_aspm = SM_DECLARE_ENUM({
-	.opt_name	= "pciexp_aspm",
-	.ui_name	= "PCI ASPM",
-	.ui_helptext	= "Controls the Active State Power Management for PCI devices."
-			  " Enabling this feature can reduce power consumption of"
-			  " PCI-connected devices during idle times.",
-	.default_value	= ASPM_L0S_L1,
-	.values		= (const struct sm_enum_value[]) {
-				{ "Disabled",		ASPM_DISABLE	},
-				{ "L0s",		ASPM_L0S	},
-				{ "L1",			ASPM_L1		},
-				{ "L0sL1",		ASPM_L0S_L1	},
-				SM_ENUM_VALUE_END			},
-});
-
-static const struct sm_object pciexp_clk_pm = SM_DECLARE_BOOL({
-	.opt_name	= "pciexp_clk_pm",
-	.ui_name	= "PCI Clock Power Management",
-	.ui_helptext	= "Enables or disables power management for the PCI clock. When"
-			  " enabled, it reduces power consumption during idle states."
-			  " This can help lower overall energy use but may impact"
-			  " performance in power-sensitive tasks.",
-	.default_value	= true,
-});
-
-static const struct sm_object pciexp_l1ss = SM_DECLARE_ENUM({
-	.opt_name	= "pciexp_l1ss",
-	.ui_name	= "PCI L1 Substates",
-	.ui_helptext	= "Controls deeper power-saving states for PCI devices."
-			  " Enabling this feature allows supported devices to achieve"
-			  " lower power states at the cost of slightly increased"
-			  " latency when exiting these states.",
-	.default_value	= L1_SS_L1_2,
-	.values		= (const struct sm_enum_value[]) {
-				{ "Disabled",		L1_SS_DISABLED	},
-				{ "L1.1",		L1_SS_L1_1	},
-				{ "L1.2",		L1_SS_L1_2	},
-				SM_ENUM_VALUE_END			},
-});
-#endif
 
 static const struct sm_object touchscreen = SM_DECLARE_BOOL({
 	.opt_name	= "touchscreen",
@@ -198,7 +127,7 @@ static struct sm_obj_form power = {
 		&charging_speed,
 		#endif
 		&power_led,
-		&power_on_after_fail,
+		&power_on_after_fail_bool,
 		NULL
 	},
 };
@@ -225,7 +154,7 @@ static struct sm_obj_form devices = {
 static struct sm_obj_form pci = {
 	.ui_name = "PCI",
 	.obj_list = (const struct sm_object *[]) {
-		#if CONFIG(SOC_INTEL_ALDERLAKE)
+		#if CONFIG(SOC_INTEL_COMMON_BLOCK_ASPM)
 		&pciexp_clk_pm,
 		&pciexp_aspm,
 		&pciexp_l1ss,

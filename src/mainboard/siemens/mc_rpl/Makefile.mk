8fdf8694e3e270fc47f682a5587b1af03837372b
Werner Zeh <werner.zeh@siemens.com>
2025-05-14 10:47:08 +0200

    mb/siemens/mc_rpl: Remove Chrome OS and EC as they are not used
    This mainboard neither uses Chrome OS nor has any embedded controller
    available. This patch removes all references from the build in this
    regard. This also requires some refactoring in board_id.c.
    
    Change-Id: If834480fbdac4b4843c265a257d3a77678f56aab
    Signed-off-by: Werner Zeh <werner.zeh@siemens.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87666
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/siemens/mc_rpl/Makefile.mk
+++ src/mainboard/siemens/mc_rpl/Makefile.mk
@@ -3,7 +3,6 @@
 subdirs-y += spd
 
 bootblock-y += bootblock.c
-bootblock-$(CONFIG_CHROMEOS) += chromeos.c
 ifeq ($(CONFIG_SOC_INTEL_ALDERLAKE_PCH_N),y)
 bootblock-y += early_gpio_n.c
 ramstage-y += gpio_n.c
@@ -12,9 +11,6 @@ bootblock-y += early_gpio.c
 ramstage-y += gpio.c
 endif
 
-verstage-$(CONFIG_CHROMEOS) += chromeos.c
-
-romstage-$(CONFIG_CHROMEOS) += chromeos.c
 romstage-y += romstage_fsp_params.c
 romstage-y += board_id.c
 romstage-y += memory.c
@@ -22,8 +18,6 @@ ifeq ($(CONFIG_BOARD_INTEL_ADLRVP_RPL_EXT_EC),y)
 romstage-y += memory_rpl.c
 endif
 
-ramstage-$(CONFIG_CHROMEOS) += chromeos.c
-ramstage-y += ec.c
 ramstage-y += mainboard.c
 ramstage-y += board_id.c
 ramstage-$(CONFIG_FW_CONFIG) += fw_config.c
--- src/mainboard/siemens/mc_rpl/board_id.h
+++ src/mainboard/siemens/mc_rpl/board_id.h
@@ -3,11 +3,6 @@
 #ifndef _MAINBOARD_COMMON_BOARD_ID_H_
 #define _MAINBOARD_COMMON_BOARD_ID_H_
 
-/* Board/FAB ID Command */
-#define EC_FAB_ID_CMD	0x0d
-/* Bit 5:0 for Board ID */
-#define BOARD_ID_MASK	0x3f
-
 /*
  * Returns board information (board id[15:8] and
  * Fab info[7:0]) on success and < 0 on error

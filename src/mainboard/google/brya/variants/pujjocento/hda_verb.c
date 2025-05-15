278a6d26822d61ca70e175543490e1f458490fc3
Lei Cao <caolei6@huaqin.corp-partner.google.com>
2025-05-15 15:57:40 +0800

    mb/google/trulo/var/pujjocento: update hda_verb table for ALC257
    update hda_verb table for pujjocento, provided by Realtek on 20250515.
    
    BUG=b:409752486
    TEST=emerge-nissa sys-boot/coreboot sys-boot/chromeos-bootimage
    
    Device list:
    cat /sys/bus/hdaudio/devices/ehdaudio0D0/chip_name
    ALC257
    cat /sys/bus/hdaudio/devices/ehdaudio0D0/vendor_name
    Realtek
    
    Headphone detection:
    Event: type 5 (EV_SW), code 2 (SW_HEADPHONE_INSERT), value 0
    Event: -------------- SYN_REPORT ------------
    Event: type 5 (EV_SW), code 2 (SW_HEADPHONE_INSERT), value 1
    Event: -------------- SYN_REPORT ------------
    Event: type 1 (EV_KEY), code 164 (KEY_PLAYPAUSE), value 1
    Event: type 1 (EV_KEY), code 115 (KEY_VOLUMEUP), value 1
    Event: type 1 (EV_KEY), code 114 (KEY_VOLUMEDOWN), value 1
    
    Change-Id: Ib0a22acdbcbe6643665f9f07469fba41e8027d7c
    Signed-off-by: Lei Cao <caolei6@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87693
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>

--- src/mainboard/google/brya/variants/pujjocento/hda_verb.c
+++ src/mainboard/google/brya/variants/pujjocento/hda_verb.c
@@ -4,13 +4,13 @@
 
 const u32 cim_verb_data[] = {
 	/* coreboot specific header */
-	0x10ec0256,	// Codec Vendor / Device ID: Realtek ALC256
-	0x10ec12ac,	// Subsystem ID
-	0x00000013,	// Number of jacks (NID entries)
+	0x10ec0257,	// Codec Vendor / Device ID: Realtek ALC257
+	0x10ec0257,	// Subsystem ID
+	0x00000010,	// Number of jacks (NID entries)
 
 	AZALIA_RESET(0x1),
 	/* NID 0x01, HDA Codec Subsystem ID Verb table */
-	AZALIA_SUBVENDOR(0, 0x10ec12ac),
+	AZALIA_SUBVENDOR(0, 0x10ec3a2c),
 
 	/* Pin Widget Verb Table */
 
@@ -27,100 +27,58 @@ const u32 cim_verb_data[] = {
 	/* Pin widget 0x18 - NPC */
 	AZALIA_PIN_CFG(0, 0x18, 0x411111F0),
 	/* Pin widget 0x19 - MIC2 (Port-F) */
-	AZALIA_PIN_CFG(0, 0x19, 0x04A11040),
+	AZALIA_PIN_CFG(0, 0x19, 0x04A11030),
 	/* Pin widget 0x1A - LINE1 (Port-C) */
 	AZALIA_PIN_CFG(0, 0x1a, 0x411111F0),
 	/* Pin widget 0x1B - NPC */
 	AZALIA_PIN_CFG(0, 0x1b, 0x411111F0),
 	/* Pin widget 0x1D - BEEP-IN */
-	AZALIA_PIN_CFG(0, 0x1d, 0x40610041),
+	AZALIA_PIN_CFG(0, 0x1d, 0x40579A2D),
 	/* Pin widget 0x1E - NPC */
 	AZALIA_PIN_CFG(0, 0x1e, 0x411111F0),
 	/* Pin widget 0x21 - HP1-OUT (Port-I) */
 	AZALIA_PIN_CFG(0, 0x21, 0x04211020),
-	/*
-	 * Widget node 0x20 - 1
-	 * Codec hidden reset and speaker power 2W/4ohm
-	 */
+	/* Widget node 0x20 - 0 */
 	0x0205001A,
 	0x0204C003,
+	0x0205001A,
+	0x0204C003,
+	/* Widget node 0x20 - 1 */
 	0x02050038,
-	0x02047901,
-	/*
-	 * Widget node 0x20 - 2
-	 * Class D power on Reset
-	 */
-	0x0205003C,
-	0x02040354,
-	0x0205003C,
-	0x02040314,
-	/*
-	 * Widget node 0x20 - 3
-	 * Disable AGC and set AGC limit to -1.5dB
-	 */
-	0x02050016,
-	0x02040C50,
-	0x02050012,
-	0x0204EBC1,
-	/*
-	 * Widget node 0x20 - 4
-	 * Set AGC Post gain +1.5dB then Enable AGC
-	 */
-	0x02050013,
-	0x02044023,
-	0x02050016,
-	0x02040E50,
-	/*
-	 * Widget node 0x20 - 5
-	 * Silence detector enabling + Set EAPD to verb control
-	 */
-	0x02050037,
-	0x0204FE15,
-	0x02050010,
-	0x02040020,
-	/*
-	 * Widget node 0x20 - 6
-	 * Silence data mode Threshold (-90dB)
-	 */
-	0x02050030,
-	0x0204A000,
+	0x02046981,
 	0x0205001B,
 	0x02040A4B,
-	/*
-	 * Widget node 0x20 - 7
-	 * Default setting - 1
-	 */
-	0x05750003,
-	0x05740DA3,
+	/* Widget node 0x20 - 2 */
+	0x0205003C,
+	0x02043154,
+	0x0205003C,
+	0x02043114,
+	/* Widget node 0x20 - 3 */
 	0x02050046,
 	0x02040004,
-	/*
-	 * Widget node 0x20 - 8
-	 * support 1 pin detect two port
-	 */
+	0x05750003,
+	0x057409A3,
+	/* Widget node 0x20 - 4 */
 	0x02050009,
-	0x0204E003,
+	0x02044003,
 	0x0205000A,
-	0x02047770,
-	/*
-	 * Widget node 0x20 - 9
-	 * To set LDO1/LDO2 as default (used for headset)
-	 */
-	0x02050008,
-	0x02046A0C,
-	0x02050008,
-	0x02046A0C,
+	0x02047778,
+	/* Widget node 0x20 - 5 */
+	0x02050037,
+	0x0204FE15,
+	0x02050030,
+	0x02041000,
 };
 
 const u32 pc_beep_verbs[] = {
 	/* Dos beep path - 1 */
-	0x01470C00,
+	0x02050010,
+	0x02040020,
 	0x02050036,
 	0x02047151,
-	0x01470740,
 	/* Dos beep path - 2 */
-	0x0143b000,
-	0x01470C02,
+	0x01470740,
+	0x0143B000,
 	0x01470C02,
 	0x01470C02,
 };

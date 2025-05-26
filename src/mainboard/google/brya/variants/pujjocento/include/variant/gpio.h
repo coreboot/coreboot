aea05e51a7c4ed7d0eadd103e3dcb8ca6493b9bf
Zhaoxiong Lv <lvzhaoxiong@huaqin.corp-partner.google.com>
2025-05-26 09:48:38 +0800

    mb/google/trulo/var/pujjocento: Enable WWAN function
    WWAN_FCPO == GPP_D6
    WWAN_RST == GPP_E17
    spec reuqest: 0 < toff <10ms
    
    LTE is controlled by bits 14 and 15 in fw_config, and P sensor
    and LTE modules exist at the same time, so we use the same bit
    to control whether to load the driver.
    
    BUG=b:419325064,b:417105553
    TEST=Confirm the measured WWAN power sequence
    
    Change-Id: Ia978aef2cc721b65618ac78c13930447d1557797
    Signed-off-by: Zhaoxiong Lv <lvzhaoxiong@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87841
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/pujjocento/include/variant/gpio.h
+++ src/mainboard/google/brya/variants/pujjocento/include/variant/gpio.h
@@ -5,7 +5,8 @@
 
 #include <baseboard/gpio.h>
 
+#define WWAN_FCPO	GPP_D6 /* FULL_CARD_POWER_OFF# */
 #define WWAN_RST	GPP_E17
-#define T2_OFF_MS	20
+#define T2_OFF_MS	5      /*spec request: 0 < t_off < 10ms*/
 #define SLP_S0_HDMI_EN	GPP_A7
 #endif

ab160ca301e430bce4f7109e8aff9015bb757855
Matt DeVillier <matt.devillier@gmail.com>
2025-05-27 10:48:40 -0500

    mb/google/byra/var/teliks: Add default VBT
    Extracted from coreboot-Google_Teliks.15217.734.0.bin
    
    Change-Id: Ib9f077cdd0536e6315b05775337bf30555562cc8
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87866
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>

--- src/mainboard/google/brya/variants/teliks/Makefile.mk
+++ src/mainboard/google/brya/variants/teliks/Makefile.mk
@@ -8,4 +8,5 @@ ramstage-y += gpio.c
 
 ramstage-y += variant.c
 
+$(call add_vbt_to_cbfs, vbt-teliks.bin, data-teliks.vbt)
 $(call add_vbt_to_cbfs, vbt-teliks_panel_11_inch.bin, data-teliks_panel_11_inch.vbt)

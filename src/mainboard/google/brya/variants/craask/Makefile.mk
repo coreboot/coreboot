58165618da7e97776656a05e1ae1877cd7d7745b
Matt DeVillier <matt.devillier@gmail.com>
2025-05-27 10:50:44 -0500

    mb/google/byra/var/craask: Add VBT for HDMI variant
    Extracted from coreboot-Google_Craask.15217-841-0.bin
    
    Change-Id: I07d4e7fe63ff6ad43806d73d9e31ffe0aa8807c5
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87867
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/craask/Makefile.mk
+++ src/mainboard/google/brya/variants/craask/Makefile.mk
@@ -6,3 +6,5 @@ romstage-y += gpio.c
 ramstage-$(CONFIG_FW_CONFIG) += fw_config.c
 ramstage-$(CONFIG_FW_CONFIG) += variant.c
 ramstage-y += gpio.c
+
+$(call add_vbt_to_cbfs, vbt-craask_hdmi.bin, data-craask_hdmi.vbt)

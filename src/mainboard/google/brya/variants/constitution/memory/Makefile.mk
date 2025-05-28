620c8d9f712b389f94993a24b2b7d013011bb883
Shon Wang <shon.wang@quanta.corp-partner.google.com>
2025-05-28 16:28:36 +0800

    mb/google/brask/var/constitution: Generate RAM ID for B3221XM3BDGVI
    Generate RAM ID for Kingston B3221XM3BDGVI
    
    DRAM Part Name                 ID to assign
    B3221XM3BDGVI                  2 (0010)
    
    BUG=b:420797833
    BRANCH=firmware-brya-14505.B
    TEST=emerge-constitution coreboot
    
    Change-Id: Ic4f92cb7d597a877b693906dee23e1b277fcf122
    Signed-off-by: Shon Wang <shon.wang@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/87883
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: David Wu <david_wu@quanta.corp-partner.google.com>

--- src/mainboard/google/brya/variants/constitution/memory/Makefile.mk
+++ src/mainboard/google/brya/variants/constitution/memory/Makefile.mk
@@ -6,3 +6,4 @@
 SPD_SOURCES =
 SPD_SOURCES += spd/lp4x/set-0/spd-1.hex      # ID = 0(0b0000)  Parts = K4U6E3S4AB-MGCL
 SPD_SOURCES += spd/lp4x/set-0/spd-3.hex      # ID = 1(0b0001)  Parts = MT53E1G32D2NP-046 WT:B, K4UBE3D4AB-MGCL, H54G56CYRBX247
+SPD_SOURCES += spd/lp4x/set-0/spd-10.hex      # ID = 2(0b0010)  Parts = B3221XM3BDGVI

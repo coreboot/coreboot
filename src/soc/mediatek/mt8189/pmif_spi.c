316f76635f0f216ccbf13788f1cbe1990ac3d4b5
Zhigang Qin <zhigang.qin@mediatek.corp-partner.google.com>
2025-05-30 11:27:48 +0800

    soc/mediatek/mt8189: Use pmif_spmi_v2 for MT8189
    MT8189 is equipped with the Power Mode Resource Collector (PMRC)
    feature, and the implementation to set PMIF to normal mode is the same
    as in pmif_spmi_v2. Use pmif_spmi_v2 to correct the configuration to
    allow PMIF to properly enter normal mode on MT8189.
    
    BUG=b:379008996
    BRANCH=none
    TEST=check the following logs for PMIC communication
    [DEBUG]  pmic_efuse_setting: Set efuses in 10 msecs
    [INFO ]  [RTC]rtc_boot,327: PMIC_RG_SCK_TOP_CON0,0x50c:0x1
    [INFO ]  [RTC]rtc_boot,330: PMIC_RG_SCK_TOP_CON0,0x50c:0x1
    [INFO ]  [RTC]rtc_enable_dcxo,66: con=0x486, osc32con=0xfe69, sec=0x0
    [INFO ]  [RTC]rtc_check_state,173: con=486, pwrkey1=a357, pwrkey2=67d2
    [INFO ]  [RTC]rtc_osc_init,62: osc32con val = 0xfe69
    [INFO ]  [RTC]rtc_eosc_cali,20: PMIC_RG_FQMTR_CKSEL=0x4a
    
    Signed-off-by: Zhigang Qin <zhigang.qin@mediatek.corp-partner.google.com>
    Change-Id: Ib3eeba7ca9bd446b641a17fbe97bcda373cb4a24
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88244
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- src/soc/mediatek/mt8189/pmif_spi.c
+++ src/soc/mediatek/mt8189/pmif_spi.c
@@ -4,6 +4,11 @@
 #include <gpio.h>
 #include <soc/pmif_spi.h>
 
+/* PMIF, SPI_MODE_CTRL */
+DEFINE_BIT(SPI_MODE_CTRL_PMIF_RDY, 9)
+DEFINE_BIT(SPI_MODE_CTRL_SRCLK_EN, 10)
+DEFINE_BIT(SPI_MODE_CTRL_SRVOL_EN, 11)
+
 /* PMIF, SLEEP_PROTECTION_CTRL */
 DEFINE_BITFIELD(SPM_SLEEP_REQ_SEL, 1, 0)
 DEFINE_BITFIELD(SCP_SLEEP_REQ_SEL, 10, 9)
@@ -19,6 +24,11 @@ void pmif_spi_iocfg(void)
 
 void pmif_spi_setting(struct pmif *arb)
 {
+	SET32_BITFIELDS(&arb->mtk_pmif->spi_mode_ctrl,
+			SPI_MODE_CTRL_SRCLK_EN, 0,
+			SPI_MODE_CTRL_SRVOL_EN, 0,
+			SPI_MODE_CTRL_PMIF_RDY, 1);
+
 	SET32_BITFIELDS(&arb->mtk_pmif->sleep_protection_ctrl,
 			SPM_SLEEP_REQ_SEL, 1,
 			SCP_SLEEP_REQ_SEL, 1);

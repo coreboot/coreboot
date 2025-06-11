d4a33638f5d000d450b69508ec09c972cb94a2ab
Luca Lai <luca.lai@lcfc.corp-partner.google.com>
2025-06-12 04:21:10 +0800

    mb/trulo/var/pujjolo: Change dram id table
    Alter dram from H9JCNNNCP3MLYR-N6E to H9JCNNNBK3MLYR-N6E
    
    BUG=b:395763555
    BRANCH=none
    TEST=Could boot to OS.
    
    Change-Id: Id2b024286b3699015be3a25341389db353d1ab8f
    Signed-off-by: Luca Lai <luca.lai@lcfc.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88061
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

--- src/mainboard/google/brya/variants/pujjolo/memory/Makefile.mk
+++ src/mainboard/google/brya/variants/pujjolo/memory/Makefile.mk
@@ -7,4 +7,4 @@ SPD_SOURCES =
 SPD_SOURCES += spd/lp5/set-0/spd-9.hex      # ID = 0(0b0000)  Parts = K3KL6L60GM-MGCT
 SPD_SOURCES += spd/lp5/set-0/spd-7.hex      # ID = 1(0b0001)  Parts = K3KL8L80CM-MGCT
 SPD_SOURCES += spd/lp5/set-0/spd-11.hex      # ID = 2(0b0010)  Parts = H58G56CK8BX146
-SPD_SOURCES += spd/lp5/set-0/spd-2.hex      # ID = 3(0b0011)  Parts = H9JCNNNCP3MLYR-N6E
+SPD_SOURCES += spd/lp5/set-0/spd-1.hex      # ID = 3(0b0011)  Parts = H9JCNNNBK3MLYR-N6E

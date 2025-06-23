8e5bdde028c39c40aa1cd41f1b82d6621a8d1286
Bora Guvendik <bora.guvendik@intel.corp-partner.google.com>
2025-06-23 14:04:32 -0700

    mb/intel/ptlrvp: Add support for H58G66BK7BX067 memory
    This commit introduces support for the H58G66BK7BX067 memory module
    within the Intel PTLRVP mainboard variant. The changes include updates
    to several configuration files to recognize and utilize this specific
    memory module. The SPD source files and DRAM ID assignments have been
    updated accordingly to integrate the new memory module into the build
    system. This addition ensures compatibility and functionality with
    the H58G66BK7BX067 memory module, allowing for expanded hardware
    support and flexibility in memory configurations.
    
    BUG=none
    TEST=Build and verify memory module detection and initialization
    on supported hardware configuration.
    
    Signed-off-by: Bora Guvendik <bora.guvendik@intel.com>
    Change-Id: Ic281125dd40bbcef50d138e912e7557d6552eb9f
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88177
    Reviewed-by: Jérémy Compostella <jeremy.compostella@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/intel/ptlrvp/variants/ptlrvp/memory/Makefile.mk
+++ src/mainboard/intel/ptlrvp/variants/ptlrvp/memory/Makefile.mk
@@ -7,3 +7,4 @@ SPD_SOURCES =
 SPD_SOURCES += spd/lp5/set-0/spd-7.hex      # ID = 0(0b0000)  Parts = H58G56BK7BX068
 SPD_SOURCES += spd/lp5/set-0/spd-10.hex      # ID = 1(0b0001)  Parts = H58G66BK8BX067
 SPD_SOURCES += spd/lp5/set-0/spd-11.hex      # ID = 2(0b0010)  Parts = H58G56BK8BX068
+SPD_SOURCES += spd/lp5/set-0/spd-8.hex      # ID = 3(0b0011)  Parts = H58G66BK7BX067

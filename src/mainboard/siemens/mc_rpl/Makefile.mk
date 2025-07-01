dcbe591201ad92ab3a7c2e3173a4dc19b4247528
Werner Zeh <werner.zeh@siemens.com>
2025-07-01 08:38:32 +0200

    mb/siemens/mc_rpl: Use SPD data from HWInfo instead of from CBFS
    Siemens boards store their memory configuration data (SPD) in a field
    inside the HWInfo block (which itself is located in CBFS). This patch
    removes all *spd.hex-files and uses HWInfo instead for the SPD data
    source. In addition, the memory data swizzling is updated to reflect
    the board wiring so that DRAM can work properly.
    
    Change-Id: I63d6e7c4543b7d99a4b1815c8ee81efcb6a87b94
    Signed-off-by: Werner Zeh <werner.zeh@siemens.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88259
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Mario Scheithauer <mario.scheithauer@siemens.com>

--- src/mainboard/siemens/mc_rpl/Makefile.mk
+++ src/mainboard/siemens/mc_rpl/Makefile.mk
@@ -1,15 +1,10 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
-subdirs-y += spd
-
 bootblock-y += bootblock.c
 
 romstage-y += romstage_fsp_params.c
 romstage-y += board_id.c
 romstage-y += memory.c
-ifeq ($(CONFIG_BOARD_INTEL_ADLRVP_RPL_EXT_EC),y)
-romstage-y += memory_rpl.c
-endif
 
 ramstage-y += mainboard.c
 ramstage-y += board_id.c
--- src/mainboard/siemens/mc_rpl/romstage_fsp_params.c
+++ src/mainboard/siemens/mc_rpl/romstage_fsp_params.c
@@ -2,26 +2,11 @@
 
 #include <console/console.h>
 #include <fsp/api.h>
+#include <hwilib.h>
 #include <soc/romstage.h>
 #include <spd_bin.h>
 #include <soc/meminit.h>
 #include <baseboard/variants.h>
-#include "board_id.h"
-
-#define SPD_ID_MASK 0x7
-
-static size_t get_spd_index(void)
-{
-	uint8_t board_id = get_board_id();
-	size_t spd_index;
-
-	printk(BIOS_INFO, "board id is 0x%x\n", board_id);
-
-	spd_index = board_id & SPD_ID_MASK;
-
-	printk(BIOS_INFO, "SPD index is 0x%x\n", (unsigned int)spd_index);
-	return spd_index;
-}
 
 /*
  * ADL-P silicon can support 7 SRC CLK's and 10 CLKREQ signals. Out of 7 SRCCLK's
@@ -49,7 +34,22 @@ void mainboard_memory_init_params(FSPM_UPD *memupd)
 {
 	FSP_M_CONFIG *m_cfg = &memupd->FspmConfig;
 	const struct mb_cfg *mem_config = variant_memory_params();
-	int board_id = get_board_id();
+
+	static struct mem_spd spd_info;
+	static uint8_t spd_data[CONFIG_DIMM_SPD_SIZE];
+	const char *cbfs_hwi_name = "hwinfo.hex";
+
+	/* Initialize SPD information for LPDDR5 from HW-Info. */
+	memset(spd_data, 0, sizeof(spd_data));
+	if ((hwilib_find_blocks(cbfs_hwi_name) == CB_SUCCESS) &&
+	    (hwilib_get_field(SPD, spd_data, 0x80) == 0x80)) {
+		spd_info.topo = MEM_TOPO_MEMORY_DOWN;
+		spd_info.spd_data.in_mem = true;
+		spd_info.spd_data.ptr = (uintptr_t)spd_data;
+		spd_info.spd_data.len = CONFIG_DIMM_SPD_SIZE;
+	} else {
+		die("No SPD in HW-Info found!\n");
+	}
 
 	/*
 	 * Set Raptor Lake specific new upds that Alder Lake doesn't have.
@@ -57,55 +57,7 @@ void mainboard_memory_init_params(FSPM_UPD *memupd)
 	 */
 	rpl_memory_params(memupd);
 
-	/*
-	 * Alder Lake common meminit block driver considers bus width to be 128-bit and
-	 * populates the meminit data accordingly. Alder Lake-N has single memory controller
-	 * with 64-bit bus width. By setting half_populated to true, only the bottom half is
-	 * populated.
-	 * TODO: Implement __weak variant_is_half_populated(void) function.
-	 */
-	const bool half_populated = (CONFIG(BOARD_INTEL_ADLRVP_N_EXT_EC)
-			 || CONFIG(BOARD_INTEL_ADLRVP_N));
-
-	const struct mem_spd memory_down_spd_info = {
-		.topo = MEM_TOPO_MEMORY_DOWN,
-		.cbfs_index = get_spd_index(),
-	};
-
-	const struct mem_spd dimm_module_spd_info = {
-		.topo = MEM_TOPO_DIMM_MODULE,
-		.smbus = {
-			[0] = {
-				.addr_dimm[0] = 0x50,
-				.addr_dimm[1] = 0x51,
-			},
-			[1] = {
-				.addr_dimm[0] = 0x52,
-				.addr_dimm[1] = 0x53,
-			},
-		},
-	};
-
-	switch (board_id) {
-	case ADL_P_DDR4_1:
-	case ADL_P_DDR4_2:
-	case ADL_P_DDR5_1:
-		memcfg_init(memupd, mem_config, &dimm_module_spd_info, half_populated);
-		break;
-	case ADL_P_DDR5_2:
-	case ADL_P_LP4_1:
-	case ADL_P_LP4_2:
-	case ADL_P_LP5_1:
-	case ADL_P_LP5_2:
-	case ADL_M_LP4:
-	case ADL_M_LP5:
-	case ADL_N_LP5:
-		memcfg_init(memupd, mem_config, &memory_down_spd_info, half_populated);
-		break;
-	default:
-		die("Unknown board id = 0x%x\n", board_id);
-		break;
-	}
+	memcfg_init(memupd, mem_config, &spd_info, 0);
 
 	if (CONFIG(GEN3_EXTERNAL_CLOCK_BUFFER))
 		configure_external_clksrc(m_cfg);

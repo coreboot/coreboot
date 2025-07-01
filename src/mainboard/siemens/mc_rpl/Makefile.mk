b3d7c40fb50222ac2c5d4b349d3fd76fbfd54dd2
Werner Zeh <werner.zeh@siemens.com>
2025-07-01 12:02:57 +0200

    mb/siemens/mc_rpl: Remove code for board_id
    This board does not use the board_id-feature hence the code can be
    removed.
    
    Change-Id: I18c67580d4611b4c53248315937277bed53bd1ea
    Signed-off-by: Werner Zeh <werner.zeh@siemens.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88263
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Mario Scheithauer <mario.scheithauer@siemens.com>

--- src/mainboard/siemens/mc_rpl/Makefile.mk
+++ src/mainboard/siemens/mc_rpl/Makefile.mk
@@ -3,11 +3,9 @@
 bootblock-y += bootblock.c
 
 romstage-y += romstage_fsp_params.c
-romstage-y += board_id.c
 romstage-y += memory.c
 
 ramstage-y += mainboard.c
-ramstage-y += board_id.c
 ramstage-y += ramstage.c
 
 CPPFLAGS_common += -I$(src)/mainboard/$(MAINBOARDDIR)/include

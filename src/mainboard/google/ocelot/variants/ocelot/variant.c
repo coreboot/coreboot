b66c8ea3d38872e0e2c9eb416239550da862ebb5
Sowmya Aralguppe <sowmya.aralguppe@intel.com>
2025-07-17 06:52:41 +0530

    mb/google/ocelot/var/ocelot: Remove Bluetooth Audio offload
    Remove Bluetooth Audio offload to align to fw_config.c
    
    Platform Mapping Document : Rev0p86
    
    BUG=b:394208231
    TEST=Build Ocelot and verify it compiles without any error.
    
    Change-Id: I30edbc0a5622e8893469384b853cad323c6ac544
    Signed-off-by: Sowmya Aralguppe <sowmya.aralguppe@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88460
    Reviewed-by: Pranava Y N <pranavayn@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/ocelot/variants/ocelot/variant.c
+++ src/mainboard/google/ocelot/variants/ocelot/variant.c
@@ -20,11 +20,6 @@ void variant_update_soc_chip_config(struct soc_intel_pantherlake_config *config)
 			fw_config_probe(FW_CONFIG(WIFI, WIFI_CNVI_7))) {
 		config->cnvi_wifi_core = true;
 		config->cnvi_bt_core = true;
-
-		if (fw_config_probe(FW_CONFIG(AUDIO, AUDIO_ALC721_SNDW))) {
-			printk(BIOS_INFO, "BT audio offload configured.\n");
-			config->cnvi_bt_audio_offload = true;
-		}
 	}
 
     /* Touchscreen and touchpad WOT support:

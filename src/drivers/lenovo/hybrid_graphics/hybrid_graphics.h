6a20caea01c5c72a4e57217990ac6aacdc6b7724
Patrick Rudolph <patrick.rudolph@9elements.com>
2025-07-23 18:07:52 +0200

    drivers/lenovo/hybrid_graphics: Add missing header
    Include the missing header stdbool.h
    
    Fixes compilation error
    hybrid_graphics.h:6:28: error: unknown type name 'bool'
    while refactoring some code.
    
    Change-Id: I96f7255049cd9e24d74838a173e920a00636b09e
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88546
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/drivers/lenovo/hybrid_graphics/hybrid_graphics.h
+++ src/drivers/lenovo/hybrid_graphics/hybrid_graphics.h
@@ -3,6 +3,8 @@
 #ifndef _DRIVERS_LENOVO_HYBRID_GRAPHICS_H_
 #define _DRIVERS_LENOVO_HYBRID_GRAPHICS_H_
 
+#include <stdbool.h>
+
 void early_hybrid_graphics(bool *enable_igd, bool *enable_peg);
 
 #endif /* _DRIVERS_LENOVO_HYBRID_GRAPHICS_CHIP_H_ */

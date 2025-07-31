32b944b77a3b8da254a7e7b63d1482f8526335d9
Tony Huang <tony-huang@quanta.corp-partner.google.com>
2025-07-31 17:13:18 +0800

    mb/google/brox/var/caboc: Update hda_verb table
    Update hda_verb table from vendor.
    
    BUG=b:435345756
    TEST=emerge-brox coreboot
         check system audio output is fine
    
    Change-Id: Id46d1798b605e7d1fbdfacf2e1899bfc40a113a6
    Signed-off-by: Tony Huang <tony-huang@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88627
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

--- src/mainboard/google/brox/variants/caboc/include/variant/hda_verb.h
+++ src/mainboard/google/brox/variants/caboc/include/variant/hda_verb.h
@@ -17,14 +17,25 @@ const u32 cim_verb_data[] = {
 
 	/* Pin Widget Verb Table */
 
+	/* 1 bit reset */
+	0x0205001A,
+	0x0204C003,
+	0x0205001A,
+	0x02048003,
+	0x0205007F,
+	0x02046001,
+	0x0205007F,
+	0x02040001,
+
 	/*
 	 * DMIC
-	 * Requirement is to use PCH DMIC. Hence,
-	 * commented out codec's Internal DMIC.
-	 * AZALIA_PIN_CFG(0, 0x12, 0x90A60130),
-	 * AZALIA_PIN_CFG(0, 0x13, 0x40000000),
+	 * Requirement is to use PCH DMIC.
+	 * Vendor suggested to follow verb table to config NID 0x12, NID 0x13.
 	 */
-
+	/* Pin widget 0x12 - DMIC */
+	AZALIA_PIN_CFG(0, 0x12, 0x411111F0),
+	/* Pin widget 0x13 - DMIC */
+	AZALIA_PIN_CFG(0, 0x13, 0x40000000),
 	/* Pin widget 0x14 - Front (Port-D) */
 	AZALIA_PIN_CFG(0, 0x14, 0x90170110),
 	/* Pin widget 0x18 - NPC */
@@ -66,15 +77,35 @@ const u32 cim_verb_data[] = {
 	0x057409A3,
 	/* disable EQ first */
 	0x05350000,
-	0x0534201A,
+	0x0534203A,
 	/* Left Channel */
 	0x0535001d,
 	0x05340800,
 	0x0535001e,
 	0x05340800,
 	0x05350003,
-	0x05341F7B,
+	0x05341F95,
 	0x05350004,
+	0x05340000,
+	0x05350005,
+	0x053403F6,
+	0x05350006,
+	0x0534854C,
+	0x05350007,
+	0x05341E09,
+	0x05350008,
+	0x05346472,
+	0x05350009,
+	0x053401FB,
+	0x0535000A,
+	0x05344836,
+	0x0535000B,
+	0x05341C00,
+	0x0535000C,
+	0x05340000,
+	0x0535000D,
+	0x05340200,
+	0x0535000E,
 	0x05340000,
 	/* Right Channel */
 	0x05450000,
@@ -84,15 +115,35 @@ const u32 cim_verb_data[] = {
 	0x0545001e,
 	0x05440800,
 	0x05450003,
-	0x05441F7B,
+	0x05441F95,
 	0x05450004,
+	0x05440000,
+	0x05450005,
+	0x054403F6,
+	0x05450006,
+	0x0544854C,
+	0x05450007,
+	0x05441E09,
+	0x05450008,
+	0x05446472,
+	0x05450009,
+	0x054401FB,
+	0x0545000A,
+	0x05444836,
+	0x0545000B,
+	0x05441C00,
+	0x0545000C,
+	0x05440000,
+	0x0545000D,
+	0x05440200,
+	0x0545000E,
 	0x05440000,
 	/* enable EQ */
 	0x05350000,
-	0x0534E01A,
-	/* 1.8W/4ohm */
+	0x0534E03A,
+	/* 2.2W/4ohm */
 	0x02050038,
-	0x02047901,
+	0x02046901,
 	/* AGC Enable */
 	0x0205004C,
 	0x0204465C,

b74d2b77d2d6468f602f0ebae7a6b9ab614cff5f
Eren Peng <peng.eren@inventec.corp-partner.google.com>
2025-08-07 10:00:59 +0800

    mb/google/trulo/var/kaladin: Add WIFI SAR table
    Add WIFI SAR table for intel WIFI SAR table
    
    BUG=b:434861866
    TEST=Build and flash to DUT, check that SAR table is loaded by cbmem -c | grep SAR
    
    Change-Id: Idf48254ec43535ff51859ecbec3ea75b7c35e70c
    Signed-off-by: Eren Peng <peng.eren@inventec.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88702
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/mainboard/google/brya/variants/kaladin/Makefile.mk
+++ src/mainboard/google/brya/variants/kaladin/Makefile.mk
@@ -4,5 +4,6 @@ bootblock-y += gpio.c
 romstage-y += gpio.c
 romstage-y += memory.c
 ramstage-y += gpio.c
+ramstage-y += variant.c
 ramstage-$(CONFIG_SOC_INTEL_COMMON_BLOCK_HDA_VERB) += hda_verb.c
 ramstage-$(CONFIG_FW_CONFIG) += fw_config.c

c36b14939298dff0623d7e1db88d35acffc217e9
Matt DeVillier <matt.devillier@gmail.com>
2025-08-31 17:29:13 -0500

    mb/google/eve/Makefile: Organize and group entries by stage
    Tidy up before adding a new CFR entry in a subsequent patch.
    
    Change-Id: I14dc011622acad43b6a21a00d97a958ea6d3152f
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90320
    Reviewed-by: Jason Glenesk <jason.glenesk@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/eve/Makefile.mk
+++ src/mainboard/google/eve/Makefile.mk
@@ -1,15 +1,15 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
-subdirs-y += spd
-
 bootblock-y += bootblock.c
-
 bootblock-$(CONFIG_CHROMEOS) += chromeos.c
+
 verstage-$(CONFIG_CHROMEOS) += chromeos.c
+
 romstage-$(CONFIG_CHROMEOS) += chromeos.c
+
 ramstage-$(CONFIG_CHROMEOS) += chromeos.c
-
-ramstage-y += mainboard.c
 ramstage-$(CONFIG_EC_GOOGLE_CHROMEEC) += ec.c
-
 ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads
+ramstage-y += mainboard.c
+
+subdirs-y += spd

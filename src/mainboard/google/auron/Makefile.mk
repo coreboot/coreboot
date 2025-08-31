7ed515d1c3a264c3d3fc79683ab0c59f0705c467
Matt DeVillier <matt.devillier@gmail.com>
2025-08-31 15:40:55 -0500

    mb/google/auron/Makefile: Organize and group entries by stage
    Tidy up before adding a new entry.
    
    Change-Id: I33b0b4cf99534eb9dbc28d43286656488d1f498c
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90164
    Reviewed-by: Sean Rhodes <sean@starlabs.systems>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/auron/Makefile.mk
+++ src/mainboard/google/auron/Makefile.mk
@@ -1,26 +1,22 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
-ramstage-$(CONFIG_EC_GOOGLE_CHROMEEC) += ec.c
-
-romstage-$(CONFIG_CHROMEOS) += chromeos.c
-ramstage-$(CONFIG_CHROMEOS) += chromeos.c
 bootblock-$(CONFIG_CHROMEOS) += chromeos.c
 
-romstage-$(CONFIG_HAVE_SPD_IN_CBFS) += spd.c
-
+romstage-$(CONFIG_CHROMEOS) += chromeos.c
+romstage-y += variants/$(VARIANT_DIR)/gpio.c
 romstage-y += variants/$(VARIANT_DIR)/pei_data.c
-ramstage-y += variants/$(VARIANT_DIR)/pei_data.c
-
+romstage-$(CONFIG_HAVE_SPD_IN_CBFS) += spd.c
 ifneq ($(CONFIG_BOARD_GOOGLE_BUDDY),y)
 romstage-y += variants/$(VARIANT_DIR)/variant.c
 endif
+
+ramstage-$(CONFIG_CHROMEOS) += chromeos.c
+ramstage-$(CONFIG_EC_GOOGLE_CHROMEEC) += ec.c
+ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads
+ramstage-y += variants/$(VARIANT_DIR)/pei_data.c
 ramstage-y += variants/$(VARIANT_DIR)/variant.c
 
 subdirs-y += variants/$(VARIANT_DIR)
 subdirs-y += variants/$(VARIANT_DIR)/spd
 
 CPPFLAGS_common += -I$(src)/mainboard/$(MAINBOARDDIR)/variants/$(VARIANT_DIR)/include
-
-romstage-y += variants/$(VARIANT_DIR)/gpio.c
-
-ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads

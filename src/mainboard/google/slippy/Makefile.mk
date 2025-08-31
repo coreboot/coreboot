e366e0ba7d2caf986bb7a80959373ba5717bd385
Matt DeVillier <matt.devillier@gmail.com>
2025-08-31 15:32:11 -0500

    mb/google/slippy/Makefile: Organize and group entries by stage
    Tidy up before adding a new entry.
    
    Change-Id: Ib37c9b4b73819b1309a7c2405830f1524e3d3f74
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90161
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Sean Rhodes <sean@starlabs.systems>

--- src/mainboard/google/slippy/Makefile.mk
+++ src/mainboard/google/slippy/Makefile.mk
@@ -1,15 +1,13 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
-ramstage-$(CONFIG_EC_GOOGLE_CHROMEEC) += ec.c
-
 romstage-$(CONFIG_CHROMEOS) += chromeos.c
-ramstage-$(CONFIG_CHROMEOS) += chromeos.c
-
 romstage-y += variants/$(VARIANT_DIR)/gpio.c
 romstage-y += variants/$(VARIANT_DIR)/romstage.c
 
-subdirs-y += variants/$(VARIANT_DIR)
-
+ramstage-$(CONFIG_CHROMEOS) += chromeos.c
+ramstage-$(CONFIG_EC_GOOGLE_CHROMEEC) += ec.c
 ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads
+
+subdirs-y += variants/$(VARIANT_DIR)
 
 CPPFLAGS_common += -I$(src)/mainboard/$(MAINBOARDDIR)/variants/$(VARIANT_DIR)/include

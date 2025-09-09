18ae0c48e110117270b043c25127bf6f178e205c
Ren Kuo <ren.kuo@quanta.corp-partner.google.com>
2025-09-09 19:06:19 +0800

    mb/google/fatcat/var/moonstone: Support new schematic changes
    Add FW_config support to distinguish schematic changes.
    Refer to schamtics MB_V20250826 and DB_V20250821
    
    BUG=b:442964982
    TEST=emerge-fatcat coreboot
    
    Change-Id: I0dd354cb1512521474a929bf4d1cfc786eb0a33c
    Signed-off-by: Ren Kuo <ren.kuo@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/89128
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/fatcat/variants/moonstone/Makefile.mk
+++ src/mainboard/google/fatcat/variants/moonstone/Makefile.mk
@@ -3,5 +3,7 @@
 bootblock-y += gpio.c
 romstage-y += gpio.c
 romstage-y += memory.c
+romstage-$(CONFIG_FW_CONFIG) += fw_config.c
 ramstage-y += gpio.c
 ramstage-$(CONFIG_FW_CONFIG) += variant.c
+ramstage-$(CONFIG_FW_CONFIG) += fw_config.c

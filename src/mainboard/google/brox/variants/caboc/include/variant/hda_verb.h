751afeb06099ce353ef5d4d8a19b7625049ecda3
Tony Huang <tony-huang@quanta.corp-partner.google.com>
2025-09-10 09:10:43 +0800

    mb/google/brox/var/caboc: Update HDA verb table
    Table is from vendor. Update HWEQ and AGC setting.
    
    BUG=b:435345756
    TEST=emerge-brox coreboot
         check system audio output is fine
    
    Change-Id: I0869a4902e38e8010274769de7f8e7b9a4160aae
    Signed-off-by: Tony Huang <tony-huang@quanta.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/89127
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Reviewed-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>

--- src/mainboard/google/brox/variants/caboc/include/variant/hda_verb.h
+++ src/mainboard/google/brox/variants/caboc/include/variant/hda_verb.h
@@ -9,7 +9,7 @@ const u32 cim_verb_data[] = {
 	/* coreboot specific header */
 	0x10ec0236,	// Codec Vendor / Device ID: Realtek ALC236
 	0x103C8C60,	// Subsystem ID
-	0x00000023,	// Number of jacks (NID entries)
+	0x0000001A,	// Number of jacks (NID entries)
 
 	AZALIA_RESET(0x1),
 	/* NID 0x01, HDA Codec Subsystem ID Verb table */
@@ -77,35 +77,15 @@ const u32 cim_verb_data[] = {
 	0x057409A3,
 	/* disable EQ first */
 	0x05350000,
-	0x0534203A,
+	0x0534201A,
 	/* Left Channel */
 	0x0535001d,
 	0x05340800,
 	0x0535001e,
 	0x05340800,
 	0x05350003,
-	0x05341F95,
+	0x05341EC4,
 	0x05350004,
-	0x05340000,
-	0x05350005,
-	0x053403F6,
-	0x05350006,
-	0x0534854C,
-	0x05350007,
-	0x05341E09,
-	0x05350008,
-	0x05346472,
-	0x05350009,
-	0x053401FB,
-	0x0535000A,
-	0x05344836,
-	0x0535000B,
-	0x05341C00,
-	0x0535000C,
-	0x05340000,
-	0x0535000D,
-	0x05340200,
-	0x0535000E,
 	0x05340000,
 	/* Right Channel */
 	0x05450000,
@@ -115,45 +95,30 @@ const u32 cim_verb_data[] = {
 	0x0545001e,
 	0x05440800,
 	0x05450003,
-	0x05441F95,
+	0x05441EC4,
 	0x05450004,
-	0x05440000,
-	0x05450005,
-	0x054403F6,
-	0x05450006,
-	0x0544854C,
-	0x05450007,
-	0x05441E09,
-	0x05450008,
-	0x05446472,
-	0x05450009,
-	0x054401FB,
-	0x0545000A,
-	0x05444836,
-	0x0545000B,
-	0x05441C00,
-	0x0545000C,
-	0x05440000,
-	0x0545000D,
-	0x05440200,
-	0x0545000E,
 	0x05440000,
 	/* enable EQ */
 	0x05350000,
-	0x0534E03A,
+	0x0534E01A,
 	/* 2.2W/4ohm */
 	0x02050038,
 	0x02046901,
-	/* AGC Enable */
-	0x0205004C,
-	0x0204465C,
+	/* Disable AGC and set AGC limit to 0dB  */
+	0x02050016,
+	0x02044C50,
+	0x02050012,
+	0x0204EBC0,
+	/* Set AGC Front and Post gain 0dB then Enable AGC */
+	0x02050013,
+	0x0204401F,
 	0x02050016,
 	0x02044E50,
-	0x02050020,
-	0x020451FF,
 	/* Headphone Pop */
 	0x05750007,
 	0x057412B2,
+	0x05750007,	/* Padding */
+	0x057412B2,	/* Padding */
 };
 
 const u32 pc_beep_verbs[] = {
@@ -161,7 +126,7 @@ const u32 pc_beep_verbs[] = {
 	0x02050036,
 	0x02047151,
 	0x02050010,
-	0x02040020,
+	0x02040320,
 	/* Dos beep path - 2 */
 	0x0143B000,
 	0x01470740,

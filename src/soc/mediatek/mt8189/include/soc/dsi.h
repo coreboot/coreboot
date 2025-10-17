979fdee1d9be2ff3d52e94a2cce607ec23b873f7
Vince Liu <vince-wl.liu@mediatek.corp-partner.google.com>
2025-10-17 13:33:13 +0800

    soc/mediatek/mt8189: Support MIPI C-PHY interface
    Add config `MEDIATEK_DSI_CPHY` to enable the MIPI C-PHY interface on
    mt8189, including necessary register definitions and integrating with
    the common MIPI driver, dsi_register_v2.
    
    BUG=b:433422905,b:428854543
    TEST=Check display initialization log on padme
    mtk_display_init: 'TM TL121BVMS07' 1600x2560@60Hz
    
    Signed-off-by: Vince Liu <vince-wl.liu@mediatek.corp-partner.google.com>
    Change-Id: Iac6c1b6d47331b63e7b45157bd60da93f104b0ee
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/89620
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>

--- src/soc/mediatek/mt8189/include/soc/dsi.h
+++ src/soc/mediatek/mt8189/include/soc/dsi.h
@@ -4,5 +4,59 @@
 #define SOC_MEDIATEK_MT8189_DSI_H
 
 #include <soc/dsi_common.h>
+#include <soc/dsi_register_v2.h>
+#include <types.h>
+
+/* DSI features */
+#define MTK_DSI_MIPI_RATIO_NUMERATOR 100
+#define MTK_DSI_MIPI_RATIO_DENOMINATOR 100
+#define MTK_DSI_DATA_RATE_MIN_MHZ 125
+#define MTK_DSI_HAVE_SIZE_CON 1
+#define PIXEL_STREAM_CUSTOM_HEADER 0xb
+
+/* MIPITX is SOC specific and cannot live in common. */
+
+/* MIPITX_REG */
+struct mipi_tx_regs {
+	u32 reserved0[3];
+	u32 lane_con;
+	u32 voltage_sel;
+	u32 reserved1[5];
+	u32 pll_pwr;
+	u32 pll_con0;
+	u32 pll_con1;
+	u32 pll_con2;
+	u32 pll_con3;
+	u32 pll_con4;
+	u32 phy_sel[4];
+	u32 reserved2[61];
+	u32 d2_sw_ctl_en;
+	u32 reserved3[63];
+	u32 d0_sw_ctl_en;
+	u32 reserved4[56];
+	u32 ck_ckmode_en;
+	u32 reserved5[6];
+	u32 ck_sw_ctl_en;
+	u32 reserved6[63];
+	u32 d1_sw_ctl_en;
+	u32 reserved7[63];
+	u32 d3_sw_ctl_en;
+};
+
+check_member(mipi_tx_regs, voltage_sel, 0x10);
+check_member(mipi_tx_regs, pll_con4, 0x3C);
+check_member(mipi_tx_regs, phy_sel[0], 0x40);
+check_member(mipi_tx_regs, phy_sel[3], 0x4C);
+check_member(mipi_tx_regs, d3_sw_ctl_en, 0x544);
+static struct mipi_tx_regs *const mipi_tx = (void *)MIPITX0_BASE;
+
+/* Register values */
+#define DSI_CK_CKMODE_EN	BIT(0)
+#define DSI_SW_CTL_EN		BIT(0)
+#define AD_DSI_PLL_SDM_PWR_ON	BIT(0)
+#define AD_DSI_PLL_SDM_ISO_EN	BIT(1)
+
+#define RG_DSI_PLL_EN		BIT(4)
+#define RG_DSI_PLL_POSDIV	(0x7 << 8)
 
 #endif

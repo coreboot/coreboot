ee599486ac4a2aed195a7c53f8eec2832359110d
Matt DeVillier <matt.devillier@gmail.com>
2025-10-19 16:13:41 -0500

    mb/google/dedede/galtic: Add CFR option for touchpad type
    The i2c auto-detection cannot distinguish between two touchpads using
    the same i2c bus and address, so drop the 'detect' flag amd implement
    a user-selectable CFR option instead, defaulting to both enabled.
    This will allow either touchpad to work properly under Linux, and
    give the user the ability to only enable the touchpad actually present
    should they wish to run Windows.
    
    Change-Id: Iaf1406c2d10bbf10b9aea30ae1cd2b2281bfcc5d
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90350
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Jason Glenesk <jason.glenesk@gmail.com>

--- src/mainboard/google/dedede/variants/galtic/variant.c
+++ src/mainboard/google/dedede/variants/galtic/variant.c
@@ -1,8 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <baseboard/variants.h>
+#include <device/device.h>
 #include <ec/google/chromeec/ec.h>
 #include <fw_config.h>
+#include <option.h>
 #include <sar.h>
+#include <static.h>
 
 enum {
 	GALTIC_SKU_START    = 0x120000,
@@ -36,4 +40,24 @@ const char *get_wifi_sar_cbfs_filename(void)
 		return "wifi_sar-galnat360.hex";
 
 	return WIFI_SAR_CBFS_DEFAULT_FILENAME;
+}
+
+#define TP_TYPE_AUTO_SELECT 0
+#define TP_TYPE_ELAN0000 1
+#define TP_TYPE_ELAN2702 2
+
+void variant_devtree_update(void)
+{
+	struct device *tp_elan0000 = DEV_PTR(elan0000);
+	struct device *tp_elan2702 = DEV_PTR(elan2702);
+
+	/* Update touchpad device */
+	switch (get_uint_option("touchpad", TP_TYPE_AUTO_SELECT)) {
+	case TP_TYPE_ELAN0000:
+		tp_elan2702->enabled = 0;
+		break;
+	case TP_TYPE_ELAN2702:
+		tp_elan0000->enabled = 0;
+		break;
+	}
 }

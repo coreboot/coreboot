e82ecc739d5f3bb614dafe8195a8502e13f8bb8a
Keith Hui <buurin@gmail.com>
2025-10-24 16:38:29 -0400

    sio/nuvoton/nct6776: Switch to common init code
    TEST=abuild
    
    Change-Id: I10ced926e8844a54fe2c6e892f10459689bbd07b
    Signed-off-by: Keith Hui <buurin@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/89745
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/superio/nuvoton/nct6776/superio.c
+++ src/superio/nuvoton/nct6776/superio.c
@@ -2,34 +2,11 @@
 
 #include <device/device.h>
 #include <device/pnp.h>
-#include <pc80/keyboard.h>
-#include <superio/conf_mode.h>
+#include <superio/nuvoton/common/nuvoton.h>
 
 #include "nct6776.h"
 
 /* Both NCT6776D and NCT6776F package variants are supported. */
-
-static void nct6776_init(struct device *dev)
-{
-	if (!dev->enabled)
-		return;
-
-	switch (dev->path.pnp.device) {
-	/* TODO: Might potentially need code for HWM or FDC etc. */
-	case NCT6776_KBC:
-		pc_keyboard_init(NO_AUX_DEVICE);
-		break;
-	}
-}
-
-static struct device_operations ops = {
-	.read_resources   = pnp_read_resources,
-	.set_resources    = pnp_set_resources,
-	.enable_resources = pnp_enable_resources,
-	.enable           = pnp_alt_enable,
-	.init             = nct6776_init,
-	.ops_pnp_mode     = &pnp_conf_mode_8787_aa,
-};
 
 static struct pnp_info pnp_dev_info[] = {
 	{ NULL, NCT6776_FDC, PNP_IO0 | PNP_IRQ0 | PNP_DRQ0, 0x0ff8 },
@@ -63,7 +40,7 @@ static struct pnp_info pnp_dev_info[] = {
 
 static void enable_dev(struct device *dev)
 {
-	pnp_enable_devices(dev, &ops, ARRAY_SIZE(pnp_dev_info), pnp_dev_info);
+	pnp_enable_devices(dev, &nuvoton_common_ops, ARRAY_SIZE(pnp_dev_info), pnp_dev_info);
 }
 
 struct chip_operations superio_nuvoton_nct6776_ops = {

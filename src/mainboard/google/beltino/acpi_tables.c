76a48b51581a3fede769947a1b7bf48389e58021
Matt DeVillier <matt.devillier@gmail.com>
2025-10-30 17:20:27 -0500

    mb/google/beltino: Remove unused GNVS fan configuration
    Remove unused GNVS fan thresholds, PWM assignments from acpi_tables.c.
    
    The Beltino thermal.asl uses compile-time macros (FAN*_THRESHOLD_OFF,
    FAN*_THRESHOLD_ON, FAN*_PWM) directly from variant/thermal.h, not
    runtime GNVS variables. The GNVS assignments were never consumed by
    the ACPI code and just wasted memory.
    
    Retained GNVS values that ARE used:
    - tpmp: TPM presence flag
    - tcrt: Critical temperature (\TCRT)
    - tpsv: Passive temperature (\TPSV)
    - tmax: Tj_max (\TMAX)
    
    Removed unused GNVS values:
    - f{0-4}{of,on,pw}: Fan thresholds and PWM values (24 values total)
    - flvl: Fan level (unused, ACPI uses \FLVL local variable)
    
    This matches the google/jecht approach which also uses compile-time
    macros and only sets the essential GNVS thermal values.
    
    No functional change - the ACPI thermal zone behavior is identical.
    
    Change-Id: I703b7d8e424d4451abf0781b4491b813be216bc7
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/89844
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Sean Rhodes <sean@starlabs.systems>

--- src/mainboard/google/beltino/acpi_tables.c
+++ src/mainboard/google/beltino/acpi_tables.c
@@ -11,28 +11,7 @@ void mainboard_fill_gnvs(struct global_nvs *gnvs)
 	/* TPM Present */
 	gnvs->tpmp = 1;
 
-	gnvs->f4of = FAN4_THRESHOLD_OFF;
-	gnvs->f4on = FAN4_THRESHOLD_ON;
-	gnvs->f4pw = FAN4_PWM;
-
-	gnvs->f3of = FAN3_THRESHOLD_OFF;
-	gnvs->f3on = FAN3_THRESHOLD_ON;
-	gnvs->f3pw = FAN3_PWM;
-
-	gnvs->f2of = FAN2_THRESHOLD_OFF;
-	gnvs->f2on = FAN2_THRESHOLD_ON;
-	gnvs->f2pw = FAN2_PWM;
-
-	gnvs->f1of = FAN1_THRESHOLD_OFF;
-	gnvs->f1on = FAN1_THRESHOLD_ON;
-	gnvs->f1pw = FAN1_PWM;
-
-	gnvs->f0of = FAN0_THRESHOLD_OFF;
-	gnvs->f0on = FAN0_THRESHOLD_ON;
-	gnvs->f0pw = FAN0_PWM;
-
 	gnvs->tcrt = CRITICAL_TEMPERATURE;
 	gnvs->tpsv = PASSIVE_TEMPERATURE;
 	gnvs->tmax = MAX_TEMPERATURE;
-	gnvs->flvl = 5;
 }

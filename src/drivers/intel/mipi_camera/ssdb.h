b5d68e41a2af98d9a5797a52f380dd91846c68fd
Matt DeVillier <matt.devillier@gmail.com>
2025-11-09 18:16:55 -0600

    drivers/intel/mipi_camera: Tidy SSDB comment wrapping
    Reflow the multiline comments in `ssdb.h` to 100 columns.
    While this slightly exceeds the 96 column recommended limit in the
    coding style guide, the overall effect improves rather than reduces
    readability.
    
    Change-Id: I5b98d48ea5a99e38eb3472dfd24be434433857cc
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90182
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Maximilian Brune <maximilian.brune@9elements.com>

--- src/drivers/intel/mipi_camera/ssdb.h
+++ src/drivers/intel/mipi_camera/ssdb.h
@@ -47,17 +47,12 @@ struct intel_ssdb {
 	uint8_t version;			/* Current version */
 	uint8_t sensor_card_sku;		/* CRD Board type */
 	guid_t csi2_data_stream_interface;	/* CSI2 data stream GUID */
-	uint16_t bdf_value;			/* Bus number of the host
-						controller */
-	uint32_t dphy_link_en_fuses;		/* Host controller's fuses
-						information used to verify if
-						link is fused out or not */
-	uint32_t lanes_clock_division;		/* Lanes/clock divisions per
-						sensor */
-	uint8_t link_used;			/* Link used by this sensor
-						stream */
-	uint8_t lanes_used;			/* Number of lanes connected for
-						the sensor */
+	uint16_t bdf_value;			/* Bus number of the host controller */
+	uint32_t dphy_link_en_fuses;		/* Host controller's fuses information used to
+						   verify if link is fused out or not */
+	uint32_t lanes_clock_division;		/* Lanes/clock divisions per sensor */
+	uint8_t link_used;			/* Link used by this sensor stream */
+	uint8_t lanes_used;			/* Number of lanes connected for the sensor */
 	uint32_t csi_rx_dly_cnt_termen_clane;	/* MIPI timing information */
 	uint32_t csi_rx_dly_cnt_settle_clane;	/* MIPI timing information */
 	uint32_t csi_rx_dly_cnt_termen_dlane0;	/* MIPI timing information */
@@ -68,27 +63,19 @@ struct intel_ssdb {
 	uint32_t csi_rx_dly_cnt_settle_dlane2;	/* MIPI timing information */
 	uint32_t csi_rx_dly_cnt_termen_dlane3;	/* MIPI timing information */
 	uint32_t csi_rx_dly_cnt_settle_dlane3;	/* MIPI timing information */
-	uint32_t max_lane_speed;		/* Maximum lane speed for
-						the sensor */
-	uint8_t sensor_cal_file_idx;		/* Legacy field for sensor
-						calibration file index */
-	uint8_t sensor_cal_file_idx_mbz[3];	/* Legacy field for sensor
-						calibration file index */
-	uint8_t rom_type;			/* NVM type of the camera
-						module */
-	uint8_t vcm_type;			/* VCM type of the camera
-						module */
+	uint32_t max_lane_speed;		/* Maximum lane speed for the sensor */
+	uint8_t sensor_cal_file_idx;		/* Legacy field for sensor calibration file index */
+	uint8_t sensor_cal_file_idx_mbz[3];	/* Legacy field for sensor calibration file index */
+	uint8_t rom_type;			/* NVM type of the camera module */
+	uint8_t vcm_type;			/* VCM type of the camera module */
 	uint8_t platform;			/* Platform information */
 	uint8_t platform_sub;			/* Platform sub-categories */
-	uint8_t flash_support;			/* Enable/disable flash
-						support */
+	uint8_t flash_support;			/* Enable/disable flash support */
 	uint8_t privacy_led;			/* Privacy LED support */
 	uint8_t degree;				/* Camera Orientation */
-	uint8_t mipi_define;			/* MIPI info defined in ACPI or
-						sensor driver */
+	uint8_t mipi_define;			/* MIPI info defined in ACPI or sensor driver */
 	uint32_t mclk_speed;			/* Clock info for sensor */
-	uint8_t control_logic_id;		/* PMIC device node used for
-						the camera sensor */
+	uint8_t control_logic_id;		/* PMIC device node used for the camera sensor */
 	uint8_t mipi_data_format;		/* MIPI data format */
 	uint8_t silicon_version;		/* Silicon version */
 	uint8_t customer_id;			/* Customer ID */

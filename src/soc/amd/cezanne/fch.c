384e6e1c375a5c58aef92d9f8942d8218e6ef252
Maximilian Brune <maximilian.brune@9elements.com>
2025-11-25 20:45:19 +0100

    soc/amd/cezanne: Remove set_resets_to_cold
    Renoir actually supports warm reset, so we don't need to toggle the
    PwrGood for all resets.
    
    Signed-off-by: Maximilian Brune <maximilian.brune@9elements.com>
    Change-Id: I45d6b559874d67b886c65f7ad722f96eba415399
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90211
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/cezanne/fch.c
+++ src/soc/amd/cezanne/fch.c
@@ -173,7 +173,9 @@ static void cgpll_clock_gate_init(void)
 
 void fch_init(void *chip_info)
 {
-	set_resets_to_cold();
+	if (!CONFIG(SOC_AMD_RENOIR))
+		set_resets_to_cold();
+
 	i2c_soc_init();
 	fch_init_acpi_ports();
 

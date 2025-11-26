ed9239cd855e649208a48584edb701c930847c45
Yunlong Jia <yunlong.jia@ecs.corp-partner.google.com>
2025-11-26 08:44:16 +0000

    mb/google/nissa/var/gothrax: Add Samsung parts to RAM ID table
    Add the support RAM parts for gothrax.
    Here is the ram part number list:
    DRAM Part Name                 ID to assign
    MT62F512M32D2DR-031 WT:B       0 (0000)
    H58G56AK6BX069                 1 (0001)
    K3LKBKB0BM-MGCP                2 (0010)
    H9JCNNNBK3MLYR-N6E             0 (0000)
    H9JCNNNCP3MLYR-N6E             3 (0011)
    K3KL8L80CM-MGCT                4 (0100)
    
    BUG=b:463859361
    BRANCH=None
    TEST=emerge-nissa coreboot
    
    Change-Id: Ia43d300e63d22df27d5632d702a404a18442ea75
    Signed-off-by: Yunlong Jia <yunlong.jia@ecs.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90239
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Subrata Banik <subratabanik@google.com>

--- src/mainboard/google/brya/variants/gothrax/memory/Makefile.mk
+++ src/mainboard/google/brya/variants/gothrax/memory/Makefile.mk
@@ -8,3 +8,4 @@ SPD_SOURCES += spd/lp5/set-0/spd-1.hex      # ID = 0(0b0000)  Parts = MT62F512M3
 SPD_SOURCES += spd/lp5/set-0/spd-3.hex      # ID = 1(0b0001)  Parts = H58G56AK6BX069
 SPD_SOURCES += spd/lp5/set-0/spd-3.hex      # ID = 2(0b0010)  Parts = K3LKBKB0BM-MGCP
 SPD_SOURCES += spd/lp5/set-0/spd-2.hex      # ID = 3(0b0011)  Parts = H9JCNNNCP3MLYR-N6E
+SPD_SOURCES += spd/lp5/set-0/spd-7.hex      # ID = 4(0b0100)  Parts = K3KL8L80CM-MGCT

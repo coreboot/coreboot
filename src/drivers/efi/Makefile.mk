b94a84a7922c428d277c829873e674ec12b38d2c
Matt DeVillier <matt.devillier@gmail.com>
2025-11-29 22:54:11 -0600

    drivers/efi: Exclude verstage from EFI variable store files
    The EFI variable store driver (efivars.c) and option backend
    (option.c) require EDK2 headers which are x86-specific and not
    available in ARM verstage. Use 'all_x86-' instead of 'all-' to
    exclude verstage while keeping other x86 stages and SMM.
    
    TEST=build google/dewatt with CFR enabled
    
    Change-Id: I6d0955423cb55658725dfa3025b2118736f5e63b
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90296
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/drivers/efi/Makefile.mk
+++ src/drivers/efi/Makefile.mk
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
-all-$(CONFIG_DRIVERS_EFI_VARIABLE_STORE)	+= efivars.c
+all_x86-$(CONFIG_DRIVERS_EFI_VARIABLE_STORE)	+= efivars.c
 smm-$(CONFIG_DRIVERS_EFI_VARIABLE_STORE)	+= efivars.c
 
 ramstage-$(CONFIG_DRIVERS_EFI_UPDATE_CAPSULES)	+= capsules.c
 
-all-$(CONFIG_USE_UEFI_VARIABLE_STORE)	+= option.c
+all_x86-$(CONFIG_USE_UEFI_VARIABLE_STORE)	+= option.c
 smm-$(CONFIG_USE_UEFI_VARIABLE_STORE)	+= option.c
 
 ramstage-$(CONFIG_DRIVERS_EFI_FW_INFO)	+= info.c

99d67bae637b935573bfbeb98598ba1e8d5d285f
Matt DeVillier <matt.devillier@gmail.com>
2025-12-01 20:31:43 -0600

    mb/google/glados: Add CFR option menu support
    Add CFR option menu support when using edk2 payload and SMMSTORE.
    Include relevant items from Skylake SoC, Intel common, and ChromeEC.
    
    TEST=build/boot google/glados (chell), verify CFR option
    functionality.
    
    Change-Id: Ie124d69e979c7d0ca8dd119046412cb637eb04da
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90316
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Jason Glenesk <jason.glenesk@gmail.com>

--- src/mainboard/google/glados/Makefile.mk
+++ src/mainboard/google/glados/Makefile.mk
@@ -12,7 +12,7 @@ romstage-$(CONFIG_CHROMEOS) += chromeos.c
 ramstage-$(CONFIG_CHROMEOS) += chromeos.c
 
 ramstage-$(CONFIG_EC_GOOGLE_CHROMEEC) += ec.c
-
+ramstage-$(CONFIG_DRIVERS_OPTION_CFR) += cfr.c
 ramstage-y += mainboard.c
 
 ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads

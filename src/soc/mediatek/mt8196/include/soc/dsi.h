74c13eead40e08dbe7fb94702042e76ba37faadf
Yu-Ping Wu <yupingso@chromium.org>
2025-12-11 00:59:58 +0800

    soc/mediatek/mt8196: Define dsi_regs/mipi_tx_regs structs
    Add dsi_register_v3.h to define dsi_regs struct for mt8196, and add
    dsi_reg.h to define mipi_tx_regs struct. Unlike other SoCs, mt8196
    has dsi1 and mipi_tx1 registers in order to support DSI dual channel.
    
    BUG=b:424782827
    TEST=util/abuild/abuild -x -t GOOGLE_SAPPHIRE -a
    BRANCH=none
    
    Change-Id: I2b541c89a007f380de482bffa86aca60c351b526
    Signed-off-by: Payne Lin <payne.lin@mediatek.corp-partner.google.com>
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90451
    Reviewed-by: Chen-Tsung Hsieh <chentsung@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- src/soc/mediatek/mt8196/include/soc/dsi.h
+++ src/soc/mediatek/mt8196/include/soc/dsi.h
@@ -4,5 +4,12 @@
 #define SOC_MEDIATEK_MT8196_DSI_H
 
 #include <soc/dsi_common.h>
+#include <soc/dsi_reg.h>
+
+#define MTK_DSI_MIPI_RATIO_NUMERATOR	100
+#define MTK_DSI_MIPI_RATIO_DENOMINATOR	100
+#define MTK_DSI_DATA_RATE_MIN_MHZ	125
+#define MTK_DSI_HAVE_SIZE_CON		1
+#define PIXEL_STREAM_CUSTOM_HEADER	0xb
 
 #endif

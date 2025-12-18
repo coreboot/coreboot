3d980dae22274e3d3f806a2ca9b60cfe5b9ca6b9
Yanqiong Huang <huangyanqiong@huaqin.corp-partner.google.com>
2025-12-18 19:19:07 +0800

    mb/google/nissa/var/rull: Add 3 DDR modules to RAM id table
    Add HYNIX H9JCNNNCP3MLYR-N6E and MICRON MT62F1G32D4DR-031 WT:B as id 5,
    and add SAMSUNG K3LKBKB0BM-MGCP as id 6, resulting in the list below:
    
    DRAM Part Name                 ID to assign
    MT62F512M32D2DR-031 WT:B       0 (0000)
    H9JCNNNBK3MLYR-N6E             0 (0000)
    K3KL8L80CM-MGCT                1 (0001)
    H58G56BK7BX068                 1 (0001)
    K3KL9L90CM-MGCT                2 (0010)
    H58G66BK8BX067                 3 (0011)
    H58G56BK8BX068                 4 (0100)
    MT62F1G32D2DS-023 WT:B         4 (0100)
    H58G56CK8BX146                 4 (0100)
    H9JCNNNCP3MLYR-N6E             5 (0101)
    MT62F1G32D4DR-031 WT:B         5 (0101)
    K3LKBKB0BM-MGCP                6 (0110)
    
    BUG=b:470691660
    TEST=Use part_id_gen to generate related settings
    
    Change-Id: Id37f83e54f5be12087010c8045b6dfe407820f48
    Signed-off-by: Yanqiong Huang <huangyanqiong@huaqin.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90548
    Reviewed-by: Qinghong Zeng <zengqinghong@huaqin.corp-partner.google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Eric Lai <ericllai@google.com>

--- src/mainboard/google/brya/variants/rull/memory/Makefile.mk
+++ src/mainboard/google/brya/variants/rull/memory/Makefile.mk
@@ -9,3 +9,5 @@ SPD_SOURCES += spd/lp5/set-0/spd-7.hex      # ID = 1(0b0001)  Parts = K3KL8L80CM
 SPD_SOURCES += spd/lp5/set-0/spd-8.hex      # ID = 2(0b0010)  Parts = K3KL9L90CM-MGCT
 SPD_SOURCES += spd/lp5/set-0/spd-10.hex      # ID = 3(0b0011)  Parts = H58G66BK8BX067
 SPD_SOURCES += spd/lp5/set-0/spd-11.hex      # ID = 4(0b0100)  Parts = H58G56BK8BX068, MT62F1G32D2DS-023 WT:B, H58G56CK8BX146
+SPD_SOURCES += spd/lp5/set-0/spd-2.hex      # ID = 5(0b0101)  Parts = H9JCNNNCP3MLYR-N6E, MT62F1G32D4DR-031 WT:B
+SPD_SOURCES += spd/lp5/set-0/spd-3.hex      # ID = 6(0b0110)  Parts = K3LKBKB0BM-MGCP

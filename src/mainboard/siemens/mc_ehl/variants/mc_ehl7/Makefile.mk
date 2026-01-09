e8ac9ffcd9a8e56667d18b12ad859851ad9de1a8
Uwe Poeche <uwe.poeche@siemens.com>
2026-01-09 08:35:59 +0100

    mb/siemens/mc_ehl7: Add new board variant based on mc_ehl6
    This new mainboard variant for the Siemens mc_ehl6 is initially based on
    a direct copy of the mc_ehl6 configuration. This commit contains the
    basic board setup with only minimal changes to enable the new variant.
    
    Further specific adaptations for the mc_ehl7 hardware will be handled
    in subsequent commits.
    
    TEST=Build and boot to OS on mc_ehl7.
    
    Change-Id: I46148492f65630175abb3ce884261d098314f2bc
    Signed-off-by: Uwe Poeche <uwe.poeche@siemens.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/90714
    Reviewed-by: Mario Scheithauer <mario.scheithauer@siemens.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/siemens/mc_ehl/variants/mc_ehl7/Makefile.mk
@@ -0,0 +1,8 @@
+## SPDX-License-Identifier: GPL-2.0-only
+
+bootblock-y += gpio.c
+romstage-y += memory.c
+ramstage-y += gpio.c
+ramstage-y += mainboard.c
+
+all-$(CONFIG_NC_FPGA_POST_CODE) += post.c

a45590126f4c86f4808f32e1c6d93908d099c6f6
Matt DeVillier <matt.devillier@gmail.com>
2026-02-02 09:39:24 -0600

    soc/intel/jasperlake: add soc_acpi_name entry for IGD
    JSL was missing an entry for the IGD, preventing the use of
    DRIVERS_GFX_GENERIC since the call to acpi_device_scope() for it
    returned null. Add the missing IGD entry, consistent with other
    modern Intel SoCs.
    
    TEST=build/boot google/magolor with chip drivers/gfx/generic entry
    and verify SSDT entry correctly created.
    
    Change-Id: Idf1d8992b45c60f68fd2b156c6e7cae816df84b4
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/91064
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
    Reviewed-by: Jérémy Compostella <jeremy.compostella@intel.com>

--- src/soc/intel/jasperlake/chip.c
+++ src/soc/intel/jasperlake/chip.c
@@ -63,6 +63,7 @@ const char *soc_acpi_name(const struct device *dev)
 
 	switch (dev->path.pci.devfn) {
 	case SA_DEVFN_ROOT:	return "MCHC";
+	case SA_DEVFN_IGD:	return "GFX0";
 	case SA_DEVFN_IPU:	return "IPU0";
 	case PCH_DEVFN_ISH:	return "ISHB";
 	case SA_DEVFN_GNA:	return "GNA";

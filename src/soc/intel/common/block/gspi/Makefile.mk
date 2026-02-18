cf5d6f1c885a5508c85bda1f574aff71de0cb58e
Jeremy Compostella <jeremy.compostella@intel.com>
2026-02-18 14:49:51 -0800

    soc/intel/common/block/gspi: Simplify Makefile using all-$()
    Replace individual stage-specific lines with all-$() for each file.
    This simplifies the Makefile from 6 lines to 2 lines while maintaining
    identical functionality.
    
    The all-$() variable automatically includes the file in all build stages
    (bootblock, verstage, romstage, postcar, ramstage, smm), which is
    exactly what the original code was doing manually for each stage.
    
    Change-Id: Ie89ba86a545c548fcc4ad0eb48a5cbb33733b541
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/91358
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/soc/intel/common/block/gspi/Makefile.mk
+++ src/soc/intel/common/block/gspi/Makefile.mk
@@ -1,6 +1,2 @@
 ## SPDX-License-Identifier: GPL-2.0-only
-bootblock-$(CONFIG_SOC_INTEL_COMMON_BLOCK_GSPI) += gspi.c
-romstage-$(CONFIG_SOC_INTEL_COMMON_BLOCK_GSPI) += gspi.c
-ramstage-$(CONFIG_SOC_INTEL_COMMON_BLOCK_GSPI) += gspi.c
-verstage-$(CONFIG_SOC_INTEL_COMMON_BLOCK_GSPI) += gspi.c
-postcar-$(CONFIG_SOC_INTEL_COMMON_BLOCK_GSPI) += gspi.c
+all-$(CONFIG_SOC_INTEL_COMMON_BLOCK_GSPI) += gspi.c

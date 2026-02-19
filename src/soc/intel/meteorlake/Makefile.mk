38baf0c5f6081367355b969af281432d8b8f4f46
Jeremy Compostella <jeremy.compostella@intel.com>
2026-02-19 07:43:35 -0800

    soc/intel/meteorlake: Use common UART device list driver
    Remove platform-specific uart.c and switch to the common UART device
    list driver. This eliminates duplicate code.
    
    The meteorlake uart.c simply defined uart_devices[] array. The common
    driver now handles this using the SOC_UART_DEVFN(n) macro defined in
    pci_devs.h which uses token concatenation to map to platform-specific
    PCI_DEVFN_UARTn definitions.
    
    This commit:
    - Selects SOC_INTEL_COMMON_FEATURE and
      SOC_INTEL_COMMON_FEATURE_UART_DEVICES in Kconfig
    - Removes uart.c and updates Makefile.mk
    
    Change-Id: Ic791eaa2521a44aba330e149fb0185094dd9ccf7
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/91243
    Reviewed-by: Kim, Wonkyu <wonkyu.kim@intel.com>
    Reviewed-by: Werner Zeh <werner.zeh@siemens.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/meteorlake/Makefile.mk
+++ src/soc/intel/meteorlake/Makefile.mk
@@ -10,7 +10,6 @@ all-y += gspi.c
 all-y += i2c.c
 all-y += pmutil.c
 all-y += spi.c
-all-y += uart.c
 all-y += gpio.c
 
 bootblock-y += bootblock/bootblock.c
@@ -55,7 +54,6 @@ smm-y += p2sb.c
 smm-y += pmutil.c
 smm-y += smihandler.c
 smm-y += soc_info.c
-smm-y += uart.c
 smm-y += xhci.c
 
 CPPFLAGS_common += -I$(src)/soc/intel/meteorlake

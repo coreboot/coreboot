bb941824cad1787c977ef6b7c89b84b96cbbb3e5
Jeremy Compostella <jeremy.compostella@intel.com>
2026-02-19 08:22:52 -0800

    soc/intel/common/feature/uart: Add common UART device list driver
    This patch introduces a common UART device list implementation that
    eliminates duplication across multiple Intel SoC platforms. Instead of
    maintaining nearly identical uart.c files in each platform directory,
    this common driver uses platform-specific macros to define UART device
    function numbers.
    
    The common implementation expects each platform to define the following
    macros in their soc/pci_devs.h header:
    - PCI_DEVFN_UART0
    - PCI_DEVFN_UART1
    - PCI_DEVFN_UART2
    
    This approach maintains platform flexibility while reducing code
    duplication and simplifying maintenance. The driver is compiled across
    all stages (bootblock, verstage, romstage, postcar, ramstage, smm) to
    support various UART usage scenarios.
    
    Change-Id: Iba82a2fe24dd9ccf704e4a0fadc481b63662b94d
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/91241
    Reviewed-by: Kim, Wonkyu <wonkyu.kim@intel.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Pranava Y N <pranavayn@google.com>

---
+++ src/soc/intel/common/feature/uart/Makefile.mk
@@ -0,0 +1,3 @@
+## SPDX-License-Identifier: GPL-2.0-only
+all-$(CONFIG_SOC_INTEL_COMMON_FEATURE_UART_DEVICES) += uart_devices.c
+smm-$(CONFIG_SOC_INTEL_COMMON_FEATURE_UART_DEVICES) += uart_devices.c

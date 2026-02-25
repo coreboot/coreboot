3c92c8402acf163379ddd0ec91d4a9bb39d41f9d
Jeremy Compostella <jeremy.compostella@intel.com>
2026-02-25 10:44:17 -0800

    soc/intel/pantherlake: Use common GSPI devfn mapping
    Replace platform-specific gspi.c with the common GSPI device function
    mapping implementation.
    
    Changes:
    - Select SOC_INTEL_COMMON_FEATURE_GSPI_DEVFN in Kconfig
    - Define SOC_GSPI_DEVFN(n) macro in pci_devs.h
    - Remove platform-specific gspi.c file
    
    TEST=Panther Lake Fatcat OS boots properly
    
    Change-Id: Ic3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/91316
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/intel/pantherlake/Makefile.mk
+++ src/soc/intel/pantherlake/Makefile.mk
@@ -7,7 +7,6 @@ subdirs-y += ../../../cpu/intel/microcode
 subdirs-y += ../../../cpu/intel/turbo
 
 # all (bootblock, verstage, romstage, postcar, ramstage)
-all-y += gspi.c
 all-y += pmutil.c
 all-y += spi.c
 all-y += gpio.c

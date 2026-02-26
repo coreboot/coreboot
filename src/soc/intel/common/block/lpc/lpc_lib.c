coreboot/coreboot

commit 339ef9b5c9297019330259828b278dab2f524cdc

soc/intel/common/block/lpc: Improve automatic window opening
When an existing LPC window covers an IO range, but is bigger or starts
earlier the current code doesn't recognize it. It will open another
window overlapping an existing one.

Check if an existing LPC IO window covers the current one and allow it
to be bigger than the entry.

TEST=Thinkpad X280 still boots and error messages are gone.

Should fix the following line seen in the coreboot log of the Lenovo
T480 and compatible:
[ERROR]  LPC: Cannot open IO window: 1604 size 1
[ERROR]  No more IO windows
[ERROR]  LPC: Cannot open IO window: 1606 size 1
[ERROR]  No more IO windows
[ERROR]  LPC: Cannot open IO window: 1610 size 10
[ERROR]  No more IO windows

Change-Id: I586066238b801d2abb1122698fb4092ee0b2f6b9
Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/91171
Reviewed-by: Ladislav Ezr <ladislav@ezr.cz>
Reviewed-by: Johann C Rode <jcrode82@gmail.com>
Reviewed-by: Paul Menzel <paulepanter@mailbox.org>
Reviewed-by: Jérémy Compostella <jeremy.compostella@intel.com>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/soc/intel/common/block/lpc/lpc_lib.c
@@ -169,14 +169,17 @@ void lpc_open_pmio_window(uint16_t base, uint16_t size)
 		alignment = 1UL << (log2_ceil(window_size));
 		window_size = ALIGN_UP(window_size, alignment);
 
-		/* Address[15:2] in LGIR[15:12] and Mask[7:2] in LGIR[23:18]. */
-		lgir = (bridge_base & LPC_LGIR_ADDR_MASK) | LPC_LGIR_EN;
-		lgir |= ((window_size - 1) << 16) & LPC_LGIR_AMASK_MASK;
+		const struct region win = region_create(bridge_base, window_size);
 
-		/* Skip programming if same range already programmed. */
+		/* Skip programming if covered by existing window. */
 		for (i = 0; i < LPC_NUM_GENERIC_IO_RANGES; i++) {
-			if (lgir == pci_read_config32(PCH_DEV_LPC,
-						LPC_GENERIC_IO_RANGE(i)))
+			const u32 reg32 = pci_read_config32(PCH_DEV_LPC, LPC_GENERIC_IO_RANGE(i));
+			if (!(reg32 & LPC_LGIR_EN))
+				continue;
+			const struct region exist = region_create(reg32 & LPC_LGIR_ADDR_MASK,
+							1 + ((reg32 & LPC_LGIR_AMASK_MASK) >> 16));
+
+			if (region_is_subregion(&exist, &win))
 				return;
 		}
 
@@ -189,6 +192,10 @@ void lpc_open_pmio_window(uint16_t base, uint16_t size)
 			return;
 		}
 		lgir_reg_offset = LPC_GENERIC_IO_RANGE(lgir_reg_num);
+
+		/* Address[15:2] in LGIR[15:12] and Mask[7:2] in LGIR[23:18]. */
+		lgir = (bridge_base & LPC_LGIR_ADDR_MASK) | LPC_LGIR_EN;
+		lgir |= ((window_size - 1) << 16) & LPC_LGIR_AMASK_MASK;
 
 		pci_write_config32(PCH_DEV_LPC, lgir_reg_offset, lgir);
 		if (CONFIG(SOC_INTEL_COMMON_BLOCK_LPC_MIRROR_TO_GPMR))

commit 34c156427dcac5311eb2e96dd847e624cf200809

soc/intel/common/block/lpc: Fix AMASK decoding in window detection
Commit 339ef9b5c929 ("soc/intel/common/block/lpc: Improve automatic
window opening") introduced a bug in the decoding of existing LPC I/O
window sizes from the LGIR (LPC Generic I/O Range) registers.

The AMASK field in the LGIR register stores bits [7:2] of the address
mask, with bits [1:0] implicitly always set to 1 (representing 4-byte
granularity). The original implementation incorrectly calculated the
window size as:

  exist_size = 1 + ((reg32 & LPC_LGIR_AMASK_MASK) >> 16)

This fails to restore the implicit lower bits [1:0] of the mask.

For example, a window programmed with size 8 bytes:
- Stored mask: (8-1) & 0xfc = 0x4 (bits [7:2] only)
- Incorrectly decoded: 0x4 + 1 = 5 bytes (WRONG)
- Correctly decoded: (0x4 | 0x3) + 1 = 8 bytes (CORRECT)

This bug caused failures on Panther Lake boards where existing windows
were not recognized as covering requested ranges, leading to:

  [ERROR] LPC: Cannot open IO window: 800 size 8
  [ERROR] No more IO windows

The fix properly reconstructs the full mask by OR-ing in the implicit
bits [1:0] before calculating the size:

  exist_size = ((amask_raw & 0xfc) | 0x3) + 1

BUG=b:486133237
TEST=Boot Panther Lake Fatcat board, verify no LPC window errors

Change-Id: I0b5f95c01da6ce84924a038106edec600e3b97f8
Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/91418
Reviewed-by: Subrata Banik <subratabanik@google.com>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Pranava Y N <pranavayn@google.com>

src/soc/intel/common/block/lpc/lpc_lib.c
@@ -176,8 +176,16 @@ void lpc_open_pmio_window(uint16_t base, uint16_t size)
 			const u32 reg32 = pci_read_config32(PCH_DEV_LPC, LPC_GENERIC_IO_RANGE(i));
 			if (!(reg32 & LPC_LGIR_EN))
 				continue;
-			const struct region exist = region_create(reg32 & LPC_LGIR_ADDR_MASK,
-							1 + ((reg32 & LPC_LGIR_AMASK_MASK) >> 16));
+
+			/* The AMASK field stores bits [7:2] of the address mask.
+			 * Bits [1:0] are always set (4-byte granularity).
+			 * To decode: mask = (amask_raw & 0xfc) | 0x3
+			 *            size = mask + 1
+			 */
+			const u32 exist_base = reg32 & LPC_LGIR_ADDR_MASK;
+			const u32 amask_raw = (reg32 & LPC_LGIR_AMASK_MASK) >> 16;
+			const u32 exist_size = ((amask_raw & 0xfc) | 0x3) + 1;
+			const struct region exist = region_create(exist_base, exist_size);
 
 			if (region_is_subregion(&exist, &win))
 				return;
e1363017b9ae1ae9597369aa2847841948f3c52f
Jan Philipp Groß <jeangrande@mailbox.org>
2026-04-22 14:23:11 +0200

    mb/asus: Add Maximus VI Extreme (Haswell)
    This port was done via autoport and subsequent manual tweaking.
    
    Working:
    - Devil's Canyon CPUs (tested with i7-4790K with MRC and NRI)
    - All four DDR3 DIMM slots (tested with 4x Kingston HX324C11SRK2/16
      on MRC and NRI)
    - All video ports
    - Gigabit LAN Port
    - All USB ports and Headers
    - All SATA 3 ports
    - All PCIe slots
    - HD audio jack (audio output tested only)
    - S3 Suspend and Resume
    - UEFI Secureboot
    
    Not (yet) tested:
    - PS/2 port
    - NGFF slot
    
    Change-Id: I39fa9652b8c820cfa90c70625d85ca05b482a238
    Signed-off-by: Jan Philipp Groß <jeangrande@mailbox.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92358
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>

---
+++ src/mainboard/asus/maximus_vi_extreme/Makefile.mk
@@ -0,0 +1,7 @@
+## SPDX-License-Identifier: GPL-2.0-only
+
+bootblock-y += bootblock.c
+bootblock-y += gpio.c
+romstage-y += gpio.c
+ramstage-$(CONFIG_MAINBOARD_USE_LIBGFXINIT) += gma-mainboard.ads
+ramstage-$(CONFIG_DRIVERS_OPTION_CFR) += cfr.c
---
+++ src/mainboard/asus/maximus_vi_extreme/bootblock.c
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/pnp_ops.h>
+#include <southbridge/intel/lynxpoint/pch.h>
+#include <superio/nuvoton/common/nuvoton.h>
+#include <superio/nuvoton/nct6791d/nct6791d.h>
+
+#define GLOBAL_DEV	PNP_DEV(0x2e, 0)
+#define ACPI_DEV	PNP_DEV(0x2e, NCT6791D_ACPI)
+
+void mainboard_config_superio(void)
+{
+	nuvoton_pnp_enter_conf_state(GLOBAL_DEV);
+
+	/* Select SIO pin mux states */
+	pnp_write_config(GLOBAL_DEV, 0x1a, 0x0a);
+	pnp_write_config(GLOBAL_DEV, 0x1b, 0x36);
+	pnp_write_config(GLOBAL_DEV, 0x1c, 0x00);
+	pnp_write_config(GLOBAL_DEV, 0x24, 0x00);
+	pnp_write_config(GLOBAL_DEV, 0x26, 0x00);
+	pnp_write_config(GLOBAL_DEV, 0x28, 0x10);
+	pnp_write_config(GLOBAL_DEV, 0x2a, 0x40);
+	pnp_write_config(GLOBAL_DEV, 0x2c, 0x00);
+	pnp_write_config(GLOBAL_DEV, 0x2f, 0x03);
+
+	/* Power RAM in S3 */
+	pnp_set_logical_device(ACPI_DEV);
+	pnp_write_config(ACPI_DEV, 0xe4, 0x10);
+
+	nuvoton_pnp_exit_conf_state(GLOBAL_DEV);
+}

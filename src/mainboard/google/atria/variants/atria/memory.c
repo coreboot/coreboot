11d851fd34f1d9e2163ed8e15ed3e364556fd9db
P, Usha <usha.p@intel.com>
2026-05-05 15:09:15 +0530

    mb/google/atria: Update LP5 memory configuration
    BRANCH=None
    TEST=Build GOOGLE_ATRIA mainboard
    
    Change-Id: I10cb3aac753c59add6cd788b459d7550b3c29a67
    Signed-off-by: P, Usha <usha.p@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92536
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/atria/variants/atria/memory.c
+++ src/mainboard/google/atria/variants/atria/memory.c
@@ -1,7 +1,71 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <baseboard/variants.h>
+#include <soc/romstage.h>
 #include <soc/meminit.h>
+
+static const struct mb_cfg lp5_mem_config = {
+	.type = MEM_TYPE_LP5X,
+
+	.lpx_dq_map = {
+		.ddr0 = {
+			.dq0 = {  0,  2,  1,  3,  5,  4,  7,  6, },
+			.dq1 = { 12, 14, 15, 13,  9, 11, 10,  8, },
+		},
+		.ddr1 = {
+			.dq0 = { 11,  9,  8, 10, 12, 13, 14, 15, },
+			.dq1 = {  6,  5,  7,  4,  0,  3,  1,  2, },
+		},
+		.ddr2 = {
+			.dq0 = {  0,  2,  1,  3,  5,  7,  4,  6, },
+			.dq1 = { 13, 14, 15, 12,  9, 11, 10,  8, },
+		},
+		.ddr3 = {
+			.dq0 = { 10,  8,  9, 11, 15, 12, 13, 14, },
+			.dq1 = {  7,  5,  6,  4,  1,  3,  0,  2, },
+		},
+		.ddr4 = {
+			.dq0 = {  4,  6,  5,  7,  3,  1,  2,  0, },
+			.dq1 = { 10,  8,  9, 11, 15, 13, 12, 14, },
+		},
+		.ddr5 = {
+			.dq0 = { 15, 13, 12, 14, 11,  9, 10,  8, },
+			.dq1 = {  1,  2,  3,  0,  5,  4,  7,  6, },
+		},
+		.ddr6 = {
+			.dq0 = {  4,  6,  5,  7,  1,  3,  0,  2, },
+			.dq1 = { 11,  8,  9, 10, 12, 14, 15, 13, },
+		},
+		.ddr7 = {
+			.dq0 = { 12, 13, 15, 14,  9, 11,  8, 10, },
+			.dq1 = {  2,  3,  0,  1,  5,  4,  7,  6, },
+		},
+	},
+
+	.lpx_dqs_map = {
+		.ddr0 = { .dqs0 = 0, .dqs1 = 1, },
+		.ddr1 = { .dqs0 = 1, .dqs1 = 0, },
+		.ddr2 = { .dqs0 = 0, .dqs1 = 1, },
+		.ddr3 = { .dqs0 = 1, .dqs1 = 0, },
+		.ddr4 = { .dqs0 = 0, .dqs1 = 1, },
+		.ddr5 = { .dqs0 = 1, .dqs1 = 0, },
+		.ddr6 = { .dqs0 = 0, .dqs1 = 1, },
+		.ddr7 = { .dqs0 = 1, .dqs1 = 0, }
+	},
+
+	.ect = true, /* Early Command Training */
+
+	.user_bd = BOARD_TYPE_ULT_ULX,
+
+	.lp5x_config = {
+		.ccc_config = 0x55,
+	},
+};
+
+const struct mb_cfg *variant_memory_params(void)
+{
+	return &lp5_mem_config;
+}
 
 void variant_get_spd_info(struct mem_spd *spd_info)
 {

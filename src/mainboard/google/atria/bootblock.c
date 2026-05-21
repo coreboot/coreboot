5a430d6f0edc71c8ad6f15db0477e836b3225162
Jamie Ryu <jamie.m.ryu@intel.com>
2026-05-21 00:19:38 -0700

    mb/google/atria: Enable Realtek EC with LPC generic memory setup
    Enable Realtek EC support for google/atria by adding board-level
    integration and required early boot setup.
    
    Program LPC Generic I/O Memory base decode in bootblock to provide
    the EC access path needed during bring-up.
    
    BUG=b:512265143
    TEST=Booted on atria platform and verified Realtek EC initialization
    path in early boot
    
    Change-Id: I544545e437163d7415fb49c4f32bf42163d67cde
    Signed-off-by: Jamie Ryu <jamie.m.ryu@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92884
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/atria/bootblock.c
+++ src/mainboard/google/atria/bootblock.c
@@ -1,9 +1,18 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootblock_common.h>
+#include <intelblocks/lpc_lib.h>
 #include <baseboard/variants.h>
 
 void bootblock_mainboard_early_init(void)
 {
 	/* TODO: Perform mainboard initialization */
+}
+
+void bootblock_mainboard_init(void)
+{
+	if (CONFIG(EC_GOOGLE_CHROMEEC_LPC_GENERIC_MEMORY_RANGE))
+		lpc_open_mmio_window(CONFIG_EC_GOOGLE_CHROMEEC_LPC_GENERIC_MEMORY_BASE,
+			CONFIG_EC_GOOGLE_CHROMEEC_LPC_GENERIC_MEMORY_SIZE);
+
 }

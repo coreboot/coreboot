337563f6d6755fa3bb697aebf48179032b9fdf1d
Jamie Ryu <jamie.m.ryu@intel.com>
2026-05-22 18:09:58 -0700

    mb/google/atria: Configure variant GPIO pad overrides in early boot
    Add an early mainboard boot hook to build and apply a padbased GPIO
    table.
    
    The new pre-device bootstate callback merges variant GPIO settings
    onto the base table and programs pads early, ensuring correct GPIO
    state before later init stages.
    
    BUG=b:515160358
    TEST=Build/Boot atria to check GPIO configuration is done.
    
    Change-Id: I4f0d191a1749bcf471a7e9b452a132cd083ab0cc
    Signed-off-by: Jamie Ryu <jamie.m.ryu@intel.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92908
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Karthik Ramasubramanian <kramasub@google.com>

--- src/mainboard/google/atria/mainboard.c
+++ src/mainboard/google/atria/mainboard.c
@@ -1,6 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <acpi/acpi.h>
+#include <baseboard/variants.h>
+#include <baseboard/gpio.h>
+#include <bootstate.h>
 #include <device/device.h>
 #include <variant/ec.h>
 
@@ -8,6 +11,21 @@ static void mainboard_init(void *chip_info)
 {
 	mainboard_ec_init();
 }
+
+static void mainboard_early(void *unused)
+{
+	struct pad_config *padbased_table;
+	const struct pad_config *base_pads;
+	size_t base_num;
+
+	padbased_table = new_padbased_table();
+	base_pads = variant_gpio_table(&base_num);
+	gpio_padbased_override(padbased_table, base_pads, base_num);
+	gpio_configure_pads_with_padbased(padbased_table);
+	free(padbased_table);
+}
+
+BOOT_STATE_INIT_ENTRY(BS_PRE_DEVICE, BS_ON_EXIT, mainboard_early, NULL);
 
 static void mainboard_fill_ssdt(const struct device *dev)
 {

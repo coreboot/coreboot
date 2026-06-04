b43eaa3059f8db47689c8fa0ccbaefd781317a70
Elyes Haouas <ehaouas@noos.fr>
2026-06-04 10:04:30 +0200

    mb/ocp: Remove unused <device/pci_def.h>
    Change-Id: I1356cf6d83e230372ed16e4707e39d64622f740c
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93237
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/ocp/tiogapass/bootblock.c
+++ src/mainboard/ocp/tiogapass/bootblock.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootblock_common.h>
-#include <device/pci_def.h>
 #include <device/pci_ops.h>
 #include <intelblocks/pcr.h>
 #include <soc/pci_devs.h>

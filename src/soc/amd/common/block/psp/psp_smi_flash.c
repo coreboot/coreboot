coreboot/coreboot

commit 3a87b84f7cae6bbe53d9ed5f772946f90de8dfcd

drivers/amd/ftpm: Add backup SPI flash support
When two SPI flash are present in the system they need to kept in sync
when it comes to RPMC and fTPM. Add code to operate on both SPI flashes
at the same time when necessary.

Do the following:
- Erase PSP_NVRAM on both SPI flashes
- Erase PSP_RPMC_NVRAM on both SPI flashes
- Sync PSP_NVRAM on regular boot if necessary
- Increment RPMC counters on both SPI flashes
- Write fTPM and RPMC data in the PSP SMI handler
  into both SPI flash chips

Signed-off-by: Maximilian Brune <maximilian.brune@9elements.com>
Change-Id: Ie0a998d5e354f3a211b7581a4e4f70c42647abbc
Reviewed-on: https://review.coreboot.org/c/coreboot/+/90782
Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/soc/amd/common/block/psp/psp_smi_flash.c
@@ -67,9 +67,16 @@ static int lookup_store(uint64_t target_nv_id, struct region_device *rstore)
 	return rdev_chain(rstore, rdev, 0, region_device_sz(rdev));
 }
 
-static enum mbox_p2c_status get_flash_device(const struct spi_flash **flash)
+static enum mbox_p2c_status get_flash_device(const enum boot_device boot_device,
+					     const struct spi_flash **flash)
 {
-	*flash = boot_device_spi_flash();
+	if (boot_device == FLASH_PRIMARY)
+		*flash = boot_device_spi_flash();
+	else if (CONFIG(SOC_AMD_COMMON_BLOCK_SPI_BACKUP_SPI_FLASH))
+		*flash = backup_boot_device_spi_flash();
+	else
+		return MBOX_PSP_INVALID_PARAMETER;
+
 	if (*flash == NULL) {
 		printk(BIOS_ERR, "PSP: Unable to find SPI device\n");
 		return MBOX_PSP_COMMAND_PROCESS_ERROR;
@@ -78,11 +85,12 @@ static enum mbox_p2c_status get_flash_device(const struct spi_flash **flash)
 	return MBOX_PSP_SUCCESS;
 }
 
-static enum mbox_p2c_status find_psp_spi_flash_device_region(uint64_t target_nv_id,
+static enum mbox_p2c_status find_psp_spi_flash_device_region(const enum boot_device boot_device,
+							     uint64_t target_nv_id,
 							     struct region_device *store,
 							     const struct spi_flash **flash)
 {
-	if (get_flash_device(flash) != MBOX_PSP_SUCCESS)
+	if (get_flash_device(boot_device, flash) != MBOX_PSP_SUCCESS)
 		return MBOX_PSP_COMMAND_PROCESS_ERROR;
 
 	if (lookup_store(target_nv_id, store) < 0) {
@@ -187,7 +195,7 @@ enum mbox_p2c_status psp_smi_spi_get_info(struct mbox_default_buffer *buffer)
 
 	target_nv_id = get_psp_spi_info_id(cmd_buf);
 
-	ret = find_psp_spi_flash_device_region(target_nv_id, &store, &flash);
+	ret = find_psp_spi_flash_device_region(FLASH_PRIMARY, target_nv_id, &store, &flash);
 
 	if (ret != MBOX_PSP_SUCCESS)
 		return ret;
@@ -235,7 +243,7 @@ enum mbox_p2c_status psp_smi_spi_read(struct mbox_default_buffer *buffer)
 		return MBOX_PSP_COMMAND_PROCESS_ERROR;
 	}
 
-	ret = find_psp_spi_flash_device_region(target_nv_id, &store, &flash);
+	ret = find_psp_spi_flash_device_region(FLASH_PRIMARY, target_nv_id, &store, &flash);
 
 	if (ret != MBOX_PSP_SUCCESS)
 		return ret;
@@ -283,12 +291,27 @@ enum mbox_p2c_status psp_smi_spi_write(struct mbox_default_buffer *buffer)
 		return MBOX_PSP_COMMAND_PROCESS_ERROR;
 	}
 
-	ret = find_psp_spi_flash_device_region(target_nv_id, &store, &flash);
+	ret = find_psp_spi_flash_device_region(FLASH_PRIMARY, target_nv_id, &store, &flash);
 
 	if (ret != MBOX_PSP_SUCCESS)
 		return ret;
 
 	addr = (lba * flash->sector_size) + offset;
+
+	printk(BIOS_SPEW, "PSP: SPI write 0x%llx bytes at 0x%zx\n", num_bytes, addr);
+
+	if (rdev_writeat(&store, data, addr, (size_t)num_bytes) != (size_t)num_bytes) {
+		printk(BIOS_ERR, "PSP: Failed to write NVRAM data\n");
+		return MBOX_PSP_COMMAND_PROCESS_ERROR;
+	}
+
+	if (!CONFIG(SOC_AMD_COMMON_BLOCK_SPI_BACKUP_SPI_FLASH))
+		return MBOX_PSP_SUCCESS;
+
+	ret = find_psp_spi_flash_device_region(FLASH_BACKUP, target_nv_id, &store, &flash);
+
+	if (ret != MBOX_PSP_SUCCESS)
+		return ret;
 
 	printk(BIOS_SPEW, "PSP: SPI write 0x%llx bytes at 0x%zx\n", num_bytes, addr);
 
@@ -324,13 +347,28 @@ enum mbox_p2c_status psp_smi_spi_erase(struct mbox_default_buffer *buffer)
 
 	get_psp_spi_erase(cmd_buf, &target_nv_id, &lba, &num_blocks);
 
-	ret = find_psp_spi_flash_device_region(target_nv_id, &store, &flash);
+	ret = find_psp_spi_flash_device_region(FLASH_PRIMARY, target_nv_id, &store, &flash);
 
 	if (ret != MBOX_PSP_SUCCESS)
 		return ret;
 
 	addr = lba * flash->sector_size;
 	num_bytes = (size_t)num_blocks * flash->sector_size;
+
+	printk(BIOS_SPEW, "PSP: SPI erase 0x%zx bytes at 0x%zx\n", num_bytes, addr);
+
+	if (rdev_eraseat(&store, addr, num_bytes) != num_bytes) {
+		printk(BIOS_ERR, "PSP: Failed to erase SPI NVRAM data\n");
+		return MBOX_PSP_COMMAND_PROCESS_ERROR;
+	}
+
+	if (!CONFIG(SOC_AMD_COMMON_BLOCK_SPI_BACKUP_SPI_FLASH))
+		return MBOX_PSP_SUCCESS;
+
+	ret = find_psp_spi_flash_device_region(FLASH_BACKUP, target_nv_id, &store, &flash);
+
+	if (ret != MBOX_PSP_SUCCESS)
+		return ret;
 
 	printk(BIOS_SPEW, "PSP: SPI erase 0x%zx bytes at 0x%zx\n", num_bytes, addr);
 
@@ -357,11 +395,22 @@ enum mbox_p2c_status psp_smi_spi_rpmc_inc_mc(struct mbox_default_buffer *buffer)
 		return MBOX_PSP_SPI_BUSY;
 	}
 
-	if (get_flash_device(&flash) != MBOX_PSP_SUCCESS)
+	if (get_flash_device(FLASH_PRIMARY, &flash) != MBOX_PSP_SUCCESS)
 		return MBOX_PSP_COMMAND_PROCESS_ERROR;
 
 	if (spi_flash_rpmc_increment(flash, cmd_buf->req.counter_address,
 				     cmd_buf->req.counter_data, cmd_buf->req.signature)
+			!= CB_SUCCESS)
+		return MBOX_PSP_COMMAND_PROCESS_ERROR;
+
+	if (!CONFIG(SOC_AMD_COMMON_BLOCK_SPI_BACKUP_SPI_FLASH))
+		return MBOX_PSP_SUCCESS;
+
+	if (get_flash_device(FLASH_BACKUP, &flash) != MBOX_PSP_SUCCESS)
+		return MBOX_PSP_COMMAND_PROCESS_ERROR;
+
+	if (spi_flash_rpmc_increment(flash, cmd_buf->req.counter_address,
+				cmd_buf->req.counter_data, cmd_buf->req.signature)
 			!= CB_SUCCESS)
 		return MBOX_PSP_COMMAND_PROCESS_ERROR;
 
@@ -383,7 +432,7 @@ enum mbox_p2c_status psp_smi_spi_rpmc_req_mc(struct mbox_default_buffer *buffer)
 		return MBOX_PSP_SPI_BUSY;
 	}
 
-	if (get_flash_device(&flash) != MBOX_PSP_SUCCESS)
+	if (get_flash_device(FLASH_PRIMARY, &flash) != MBOX_PSP_SUCCESS)
 		return MBOX_PSP_COMMAND_PROCESS_ERROR;
 
 	if (spi_flash_rpmc_request(flash, cmd_buf->req.counter_address, cmd_buf->req.tag,

src/drivers/amd/ftpm/tpm.c
@@ -25,8 +25,10 @@
  */
 
 #include <timer.h>
+#include <amdblocks/backup_boot_device.h>
 #include <amdblocks/psp.h>
 #include <amdblocks/reset.h>
+#include <boot_device.h>
 #include <cbmem.h>
 #include <console/console.h>
 #include <commonlib/region.h>
@@ -93,12 +95,39 @@ static tpm_result_t crb_wait_for_reg32(const void *addr,
 	}
 }
 
-static int erase_region(const char *name)
+static int synchronize_region(const char *name)
+{
+	struct region ar;
+
+	if (fmap_locate_area(name, &ar)) {
+		printk(BIOS_ERR, "FTPM: Unable to find FMAP region %s\n", name);
+		return -1;
+	}
+
+	if (backup_boot_device_sync_subregion(&ar, true)) {
+		printk(BIOS_ERR, "FTPM: Failed to synchronize subregion %s\n", name);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int erase_region(const char *name, const enum boot_device boot_device)
 {
 	struct region_device store;
+	struct region ar;
 
-	if (fmap_locate_area_as_rdev_rw(name, &store)) {
-		printk(BIOS_ERR, "fTPM: Unable to find FMAP region %s\n", name);
+	if (fmap_locate_area(name, &ar)) {
+		printk(BIOS_ERR, "FTPM: Unable to find FMAP region %s\n", name);
+		return -1;
+	}
+	if (boot_device == FLASH_PRIMARY && boot_device_rw_subregion(&ar, &store)) {
+		printk(BIOS_ERR, "FTPM: Unable to find FMAP region %s\n", name);
+		return -1;
+	}
+	if (CONFIG(SOC_AMD_COMMON_BLOCK_SPI_BACKUP_SPI_FLASH) && boot_device == FLASH_BACKUP &&
+	    backup_boot_device_rw_subregion(&ar, &store)) {
+		printk(BIOS_ERR, "FTPM: Unable to find FMAP region %s\n", name);
 		return -1;
 	}
 
@@ -107,7 +136,6 @@ static int erase_region(const char *name)
 		return -1;
 
 	printk(BIOS_NOTICE, "fTPM: Erased FMAP region %s\n", name);
-
 	return 0;
 }
 
@@ -129,15 +157,23 @@ tpm_result_t crb_tpm_init(void)
 	psp_ftpm_needs_recovery(&psp_rpmc_nvram, &psp_nvram, &psp_dir);
 
 	if (psp_rpmc_nvram) {
-		if (erase_region(FMAP_NAME_PSP_RPMC_NVRAM))
-			psp_rpmc_nvram = false;	/* Skip reset if erase failed */
-	}
+		if (erase_region(FMAP_NAME_PSP_RPMC_NVRAM, FLASH_PRIMARY))
+			psp_rpmc_nvram = false; /* Skip reset if erase failed */
 
+		if (CONFIG(SOC_AMD_COMMON_BLOCK_SPI_BACKUP_SPI_FLASH) &&
+		    erase_region(FMAP_NAME_PSP_RPMC_NVRAM, FLASH_BACKUP))
+			printk(BIOS_ERR, "Failed to erase backup " FMAP_NAME_PSP_RPMC_NVRAM
+					 " FMAP region\n");
+	}
 	if (psp_nvram) {
-		if (erase_region(FMAP_NAME_PSP_NVRAM))
-			psp_nvram = false;	/* Skip reset if erase failed */
-	}
+		if (erase_region(FMAP_NAME_PSP_NVRAM, FLASH_PRIMARY))
+			psp_nvram = false; /* Skip reset if erase failed */
 
+		if (CONFIG(SOC_AMD_COMMON_BLOCK_SPI_BACKUP_SPI_FLASH) &&
+		    erase_region(FMAP_NAME_PSP_NVRAM, FLASH_BACKUP))
+			printk(BIOS_ERR, "Failed to erase backup " FMAP_NAME_PSP_NVRAM
+					 " FMAP region\n");
+	}
 	if (psp_rpmc_nvram || psp_nvram) {
 		printk(BIOS_DEBUG, "fTPM: Reset to recover fTPM...\n");
 		cold_reset();
@@ -150,6 +186,11 @@ tpm_result_t crb_tpm_init(void)
 
 	if (!psp_ftpm_is_active())
 		return CB_ERR_NOT_IMPLEMENTED;
+
+	if (CONFIG(SOC_AMD_COMMON_BLOCK_SPI_BACKUP_SPI_FLASH)) {
+		synchronize_region(FMAP_NAME_PSP_NVRAM);
+		synchronize_region(FMAP_NAME_PSP_RPMC_NVRAM);
+	}
 
 	if (ENV_HAS_CBMEM) {
 		/* setting up fTPM communication buffers in cbmem */
@@ -177,7 +218,6 @@ tpm_result_t crb_tpm_init(void)
 		write64(CRB_REG(CRB_REG_CMD_ADDR), (uintptr_t)buf);
 		write32(CRB_REG(CRB_REG_RESP_SIZE), FTPM_CRB_SIZE);
 		write64(CRB_REG(CRB_REG_RESP_ADDR), (uintptr_t)buf + FTPM_CRB_SIZE);
-
 		/*
 		 * The OS will read the CRB registers to find the communication
 		 * buffers. No need to advertise it in ACPI.

src/soc/amd/common/block/include/amdblocks/backup_boot_device.h
@@ -6,6 +6,11 @@
 #include <commonlib/region.h>
 #include <spi_flash.h>
 
+enum boot_device {
+	FLASH_PRIMARY, // primary SPI flash
+	FLASH_BACKUP,  // backup  SPI flash
+};
+
 /* Retrieve the SPI CS index of the backup boot device. */
 int backup_boot_device_spi_cs(void);
 

commit ee0738442ca26a846d3c3fa940255c80aaac8490

soc/amd/common/block/psp/psp_smi_flash: Fix backup SPI
Fix regression introduced in commit 3a87b84f7cae
("drivers/amd/ftpm: Add backup SPI flash support").

Currently find_psp_spi_flash_device_region() returns the backup struct
spi_flash, but the struct region_device is always on primary SPI flash.
All R/W/E operations are thus executed on the wrong SPI flash when
attempting to access the backup SPI flash. RPMC isn't affected as it
used the correct struct spi_flash already.

Fix that by returning the correct struct region_device for the backup
SPI flash.

Change-Id: Ia53c1188ba262aeeccf5ab3bb0b5125a761f613c
Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/93173
Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

src/soc/amd/common/block/psp/psp_smi_flash.c
@@ -33,40 +33,6 @@ static const char *id_to_region_name(uint64_t target_nv_id)
 	return NULL;
 }
 
-/*
- * Do not cache the location to cope with flash changing underneath (e.g. due
- * to an update)
- */
-static int lookup_store(uint64_t target_nv_id, struct region_device *rstore)
-{
-	/* read_rdev, write_rdev and store_irdev need to be static to not go out of scope when
-	   this function returns */
-	static struct region_device read_rdev, write_rdev;
-	static struct incoherent_rdev store_irdev;
-	const char *name;
-	struct region region;
-	const struct region_device *rdev;
-
-	name = id_to_region_name(target_nv_id);
-	if (!name)
-		return -1;
-
-	if (fmap_locate_area(name, &region) < 0)
-		return -1;
-
-	if (boot_device_ro_subregion(&region, &read_rdev) < 0)
-		return -1;
-
-	if (boot_device_rw_subregion(&region, &write_rdev) < 0)
-		return -1;
-
-	rdev = incoherent_rdev_init(&store_irdev, &region, &read_rdev, &write_rdev);
-	if (rdev == NULL)
-		return -1;
-
-	return rdev_chain(rstore, rdev, 0, region_device_sz(rdev));
-}
-
 static enum mbox_p2c_status get_flash_device(const enum boot_device boot_device,
 					     const struct spi_flash **flash)
 {
@@ -90,12 +56,46 @@ static enum mbox_p2c_status find_psp_spi_flash_device_region(const enum boot_dev
 							     struct region_device *store,
 							     const struct spi_flash **flash)
 {
+	static struct region_device read_rdev, write_rdev;
+	static struct incoherent_rdev store_irdev;
+	const struct region_device *rdev;
+	const char *name;
+	struct region region;
+
 	if (get_flash_device(boot_device, flash) != MBOX_PSP_SUCCESS)
 		return MBOX_PSP_COMMAND_PROCESS_ERROR;
 
-	if (lookup_store(target_nv_id, store) < 0) {
-		printk(BIOS_ERR, "PSP: Unable to find PSP SPI region\n");
+	name = id_to_region_name(target_nv_id);
+	if (!name)
 		return MBOX_PSP_COMMAND_PROCESS_ERROR;
+
+	if (fmap_locate_area(name, &region) < 0)
+		return MBOX_PSP_COMMAND_PROCESS_ERROR;
+
+	if (boot_device == FLASH_PRIMARY) {
+		if (boot_device_ro_subregion(&region, &read_rdev) < 0)
+			return MBOX_PSP_COMMAND_PROCESS_ERROR;
+
+		if (boot_device_rw_subregion(&region, &write_rdev) < 0)
+			return MBOX_PSP_COMMAND_PROCESS_ERROR;
+
+		rdev = incoherent_rdev_init(&store_irdev, &region, &read_rdev, &write_rdev);
+		if (rdev == NULL)
+			return MBOX_PSP_COMMAND_PROCESS_ERROR;
+
+		return rdev_chain(store, rdev, 0, region_device_sz(rdev));
+
+	} else if (CONFIG(SOC_AMD_COMMON_BLOCK_SPI_BACKUP_SPI_FLASH)) {
+		/*
+		 * FIXME: No backup_boot_device_ro_subregion() implementation yet, so use
+		 * the rw one for both read and write. This is slower!
+		 */
+		if (backup_boot_device_rw_subregion(&region, &write_rdev) < 0)
+			return MBOX_PSP_COMMAND_PROCESS_ERROR;
+
+		return rdev_chain(store, &write_rdev, 0, region_device_sz(&write_rdev));
+	} else {
+		return MBOX_PSP_INVALID_PARAMETER;
 	}
 
 	return MBOX_PSP_SUCCESS;
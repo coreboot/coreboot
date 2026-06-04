6aeedc22aebd8add2bc79528f603148f1b2c3ade
liu.gary <liu.gary@inventec.corp-partner.google.com>
2026-06-05 00:40:31 +0800

    mb/google/ocelot/var/ocicat: Refactor code to distinguish between PCIE and CNVI WLAN card
    Follow the ocelot structure, refactor code to identify whether the WLAN card is PCIE or CNVI
    
    BUG=b:517316519
    TEST= Build Ocicat and flash to DUT. check can enter S0i2.2 successfully
    
    Change-Id: Ic8192c39fbdc7484535b66b268edbec5b01b77b3
    Signed-off-by: liu.gary <liu.gary@inventec.corp-partner.google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93261
    Reviewed-by: Pranava Y N <pranavayn@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/ocelot/variants/ocicat/fw_config.c
+++ src/mainboard/google/ocelot/variants/ocicat/fw_config.c
@@ -11,6 +11,63 @@
 /* t: table */
 #define GPIO_CONFIGURE_PADS(t) gpio_configure_pads(t, ARRAY_SIZE(t))
 
+static const struct pad_config pcie_wlan_enable_pads[] = {
+	/* GPP_A11:     WLAN_RST_N */
+	PAD_CFG_GPO(GPP_A11, 1, PLTRST),
+	/* GPP_E07:     WIFI_WAKE_N */
+	PAD_CFG_GPI_SCI_LOW(GPP_E07, NONE, DEEP, LEVEL),
+};
+
+static const struct pad_config pcie_wlan_disable_pads[] = {
+	/* GPP_A11:     WLAN_RST_N */
+	PAD_NC(GPP_A11, NONE),
+	/* GPP_E07:     WIFI_WAKE_N */
+	PAD_NC(GPP_E07, NONE),
+};
+
+static const struct pad_config cnvi_enable_pads[] = {
+	/* GPP_B09:     BT_RF_KILL_N */
+	PAD_CFG_GPO(GPP_B09, 1, DEEP),
+	/* GPP_C10:     WIFI_RF_KILL_N */
+	PAD_CFG_GPO(GPP_C10, 1, DEEP),
+	/* GPP_F00:     M.2_CNV_BRI_DT_BT_UART2_RTS_N */
+	PAD_CFG_NF_IOSTANDBY_IGNORE(GPP_F00, NONE, DEEP, NF1),
+	/* GPP_F01:     M.2_CNV_BRI_RSP_BT_UART2_RXD */
+	/* NOTE: IOSSTAGE: 'Ignore' for S0ix */
+	PAD_CFG_NF_IOSTANDBY_IGNORE(GPP_F01, NONE, DEEP, NF1),
+	/* GPP_F02:     M.2_CNV_RGI_DT_BT_UART2_TXD */
+	/* NOTE: IOSSTAGE: 'Ignore' for S0ix */
+	PAD_CFG_NF_IOSTANDBY_IGNORE(GPP_F02, NONE, DEEP, NF1),
+	/* GPP_F03:     M.2_CNV_RGI_RSP_BT_UART2_CTS_N */
+	/* NOTE: IOSSTAGE: 'Ignore' for S0ix */
+	PAD_CFG_NF_IOSTANDBY_IGNORE(GPP_F03, NONE, DEEP, NF1),
+	/* GPP_F04:     CNV_RF_RESET_R_N */
+	/* NOTE: IOSSTAGE: 'Ignore' for S0ix */
+	PAD_CFG_NF_IOSTANDBY_IGNORE(GPP_F04, NONE, DEEP, NF1),
+	/* GPP_F05:     CRF_CLKREQ_R */
+	/* NOTE: IOSSTAGE: 'Ignore' for S0ix */
+	PAD_CFG_NF_IOSTANDBY_IGNORE(GPP_F05, NONE, DEEP, NF3),
+};
+
+static const struct pad_config cnvi_disable_pads[] = {
+	/* GPP_B09:     BT_RF_KILL_N */
+	PAD_NC(GPP_B09, NONE),
+	/* GPP_C10:     WIFI_RF_KILL_N */
+	PAD_NC(GPP_C10, NONE),
+	/* GPP_F00:     M.2_CNV_BRI_DT_BT_UART2_RTS_N */
+	PAD_NC(GPP_F00, NONE),
+	/* GPP_F01:     M.2_CNV_BRI_RSP_BT_UART2_RXD */
+	PAD_NC(GPP_F01, NONE),
+	/* GPP_F02:     M.2_CNV_RGI_DT_BT_UART2_TXD */
+	PAD_NC(GPP_F02, NONE),
+	/* GPP_F03:     M.2_CNV_RGI_RSP_BT_UART2_CTS_N */
+	PAD_NC(GPP_F03, NONE),
+	/* GPP_F04:     CNV_RF_RESET_R_N */
+	PAD_NC(GPP_F04, NONE),
+	/* GPP_F05:     CRF_CLKREQ_R */
+	PAD_NC(GPP_F05, NONE),
+};
+
 void fw_config_configure_pre_mem_gpio(void)
 {
 	if (!fw_config_is_provisioned()) {
@@ -24,5 +81,13 @@ void fw_config_gpio_padbased_override(struct pad_config *padbased_table)
 	if (!fw_config_is_provisioned()) {
 		printk(BIOS_WARNING, "FW_CONFIG is not provisioned, Exiting\n");
 		return;
+	}
+
+	if (fw_config_probe(FW_CONFIG(WIFI_INTERFACE, WIFI_INTERFACE_PCIE_7))) {
+		GPIO_PADBASED_OVERRIDE(padbased_table, pcie_wlan_enable_pads);
+		GPIO_PADBASED_OVERRIDE(padbased_table, cnvi_disable_pads);
+	} else if (fw_config_probe(FW_CONFIG(WIFI_INTERFACE, WIFI_INTERFACE_CNVI_7))) {
+		GPIO_PADBASED_OVERRIDE(padbased_table, cnvi_enable_pads);
+		GPIO_PADBASED_OVERRIDE(padbased_table, pcie_wlan_disable_pads);
 	}
 }

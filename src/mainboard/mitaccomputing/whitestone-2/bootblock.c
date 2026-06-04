7f7fbea011aa8a0e3c13db182302bf68e11b20c2
Elyes Haouas <ehaouas@noos.fr>
2026-06-04 10:08:07 +0200

    mb/mitaccomputing/whitestone-2: Remove unused <device/pci_def.h>
    Change-Id: I9188f0bc88cbd480f150e35b17e69759e14a0445
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93240
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/mitaccomputing/whitestone-2/bootblock.c
+++ src/mainboard/mitaccomputing/whitestone-2/bootblock.c
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootblock_common.h>
-#include <device/pci_def.h>
 #include <intelblocks/lpc_lib.h>
 #include <intelblocks/pcr.h>
 #include <soc/pci_devs.h>

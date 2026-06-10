996f2e8928a0d293d12f1b2472de74aea8311ef0
shfil <shfil@paranoici.org>
2026-06-10 04:14:53 +0200

    util/inteltool: add missing Panther Point-M chipset in GPIO file
    Change-Id: I252fe9c36ceee3eeb5a6ba657f1ea7f011215017
    Signed-off-by: shfil <shfil@paranoici.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93362
    Reviewed-by: Máté Kukri <km@mkukri.xyz>
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Alicja Michalska <ahplka19@gmail.com>

--- util/inteltool/gpio.c
+++ util/inteltool/gpio.c
@@ -944,6 +944,7 @@ int print_gpios(struct pci_dev *sb, int show_all, int show_diffs)
 	case PCI_DEVICE_ID_INTEL_HM76:
 	case PCI_DEVICE_ID_INTEL_HM75:
 	case PCI_DEVICE_ID_INTEL_HM70:
+	case PCI_DEVICE_ID_INTEL_NM70:
 		gpiobase = pci_read_word(sb, 0x48) & 0xfffc;
 		gpio_registers = pch_gpio_registers;
 		size = ARRAY_SIZE(pch_gpio_registers);

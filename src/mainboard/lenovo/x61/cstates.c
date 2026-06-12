0e0c99c92f0243752cca44e877e2beff4c759a0f
Arthur Heymans <arthur@aheymans.xyz>
2026-06-12 19:45:30 +0200

    mb/lenovo/x61: Use C4 on battery
    Provide a battery-specific C-state table for the X61.  Keep the normal
    C1/C2/C3 list on AC power and expose C1/C2/C4 on battery power for
    lower idle power, matching the vendor firmware policy.
    
    TEST: See that /sys/devices/system/cpu/cpu0/cpuidle/state3/desc changes
    depending on power source being the battery or the AC.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Change-Id: I77046cb0dcb271ddc9c59b616c31e7126a6a6964
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93434
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/lenovo/x61/cstates.c
+++ src/mainboard/lenovo/x61/cstates.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <acpi/acpigen.h>
+#include <cpu/intel/speedstep.h>
 
 static const acpi_cstate_t cst_entries[] = {
 	{
@@ -15,8 +16,26 @@ static const acpi_cstate_t cst_entries[] = {
 	},
 	{
 		/* ACPI C3 / CPU C3 */
-		3, 0x37,  250,
-		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 1, 0x20, 0 }
+		3, 0x11,  250,
+		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 3, 0x20, 0 }
+	},
+};
+
+static const acpi_cstate_t bat_cst_entries[] = {
+	{
+		/* ACPI C1 / CPU C1 */
+		1, 0x01, 1000,
+		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 1, 0, 0 }
+	},
+	{
+		/* ACPI C2 / CPU C2 */
+		2, 0x01,  500,
+		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 1, 0x10, 0 }
+	},
+	{
+		/* ACPI C3 / CPU C4 */
+		3, 0x39,  100,
+		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 3, 0x30, 0 }
 	},
 };
 
@@ -24,4 +43,10 @@ int get_cst_entries(const acpi_cstate_t **entries)
 {
 	*entries = cst_entries;
 	return ARRAY_SIZE(cst_entries);
+}
+
+int get_bat_cst_entries(const acpi_cstate_t **entries)
+{
+	*entries = bat_cst_entries;
+	return ARRAY_SIZE(bat_cst_entries);
 }

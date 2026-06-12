57b04aee055c47fe0fd792c69ef771e1c7cb5a56
Arthur Heymans <arthur@aheymans.xyz>
2026-06-12 19:32:22 +0200

    mb/lenovo/x200: Use C6 on battery
    Provide a battery-specific C-state table for the X200.  Keep the normal
    C1/C2/C3 list on AC power and expose C1/C2/C6 on battery power for
    lower idle power.
    
    This restores the behavior from abandoned review CB:22932.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Change-Id: I91204703ac1fcc9e2260de51bf38f63f6a6a6964
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93433
    Reviewed-by: Abdelkader Boudih <coreboot@seuros.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- src/mainboard/lenovo/x200/cstates.c
+++ src/mainboard/lenovo/x200/cstates.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <acpi/acpigen.h>
+#include <cpu/intel/speedstep.h>
 
 static const acpi_cstate_t cst_entries[] = {
 	{
@@ -14,9 +15,27 @@ static const acpi_cstate_t cst_entries[] = {
 		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 1, 0x10, 0 }
 	},
 	{
-		/* acpi C3 / cpu C3 */
+		/* ACPI C3 / CPU C3 */
 		3, 0x37,  250,
 		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 1, 0x20, 0 }
+	},
+};
+
+static const acpi_cstate_t bat_cst_entries[] = {
+	{
+		/* ACPI C1 / CPU C1 */
+		1, 0x01, 1000,
+		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 1, 0, 0 }
+	},
+	{
+		/* ACPI C2 / CPU C2 */
+		2, 0x01,  500,
+		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 1, 0x10, 0 }
+	},
+	{
+		/* ACPI C3 / CPU C6 */
+		3, 0x37,  250,
+		{ ACPI_ADDRESS_SPACE_FIXED, 1, 2, 1, 0x50, 0 }
 	},
 };
 
@@ -24,4 +43,10 @@ int get_cst_entries(const acpi_cstate_t **entries)
 {
 	*entries = cst_entries;
 	return ARRAY_SIZE(cst_entries);
+}
+
+int get_bat_cst_entries(const acpi_cstate_t **entries)
+{
+	*entries = bat_cst_entries;
+	return ARRAY_SIZE(bat_cst_entries);
 }

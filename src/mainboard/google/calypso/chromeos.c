f1a5a521faa55774dcf654832d8c0028cea814c5
Subrata Banik <subratabanik@google.com>
2026-06-18 14:24:07 +0530

    mb/google/calypso: Make GPIO_EN_FP_RAILS available for all FP variants
    Move `GPIO_EN_FP_RAILS` out of the SPI-specific configuration block and
    into the general fingerprint block (`MAINBOARD_HAS_FINGERPRINT`).
    
    This ensures the fingerprint power rails can be controlled regardless
    of whether the fingerprint module communicates via SPI or another
    interface.
    
    Update `setup_chromeos_gpios()` to initialize the rail to 0 for all
    fingerprint-enabled configurations.
    
    BUG=none
    TEST=Builds successfully for calypso.
    
    Change-Id: I0757a4bce24a0dcefcfca0a283d02ca44b92792e
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93571
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/calypso/chromeos.c
+++ src/mainboard/google/calypso/chromeos.c
@@ -15,9 +15,9 @@ void setup_chromeos_gpios(void)
 
 	if (CONFIG(MAINBOARD_HAS_FINGERPRINT)) {
 		gpio_output(GPIO_FP_RST_L, 0);
+		gpio_output(GPIO_EN_FP_RAILS, 0);
 		if (CONFIG(MAINBOARD_HAS_FINGERPRINT_VIA_SPI)) {
 			gpio_output(GPIO_FPMCU_BOOT0, 0);
-			gpio_output(GPIO_EN_FP_RAILS, 0);
 			gpio_input_irq(GPIO_FPMCU_INT, IRQ_TYPE_LEVEL, GPIO_PULL_UP);
 		}
 	}

9afd8a0b09a70eacc2cb94cab50da12e82f10ea6
Jan Philipp Groß <jeangrande@mailbox.org>
2026-06-23 13:55:28 +0200

    mb/asrock/b85m_pro4/hda_verb.c: Drop extraneous codec verbs
    According to vendor specifications, the ASRock B85M Pro4 only features
    an ALC892 chip. This was confirmed by the owner of the board through
    physical inspection, so drop the ALC887 verbs. Also drop the Intel audio
    verbs, which are for 0x80862806 (Ivy Bridge's built-in display audio),
    so they are without effect and can be removed.
    
    The reason why these verbs appeared is most likely the same as that of
    commit 054318251cff ("mb/asus/h61m-a_usb3/hda_verb.c: Drop extraneous
    codec verbs"), i.e. some leftover log files in autoport's logs folder.
    
    Change-Id: I28addcf1c83f9eebda8519324eae3b7b9022dd78
    Signed-off-by: Jan Philipp Groß <jeangrande@mailbox.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93659
    Reviewed-by: Nicholas <nic.c3.14@gmail.com>
    Reviewed-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/mainboard/asrock/b85m_pro4/hda_verb.c
+++ src/mainboard/asrock/b85m_pro4/hda_verb.c
@@ -17,31 +17,6 @@ static const u32 realtek_alc892_verbs[] = {
 	AZALIA_PIN_CFG(0, 0x21, 0x0221102f),
 };
 
-static const u32 realtek_alc887_verbs[] = {
-	AZALIA_SUBVENDOR(2, 0x1458a002),
-	AZALIA_PIN_CFG(2, ALC887_SPDIF_OUT2, 0x411110f0),
-	AZALIA_PIN_CFG(2, ALC887_DMIC_LR, AZALIA_PIN_CFG_NC(0)),
-	AZALIA_PIN_CFG(2, ALC887_FRONT, 0x01014410),
-	AZALIA_PIN_CFG(2, ALC887_SURROUND, AZALIA_PIN_CFG_NC(0)),
-	AZALIA_PIN_CFG(2, ALC887_CENTER_LFE, AZALIA_PIN_CFG_NC(0)),
-	AZALIA_PIN_CFG(2, ALC887_SIDE_SURROUND, AZALIA_PIN_CFG_NC(0)),
-	AZALIA_PIN_CFG(2, ALC887_MIC1, 0x01a19c50),
-	AZALIA_PIN_CFG(2, ALC887_MIC2, 0x02a19c60),
-	AZALIA_PIN_CFG(2, ALC887_LINE1, 0x0181345f),
-	AZALIA_PIN_CFG(2, ALC887_LINE2, 0x02214c20),
-	AZALIA_PIN_CFG(2, ALC887_CD, AZALIA_PIN_CFG_NC(0)),
-	AZALIA_PIN_CFG(2, ALC887_PC_BEEP, 0x4004c601),
-	AZALIA_PIN_CFG(2, ALC887_SPDIF_OUT1, AZALIA_PIN_CFG_NC(0)),
-	AZALIA_PIN_CFG(2, ALC887_SPDIF_IN, 0x41c46060),
-};
-
-static const u32 intel_display_audio_verbs[] = {
-	AZALIA_SUBVENDOR(3, 0x80860101),
-	AZALIA_PIN_CFG(3, 0x05, 0x58560010),
-	AZALIA_PIN_CFG(3, 0x06, 0x18560020),
-	AZALIA_PIN_CFG(3, 0x07, 0x58560030),
-};
-
 const u32 pc_beep_verbs[0] = {};
 
 struct azalia_codec mainboard_azalia_codecs[] = {
@@ -52,22 +27,6 @@ struct azalia_codec mainboard_azalia_codecs[] = {
 		.address      = 0,
 		.verbs        = realtek_alc892_verbs,
 		.verb_count   = ARRAY_SIZE(realtek_alc892_verbs),
-	},
-	{
-		.name         = "Realtek ALC887",
-		.vendor_id    = 0x10ec0887,
-		.subsystem_id = 0x1458a002,
-		.address      = 2,
-		.verbs        = realtek_alc887_verbs,
-		.verb_count   = ARRAY_SIZE(realtek_alc887_verbs),
-	},
-	{
-		.name         = "Intel Display Audio (HDMI/DP)",
-		.vendor_id    = 0x80862806,
-		.subsystem_id = 0x80860101,
-		.address      = 3,
-		.verbs        = intel_display_audio_verbs,
-		.verb_count   = ARRAY_SIZE(intel_display_audio_verbs),
 	},
 	{ /* terminator */ }
 };

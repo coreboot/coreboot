bfd7bb586dbcaf5b94137f960d338ea4f21af0f6
Matt DeVillier <matt.devillier@gmail.com>
2026-06-28 08:45:12 -0500

    mb/lattepanda/mu: Align IT8613E EC config with vendor dump
    Now that the IT8613E EC is configured through the devicetree, correct
    the values to match the vendor firmware (LP-BS-S70NC1R200-SR-B) as
    captured by superiotool, and finish moving EC programming out of the
    bootblock.
    
      - Drop the remaining bogus bootblock EC writes that neither match the
        dump nor do what their values imply: 0xf1 (APC/PME status, write-1-
        to-clear), 0xfb (status/reserved bits), 0xfa (5VSB_CTRL#, already
        off at reset), ite_disable_pme_out() and ite_ac_resume_southbridge()
        (redundant with 0xf4).
      - Move the EC power/PME programming into pnp 2e.4: APC/PME event
        enable (0xf2 = 0x40) and APC/PME control 2 (0xf4 = 0xe0), which
        control the AC-resume behavior.
      - Replace the provisional thermal/fan curves: route CPU PECI to
        TMPIN1 with the vendor temperature offset (0x68), and set the
        CPU_FAN/SYS_FAN automatic curves to the vendor defaults
        (off/start/full = 35/40/100 C, delta 4, start PWM 31%, slope 16).
    
    TEST=Build LattePanda Mu and DFR1142
    
    Change-Id: I00784cc1c3ee026d5ef68c6a57456a6a2809d903
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93738
    Reviewed-by: Alicja Michalska <ahplka19@gmail.com>
    Reviewed-by: KunYi Chen <kunyi.chen@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/lattepanda/mu/bootblock.c
+++ src/mainboard/lattepanda/mu/bootblock.c
@@ -3,7 +3,6 @@
 #include <bootblock_common.h>
 #include <baseboard/variants.h>
 #include <device/pci_ops.h>
-#include <device/pnp_ops.h>
 #include <intelpch/espi.h>
 #include <soc/pci_devs.h>
 #include <superio/ite/common/ite.h>
@@ -11,26 +10,7 @@
 
 #define it8613e_index	0x2e
 
-#define EC_DEV		PNP_DEV(it8613e_index, IT8613E_EC)
 #define UART_DEV	PNP_DEV(it8613e_index, IT8613E_SP1)
-
-/* Environment Controller IO bases (HWM block / PME). */
-#define EC_HWM_BASE	0x0a40
-#define EC_PME_BASE	0x0a30
-
-/* Bring up the Environment Controller's logical-device config */
-static void it8613e_ec_init(void)
-{
-	pnp_enter_conf_state(EC_DEV);
-	pnp_set_logical_device(EC_DEV);
-	pnp_set_iobase(EC_DEV, PNP_IDX_IO0, EC_HWM_BASE);
-	pnp_set_iobase(EC_DEV, PNP_IDX_IO1, EC_PME_BASE);
-	pnp_write_config(EC_DEV, 0xf1, 0xbf);
-	pnp_write_config(EC_DEV, 0xf4, 0xe0);
-	pnp_write_config(EC_DEV, 0xfb, 0x0c);
-	pnp_set_enable(EC_DEV, 1);
-	pnp_exit_conf_state(EC_DEV);
-}
 
 void bootblock_mainboard_early_init(void)
 {
@@ -42,13 +22,6 @@ void bootblock_mainboard_early_init(void)
 
 	early_pads = variant_early_gpio_table(&early_num);
 	gpio_configure_pads(early_pads, early_num);
-
-	it8613e_ec_init();
-
-	/* 5VSB_CTRL# disable */
-	ite_reg_write(EC_DEV, 0xfa, 0);
-	ite_disable_pme_out(EC_DEV);
-	ite_ac_resume_southbridge(EC_DEV);
 
 	ite_enable_serial(UART_DEV, CONFIG_TTYS0_BASE);
 }

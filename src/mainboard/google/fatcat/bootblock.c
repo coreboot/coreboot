0a4ca68864b72f148fb2c589b7a43503c58d8437
Subrata Banik <subratabanik@google.com>
2026-06-30 14:50:46 +0530

    mb/google/fatcat: Prevent boot loops by rejecting ACPI S3 resume
    The fatcat platform does not support ACPI S3 resume. If the system
    attempts an S3 resume, it can get trapped in a boot loop.
    
    Catch the S3 resume state early in `bootblock_mainboard_init`, log the
    unsupported state, prepare the platform for a clean reboot using vboot,
    and trigger a board reset to clear the sleep state.
    
    BUG=b:525008017
    TEST=Builds successfully for google/fatcat.
    
    Change-Id: I736abdbf9e319e48a692eecb40b002e01c88db63
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93804
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/mainboard/google/fatcat/bootblock.c
+++ src/mainboard/google/fatcat/bootblock.c
@@ -2,7 +2,10 @@
 
 #include <baseboard/variants.h>
 #include <bootblock_common.h>
+#include <bootmode.h>
 #include <intelblocks/lpc_lib.h>
+#include <reset.h>
+#include <security/vboot/vboot_common.h>
 
 void bootblock_mainboard_early_init(void)
 {
@@ -15,6 +18,14 @@ void bootblock_mainboard_early_init(void)
 
 void bootblock_mainboard_init(void)
 {
+	if (platform_is_resuming()) {
+		printk(BIOS_EMERG, "ACPI S3 resume is not supported on this platform\n");
+		/* Prepare for reboot to clear the sleep state such that the board
+		   is not stuck in reboot loop. */
+		vboot_platform_prepare_reboot();
+		board_reset();
+	}
+
 	if (CONFIG(EC_GOOGLE_CHROMEEC_LPC_GENERIC_MEMORY_RANGE))
 		lpc_open_mmio_window(CONFIG_EC_GOOGLE_CHROMEEC_LPC_GENERIC_MEMORY_BASE,
 			CONFIG_EC_GOOGLE_CHROMEEC_LPC_GENERIC_MEMORY_SIZE);

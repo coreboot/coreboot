be244d86c2442b25e3eccb0f712a68af3abaf160
Daniel Schaefer <dhs@frame.work>
2026-07-01 17:19:43 +0800

    mb/framework: Signal POST complete to EC
    To make the EC diagnostics happy. The side LEDs on the laptops blink
    error codes if it did not receive this signal after X seconds.
    The purpose is to know if you were stuck somewhere in hardware
    initialization. Run this just before the payload is executed.
    
    Change-Id: I9413c60d5fb88f699b331c93365030f5aa29fbcc
    Signed-off-by: Daniel Schaefer <dhs@frame.work>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/93828
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
    Reviewed-by: Alicja Michalska <ahplka19@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/framework/common/ec.c
+++ src/mainboard/framework/common/ec.c
@@ -1,10 +1,17 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <acpi/acpi.h>
+#include <bootstate.h>
 #include <console/console.h>
 #include <ec/ec.h>
 #include <ec/google/chromeec/ec.h>
 #include <mainboard/framework/common/ec.h>
+
+/*
+ * Signal to the EC that BIOS has finished POST.
+ */
+#define EC_CMD_DIAGNOSIS	0x3E0B
+#define EC_DIAGNOSIS_PORT80_COMPLETE	0xFF
 
 void mainboard_ec_init(void)
 {
@@ -20,3 +27,24 @@ void mainboard_ec_init(void)
 
 	google_chromeec_events_init(&info, acpi_is_wakeup_s3());
 }
+
+static void framework_ec_signal_bios_complete(void *unused)
+{
+	const uint8_t diagnosis_code = EC_DIAGNOSIS_PORT80_COMPLETE;
+	struct chromeec_command cmd = {
+		.cmd_code = EC_CMD_DIAGNOSIS,
+		.cmd_version = 0,
+		.cmd_data_in = &diagnosis_code,
+		.cmd_size_in = sizeof(diagnosis_code),
+		.cmd_data_out = NULL,
+		.cmd_size_out = 0,
+		.cmd_dev_index = 0,
+	};
+
+	if (google_chromeec_command(&cmd))
+		printk(BIOS_ERR, "mainboard: Failed to signal BIOS complete to EC\n");
+	else
+		printk(BIOS_DEBUG, "mainboard: Signalled BIOS complete to EC\n");
+}
+BOOT_STATE_INIT_ENTRY(BS_PAYLOAD_LOAD, BS_ON_EXIT,
+		      framework_ec_signal_bios_complete, NULL);

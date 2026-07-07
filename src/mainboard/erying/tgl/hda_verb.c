7695f1a65b9216e436460ec120b347985787d98f
Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
2026-07-08 08:48:43 +0900

    mb/erying: Replace verb tables with reworked implementation
    No modifications/assumptions about the codecs were made on these boards.
    
    It should be noted that we do not modifiy the verb tables in any case,
    as it would break the regression test script mentioned in the TEST
    section below.
    
    For an overall rationale for this rework, see commit 31fc5b06a6be
    ("device: Introduce reworked azalia verb table").
    
    TEST= All boards passed regression test (CB:88763)
    
    Change-Id: Id8ea5105724f132e0dafb3af8e8e2ca7fb2c5429
    Signed-off-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88668
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
    Reviewed-by: Alicja Michalska <ahplka19@gmail.com>

--- src/mainboard/erying/tgl/hda_verb.c
+++ src/mainboard/erying/tgl/hda_verb.c
@@ -2,11 +2,7 @@
 
 #include <device/azalia_device.h>
 
-const u32 cim_verb_data[] = {
-	/* Realtek ALC897 */
-	0x10ec0897, /* Vendor ID */
-	0x10ec3000, /* Subsystem ID */
-	15, /* Number of entries */
+static const u32 realtek_alc897_verbs[] = {
 	AZALIA_SUBVENDOR(0, 0x10ec3000),
 	AZALIA_PIN_CFG(0, 0x11, 0x40000000),
 	AZALIA_PIN_CFG(0, 0x12, 0x411111f0),
@@ -22,17 +18,34 @@ const u32 cim_verb_data[] = {
 	AZALIA_PIN_CFG(0, 0x1d, 0x4024c601),
 	AZALIA_PIN_CFG(0, 0x1e, 0x411111f0),
 	AZALIA_PIN_CFG(0, 0x1f, 0x411111f0),
+};
 
+static const u32 intel_display_audio_verbs[] = {
 #if !CONFIG(SOC_INTEL_DISABLE_IGD)
-	/* Tigerlake HDMI */
-	0x80862812, /* Vendor ID */
-	0x80860101, /* Subsystem ID */
-	2, /* Number of entries */
 	AZALIA_SUBVENDOR(2, 0x80860101),
 	AZALIA_PIN_CFG(2, 0x04, 0x18560010),
 #endif
 };
 
 const u32 pc_beep_verbs[] = {};
+
+static struct azalia_codec mainboard_azalia_codecs[] = {
+	{
+		.name         = "Realtek ALC897",
+		.vendor_id    = 0x10ec0897,
+		.subsystem_id = 0x10ec3000,
+		.address      = 0,
+		.verbs        = realtek_alc897_verbs,
+		.verb_count   = ARRAY_SIZE(realtek_alc897_verbs),
+	},
+	{
+		.name         = "Intel Display Audio (HDMI/DP)",
+		.vendor_id    = 0x80862812,
+		.subsystem_id = 0x80860101,
+		.address      = 2,
+		.verbs        = intel_display_audio_verbs,
+		.verb_count   = ARRAY_SIZE(intel_display_audio_verbs),
+	},
+};
 
 AZALIA_ARRAY_SIZES;

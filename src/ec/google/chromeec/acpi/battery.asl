coreboot/coreboot

commit 7ad15538e032ba34d572740ef8ac3ebe05458539

ec/google/chromeec: Hide battery and MKBP devices when absent
Use Chrome EC feature bits to hide these ACPI devices when the EC
reports that the hardware is not present.

Return _STA = 0 for the ACPI battery device when neither BATT nor SBAT
is reported by the EC. Return _STA = 0 for the Chrome EC MKBP keyboard/
buttons device (GOOG0007) when the EC does not report KEYB. Ensure
DFUD is valid (0) when using these feature flag checks; fall back to
present (0xf) if DFUD is not supported (1).

These changes result in the battery and MKBP devices reporting as not
present on Chromeboxes like fizz, puff, and brask:

PNP0C0A:00 battery path \_SB_.PCI0.LPCB.EC0_.BAT0 reports status=0.
GOOG0007:00 path \_SB_.PCI0.LPCB.EC0_.CREC.CKSC reports status=0.
cros_ec_buttons is no longer present in /sys/class/input.

Change-Id: I84b4b734d139ab09d82394b2aa7ee153bc8de7e7
Co-authored-by: jackadam1981 <jackadam@sina.com>
Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/93260
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Martin L Roth <gaumless@gmail.com>

src/ec/google/chromeec/acpi/battery.asl
@@ -40,6 +40,10 @@ Method (BTSW, 1)
 //   Arg0 = battery index
 Method (BSTA, 1, Serialized)
 {
+	If (!DFUD &&!(BATT || SBAT)) {
+		Return (0)
+	}
+
 	If (Acquire (^BATM, 1000)) {
 		Return (0)
 	}

src/ec/google/chromeec/acpi/cros_ec.asl
@@ -62,6 +62,15 @@ CONFIG(EC_GOOGLE_CHROMEEC_LPC_GENERIC_MEMORY_RANGE)
 		Name (_HID, "GOOG0007")
 		Name (_UID, 1)
 		Name (_DDN, "EC MKBP Device")
+
+		Method (_STA, 0)
+		{
+			If (DFUD || KEYB) {
+				Return (0xF)
+			}
+
+			Return (0)
+		}
 	}
 #endif
 

commit 84af633e94fd9a14aab7c8a8e79807e33a720600

ec/google/chromeec: Fix hiding ACPI battery on legacy ECs
Commit 7ad15538e03 ("ec/google/chromeec: Hide battery and MKBP devices
when absent") caused BAT0._STA to return 0 on platforms whose EC
firmware does not populate the device feature flags (BATT/SBAT), even
when a battery is present.

Fix this by also verifying that BTEX (battery physically present) is
non-zero when determining that no battery is present.

TEST=build/boot google/link and wolf, verify battery status reported
properly in OS (Linux and Win11).

Change-Id: Ie85914348b8242b726d444d4286e0ff5954e6a8f
Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
Reviewed-on: https://review.coreboot.org/c/coreboot/+/93940
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>

src/ec/google/chromeec/acpi/battery.asl
@@ -40,7 +40,7 @@ Method (BTSW, 1)
 //   Arg0 = battery index
 Method (BSTA, 1, Serialized)
 {
-	If (!DFUD &&!(BATT || SBAT)) {
+	If (!DFUD && !(BATT || SBAT) && !BTEX) {
 		Return (0)
 	}
 
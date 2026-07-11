b06528509f7c8eea313513dd6b4c1ff422d82bc1
Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
2026-07-11 11:00:58 +0900

    mb/packardbell: Replace verb tables with reworked implementation
    No modifications/assumptions about the codecs were made on these boards.
    
    It should be noted that we do not modifiy the verb tables in any case,
    as it would break the regression test script mentioned in the TEST
    section below.
    
    For an overall rationale for this rework, see commit 31fc5b06a6be
    ("device: Introduce reworked azalia verb table").
    
    TEST= All boards passed regression test (CB:88763)
    
    Change-Id: I36fb6e03c5a344a7acb97106d4681b1c4e535aa8
    Signed-off-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88682
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/packardbell/ms2290/hda_verb.c
+++ src/mainboard/packardbell/ms2290/hda_verb.c
@@ -2,20 +2,16 @@
 
 #include <device/azalia_device.h>
 
-const u32 cim_verb_data[] = {
-	0x10ec0272,	/* Codec Vendor / Device ID: Realtek ALC272X */
-	0x10250379,	/* Subsystem ID  */
-	6,		/* Number of 4 dword sets */
+static const u32 realtek_alc272x_verbs[] = {
 	AZALIA_SUBVENDOR(0, 0x10250379),
 	AZALIA_PIN_CFG(0, 0x14, 0x99130110),
 	AZALIA_PIN_CFG(0, 0x18, 0x03a11830),
 	AZALIA_PIN_CFG(0, 0x19, 0x99a30920),
 	AZALIA_PIN_CFG(0, 0x1d, 0x4017992d),
 	AZALIA_PIN_CFG(0, 0x21, 0x0321101f),
+};
 
-	0x80862804,	/* Codec Vendor / Device ID: Intel Ibexpeak HDMI */
-	0x80860101,	/* Subsystem ID  */
-	4,		/* Number of 4 dword sets */
+static const u32 intel_display_audio_verbs[] = {
 	AZALIA_SUBVENDOR(3, 0x80860101),
 	AZALIA_PIN_CFG(3, 0x04, 0x18560010),
 	AZALIA_PIN_CFG(3, 0x05, 0x58560020),
@@ -23,5 +19,24 @@ const u32 cim_verb_data[] = {
 };
 
 const u32 pc_beep_verbs[0] = {};
+
+static struct azalia_codec mainboard_azalia_codecs[] = {
+	{
+		.name         = "Realtek ALC272X",
+		.vendor_id    = 0x10ec0272,
+		.subsystem_id = 0x10250379,
+		.address      = 0,
+		.verbs        = realtek_alc272x_verbs,
+		.verb_count   = ARRAY_SIZE(realtek_alc272x_verbs),
+	},
+	{
+		.name         = "Intel Display Audio (HDMI/DP)",
+		.vendor_id    = 0x80862804,
+		.subsystem_id = 0x80860101,
+		.address      = 3,
+		.verbs        = intel_display_audio_verbs,
+		.verb_count   = ARRAY_SIZE(intel_display_audio_verbs),
+	},
+};
 
 AZALIA_ARRAY_SIZES;

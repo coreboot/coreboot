92587b97bd2aaa6cd8feeb74b592f22b17dce662
Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
2026-07-11 11:19:02 +0900

    mb/protectli: Replace verb tables with reworked implementation
    Some boards did not provide the chip name for the audio codecs in the
    comments, and therefore we made the following assumptions based off the
    vendor ID:
    
      vault_cml:
        - 0x10ec0897 -> Realtek ALC897
    
    It should be noted that some factors (e.g. revision) can cause the chip
    name to differ even with the same vendor ID. This is especially common
    for Realtek audio codecs.
    
    It should be noted that we do not modifiy the verb tables in any case,
    as it would break the regression test script mentioned in the TEST
    section below.
    
    For an overall rationale for this rework, see commit 31fc5b06a6be
    ("device: Introduce reworked azalia verb table").
    
    TEST= All boards passed regression test (CB:88763)
    
    Change-Id: I000c0a6cac222060666fc8c683e143898c2e0dbb
    Signed-off-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88685
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/protectli/vault_adl_p/hda_verb.c
+++ src/mainboard/protectli/vault_adl_p/hda_verb.c
@@ -2,11 +2,7 @@
 
 #include <device/azalia_device.h>
 
-const u32 cim_verb_data[] = {
-	/* Alderlake-P HDMI */
-	0x8086281c, /* Vendor ID */
-	0x80860101, /* Subsystem ID */
-	7, /* Number of entries */
+static const u32 intel_display_audio_verbs[] = {
 	AZALIA_SUBVENDOR(2, 0x80860101),
 	0x20278111,
 	0x20278111,
@@ -23,5 +19,16 @@ const u32 cim_verb_data[] = {
 };
 
 const u32 pc_beep_verbs[] = {};
+
+static struct azalia_codec mainboard_azalia_codecs[] = {
+	{
+		.name         = "Intel Display Audio (HDMI/DP)",
+		.vendor_id    = 0x8086281c,
+		.subsystem_id = 0x80860101,
+		.address      = 2,
+		.verbs        = intel_display_audio_verbs,
+		.verb_count   = ARRAY_SIZE(intel_display_audio_verbs),
+	},
+};
 
 AZALIA_ARRAY_SIZES;

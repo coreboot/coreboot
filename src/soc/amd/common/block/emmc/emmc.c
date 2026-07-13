f40a7b2c7dab3efccb2a0766b873e5bae0c5342b
Matt DeVillier <matt.devillier@gmail.com>
2026-07-13 18:32:36 -0500

    soc/amd/common/block/emmc: Publish SDHCI non-PCI info via LB_TAG
    Advertise the fixed-MMIO eMMC controller (Picasso/Cezanne) via
    lb_add_sdhci_nonpci() so UEFI payloads can register it as a
    non-discoverable SDHCI device without hardcoding the BAR.
    
    TEST=build/boot google/vilboz with edk2 payload; eMMC enumerates
    
    Change-Id: I4f450ce2827d14ad086818c00536237d90ec4643
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/94056
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/amd/common/block/emmc/emmc.c
+++ src/soc/amd/common/block/emmc/emmc.c
@@ -1,19 +1,41 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <acpi/acpi.h>
 #include <acpi/acpigen.h>
 #include <amdblocks/aoac.h>
+#include <boot/coreboot_tables.h>
+#include <commonlib/sdhci_nonpci_info.h>
 #include <device/device.h>
 #include <soc/aoac_defs.h>
 
+#define EMMC_SDHCI_MMIO_SIZE	(4 * KiB)
+
+static void emmc_publish_sdhci_nonpci(uint32_t bar0)
+{
+	if (!bar0)
+		return;
+
+	lb_add_sdhci_nonpci(bar0, EMMC_SDHCI_MMIO_SIZE, SDHCI_NONPCI_SLOT_EMMC,
+			    SDHCI_NONPCI_FLAG_EMBEDDED);
+}
+
 static void emmc_read_resources(struct device *dev)
 {
-	mmio_range(dev, 0, dev->path.mmio.addr, 4 * KiB);
+	mmio_range(dev, 0, dev->path.mmio.addr, EMMC_SDHCI_MMIO_SIZE);
 }
 
 static void emmc_enable(struct device *dev)
 {
 	if (!dev->enabled && !CONFIG(SOC_AMD_COMMON_BLOCK_EMMC_SKIP_POWEROFF))
 		power_off_aoac_device(FCH_AOAC_DEV_EMMC);
+}
+
+static void emmc_init(struct device *dev)
+{
+	if (!dev->enabled || acpi_is_wakeup_s3())
+		return;
+
+	emmc_publish_sdhci_nonpci(dev->path.mmio.addr);
 }
 
 static const char *emmc_acpi_name(const struct device *dev)
@@ -33,6 +55,7 @@ struct device_operations amd_emmc_mmio_ops = {
 	.set_resources = noop_set_resources,
 	.scan_bus = scan_static_bus,
 	.enable = emmc_enable,
+	.init = emmc_init,
 	.acpi_name = emmc_acpi_name,
 	.acpi_fill_ssdt = emmc_acpi_fill_ssdt,
 };

0a4ed87021b3ee6b32c15d2f4bfc97af3b1eac2b
Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
2026-07-17 08:50:42 +0900

    mb/portwell: Replace verb tables with reworked implementation
    No modifications/assumptions about the codecs were made on these boards.
    
    It should be noted that we do not modifiy the verb tables in any case,
    as it would break the regression test script mentioned in the TEST
    section below.
    
    For an overall rationale for this rework, see commit 31fc5b06a6be
    ("device: Introduce reworked azalia verb table").
    
    TEST= All boards passed regression test (CB:88763)
    
    Change-Id: Ic1cabb0e3e30820d9b56a37f6e1a52d4c5c44195
    Signed-off-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/88683
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Frans Hendriks <fhendriks@eltan.com>

--- src/mainboard/portwell/m107/hda_verb.c
+++ src/mainboard/portwell/m107/hda_verb.c
@@ -2,8 +2,8 @@
 
 #include <device/azalia_device.h>
 
-const u32 cim_verb_data[0] = {};
-
 const u32 pc_beep_verbs[0] = {};
+
+static struct azalia_codec mainboard_azalia_codecs[] = {};
 
 AZALIA_ARRAY_SIZES;

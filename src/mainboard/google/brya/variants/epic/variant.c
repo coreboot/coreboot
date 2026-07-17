cbcc8395cf96e3447a328e44973c27446b6dfe99
Yunlong Jia <yunlong.jia@ecs.corp-partner.google.com>
2026-07-17 09:22:01 +0000

    mb/google/nissa/var/epic: Add DB field option for non-TypeC with LTE
    Previous LTE SKUs were all limited to Type-C daughter boards. To support the requested non-Type-C LTE SKU, so we defined the DB_A_LTE option for Bit 4 in the fw_config DB field.
    
    BUG=b:515635860
    TEST=emerge-nissa coreboot and run in DUT
    
    Signed-off-by: Yunlong Jia <yunlong.jia@ecs.corp-partner.google.com>
    Change-Id: I8b32af8d7e42d43cbb98a6e66af8be9fe733916d
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/94161
    Reviewed-by: Eric Lai <ericllai@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/google/brya/variants/epic/variant.c
+++ src/mainboard/google/brya/variants/epic/variant.c
@@ -8,7 +8,8 @@
 void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
 {
 	if (!(fw_config_probe(FW_CONFIG(DB_USB, DB_C_A_LTE)) ||
-		fw_config_probe(FW_CONFIG(DB_USB, DB_A_HDMI_LTE)))) {
+		fw_config_probe(FW_CONFIG(DB_USB, DB_A_HDMI_LTE)) ||
+		fw_config_probe(FW_CONFIG(DB_USB, DB_A_LTE)))) {
 		printk(BIOS_INFO, "Disable usb2_port5 and usb3_port3 of WWAN.\n");
 
 		config->usb2_ports[4] = (struct usb2_port_config) USB2_PORT_EMPTY;
@@ -21,7 +22,8 @@ void variant_update_soc_chip_config(struct soc_intel_alderlake_config *config)
 		config->usb2_ports[6] = (struct usb2_port_config) USB2_PORT_EMPTY;
 	}
 
-	if (fw_config_probe(FW_CONFIG(DB_USB, DB_A)) || fw_config_probe(FW_CONFIG(DB_USB, DB_A_HDMI_LTE))) {
+	if (fw_config_probe(FW_CONFIG(DB_USB, DB_A)) || fw_config_probe(FW_CONFIG(DB_USB, DB_A_HDMI_LTE)) ||
+		fw_config_probe(FW_CONFIG(DB_USB, DB_A_LTE))) {
 		printk(BIOS_INFO, "Disable typec aux_bias_pads in the SOC.\n");
 		config->typec_aux_bias_pads[1].pad_auxp_dc = 0x00;
 		config->typec_aux_bias_pads[1].pad_auxn_dc = 0x00;

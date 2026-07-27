6b2a8a8324ae4375bf2834aa13c2a70c655b6134
Werner Zeh <werner.zeh@siemens.com>
2026-07-27 08:24:42 +0200

    mb/siemens/fa_asl/ramstage.c: Remove board dependent Type-C adjustments
    Since this is just one board, the required Type-C configuration can be
    set in devicetree with no need to override it based on a board
    variant. Remove the additional code as it is not needed. Follow-up
    patches will take care of Type-C configuration in devicetree.
    
    Change-Id: I0b814b35d29b13a43db472d3c2f5410cee3bfa9f
    Signed-off-by: Werner Zeh <werner.zeh@siemens.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/94430
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/siemens/fa_asl/ramstage.c
+++ src/mainboard/siemens/fa_asl/ramstage.c
@@ -11,7 +11,6 @@
 #include <static.h>
 #include <string.h>
 #include <drivers/intel/dptf/chip.h>
-#include "board_id.h"
 #include <intelblocks/power_limit.h>
 
 const struct cpu_power_limits limits[] = {
@@ -63,41 +62,7 @@ void variant_update_power_limits(void)
 	}
 }
 
-static const struct typec_aux_bias_pads pad_config = { GPP_E23, GPP_E22 };
-
-static const struct board_id_iom_port_config {
-	int board_id;
-	enum typec_port_index port;
-} port_config[] = {
-	{ ADL_P_LP4_1,	TYPE_C_PORT_2 },
-	{ ADL_P_LP4_2,	TYPE_C_PORT_2 },
-	{ ADL_P_DDR4_1,	TYPE_C_PORT_2 },
-	{ ADL_P_DDR4_2,	TYPE_C_PORT_2 },
-	{ ADL_P_LP5_1,	TYPE_C_PORT_2 },
-	{ ADL_P_LP5_2,	TYPE_C_PORT_2 },
-	{ ADL_M_LP4,	TYPE_C_PORT_1 },
-	{ ADL_M_LP5,	TYPE_C_PORT_0 },
-};
-
-static void variant_update_typec_init_config(void)
-{
-	/* Skip filling aux bias gpio pads for Windows SKUs */
-	if (!(CONFIG(BOARD_INTEL_ADLRVP_P_EXT_EC) || CONFIG(BOARD_INTEL_ADLRVP_RPL_EXT_EC)))
-		return;
-
-	config_t *config = config_of_soc();
-	int board_id = get_board_id();
-	for (int i = 0; i < ARRAY_SIZE(port_config); i++) {
-		if (board_id != port_config[i].board_id)
-			continue;
-
-		memcpy(&config->typec_aux_bias_pads[port_config[i].port], &pad_config,
-			sizeof(pad_config));
-	}
-}
-
 void variant_devtree_update(void)
 {
 	variant_update_power_limits();
-	variant_update_typec_init_config();
 }

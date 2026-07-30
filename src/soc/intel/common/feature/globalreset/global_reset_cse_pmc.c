8733774fa44f3b0ecdf52c33a0bc5c19f67237fd
Bhargav Rathod <bhargav.rathod1905@gmail.com>
2026-07-30 18:42:03 +0000

    soc/intel/common: Fall back if CSE global reset does not occur
    CSE acknowledging a global reset request only indicates that the request
    was accepted. It does not guarantee that the platform reset actually
    takes place.
    
    On Lenovo m920q, CSE reports a successful global reset request, but the
    system never resets after coreboot 25.06 (Bug #655). Wait briefly for
    the reset to occur, then fall back to the PMC/CF9 reset path if the
    platform is still running.
    
    BUG=https://ticket.coreboot.org/issues/655
    
    Change-Id: I5202c66b01910ec538037691f2a21b43c0042d10
    Signed-off-by: Bhargav Rathod <bhargav.rathod1905@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/94486
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/soc/intel/common/feature/globalreset/global_reset_cse_pmc.c
+++ src/soc/intel/common/feature/globalreset/global_reset_cse_pmc.c
@@ -1,15 +1,27 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <cf9_reset.h>
+#include <console/console.h>
+#include <delay.h>
 #include <intelblocks/cse.h>
 #include <intelblocks/pmclib.h>
 #include <soc/intel/common/reset.h>
 
+/* Time to give CSE to actually perform the reset once it acked the request. */
+#define CSE_GLOBAL_RESET_TIMEOUT_MS 1000
+
 void do_global_reset(void)
 {
 	/* Ask CSE to do the global reset */
-	if (cse_request_global_reset() == CSE_TX_RX_SUCCESS)
-		return;
+	if (cse_request_global_reset() == CSE_TX_RX_SUCCESS) {
+		/*
+		 * CSE acking the request only means it accepted the message; it does
+		 * not guarantee the platform reset it triggers actually happens (see
+		 * coreboot Bug #655). Give CSE a chance to reset the system and fall
+		 * back to the PMC/CF9 reset below if it doesn't come through.
+		 */
+		mdelay(CSE_GLOBAL_RESET_TIMEOUT_MS);
+	}
 
 	/* global reset if CSE fail to reset */
 	pmc_global_reset_enable(1);

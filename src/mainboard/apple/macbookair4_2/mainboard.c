64351260b31ae7c6d80d2dec7f2ae77b0cc22f08
Patrick Rudolph <patrick.rudolph@9elements.com>
2026-08-30 08:30:49 +0200

    mb/*/devicetree.cb: Use spi_uvscc setting
    Use devicetree settings and drop duplicated mainboard code.
    
    Change-Id: I1d644c84f883785cb421f87f247bad9aa51c15b9
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/95158
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-by: Walter Sonius <walterav1984@gmail.com>

--- src/mainboard/apple/macbookair4_2/mainboard.c
+++ src/mainboard/apple/macbookair4_2/mainboard.c
@@ -5,16 +5,8 @@
 #include <southbridge/intel/bd82x6x/pch.h>
 #include <ec/acpi/ec.h>
 
-static void mainboard_init(struct device *dev)
-{
-	RCBA32(0x38c8) = 0x00002005;
-	RCBA32(0x38c4) = 0x00800000;
-}
-
 static void mainboard_enable(struct device *dev)
 {
-	dev->ops->init = mainboard_init;
-
 	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_EDP, GMA_INT15_PANEL_FIT_DEFAULT, GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
 }
 
--- src/mainboard/sapphire/pureplatinumh61/mainboard.c
+++ src/mainboard/sapphire/pureplatinumh61/mainboard.c
@@ -4,16 +4,8 @@
 #include <drivers/intel/gma/int15.h>
 #include <southbridge/intel/bd82x6x/pch.h>
 
-static void mainboard_init(struct device *dev)
-{
-	RCBA32(0x38c8) = 0x00002009;
-	RCBA32(0x38c4) = 0x00802009;
-}
-
 static void mainboard_enable(struct device *dev)
 {
-	dev->ops->init = mainboard_init;
-
 	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_INT_LVDS,
 					GMA_INT15_PANEL_FIT_DEFAULT,
 					GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);

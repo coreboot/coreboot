603c189906ae33cec66a6da1cbce164da337b98a
Matt DeVillier <matt.devillier@gmail.com>
2026-09-10 11:21:49 -0500

    mb/google/poppy/var/atlas: Replace static camera ASL with MIPI DT
    Migrate atlas camera ACPI from hand-written ASL to runtime-generated
    ACPI via the Intel MIPI camera driver. Since the atlas camera ASL
    (discrete DSC0, IMX208, NVM0, CIO2 graph, local power) is fully
    expressible in the MIPI driver for both ChromeOS and Windows/Linux, drop
    VARIANT_HAS_CAMERA_ACPI and use the generator for both types.
    
    Kconfig:
    - Select DRIVERS_GFX_GENERIC, DRIVERS_INTEL_MIPI_CAMERA, and
      DRIVERS_INTEL_MIPI_CAMERA_HAS_PMIC
    - Drop VARIANT_HAS_CAMERA_ACPI
    - Show the CFR IPU camera option when the MIPI driver is enabled
    
    Devicetree:
    - Add GFX generic LCD0 panel and SKC0 IPU entries
    - Nest CIO2 port/_DSD under device ref cio (uses skl cio_ops)
    - Set scope_into_parent so SSDT Scopes DSDT Device (CIO2)
    - Add discrete INT3472 DSC0 for power/clock/reset GPIOs
    - Add IMX208 CAM0 with _DEP on DSC0 and a local PRIC PowerResource.
    - Add NVM0 with _DEP and _PR0 on CAM0.PRIC
    
    Delete the static atlas camera ASL under variants/atlas/include.
    
    TEST=Build/boot Windows/Linux on google/atlas; verify CAM0/CIO2 in ACPI
    and sensor enumeration on Linux.
    
    Change-Id: I3779efd3fdc1d7c8da76042959b04d036680c634
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/95251
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Martin L Roth <gaumless@gmail.com>

--- src/mainboard/google/poppy/cfr.c
+++ src/mainboard/google/poppy/cfr.c
@@ -10,7 +10,7 @@ static const struct sm_object ipu_camera = SM_DECLARE_BOOL({
 	.ui_name	= "IPU Camera",
 	.ui_helptext	= "Enable or disable integrated camera devices",
 	.default_value	= true,
-	#if !CONFIG(VARIANT_HAS_CAMERA_ACPI)
+	#if !CONFIG(DRIVERS_INTEL_MIPI_CAMERA) && !CONFIG(VARIANT_HAS_CAMERA_ACPI)
 	.flags		= CFR_OPTFLAG_SUPPRESS,
 	#endif
 });

8f806ac09420767bb92292f1bceb177bca3ce940
Elyes Haouas <ehaouas@noos.fr>
2026-09-13 09:24:01 +0200

    include/stdint.h: Avoid integer overflow in INT*_MIN macros
    The hexadecimal values used here are outside the range of the signed
    types. Using -INT*_MAX - 1 avoids this issue and gives the correct
    minimum value.
    
    Change-Id: Ia412b49f28c1a7ba5c586d9f1cdc4df6c3b25f63
    Signed-off-by: Elyes Haouas <ehaouas@noos.fr>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/95435
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- src/include/stdint.h
+++ src/include/stdint.h
@@ -54,24 +54,24 @@ typedef int64_t  s64;
 typedef uint64_t u64;
 
 /* Limits of integer types */
-#define INT8_MIN   ((int8_t)0x80)
 #define INT8_MAX   ((int8_t)0x7F)
+#define INT8_MIN   (-INT8_MAX - 1)
 #define UINT8_MAX  ((uint8_t)0xFF)
 
-#define INT16_MIN  ((int16_t)0x8000)
 #define INT16_MAX  ((int16_t)0x7FFF)
+#define INT16_MIN  (-INT16_MAX - 1)
 #define UINT16_MAX ((uint16_t)0xFFFF)
 
-#define INT32_MIN  ((int32_t)0x80000000)
 #define INT32_MAX  ((int32_t)0x7FFFFFFF)
+#define INT32_MIN  (-INT32_MAX - 1)
 #define UINT32_MAX ((uint32_t)0xFFFFFFFF)
 
-#define INT64_MIN  ((int64_t)0x8000000000000000)
 #define INT64_MAX  ((int64_t)0x7FFFFFFFFFFFFFFF)
+#define INT64_MIN  (-INT64_MAX - 1)
 #define UINT64_MAX ((uint64_t)0xFFFFFFFFFFFFFFFF)
 
-#define INTMAX_MIN  INT64_MIN
 #define INTMAX_MAX  INT64_MAX
+#define INTMAX_MIN  INT64_MIN
 #define UINTMAX_MAX UINT64_MAX
 
 #endif /* STDINT_H */
